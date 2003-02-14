78f674e3df3c6781c2e2aabdbb5ac291bcda79b3
João Luz <joao.luz@ist.utl.pt>
2003-02-14 21:28:41 +0000

    DAO to EXECUTION_YEAR table

---
+++ src/ServidorPersistente/IPersistentExecutionYear.java
@@ -0,0 +1,42 @@
+package ServidorPersistente;
+
+import java.util.ArrayList;
+
+import Dominio.IExecutionYear;
+
+/**
+ * Created on 11/Fev/2003
+ * @author João Mota
+ * Package ServidorPersistente
+ * 
+ */
+public interface IPersistentExecutionYear extends IPersistentObject {
+	/**
+	 * 
+	 * @param year
+	 * @return IExecutionYear
+	 */
+	public IExecutionYear readExecutionYearByName(String year) throws ExcepcaoPersistencia;
+	/**
+	 * 
+	 * @return ArrayList
+	 */
+	public ArrayList readAllExecutionYear()throws ExcepcaoPersistencia;
+	/**
+	 * 
+	 * @param executionYear
+	 * @return boolean
+	 */
+	public boolean writeExecutionYear(IExecutionYear executionYear);
+	/**
+	 * 
+	 * @param executionYear
+	 * @return boolean
+	 */
+	public boolean delete(IExecutionYear executionYear);
+	/**
+	 * 
+	 * @return boolean
+	 */
+	public boolean deleteAll();
+}
