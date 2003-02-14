238136b2c00eef1714dd57952c11550d09e40e1b
João Luz <joao.luz@ist.utl.pt>
2003-02-14 21:26:54 +0000

    InfoExecutionYear added.

---
+++ src/DataBeans/InfoExecutionYear.java
@@ -0,0 +1,29 @@
+package DataBeans;
+
+
+/**
+ * @author Nuno & Joana
+ */
+public class InfoExecutionYear {
+	private String year;
+	
+	public InfoExecutionYear() {
+	}
+
+	/**
+	 * Returns the year.
+	 * @return String
+	 */
+	public String getYear() {
+		return year;
+	}
+
+	/**
+	 * Sets the year.
+	 * @param year The year to set
+	 */
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+}
