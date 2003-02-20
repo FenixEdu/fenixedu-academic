101b9d77926f38db5db25d1ac3350a7c0d68911c
João Luz <joao.luz@ist.utl.pt>
2003-02-20 10:29:06 +0000

    *** empty log message ***

--- src/Dominio/TurnoAula.java
+++ src/Dominio/TurnoAula.java
@@ -77,35 +77,7 @@ public class TurnoAula implements ITurnoAula {
     boolean resultado = false;
     if (obj instanceof ITurnoAula) {
       ITurnoAula turnoAula = (ITurnoAula)obj;
-      resultado = getTurno().getNome().equals(turnoAula.getTurno().getNome()) &&
-                  getTurno().getTipo().equals(turnoAula.getTurno().getTipo()) &&
-                  getTurno().getLotacao().equals(turnoAula.getTurno().getLotacao()) &&
-                  getTurno().getDisciplinaExecucao().getNome().equals(turnoAula.getTurno().getDisciplinaExecucao().getNome()) &&
-                  getTurno().getDisciplinaExecucao().getSigla().equals(turnoAula.getTurno().getDisciplinaExecucao().getSigla()) &&
-                  getTurno().getDisciplinaExecucao().getPrograma().equals(turnoAula.getTurno().getDisciplinaExecucao().getPrograma()) &&
-                  getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getAnoLectivo().equals(turnoAula.getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getAnoLectivo()) &&
-                  getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getNome().equals(turnoAula.getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getNome()) &&
-                  getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getSigla().equals(turnoAula.getTurno().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getSigla()) &&
-                  getAula().getDiaSemana().equals(turnoAula.getAula().getDiaSemana()) &&
-                  (getAula().getInicio().get(Calendar.HOUR_OF_DAY) == turnoAula.getAula().getInicio().get(Calendar.HOUR_OF_DAY)) &&
-                  (getAula().getInicio().get(Calendar.MINUTE) == turnoAula.getAula().getInicio().get(Calendar.MINUTE)) &&
-                  (getAula().getInicio().get(Calendar.SECOND) == turnoAula.getAula().getInicio().get(Calendar.SECOND)) &&
-                  (getAula().getFim().get(Calendar.HOUR_OF_DAY) == turnoAula.getAula().getFim().get(Calendar.HOUR_OF_DAY)) &&
-                  (getAula().getFim().get(Calendar.MINUTE) == turnoAula.getAula().getFim().get(Calendar.MINUTE)) &&
-                  (getAula().getFim().get(Calendar.SECOND) == turnoAula.getAula().getFim().get(Calendar.SECOND)) &&
-                  getAula().getTipo().equals(turnoAula.getAula().getTipo()) &&
-                  getAula().getSala().getNome().equals(turnoAula.getAula().getSala().getNome()) &&
-                  getAula().getSala().getEdificio().equals(turnoAula.getAula().getSala().getEdificio()) &&
-                  getAula().getSala().getPiso().equals(turnoAula.getAula().getSala().getPiso()) &&
-                  getAula().getSala().getTipo().equals(turnoAula.getAula().getSala().getTipo()) &&
-                  getAula().getSala().getCapacidadeNormal().equals(turnoAula.getAula().getSala().getCapacidadeNormal()) &&
-                  getAula().getSala().getCapacidadeExame().equals(turnoAula.getAula().getSala().getCapacidadeExame()) &&
-                  getAula().getDisciplinaExecucao().getNome().equals(turnoAula.getAula().getDisciplinaExecucao().getNome()) &&
-                  getAula().getDisciplinaExecucao().getSigla().equals(turnoAula.getAula().getDisciplinaExecucao().getSigla()) &&
-                  getAula().getDisciplinaExecucao().getPrograma().equals(turnoAula.getAula().getDisciplinaExecucao().getPrograma()) &&
-                  getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getAnoLectivo().equals(turnoAula.getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getAnoLectivo()) &&
-                  getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getNome().equals(turnoAula.getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getNome()) &&
-                  getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getSigla().equals(turnoAula.getAula().getDisciplinaExecucao().getLicenciaturaExecucao().getCurso().getSigla());
+      resultado = getTurno().equals(turnoAula.getTurno()) && getAula().equals(turnoAula.getAula());
     }
     return resultado;
   }
--- src_tests/ServidorPersistente/OJB/TurnoAulaOJBTest.java
+++ src_tests/ServidorPersistente/OJB/TurnoAulaOJBTest.java
@@ -23,229 +23,250 @@ import ServidorPersistente.*;
 import Dominio.*;
 
 public class TurnoAulaOJBTest extends TestCaseOJB {
-    public TurnoAulaOJBTest(java.lang.String testName) {
-    super(testName);
-  }
-    
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-    
-  public static Test suite() {
-    TestSuite suite = new TestSuite(TurnoAulaOJBTest.class);
 
-    return suite;
-  }
-    
-  protected void setUp() {
-    super.setUp();
-  }
-    
-  protected void tearDown() {
-    super.tearDown();
-  }
-    
-  /** Test of readByTurnoAndAula method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testreadByTurnoAndAula() {
-    ITurnoAula turnoAula = null;
-    // read existing TurnoAula
-    try {
-      _suportePersistente.iniciarTransaccao();
-      turnoAula = _turnoAulaPersistente.readByTurnoAndAula(_turno3, _aula1);
-      _suportePersistente.confirmarTransaccao();
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testReadByTurnoAndAula:fail read existing turnoAula");
-    }
-    assertEquals("testReadByTurnoAndAula:read existing TurnoAula",turnoAula,_turnoAula1);
-      
-    // read unexisting TurnoAula
-    try {
-      _suportePersistente.iniciarTransaccao();
-      turnoAula = _turnoAulaPersistente.readByTurnoAndAula(_turnoInexistente, _aula1);
-      assertNull(turnoAula);
-      _suportePersistente.confirmarTransaccao();
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testReadByTurnoAndAula:fail read unexisting TurnoAula");
-    }
-  }
+	
+	public TurnoAulaOJBTest(java.lang.String testName) {
+		super(testName);
+	}
 
-  // write new existing TurnoAula
-  public void testCreateExistingTurnoAula() { 
-    ITurnoAula turnoAula = new TurnoAula(_turno3, _aula1);
-    try {
-      _suportePersistente.iniciarTransaccao();
-      _turnoAulaPersistente.lockWrite(turnoAula);
-      _suportePersistente.confirmarTransaccao();
-      fail("testCreateExistingTurnoAula");
-    } catch (ExcepcaoPersistencia ex) {
-      //all is ok
-    }
-  }
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
 
-  // write new non-existing TurnoAula
-  public void testCreateNonExistingTurnoAula() {
-    ITurnoAula turnoAula = null;
-    try {
-    	_suportePersistente.iniciarTransaccao();
-    	IAula aula = _aulaPersistente.readByDiaSemanaAndInicioAndFimAndSala(_diaSemana1, _inicio, _fim, _sala1);
-    	ITurno turno = _turnoPersistente.readByNome(_turno1.getNome());
-    	_suportePersistente.confirmarTransaccao();
+	public static Test suite() {
+		TestSuite suite = new TestSuite(TurnoAulaOJBTest.class);
 
-    	turnoAula = new TurnoAula(turno, aula);
+		return suite;
+	}
 
-    	_suportePersistente.iniciarTransaccao();
-    	_turnoAulaPersistente.lockWrite(turnoAula);
-    	_suportePersistente.confirmarTransaccao();
-    	
-//      assertTrue(((Item)item).getCodigoInterno() != 0);
-    } catch (ExcepcaoPersistencia ex) {
-    	fail("testCreateNonExistingTurnoAula");
-    }
-  }
+	protected void setUp() {
+		super.setUp();
 
-  /** Test of write method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testWriteExistingUnchangedObject() {
-    // write TurnoAula already mapped into memory
-    try {
-    	_suportePersistente.iniciarTransaccao();
-    	ITurnoAula turnoAula = _turnoAulaPersistente.readByTurnoAndAula(_turno3, _aula1);
-    	_suportePersistente.confirmarTransaccao();
+	}
 
-      _suportePersistente.iniciarTransaccao();
-      _turnoAulaPersistente.lockWrite(turnoAula);
-      _suportePersistente.confirmarTransaccao();
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testWriteExistingUnchangedObject");
-    }
-  }
+	protected void tearDown() {
+		super.tearDown();
+	}
 
-  /** Test of write method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testWriteExistingChangedObject() {
-    ITurnoAula turnoAula1 = null;
-    ITurnoAula turnoAula2 = null;
-      
-    // write TurnoALuno already mapped into memory
-    try {
-    	_suportePersistente.iniciarTransaccao();
-    	IAula aula1 = _aulaPersistente.readByDiaSemanaAndInicioAndFimAndSala(_diaSemana1, _inicio, _fim, _sala1);
-    	ITurno turno3 = _turnoPersistente.readByNome(_turno3.getNome());
-    	ITurno turno5 = _turnoPersistente.readByNome(_turno5.getNome());
-    	_suportePersistente.confirmarTransaccao();
+	public void testWriteShiftLesson() {
+		try {
 
-      _suportePersistente.iniciarTransaccao();
-      turnoAula1 = _turnoAulaPersistente.readByTurnoAndAula(turno3, aula1);
-      turnoAula1.setTurno(turno5);
-      _suportePersistente.confirmarTransaccao();
+			ITurnoAula createdShiftLesson = null;
+			ITurno shift = null;
+			IAula lesson = null;
 
-      _suportePersistente.iniciarTransaccao();
-      turnoAula2 = _turnoAulaPersistente.readByTurnoAndAula(turno5, aula1);
-      _suportePersistente.confirmarTransaccao();
+			IExecutionPeriod executionPeriod = new ExecutionPeriod();
+			IExecutionYear executionYear = new ExecutionYear();
+			executionYear.setYear("2002/2003");
+			executionPeriod.setName("2º Semestre");
+			executionPeriod.setExecutionYear(executionYear);
+			IDisciplinaExecucao executionCourse = new DisciplinaExecucao();
+			executionCourse.setSigla("APR");
+			executionCourse.setExecutionPeriod(executionPeriod);
 
-      assertTrue(turnoAula2.getTurno().equals(turno5));
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testWriteExistingChangedObject");
-    }
-  }
+		
+			try {
+				_suportePersistente.iniciarTransaccao();
+				shift =
+					_suportePersistente
+						.getITurnoPersistente()
+						.readByNameAndExecutionCourse(
+						"turno_apr_teorico1",
+						executionCourse);
+				_suportePersistente.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				fail("Read existing shift : turno_apr_teorico1");
+			}
 
-  /** Test of delete method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testDeleteTurnoAula() {
-    try {
-    	_suportePersistente.iniciarTransaccao();
-    	ITurnoAula turnoAula = _turnoAulaPersistente.readByTurnoAndAula(_turno3, _aula1);
-    	_suportePersistente.confirmarTransaccao();
-    	
-      _suportePersistente.iniciarTransaccao();
-      _turnoAulaPersistente.delete(turnoAula);
-      _suportePersistente.confirmarTransaccao();
+		
+			ISala room = new Sala();
+			room.setNome("Ga1");
+			
+			
+			try {
+				_suportePersistente.iniciarTransaccao();
+				List lessonRoomList =
+					_suportePersistente
+						.getIAulaPersistente()
+						.readByRoomAndExecutionPeriod(
+						room,
+						executionPeriod);
+				lesson = (IAula) lessonRoomList.get(0);
+				_suportePersistente.confirmarTransaccao();
+			} catch (Exception e) {
+				fail("Reading lesson");
+			}
+		
 
-      _suportePersistente.iniciarTransaccao();
-      turnoAula = _turnoAulaPersistente.readByTurnoAndAula(_turno3, _aula1);
-      _suportePersistente.confirmarTransaccao();
+			write(shift, lesson);
+			
+			writeDuplicateShiftLesson(shift, lesson);
+						
+			update(new TurnoAula(shift, lesson));
+			
+			updateWithModifications(new TurnoAula(shift, lesson));
 
-      assertEquals(turnoAula, null);
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testDeleteTurnoAula");
-    }
-  }
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+			fail("testCreateNonExistingShiftLesson");
+		}
+	}
 
-  /** Test of deleteAll method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testDeleteAll() {
-    try {
-      _suportePersistente.iniciarTransaccao();
-      _turnoAulaPersistente.deleteAll();
-      _suportePersistente.confirmarTransaccao();
+	/** Test of delete method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
+	public void testDeleteTurnoAula() {
+		try {
+			ITurnoAula createdShiftLesson = null;
+			ITurno shift = null;
+			IAula lesson = null;
 
-      _suportePersistente.iniciarTransaccao();
-      List result = null;
-      try {
-        Implementation odmg = OJB.getInstance();
-        OQLQuery query = odmg.newOQLQuery();;
-        String oqlQuery = "select turnoAula from " + TurnoAula.class.getName();
-        query.create(oqlQuery);
-        result = (List) query.execute();
-      } catch (QueryException ex) {
-        throw new ExcepcaoPersistencia(ExcepcaoPersistencia.QUERY, ex);
-      }
-      _suportePersistente.confirmarTransaccao();
-      assertNotNull(result);
-      assertTrue(result.isEmpty());
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testDeleteTurnoAula");
-    }
+			IExecutionPeriod executionPeriod = new ExecutionPeriod();
+			IExecutionYear executionYear = new ExecutionYear();
+			executionYear.setYear("2002/2003");
+			executionPeriod.setName("2º Semestre");
+			executionPeriod.setExecutionYear(executionYear);
+			IDisciplinaExecucao executionCourse = new DisciplinaExecucao();
+			executionCourse.setSigla("APR");
+			executionCourse.setExecutionPeriod(executionPeriod);
 
-  }
+		
+			try {
+				_suportePersistente.iniciarTransaccao();
+				shift =
+					_suportePersistente
+						.getITurnoPersistente()
+						.readByNameAndExecutionCourse(
+						"turno_apr_teorico1",
+						executionCourse);
+				_suportePersistente.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				fail("Read existing shift : turno_apr_teorico1");
+			}
 
+		
+			ISala room = new Sala();
+			room.setNome("Ga1");
+			
+			
+			try {
+				_suportePersistente.iniciarTransaccao();
+				List lessonRoomList =
+					_suportePersistente
+						.getIAulaPersistente()
+						.readByRoomAndExecutionPeriod(
+						room,
+						executionPeriod);
+				lesson = (IAula) lessonRoomList.get(0);
+				_suportePersistente.confirmarTransaccao();
+			} catch (Exception e) {
+				fail("Reading lesson");
+			}
 
-    public void testReadAulasDeTurno() {
-    try {
-      List aulas = null;
+			assertNotNull("Missing shift",shift);
+			assertNotNull("Missing lesson", lesson);
+			
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.delete(new TurnoAula(shift, lesson));
+			_suportePersistente.confirmarTransaccao();
 
-      /* Testa metodo qdo ha mais do q uma aula de um turno na BD */
-      _suportePersistente.iniciarTransaccao();
-      aulas = _turnoAulaPersistente.readByShift(new Turno("455",null, null, null));
-      _suportePersistente.confirmarTransaccao();
-      assertEquals("testReadAulasDeTurno: qdo ha 2 aulas da turma na BD", aulas.size(),2);
+			_suportePersistente.iniciarTransaccao();
+			ITurnoAula turnoAula =
+				_turnoAulaPersistente.readByShiftAndLesson(shift, lesson);
+			_suportePersistente.confirmarTransaccao();
+			assertEquals(turnoAula, null);
+			
+		} catch (ExcepcaoPersistencia ex) {
+			fail("testDeleteTurnoAula");
+		}
+	}
 
-        /* Testa metodo qdo nao ha nenhuma aula do turno */
-      _suportePersistente.iniciarTransaccao();
-      _turnoAulaPersistente.deleteAll();
-      _suportePersistente.confirmarTransaccao();
-      
-      _suportePersistente.iniciarTransaccao();
-      aulas = _turnoAulaPersistente.readByShift(new Turno("turno1",null, null, null));
-      _suportePersistente.confirmarTransaccao();
-      assertEquals("testReadAulasDeTurno: qdo nao nenhuma aula do turno na BD", aulas.size(),0);
+	/** Test of deleteAll method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
+	public void testDeleteAll() {
+		try {
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.deleteAll();
+			_suportePersistente.confirmarTransaccao();
 
-    } catch (ExcepcaoPersistencia ex) {
-      fail("testReadAulasDeTurno");
-    }
-  }      
-    
+			_suportePersistente.iniciarTransaccao();
+			List result = null;
+			try {
+				Implementation odmg = OJB.getInstance();
+				OQLQuery query = odmg.newOQLQuery();
 
-  /** Test of delete method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
-  public void testDeleteTurnoAulaByKeys() {
-//    try {
-//      _suportePersistente.iniciarTransaccao();
-//      _turnoAulaPersistente.delete(_turno3.getNome(), _aula1.getDiaSemana(), _aula1.getInicio(),
-//                                   _aula1.getFim(), _aula1.getSala());
-//      _suportePersistente.confirmarTransaccao();
-//
-//      _suportePersistente.iniciarTransaccao();
-//      IAula aula1 = _aulaPersistente.readByDiaSemanaAndInicioAndFimAndSala(_diaSemana1, _inicio, _fim, _sala1);
-//      ITurno turno3 = _turnoPersistente.readByNome(_turno3.getNome());
-//      _suportePersistente.confirmarTransaccao();
-// 
-//      _suportePersistente.iniciarTransaccao();
-//      ITurnoAula turnoAula = _turnoAulaPersistente.readByTurnoAndAula(turno3, aula1);
-//      _suportePersistente.confirmarTransaccao();
-//
-//      assertEquals(turnoAula, null);
-//    } catch (ExcepcaoPersistencia ex) {
-//      fail("testDeleteTurnoAula");
-//    }
-  }    
+				String oqlQuery =
+					"select turnoAula from " + TurnoAula.class.getName();
+				query.create(oqlQuery);
+				result = (List) query.execute();
+			} catch (QueryException ex) {
+				throw new ExcepcaoPersistencia(ExcepcaoPersistencia.QUERY, ex);
+			}
+			_suportePersistente.confirmarTransaccao();
+			assertNotNull(result);
+			assertTrue(result.isEmpty());
+		} catch (ExcepcaoPersistencia ex) {
+			fail("testDeleteTurnoAula");
+		}
 
-  	/** Test of ReadLessonsByStudent method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
+	}
+
+	public void testReadAulasDeTurno() {
+		try {
+			List aulas = null;
+
+			/* Testa metodo qdo ha mais do q uma aula de um turno na BD */
+			_suportePersistente.iniciarTransaccao();
+			aulas =
+				_turnoAulaPersistente.readByShift(
+					new Turno("455", null, null, null));
+			_suportePersistente.confirmarTransaccao();
+			assertEquals(
+				"testReadAulasDeTurno: qdo ha 2 aulas da turma na BD",
+				aulas.size(),
+				2);
+
+			/* Testa metodo qdo nao ha nenhuma aula do turno */
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.deleteAll();
+			_suportePersistente.confirmarTransaccao();
+
+			_suportePersistente.iniciarTransaccao();
+			aulas =
+				_turnoAulaPersistente.readByShift(
+					new Turno("turno1", null, null, null));
+			_suportePersistente.confirmarTransaccao();
+			assertEquals(
+				"testReadAulasDeTurno: qdo nao nenhuma aula do turno na BD",
+				aulas.size(),
+				0);
+
+		} catch (ExcepcaoPersistencia ex) {
+			fail("testReadAulasDeTurno");
+		}
+	}
+
+	/** Test of delete method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
+//	public void testDeleteTurnoAulaByKeys() {
+//		//    try {
+//		//      _suportePersistente.iniciarTransaccao();
+//		//      _turnoAulaPersistente.delete(_turno3.getNome(), _aula1.getDiaSemana(), _aula1.getInicio(),
+//		//                                   _aula1.getFim(), _aula1.getSala());
+//		//      _suportePersistente.confirmarTransaccao();
+//		//
+//		//      _suportePersistente.iniciarTransaccao();
+//		//      IAula aula1 = _aulaPersistente.readByDiaSemanaAndInicioAndFimAndSala(_diaSemana1, _inicio, _fim, _sala1);
+//		//      ITurno turno3 = _turnoPersistente.readByNome(_turno3.getNome());
+//		//      _suportePersistente.confirmarTransaccao();
+//		// 
+//		//      _suportePersistente.iniciarTransaccao();
+//		//      ITurnoAula turnoAula = _turnoAulaPersistente.readByTurnoAndAula(turno3, aula1);
+//		//      _suportePersistente.confirmarTransaccao();
+//		//
+//		//      assertEquals(turnoAula, null);
+//		//    } catch (ExcepcaoPersistencia ex) {
+//		fail("testDeleteTurnoAula");
+//		//    }
+//	}
+
+	/** Test of ReadLessonsByStudent method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
 	public void testReadLessonsByStudent() {
 		try {
 			List lessons = null;
@@ -262,19 +283,116 @@ public class TurnoAulaOJBTest extends TestCaseOJB {
 	}
 
 	/** Test of ReadLessonsByShift method, of class ServidorPersistente.OJB.TurnoAulaOJB. */
- 	public void testReadLessonsByShift() {
-//		  try {
-//			  List lessons = null;
-//
-//			  _suportePersistente.iniciarTransaccao();
-//			  lessons = _turnoAulaPersistente.readLessonsByShift("turno_apr_teorico1");
-//			  _suportePersistente.confirmarTransaccao();
-//			  assertNotNull("testReadLessonsByStudent", lessons);
-//			  assertEquals("testReadLessonsByStudent", lessons.size(), 1);
-//
-//		  } catch (ExcepcaoPersistencia ex) {
-			  fail("testReadLessonsByStudent");
-//		  }
-	  }
+	public void testReadLessonsByShift() {
+		//		  try {
+		//			  List lessons = null;
+		//
+		//			  _suportePersistente.iniciarTransaccao();
+		//			  lessons = _turnoAulaPersistente.readLessonsByShift("turno_apr_teorico1");
+		//			  _suportePersistente.confirmarTransaccao();
+		//			  assertNotNull("testReadLessonsByStudent", lessons);
+		//			  assertEquals("testReadLessonsByStudent", lessons.size(), 1);
+		//
+		//		  } catch (ExcepcaoPersistencia ex) {
+		fail("testReadLessonsByStudent");
+		//		  }
+	}
+	/**
+	 * Method updateWithModifications.
+	 * @param turnoAula
+	 */
+	private void updateWithModifications(ITurnoAula shiftLesson) {
+		try {
+			/** get with keys */
+			_suportePersistente.iniciarTransaccao();
+			 shiftLesson = _turnoAulaPersistente.readByShiftAndLesson(shiftLesson.getTurno(), shiftLesson.getAula());
+			_suportePersistente.confirmarTransaccao();
+				
+			_suportePersistente.iniciarTransaccao();
+			List lessonList = (List) _aulaPersistente.readByExecutionCourse(shiftLesson.getTurno().getDisciplinaExecucao());
+			
+			IAula lesson = (IAula) lessonList.get(1);
+			_suportePersistente.confirmarTransaccao();		
+
+			shiftLesson.setAula(lesson);
+			
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.lockWrite(shiftLesson);
+			_suportePersistente.confirmarTransaccao();
+						
+		} catch (Exception e) {
+			/* to lockWrite shift and lesson must be mapped */
+			fail("Update (with modification) shift lesson");
+		}
+		
+	}
+
+	/**
+	 * Method update.
+	 * @param shift 
+	 * @param lesson
+	 */
+	private void update(ITurnoAula shiftLesson) {
+		try {
+			/** get with keys */
+			_suportePersistente.iniciarTransaccao();
+			shiftLesson = _turnoAulaPersistente.readByShiftAndLesson(shiftLesson.getTurno(), shiftLesson.getAula());
+			_suportePersistente.confirmarTransaccao();
+				
+		
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.lockWrite(shiftLesson);
+			_suportePersistente.confirmarTransaccao();
+						
+		} catch (Exception e) {
+			/* to lockWrite shift and lesson must be mapped */
+			fail("Update (no modification) shift lesson");
+		}
+
+	}
+
+	private void write(ITurno shift, IAula lesson) {
+		try {
+			ITurnoAula createdShiftLesson = null;
+			try {
+				createdShiftLesson = new TurnoAula(shift, lesson);
+				_suportePersistente.iniciarTransaccao();
+				_turnoAulaPersistente.lockWrite(createdShiftLesson);
+				_suportePersistente.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia ex) {
+				fail("Creating new shiftLesson");
+			}
+
+			ITurnoAula shiftLesson = null;
+			try {
+				_suportePersistente.iniciarTransaccao();
+				shiftLesson =
+					_turnoAulaPersistente.readByShiftAndLesson(shift, lesson);
+				_suportePersistente.confirmarTransaccao();
+				assertNotNull(
+					"ShiftLesson readed after creation must be not null",
+					shiftLesson);
+				assertEquals(shiftLesson, createdShiftLesson);
+			} catch (ExcepcaoPersistencia e) {
+				fail("Reading already created shiftLesson");
+			}
+		} catch (Exception e) {
+			fail("writing new shiftLesson");
+		}
+	}
+
+	private void writeDuplicateShiftLesson(ITurno shift, IAula lesson) {
+		ITurnoAula turnoAula = new TurnoAula(shift, lesson);
+		
+		try {
+			_suportePersistente.iniciarTransaccao();
+			_turnoAulaPersistente.lockWrite(turnoAula);
+			_suportePersistente.confirmarTransaccao();
+			fail("testCreateExistingTurnoAula");
+		} catch (ExcepcaoPersistencia ex) {
+			//all is ok
+		}
+	}
+
 
 }
