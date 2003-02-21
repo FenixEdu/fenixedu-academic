d7c2abec4d9a1901726410771cf4bcce3d909d0a
David Santos <david.santos@ist.utl.pt>
2003-02-21 12:22:25 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/TestCaseServicosWithAuthorization.java
+++ src_tests/ServidorAplicacao/Servicos/TestCaseServicosWithAuthorization.java
@@ -3,7 +3,7 @@ package ServidorAplicacao.Servicos;
 /**
  * @author dcs-rjao at 21/Fev/2003
  */
-public class TestCaseServicosWithAuthorization extends TestCaseServicos {
+abstract public class TestCaseServicosWithAuthorization extends TestCaseServicos {
 
 	public TestCaseServicosWithAuthorization(String testName) {
 		super(testName);
@@ -24,10 +24,27 @@ public class TestCaseServicosWithAuthorization extends TestCaseServicos {
 
 		Object result = null;
 		try {
-			result = _gestor.executar(_userView2, serviceName, serviceArguments);
+			result = _gestor.executar(_userView2, getServiceName(), serviceArguments);
 			fail("testUnauthorizedExecutionOfService - Service Name: " + serviceName);
 		} catch (Exception ex) {
 			assertNull("testUnauthorizedExecutionOfService", result);
 		}
+	}
+
+	public void testUnauthorizedExecutionOfService() {
+
+		Object serviceArguments[] = { null };
+
+		Object result = null;
+		try {
+			result = _gestor.executar(_userView2, getServiceName(), serviceArguments);
+			fail(this.getClass().getName() + " :testUnauthorizedExecutionOfService - Service Name: " + getServiceName());
+		} catch (Exception ex) {
+			assertNull(this.getClass().getName() + " :testUnauthorizedExecutionOfService", result);
+		}
+	}
+	
+	public String getServiceName(){
+		return "";
 	}
 }
