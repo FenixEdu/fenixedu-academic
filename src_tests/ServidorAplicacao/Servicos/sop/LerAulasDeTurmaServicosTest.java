0f5b3c83e7f13160ad3f825d46233fc9f32b6e85
João Mota <joao.mota@ist.utl.pt>
2003-02-24 18:21:35 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/sop/LerAulasDeTurmaServicosTest.java
+++ src_tests/ServidorAplicacao/Servicos/sop/LerAulasDeTurmaServicosTest.java
@@ -11,76 +11,143 @@ package ServidorAplicacao.Servicos.sop;
  *
  * @author tfc130
  */
-import java.util.List;
+import java.util.Calendar;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import DataBeans.ClassKey;
-import ServidorAplicacao.Servicos.TestCaseServicos;
+import DataBeans.InfoClass;
+import DataBeans.InfoDegree;
+import DataBeans.InfoDegreeCurricularPlan;
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoExecutionDegree;
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoExecutionYear;
+import DataBeans.InfoLesson;
+import DataBeans.InfoRoom;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import Util.DiaSemana;
+import Util.TipoAula;
+import Util.TipoSala;
 
-public class LerAulasDeTurmaServicosTest extends TestCaseServicos {
-    public LerAulasDeTurmaServicosTest(java.lang.String testName) {
-    super(testName);
-  }
-    
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-    
-  public static Test suite() {
-    TestSuite suite = new TestSuite(LerAulasDeTurmaServicosTest.class);
+public class LerAulasDeTurmaServicosTest extends TestCaseReadServices {
+	public LerAulasDeTurmaServicosTest(java.lang.String testName) {
+		super(testName);
+	}
 
-    return suite;
-  }
-    
-  protected void setUp() {
-    super.setUp();
-  }
-    
-  protected void tearDown() {
-    super.tearDown();
-  }
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
 
-    // read aulas by unauthorized user
-  public void testUnauthorizedReadAulasDeTurma() {
-    Object argsLerAulasDeTurma[] = new Object[1];
-    argsLerAulasDeTurma[0] = new ClassKey("turma1");
-    
-    Object result = null; 
-      try {
-        result = _gestor.executar(_userView2, "LerAulasDeTurma", argsLerAulasDeTurma);
-        fail("testUnauthorizedReadAulasDeTurma");
-      } catch (Exception ex) {
-        assertNull("testUnauthorizedReadAulasDeTurma", result);
-      }
-  }
+	public static Test suite() {
+		TestSuite suite = new TestSuite(LerAulasDeTurmaServicosTest.class);
 
-  // read new existing aulas de turma
-  public void testReadExistingAulasDeTurma() {
-    Object argsLerAulasDeTurma[] = new Object[1];
-    argsLerAulasDeTurma[0] = new ClassKey("turma1");
-    
-    Object result = null; 
-      try {
-        result = _gestor.executar(_userView, "LerAulasDeTurma", argsLerAulasDeTurma);
-        assertEquals("testReadExistingAulasDeTurma", 1, ((List) result).size());
-      } catch (Exception ex) {
-      	fail("testReadExistingAulasDeTurma");
-      }
-  }
+		return suite;
+	}
 
-    // read new non-existing aulas de turma
-  public void testReadNonExistingAulasDeTurma() {
-    Object argsLerAulasDeTurma[] = new Object[1];
-    argsLerAulasDeTurma[0] = new ClassKey("turma2");
-    
-    Object result = null; 
-      try {
-        result = _gestor.executar(_userView, "LerAulasDeTurma", argsLerAulasDeTurma);
-        assertTrue("testReadExistingAulasDeTurma", ((List) result).isEmpty());
-      } catch (Exception ex) {
-      	fail("testReadExistingAulasDeTurma");
-      }
-  }
-    
+	protected void setUp() {
+		super.setUp();
+	}
+
+	protected void tearDown() {
+		super.tearDown();
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "LerAulasDeTurma";
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		InfoClass infoClass =
+			new InfoClass(
+				"xpto",
+				new Integer(1),
+				new InfoExecutionDegree(
+					new InfoDegreeCurricularPlan(
+						"plano1",
+						new InfoDegree(
+							"LEIC",
+							"Licenciatura de Engenharia Informatica e de Computadores")),
+					new InfoExecutionYear("2002/2003")),
+				new InfoExecutionPeriod(
+					"2º Semestre",
+					new InfoExecutionYear("2002/2003")));
+		Object[] result = { infoClass };
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		InfoClass infoClass =
+			new InfoClass(
+				"10501",
+				new Integer(1),
+				new InfoExecutionDegree(
+					new InfoDegreeCurricularPlan(
+						"plano1",
+						new InfoDegree(
+							"LEIC",
+							"Licenciatura de Engenharia Informatica e de Computadores")),
+					new InfoExecutionYear("2002/2003")),
+				new InfoExecutionPeriod(
+					"2º Semestre",
+					new InfoExecutionYear("2002/2003")));
+		Object[] result = { infoClass };
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
+	 */
+	protected int getNumberOfItemsToRetrieve() {
+		return 1;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
+	 */
+	protected Object getObjectToCompare() {
+
+		Calendar inicio = Calendar.getInstance();
+		inicio.set(Calendar.HOUR_OF_DAY, 12);
+		inicio.set(Calendar.MINUTE, 30);
+		inicio.set(Calendar.SECOND, 0);
+		Calendar fim = Calendar.getInstance();
+		fim.set(Calendar.HOUR_OF_DAY, 13);
+		fim.set(Calendar.MINUTE, 0);
+		fim.set(Calendar.SECOND, 0);
+		InfoLesson infoLesson =
+			new InfoLesson(
+				new DiaSemana(2),
+				inicio,
+				fim,
+				new TipoAula(1),
+				new InfoRoom(
+					"Ga1",
+					"Pavilhao Central",
+					new Integer(0),
+					new TipoSala(1),
+					new Integer(100),
+					new Integer(50)),
+				new InfoExecutionCourse(
+					"Trabalho Final de Curso I",
+					"TFCI",
+					"programa1",
+					new Double(0),
+					new Double(0),
+					new Double(0),
+					new Double(0),
+					new InfoExecutionPeriod(
+						"2º Semestre",
+						new InfoExecutionYear("2002/2003"))));
+		return infoLesson;
+	}
+
 }
