7cab6bf2a55fac3be662743cdf9496327b7c6bac
João Mota <joao.mota@ist.utl.pt>
2003-02-24 20:03:43 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/sop/LerSalasServicosTest.java
+++ src_tests/ServidorAplicacao/Servicos/sop/LerSalasServicosTest.java
@@ -74,50 +74,6 @@ public class LerSalasServicosTest extends TestCaseReadServices {
 
 
   
-//  public void testReadAll() {
-//    Object argsLerSalas[] = new Object[0];
-//    Object result = null;     
-//    
-//   // Nao ha salas na BD
-//    try {
-//      _suportePersistente.iniciarTransaccao();    
-//      _salaPersistente.deleteAll();
-//      _suportePersistente.confirmarTransaccao();      
-//    } catch (ExcepcaoPersistencia excepcao) {
-//      fail("Exception when setUp");
-//    }
-//      
-//    try {
-//      result = _gestor.executar(_userView, "LerSalas", argsLerSalas);
-//      assertEquals("testReadAll: nao ha salas para ler", ((List)result).size(), 0);
-//      } catch (Exception ex) {
-//      	fail("testReadAll: nao ha salas para ler");
-//      }
-//    
-//    // Ha 2 Salas na BD
-//    try {
-//      _suportePersistente.iniciarTransaccao();    
-//     _salaPersistente.lockWrite(_sala1);
-//     _salaPersistente.lockWrite(_sala2);
-//      _suportePersistente.confirmarTransaccao();      
-//    } catch (ExcepcaoPersistencia excepcao) {
-//      fail("Exception when setUp");
-//    }
-//    
-//    try {
-//      result = _gestor.executar(_userView, "LerSalas", argsLerSalas);
-//      assertEquals("testReadSalas: ha 2 salas para ler", ((List)result).size(), 2);
-//      InfoRoom sala1 = new InfoRoom(_sala1.getNome(), _sala1.getEdificio(), _sala1.getPiso(),
-//                                    _sala1.getTipo(), _sala1.getCapacidadeNormal(),
-//                                    _sala1.getCapacidadeExame());
-//      InfoRoom sala2 = new InfoRoom(_sala2.getNome(), _sala2.getEdificio(), _sala2.getPiso(),
-//                                    _sala2.getTipo(), _sala2.getCapacidadeNormal(),
-//                                    _sala2.getCapacidadeExame());
-//      assertTrue("testReadSalas: sala lida e _sala1", ((List)result).contains(sala1));
-//      assertTrue("testReadSalas: sala lida e _sala2", ((List)result).contains(sala2));
-//      } catch (Exception ex) {
-//      	fail("testReadSalas");
-//      }
-//  }
+
       
 }
