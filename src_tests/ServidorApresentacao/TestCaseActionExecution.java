be7d91e9bfa181fecf45a92d5ab0cc5d3451c997
David Santos <david.santos@ist.utl.pt>
2003-02-26 16:35:56 +0000

    *** empty log message ***

---
+++ src_tests/ServidorApresentacao/TestCaseActionExecution.java
@@ -0,0 +1,28 @@
+/*
+ * Created on 26/Fev/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorApresentacao;
+
+/**
+ * @author dcs-rjao
+ *
+ * 26/Fev/2003
+ */
+public class TestCaseActionExecution extends TestCasePresentation {
+
+	public TestCaseActionExecution(String testName) {
+		super(testName);
+	}
+
+	public void setUp() {
+		super.setUp();
+	}
+
+	protected void tearDown() {
+		super.tearDown();
+	}
+
+}
