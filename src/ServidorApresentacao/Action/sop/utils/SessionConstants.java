1a0a78f43595e4b9a2c151ee76e2ea03af14ec59
João Mota <joao.mota@ist.utl.pt>
2003-02-26 11:35:41 +0000

    *** empty log message ***

--- src/ServidorApresentacao/Action/sop/utils/SessionConstants.java
+++ src/ServidorApresentacao/Action/sop/utils/SessionConstants.java
@@ -27,19 +27,26 @@ public abstract class SessionConstants {
 	public static final String EXECUTION_COURSE_LIST_KEY =
 		CONTEXT_PREFIX + "disciplinasExecucao";
 	public static final String INFO_STUDENT_KEY = "infoStudent";
-	
+
 	public static final String SEMESTER_LIST_KEY = "semester.list";
 	public static final String INFO_DEGREE_LIST_KEY = "infoDegree.list";
 	public static final String CURRICULAR_YEAR_LIST_KEY = "curricularYear.list";
 	public static final String SHIFT_VIEW = "shiftview";
-	public static final String INFO_SHIFTS_EXECUTION_COURSE_KEY = "infoTurnosDeDisciplinaExecucao";
-	public static final String INFO_EXECUTION_PERIOD_KEY = "info_execution_period_key"; 
-	public static final String INFO_EXECUTION_DEGREE_LIST_KEY = "info_execution_degree_list_key";
+	public static final String INFO_SHIFTS_EXECUTION_COURSE_KEY =
+		"infoTurnosDeDisciplinaExecucao";
+	public static final String INFO_EXECUTION_PERIOD_KEY =
+		"info_execution_period_key";
+	public static final String INFO_EXECUTION_DEGREE_LIST_KEY =
+		"info_execution_degree_list_key";
 	public static final String CURRICULAR_YEAR_KEY = "curricular_year_key";
+
+	public static final String CLASS_INFO_SHIFT_LIST_KEY =
+		"class_info_shift_list_key";
+
+	public static final String AVAILABLE_INFO_SHIFT_LIST_KEY =
+		"available_info_shift_list_key";
+
 	public static final String ORIGINAL_MAPPING_KEY = "original_mapping_key";
 
 	public static final String EXCEPTION_STACK_TRACE = "exception_stack_trace";
-	public static final String CLASS_INFO_SHIFT_LIST_KEY = "class_info_shift_list_key";
-
-		public static final String AVAILABLE_INFO_SHIFT_LIST_KEY = "available_info_shift_list_key";
 }
