7ae31cc36831d183540e18bd852b0ba0e5f1c56b
João Luz <joao.luz@ist.utl.pt>
2003-02-27 14:31:06 +0000

    ColorPicker for diferent timetable types

---
+++ src/ServidorApresentacao/TagLib/sop/v3/colorPickers/ClassTimeTableColorPicker.java
@@ -0,0 +1,27 @@
+/*
+ * Created on 27/Fev/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package ServidorApresentacao.TagLib.sop.v3.colorPickers;
+
+import DataBeans.InfoLesson;
+import ServidorApresentacao.TagLib.sop.v3.ColorPicker;
+
+/**
+ * @author jpvl
+ */
+public class ClassTimeTableColorPicker extends ColorPicker {
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TagLib.sop.v3.ColorPicker#getColorKeyFromInfoLesson(DataBeans.InfoLesson)
+	 */
+	protected String getColorKeyFromInfoLesson(InfoLesson infoLesson) {
+		StringBuffer strBuffer = new StringBuffer();
+		strBuffer.append(
+			infoLesson.getInfoDisciplinaExecucao().getSigla());
+		return strBuffer.toString();
+	}
+
+}
