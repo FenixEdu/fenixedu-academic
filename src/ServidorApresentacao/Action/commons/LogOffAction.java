56d861934ef3055ec6fbfd67b2968999ed033313
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-03-06 15:05:34 +0000

    *** empty log message ***

--- src/ServidorApresentacao/Action/commons/LogOffAction.java
+++ src/ServidorApresentacao/Action/commons/LogOffAction.java
@@ -11,7 +11,7 @@
  *
  */
  
-package ServidorApresentacao.Action.sop;
+package ServidorApresentacao.Action.commons;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
