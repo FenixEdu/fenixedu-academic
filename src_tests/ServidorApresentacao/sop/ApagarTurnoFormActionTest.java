b2d6c5654a22881a3bd6941f66a95e6a1f5e01b1
Ivo Brandão <imbr@mega.ist.utl.pt>
2003-03-06 10:00:03 +0000

    *** empty log message ***

--- src_tests/ServidorApresentacao/sop/ApagarTurnoFormActionTest.java
+++ src_tests/ServidorApresentacao/sop/ApagarTurnoFormActionTest.java
@@ -1,114 +1,127 @@
+/*
+ * ApagarTurnoFormAction2.java
+ * Mar 6, 2003
+ */
 package ServidorApresentacao.sop;
-import java.util.ArrayList;
-import java.util.HashSet;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
 import DataBeans.InfoExecutionCourse;
 import DataBeans.InfoExecutionPeriod;
 import DataBeans.InfoExecutionYear;
 import DataBeans.InfoShift;
 import ServidorAplicacao.GestorServicos;
-import ServidorAplicacao.IUserView;
-import ServidorAplicacao.Servico.UserView;
-import ServidorApresentacao.TestCasePresentation;
+import ServidorApresentacao.TestCaseActionExecution;
 import ServidorApresentacao.Action.sop.utils.SessionConstants;
 import Util.TipoAula;
+
 /**
-@author tfc130
-*/
-public class ApagarTurnoFormActionTest extends TestCasePresentation {
- 
+ * @author Ivo Brandão
+ */
+public class ApagarTurnoFormActionTest extends TestCaseActionExecution {
 
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-   
-  public static Test suite() {
-    TestSuite suite = new TestSuite(ApagarTurnoFormActionTest.class);
-        
-    return suite;
-  }
-  public void setUp() {
-    super.setUp();
-    // define ficheiro de configuracao Struts a utilizar
-    setServletConfigFile("/WEB-INF/tests/web-sop.xml");
-    
-    
-  }
+	/**
+	 * @param testName
+	 */
+	public ApagarTurnoFormActionTest(String testName) {
+		super(testName);
+	}
 
- 
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getServletConfigFile()
+	 */
+	protected String getServletConfigFile() {
+		return "/WEB-INF/tests/web-sop.xml";
+	}
 
-  public ApagarTurnoFormActionTest(String testName) {
-    super(testName);
-  }
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoPathAction()
+	 */
+	protected String getRequestPathInfoPathAction() {
+		return "/sop";
+	}
 
-  public void testSuccessfulApagarTurno() {
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoNameAction()
+	 */
+	protected String getRequestPathInfoNameAction() {
+		return "/apagarTurno";
+	}
 
-  	// Necessario para colocar form manipularAulasForm em sessao
-  	setRequestPathInfo("/sop", "/manipularTurnosForm");
-  	addRequestParameter("indexTurno", new Integer(0).toString());
-  	actionPerform();
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getSuccessfulForward()
+	 */
+	protected String getSuccessfulForward() {
+		return "Sucesso";
+	}
 
-  	// define mapping de origem
-  	setRequestPathInfo("/sop", "/apagarTurno");
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#testSuccessfulExecutionOfAction()
+	 */
+	public void testSuccessfulExecutionOfAction() {
+		//place form manipularAulasForm in session
+		setRequestPathInfo("/sop", "/manipularTurnosForm");
+		addRequestParameter("indexTurno", new Integer(0).toString());
+		actionPerform();
 
-    // coloca credenciais na sessao
-    HashSet privilegios = new HashSet();
-    privilegios.add("ApagarTurno");
-    privilegios.add("LerTurnosDeDisciplinaExecucao");
-    IUserView userView = new UserView("user", privilegios);
-    getSession().setAttribute("UserView", userView);
-    try {
-    	GestorServicos gestor = GestorServicos.manager();
-		InfoExecutionCourse iDE = 
-		new InfoExecutionCourse("Trabalho Final de Curso I","TFCI",
-					"programa1",
-					new Double(0),
-					new Double(0),
-					new Double(0),
-					new Double(0),
-					new InfoExecutionPeriod("2º Semestre",new InfoExecutionYear("2002/2003")));
-   
+		//run the test
+		doTest(null, getItemsToPutInSessionForActionToBeTestedSuccessfuly(), getSuccessfulForward(), null, null, null, null);
+	}
 
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#testUnsuccessfulExecutionOfAction()
+	 * 
+	 * dummy method
+	 */
+	public void testUnsuccessfulExecutionOfAction() {
+	}
 
-    	getSession().setAttribute(SessionConstants.EXECUTION_COURSE_KEY, iDE);
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInSessionForActionToBeTestedSuccessfuly()
+	 */
+	protected Map getItemsToPutInSessionForActionToBeTestedSuccessfuly() {
+		Map result = new Hashtable();
+
+		try {
+			GestorServicos manager = GestorServicos.manager();
+			InfoExecutionCourse iDE = 
+			new InfoExecutionCourse("Trabalho Final de Curso I","TFCI",
+						"programa1",
+						new Double(0),
+						new Double(0),
+						new Double(0),
+						new Double(0),
+						new InfoExecutionPeriod("2º Semestre",new InfoExecutionYear("2002/2003")));
+
+			result.put(SessionConstants.EXECUTION_COURSE_KEY, iDE);
     	
-    	
-    	Object argsLerTurnos[] = new Object[1];
-    	argsLerTurnos[0] = iDE;
-    	ArrayList infoTurnos = (ArrayList) gestor.executar(userView, "LerTurnosDeDisciplinaExecucao", argsLerTurnos);
-    	getSession().setAttribute("infoTurnosDeDisciplinaExecucao", infoTurnos);
+			Object argsReadShifts[] = new Object[1];
+			argsReadShifts[0] = iDE;
+			ArrayList infoShifts = (ArrayList) manager.executar(userView, "LerTurnosDeDisciplinaExecucao", argsReadShifts);
+			result.put("infoTurnosDeDisciplinaExecucao", infoShifts);
 
-		InfoExecutionCourse iDE1 = 
-		new InfoExecutionCourse("Trabalho Final de Curso I","TFCI",
-					"programa1",
-					new Double(0),
-					new Double(0),
-					new Double(0),
-					new Double(0),
-					new InfoExecutionPeriod("2º Semestre",new InfoExecutionYear("2002/2003")));
+			InfoExecutionCourse iDE1 = 
+			new InfoExecutionCourse("Trabalho Final de Curso I","TFCI",
+						"programa1",
+						new Double(0),
+						new Double(0),
+						new Double(0),
+						new Double(0),
+						new InfoExecutionPeriod("2º Semestre",new InfoExecutionYear("2002/2003")));
 
-		InfoShift infoTurno1 = new InfoShift("turno1", new TipoAula(2),new Integer(100), iDE);
-    	InfoShift infoTurno = (InfoShift) infoTurnos.get(infoTurnos.indexOf((InfoShift) infoTurno1));
-    	getSession().setAttribute("infoTurno", infoTurno);
+			InfoShift infoShift1 = new InfoShift("turno1", new TipoAula(2),new Integer(100), iDE);
+			InfoShift infoShift = (InfoShift) infoShifts.get(infoShifts.indexOf((InfoShift) infoShift1));
+			result.put("infoTurno", infoShift);
 
-    } catch (Exception ex) {
-    	System.out.println("Erro na invocacao do servico " + ex);
-    }
+		} catch (Exception exception) {
+			String message = "Failed while executing the service";
+			System.out.println(message + " " + exception);
+			fail(message);
+		}
+		
+		return result;
+	}
 
-    // invoca acção
-    actionPerform();
-    
-    // verifica reencaminhamento
-    verifyForward("Sucesso");
-    
-    //verifica ausencia de erros
-    verifyNoActionErrors();
-  }
-  
-
-
-  
-  
 }
