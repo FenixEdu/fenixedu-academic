eec9d63af8bd38d47f29031baf351b0989f5bc4f
João Luz <joao.luz@ist.utl.pt>
2003-03-10 16:27:02 +0000

    added some comments...

--- src/ServidorAplicacao/Servico/sop/ReadEmptyRoomsService.java
+++ src/ServidorAplicacao/Servico/sop/ReadEmptyRoomsService.java
@@ -23,11 +23,6 @@ import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 /**
  * @author jpvl
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
  */
 public class ReadEmptyRoomsService implements IServico {
 	private static ReadEmptyRoomsService serviceInstance = new ReadEmptyRoomsService();
@@ -66,6 +61,7 @@ public class ReadEmptyRoomsService implements IServico {
 			}
 
 			
+
 			// Read all Rooms with a capacity
 			List roomList = roomDAO.readSalas(
 							null,
@@ -99,6 +95,7 @@ public class ReadEmptyRoomsService implements IServico {
 						
 			Iterator lessonIterator = lessonList.iterator();
 			
+			/* remove lesson's rooms from room list */
 			while (lessonIterator.hasNext()) {
 				IAula lessonAux = (IAula) lessonIterator.next();
 				InfoLesson infoLessonAux = Cloner.copyILesson2InfoLesson(lessonAux);
