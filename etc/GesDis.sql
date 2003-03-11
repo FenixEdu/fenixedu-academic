b75af57abeebefbaaad88ff22cd39720cd715a6d
João Mota <joao.mota@ist.utl.pt>
2003-03-11 13:58:20 +0000

    added curriculum

--- etc/GesDis.sql
+++ etc/GesDis.sql
@@ -1,69 +1,77 @@
-# MySQL-Front Dump 2.5
-#
-# Host: localhost   Database: ciapl
-# --------------------------------------------------------
-# Server version 3.23.51-max-debug
+/*
+Mascon Dump
+Source Host:           localhost
+Source Server Version: 3.23.53-max
+Source Database:       ciapl
+Date:                  2003-03-11 13:57:44
+*/
 
-USE ciapl;
+use ciapl ;
+#----------------------------
+# Table structure for announcement
+#----------------------------
+drop table if exists announcement;
+create table announcement (
+   ID_INTERNAL int(11) unsigned not null auto_increment,
+   TITLE varchar(100),
+   CREATION_DATE date,
+   LAST_MODIFICATION_DATE date,
+   INFORMATION varchar(100),
+   KEY_SITE int(11) unsigned not null default '0',
+   primary key (ID_INTERNAL))
+   type=InnoDB comment="InnoDB free: 372736 kB";
 
+#----------------------------
+# Table structure for curriculum
+#----------------------------
+drop table if exists curriculum;
+create table curriculum (
+   ID_INTERNAL int(11) not null auto_increment,
+   KEY_EXECUTION_COURSE int(11) not null default '0',
+   GENERAL_OBJECTIVES varchar(50),
+   OPERACIONAL_OBJECTIVES varchar(50),
+   PROGRAM varchar(50),
+   primary key (ID_INTERNAL))
+   type=InnoDB comment="InnoDB free: 372736 kB";
 
-#
-# Table structure for table 'ANNOUNCEMENT'
-#
+#----------------------------
+# Table structure for item
+#----------------------------
+drop table if exists item;
+create table item (
+   ID_INTERNAL int(11) unsigned not null auto_increment,
+   NAME varchar(100),
+   `ORDER` int(11) unsigned,
+   INFORMATION text,
+   URGENT int(11) unsigned,
+   KEY_SECTION int(11) unsigned not null default '0',
+   primary key (ID_INTERNAL),
+   unique ID_INTERNAL (ID_INTERNAL))
+   type=InnoDB comment="InnoDB free: 372736 kB";
 
-CREATE TABLE `ANNOUNCEMENT` (
-  `ID_INTERNAL` int(11) unsigned NOT NULL auto_increment,
-  `TITLE` varchar(100) default NULL,
-  `CREATION_DATE` date default NULL,
-  `LAST_MODIFICATION_DATE` date default NULL,
-  `INFORMATION` varchar(100) default NULL,
-  `KEY_SITE` int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`ID_INTERNAL`)
-) TYPE=InnoDB;
+#----------------------------
+# Table structure for section
+#----------------------------
+drop table if exists section;
+create table section (
+   ID_INTERNAL int(11) unsigned not null auto_increment,
+   NAME varchar(100),
+   `ORDER` int(11) unsigned,
+   KEY_SITE int(10) unsigned not null default '0',
+   KEY_SUPERIOR_SECTION int(10) unsigned not null default '0',
+   LAST_MODIFIED_DATE date,
+   primary key (ID_INTERNAL))
+   type=InnoDB comment="InnoDB free: 372736 kB";
 
+#----------------------------
+# Table structure for site
+#----------------------------
+drop table if exists site;
+create table site (
+   ID_INTERNAL int(11) unsigned not null auto_increment,
+   KEY_EXECUTION_COURSE int(11) unsigned not null default '0',
+   primary key (ID_INTERNAL),
+   unique ID_INTERNAL (ID_INTERNAL, KEY_EXECUTION_COURSE))
+   type=MyISAM;
 
-
-#
-# Table structure for table 'ITEM'
-#
-
-CREATE TABLE `ITEM` (
-  `ID_INTERNAL` int(11) unsigned NOT NULL auto_increment,
-  `NAME` varchar(100) default NULL,
-  `ORDER` int(11) unsigned default NULL,
-  `INFORMATION` text,
-  `URGENT` int(11) unsigned default NULL,
-  `KEY_SECTION` int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`ID_INTERNAL`),
-  UNIQUE KEY `ID_INTERNAL` (`ID_INTERNAL`)
-) TYPE=InnoDB;
-
-
-
-#
-# Table structure for table 'SECTION'
-#
-
-CREATE TABLE `SECTION` (
-  `ID_INTERNAL` int(11) unsigned NOT NULL auto_increment,
-  `NAME` varchar(100) default NULL,
-  `ORDER` int(11) unsigned default NULL,
-  `KEY_SITE` int(10) unsigned NOT NULL default '0',
-  `KEY_SUPERIOR_SECTION` int(10) unsigned NOT NULL default '0',
-  `LAST_MODIFIED_DATE` date default NULL,
-  PRIMARY KEY  (`ID_INTERNAL`)
-) TYPE=InnoDB;
-
-
-
-#
-# Table structure for table 'SITE'
-#
-
-CREATE TABLE `SITE` (
-  `ID_INTERNAL` int(11) unsigned NOT NULL auto_increment,
-  `KEY_EXECUTION_COURSE` int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`ID_INTERNAL`),
-  UNIQUE KEY `ID_INTERNAL` (`ID_INTERNAL`,`KEY_EXECUTION_COURSE`)
-) TYPE=MyISAM;
 
