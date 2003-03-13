e173c6512e081cc154aa9f2d9d80794e827d1d14
João Mota <joao.mota@ist.utl.pt>
2003-03-13 16:49:33 +0000

    fixed the service. now returns a boolean when everything is ok

--- src/ServidorAplicacao/Servico/gesdis/teacher/AssociateTeacher.java
+++ src/ServidorAplicacao/Servico/gesdis/teacher/AssociateTeacher.java
@@ -1,79 +1,102 @@
 package ServidorAplicacao.Servico.gesdis.teacher;
 
 import java.util.List;
-
 import DataBeans.InfoExecutionCourse;
 import DataBeans.util.Cloner;
 import Dominio.IDisciplinaExecucao;
-import Dominio.ISite;
 import Dominio.ITeacher;
 import ServidorAplicacao.FenixServiceException;
 import ServidorAplicacao.IServico;
 import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
 import ServidorAplicacao.Servico.exceptions.InvalidArgumentsServiceException;
 import ServidorPersistente.ExcepcaoPersistencia;
-import ServidorPersistente.IPersistentSite;
 import ServidorPersistente.IPersistentTeacher;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
 
- 
-public class AssociateTeacher implements IServico{
-    
-    
-    private static AssociateTeacher service = new AssociateTeacher();
-    
-    /**
+public class AssociateTeacher implements IServico {
+
+	private static AssociateTeacher service = new AssociateTeacher();
+
+	/**
 	 * The singleton access method of this class.
 	 **/
-    public static AssociateTeacher getService() {
+
+	public static AssociateTeacher getService() {
+
 		return service;
-    }
-    
-    /**
+
+	}
+
+	/**
 	 * The Actor of this class.
 	 **/
-    private AssociateTeacher() { }
-    
-    /**
+
+	private AssociateTeacher() {
+	}
+
+	/**
 	 * returns the service name
 	 **/
-    public final String getNome() {
-		return "gesdis.teacher.AssociateTeacher";
-    }
-    
-    /**
+
+	public final String getNome() {
+
+		return "AssociateTeacher";
+
+	}
+
+	/**
 	 * Executes the service.
 	 *
 	 **/
-	
-    public void run(InfoExecutionCourse infoExecutionCourse,Integer teacherNumber) throws FenixServiceException{
-		try{
+
+	public boolean run(
+		InfoExecutionCourse infoExecutionCourse,
+		Integer teacherNumber)
+		throws FenixServiceException {
+
+		try {
+
 			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
 			IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
-			IPersistentSite persistentSite = sp.getIPersistentSite();
 			ITeacher teacher = null;
-            IDisciplinaExecucao executionCourse = null;
-			ISite site=null;
-			
-			teacher = persistentTeacher.readTeacherByNumber(teacherNumber);		
-			
-			Cloner.copyInfoExecutionCourse2ExecutionCourse(infoExecutionCourse);
-			site = persistentSite.readByExecutionCourse(executionCourse);
-             if(teacher == null )  
-                            throw new InvalidArgumentsServiceException();
-			List professorShipsExecutionCoursesList=teacher.getProfessorShipsExecutionCourses();
-			if(professorShipsExecutionCoursesList.contains(executionCourse))
-							throw new ExistingServiceException();
-			professorShipsExecutionCoursesList.add(executionCourse);
-			teacher.setProfessorShipsExecutionCourses(professorShipsExecutionCoursesList);
-			
-			
-			persistentTeacher.lockWrite(teacher);
-		}
-		catch(ExcepcaoPersistencia ex){
-			throw new ExistingServiceException(ex);
-		}
-    }
-}
+			IDisciplinaExecucao executionCourse = null;
+			teacher = persistentTeacher.readTeacherByNumber(teacherNumber);
 
+			executionCourse =
+				Cloner.copyInfoExecutionCourse2ExecutionCourse(
+					infoExecutionCourse);
+			System.out.println("Teacher: " + teacher.getTeacherNumber());
+			System.out.println("Teacher: " + teacher);
+			if (teacher == null) {
+				System.out.println("O Teacher está a null: ");
+				
+				throw new InvalidArgumentsServiceException();
+			} else {
+
+				List professorShipsExecutionCoursesList =
+					teacher.getProfessorShipsExecutionCourses();
+
+				if (professorShipsExecutionCoursesList
+					.contains(executionCourse))
+					throw new ExistingServiceException();
+
+				professorShipsExecutionCoursesList.add(executionCourse);
+
+				teacher.setProfessorShipsExecutionCourses(
+					professorShipsExecutionCoursesList);
+
+				persistentTeacher.lockWrite(teacher);
+				
+				return true;
+			}
+
+		} catch (ExcepcaoPersistencia ex) {
+			
+			throw new ExistingServiceException(ex);
+
+		}
+
+	}
+
+}
