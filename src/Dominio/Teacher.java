0bd51ce0f3fd7be457450ab22360f58323412090
João Mota <joao.mota@ist.utl.pt>
2003-03-13 13:31:39 +0000

    added toString()

--- src/Dominio/Teacher.java
+++ src/Dominio/Teacher.java
@@ -11,50 +11,47 @@ package Dominio;
 import java.util.ArrayList;
 import java.util.List;
 public class Teacher extends DomainObject implements ITeacher {
-    private Integer teacherNumber;
-    private List professorShipsExecutionCourses;
-    private List responsibleForExecutionCourses;
-    private IPessoa person;
-    private Integer keyPerson;
-    
-    /** Creates a new instance of Teacher */
-    public Teacher() {
-    }
-    public Teacher(IPessoa person,Integer teacherNumber){
-		setPerson(person);
-        setTeacherNumber(teacherNumber);
-        setProfessorShipsExecutionCourses(new ArrayList());
-        setResponsibleForExecutionCourses(new ArrayList());
+	private Integer teacherNumber;
+	private List professorShipsExecutionCourses;
+	private List responsibleForExecutionCourses;
+	private IPessoa person;
+	private Integer keyPerson;
 
-    }
-    public boolean equals(Object obj) {
-        boolean resultado = false;
-        if (obj instanceof ITeacher) {
-            resultado = getTeacherNumber().equals(((ITeacher) obj).getTeacherNumber());
-        }
-        return resultado;
-    }
+	/** Creates a new instance of Teacher */
+	public Teacher() {
+	}
+	public Teacher(IPessoa person, Integer teacherNumber) {
+		setPerson(person);
+		setTeacherNumber(teacherNumber);
+		setProfessorShipsExecutionCourses(new ArrayList());
+		setResponsibleForExecutionCourses(new ArrayList());
+	}
+	public boolean equals(Object obj) {
+		boolean resultado = false;
+		if (obj instanceof ITeacher) {
+			resultado =
+				getTeacherNumber().equals(((ITeacher) obj).getTeacherNumber());
+		}
+		return resultado;
+	}
 	/**
 	 * @return List
 	 */
 	public List getProfessorShipsExecutionCourses() {
 		return professorShipsExecutionCourses;
 	}
-
 	/**
 	 * @return List
 	 */
 	public List getResponsibleForExecutionCourses() {
 		return responsibleForExecutionCourses;
 	}
-
 	/**
 	 * @return Integer
 	 */
 	public Integer getTeacherNumber() {
 		return teacherNumber;
 	}
-
 	/**
 	 * Sets the professorShipsExecutionCourses.
 	 * @param professorShipsExecutionCourses The professorShipsExecutionCourses to set
@@ -62,7 +59,6 @@ public class Teacher extends DomainObject implements ITeacher {
 	public void setProfessorShipsExecutionCourses(List professorShipsExecutionCourses) {
 		this.professorShipsExecutionCourses = professorShipsExecutionCourses;
 	}
-
 	/**
 	 * Sets the responsableForExecutionCourses.
 	 * @param responsableForExecutionCourses The responsableForExecutionCourses to set
@@ -70,7 +66,6 @@ public class Teacher extends DomainObject implements ITeacher {
 	public void setResponsibleForExecutionCourses(List responsableForExecutionCourses) {
 		this.responsibleForExecutionCourses = responsableForExecutionCourses;
 	}
-
 	/**
 	 * Sets the teacherNumber.
 	 * @param teacherNumber The teacherNumber to set
@@ -78,21 +73,18 @@ public class Teacher extends DomainObject implements ITeacher {
 	public void setTeacherNumber(Integer teacherNumber) {
 		this.teacherNumber = teacherNumber;
 	}
-
 	/**
 	 * @return Integer
 	 */
 	public Integer getKeyPerson() {
 		return keyPerson;
 	}
-
 	/**
 	 * @return IPessoa
 	 */
 	public IPessoa getPerson() {
 		return person;
 	}
-
 	/**
 	 * Sets the keyPerson.
 	 * @param keyPerson The keyPerson to set
@@ -100,7 +92,6 @@ public class Teacher extends DomainObject implements ITeacher {
 	public void setKeyPerson(Integer keyPerson) {
 		this.keyPerson = keyPerson;
 	}
-
 	/**
 	 * Sets the person.
 	 * @param person The person to set
@@ -108,5 +99,13 @@ public class Teacher extends DomainObject implements ITeacher {
 	public void setPerson(IPessoa person) {
 		this.person = person;
 	}
-
+	public String toString() {
+		String result = "[Dominio.Teacher ";
+		result += ", teacherNumber=" + getTeacherNumber();
+		result += ", person=" + getPerson();
+		result += ", professorShipsExecutionCourses=" + getProfessorShipsExecutionCourses();
+		result += ", responsibleForExecutionCourses=" + getResponsibleForExecutionCourses();		
+		result += "]";
+		return null;
+	}
 }
