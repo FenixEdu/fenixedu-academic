b0a4a195975690d277e636883510d4d92626aacf
João Luz <joao.luz@ist.utl.pt>
2003-03-13 10:15:20 +0000

    Roles added

--- src/ServidorAplicacao/Servico/UserView.java
+++ src/ServidorAplicacao/Servico/UserView.java
@@ -1,5 +1,6 @@
 package ServidorAplicacao.Servico;
 
+import java.util.Collection;
 import java.util.Set;
 
 import ServidorAplicacao.IUserView;
@@ -8,27 +9,43 @@ import ServidorAplicacao.IUserView;
  * @author jorge
  */
 public class UserView implements IUserView {
-  private String _utilizador;
-  private Set _privilegios;
+	private String utilizador;
+	private Set _privilegios;
+	private Collection roles;
 
-  public UserView(String utilizador, Set privilegios) {
-    setUtilizador(utilizador);
-    setPrivilegios(privilegios);
-  }
+	public UserView(String utilizador, Set privilegios) {
+		setUtilizador(utilizador);
+		setPrivilegios(privilegios);
+	}
 
-  public String getUtilizador() {
-    return _utilizador;
-  }
+	public String getUtilizador() {
+		return utilizador;
+	}
 
-  public Set getPrivilegios() {
-    return _privilegios;
-  }
-  
-  public void setUtilizador(String utilizador) {
-    _utilizador = utilizador;
-  }
-  
-  public void setPrivilegios(Set privilegios) {
-    _privilegios = privilegios;
-  }
+	public Set getPrivilegios() {
+		return _privilegios;
+	}
+
+	public void setUtilizador(String utilizador) {
+		this.utilizador = utilizador;
+	}
+
+	public void setPrivilegios(Set privilegios) {
+		_privilegios = privilegios;
+	}
+	/**
+	 * @return Collection
+	 */
+	public Collection getRoles() {
+		return roles;
+	}
+
+	/**
+	 * Sets the roles.
+	 * @param roles The roles to set
+	 */
+	public void setRoles(Collection roles) {
+		this.roles = roles;
+	}
+
 }
