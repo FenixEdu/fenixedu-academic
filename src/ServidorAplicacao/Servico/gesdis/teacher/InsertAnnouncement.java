c9a184eb6fd93d9c759db9bef4e23e04b7feb758
Ivo Brandão <imbr@mega.ist.utl.pt>
2003-03-13 14:54:43 +0000

    GesDis

---
+++ src/ServidorAplicacao/Servico/gesdis/teacher/InsertAnnouncement.java
@@ -0,0 +1,135 @@
+/*
+ * InserirAnuncio.java
+ */
+package ServidorAplicacao.Servico.gesdis.teacher;
+/**
+ * @author  EP15
+ * @author Ivo Brandão
+ */
+import java.util.Calendar;
+import java.util.Date;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoExecutionYear;
+import DataBeans.gesdis.InfoAnnouncement;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
+import Dominio.Announcement;
+import Dominio.IAnnouncement;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import Dominio.ISite;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentAnnouncement;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.IPersistentSite;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+public class InsertAnnouncement implements IServico {
+
+    private ISuportePersistente persistentSupport = null;
+    private IPersistentExecutionYear persistentExecutionYear = null;
+    private IPersistentExecutionPeriod persistentExecutionPeriod = null;
+    private IDisciplinaExecucaoPersistente persistentExecutionCourse = null;
+	private IPersistentSite persistentSite = null;
+	private IPersistentAnnouncement persistentAnnouncement = null;
+    private static InsertAnnouncement service = new InsertAnnouncement();
+    /**
+     * The singleton access method of this class.
+     */
+    public static InsertAnnouncement getService() {
+        return service;
+    }
+    /**
+     * The constructor of this class.
+     */
+    private InsertAnnouncement() { }
+    /**
+     * The name of the service
+     */
+    public final String getNome() {
+        return "Teacher.InsertAnnouncement";
+    }
+
+    private void checkIfAnnouncementExists(String announcementTitle, ISite announcementSite, Date currentDate) throws FenixServiceException {
+        IAnnouncement announcement = null;
+        persistentAnnouncement = persistentSupport.getIPersistentAnnouncement();
+
+		try {
+		announcement =
+			persistentAnnouncement
+				.readAnnouncementByTitleAndCreationDateAndSite(
+				announcementTitle,
+				currentDate,
+				announcementSite);
+		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
+			throw new FenixServiceException(excepcaoPersistencia.getMessage());
+		}
+		
+        if (announcement != null) throw new ExistingServiceException();
+    }
+    /**
+     * Executes the service.
+     */
+    public InfoAnnouncement run (InfoSite infoSite, String newAnnouncementTitle, String newAnnouncementInformation) throws FenixServiceException {
+		ISite site = null;
+
+		//retrieve current date
+		Calendar calendar = Calendar.getInstance();
+		
+		try {
+			persistentSupport = SuportePersistenteOJB.getInstance();			
+		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
+			throw new FenixServiceException(excepcaoPersistencia.getMessage());
+		}
+
+        persistentExecutionYear = persistentSupport.getIPersistentExecutionYear();
+        persistentExecutionPeriod = persistentSupport.getIPersistentExecutionPeriod();
+        persistentExecutionCourse = persistentSupport.getIDisciplinaExecucaoPersistente();
+        persistentSite = persistentSupport.getIPersistentSite();
+
+		InfoExecutionCourse infoExecutionCourse = infoSite.getInfoExecutionCourse();
+		InfoExecutionPeriod infoExecutionPeriod = infoExecutionCourse.getInfoExecutionPeriod();
+		InfoExecutionYear infoExecutionYear = infoExecutionPeriod.getInfoExecutionYear();
+
+		try {
+			IExecutionYear executionYear = 
+				persistentExecutionYear.readExecutionYearByName(infoExecutionYear.getYear());
+			IExecutionPeriod executionPeriod = 
+				persistentExecutionPeriod.readByNameAndExecutionYear(infoExecutionPeriod.getName(), executionYear);
+			IDisciplinaExecucao executionCourse = 
+				persistentExecutionCourse.readByExecutionCourseInitialsAndExecutionPeriod(infoExecutionCourse.getSigla(), 
+					executionPeriod); 
+			site = persistentSite.readByExecutionCourse(executionCourse);
+		} catch (ExcepcaoPersistencia excepcaoPersistencia){
+			throw new FenixServiceException(excepcaoPersistencia.getMessage());
+		}
+		
+		checkIfAnnouncementExists(newAnnouncementTitle, site, calendar.getTime());
+
+		IAnnouncement newAnnouncement = null;
+		try {
+			newAnnouncement =
+				new Announcement(
+					null,
+					newAnnouncementTitle,
+					calendar.getTime(),
+					null,
+					newAnnouncementInformation,
+					site,
+					null);
+			persistentAnnouncement.lockWrite(newAnnouncement);
+		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
+			throw new FenixServiceException(excepcaoPersistencia.getMessage());
+		}
+        
+		return Cloner.copyIAnnouncement2InfoAnnouncement(newAnnouncement);
+    }
+}
