134716d95aca5ca6a515ee14740eb0e0220a1d18
João Luz <joao.luz@ist.utl.pt>
2003-03-14 16:00:03 +0000

    convert integer to RoleType

---
+++ src/ServidorPersistente/Conversores/RoleTypeFieldConversion.java
@@ -0,0 +1,56 @@
+/*
+ * Created on 11/Mar/2003 by jpvl
+ *
+ */
+package ServidorPersistente.Conversores;
+
+import org.apache.ojb.broker.accesslayer.conversions.ConversionException;
+import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
+
+import Util.RoleType;
+
+/**
+ * @author jpvl
+ */
+public class RoleTypeFieldConversion implements FieldConversion {
+
+	/* (non-Javadoc)
+	 * @see org.apache.ojb.broker.accesslayer.conversions.FieldConversion#javaToSql(java.lang.Object)
+	 */
+	public Object javaToSql(Object obj) throws ConversionException {
+		if (obj instanceof RoleType) {
+			RoleType roleType = (RoleType) obj;
+			return new Integer(roleType.getId());
+		}
+		return obj;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.ojb.broker.accesslayer.conversions.FieldConversion#sqlToJava(java.lang.Object)
+	 */
+	public Object sqlToJava(Object obj) throws ConversionException {
+		RoleType roleType = null;
+		if (obj instanceof Integer) {
+			Integer roleTypeId = (Integer) obj;
+			switch (roleTypeId.intValue()) {
+				case RoleType.PERSON_TYPE :
+					roleType = RoleType.PERSON;
+					break;
+				case RoleType.STUDENT_TYPE :
+					roleType = RoleType.STUDENT;
+					break;
+				case RoleType.TEACHER_TYPE :
+					roleType = RoleType.TEACHER;
+					break;
+				case RoleType.TIME_TABLE_MANAGER_TYPE :
+					roleType = RoleType.TIME_TABLE_MANAGER;
+
+			}
+		}else{
+			throw new IllegalArgumentException ("Illegal role type!("+obj+")");
+		}
+		return roleType;
+
+	}
+
+}
