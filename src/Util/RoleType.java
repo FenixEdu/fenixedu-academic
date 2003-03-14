91f13108962cb3a200528e9c94e619c8bc256c15
João Luz <joao.luz@ist.utl.pt>
2003-03-14 16:05:35 +0000

    RoleType defines the existing role types

---
+++ src/Util/RoleType.java
@@ -0,0 +1,43 @@
+/*
+ * Created on 11/Mar/2003 by jpvl
+ *
+ */
+package Util;
+
+/**
+ * @author jpvl
+ */
+public class RoleType {
+	/* must match with field ROLE_TYPE in table ROLE */
+	public static final int PERSON_TYPE = 1;
+	public static final int STUDENT_TYPE = 2;
+	public static final int TEACHER_TYPE = 3;
+	public static final int TIME_TABLE_MANAGER_TYPE = 4;
+	
+	public static final RoleType PERSON = new RoleType(RoleType.PERSON_TYPE);
+	public static final RoleType STUDENT = new RoleType(RoleType.STUDENT_TYPE);
+	public static final RoleType TEACHER = new RoleType(RoleType.TEACHER_TYPE);
+	public static final RoleType TIME_TABLE_MANAGER = new RoleType(RoleType.TIME_TABLE_MANAGER_TYPE);
+	
+	private int id;
+
+	private RoleType (int id){
+		this.id = id;
+	}
+	
+	public RoleType(RoleType roleType) {
+		this.id = roleType.getId();
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	public boolean equals(Object obj) {
+		return (
+			(obj instanceof RoleType) && (((RoleType) obj).getId() == this.id));
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+}
