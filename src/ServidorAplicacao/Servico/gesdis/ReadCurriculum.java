d3e773227be9ae59d9fae0b3266539e2ae8cda49
João Mota <joao.mota@ist.utl.pt>
2003-03-14 13:11:53 +0000

    added readCurriculum

---
+++ src/ServidorAplicacao/Servico/gesdis/ReadCurriculum.java
@@ -0,0 +1,77 @@
+package ServidorAplicacao.Servico.gesdis;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.gesdis.InfoCurriculum;
+import DataBeans.util.Cloner;
+import Dominio.ICurriculum;
+import Dominio.IDisciplinaExecucao;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.IServico;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+public class ReadCurriculum implements IServico {
+
+	private static ReadCurriculum service = new ReadCurriculum();
+
+	/**
+	 * The singleton access method of this class.
+	 **/
+
+	public static ReadCurriculum getService() {
+
+		return service;
+
+	}
+
+	/**
+	 * The ctor of this class.
+	 **/
+
+	private ReadCurriculum() {
+
+	}
+
+	/**
+	 * Returns the name of this service.
+	 **/
+
+	public final String getNome() {
+
+		return "ReadCurriculum";
+
+	}
+
+	public InfoCurriculum run(InfoExecutionCourse infoExecutionCourse)
+		throws FenixServiceException {
+
+		try {
+			ICurriculum curriculum = null;
+			IDisciplinaExecucao executionCourse =
+				Cloner.copyInfoExecutionCourse2ExecutionCourse(
+					infoExecutionCourse);
+
+			ISuportePersistente sp;
+
+			sp = SuportePersistenteOJB.getInstance();
+
+			curriculum =
+				sp.getIPersistentCurriculum().readCurriculumByExecutionCourse(
+					executionCourse);
+
+			
+
+			InfoCurriculum infoCurriculum =null;
+			if (curriculum!=null){
+				infoCurriculum =Cloner.copyICurriculum2InfoCurriculum(curriculum);
+			}
+
+			return infoCurriculum;
+		} catch (ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		}
+
+	}
+
+}
