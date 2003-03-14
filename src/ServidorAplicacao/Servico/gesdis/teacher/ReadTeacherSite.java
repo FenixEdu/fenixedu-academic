4b33b2b085dc9f0ed1eabc312fd984de4472c101
João Mota <joao.mota@ist.utl.pt>
2003-03-14 17:52:35 +0000

    one more done

---
+++ src/ServidorAplicacao/Servico/gesdis/teacher/ReadTeacherSite.java
@@ -0,0 +1,83 @@
+package ServidorAplicacao.Servico.gesdis.teacher;
+
+/**
+ *
+ * @author  EP 15
+ */
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
+import Dominio.IDisciplinaExecucao;
+import Dominio.ISite;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.IServico;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+public class ReadTeacherSite implements IServico {
+
+	private static ReadTeacherSite service = new ReadTeacherSite();
+
+	/**
+	 * The singleton access method of this class.
+	 **/
+
+	public static ReadTeacherSite getService() {
+
+		return service;
+
+	}
+
+	/**
+	 * The ctor of this class.
+	 **/
+
+	private ReadTeacherSite() {
+	}
+
+	/**
+	 * Devolve o nome do servico
+	 **/
+
+	public final String getNome() {
+
+		return "ReadTeacherSite";
+
+	}
+
+	/**
+	 * Executes the service. Returns the current collection of
+	 * sitios names.
+	 *
+	 * @throws ExcepcaoInexistente is there is none sitio.
+	 **/
+
+	public InfoSite run(InfoExecutionCourse infoExecutionCourse)
+		throws FenixServiceException {
+
+		//   List allSitios = ((UserViewDocente) user).getSitios();
+
+		try {
+			ISite site = null;
+			
+			ISuportePersistente sp;
+			
+			sp = SuportePersistenteOJB.getInstance();
+			IDisciplinaExecucao executionCourse =
+				Cloner.copyInfoExecutionCourse2ExecutionCourse(infoExecutionCourse);
+			site = sp.getIPersistentSite().readByExecutionCourse(executionCourse);
+			InfoSite  infoSite=null;
+			if (site != null){
+				infoSite = Cloner.copyISite2InfoSite(site);
+			}
+					
+			
+			return infoSite;
+		} catch (ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		} 
+	}
+
+}
