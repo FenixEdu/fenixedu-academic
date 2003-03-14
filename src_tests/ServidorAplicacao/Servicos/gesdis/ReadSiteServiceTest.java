c85c94f0493524615d430ef729accc1edd9df7a3
João Mota <joao.mota@ist.utl.pt>
2003-03-14 15:30:34 +0000

    it runs

---
+++ src_tests/ServidorAplicacao/Servicos/gesdis/ReadSiteServiceTest.java
@@ -0,0 +1,149 @@
+/*
+ * Created on 14/Mar/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package ServidorAplicacao.Servicos.gesdis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.gesdis.InfoSection;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jmota
+ */
+public class ReadSiteServiceTest extends TestCaseReadServices {
+
+	/**
+	 * @param testName
+	 */
+	public ReadSiteServiceTest(String testName) {
+		super(testName);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "ReadSite";
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		// TODO Auto-generated method stub
+		return null;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		ISuportePersistente sp = null;
+		IExecutionYear executionYear = null;
+		IExecutionPeriod executionPeriod = null;
+		IDisciplinaExecucao executionCourse = null;
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+			sp.iniciarTransaccao();
+
+			IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+			executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+			IPersistentExecutionPeriod iepp =
+				sp.getIPersistentExecutionPeriod();
+
+			executionPeriod =
+				iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+			IDisciplinaExecucaoPersistente idep =
+				sp.getIDisciplinaExecucaoPersistente();
+			executionCourse =
+				idep.readByExecutionCourseInitialsAndExecutionPeriod(
+					"TFCI",
+					executionPeriod);
+
+			sp.confirmarTransaccao();
+		} catch (ExcepcaoPersistencia e) {
+			System.out.println("failed setting up the test data");
+		}
+
+		InfoExecutionCourse infoExecutionCourse =
+			Cloner.copyIExecutionCourse2InfoExecutionCourse(executionCourse);
+		Object[] args = { infoExecutionCourse };
+		return args;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
+	 */
+	protected int getNumberOfItemsToRetrieve() {
+
+		return 0;
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
+	 */
+	protected Object getObjectToCompare() {
+		ISuportePersistente sp = null;
+		IExecutionYear executionYear = null;
+		IExecutionPeriod executionPeriod = null;
+		IDisciplinaExecucao executionCourse = null;
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+			sp.iniciarTransaccao();
+
+			IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+			executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+			IPersistentExecutionPeriod iepp =
+				sp.getIPersistentExecutionPeriod();
+
+			executionPeriod =
+				iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+			IDisciplinaExecucaoPersistente idep =
+				sp.getIDisciplinaExecucaoPersistente();
+			executionCourse =
+				idep.readByExecutionCourseInitialsAndExecutionPeriod(
+					"TFCI",
+					executionPeriod);
+
+			sp.confirmarTransaccao();
+		} catch (ExcepcaoPersistencia e) {
+			System.out.println("failed setting up the test data");
+		}
+
+		InfoExecutionCourse infoExecutionCourse =
+			Cloner.copyIExecutionCourse2InfoExecutionCourse(executionCourse);
+		List infoSections = new ArrayList();	
+		InfoSite infoSite = new InfoSite( infoExecutionCourse);
+		infoSections.add(new InfoSection( "Seccao1deTFCI", new Integer(0), infoSite, null, null, null));
+		infoSite.setSections(infoSections);
+		return infoSite;
+
+	}
+
+}
