6794dd759d5b55761ea6b19c87f5880fcc508d2f
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-03-15 21:39:36 +0000

    *** empty log message ***

---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/CreateMasterDegreeCandidateServiceTest.java
@@ -0,0 +1,100 @@
+
+/*
+ * CriarSalaServicosTest.java
+ * JUnit based test
+ *
+ * Created on 24 de Outubro de 2002, 12:00
+ */
+
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice;
+
+/**
+ *
+ * @author Nuno Nunes & Joana Mota 
+ */
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import DataBeans.InfoMasterDegreeCandidate;
+import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
+import ServidorAplicacao.Servicos.TestCaseServicos;
+import Util.Specialization;
+import Util.TipoDocumentoIdentificacao;
+
+public class CreateMasterDegreeCandidateServiceTest extends TestCaseServicos {
+
+	public CreateMasterDegreeCandidateServiceTest(java.lang.String testName) {
+		super(testName);
+	}
+    
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+    
+	public static Test suite() {
+		TestSuite suite = new TestSuite(CreateMasterDegreeCandidateServiceTest.class);
+        
+		return suite;
+	}
+    
+	protected void setUp() {
+		super.setUp();
+        
+	}
+    
+	protected void tearDown() {
+		super.tearDown();
+	}
+	public void testReadMasterDegreeCandidateNonExisting() {
+		
+		InfoMasterDegreeCandidate infoMasterDegreeCandidate = new InfoMasterDegreeCandidate();
+		infoMasterDegreeCandidate.setName("Nuno Nunes");
+		infoMasterDegreeCandidate.setIdentificationDocumentNumber("1234567788");
+		infoMasterDegreeCandidate.setInfoIdentificationDocumentType(TipoDocumentoIdentificacao.BILHETE_DE_IDENTIDADE_DA_FORCA_AEREA_STRING);
+		infoMasterDegreeCandidate.setSpecialization(Specialization.MESTRADO_STRING);
+				
+		Object createCandidateArgs[] = new Object[2];
+		createCandidateArgs[0] = infoMasterDegreeCandidate;
+		createCandidateArgs[1] = "Licenciatura de Engenharia Electrotecnica e de Computadores";
+	
+		InfoMasterDegreeCandidate result = null; 
+		
+		try {
+			result = (InfoMasterDegreeCandidate) _gestor.executar(_userView, "CreateMasterDegreeCandidate", createCandidateArgs);
+			assertNotNull(result);
+		} catch (Exception ex) {
+			System.out.println("Service Not Executed: " + ex);
+		} 
+		
+		assertEquals(infoMasterDegreeCandidate.getName(), result.getName());
+		assertEquals(infoMasterDegreeCandidate.getIdentificationDocumentNumber(), result.getIdentificationDocumentNumber());
+		assertEquals(infoMasterDegreeCandidate.getInfoIdentificationDocumentType(), result.getInfoIdentificationDocumentType());
+		assertEquals(infoMasterDegreeCandidate.getSpecialization(), result.getSpecialization());
+		assertEquals(result.getCandidateNumber(), new Integer(3));
+		
+	}
+	
+	public void testReadMasterDegreeCandidateExisting() {
+		
+		InfoMasterDegreeCandidate infoMasterDegreeCandidate = new InfoMasterDegreeCandidate();
+		infoMasterDegreeCandidate.setName("Nuno Nunes");
+		infoMasterDegreeCandidate.setIdentificationDocumentNumber("112233");
+		infoMasterDegreeCandidate.setInfoIdentificationDocumentType(TipoDocumentoIdentificacao.BILHETE_DE_IDENTIDADE_STRING);
+		infoMasterDegreeCandidate.setSpecialization(Specialization.MESTRADO_STRING);
+				
+		Object createCandidateArgs[] = new Object[2];
+		createCandidateArgs[0] = infoMasterDegreeCandidate;
+		createCandidateArgs[1] = "Licenciatura de Engenharia Electrotecnica e de Computadores";
+	
+		InfoMasterDegreeCandidate result = null; 
+		
+		try {
+			result = (InfoMasterDegreeCandidate) _gestor.executar(_userView, "CreateMasterDegreeCandidate", createCandidateArgs);
+		} catch (ExistingServiceException ex) {
+			System.out.println("Caught expected Error");
+		} catch (Exception ex) {
+			fail("Error in testReadMasterDegreeCandidateExisting ");
+		} 
+				
+	}
+
+}
---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/ReadMasterDegreesServiceTest.java
@@ -0,0 +1,71 @@
+/*
+ * ReadMasterDegreeCandidateByUsernameTest.java
+ *
+ * Created on 06 de Dezembro de 2002, 18:51
+ *
+ * Tests :
+ * 
+ * - 1 : Read an existing Master Degree Candidate
+ * - 2 : Read an non existing Master Degree Candidate
+ * 
+ */
+
+/**
+ *
+ * Autores : 
+ *   - Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ *   - Joana Mota (jccm@rnl.ist.utl.pt)
+ *
+ */
+
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice;
+
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ServidorAplicacao.Servicos.TestCaseServicos;
+
+public class ReadMasterDegreesServiceTest extends TestCaseServicos {
+    
+    public ReadMasterDegreesServiceTest(java.lang.String testName) {
+        super(testName);
+    }
+    
+    public static void main(java.lang.String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ReadMasterDegreesServiceTest.class);
+        
+        return suite;
+    }
+    
+    protected void setUp() {
+        super.setUp();
+        
+    }
+    
+    protected void tearDown() {
+        super.tearDown();
+    }
+    public void testReadMasterDegreeCandidateExisting() {
+        System.out.println("- Test 1 : Read Master Degrees");
+        List masterDegrees = null;
+
+        try {
+            masterDegrees = (List) _gestor.executar(_userView, "ReadMasterDegrees", null);
+        } catch (Exception ex) {
+            System.out.println("Service Not Executed: " + ex);
+        }   
+        
+        
+        assertNotNull(masterDegrees);
+        assertTrue(!masterDegrees.isEmpty());
+        assertEquals(masterDegrees.size(), 1);
+
+   }
+    
+}
+
