6af490f65a24ea69d9158974595a3e6c72c06baf
João Mota <joao.mota@ist.utl.pt>
2003-03-17 12:24:59 +0000

    test to read bibliographic reference

---
+++ src_tests/ServidorAplicacao/Servicos/gesdis/teacher/ReadBibliographicReferenceTest.java
@@ -0,0 +1,96 @@
+/*
+ * Created on 17/Mar/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorAplicacao.Servicos.gesdis.teacher;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.gesdis.InfoBibliographicReference;
+import DataBeans.util.Cloner;
+import Dominio.IBibliographicReference;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentBibliographicReference;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author PTRLV
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ReadBibliographicReferenceTest extends TestCaseReadServices{
+
+		public ReadBibliographicReferenceTest(String testName) {
+			super(testName);		
+		}
+
+		protected String getNameOfServiceToBeTested() {
+			return "ReadBibliographicReference";
+		}
+
+		protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+			ISuportePersistente sp = null;
+			IExecutionYear executionYear = null;
+			IExecutionPeriod executionPeriod = null;
+			IDisciplinaExecucao executionCourse = null;
+			IBibliographicReference biblioRef = null;
+			try {
+				sp = SuportePersistenteOJB.getInstance();
+				sp.iniciarTransaccao();
+
+				IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+				executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+				IPersistentExecutionPeriod iepp =
+					sp.getIPersistentExecutionPeriod();
+				executionPeriod =
+					iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+				IDisciplinaExecucaoPersistente idep =
+					sp.getIDisciplinaExecucaoPersistente();
+				executionCourse =
+					idep.readByExecutionCourseInitialsAndExecutionPeriod(
+						"TFCI",
+						executionPeriod);
+
+				IPersistentBibliographicReference ipbr =
+					sp.getIPersistentBibliographicReference();
+				biblioRef =
+					ipbr.readBibliographicReference(
+						executionCourse,
+						"xpto",
+						"pedro",
+						"ref",
+						"2002");
+				sp.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				System.out.println("failed setting up the test data");
+				e.printStackTrace();
+			}
+
+			InfoExecutionCourse infoExecutionCourse =
+				Cloner.copyIExecutionCourse2InfoExecutionCourse(executionCourse);
+			InfoBibliographicReference infoBiblioRef =
+				Cloner.copyIBibliographicReference2InfoBibliographicReference(
+					biblioRef);		
+			Object[] testArgs = { infoExecutionCourse, new Boolean(false)};
+			return testArgs;		
+		}
+	
+		protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+			return null;
+		}
+	
+		protected int getNumberOfItemsToRetrieve() {return 2;}
+		protected Object getObjectToCompare() {return null;}
+}
