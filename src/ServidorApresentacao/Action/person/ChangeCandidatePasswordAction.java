e17502eb709b32a7c1f1ff7bad2bb8c0d841ed8c
João Luz <joao.luz@ist.utl.pt>
2003-03-17 19:28:39 +0000

    Portal person

---
+++ src/ServidorApresentacao/Action/person/ChangeCandidatePasswordAction.java
@@ -0,0 +1,59 @@
+/*
+ * Created on 13/Mar/2003
+ *
+ */
+package ServidorApresentacao.Action.person;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
+
+import DataBeans.InfoMasterDegreeCandidate;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.exceptions.InvalidPasswordServiceException;
+import ServidorApresentacao.Action.exceptions.InvalidPasswordActionException;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
+import ServidorApresentacao.Action.sop.utils.SessionUtils;
+
+/**
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ * @author Joana Mota (jccm@rnl.ist.utl.pt)
+ */
+public class ChangeCandidatePasswordAction extends ServidorApresentacao.Action.FenixAction {
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+									HttpServletRequest request,
+									HttpServletResponse response)
+		  throws Exception {
+
+	    SessionUtils.validSessionVerification(request, mapping);
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			DynaActionForm changePasswordForm = (DynaActionForm) form;
+			IUserView userView = (IUserView) session.getAttribute("UserView");
+			String oldPassword = (String) changePasswordForm.get("oldPassword");
+			String newPassword = (String) changePasswordForm.get("newPassword");
+
+			// Check the old Password
+			
+			InfoMasterDegreeCandidate masterDegreeCandidate = null;
+		  
+			Object args[] = { userView, oldPassword, newPassword };
+		  
+			try {
+				ServiceUtils.executeService(userView, "ChangePasswordService", args);
+			} catch (InvalidPasswordServiceException e) {
+				throw new InvalidPasswordActionException(e);
+			} 
+		  return mapping.findForward("Success");
+		} else
+		  throw new Exception();   
+	  }
+
+}
