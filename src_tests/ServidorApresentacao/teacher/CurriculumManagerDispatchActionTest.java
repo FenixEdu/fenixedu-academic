51c524861207a095e5a39f31de5802309254198e
João Mota <joao.mota@ist.utl.pt>
2003-03-18 17:02:50 +0000

    *** empty log message ***

---
+++ src_tests/ServidorApresentacao/teacher/CurriculumManagerDispatchActionTest.java
@@ -0,0 +1,22 @@
+/*
+ * Created on 18/Mar/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorApresentacao.teacher;
+
+import ServidorApresentacao.TestCaseActionExecution;
+
+/**
+ * @author jmota
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class CurriculumManagerDispatchActionTest
+	extends TestCase {
+
+	
+
+}
