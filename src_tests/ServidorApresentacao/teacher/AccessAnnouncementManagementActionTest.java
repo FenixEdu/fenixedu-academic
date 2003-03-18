9a15c671d55a0a0a9bc1f6f4581028a1cfe3de79
Ivo Brandão <imbr@mega.ist.utl.pt>
2003-03-18 09:53:58 +0000

    GesDis

---
+++ src_tests/ServidorApresentacao/teacher/AccessAnnouncementManagementActionTest.java
@@ -0,0 +1,148 @@
+package ServidorApresentacao.teacher;
+  
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoExecutionYear;
+import DataBeans.gesdis.InfoSection;
+import DataBeans.gesdis.InfoSite;
+import ServidorApresentacao.TestCaseActionExecution;
+
+/**
+ * AccessAnnouncementManagementTest.java
+ * 
+ * @author Ivo Brandão
+ */
+public class AccessAnnouncementManagementActionTest extends TestCaseActionExecution {
+
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AccessAnnouncementManagementActionTest.class);
+		return suite;
+	}
+	
+	public void setUp() {
+		super.setUp();
+	}
+
+	public AccessAnnouncementManagementActionTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInSessionForActionToBeTestedSuccessfuly()
+	 */
+	protected Map getItemsToPutInSessionForActionToBeTestedSuccessfuly() {
+
+		HashMap items = new HashMap();
+
+		InfoSection infoSection = new InfoSection();
+		InfoSite infoSite = new InfoSite();
+
+		//infoSection
+		infoSection = new InfoSection("Seccao1deTFCI", new Integer(0), null);
+
+		//infoSite
+		InfoExecutionYear infoExecutionYear = new InfoExecutionYear("2002/2003");
+		InfoExecutionPeriod infoExecutionPeriod = new InfoExecutionPeriod("2º Semestre", infoExecutionYear);
+		InfoExecutionCourse infoExecutionCourse =
+			new InfoExecutionCourse(
+				"Trabalho Final de Curso I",
+				"TFCI",
+				"programa1",
+				new Double(1.5),
+				new Double(2),
+				new Double(1.5),
+				new Double(2),
+				infoExecutionPeriod);
+		infoSite = new InfoSite(null, new ArrayList(), infoExecutionCourse, new ArrayList());
+		infoSection.setInfoSite(infoSite);
+		infoSite.setInitialInfoSection(infoSection);
+
+		items.put("Site", infoSite);
+
+		return items;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInSessionForActionToBeTestedUnsuccessfuly()
+	 */
+	protected Map getItemsToPutInSessionForActionToBeTestedUnsuccessfuly() {
+
+		HashMap items = new HashMap();
+
+		return items;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getExistingAttributesListToVerifyInSuccessfulExecution()
+	 */
+	protected Map getExistingAttributesListToVerifyInSuccessfulExecution() {
+		return null;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getExistingAttributesListToVerifyInUnsuccessfulExecution()
+	 */
+	protected Map getExistingAttributesListToVerifyInUnsuccessfulExecution() {
+		return null;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getNonExistingAttributesListToVerifyInSuccessfulExecution()
+	 */
+	protected Map getNonExistingAttributesListToVerifyInSuccessfulExecution() {
+		return null;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getNonExistingAttributesListToVerifyInUnsuccessfulExecution()
+	 */
+	protected Map getNonExistingAttributesListToVerifyInUnsuccessfulExecution() {
+		return null;
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoNameAction()
+	 */
+	protected String getRequestPathInfoNameAction() {
+		return "/teacher/accessAnnouncementManagementAction";
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoPathAction()
+	 */
+	protected String getRequestPathInfoPathAction() {
+		return "/gesdis";
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getServletConfigFile()
+	 */
+	protected String getServletConfigFile() {
+		return "/WEB-INF/tests/web-gesdis.xml";
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getSuccessfulForward()
+	 */
+	protected String getSuccessfulForward() {
+		return "AnnouncementManagement";
+	}
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#testUnsuccessfulExecutionOfAction()
+	 */
+	public void testUnsuccessfulExecutionOfAction() {
+		// TODO test
+	}
+
+}
