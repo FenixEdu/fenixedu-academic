82721291ee96a8295dc8f40487abd07f1b847a87
João Mota <joao.mota@ist.utl.pt>
2003-03-18 20:01:28 +0000

    runs

---
+++ src_tests/ServidorAplicacao/Servicos/gesdis/teacher/EditCurriculumServiceTest.java
@@ -0,0 +1,102 @@
+/*
+ * Created on 18/Mar/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorAplicacao.Servicos.gesdis.teacher;
+
+import DataBeans.gesdis.InfoCurriculum;
+import DataBeans.util.Cloner;
+import Dominio.ICurriculum;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentCurriculum;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jmota
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class EditCurriculumServiceTest extends TestCaseDeleteAndEditServices {
+
+	/**
+	 * @param testName
+	 */
+	public EditCurriculumServiceTest(String testName) {
+		super(testName);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "EditCurriculum";
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		InfoCurriculum oldCurriculum = new InfoCurriculum();
+		InfoCurriculum newCurriculum = new InfoCurriculum();
+		ISuportePersistente sp = null;
+		IExecutionYear executionYear = null;
+		IExecutionPeriod executionPeriod = null;
+		IDisciplinaExecucao executionCourse = null;
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+			sp.iniciarTransaccao();
+
+			IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+			executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+			IPersistentExecutionPeriod iepp =
+				sp.getIPersistentExecutionPeriod();
+
+			executionPeriod =
+				iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+			IDisciplinaExecucaoPersistente idep =
+				sp.getIDisciplinaExecucaoPersistente();
+			executionCourse =
+				idep.readByExecutionCourseInitialsAndExecutionPeriod(
+					"TFCI",
+					executionPeriod);
+			IPersistentCurriculum persistentCurriculum =
+				sp.getIPersistentCurriculum();
+
+			ICurriculum curriculum =
+				persistentCurriculum.readCurriculumByExecutionCourse(
+					executionCourse);
+			sp.confirmarTransaccao();
+
+			oldCurriculum = Cloner.copyICurriculum2InfoCurriculum(curriculum);
+			newCurriculum = Cloner.copyICurriculum2InfoCurriculum(curriculum);
+			newCurriculum.setGeneralObjectives("blablablabla");
+		} catch (ExcepcaoPersistencia e) {
+			System.out.println("failed setting up the test data");
+			e.printStackTrace();
+		}
+		Object[] args = { oldCurriculum, newCurriculum };
+		return args;
+	}
+
+}
