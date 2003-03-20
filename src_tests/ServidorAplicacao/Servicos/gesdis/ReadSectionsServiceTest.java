31d83d115df44af27cc3a1e850cb9c5e53a25e57
Hugo Pereira <peppe@clix.pt>
2003-03-20 09:39:35 +0000

    new service

---
+++ src_tests/ServidorAplicacao/Servicos/gesdis/ReadSectionsServiceTest.java
@@ -0,0 +1,111 @@
+/*
+ * Created on 20/Mar/2003
+ */
+package ServidorAplicacao.Servicos.gesdis;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import Dominio.ISite;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.IPersistentSite;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author lmac1
+ */
+public class ReadSectionsServiceTest extends TestCaseReadServices {
+	
+	/**
+		 * @param testName
+		 */
+		 public ReadSectionsServiceTest(String testName) {
+			super(testName);
+				}
+
+		/* (non-Javadoc)
+		 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+		 */
+		 protected String getNameOfServiceToBeTested() {
+			return "ReadSections";
+		   }
+       
+		public static void main(String[] args) {
+			junit.textui.TestRunner.run(suite());
+		  }
+    
+		  public static Test suite() {
+			TestSuite suite = new TestSuite(ReadSectionsServiceTest.class);
+
+			return suite;
+		  }
+
+		/* (non-Javadoc)
+		 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+		 */
+		protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+			return null;
+		   }
+
+		/* (non-Javadoc)
+		 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+		 */
+		protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+			ISuportePersistente sp = null;
+			IExecutionYear executionYear = null;
+			IExecutionPeriod executionPeriod = null;
+			IDisciplinaExecucao executionCourse = null;
+			ISite site = null;
+			try {
+					sp = SuportePersistenteOJB.getInstance();
+					sp.iniciarTransaccao();
+
+					IPersistentExecutionYear ipey = sp.getIPersistentExecutionYear();
+					executionYear = ipey.readExecutionYearByName("2002/2003");
+
+					IPersistentExecutionPeriod ipep = sp.getIPersistentExecutionPeriod();
+					executionPeriod = ipep.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+					IDisciplinaExecucaoPersistente idep = sp.getIDisciplinaExecucaoPersistente();
+					executionCourse = idep.readByExecutionCourseInitialsAndExecutionPeriod("PO",executionPeriod);
+
+					IPersistentSite ips = sp.getIPersistentSite();
+					site = ips.readByExecutionCourse(executionCourse);
+                
+					sp.confirmarTransaccao();
+				}
+			catch (ExcepcaoPersistencia e) {
+							System.out.println("failed setting up the test data");
+											}
+       
+			InfoSite infoSite = Cloner.copyISite2InfoSite(site);
+			Object[] args = { infoSite };
+			return args;
+			}
+
+	   /* (non-Javadoc)
+		* @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
+		*/
+			protected int getNumberOfItemsToRetrieve() {
+			return 4;
+			}
+        
+		/* (non-Javadoc)
+			 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
+			 */
+		protected Object getObjectToCompare() {
+		
+				return null;
+
+				}
+
+}
