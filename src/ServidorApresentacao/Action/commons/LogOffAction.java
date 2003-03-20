0d606e3c3456f3c9cf0523837c029220d6357c6d
João Luz <joao.luz@ist.utl.pt>
2003-03-20 20:43:33 +0000

    Removed session.setMaxInactiveInterval(0)...

--- src/ServidorApresentacao/Action/commons/LogOffAction.java
+++ src/ServidorApresentacao/Action/commons/LogOffAction.java
@@ -17,20 +17,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class LogOffAction extends ServidorApresentacao.Action.base.FenixAction {
+public class LogOffAction extends Action{
 
   public ActionForward execute(ActionMapping mapping, ActionForm form,
                                 HttpServletRequest request,
                                 HttpServletResponse response)
       throws Exception {
-
 	HttpSession session = request.getSession(false);
 	if (session != null){
-	  session.setMaxInactiveInterval(0);
       session.invalidate();
 	}
     return mapping.findForward("Success");
