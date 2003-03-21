e38ea64fb2566de3a9d6fa759e2f5158a5587ffb
Ivo Brandão <imbr@mega.ist.utl.pt>
2003-03-21 15:59:15 +0000

    *** empty log message ***

--- src_tests/ServidorApresentacao/teacher/AnnouncementManagementActionTest.java
+++ src_tests/ServidorApresentacao/teacher/AnnouncementManagementActionTest.java
@@ -1,10 +1,20 @@
 package ServidorApresentacao.teacher;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoExecutionYear;
+import DataBeans.gesdis.InfoAnnouncement;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
 import ServidorApresentacao.TestCasePresentationTeacherPortal;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 /**
  * @author Ivo Brandão
@@ -57,66 +67,78 @@ public class AnnouncementManagementActionTest extends TestCasePresentationTeache
 		//sets needed objects to session/request
 		addRequestParameter("method", "Guardar");
 
-//		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
-//		infoExecutionCourse.setNome("Trabalho Final de Curso I");
-//		infoExecutionCourse.setSigla("TFCI");
-//		infoExecutionCourse.setPrograma("programa1");
-//		infoExecutionCourse.setTheoreticalHours(new Double(1.5));
-//		infoExecutionCourse.setPraticalHours(new Double(2));
-//		infoExecutionCourse.setTheoPratHours(new Double(1.5));
-//		infoExecutionCourse.setLabHours(new Double(2));
-//		InfoExecutionPeriod iep =
-//			new InfoExecutionPeriod(
-//				"2º Semestre",
-//				new InfoExecutionYear("2002/2003"));
-//		infoExecutionCourse.setInfoExecutionPeriod(iep);
-//		getSession().setAttribute("InfoExecutionCourse", infoExecutionCourse);
-//
-//		Object args1[] = { infoExecutionCourse, null };
-//		GestorServicos gestor = GestorServicos.manager();
-//		UserView userView = (UserView) getSession().getAttribute("UserView");
-//		ArrayList references = null;
-//		try {
-//			references =
-//				(ArrayList) gestor.executar(
-//					userView,
-//					"ReadBibliographicReference",
-//					args1);
-//		} catch (Exception e) {
-//			System.out.println(e.toString());
-//		}
-//		InfoBibliographicReference infoBibRef =
-//			(InfoBibliographicReference) references.get(0);
-//
-//		getSession().setAttribute("BibliographicReference", infoBibRef);
-//		setAuthorizedUser();
-//		//fills the form
-//		addRequestParameter("title", "matemática");
-//		addRequestParameter("authors", "jose");
-//		addRequestParameter("reference", "ref4");
-//		addRequestParameter("year", "2002");
-//		addRequestParameter("optional", "0");
-//
-//		//action perform
-//		actionPerform();		
-//
-//		try {
-//			references =
-//				(ArrayList) gestor.executar(
-//					userView,
-//					"ReadBibliographicReference",
-//					args1);
-//		} catch (Exception e) {
-//			System.out.println(e.toString());
-//		}
-//
-//		InfoBibliographicReference infoBiblioRefVerify = (InfoBibliographicReference)references.get(0);		
-//		assertEquals(infoBiblioRefVerify.getTitle(),"matemática");
-//		assertEquals(infoBiblioRefVerify.getAuthors(),"jose");
-//		assertEquals(infoBiblioRefVerify.getReference(),"ref4");
-//		assertEquals(infoBiblioRefVerify.getYear(),"2002");		
-//		verifyForward("bibliographyManagement");
-//
+		//infoSite in session
+		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
+		infoExecutionCourse.setNome("Trabalho Final de Curso I");
+		infoExecutionCourse.setSigla("TFCI");
+		infoExecutionCourse.setPrograma("programa1");
+		infoExecutionCourse.setTheoreticalHours(new Double(1.5));
+		infoExecutionCourse.setPraticalHours(new Double(2));
+		infoExecutionCourse.setTheoPratHours(new Double(1.5));
+		infoExecutionCourse.setLabHours(new Double(2));
+		InfoExecutionPeriod iep =
+			new InfoExecutionPeriod(
+				"2º Semestre",
+				new InfoExecutionYear("2002/2003"));
+		infoExecutionCourse.setInfoExecutionPeriod(iep);
+		InfoSite infoSite = new InfoSite(infoExecutionCourse);
+		getSession().setAttribute("Site", infoSite);
+
+		//infoAnnouncement in session
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, 2003);
+		calendar.set(Calendar.MONTH, Calendar.JANUARY);
+		calendar.set(Calendar.DATE, 22);
+		Date creationDate = calendar.getTime();
+		calendar.set(Calendar.DATE, 24);
+		Date lastModifiedDate = calendar.getTime();
+		
+		InfoAnnouncement infoAnnouncement = null;
+
+		try {
+			SuportePersistenteOJB.getInstance().iniciarTransaccao();
+			infoAnnouncement = Cloner.copyIAnnouncement2InfoAnnouncement(SuportePersistenteOJB
+				.getInstance()
+				.getIPersistentAnnouncement()
+				.readAnnouncementByTitleAndCreationDateAndSite(
+					"announcement2deTFCI",
+					creationDate,
+					Cloner.copyInfoSite2ISite(infoSite)));
+			SuportePersistenteOJB.getInstance().confirmarTransaccao();					
+		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
+			fail("testAuthorizedEditAnnouncement - before alteration:" + excepcaoPersistencia.getMessage());
+		}
+
+		getSession().setAttribute("Announcement", infoAnnouncement);
+				
+//		fills the form
+		addRequestParameter("title", "newTitle");
+		addRequestParameter("information", "newInformation");
+
+//		action perform
+		actionPerform();		
+
+//		verify forward
+		verifyForward("accessAnnouncementManagement");
+
+//		post conditions
+		infoAnnouncement = null;
+
+		try {
+			SuportePersistenteOJB.getInstance().iniciarTransaccao();
+			infoAnnouncement = Cloner.copyIAnnouncement2InfoAnnouncement(SuportePersistenteOJB
+				.getInstance()
+				.getIPersistentAnnouncement()
+				.readAnnouncementByTitleAndCreationDateAndSite(
+					"newTitle",
+					creationDate,
+					Cloner.copyInfoSite2ISite(infoSite)));
+			SuportePersistenteOJB.getInstance().confirmarTransaccao();					
+		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
+			fail("testAuthorizedEditAnnouncement - after alteration:" + excepcaoPersistencia.getMessage());
+		}
+		assertNotNull(infoAnnouncement);
+
 	}
 
 	public void testAuthorizedPrepareEditAnnouncement() {
