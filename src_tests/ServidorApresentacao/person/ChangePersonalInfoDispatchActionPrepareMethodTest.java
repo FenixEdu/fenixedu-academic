065934c261a931fce83339c4b54fcb8a1a2a9a5b
João Luz <joao.luz@ist.utl.pt>
2003-03-23 19:56:35 +0000

    method prepare of action

---
+++ src_tests/ServidorApresentacao/person/ChangePersonalInfoDispatchActionPrepareMethodTest.java
@@ -0,0 +1,146 @@
+/*
+ * Created on 21/Mar/2003 by jpvl
+ *
+ */
+package ServidorApresentacao.person;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import ServidorApresentacao.ScopeConstants;
+import ServidorApresentacao.TestCaseActionPresentationPersonPortal;
+import ServidorApresentacao.Action.sop.utils.SessionConstants;
+
+/**
+ * @author jpvl
+ */
+public class ChangePersonalInfoDispatchActionPrepareMethodTest
+	extends TestCaseActionPresentationPersonPortal {
+	/**
+	 * Main method 
+	 * @param args
+	 */
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	/**
+	 * 
+	 * @return Test to be done
+	 */
+	public static Test suite() {
+		TestSuite suite = new TestSuite(ChangePersonalInfoDispatchActionPrepareMethodTest.class);
+		return suite;
+	}
+
+	/**
+	 * @param testName
+	 */
+	public ChangePersonalInfoDispatchActionPrepareMethodTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * Only needs SessionConstants.U_VIEW that is already in session 
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInSessionForActionToBeTestedSuccessfuly()
+	 */
+	protected Map getItemsToPutInSessionForActionToBeTestedSuccessfuly() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInSessionForActionToBeTestedUnsuccessfuly()
+	 */
+	protected Map getItemsToPutInSessionForActionToBeTestedUnsuccessfuly() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/**
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInRequestForActionToBeTestedSuccessfuly() 
+	 */
+	protected Map getItemsToPutInRequestForActionToBeTestedSuccessfuly() {
+		// TODO Auto-generated method stub
+		HashMap requestParameters = new HashMap();
+		requestParameters.put("method","prepare");
+		return requestParameters;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getItemsToPutInRequestForActionToBeTestedUnsuccessfuly()
+	 */
+	protected Map getItemsToPutInRequestForActionToBeTestedUnsuccessfuly() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getExistingAttributesListToVerifyInSuccessfulExecution()
+	 */
+	protected Map getExistingAttributesListToVerifyInSuccessfulExecution() {
+		HashMap attributes = new HashMap();
+		
+		List sessionAttributes = new ArrayList();
+		sessionAttributes.add(SessionConstants.PERSONAL_INFO_KEY);
+		sessionAttributes.add(SessionConstants.NATIONALITY_LIST_KEY);
+		sessionAttributes.add(
+			SessionConstants.MARITAL_STATUS_LIST_KEY);
+		sessionAttributes.add(
+			SessionConstants.IDENTIFICATION_DOCUMENT_TYPE_LIST_KEY);
+		sessionAttributes.add(SessionConstants.SEX_LIST_KEY);
+		sessionAttributes.add(SessionConstants.MONTH_DAYS_KEY);
+		sessionAttributes.add(SessionConstants.MONTH_LIST_KEY);
+		sessionAttributes.add(SessionConstants.YEARS_KEY);
+		sessionAttributes.add(SessionConstants.EXPIRATION_YEARS_KEY);
+		
+		
+		attributes.put(new Integer(ScopeConstants.SESSION),sessionAttributes);
+		return attributes;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getNonExistingAttributesListToVerifyInSuccessfulExecution()
+	 */
+	protected Map getNonExistingAttributesListToVerifyInSuccessfulExecution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getExistingAttributesListToVerifyInUnsuccessfulExecution()
+	 */
+	protected Map getExistingAttributesListToVerifyInUnsuccessfulExecution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getNonExistingAttributesListToVerifyInUnsuccessfulExecution()
+	 */
+	protected Map getNonExistingAttributesListToVerifyInUnsuccessfulExecution() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoPathAction()
+	 */
+	protected String getRequestPathInfoPathAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorApresentacao.TestCaseActionExecution#getRequestPathInfoNameAction()
+	 */
+	protected String getRequestPathInfoNameAction() {
+		// TODO Auto-generated method stub
+		return "prepareReady";
+	}
+
+}
