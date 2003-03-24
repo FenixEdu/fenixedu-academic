638a448cd639935e7d9cf91a8a9a4ce7c507dc39
David Santos <david.santos@ist.utl.pt>
2003-03-24 13:27:50 +0000

    *** empty log message ***

--- src/ServidorPersistente/ISuportePersistente.java
+++ src/ServidorPersistente/ISuportePersistente.java
@@ -11,57 +11,59 @@ package ServidorPersistente;
  * @author  ars
  */
 public interface ISuportePersistente {
-    public void iniciarTransaccao() throws ExcepcaoPersistencia;
-    public void confirmarTransaccao() throws ExcepcaoPersistencia;
-    public void cancelarTransaccao() throws ExcepcaoPersistencia;
+	public void iniciarTransaccao() throws ExcepcaoPersistencia;
+	public void confirmarTransaccao() throws ExcepcaoPersistencia;
+	public void cancelarTransaccao() throws ExcepcaoPersistencia;
 
-    //public IPessoaPersistente getIPessoaPersistente();
+	//public IPessoaPersistente getIPessoaPersistente();
 
-    public IAulaPersistente getIAulaPersistente();
-    public ISalaPersistente getISalaPersistente();
-    public ITurmaPersistente getITurmaPersistente();
-    public ITurnoPersistente getITurnoPersistente();
-    
-    public IFrequentaPersistente getIFrequentaPersistente();
-    public IPersistentEnrolment getIInscricaoPersistente();
-    public ITurmaTurnoPersistente getITurmaTurnoPersistente();    
-    public ITurnoAlunoPersistente getITurnoAlunoPersistente();
-    public ITurnoAulaPersistente getITurnoAulaPersistente();
-    
-    public IPersistentCurricularCourse getIPersistentCurricularCourse();
-    public IDisciplinaExecucaoPersistente getIDisciplinaExecucaoPersistente();
-    public IPessoaPersistente getIPessoaPersistente();
-    public IPersistentCountry getIPersistentCountry();
+	public IAulaPersistente getIAulaPersistente();
+	public ISalaPersistente getISalaPersistente();
+	public ITurmaPersistente getITurmaPersistente();
+	public ITurnoPersistente getITurnoPersistente();
 
-    public ICursoPersistente getICursoPersistente();
-    public ICursoExecucaoPersistente getICursoExecucaoPersistente();
-    
-    public IPersistentStudent getIPersistentStudent();
-    public IPersistentBibliographicReference getIPersistentBibliographicReference();
-    
-    public IDepartamentoPersistente getIDepartamentoPersistente();
-    public IDisciplinaDepartamentoPersistente getIDisciplinaDepartamentoPersistente();
-    public IPersistentDegreeCurricularPlan getIPersistentDegreeCurricularPlan();
-    public IStudentCurricularPlanPersistente getIStudentCurricularPlanPersistente();
+	public IFrequentaPersistente getIFrequentaPersistente();
+	public IPersistentEnrolment getIPersistentEnrolment();
+	public ITurmaTurnoPersistente getITurmaTurnoPersistente();
+	public ITurnoAlunoPersistente getITurnoAlunoPersistente();
+	public ITurnoAulaPersistente getITurnoAulaPersistente();
+
+	public IPersistentCurricularCourse getIPersistentCurricularCourse();
+	public IDisciplinaExecucaoPersistente getIDisciplinaExecucaoPersistente();
+	public IPessoaPersistente getIPessoaPersistente();
+	public IPersistentCountry getIPersistentCountry();
+
+	public ICursoPersistente getICursoPersistente();
+	public ICursoExecucaoPersistente getICursoExecucaoPersistente();
+
+	public IPersistentStudent getIPersistentStudent();
+	public IPersistentBibliographicReference getIPersistentBibliographicReference();
+
+	public IDepartamentoPersistente getIDepartamentoPersistente();
+	public IDisciplinaDepartamentoPersistente getIDisciplinaDepartamentoPersistente();
+	public IPersistentDegreeCurricularPlan getIPersistentDegreeCurricularPlan();
+	public IStudentCurricularPlanPersistente getIStudentCurricularPlanPersistente();
 
 	public IPersistentMasterDegreeCandidate getIPersistentMasterDegreeCandidate();
 	public IPersistentCandidateSituation getIPersistentCandidateSituation();
-	
+
 	public IPersistentExecutionPeriod getIPersistentExecutionPeriod();
-	public IPersistentExecutionYear getIPersistentExecutionYear();	
-	
+	public IPersistentExecutionYear getIPersistentExecutionYear();
+
 	public IPersistentSite getIPersistentSite();
 	public IPersistentSection getIPersistentSection();
 	public IPersistentItem getIPersistentItem();
 	public IPersistentAnnouncement getIPersistentAnnouncement();
 	public IPersistentCurriculum getIPersistentCurriculum();
-	
+
 	public IPersistentTeacher getIPersistentTeacher();
-	
+
 	public IPersistentExam getIPersistentExam();
-	
+
 	public IPersistentBranch getIPersistentBranch();
-	
+
 	public IPersistentCurricularYear getIPersistentCurricularYear();
 	public IPersistentContributor getIPersistentContributor();
+
+	public IPersistentCurricularSemester getIPersistentCurricularSemester();
 }
