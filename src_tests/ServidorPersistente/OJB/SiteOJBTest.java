8c160777a24d273554669be86da33e63e10c5908
Sara Oliveira <ssara@clix.pt>
2003-03-31 10:06:28 +0000

    testing all methods

--- src_tests/ServidorPersistente/OJB/SiteOJBTest.java
+++ src_tests/ServidorPersistente/OJB/SiteOJBTest.java
@@ -7,8 +7,11 @@
 package ServidorPersistente.OJB;
 
 
+import java.util.List;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import Dominio.DisciplinaExecucao;
 import Dominio.IDisciplinaExecucao;
 import Dominio.IExecutionPeriod;
 import Dominio.IExecutionYear;
@@ -55,13 +58,18 @@ public class SiteOJBTest extends TestCaseOJB{
 
   protected void setUp() {
 	  try {
-		
+		  
 		  persistentSupport = SuportePersistenteOJB.getInstance();		
+		  persistentSupport.iniciarTransaccao();
 		  persistentExecutionCourse = persistentSupport.getIDisciplinaExecucaoPersistente();
 		  persistentSite = persistentSupport.getIPersistentSite();
 		  persistentExecutionYear = persistentSupport.getIPersistentExecutionYear();
 		  persistentExecutionPeriod = persistentSupport.getIPersistentExecutionPeriod();
-	  
+		  executionCourse =persistentExecutionCourse.readBySiglaAndAnoLectivoAndSiglaLicenciatura(
+							"PO",
+							"2002/2003",
+							"LEEC");
+		persistentSupport.confirmarTransaccao();
 	  } catch (ExcepcaoPersistencia e) {
 		  e.printStackTrace();
 		  fail("Error");
@@ -76,57 +84,196 @@ public class SiteOJBTest extends TestCaseOJB{
 	
 	
 	/** Test of readByExecutionCourse method, of class ServidorPersistente.OJB.SiteOJB. */
-		
-		public void testReadByExecutionCourse() {
-			ISite site = null;
-			
+
+	public void testReadByExecutionCourse() {
+		ISite site = null;
+
 		// read existing execution Course
-			
-		try{
-			 persistentSupport.iniciarTransaccao();
-		 	 executionCourse = persistentExecutionCourse.readBySiglaAndAnoLectivoAndSiglaLicenciatura("PO","2002/2003","LEEC");	
-		 	 assertNotNull(executionCourse);
-		 	 persistentSupport.confirmarTransaccao();
-		
-			 } catch (ExcepcaoPersistencia ex) {
-		 	fail("    -> Failed Reading Existing Execution Course");
-		 }
-		
-		
+
+		try {
+			persistentSupport.iniciarTransaccao();
+			assertNotNull(executionCourse);
+			persistentSupport.confirmarTransaccao();
+
+		} catch (ExcepcaoPersistencia ex) {
+			fail("    -> Failed Reading Existing Execution Course");
+		}
+
 		// read existing Site
 		try {
+
+			persistentSupport.iniciarTransaccao();
+			site = persistentSite.readByExecutionCourse(executionCourse);
+			persistentSupport.confirmarTransaccao();
+
+		} catch (ExcepcaoPersistencia ex) {
+			fail("testReadByExecutionCourse:fail read existing site");
+		}
+		assertEquals(
+			"testReadByExecutionCourse:read existing site",
+			executionCourse,
+			site.getExecutionCourse());
+		assertEquals(
+			"testReadByExecutionCourse:read existing site",
+			null,
+			site.getInitialSection());
+
+
+		//read non-existing Site
+		try {
+			persistentSupport.iniciarTransaccao();
+
+			executionYear =
+				persistentExecutionYear.readExecutionYearByName("2002/2003");
+
+			executionPeriod =
+				persistentExecutionPeriod.readByNameAndExecutionYear(
+					"2º Semestre",
+					executionYear);
+
+			IDisciplinaExecucao nonExistingExecutionCourse =
+				new DisciplinaExecucao(
+					"nonExistingExecutionCourse",
+					"NEEC",
+					"",
+					new Double(1),
+					new Double(2),
+					new Double(0),
+					new Double(0),
+					executionPeriod);
+			site = persistentSite.readByExecutionCourse(nonExistingExecutionCourse);
+			assertNull(site);
+			persistentSupport.confirmarTransaccao();
+
+
+		} catch (ExcepcaoPersistencia ex) {
+			fail("    -> Failed Reading NON Existing Site");
+		}
+
+	}
+
+
+
+
+
+	/** Test of readAll method, of class ServidorPersistente.OJB.SiteOJB. */
+		
+		public void testReadAll() {
+			List allSites = null;
+			
+		// read existing Sites
+		try {
 				
-				persistentSupport.iniciarTransaccao();
-				site = persistentSite.readByExecutionCourse(executionCourse);
-				persistentSupport.confirmarTransaccao();
-	  	
+			persistentSupport.iniciarTransaccao();
+			allSites = persistentSite.readAll();
+			persistentSupport.confirmarTransaccao();
+	  		
 				
 			} catch (ExcepcaoPersistencia ex) {
-				fail("testReadByExecutionCourse:fail read existing site");
+				fail("testReadAll:fail reading all sites");
 			}
-			assertEquals("testReadByExecutionCourse:read existing site",executionCourse,site.getExecutionCourse());
-			assertEquals("testReadByExecutionCourse:read existing site",null,site.getInitialSection());
+			assertEquals("testReadAll:read all sites",10, allSites.size());
+			
+			
+			
+			
+		}
+
+
+
+
+
+	/** Test of deleteAll method, of class ServidorPersistente.OJB.SiteOJB. */
+		
+		public void testDeleteAll() {
+			List allSites = null;
+			
+		// delete existing Sites
+		try {
+				
+			persistentSupport.iniciarTransaccao();
+			persistentSite.deleteAll();
+			persistentSupport.confirmarTransaccao();
+			
+			persistentSupport.iniciarTransaccao();
+			allSites= persistentSite.readAll();	
+			persistentSupport.confirmarTransaccao();
+			
+			} catch (ExcepcaoPersistencia ex) {
+				fail("testDeleteAll:fail deleting all sites");
+			}
+			assertEquals("testDeleteAll:delete all sites",0, allSites.size());
 			
 				
-//				delete Site
-			 try{
-				 persistentSupport.iniciarTransaccao();
-				 site=persistentSite.readByExecutionCourse(executionCourse);
-				 assertNotNull(site);
-				 persistentSite.delete(site);
-				 persistentSupport.confirmarTransaccao();
-				
-
-//				read non-Existing Site				 
-				 persistentSupport.iniciarTransaccao();
-				 site=persistentSite.readByExecutionCourse(executionCourse);
-				 assertNull(site);
-				 persistentSupport.confirmarTransaccao();
-		
-			 } catch (ExcepcaoPersistencia ex) {
-				 fail("    -> Failed Reading NON Existing Site");
-			 }
-		 
 		}
+		
+		
+
+	/** Test of delete method, of class ServidorPersistente.OJB.SiteOJB. */
+
+	public void testDelete() {
+		ISite siteToDelete = null;
+
+		//delete Site
+
+		try {
+			persistentSupport.iniciarTransaccao();
+			siteToDelete = persistentSite.readByExecutionCourse(executionCourse);
+			assertNotNull(siteToDelete);
+			persistentSite.delete(siteToDelete);
+			persistentSupport.confirmarTransaccao();
+
+		//read deleted Site				 
+			persistentSupport.iniciarTransaccao();
+			ISite siteDeleted = persistentSite.readByExecutionCourse(executionCourse);
+			assertNull(siteDeleted);
+			persistentSupport.confirmarTransaccao();
+
+		} catch (ExcepcaoPersistencia ex) {
+		 fail("testDelete:fail delete existing site");
+	}
+	
+	}
+		
+
+
+	/** Test of lockWrite method, of class ServidorPersistente.OJB.SiteOJB. */
+
+	public void testLockWrite() {
+		ISite siteToWrite = null;
+
+		
+		  // first delete Site 
+		try {
+			persistentSupport.iniciarTransaccao();
+			siteToWrite = persistentSite.readByExecutionCourse(executionCourse);
+			assertNotNull(siteToWrite);
+			persistentSite.delete(siteToWrite);
+			persistentSupport.confirmarTransaccao();
+		
+
+		// write Site
+		 
+			persistentSupport.iniciarTransaccao();
+			ISite siteDeleted=persistentSite.readByExecutionCourse(executionCourse);
+			assertNull(siteDeleted);
+			persistentSite.lockWrite(siteToWrite);
+			persistentSupport.confirmarTransaccao();
+
+		//read Site
+			persistentSupport.iniciarTransaccao();
+			ISite siteWritten = persistentSite.readByExecutionCourse(executionCourse);
+			assertEquals(siteToWrite,siteWritten);
+			persistentSupport.confirmarTransaccao();
+
+
+		} catch (ExcepcaoPersistencia ex) {
+		 fail("testLockWrite:fail writting a site");
+	}
+	
+	}
 	  			
+	
+
+
 }
