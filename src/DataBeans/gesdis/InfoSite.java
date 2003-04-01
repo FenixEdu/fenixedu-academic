8fcf9ac1d98bec5c72d06da5e6117c3b122f0c49
João Mota <joao.mota@ist.utl.pt>
2003-04-01 19:51:11 +0000

    *** empty log message ***

--- src/DataBeans/gesdis/InfoSite.java
+++ src/DataBeans/gesdis/InfoSite.java
@@ -136,6 +136,7 @@ public class InfoSite {
 		result += ", introduction=" + getIntroduction();	
 		result += ", mail =" + getMail();
 		result += ", alternativeSite="+getAlternativeSite();
+		result += ", style="+getStyle();
 		result += "]";
 		return result;
 	}
