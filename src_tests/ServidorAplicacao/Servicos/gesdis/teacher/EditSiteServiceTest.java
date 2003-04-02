249613ff61bf8070e3e55fea105c512c8af358de
Ivo Brandão <imbr@mega.ist.utl.pt>
2003-04-02 17:31:41 +0000

    *** empty log message ***

---
+++ src_tests/ServidorAplicacao/Servicos/gesdis/teacher/EditSiteServiceTest.java
@@ -0,0 +1,95 @@
+package ServidorAplicacao.Servicos.gesdis.teacher;
+
+import DataBeans.gesdis.InfoSection;
+import DataBeans.gesdis.InfoSite;
+import DataBeans.util.Cloner;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import Dominio.ISection;
+import Dominio.ISite;
+import ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.IPersistentSection;
+import ServidorPersistente.IPersistentSite;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author Ivo Brandão
+ */
+public class EditSiteServiceTest extends TestCaseDeleteAndEditServices {
+
+	/**
+	 * @param testName
+	 */
+	public EditSiteServiceTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "EditSite";
+	}
+
+	/**
+	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		return null;
+	}
+
+	/**
+	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		ISuportePersistente sp = null;
+		IExecutionYear executionYear = null;
+		IExecutionPeriod executionPeriod = null;
+		IDisciplinaExecucao executionCourse = null;
+		InfoSection infoSection = null;
+		ISite site = null;
+
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+			sp.iniciarTransaccao();
+
+			IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+			executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+			IPersistentExecutionPeriod iepp = sp.getIPersistentExecutionPeriod();
+
+			executionPeriod = iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+			IDisciplinaExecucaoPersistente idep = sp.getIDisciplinaExecucaoPersistente();
+			executionCourse = idep.readByExecutionCourseInitialsAndExecutionPeriod("TFCI", executionPeriod);
+			IPersistentSite persistentSite = sp.getIPersistentSite();
+
+			site = persistentSite.readByExecutionCourse(executionCourse);
+			
+			IPersistentSection persistentSection = sp.getIPersistentSection();
+
+			ISection section = persistentSection.readBySiteAndSectionAndName(site,null,"Seccao1deTFCI");
+			
+			sp.confirmarTransaccao();
+		} catch (ExcepcaoPersistencia e) {
+			System.out.println("failed setting up the test data");
+			e.printStackTrace();
+		}
+		
+		InfoSite oldInfoSite = Cloner.copyISite2InfoSite(site);
+		InfoSite newInfoSite = oldInfoSite;
+		newInfoSite.setAlternativeSite("newAlternativeSite");
+		newInfoSite.setMail("newMail");
+		newInfoSite.setInitialStatement("newInitialStatement");
+		newInfoSite.setIntroduction("newIntroduction");
+		
+		Object[] args = {oldInfoSite, newInfoSite};
+		return args;
+	}
+}
