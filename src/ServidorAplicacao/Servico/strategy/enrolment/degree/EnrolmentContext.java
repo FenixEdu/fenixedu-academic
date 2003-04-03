e5277a9ece61f681339874fc4bebf92d0583aaa4
João Luz <joao.luz@ist.utl.pt>
2003-04-03 17:30:08 +0000

    Context of the enrolment context. Used by Strategies...

---
+++ src/ServidorAplicacao/Servico/strategy/enrolment/degree/EnrolmentContext.java
@@ -0,0 +1,132 @@
+/*
+ * Created on 3/Abr/2003 by jpvl
+ *
+ */
+package ServidorAplicacao.Servico.strategy.enrolment.degree;
+
+import java.util.List;
+
+import Dominio.ICurso;
+import Dominio.IStudent;
+
+/**
+ * @author jpvl
+ */
+public class EnrolmentContext {
+	private ICurso degree;
+	private IStudent student;
+	private List curricularCoursesDoneByStudent;
+	private List curricularCoursesNotDoneByStudent;
+	private Integer semester;
+	
+	/**
+	 * defines the list that student can be enrolled
+	 */
+	private List finalCurricularCoursesSpanToBeEnrolled;
+	
+	
+	/**
+	 *  
+	 */
+	public EnrolmentContext() {
+		super();
+	}
+
+	/**
+	 * @return
+	 */
+	public List getCurricularCoursesDoneByStudent() {
+		return curricularCoursesDoneByStudent;
+	}
+
+	/**
+	 * @return
+	 */
+	public List getCurricularCourseNotDoneByStudent() {
+		return curricularCoursesNotDoneByStudent;
+	}
+
+	/**
+	 * @return
+	 */
+	public ICurso getDegree() {
+		return degree;
+	}
+
+	/**
+	 * @return
+	 */
+	public Integer getSemester() {
+		return semester;
+	}
+
+	/**
+	 * @return
+	 */
+	public IStudent getStudent() {
+		return student;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setCurricularCoursesDoneByStudent(List list) {
+		curricularCoursesDoneByStudent = list;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setCurricularCourseNotDoneByStudent(List list) {
+		curricularCoursesNotDoneByStudent = list;
+	}
+
+	/**
+	 * @param curso
+	 */
+	public void setDegree(ICurso degree) {
+		this.degree = degree;
+	}
+
+	/**
+	 * @param integer
+	 */
+	public void setSemester(Integer integer) {
+		semester = integer;
+	}
+
+	/**
+	 * @param student
+	 */
+	public void setStudent(IStudent student) {
+		this.student = student;
+	}
+	/**
+	 * @return
+	 */
+	public List getCurricularCoursesNotDoneByStudent() {
+		return curricularCoursesNotDoneByStudent;
+	}
+
+	/**
+	 * @return
+	 */
+	public List getFinalCurricularCoursesSpanToBeEnrolled() {
+		return finalCurricularCoursesSpanToBeEnrolled;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setCurricularCoursesNotDoneByStudent(List list) {
+		curricularCoursesNotDoneByStudent = list;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setFinalCurricularCoursesSpanToBeEnrolled(List list) {
+		finalCurricularCoursesSpanToBeEnrolled = list;
+	}
+
+}
