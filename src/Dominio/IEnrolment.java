64d519ba96311e1ba057a150fa3422de73599a57
João Luz <joao.luz@ist.utl.pt>
2003-04-04 14:51:15 +0000

    added executionPeriod set and get

--- src/Dominio/IEnrolment.java
+++ src/Dominio/IEnrolment.java
@@ -13,7 +13,10 @@ public interface IEnrolment {
 	public ICurricularCourse getCurricularCourse();
 	public IStudentCurricularPlan getStudentCurricularPlan();
 	public EnrolmentState getState();
+	IExecutionPeriod getExecutionPeriod();
+	
 	public void setState(EnrolmentState state);
 	public void setCurricularCourse(ICurricularCourse curricularCourse);
 	public void setStudentCurricularPlan(IStudentCurricularPlan studentCurricularPlan);
+	void setExecutionPeriod(IExecutionPeriod executionPeriod);
 }
