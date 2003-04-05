5a554cd0f5f16027471c114176d55cac9f2840d3
João Luz <joao.luz@ist.utl.pt>
2003-04-05 19:29:08 +0000

    some assertions added to test...

--- src_tests/ServidorAplicacao/Servicos/strategy/enrolment/degree/StrategyFactoryTest.java
+++ src_tests/ServidorAplicacao/Servicos/strategy/enrolment/degree/StrategyFactoryTest.java
@@ -5,9 +5,13 @@
 package ServidorAplicacao.Servicos.strategy.enrolment.degree;
 
 import java.util.HashMap;
+import java.util.List;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
+
+import org.apache.commons.collections.CollectionUtils;
+
 import Dominio.CurricularCourse;
 import Dominio.IStudentCurricularPlan;
 import ServidorAplicacao.Servicos.TestCaseServicos;
@@ -31,24 +35,32 @@ public class StrategyFactoryTest extends TestCaseServicos {
 	 */
 	public StrategyFactoryTest(String testName) {
 		super(testName);
-		
+
 	}
 
-	// TODO finish the test
+	/**
+	 * it must:
+	 * 		initialize acumulated enrolments...
+	 * 		initialize final span with not yet done courses.
+	 * 		initialize curricular courses done
+	 */
 	public void testSucessfullCreationOfStrategy() {
 		EnrolmentContext enrolmentContext = new EnrolmentContext();
 		IStudentCurricularPlan studentCurricularPlan = null;
 		try {
 			sp.iniciarTransaccao();
-			studentCurricularPlan = studentCurricularPlanDAO.readActiveStudentCurricularPlan(
-				new Integer(600),
-				new TipoCurso(TipoCurso.LICENCIATURA));
+			studentCurricularPlan =
+				studentCurricularPlanDAO.readActiveStudentCurricularPlan(
+					new Integer(600),
+					new TipoCurso(TipoCurso.LICENCIATURA));
 			sp.confirmarTransaccao();
 		} catch (ExcepcaoPersistencia e1) {
 			e1.printStackTrace(System.out);
 			fail("Reading active student curricular plan!");
 		}
-		assertNotNull("Student curricular plan must be not null!",studentCurricularPlan);
+		assertNotNull(
+			"Student curricular plan must be not null!",
+			studentCurricularPlan);
 
 		enrolmentContext.setSemester(new Integer(2));
 		enrolmentContext.setStudent(studentCurricularPlan.getStudent());
@@ -57,6 +69,7 @@ public class StrategyFactoryTest extends TestCaseServicos {
 		IEnrolmentStrategy enrolmentStrategy = null;
 		try {
 			sp.iniciarTransaccao();
+			
 			enrolmentStrategy =
 				EnrolmentStrategyFactory.getEnrolmentStrategyInstance(
 					EnrolmentStrategyFactory.LERCI,
@@ -80,32 +93,55 @@ public class StrategyFactoryTest extends TestCaseServicos {
 			"Degree not equal!",
 			enrolmentContext.getDegree(),
 			strategyEnrolmentContext.getDegree());
-		
-		HashMap acumulatedEnrolments = (HashMap) strategyEnrolmentContext.getAcumulatedEnrolments();
-		
-		assertNotNull("Acumulated enrolments is null!",acumulatedEnrolments);
-		
-		
+
+		HashMap acumulatedEnrolments =
+			(HashMap) strategyEnrolmentContext.getAcumulatedEnrolments();
+
+		assertNotNull("Acumulated enrolments is null!", acumulatedEnrolments);
+
 		assertEquals(3, acumulatedEnrolments.size());
-		
+
 		CurricularCourse curricularCourse = new CurricularCourse();
-		curricularCourse.setDegreeCurricularPlan(studentCurricularPlan.getDegreeCurricularPlan());
-		
+		curricularCourse.setDegreeCurricularPlan(
+			studentCurricularPlan.getDegreeCurricularPlan());
 		curricularCourse.setCode("AMII");
 		curricularCourse.setName("Analise Matematica II");
-		assertEquals("AMII",new Integer(2), acumulatedEnrolments.get(curricularCourse));
+		assertEquals(
+			"AMII",
+			new Integer(2),
+			acumulatedEnrolments.get(curricularCourse));
 
-
+		curricularCourse = new CurricularCourse();
+		curricularCourse.setDegreeCurricularPlan(
+			studentCurricularPlan.getDegreeCurricularPlan());
 		curricularCourse.setCode("TFCI");
 		curricularCourse.setName("Trabalho Final de Curso I");
-		assertEquals("TFCI", new Integer(1),acumulatedEnrolments.get(curricularCourse));
-		
-		
-		curricularCourse.setCode("AMI");
-		curricularCourse.setName("Analise Matematica I");		
-		assertEquals("AMI", new Integer(1), acumulatedEnrolments.get(curricularCourse));
+		assertEquals(
+			"TFCI",
+			new Integer(1),
+			acumulatedEnrolments.get(curricularCourse));
 
-		
+		curricularCourse = new CurricularCourse();
+		curricularCourse.setDegreeCurricularPlan(
+			studentCurricularPlan.getDegreeCurricularPlan());
+		curricularCourse.setCode("AMI");
+		curricularCourse.setName("Analise Matematica I");
+		assertEquals(
+			"AMI",
+			new Integer(1),
+			acumulatedEnrolments.get(curricularCourse));
+
+		List doneCourses = enrolmentContext.getCurricularCoursesDoneByStudent();
+		assertNotNull("curricular courses done is null!", doneCourses);
+		assertEquals("Curricular courses done!", 0, doneCourses.size());
+
+		List finalSpan =
+			enrolmentContext.getFinalCurricularCoursesSpanToBeEnrolled();
+		assertNotNull("Final span is null!", finalSpan);
+		assertEquals("Final span size!", 8, finalSpan.size());
+		assertEquals("Final span is wrong!",
+		true,
+		CollectionUtils.isEqualCollection(studentCurricularPlan.getDegreeCurricularPlan().getCurricularCourses(), finalSpan));
 	}
 
 	public static void main(java.lang.String[] args) {
