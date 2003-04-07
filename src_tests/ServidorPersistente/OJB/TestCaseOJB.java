efb05a05a12af5a972efa547e51344d6a559420b
João Luz <joao.luz@ist.utl.pt>
2003-04-07 13:16:35 +0000

    added a method to retrieve data set for the test...

--- src_tests/ServidorPersistente/OJB/TestCaseOJB.java
+++ src_tests/ServidorPersistente/OJB/TestCaseOJB.java
@@ -1,42 +1,56 @@
 package ServidorPersistente.OJB;
 
 import junit.framework.TestCase;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.ISuportePersistente;
 import Tools.dbaccess;
 
 public class TestCaseOJB extends TestCase {
 
-  private dbaccess dbAcessPoint = null;
-    
-  public TestCaseOJB(String testName) {
-    super(testName);
-  }
+	private dbaccess dbAcessPoint = null;
+	protected ISuportePersistente sp;
+	public TestCaseOJB(String testName) {
+		super(testName);
+	}
 
-  protected void setUp() {
-  	// The following code backs up the contents of the database
-  	// and loads the database with the data set required to run
-  	// the test cases.
-  	try {
-  		dbAcessPoint = new dbaccess();
-  		dbAcessPoint.openConnection();
-  		dbAcessPoint.backUpDataBaseContents("etc/testBackup.xml");
-  		dbAcessPoint.loadDataBase("etc/testDataSet.xml");
-  		dbAcessPoint.closeConnection();
-  	} catch (Exception ex) {
-  		System.out.println("Setup failed: " + ex);
-  	}
-  	
-  }
+	protected void setUp() {
+		// The following code backs up the contents of the database
+		// and loads the database with the data set required to run
+		// the test cases.
+		try {
+			dbAcessPoint = new dbaccess();
+			dbAcessPoint.openConnection();
+			dbAcessPoint.backUpDataBaseContents("etc/testBackup.xml");
+			dbAcessPoint.loadDataBase(getDataSetFilePath());
+			dbAcessPoint.closeConnection();
+		} catch (Exception ex) {
+			System.out.println("Setup failed: " + ex);
+		}
+		
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+		} catch (ExcepcaoPersistencia e) {
+			e.printStackTrace();
+			fail ("Setup: getting persistent factory!");
+		}
+	}
 
-  protected void tearDown() {
-  	try {
-  		dbAcessPoint.openConnection();
-  		dbAcessPoint.loadDataBase("etc/testBackup.xml");
-  		//dbAcessPoint.loadDataBase("etc/testDataSet.xml");
-  		dbAcessPoint.closeConnection();
-  	} catch (Exception ex) {
-  		System.out.println("Tear down failed: " +ex);
-  	}
-  }
-            
-    
+	/**
+	* @return
+	*/
+	protected String getDataSetFilePath() {
+		return "etc/testDataSet.xml";
+	}
+
+	protected void tearDown() {
+		try {
+			dbAcessPoint.openConnection();
+			dbAcessPoint.loadDataBase("etc/testBackup.xml");
+			//dbAcessPoint.loadDataBase("etc/testDataSet.xml");
+			dbAcessPoint.closeConnection();
+		} catch (Exception ex) {
+			System.out.println("Tear down failed: " + ex);
+		}
+	}
+
 }
