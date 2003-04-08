75a56685704272f2c892f4286c9f16ecf8647bd4
João Luz <joao.luz@ist.utl.pt>
2003-04-08 15:08:42 +0000

    Predence Rule test

--- src_tests/ServidorAplicacao/Servicos/strategy/enrolment/degree/rules/PrecedenceRuleTest.java
+++ src_tests/ServidorAplicacao/Servicos/strategy/enrolment/degree/rules/PrecedenceRuleTest.java
@@ -4,18 +4,26 @@
  */
 package ServidorAplicacao.Servicos.strategy.enrolment.degree.rules;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import Dominio.ICurricularCourse;
+import ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext;
+import ServidorAplicacao.strategy.enrolment.degree.rules.IEnrolmentRule;
+import ServidorAplicacao.strategy.enrolment.degree.rules.PrecedenceRule;
+import ServidorPersistente.ExcepcaoPersistencia;
+import Tools.dbaccess;
+import Util.TipoCurso;
 
 /**
  * @author jpvl
  */
-public class PrecedenceRuleTest extends TestCase {
+public class PrecedenceRuleTest extends BaseEnrolmentRuleTest {
 
-	public PrecedenceRuleTest(String testName){
-		super(testName);
-	}
+	private dbaccess dbAcessPoint;
+
 	public static void main(java.lang.String[] args) {
 		junit.textui.TestRunner.run(suite());
 	}
@@ -25,15 +33,44 @@ public class PrecedenceRuleTest extends TestCase {
 		return suite;
 	}
 	
-	public void testCurricularCoursePrecendence(){
-		fail("not done yet!");	
+	public void testApplyPrecendenceRule(){
+		List finalSpan = new ArrayList();
+		List initialSpan = null;
+		
+		EnrolmentContext enrolmentContext = getEnrolmentContext(new Integer(1), new TipoCurso(TipoCurso.LICENCIATURA), new Integer(1));
+		initialSpan = enrolmentContext.getFinalCurricularCoursesSpanToBeEnrolled();		
+		
+		doApplyRule(new PrecedenceRule(), enrolmentContext);
+		
+		finalSpan = enrolmentContext.getFinalCurricularCoursesSpanToBeEnrolled();
+		
+		assertEquals("Final span size:",true, initialSpan.size() > finalSpan.size());
+		assertEquals ("Contains assertion!",true,initialSpan.containsAll(finalSpan));
+		
+		ICurricularCourse curricularCourse = getCurricularCourse("Analise Matematica I", "AMI");
+		assertEquals(true, !finalSpan.contains(curricularCourse));
+		
+		enrolmentContext = getEnrolmentContext(new Integer(1), new TipoCurso(TipoCurso.LICENCIATURA), new Integer(1));		
+		initialSpan = enrolmentContext.getFinalCurricularCoursesSpanToBeEnrolled();
+		
+		doApplyRule(new PrecedenceRule(), enrolmentContext);
+		
+		finalSpan = enrolmentContext.getFinalCurricularCoursesSpanToBeEnrolled();
+		
+		assertEquals("Final span size:",true, initialSpan.size() == finalSpan.size());
+		assertEquals ("Contains assertion!",true,initialSpan.containsAll(finalSpan));
 	}
 	
-	public void testNumberOfCurricularCourseDonePrecedence(){
-		fail("not done yet!");
+	public void doApplyRule(IEnrolmentRule enrolmentRule, EnrolmentContext enrolmentContext){
+		try {
+			sp.iniciarTransaccao();
+			enrolmentRule.apply(enrolmentContext);
+			sp.confirmarTransaccao();
+		} catch (ExcepcaoPersistencia e1) {
+			e1.printStackTrace();
+			fail("Applying rule!");
+		}
 	}
 	
-	public void testComposedPrecedence(){
-		fail("not done yet!");
-	}
+	
 }
