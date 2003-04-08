37fce6ade22bddf1333d5a5459c5a0c54e398051
João Luz <joao.luz@ist.utl.pt>
2003-04-08 15:07:23 +0000

    PrecedenceRule

---
+++ src/ServidorAplicacao/strategy/enrolment/degree/rules/PrecedenceRule.java
@@ -0,0 +1,51 @@
+/*
+ * Created on 8/Abr/2003 by jpvl
+ *
+ */
+package ServidorAplicacao.strategy.enrolment.degree.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Dominio.ICurricularCourse;
+import Dominio.IPrecedence;
+import Dominio.IStudentCurricularPlan;
+import ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentPrecedence;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jpvl
+ */
+public class PrecedenceRule implements IEnrolmentRule {
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.strategy.enrolment.degree.rules.IEnrolmentRule#apply(ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext)
+	 */
+	public EnrolmentContext apply(EnrolmentContext enrolmentContext) throws ExcepcaoPersistencia {
+		List precedenceList = null;
+		ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+		IStudentCurricularPlan studentActiveCurricularPlan = enrolmentContext.getStudentActiveCurricularPlan();
+	
+		IPersistentPrecedence precedenceDAO = sp.getIPersistentPrecedence();
+		
+		precedenceList = precedenceDAO.readByDegreeCurricularPlan(studentActiveCurricularPlan.getDegreeCurricularPlan());
+		
+		List curricularCoursesToStay = new ArrayList();
+		for (int i = 0; i < precedenceList.size(); i++){
+			IPrecedence precedence = (IPrecedence) precedenceList.get(i);
+			if (precedence.evaluate(enrolmentContext)){
+				ICurricularCourse curricularCourse = precedence.getCurricularCourse();
+				if (!curricularCoursesToStay.contains(curricularCourse)){
+					curricularCoursesToStay.add(curricularCourse);
+				}
+			}
+				
+		}
+		enrolmentContext.setFinalCurricularCoursesSpanToBeEnrolled(curricularCoursesToStay);
+		return enrolmentContext;
+	}
+
+}
