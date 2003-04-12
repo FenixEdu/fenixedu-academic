3efabe1fac94088afc26fb4bc063a29dd85efea3
João Luz <joao.luz@ist.utl.pt>
2003-04-12 15:25:22 +0000

    readEquivalenceByEnrolmentAndEquivalentEnrolment query changed... CTRL+SHIFT+F

--- src/ServidorPersistente/OJB/EquivalenceOJB.java
+++ src/ServidorPersistente/OJB/EquivalenceOJB.java
@@ -19,8 +19,10 @@ import ServidorPersistente.exceptions.ExistingPersistentException;
  * 24/Mar/2003
  */
 
-public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquivalence {
-    
+public class EquivalenceOJB
+	extends ObjectFenixOJB
+	implements IPersistentEquivalence {
+
 	public void deleteAll() throws ExcepcaoPersistencia {
 		try {
 			String oqlQuery = "select all from " + Equivalence.class.getName();
@@ -30,7 +32,8 @@ public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquival
 		}
 	}
 
-	public void lockWrite(IEquivalence equivalenceToWrite) throws ExcepcaoPersistencia, ExistingPersistentException {
+	public void lockWrite(IEquivalence equivalenceToWrite)
+		throws ExcepcaoPersistencia, ExistingPersistentException {
 
 		IEquivalence equivalenceFromDB = null;
 
@@ -40,13 +43,19 @@ public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquival
 		}
 
 		// Read Equivalence from database.
-		equivalenceFromDB = this.readEquivalenceByEnrolmentAndEquivalentEnrolment(equivalenceToWrite.getEnrolment(), equivalenceToWrite.getEquivalentEnrolment());
+		equivalenceFromDB =
+			this.readEquivalenceByEnrolmentAndEquivalentEnrolment(
+				equivalenceToWrite.getEnrolment(),
+				equivalenceToWrite.getEquivalentEnrolment());
 
 		// If Equivalence is not in database, then write it.
 		if (equivalenceFromDB == null) {
 			super.lockWrite(equivalenceToWrite);
-		// else If the Equivalence is mapped to the database, then write any existing changes.
-		} else if ((equivalenceToWrite instanceof Equivalence) && ((Equivalence) equivalenceFromDB).getInternalID().equals(((Equivalence) equivalenceToWrite).getInternalID())) {
+			// else If the Equivalence is mapped to the database, then write any existing changes.
+		} else if (
+			(equivalenceToWrite instanceof Equivalence)
+				&& ((Equivalence) equivalenceFromDB).getInternalID().equals(
+					((Equivalence) equivalenceToWrite).getInternalID())) {
 			super.lockWrite(equivalenceToWrite);
 			// else Throw an already existing exception
 		} else
@@ -61,48 +70,91 @@ public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquival
 		}
 	}
 
-	public IEquivalence readEquivalenceByEnrolmentAndEquivalentEnrolment(IEnrolment enrolment, IEnrolment equivalentEnrolment) throws ExcepcaoPersistencia {
+	public IEquivalence readEquivalenceByEnrolmentAndEquivalentEnrolment(
+		IEnrolment enrolment,
+		IEnrolment equivalentEnrolment)
+		throws ExcepcaoPersistencia {
 
 		try {
 			IEquivalence equivalence = null;
 			String oqlQuery = "select all from " + Equivalence.class.getName();
-			oqlQuery += " where enrolment.studentCurricularPlan.student.number = $1";
-			oqlQuery += " and enrolment.studentCurricularPlan.student.degreeType = $2";
-			oqlQuery += " and enrolment.studentCurricularPlan.currentState = $3";
+			oqlQuery
+				+= " where enrolment.studentCurricularPlan.student.number = $1";
+			oqlQuery
+				+= " and enrolment.studentCurricularPlan.student.degreeType = $2";
+			oqlQuery
+				+= " and enrolment.studentCurricularPlan.currentState = $3";
 			oqlQuery += " and enrolment.curricularCourse.name = $4";
 			oqlQuery += " and enrolment.curricularCourse.code = $5";
-			oqlQuery += " and enrolment.curricularCourse.degreeCurricularPlan.name = $6";
-			oqlQuery += " and enrolment.curricularCourse.degreeCurricularPlan.degree.nome = $7";
-			oqlQuery += " and enrolment.curricularCourse.degreeCurricularPlan.degree.sigla = $8";
+			oqlQuery
+				+= " and enrolment.curricularCourse.degreeCurricularPlan.name = $6";
+			oqlQuery
+				+= " and enrolment.curricularCourse.degreeCurricularPlan.degree.sigla = $7";
 
-			oqlQuery += " and equivalentEnrolment.studentCurricularPlan.student.number = $9";
-			oqlQuery += " and equivalentEnrolment.studentCurricularPlan.student.degreeType = $10";
-			oqlQuery += " and equivalentEnrolment.studentCurricularPlan.currentState = $11";
-			oqlQuery += " and equivalentEnrolment.curricularCourse.name = $12";
-			oqlQuery += " and equivalentEnrolment.curricularCourse.code = $13";
-			oqlQuery += " and equivalentEnrolment.curricularCourse.degreeCurricularPlan.name = $14";
-			oqlQuery += " and equivalentEnrolment.curricularCourse.degreeCurricularPlan.degree.nome = $15";
-			oqlQuery += " and equivalentEnrolment.curricularCourse.degreeCurricularPlan.degree.sigla = $16";
+			oqlQuery
+				+= " and equivalentEnrolment.studentCurricularPlan.student.number = $8";
+			oqlQuery
+				+= " and equivalentEnrolment.studentCurricularPlan.student.degreeType = $9";
+			oqlQuery
+				+= " and equivalentEnrolment.studentCurricularPlan.currentState = $10";
+			oqlQuery += " and equivalentEnrolment.curricularCourse.name = $11";
+			oqlQuery += " and equivalentEnrolment.curricularCourse.code = $12";
+			oqlQuery
+				+= " and equivalentEnrolment.curricularCourse.degreeCurricularPlan.name = $13";
+			oqlQuery
+				+= " and equivalentEnrolment.curricularCourse.degreeCurricularPlan.degree.sigla = $14";
 
 			query.create(oqlQuery);
 
-			query.bind(enrolment.getStudentCurricularPlan().getStudent().getNumber());
-			query.bind(enrolment.getStudentCurricularPlan().getStudent().getDegreeType());
+			query.bind(
+				enrolment.getStudentCurricularPlan().getStudent().getNumber());
+			query.bind(
+				enrolment
+					.getStudentCurricularPlan()
+					.getStudent()
+					.getDegreeType());
 			query.bind(enrolment.getStudentCurricularPlan().getCurrentState());
 			query.bind(enrolment.getCurricularCourse().getName());
 			query.bind(enrolment.getCurricularCourse().getCode());
-			query.bind(enrolment.getCurricularCourse().getDegreeCurricularPlan().getName());
-			query.bind(enrolment.getCurricularCourse().getDegreeCurricularPlan().getDegree().getNome());
-			query.bind(enrolment.getCurricularCourse().getDegreeCurricularPlan().getDegree().getSigla());
+			query.bind(
+				enrolment
+					.getCurricularCourse()
+					.getDegreeCurricularPlan()
+					.getName());
+			query.bind(
+				enrolment
+					.getCurricularCourse()
+					.getDegreeCurricularPlan()
+					.getDegree()
+					.getSigla());
 
-			query.bind(equivalentEnrolment.getStudentCurricularPlan().getStudent().getNumber());
-			query.bind(equivalentEnrolment.getStudentCurricularPlan().getStudent().getDegreeType());
-			query.bind(equivalentEnrolment.getStudentCurricularPlan().getCurrentState());
+			query.bind(
+				equivalentEnrolment
+					.getStudentCurricularPlan()
+					.getStudent()
+					.getNumber());
+			query.bind(
+				equivalentEnrolment
+					.getStudentCurricularPlan()
+					.getStudent()
+					.getDegreeType());
+			query.bind(
+				equivalentEnrolment
+					.getStudentCurricularPlan()
+					.getCurrentState());
 			query.bind(equivalentEnrolment.getCurricularCourse().getName());
 			query.bind(equivalentEnrolment.getCurricularCourse().getCode());
-			query.bind(equivalentEnrolment.getCurricularCourse().getDegreeCurricularPlan().getName());
-			query.bind(equivalentEnrolment.getCurricularCourse().getDegreeCurricularPlan().getDegree().getNome());
-			query.bind(equivalentEnrolment.getCurricularCourse().getDegreeCurricularPlan().getDegree().getSigla());
+			query.bind(
+				equivalentEnrolment
+					.getCurricularCourse()
+					.getDegreeCurricularPlan()
+					.getName());
+			query.bind(
+				equivalentEnrolment
+					.getCurricularCourse()
+					.getDegreeCurricularPlan()
+					.getDegree()
+					.getSigla());
 
 			List result = (List) query.execute();
 			try {
@@ -111,7 +163,7 @@ public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquival
 				throw ex;
 			}
 
-			if( (result != null) && (result.size() != 0) ) {
+			if ((result != null) && (result.size() != 0)) {
 				equivalence = (IEquivalence) result.get(0);
 			}
 			return equivalence;
@@ -135,7 +187,7 @@ public class EquivalenceOJB extends ObjectFenixOJB implements IPersistentEquival
 				throw ex;
 			}
 
-			if( (result != null) && (result.size() != 0) ) {
+			if ((result != null) && (result.size() != 0)) {
 				ListIterator iterator = result.listIterator();
 				while (iterator.hasNext())
 					list.add((IEquivalence) iterator.next());
