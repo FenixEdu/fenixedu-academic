b242b3f89298a3004c130103411e73e61cc88f71
João Luz <joao.luz@ist.utl.pt>
2003-04-13 16:42:49 +0000

    comment hashCode function... This doesn't work :(

--- src/Dominio/CurricularCourse.java
+++ src/Dominio/CurricularCourse.java
@@ -343,11 +343,11 @@ public class CurricularCourse implements ICurricularCourse {
 		this.scopes = scopes;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	public int hashCode() {
-		return name.hashCode() + code.hashCode();
-	}
+//	/* (non-Javadoc)
+//	 * @see java.lang.Object#hashCode()
+//	 */
+//	public int hashCode() {
+//		return name.hashCode() + code.hashCode();
+//	}
 
 }
