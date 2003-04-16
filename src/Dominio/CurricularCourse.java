1f1e9ad218297e6640cf46e5be0a00cd1b394a3a
David Santos <david.santos@ist.utl.pt>
2003-04-16 17:13:03 +0000

    *** empty log message ***

--- src/Dominio/CurricularCourse.java
+++ src/Dominio/CurricularCourse.java
@@ -371,13 +371,6 @@ public class CurricularCourse implements ICurricularCourse {
 		this.scopes = scopes;
 	}
 
-//	/* (non-Javadoc)
-//	 * @see java.lang.Object#hashCode()
-//	 */
-//	public int hashCode() {
-//		return name.hashCode() + code.hashCode();
-//	}
-
 	/**
 	 * @return CurricularCourseType
 	 */
