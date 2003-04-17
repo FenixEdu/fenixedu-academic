75fc546f8bc56ad7099f868af36272ecd728b3aa
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-04-17 10:52:13 +0000

    *** empty log message ***

---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/contributor/ReadAllContributorsTest.java
@@ -0,0 +1,82 @@
+
+/*
+ * CriarSalaServicosTest.java
+ * JUnit based test
+ *
+ * Created on 24 de Outubro de 2002, 12:00
+ */
+
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.contributor;
+
+/**
+ *
+ * @author Nuno Nunes & Joana Mota 
+ */
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import DataBeans.InfoRole;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.Servico.UserView;
+import ServidorAplicacao.Servicos.TestCaseServicos;
+import Util.RoleType;
+
+public class ReadAllContributorsTest extends TestCaseServicos {
+	
+	public ReadAllContributorsTest(java.lang.String testName) {
+		super(testName);
+	}
+
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(ReadAllContributorsTest.class);
+
+		return suite;
+	}
+
+	protected void setUp() {
+		super.setUp();
+	}
+
+	protected void tearDown() {
+		super.tearDown();
+	}
+
+
+	public void testReadAllContributors() {
+		System.out.println("- Test 1 : Read All Contributors");
+		
+		UserView userView = this.getUserViewToBeTested("nmsn", true);
+
+		List result = null;
+	 
+		 try {
+			 result = (List) _gestor.executar(userView, "ReadAllContributors", null);
+		 } catch (FenixServiceException ex) {
+			fail("Fenix Service Exception");
+		 } catch (Exception ex) {
+			fail("Exception");
+		 }
+		 
+		 assertEquals(result.size(), 2);
+		
+	}
+
+	private UserView getUserViewToBeTested(String username, boolean withRole) {
+		Collection roles = new ArrayList();
+		InfoRole infoRole = new InfoRole();
+		if (withRole) infoRole.setRoleType(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE);
+		else infoRole.setRoleType(RoleType.PERSON);
+		roles.add(infoRole);
+		UserView userView = new UserView(username, roles);
+		return userView;
+	}
+
+
+}
---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/contributor/ReadContributorListTest.java
@@ -0,0 +1,99 @@
+
+/*
+ * CriarSalaServicosTest.java
+ * JUnit based test
+ *
+ * Created on 24 de Outubro de 2002, 12:00
+ */
+
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.contributor;
+
+/**
+ *
+ * @author Nuno Nunes & Joana Mota 
+ */
+import java.util.ArrayList;
+import java.util.Collection;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import DataBeans.InfoContributor;
+import DataBeans.InfoRole;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.Servico.ExcepcaoInexistente;
+import ServidorAplicacao.Servico.UserView;
+import ServidorAplicacao.Servicos.TestCaseServicos;
+import Util.RoleType;
+
+public class ReadContributorListTest extends TestCaseServicos {
+	
+	public ReadContributorListTest(java.lang.String testName) {
+		super(testName);
+	}
+
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(ReadContributorListTest.class);
+
+		return suite;
+	}
+
+	protected void setUp() {
+		super.setUp();
+	}
+
+	protected void tearDown() {
+		super.tearDown();
+	}
+
+
+	public void testReadContributorList() {
+		System.out.println("- Test 1 : Read Contributor List");
+		
+		UserView userView = this.getUserViewToBeTested("nmsn", true);
+
+		InfoContributor infoContributor = null;
+	 
+	 	Object[] args = { new Integer(123)};
+	 
+		try {
+		    infoContributor = (InfoContributor) _gestor.executar(userView, "ReadContributor", args);
+		} catch (FenixServiceException ex) {
+			fail("Fenix Service Exception");
+		} catch (Exception ex) {
+			fail("Exception");
+		}
+		 
+		assertNotNull(infoContributor);
+		assertEquals(infoContributor.getContributorNumber(), new Integer(123));
+
+		Object[] args2 = { new Integer(9999)};
+		
+		infoContributor = null;
+	 
+		try {
+			infoContributor = (InfoContributor) _gestor.executar(userView, "ReadContributor", args2);
+		} catch (ExcepcaoInexistente ex) {
+			// All is OK
+		} catch (Exception ex) {
+			fail("Exception");
+		}
+	}
+	
+	
+
+	private UserView getUserViewToBeTested(String username, boolean withRole) {
+		Collection roles = new ArrayList();
+		InfoRole infoRole = new InfoRole();
+		if (withRole) infoRole.setRoleType(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE);
+		else infoRole.setRoleType(RoleType.PERSON);
+		roles.add(infoRole);
+		UserView userView = new UserView(username, roles);
+		return userView;
+	}
+
+
+}
