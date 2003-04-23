18a1cd620376a07f651d1b8f6ecf468e3c5cc175
João Mota <joao.mota@ist.utl.pt>
2003-04-23 16:14:17 +0000

    runs

---
+++ src_tests/ServidorAplicacao/Servicos/commons/ReadExecutionPeriodServiceTest.java
@@ -0,0 +1,112 @@
+/*
+ * Created on 23/Abr/2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorAplicacao.Servicos.commons;
+
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoExecutionYear;
+import DataBeans.util.Cloner;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jmota
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ReadExecutionPeriodServiceTest extends TestCaseReadServices {
+
+	/**
+	 * @param testName
+	 */
+	public ReadExecutionPeriodServiceTest(String testName) {
+		super(testName);
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "ReadExecutionPeriod";
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		ISuportePersistente sp = null;
+			IExecutionYear executionYear = null;
+			
+			try {
+				sp = SuportePersistenteOJB.getInstance();
+				sp.iniciarTransaccao();
+
+				IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+				executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+				sp.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				System.out.println("failed setting up the test data");
+			}
+		InfoExecutionYear infoExecutionYear = Cloner.copyIExecutionYear2InfoExecutionYear(executionYear);
+		Object[] args = {"2º Semestre",infoExecutionYear};
+		return args;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
+	 */
+	protected int getNumberOfItemsToRetrieve() {
+		
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
+	 */
+	protected Object getObjectToCompare() {
+		ISuportePersistente sp = null;
+			IExecutionYear executionYear = null;
+			IExecutionPeriod executionPeriod = null;
+			
+			try {
+				sp = SuportePersistenteOJB.getInstance();
+				sp.iniciarTransaccao();
+
+				IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+				executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+				IPersistentExecutionPeriod iepp =
+					sp.getIPersistentExecutionPeriod();
+
+				executionPeriod =
+					iepp.readByNameAndExecutionYear("2º Semestre", executionYear);
+
+				
+				sp.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				System.out.println("failed setting up the test data");
+			}
+			InfoExecutionPeriod infoExecutionPeriod = Cloner.copyIExecutionPeriod2InfoExecutionPeriod(executionPeriod);
+		return infoExecutionPeriod;
+	}
+
+}
---
+++ src_tests/ServidorAplicacao/Servicos/commons/ReadExecutionYearServiceTest.java
@@ -0,0 +1,87 @@
+/*
+ * Created on 23/Abr/2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorAplicacao.Servicos.commons;
+
+import DataBeans.InfoExecutionYear;
+import DataBeans.util.Cloner;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servicos.TestCaseReadServices;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jmota
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ReadExecutionYearServiceTest extends TestCaseReadServices {
+
+	/**
+	 * @param testName
+	 */
+	public ReadExecutionYearServiceTest(String testName) {
+		super(testName);
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
+	 */
+	protected String getNameOfServiceToBeTested() {
+		return "ReadExecutionYear";
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
+	 */
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		Object[] args = {"2002/2003"};
+		return args;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
+	 */
+	protected int getNumberOfItemsToRetrieve() {
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
+	 */
+	protected Object getObjectToCompare() {
+		ISuportePersistente sp = null;
+			IExecutionYear executionYear = null;
+			
+			try {
+				sp = SuportePersistenteOJB.getInstance();
+				sp.iniciarTransaccao();
+
+				IPersistentExecutionYear ieyp = sp.getIPersistentExecutionYear();
+				executionYear = ieyp.readExecutionYearByName("2002/2003");
+
+				sp.confirmarTransaccao();
+			} catch (ExcepcaoPersistencia e) {
+				System.out.println("failed setting up the test data");
+			}
+		InfoExecutionYear infoExecutionYear = Cloner.copyIExecutionYear2InfoExecutionYear(executionYear);
+		
+		return infoExecutionYear;
+	
+	}
+
+}
