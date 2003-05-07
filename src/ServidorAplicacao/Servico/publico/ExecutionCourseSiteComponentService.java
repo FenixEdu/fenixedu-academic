44304c3729091a07ca61aeec47476415a4389868
João Mota <joao.mota@ist.utl.pt>
2003-05-07 10:11:57 +0000

    component building service

---
+++ src/ServidorAplicacao/Servico/publico/ExecutionCourseSiteComponentService.java
@@ -0,0 +1,109 @@
+/*
+ * Created on 6/Mai/2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package ServidorAplicacao.Servico.publico;
+
+import DataBeans.ExecutionCourseSiteView;
+import DataBeans.ISiteComponent;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import Dominio.ISite;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Factory.ExecutionCourseSiteComponentBuilder;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.IPersistentSite;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author jmota
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class ExecutionCourseSiteComponentService implements IServico {
+
+	private static ExecutionCourseSiteComponentService _servico =
+		new ExecutionCourseSiteComponentService();
+
+	/**
+	  * The actor of this class.
+	  **/
+
+	private ExecutionCourseSiteComponentService() {
+
+	}
+
+	/**
+	 * Returns Service Name
+	 */
+	public String getNome() {
+		return "ExecutionCourseSiteComponentService";
+	}
+
+	/**
+	 * Returns the _servico.
+	 * @return ReadExecutionCourse
+	 */
+	public static ExecutionCourseSiteComponentService getService() {
+		return _servico;
+	}
+
+	public Object run(
+		ISiteComponent commonComponent,
+		ISiteComponent bodyComponent,		
+		String executionYearName,
+		String executionPeriodName,
+		String executionCourseCode,
+		Integer sectionIndex)
+		throws FenixServiceException {
+			ExecutionCourseSiteView siteView = null;
+			
+		try {
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			IDisciplinaExecucaoPersistente persistentExecutionCourse =
+				sp.getIDisciplinaExecucaoPersistente();
+			IPersistentExecutionPeriod persistentExecutionPeriod =
+				sp.getIPersistentExecutionPeriod();
+			IPersistentExecutionYear persistentExecutionYear =
+				sp.getIPersistentExecutionYear();
+			IPersistentSite persistentSite = sp.getIPersistentSite();
+
+			IExecutionYear executionYear =
+				persistentExecutionYear.readExecutionYearByName(
+					executionYearName);
+			
+			IExecutionPeriod executionPeriod =
+				persistentExecutionPeriod.readByNameAndExecutionYear(
+					executionPeriodName,
+					executionYear);
+
+			IDisciplinaExecucao executionCourse =
+				persistentExecutionCourse
+					.readByExecutionCourseInitialsAndExecutionPeriod(
+					executionCourseCode,
+					executionPeriod);
+
+			ISite site = persistentSite.readByExecutionCourse(executionCourse);
+
+			ExecutionCourseSiteComponentBuilder componentBuilder =
+				ExecutionCourseSiteComponentBuilder.getInstance();
+			commonComponent = componentBuilder.getComponent(commonComponent,site,null,null);
+			bodyComponent = componentBuilder.getComponent(bodyComponent,site,commonComponent,sectionIndex);
+			
+			siteView = new ExecutionCourseSiteView(commonComponent,bodyComponent);
+		} catch (ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		}
+
+		return siteView;
+	}
+}
