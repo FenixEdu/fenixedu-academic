6a16528f2a93189b36b8b35df6cca1b1e4291364
João Luz <joao.luz@ist.utl.pt>
2003-05-09 17:47:08 +0000

    refactored name of EnrolmentFilterPrecedenceRule

---
+++ src/ServidorAplicacao/strategy/enrolment/degree/rules/EnrolmentFilterPrecedenceSpanRule.java
@@ -0,0 +1,64 @@
+/*
+ * Created on 8/Abr/2003 by jpvl
+ *
+ */
+package ServidorAplicacao.strategy.enrolment.degree.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Dominio.ICurricularCourse;
+import Dominio.ICurricularCourseScope;
+import Dominio.IPrecedence;
+import ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext;
+import Util.PrecedenceScopeToApply;
+
+/**
+ * @see ServidorAplicacao.strategy.enrolment.degree.rules.PrecedenceRule
+ * @author jpvl
+ */
+public class EnrolmentFilterPrecedenceSpanRule extends PrecedenceRule implements IEnrolmentRule {
+
+	protected PrecedenceScopeToApply getScopeToApply() {
+		return PrecedenceScopeToApply.TO_APPLY_TO_SPAN;
+	}
+
+	protected void doApply(EnrolmentContext enrolmentContext, List precedenceList, List curricularCourseToApply) {
+		List curricularCourseScopesNotToStay = new ArrayList();
+		for (int i = 0; i < precedenceList.size(); i++) {
+			IPrecedence precedence = (IPrecedence) precedenceList.get(i);
+			if (!precedence.evaluate(enrolmentContext)) {
+				ICurricularCourse curricularCourse =
+					precedence.getCurricularCourse();
+				List scopes = curricularCourse.getScopes();
+				for (int scopeIndex = 0;
+					scopeIndex < scopes.size();
+					scopeIndex++) {
+					ICurricularCourseScope curricularCourseScope =
+						(ICurricularCourseScope) scopes.get(scopeIndex);
+					if (!curricularCourseScopesNotToStay
+						.contains(curricularCourseScope)) {
+						curricularCourseScopesNotToStay.add(
+							curricularCourseScope);
+					}
+				}
+			}
+		}
+		enrolmentContext
+			.getCurricularCoursesScopesAutomaticalyEnroled()
+			.removeAll(
+			curricularCourseScopesNotToStay);
+		curricularCourseToApply.removeAll(curricularCourseScopesNotToStay);
+	}
+
+	/**
+	 * 
+	 * @param enrolmentContext
+	 * @return List to apply this rule
+	 */
+	protected List getListOfCurricularCoursesToApply(EnrolmentContext enrolmentContext) {
+		List finalCurricularCourseScopesSpan =
+			enrolmentContext.getFinalCurricularCoursesScopesSpanToBeEnrolled();
+		return finalCurricularCourseScopesSpan;
+	}
+}
