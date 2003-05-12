1b43d4f7f89d2fafe951ea49e0d3db476e951be4
João Luz <joao.luz@ist.utl.pt>
2003-05-12 12:29:22 +0000

    name self explanatory

---
+++ src/Dominio/RestrictionDoneOrAlreadyEnrolledInCurricularCourse.java
@@ -0,0 +1,23 @@
+/*
+ * Created on 12/Mai/2003 by jpvl
+ *
+ */
+package Dominio;
+
+import ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext;
+
+/**
+ * @author jpvl
+ */
+public class RestrictionDoneOrAlreadyEnrolledInCurricularCourse
+	extends RestrictionCurricularCourseDone {
+
+	/* (non-Javadoc)
+	 * @see Dominio.IRestriction#evaluate(ServidorAplicacao.strategy.enrolment.degree.EnrolmentContext)
+	 */
+	public boolean evaluate(EnrolmentContext enrolmentContext) {
+		ICurricularCourse curricularCourse = this.getPrecedentCurricularCourse();
+		return super.evaluate(enrolmentContext) || enrolmentContext.getCurricularCourseAcumulatedEnrolments(curricularCourse).intValue() > 0;
+	}
+
+}
