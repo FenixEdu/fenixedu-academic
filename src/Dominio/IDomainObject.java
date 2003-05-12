1f0d590017bd7bd532d35ee74e0583cc3a7597c6
João Mota <joao.mota@ist.utl.pt>
2003-05-12 18:28:20 +0000

    domainObject interface

---
+++ src/Dominio/IDomainObject.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 12/Mai/2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package Dominio;
+
+/**
+ * @author jmota
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface IDomainObject {
+	
+	public Integer getIdInternal();
+	public void setIdInternal(Integer idInternal);
+
+}
