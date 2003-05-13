e28bd8ed15aa4d98d00808f394ac234acfeb6a84
David Santos <david.santos@ist.utl.pt>
2003-05-13 16:14:49 +0000

    *** empty log message ***

--- src/UtilInsc/InscTeste.java
+++ src/UtilInsc/InscTeste.java
@@ -1,3 +1,5 @@
+package UtilInsc;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
--- src/UtilInsc/InscTesteIO.java
+++ src/UtilInsc/InscTesteIO.java
@@ -1,3 +1,5 @@
+package UtilInsc;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
