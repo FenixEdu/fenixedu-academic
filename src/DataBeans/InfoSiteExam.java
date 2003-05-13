1d86fc406bff7dfdc0a70eebd2c45905ca1bfbc5
João Mota <joao.mota@ist.utl.pt>
2003-05-13 18:09:53 +0000

    component for display of executionCourse exams

---
+++ src/DataBeans/InfoSiteExam.java
@@ -0,0 +1,36 @@
+/*
+ * Created on 13/Mai/2003
+ *
+ * 
+ */
+package DataBeans;
+
+import java.util.List;
+
+/**
+ * @author João Mota
+ * @author Fernanda Quitério
+ * 
+ *
+ */
+public class InfoSiteExam implements ISiteComponent {
+	
+	private List infoExams;
+	
+	
+
+	/**
+	 * @return
+	 */
+	public List getInfoExams() {
+		return infoExams;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setInfoExams(List list) {
+		infoExams = list;
+	}
+
+}
