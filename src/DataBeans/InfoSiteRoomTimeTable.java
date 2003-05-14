c7c22038fd33be1c6e5a60813c43a895ac344efa
João Mota <joao.mota@ist.utl.pt>
2003-05-14 18:43:59 +0000

    view for room timetable

---
+++ src/DataBeans/InfoSiteRoomTimeTable.java
@@ -0,0 +1,47 @@
+/*
+ * Created on 14/Mai/2003
+ *
+ * 
+ */
+package DataBeans;
+
+import java.util.List;
+
+/**
+ * @author João Mota
+ *
+ */
+public class InfoSiteRoomTimeTable implements ISiteComponent {
+
+	private List infoLessons;
+	private InfoRoom infoRoom;
+
+	/**
+	 * @return
+	 */
+	public List getInfoLessons() {
+		return infoLessons;
+	}
+
+	/**
+	 * @return
+	 */
+	public InfoRoom getInfoRoom() {
+		return infoRoom;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setInfoLessons(List list) {
+		infoLessons = list;
+	}
+
+	/**
+	 * @param list
+	 */
+	public void setInfoRoom(InfoRoom list) {
+		infoRoom = list;
+	}
+
+}
