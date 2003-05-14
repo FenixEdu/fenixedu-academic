f05f8b1769c7ae72df030ad4072dfa1fdeea6c49
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-05-14 11:22:11 +0000

    *** empty log message ***

---
+++ src/ServidorAplicacao/Servico/masterDegree/administrativeOffice/student/certificate/CreateDeclaration.java
@@ -0,0 +1,107 @@
+
+package ServidorAplicacao
+	.Servico
+	.masterDegree
+	.administrativeOffice
+	.student
+	.certificate;
+
+import DataBeans.InfoStudent;
+import DataBeans.InfoStudentCurricularPlan;
+import DataBeans.util.Cloner;
+import Dominio.IPessoa;
+import Dominio.IRole;
+import Dominio.IStudentCurricularPlan;
+import ServidorAplicacao.FenixServiceException;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+import ServidorPersistente.exceptions.ExistingPersistentException;
+
+/**
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ *         Joana Mota (jccm@rnl.ist.utl.pt)
+ */
+
+public class CreateDeclaration implements IServico {
+
+	private static CreateDeclaration servico = new CreateDeclaration();
+
+	/**
+	 * The singleton access method of this class.
+	 **/
+	public static CreateDeclaration getService() {
+		return servico;
+	}
+
+	/**
+	 * The actor of this class.
+	 **/
+	private CreateDeclaration() {
+	}
+
+	/**
+	 * Returns The Service Name */
+
+	public final String getNome() {
+		return "CreateDeclaration";
+	}
+
+	public InfoStudentCurricularPlan run(InfoStudent infoStudent)
+		throws Exception {
+
+		IStudentCurricularPlan studentCurricularPlan = null;
+
+		ISuportePersistente sp = null;
+
+		IRole role = null;
+		IPessoa person = null;
+
+		try {
+			sp = SuportePersistenteOJB.getInstance();
+
+			studentCurricularPlan =
+				sp
+					.getIStudentCurricularPlanPersistente()
+					.readActiveStudentCurricularPlan(
+					infoStudent.getNumber(),
+					infoStudent.getDegreeType());
+					
+	
+		} catch (ExistingPersistentException ex) {
+			throw new ExistingServiceException(ex);
+		} catch (ExcepcaoPersistencia ex) {
+			FenixServiceException newEx =
+				new FenixServiceException("Persistence layer error");
+			newEx.fillInStackTrace();
+			throw newEx;
+		}
+System.out.println("serviço create declaration");
+		if (studentCurricularPlan==null)
+		    return null;
+		else
+			return Cloner.copyIStudentCurricularPlan2InfoStudentCurricularPlan(studentCurricularPlan);
+			
+		//		try {
+		//			// Give the Master Degree Candidate Role
+		//			IPersonRole personRole = new PersonRole();
+		//			role = sp.getIPersistentRole().readByRoleType(RoleType.MASTER_DEGREE_CANDIDATE);
+		//			personRole.setPerson(person);
+		//			personRole.setRole(role);
+		//				
+		//			sp.getIPersistentPersonRole().write(personRole);
+		//
+		//		} catch (ExistingPersistentException ex) {
+		//			// This person is already a Candidate. No need to give the role again
+		//		} catch (ExcepcaoPersistencia ex) {
+		//			FenixServiceException newEx = new FenixServiceException("Persistence layer error");
+		//			newEx.fillInStackTrace();
+		//			throw newEx;
+		//		} 
+		//		
+		//		// Return the new Candidate
+		
+	}
+}
