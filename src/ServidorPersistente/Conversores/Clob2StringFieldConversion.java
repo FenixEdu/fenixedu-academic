9b8d1a2eb60008f9c4cb52b495c5f44926321ab7
David Santos <david.santos@ist.utl.pt>
2003-05-15 11:27:35 +0000

    FIXED: java.lang nullPointerException Bug in java to sql

--- src/ServidorPersistente/Conversores/Clob2StringFieldConversion.java
+++ src/ServidorPersistente/Conversores/Clob2StringFieldConversion.java
@@ -5,9 +5,6 @@
 
 package ServidorPersistente.Conversores;
 
-
-
-
 /**
  *
  * @author  Luis Cruz & Sara Ribeiro
@@ -17,41 +14,39 @@ import java.sql.SQLException;
 
 import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
 
-public class Clob2StringFieldConversion implements FieldConversion
-{
+public class Clob2StringFieldConversion implements FieldConversion {
 
-    public Object javaToSql(Object source)
-    {
-		String str = (String) source; 
-		Clob clob = new com.mysql.jdbc.Clob(str);
-		try {
-			clob.setAsciiStream(str.length());
-		} catch (SQLException e) {
-			e.printStackTrace(System.out);
-		}
-    	return clob;
-    }
+	public Object javaToSql(Object source) {
+		String str = (String) source;
+		Clob clob = null;
+		if (str != null) {
 
-    public Object sqlToJava(Object source)
-    {
-        if (source instanceof Clob)
-        {
-            Clob c = (Clob) source;
-           
-            Long l;
-			String str = null;
+			clob = new com.mysql.jdbc.Clob(str);
 			try {
-				l = new Long(c.length());
-				str = c.getSubString(1, l.intValue());				
+				clob.setAsciiStream(str.length());
 			} catch (SQLException e) {
 				e.printStackTrace(System.out);
 			}
-            return str;
-        }
-        else
-        {
-         	return source;   
-        }
-    }
+		}
+		return clob;
+	}
+
+	public Object sqlToJava(Object source) {
+		if (source instanceof Clob) {
+			Clob c = (Clob) source;
+
+			Long l;
+			String str = null;
+			try {
+				l = new Long(c.length());
+				str = c.getSubString(1, l.intValue());
+			} catch (SQLException e) {
+				e.printStackTrace(System.out);
+			}
+			return str;
+		} else {
+			return source;
+		}
+	}
 
 }
