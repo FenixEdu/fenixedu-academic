51a0e500765b31b13efc236b0ff3d02cc238d53b
João Luz <joao.luz@ist.utl.pt>
2003-05-22 13:34:16 +0000

    manages the form executionCourseShiftsPercentage

--- src/ServidorApresentacao/Action/teacher/credits/ProfessorShipShiftPercentageDispatchAction.java
+++ src/ServidorApresentacao/Action/teacher/credits/ProfessorShipShiftPercentageDispatchAction.java
@@ -4,6 +4,7 @@
  */
 package ServidorApresentacao.Action.teacher.credits;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,10 +15,12 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
 import org.apache.struts.actions.DispatchAction;
 import org.apache.struts.validator.DynaValidatorForm;
 
 import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoShift;
 import DataBeans.InfoTeacher;
 import DataBeans.teacher.credits.InfoShiftPercentage;
 import DataBeans.teacher.credits.InfoTeacherShiftPercentage;
@@ -63,12 +66,12 @@ public class ProfessorShipShiftPercentageDispatchAction
 
 		initializeForm(
 			professorshipShiftPercentageForm,
-			infoShiftPercentageList, infoTeacher);
+			infoShiftPercentageList,
+			infoTeacher);
 
 		request.setAttribute(
 			"infoShiftPercentageList",
 			infoShiftPercentageList);
-		System.out.println("AQUI::::::::::::::::::::::::::::::");
 		return mapping.findForward("showTable");
 	}
 
@@ -78,31 +81,36 @@ public class ProfessorShipShiftPercentageDispatchAction
 	 */
 	private void initializeForm(
 		DynaValidatorForm professorshipShiftPercentageForm,
-		List infoShiftPercentageList, InfoTeacher infoTeacher) {
-			
-		Integer [] shiftsInternalCodes = new Integer [infoShiftPercentageList.size()];
-		Double [] percentages = new Double [infoShiftPercentageList.size()];
-		
-		for (int i = 0; i < infoShiftPercentageList.size(); i++){
+		List infoShiftPercentageList,
+		InfoTeacher infoTeacher) {
+
+		Integer[] shiftsInternalCodes =
+			new Integer[infoShiftPercentageList.size()];
+
+		for (int i = 0; i < infoShiftPercentageList.size(); i++) {
 			InfoShiftPercentage infoShiftPercentage =
 				(InfoShiftPercentage) infoShiftPercentageList.get(i);
-			percentages[i] = infoShiftPercentage.getAvailablePercentage();
-			
-			List infoTeacherShiftPercentageList = infoShiftPercentage.getTeacherShiftPercentageList();
-			
+
+			List infoTeacherShiftPercentageList =
+				infoShiftPercentage.getTeacherShiftPercentageList();
+
 			Iterator iterator = infoTeacherShiftPercentageList.iterator();
-						
 			while (iterator.hasNext()) {
-				InfoTeacherShiftPercentage infoTeacherShiftPercentage = (InfoTeacherShiftPercentage) iterator.next();
-				if (!infoTeacherShiftPercentage.getInfoProfesorShip().getInfoTeacher().equals(infoTeacher)){
-					shiftsInternalCodes [i] = infoShiftPercentage.getShift().getIdInternal();
+				InfoTeacherShiftPercentage infoTeacherShiftPercentage =
+					(InfoTeacherShiftPercentage) iterator.next();
+				if (infoTeacherShiftPercentage
+					.getInfoProfessorship()
+					.getInfoTeacher()
+					.equals(infoTeacher)) {
+					shiftsInternalCodes[i] =
+						infoShiftPercentage.getShift().getIdInternal();
 					break;
 				}
 			}
 		}
-		
-		professorshipShiftPercentageForm.set("shiftProfessorships",shiftsInternalCodes);
-		professorshipShiftPercentageForm.set("shiftProfessorshipsPercentages", percentages);
+		professorshipShiftPercentageForm.set(
+			"shiftProfessorships",
+			shiftsInternalCodes);
 	}
 
 	public ActionForward accept(
@@ -112,6 +120,66 @@ public class ProfessorShipShiftPercentageDispatchAction
 		HttpServletResponse response)
 		throws Exception {
 
+		HttpSession session = request.getSession();
+
+		IUserView userView = SessionUtils.getUserView(request);
+		InfoTeacher infoTeacher =
+			(InfoTeacher) session.getAttribute(SessionConstants.INFO_TEACHER);
+
+		List infoTeacherShiftPercentageList =
+			processForm((DynaActionForm) form, request);
+
+		Object[] args = { infoTeacher, getInfoExecutionCourse((DynaActionForm)form), infoTeacherShiftPercentageList };
+		ServiceUtils.executeService(
+			userView,
+			"AcceptTeacherExecutionCourseShiftPercentage",
+			args);
+
 		return mapping.findForward("acceptSuccess");
+	}
+
+	private InfoExecutionCourse getInfoExecutionCourse(DynaActionForm form) {
+		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
+		infoExecutionCourse.setIdInternal((Integer) form.get("executionCourseInternalCode"));
+		return infoExecutionCourse;
+	}
+
+	private List processForm(DynaActionForm form, HttpServletRequest request) {
+		List infoTeacherShiftPercentageList = new ArrayList();
+
+		InfoTeacherShiftPercentage infoTeacherShiftPercentage =
+			new InfoTeacherShiftPercentage();
+
+		Integer[] shiftProfessorships =
+			(Integer[]) form.get("shiftProfessorships");
+
+		for (int i = 0; i < shiftProfessorships.length; i++) {
+			Integer shiftInternalCode = shiftProfessorships[i];
+			if (shiftInternalCode != null) {
+				Double percentage = null;
+				try {
+					percentage =
+						Double.valueOf(
+							request.getParameter(
+								"percentage_" + shiftInternalCode));
+				} catch (NumberFormatException e) {
+
+				}
+
+				if (percentage != null) {
+					infoTeacherShiftPercentage =
+						new InfoTeacherShiftPercentage();
+					infoTeacherShiftPercentage.setPercentage(percentage);
+
+					InfoShift infoShift = new InfoShift();
+					infoShift.setIdInternal(shiftInternalCode);
+					infoTeacherShiftPercentage.setInfoShift(infoShift);
+					infoTeacherShiftPercentageList.add(
+						infoTeacherShiftPercentage);
+				}
+			}
+		}
+
+		return infoTeacherShiftPercentageList;
 	}
 }
