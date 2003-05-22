e8211eb557ff49c8c861d6d92835b7c9f681861a
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-05-22 16:23:56 +0000

    Changed to the new Philosofy in Updating

--- src/ServidorAplicacao/Servico/person/ServicoSeguroActualizarFuncsNaoDocentes.java
+++ src/ServidorAplicacao/Servico/person/ServicoSeguroActualizarFuncsNaoDocentes.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
+
+import middleware.RoleFunctions;
+
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryByCriteria;
 
 import Dominio.FuncNaoDocente;
 import Dominio.Funcionario;
-import ServidorAplicacao.Servico.exceptions.NotExecuteException;
-import ServidorPersistenteJDBC.IFuncNaoDocentePersistente;
-import ServidorPersistenteJDBC.IFuncionarioPersistente;
-import ServidorPersistenteJDBC.SuportePersistente;
+import Dominio.IPersonRole;
+import Dominio.Pessoa;
 import Util.LeituraFicheiroFuncNaoDocente;
+import Util.RoleType;
 
 /**
  * @author  Ivo Brandão
@@ -43,71 +51,93 @@ public class ServicoSeguroActualizarFuncsNaoDocentes {
 	}
 
 	/** Executa a actualizacao da tabela FuncNaoDocente na Base de Dados */
-	public static void main(String[] args) throws NotExecuteException {
+	public static void main(String[] args) throws Exception {
 		ServicoSeguroActualizarFuncsNaoDocentes servico = new ServicoSeguroActualizarFuncsNaoDocentes(args);
 		LeituraFicheiroFuncNaoDocente servicoLeitura = new LeituraFicheiroFuncNaoDocente();
+		PersistenceBroker broker = PersistenceBrokerFactory.defaultPersistenceBroker();
+
+		broker.clearCache();
 
 		lista = servicoLeitura.lerFicheiro(ficheiro, delimitador, estrutura, ordem);
 
+		System.out.println("Migrating " + lista.size() + " Funcionarios Nao Docentes ... ");
+
+		FuncNaoDocente funcNaoDocente = null;
+		Integer numeroMecanografico = null; 
+		
 		/* ciclo para percorrer a Collection de Funcionarios */
 		/* algoritmo */
 
-		/* Recuperar registos */
-		System.out.println("ServicoSeguroActualizarFuncsNaoDocentes.main:Lista de Resultados...");
-		Iterator iterador = lista.iterator();
-		while (iterador.hasNext()) {
-			System.out.println(iterador.next());
-		}
 
 		/* Procurar chaveFuncionario correspondente e criar funcNaoDocente */
 		Iterator iteradorNovo = lista.iterator();
-		while (iteradorNovo.hasNext()) {
-			IFuncionarioPersistente iFuncionarioPersistente = SuportePersistente.getInstance().iFuncionarioPersistente();
-			IFuncNaoDocentePersistente iFuncNaoDocentePersistente = SuportePersistente.getInstance().iFuncNaoDocentePersistente();
-			Hashtable instanciaTemporaria = (Hashtable) iteradorNovo.next();
-
-			Integer numeroMecanografico = new Integer((String) instanciaTemporaria.get("numeroMecanografico"));
-
-			//ler funcionario correspondente ao nao docente
-			Funcionario funcionario = iFuncionarioPersistente.lerFuncionarioPorNumMecanografico(numeroMecanografico.intValue());
-			System.out.println("ServicoSeguroActualizarFuncsNaoDocentes.main:FuncNaoDocente " + numeroMecanografico.intValue());
-
-			if (funcionario != null) {
-				System.out.println(
-					"ServicoSeguroActualizarFuncsNaoDocentes.main:FuncNaoDocente "
-						+ numeroMecanografico.intValue()
-						+ " tem Funcionario correspondente.");
-
-				FuncNaoDocente funcNaoDocenteBD =
-					iFuncNaoDocentePersistente.lerFuncNaoDocentePorNumMecanografico(numeroMecanografico.intValue());
-
-				if (funcNaoDocenteBD != null) {
-					System.out.println(
-						"ServicoSeguroActualizarFuncsNaoDocentes.main:FuncNaoDocente "
-							+ numeroMecanografico.intValue()
-							+ " tem FuncNaoDocente correspondente.");
-				} else {
-					FuncNaoDocente funcNaoDocente =
-						new FuncNaoDocente(iFuncNaoDocentePersistente.ultimoCodigoInterno() + 1, funcionario.getCodigoInterno());
-
-					iFuncNaoDocentePersistente.escreverFuncNaoDocente(funcNaoDocente);
-
-					System.out.println(
-						"ServicoSeguroActualizarFuncsNaoDocentes.main:FuncNaoDocente "
-							+ numeroMecanografico.intValue()
-							+ " nao tem FuncNaoDocente correspondente.");
+		
+		try {
+			broker.beginTransaction();
+			while (iteradorNovo.hasNext()) {
+	
+				Hashtable instanciaTemporaria = (Hashtable) iteradorNovo.next();
+	
+				numeroMecanografico = new Integer((String) instanciaTemporaria.get("numeroMecanografico"));
+				String numeroDocumentoIdentificacao = (String) instanciaTemporaria.get("numeroDocumentoIdentificacao");
+				Integer tipoDocumentoIdentificacao = (Integer) instanciaTemporaria.get("tipoDocumentoIdentificacao");
+	
+				Criteria criteria = new Criteria();
+				Query query = null;
+				
+				// Read The Person
+				criteria.addEqualTo("numeroDocumentoIdentificacao", numeroDocumentoIdentificacao);
+				criteria.addEqualTo("tipoDocumentoIdentificacao", tipoDocumentoIdentificacao);
+				query = new QueryByCriteria(Pessoa.class,criteria);
+				List resultPerson = (List) broker.getCollectionByQuery(query);	
+	
+				if (resultPerson.size() == 0){
+					throw new Exception("Error Reading Existing Person");
 				}
-
-			} //(funcionario!=null)
-			else {
-				System.out.println(
-					"ServicoSeguroActualizarFuncsNaoDocentes.main:FuncNaoDocente "
-						+ numeroMecanografico.intValue()
-						+ " nao tem Funcionario correspondente.");
-			} //(funcionario==null)
-
+				
+				Pessoa person = (Pessoa) resultPerson.get(0);
+				
+				
+				// Check if a Employee exists
+	
+				criteria = new Criteria();
+				query = null;
+				
+				criteria.addEqualTo("numeroMecanografico", numeroMecanografico);
+				query = new QueryByCriteria(Funcionario.class,criteria);
+				List resultFuncionario = (List) broker.getCollectionByQuery(query);	
+	
+				if (resultFuncionario.size() == 0){
+					throw new Exception("Error Reading Existing Employee");
+				}
+				
+				Funcionario funcionario = (Funcionario) resultFuncionario.get(0);
+				
+				criteria = new Criteria();
+				query = null;
+				
+				criteria.addEqualTo("chaveFuncionario", new Integer(funcionario.getCodigoInterno()));
+				query = new QueryByCriteria(FuncNaoDocente.class,criteria);
+				List resultFuncNaoDocente = (List) broker.getCollectionByQuery(query);	
+	
+	
+				if (resultFuncNaoDocente.size() == 0){
+					funcNaoDocente = new FuncNaoDocente();
+					funcNaoDocente.setChaveFuncionario(funcionario.getCodigoInterno());
+					broker.store(funcNaoDocente);
+					
+					IPersonRole personRole = RoleFunctions.readPersonRole(person, RoleType.EMPLOYEE, broker);
+					if (personRole == null){
+						RoleFunctions.giveRole(person, RoleType.EMPLOYEE, broker);
+					}
+					
+				}
+			}
+			broker.commitTransaction();
+		} catch(Exception e) {
+			broker.commitTransaction();
+			throw new Exception("Error migrating employee " + numeroMecanografico + "\n"+ e);
 		}
-
+		System.out.println("  Done !");
 	}
-
 }
