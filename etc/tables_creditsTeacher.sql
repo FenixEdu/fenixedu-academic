81b67fc98f3b665c43f5043a1c4af80eb4bb5e02
João Luz <joao.luz@ist.utl.pt>
2003-05-27 15:20:57 +0000

    Table Credits added

--- etc/tables_creditsTeacher.sql
+++ etc/tables_creditsTeacher.sql
@@ -10,3 +10,19 @@ create table TEACHER_SHIFT_PERCENTAGE (
    primary key (ID_INTERNAL),
    unique U1 (KEY_PROFESSOR_SHIP, KEY_SHIFT))
    type=InnoDB;
+   
+----------------------------
+-- Table structure for CREDITS
+----------------------------
+drop table if exists CREDITS;
+create table CREDITS (
+   ID_INTERNAL int(11) not null auto_increment,
+   KEY_EXECUTION_PERIOD int(11) not null,
+   KEY_TEACHER int(11) not null,
+   TFC_STUDENTS_NUMBER int(11) not null,
+   CREDITS float,
+   primary key (ID_INTERNAL),
+   unique U1 (KEY_EXECUTION_PERIOD, KEY_TEACHER))
+   type=InnoDB;
+   
+   
