07729ab90d481c33737a31f563f2069c7e9c5c93
João Mota <joao.mota@ist.utl.pt>
2003-05-28 20:44:28 +0000

    service that returns a siteView for the list of students enrolled in an exam

---
+++ src/ServidorAplicacao/Servico/teacher/ReadStudentsEnrolledInExam.java
@@ -0,0 +1,75 @@
+package ServidorAplicacao.Servico.teacher;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import DataBeans.ISiteComponent;
+import DataBeans.InfoSiteTeacherStudentsEnrolledList;
+import DataBeans.InfoStudent;
+import DataBeans.SiteView;
+import DataBeans.util.Cloner;
+import Dominio.Exam;
+import Dominio.IExam;
+import Dominio.IStudent;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExam;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author João Mota
+ *
+ */
+public class ReadStudentsEnrolledInExam implements IServico {
+	private static ReadStudentsEnrolledInExam service =
+		new ReadStudentsEnrolledInExam();
+
+	/**
+	 * The singleton access method of this class.
+	 */
+	public static ReadStudentsEnrolledInExam getService() {
+		return service;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.IServico#getNome()
+	 */
+	public String getNome() {
+		return "ReadStudentsEnrolledInExam";
+	}
+
+	public Object run(Integer executionCourseCode, Integer examCode)
+		throws FenixServiceException {
+		try {
+
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			IPersistentExam persistentExam = sp.getIPersistentExam();
+			IDisciplinaExecucaoPersistente persistentExecutionCourse =
+				sp.getIDisciplinaExecucaoPersistente();
+			IExam exam = new Exam();
+			exam.setIdInternal(examCode);
+			exam = (IExam) persistentExam.readByOId(exam);
+			List students = exam.getStudentsEnrolled();
+			List infoStudents = new ArrayList();
+			Iterator iter = students.iterator();
+			while (iter.hasNext()) {
+				IStudent student = (IStudent) iter.next();
+				InfoStudent infoStudent =
+					Cloner.copyIStudent2InfoStudent(student);
+				infoStudents.add(infoStudent);
+			}
+			ISiteComponent component =
+				new InfoSiteTeacherStudentsEnrolledList(infoStudents);
+
+			SiteView siteView = new SiteView(component);
+			return siteView;
+		} catch (ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		}
+
+	}
+}
