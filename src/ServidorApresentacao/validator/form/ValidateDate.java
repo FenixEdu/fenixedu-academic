c0c8ceb5b506bd67326daf9369edc2541ed080dc
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-06-05 09:33:33 +0000

    *** empty log message ***

--- src/ServidorApresentacao/validator/form/ValidateDate.java
+++ src/ServidorApresentacao/validator/form/ValidateDate.java
@@ -40,14 +40,17 @@ public class ValidateDate {
 		String sProperty3 = field.getVarValue("day");
 		Integer day = new Integer(ValidatorUtil.getValueAsString(bean,sProperty3));
 
-//		if ((day.intValue() == -1) || (month.intValue() == -1) || (year.intValue() == -1))
-//			return false;
+
+		if ((day.intValue() == -1) || (month.intValue() == -1) || (year.intValue() == -1)){
+			errors.add(field.getKey(),new ActionError(va.getMsg(), field.getArg0().getKey()));
+			return false;
+		}
 		
 		if (!GenericValidator.isBlankOrNull(valueString)) {
 			  if (!Data.validDate(day, month, year) || 
 			      year == null || month == null || day == null ||
 			      year.intValue() < 1 || month.intValue() < 0 || day.intValue() < 1) 
-				 errors.add(field.getKey(),new ActionError("errors.invalid.date", field.getArg0().getKey()));
+				 errors.add(field.getKey(),new ActionError(va.getMsg(), field.getArg0().getKey()));
 
 				 return false;
 		}
