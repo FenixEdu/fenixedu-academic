cc4452da5e9d28b183633b62335e05b71c64eb18
Sara Ribeiro <srmr@mega.ist.utl.pt>
2003-06-06 16:29:05 +0000

    Added more missing execution courses.

--- src/middleware/thor/MissingExecutionCourses.sql
+++ src/middleware/thor/MissingExecutionCourses.sql
@@ -68,3 +68,135 @@ insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1794, 34771);
 
 ###########################################################
 ###########################################################
+
+
+###########################################################
+######## Curso de Engenharia em Matemática Aplicada e Computação
+
+insert into EXECUTION_COURSE values (34772, 'ANÁLISE MATEMÁTICA I A', 'AMIA', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2299, 34772);
+
+insert into EXECUTION_COURSE values (34773, 'ÁLGEBRA LINEAR A', 'ALA', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2297, 34773);
+
+insert into EXECUTION_COURSE values (34774, 'LÓGICA E TEORIA DA COMPUTAÇÃO', 'LTC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2338, 34774);
+
+insert into EXECUTION_COURSE values (34775, 'MATEMÁTICA EXPERIMENTAL', 'ME', 2, 0, 4, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2341, 34775);
+
+insert into EXECUTION_COURSE values (34776, 'ANÁLISE MATEMÁTICA III A', 'AMIIIA', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2303, 34776);
+
+insert into EXECUTION_COURSE values (34777, 'TERMODINÂMICA E ESTRUTURA DA MATÉRIA', 'TEM', 3, 1, 2, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2333, 34777);
+
+insert into EXECUTION_COURSE values (34778, 'SONDAGENS', 'S', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2170, 34778);
+
+insert into EXECUTION_COURSE values (34779, 'COMPUTABILIDADE E COMPLEXIDADE I', 'CCI', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2171, 34779);
+
+insert into EXECUTION_COURSE values (34780, 'MÉTODOS NUMÉRICOS PARA PROBLEMAS ELÍPTICOS', 'MNPE', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2024, 34780);
+
+insert into EXECUTION_COURSE values (34781, 'GEOMETRIA DIFERENCIAL (M)', 'GD', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2193, 34781);
+
+insert into EXECUTION_COURSE values (34782, 'TRABALHO FINAL DE CURSO I', 'TFCI', 3, 3, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1793, 34782);
+
+insert into EXECUTION_COURSE values (34783, 'TÓPICOS ESPECIAIS I', 'TE', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2027, 34783);
+
+insert into EXECUTION_COURSE values (34784, 'ANÁLISE NUMÉRICA DE EQUAÇÕES INTEGRAIS (M)', 'ANEI', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2182, 34784);
+
+insert into EXECUTION_COURSE values (34785, 'ÁLGEBRA COMUTATIVA (M)', 'AC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2183, 34785);
+
+insert into EXECUTION_COURSE values (34786, 'ÁGEBRAS DE OPERADORES (M)', 'AO', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2186, 34786);
+
+insert into EXECUTION_COURSE values (34787, 'ESTATÍSTICA COMPUTACIONAL (M)', 'EC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2187, 34787);
+
+insert into EXECUTION_COURSE values (34788, 'CÁLCULO DE VARIAÇÕES (M)' 'CV', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2193, 34788);
+
+insert into EXECUTION_COURSE values (34789, 'TEORIA DA COMPUTABILIDADE (M)' 'TC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2276, 34789);
+
+insert into EXECUTION_COURSE values (34790, 'CÁLCULO ESTOCÁSTICO E APLICAÇÕES A EQUAÇÕES DIFERENCIAIS PARCIAIS (M)' 'CEAEDP', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2342, 34790);
+
+insert into EXECUTION_COURSE values (34791, 'TOPOLOGIA ALGÉBRICA (M)' 'TA', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2277, 34791);
+
+insert into EXECUTION_COURSE values (34792, 'TEORIA DAS CATEGORIAS (M)' 'TC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2184, 34792);
+
+insert into EXECUTION_COURSE values (34793, 'GRUPOS DE LIE E ÁLGEBRAS DE LIE (M)' 'GLAL', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2278, 34793);
+
+insert into EXECUTION_COURSE values (34794, 'MÉTODOS NUMÉRICOS PARA EQUAÇÕES DIFERENCIAIS ORDINÁRIAS (M)' 'MNEDO', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2343, 34794);
+
+###########################################################
+###########################################################
+
+
+###########################################################
+######## Curso de Engenharia de Materiais
+
+insert into EXECUTION_COURSE values (34795, 'QUÍMICA', 'Q', 3, 0, 3, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1970, 34795);
+
+insert into EXECUTION_COURSE values (34796, 'COMPUTAÇÃO E PROGRAMAÇÃO', 'CP', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2308, 34796);
+
+insert into EXECUTION_COURSE values (34797, 'DESENHO E MODELAÇÃO GEOMÉTRICA', 'DMG', 5, 0, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2315, 34797);
+
+insert into EXECUTION_COURSE values (34798, 'INTRODUÇÃO À QUÍMICA-FÍSICA', 'IQF', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1849, 34798);
+
+insert into EXECUTION_COURSE values (34799, 'ELECTROMAGNETISMO E ÓPTICA', 'EO', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2326, 34799);
+
+insert into EXECUTION_COURSE values (34800, 'MATERIAIS LENHO-CELULÓSICOS', 'MLC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2199, 34800);
+
+insert into EXECUTION_COURSE values (34801, 'MATERIAIS ELECTRÓNICOS', 'ME', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2200, 34801);
+
+###########################################################
+###########################################################
+
+
+###########################################################
+######## Curso de Engenharia de Civil
+
+insert into EXECUTION_COURSE values (34802, 'DESENHO TÉCNICO', 'DT', 0, 0, 0, 4, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2313, 34802);
+
+insert into EXECUTION_COURSE values (34803, 'PROGRAMAÇÃO E BASES DE DADOS', 'PBD', 2, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2312, 34803);
+
+insert into EXECUTION_COURSE values (34804, 'QUÍMICA GERAL', 'QG', 3, 1, 1, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1890, 34804);
+
+insert into EXECUTION_COURSE values (34805, 'ARQUITECTURA', 'A', 2, 3, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1842, 34805);
+insert into CURRICULAR_COURSE_SCOPE values(null, 3, 1842, 7913, 2, 3, 0, 0, 0, null, null, null);
+
+insert into EXECUTION_COURSE values (34806, 'EDIFICAÇÕES II', 'EII', 0, 0, 4, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1946, 34806);
+insert into CURRICULAR_COURSE_SCOPE values(null, 9, 1946, 7910, 0, 0, 4, 0, 0, null, null, null);
+
+insert into EXECUTION_COURSE values (34807, 'PROCESSOS DE CONSTRUÇÃO', 'PC', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2057, 34807);
+insert into CURRICULAR_COURSE_SCOPE values(null, 9, 2057, 7910, 2, 3, 0, 0, 0, null, null, null);
+
+###########################################################
+###########################################################
