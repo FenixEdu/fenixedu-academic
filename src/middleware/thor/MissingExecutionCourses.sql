dd5b63dfac0106e1d180f20f0ed5abaa4e422c94
Luis Cruz <luis.cruz@ist.utl.pt>
2003-06-06 15:07:47 +0000

    Corrected id's.

--- src/middleware/thor/MissingExecutionCourses.sql
+++ src/middleware/thor/MissingExecutionCourses.sql
@@ -2,29 +2,30 @@
 ###########################################################
 ######## Curso de Engenharia Biológica
 
-insert into EXECUTION_COURSE values (34726, 'COMPUTAÇÃO E PROGRAMAÇÃO', 'CEP', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2373, 34726);
+insert into EXECUTION_COURSE values (34756, 'COMPUTAÇÃO E PROGRAMAÇÃO', 'CEP', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2309, 34756);
 
 ### PE
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1934, 34540);
+#insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1934, 34540);
 
-insert into EXECUTION_COURSE values (34727, 'ELECTROMAGNETISMO E ÓPTICA', 'EE0', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2397, 34727);
+insert into EXECUTION_COURSE values (34757, 'ELECTROMAGNETISMO E ÓPTICA', 'EE0', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2329, 34757);
 
-insert into EXECUTION_COURSE values (34728, 'ANÁLISE E SIMULAÇÃO NUMÉRICA', 'ASN', 3, 1, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2369, 34728);
+insert into EXECUTION_COURSE values (34758, 'ANÁLISE E SIMULAÇÃO NUMÉRICA', 'ASN', 3, 1, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2307, 34758);
 
-insert into EXECUTION_COURSE values (34729, 'BIOQUÍMICA E FISIOLOGIA MICROBIANA', 'BEFM', 3, 0, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2356, 34729);
+insert into EXECUTION_COURSE values (34759, 'BIOQUÍMICA E FISIOLOGIA MICROBIANA', 'BEFM', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2296, 34759);
 
-insert into EXECUTION_COURSE values (34730, 'MODELOS MULTICRITÉRIO DE APOIO À DECISÃO', 'MMAD', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2131, 34730);
+insert into EXECUTION_COURSE values (34760, 'MODELOS MULTICRITÉRIO DE APOIO À DECISÃO', 'MMAD', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2114, 34760);
 
-insert into EXECUTION_COURSE values (34731, 'BIOLOGIA ESTRUTURAL', 'BE', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2434, 34731);
+insert into EXECUTION_COURSE values (34761, 'BIOLOGIA ESTRUTURAL', 'BE', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2363, 34761);
 
 ### LIMITES DA CIENCIA
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2348, 34682);
+insert into EXECUTION_COURSE values (34762, 'LIMITES DA CIÊNCIA', 'LC', 2, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2290, 34762);
 
 ###########################################################
 ###########################################################
@@ -34,35 +35,36 @@ insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2348, 34682);
 ###########################################################
 ######## Curso de Engenharia Electrotécnica e de Computadores
 
-insert into EXECUTION_COURSE values (34732, 'PROGRAMAÇÃO', 'P', 3, 0, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1916, 34732);
+insert into EXECUTION_COURSE values (34763, 'PROGRAMAÇÃO', 'P', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1910, 34763);
 
 ### QUÍMICA GERAL
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1894, 34569);
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1892, 34620);
 
 ### ELECTROMAGNETISMO E ÓPTICA
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2395, 34727);
+insert into EXECUTION_COURSE values (34764, 'ELECTROMAGNETISMO E ÓPTICA', 'EO', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2328, 34764);
 
-insert into EXECUTION_COURSE values (34733, 'REDES E SISTEMAS DE ENERGIA I', 'RSEI', 3, 1, 0, 1, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2260, 34733);
+insert into EXECUTION_COURSE values (34765, 'REDES E SISTEMAS DE ENERGIA I', 'RSEI', 3, 1, 0, 1, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2213, 34756);
 
-insert into EXECUTION_COURSE values (34734, 'ELECTRÓNICA DE COMPUTADORES', 'EDC', 3, 0, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2276, 34734);
+insert into EXECUTION_COURSE values (34766, 'ELECTRÓNICA DE COMPUTADORES', 'EDC', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2229, 34766);
 
-insert into EXECUTION_COURSE values (34735, 'PROTECÇÃO DE EQUIPAMENTOS E SISTEMAS DE ENERGIA', 'PESE', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2284, 34735);
+insert into EXECUTION_COURSE values (34767, 'PROTECÇÃO DE EQUIPAMENTOS E SISTEMAS DE ENERGIA', 'PESE', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2237, 34767);
 
-insert into EXECUTION_COURSE values (34736, 'OPTIMIZAÇÃO E ALGORITMOS', 'OEA', 3, 2, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2301, 34736);
+insert into EXECUTION_COURSE values (34768, 'OPTIMIZAÇÃO E ALGORITMOS', 'OEA', 3, 2, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2254, 34768);
 
-insert into EXECUTION_COURSE values (34737, 'MODELAÇÃO IDENTIFICAÇÃO E CONTROLO DIGITAL', 'MICD', 3, 0, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2325, 34737);
+insert into EXECUTION_COURSE values (34769, 'MODELAÇÃO IDENTIFICAÇÃO E CONTROLO DIGITAL', 'MICD', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2272, 34769);
 
-insert into EXECUTION_COURSE values (34738, 'SISTEMAS DISTRIBUÍDOS', 'SDST', 3, 0, 0, 2, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2332, 34738);
+insert into EXECUTION_COURSE values (34770, 'SISTEMAS DISTRIBUÍDOS', 'SDST', 3, 0, 0, 2, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 2279, 34770);
 
-insert into EXECUTION_COURSE values (34739, 'TRABALHO FINAL DE CURSO I', 'TFCI', 0, 0, 0, 0, 1, 2, "");
-insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1794, 34739);
+insert into EXECUTION_COURSE values (34771, 'TRABALHO FINAL DE CURSO I', 'TFCI', 0, 0, 0, 0, 1, 2, "");
+insert into CURRICULAR_COURSE_EXECUTION_COURSE values (null, 1794, 34771);
 
 ###########################################################
 ###########################################################
