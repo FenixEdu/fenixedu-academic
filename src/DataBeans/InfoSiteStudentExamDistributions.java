1b4011bb768b4eb3e9751763a8a3955a0ec6159d
João Mota <joao.mota@ist.utl.pt>
2003-06-09 18:44:35 +0000

    databean for exam distribution in student portal

---
+++ src/DataBeans/InfoSiteStudentExamDistributions.java
@@ -0,0 +1,41 @@
+/*
+ * Created on 9/Jun/2003
+ *
+ * 
+ */
+package DataBeans;
+
+import java.util.List;
+
+/**
+ * @author João Mota
+ *
+ */
+public class InfoSiteStudentExamDistributions implements ISiteComponent {
+
+private List examDistributions;
+	/**
+	 * 
+	 */
+	public InfoSiteStudentExamDistributions() {
+	}
+
+	public InfoSiteStudentExamDistributions(List examDistributions) {
+		setExamDistributions(examDistributions);
+		}
+
+/**
+ * @return
+ */
+public List getExamDistributions() {
+	return examDistributions;
+}
+
+/**
+ * @param list
+ */
+public void setExamDistributions(List list) {
+	examDistributions = list;
+}
+
+}
