b4e01fe4d354caca26c1b4e30138cf7745756e17
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-06-11 10:02:34 +0000

    Field Conversion

---
+++ src/ServidorPersistente/Conversores/JavaMarkType2SqlMarkTypeFieldConversion.java
@@ -0,0 +1,46 @@
+
+package ServidorPersistente.Conversores;
+
+import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
+
+import Util.MarkType;
+
+
+/**
+ * 
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ * @author Joana Mota (jccm@rnl.ist.utl.pt)
+ */
+public class JavaMarkType2SqlMarkTypeFieldConversion implements FieldConversion {
+    
+    /*
+     * @see FieldConversion#javaToSql(Object)
+     */
+    public Object javaToSql(Object source)
+    {
+        if (source instanceof MarkType)
+        {
+            MarkType s = (MarkType) source;
+            return s.getType();
+        }
+        else {
+            return source;
+        }
+    }    
+
+   /*
+     * @see FieldConversion#sqlToJava(Object)
+     */
+    public Object sqlToJava(Object source)
+    {
+        if (source instanceof Integer)
+        {
+            Integer src = (Integer) source;
+            return new MarkType(src);
+        }
+        else
+        {
+            return source;
+        }
+    }    
+}
