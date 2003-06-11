e4568b4f1cdfbae396a32935501928c8bb843aa3
João Luz <joao.luz@ist.utl.pt>
2003-06-11 18:50:22 +0000

    added alter table

--- docs/CorrectDatabase027.sql
+++ docs/CorrectDatabase027.sql
@@ -8,3 +8,20 @@ create table CREDITS (
    primary key (ID_INTERNAL),
    unique U1 (KEY_EXECUTION_PERIOD, KEY_TEACHER))
    type=InnoDB;
+   
+#----------------------------
+# Table structure for exam_student_room
+#----------------------------
+drop table if exists EXAM_STUDENT_ROOM;
+create table EXAM_STUDENT_ROOM (
+   ID_INTERNAL int(11) not null auto_increment,
+   KEY_EXAM int(11) not null,
+   KEY_STUDENT int(11) not null,
+   KEY_ROOM int(11) not null,   
+   primary key (ID_INTERNAL),
+   unique U1 (KEY_EXAM, KEY_STUDENT, KEY_ROOM))
+   type=InnoDB;
+
+ALTER TABLE DEGREE_CURRICULAR_PLAN ADD MARK_TYPE integer (11);
+ALTER TABLE DEGREE_CURRICULAR_PLAN ADD NEEDED_CREDITS float (11,2);
+ALTER TABLE ATTEND ADD KEY_ENROLMENT int(11);
