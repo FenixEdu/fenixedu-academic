71a9c2c8c2f20753e6369a466a23c98f6f8cc19e
João Luz <joao.luz@ist.utl.pt>
2003-06-24 11:20:55 +0000

    changed sort

--- src/ServidorApresentacao/Action/teacher/ShowStudentsEnrolledInExamAction.java
+++ src/ServidorApresentacao/Action/teacher/ShowStudentsEnrolledInExamAction.java
@@ -9,19 +9,25 @@ package ServidorApresentacao.Action.teacher;
  * @author lmac1
  *
  */
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.beanutils.BeanComparator;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 import org.apache.commons.collections.comparators.ComparatorChain;
+import org.apache.commons.collections.comparators.NullComparator;
 import org.apache.commons.collections.comparators.ReverseComparator;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import DataBeans.InfoExamStudentRoom;
 import DataBeans.InfoSiteTeacherStudentsEnrolledList;
 import DataBeans.SiteView;
 import ServidorAplicacao.Servico.UserView;
@@ -62,10 +68,23 @@ public class ShowStudentsEnrolledInExamAction extends FenixAction{
 		InfoSiteTeacherStudentsEnrolledList infoSiteTeacherStudentsEnrolledList = (InfoSiteTeacherStudentsEnrolledList) siteView.getComponent();
 		
 		ComparatorChain comparatorChain = new ComparatorChain();
+		
 		comparatorChain.addComparator(new ReverseComparator(new BeanComparator("infoRoom.capacidadeExame")));
 		comparatorChain.addComparator(new BeanComparator("infoRoom.nome"));		
 		comparatorChain.addComparator(new BeanComparator("infoStudent.number"));
-		Collections.sort(infoSiteTeacherStudentsEnrolledList.getInfoExamStudentRoomList(), comparatorChain);
+		
+		List infoExamStudentRoomList = (List) CollectionUtils.select(infoSiteTeacherStudentsEnrolledList.getInfoExamStudentRoomList(), new Predicate(){
+			public boolean evaluate(Object input) {
+				InfoExamStudentRoom infoExamStudentRoom = (InfoExamStudentRoom) input;
+				return infoExamStudentRoom.getInfoRoom() != null;
+			}
+		});
+		
+		Collections.sort(infoExamStudentRoomList, comparatorChain);
+		Collection collection = CollectionUtils.subtract(infoSiteTeacherStudentsEnrolledList.getInfoExamStudentRoomList(),infoExamStudentRoomList);	
+		infoSiteTeacherStudentsEnrolledList.setInfoExamStudentRoomList((List) collection);
+		infoSiteTeacherStudentsEnrolledList.getInfoExamStudentRoomList().addAll(infoExamStudentRoomList);
+		
 		
 		request.setAttribute("siteView",siteView);
 		request.setAttribute("objectCode",executionCourseCode);
