6d2ce9eb91c8fef7a6ddf6b360f088da0079cd5d
Ângela Almeida <angela.almeida@ist.utl.pt>
2003-06-25 11:25:55 +0000

    *** empty log message ***

---
+++ src/DataBeans/InfoEvaluation.java
@@ -0,0 +1,80 @@
+package DataBeans;
+
+import Util.EvaluationType;
+
+
+
+/**
+ * @author dcs-rjao
+ *
+ * 19/Mar/2003
+ */
+
+public class InfoEvaluation  {
+
+	
+	
+	private String publishmentMessage;
+	private EvaluationType evaluationType;
+
+
+	public InfoEvaluation() {
+		setPublishmentMessage(null);
+		setEvaluationType(null);
+	}
+
+	public InfoEvaluation(String publishmentMessage, EvaluationType type) {
+		this();
+		setPublishmentMessage(publishmentMessage);
+		setEvaluationType(type);
+	}
+
+	public boolean equals(Object obj) {
+		boolean resultado = false;
+		if (obj instanceof InfoEvaluation) {
+			InfoEvaluation infoEvaluation = (InfoEvaluation) obj;
+			resultado = this.getPublishmentMessage().equals(infoEvaluation.getPublishmentMessage()) && this.getEvaluationType()== infoEvaluation.getEvaluationType();
+		}
+		return resultado;
+	}
+
+	public String toString() {
+		String result = "[" + this.getClass().getName() + ": ";
+		result += "publishmentMessage = " + this.publishmentMessage + "; ";
+		result += "type = " + this.evaluationType + "]";
+		return result;
+	}
+	
+	
+
+	/**
+	 * @return
+	 */
+	public String getPublishmentMessage() {
+		return publishmentMessage;
+	}
+
+
+	/**
+	 * @param string
+	 */
+	public void setPublishmentMessage(String string) {
+		this.publishmentMessage = string;
+	}
+
+	
+	/**
+	 * @return
+	 */
+	public EvaluationType getEvaluationType() {
+		return evaluationType;
+	}
+
+	/**
+	 * @param type
+	 */
+	public void setEvaluationType(EvaluationType type) {
+		evaluationType = type;
+	}
+
+}
---
+++ src/Util/EvaluationType.java
@@ -0,0 +1,79 @@
+package Util;
+
+
+/**
+ * 
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ *         Joana Mota (jccm@rnl.ist.utl.pt)
+ */
+
+public class EvaluationType {
+
+	public static final int EXAM = 1;
+	public static final int FINAL = 2;
+	
+	
+	public static final EvaluationType EXAM_TYPE = new EvaluationType(EXAM);
+	public static final EvaluationType FINAL_TYPE = new EvaluationType(FINAL);
+	
+	
+	public static final String EXAM_STRING = "Exam";
+	public static final String FINAL_STRING = "Final";
+	
+	
+
+
+
+	private Integer type;
+
+	public EvaluationType() {
+	}
+
+	public EvaluationType(int type) {
+		this.type = new Integer(type);
+	}
+
+	public EvaluationType(Integer type) {
+		this.type = type;
+	}
+
+	public EvaluationType(String type) {
+		if (type.equals(EvaluationType.EXAM_STRING)) this.type = new Integer(EvaluationType.EXAM);
+		if (type.equals(EvaluationType.FINAL_STRING)) this.type = new Integer(EvaluationType.FINAL);
+		
+		
+	}
+
+	public boolean equals(Object obj) {
+		boolean resultado = false;
+		if (obj instanceof EvaluationType) {
+			EvaluationType ds = (EvaluationType) obj;
+			resultado = this.getType().equals(ds.getType());
+		}
+		return resultado;
+	}
+
+	
+	public String toString() {
+		if (type.intValue()== EvaluationType.EXAM) return EvaluationType.EXAM_STRING;
+		if (type.intValue()== EvaluationType.FINAL) return EvaluationType.FINAL_STRING;
+		
+		return "ERRO!"; // Nunca e atingido
+	}      
+
+
+	/**
+	 * @return
+	 */
+	public Integer getType() {
+		return type;
+	}
+
+	/**
+	 * @param integer
+	 */
+	public void setType(Integer integer) {
+		type = integer;
+	}
+
+}
