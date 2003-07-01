028dd02e5f0c0b26cb1dd5a16f5101689fddb012
João Luz <joao.luz@ist.utl.pt>
2003-07-01 14:53:16 +0000

    DTO that contains infoCredits and professorships...

---
+++ src/DataBeans/CreditsView.java
@@ -0,0 +1,45 @@
+/*
+ * Created on 1/Jul/2003 by jpvl
+ *
+ */
+package DataBeans;
+
+import java.util.List;
+
+import DataBeans.teacher.credits.InfoCredits;
+
+/**
+ * @author jpvl
+ */
+public class CreditsView {
+	private InfoCredits infoCredits;
+	private List infoProfessorshipList;
+	/**
+	 * @return
+	 */
+	public InfoCredits getInfoCredits() {
+		return this.infoCredits;
+	}
+
+	/**
+	 * @param infoCredits
+	 */
+	public void setInfoCredits(InfoCredits infoCredits) {
+		this.infoCredits = infoCredits;
+	}
+
+	/**
+	 * @return
+	 */
+	public List getInfoProfessorshipList() {
+		return this.infoProfessorshipList;
+	}
+
+	/**
+	 * @param infoProfessorshipList
+	 */
+	public void setInfoProfessorshipList(List infoProfessorshipList) {
+		this.infoProfessorshipList = infoProfessorshipList;
+	}
+
+}
