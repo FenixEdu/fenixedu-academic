24ab728ef0de1240b34badbcfb8a7c050010a807
João Luz <joao.luz@ist.utl.pt>
2003-07-07 14:17:48 +0000

    merged from branch

---
+++ src/ServidorAplicacao/Servico/sop/AddShiftToClasses.java
@@ -0,0 +1,95 @@
+/*
+ * Created on 1/Jul/2003
+ *
+ * 
+ */
+package ServidorAplicacao.Servico.sop;
+
+import java.util.List;
+
+import Dominio.ITurma;
+import Dominio.ITurmaTurno;
+import Dominio.ITurno;
+import Dominio.Turma;
+import Dominio.TurmaTurno;
+import Dominio.Turno;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servico.exceptions.InvalidArgumentsServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.ITurmaPersistente;
+import ServidorPersistente.ITurmaTurnoPersistente;
+import ServidorPersistente.ITurnoPersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author João Mota
+ *
+ * 1/Jul/2003
+ * fenix-branch
+ * ServidorAplicacao.Servico.sop
+ * 
+ */
+public class AddShiftToClasses implements IServico {
+
+	/**
+	 * 
+	 */
+	public AddShiftToClasses() {
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.IServico#getNome()
+	 */
+	public String getNome() {
+		return "AddShiftToClasses";
+	}
+
+	private static AddShiftToClasses service = new AddShiftToClasses();
+
+	public static AddShiftToClasses getService() {
+		return service;
+	}
+
+	public void run(Integer keyShift, String[] classesList)
+		throws FenixServiceException {
+		List errors = null;
+		try {
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			ITurnoPersistente persistentShift = sp.getITurnoPersistente();
+			ITurmaPersistente persistentClass = sp.getITurmaPersistente();
+			ITurmaTurnoPersistente persistentClassShift =
+				sp.getITurmaTurnoPersistente();
+			ITurno shift = new Turno(keyShift);
+			shift = (ITurno) persistentShift.readByOId(shift, false);
+			if (shift == null || classesList == null) {
+				throw new InvalidArgumentsServiceException();
+			}
+			int iter = 0;
+			int length = classesList.length;
+			while (iter < length) {
+				Integer keyClass = new Integer(classesList[iter]);
+				ITurma dClass = new Turma(keyClass);
+				dClass = (ITurma) persistentClass.readByOId(dClass, false);
+				if (dClass == null) {
+					throw new InvalidArgumentsServiceException();
+				}
+				if (persistentClassShift.readByTurmaAndTurno(dClass, shift)
+					== null) {
+					ITurmaTurno classShift = new TurmaTurno();
+					classShift.setTurma(dClass);
+					classShift.setTurno(shift);
+					persistentClassShift.lockWrite(classShift);
+
+				}
+				iter++;
+			}
+
+		} catch (ExcepcaoPersistencia e) {
+
+		}
+
+	}
+
+}
---
+++ src/ServidorAplicacao/Servico/sop/ReadAllClasses.java
@@ -0,0 +1,100 @@
+/*
+ * Created on 30/Jun/2003
+ *
+ * 
+ */
+package ServidorAplicacao.Servico.sop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanComparator;
+
+import DataBeans.ISiteComponent;
+import DataBeans.InfoClass;
+import DataBeans.InfoSiteClassesComponent;
+import DataBeans.SiteView;
+import DataBeans.util.Cloner;
+import Dominio.ExecutionPeriod;
+import Dominio.IExecutionPeriod;
+import Dominio.ITurma;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.ITurmaPersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author João Mota
+ *
+ * 30/Jun/2003
+ * fenix-branch
+ * ServidorAplicacao.Servico.sop
+ * 
+ */
+public class ReadAllClasses implements IServico {
+
+	/**
+	 * 
+	 */
+	private ReadAllClasses() {
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.IServico#getNome()
+	 */
+	public String getNome() {
+		return "ReadAllClasses";
+	}
+
+	private static ReadAllClasses service = new ReadAllClasses();
+
+	public static ReadAllClasses getService() {
+		return service;
+	}
+
+	public SiteView run(Integer keyExecutionPeriod)
+		throws FenixServiceException {
+		List infoClasses = null;
+		try {
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			IPersistentExecutionPeriod persistentExecutionPeriod =
+				sp.getIPersistentExecutionPeriod();
+
+			IExecutionPeriod executionPeriod =
+				new ExecutionPeriod(keyExecutionPeriod);
+			executionPeriod =
+				(IExecutionPeriod) persistentExecutionPeriod.readByOId(
+					executionPeriod,
+					false);
+
+			ITurmaPersistente persistentClass = sp.getITurmaPersistente();
+			List classes =
+				persistentClass.readByExecutionPeriod(executionPeriod);
+
+			infoClasses = new ArrayList();
+			Iterator iter = classes.iterator();
+			while (iter.hasNext()) {
+				ITurma dClass = (ITurma) iter.next();
+				InfoClass infoClass = Cloner.copyClass2InfoClass(dClass);
+				infoClasses.add(infoClass);
+			}
+			Collections.sort(
+				infoClasses,
+				new BeanComparator("infoExecutionDegree.infoDegreeCurricularPlan.infoDegree.sigla"));
+
+		} catch (ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		}
+		ISiteComponent classesComponent =
+			new InfoSiteClassesComponent(infoClasses);
+		SiteView siteView = new SiteView(classesComponent);
+
+		return siteView;
+	}
+
+}
