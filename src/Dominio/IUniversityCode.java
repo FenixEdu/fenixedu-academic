e409a8fcb2b9185ab6b05a6bc682c4c2ca3ecf4d
Ricardo Oliveira <rjao@mega.ist.utl.pt>
2003-07-09 10:21:27 +0000

    *** empty log message ***

---
+++ src/Dominio/IUniversityCode.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 3/Jul/2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package Dominio;
+
+/**
+ * @author dcs-rjao
+ *
+ * 3/Jul/2003
+ */
+public interface IUniversityCode extends IDomainObject{
+	public abstract String getUniversityCode();
+	public abstract void setUniversityCode(String string);
+}
