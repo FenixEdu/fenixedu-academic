e25872ff07b6cafa975bbd0041e667fd2b39efab
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-07-19 18:09:01 +0000

    Info For Display

---
+++ src/DataBeans/InfoCandidateApproval.java
@@ -0,0 +1,97 @@
+
+package DataBeans;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ *         Joana Mota (jccm@rnl.ist.utl.pt)
+ */
+public class InfoCandidateApproval extends InfoObject implements Serializable {
+
+	protected Integer idInternal;
+	protected Integer orderPosition;
+	
+	protected String candidateName;
+	protected String remarks;
+	protected String situationName;
+	
+	
+	
+
+	
+	public InfoCandidateApproval() {}
+
+
+	/**
+	 * @return
+	 */
+	public String getCandidateName() {
+		return candidateName;
+	}
+
+	/**
+	 * @return
+	 */
+	public Integer getIdInternal() {
+		return idInternal;
+	}
+
+	/**
+	 * @return
+	 */
+	public Integer getOrderPosition() {
+		return orderPosition;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getRemarks() {
+		return remarks;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getSituationName() {
+		return situationName;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setCandidateName(String string) {
+		candidateName = string;
+	}
+
+	/**
+	 * @param integer
+	 */
+	public void setIdInternal(Integer integer) {
+		idInternal = integer;
+	}
+
+	/**
+	 * @param integer
+	 */
+	public void setOrderPosition(Integer integer) {
+		orderPosition = integer;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setRemarks(String string) {
+		remarks = string;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setSituationName(String string) {
+		situationName = string;
+	}
+
+}
