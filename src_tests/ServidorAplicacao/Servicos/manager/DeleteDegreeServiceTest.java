62fd09a0bb6734da0438131797c5ad5a2d667ec4
Hugo Pereira <peppe@clix.pt>
2003-07-22 10:37:20 +0000

    *** empty log message ***

---
+++ src_tests/ServidorAplicacao/Servicos/manager/DeleteDegreeServiceTest.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 22/Jul/2003
+ *
+ * To change this generated comment go to 
+ * Window>Preferences>Java>Code Generation>Code Template
+ */
+package ServidorAplicacao.Servicos.manager;
+
+/**
+ * @author lmac1
+ */
+public class DeleteDegreeServiceTest {
+
+}
