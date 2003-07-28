3340bd7c23aeae5aea4bbc70a09cd157d1bac6cb
Luis Cruz <luis.cruz@ist.utl.pt>
2003-07-28 15:41:17 +0000

    Created wrapper for placing execution period in request.

---
+++ src/ServidorApresentacao/Action/utils/ContextUtils.java
@@ -0,0 +1,53 @@
+/*
+ * Created on 2003/07/28
+ *
+ */
+package ServidorApresentacao.Action.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import DataBeans.InfoExecutionPeriod;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
+import ServidorApresentacao.Action.sop.utils.SessionConstants;
+
+/**
+ * @author Luis Cruz & Sara Ribeiro
+ *
+ */
+public class ContextUtils {
+
+	public static final void setExecutionPeriodContext(HttpServletRequest request) {
+		Integer executionPeriodOID =
+			new Integer(request.getParameter("executionPeriodOID"));
+		InfoExecutionPeriod infoExecutionPeriod = null;
+
+		if (executionPeriodOID != null) {
+			// Read from database
+			try {
+				Object[] args = { executionPeriodOID };
+				infoExecutionPeriod =
+					(InfoExecutionPeriod) ServiceUtils.executeService(
+						null,
+						"ReadExecutionPeriodByOID",
+						args);
+			} catch (FenixServiceException e) {
+				e.printStackTrace();
+			}
+		} else {
+			// Read currente execution period from database
+			try {
+				infoExecutionPeriod =
+					(InfoExecutionPeriod) ServiceUtils.executeService(
+						null,
+						"ReadCurrentExecutionPeriod",
+						new Object[0]);
+			} catch (FenixServiceException e) {
+				e.printStackTrace();
+			}
+		}
+		// Place it in request
+		request.setAttribute(SessionConstants.EXECUTION_PERIOD, infoExecutionPeriod);
+	}
+
+}
