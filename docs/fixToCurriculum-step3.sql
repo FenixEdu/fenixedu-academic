2bded82e82da6e32f731ddf18b8c15edf9418587
João Luz <joao.luz@ist.utl.pt>
2003-08-20 19:06:28 +0000

    added fix to evaluation_method table...

--- docs/fixToCurriculum-step3.sql
+++ docs/fixToCurriculum-step3.sql
@@ -1,8 +1,12 @@
-use ciapl;
-
 alter table CURRICULUM
  drop column  KEY_EXECUTION_COURSE
 , add unique U1(KEY_CURRICULAR_COURSE)
 , type=InnoDB;      
 
+alter table EVALUATION_METHOD
+ drop column  KEY_EXECUTION_COURSE
+, add unique U1(KEY_CURRICULAR_COURSE)
+, type=InnoDB;      
+
 delete from CURRICULUM where KEY_CURRICULAR_COURSE is null;
+delete from EVALUATION_METHOD where KEY_CURRICULAR_COURSE is null;
