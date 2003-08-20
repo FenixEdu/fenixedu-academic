42098477e028e3de1b6d798eeab9f63ca74477ab
João Luz <joao.luz@ist.utl.pt>
2003-08-20 19:05:27 +0000

    1. remove use ciapl; 2. added evaluation_method table fix... 3. set current execution period and execution year

--- docs/fixToCurriculum-step1.sql
+++ docs/fixToCurriculum-step1.sql
@@ -1,6 +1,14 @@
-use ciapl;
-
 alter table CURRICULUM
  add column  KEY_CURRICULAR_COURSE int after ID_INTERNAL
 ,  drop index U1
 , type=InnoDB;
+
+alter table EVALUATION_METHOD
+ add column  KEY_CURRICULAR_COURSE int after ID_INTERNAL
+,  drop index ID_INTERNAL
+, type=InnoDB;
+
+update EXECUTION_YEAR set state = "O" where id_internal = 1;
+update EXECUTION_YEAR set state = "C" where id_internal = 2;
+update EXECUTION_PERIOD set state = "O" where id_internal = 1;
+update EXECUTION_PERIOD set state = "C" where id_internal = 2;
