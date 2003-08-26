1371d07b86c78f9c0f83c6399d69c69adeda110f
João Mota <joao.mota@ist.utl.pt>
2003-08-26 16:09:21 +0000

    fixed script to run the fixes to the database

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,5 +1,8 @@
 mysql -Dfenix < CorrectDataBaseForDeploy.sql
 mysql -Dfenix < fixToCurriculum-step1.sql
-mysql -Dfenix < fixToCurriculum-step2.sql 
+mysql -Dfenix < fixToCurriculum-step2.sql > insertToCurriculum.sql
 mysql -Dfenix < insertToCurriculum.sql
 mysql -Dfenix < fixToCurriculum-step3.sql
+mysql -Dfenix < fixToEvaluationMethod-step1.sql
+mysql -Dfenix < fixToEvaluationMethod-step2.sql > insertEvaluationMethod.sql
+mysql -Dfenix < insertEvaluationMethod.sql
