d405786c992532c64cab9c1543c17b65439aa014
João Luz <joao.luz@ist.utl.pt>
2003-08-29 14:33:10 +0000

    added comments to class

--- src/ServidorApresentacao/Action/commons/TransactionalDispatchAction.java
+++ src/ServidorApresentacao/Action/commons/TransactionalDispatchAction.java
@@ -9,22 +9,36 @@ import org.apache.struts.actions.DispatchAction;
 import ServidorApresentacao.Action.exceptions.FenixTransactionException;
 
 /**
+ * Contains util functions for struts transaction management. 
  * @author David Santos
  * 9/Jul/2003
  */
 
 public class TransactionalDispatchAction extends DispatchAction {
 
+	/**
+	 * Creates a token and saves it on request
+	 * @param request
+	 */
 	protected void createToken(HttpServletRequest request) {
 		generateToken(request);
 		saveToken(request);
 	}
 
-	protected void validateToken(HttpServletRequest request, ActionForm form, ActionMapping mapping, String errorMessage) throws FenixTransactionException {
+	/**
+	 * If the token is valid it creates a new token @see #createToken(HttpServletRequest)
+	 * Otherwise it resets the form and throws a <code> FenixTransactionException </code>
+	 * @param request
+	 * @param form
+	 * @param mapping
+	 * @param errorMessageKey
+	 * @throws FenixTransactionException when the token is invalid.
+	 */
+	protected void validateToken(HttpServletRequest request, ActionForm form, ActionMapping mapping, String errorMessageKey) throws FenixTransactionException {
 
 		if (!isTokenValid(request)) {
 			form.reset(mapping, request);
-			throw new FenixTransactionException(errorMessage);
+			throw new FenixTransactionException(errorMessageKey);
 		} else {
 			createToken(request);
 		}
