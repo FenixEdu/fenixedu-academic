5ca2c740b106830e31692ded3b66af85d3021926
Luis Cruz <luis.cruz@ist.utl.pt>
2003-09-01 01:07:36 +0000

    Altered the way SchoolClass/Shift associations are deleted. This is necessary due to the addition of a list of shifts to the SchoolClass class.

--- src/ServidorAplicacao/Servico/sop/RemoveClasses.java
+++ src/ServidorAplicacao/Servico/sop/RemoveClasses.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import DataBeans.InfoShift;
 import Dominio.ITurma;
+import Dominio.ITurmaTurno;
 import Dominio.ITurno;
 import Dominio.Turma;
 import Dominio.Turno;
@@ -66,8 +67,13 @@ public class RemoveClasses implements IServico {
 					(ITurma) sp.getITurmaPersistente().readByOID(
 						Turma.class,
 						(Integer) classOIDs.get(i));
+				ITurmaTurno classShift = sp.getITurmaTurnoPersistente().readByTurmaAndTurno(
+										schoolClass,
+										shift);
 
-				shift.getAssociatedClasses().remove(schoolClass);
+				if (classShift != null) {
+					sp.getITurmaTurnoPersistente().delete(classShift);
+				}
 			}
 
 			result = true;
