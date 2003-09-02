99845506f1cb7e9d5176a096d5cb9c4ecbf92125
João Luz <joao.luz@ist.utl.pt>
2003-09-02 20:24:13 +0000

    added method for validate token and not renew it

--- src/ServidorApresentacao/Action/commons/TransactionalDispatchAction.java
+++ src/ServidorApresentacao/Action/commons/TransactionalDispatchAction.java
@@ -35,12 +35,29 @@ public class TransactionalDispatchAction extends DispatchAction {
 	 * @throws FenixTransactionException when the token is invalid.
 	 */
 	protected void validateToken(HttpServletRequest request, ActionForm form, ActionMapping mapping, String errorMessageKey) throws FenixTransactionException {
+		validateToken(request, form, mapping, errorMessageKey, true);
+	}
+
+	/**
+	 * If the token is valid it creates a new token @see #createToken(HttpServletRequest)
+	 * Otherwise it resets the form and throws a <code> FenixTransactionException </code>
+	 * @param request
+	 * @param form
+	 * @param mapping
+	 * @param errorMessageKey
+	 * @param renewToken if false doesn't create a new token.
+	 * @throws FenixTransactionException when the token is invalid.
+	 */
+	protected void validateToken(HttpServletRequest request, ActionForm form, ActionMapping mapping, String errorMessageKey, boolean renewToken) throws FenixTransactionException {
 
 		if (!isTokenValid(request)) {
 			form.reset(mapping, request);
 			throw new FenixTransactionException(errorMessageKey);
 		} else {
-			createToken(request);
+			if (renewToken) {
+				createToken(request);
+			}				
 		}
 	}
+	
 }
