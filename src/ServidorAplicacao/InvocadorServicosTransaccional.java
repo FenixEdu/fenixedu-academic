da1d349d0143d8b3a78bb08ca8a49aa952accbe6
João Luz <joao.luz@ist.utl.pt>
2003-09-18 19:19:06 +0000

    removed some unnecessary prints... now it only prints when exception occurs..

--- src/ServidorAplicacao/InvocadorServicosTransaccional.java
+++ src/ServidorAplicacao/InvocadorServicosTransaccional.java
@@ -14,57 +14,58 @@ import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 public class InvocadorServicosTransaccional extends InvocadorServicos {
 
-	public final static InvocadorServicosTransaccional invocador =
-		new InvocadorServicosTransaccional();
+	public final static InvocadorServicosTransaccional invocador = new InvocadorServicosTransaccional();
 
-	public final Object invoke(IUserView user, IServico servico, Object argumentos[], GestorFiltros filterChain)
-		throws Exception {
+	public final Object invoke(IUserView user, IServico servico, Object argumentos[], GestorFiltros filterChain) throws Exception {
 		Object result = null;
 
 		try {
 			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
 			try {
-				System.out.print("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome());
-				if (user != null)  {
-					System.out.println(" USERVIEW= " + user.getUtilizador());
-				} else {
-					System.out.println(" USERVIEW= no user view");
-				}
+				//				System.out.print("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome());
+				//				if (user != null)  {
+				//					System.out.println(" USERVIEW= " + user.getUtilizador());
+				//				} else {
+				//					System.out.println(" USERVIEW= no user view");
+				//				}
 				sp.iniciarTransaccao();
-				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + "Started Transaction");
+				//				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + "Started Transaction");
 				filterChain.preFiltragem(user, servico, argumentos);
-				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + "Finished PreFilter");
+				//				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + "Finished PreFilter");
 				sp.confirmarTransaccao();
 				sp.iniciarTransaccao();
 				result = doInvocation(servico, "run", argumentos);
 				sp.confirmarTransaccao();
-				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + " finished sucessfully.");
+				//				System.out.println("LOGTIME= " + new Date() + " : SERVICE= " + servico.getNome() + " finished sucessfully.");
 			} catch (Exception ex) {
-					System.out.println("=========== Exception running one service ==============");
-					if (ex instanceof RuntimeException) {
-						System.out.println("The cache was cleared!");
-						sp.clearCache();
-					}
-					System.out.print("LOGTIME= " + new Date());
-					if (user != null)  {
-						System.out.print(" USERVIEW= " + user.getUtilizador());
-					} else {
-						System.out.print(" USERVIEW= no user view");
-					}
-					if (servico != null) {
-						System.out.println(" SERVICE= " + servico.getNome());
-					} else {
-						System.out.println(" SERVICE= no service");
-					}
-					ex.printStackTrace(System.out);
-					System.out.println("========= End of Exception (before abort transaction) ================");
-					try {
-						sp.cancelarTransaccao();
-					} catch (ExcepcaoPersistencia newEx) {
-						throw new FenixServiceException(newEx.getMessage());
-					}
-					throw ex;
-					//throw new FenixServiceException(ex.getMessage());         	
+				System.out.println("=========== Exception running one service ==============");
+				System.out.print("LOGTIME= " + new Date());
+				if (user != null) {
+					System.out.print(" USERVIEW= " + user.getUtilizador());
+				} else {
+					System.out.print(" USERVIEW= no user view");
+				}
+				if (servico != null) {
+					System.out.println(" SERVICE= " + servico.getNome());
+				} else {
+					System.out.println(" SERVICE= no service");
+				}
+				System.out.println("Stacktrace:");
+				ex.printStackTrace(System.out);
+
+				if (ex instanceof RuntimeException) {
+					System.out.println("The cache was cleared!");
+					sp.clearCache();
+				}
+
+				System.out.println("========= End of Exception (before abort transaction) ================");
+				try {
+					sp.cancelarTransaccao();
+				} catch (ExcepcaoPersistencia newEx) {
+					throw new FenixServiceException(newEx.getMessage());
+				}
+				throw ex;
+				//throw new FenixServiceException(ex.getMessage());         	
 			}
 		} catch (ExcepcaoPersistencia ex) {
 			throw new FenixServiceException(ex.getMessage());
