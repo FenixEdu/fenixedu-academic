ebc65ab6794a48670625516057d0cf58a68a2190
João Luz <joao.luz@ist.utl.pt>
2003-09-23 11:28:20 +0000

    merged from branch

--- src/ServidorAplicacao/Servico/commons/SendMail.java
+++ src/ServidorAplicacao/Servico/commons/SendMail.java
@@ -7,7 +7,7 @@ package ServidorAplicacao.Servico.commons;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
+import java.util.ResourceBundle;
 import ServidorAplicacao.IServico;
 import Util.EMail;
 /**
@@ -20,6 +20,9 @@ import Util.EMail;
 public class SendMail implements IServico
 {
 	private static SendMail service= new SendMail();
+	private static String bundleFile= new String("SMTPConfiguration");
+	private static ResourceBundle bundle= null;
+	private static String mailServer= null;
 	/**
 	 * The singleton access method of this class.
 	 **/
@@ -37,6 +40,19 @@ public class SendMail implements IServico
 	//returns the list of the e-mail addresses to which it wasn't possible to deliver the mail
 	public List run(List toList, String fromName, String from, String subject, String text)
 	{
+		if (bundle == null)
+		{
+			try
+			{
+				SendMail.bundle= ResourceBundle.getBundle(SendMail.bundleFile);
+				SendMail.mailServer= SendMail.bundle.getString("server.url");
+			}
+			catch (Exception e)
+			{
+                // the default server
+				SendMail.mailServer = "mail.adm";
+			}
+		}
 		List failedMails= new LinkedList();
 		for (Iterator iter= toList.iterator(); iter.hasNext();)
 		{
@@ -44,7 +60,7 @@ public class SendMail implements IServico
 			String to= (String) iter.next();
 			if (!fromName.equals(""))
 				composedFrom= "\"" + fromName + "\"" + " <" + from + ">";
-			EMail email= new EMail("mail.adm", composedFrom);
+			EMail email= new EMail(SendMail.mailServer, composedFrom);
 			if (!email.send(to, subject, text))
 				failedMails.add(to);
 		}
