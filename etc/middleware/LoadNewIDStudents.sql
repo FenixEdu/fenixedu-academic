7fef55e9711a59184c650c5285892c6ff03b77af
Nuno Nunes <nmsn@rnl.ist.utl.pt>
2003-09-24 17:18:29 +0000

    *** empty log message ***

--- etc/middleware/LoadNewIDStudents.sql
+++ etc/middleware/LoadNewIDStudents.sql
@@ -1,4 +1,4 @@
 -- Existing with diferent ID's
-drop table if exists mw_aluno;
-create table mw_aluno
-select a.* from mw_aluno_temp a inner join student s on s.number = a.number inner join person p on p.id_internal = s.key_person where p.document_id_number <> a.documentidnumber;
+drop table if exists mw_ALUNO;
+create table mw_ALUNO
+select a.* from mw_ALUNO_temp a inner join student s on s.number = a.number inner join person p on p.id_internal = s.key_person where p.document_id_number <> a.documentidnumber;
--- etc/middleware/LoadNewStudents.sql
+++ etc/middleware/LoadNewStudents.sql
@@ -1,4 +1,4 @@
 -- Non Existing
-drop table if exists mw_aluno;
-create table mw_aluno
-select a.* from mw_aluno_temp a left join STUDENT s on s.number = a.number where s.id_internal is null;
+drop table if exists mw_ALUNO;
+create table mw_ALUNO
+select a.* from mw_ALUNO_temp a left join STUDENT s on s.number = a.number where s.id_internal is null;
