672a20277397f75e4b9513287b65f3f793e8667d
João Mota <joao.mota@ist.utl.pt>
2003-09-29 14:54:13 +0000

    *** empty log message ***

---
+++ src/ServidorAplicacao/Filtro/ExecutionCourseAndItemLecturingTeacherAuthorizationFilter.java
@@ -0,0 +1,168 @@
+/*
+ * Created on 19/Mai/2003
+ *
+ * 
+ */
+package ServidorAplicacao.Filtro;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoItem;
+import DataBeans.util.Cloner;
+import Dominio.DisciplinaExecucao;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IItem;
+import Dominio.IProfessorship;
+import Dominio.ITeacher;
+import Dominio.Item;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.exceptions.NotAuthorizedException;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentItem;
+import ServidorPersistente.IPersistentProfessorship;
+import ServidorPersistente.IPersistentTeacher;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+import Util.RoleType;
+
+/**
+ * @author João Mota
+ *
+ */
+public class ExecutionCourseAndItemLecturingTeacherAuthorizationFilter
+	extends AuthorizationByRoleFilter {
+
+	public final static ExecutionCourseAndItemLecturingTeacherAuthorizationFilter instance =
+		new ExecutionCourseAndItemLecturingTeacherAuthorizationFilter();
+
+	/**
+	 * The singleton access method of this class.
+	 *
+	 * @return Returns the instance of this class responsible for the
+	 * authorization access to services.
+	 **/
+	public static Filtro getInstance() {
+		return instance;
+	}
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Filtro.AuthorizationByRoleFilter#getRoleType()
+	 */
+	protected RoleType getRoleType() {
+		return RoleType.TEACHER;
+	}
+
+	public void preFiltragem(
+		IUserView id,
+		IServico servico,
+		Object[] argumentos)
+		throws Exception {
+		if ((id == null)
+			|| (id.getRoles() == null)
+			|| !AuthorizationUtils.containsRole(id.getRoles(), getRoleType())
+			|| !lecturesExecutionCourse(id, argumentos)
+			|| !itemBelongsExecutionCourse(id, argumentos)) {
+			throw new NotAuthorizedException();
+		}
+	}
+
+	/**
+	 * @param id
+	 * @param argumentos
+	 * @return
+	 */
+	private boolean itemBelongsExecutionCourse(
+		IUserView id,
+		Object[] argumentos) {
+		InfoExecutionCourse infoExecutionCourse = null;
+		IDisciplinaExecucao executionCourse = null;
+		ISuportePersistente sp;
+		IItem item = null;
+		InfoItem infoItem = null;
+
+		if (argumentos == null) {
+			return false;
+		}
+		try {
+
+			sp = SuportePersistenteOJB.getInstance();
+			IDisciplinaExecucaoPersistente persistentExecutionCourse =
+				sp.getIDisciplinaExecucaoPersistente();
+			if (argumentos[0] instanceof InfoExecutionCourse) {
+				infoExecutionCourse = (InfoExecutionCourse) argumentos[0];
+				executionCourse =
+					Cloner.copyInfoExecutionCourse2ExecutionCourse(
+						infoExecutionCourse);
+			} else {
+				executionCourse =
+					(IDisciplinaExecucao) persistentExecutionCourse.readByOId(
+						new DisciplinaExecucao((Integer) argumentos[0]),
+						false);
+			}
+			IPersistentItem persistentItem = sp.getIPersistentItem();
+			if (argumentos[1] instanceof InfoItem) {
+				infoItem = (InfoItem) argumentos[1];
+				item =
+					Cloner.copyInfoItem2IItem(
+						infoItem);
+				item = (IItem) persistentItem.readByOId(item,false);		
+			} else {
+				item = (IItem) persistentItem.readByOId(new Item((Integer) argumentos[1]),false);	
+				
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return item.getSection().getSite().getExecutionCourse().equals(executionCourse);
+	}
+
+	/**
+	 * @param id
+	 * @param argumentos
+	 * @return
+	 */
+	private boolean lecturesExecutionCourse(
+		IUserView id,
+		Object[] argumentos) {
+		InfoExecutionCourse infoExecutionCourse = null;
+		IDisciplinaExecucao executionCourse = null;
+		ISuportePersistente sp;
+		IProfessorship professorship = null;
+		if (argumentos == null) {
+			return false;
+		}
+		try {
+
+			sp = SuportePersistenteOJB.getInstance();
+			IDisciplinaExecucaoPersistente persistentExecutionCourse =
+				sp.getIDisciplinaExecucaoPersistente();
+			if (argumentos[0] instanceof InfoExecutionCourse) {
+				infoExecutionCourse = (InfoExecutionCourse) argumentos[0];
+				executionCourse =
+					Cloner.copyInfoExecutionCourse2ExecutionCourse(
+						infoExecutionCourse);
+			} else {
+				executionCourse =
+					(IDisciplinaExecucao) persistentExecutionCourse.readByOId(
+						new DisciplinaExecucao((Integer) argumentos[0]),
+						false);
+			}
+
+			IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
+			ITeacher teacher =
+				persistentTeacher.readTeacherByUsernamePB(id.getUtilizador());
+			if (teacher != null && executionCourse != null) {
+				IPersistentProfessorship persistentProfessorship =
+					sp.getIPersistentProfessorship();
+				professorship =
+					persistentProfessorship.readByTeacherAndExecutionCoursePB(
+						teacher,
+						executionCourse);
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return professorship != null;
+	}
+
+}
---
+++ src/ServidorAplicacao/Servico/exceptions/FileAlreadyExistsServiceException.java
@@ -0,0 +1,29 @@
+
+package ServidorAplicacao.Servico.exceptions;
+
+/**
+ * 
+ * @author João Mota
+ */
+public class FileAlreadyExistsServiceException extends FenixServiceException {
+
+    public FileAlreadyExistsServiceException() {
+    }
+    
+	public FileAlreadyExistsServiceException(Throwable cause) {
+		super(cause);
+	}
+
+	public FileAlreadyExistsServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	public String toString() {
+				String result = "[FileAlreadyExistsServiceException\n";
+				result += "message" +this.getMessage()+ "\n";
+				result += "cause" +this.getCause()+ "\n";
+				result += "]";
+				return result;
+			}
+
+
+}
