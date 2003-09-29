2ffa9c57624999f8f28ad82eee6fe4711211fb90
Hugo Pereira <peppe@clix.pt>
2003-09-29 09:24:04 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/manager/InsertExecutionCourseAtExecutionPeriodTest.java
+++ src_tests/ServidorAplicacao/Servicos/manager/InsertExecutionCourseAtExecutionPeriodTest.java
@@ -1,55 +1,51 @@
-///*
-// * Created on 27/Set/2003
-// */
-//package ServidorAplicacao.Servicos.manager;
-//
-//import DataBeans.InfoDegree;
-//import DataBeans.InfoDegreeCurricularPlan;
-//import DataBeans.InfoExecutionCourse;
-//import DataBeans.InfoExecutionPeriod;
-//import Util.DegreeCurricularPlanState;
-//import Util.MarkType;
-//
-///**
-// * @author lmac1
-// */
-//public class InsertExecutionCourseAtExecutionPeriodTest extends TestCaseManagerInsertAndEditServices {
-//
-//	public InsertExecutionCourseAtExecutionPeriodTest(String testName) {
-//			super(testName);
-//		}
-//
-//	protected String getNameOfServiceToBeTested(){
-//		return "InsertExecutionCourseAtExecutionPeriod";
-//	}
-//
-//	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-//		
-//		InfoExecutionPeriod infoExecutionPeriod = new InfoExecutionPeriod();
-//		infoExecutionPeriod.setIdInternal(new Integer(1));
-//					
-//		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
-//		infoExecutionCourse.setInfoExecutionPeriod(infoExecutionPeriod);
-//		infoExecutionCourse.setNome("DisciplinaExecucaoSucesso");
-//		infoExecutionCourse.setSigla("DES");
-//		
-//		Object[] args = { infoExecutionCourse };
-//		return args;
-//	}
-//
-////	insert execution course with code already existing in DB
-//	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
-//		InfoExecutionPeriod infoExecutionPeriod = new InfoExecutionPeriod();
-//			infoExecutionPeriod.setIdInternal(new Integer(1));
-//					
-//			InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
-//			infoExecutionCourse.setInfoExecutionPeriod(infoExecutionPeriod);
-//			infoExecutionCourse.setNome("DisciplinaExecucaoSucesso");
-//			infoExecutionCourse.setSigla("TFCI");
-//		
-//			Object[] args = { infoExecutionCourse };
-//			return args;
-//	}
-//
-//}
-//
+/*
+ * Created on 27/Set/2003
+ */
+package ServidorAplicacao.Servicos.manager;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoExecutionPeriod;
+
+/**
+ * @author lmac1
+ */
+public class InsertExecutionCourseAtExecutionPeriodTest extends TestCaseManagerInsertAndEditServices {
+
+	public InsertExecutionCourseAtExecutionPeriodTest(String testName) {
+			super(testName);
+		}
+
+	protected String getNameOfServiceToBeTested(){
+		return "InsertExecutionCourseAtExecutionPeriod";
+	}
+
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		
+		InfoExecutionPeriod infoExecutionPeriod = new InfoExecutionPeriod();
+		infoExecutionPeriod.setIdInternal(new Integer(1));
+					
+		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
+		infoExecutionCourse.setInfoExecutionPeriod(infoExecutionPeriod);
+		infoExecutionCourse.setNome("DisciplinaExecucaoSucesso");
+		infoExecutionCourse.setSigla("DES");
+		
+		Object[] args = { infoExecutionCourse };
+		return args;
+	}
+
+//	insert execution course with code already existing in DB
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+		InfoExecutionPeriod infoExecutionPeriod = new InfoExecutionPeriod();
+			infoExecutionPeriod.setIdInternal(new Integer(1));
+					
+			InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse();
+			infoExecutionCourse.setInfoExecutionPeriod(infoExecutionPeriod);
+			infoExecutionCourse.setNome("DisciplinaExecucaoSucesso");
+			infoExecutionCourse.setSigla("TFCI");
+		
+			Object[] args = { infoExecutionCourse };
+			return args;
+	}
+
+}
+
--- src_tests/ServidorAplicacao/Servicos/manager/InsertProfessorShipTest.java
+++ src_tests/ServidorAplicacao/Servicos/manager/InsertProfessorShipTest.java
@@ -1,58 +1,58 @@
-///*
-// * Created on 27/Set/2003
-// */
-//package ServidorAplicacao.Servicos.manager;
-//
-//import DataBeans.InfoExecutionCourse;
-//import DataBeans.InfoProfessorShip;
-//import DataBeans.InfoTeacher;
-//
-///**
-// * @author lmac1
-// */
-//public class InsertProfessorShipTest extends TestCaseManagerInsertAndEditServices {
-//
-//	public InsertProfessorShipTest(String testName) {
-//			super(testName);
-//		}
-//
-//	protected String getNameOfServiceToBeTested(){
-//		return "InsertProfessorShip";
-//	}
-//
-//	
-//	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-//		
-//		InfoTeacher infoTeacher = new InfoTeacher();
-//		infoTeacher.setTeacherNumber(new Integer(8));
-//				
-//				
-//		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse(new Integer(24));
-//				
-//		InfoProfessorShip infoProfessorShip = new InfoProfessorShip();
-//		infoProfessorShip.setInfoExecutionCourse(infoExecutionCourse);
-//		infoProfessorShip.setInfoTeacher(infoTeacher);
-//		
-//		Object[] args = { infoProfessorShip };
-//		return args;
-//	}
-//
-////	try to inssert a teacher that doesn't exist in DB
-//	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
-//
-//		InfoTeacher infoTeacher = new InfoTeacher();
-//		infoTeacher.setTeacherNumber(new Integer(25));
-//		
-//		
-//		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse(new Integer(24));
-//		
-//		InfoProfessorShip infoProfessorShip = new InfoProfessorShip();
-//		infoProfessorShip.setInfoExecutionCourse(infoExecutionCourse);
-//		infoProfessorShip.setInfoTeacher(infoTeacher);
-//
-//		Object[] args = { infoProfessorShip };
-//		return args;
-//	}
-//
-//}
-//
+/*
+ * Created on 27/Set/2003
+ */
+package ServidorAplicacao.Servicos.manager;
+
+import DataBeans.InfoExecutionCourse;
+import DataBeans.InfoProfessorShip;
+import DataBeans.InfoTeacher;
+
+/**
+ * @author lmac1
+ */
+public class InsertProfessorShipTest extends TestCaseManagerInsertAndEditServices {
+
+	public InsertProfessorShipTest(String testName) {
+			super(testName);
+		}
+
+	protected String getNameOfServiceToBeTested(){
+		return "InsertProfessorShip";
+	}
+
+	
+	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
+		
+		InfoTeacher infoTeacher = new InfoTeacher();
+		infoTeacher.setTeacherNumber(new Integer(8));
+				
+				
+		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse(new Integer(24));
+				
+		InfoProfessorShip infoProfessorShip = new InfoProfessorShip();
+		infoProfessorShip.setInfoExecutionCourse(infoExecutionCourse);
+		infoProfessorShip.setInfoTeacher(infoTeacher);
+		
+		Object[] args = { infoProfessorShip };
+		return args;
+	}
+
+//	try to inssert a teacher that doesn't exist in DB
+	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
+
+		InfoTeacher infoTeacher = new InfoTeacher();
+		infoTeacher.setTeacherNumber(new Integer(25));
+		
+		
+		InfoExecutionCourse infoExecutionCourse = new InfoExecutionCourse(new Integer(24));
+		
+		InfoProfessorShip infoProfessorShip = new InfoProfessorShip();
+		infoProfessorShip.setInfoExecutionCourse(infoExecutionCourse);
+		infoProfessorShip.setInfoTeacher(infoTeacher);
+
+		Object[] args = { infoProfessorShip };
+		return args;
+	}
+
+}
+
