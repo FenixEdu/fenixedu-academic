4b23609d024866b80d80cf233094a0f8b0665079
João Luz <joao.luz@ist.utl.pt>
2003-10-03 13:11:57 +0000

    Changed readByDegreeCodeAndExecutionYear to use PersistenceBroker API

--- src/ServidorPersistente/OJB/CursoExecucaoOJB.java
+++ src/ServidorPersistente/OJB/CursoExecucaoOJB.java
@@ -190,21 +190,25 @@ public class CursoExecucaoOJB extends ObjectFenixOJB implements ICursoExecucaoPe
 	}
 
 	public ICursoExecucao readByDegreeCodeAndExecutionYear(String degreeCode, IExecutionYear executionYear) throws ExcepcaoPersistencia {
-		try {
-			String oqlQuery = "select all from " + CursoExecucao.class.getName() + " where executionYear.year = $1" + " and curricularPlan.degree.sigla = $2";
-			query.create(oqlQuery);
-
-			query.bind(executionYear.getYear());
-			query.bind(degreeCode);
-
-			List result = (List) query.execute();
-			lockRead(result);
-			if (result.size() != 0)
-				return (ICursoExecucao) result.get(0);
-			return null;
-		} catch (QueryException e) {
-			throw new ExcepcaoPersistencia(ExcepcaoPersistencia.QUERY, e);
-		}
+		Criteria criteria = new Criteria();
+		criteria.addEqualTo("executionYear.year ", executionYear.getYear());
+		criteria.addEqualTo("curricularPlan.degree.sigla", degreeCode);
+		return (ICursoExecucao) queryObject(CursoExecucao.class, criteria);
+//		try {
+//			String oqlQuery = "select all from " + CursoExecucao.class.getName() + " where executionYear.year = $1" + " and curricularPlan.degree.sigla = $2";
+//			query.create(oqlQuery);
+//
+//			query.bind(executionYear.getYear());
+//			query.bind(degreeCode);
+//
+//			List result = (List) query.execute();
+//			lockRead(result);
+//			if (result.size() != 0)
+//				return (ICursoExecucao) result.get(0);
+//			return null;
+//		} catch (QueryException e) {
+//			throw new ExcepcaoPersistencia(ExcepcaoPersistencia.QUERY, e);
+//		}
 	}
 
 	public List readByTeacher(ITeacher teacher) throws ExcepcaoPersistencia {
