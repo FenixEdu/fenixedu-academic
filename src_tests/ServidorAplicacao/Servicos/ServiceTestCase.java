b6a415b9f0018580a5e0b4b61acc3668e90a63f9
João Simas <jmls@mega.ist.utl.pt>
2003-10-07 13:03:31 +0000

    inclui o compareDataSet

--- src_tests/ServidorAplicacao/Servicos/ServiceTestCase.java
+++ src_tests/ServidorAplicacao/Servicos/ServiceTestCase.java
@@ -5,15 +5,19 @@
 package ServidorAplicacao.Servicos;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 import junit.framework.TestCase;
 
+import org.dbunit.Assertion;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
@@ -23,12 +27,13 @@ import ServidorAplicacao.GestorServicos;
 public abstract class ServiceTestCase extends TestCase {
 
 	protected GestorServicos gestor = null;
-	
+
 	public ServiceTestCase(String name) {
 		super(name);
 	}
 
 	protected IDatabaseConnection getConnection() throws Exception {
+
 		Class driverClass = Class.forName("com.mysql.jdbc.Driver");
 		Connection jdbcConnection =
 			DriverManager.getConnection(
@@ -43,6 +48,7 @@ public abstract class ServiceTestCase extends TestCase {
 	}
 
 	public void backUpDataBaseContents() throws Exception {
+
 		IDataSet fullDataSet = getConnection().createDataSet();
 		FileWriter fileWriter =
 			new FileWriter(new File(getBackUpDataSetFilePath()));
@@ -50,6 +56,7 @@ public abstract class ServiceTestCase extends TestCase {
 	}
 
 	public void loadDataBase() throws Exception {
+
 		FileReader fileReader =
 			new FileReader(new File(getBackUpDataSetFilePath()));
 		IDataSet dataSet = new FlatXmlDataSet(fileReader);
@@ -65,11 +72,10 @@ public abstract class ServiceTestCase extends TestCase {
 			IDataSet dataSet = getDataSet();
 
 			backUpDataBaseContents();
-
 			DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
 
 			gestor = GestorServicos.manager();
-			
+
 			connection.close();
 		} catch (Exception ex) {
 			fail("Setup failed loading database with test data set: " + ex);
@@ -78,15 +84,22 @@ public abstract class ServiceTestCase extends TestCase {
 
 	protected void tearDown() throws Exception {
 
-//		try {
-//			super.tearDown();
-//			loadDataBase();
-//		} catch (Exception ex) {
-//			fail("Tear down failed: " + ex);
-//		}
+		//		try {
+		//			super.tearDown();
+		//			loadDataBase();
+		//		} catch (Exception ex) {
+		//			fail("Tear down failed: " + ex);
+		//		}
 	}
 
+	protected void compareDataSet(String expectedFileName) throws Exception {
 
+		FileReader fileReader = new FileReader(new File(expectedFileName));
+		IDataSet expectedDataSet = new FlatXmlDataSet(fileReader);
+
+		IDataSet currentDataSet = getConnection().createDataSet();
+		Assertion.assertEquals(currentDataSet, expectedDataSet);
+	}
 
 	protected String getBackUpDataSetFilePath() {
 		return "etc/testBackup.xml";
