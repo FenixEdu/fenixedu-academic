4a8046ff4e20a9162f03fef28ef240c8de85df48
João Simas <jmls@mega.ist.utl.pt>
2003-10-08 17:17:27 +0000

    nova versao do teste utilizando dbunit

--- src_tests/ServidorAplicacao/Servicos/teacher/ReadSummariesTest.java
+++ src_tests/ServidorAplicacao/Servicos/teacher/ReadSummariesTest.java
@@ -1,120 +1,131 @@
 package ServidorAplicacao.Servicos.teacher;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import DataBeans.ExecutionCourseSiteView;
-import DataBeans.InfoExecutionCourse;
 import DataBeans.InfoSiteSummaries;
-import DataBeans.InfoSummary;
 import DataBeans.SiteView;
-import DataBeans.util.Cloner;
-import Dominio.DisciplinaExecucao;
-import Dominio.IDisciplinaExecucao;
-import Dominio.ISummary;
-import Dominio.Summary;
-import ServidorAplicacao.Servicos.TestCaseReadServices;
-import ServidorPersistente.ExcepcaoPersistencia;
-import ServidorPersistente.IDisciplinaExecucaoPersistente;
-import ServidorPersistente.IPersistentSummary;
-import ServidorPersistente.ISuportePersistente;
-import ServidorPersistente.OJB.SuportePersistenteOJB;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.Autenticacao;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servicos.ServiceNeedsAuthenticationTestCase;
+import Util.TipoAula;
 
 /**
- * @author João Mota
- * @author Susana Fernandes
+ * @author Leonor Almeida
+ * @author Sérgio Montelobo
  */
-public class ReadSummariesTest extends TestCaseReadServices {
+public class ReadSummariesTest extends ServiceNeedsAuthenticationTestCase {
 
 	/**
 	 * @param testName
 	 */
 	public ReadSummariesTest(String testName) {
 		super(testName);
-
 	}
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
-	 */
+	protected String getDataSetFilePath() {
+		return "etc/testReadSummariesDataSet.xml";
+	}
+
 	protected String getNameOfServiceToBeTested() {
 		return "ReadSummaries";
-
 	}
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
-	 */
-	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
-		
-		return null;
-	}
+	protected String[] getAuthorizedUser() {
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getArgumentsOfServiceToBeTestedSuccessfuly()
-	 */
-	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-		Object[] args = { new Integer(24)};
+		String[] args = { "user", "pass", getApplication() };
 		return args;
 	}
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getNumberOfItemsToRetrieve()
-	 */
-	protected int getNumberOfItemsToRetrieve() {
-		return 0;
+	protected String[] getUnauthorizedUser() {
+
+		String[] args = { "julia", "pass", getApplication() };
+		return args;
 	}
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseReadServices#getObjectToCompare()
-	 */
-	protected Object getObjectToCompare() {
+	protected String[] getNonTeacherUser() {
 
-		InfoSiteSummaries bodyComponent = new InfoSiteSummaries();
-		InfoExecutionCourse infoExecutionCourse = null;
-		InfoSummary infoSummary1 = null;
-		InfoSummary infoSummary2 = null;
-		List infoSummaries = new ArrayList();
+		String[] args = { "jccm", "pass", getApplication() };
+		return args;
+	}
+
+	protected Object[] getAuthorizeArguments() {
+
+		Integer executionCourseId = new Integer(24);
+		int tipoAula = TipoAula.TEORICA;
+		TipoAula summaryType = new TipoAula(tipoAula);
+
+		Object[] args = { executionCourseId, summaryType };
+		return args;
+	}
+
+	protected String getApplication() {
+		return Autenticacao.EXTRANET;
+	}
+
+	public void testSuccessfullCourseWithSummaries() {
+
 		try {
-			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
-			sp.iniciarTransaccao();
-			IDisciplinaExecucaoPersistente persistentExecutionCourse =
-				sp.getIDisciplinaExecucaoPersistente();
-			IPersistentSummary persistentSummary = sp.getIPersistentSummary();
-			IDisciplinaExecucao executionCourse =
-				new DisciplinaExecucao(new Integer(24));
-				
-			ISummary summary1 = new Summary(new Integer(261));
-			ISummary summary2 = new Summary(new Integer(281));
-			executionCourse =
-				(IDisciplinaExecucao) persistentExecutionCourse.readByOId(
-					executionCourse,
-					false);
-			assertNotNull("executionCourse null",executionCourse);		
-			summary1 = (ISummary) persistentSummary.readByOId(summary1, false);
-			summary2 = (ISummary) persistentSummary.readByOId(summary2,false);
-			assertNotNull("summary1 null",summary1);
-			assertNotNull("summary2 null",summary2);
-			sp.confirmarTransaccao();
-			infoExecutionCourse =
-				Cloner.copyIExecutionCourse2InfoExecutionCourse(
-					executionCourse);
-			infoSummary1 = Cloner.copyISummary2InfoSummary(summary1);
-			infoSummary2 = Cloner.copyISummary2InfoSummary(summary2);
-		} catch (ExcepcaoPersistencia e) {
-			fail("exception: ExcepcaoPersistencia ");
+			SiteView result = null;
+			Integer executionCourseId = new Integer(24);
+			int tipoAula = TipoAula.TEORICA;
+			TipoAula summaryType = new TipoAula(tipoAula);
+
+			String[] args1 = getAuthorizedUser();
+			IUserView userView = authenticateUser(args1);
+
+			Object[] args2 = { executionCourseId, summaryType };
+
+			result =
+				(SiteView) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args2);
+
+			InfoSiteSummaries infoSiteSummaries =
+				(InfoSiteSummaries) result.getComponent();
+			assertEquals(infoSiteSummaries.getSummaryType(), summaryType);
+			List infoSummaries = infoSiteSummaries.getInfoSummaries();
+			assertEquals(infoSummaries.size(), 2);
+			// verifica se a base de dados nao foi alterada
+			compareDataSet(getDataSetFilePath());
+		} catch (FenixServiceException ex) {
+			fail("Reading the Summaries of a Site" + ex);
+		} catch (Exception ex) {
+			fail("Reading the Summaries of a Site" + ex);
 		}
-		
-		bodyComponent.setExecutionCourse(infoExecutionCourse);
-		infoSummaries.add(infoSummary1); 
-		infoSummaries.add(infoSummary2); 
-		bodyComponent.setInfoSummaries(infoSummaries);
-		SiteView siteView =
-			new ExecutionCourseSiteView(bodyComponent, bodyComponent);
-		return siteView;
 	}
-	
-	protected boolean needsAuthorization() {
-			return true;
+
+	public void testSuccessfullCourseWithoutSummaries() {
+
+		try {
+			SiteView result = null;
+			Integer executionCourseId = new Integer(24);
+			int tipoAula = TipoAula.DUVIDAS;
+			TipoAula summaryType = new TipoAula(tipoAula);
+
+			String[] args1 = getAuthorizedUser();
+			IUserView userView = authenticateUser(args1);
+
+			Object[] args2 = { executionCourseId, summaryType };
+
+			result =
+				(SiteView) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args2);
+
+			InfoSiteSummaries infoSiteSummaries =
+				(InfoSiteSummaries) result.getComponent();
+			assertEquals(infoSiteSummaries.getSummaryType(), summaryType);
+			List infoSummaries = infoSiteSummaries.getInfoSummaries();
+			assertEquals(infoSummaries.size(), 0);
+			// verifica se a base de dados nao foi alterada
+			compareDataSet(getDataSetFilePath());
+		} catch (FenixServiceException ex) {
+			fail("Reading the Summaries of a Site" + ex);
+		} catch (Exception ex) {
+			fail("Reading the Summaries of a Site" + ex);
 		}
+	}
 }
