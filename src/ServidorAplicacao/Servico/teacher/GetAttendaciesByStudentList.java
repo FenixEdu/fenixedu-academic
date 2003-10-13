c57b370e24c3e161466282ada1df425d3c6e980c
João Mota <joao.mota@ist.utl.pt>
2003-10-13 14:47:16 +0000

    fixed a bug where the info was cloned to a domain object and the id_internal was used without checking with the db again!

--- src/ServidorAplicacao/Servico/teacher/GetAttendaciesByStudentList.java
+++ src/ServidorAplicacao/Servico/teacher/GetAttendaciesByStudentList.java
@@ -12,10 +12,12 @@ import DataBeans.InfoStudent;
 import DataBeans.util.Cloner;
 import Dominio.IFrequenta;
 import Dominio.IStudent;
+import Dominio.Student;
 import ServidorAplicacao.IServico;
 import ServidorApresentacao.Action.Seminaries.Exceptions.BDException;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IFrequentaPersistente;
+import ServidorPersistente.IPersistentStudent;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
 /**
@@ -25,56 +27,66 @@ import ServidorPersistente.OJB.SuportePersistenteOJB;
  * Created at 16/Set/2003, 12:18:28
  * 
  */
-public class GetAttendaciesByStudentList implements IServico
-{
-	private static GetAttendaciesByStudentList service= new GetAttendaciesByStudentList();
+public class GetAttendaciesByStudentList implements IServico {
+	private static GetAttendaciesByStudentList service =
+		new GetAttendaciesByStudentList();
 	/**
 	 * The singleton access method of this class.
 	 **/
-	public static GetAttendaciesByStudentList getService()
-	{
+	public static GetAttendaciesByStudentList getService() {
 		return service;
 	}
 	/**
 	 * The actor of this class.
 	 **/
-	private GetAttendaciesByStudentList()
-	{
+	private GetAttendaciesByStudentList() {
 	}
 	/**
 	 * Returns The Service Name */
-	public final String getNome()
-	{
+	public final String getNome() {
 		return "teacher.GetAttendaciesByStudentList";
 	}
-	public List run(Integer executionCourseID, List infoStudents) throws BDException
-	{
-		List students= new LinkedList();
-		for (Iterator infoStudentsIterator= infoStudents.iterator(); infoStudentsIterator.hasNext();)
-		{
-			InfoStudent infoStudent= (InfoStudent) infoStudentsIterator.next();
-			students.add(Cloner.copyInfoStudent2IStudent(infoStudent));
-		}
-		List attendacies= new LinkedList();
-		try
-		{
-			ISuportePersistente persistenceSupport= SuportePersistenteOJB.getInstance();
-			IFrequentaPersistente persistentAttendacy= persistenceSupport.getIFrequentaPersistente();
-			//
-			for (Iterator studentsIterator= students.iterator(); studentsIterator.hasNext();)
-			{
-				IStudent student= (IStudent) studentsIterator.next();
-				IFrequenta attendacy=
+	public List run(Integer executionCourseID, List infoStudents)
+		throws BDException {
+			List attendacies = new LinkedList();
+		try {
+			ISuportePersistente persistenceSupport =
+				SuportePersistenteOJB.getInstance();
+			IFrequentaPersistente persistentAttendacy =
+				persistenceSupport.getIFrequentaPersistente();
+			IPersistentStudent persistentStudent =
+				persistenceSupport.getIPersistentStudent();
+			List students = new LinkedList();
+			for (Iterator infoStudentsIterator = infoStudents.iterator();
+				infoStudentsIterator.hasNext();
+				) {
+				InfoStudent infoStudent =
+					(InfoStudent) infoStudentsIterator.next();
+				IStudent student = new Student(infoStudent.getIdInternal());
+				student =
+					(IStudent) persistentStudent.readByOId(student, false);
+				if (student!=null) {
+					students.add(student);
+				}	
+				
+			}
+			
+			for (Iterator studentsIterator = students.iterator();
+				studentsIterator.hasNext();
+				) {
+				IStudent student = (IStudent) studentsIterator.next();
+				IFrequenta attendacy =
 					persistentAttendacy.readByAlunoIdAndDisciplinaExecucaoId(
 						student.getIdInternal(),
 						executionCourseID);
-				InfoFrequenta infoFrequenta= Cloner.copyIFrequenta2InfoFrequenta(attendacy);
+				InfoFrequenta infoFrequenta =
+					Cloner.copyIFrequenta2InfoFrequenta(attendacy);
 				attendacies.add(infoFrequenta);
 			}
-		}
-		catch (ExcepcaoPersistencia ex)
-		{
-			throw new BDException("Got an error while trying to get info about a student's work group", ex);
+		} catch (ExcepcaoPersistencia ex) {
+			throw new BDException(
+				"Got an error while trying to get info about a student's work group",
+				ex);
 		}
 		return attendacies;
 	}
