a75c1c77932c9704c36904e5a50536c40e4f6ea2
Nuno Barbosa <nmdb@mega.ist.utl.pt>
2003-10-21 11:24:37 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/teacher/CreateAnnouncementTest.java
+++ src_tests/ServidorAplicacao/Servicos/teacher/CreateAnnouncementTest.java
@@ -1,14 +1,28 @@
 package ServidorAplicacao.Servicos.teacher;
 
-import java.util.HashMap;
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryByCriteria;
 
-import ServidorAplicacao.Servicos.TestCaseCreateServices;
+import Dominio.Announcement;
+import Dominio.IAnnouncement;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.Autenticacao;
+import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servicos.ServiceNeedsAuthenticationTestCase;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 /**
- * @author Fernanda Quitério
+ * @author Barbosa
+ * @author Pica
  * 
  */
-public class CreateAnnouncementTest extends TestCaseCreateServices {
+public class CreateAnnouncementTest
+	extends ServiceNeedsAuthenticationTestCase {
 
 	/**
 	 * @param testName
@@ -16,41 +30,119 @@ public class CreateAnnouncementTest extends TestCaseCreateServices {
 	public CreateAnnouncementTest(String testName) {
 		super(testName);
 	}
+	protected String getApplication() {
+		return Autenticacao.EXTRANET;
+	}
 
-	/**
-	 * @see ServidorAplicacao.Servicos.TestCaseNeedAuthorizationServices#getNameOfServiceToBeTested()
-	 */
+	protected String getDataSetFilePath() {
+		return "etc/datasets/servicos/teacher/testCreateAnnouncementDataSet.xml";
+	}
+
+	protected String getExpectedDataSetFilePath() {
+		return "etc/datasets/servicos/teacher/testCreateAnnouncementExpectedDataSet.xml";
+	}
+
 	protected String getNameOfServiceToBeTested() {
 		return "CreateAnnouncement";
 	}
 
-	/**
-	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedUnsuccessfuly()
-	 */
-	protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
-		return null;
-	}
-
-	/**
-	 * @see ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices#getArgumentsOfServiceToBeTestedSuccessfuly()
-	 */
-	protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-		Object[] args =
-			{ new Integer(26), new String("title"), new String("information")};
+	protected String[] getAuthorizedUser() {
+		String[] args = { "user", "pass", getApplication()};
 		return args;
 	}
 
-	/**
-	 * This method must return 'true' if the service needs authorization to be runned and 'false' otherwise.
-	 */
-	protected boolean needsAuthorization() {
-		return true;
+	protected String[] getUnauthorizedUser() {
+		String[] args = { "nmsn", "pass", getApplication()};
+		return args;
 	}
 
-	/* (non-Javadoc)
-	 * @see ServidorAplicacao.Servicos.TestCaseCreateServices#getArgumentListOfServiceToBeTestedUnsuccessfuly()
-	 */
-	protected HashMap getArgumentListOfServiceToBeTestedUnsuccessfuly() {
-		return null;
+	protected String[] getNonTeacherUser() {
+		String[] args = { "fiado", "pass", getApplication()};
+		return args;
 	}
+
+	protected Object[] getAuthorizeArguments() {
+		Integer infoExecutionCourseCode = new Integer(24);
+		String newAnnouncementTitle = "Anuncio Teste SUCESSO!";
+		String newAnnouncementInformation = "Sucesso! Over & Out :)";
+		Object[] args =
+			{
+				infoExecutionCourseCode,
+				newAnnouncementTitle,
+				newAnnouncementInformation };
+		return args;
+	}
+
+	public void testCreateAnnouncementSuccessful() {
+		try {
+			boolean result = false;
+			String[] args = getAuthorizedUser();
+			IUserView id = authenticateUser(args);
+			Object[] args2 = getAuthorizeArguments();
+
+			gestor.executar(id, getNameOfServiceToBeTested(), args2);
+
+			PersistenceBroker broker =
+				PersistenceBrokerFactory.defaultPersistenceBroker();
+
+			Criteria criteria = new Criteria();
+			criteria.addOrderBy("lastModifiedDate", false);
+			Query queryCriteria =
+				new QueryByCriteria(Announcement.class, criteria);
+			IAnnouncement announcement =
+				(IAnnouncement) broker.getObjectByQuery(queryCriteria);
+			broker.close();
+
+			assertEquals(announcement.getTitle(), args2[1]);
+			assertEquals(announcement.getInformation(), args2[2]);
+
+			//erase new created announcement to verify nothing else has changed
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			sp.iniciarTransaccao();
+			sp.getIPersistentAnnouncement().deleteByOID(
+				Announcement.class,
+				announcement.getIdInternal());
+			sp.confirmarTransaccao();
+
+			compareDataSet(getExpectedDataSetFilePath());
+			System.out.println(
+				"CreateAnnouncementSuccessfulTest was SUCCESSFULY runned by service: "
+					+ this.getClass().getName());
+
+		} catch (FenixServiceException e) {
+			fail("Creating a new Announcement for a Site " + e);
+		} catch (Exception e) {
+			fail("Creating a new Announcement for a Site " + e);
+		}
+	}
+/*
+	public void testCreateExistingAnnouncement() {
+		Integer infoExecutionCourseCode = new Integer(24);
+		String newAnnouncementTitle = "announcement1deTFCI";
+		String newAnnouncementInformation = "information1";
+		Object[] args =
+			{
+				infoExecutionCourseCode,
+				newAnnouncementTitle,
+				newAnnouncementInformation };
+
+		try {
+			gestor.executar(userView, getNameOfServiceToBeTested(), args);
+			System.out.println(
+				"testCreateExistingAnnouncement was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testCreateExistingAnnouncement");
+
+		} catch (ExistingServiceException e) {
+			compareDataSet("etc/datasets/servicos/teacher/testCreateExistingAnnouncementExpectedDataSet.xml");
+			System.out.println(
+				"testCreateExistingAnnouncement was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+		} catch (Exception ex) {
+			System.out.println(
+				"testCreateExistingAnnouncement was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testCreateExistingAnnouncement");
+		}
+	}*/
 }
---
+++ src_tests/ServidorAplicacao/Servicos/teacher/ReadSiteAnnouncementTest.java
@@ -0,0 +1,149 @@
+package ServidorAplicacao.Servicos.teacher;
+
+import java.util.List;
+
+import DataBeans.InfoSiteAnnouncement;
+import DataBeans.InfoSiteCommon;
+import DataBeans.SiteView;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.Autenticacao;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servicos.ServiceNeedsAuthenticationTestCase;
+
+/**
+ * @author Barbosa
+ * @author Pica
+ * 
+ */
+public class ReadSiteAnnouncementTest
+	extends ServiceNeedsAuthenticationTestCase {
+
+	/**
+	 * @param testName
+	 */
+
+	public ReadSiteAnnouncementTest(String testName) {
+		super(testName);
+	}
+
+	protected String getApplication() {
+		return Autenticacao.EXTRANET;
+	}
+
+	protected String getDataSetFilePath() {
+		return "etc/datasets/servicos/teacher/testReadSiteAnnouncementDataSet.xml";
+	}
+
+	protected String getExpectedDataSetFilePath() {
+		return "etc/datasets/servicos/teacher/testReadSiteAnnouncementExpectedDataSet.xml";
+	}
+
+	protected String getNameOfServiceToBeTested() {
+		return "TeacherAdministrationSiteComponentService";
+	}
+
+	protected String[] getAuthorizedUser() {
+		String[] args = { "user", "pass", getApplication()};
+		return args;
+	}
+
+	protected String[] getUnauthorizedUser() {
+		String[] args = { "nmsn", "pass", getApplication()};
+		return args;
+	}
+
+	protected String[] getNonTeacherUser() {
+		String[] args = { "fiado", "pass", getApplication()};
+		return args;
+	}
+
+	protected Object[] getAuthorizeArguments() {
+		Integer infoExecutionCourseCode = new Integer(24);
+		Integer infoSiteCode = new Integer(1);
+		InfoSiteCommon commonComponent = new InfoSiteCommon();
+		InfoSiteAnnouncement bodyComponent = new InfoSiteAnnouncement();
+		Object obj1 = null;
+		Object obj2 = null;
+
+		Object[] args =
+			{
+				infoExecutionCourseCode,
+				commonComponent,
+				bodyComponent,
+				infoSiteCode,
+				obj1,
+				obj2 };
+		return args;
+	}
+
+	public void testSuccessfullSiteWithAnnouncements() {
+
+		try {
+			SiteView result = null;
+			String[] args1 = getAuthorizedUser();
+			IUserView userView = authenticateUser(args1);
+
+			Object[] args2 = getAuthorizeArguments();
+
+			result =
+				(SiteView) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args2);
+
+			InfoSiteAnnouncement infoSiteAnnouncement =
+				(InfoSiteAnnouncement) result.getComponent();
+			List infoAnnouncements = infoSiteAnnouncement.getAnnouncements();
+			assertEquals(infoAnnouncements.size(), 4);
+
+			//check if database hasn't changed
+			compareDataSet(getExpectedDataSetFilePath());
+		} catch (FenixServiceException ex) {
+			fail("Reading the Announcements of a Site with announcements" + ex);
+		} catch (Exception ex) {
+			fail("Reading the Announcements of a Site with announcements" + ex);
+		}
+	}
+
+	public void testSuccessfullSiteWithoutAnnouncements() {
+
+		try {
+			SiteView result = null;
+			Integer infoExecutionCourseCode = new Integer(25);
+			Integer infoSiteCode = new Integer(2);
+			InfoSiteCommon commonComponent = new InfoSiteCommon();
+			InfoSiteAnnouncement bodyComponent = new InfoSiteAnnouncement();
+			Object obj1 = null;
+			Object obj2 = null;
+
+			String[] args1 = getAuthorizedUser();
+			Object[] args2 =
+				{
+					infoExecutionCourseCode,
+					commonComponent,
+					bodyComponent,
+					infoSiteCode,
+					obj1,
+					obj2 };
+			
+			IUserView userView = authenticateUser(args1);
+
+			result =
+				(SiteView) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args2);
+
+			InfoSiteAnnouncement infoSiteAnnouncement =
+				(InfoSiteAnnouncement) result.getComponent();
+			List infoAnnouncements = infoSiteAnnouncement.getAnnouncements();
+			assertEquals(infoAnnouncements.size(), 0);
+			//check if database hasn't changed
+			compareDataSet(getExpectedDataSetFilePath());
+		} catch (FenixServiceException ex) {
+			fail("Reading the Announcements of a Site without announcements" + ex);
+		} catch (Exception ex) {
+			fail("Reading the Announcements of a Site without announcements" + ex);
+		}
+	}
+}
