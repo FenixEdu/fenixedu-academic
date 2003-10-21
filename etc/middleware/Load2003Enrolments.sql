153d3169f5bc1eb935796051b7842de1f5f49cf5
João Luz <joao.luz@ist.utl.pt>
2003-10-21 10:09:09 +0000

    *** empty log message ***

--- etc/middleware/Load2003Enrolments.sql
+++ etc/middleware/Load2003Enrolments.sql
@@ -1,7 +1,7 @@
 -- 2003 Enrolments
 drop table if exists mw_ENROLMENT;
 create table mw_ENROLMENT
-select * from mw_ENROLMENT_temp where enrolmentYear = 2003 and degreeCode = 2;
+select * from mw_ENROLMENT_temp where enrolmentYear = 2003;
 
 update DEGREE_CURRICULAR_PLAN set state = 2 where name like "L%" and name not like "%2003/2004";
 update DEGREE_CURRICULAR_PLAN set state = 1 where name like "L%2003/2004";
