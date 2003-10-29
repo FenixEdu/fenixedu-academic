83d6e96160374e05d2f4e826e9ccca0439ea1f46
Luís Egídio <luis.egidio@ist.utl.pt>
2003-10-29 18:20:43 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/publico/ExamSiteComponentServiceTest.java
+++ src_tests/ServidorAplicacao/Servicos/publico/ExamSiteComponentServiceTest.java
@@ -5,6 +5,7 @@
 package ServidorAplicacao.Servicos.publico;
 
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import DataBeans.InfoExam;
@@ -12,7 +13,23 @@ import DataBeans.InfoExamsMap;
 import DataBeans.InfoExecutionCourse;
 import DataBeans.InfoSiteExamMap;
 import DataBeans.SiteView;
+import DataBeans.util.Cloner;
+import Dominio.DisciplinaExecucao;
+import Dominio.Exam;
+import Dominio.ICursoExecucao;
+import Dominio.IDisciplinaExecucao;
+import Dominio.IExam;
+import Dominio.IExecutionPeriod;
+import Dominio.IExecutionYear;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorAplicacao.Servicos.ServiceTestCase;
+import ServidorPersistente.ICursoExecucaoPersistente;
+import ServidorPersistente.IDisciplinaExecucaoPersistente;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentExecutionYear;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+import Util.Season;
 
 /**
  * @author  Luis Egidio, lmre@mega.ist.utl.pt
@@ -21,28 +38,31 @@ import ServidorAplicacao.Servicos.ServiceTestCase;
  */
 public class ExamSiteComponentServiceTest extends ServiceTestCase {
 
+	/**
+	 * @param testName
+	 */
 	public ExamSiteComponentServiceTest(String nome) {
 		super(nome);
-	}
-
-	protected String getDataSetFilePath() {
-		return null;
 	}
 
 	protected String getNameOfServiceToBeTested() {
 		return "ExamSiteComponentService";
 	}
 
-	public void testExamSite() {
-		SiteView result = null;
+	protected String getDataSetFilePath() {
+		return "etc/datasets/servicos/publico/testExamSiteComponentServiceDataSet.xml";
+	}
+
+	protected Object[] getArguments() {
+
 		InfoSiteExamMap bodyComponent = new InfoSiteExamMap();
 		String executionYearName = "2003/2004";
 		String executionPeriodName = "1 Semestre";
+		String degreeInitials = "LEIC";
+		String nameDegreeCurricularPlan = "LEIC2003/2004";
 		List curricularYears = new ArrayList();
 		curricularYears.add(new Integer(1));
 		curricularYears.add(new Integer(2));
-		String degreeInitials = "LEIC";
-		String nameDegreeCurricularPlan = "LEIC2003/2004";
 
 		Object[] args =
 			{
@@ -52,30 +72,157 @@ public class ExamSiteComponentServiceTest extends ServiceTestCase {
 				degreeInitials,
 				nameDegreeCurricularPlan,
 				curricularYears };
+
+		return args;
+	}
+
+	public void testExamSite() {
+		SiteView result = null;
+		Object[] args = getArguments();
+
 		try {
+			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+			IDisciplinaExecucaoPersistente persistentExecutionCourse =
+				sp.getIDisciplinaExecucaoPersistente();
+			IPersistentExecutionPeriod persistentExecutionPeriod =
+				sp.getIPersistentExecutionPeriod();
+			IPersistentExecutionYear persistentExecutionYear =
+				sp.getIPersistentExecutionYear();
+			ICursoExecucaoPersistente executionDegreeDAO =
+				sp.getICursoExecucaoPersistente();
+			IDisciplinaExecucaoPersistente disciplinaExecucaoPersistente =
+				sp.getIDisciplinaExecucaoPersistente();
+			sp.iniciarTransaccao();
+
+			IExecutionYear executionYear =
+				persistentExecutionYear.readExecutionYearByName(
+					(String) args[1]);
+
+			IExecutionPeriod executionPeriod =
+				persistentExecutionPeriod.readByNameAndExecutionYear(
+					(String) args[2],
+					executionYear);
+
+			ICursoExecucao executionDegree =
+				executionDegreeDAO
+					.readByDegreeInitialsAndNameDegreeCurricularPlanAndExecutionYear(
+					(String) args[3],
+					(String) args[4],
+					executionYear);
+
+			List executionCourses1ano =
+				disciplinaExecucaoPersistente
+					.readByCurricularYearAndExecutionPeriodAndExecutionDegree(
+					new Integer(1),
+					executionPeriod,
+					executionDegree);
+
+			List executionCourses2ano =
+				disciplinaExecucaoPersistente
+					.readByCurricularYearAndExecutionPeriodAndExecutionDegree(
+					new Integer(2),
+					executionPeriod,
+					executionDegree);
+
+			IExam exam1 =
+				new Exam(
+					new GregorianCalendar(2004, 1, 27),
+					new GregorianCalendar(2004, 1, 27, 10, 0),
+					new GregorianCalendar(2004, 1, 27, 11, 30),
+					new Season(1));
+			IExam exam2 =
+				new Exam(
+					new GregorianCalendar(2004, 2, 17),
+					new GregorianCalendar(2004, 1, 27, 10, 0),
+					new GregorianCalendar(2004, 1, 27, 11, 30),
+					new Season(1));
+					
+					System.out.println(executionCourses1ano.size());
+			IDisciplinaExecucao executionCourse1 =
+				(DisciplinaExecucao) executionCourses1ano.get(0);
+
+			List list1 = new ArrayList();
+			list1.add(exam1);
+			executionCourse1.setAssociatedExams(list1);
+
+			IDisciplinaExecucao executionCourse2 =
+				(DisciplinaExecucao) executionCourses1ano.get(1);
+
+			List list2 = new ArrayList();
+			list2.add(exam1);
+			list2.add(exam2);
+			executionCourse2.setAssociatedExams(list2);
+
+			IDisciplinaExecucao executionCourse3 =
+				(DisciplinaExecucao) executionCourses1ano.get(2);
+
+			List list3 = new ArrayList();
+			executionCourse3.setAssociatedExams(list3);
+
+			for (int i = 3; executionCourses1ano.size() < i; i++) {
+				IDisciplinaExecucao executionCourse4 =
+					(DisciplinaExecucao) executionCourses1ano.get(i);
+
+				List list4 = new ArrayList();
+				list4.add(exam1);
+				executionCourse4.setAssociatedExams(list4);
+			}
+
+			for (int i = 0; executionCourses2ano.size() < i; i++) {
+				IDisciplinaExecucao executionCourse5 =
+					(DisciplinaExecucao) executionCourses1ano.get(i);
+
+				List list5 = new ArrayList();
+				list5.add(exam2);
+				executionCourse5.setAssociatedExams(list5);
+			}
+
+			sp.confirmarTransaccao();
+
+			executionCourses1ano.addAll(executionCourses2ano);
+
 			result =
 				(SiteView) gestor.executar(
 					null,
 					getNameOfServiceToBeTested(),
-					args);
-						InfoSiteExamMap siteExamMap =
-							(InfoSiteExamMap) result.getComponent();
-						InfoExamsMap examsMap =
-							(InfoExamsMap) siteExamMap.getInfoExamsMap();
-						List executionCourses = examsMap.getExecutionCourses();
-						int size = executionCourses.size();
-						int n_exams=0;
-						for (int i = 0; i < size; i++) {
-							InfoExecutionCourse executionCourse =
-								(InfoExecutionCourse) executionCourses.get(i);
-							List exams = executionCourse.getAssociatedInfoExams();
-							n_exams = exams.size();
-							for (int j = 0; j < n_exams; j++) {
-								InfoExam exam = (InfoExam) exams.get(j);
-							}
-						}
-			//			compareDataSet("");
+					getArguments());
+			InfoSiteExamMap siteExamMap =
+				(InfoSiteExamMap) result.getComponent();
+			InfoExamsMap examsMap =
+				(InfoExamsMap) siteExamMap.getInfoExamsMap();
+			List executionCourses = examsMap.getExecutionCourses();
+			int size = executionCourses.size();
+			int n_exams = 0;
+			for (int i = 0; i < size; i++) {
+				InfoExecutionCourse infoExecutionCourse =
+					(InfoExecutionCourse) executionCourses.get(i);
+				IDisciplinaExecucao executionCourse =
+					(IDisciplinaExecucao) executionCourses1ano.get(i);
+				List examsService =
+					infoExecutionCourse.getAssociatedInfoExams();
+				List exams = executionCourse.getAssociatedExams();
+				assertEquals(exams.size(), examsService.size());
+				n_exams = exams.size();
+				for (int j = 0; j < n_exams; j++) {
+					InfoExam infoExam = (InfoExam) examsService.get(j);
+					IExam oldExam = (IExam) exams.get(j);
+					IExam newExam = Cloner.copyInfoExam2IExam(infoExam);
+					assertEquals(newExam, oldExam);
+				}
+			}
 
+		} catch (FenixServiceException ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testReadSiteItems was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testReadSiteItems");
+		} catch (OutOfMemoryError ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testReadSiteItems was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testReadSiteItems");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			System.out.println(
