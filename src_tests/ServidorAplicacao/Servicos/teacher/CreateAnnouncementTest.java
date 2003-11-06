ab933f6dd865fe38cf3391a35f5bf7004487d6f7
Nuno Barbosa <nmdb@mega.ist.utl.pt>
2003-11-06 16:11:18 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/teacher/CreateAnnouncementTest.java
+++ src_tests/ServidorAplicacao/Servicos/teacher/CreateAnnouncementTest.java
@@ -1,24 +1,14 @@
 package ServidorAplicacao.Servicos.teacher;
 
-import org.apache.ojb.broker.PersistenceBroker;
-import org.apache.ojb.broker.PersistenceBrokerFactory;
-import org.apache.ojb.broker.query.Criteria;
-import org.apache.ojb.broker.query.Query;
-import org.apache.ojb.broker.query.QueryByCriteria;
-
-import Dominio.Announcement;
-import Dominio.IAnnouncement;
 import ServidorAplicacao.IUserView;
 import ServidorAplicacao.Servico.Autenticacao;
 import ServidorAplicacao.Servico.exceptions.ExistingServiceException;
 import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorAplicacao.Servicos.ServiceNeedsAuthenticationTestCase;
-import ServidorPersistente.ISuportePersistente;
-import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 /**
- * @author Barbosa
- * @author Pica
+ * @author  Barbosa
+ * @author  Pica
  * 
  */
 public class CreateAnnouncementTest
@@ -41,30 +31,34 @@ public class CreateAnnouncementTest
 	protected String getExpectedDataSetFilePath() {
 		return "etc/datasets/servicos/teacher/testCreateAnnouncementExpectedDataSet.xml";
 	}
+	
+	protected String getExistingExpectedDataSetFilePath() {
+		return "etc/datasets/servicos/teacher/testCreateExistingAnnouncementExpectedDataSet.xml";
+	}
 
 	protected String getNameOfServiceToBeTested() {
 		return "CreateAnnouncement";
 	}
 
-	protected String[] getAuthenticatedAndAuthorizedUser() {
+	protected String[] getAuthorizedUser() {
 		String[] args = { "user", "pass", getApplication()};
 		return args;
 	}
 
-	protected String[] getAuthenticatedAndUnauthorizedUser() {
+	protected String[] getUnauthorizedUser() {
 		String[] args = { "nmsn", "pass", getApplication()};
 		return args;
 	}
 
-	protected String[] getNotAuthenticatedUser() {
+	protected String[] getNonTeacherUser() {
 		String[] args = { "fiado", "pass", getApplication()};
 		return args;
 	}
 
 	protected Object[] getAuthorizeArguments() {
 		Integer infoExecutionCourseCode = new Integer(24);
-		String newAnnouncementTitle = "Anuncio Teste SUCESSO!";
-		String newAnnouncementInformation = "Sucesso! Over & Out :)";
+		String newAnnouncementTitle = "Anuncio Teste SUCESSO";
+		String newAnnouncementInformation = "Sucesso Sucesso";
 		Object[] args =
 			{
 				infoExecutionCourseCode,
@@ -76,35 +70,13 @@ public class CreateAnnouncementTest
 	public void testCreateAnnouncementSuccessful() {
 		try {
 			boolean result = false;
-			String[] args = getAuthenticatedAndAuthorizedUser();
+			String[] args = getAuthorizedUser();
 			IUserView id = authenticateUser(args);
 			Object[] args2 = getAuthorizeArguments();
 
 			gestor.executar(id, getNameOfServiceToBeTested(), args2);
 
-			PersistenceBroker broker =
-				PersistenceBrokerFactory.defaultPersistenceBroker();
-
-			Criteria criteria = new Criteria();
-			criteria.addOrderBy("lastModifiedDate", false);
-			Query queryCriteria =
-				new QueryByCriteria(Announcement.class, criteria);
-			IAnnouncement announcement =
-				(IAnnouncement) broker.getObjectByQuery(queryCriteria);
-			broker.close();
-
-			assertEquals(announcement.getTitle(), args2[1]);
-			assertEquals(announcement.getInformation(), args2[2]);
-
-			//erase new created announcement to verify nothing else has changed
-			ISuportePersistente sp = SuportePersistenteOJB.getInstance();
-			sp.iniciarTransaccao();
-			sp.getIPersistentAnnouncement().deleteByOID(
-				Announcement.class,
-				announcement.getIdInternal());
-			sp.confirmarTransaccao();
-
-			compareDataSet(getExpectedDataSetFilePath());
+			compareDataSetUsingExceptedDataSetTableColumns(getExpectedDataSetFilePath());
 			System.out.println(
 				"CreateAnnouncementSuccessfulTest was SUCCESSFULY runned by service: "
 					+ this.getClass().getName());
@@ -134,7 +106,7 @@ public class CreateAnnouncementTest
 			fail("testCreateExistingAnnouncement");
 
 		} catch (ExistingServiceException e) {
-			compareDataSet("etc/datasets/servicos/teacher/testCreateExistingAnnouncementExpectedDataSet.xml");
+			compareDataSetUsingExceptedDataSetTableColumns(getExistingExpectedDataSetFilePath());
 			System.out.println(
 				"testCreateExistingAnnouncement was SUCCESSFULY runned by class: "
 					+ this.getClass().getName());
