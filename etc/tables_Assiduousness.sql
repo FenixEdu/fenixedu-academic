c35e62b7ccdcf1f3a0b0b8f128a95f2e5674c053
Tânia Pousão <tania.pousao@ist.utl.pt>
2003-11-07 17:31:02 +0000

    *** empty log message ***

--- etc/tables_Assiduousness.sql
+++ etc/tables_Assiduousness.sql
@@ -118,30 +118,40 @@ CREATE TABLE ass_FUNC_NAO_DOCENTE (
 ) TYPE=InnoDB COMMENT='InnoDB free: 378880 kB; InnoDB free: 378880 kB';
 
 --
--- Table structure for table 'ass_FUNCIONARIO'
+-- Table structure for table `ass_FUNCIONARIO`
 --
 
 DROP TABLE IF EXISTS ass_FUNCIONARIO;
 CREATE TABLE ass_FUNCIONARIO (
-  codigoInterno int(11) NOT NULL default '0',
+  codigoInterno int(11) NOT NULL auto_increment,
   chavePessoa int(11) NOT NULL default '0',
   numeroMecanografico int(11) NOT NULL default '0',
   chaveHorarioActual int(11) NOT NULL default '0',
   antiguidade date NOT NULL default '0000-00-00',
-  chaveFuncResponsavel int(11) NOT NULL default '0',
-  chaveCCLocalTrabalho int(11) NOT NULL default '0',
-  chaveCCCorrespondencia int(11) NOT NULL default '0',
-  chaveCCVencimento int(11) NOT NULL default '0',
-  calendario varchar(50) NOT NULL default '',
-  chaveStatus int(11) NOT NULL default '0',
+  PRIMARY KEY  (codigoInterno),
+  UNIQUE KEY u1 (numeroMecanografico),
+  UNIQUE KEY u2 (chavePessoa)
+) TYPE=InnoDB;TYPE=InnoDB COMMENT='InnoDB free: 378880 kB; InnoDB free: 378880 kB';
+
+--
+-- Table structure for table 'ass_FUNCIONARIO_HISTORICO'
+--
+DROP TABLE IF EXISTS ass_FUNCIONARIO_HISTORICO;
+CREATE TABLE ass_FUNCIONARIO_HISTORICO (
+  codigoInterno int(11) NOT NULL auto_increment,
+  chaveFuncionario int(11) NOT NULL default '0',
+  chaveFuncResponsavel int(11) default '0',
+  chaveCCLocalTrabalho int(11) default '0',
+  chaveCCCorrespondencia int(11) default '0',
+  chaveCCVencimento int(11) default '0',
+  calendario varchar(50) default '',
+  chaveStatus int(11) default '0',
   dataInicio date NOT NULL default '0000-00-00',
   dataFim date default NULL,
   quem int(11) NOT NULL default '0',
   quando datetime NOT NULL default '0000-00-00 00:00:00',
-  PRIMARY KEY  (codigoInterno),
-  UNIQUE KEY u2 (chavePessoa),
-  UNIQUE KEY u1 (numeroMecanografico)
-) TYPE=InnoDB COMMENT='InnoDB free: 378880 kB; InnoDB free: 378880 kB';
+  PRIMARY KEY  (codigoInterno)
+) TYPE=InnoDB;
 
 --
 -- Table structure for table 'ass_HORARIO'
