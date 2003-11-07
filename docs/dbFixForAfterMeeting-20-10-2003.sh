0e0f8de5752774ad128291e4a4dd028302777381
João Mota <joao.mota@ist.utl.pt>
2003-11-07 14:54:14 +0000

    *** empty log message ***

--- docs/dbFixForAfterMeeting-20-10-2003.sh
+++
@@ -1,2 +0,0 @@
-mysql -Dfenix -p < dbFixForAfterMeeting-20-10-2003-n1.sql
-mysql -Dfenix -p < dbFixForAfterMeeting-20-10-2003-n2.sql | mysql -Dfenix -p
--- docs/fixForDeploy-29-10-2003.sql
+++
@@ -1,9 +0,0 @@
--- alter table METADATA add visibility bit not null default "1";
--- alter table XML_DOCUMENTS add visibility bit not null default "1";
-
-select CONCAT(
-		"insert into COORDINATOR (key_execution_degree,key_teacher,responsible) values (",
-         id_internal,
-         ","
-         ,key_teacher,
-          ",1);" ) as "" from EXECUTION_DEGREE; 
