ef4cfc51919aef420ae7c55dce7b68952f51bf39
João Mota <joao.mota@ist.utl.pt>
2003-11-10 17:01:50 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/UtilsTestCase.java
+++ src_tests/ServidorAplicacao/Servicos/UtilsTestCase.java
@@ -4,12 +4,11 @@
  */
 package ServidorAplicacao.Servicos;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.beanutils.PropertyUtils;
-
-import DataBeans.InfoExamStudentRoom;
 
 /**
  * @author Luis Egidio, lmre@mega.ist.utl.pt Nuno Ochoa, nmgo@mega.ist.utl.pt
@@ -34,29 +33,41 @@ public class UtilsTestCase {
 
 		if ((objects.size() == 0) || (objects.size() != values.length))
 			return false;
-
 		if (values.length == 0)
 			return true;
-
 		try {
 			Object object = objects.get(0);
-
+			List propertiesValues = new ArrayList();
 			if (object.getClass().equals(infoType)) {
-
 				for (int i = 0; i < values.length && encontrou; i++) {
-					Object temp = values[i];
-					System.out.println(InfoExamStudentRoom.class);
-					Object object2 =
-						PropertyUtils.getSimpleProperty(
-							objects.get(i),
-							property);
+					Object object2 = PropertyUtils.getSimpleProperty(objects.get(i), property);
+					propertiesValues.add(object2);
 					encontrou = Arrays.asList(values).contains(object2);
 
 				}
+				if (!encontrou) {
+					System.out.println("expected Value Type->" + values[0].getClass().toString());
+					System.out.println(
+						"object Type Received->"
+							+ propertiesValues
+								.get(propertiesValues.size() - 1)
+								.getClass()
+								.toString());
+
+					System.out.println("Wrong Values Returned!");
+					System.out.println("Was Expecting: " + Arrays.asList(values));
+					System.out.println("But Received: " + propertiesValues);
+				}
 				return encontrou;
 
-			} else
+			} else {
+				System.out.println(
+					"Wrong Class Returned! Was Expecting: "
+						+ infoType
+						+ " but received: "
+						+ object.getClass());
 				return false;
+			}
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
