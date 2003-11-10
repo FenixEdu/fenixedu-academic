82019f807433bcb95a5011f3a32e109f61b0e6ea
João Mota <joao.mota@ist.utl.pt>
2003-11-10 16:09:41 +0000

    *** empty log message ***

--- docs/generationStudentCurricularPlan.sql
+++
@@ -1,7 +0,0 @@
-select CONCAT("update STUDENT_CURRICULAR_PLAN set key_degree_curricular_plan =",
-              dcp2.id_internal,
-              " where key_degree_curricular_plan=", dcp.id_internal, ";") as ""
-from DEGREE_CURRICULAR_PLAN dcp inner join DEGREE_CURRICULAR_PLAN dcp2
-  on dcp2.name like CONCAT(dcp.name,"2%4")
-where left(dcp.name,1) = "L"
-order by dcp.name;
