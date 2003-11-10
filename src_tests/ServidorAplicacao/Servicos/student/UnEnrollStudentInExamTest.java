79be6c9213c8c5611469cd3bf6be62eadc872beb
Luís Egídio <luis.egidio@ist.utl.pt>
2003-11-10 15:10:53 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/student/UnEnrollStudentInExamTest.java
+++ src_tests/ServidorAplicacao/Servicos/student/UnEnrollStudentInExamTest.java
@@ -1,65 +1,221 @@
-/*
- * ReadCourseByStudentTest.java JUnit based test
- *
- * Created on February 26th, 2003, 15:33
- */
-
 package ServidorAplicacao.Servicos.student;
 
+import ServidorAplicacao.Servico.Autenticacao;
+import ServidorAplicacao.Servico.exceptions.notAuthorizedServiceDeleteException;
+import ServidorAplicacao.Servicos.ServiceNeedsAuthenticationTestCase;
+
 /**
+ * @author  Luis Egidio, lmre@mega.ist.utl.pt
+ * 			Nuno Ochoa,  nmgo@mega.ist.utl.pt
  *
- * @author João Mota
  */
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import DataBeans.InfoPerson;
-import DataBeans.InfoShiftEnrolment;
-import DataBeans.InfoStudent;
-import ServidorAplicacao.Servicos.TestCaseDeleteAndEditServices;
+public class UnEnrollStudentInExamTest
+	extends ServiceNeedsAuthenticationTestCase {
 
-public class UnEnrollStudentInExamTest extends TestCaseDeleteAndEditServices {
+	/**
+	 * @param testName
+	 */
+	public UnEnrollStudentInExamTest(String name) {
+		super(name);
+	}
 
-	private InfoPerson infoPerson = null;
-	private InfoStudent infoStudent = null;
-	private InfoShiftEnrolment infoShiftEnrolment = null;
-	
-    public UnEnrollStudentInExamTest(java.lang.String testName) {
-    super(testName);
-  }
-    
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-    
-  public static Test suite() {
-    TestSuite suite = new TestSuite(UnEnrollStudentInExamTest.class);
-        
-    return suite;
-  }
-    
-  protected void setUp() {
-    super.setUp();
-  }
-    
-  protected void tearDown() {
-    super.tearDown();
-  }
+	protected String getNameOfServiceToBeTested() {
+		return "UnEnrollStudentInExam";
+	}
 
+	protected String getDataSetFilePath() {
+		return "etc/datasets/servicos/student/testUnEnrollStudentInExamDataSet.xml";
+	}
 
+	protected String[] getAuthenticatedAndAuthorizedUser() {
+		String[] args = { "13", "pass", getApplication()};
+		return args;
+	}
 
-  protected String getNameOfServiceToBeTested() {
-	  return "UnEnrollStudentInExam";
-  }
+	protected String[] getAuthenticatedAndUnauthorizedUser() {
+		String[] args = { "nmsn", "pass", getApplication()};
+		return args;
+	}
 
-  protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {	
-	return null;
-	 
-  }
+	protected String[] getNotAuthenticatedUser() {
+		String[] args = { "fiado", "pass", getApplication()};
+		return args;
+	}
 
-  protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-	  Object[] result = { "3",new Integer(1)};
-	  return result;
-  }
+	protected Object[] getAuthorizeArguments() {
+		Object[] args = { userView.getUtilizador(), new Integer(2)};
+		return args;
+	}
 
- 
+	protected String getApplication() {
+		return Autenticacao.EXTRANET;
+	}
+
+	public void testUnEnrollNonExistingStudentInExam() {
+		Object[] args = { "UsernameOfNonExistingStudent", new Integer(2)};
+		Boolean result;
+
+		try {
+			result =
+				(Boolean) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args);
+			assertTrue(result.booleanValue());
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedUnEnrollNonExistingStudentInExamDataSet.xml");
+			System.out.println(
+				"testUnEnrollNonExistingStudentInExam was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testUnEnrollNonExistingStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testUnEnrollNonExistingStudentInExam");
+		}
+	}
+
+	public void testUnEnrollStudentInNonExistingExam() {
+		Object[] args = { userView.getUtilizador(), new Integer(7)};
+		Boolean result;
+
+		try {
+			result =
+				(Boolean) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args);
+			assertTrue(result.booleanValue());
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedUnEnrollStudentInNonExistingExamDataSet.xml");
+			System.out.println(
+				"testUnEnrollStudentInNonExistingExam was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testUnEnrollStudentInNonExistingExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testUnEnrollStudentInNonExistingExam");
+		}
+	}
+
+	public void testInvalidUnEnrollStudentInExam() {
+		Object[] args = { userView.getUtilizador(), new Integer(6)};
+		// This exam has no Enrollment_End_Day or Time associated. In this case,
+		// it is considered to be an invalid unenrollment of such exam.
+
+		try {
+			gestor.executar(userView, getNameOfServiceToBeTested(), args);
+			System.out.println(
+				"testInvalidUnEnrollStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testInvalidUnEnrollStudentInExam");
+
+		} catch (notAuthorizedServiceDeleteException ex) {
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedInvalidUnEnrollStudentInExamDataSet.xml");
+			ex.printStackTrace();
+			System.out.println(
+				"testInvalidUnEnrollStudentInExam was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testInvalidUnEnrollStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testInvalidUnEnrollStudentInExam");
+		}
+	}
+
+	public void testNotAuthorizedUnEnrollStudentInExam() {
+		Object[] args = { userView.getUtilizador(), new Integer(1)};
+		// Reservation for this student has already been made in a exam room.
+
+		try {
+			gestor.executar(userView, getNameOfServiceToBeTested(), args);
+			System.out.println(
+				"testNotAuthorizedUnEnrollStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testNotAuthorizedUnEnrollStudentInExam");
+
+		} catch (notAuthorizedServiceDeleteException ex) {
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedNotAuthorizedUnEnrollStudentInExamDataSet.xml");
+			ex.printStackTrace();
+			System.out.println(
+				"testNotAuthorizedUnEnrollStudentInExam was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testNotAuthorizedUnEnrollStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testNotAuthorizedUnEnrollStudentInExam");
+		}
+	}
+
+	public void testUnEnrollNonExistingEnrollment() {
+		Object[] args = { userView.getUtilizador(), new Integer(3)};
+		Boolean result;
+
+		try {
+			result =
+				(Boolean) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args);
+			assertTrue(result.booleanValue());
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedUnEnrollNonExistingEnrollmentDataSet.xml");
+			System.out.println(
+				"testUnEnrollNonExistingEnrollment was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testUnEnrollNonExistingEnrollment was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testUnEnrollNonExistingEnrollment");
+		}
+	}
+
+	public void testUnEnrollStudentInExam() {
+		Object[] args = { userView.getUtilizador(), new Integer(2)};
+		Boolean result;
+
+		try {
+			result =
+				(Boolean) gestor.executar(
+					userView,
+					getNameOfServiceToBeTested(),
+					args);
+			assertTrue(result.booleanValue());
+			compareDataSetUsingExceptedDataSetTableColumns(
+				"etc/datasets/servicos/student/"
+					+ "testExpectedUnEnrollStudentInExamDataSet.xml");
+			System.out.println(
+				"testUnEnrollStudentInExam was SUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println(
+				"testUnEnrollStudentInExam was UNSUCCESSFULY runned by class: "
+					+ this.getClass().getName());
+			fail("testUnEnrollStudentInExam");
+		}
+	}
+
 }
