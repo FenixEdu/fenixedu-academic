9fb01b5c03dae267c726d2721812ca0f237cd8bd
Nuno Barbosa <nmdb@mega.ist.utl.pt>
2003-11-14 17:19:12 +0000

    *** empty log message ***

--- src/ServidorAplicacao/Servico/grant/owner/CreateGrantOwner.java
+++ src/ServidorAplicacao/Servico/grant/owner/CreateGrantOwner.java
@@ -27,149 +27,160 @@ import Util.RoleType;
  *  
  */
 public class CreateGrantOwner
-	extends ServidorAplicacao.Servico.person.base.CreatePersonBaseClass
-	implements IServico {
+    extends ServidorAplicacao.Servico.person.base.CreatePersonBaseClass
+    implements IServico
+{
 
-	private static CreateGrantOwner service = new CreateGrantOwner();
-	/**
-	 * The singleton access method of this class.
-	 */
-	public static CreateGrantOwner getService() {
-		return service;
-	}
-	/**
-	 * The constructor of this class.
-	 */
-	private CreateGrantOwner() {
-	}
-	/**
-	 * The name of the service
-	 */
-	public final String getNome() {
-		return "CreateGrantOwner";
-	}
+    private static CreateGrantOwner service = new CreateGrantOwner();
+    /**
+     * The singleton access method of this class.
+     */
+    public static CreateGrantOwner getService()
+    {
+        return service;
+    }
+    /**
+     * The constructor of this class.
+     */
+    private CreateGrantOwner()
+    {
+    }
+    /**
+     * The name of the service
+     */
+    public final String getNome()
+    {
+        return "CreateGrantOwner";
+    }
 
-	private String generateGrantOwnerPersonUsername(Integer grantOwnerNumber)
-		throws ExcepcaoPersistencia {
-		String result = null;
-		result = "B" + grantOwnerNumber.toString();
-		return result;
-	}
+    private String generateGrantOwnerPersonUsername(Integer grantOwnerNumber)
+        throws ExcepcaoPersistencia
+    {
+        String result = null;
+        result = "B" + grantOwnerNumber.toString();
+        return result;
+    }
 
-	private IPessoa checkIfPersonExists(InfoPerson infoPerson,IPessoaPersistente persistentPerson)
-		throws FenixServiceException {
-			IPessoa person = null;
+    private IPessoa checkIfPersonExists(InfoPerson infoPerson, IPessoaPersistente persistentPerson)
+        throws FenixServiceException
+    {
+        IPessoa person = null;
 
-		try {
-			person =
-				persistentPerson.lerPessoaPorNumDocIdETipoDocId(
-					infoPerson.getNumeroDocumentoIdentificacao(),
-					infoPerson.getTipoDocumentoIdentificacao());
-		} catch (ExcepcaoPersistencia persistentException) {
-			throw new FenixServiceException(persistentException.getMessage());
-		}
+        try
+        {
+            person =
+                persistentPerson.lerPessoaPorNumDocIdETipoDocId(
+                    infoPerson.getNumeroDocumentoIdentificacao(),
+                    infoPerson.getTipoDocumentoIdentificacao());
+        } catch (ExcepcaoPersistencia persistentException)
+        {
+            throw new FenixServiceException(persistentException.getMessage());
+        }
 
-		return person;
-	}
+        return person;
+    }
 
-	private IGrantOwner checkIfGrantOwnerExists(
-		Integer personIdInternal,
-		IPessoaPersistente persistentPerson,
-		IPersistentGrantOwner persistentGrantOwner)
-		throws FenixServiceException {
-		IGrantOwner grantOwner = null;
-		try {
-			grantOwner =
-				persistentGrantOwner.readGrantOwnerByPerson(personIdInternal);
-		} catch (ExcepcaoPersistencia persistentException) {
-			throw new FenixServiceException(persistentException.getMessage());
-		}
-		return grantOwner;
-	}
+    private IGrantOwner checkIfGrantOwnerExists(
+        Integer personIdInternal,
+        IPessoaPersistente persistentPerson,
+        IPersistentGrantOwner persistentGrantOwner)
+        throws FenixServiceException
+    {
+        IGrantOwner grantOwner = null;
+        try
+        {
+            grantOwner = persistentGrantOwner.readGrantOwnerByPerson(personIdInternal);
+        } catch (ExcepcaoPersistencia persistentException)
+        {
+            throw new FenixServiceException(persistentException.getMessage());
+        }
+        return grantOwner;
+    }
 
-	/**
-	 * Executes the service.
-	 */
-	public boolean run(InfoGrantOwner infoGrantOwner)
-		throws FenixServiceException {
-		ISuportePersistente persistentSupport = null;
-		IPersistentGrantOwner persistentGrantOwner = null;
-		IPersistentPersonRole persistentPersonRole = null;
-		IPessoaPersistente persistentPerson = null;
-		
-		try {
-			persistentSupport = SuportePersistenteOJB.getInstance();
-		} catch (ExcepcaoPersistencia e) {
-			e.printStackTrace();
-			throw new FenixServiceException("Unable to dao factory!", e);
-		}
-		persistentGrantOwner = persistentSupport.getIPersistentGrantOwner();
-		persistentPersonRole = persistentSupport.getIPersistentPersonRole();
-		persistentPerson = persistentSupport.getIPessoaPersistente();
-		
-		try {
-			IPessoa person = null;
-			person = checkIfPersonExists(
-				infoGrantOwner.getPersonInfo(),
-				persistentPerson);
-				
-			//next 2 lines are necessary due to a possible OJB lock problem
-			persistentSupport.confirmarTransaccao();
-			persistentSupport.iniciarTransaccao();
-		
-			IGrantOwner grantOwner = null;
-			if (person != null)
-				grantOwner = checkIfGrantOwnerExists(
-									person.getIdInternal(),
-									persistentPerson,
-									persistentGrantOwner);
-		
-			//create or edit person information
-			person =
-				createPersonBase(
-					infoGrantOwner.getPersonInfo(),
-					persistentSupport,
-					persistentPerson,
-					persistentPersonRole);
-			
-			//create or edit grantOwner information
-			IPersonRole personRole = null;
-			if(grantOwner == null) {
-				grantOwner = new GrantOwner();
+    /**
+     * Executes the service.
+     */
+    public boolean run(InfoGrantOwner infoGrantOwner) throws FenixServiceException
+    {
+        ISuportePersistente persistentSupport = null;
+        IPersistentGrantOwner persistentGrantOwner = null;
+        IPersistentPersonRole persistentPersonRole = null;
+        IPessoaPersistente persistentPerson = null;
 
-				//Set the GRANT_OWNER Role to this new GrantOwner
-				personRole = new PersonRole();
-				personRole.setPerson(person);
-				persistentPersonRole.simpleLockWrite(personRole);
-				personRole.setRole(
-					persistentSupport.getIPersistentRole().readByRoleType(
-						RoleType.GRANT_OWNER));
-			}
-			
-			grantOwner.setPerson(person);
-			if(grantOwner.getCardCopyNumber() == null)
-				grantOwner.setCardCopyNumber(infoGrantOwner.getCardCopyNumber());
-			if(grantOwner.getDateSendCGD() == null)
-				grantOwner.setDateSendCGD(infoGrantOwner.getDateSendCGD());
-			persistentGrantOwner.simpleLockWrite(grantOwner);
+        try
+        {
+            persistentSupport = SuportePersistenteOJB.getInstance();
+        } catch (ExcepcaoPersistencia e)
+        {
+            e.printStackTrace();
+            throw new FenixServiceException("Unable to dao factory!", e);
+        }
+        persistentGrantOwner = persistentSupport.getIPersistentGrantOwner();
+        persistentPersonRole = persistentSupport.getIPersistentPersonRole();
+        persistentPerson = persistentSupport.getIPessoaPersistente();
 
-			if(infoGrantOwner.getGrantOwnerNumber() == null) {
-				//Generate the GrantOwner's number
-				Integer maxNumber = persistentGrantOwner.readMaxGrantOwnerNumber();
-				int aux = maxNumber.intValue() + 1;
-				Integer nextNumber = new Integer(aux);
-				grantOwner.setNumber(nextNumber);
-			}
-			else grantOwner.setNumber(infoGrantOwner.getGrantOwnerNumber());
+        try
+        {
+            IPessoa person = null;
+            person = checkIfPersonExists(infoGrantOwner.getPersonInfo(), persistentPerson);
 
-			//Generate the GrantOwner's Person Username
-			if (person.getUsername() == null)
-				person.setUsername(
-					generateGrantOwnerPersonUsername(grantOwner.getNumber()));
+            //next 2 lines are necessary due to a possible OJB lock problem
+            persistentSupport.confirmarTransaccao();
+            persistentSupport.iniciarTransaccao();
 
-		} catch (ExcepcaoPersistencia excepcaoPersistencia) {
-			throw new FenixServiceException(excepcaoPersistencia.getMessage());
-		}
-		return true;
-	}
+            IGrantOwner grantOwner = null;
+            if (person != null)
+                grantOwner =
+                    checkIfGrantOwnerExists(
+                        person.getIdInternal(),
+                        persistentPerson,
+                        persistentGrantOwner);
+
+            //create or edit person information
+            person =
+                createPersonBase(
+                    infoGrantOwner.getPersonInfo(),
+                    persistentSupport,
+                    persistentPerson,
+                    persistentPersonRole);
+
+            //create or edit grantOwner information
+            IPersonRole personRole = null;
+            if (grantOwner == null)
+            {
+                grantOwner = new GrantOwner();
+
+                //Set the GRANT_OWNER Role to this new GrantOwner
+                personRole = new PersonRole();
+                personRole.setPerson(person);
+                persistentPersonRole.simpleLockWrite(personRole);
+                personRole.setRole(
+                    persistentSupport.getIPersistentRole().readByRoleType(RoleType.GRANT_OWNER));
+            }
+
+            grantOwner.setPerson(person);
+            grantOwner.setCardCopyNumber(infoGrantOwner.getCardCopyNumber());
+            grantOwner.setDateSendCGD(infoGrantOwner.getDateSendCGD());
+            persistentGrantOwner.simpleLockWrite(grantOwner);
+
+            if (infoGrantOwner.getGrantOwnerNumber() == null)
+            {
+                //Generate the GrantOwner's number
+                Integer maxNumber = persistentGrantOwner.readMaxGrantOwnerNumber();
+                int aux = maxNumber.intValue() + 1;
+                Integer nextNumber = new Integer(aux);
+                grantOwner.setNumber(nextNumber);
+            } else
+                grantOwner.setNumber(infoGrantOwner.getGrantOwnerNumber());
+
+            //Generate the GrantOwner's Person Username
+            if (person.getUsername() == null)
+                person.setUsername(generateGrantOwnerPersonUsername(grantOwner.getNumber()));
+
+        } catch (ExcepcaoPersistencia excepcaoPersistencia)
+        {
+            throw new FenixServiceException(excepcaoPersistencia.getMessage());
+        }
+        return true;
+    }
 }
--- src/ServidorAplicacao/Servico/person/base/CreatePersonBaseClass.java
+++ src/ServidorAplicacao/Servico/person/base/CreatePersonBaseClass.java
@@ -34,124 +34,101 @@ import Util.RoleType;
  * 
  */
 
-public class CreatePersonBaseClass {
+public class CreatePersonBaseClass
+{
+    public IPessoa createPersonBase(
+        InfoPerson newPerson,
+        ISuportePersistente persistentSupport,
+        IPessoaPersistente persistentPerson,
+        IPersistentPersonRole persistentPersonRole)
+        throws FenixServiceException
+    {
+        IPersistentCountry persistentCountry = null;
 
-	public IPessoa createPersonBase(InfoPerson newPerson, ISuportePersistente persistentSupport, IPessoaPersistente persistentPerson,IPersistentPersonRole persistentPersonRole)
-		throws FenixServiceException {
-		
-		IPersistentCountry persistentCountry = null;
+        IPessoa person = null;
+        try
+        {
+            persistentCountry = persistentSupport.getIPersistentCountry();
 
-		IPessoa person = null;
+            //Check if the person Exists
+            person =
+                persistentPerson.lerPessoaPorNumDocIdETipoDocId(
+                    newPerson.getNumeroDocumentoIdentificacao(),
+                    newPerson.getTipoDocumentoIdentificacao());
 
-		try {
-			persistentCountry = persistentSupport.getIPersistentCountry();
+            // Create the new Person if it does not exist
+            if (person == null)
+            {
+                person = new Pessoa();
 
-			//Check if the person Exists
-			person =
-				persistentPerson.lerPessoaPorNumDocIdETipoDocId(
-					newPerson.getNumeroDocumentoIdentificacao(),
-					newPerson.getTipoDocumentoIdentificacao());
+                IPersonRole personRole = new PersonRole();
+                persistentPersonRole.simpleLockWrite(personRole);
+                personRole.setPerson(person);
+                personRole.setRole(
+                    persistentSupport.getIPersistentRole().readByRoleType(RoleType.PERSON));
+            }
+            //lock person for WRITE
+            persistentPerson.simpleLockWrite(person);
 
-			// Create the new Person if it does not exist
-			if (person == null) {
-				person = new Pessoa();
-				
-				IPersonRole personRole = new PersonRole();
-				persistentPersonRole.simpleLockWrite(personRole);
-				personRole.setPerson(person);
-				personRole.setRole(
-					persistentSupport.getIPersistentRole().readByRoleType(RoleType.PERSON));
-			} 
-			//lock person for WRITE
-			persistentPerson.simpleLockWrite(person);
+            //if (newPerson.getNome() != null)
+            person.setNome(newPerson.getNome());
+            if (newPerson.getNumeroDocumentoIdentificacao() != null)
+                person.setNumeroDocumentoIdentificacao(newPerson.getNumeroDocumentoIdentificacao());
+            if (newPerson.getTipoDocumentoIdentificacao() != null)
+                person.setTipoDocumentoIdentificacao(newPerson.getTipoDocumentoIdentificacao());
 
-			if (newPerson.getNome() != null)
-				person.setNome(newPerson.getNome());
-			if (newPerson.getNumeroDocumentoIdentificacao() != null)
-				person.setNumeroDocumentoIdentificacao(
-					newPerson.getNumeroDocumentoIdentificacao());
-			if (newPerson.getTipoDocumentoIdentificacao() != null)
-				person.setTipoDocumentoIdentificacao(
-					newPerson.getTipoDocumentoIdentificacao());
+            person.setCodigoFiscal(newPerson.getCodigoFiscal());
+            person.setCodigoPostal(newPerson.getCodigoPostal());
+            person.setConcelhoMorada(newPerson.getConcelhoMorada());
+            person.setConcelhoNaturalidade(newPerson.getConcelhoNaturalidade());
+            person.setDataEmissaoDocumentoIdentificacao(
+                newPerson.getDataEmissaoDocumentoIdentificacao());
+            person.setDataValidadeDocumentoIdentificacao(
+                newPerson.getDataValidadeDocumentoIdentificacao());
+            person.setDistritoMorada(newPerson.getDistritoMorada());
+            person.setDistritoNaturalidade(newPerson.getDistritoNaturalidade());
+            person.setEmail(newPerson.getEmail());
+            person.setEnderecoWeb(newPerson.getEnderecoWeb());
+            person.setEstadoCivil(newPerson.getEstadoCivil());
+            person.setFreguesiaMorada(newPerson.getFreguesiaMorada());
+            person.setFreguesiaNaturalidade(newPerson.getFreguesiaNaturalidade());
+            person.setLocalEmissaoDocumentoIdentificacao(
+                newPerson.getLocalEmissaoDocumentoIdentificacao());
+            person.setLocalidade(newPerson.getLocalidade());
+            person.setLocalidadeCodigoPostal(newPerson.getLocalidadeCodigoPostal());
+            person.setMorada(newPerson.getMorada());
+            person.setNacionalidade(newPerson.getNacionalidade());
+            person.setNascimento(newPerson.getNascimento());
+            person.setNomeMae(newPerson.getNomeMae());
+            person.setNomePai(newPerson.getNomePai());
+            person.setNumContribuinte(newPerson.getNumContribuinte());
 
-			if (newPerson.getCodigoFiscal() != null)
-				person.setCodigoFiscal(newPerson.getCodigoFiscal());
-			if (newPerson.getCodigoPostal() != null)
-				person.setCodigoPostal(newPerson.getCodigoPostal());
-			if (newPerson.getConcelhoMorada() != null)
-				person.setConcelhoMorada(newPerson.getConcelhoMorada());
-			if (newPerson.getConcelhoNaturalidade() != null)
-				person.setConcelhoNaturalidade(
-					newPerson.getConcelhoNaturalidade());
-			if (newPerson.getDataEmissaoDocumentoIdentificacao() != null)
-				person.setDataEmissaoDocumentoIdentificacao(
-					newPerson.getDataEmissaoDocumentoIdentificacao());
-			if (newPerson.getDataValidadeDocumentoIdentificacao() != null)
-				person.setDataValidadeDocumentoIdentificacao(
-					newPerson.getDataValidadeDocumentoIdentificacao());
-			if (newPerson.getDistritoMorada() != null)
-				person.setDistritoMorada(newPerson.getDistritoMorada());
-			if (newPerson.getDistritoNaturalidade() != null)
-				person.setDistritoNaturalidade(
-					newPerson.getDistritoNaturalidade());
-			if (newPerson.getEmail() != null)
-				person.setEmail(newPerson.getEmail());
-			if (newPerson.getEnderecoWeb() != null)
-				person.setEnderecoWeb(newPerson.getEnderecoWeb());
-			if (newPerson.getEstadoCivil() != null)
-				person.setEstadoCivil(newPerson.getEstadoCivil());
-			if (newPerson.getFreguesiaMorada() != null)
-				person.setFreguesiaMorada(newPerson.getFreguesiaMorada());
-			if (newPerson.getFreguesiaNaturalidade() != null)
-				person.setFreguesiaNaturalidade(
-					newPerson.getFreguesiaNaturalidade());
-			if (newPerson.getLocalEmissaoDocumentoIdentificacao() != null)
-				person.setLocalEmissaoDocumentoIdentificacao(
-					newPerson.getLocalEmissaoDocumentoIdentificacao());
-			if (newPerson.getLocalidade() != null)
-				person.setLocalidade(newPerson.getLocalidade());
-			if (newPerson.getLocalidadeCodigoPostal() != null)
-				person.setLocalidadeCodigoPostal(
-					newPerson.getLocalidadeCodigoPostal());
-			if (newPerson.getMorada() != null)
-				person.setMorada(newPerson.getMorada());
-			if (newPerson.getNacionalidade() != null)
-				person.setNacionalidade(newPerson.getNacionalidade());
-			if (newPerson.getNascimento() != null)
-				person.setNascimento(newPerson.getNascimento());
-			if (newPerson.getNomeMae() != null)
-				person.setNomeMae(newPerson.getNomeMae());
-			if (newPerson.getNomePai() != null)
-				person.setNomePai(newPerson.getNomePai());
-			if (newPerson.getNumContribuinte() != null)
-				person.setNumContribuinte(newPerson.getNumContribuinte());
+            person.setProfissao(newPerson.getProfissao());
+            person.setSexo(newPerson.getSexo());
+            person.setTelefone(newPerson.getTelefone());
+            person.setTelemovel(newPerson.getTelemovel());
 
-			if (newPerson.getProfissao() != null)
-				person.setProfissao(newPerson.getProfissao());
-			if (newPerson.getSexo() != null)
-				person.setSexo(newPerson.getSexo());
-			if (newPerson.getTelefone() != null)
-				person.setTelefone(newPerson.getTelefone());
-			if (newPerson.getTelemovel() != null)
-				person.setTelemovel(newPerson.getTelemovel());
+            if (newPerson.getInfoPais() != null)
+            {
+                ICountry country =
+                    persistentCountry.readCountryByCode(newPerson.getInfoPais().getCode());
+                person.setPais(country);
+            }
 
-			if (newPerson.getInfoPais() != null) {
-				ICountry country = persistentCountry.readCountryByCode(newPerson.getInfoPais().getCode());
-				person.setPais(country);
-			}				
-			
-			//Generate person's Password
-			if(person.getPassword() == null)
-				person.setPassword(PasswordEncryptor.encryptPassword(GeneratePassword.generatePassword()));
+            //Generate person's Password
+            if (person.getPassword() == null)
+                person.setPassword(
+                    PasswordEncryptor.encryptPassword(GeneratePassword.generatePassword()));
 
-		} catch (ExistingPersistentException ex) {
-			throw new ExistingServiceException(ex);
-		} catch (ExcepcaoPersistencia ex) {
-			FenixServiceException newEx =
-				new FenixServiceException("Persistence layer error");
-			newEx.fillInStackTrace();
-			throw newEx;
-		}
-		return person;
-	}
+        } catch (ExistingPersistentException ex)
+        {
+            throw new ExistingServiceException(ex);
+        } catch (ExcepcaoPersistencia ex)
+        {
+            FenixServiceException newEx = new FenixServiceException("Persistence layer error");
+            newEx.fillInStackTrace();
+            throw newEx;
+        }
+        return person;
+    }
 }
