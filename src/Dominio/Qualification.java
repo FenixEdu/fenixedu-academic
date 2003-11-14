8eacc553dc0e6fe9f83774faf2a9aca9ab042ed7
João Simas <jmls@mega.ist.utl.pt>
2003-11-14 13:20:00 +0000

    *** empty log message ***

--- src/Dominio/Qualification.java
+++ src/Dominio/Qualification.java
@@ -1,121 +1,131 @@
-
 package Dominio;
 
-
 /**
- * 
  * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
  * @author Joana Mota (jccm@rnl.ist.utl.pt)
  */
-public class Qualification extends DomainObject implements IQualification {
- 
-    private Integer year;
-    private String mark;
-    private String school;
-    private String title;
-    private Integer personKey; 
-    
-    private IPessoa person;
-    
-    
-    public Qualification() {} 
-    
-    
-    public String toString() {
-        String result = "Qualification :\n";
-        result += "\n  - Internal Code : " + getIdInternal();
-        result += "\n  - Year: " + year;
-        result += "\n  - School : " + school;
-        result += "\n  - Title : " + title;
-        result += "\n  - Mark : " + mark;
-        result += "\n  - Person : " + person;
-        
-        return result;
-    }
-    
+public class Qualification extends DomainObject implements IQualification
+{
+
+	private Integer year;
+	private String mark;
+	private String school;
+	private String title;
+	private Integer personKey;
+
+	private IPessoa person;
+
+	public Qualification()
+	{
+	}
+
+	public String toString()
+	{
+		String result = "Qualification :\n";
+		result += "\n  - Internal Code : " + getIdInternal();
+		result += "\n  - Year: " + year;
+		result += "\n  - School : " + school;
+		result += "\n  - Title : " + title;
+		result += "\n  - Mark : " + mark;
+		result += "\n  - Person : " + person;
+
+		return result;
+	}
 
 	/**
 	 * @return
 	 */
-	public String getMark() {
+	public String getMark()
+	{
 		return mark;
 	}
 
 	/**
 	 * @param mark
 	 */
-	public void setMark(String mark) {
+	public void setMark(String mark)
+	{
 		this.mark = mark;
 	}
 
 	/**
 	 * @return
 	 */
-	public IPessoa getPerson() {
+	public IPessoa getPerson()
+	{
 		return person;
 	}
 
 	/**
 	 * @param person
 	 */
-	public void setPerson(IPessoa person) {
+	public void setPerson(IPessoa person)
+	{
 		this.person = person;
 	}
 
 	/**
 	 * @return
 	 */
-	public Integer getPersonKey() {
+	public Integer getPersonKey()
+	{
 		return personKey;
 	}
 
 	/**
 	 * @param personKey
 	 */
-	public void setPersonKey(Integer personKey) {
+	public void setPersonKey(Integer personKey)
+	{
 		this.personKey = personKey;
 	}
 
 	/**
 	 * @return
 	 */
-	public String getSchool() {
+	public String getSchool()
+	{
 		return school;
 	}
 
 	/**
 	 * @param school
 	 */
-	public void setSchool(String school) {
+	public void setSchool(String school)
+	{
 		this.school = school;
 	}
 
 	/**
 	 * @return
 	 */
-	public String getTitle() {
+	public String getTitle()
+	{
 		return title;
 	}
 
 	/**
 	 * @param title
 	 */
-	public void setTitle(String title) {
+	public void setTitle(String title)
+	{
 		this.title = title;
 	}
 
 	/**
 	 * @return
 	 */
-	public Integer getYear() {
+	public Integer getYear()
+	{
 		return year;
 	}
 
 	/**
 	 * @param year
 	 */
-	public void setYear(Integer year) {
+	public void setYear(Integer year)
+	{
 		this.year = year;
 	}
 
-} 
+}
