25da5f68c508a9670a6d55f913a294b0a575d812
João Luz <joao.luz@ist.utl.pt>
2003-11-15 18:32:18 +0000

    Create Read Update Delete action.

---
+++ src/ServidorApresentacao/framework/actions/CRUDActionByOID.java
@@ -0,0 +1,283 @@
+/*
+ * Created on Nov 15, 2003 by jpvl
+ *  
+ */
+package ServidorApresentacao.framework.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+import DataBeans.InfoObject;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
+import ServidorApresentacao.Action.sop.utils.SessionUtils;
+import ServidorApresentacao.framework.actions.mappings.CRUDMapping;
+
+/**
+ * 
+ * Example: <br>
+ * 
+ * <pre>
+ * 	  <action path="...."  type="...."  name="..." input="..." scope="request" className="@link ServidorApresentacao.framework.actions.mappings.CRUDMapping"> 
+ * 								<set-property property="editService" value="..."/>
+ * 								<set-property property="deleteService" value="..."/>
+ * 								<set-property property="readService" value="..."/>
+ *  								<set-property property="oidProperty" value="..."/>
+ * 								<forward name="show-form" path="..."/> 
+ * 								<forward name="successfull-edit" path="..."/> 
+ *   								<forward name="successfull-read" path="..."/>
+ *   								<forward name="successfull-delete" path="..."/>
+ * 		</action>
+ * </pre>
+ * 
+ * 
+ * 
+ * Methods availables are: <br>
+ * 
+ * <pre>
+ * 	<b>prepareEdit</b> <br>
+ *     <b>edit</b> <br>
+ * 	<b>delete</b> <br>
+ *     <b>read</b>
+ * </pre>
+ * 
+ * <b>Notes:</b><br>
+ * 
+ * <pre>
+ * <li>Specializations of this class should implement method @link #populateFormFromInfoObject(ActionMapping, InfoObject, ActionForm, HttpServletRequest) and @link #getInfoObjectFromForm(ActionForm).
+ * </li>
+ * <li>The Struts Exception handling feature should be used to handle exceptions from the services configured.
+ * </li>
+ * </pre>
+ * 
+ * @see ServidorApresentacao.framework.actions.mappings.CRUDMapping
+ * @author jpvl
+ */
+public abstract class CRUDActionByOID extends DispatchAction
+{
+    /**
+	 * Method that invokes a service that extends @link
+	 * ServidorAplicacao.Servico.framework.DeleteDomainObjectService called
+	 * @link CRUDMapping#getDeleteService()
+	 * 
+	 * @param mapping
+	 *                   should be an instance of @link CRUDMapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return forward to the action mapping configuration called
+	 *              successfull-delete
+	 * @throws Exception
+	 */
+    public ActionForward delete(
+        ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,
+        HttpServletResponse response)
+        throws Exception
+    {
+        CRUDMapping crudMapping = (CRUDMapping) mapping;
+        Object[] args = { getOIDProperty(crudMapping, form)};
+        ServiceUtils.executeService(
+            SessionUtils.getUserView(request),
+            crudMapping.getDeleteService(),
+            args);
+        return crudMapping.findForward("successfull-delete");
+    }
+
+    /**
+	 * Method that invokes a service that extends @link
+	 * ServidorAplicacao.Servico.framework.EditDomainObjectService called @link
+	 * CRUDMapping#getEditService()
+	 * 
+	 * @param mapping
+	 *                   should be an instance of @link CRUDMapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return forward to the action mapping configuration called
+	 *              successfull-edit
+	 * @throws Exception
+	 */
+    public ActionForward edit(
+        ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,
+        HttpServletResponse response)
+        throws Exception
+    {
+        CRUDMapping crudMapping = (CRUDMapping) mapping;
+        InfoObject infoObject = getInfoObjectFromForm(form);
+        Object[] args = { getOIDProperty(crudMapping, form), infoObject };
+        ServiceUtils.executeService(
+            SessionUtils.getUserView(request),
+            crudMapping.getEditService(),
+            args);
+        return crudMapping.findForward("successfull-edit");
+    }
+
+    /**
+	 * This method creates an InfoObject using the form properties.
+	 * 
+	 * @param form
+	 * @return InfoObject created
+	 */
+    abstract protected InfoObject getInfoObjectFromForm(ActionForm form);
+
+    /**
+	 * @param crudMapping
+	 * @param form
+	 * @return
+	 */
+    private Integer getOIDProperty(CRUDMapping crudMapping, ActionForm form)
+    {
+        try
+        {
+            return new Integer(BeanUtils.getProperty(form, crudMapping.getOidProperty()));
+        }
+        catch (NumberFormatException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+	 * Tests if errors are presented.
+	 * 
+	 * @param request
+	 * @return
+	 */
+    private boolean hasErrors(HttpServletRequest request)
+    {
+
+        return request.getAttribute(Globals.ERROR_KEY) != null;
+    }
+
+    /**
+	 * By default, it does a @link BeanUtils#copyProperties(java.lang.Object,
+	 * java.lang.Object)
+	 * 
+	 * @param mapping
+	 * @param form
+	 * @param request
+	 */
+    protected void populateFormFromInfoObject(
+        ActionMapping mapping,
+        InfoObject infoObject,
+        ActionForm form,
+        HttpServletRequest request)
+    {
+        try
+        {
+            BeanUtils.copyProperties(form, infoObject);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+	 * Method that invokes a service that extends @link
+	 * ServidorAplicacao.Servico.framework.ReadDomainObjectService called @link
+	 * CRUDMapping#getReadService() Sets an attribute @link
+	 * CRUDMapping#getRequestAttribute() with the infoObject readed.
+	 * 
+	 * @param mapping
+	 *                   should be an instance of @link CRUDMapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return forward to the action mapping configuration called show-form
+	 * @throws Exception
+	 */
+    public ActionForward prepareEdit(
+        ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,
+        HttpServletResponse response)
+        throws Exception
+    {
+        CRUDMapping crudMapping = (CRUDMapping) mapping;
+        InfoObject infoObject = readInfoObject(crudMapping, form, request);
+        if (!hasErrors(request) && infoObject != null)
+        {
+            populateFormFromInfoObject(mapping, infoObject, form, request);
+        }
+        setInfoObjectToRequest(request, infoObject, crudMapping);
+        return crudMapping.findForward("show-form");
+    }
+
+    /**
+	 * Method that invokes a service that extends @link
+	 * ServidorAplicacao.Servico.framework.ReadDomainObjectService called @link
+	 * CRUDMapping#getReadService()
+	 * 
+	 * @param mapping
+	 *                   should be an instance of @link CRUDMapping
+	 * @param form
+	 * @param request
+	 * @param response
+	 * @return forward to the action mapping configuration called
+	 *              successfull-read
+	 * @throws Exception
+	 */
+    public ActionForward read(
+        ActionMapping mapping,
+        ActionForm form,
+        HttpServletRequest request,
+        HttpServletResponse response)
+        throws Exception
+    {
+        CRUDMapping crudMapping = (CRUDMapping) mapping;
+        InfoObject infoObject = readInfoObject(crudMapping, form, request);
+        setInfoObjectToRequest(request, infoObject, crudMapping);
+        return crudMapping.findForward("sucessfull-read");
+    }
+
+    /**
+	 * Reads the infoObject using de read service associated to the action
+	 * 
+	 * @param crudMapping
+	 * @param form
+	 * @return
+	 */
+    private InfoObject readInfoObject(
+        CRUDMapping crudMapping,
+        ActionForm form,
+        HttpServletRequest request)
+        throws FenixServiceException
+    {
+        IUserView userView = SessionUtils.getUserView(request);
+        Object[] args = { getOIDProperty(crudMapping, form)};
+        return (InfoObject) ServiceUtils.executeService(userView, crudMapping.getReadService(), args);
+    }
+
+    /**
+	 * @param request
+	 * @param infoObject
+	 */
+    private void setInfoObjectToRequest(
+        HttpServletRequest request,
+        InfoObject infoObject,
+        CRUDMapping crudMapping)
+    {
+        if (infoObject != null)
+        {
+            request.setAttribute(crudMapping.getRequestAttribute(), infoObject);
+        }
+    }
+}
---
+++ src/ServidorApresentacao/framework/actions/mappings/CRUDMapping.java
@@ -0,0 +1,103 @@
+/*
+ * Created on Nov 15, 2003 by jpvl
+ *
+ */
+package ServidorApresentacao.framework.actions.mappings;
+
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * @author jpvl
+ */
+public class CRUDMapping extends ActionMapping
+{
+    /**
+     * Represents the form property that identify the form property that we 
+     */
+    private String oidProperty = "objectOID";
+    private String requestAttribute;
+    private String editService;
+    private String readService;
+    private String deleteService;
+    
+    /**
+     * @return Returns the oidProperty.
+     */
+    public String getOidProperty()
+    {
+        return this.oidProperty;
+    }
+
+    /**
+     * @param oidProperty The oidProperty to set.
+     */
+    public void setOidProperty(String oidProperty)
+    {
+        this.oidProperty = oidProperty;
+    }
+
+    /**
+     * @return Returns the requestAttribute.
+     */
+    public String getRequestAttribute()
+    {
+        return this.requestAttribute;
+    }
+
+    /**
+     * @param requestAttribute The requestAttribute to set.
+     */
+    public void setRequestAttribute(String requestAttribute)
+    {
+        this.requestAttribute = requestAttribute;
+    }
+
+    /**
+     * @return Returns the deleteService.
+     */
+    public String getDeleteService()
+    {
+        return this.deleteService;
+    }
+
+    /**
+     * @param deleteService The deleteService to set.
+     */
+    public void setDeleteService(String deleteService)
+    {
+        this.deleteService = deleteService;
+    }
+
+    /**
+     * @return Returns the editService.
+     */
+    public String getEditService()
+    {
+        return this.editService;
+    }
+
+    /**
+     * @param editService The editService to set.
+     */
+    public void setEditService(String editService)
+    {
+        this.editService = editService;
+    }
+
+    /**
+     * @return Returns the readService.
+     */
+    public String getReadService()
+    {
+        return this.readService;
+    }
+
+    /**
+     * @param readService The readService to set.
+     */
+    public void setReadService(String readService)
+    {
+        this.readService = readService;
+    }
+
+}
