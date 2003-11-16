70fb8ae404e71847a1e7e377a3fe0c4e9bedc995
João Luz <joao.luz@ist.utl.pt>
2003-11-16 09:50:17 +0000

    already deployed

--- docs/deploy.sh
+++
@@ -1,1 +0,0 @@
-mysql -Dfenix -p < fixForDeploy-29-10-2003.sql | mysql -Dfenix
--- docs/updateBranches-07-11-2003.sql
+++
@@ -1,3 +0,0 @@
-delete from branch where id_internal=93;
-update branch set branch_code='' where branch_name='';
-update student_curricular_plan set key_branch=8272 where key_branch=93;
