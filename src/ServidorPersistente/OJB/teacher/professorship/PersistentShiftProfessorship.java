e28ba1b7cb9c763f094d02c61f27999bb839d7ce
João Luz <joao.luz@ist.utl.pt>
2003-11-16 11:27:26 +0000

    DAO for IShiftProfessorship

---
+++ src/ServidorPersistente/OJB/teacher/professorship/PersistentShiftProfessorship.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Nov 16, 2003 by jpvl
+ *
+ */
+package ServidorPersistente.OJB.teacher.professorship;
+
+import ServidorPersistente.IPersistentShiftProfessorship;
+import ServidorPersistente.OJB.ObjectFenixOJB;
+
+/**
+ * @author jpvl
+ */
+public class PersistentShiftProfessorship
+    extends ObjectFenixOJB
+    implements IPersistentShiftProfessorship
+{
+
+}
