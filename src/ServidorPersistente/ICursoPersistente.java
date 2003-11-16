f319ae3427cb3b1b6000c57a1f42697bf9269608
João Luz <joao.luz@ist.utl.pt>
2003-11-16 09:28:55 +0000

    - removed unused methods. - code format.

--- src/ServidorPersistente/ICursoPersistente.java
+++ src/ServidorPersistente/ICursoPersistente.java
@@ -8,21 +8,21 @@ package ServidorPersistente;
 
 /**
  * @author rpfi
+ * @author jpvl
  */
 
 import java.util.List;
 
 import Dominio.ICurso;
-import ServidorPersistente.exceptions.ExistingPersistentException;
 import Util.TipoCurso;
 
-public interface ICursoPersistente extends IPersistentObject {
-	ICurso readBySigla(String sigla) throws ExcepcaoPersistencia;
-	ICurso readByIdInternal(Integer idInternal) throws ExcepcaoPersistencia;
-	void lockWrite(ICurso degree) throws ExcepcaoPersistencia, ExistingPersistentException;
-	void delete(ICurso degree) throws ExcepcaoPersistencia;
-	void deleteAll() throws ExcepcaoPersistencia;
-	public List readAll() throws ExcepcaoPersistencia;
-	public List readAllByDegreeType(TipoCurso degreeType) throws ExcepcaoPersistencia;
-	public ICurso readByNameAndDegreeType(String name, TipoCurso degreeType) throws ExcepcaoPersistencia;
+public interface ICursoPersistente extends IPersistentObject
+{
+    ICurso readBySigla(String sigla) throws ExcepcaoPersistencia;
+    ICurso readByIdInternal(Integer idInternal) throws ExcepcaoPersistencia;
+    void delete(ICurso degree) throws ExcepcaoPersistencia;
+    public List readAll() throws ExcepcaoPersistencia;
+    public List readAllByDegreeType(TipoCurso degreeType) throws ExcepcaoPersistencia;
+    public ICurso readByNameAndDegreeType(String name, TipoCurso degreeType)
+        throws ExcepcaoPersistencia;
 }
