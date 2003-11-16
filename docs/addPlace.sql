8dd220e8e5e3c4469643aefa7ef500ed129529bf
João Luz <joao.luz@ist.utl.pt>
2003-11-16 09:50:40 +0000

    removed table definition because it is already deployed..

--- docs/addPlace.sql
+++ docs/addPlace.sql
@@ -1,15 +1,3 @@
-drop table if exists CAMPUS;
-create table CAMPUS(
-	ID_INTERNAL int(11) not null auto_increment,
-	NAME varchar(50) not null,
-	primary key (ID_INTERNAL),
-	unique U1 (NAME)
-) type=InnoDB;
-
-insert CAMPUS values(1, "Alameda");
-insert CAMPUS values(2, "TagusPark");
-
-
 alter table EXECUTION_DEGREE ADD KEY_CAMPUS int(11) not null;
 alter table EXECUTION_DEGREE DROP INDEX U1;
 alter table EXECUTION_DEGREE ADD UNIQUE U1 (ACADEMIC_YEAR,KEY_DEGREE_CURRICULAR_PLAN, KEY_CAMPUS);
