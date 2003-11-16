b64fefe9f80a071a8f3887366d8d686d43beac9a
João Luz <joao.luz@ist.utl.pt>
2003-11-16 11:33:22 +0000

    added credits property

--- src/DataBeans/InfoProfessorShip.java
+++ src/DataBeans/InfoProfessorShip.java
@@ -1,42 +1,64 @@
 /*
  * Created on 14/Mai/2003 by jpvl
- *
+ *  
  */
 package DataBeans;
 
 /**
  * @author jpvl
  */
-public class InfoProfessorShip extends InfoObject {
-	protected InfoTeacher infoTeacher;
-	protected InfoExecutionCourse infoExecutionCourse;
-
-	/**
+public class InfoProfessorShip extends InfoObject
+{
+    protected InfoTeacher infoTeacher;
+    protected InfoExecutionCourse infoExecutionCourse;
+    private Float credits;
+    /**
 	 * @return
 	 */
-	public InfoExecutionCourse getInfoExecutionCourse() {
-		return infoExecutionCourse;
-	}
+    public InfoExecutionCourse getInfoExecutionCourse()
+    {
+        return infoExecutionCourse;
+    }
 
-	/**
+    /**
 	 * @return
 	 */
-	public InfoTeacher getInfoTeacher() {
-		return infoTeacher;
-	}
+    public InfoTeacher getInfoTeacher()
+    {
+        return infoTeacher;
+    }
 
-	/**
+    /**
 	 * @param course
 	 */
-	public void setInfoExecutionCourse(InfoExecutionCourse course) {
-		infoExecutionCourse = course;
-	}
+    public void setInfoExecutionCourse(InfoExecutionCourse course)
+    {
+        infoExecutionCourse = course;
+    }
 
-	/**
+    /**
 	 * @param teacher
 	 */
-	public void setInfoTeacher(InfoTeacher teacher) {
-		infoTeacher = teacher;
-	}
+    public void setInfoTeacher(InfoTeacher teacher)
+    {
+        infoTeacher = teacher;
+    }
+
+    /**
+	 * @return Returns the credits.
+	 */
+    public Float getCredits()
+    {
+        return this.credits;
+    }
+
+    /**
+	 * @param credits
+	 *                   The credits to set.
+	 */
+    public void setCredits(Float credits)
+    {
+        this.credits = credits;
+    }
 
 }
