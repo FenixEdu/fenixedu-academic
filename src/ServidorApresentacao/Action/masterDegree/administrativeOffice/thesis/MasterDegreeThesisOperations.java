14b250275ec67ad6189c74ddd470f4231339a8c1
João Mota <joao.mota@ist.utl.pt>
2003-11-17 18:31:47 +0000

    removed unnecessary casts

--- src/ServidorApresentacao/Action/masterDegree/administrativeOffice/thesis/MasterDegreeThesisOperations.java
+++ src/ServidorApresentacao/Action/masterDegree/administrativeOffice/thesis/MasterDegreeThesisOperations.java
@@ -30,183 +30,240 @@ import Util.TipoCurso;
  *
  */
 
-public class MasterDegreeThesisOperations extends DispatchAction {
+public class MasterDegreeThesisOperations extends DispatchAction
+{
 
-	public boolean getStudentByNumberAndDegreeType(ActionForm form, HttpServletRequest request, ActionErrors actionErrors)
-		throws FenixActionException {
+    public boolean getStudentByNumberAndDegreeType(
+        ActionForm form,
+        HttpServletRequest request,
+        ActionErrors actionErrors)
+        throws FenixActionException
+    {
 
-		boolean result = false;
+        boolean result = false;
 
-		DynaActionForm getStudentByNumberAndDegreeTypeForm = (DynaActionForm) form;
-		IUserView userView = SessionUtils.getUserView(request);
+        DynaActionForm getStudentByNumberAndDegreeTypeForm = (DynaActionForm) form;
+        IUserView userView = SessionUtils.getUserView(request);
 
-		Integer degreeType = Integer.valueOf(this.getFromRequest("degreeType", request));
-		Integer studentNumber = Integer.valueOf(this.getFromRequest("studentNumber", request));
+        Integer degreeType = Integer.valueOf(this.getFromRequest("degreeType", request));
+        Integer studentNumber = Integer.valueOf(this.getFromRequest("studentNumber", request));
 
-		if ((degreeType == null) || (studentNumber == null)) {
+        if ((degreeType == null) || (studentNumber == null))
+        {
 
-			try {
-				degreeType = new Integer((String) getStudentByNumberAndDegreeTypeForm.get("degreeType"));
-				studentNumber = new Integer((String) getStudentByNumberAndDegreeTypeForm.get("studentNumber"));
-			} catch (NumberFormatException e) {
-				degreeType = (Integer) request.getAttribute("degreeType");
-				studentNumber = (Integer) request.getAttribute("studentNumber");
-				getStudentByNumberAndDegreeTypeForm.set("degreeType", degreeType.toString());
-				getStudentByNumberAndDegreeTypeForm.set("studentNumber", studentNumber.toString());
-			}
-		}
+            try
+            {
+                degreeType = new Integer((String) getStudentByNumberAndDegreeTypeForm.get("degreeType"));
+                studentNumber =
+                    new Integer((String) getStudentByNumberAndDegreeTypeForm.get("studentNumber"));
+            } catch (NumberFormatException e)
+            {
+                degreeType = (Integer) request.getAttribute("degreeType");
+                studentNumber = (Integer) request.getAttribute("studentNumber");
+                getStudentByNumberAndDegreeTypeForm.set("degreeType", degreeType.toString());
+                getStudentByNumberAndDegreeTypeForm.set("studentNumber", studentNumber.toString());
+            }
+        }
 
-		InfoStudent infoStudent = null;
+        InfoStudent infoStudent = null;
 
-		Object args[] = { studentNumber, new TipoCurso(degreeType)};
-		try {
-			infoStudent = (InfoStudent) ServiceUtils.executeService(userView, "ReadStudentByNumberAndDegreeType", args);
-		} catch (FenixServiceException e) {
-			throw new FenixActionException(e);
-		}
+        Object args[] = { studentNumber, new TipoCurso(degreeType)};
+        try
+        {
+            infoStudent =
+                (InfoStudent) ServiceUtils.executeService(
+                    userView,
+                    "ReadStudentByNumberAndDegreeType",
+                    args);
+        } catch (FenixServiceException e)
+        {
+            throw new FenixActionException(e);
+        }
 
-		if (infoStudent == null) {
-			actionErrors.add("error.no.student.in.database", new ActionError("error.no.student.in.database", studentNumber.toString()));
-			result = false;
-		} else {
-			request.setAttribute(SessionConstants.STUDENT, infoStudent);
-			result = true;
-		}
+        if (infoStudent == null)
+        {
+            actionErrors.add(
+                "error.no.student.in.database",
+                new ActionError("error.no.student.in.database", studentNumber.toString()));
+            result = false;
+        } else
+        {
+            request.setAttribute(SessionConstants.STUDENT, infoStudent);
+            result = true;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	public ArrayList getTeachersByNumbers(
-		ActionForm form,
-		HttpServletRequest request,
-		String teachersNumbersListField,
-		String sessionConstant,
-		ActionErrors actionErrors)
-		throws FenixActionException {
+    public ArrayList getTeachersByNumbers(
+        ActionForm form,
+        HttpServletRequest request,
+        String teachersNumbersListField,
+        String sessionConstant,
+        ActionErrors actionErrors)
+        throws FenixActionException
+    {
 
-		DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
-		IUserView userView = SessionUtils.getUserView(request);
+        DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
+        IUserView userView = SessionUtils.getUserView(request);
 
-		Integer[] teachersNumbersList = (Integer[]) masterDegreeThesisForm.get(teachersNumbersListField);
+        Integer[] teachersNumbersList = (Integer[]) masterDegreeThesisForm.get(teachersNumbersListField);
 
-		ArrayList infoTeachersList = new ArrayList();
+        ArrayList infoTeachersList = new ArrayList();
 
-		InfoTeacher infoTeacher = null;
-		Object args[] = new Object[1];
-		Integer teacherNumber = null;
+        InfoTeacher infoTeacher = null;
+        Object args[] = new Object[1];
+        Integer teacherNumber = null;
 
-		for (int i = 0; i < teachersNumbersList.length; i++) {
+        for (int i = 0; i < teachersNumbersList.length; i++)
+        {
 
-			teacherNumber = (Integer) teachersNumbersList[i];
+            teacherNumber = teachersNumbersList[i];
 
-			if (teacherNumber.intValue() == 0)
-				continue;
+            if (teacherNumber.intValue() == 0)
+                continue;
 
-			args[0] = teacherNumber;
+            args[0] = teacherNumber;
 
-			try {
-				infoTeacher = (InfoTeacher) ServiceUtils.executeService(userView, "ReadTeacherByNumber", args);
-			} catch (FenixServiceException e) {
-				throw new FenixActionException(e);
-			}
+            try
+            {
+                infoTeacher =
+                    (InfoTeacher) ServiceUtils.executeService(userView, "ReadTeacherByNumber", args);
+            } catch (FenixServiceException e)
+            {
+                throw new FenixActionException(e);
+            }
 
-			if (infoTeacher == null)
-				actionErrors.add("error.no.teacher.in.database", new ActionError("error.no.teacher.in.database", teacherNumber.toString()));
-			else if (infoTeachersList.contains(infoTeacher) == false) {
-				infoTeachersList.add(infoTeacher);
-			}
+            if (infoTeacher == null)
+                actionErrors.add(
+                    "error.no.teacher.in.database",
+                    new ActionError("error.no.teacher.in.database", teacherNumber.toString()));
+            else if (infoTeachersList.contains(infoTeacher) == false)
+            {
+                infoTeachersList.add(infoTeacher);
+            }
 
-		}
+        }
 
-		if (infoTeachersList.isEmpty() == false)
-			request.setAttribute(sessionConstant, infoTeachersList);
+        if (infoTeachersList.isEmpty() == false)
+            request.setAttribute(sessionConstant, infoTeachersList);
 
-		return infoTeachersList;
+        return infoTeachersList;
 
-	}
+    }
 
-	public void getExternalPersonsByName(ActionForm form, HttpServletRequest request, String sessionConstant, ActionErrors actionErrors)
-		throws FenixActionException {
+    public void getExternalPersonsByName(
+        ActionForm form,
+        HttpServletRequest request,
+        String sessionConstant,
+        ActionErrors actionErrors)
+        throws FenixActionException
+    {
 
-		DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
-		IUserView userView = SessionUtils.getUserView(request);
+        DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
+        IUserView userView = SessionUtils.getUserView(request);
 
-		String externalAssistentGuiderName = (String) masterDegreeThesisForm.get("externalAssistentGuiderName");
+        String externalAssistentGuiderName =
+            (String) masterDegreeThesisForm.get("externalAssistentGuiderName");
 
-		ArrayList infoExternalPersonsList = null;
-		Object args[] = { externalAssistentGuiderName };
+        ArrayList infoExternalPersonsList = null;
+        Object args[] = { externalAssistentGuiderName };
 
-		try {
-			if (!externalAssistentGuiderName.equals(""))
-				infoExternalPersonsList = (ArrayList) ServiceUtils.executeService(userView, "SearchExternalPersonsByName", args);
-		} catch (FenixServiceException e) {
-			throw new FenixActionException(e);
-		}
+        try
+        {
+            if (!externalAssistentGuiderName.equals(""))
+                infoExternalPersonsList =
+                    (ArrayList) ServiceUtils.executeService(
+                        userView,
+                        "SearchExternalPersonsByName",
+                        args);
+        } catch (FenixServiceException e)
+        {
+            throw new FenixActionException(e);
+        }
 
-		if (infoExternalPersonsList != null)
-			if (infoExternalPersonsList.isEmpty() == false)
-				request.setAttribute(sessionConstant, infoExternalPersonsList);
+        if (infoExternalPersonsList != null)
+            if (infoExternalPersonsList.isEmpty() == false)
+                request.setAttribute(sessionConstant, infoExternalPersonsList);
 
+        if ((infoExternalPersonsList == null) || (infoExternalPersonsList.isEmpty()))
+            actionErrors.add(
+                "label.masterDegree.administrativeOffice.searchResultsEmpty",
+                new ActionError("label.masterDegree.administrativeOffice.searchResultsEmpty"));
 
-		if ((infoExternalPersonsList == null) || (infoExternalPersonsList.isEmpty()))
-				actionErrors.add("label.masterDegree.administrativeOffice.searchResultsEmpty", new ActionError("label.masterDegree.administrativeOffice.searchResultsEmpty"));
+        request.setAttribute(
+            SessionConstants.EXTERNAL_ASSISTENT_GUIDERS_SEARCH_RESULTS_FLAG,
+            new Boolean(true));
 
-		request.setAttribute(SessionConstants.EXTERNAL_ASSISTENT_GUIDERS_SEARCH_RESULTS_FLAG, new Boolean(true));
+    }
 
-	}
+    public ArrayList getExternalPersonsByIDs(
+        ActionForm form,
+        HttpServletRequest request,
+        String externalPersonsIDsListField,
+        String sessionConstant,
+        ActionErrors actionErrors)
+        throws FenixActionException
+    {
 
-	public ArrayList getExternalPersonsByIDs(
-		ActionForm form,
-		HttpServletRequest request,
-		String externalPersonsIDsListField,
-		String sessionConstant,
-		ActionErrors actionErrors)
-		throws FenixActionException {
+        DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
+        IUserView userView = SessionUtils.getUserView(request);
 
-		DynaActionForm masterDegreeThesisForm = (DynaActionForm) form;
-		IUserView userView = SessionUtils.getUserView(request);
+        Integer[] externalPersonsIDsList =
+            (Integer[]) masterDegreeThesisForm.get(externalPersonsIDsListField);
 
-		Integer[] externalPersonsIDsList = (Integer[]) masterDegreeThesisForm.get(externalPersonsIDsListField);
+        ArrayList infoExternalPersonsList = new ArrayList();
 
-		ArrayList infoExternalPersonsList = new ArrayList();
+        InfoExternalPerson infoExternalPerson = null;
+        Object args[] = new Object[1];
+        Integer externalPersonID = null;
 
-		InfoExternalPerson infoExternalPerson = null;
-		Object args[] = new Object[1];
-		Integer externalPersonID = null;
+        for (int i = 0; i < externalPersonsIDsList.length; i++)
+        {
 
-		for (int i = 0; i < externalPersonsIDsList.length; i++) {
+            externalPersonID = externalPersonsIDsList[i];
+            args[0] = externalPersonID;
 
-			externalPersonID = (Integer) externalPersonsIDsList[i];
-			args[0] = externalPersonID;
+            try
+            {
+                infoExternalPerson =
+                    (InfoExternalPerson) ServiceUtils.executeService(
+                        userView,
+                        "ReadExternalPersonByID",
+                        args);
+            } catch (NonExistingServiceException e)
+            {
+                actionErrors.add(
+                    e.getMessage(),
+                    new ActionError(e.getMessage(), externalPersonID.toString()));
+                continue;
+            } catch (FenixServiceException e)
+            {
+                throw new FenixActionException(e);
+            }
 
-			try {
-				infoExternalPerson = (InfoExternalPerson) ServiceUtils.executeService(userView, "ReadExternalPersonByID", args);
-			} catch (NonExistingServiceException e) {
-				actionErrors.add(e.getMessage(), new ActionError(e.getMessage(), externalPersonID.toString()));
-				continue;
-			} catch (FenixServiceException e) {
-				throw new FenixActionException(e);
-			}
+            if (infoExternalPersonsList.contains(infoExternalPerson) == false)
+            {
+                infoExternalPersonsList.add(infoExternalPerson);
+            }
 
-			if (infoExternalPersonsList.contains(infoExternalPerson) == false) {
-				infoExternalPersonsList.add(infoExternalPerson);
-			}
+        }
 
-		}
+        if (infoExternalPersonsList.isEmpty() == false)
+            request.setAttribute(sessionConstant, infoExternalPersonsList);
 
-		if (infoExternalPersonsList.isEmpty() == false)
-			request.setAttribute(sessionConstant, infoExternalPersonsList);
+        return infoExternalPersonsList;
 
-		return infoExternalPersonsList;
+    }
 
-	}
-
-	private String getFromRequest(String parameter, HttpServletRequest request) {
-		String parameterString = request.getParameter(parameter);
-		if (parameterString == null) {
-			parameterString = (String) request.getAttribute(parameter);
-		}
-		return parameterString;
-	}
+    private String getFromRequest(String parameter, HttpServletRequest request)
+    {
+        String parameterString = request.getParameter(parameter);
+        if (parameterString == null)
+        {
+            parameterString = (String) request.getAttribute(parameter);
+        }
+        return parameterString;
+    }
 
 }
