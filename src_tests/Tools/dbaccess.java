0653704e130bb27ab1466fb2dd476a259f5b9030
João Mota <joao.mota@ist.utl.pt>
2003-11-18 19:42:04 +0000

    *** empty log message ***

--- src_tests/Tools/dbaccess.java
+++ src_tests/Tools/dbaccess.java
@@ -23,95 +23,97 @@ import org.dbunit.operation.DatabaseOperation;
 
 public class dbaccess
 {
-	private IDatabaseConnection _connection = null;
-	private String dbName;
-	private String username;
-	private String password;
+    private IDatabaseConnection _connection = null;
+    private String dbName;
+    private String username;
+    private String password;
 
-	public dbaccess()
-	{}
+    public dbaccess()
+    {
+    }
 
-	public void openConnection() throws Exception
-	{
-		if (_connection == null)
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection jdbcConnection = DriverManager.getConnection(this.getDbName(), this.getUsername(), this.getPassword());
-			_connection = new DatabaseConnection(jdbcConnection);
-		}
-	}
+    public void openConnection() throws Exception
+    {
+        if (_connection == null)
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection jdbcConnection =
+                DriverManager.getConnection(this.getDbName(), this.getUsername(), this.getPassword());
+            _connection = new DatabaseConnection(jdbcConnection);
+        }
+    }
 
-	public void closeConnection() throws Exception
-	{
-		_connection.close();
-		_connection = null;
-	}
+    public void closeConnection() throws Exception
+    {
+        _connection.close();
+        _connection = null;
+    }
 
-	public void backUpDataBaseContents(String filename) throws Exception
-	{
-		IDataSet fullDataSet = _connection.createDataSet();
-		FileWriter fileWriter = new FileWriter(new File(filename));
-		FlatXmlDataSet.write(fullDataSet, fileWriter, "ISO-8859-1");
-	}
+    public void backUpDataBaseContents(String filename) throws Exception
+    {
+        IDataSet fullDataSet = _connection.createDataSet();
+        FileWriter fileWriter = new FileWriter(new File(filename));
+        FlatXmlDataSet.write(fullDataSet, fileWriter, "ISO-8859-1");
+    }
 
-	public void loadDataBase(String filename) throws Exception
-	{
-		FileReader fileReader = new FileReader(new File(filename));
-		IDataSet dataSet = new FlatXmlDataSet(fileReader);
-		DatabaseOperation.CLEAN_INSERT.execute(_connection, dataSet);
-	}
+    public void loadDataBase(String filename) throws Exception
+    {
+        FileReader fileReader = new FileReader(new File(filename));
+        IDataSet dataSet = new FlatXmlDataSet(fileReader);
+        DatabaseOperation.CLEAN_INSERT.execute(_connection, dataSet);
+    }
 
-	public IDatabaseConnection getConnection()
-	{
-		return _connection;
-	}
+    public IDatabaseConnection getConnection()
+    {
+        return _connection;
+    }
 
-	public String getDbName()
-	{
-		if (this.dbName == null)
-		{
-			return "jdbc:mysql://localhost/ciapl";
-		} else
-		{
-			return "jdbc:mysql://localhost/" + this.dbName;
-		}
-	}
+    public String getDbName()
+    {
+        if (this.dbName == null)
+        {
+            return "jdbc:mysql://localhost/ciapl";
+        } else
+        {
+            return "jdbc:mysql://localhost/" + this.dbName;
+        }
+    }
 
-	public void setDbName(String dbName)
-	{
-		this.dbName = dbName;
-	}
+    public void setDbName(String dbName)
+    {
+        this.dbName = dbName;
+    }
 
-	public String getPassword()
-	{
-		if (this.password == null)
-		{
-			return "";
-		} else
-		{
-			return this.password;
-		}
-	}
+    public String getPassword()
+    {
+        if (this.password == null)
+        {
+            return "";
+        } else
+        {
+            return this.password;
+        }
+    }
 
-	public void setPassword(String password)
-	{
-		this.password = password;
-	}
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 
-	public String getUsername()
-	{
-		if (this.username == null)
-		{
-			return "root";
-		} else
-		{
-			return this.username;
-		}
-	}
+    public String getUsername()
+    {
+        if (this.username == null)
+        {
+            return "root";
+        } else
+        {
+            return this.username;
+        }
+    }
 
-	public void setUsername(String username)
-	{
-		this.username = username;
-	}
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
 
 }
