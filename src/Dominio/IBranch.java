aa537d97fa7a2e1dba6b1e8f6fbc54f61e8fa3d0
Nuno Correia <nrmc@mega.ist.utl.pt>
2003-11-20 12:16:59 +0000

    Added get and set for Acronym field

--- src/Dominio/IBranch.java
+++ src/Dominio/IBranch.java
@@ -15,9 +15,11 @@ public interface IBranch extends IDomainObject {
 	public String getName();
 	public List getScopes();
 	public IDegreeCurricularPlan getDegreeCurricularPlan();
+	public String getAcronym();
 
 	public void setCode(String code);
 	public void setName(String name);
 	public void setScopes(List scopes);
 	public void setDegreeCurricularPlan(IDegreeCurricularPlan degreeCurricularPlan);
+	public void setAcronym(String acronym);
 }
