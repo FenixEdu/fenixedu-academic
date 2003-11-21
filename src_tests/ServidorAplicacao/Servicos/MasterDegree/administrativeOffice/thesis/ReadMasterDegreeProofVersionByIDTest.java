51cc5e3b974d6e8428be81b08161b59a37d6e838
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2003-11-21 17:54:04 +0000

    initial version

---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/thesis/ReadMasterDegreeProofVersionByIDTest.java
@@ -0,0 +1,120 @@
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.thesis;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import DataBeans.InfoMasterDegreeProofVersion;
+import DataBeans.InfoTeacher;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servico.exceptions.NonExistingServiceException;
+import ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.AdministrativeOfficeBaseTest;
+import Util.MasterDegreeClassification;
+import Util.State;
+
+/**
+ * @author - Shezad Anavarali (sana@mega.ist.utl.pt) - Nadir Tarmahomed
+ *               (naat@mega.ist.utl.pt)
+ */
+public class ReadMasterDegreeProofVersionByIDTest extends AdministrativeOfficeBaseTest
+{
+
+    /**
+     * @param testName
+     */
+    public ReadMasterDegreeProofVersionByIDTest(String testName)
+    {
+        super(testName);
+        this.dataSetFilePath =
+            "etc/datasets/servicos/MasterDegree/administrativeOffice/thesis/testReadMasterDegreeProofVersionByIDDataSet.xml";
+    }
+
+    protected String getNameOfServiceToBeTested()
+    {
+        return "ReadMasterDegreeProofVersionByID";
+    }
+
+    protected Object[] getServiceArgumentsForNotAuthenticatedUser() throws FenixServiceException
+    {
+        Object[] argsReadMasterDegreeProofVersion = { null };
+
+        return argsReadMasterDegreeProofVersion;
+    }
+
+    protected Object[] getServiceArgumentsForNotAuthorizedUser() throws FenixServiceException
+    {
+        Object[] argsReadMasterDegreeProofVersion = { new Integer(1)};
+
+        return argsReadMasterDegreeProofVersion;
+    }
+
+    public void testReadExistingMasterDegreeProofVersion()
+    {
+        try
+        {
+            Object[] argsReadMasterDegreeProofVersion = { new Integer(1)};
+            InfoMasterDegreeProofVersion infoMasterDegreeProofVersion =
+                (InfoMasterDegreeProofVersion) serviceManager.executar(
+                    userView,
+                    getNameOfServiceToBeTested(),
+                    argsReadMasterDegreeProofVersion);
+
+            assertEquals(infoMasterDegreeProofVersion.getIdInternal(), new Integer(1));
+            assertEquals(
+                infoMasterDegreeProofVersion.getInfoMasterDegreeThesis().getIdInternal(),
+                new Integer(1));
+            assertEquals(
+                infoMasterDegreeProofVersion.getInfoResponsibleEmployee().getIdInternal(),
+                new Integer(1194));
+            assertEquals(infoMasterDegreeProofVersion.getCurrentState(), new State(State.ACTIVE));
+            assertEquals(infoMasterDegreeProofVersion.getAttachedCopiesNumber(), new Integer(5));
+            assertEquals(
+                infoMasterDegreeProofVersion.getFinalResult(),
+                MasterDegreeClassification.APPROVED);
+            Date proofDate = new GregorianCalendar(2003, Calendar.OCTOBER, 10).getTime();
+            assertEquals(infoMasterDegreeProofVersion.getProofDate(), proofDate);
+            Date thesisDeliveryDate = new GregorianCalendar(2003, Calendar.NOVEMBER, 11).getTime();
+            assertEquals(infoMasterDegreeProofVersion.getThesisDeliveryDate(), thesisDeliveryDate);
+            assertEquals(
+                ((InfoTeacher) infoMasterDegreeProofVersion.getInfoJuries().get(0)).getIdInternal(),
+                new Integer(954));
+
+            //ok
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            fail("testReadExistingMasterDegreeProofVersion " + ex.getMessage());
+        }
+
+    }
+
+    public void testReadNonExistingMasterDegreeProofVersion()
+    {
+        try
+        {
+            Object[] argsReadMasterDegreeProofVersion = { new Integer(50)};
+
+            serviceManager.executar(
+                userView,
+                getNameOfServiceToBeTested(),
+                argsReadMasterDegreeProofVersion);
+
+            fail("testReadNonExistingMasterDegreeProofVersion did not throw NonExistingServiceException");
+
+        }
+        catch (NonExistingServiceException ex)
+        {
+            //ok
+
+        }
+        catch (FenixServiceException e)
+        {
+            e.printStackTrace();
+            fail("testReadNonExistingMasterDegreeProofVersion " + e.getMessage());
+        }
+
+    }
+
+}
---
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/thesis/ReadMasterDegreeThesisDataVersionByIDTest.java
@@ -0,0 +1,117 @@
+package ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.thesis;
+
+import java.util.List;
+
+import DataBeans.InfoExternalPerson;
+import DataBeans.InfoMasterDegreeThesisDataVersion;
+import DataBeans.InfoTeacher;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servico.exceptions.NonExistingServiceException;
+import ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.AdministrativeOfficeBaseTest;
+import Util.State;
+
+/**
+ * 
+ * @author
+ *   - Shezad Anavarali (sana@mega.ist.utl.pt)
+ *   - Nadir Tarmahomed (naat@mega.ist.utl.pt)
+ */
+public class ReadMasterDegreeThesisDataVersionByIDTest extends AdministrativeOfficeBaseTest
+{
+
+    /**
+     * @param testName
+     */
+    public ReadMasterDegreeThesisDataVersionByIDTest(String testName)
+    {
+        super(testName);
+        this.dataSetFilePath =
+            "etc/datasets/servicos/MasterDegree/administrativeOffice/thesis/testReadMasterDegreeThesisDataVersionByIDDataSet.xml";
+    }
+
+    protected String getNameOfServiceToBeTested()
+    {
+        return "ReadMasterDegreeThesisDataVersionByID";
+    }
+
+    protected Object[] getServiceArgumentsForNotAuthenticatedUser() throws FenixServiceException
+    {
+        Object[] argsReadMasterDegreeThesisDataVersion = { null };
+
+        return argsReadMasterDegreeThesisDataVersion;
+    }
+
+    protected Object[] getServiceArgumentsForNotAuthorizedUser() throws FenixServiceException
+    {
+        Object[] argsReadMasterDegreeThesisDataVersion = { new Integer(1)};
+
+        return argsReadMasterDegreeThesisDataVersion;
+    }
+
+    public void testReadExistingMasterDegreeThesisDataVersion()
+    {
+        try
+        {
+            Object[] argsReadMasterDegreeThesisDataVersion = { new Integer(1)};
+            InfoMasterDegreeThesisDataVersion infoMasterDegreeThesisDataVersion =
+                (InfoMasterDegreeThesisDataVersion) serviceManager.executar(
+                    userView,
+                    getNameOfServiceToBeTested(),
+                    argsReadMasterDegreeThesisDataVersion);
+
+            assertEquals(infoMasterDegreeThesisDataVersion.getIdInternal(), new Integer(1));
+            assertEquals(
+                infoMasterDegreeThesisDataVersion.getInfoMasterDegreeThesis().getIdInternal(),
+                new Integer(1));
+            assertEquals(
+                infoMasterDegreeThesisDataVersion.getInfoResponsibleEmployee().getIdInternal(),
+                new Integer(1194));
+            assertEquals(infoMasterDegreeThesisDataVersion.getDissertationTitle(), "some title");
+            assertEquals(infoMasterDegreeThesisDataVersion.getCurrentState(), new State(State.ACTIVE));
+            List infoGuiders = infoMasterDegreeThesisDataVersion.getInfoGuiders();
+            List infoAssistentGuiders = infoMasterDegreeThesisDataVersion.getInfoAssistentGuiders();
+            List infoExternalAssistentGuiders =
+                infoMasterDegreeThesisDataVersion.getInfoExternalAssistentGuiders();
+            assertEquals(((InfoTeacher) infoGuiders.get(0)).getIdInternal(), new Integer(954));
+            assertEquals(((InfoTeacher) infoAssistentGuiders.get(0)).getIdInternal(), new Integer(955));
+            assertEquals(
+                ((InfoExternalPerson) infoExternalAssistentGuiders.get(0)).getIdInternal(),
+                new Integer(1));
+            //ok
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            fail("testReadExistingMasterDegreeThesisDataVersion " + ex.getMessage());
+        }
+
+    }
+    public void testReadNonExistentMasterDegreeThesisDataVersion()
+    {
+        try
+        {
+            Object[] argsReadMasterDegreeThesisDataVersion = { new Integer(65456) };
+            InfoMasterDegreeThesisDataVersion infoMasterDegreeThesisDataVersion =
+                (InfoMasterDegreeThesisDataVersion) serviceManager.executar(
+                    userView,
+                    getNameOfServiceToBeTested(),
+                    argsReadMasterDegreeThesisDataVersion);
+
+            fail("testReadNonExistentMasterDegreeThesisDataVersion did not throw NonExistingServiceException");
+
+        }
+        catch (NonExistingServiceException ex)
+        {
+            //ok
+
+        }
+        catch (FenixServiceException e)
+        {
+            e.printStackTrace();
+            fail("testReadNonExistentMasterDegreeThesisDataVersion " + e.getMessage());
+        }
+
+    }
+
+}
