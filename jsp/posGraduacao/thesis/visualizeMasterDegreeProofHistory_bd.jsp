71fbaff9c6eecb57916fc44bd7674bbaf709b1f7
Shezad Anavarali <shezad@ist.utl.pt>
2003-11-21 18:16:58 +0000

    initial version

---
+++ jsp/posGraduacao/thesis/visualizeMasterDegreeProofHistory_bd.jsp
@@ -0,0 +1,107 @@
+<%@ page language="java" %>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
+<%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
+<%@ page import="ServidorApresentacao.Action.sop.utils.SessionConstants" %>
+<%@ page import="DataBeans.InfoStudent" %>
+<%@ page import="DataBeans.InfoTeacher" %>
+
+
+<bean:define id="student" name="<%= SessionConstants.STUDENT %>" scope="request"/>
+<bean:define id="finalResult" name="<%= SessionConstants.FINAL_RESULT %>" />
+<bean:define id="attachedCopiesNumber" name="<%= SessionConstants.ATTACHED_COPIES_NUMBER %>" />
+<bean:define id="proofDate" name="<%= SessionConstants.PROOF_DATE %>" />
+<bean:define id="thesisDeliveryDate" name="<%= SessionConstants.THESIS_DELIVERY_DATE %>" />
+<bean:define id="responsibleEmployee" name="<%= SessionConstants.RESPONSIBLE_EMPLOYEE %>" scope="request"/>
+<bean:define id="lastModification" name="<%= SessionConstants.LAST_MODIFICATION %>" scope="request"/>
+
+			
+<h2 align="center">
+	<bean:message key="title.masterDegree.administrativeOffice.thesis.visualizeProofHistory"/> - 
+	<bean:write name="lastModification" />
+</h2>
+<h2 align="center">
+	<bean:message key="label.masterDegree.administrativeOffice.modifiedBy"/>: 
+	<bean:write name="responsibleEmployee" property="person.nome" />
+</h2>	
+		
+<center>
+<span class="error"><html:errors/></span>
+
+	<br/>
+
+	<table border="0" width="100%" cellspacing="3" cellpadding="10">
+		<tr>
+			<th align="left"><bean:message key="label.masterDegree.administrativeOffice.studentNumber"/></th>
+			<th align="left"><bean:message key="label.masterDegree.administrativeOffice.studentName"/></th>
+		</tr>
+	
+		<tr>
+			<td align="left">
+				<bean:write name="student" property="number"/>
+			</td>
+			<td align="left">
+				<bean:write name="student" property="infoPerson.nome"/>
+			</td>			
+		</tr>
+		<tr> 
+			<td>&nbsp;</td>
+		</tr>
+		
+		<!-- Proof Date -->
+		<tr >
+			<th align="left" ><bean:message key="label.masterDegree.administrativeOffice.proofDate"/>:&nbsp;</th>
+			<td><bean:write name="proofDate"/></td>
+		</tr>
+		
+		<!-- Thesis Delivery Date -->
+		<tr >
+			<th align="left" ><bean:message key="label.masterDegree.administrativeOffice.thesisDeliveryDate"/>:&nbsp;</th>
+			<td><bean:write name="thesisDeliveryDate"/></td>
+		</tr>
+			
+	
+	<!-- Final Result -->
+		<tr>	
+			<th align="left" >
+				<bean:message key="label.masterDegree.administrativeOffice.finalResult"/>:&nbsp;
+			</th>
+			<td><bean:write name="finalResult"/></td>
+		</tr>
+					
+		
+		<!-- Attached Copies Number -->
+		<tr>
+			<th align="left" >
+				<bean:message key="label.masterDegree.administrativeOffice.attachedCopiesNumber"/>:&nbsp;
+			</th>
+			<td><bean:write name="attachedCopiesNumber"/></td>
+		</tr>
+		<tr> 
+			<td>&nbsp;</td>
+		</tr>
+				
+		<!-- Juries -->
+		<tr>
+			<th align="left" colspan="2"><bean:message key="label.masterDegree.administrativeOffice.juries"/></th>				
+		</tr>
+			<logic:present name="<%= SessionConstants.JURIES_LIST %>" scope="request">
+				<bean:define id="juriesList" name="<%= SessionConstants.JURIES_LIST %>" type="java.util.List"/>
+				<tr>
+					<th align="left"><bean:message key="label.masterDegree.administrativeOffice.teacherNumber"/></th>
+					<th align="left"><bean:message key="label.masterDegree.administrativeOffice.teacherName"/></th>				
+				</tr>					
+				<logic:iterate id="jury" name="juriesList">
+					<tr>
+						<td align="left" ><bean:write name="jury" property="teacherNumber"/></td>
+						<td align="left"><bean:write name="jury" property="infoPerson.nome"/></td>					
+					</tr>				
+				</logic:iterate>
+				<tr> 
+					<td>&nbsp;</td>
+				</tr>
+			</logic:present >				
+		
+	</table>
+
+</center>
---
+++ src/ServidorApresentacao/Action/masterDegree/administrativeOffice/thesis/VisualizeMasterDegreeThesisHistoryDispatchAction.java
@@ -0,0 +1,119 @@
+package ServidorApresentacao.Action.masterDegree.administrativeOffice.thesis;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+import DataBeans.InfoMasterDegreeThesisDataVersion;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servico.exceptions.NonExistingServiceException;
+import ServidorApresentacao.Action.exceptions.FenixActionException;
+import ServidorApresentacao.Action.exceptions.NonExistingActionException;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
+import ServidorApresentacao.Action.sop.utils.SessionConstants;
+import ServidorApresentacao.Action.sop.utils.SessionUtils;
+
+/**
+ * 
+ * @author :
+ *   - Shezad Anavarali (sana@mega.ist.utl.pt)
+ *   - Nadir Tarmahomed (naat@mega.ist.utl.pt)
+ *
+ */
+
+public class VisualizeMasterDegreeThesisHistoryDispatchAction extends DispatchAction
+{
+
+	public ActionForward getStudentAndMasterDegreeThesisDataVersion(
+		ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws Exception
+	{
+
+		IUserView userView = SessionUtils.getUserView(request);
+
+		//Integer degreeType = (Integer) request.getAttribute("degreeType");
+		//Integer studentNumber = (Integer) request.getAttribute("studentNumber");
+		Integer degreeType = Integer.valueOf(request.getParameter("degreeType"));
+		Integer studentNumber = Integer.valueOf(request.getParameter("studentNumber"));
+		Integer masterDegreeThesisDataVersionID = Integer.valueOf(request.getParameter("masterDegreeThesisDataVersionID"));
+
+		MasterDegreeThesisOperations operations = new MasterDegreeThesisOperations();
+		ActionErrors actionErrors = new ActionErrors();
+		boolean isSuccess = operations.getStudentByNumberAndDegreeType(form, request, actionErrors);
+
+		if (isSuccess == false)
+		{
+			throw new NonExistingActionException(
+				"error.exception.masterDegree.nonExistentStudent",
+				mapping.findForward("error"));
+
+		}
+
+		InfoMasterDegreeThesisDataVersion infoMasterDegreeThesisDataVersion = null;
+
+		/* * * get master degree thesis history data * * */
+		Object argsMasterDegreeThesisDataVersion[] = { masterDegreeThesisDataVersionID };
+		try
+		{
+			infoMasterDegreeThesisDataVersion =
+				(InfoMasterDegreeThesisDataVersion) ServiceUtils.executeService(
+					userView,
+					"ReadMasterDegreeThesisDataVersionByID",
+					argsMasterDegreeThesisDataVersion);
+		} catch (NonExistingServiceException e)
+		{
+			throw new NonExistingActionException(
+				"error.exception.masterDegree.nonExistingMasterDegreeThesis",
+				mapping.findForward("error"));
+
+		} catch (FenixServiceException e)
+		{
+			throw new FenixActionException(e);
+		}
+
+
+		if (infoMasterDegreeThesisDataVersion.getInfoGuiders().isEmpty() == false)
+			request.setAttribute(
+				SessionConstants.GUIDERS_LIST,
+				infoMasterDegreeThesisDataVersion.getInfoGuiders());
+
+		if (infoMasterDegreeThesisDataVersion.getInfoAssistentGuiders().isEmpty() == false)
+			request.setAttribute(
+				SessionConstants.ASSISTENT_GUIDERS_LIST,
+				infoMasterDegreeThesisDataVersion.getInfoAssistentGuiders());
+
+		if (infoMasterDegreeThesisDataVersion.getInfoExternalAssistentGuiders().isEmpty() == false)
+			request.setAttribute(
+				SessionConstants.EXTERNAL_ASSISTENT_GUIDERS_LIST,
+				infoMasterDegreeThesisDataVersion.getInfoExternalAssistentGuiders());
+
+		Date lastModification =
+			new Date(infoMasterDegreeThesisDataVersion.getLastModification().getTime());
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy k:mm:ss");
+		String formattedLastModification = simpleDateFormat.format(lastModification);
+
+		request.setAttribute(
+			SessionConstants.RESPONSIBLE_EMPLOYEE,
+			infoMasterDegreeThesisDataVersion.getInfoResponsibleEmployee());
+		request.setAttribute(SessionConstants.LAST_MODIFICATION, formattedLastModification);
+		request.setAttribute(
+			SessionConstants.DISSERTATION_TITLE,
+			infoMasterDegreeThesisDataVersion.getDissertationTitle());
+
+		return mapping.findForward("start");
+
+	}
+
+}
