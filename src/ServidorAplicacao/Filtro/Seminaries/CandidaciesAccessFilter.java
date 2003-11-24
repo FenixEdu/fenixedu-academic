0d6c9b603efef902a6d7e7e133a7b5e8700be051
João Luz <joao.luz@ist.utl.pt>
2003-11-24 15:52:22 +0000

    CTRL+SHIFT+O plus CTRL+SHIFT+F

--- src/ServidorAplicacao/Filtro/Seminaries/CandidaciesAccessFilter.java
+++ src/ServidorAplicacao/Filtro/Seminaries/CandidaciesAccessFilter.java
@@ -1,12 +1,9 @@
 /*
  * Created on 5/Set/2003, 16:22:14
- *
- *By Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
+ * 
+ * By Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
  */
 package ServidorAplicacao.Filtro.Seminaries;
-import java.util.Collection;
-import java.util.Iterator;
-import DataBeans.InfoRole;
 import ServidorAplicacao.IServico;
 import ServidorAplicacao.IUserView;
 import ServidorAplicacao.Filtro.AuthorizationUtils;
@@ -15,33 +12,33 @@ import ServidorAplicacao.Servico.exceptions.NotAuthorizedException;
 import Util.RoleType;
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
- *
+ * 
  * 
  * Created at 5/Set/2003, 16:22:14
- * 
+ *  
  */
 public class CandidaciesAccessFilter extends Filtro
 {
-	//  the singleton of this class
-	public final static CandidaciesAccessFilter filter= new CandidaciesAccessFilter();
-	public static CandidaciesAccessFilter getInstance()
-	{
-		return filter;
-	}
-	//  the singleton of this class
-	public void preFiltragem(IUserView id, IServico servico, Object[] argumentos) throws Exception
-	{
-		if (((id != null
-			&& id.getRoles() != null
-			&& !AuthorizationUtils.containsRole(id.getRoles(), getRoleType())))
-			|| (id == null)
-			|| (id.getRoles() == null))
-		{
-			throw new NotAuthorizedException();
-		}
-	}
-	private RoleType getRoleType()
-	{
-		return RoleType.SEMINARIES_COORDINATOR;
-	}
+    //  the singleton of this class
+    public final static CandidaciesAccessFilter filter = new CandidaciesAccessFilter();
+    public static CandidaciesAccessFilter getInstance()
+    {
+        return filter;
+    }
+    //  the singleton of this class
+    public void preFiltragem(IUserView id, IServico servico, Object[] argumentos) throws Exception
+    {
+        if (((id != null
+            && id.getRoles() != null
+            && !AuthorizationUtils.containsRole(id.getRoles(), getRoleType())))
+            || (id == null)
+            || (id.getRoles() == null))
+        {
+            throw new NotAuthorizedException();
+        }
+    }
+    private RoleType getRoleType()
+    {
+        return RoleType.SEMINARIES_COORDINATOR;
+    }
 }
