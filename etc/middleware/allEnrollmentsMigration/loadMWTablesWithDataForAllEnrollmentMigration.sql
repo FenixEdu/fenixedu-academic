5279ffaa1d2a1aced7751636ad5cf364bb487a42
David Santos <david.santos@ist.utl.pt>
2003-11-25 11:13:14 +0000

    *** empty log message ***

--- etc/middleware/allEnrollmentsMigration/loadMWTablesWithDataForAllEnrollmentMigration.sql
+++ etc/middleware/allEnrollmentsMigration/loadMWTablesWithDataForAllEnrollmentMigration.sql
@@ -8,15 +8,7 @@ select * from mw_CURRICULAR_COURSE_SCOPE_temp;
 
 -- drop table if exists mw_ALUNO;
 -- create table mw_ALUNO
--- select mwa.* from mw_ALUNO_temp mwa inner join mw_ENROLMENT mwe on mwa.number = mwe.number group by mwa.number;
-
-drop table if exists mw_ENROLMENT;
-create table mw_ENROLMENT
-select * from mw_ENROLMENT_temp use index (I2) where degreeCode = 11 and enrolmentYear <> 2003;
-
-drop table if exists mw_ALUNO;
-create table mw_ALUNO
-select * from mw_ALUNO_temp where degreeCode = 11;
+-- select mwa.* from mw_ALUNO_temp mwa inner join mw_ENROLMENT mwe on mwa.number = mwe.number;
 
 ----------------------------------------------------------------------------------------------------------------------
 -- Especificações do ficheiro 'curriculo.txt' que vem do Almeida:
