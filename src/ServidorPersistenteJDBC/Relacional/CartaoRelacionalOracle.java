bed2c492c62178c677eb99784f19689a4481802c
João Luz <joao.luz@ist.utl.pt>
2003-11-27 03:30:45 +0000

    removed finnaly warning code formatted

--- src/ServidorPersistenteJDBC/Relacional/CartaoRelacionalOracle.java
+++ src/ServidorPersistenteJDBC/Relacional/CartaoRelacionalOracle.java
@@ -10,172 +10,220 @@ import ServidorPersistenteJDBC.ICartaoPersistente;
 import ServidorPersistenteJDBC.SuportePersistenteOracle;
 
 /**
- *
- * @author  Fernanda Quitério e Tania Pousão
+ * @author Fernanda Quitério e Tania Pousão
  */
-public class CartaoRelacionalOracle implements ICartaoPersistente {
+public class CartaoRelacionalOracle implements ICartaoPersistente
+{
 
-	public boolean alterarCartao(Cartao cartao) {
-		boolean resultado = false;
-		return resultado;
-	} /* alterarCartao */
+    public boolean alterarCartao(Cartao cartao)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* alterarCartao */
 
-	public boolean apagarCartao(int codigoInterno) {
-		boolean resultado = false;
-		return resultado;
-	} /* apagarCartao */
+    public boolean apagarCartao(int codigoInterno)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* apagarCartao */
 
-	public boolean apagarCartaoPorNumero(int numCartao) {
-		boolean resultado = false;
-		return resultado;
-	} /* apagarCartaoPorNumero */
+    public boolean apagarCartaoPorNumero(int numCartao)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* apagarCartaoPorNumero */
 
-	public boolean apagarTodosCartoes() {
-		return false;
-	}
+    public boolean apagarTodosCartoes()
+    {
+        return false;
+    }
 
-	public Cartao cartaoAtribuido(int numCartao) {
-		Cartao cartao = null;
-		return cartao;
-	} /* cartaoAtribuido */
+    public Cartao cartaoAtribuido(int numCartao)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* cartaoAtribuido */
 
-	public ArrayList cartaoUtilizado(int numCartao, Timestamp dataInicio, Timestamp dataFim) {
-		ArrayList lista = null;
-		return lista;
-	} /* cartaoUtilizado */
+    public ArrayList cartaoUtilizado(int numCartao, Timestamp dataInicio, Timestamp dataFim)
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* cartaoUtilizado */
 
-	public boolean escreverCartao(Cartao cartao) {
-		boolean resultado = false;
-		return resultado;
-	} /* escreverCartao */
+    public boolean escreverCartao(Cartao cartao)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* escreverCartao */
 
-	public boolean escreverCartoes(ArrayList listaCartoes) {
-		boolean resultado = false;
-		return resultado;
-	} /* escreverCartoes */
+    public boolean escreverCartoes(ArrayList listaCartoes)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* escreverCartoes */
 
-	public boolean existeCartao(int numCartao) {
-		boolean resultado = false;
-		return resultado;
-	} /* existeCartao */
+    public boolean existeCartao(int numCartao)
+    {
+        boolean resultado = false;
+        return resultado;
+    } /* existeCartao */
 
-	public Cartao lerCartao(int codigoInterno) {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerCartao */
+    public Cartao lerCartao(int codigoInterno)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartao */
 
-	public Cartao lerCartaoActualFuncionario(int chaveFuncionario) {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerCartaoActualFuncionario */
+    public Cartao lerCartaoActualFuncionario(int chaveFuncionario)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoActualFuncionario */
 
-	public Cartao lerCartaoLivre() {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerCartaoLivre */
+    public Cartao lerCartaoLivre()
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoLivre */
 
-	public Cartao lerCartaoPorNumero(int numCartao) {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerCartaoPorNumero */
+    public Cartao lerCartaoPorNumero(int numCartao)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoPorNumero */
 
-	public Cartao lerCartaoPorNumero(int numCartao, Timestamp dataMarcacao) {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerCartaoPorNumero */
+    public Cartao lerCartaoPorNumero(int numCartao, Timestamp dataMarcacao)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoPorNumero */
 
-	public Cartao lerCartaoSubstitutoFuncionario(int chaveFuncionario) {
-		Cartao cartao = null;
-		return cartao;
-	} /*lerCartaoSubstitutoFuncionario*/
+    public Cartao lerCartaoSubstitutoFuncionario(int chaveFuncionario)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoSubstitutoFuncionario */
 
-	public Cartao lerCartaoSubstitutoFuncionario(int chaveFuncionario, Timestamp data) {
-		Cartao cartao = null;
-		return cartao;
-	} /*lerCartaoSubstitutoFuncionario*/
+    public Cartao lerCartaoSubstitutoFuncionario(int chaveFuncionario, Timestamp data)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerCartaoSubstitutoFuncionario */
 
-	public ArrayList lerCartoesFuncionario(int chaveFuncionario) {
-		ArrayList lista = null;
-		return lista;
-	} /* lerCartoesFuncionario */
+    public ArrayList lerCartoesFuncionario(int chaveFuncionario)
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* lerCartoesFuncionario */
 
-	public ArrayList lerCartoesFuncionarioComValidade(int chaveFuncionario, Timestamp dataInicio, Timestamp dataFim) {
-		ArrayList lista = null;
-		return lista;
-	} /* lerCartoesFuncionarioComValidade */
+    public ArrayList lerCartoesFuncionarioComValidade(
+        int chaveFuncionario,
+        Timestamp dataInicio,
+        Timestamp dataFim)
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* lerCartoesFuncionarioComValidade */
 
-	public ArrayList lerHistorialCartao(int numCartao) {
-		ArrayList lista = null;
-		return lista;
-	} /* lerHistorialCartao */
+    public ArrayList lerHistorialCartao(int numCartao)
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* lerHistorialCartao */
 
-	public ArrayList lerTodosCartoes() {
-		//ORACLE: acede à BD do teleponto para carregar os cartões
-		ArrayList listaCartoesTeleponto = null;
+    public ArrayList lerTodosCartoes()
+    {
+        //ORACLE: acede à BD do teleponto para carregar os cartões
+        ArrayList listaCartoesTeleponto = null;
 
-		try {
-			SuportePersistenteOracle.getInstance().iniciarTransaccao();
-			try {
-				PreparedStatement sql =
-					UtilRelacionalOracle.prepararComando(
-						"SELECT CARSUB.CARTNO_CART, EMPREGADO.EMP_NUM, CARSUB.CARSUB_DHINI, "
-							+ "CARSUB.CARSUB_DHFIM, CARSUB.CARSUB_WHO, CARSUB.CARSUB_WHEN FROM CARSUB, EMPREGADO "
-							+ "WHERE CARSUB.ENT_NUM = EMPREGADO.ENT_NUM ORDER BY CARSUB.CARSUB_SEQ");
+        try
+        {
+            SuportePersistenteOracle.getInstance().iniciarTransaccao();
+            try
+            {
+                PreparedStatement sql =
+                    UtilRelacionalOracle.prepararComando(
+                        "SELECT CARSUB.CARTNO_CART, EMPREGADO.EMP_NUM, CARSUB.CARSUB_DHINI, "
+                            + "CARSUB.CARSUB_DHFIM, CARSUB.CARSUB_WHO, CARSUB.CARSUB_WHEN FROM CARSUB, EMPREGADO "
+                            + "WHERE CARSUB.ENT_NUM = EMPREGADO.ENT_NUM ORDER BY CARSUB.CARSUB_SEQ");
 
-				ResultSet resultado = sql.executeQuery();
-				listaCartoesTeleponto = new ArrayList();
-				while (resultado.next()) {
-					listaCartoesTeleponto.add(new Cartao(resultado.getInt("CARTNO_CART"), resultado.getInt("EMP_NUM"),
-					// só pode levar o numero de funcionario, e no servico vamos descobrir qual a chave do funcionario
-					Timestamp.valueOf(resultado.getString("CARSUB_DHINI")),
-						Timestamp.valueOf(resultado.getString("CARSUB_DHFIM")),
-						resultado.getInt("CARSUB_WHO"),
-						Timestamp.valueOf(resultado.getString("CARSUB_WHEN"))));
-				}
-				sql.close();
+                ResultSet resultado = sql.executeQuery();
+                listaCartoesTeleponto = new ArrayList();
+                while (resultado.next())
+                {
+                    listaCartoesTeleponto
+                        .add(new Cartao(resultado.getInt("CARTNO_CART"), resultado.getInt("EMP_NUM"),
+                    // só pode levar o numero de funcionario, e no servico vamos descobrir qual a chave
+					// do funcionario
+                    Timestamp.valueOf(resultado.getString("CARSUB_DHINI")),
+                        Timestamp.valueOf(resultado.getString("CARSUB_DHFIM")),
+                        resultado.getInt("CARSUB_WHO"),
+                        Timestamp.valueOf(resultado.getString("CARSUB_WHEN"))));
+                }
+                sql.close();
 
-			} catch (Exception e) {
-				SuportePersistenteOracle.getInstance().cancelarTransaccao();
-				System.out.println("CartaoRelacionalOracle.lerTodosCartoes: " + e.toString());
-				return null;
-			}
-			SuportePersistenteOracle.getInstance().confirmarTransaccao();
-		} catch (Exception e) {
-			System.out.println("CartaoRelacionalOracle.lerTodosCartoes: " + e.toString());
-			return null;
-		} finally {
-			return listaCartoesTeleponto;
-		}
-	} /* lerTodosCartoes */
+            }
+            catch (Exception e)
+            {
+                SuportePersistenteOracle.getInstance().cancelarTransaccao();
+                System.out.println("CartaoRelacionalOracle.lerTodosCartoes: " + e.toString());
+                return null;
+            }
+            SuportePersistenteOracle.getInstance().confirmarTransaccao();
+        }
+        catch (Exception e)
+        {
+            System.out.println("CartaoRelacionalOracle.lerTodosCartoes: " + e.toString());
+            listaCartoesTeleponto = null;
+        }
+        return listaCartoesTeleponto;
+    } /* lerTodosCartoes */
 
-	public ArrayList lerTodosCartoesSubstitutos() {
-		ArrayList lista = null;
-		return lista;
-	} /* lerTodosCartoesSubstitutos */
+    public ArrayList lerTodosCartoesSubstitutos()
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* lerTodosCartoesSubstitutos */
 
-	public ArrayList lerTodosCartoesSubstitutosAtribuidos() {
-		ArrayList lista = null;
-		return lista;
-	} /* lerTodosCartoesSubstitutosAtribuidos */
+    public ArrayList lerTodosCartoesSubstitutosAtribuidos()
+    {
+        ArrayList lista = null;
+        return lista;
+    } /* lerTodosCartoesSubstitutosAtribuidos */
 
-	public Cartao lerUltimaUtilizacaoCartao(int numCartao, Timestamp dataMarcacao) {
-		Cartao cartao = null;
-		return cartao;
-	} /* lerUltimaUtilizacaoCartao */
+    public Cartao lerUltimaUtilizacaoCartao(int numCartao, Timestamp dataMarcacao)
+    {
+        Cartao cartao = null;
+        return cartao;
+    } /* lerUltimaUtilizacaoCartao */
 
-	public int ultimoCodigoInterno() {
-		return 0;
-	} /* ultimoCodigoInterno */
+    public int ultimoCodigoInterno()
+    {
+        return 0;
+    } /* ultimoCodigoInterno */
 
-	public Cartao lerCartaoSubstitutoFuncionario(int chaveFuncionario, Timestamp dataInicio, Timestamp dataFim) {
-		return null;
-	}
+    public Cartao lerCartaoSubstitutoFuncionario(
+        int chaveFuncionario,
+        Timestamp dataInicio,
+        Timestamp dataFim)
+    {
+        return null;
+    }
 
-	public ArrayList consultarCartao(ArrayList listaFuncionarios, ArrayList listaCartoes, Timestamp dataInicio, Timestamp dataFim) {
-		return null;
-	}
+    public ArrayList consultarCartao(
+        ArrayList listaFuncionarios,
+        ArrayList listaCartoes,
+        Timestamp dataInicio,
+        Timestamp dataFim)
+    {
+        return null;
+    }
 
-	public ArrayList lerCartoesSubstitutosFuncionario(int chaveFuncionario, Timestamp dataInicio, Timestamp dataFim) {
-		return null;
-	}
+    public ArrayList lerCartoesSubstitutosFuncionario(
+        int chaveFuncionario,
+        Timestamp dataInicio,
+        Timestamp dataFim)
+    {
+        return null;
+    }
 }
--- src/ServidorPersistenteJDBC/Relacional/CentroCustoRelacional.java
+++ src/ServidorPersistenteJDBC/Relacional/CentroCustoRelacional.java
@@ -7,141 +7,167 @@ import java.util.ArrayList;
 import Dominio.CentroCusto;
 import ServidorPersistenteJDBC.ICentroCustoPersistente;
 /**
- *
- * @author  Fernanda Quitério e Tania Pousão
+ * @author Fernanda Quitério e Tania Pousão
  */
-public class CentroCustoRelacional implements ICentroCustoPersistente {
+public class CentroCustoRelacional implements ICentroCustoPersistente
+{
 
-	public boolean alterarCentroCusto(CentroCusto centroCusto) {
-		boolean resultado = false;
+    public boolean alterarCentroCusto(CentroCusto centroCusto)
+    {
+        boolean resultado = false;
 
-		try {
-			PreparedStatement sql =
-				UtilRelacional.prepararComando(
-					"UPDATE ass_CENTRO_CUSTO SET "
-						+ "codigoInterno = ? , "
-						+ "sigla = ? , "
-						+ "departamento = ? ,"
-						+ "seccao1 = ? ," +
-							"seccao2 = ? "
-						+ "WHERE codigoInterno = ? ");
+        try
+        {
+            PreparedStatement sql =
+                UtilRelacional.prepararComando(
+                    "UPDATE ass_CENTRO_CUSTO SET "
+                        + "codigoInterno = ? , "
+                        + "sigla = ? , "
+                        + "departamento = ? ,"
+                        + "seccao1 = ? ,"
+                        + "seccao2 = ? "
+                        + "WHERE codigoInterno = ? ");
 
-			sql.setInt(1, centroCusto.getCodigoInterno());
-			sql.setString(2, centroCusto.getSigla());
-			sql.setString(3, centroCusto.getDepartamento());
-			sql.setString(4, centroCusto.getSeccao1());
-			sql.setString(5, centroCusto.getSeccao2());
-			sql.setInt(6, centroCusto.getCodigoInterno());
+            sql.setInt(1, centroCusto.getCodigoInterno());
+            sql.setString(2, centroCusto.getSigla());
+            sql.setString(3, centroCusto.getDepartamento());
+            sql.setString(4, centroCusto.getSeccao1());
+            sql.setString(5, centroCusto.getSeccao2());
+            sql.setInt(6, centroCusto.getCodigoInterno());
 
-			sql.executeUpdate();
-			sql.close();
-			resultado = true;
-		} catch (Exception e) {
-			System.out.println("CentroCustoRelacional.alterarCentroCusto: " + e.toString());
-		} finally {
-			return resultado;
-		}
-	} /* alterarCentroCusto */
+            sql.executeUpdate();
+            sql.close();
+            resultado = true;
+        }
+        catch (Exception e)
+        {
+            System.out.println("CentroCustoRelacional.alterarCentroCusto: " + e.toString());
+        }
+        return resultado;
+    } /* alterarCentroCusto */
 
-	public boolean escreverCentroCusto(CentroCusto centroCusto) {
-		boolean resultado = false;
-    
-		try {
-			PreparedStatement sql =
-			UtilRelacional.prepararComando("INSERT INTO ass_CENTRO_CUSTO VALUES (?, ?, ?, ?, ?)");
-      
-			sql.setInt(1, centroCusto.getCodigoInterno());
-			sql.setString(2, centroCusto.getSigla());
-			sql.setString(3, centroCusto.getDepartamento());
-			sql.setString(4, centroCusto.getSeccao1());
-			sql.setString(5, centroCusto.getSeccao2());
-      
-			sql.executeUpdate();
-			sql.close();
-			resultado = true;
-		}
-		catch(Exception e) {
-			System.out.println("CentroCustoRelacional.escreverCentroCusto: " + e.toString());
-		}
-		finally {
-			return resultado;
-		}
-	} /* escreverCentroCusto */
-	
-	public CentroCusto lerCentroCusto(String sigla) {
-		CentroCusto centroCusto = null;
+    public boolean escreverCentroCusto(CentroCusto centroCusto)
+    {
+        boolean resultado = false;
 
-		try {
-			PreparedStatement sql = UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO WHERE sigla = ?");
+        try
+        {
+            PreparedStatement sql =
+                UtilRelacional.prepararComando("INSERT INTO ass_CENTRO_CUSTO VALUES (?, ?, ?, ?, ?)");
 
-			sql.setString(1, sigla);
+            sql.setInt(1, centroCusto.getCodigoInterno());
+            sql.setString(2, centroCusto.getSigla());
+            sql.setString(3, centroCusto.getDepartamento());
+            sql.setString(4, centroCusto.getSeccao1());
+            sql.setString(5, centroCusto.getSeccao2());
 
-			ResultSet resultado = sql.executeQuery();
-	
-			if (resultado.next()) {
-				centroCusto =
-					new CentroCusto(
-						resultado.getInt("codigoInterno"),
-						resultado.getString("sigla"),
-						resultado.getString("departamento"),
-						resultado.getString("seccao1"),
-						resultado.getString("seccao2"));
-			}
-			sql.close();
-		} catch (Exception e) {
-			System.out.println("CentroCustoRelacional.lerCentroCusto: " + e.toString());
-		} finally {
-			return centroCusto;
-		}
-	} /* lerCentroCusto */
+            sql.executeUpdate();
+            sql.close();
+            resultado = true;
+        }
+        catch (Exception e)
+        {
+            System.out.println("CentroCustoRelacional.escreverCentroCusto: " + e.toString());
+        }
 
-	public CentroCusto lerCentroCusto(int codigoInterno) {
-		CentroCusto centroCusto = null;
+        return resultado;
 
-		try {
-			PreparedStatement sql = UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO WHERE codigoInterno = ?");
+    } /* escreverCentroCusto */
 
-			sql.setInt(1, codigoInterno);
+    public CentroCusto lerCentroCusto(String sigla)
+    {
+        CentroCusto centroCusto = null;
 
-			ResultSet resultado = sql.executeQuery();
-			if (resultado.next()) {
-				centroCusto = new CentroCusto(
-				resultado.getInt("codigoInterno"),
-				resultado.getString("sigla"),
-				resultado.getString("departamento"),
-				resultado.getString("seccao1"),
-				resultado.getString("seccao2"));
-			}
-			sql.close();
-		} catch (Exception e) {
-			System.out.println("CentroCustoRelacional.lerCentroCusto: " + e.toString());
-		} finally {
-			return centroCusto;
-		}
-	} /* lerCentroCusto */
-	
-	public ArrayList lerTodosCentrosCusto(){
-		ArrayList listaCentrosCusto = null;
-		
-		try{
-		PreparedStatement sql = UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO ORDER BY sigla");
-		ResultSet resultadoQuery = sql.executeQuery();
-		
-		listaCentrosCusto = new ArrayList();
-		while(resultadoQuery.next()){
-			listaCentrosCusto.add(new CentroCusto(
-				resultadoQuery.getInt("codigoInterno"),
-				resultadoQuery.getString("sigla"),
-				resultadoQuery.getString("departamento"),
-				resultadoQuery.getString("seccao1"),
-				resultadoQuery.getString("seccao2")));
-		}
-		sql.close();
-		} catch (Exception e){
-			e.printStackTrace();
-			System.out.println("CentroCustoRelacional.lerTodosCentrosCusto: " + e.toString());
-		} finally {
-			return listaCentrosCusto;
-		}
-	} /* lerTodosCentrosCusto */
+        try
+        {
+            PreparedStatement sql =
+                UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO WHERE sigla = ?");
+
+            sql.setString(1, sigla);
+
+            ResultSet resultado = sql.executeQuery();
+
+            if (resultado.next())
+            {
+                centroCusto =
+                    new CentroCusto(
+                        resultado.getInt("codigoInterno"),
+                        resultado.getString("sigla"),
+                        resultado.getString("departamento"),
+                        resultado.getString("seccao1"),
+                        resultado.getString("seccao2"));
+            }
+            sql.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("CentroCustoRelacional.lerCentroCusto: " + e.toString());
+        }
+        return centroCusto;
+
+    } /* lerCentroCusto */
+
+    public CentroCusto lerCentroCusto(int codigoInterno)
+    {
+        CentroCusto centroCusto = null;
+
+        try
+        {
+            PreparedStatement sql =
+                UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO WHERE codigoInterno = ?");
+
+            sql.setInt(1, codigoInterno);
+
+            ResultSet resultado = sql.executeQuery();
+            if (resultado.next())
+            {
+                centroCusto =
+                    new CentroCusto(
+                        resultado.getInt("codigoInterno"),
+                        resultado.getString("sigla"),
+                        resultado.getString("departamento"),
+                        resultado.getString("seccao1"),
+                        resultado.getString("seccao2"));
+            }
+            sql.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("CentroCustoRelacional.lerCentroCusto: " + e.toString());
+        }
+        return centroCusto;
+
+    } /* lerCentroCusto */
+
+    public ArrayList lerTodosCentrosCusto()
+    {
+        ArrayList listaCentrosCusto = null;
+
+        try
+        {
+            PreparedStatement sql =
+                UtilRelacional.prepararComando("SELECT * FROM ass_CENTRO_CUSTO ORDER BY sigla");
+            ResultSet resultadoQuery = sql.executeQuery();
+
+            listaCentrosCusto = new ArrayList();
+            while (resultadoQuery.next())
+            {
+                listaCentrosCusto.add(
+                    new CentroCusto(
+                        resultadoQuery.getInt("codigoInterno"),
+                        resultadoQuery.getString("sigla"),
+                        resultadoQuery.getString("departamento"),
+                        resultadoQuery.getString("seccao1"),
+                        resultadoQuery.getString("seccao2")));
+            }
+            sql.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("CentroCustoRelacional.lerTodosCentrosCusto: " + e.toString());
+        }
+        return listaCentrosCusto;
+
+    } /* lerTodosCentrosCusto */
 }
