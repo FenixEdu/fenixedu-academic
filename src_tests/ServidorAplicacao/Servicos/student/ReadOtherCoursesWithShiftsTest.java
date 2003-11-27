76f9cdc44ced7741b030b91c765908bcdfed2429
João Luz <joao.luz@ist.utl.pt>
2003-11-27 03:12:12 +0000

    removed unused members. imports organized

--- src_tests/ServidorAplicacao/Servicos/student/ReadOtherCoursesWithShiftsTest.java
+++ src_tests/ServidorAplicacao/Servicos/student/ReadOtherCoursesWithShiftsTest.java
@@ -1,75 +1,76 @@
 /*
  * ReadShiftEnrolmentTest.java JUnit based test
- *
+ * 
  * Created on January 13th, 2002, 17:42
  */
 
 package ServidorAplicacao.Servicos.student;
 
 /**
- *
  * @author tfc130
  */
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import DataBeans.InfoPerson;
-import DataBeans.InfoShiftEnrolment;
-import DataBeans.InfoStudent;
 import ServidorAplicacao.Servicos.TestCaseReadServices;
 import Util.TipoCurso;
 
-public class ReadOtherCoursesWithShiftsTest extends TestCaseReadServices {
+public class ReadOtherCoursesWithShiftsTest extends TestCaseReadServices
+{
 
-	private InfoPerson infoPerson = null;
-	private InfoStudent infoStudent = null;
-	private InfoShiftEnrolment infoShiftEnrolment = null;
-	
-    public ReadOtherCoursesWithShiftsTest(java.lang.String testName) {
-    super(testName);
-  }
-    
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-    
-  public static Test suite() {
-    TestSuite suite = new TestSuite(ReadOtherCoursesWithShiftsTest.class);
-        
-    return suite;
-  }
-    
-  protected void setUp() {
-    super.setUp();
-  }
-    
-  protected void tearDown() {
-    super.tearDown();
-  }
+    public ReadOtherCoursesWithShiftsTest(java.lang.String testName)
+    {
+        super(testName);
+    }
 
-// FIXME : The service seens to be REALLY messed up
-// Wrong Arguments passed
-  protected String getNameOfServiceToBeTested() {
-	  return "ReadOtherCoursesWithShifts";
-  }
+    public static void main(java.lang.String[] args)
+    {
+        junit.textui.TestRunner.run(suite());
+    }
 
-  protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly() {
-	  Object[] result = { new Integer(800), new TipoCurso(TipoCurso.MESTRADO) };
-	  return result;
-  }
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite(ReadOtherCoursesWithShiftsTest.class);
 
-  protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly() {
-	  Object[] result = { new Integer(600), new TipoCurso(TipoCurso.LICENCIATURA) };
-	  return result;
-  }
+        return suite;
+    }
 
-  protected int getNumberOfItemsToRetrieve() {
-	  return 0;
-  }
+    protected void setUp()
+    {
+        super.setUp();
+    }
 
-  protected Object getObjectToCompare() {
-	return null;
-  }
+    protected void tearDown()
+    {
+        super.tearDown();
+    }
 
+    // FIXME : The service seens to be REALLY messed up
+    // Wrong Arguments passed
+    protected String getNameOfServiceToBeTested()
+    {
+        return "ReadOtherCoursesWithShifts";
+    }
 
+    protected Object[] getArgumentsOfServiceToBeTestedUnsuccessfuly()
+    {
+        Object[] result = { new Integer(800), new TipoCurso(TipoCurso.MESTRADO)};
+        return result;
+    }
+
+    protected Object[] getArgumentsOfServiceToBeTestedSuccessfuly()
+    {
+        Object[] result = { new Integer(600), new TipoCurso(TipoCurso.LICENCIATURA)};
+        return result;
+    }
+
+    protected int getNumberOfItemsToRetrieve()
+    {
+        return 0;
+    }
+
+    protected Object getObjectToCompare()
+    {
+        return null;
+    }
 
 }
