cbfdbea5666bd9da6e1577acd08a5461145f6161
Luis Cruz <luis.cruz@ist.utl.pt>
2003-11-28 19:38:00 +0000

    Checking in copyCode... I can't replicate the indicated bug. If anyone can, please let me know.

--- src/DataBeans/util/CopyUtils.java
+++ src/DataBeans/util/CopyUtils.java
@@ -4,11 +4,16 @@
  */
 package DataBeans.util;
 
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.collections.CollectionUtils;
 
 import Dominio.IDomainObject;
 
@@ -27,33 +32,29 @@ public class CopyUtils
 		interfacesNotToCopy.add(IDomainObject.class);
 	}
 	public static Object copyProperties(Object destinationBean, Object sourceBean)
-		throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException
+		throws IllegalAccessException, InvocationTargetException, NoSuchMethodException
 	{
-		BeanUtils.copyProperties(destinationBean, sourceBean);
 		/**
-		 * FIXME: The code below have some problems with introspection. To see
-		 * the problem try to use this code on use case Criar Candidato on
-		 * post-graduation office...
+		 * FIXME: The code below have some problems with introspection. To see the problem try to use
+		 * this code on use case Criar Candidato on post-graduation office...
 		 * 
 		 * @author jpvl
 		 */
-		//		PropertyDescriptor[] fields =
-		// PropertyUtils.getPropertyDescriptors(sourceBean.getClass());
-		//		for (int i = 0; i < fields.length; i++) {
-		//			PropertyDescriptor propertyDescriptor = fields[i];
-		//			Class fieldClass = propertyDescriptor.getPropertyType();
-		//			Class[] interfaces = fieldClass.getInterfaces();
-		//			List interfacesList = Arrays.asList(interfaces);
-		//
-		//			if (CollectionUtils
-		//				.intersection(interfacesNotToCopy, interfacesList)
-		//				.isEmpty() && !propertyDescriptor.getName().equals("class")) {
-		//				Object value = PropertyUtils.getProperty(sourceBean,
-		// propertyDescriptor.getName());
-		//				BeanUtils.copyProperty(destinationBean,
-		// propertyDescriptor.getName(), value);
-		//			}
-		//		}
+		PropertyDescriptor[] fields = PropertyUtils.getPropertyDescriptors(sourceBean.getClass());
+		for (int i = 0; i < fields.length; i++)
+		{
+			PropertyDescriptor propertyDescriptor = fields[i];
+			Class fieldClass = propertyDescriptor.getPropertyType();
+			Class[] interfaces = fieldClass.getInterfaces();
+			List interfacesList = Arrays.asList(interfaces);
+
+			if (CollectionUtils.intersection(interfacesNotToCopy, interfacesList).isEmpty()
+				&& !propertyDescriptor.getName().equals("class"))
+			{
+				Object value = PropertyUtils.getProperty(sourceBean, propertyDescriptor.getName());
+				BeanUtils.copyProperty(destinationBean, propertyDescriptor.getName(), value);
+			}
+		}
 		return destinationBean;
 	}
 }
