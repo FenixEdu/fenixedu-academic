9d3c7e0c41db637be02edea9183c76cab38c7206
João Luz <joao.luz@ist.utl.pt>
2003-11-28 20:49:24 +0000

    reads a teacher by his idInternal

---
+++ src/ServidorAplicacao/Servico/teacher/ReadTeacherByOID.java
@@ -0,0 +1,69 @@
+/*
+ * Created on Nov 28, 2003 by jpvl
+ *  
+ */
+package ServidorAplicacao.Servico.teacher;
+
+import DataBeans.InfoObject;
+import DataBeans.util.Cloner;
+import Dominio.IDomainObject;
+import Dominio.ITeacher;
+import Dominio.Teacher;
+import ServidorAplicacao.Servico.framework.ReadDomainObjectService;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentObject;
+import ServidorPersistente.ISuportePersistente;
+
+/**
+ * @author jpvl
+ */
+public class ReadTeacherByOID extends ReadDomainObjectService
+{
+    private static ReadTeacherByOID service = new ReadTeacherByOID();
+
+    public static ReadTeacherByOID getService()
+    {
+        return service;
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.ReadDomainObjectService#getDomainObjectClass()
+	 */
+    protected Class getDomainObjectClass()
+    {
+        return Teacher.class;
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.ReadDomainObjectService#getIPersistentObject(ServidorPersistente.ISuportePersistente)
+	 */
+    protected IPersistentObject getIPersistentObject(ISuportePersistente sp) throws ExcepcaoPersistencia
+    {
+        return sp.getIPersistentTeacher();
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.ReadDomainObjectService#clone2InfoObject(Dominio.IDomainObject)
+	 */
+    protected InfoObject clone2InfoObject(IDomainObject domainObject)
+    {
+        return Cloner.copyITeacher2InfoTeacher((ITeacher) domainObject);
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.IServico#getNome()
+	 */
+    public String getNome()
+    {
+        return "ReadTeacherByOID";
+    }
+
+}
