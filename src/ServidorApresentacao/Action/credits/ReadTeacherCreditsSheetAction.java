785196d71cb9ef9761ad85012b06093d0d17d467
João Luz <joao.luz@ist.utl.pt>
2003-12-03 23:01:50 +0000

    action to read CreditsSheet

---
+++ src/ServidorApresentacao/Action/credits/ReadTeacherCreditsSheetAction.java
@@ -0,0 +1,45 @@
+/*
+ * Created on Dec 3, 2003 by jpvl
+ *  
+ */
+package ServidorApresentacao.Action.credits;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import DataBeans.InfoTeacher;
+import DataBeans.credits.TeacherCreditsSheetDTO;
+import ServidorAplicacao.IUserView;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
+import ServidorApresentacao.Action.sop.utils.SessionUtils;
+
+/**
+ * @author jpvl
+ */
+public class ReadTeacherCreditsSheetAction extends Action
+{
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping,
+	 *          org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest,
+	 *          javax.servlet.http.HttpServletResponse)
+	 */
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception
+    {
+        IUserView userView = SessionUtils.getUserView(request);
+        InfoTeacher infoTeacher = (InfoTeacher) request.getAttribute("infoTeacher");
+        Object args[] = {infoTeacher, null};
+        TeacherCreditsSheetDTO teacherCreditsSheetDTO = (TeacherCreditsSheetDTO) ServiceUtils
+                .executeService(userView, "ReadTeacherCreditsSheet", args);
+        request.setAttribute("teacherCreditsSheet", teacherCreditsSheetDTO);
+        return mapping.findForward("successfull-read");
+    }
+}
