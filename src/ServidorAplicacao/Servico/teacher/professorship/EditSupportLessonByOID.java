694615def7826db2c2ae68dc74cee5d277354534
João Luz <joao.luz@ist.utl.pt>
2003-12-03 20:52:14 +0000

    added validation code on doBeforeLock method

--- src/ServidorAplicacao/Servico/teacher/professorship/EditSupportLessonByOID.java
+++ src/ServidorAplicacao/Servico/teacher/professorship/EditSupportLessonByOID.java
@@ -4,32 +4,59 @@
  */
 package ServidorAplicacao.Servico.teacher.professorship;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import DataBeans.InfoObject;
 import DataBeans.teacher.professorship.InfoSupportLesson;
 import DataBeans.util.Cloner;
 import Dominio.IDomainObject;
+import Dominio.IExecutionPeriod;
+import Dominio.IProfessorship;
 import Dominio.ISupportLesson;
+import Dominio.ITeacher;
+import Dominio.Professorship;
+import ServidorAplicacao.Servico.credits.validator.CreditsValidator;
+import ServidorAplicacao.Servico.credits.validator.OverlappingSupportLessonPeriod;
+import ServidorAplicacao.Servico.credits.validator.PeriodType;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorAplicacao.Servico.framework.EditDomainObjectService;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IPersistentObject;
+import ServidorPersistente.IPersistentProfessorship;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.teacher.professorship.IPersistentSupportLesson;
+import Util.DiaSemana;
 
 /**
  * @author jpvl
  */
 public class EditSupportLessonByOID extends EditDomainObjectService
 {
-	private static EditSupportLessonByOID service = new EditSupportLessonByOID();
+    /**
+	 * @author jpvl
+	 */
+    public class InvalidPeriodException extends FenixServiceException
+    {
 
-	/**
+        /**
+		 *  
+		 */
+        public InvalidPeriodException()
+        {
+            super();
+        }
+    }
+    private static EditSupportLessonByOID service = new EditSupportLessonByOID();
+
+    /**
 	 * The singleton access method of this class.
 	 */
-	public static EditSupportLessonByOID getService()
-	{
-		return service;
-	}
-
+    public static EditSupportLessonByOID getService()
+    {
+        return service;
+    }
 
     /*
 	 * (non-Javadoc)
@@ -68,7 +95,7 @@ public class EditSupportLessonByOID extends EditDomainObjectService
 	 *          ServidorPersistente.ISuportePersistente)
 	 */
     protected IDomainObject readObjectByUnique(IDomainObject domainObject, ISuportePersistente sp)
-        throws ExcepcaoPersistencia
+            throws ExcepcaoPersistencia
     {
         IPersistentSupportLesson supportLessonDAO = sp.getIPersistentSupportLesson();
 
@@ -76,4 +103,74 @@ public class EditSupportLessonByOID extends EditDomainObjectService
         return supportLesson;
     }
 
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#doBeforeLock(Dominio.IDomainObject,
+	 *          DataBeans.InfoObject, ServidorPersistente.ISuportePersistente)
+	 */
+    protected void doBeforeLock(IDomainObject domainObjectToLock, InfoObject infoObject,
+            ISuportePersistente sp) throws FenixServiceException
+    {
+        InfoSupportLesson infoSupportLesson = (InfoSupportLesson) infoObject;
+
+        Calendar begin = Calendar.getInstance();
+        begin.setTime(infoSupportLesson.getStartTime());
+        Calendar end = Calendar.getInstance();
+        end.setTime(infoSupportLesson.getEndTime());
+
+        if (end.before(begin))
+            throw new InvalidPeriodException();
+
+        IPersistentProfessorship professorshipDAO = sp.getIPersistentProfessorship();
+
+        IProfessorship professorship = (IProfessorship) professorshipDAO.readByOId(new Professorship(
+                infoSupportLesson.getInfoProfessorship().getIdInternal()),
+                false);
+
+        ITeacher teacher = professorship.getTeacher();
+        IExecutionPeriod executionPeriod = professorship.getExecutionCourse().getExecutionPeriod();
+        DiaSemana weekDay = infoSupportLesson.getWeekDay();
+        Date startTime = infoSupportLesson.getStartTime();
+        Date endTime = infoSupportLesson.getEndTime();
+
+        CreditsValidator.validatePeriod(teacher, executionPeriod, weekDay, startTime, endTime,
+                PeriodType.SUPPORT_LESSON_PERIOD);
+
+        IPersistentSupportLesson supportLessonDAO = sp.getIPersistentSupportLesson();
+
+        try
+        {
+            List supportLessonList = supportLessonDAO.readOverlappingPeriod(teacher, executionPeriod,
+                    weekDay, startTime, endTime);
+            boolean ok = true;
+            if (!supportLessonList.isEmpty())
+            {
+                if (supportLessonList.size() == 1)
+                {
+                    ISupportLesson supportLesson = (ISupportLesson) supportLessonList.get(0);
+                    if (!supportLesson.getIdInternal().equals(infoSupportLesson.getIdInternal()))
+                    {
+                        ok = false;
+                    }
+
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+            if (!ok)
+            {
+                throw new OverlappingSupportLessonPeriod();
+            }
+        }
+        catch (ExcepcaoPersistencia e)
+        {
+            e.printStackTrace(System.out);
+            throw new FenixServiceException("Problems on database!", e);
+        }
+
+        super.doBeforeLock(domainObjectToLock, infoObject, sp);
+    }
 }
--- src/ServidorAplicacao/Servico/teacher/workingTime/EditTeacherInstitutionWorkingTimeByOID.java
+++ src/ServidorAplicacao/Servico/teacher/workingTime/EditTeacherInstitutionWorkingTimeByOID.java
@@ -4,24 +4,52 @@
  */
 package ServidorAplicacao.Servico.teacher.workingTime;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import DataBeans.InfoObject;
 import DataBeans.teacher.workTime.InfoTeacherInstitutionWorkTime;
 import DataBeans.util.Cloner;
+import Dominio.ExecutionPeriod;
 import Dominio.IDomainObject;
+import Dominio.IExecutionPeriod;
+import Dominio.ITeacher;
+import Dominio.Teacher;
 import Dominio.teacher.workTime.ITeacherInstitutionWorkTime;
+import ServidorAplicacao.Servico.credits.validator.CreditsValidator;
+import ServidorAplicacao.Servico.credits.validator.OverlappingInstitutionWorkingPeriod;
+import ServidorAplicacao.Servico.credits.validator.OverlappingSupportLessonPeriod;
+import ServidorAplicacao.Servico.credits.validator.PeriodType;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorAplicacao.Servico.framework.EditDomainObjectService;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IPersistentObject;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.teacher.workingTime.IPersistentTeacherInstitutionWorkingTime;
+import Util.DiaSemana;
 
 /**
  * @author jpvl
  */
 public class EditTeacherInstitutionWorkingTimeByOID extends EditDomainObjectService
 {
-    private static EditTeacherInstitutionWorkingTimeByOID service =
-        new EditTeacherInstitutionWorkingTimeByOID();
+    /**
+	 * @author jpvl
+	 */
+    public class InvalidPeriodException extends FenixServiceException
+    {
+
+        /**
+		 *  
+		 */
+        public InvalidPeriodException()
+        {
+            super();
+        }
+    }
+
+    private static EditTeacherInstitutionWorkingTimeByOID service = new EditTeacherInstitutionWorkingTimeByOID();
 
     /**
 	 * The singleton access method of this class.
@@ -29,15 +57,6 @@ public class EditTeacherInstitutionWorkingTimeByOID extends EditDomainObjectServ
     public static EditTeacherInstitutionWorkingTimeByOID getService()
     {
         return service;
-    }
-    /*
-	 * (non-Javadoc)
-	 * 
-	 * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#getIPersistentObject(ServidorPersistente.ISuportePersistente)
-	 */
-    protected IPersistentObject getIPersistentObject(ISuportePersistente sp) throws ExcepcaoPersistencia
-    {
-        return sp.getIPersistentTeacherInstitutionWorkingTime();
     }
 
     /*
@@ -48,7 +67,85 @@ public class EditTeacherInstitutionWorkingTimeByOID extends EditDomainObjectServ
     protected IDomainObject clone2DomainObject(InfoObject infoObject)
     {
         return Cloner.copyInfoTeacherInstitutionWorkingTime2ITeacherInstitutionWorkTime(
-            (InfoTeacherInstitutionWorkTime) infoObject);
+                (InfoTeacherInstitutionWorkTime) infoObject);
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#doBeforeLock(Dominio.IDomainObject,
+	 *          DataBeans.InfoObject, ServidorPersistente.ISuportePersistente)
+	 */
+    protected void doBeforeLock(IDomainObject domainObjectToLock, InfoObject infoObject,
+            ISuportePersistente sp) throws FenixServiceException
+    {
+        super.doBeforeLock(domainObjectToLock, infoObject, sp);
+
+        InfoTeacherInstitutionWorkTime infoTeacherInstitutionWorkTime = (InfoTeacherInstitutionWorkTime) infoObject;
+
+        Calendar begin = Calendar.getInstance();
+        begin.setTime(infoTeacherInstitutionWorkTime.getStartTime());
+        Calendar end = Calendar.getInstance();
+        end.setTime(infoTeacherInstitutionWorkTime.getEndTime());
+
+        if (end.before(begin))
+            throw new InvalidPeriodException();
+
+        ITeacher teacher = new Teacher(infoTeacherInstitutionWorkTime.getInfoTeacher().getIdInternal());
+        IExecutionPeriod executionPeriod = new ExecutionPeriod(infoTeacherInstitutionWorkTime
+                .getInfoExecutionPeriod().getIdInternal());
+        DiaSemana weekDay = infoTeacherInstitutionWorkTime.getWeekDay();
+        Date startTime = infoTeacherInstitutionWorkTime.getStartTime();
+        Date endTime = infoTeacherInstitutionWorkTime.getEndTime();
+
+        CreditsValidator.validatePeriod(teacher, executionPeriod, weekDay, startTime, endTime,
+                PeriodType.INSTITUTION_WORKING_TIME_PERIOD);
+        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkingTimeDAO = sp
+                .getIPersistentTeacherInstitutionWorkingTime();
+
+        try
+        {
+            List teacherInstitutionWorkingTime = teacherInstitutionWorkingTimeDAO
+                    .readOverlappingPeriod(teacher, executionPeriod, weekDay, startTime, endTime);
+            boolean ok = true;
+            if (!teacherInstitutionWorkingTime.isEmpty())
+            {
+                if (teacherInstitutionWorkingTime.size() == 1)
+                {
+                    ITeacherInstitutionWorkTime teacherInstitutionWorkTime = (ITeacherInstitutionWorkTime) teacherInstitutionWorkingTime
+                            .get(0);
+                    if (!teacherInstitutionWorkTime.getIdInternal()
+                            .equals(infoTeacherInstitutionWorkTime.getIdInternal()))
+                    {
+                        ok = false;
+                    }
+
+                }
+                else
+                {
+                    ok = false;
+                }
+            }
+            if (!ok)
+            {
+                throw new OverlappingInstitutionWorkingPeriod();
+            }
+        }
+        catch (ExcepcaoPersistencia e)
+        {
+            e.printStackTrace(System.out);
+            throw new FenixServiceException("Problems on database!", e);
+        }
+
+    }
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#getIPersistentObject(ServidorPersistente.ISuportePersistente)
+	 */
+    protected IPersistentObject getIPersistentObject(ISuportePersistente sp) throws ExcepcaoPersistencia
+    {
+        return sp.getIPersistentTeacherInstitutionWorkingTime();
     }
 
     /*
@@ -68,14 +165,13 @@ public class EditTeacherInstitutionWorkingTimeByOID extends EditDomainObjectServ
 	 *          ServidorPersistente.ISuportePersistente)
 	 */
     protected IDomainObject readObjectByUnique(IDomainObject domainObject, ISuportePersistente sp)
-        throws ExcepcaoPersistencia
+            throws ExcepcaoPersistencia
     {
-        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkingTimeDAO =
-            sp.getIPersistentTeacherInstitutionWorkingTime();
-        ITeacherInstitutionWorkTime teacherInstitutionWorkTime =
-            (ITeacherInstitutionWorkTime) domainObject;
-        teacherInstitutionWorkTime = teacherInstitutionWorkingTimeDAO.readByUnique(teacherInstitutionWorkTime);
+        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkingTimeDAO = sp
+                .getIPersistentTeacherInstitutionWorkingTime();
+        ITeacherInstitutionWorkTime teacherInstitutionWorkTime = (ITeacherInstitutionWorkTime) domainObject;
+        teacherInstitutionWorkTime = teacherInstitutionWorkingTimeDAO.readByUnique(
+                teacherInstitutionWorkTime);
         return teacherInstitutionWorkTime;
     }
-
 }
