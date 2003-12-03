a5ed4b8023e48c84108833986352b2746e4a4511
João Luz <joao.luz@ist.utl.pt>
2003-12-03 23:09:31 +0000

    *** empty log message ***

---
+++ src/ServidorAplicacao/Servico/credits/ReadTeacherCreditsSheet.java
@@ -0,0 +1,317 @@
+/*
+ * Created on Nov 19, 2003 by jpvl
+ *  
+ */
+package ServidorAplicacao.Servico.credits;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+
+import DataBeans.InfoExecutionPeriod;
+import DataBeans.InfoProfessorship;
+import DataBeans.InfoTeacher;
+import DataBeans.credits.TeacherCreditsSheetDTO;
+import DataBeans.degree.finalProject.InfoTeacherDegreeFinalProjectStudent;
+import DataBeans.teacher.credits.InfoShiftProfessorship;
+import DataBeans.teacher.professorship.InfoSupportLesson;
+import DataBeans.teacher.workTime.InfoTeacherInstitutionWorkTime;
+import DataBeans.util.Cloner;
+import Dominio.ExecutionPeriod;
+import Dominio.IExecutionPeriod;
+import Dominio.IProfessorship;
+import Dominio.IShiftProfessorship;
+import Dominio.ISupportLesson;
+import Dominio.ITeacher;
+import Dominio.degree.finalProject.ITeacherDegreeFinalProjectStudent;
+import Dominio.teacher.workTime.ITeacherInstitutionWorkTime;
+import ServidorAplicacao.IServico;
+import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentExecutionPeriod;
+import ServidorPersistente.IPersistentProfessorship;
+import ServidorPersistente.IPersistentShiftProfessorship;
+import ServidorPersistente.IPersistentTeacher;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+import ServidorPersistente.degree.finalProject.IPersistentTeacherDegreeFinalProjectStudent;
+import ServidorPersistente.teacher.professorship.IPersistentSupportLesson;
+import ServidorPersistente.teacher.workingTime.IPersistentTeacherInstitutionWorkingTime;
+import Util.TipoCurso;
+
+/**
+ * @author jpvl
+ */
+public class ReadTeacherCreditsSheet implements IServico
+{
+
+    private static ReadTeacherCreditsSheet instance = new ReadTeacherCreditsSheet();
+
+    public static ReadTeacherCreditsSheet getService()
+    {
+        return instance;
+    }
+
+    /*
+	 * (non-Javadoc)
+	 * 
+	 * @see ServidorAplicacao.IServico#getNome()
+	 */
+    public String getNome()
+    {
+        return "ReadTeacherCreditsSheet";
+    }
+    private IExecutionPeriod readExecutionPeriod(InfoExecutionPeriod infoExecutionPeriod,
+            IPersistentExecutionPeriod executionPeriodDAO) throws FenixServiceException
+    {
+        IExecutionPeriod executionPeriod;
+        try
+        {
+            if (infoExecutionPeriod == null)
+            {
+                executionPeriod = executionPeriodDAO.readActualExecutionPeriod();
+            }
+            else
+            {
+                executionPeriod = (IExecutionPeriod) executionPeriodDAO.readByOId(new ExecutionPeriod(
+                        infoExecutionPeriod.getIdInternal()),
+                        false);
+            }
+        }
+        catch (ExcepcaoPersistencia e1)
+        {
+            e1.printStackTrace(System.out);
+            throw new FenixServiceException("Error getting execution period!", e1);
+        }
+        return executionPeriod;
+    }
+    /**
+	 * @param sp
+	 * @param infoTeacher
+	 * @param infoExecutionPeriod
+	 * @return
+	 */
+    private List readInfoMasterDegreeProfessorships(ISuportePersistente sp, ITeacher teacher,
+            IExecutionPeriod executionPeriod) throws FenixServiceException
+    {
+        IPersistentProfessorship professorshipDAO = sp.getIPersistentProfessorship();
+        List masterDegreeProfessorshipList;
+        try
+        {
+            masterDegreeProfessorshipList = professorshipDAO.readByTeacherAndTypeOfDegree(teacher,
+                    TipoCurso.MESTRADO_OBJ);
+        }
+        catch (ExcepcaoPersistencia e)
+        {
+            e.printStackTrace(System.out);
+            throw new FenixServiceException("Problems getting master degree professorships!", e);
+        }
+        List infoMasterDegreeProfessorships = (List) CollectionUtils.collect(
+                masterDegreeProfessorshipList, new Transformer()
+                {
+
+                    public Object transform(Object input)
+                    {
+                        IProfessorship professorship = (IProfessorship) input;
+                        InfoProfessorship infoProfessorship = Cloner
+                                .copyIProfessorship2InfoProfessorship(professorship);
+                        return infoProfessorship;
+                    }
+                });
+
+        return infoMasterDegreeProfessorships;
+    }
+    /**
+	 * @param sp
+	 * @param infoTeacher
+	 * @param infoExecutionPeriod
+	 * @return
+	 */
+    private List readInfoShiftProfessorships(ISuportePersistente sp, ITeacher teacher,
+            IExecutionPeriod executionPeriod) throws ExcepcaoPersistencia
+    {
+        IPersistentShiftProfessorship shiftProfessorshipDAO = sp.getIPersistentShiftProfessorship();
+
+        List shiftProfessorships = shiftProfessorshipDAO.readByTeacherAndExecutionPeriodAndDegreeType(
+                teacher, executionPeriod, TipoCurso.LICENCIATURA_OBJ);
+
+        List infoShiftProfessorships = (List) CollectionUtils.collect(shiftProfessorships,
+                new Transformer()
+                {
+
+                    public Object transform(Object input)
+                    {
+                        IShiftProfessorship shiftProfessorship = (IShiftProfessorship) input;
+                        InfoShiftProfessorship infoShiftProfessorship = Cloner
+                                .copyIShiftProfessorship2InfoShiftProfessorship(shiftProfessorship);
+                        return infoShiftProfessorship;
+                    }
+                });
+
+        return infoShiftProfessorships;
+    }
+
+    /**
+	 * @param teacher
+	 * @param executionPeriod
+	 * @param sp
+	 * @return
+	 */
+    private List readInfoSupportLessonList(ITeacher teacher, IExecutionPeriod executionPeriod,
+            ISuportePersistente sp) throws ExcepcaoPersistencia
+    {
+        IPersistentSupportLesson supportLessonDAO = sp.getIPersistentSupportLesson();
+        List supportLessonList = supportLessonDAO.readByTeacherAndExecutionPeriod(teacher,
+                executionPeriod);
+        List infoSupportLessonList = (List) CollectionUtils.collect(supportLessonList, new Transformer()
+        {
+
+            public Object transform(Object input)
+            {
+                ISupportLesson supportLesson = (ISupportLesson) input;
+                InfoSupportLesson infoSupportLesson = Cloner.copyISupportLesson2InfoSupportLesson(
+                        supportLesson);
+                return infoSupportLesson;
+            }
+        });
+
+        return infoSupportLessonList;
+    }
+
+    /**
+	 * @param teacher
+	 * @param executionPeriod
+	 * @param sp
+	 * @return
+	 */
+    private List readInfoTeacherInstitutionWorkingTime(ITeacher teacher,
+            IExecutionPeriod executionPeriod, ISuportePersistente sp) throws ExcepcaoPersistencia
+    {
+        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkingTimeDAO = sp
+                .getIPersistentTeacherInstitutionWorkingTime();
+        List teacherInstitutionWorkingTimeList = teacherInstitutionWorkingTimeDAO
+                .readByTeacherAndExecutionPeriod(teacher, executionPeriod);
+
+        List infoTeacherInstitutionWorkingTimeList = (List) CollectionUtils.collect(
+                teacherInstitutionWorkingTimeList, new Transformer()
+                {
+
+                    public Object transform(Object input)
+                    {
+                        ITeacherInstitutionWorkTime teacherInstitutionWorkTime = (ITeacherInstitutionWorkTime) input;
+                        InfoTeacherInstitutionWorkTime infoTeacherInstitutionWorkTime = Cloner
+                                .copyITeacherInstitutionWorkingTime2InfoTeacherInstitutionWorkTime(
+                                        teacherInstitutionWorkTime);
+                        return infoTeacherInstitutionWorkTime;
+                    }
+                });
+        return infoTeacherInstitutionWorkingTimeList;
+    }
+
+    private ITeacher readTeacher(Integer teacherNumber, IPersistentTeacher teacherDAO)
+            throws FenixServiceException
+    {
+        ITeacher teacher;
+        try
+        {
+            teacher = teacherDAO.readByNumber(teacherNumber);
+        }
+        catch (ExcepcaoPersistencia e2)
+        {
+            e2.printStackTrace(System.out);
+            throw new FenixServiceException("Error getting teacher!", e2);
+        }
+        return teacher;
+    }
+
+    /**
+	 * @param teacher
+	 * @param executionPeriod
+	 * @param sp
+	 * @return
+	 */
+    private List readTeacherDegreeFinalProjectStudentList(ITeacher teacher,
+            IExecutionPeriod executionPeriod, ISuportePersistente sp) throws ExcepcaoPersistencia
+    {
+        IPersistentTeacherDegreeFinalProjectStudent teacherDegreeFinalProjectStudentDAO = sp
+                .getIPersistentTeacherDegreeFinalProjectStudent();
+
+        List teacherDegreeFinalProjectStudentList = teacherDegreeFinalProjectStudentDAO
+                .readByTeacherAndExecutionYear(teacher, executionPeriod.getExecutionYear());
+        List infoTeacherDegreeFinalProjectStudentList = (List) CollectionUtils.collect(
+                teacherDegreeFinalProjectStudentList, new Transformer()
+                {
+
+                    public Object transform(Object input)
+                    {
+                        ITeacherDegreeFinalProjectStudent teacherDegreeFinalProjectStudent = (ITeacherDegreeFinalProjectStudent) input;
+                        InfoTeacherDegreeFinalProjectStudent infoTeacherDegreeFinalProjectStudent = Cloner
+                                .copyITeacherDegreeFinalProjectStudent2InfoTeacherDegreeFinalProjectStudent(
+                                        teacherDegreeFinalProjectStudent);
+                        return infoTeacherDegreeFinalProjectStudent;
+                    }
+                });
+
+        return infoTeacherDegreeFinalProjectStudentList;
+    }
+
+    public TeacherCreditsSheetDTO run(InfoTeacher infoTeacherParam,
+            InfoExecutionPeriod infoExecutionPeriodParam) throws FenixServiceException
+    {
+
+        TeacherCreditsSheetDTO teacherCreditsSheetDTO = new TeacherCreditsSheetDTO();
+
+        ISuportePersistente sp;
+        try
+        {
+            sp = SuportePersistenteOJB.getInstance();
+
+            IPersistentTeacher teacherDAO = sp.getIPersistentTeacher();
+            IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
+            IExecutionPeriod executionPeriod = readExecutionPeriod(infoExecutionPeriodParam,
+                    executionPeriodDAO);
+            ITeacher teacher = readTeacher(infoTeacherParam.getTeacherNumber(), teacherDAO);
+
+            List infoMasterDegreeProfessorships = readInfoMasterDegreeProfessorships(sp, teacher,
+                    executionPeriod);
+
+            List infoShiftProfessorships = readInfoShiftProfessorships(sp, teacher, executionPeriod);
+
+            List infoTimeInstitutionWorkingTimeList = readInfoTeacherInstitutionWorkingTime(teacher,
+                    executionPeriod, sp);
+
+            List infoSupportLessonList = readInfoSupportLessonList(teacher, executionPeriod, sp);
+
+            List infoTeacherDegreeFinalProjectStudentList = readTeacherDegreeFinalProjectStudentList(
+                    teacher, executionPeriod, sp);
+
+            InfoTeacher infoTeacher = Cloner.copyITeacher2InfoTeacher(teacher);
+            InfoExecutionPeriod infoExecutionPeriod = Cloner.copyIExecutionPeriod2InfoExecutionPeriod(
+                    executionPeriod);
+
+            teacherCreditsSheetDTO.setInfoTeacher(infoTeacher);
+            teacherCreditsSheetDTO.setInfoExecutionPeriod(infoExecutionPeriod);
+
+            teacherCreditsSheetDTO.setInfoMasterDegreeProfessorships(infoMasterDegreeProfessorships);
+
+            teacherCreditsSheetDTO.setInfoTeacherInstitutionWorkingTimeList(
+                    infoTimeInstitutionWorkingTimeList);
+
+            teacherCreditsSheetDTO.setInfoShiftProfessorshipList(infoShiftProfessorships);
+
+            teacherCreditsSheetDTO.setInfoSupportLessonList(infoSupportLessonList);
+
+            teacherCreditsSheetDTO.setInfoTeacherDegreeFinalProjectStudentList(
+                    infoTeacherDegreeFinalProjectStudentList);
+
+        }
+        catch (ExcepcaoPersistencia e)
+        {
+            e.printStackTrace(System.out);
+            throw new FenixServiceException("Problems with database!", e);
+        }
+
+        return teacherCreditsSheetDTO;
+    }
+}
