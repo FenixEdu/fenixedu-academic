82af7e599be8727b65de4b0d756fec426f0e3ce2
João Luz <joao.luz@ist.utl.pt>
2003-12-05 20:38:07 +0000

    ICampus dao object

---
+++ src/ServidorPersistente/OJB/places/campus/CampusOJB.java
@@ -0,0 +1,31 @@
+/*
+ * Created on Dec 5, 2003 by jpvl
+ *
+ */
+package ServidorPersistente.OJB.places.campus;
+
+import java.util.List;
+
+import org.apache.ojb.broker.query.Criteria;
+
+import Dominio.Campus;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.OJB.ObjectFenixOJB;
+import ServidorPersistente.places.campus.IPersistentCampus;
+
+/**
+ * @author jpvl
+ */
+public class CampusOJB extends ObjectFenixOJB implements IPersistentCampus
+{
+
+    /* (non-Javadoc)
+     * @see ServidorPersistente.places.campus.IPersistentCampus#readAll()
+     */
+    public List readAll() throws ExcepcaoPersistencia
+    {
+        Criteria criteria = new Criteria();
+        return queryList(Campus.class, criteria);
+    }
+
+}
