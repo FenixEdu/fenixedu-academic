49e0ec435b69354bddc39dd150bdf87a56caaa3f
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2003-12-05 13:59:48 +0000

    changed worklocation type from String  to InfoWorkLocation

--- src/DataBeans/InfoExternalPerson.java
+++ src/DataBeans/InfoExternalPerson.java
@@ -4,48 +4,58 @@
  */
 package DataBeans;
 
+
 /**
- * @author: 
- * 			- Shezad Anavarali (sana@mega.ist.utl.pt)
- * 			- Nadir Tarmahomed (naat@mega.ist.utl.pt)
+ * @author Shezad Anavarali (sana@mega.ist.utl.pt)
+ * @author Nadir Tarmahomed (naat@mega.ist.utl.pt)
  *  
  */
-public class InfoExternalPerson extends InfoObject {
-	private InfoPerson infoPerson;
-	private String workLocation;
+public class InfoExternalPerson extends InfoObject
+{
+    private InfoPerson infoPerson;
+    private InfoWorkLocation infoWorkLocation;
 
-	/**
+    /**
 	 * @return Returns the infoPerson.
 	 */
-	public InfoPerson getInfoPerson() {
-		return infoPerson;
-	}
-	/**
-	 * @param infoPerson The infoPerson to set.
+    public InfoPerson getInfoPerson()
+    {
+        return infoPerson;
+    }
+    /**
+	 * @param infoPerson
+	 *            The infoPerson to set.
 	 */
-	public void setInfoPerson(InfoPerson infoPerson) {
-		this.infoPerson = infoPerson;
-	}
-	/**
-	 * @return Returns the workLocation.
-	 */
-	public String getWorkLocation() {
-		return workLocation;
-	}
-	/**
-	 * @param workLocation The workLocation to set.
-	 */
-	public void setWorkLocation(String workLocation) {
-		this.workLocation = workLocation;
-	}
+    public void setInfoPerson(InfoPerson infoPerson)
+    {
+        this.infoPerson = infoPerson;
+    }
+    
+    /**
+     * @return Returns the infoWorkLocation.
+     */
+    public InfoWorkLocation getInfoWorkLocation()
+    {
+        return infoWorkLocation;
+    }
 
-	public boolean equals(Object obj) {
-		boolean result = false;
+    /**
+     * @param infoWorkLocation The infoWorkLocation to set.
+     */
+    public void setInfoWorkLocation(InfoWorkLocation infoWorkLocation)
+    {
+        this.infoWorkLocation = infoWorkLocation;
+    }
 
-		if (obj instanceof InfoExternalPerson) {
-			InfoExternalPerson infoExternalPerson = (InfoExternalPerson) obj;
-			result = this.getInfoPerson().equals(infoExternalPerson.getInfoPerson());
-		}
-		return result;
-	}
+    public boolean equals(Object obj)
+    {
+        boolean result = false;
+
+        if (obj instanceof InfoExternalPerson)
+        {
+            InfoExternalPerson infoExternalPerson = (InfoExternalPerson) obj;
+            result = this.getInfoPerson().equals(infoExternalPerson.getInfoPerson());
+        }
+        return result;
+    }
 }
