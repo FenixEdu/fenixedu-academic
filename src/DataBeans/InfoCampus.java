efa608e6ba2a0a2583b02433d0bc809a48ac592b
João Luz <joao.luz@ist.utl.pt>
2003-12-05 20:42:32 +0000

    infoExecutionDegreeList removed

--- src/DataBeans/InfoCampus.java
+++ src/DataBeans/InfoCampus.java
@@ -1,69 +1,59 @@
 package DataBeans;
 
-import java.util.List;
 
 /**
- * @author Tânia Pousão
- * Create on 10/Nov/2003
+ * @author Tânia Pousão Create on 10/Nov/2003
  */
-public class InfoCampus extends InfoObject {
-	private String name;
-	
-	private List executionDegreeList;
+public class InfoCampus extends InfoObject
+{
+    private String name;
 
-	public InfoCampus() {	
-	}
-	
-	public InfoCampus(String name, List executionDegreeList) {
-		this.name = name;
-		this.executionDegreeList = executionDegreeList;
-	}
-	
-	/**
+    public InfoCampus()
+    {
+    }
+
+    /**
+     * @param integer
+     */
+    public InfoCampus(Integer campusId)
+    {
+        super(campusId);
+    }
+
+    /**
 	 * @return Returns the name.
 	 */
-	public String getName() {
-		return name;
-	}
+    public String getName()
+    {
+        return name;
+    }
 
-	/**
-	 * @param name The name to set.
+    /**
+	 * @param name
+	 *                   The name to set.
 	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	/**
-	 * @return Returns the executionDegreeList.
-	 */
-	public List getExecutionDegreeList() {
-		return executionDegreeList;
-	}
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 
-	/**
-	 * @param executionDegreeList The executionDegreeList to set.
-	 */
-	public void setExecutionDegreeList(List executionDegreeList) {
-		this.executionDegreeList = executionDegreeList;
-	}
-	
-	public boolean equals(Object obj) {
-		boolean result = false;
-		if (obj instanceof InfoCampus) {
-			InfoCampus infoCampus = (InfoCampus) obj;
-			result = getName().equals(infoCampus.getName());
-		}
-		return result;
-	}
-	
-	public String toString() {
-		String result = "[INFODEGREE_INFO:";
-		result += " codigo interno= " + getIdInternal();
-		if (getExecutionDegreeList() != null) {
-			result += "nr execution Degrees= " + getExecutionDegreeList().size();		
-		}
-		result += " name= " + getName();
-		result += "]";
-		return result;
-	}
+    public boolean equals(Object obj)
+    {
+        boolean result = false;
+        if (obj instanceof InfoCampus)
+        {
+            InfoCampus infoCampus = (InfoCampus) obj;
+            result = getName().equals(infoCampus.getName());
+        }
+        return result;
+    }
+
+    public String toString()
+    {
+        String result = "[INFODEGREE_INFO:";
+        result += " codigo interno= " + getIdInternal();
+        result += " name= " + getName();
+        result += "]";
+        return result;
+    }
 }
