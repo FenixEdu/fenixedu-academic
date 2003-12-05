f55be7a371c7b93b70517e41fd8e77f3e4c5efbc
João Luz <joao.luz@ist.utl.pt>
2003-12-05 20:38:47 +0000

    Campus DAO interface

---
+++ src/ServidorPersistente/places/campus/IPersistentCampus.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Dec 5, 2003 by jpvl
+ *
+ */
+package ServidorPersistente.places.campus;
+
+import java.util.List;
+
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentObject;
+
+/**
+ * @author jpvl
+ */
+public interface IPersistentCampus extends IPersistentObject
+{
+
+    /**
+     * @return
+     */
+    List readAll() throws ExcepcaoPersistencia;
+
+}
