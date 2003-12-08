d0c666fb54ff6ff5b8700d8d6179d41c604e0265
Luis Cruz <luis.cruz@ist.utl.pt>
2003-12-08 16:09:29 +0000

    Readded service logic. Direct implementation of the execute method to have access to the service argument.

--- src/ServidorAplicacao/Filtro/DegreeAndMasterAdministrativeOfficeAuthorizationFilter.java
+++ src/ServidorAplicacao/Filtro/DegreeAndMasterAdministrativeOfficeAuthorizationFilter.java
@@ -1,7 +1,14 @@
 package ServidorAplicacao.Filtro;
 
+import pt.utl.ist.berserk.ServiceRequest;
+import pt.utl.ist.berserk.ServiceResponse;
+import DataBeans.InfoStudent;
+import ServidorAplicacao.IServico;
 import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Servico.exceptions.NotAuthorizedException;
+import ServidorAplicacao.strategy.enrolment.context.InfoEnrolmentContext;
 import Util.RoleType;
+import Util.TipoCurso;
 
 /**
  * @author David Santos
@@ -27,12 +34,65 @@ public class DegreeAndMasterAdministrativeOfficeAuthorizationFilter extends Auth
 				return RoleType.DEGREE_ADMINISTRATIVE_OFFICE;
 		}
 
-		public void preFiltragem(IUserView id, Object[] argumentos) throws Exception {
-				if(!AuthorizationUtils.containsRole(id.getRoles(), RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE)) {
-						if(!AuthorizationUtils.containsRole(id.getRoles(), RoleType.DEGREE_ADMINISTRATIVE_OFFICE_SUPER_USER)) {
-								super.preFiltragem(id, argumentos);
-						}
+	public void execute(ServiceRequest arg0, ServiceResponse arg1) throws Exception
+	{
+		IUserView id = (IUserView) arg0.getRequester();
+		IServico servico = (IServico) arg0.getService();
+		Object[] argumentos = arg0.getArguments();
+
+		if(AuthorizationUtils.containsRole(id.getRoles(), RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE)) {
+				if(!this.okToExecute(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE, TipoCurso.MESTRADO, servico, argumentos)) {
+						throw new NotAuthorizedException();
 				}
+		} else if(AuthorizationUtils.containsRole(id.getRoles(), RoleType.DEGREE_ADMINISTRATIVE_OFFICE_SUPER_USER)) {
+				if(!this.okToExecute(RoleType.DEGREE_ADMINISTRATIVE_OFFICE_SUPER_USER, TipoCurso.LICENCIATURA, servico, argumentos)) {
+						throw new NotAuthorizedException();
+				}
+		} else {
+				throw new NotAuthorizedException();
 		}
 
+	}
+
+	private boolean okToExecute(RoleType userViewRoleType, int degreeType, IServico servico, Object[] argumentos) {
+			if(servico.getNome().equals("GetStudentByNumberAndDegreeType")) {
+					if( ((Integer)argumentos[0]).intValue() != degreeType ) {
+							return false;
+					} else {
+							return true;
+					}
+			} else if(servico.getNome().equals("ReadExecutionDegreesByExecutionYearAndDegreeType")) {
+					if(userViewRoleType.equals(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE)) {
+							return true;
+					} else if( ((TipoCurso)argumentos[1]).getTipoCurso().intValue() != degreeType ) {
+							return false;
+					} else {
+							return true;
+					}
+			} else if(servico.getNome().equals("PrepareEnrolmentContext")) {
+					if( ((InfoStudent)argumentos[0]).getDegreeType().getTipoCurso().intValue() != degreeType ) {
+							return false;
+					} else {
+							return true;
+					}
+			} else if(servico.getNome().equals("ValidateActualEnrolmentWithoutRules")) {
+					if( ((InfoEnrolmentContext)argumentos[0]).getInfoStudentActiveCurricularPlan().getInfoStudent().getDegreeType().getTipoCurso().intValue() != degreeType ) {
+							return false;
+					} else {
+							return true;
+					}
+			} else if(servico.getNome().equals("ConfirmActualEnrolmentWithoutRules")) {
+					if( ((InfoEnrolmentContext)argumentos[0]).getInfoStudentActiveCurricularPlan().getInfoStudent().getDegreeType().getTipoCurso().intValue() != degreeType ) {
+							return false;
+					} else {
+							return true;
+					}
+			} else if(servico.getNome().equals("ReadAllExecutionPeriodsForEnrollment")) {
+					return true;
+			} else if(servico.getNome().equals("ReadExecutionPeriodByOIDForEnrollment")) {
+					return true;
+			} else {
+					return false;
+			}
+	}
 }
