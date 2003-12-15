e46fe1827e4220073530417583f8fae842e62626
João Luz <joao.luz@ist.utl.pt>
2003-12-15 00:01:45 +0000

    deployed

--- docs/inactiveEmployees.sql
+++
@@ -1,5 +0,0 @@
-alter table ass_FUNCIONARIO add ACTIVE bit NOT NULL default 1;
-select CONCAT("update ass_FUNCIONARIO set ACTIVE = 0 where codigoInterno=", f.codigoInterno,";") as "" 
-from PERSON p 
-		inner join ass_FUNCIONARIO f on f.chavePessoa = p.id_internal 
-where username like "INA%";
