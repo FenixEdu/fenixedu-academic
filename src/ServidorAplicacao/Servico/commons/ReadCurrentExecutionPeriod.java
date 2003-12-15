f88a1777b60948a2edd20c07185fc336e10f3b40
João Mota <joao.mota@ist.utl.pt>
2003-12-15 11:52:57 +0000

    format

--- src/ServidorAplicacao/Servico/commons/ReadCurrentExecutionPeriod.java
+++ src/ServidorAplicacao/Servico/commons/ReadCurrentExecutionPeriod.java
@@ -10,38 +10,46 @@ import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 /**
- *   14/Fev/2003
- *   @author     jpvl
+ * 14/Fev/2003
+ * 
+ * @author jpvl
  */
-public class ReadCurrentExecutionPeriod implements IServico {
-	private static ReadCurrentExecutionPeriod service = new ReadCurrentExecutionPeriod();
-	/**
+public class ReadCurrentExecutionPeriod implements IServico
+{
+    private static ReadCurrentExecutionPeriod service = new ReadCurrentExecutionPeriod();
+    /**
 	 * The singleton access method of this class.
-	 **/
-	public static ReadCurrentExecutionPeriod getService() {
-	  return service;
-	}
+	 */
+    public static ReadCurrentExecutionPeriod getService()
+    {
+        return service;
+    }
 
-	/**
+    /**
 	 * @see ServidorAplicacao.IServico#getNome()
 	 */
-	public String getNome() {
-		return "ReadCurrentExecutionPeriod";
-	}
-	
-	public InfoExecutionPeriod run() {
+    public String getNome()
+    {
+        return "ReadCurrentExecutionPeriod";
+    }
 
-	  InfoExecutionPeriod infoExecutionPeriod = null;
-	  try {
-		ISuportePersistente sp = SuportePersistenteOJB.getInstance();
-		IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
-		IExecutionPeriod executionPeriod = executionPeriodDAO.readActualExecutionPeriod();
-		infoExecutionPeriod = Cloner.copyIExecutionPeriod2InfoExecutionPeriod(executionPeriod);
-	  } catch (ExcepcaoPersistencia ex) {
-	  	throw new RuntimeException(ex);
-	  }
-    
-	  return infoExecutionPeriod;
-	}
+    public InfoExecutionPeriod run()
+    {
+
+        InfoExecutionPeriod infoExecutionPeriod = null;
+        try
+        {
+            ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+            IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
+            IExecutionPeriod executionPeriod = executionPeriodDAO.readActualExecutionPeriod();
+            infoExecutionPeriod = Cloner.copyIExecutionPeriod2InfoExecutionPeriod(executionPeriod);
+        }
+        catch (ExcepcaoPersistencia ex)
+        {
+            throw new RuntimeException(ex);
+        }
+
+        return infoExecutionPeriod;
+    }
 
 }
