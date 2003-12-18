93484783ceb9aeea038ac4bd97c68ea73756a610
David Santos <david.santos@ist.utl.pt>
2003-12-18 16:59:01 +0000

    *** empty log message ***

--- src/ServidorAplicacao/Servico/commons/student/GetEnrolmentGrade.java
+++ src/ServidorAplicacao/Servico/commons/student/GetEnrolmentGrade.java
@@ -1,9 +1,6 @@
 package ServidorAplicacao.Servico.commons.student;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.beanutils.BeanComparator;
@@ -18,20 +15,33 @@ import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IPersistentEmployee;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
+import Util.EnrolmentEvaluationType;
 import Util.EnrolmentState;
-import Util.MarkType;
 
-
+/**
+ * 
+ * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
+ * @author Joana Mota (jccm@rnl.ist.utl.pt)
+ */
 public class GetEnrolmentGrade implements IServico {
 
 	private static GetEnrolmentGrade servico = new GetEnrolmentGrade();
 
+	/**
+	 * The singleton access method of this class.
+	 **/
 	public static GetEnrolmentGrade getService() {
 		return servico;
 	}
 
+	/**
+	 * The actor of this class.
+	 **/
 	private GetEnrolmentGrade() {
 	}
+
+	/**
+	 * Returns The Service Name */
 
 	public final String getNome() {
 		return "GetEnrolmentGrade";
@@ -45,121 +55,177 @@ public class GetEnrolmentGrade implements IServico {
 			return null;
 		}
 
-		if (enrolmentEvaluations.size() == 1)
-		{
+		// if there's only one evaluation ...
+		if (enrolmentEvaluations.size() == 1) {
+
 			IEnrolmentEvaluation enrolmentEvaluation = (IEnrolmentEvaluation) enrolmentEvaluations.get(0);
-			return this.getInfoLatestEvaluation(enrolmentEvaluation);
-		} else
-		{
-			IEnrolmentEvaluation enrolmentEvaluation = this.getRealEnrollmentEvaluation(enrolment);
-			return this.getInfoLatestEvaluation(enrolmentEvaluation);
+
+			try {
+				enrolmentEvaluation.setGrade((new Integer(enrolmentEvaluation.getGrade())).toString());
+
+			} catch (NumberFormatException e) {
+
+			}
+			InfoEnrolmentEvaluation infolatestEvaluation = Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(enrolmentEvaluation);
+			if (enrolmentEvaluation.getEmployee() != null) {
+
+				try {
+					if (String.valueOf(enrolmentEvaluation.getEmployee().getIdInternal()) != null
+						|| String.valueOf(enrolmentEvaluation.getEmployee().getIdInternal()).length() > 0) {
+						IEmployee employee = readEmployee(enrolmentEvaluation.getEmployee().getIdInternal().intValue());
+						enrolmentEvaluation.setEmployee(employee);
+						infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+					}
+				} catch (ExcepcaoPersistencia e1) {
+					throw new FenixServiceException(e1);
+				}
+			}
+			return infolatestEvaluation;
+
 		}
+
+		BeanComparator dateComparator = new BeanComparator("when");
+
+		Collections.sort(enrolmentEvaluations, dateComparator);
+		Collections.reverse(enrolmentEvaluations);
+
+		IEnrolmentEvaluation latestEvaluation = (IEnrolmentEvaluation) enrolmentEvaluations.get(0);
+
+		if (enrolment.getEnrolmentState().equals(EnrolmentState.NOT_APROVED)) {
+			return getInfoLatestEvaluation(latestEvaluation);
+		}
+
+		// if the last evaluation is Not of "IMPROVEMENT" type
+		if (!latestEvaluation.getEnrolmentEvaluationType().equals(EnrolmentEvaluationType.IMPROVEMENT_OBJ)) {
+			try {
+				latestEvaluation.setGrade((new Integer(latestEvaluation.getGrade())).toString());
+			} catch (NumberFormatException e) {
+
+			}
+
+			InfoEnrolmentEvaluation infolatestEvaluation = Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(latestEvaluation);
+			try {
+				if (latestEvaluation.getEmployee() != null) {
+					if (String.valueOf(latestEvaluation.getEmployee().getIdInternal()) != null
+						|| String.valueOf(latestEvaluation.getEmployee().getIdInternal()).length() > 0) {
+						IEmployee employee = readEmployee(latestEvaluation.getEmployee().getIdInternal().intValue());
+						latestEvaluation.setEmployee(employee);
+						infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+					}
+				}
+			} catch (ExcepcaoPersistencia e1) {
+				throw new FenixServiceException(e1);
+			}
+			return infolatestEvaluation;
+		} else {
+			IEnrolmentEvaluation previousEvaluation = null;
+			for (int i = 1; i < enrolmentEvaluations.size(); i++) {
+				previousEvaluation = (IEnrolmentEvaluation) enrolmentEvaluations.get(i);
+				if (!previousEvaluation.getEnrolmentEvaluationType().equals(EnrolmentEvaluationType.IMPROVEMENT_OBJ)) {
+					break;
+				}
+			}
+
+			Integer latestMark = new Integer(0);
+			try {
+				latestMark = Integer.valueOf(latestEvaluation.getGrade());
+			} catch (NumberFormatException e) {
+				// If there's an Exception , the the student wasn't able to improve
+				//exception is to be ignored				
+			}
+
+			// if there is no exception we must check wick is the higher grade
+
+			Integer previousMark = new Integer(0);
+			try {
+				previousMark = Integer.valueOf(previousEvaluation.getGrade());
+			} catch (NumberFormatException e) {
+//				latestEvaluation.setGrade((new Integer(latestEvaluation.getGrade())).toString());
+				InfoEnrolmentEvaluation infolatestEvaluation = Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(latestEvaluation);
+				try {
+
+					if (latestEvaluation.getEmployee() != null) {
+						if (String.valueOf(latestEvaluation.getEmployee().getIdInternal()) != null
+							|| String.valueOf(latestEvaluation.getEmployee().getIdInternal()).length() > 0) {
+							IEmployee employee = readEmployee(latestEvaluation.getEmployee().getIdInternal().intValue());
+							latestEvaluation.setEmployee(employee);
+							infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+						}
+					}
+					return infolatestEvaluation;
+				} catch (ExcepcaoPersistencia e1) {
+					throw new FenixServiceException(e1);
+				}
+
+			}
+
+			if (previousMark.intValue() >= latestMark.intValue()) {
+				try {
+					previousEvaluation.setGrade((new Integer(previousEvaluation.getGrade())).toString());
+					InfoEnrolmentEvaluation infolatestEvaluation =
+						Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(previousEvaluation);
+					if (previousEvaluation.getEmployee() != null) {
+						if (String.valueOf(previousEvaluation.getEmployee().getIdInternal()) != null
+							|| String.valueOf(previousEvaluation.getEmployee().getIdInternal()).length() > 0) {
+							IEmployee employee = readEmployee(previousEvaluation.getEmployee().getIdInternal().intValue());
+							latestEvaluation.setEmployee(employee);
+							infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+						}
+					}
+					return infolatestEvaluation;
+				} catch (ExcepcaoPersistencia e1) {
+					throw new FenixServiceException(e1);
+				}
+
+			} else {
+				try {
+					latestEvaluation.setGrade((new Integer(latestEvaluation.getGrade())).toString());
+					InfoEnrolmentEvaluation infolatestEvaluation =
+						Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(latestEvaluation);
+					if (latestEvaluation.getEmployee() != null) {
+						if (String.valueOf(latestEvaluation.getEmployee().getIdInternal()) != null
+							|| String.valueOf(latestEvaluation.getEmployee().getIdInternal()).length() > 0) {
+							IEmployee employee = readEmployee(latestEvaluation.getEmployee().getIdInternal().intValue());
+							latestEvaluation.setEmployee(employee);
+							infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+						}
+					}
+					return infolatestEvaluation;
+				} catch (ExcepcaoPersistencia e1) {
+					throw new FenixServiceException(e1);
+				}
+			}
+		}
+
 	}
 
-	/**
-	 * @param latestEvaluation
-	 * @return
-	 * @throws FenixServiceException
-	 */
 	private InfoEnrolmentEvaluation getInfoLatestEvaluation(IEnrolmentEvaluation latestEvaluation) throws FenixServiceException {
 		InfoEnrolmentEvaluation infolatestEvaluation = Cloner.copyIEnrolmentEvaluation2InfoEnrolmentEvaluation(latestEvaluation);
 		if (latestEvaluation.getEmployee() != null) {
-			IEmployee employee = readEmployee(latestEvaluation.getEmployee().getIdInternal());
-			infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+			if (String.valueOf(latestEvaluation.getEmployee().getIdInternal()) != null
+				|| String.valueOf(latestEvaluation.getEmployee().getIdInternal()).length() > 0) {
+				try {
+					IEmployee employee = readEmployee(latestEvaluation.getEmployee().getIdInternal().intValue());
+					latestEvaluation.setEmployee(employee);
+					infolatestEvaluation.setInfoEmployee(Cloner.copyIPerson2InfoPerson(employee.getPerson()));
+				} catch (ExcepcaoPersistencia e) {
+					throw new FenixServiceException(e);
+				}
+			}
 		}
 		return infolatestEvaluation;
 	}
 
-	/**
-	 * @param id
-	 * @return
-	 * @throws ExcepcaoPersistencia
-	 */
-	private IEmployee readEmployee(Integer id) throws FenixServiceException {
+	private IEmployee readEmployee(int id) throws ExcepcaoPersistencia {
 		IEmployee employee = null;
 		IPersistentEmployee persistentEmployee;
 		try {
 			persistentEmployee = SuportePersistenteOJB.getInstance().getIPersistentEmployee();
-			employee = persistentEmployee.readByIdInternal(id.intValue());
+			employee = persistentEmployee.readByIdInternal(id);
 		} catch (ExcepcaoPersistencia e) {
 			e.printStackTrace();
-			throw new FenixServiceException(e);
 		}
 		return employee;
 	}
 
-	/**
-	 * @param enrolment
-	 * @return
-	 */
-	private IEnrolmentEvaluation getRealEnrollmentEvaluation(IEnrolment enrolment)
-	{
-		List enrollmentEvaluationsList = enrolment.getEvaluations();
-		IEnrolmentEvaluation enrolmentEvaluationToReturn = null;
-
-		if (enrolment.getEnrolmentState().equals(EnrolmentState.NOT_APROVED))
-		{
-			Iterator iterator = enrollmentEvaluationsList.iterator();
-			while (iterator.hasNext())
-			{
-				IEnrolmentEvaluation enrolmentEvaluation = (IEnrolmentEvaluation) iterator.next();
-				List aux = Arrays.asList(MarkType.REP_MARKS);
-				if(aux.contains(enrolmentEvaluation.getGrade()))
-				{
-					enrolmentEvaluationToReturn = enrolmentEvaluation;
-				}
-			}
-			return enrolmentEvaluationToReturn;
-		} else if (enrolment.getEnrolmentState().equals(EnrolmentState.NOT_EVALUATED))
-		{
-			Iterator iterator = enrollmentEvaluationsList.iterator();
-			while (iterator.hasNext())
-			{
-				IEnrolmentEvaluation enrolmentEvaluation = (IEnrolmentEvaluation) iterator.next();
-				List aux = Arrays.asList(MarkType.NA_MARKS);
-				if(aux.contains(enrolmentEvaluation.getGrade()) || enrolmentEvaluation.getGrade().equals(""))
-				{
-					enrolmentEvaluationToReturn = enrolmentEvaluation;
-				}
-			}
-		} else if (enrolment.getEnrolmentState().equals(EnrolmentState.APROVED))
-		{
-			List enrolmentEvaluationsToAnalyse = new ArrayList();
-			enrolmentEvaluationsToAnalyse.addAll(enrollmentEvaluationsList);
-			Iterator iterator = enrollmentEvaluationsList.iterator();
-			while (iterator.hasNext())
-			{
-				IEnrolmentEvaluation enrolmentEvaluation = (IEnrolmentEvaluation) iterator.next();
-				List aux = Arrays.asList(MarkType.OTHER_MARKS);
-				List enrolmentEvaluationsToRemove = new ArrayList();
-				if(aux.contains(enrolmentEvaluation.getGrade()) || enrolmentEvaluation.getGrade().equals(""))
-				{
-					enrolmentEvaluationsToRemove.add(enrolmentEvaluation);
-				}
-				enrolmentEvaluationsToAnalyse.removeAll(enrolmentEvaluationsToRemove);
-			}
-
-			if(!enrolmentEvaluationsToAnalyse.isEmpty())
-			{
-				BeanComparator dateComparator = new BeanComparator("grade");
-				Collections.sort(enrolmentEvaluationsToAnalyse, dateComparator);
-				Collections.reverse(enrolmentEvaluationsToAnalyse);
-				enrolmentEvaluationToReturn = (IEnrolmentEvaluation) enrolmentEvaluationsToAnalyse.get(0);
-			} else
-			{
-				iterator = enrollmentEvaluationsList.iterator();
-				while (iterator.hasNext())
-				{
-					IEnrolmentEvaluation enrolmentEvaluation = (IEnrolmentEvaluation) iterator.next();
-					List aux = Arrays.asList(MarkType.AP_MARKS);
-					if(aux.contains(enrolmentEvaluation.getGrade()))
-					{
-						enrolmentEvaluationToReturn = enrolmentEvaluation;
-					}
-				}
-			}
-		}
-
-		return enrolmentEvaluationToReturn;
-	}
 }
