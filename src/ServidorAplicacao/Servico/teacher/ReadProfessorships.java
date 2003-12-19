5c2e0a2e4784d2a1caa8dd150ecd93319f3b21f4
João Luz <joao.luz@ist.utl.pt>
2003-12-19 21:20:08 +0000

    extends ReadDetailedTeacherProfessorshipsAbstractService

--- src/ServidorAplicacao/Servico/teacher/ReadProfessorships.java
+++ src/ServidorAplicacao/Servico/teacher/ReadProfessorships.java
@@ -5,23 +5,10 @@
 package ServidorAplicacao.Servico.teacher;
 
 import java.util.List;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-
-import pt.utl.ist.berserk.logic.serviceManager.IService;
-import DataBeans.InfoCurricularCourse;
-import DataBeans.InfoProfessorship;
-import DataBeans.teacher.professorship.DetailedProfessorship;
-import DataBeans.util.Cloner;
-import Dominio.ICurricularCourse;
-import Dominio.IExecutionCourse;
-import Dominio.IProfessorship;
-import Dominio.IResponsibleFor;
 import Dominio.ITeacher;
-import Dominio.ResponsibleFor;
 import ServidorAplicacao.IUserView;
 import ServidorAplicacao.Servico.exceptions.FenixServiceException;
+import ServidorAplicacao.Servico.teacher.professorship.ReadDetailedTeacherProfessorshipsAbstractService;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IPersistentProfessorship;
 import ServidorPersistente.IPersistentResponsibleFor;
@@ -32,79 +19,28 @@ import ServidorPersistente.OJB.SuportePersistenteOJB;
 /**
  * @author jpvl
  */
-public class ReadProfessorships implements IService
+public class ReadProfessorships extends ReadDetailedTeacherProfessorshipsAbstractService
 {
     public ReadProfessorships()
     {
     }
 
-    public List run(IUserView userView) throws FenixServiceException
+    public List run( IUserView userView ) throws FenixServiceException
     {
         try
         {
             ISuportePersistente persistentSuport;
             persistentSuport = SuportePersistenteOJB.getInstance();
             IPersistentResponsibleFor responsibleForDAO = persistentSuport
-                    .getIPersistentResponsibleFor();
-
+                            .getIPersistentResponsibleFor();
             IPersistentProfessorship persistentProfessorship = persistentSuport
-                    .getIPersistentProfessorship();
+                            .getIPersistentProfessorship();
             IPersistentTeacher teacherDAO = persistentSuport.getIPersistentTeacher();
             ITeacher teacher = teacherDAO.readTeacherByUsername(userView.getUtilizador());
-
             List professorships = persistentProfessorship.readByTeacher(teacher);
             final List responsibleFors = responsibleForDAO.readByTeacher(teacher);
-
-            List detailedProfessorshipList = (List) CollectionUtils.collect(professorships,
-                    new Transformer()
-                    {
-
-                        public Object transform(Object input)
-                        {
-                            IProfessorship professorship = (IProfessorship) input;
-                            InfoProfessorship infoProfessorShip = Cloner
-                                    .copyIProfessorship2InfoProfessorship(professorship);
-                            IResponsibleFor responsibleFor = new ResponsibleFor();
-
-                            responsibleFor.setExecutionCourse(professorship.getExecutionCourse());
-                            responsibleFor.setTeacher(professorship.getTeacher());
-
-                            
-
-                            List executionCourseCurricularCoursesList = getInfoCurricularCourses(professorship
-                                    .getExecutionCourse());
-
-                            DetailedProfessorship detailedProfessorship = new DetailedProfessorship();
-
-                            detailedProfessorship.setResponsibleFor(Boolean.valueOf(responsibleFors.contains(responsibleFor)));
-                            
-                            detailedProfessorship.setInfoProfessorship(infoProfessorShip);
-                            detailedProfessorship
-                                    .setExecutionCourseCurricularCoursesList(executionCourseCurricularCoursesList);
-
-                            return detailedProfessorship;
-                        }
-
-                        private List getInfoCurricularCourses(IExecutionCourse executionCourse)
-                        {
-
-                            List infoCurricularCourses = (List) CollectionUtils.collect(executionCourse
-                                    .getAssociatedCurricularCourses(),
-                                    new Transformer()
-                                    {
-
-                                        public Object transform(Object input)
-                                        {
-                                            ICurricularCourse curricularCourse = (ICurricularCourse) input;
-                                            InfoCurricularCourse infoCurricularCourse = Cloner
-                                                    .copyCurricularCourse2InfoCurricularCourse(curricularCourse);
-                                            return infoCurricularCourse;
-                                        }
-                                    });
-                            return infoCurricularCourses;
-                        }
-                    });
-
+            List detailedProfessorshipList = getDetailedProfessorships(professorships, responsibleFors,
+                            persistentSuport);
             return detailedProfessorshipList;
         }
         catch (ExcepcaoPersistencia e)
@@ -113,5 +49,4 @@ public class ReadProfessorships implements IService
             throw new FenixServiceException("Problems on database!", e);
         }
     }
-
 }
