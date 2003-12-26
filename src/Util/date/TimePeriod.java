0e4e533763b4b2a50a3b1366672d8d3094e2411d
João Luz <joao.luz@ist.utl.pt>
2003-12-26 17:58:15 +0000

    this class maps a time period (hours and minutes)

---
+++ src/Util/date/TimePeriod.java
@@ -0,0 +1,57 @@
+/*
+ * Created on Dec 26, 2003 by jpvl
+ *  
+ */
+package Util.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author jpvl
+ */
+public class TimePeriod
+{
+    private long start;
+
+    private long end;
+
+    public TimePeriod( long start, long end )
+    {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     *  
+     */
+    public TimePeriod( Date start, Date end )
+    {
+        this(start.getTime(), end.getTime());
+    }
+
+    public TimePeriod( Calendar start, Calendar end )
+    {
+        this(start.getTimeInMillis(), end.getTimeInMillis());
+    }
+
+    public Double hours()
+    {
+        Calendar endCalendar = Calendar.getInstance();
+
+        endCalendar.setTimeInMillis(this.end);
+
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTimeInMillis(start);
+
+        endCalendar.roll(Calendar.HOUR_OF_DAY, -(startCalendar.get(Calendar.HOUR_OF_DAY)));
+        endCalendar.roll(Calendar.MINUTE, -(startCalendar.get(Calendar.MINUTE)));
+
+        int hours = endCalendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = endCalendar.get(Calendar.MINUTE);
+
+        double minutesInHours = (double) minutes / 60.0;
+
+        return new Double((double) hours + minutesInHours);
+    }
+}
