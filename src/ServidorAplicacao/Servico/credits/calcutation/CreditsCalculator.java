9986e77cfd5dcd420798952de3c611c3584922f9
João Luz <joao.luz@ist.utl.pt>
2003-12-26 17:55:42 +0000

    second version

--- src/ServidorAplicacao/Servico/credits/calcutation/CreditsCalculator.java
+++ src/ServidorAplicacao/Servico/credits/calcutation/CreditsCalculator.java
@@ -4,11 +4,8 @@
  */
 package ServidorAplicacao.Servico.credits.calcutation;
 
-import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
-
-
 
 import Dominio.IExecutionPeriod;
 import Dominio.ISupportLesson;
@@ -17,7 +14,10 @@ import Dominio.degree.finalProject.ITeacherDegreeFinalProjectStudent;
 import Dominio.teacher.workTime.ITeacherInstitutionWorkTime;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.degree.finalProject.IPersistentTeacherDegreeFinalProjectStudent;
 import ServidorPersistente.teacher.professorship.IPersistentSupportLesson;
+import ServidorPersistente.teacher.workingTime.IPersistentTeacherInstitutionWorkingTime;
+import Util.date.TimePeriod;
 
 /**
  * @author jpvl
@@ -25,7 +25,7 @@ import ServidorPersistente.teacher.professorship.IPersistentSupportLesson;
 public class CreditsCalculator
 {
     private CreditsCalculator calculator = new CreditsCalculator();
-    
+
     public CreditsCalculator getInstance()
     {
         return calculator;
@@ -39,62 +39,202 @@ public class CreditsCalculator
         super();
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param supportLesson
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calculateSupportLessonsAfterInsert( ITeacher teacher,
                     IExecutionPeriod executionPeriod, ISupportLesson supportLesson,
                     ISuportePersistente sp ) throws ExcepcaoPersistencia
     {
-        IPersistentSupportLesson supportLessonDAO= sp.getIPersistentSupportLesson();
-        
-        List supportLessonList = supportLessonDAO.readByTeacherAndExecutionPeriod(teacher, executionPeriod);
-        
+        IPersistentSupportLesson supportLessonDAO = sp.getIPersistentSupportLesson();
+
+        List supportLessonList = supportLessonDAO.readByTeacherAndExecutionPeriod(teacher,
+                        executionPeriod);
+
         supportLessonList.remove(supportLesson);
-        
+
         Iterator iterator = supportLessonList.iterator();
+
+        TimePeriod timePeriod = new TimePeriod(supportLesson.getStartTime(), supportLesson.getEndTime());
+        double hours = timePeriod.hours().doubleValue();
+
         while (iterator.hasNext())
         {
             ISupportLesson sl = (ISupportLesson) iterator.next();
-            Calendar calendar =Calendar.getInstance();
-            
-            calendar.setTime(sl.getEndTime());
-            
+            if (!sl.getIdInternal().equals(supportLesson.getIdInternal()))
+            {
+                TimePeriod timePeriod2 = new TimePeriod(sl.getStartTime(), sl.getEndTime());
+                hours += timePeriod2.hours().doubleValue();
+            }
         }
-        
-        
-        return null;
+
+        return new Double(hours + timePeriod.hours().doubleValue());
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param supportLesson
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calculateSupportLessonsAfterDelete( ITeacher teacher,
                     IExecutionPeriod executionPeriod, ISupportLesson supportLesson,
-                    ISuportePersistente sp )
+                    ISuportePersistente sp ) throws ExcepcaoPersistencia
     {
-        return null;
+        IPersistentSupportLesson supportLessonDAO = sp.getIPersistentSupportLesson();
+
+        List supportLessonList = supportLessonDAO.readByTeacherAndExecutionPeriod(teacher,
+                        executionPeriod);
+
+        supportLessonList.remove(supportLesson);
+
+        Iterator iterator = supportLessonList.iterator();
+        double hours = 0;
+        while (iterator.hasNext())
+        {
+            ISupportLesson sl = (ISupportLesson) iterator.next();
+            if (!sl.getIdInternal().equals(supportLesson.getIdInternal()))
+            {
+                TimePeriod timePeriod = new TimePeriod(sl.getStartTime(), sl.getEndTime());
+                hours += timePeriod.hours().doubleValue();
+            }
+        }
+        return new Double(hours);
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param degreeFinalProjectStudent
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calcuteDegreeFinalProjectStudentAfterInsert( ITeacher teacher,
                     IExecutionPeriod executionPeriod,
                     ITeacherDegreeFinalProjectStudent degreeFinalProjectStudent, ISuportePersistente sp )
+                    throws ExcepcaoPersistencia
     {
-        return null;
+
+        IPersistentTeacherDegreeFinalProjectStudent teacherDegreeFinalProjectStudentDAO = sp
+                        .getIPersistentTeacherDegreeFinalProjectStudent();
+        List teacherDegreeFinalProjectStudents = teacherDegreeFinalProjectStudentDAO
+                        .readByTeacherAndExecutionYear(teacher, executionPeriod.getExecutionYear());
+
+        double numberOfStudents = (double) teacherDegreeFinalProjectStudents.size();
+        if (!teacherDegreeFinalProjectStudents.contains(degreeFinalProjectStudent))
+        {
+            numberOfStudents++;
+        }
+
+        return new Double(numberOfStudents);
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param degreeFinalProjectStudent
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calcuteDegreeFinalProjectStudentAfterDelete( ITeacher teacher,
                     IExecutionPeriod executionPeriod,
                     ITeacherDegreeFinalProjectStudent degreeFinalProjectStudent, ISuportePersistente sp )
+                    throws ExcepcaoPersistencia
     {
-        return null;
+        IPersistentTeacherDegreeFinalProjectStudent teacherDegreeFinalProjectStudentDAO = sp
+                        .getIPersistentTeacherDegreeFinalProjectStudent();
+        List teacherDegreeFinalProjectStudents = teacherDegreeFinalProjectStudentDAO
+                        .readByTeacherAndExecutionYear(teacher, executionPeriod.getExecutionYear());
+
+        double numberOfStudents = (double) teacherDegreeFinalProjectStudents.size();
+        if (teacherDegreeFinalProjectStudents.contains(degreeFinalProjectStudent))
+        {
+            numberOfStudents--;
+        }
+
+        return new Double(numberOfStudents);
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param teacherInstitutionWorkTime
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calcuteTeacherInstitutionWorkingTimeAfterInsert( ITeacher teacher,
                     IExecutionPeriod executionPeriod,
                     ITeacherInstitutionWorkTime teacherInstitutionWorkTime, ISuportePersistente sp )
+                    throws ExcepcaoPersistencia
     {
-        return null;
+        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkTimeDAO = sp
+                        .getIPersistentTeacherInstitutionWorkingTime();
+
+        List teacherInstitutionWorkTimes = teacherInstitutionWorkTimeDAO
+                        .readByTeacherAndExecutionPeriod(teacher, executionPeriod);
+
+        teacherInstitutionWorkTimes.remove(teacherInstitutionWorkTime);
+
+        Iterator iterator = teacherInstitutionWorkTimes.iterator();
+        TimePeriod timePeriod = new TimePeriod(teacherInstitutionWorkTime.getStartTime(),
+                        teacherInstitutionWorkTime.getEndTime());
+        double hours = timePeriod.hours().doubleValue();
+        while (iterator.hasNext())
+        {
+            ITeacherInstitutionWorkTime tiwt = (ITeacherInstitutionWorkTime) iterator.next();
+            if (!tiwt.getIdInternal().equals(teacherInstitutionWorkTime.getIdInternal()))
+            {
+                TimePeriod timePeriod2 = new TimePeriod(tiwt.getStartTime(), tiwt.getEndTime());
+                hours += timePeriod2.hours().doubleValue();
+            }
+        }
+
+        return new Double(hours);
     }
 
+    /**
+     * @param teacher
+     * @param executionPeriod
+     * @param teacherInstitutionWorkTime
+     * @param sp
+     * @return @throws
+     *              ExcepcaoPersistencia
+     */
     public Double calcuteTeacherInstitutionWorkingTimeAfterDelete( ITeacher teacher,
                     IExecutionPeriod executionPeriod,
                     ITeacherInstitutionWorkTime teacherInstitutionWorkTime, ISuportePersistente sp )
+                    throws ExcepcaoPersistencia
     {
-        return null;
+        IPersistentTeacherInstitutionWorkingTime teacherInstitutionWorkingTimeDAO = sp
+                        .getIPersistentTeacherInstitutionWorkingTime();
+
+        List teacherInstitutionWorkTimes = teacherInstitutionWorkingTimeDAO
+                        .readByTeacherAndExecutionPeriod(teacher, executionPeriod);
+
+        teacherInstitutionWorkTimes.remove(teacherInstitutionWorkTime);
+
+        Iterator iterator = teacherInstitutionWorkTimes.iterator();
+        double hours = 0;
+        while (iterator.hasNext())
+        {
+            ITeacherInstitutionWorkTime tiwt = (ITeacherInstitutionWorkTime) iterator.next();
+            if (!tiwt.getIdInternal().equals(teacherInstitutionWorkTime.getIdInternal()))
+            {
+                TimePeriod timePeriod = new TimePeriod(tiwt.getStartTime(), tiwt.getEndTime());
+                hours += timePeriod.hours().doubleValue();
+            }
+        }
+
+        return new Double(hours);
     }
 }
