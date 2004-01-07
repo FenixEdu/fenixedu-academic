e7d6740ca4f67107ee3d5e3ee0ffaeaf3ee6c45a
Ângela Almeida <angela.almeida@ist.utl.pt>
2004-01-07 15:58:06 +0000

    *** empty log message ***

--- docs/alterTables.sql
+++ docs/alterTables.sql
@@ -139,6 +139,7 @@ alter table Turmas add ACK_OPT_LOCK int(11) not null default '1';
 alter table TESTS add ACK_OPT_LOCK int(11) not null default '1';
 alter table mw_ENROLMENT_temp add ACK_OPT_LOCK int(11) not null default '1';
 alter table mw_ALUNO_temp add ACK_OPT_LOCK int(11) not null default '1';
+alter table WORK_LOCATION add ACK_OPT_LOCK int(11) not null default '1';
 
 -- alter table posgrad_pessoa_cargo add ACK_OPT_LOCK int(11) not null default '1';
 -- alter table ALMEIDA_ESCOLA add ACK_OPT_LOCK int(11) not null default '1';
