2d76a8811546a2fd995357913611e8d0e2c49b59
David Santos <david.santos@ist.utl.pt>
2004-01-08 17:46:05 +0000

    *** empty log message ***

--- etc/middleware/allEnrollmentsMigration/dif.sql
+++ etc/middleware/allEnrollmentsMigration/dif.sql
@@ -1,5 +1,36 @@
-CREATE INDEX mw_ENROLMENT_INDEX_1 on mw_ENROLMENT_1(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
-CREATE INDEX mw_ENROLMENT_INDEX_1 on mw_ENROLMENT_2(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
+-- RENAME TABLE mw_ENROLMENT_temp TO mw_ENROLMENT_AUXILIARY_TABLE_1;
+-- RENAME TABLE mw_ALUNO_temp TO mw_STUDENT_AUXILIARY_TABLE;
+-- RENAME TABLE mw_ALUNO TO mw_STUDENT;
+-- RENAME TABLE mw_CURRICULAR_COURSE_SCOPE_temp TO mw_CURRICULAR_COURSE_SCOPE_AUXILIARY_TABLE;
+-- RENAME TABLE mw_PESSOA TO mw_PERSON;
+
+CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_1 on mw_ENROLMENT_AUXILIARY_TABLE_1(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
+CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_2 on mw_ENROLMENT_AUXILIARY_TABLE_1(enrolmentYear,degreeCode);
+CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_3 on mw_ENROLMENT_AUXILIARY_TABLE_1(number);
+CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_4 on mw_ENROLMENT_AUXILIARY_TABLE_1(enrolmentYear);
+
+CREATE INDEX mw_STUDENT_AUXILIARY_TABLE_INDEX_1 on mw_STUDENT_AUXILIARY_TABLE(number);
+
+drop table if exists mw_ENROLMENT_AUXILIARY_TABLE_2;
+create table mw_ENROLMENT_AUXILIARY_TABLE_2
+select * from mw_ENROLMENT_AUXILIARY_TABLE_1 where enrolmentYear <> 2003;
+
+CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_2_INDEX_1 on mw_ENROLMENT_AUXILIARY_TABLE_2(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+drop table if exists mw_ENROLMENT;
+create table mw_ENROLMENT
+select * from mw_ENROLMENT_AUXILIARY_TABLE_1 where degreeCode = 1 and enrolmentYear <> 2003;
+
+drop table if exists mw_STUDENT;
+create table mw_STUDENT
+select mwa.* from mw_STUDENT_AUXILIARY_TABLE mwa inner join mw_ENROLMENT mwe on mwa.number = mwe.number;
+
+CREATE INDEX mw_ENROLMENT_INDEX_1 on mw_ENROLMENT(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
+CREATE INDEX mw_STUDENT_INDEX_1 on mw_STUDENT(number);
+
+-----------------------------------------------------------------------------------------------------------------------------
 
 select * from mw_ENROLMENT_1 mwe1 left join mw_ENROLMENT_2 mwe2 on
 mwe1.number = mwe2.number and
@@ -17,34 +48,3 @@ mwe1.universityCode = mwe2.universityCode and
 mwe1.remarks = mwe2.remarks
 where
 mwe2.number is null;
-
-
-
--- RENAME TABLE mw_ENROLMENT_temp TO mw_ENROLMENT_AUXILIARY_TABLE_1;
--- RENAME TABLE mw_ALUNO_temp TO mw_STUDENT_AUXILIARY_TABLE;
--- RENAME TABLE mw_ALUNO TO mw_STUDENT;
--- RENAME TABLE mw_CURRICULAR_COURSE_SCOPE_temp TO mw_CURRICULAR_COURSE_SCOPE_AUXILIARY_TABLE;
--- RENAME TABLE mw_PESSOA TO mw_PERSON;
-
-drop table if exists mw_ENROLMENT_AUXILIARY_TABLE_2;
-create table mw_ENROLMENT_AUXILIARY_TABLE_2
-select * from mw_ENROLMENT_AUXILIARY_TABLE_1 where enrolmentYear <> 2003;
-
-CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_2_INDEX_1 on mw_ENROLMENT_AUXILIARY_TABLE_2(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
-
-CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_1 on mw_ENROLMENT_AUXILIARY_TABLE_1(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
-CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_2 on mw_ENROLMENT_AUXILIARY_TABLE_1(enrolmentYear,degreeCode);
-CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_3 on mw_ENROLMENT_AUXILIARY_TABLE_1(number);
-
-CREATE INDEX mw_STUDENT_AUXILIARY_TABLE_INDEX_1 on mw_STUDENT_AUXILIARY_TABLE(number);
-
-drop table if exists mw_ENROLMENT;
-create table mw_ENROLMENT
-select * from mw_ENROLMENT_AUXILIARY_TABLE_1 where degreeCode = 1 and enrolmentYear <> 2003;
-
-drop table if exists mw_STUDENT;
-create table mw_STUDENT
-select mwa.* from mw_STUDENT_AUXILIARY_TABLE mwa inner join mw_ENROLMENT mwe on mwa.number = mwe.number;
-
-CREATE INDEX mw_ENROLMENT_INDEX_1 on mw_ENROLMENT(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
-CREATE INDEX mw_STUDENT_INDEX_1 on mw_STUDENT(number);
