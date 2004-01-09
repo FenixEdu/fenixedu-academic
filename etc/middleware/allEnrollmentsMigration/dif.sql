d63234552632e61aa1b5745232c341c12ccd82ee
David Santos <david.santos@ist.utl.pt>
2004-01-09 10:56:02 +0000

    *** empty log message ***

--- etc/middleware/allEnrollmentsMigration/dif.sql
+++ etc/middleware/allEnrollmentsMigration/dif.sql
@@ -4,6 +4,9 @@
 -- RENAME TABLE mw_CURRICULAR_COURSE_SCOPE_temp TO mw_CURRICULAR_COURSE_SCOPE_AUXILIARY_TABLE;
 -- RENAME TABLE mw_PESSOA TO mw_PERSON;
 
+-- ALTER TABLE mw_ENROLMENT_AUXILIARY_TABLE_1 DROP INDEX I1;
+-- ALTER TABLE mw_ENROLMENT_AUXILIARY_TABLE_1 DROP INDEX I2;
+
 CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_1 on mw_ENROLMENT_AUXILIARY_TABLE_1(number,enrolmentYear,curricularCourseYear,curricularCourseSemester,season,courseCode,degreeCode,branchCode,grade,teacherNumber,examDate,universityCode,remarks);
 CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_2 on mw_ENROLMENT_AUXILIARY_TABLE_1(enrolmentYear,degreeCode);
 CREATE INDEX mw_ENROLMENT_AUXILIARY_TABLE_1_INDEX_3 on mw_ENROLMENT_AUXILIARY_TABLE_1(number);
@@ -32,7 +35,7 @@ CREATE INDEX mw_STUDENT_INDEX_1 on mw_STUDENT(number);
 
 -----------------------------------------------------------------------------------------------------------------------------
 
-select * from mw_ENROLMENT_1 mwe1 left join mw_ENROLMENT_2 mwe2 on
+select mwe1.* from mw_ENROLMENT_AUXILIARY_TABLE_1 mwe1 left join mw_ENROLMENT_AUXILIARY_TABLE_2 mwe2 on
 mwe1.number = mwe2.number and
 mwe1.enrolmentYear = mwe2.enrolmentYear and
 mwe1.curricularCourseYear = mwe2.curricularCourseYear and
