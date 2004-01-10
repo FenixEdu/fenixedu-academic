8bddba620028e62270e1291bffe52be19ec5d96f
Fernanda Quitério <fsq@rnl.ist.utl.pt>
2004-01-10 14:38:22 +0000

    *** empty log message ***

--- src/DataBeans/InfoGratuityValues.java
+++ src/DataBeans/InfoGratuityValues.java
@@ -26,4 +26,180 @@ public class InfoGratuityValues extends InfoObject implements Serializable
 	
 	private List infoPaymentPhases;
 	
+	/**
+	 * @return Returns the anualValue.
+	 */
+	public Double getAnualValue()
+	{
+		return anualValue;
+	}
+
+	/**
+	 * @param anualValue The anualValue to set.
+	 */
+	public void setAnualValue(Double anualValue)
+	{
+		this.anualValue = anualValue;
+	}
+
+	/**
+	 * @return Returns the courseValue.
+	 */
+	public Double getCourseValue()
+	{
+		return courseValue;
+	}
+
+	/**
+	 * @param courseValue The courseValue to set.
+	 */
+	public void setCourseValue(Double courseValue)
+	{
+		this.courseValue = courseValue;
+	}
+
+	/**
+	 * @return Returns the creditValue.
+	 */
+	public Double getCreditValue()
+	{
+		return creditValue;
+	}
+
+	/**
+	 * @param creditValue The creditValue to set.
+	 */
+	public void setCreditValue(Double creditValue)
+	{
+		this.creditValue = creditValue;
+	}
+
+	/**
+	 * @return Returns the endPayment.
+	 */
+	public Date getEndPayment()
+	{
+		return endPayment;
+	}
+
+	/**
+	 * @param endPayment The endPayment to set.
+	 */
+	public void setEndPayment(Date endPayment)
+	{
+		this.endPayment = endPayment;
+	}
+
+	/**
+	 * @return Returns the finalProofValue.
+	 */
+	public Double getFinalProofValue()
+	{
+		return finalProofValue;
+	}
+
+	/**
+	 * @param finalProofValue The finalProofValue to set.
+	 */
+	public void setFinalProofValue(Double finalProofValue)
+	{
+		this.finalProofValue = finalProofValue;
+	}
+
+	/**
+	 * @return Returns the infoEmployee.
+	 */
+	public InfoEmployee getInfoEmployee()
+	{
+		return infoEmployee;
+	}
+
+	/**
+	 * @param infoEmployee The infoEmployee to set.
+	 */
+	public void setInfoEmployee(InfoEmployee infoEmployee)
+	{
+		this.infoEmployee = infoEmployee;
+	}
+
+	/**
+	 * @return Returns the infoExecutionDegree.
+	 */
+	public InfoExecutionDegree getInfoExecutionDegree()
+	{
+		return infoExecutionDegree;
+	}
+
+	/**
+	 * @param infoExecutionDegree The infoExecutionDegree to set.
+	 */
+	public void setInfoExecutionDegree(InfoExecutionDegree infoExecutionDegree)
+	{
+		this.infoExecutionDegree = infoExecutionDegree;
+	}
+
+	/**
+	 * @return Returns the infoPaymentPhases.
+	 */
+	public List getInfoPaymentPhases()
+	{
+		return infoPaymentPhases;
+	}
+
+	/**
+	 * @param infoPaymentPhases The infoPaymentPhases to set.
+	 */
+	public void setInfoPaymentPhases(List infoPaymentPhases)
+	{
+		this.infoPaymentPhases = infoPaymentPhases;
+	}
+
+	/**
+	 * @return Returns the proofRequestPayment.
+	 */
+	public Boolean getProofRequestPayment()
+	{
+		return proofRequestPayment;
+	}
+
+	/**
+	 * @param proofRequestPayment The proofRequestPayment to set.
+	 */
+	public void setProofRequestPayment(Boolean proofRequestPayment)
+	{
+		this.proofRequestPayment = proofRequestPayment;
+	}
+
+	/**
+	 * @return Returns the scholarShipValue.
+	 */
+	public Double getScholarShipValue()
+	{
+		return scholarShipValue;
+	}
+
+	/**
+	 * @param scholarShipValue The scholarShipValue to set.
+	 */
+	public void setScholarShipValue(Double scholarShipValue)
+	{
+		this.scholarShipValue = scholarShipValue;
+	}
+
+	/**
+	 * @return Returns the startPayment.
+	 */
+	public Date getStartPayment()
+	{
+		return startPayment;
+	}
+
+	/**
+	 * @param startPayment The startPayment to set.
+	 */
+	public void setStartPayment(Date startPayment)
+	{
+		this.startPayment = startPayment;
+	}
+
 }
--- src/DataBeans/InfoPaymentPhase.java
+++ src/DataBeans/InfoPaymentPhase.java
@@ -17,4 +17,84 @@ public class InfoPaymentPhase extends InfoObject implements Serializable
 
 	private InfoGratuityValues infoGratuityValues;
 	
+	/**
+	 * @return Returns the description.
+	 */
+	public String getDescription()
+	{
+		return description;
+	}
+
+	/**
+	 * @param description The description to set.
+	 */
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	/**
+	 * @return Returns the endDate.
+	 */
+	public Date getEndDate()
+	{
+		return endDate;
+	}
+
+	/**
+	 * @param endDate The endDate to set.
+	 */
+	public void setEndDate(Date endDate)
+	{
+		this.endDate = endDate;
+	}
+
+	/**
+	 * @return Returns the infoGratuityValues.
+	 */
+	public InfoGratuityValues getInfoGratuityValues()
+	{
+		return infoGratuityValues;
+	}
+
+	/**
+	 * @param infoGratuityValues The infoGratuityValues to set.
+	 */
+	public void setInfoGratuityValues(InfoGratuityValues infoGratuityValues)
+	{
+		this.infoGratuityValues = infoGratuityValues;
+	}
+
+	/**
+	 * @return Returns the startDate.
+	 */
+	public Date getStartDate()
+	{
+		return startDate;
+	}
+
+	/**
+	 * @param startDate The startDate to set.
+	 */
+	public void setStartDate(Date startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	/**
+	 * @return Returns the value.
+	 */
+	public Float getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value The value to set.
+	 */
+	public void setValue(Float value)
+	{
+		this.value = value;
+	}
+
 }
