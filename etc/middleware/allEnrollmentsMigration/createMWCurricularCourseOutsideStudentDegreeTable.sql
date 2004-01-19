349a84e09c8fc685c19226e92cefd9b883df07e8
David Santos <david.santos@ist.utl.pt>
2004-01-19 16:04:27 +0000

    *** empty log message ***

--- etc/middleware/allEnrollmentsMigration/createMWCurricularCourseOutsideStudentDegreeTable.sql
+++ etc/middleware/allEnrollmentsMigration/createMWCurricularCourseOutsideStudentDegreeTable.sql
@@ -8,6 +8,7 @@ CREATE TABLE mw_CURRICULAR_COURSE_OUTSIDE_STUDENT_DEGREE (
   degreeCode int(20) NOT NULL default '0',
   keyCurricularCourse int(11) NOT NULL default '0',
   idInternal int(11) NOT NULL auto_increment,
+  ACK_OPT_LOCK int(11) not null default '1',
   PRIMARY KEY  (idInternal)
 ) TYPE=InnoDB;
 
@@ -15,27 +16,27 @@ CREATE TABLE mw_CURRICULAR_COURSE_OUTSIDE_STUDENT_DEGREE (
 -- Dumping data for table `mw_CURRICULAR_COURSE_OUTSIDE_STUDENT_DEGREE`
 --
 
-INSERT INTO mw_CURRICULAR_COURSE_OUTSIDE_STUDENT_DEGREE VALUES ('PY',2,2700,1),
-('UN',2,2733,2),
-('P5',2,2689,3),
-('QN',2,2707,4),
-('SF',2,2717,5),
-('AZ9',2,3101,6),
-('A01',2,2762,7),
-('AF2',2,2847,8),
-('AZ8',2,3586,9),
-('N5',2,2678,10),
-('EX',2,2649,11),
-('AT6',7,3014,12),
-('6F',7,3670,13),
-('ARI',8,2968,14),
-('AE2',9,3383,15),
-('AXB',9,3551,16),
-('AF1',9,2845,17),
-('IZ',9,3224,18),
-('6L',9,3193,19),
-('IV',9,3222,20),
-('0M',9,3174,21),
-('TU',9,3290,22),
-('AJA',10,3247,23);
+INSERT INTO mw_CURRICULAR_COURSE_OUTSIDE_STUDENT_DEGREE VALUES ('PY',2,2700,1,1),
+('UN',2,2733,2,1),
+('P5',2,2689,3,1),
+('QN',2,2707,4,1),
+('SF',2,2717,5,1),
+('AZ9',2,3101,6,1),
+('A01',2,2762,7,1),
+('AF2',2,2847,8,1),
+('AZ8',2,3586,9,1),
+('N5',2,2678,10,1),
+('EX',2,2649,11,1),
+('AT6',7,3014,12,1),
+('6F',7,3670,13,1),
+('ARI',8,2968,14,1),
+('AE2',9,3383,15,1),
+('AXB',9,3551,16,1),
+('AF1',9,2845,17,1),
+('IZ',9,3224,18,1),
+('6L',9,3193,19,1),
+('IV',9,3222,20,1),
+('0M',9,3174,21,1),
+('TU',9,3290,22,1),
+('AJA',10,3247,23,1);
 
--- etc/middleware/commands/migrateAllStudenCurricularPlans.bat
+++ etc/middleware/commands/migrateAllStudenCurricularPlans.bat
@@ -1,68 +1,35 @@
-REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree01.sql
-REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-01-#2.txt
+REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree01-1.sql
+REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-01-1-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree02.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-02-#2.txt
+REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree01-2.sql
+REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-01-2-#2.txt
 
-REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree03.sql
-REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-03-#2.txt
+REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree01-3.sql
+REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-01-3-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree05.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-05-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree03-1.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-03-1-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree06.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-06-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree03-2.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-03-2-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree07.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-07-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree03-3.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-03-3-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree08.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-08-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree10-1.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-10-1-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree09.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-09-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree10-2.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-10-2-#2.txt
 
-REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree10.sql
-REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-10-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree10-3.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-10-3-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree11.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-11-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree14-1.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-14-1-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree12.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-12-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree14-2.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-14-2-#2.txt
 
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree13.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-13-#2.txt
-
-REM call mysql ciapl < D:\Fenix\Temp\migration\loadDegree14.sql
-REM call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-14-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree15.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-15-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree16.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-16-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree17.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-17-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree18.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-18-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree19.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-19-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree20.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-20-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree21.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-21-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree22.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-22-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree23.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-23-#2.txt
-
-call mysql ciapl < D:\Fenix\Temp\migration\loadDegree24.sql
-call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-24-#2.txt
+call mysql ciapl < D:\Fenix\Temp\migration\loadDegree14-3.sql
+call ant -f D:\Fenix\Code\build_enrollmentDataMigration.xml create-and-update-all-students-enrolments > D:\Fenix\Results\create-and-update-all-past-students-enrollments-degree-14-3-#2.txt
