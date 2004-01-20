4251bf1091f1083da84a43bb92bd2801d1e71eff
Fernanda Quitério <fsq@rnl.ist.utl.pt>
2004-01-20 10:35:13 +0000

    *** empty log message ***

--- src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/gratuity/InsertGratuityDataTest.java
+++ src_tests/ServidorAplicacao/Servicos/MasterDegree/administrativeOffice/gratuity/InsertGratuityDataTest.java
@@ -4,17 +4,27 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import DataBeans.InfoCurriculum;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.commons.collections.Transformer;
+
 import DataBeans.InfoEmployee;
 import DataBeans.InfoExecutionDegree;
 import DataBeans.InfoGratuityValues;
 import DataBeans.InfoPaymentPhase;
 import DataBeans.InfoPerson;
+import DataBeans.util.Cloner;
+import Dominio.CursoExecucao;
+import Dominio.GratuityValues;
+import Dominio.ICursoExecucao;
+import Dominio.IGratuityValues;
+import Dominio.IPaymentPhase;
 import ServidorAplicacao.IUserView;
 import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.AdministrativeOfficeBaseTest;
 import ServidorApresentacao.Action.masterDegree.utils.SessionConstants;
 import ServidorPersistente.IPersistentGratuityValues;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
 import framework.factory.ServiceManagerServiceFactory;
 
 /**
@@ -32,7 +42,6 @@ public class InsertGratuityDataTest extends AdministrativeOfficeBaseTest
 	public InsertGratuityDataTest(String name)
 	{
 		super(name);
-		// TODO Auto-generated constructor stub
 	}
 
 	/* (non-Javadoc)
@@ -108,7 +117,7 @@ public class InsertGratuityDataTest extends AdministrativeOfficeBaseTest
 	/* (non-Javadoc)
 	 * @see ServidorAplicacao.Servicos.MasterDegree.administrativeOffice.AdministrativeOfficeBaseTest#getServiceArgumentsForNotAuthorizedUser()
 	 */
-	protected Object[] getServiceArgumentForSuccess() throws FenixServiceException
+	protected Object[] getServiceArgumentForInsertSuccess() throws FenixServiceException
 	{
 		Calendar now = Calendar.getInstance();
 		
@@ -162,10 +171,64 @@ public class InsertGratuityDataTest extends AdministrativeOfficeBaseTest
 		return args;
 	}
 	
-	public void testInsertGratuityDataSuccessfull() {
+	protected Object[] getServiceArgumentForEditSuccess() throws FenixServiceException
+	{
+		Calendar now = Calendar.getInstance();
+		
+		InfoPerson infoPerson = new InfoPerson();
+		infoPerson.setUsername(getAuthenticatedAndAuthorizedUser()[0]);
+		
+		InfoEmployee infoEmployee = new InfoEmployee();
+		infoEmployee.setPerson(infoPerson);
+		
+		InfoExecutionDegree infoExecutionDegree = new InfoExecutionDegree();
+		infoExecutionDegree.setIdInternal(new Integer(28));
+		
+		List infoPaymentPhases = new ArrayList();
+		InfoPaymentPhase infoPaymentPhase = new InfoPaymentPhase();
+		infoPaymentPhase.setDescription(SessionConstants.REGISTRATION_PAYMENT);
+		now.set(2004, 1, 20);
+		infoPaymentPhase.setEndDate(now.getTime());
+		infoPaymentPhase.setValue(new Double(150));
+		
+		infoPaymentPhases.add(infoPaymentPhase);
+		
+		infoPaymentPhase = new InfoPaymentPhase();
+		now.set(2004, 5, 21);
+		infoPaymentPhase.setEndDate(now.getTime());
+		now.set(2004, 1, 21);
+		infoPaymentPhase.setStartDate(now.getTime());
+		infoPaymentPhase.setValue(new Double(200));
+		
+		infoPaymentPhases.add(infoPaymentPhase);
+		
+		infoPaymentPhase = new InfoPaymentPhase();
+		now.set(2004, 7, 22);
+		infoPaymentPhase.setEndDate(now.getTime());
+		now.set(2004, 5, 22);
+		infoPaymentPhase.setStartDate(now.getTime());
+		infoPaymentPhase.setValue(new Double(300));
+		
+		infoPaymentPhases.add(infoPaymentPhase);
+		
+		InfoGratuityValues infoGratuityValues = new InfoGratuityValues();
+		infoGratuityValues.setIdInternal(new Integer(222));
+		infoGratuityValues.setAnualValue(new Double(650));
+		infoGratuityValues.setCreditValue(new Double(50));
+		infoGratuityValues.setRegistrationPayment(Boolean.TRUE);
+		infoGratuityValues.setWhen(now.getTime());
+		infoGratuityValues.setInfoEmployee(infoEmployee);
+		infoGratuityValues.setInfoExecutionDegree(infoExecutionDegree);
+		infoGratuityValues.setInfoPaymentPhases(infoPaymentPhases);
+		
+		Object args[] = { infoGratuityValues };
+		return args;
+	}
+	
+	public void testInsertGratuityDataInsertSuccessfull() {
 		try {
 
-			Object[] args = getServiceArgumentForSuccess();
+			Object[] args = getServiceArgumentForInsertSuccess();
 
 			//Valid user
 			String[] argsUser = getAuthenticatedAndAuthorizedUser();
@@ -173,33 +236,125 @@ public class InsertGratuityDataTest extends AdministrativeOfficeBaseTest
 
 			ServiceManagerServiceFactory.executeService(id, getNameOfServiceToBeTested(), args);
 
-			IPersistentGratuityValues persistentGratuityValues = S
+			SuportePersistenteOJB sp = SuportePersistenteOJB.getInstance();
+			IPersistentGratuityValues persistentGratuityValues = sp.getIPersistentGratuityValues();
+			
+			ICursoExecucao executionDegree = new CursoExecucao();
+			executionDegree.setIdInternal(new Integer(76));
+			
+			IGratuityValues gratuityValues = (IGratuityValues) persistentGratuityValues.readGratuityValuesByExecutionDegree(executionDegree);
+
+			InfoGratuityValues infoGratuityValuesDB = cloneGratuityValues(gratuityValues);
+			
+			InfoGratuityValues infoGratuityValues = (InfoGratuityValues) getServiceArgumentForInsertSuccess()[0];
 			
 			//Check information read is correct
-			assertEquals(new String("objectivos gerais em portugues"), infoCurriculum.getGeneralObjectives());
-			assertEquals(new String("objectivos gerais em ingles"), infoCurriculum.getGeneralObjectivesEn());
-			assertEquals(new String("objectivos operacionais em portugues"), infoCurriculum.getOperacionalObjectives());
-			assertEquals(new String("objectivos operacionais em ingles"), infoCurriculum.getOperacionalObjectivesEn());
-			assertEquals(new String("programa"), infoCurriculum.getProgram());
-			assertEquals(new String("programa em ingles"), infoCurriculum.getProgramEn());
-			Calendar calendar = Calendar.getInstance();
-			calendar.clear();
-			calendar.set(2003, 10-1, 20, 0, 0, 0);
-			assertEquals(calendar.getTime().getTime(), infoCurriculum.getLastModificationDate().getTime());
-
-			assertEquals(infoCurriculum.getInfoCurricularCourse().getIdInternal(), new Integer(1));
-			assertEquals(new Integer(infoCurriculum.getInfoCurricularCourse().getInfoScopes().size()), new Integer(1));
-			assertEquals(new Integer(infoCurriculum.getInfoCurricularCourse().getInfoAssociatedExecutionCourses().size()), new Integer(0));
-
+			assertEquals(infoGratuityValues.getAnualValue(), infoGratuityValuesDB.getAnualValue());
+			assertEquals(infoGratuityValues.getCourseValue(), infoGratuityValuesDB.getCourseValue());
+			assertEquals(infoGratuityValues.getCreditValue(), infoGratuityValuesDB.getCreditValue());
+			assertEquals(infoGratuityValues.getEndPayment(), infoGratuityValuesDB.getEndPayment());
+			assertEquals(infoGratuityValues.getFinalProofValue(), infoGratuityValuesDB.getFinalProofValue());
+			assertEquals(infoGratuityValues.getInfoEmployee().getPerson().getUsername(), infoGratuityValuesDB.getInfoEmployee().getPerson().getUsername());
+			assertEquals(infoGratuityValues.getInfoExecutionDegree().getIdInternal(), infoGratuityValuesDB.getInfoExecutionDegree().getIdInternal());
+			assertEquals(infoGratuityValues.getInfoPaymentPhases().size(), infoGratuityValuesDB.getInfoPaymentPhases().size());
+			assertEquals(infoGratuityValues.getProofRequestPayment(), infoGratuityValuesDB.getProofRequestPayment());
+			assertEquals(infoGratuityValues.getRegistrationPayment(), infoGratuityValuesDB.getRegistrationPayment());
+			assertEquals(infoGratuityValues.getScholarShipValue(), infoGratuityValuesDB.getScholarShipValue());
+			assertEquals(infoGratuityValues.getStartPayment(), infoGratuityValuesDB.getStartPayment());
+			assertEquals(infoGratuityValues.getWhen(), infoGratuityValuesDB.getWhen());
+			
 			System.out.println(
-					"testReadCurrentCurriculumByCurricularCourseCodeSuccessfull was SUCCESSFULY runned by service: " + getNameOfServiceToBeTested());
+					"testInsertGratuityDataInsertSuccessfull was SUCCESSFULY runned by service: " + getNameOfServiceToBeTested());
 
 		} catch (FenixServiceException e) {
 			e.printStackTrace();
-			fail("Reading a Curriculum from database " + e);
+			fail("Inserting gratuity values into database " + e);
 		} catch (Exception e) {
 			e.printStackTrace();
-			fail("Reading a Curriculum from database " + e);
+			fail("Inserting gratuity values into database " + e);
+		}
+	}
+
+	private InfoGratuityValues cloneGratuityValues(IGratuityValues gratuityValues)
+	{
+		InfoGratuityValues infoGratuityValuesDB = Cloner.copyIGratuityValues2InfoGratuityValues(gratuityValues);
+		List infoPaymentPhases = new ArrayList();
+		CollectionUtils.collect(gratuityValues.getPaymentPhaseList(), new Transformer()
+				{
+			public Object transform(Object input)
+			{
+				IPaymentPhase paymentPhase = (IPaymentPhase) input;
+				return Cloner.copyIPaymentPhase2InfoPaymentPhase(paymentPhase);
+			}
+		}, infoPaymentPhases);
+		
+		InfoPaymentPhase infoPaymentPhase = (InfoPaymentPhase) CollectionUtils.find(infoPaymentPhases, new Predicate()
+				{
+			public boolean evaluate(Object input)
+			{
+				InfoPaymentPhase aInfoPaymentPhase = (InfoPaymentPhase)input;
+				if(aInfoPaymentPhase.getDescription().equals(SessionConstants.REGISTRATION_PAYMENT)) {
+					return true;
+				}
+				return false;
+			}
+		});
+		
+		if(infoPaymentPhase != null) {
+			infoGratuityValuesDB.setRegistrationPayment(Boolean.TRUE);
+		}
+		
+		infoGratuityValuesDB.setInfoPaymentPhases(infoPaymentPhases);
+		return infoGratuityValuesDB;
+	}
+	public void testInsertGratuityDataEditSuccessfull() {
+		try {
+
+			Object[] args = getServiceArgumentForEditSuccess();
+
+			//Valid user
+			String[] argsUser = getAuthenticatedAndAuthorizedUser();
+			IUserView id = (IUserView) ServiceManagerServiceFactory.executeService(null, "Autenticacao", argsUser);
+
+			ServiceManagerServiceFactory.executeService(id, getNameOfServiceToBeTested(), args);
+
+			SuportePersistenteOJB sp = SuportePersistenteOJB.getInstance();
+			IPersistentGratuityValues persistentGratuityValues = sp.getIPersistentGratuityValues();
+			
+			IGratuityValues gratuityValues = new GratuityValues();
+			gratuityValues.setIdInternal(new Integer(222));
+			
+			gratuityValues = (IGratuityValues) persistentGratuityValues.readByOId(gratuityValues, false);
+
+			InfoGratuityValues infoGratuityValuesDB = cloneGratuityValues(gratuityValues);
+			
+			InfoGratuityValues infoGratuityValues = (InfoGratuityValues) getServiceArgumentForEditSuccess()[0];
+			
+			//Check information read is correct
+			assertEquals(infoGratuityValues.getIdInternal(), infoGratuityValuesDB.getIdInternal());
+			assertEquals(infoGratuityValues.getAnualValue(), infoGratuityValuesDB.getAnualValue());
+			assertEquals(infoGratuityValues.getCourseValue(), infoGratuityValuesDB.getCourseValue());
+			assertEquals(infoGratuityValues.getCreditValue(), infoGratuityValuesDB.getCreditValue());
+			assertEquals(infoGratuityValues.getEndPayment(), infoGratuityValuesDB.getEndPayment());
+			assertEquals(infoGratuityValues.getFinalProofValue(), infoGratuityValuesDB.getFinalProofValue());
+			assertEquals(infoGratuityValues.getInfoEmployee().getPerson().getUsername(), infoGratuityValuesDB.getInfoEmployee().getPerson().getUsername());
+			assertEquals(infoGratuityValues.getInfoExecutionDegree().getIdInternal(), infoGratuityValuesDB.getInfoExecutionDegree().getIdInternal());
+			assertEquals(infoGratuityValues.getInfoPaymentPhases().size(), infoGratuityValuesDB.getInfoPaymentPhases().size());
+			assertEquals(infoGratuityValues.getProofRequestPayment(), infoGratuityValuesDB.getProofRequestPayment());
+			assertEquals(infoGratuityValues.getRegistrationPayment(), infoGratuityValuesDB.getRegistrationPayment());
+			assertEquals(infoGratuityValues.getScholarShipValue(), infoGratuityValuesDB.getScholarShipValue());
+			assertEquals(infoGratuityValues.getStartPayment(), infoGratuityValuesDB.getStartPayment());
+			assertEquals(infoGratuityValues.getWhen(), infoGratuityValuesDB.getWhen());
+			
+			System.out.println(
+					"testInsertGratuityDataEditSuccessfull was SUCCESSFULY runned by service: " + getNameOfServiceToBeTested());
+
+		} catch (FenixServiceException e) {
+			e.printStackTrace();
+			fail("Inserting gratuity values into database " + e);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Inserting gratuity values into database " + e);
 		}
 	}
 	
