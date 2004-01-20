e9581799ce3b1f1e13194d053acfba064cf2847f
Nuno Correia <nrmc@mega.ist.utl.pt>
2004-01-20 09:49:15 +0000

    added methods getPrecedenceScopeToApply and setPrecedenceScopeToApply

--- src/Dominio/IPrecedence.java
+++ src/Dominio/IPrecedence.java
@@ -7,11 +7,13 @@ package Dominio;
 import java.util.List;
 
 import ServidorAplicacao.strategy.enrolment.context.EnrolmentContext;
+import Util.PrecedenceScopeToApply;
 
 /**
  * @author jpvl
  */
-public interface IPrecedence {
+public interface IPrecedence extends IDomainObject
+{
 	/**
 	 * @return
 	 */
@@ -28,11 +30,14 @@ public interface IPrecedence {
 	 * @param list
 	 */
 	public abstract void setRestrictions(List restrictionList);
-	
+
 	/**
 	 * If restriction list as two restriction A and B: this method returns (A ^ B).
 	 * @param curricularCoursesDone
 	 * @return
 	 */
 	public abstract boolean evaluate(EnrolmentContext enrolmentContext);
+
+	public PrecedenceScopeToApply getPrecedenceScopeToApply();
+	public void setPrecedenceScopeToApply(PrecedenceScopeToApply apply);
 }
