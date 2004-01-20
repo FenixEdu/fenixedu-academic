d9debcb893b577feccc72528dff59d5c78f5513a
Nuno Correia <nrmc@mega.ist.utl.pt>
2004-01-20 09:04:50 +0000

    *** empty log message ***

--- src/middleware/persistentMiddlewareSupport/IPersistentMWDisciplinaIleec.java
+++ src/middleware/persistentMiddlewareSupport/IPersistentMWDisciplinaIleec.java
@@ -7,17 +7,19 @@ import middleware.middlewareDomain.MWDisciplinaIleec;
 import middleware.middlewareDomain.MWGrupoIleec;
 import middleware.persistentMiddlewareSupport.exceptions.PersistentMiddlewareSupportException;
 import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.IPersistentObject;
 
 /**
  * @author David Santos
  * 3/Dez/2003
  */
 
-public interface IPersistentMWDisciplinaIleec
+public interface IPersistentMWDisciplinaIleec extends IPersistentObject
 {
 	public List readAll() throws ExcepcaoPersistencia;
 	public MWDisciplinaIleec readByCodigoDisciplina(String codigoDisciplina) throws ExcepcaoPersistencia;
 	public List readAllBySpan(Integer spanNumber, Integer numberOfElementsInSpan) throws ExcepcaoPersistencia;
 	public Integer countAll();
 	public ArrayList readByGroup(MWGrupoIleec grupoILeec)throws PersistentMiddlewareSupportException, ExcepcaoPersistencia;
+	public MWDisciplinaIleec readByIdInternal(Integer idInternal) throws ExcepcaoPersistencia;
 }
