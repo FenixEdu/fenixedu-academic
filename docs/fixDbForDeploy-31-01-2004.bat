fdcb6896df769ca03571a849de41d6476d1f9c8a
João Mota <joao.mota@ist.utl.pt>
2004-01-31 18:19:45 +0000

    *** empty log message ***

--- docs/fixDbForDeploy-31-01-2004.bat
+++ docs/fixDbForDeploy-31-01-2004.bat
@@ -1,11 +1,11 @@
-call mysql fenix < fixDbForDeploy1-31-01-2004.sql > temp1.sql
-call mysql fenix < fixDbForDeploy2-31-01-2004.sql
-call mysql fenix < temp1.sql
-call mysql fenix < fixDbForDeploy3-31-01-2004.sql > temp2.sql
-call mysql fenix < temp2.sql
-call mysql fenix < fixDbForDeploy4-31-01-2004.sql
-call mysql fenix < fixDbForDeploy5-31-01-2004.sql > temp3.sql
-call mysql fenix < temp3.sql
+call mysql -f fenix < fixDbForDeploy1-31-01-2004.sql > temp1.sql
+call mysql -f fenix < fixDbForDeploy2-31-01-2004.sql
+call mysql -f fenix < temp1.sql
+call mysql -f fenix < fixDbForDeploy3-31-01-2004.sql > temp2.sql
+call mysql -f fenix < temp2.sql
+call mysql -f fenix < fixDbForDeploy4-31-01-2004.sql
+call mysql -f fenix < fixDbForDeploy5-31-01-2004.sql > temp3.sql
+call mysql -f fenix < temp3.sql
 
 
 
--- docs/fixDbForDeploy-31-01-2004.sh
+++ docs/fixDbForDeploy-31-01-2004.sh
@@ -1,11 +1,11 @@
-mysql fenix < fixDbForDeploy1-31-01-2004.sql > temp1.sql
-mysql fenix < fixDbForDeploy2-31-01-2004.sql
-mysql fenix < temp1.sql
-mysql fenix < fixDbForDeploy3-31-01-2004.sql > temp2.sql
-mysql fenix < temp2.sql
-mysql fenix < fixDbForDeploy4-31-01-2004.sql
-mysql fenix < fixDbForDeploy5-31-01-2004.sql > temp3.sql
-mysql fenix < temp3.sql
+mysql -f fenix < fixDbForDeploy1-31-01-2004.sql > temp1.sql
+mysql -f fenix < fixDbForDeploy2-31-01-2004.sql
+mysql -f fenix < temp1.sql
+mysql -f fenix < fixDbForDeploy3-31-01-2004.sql > temp2.sql
+mysql -f fenix < temp2.sql
+mysql -f fenix < fixDbForDeploy4-31-01-2004.sql
+mysql -f fenix < fixDbForDeploy5-31-01-2004.sql > temp3.sql
+mysql -f fenix < temp3.sql
 
 
 
