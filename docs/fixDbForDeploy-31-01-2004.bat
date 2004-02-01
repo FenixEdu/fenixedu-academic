f08b53bfd92bdbf04dce75f37ce2f003f7a885de
João Mota <joao.mota@ist.utl.pt>
2004-02-01 18:45:34 +0000

    *** empty log message ***

--- docs/fixDbForDeploy-31-01-2004.bat
+++ docs/fixDbForDeploy-31-01-2004.bat
@@ -6,6 +6,10 @@ call mysql -f fenix < temp2.sql
 call mysql -f fenix < fixDbForDeploy4-31-01-2004.sql
 call mysql -f fenix < fixDbForDeploy5-31-01-2004.sql > temp3.sql
 call mysql -f fenix < temp3.sql
+call mysql -f fenix < updateQualificationBeforeScript.sql
+call mysql -f fenix < updateQualificationScript1.sql | mysql fenix
+call mysql -f fenix < updateQualificationScript2.sql | mysql fenix
+call mysql -f fenix < updateQualification.AfterScript.sql
 
 
 
