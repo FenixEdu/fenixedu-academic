72c660a923ebbf19be7ae68c1b0575f749048f0a
Luis Cruz <luis.cruz@ist.utl.pt>
2004-02-13 15:00:07 +0000

    New filter that garanties students only alter their own data.

---
+++ src/ServidorAplicacao/Filtro/student/StudentIsUserFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Created on 13/Fev/2004
+ *  
+ */
+package ServidorAplicacao.Filtro.student;
+
+import pt.utl.ist.berserk.ServiceRequest;
+import pt.utl.ist.berserk.ServiceResponse;
+import pt.utl.ist.berserk.logic.filterManager.exceptions.FilterException;
+import Dominio.IStudent;
+import Dominio.Student;
+import ServidorAplicacao.IUserView;
+import ServidorAplicacao.Filtro.Filtro;
+import ServidorAplicacao.Filtro.exception.NotAuthorizedFilterException;
+import ServidorPersistente.ISuportePersistente;
+import ServidorPersistente.OJB.SuportePersistenteOJB;
+
+/**
+ * @author Luis Cruz
+ */
+public class StudentIsUserFilter extends Filtro {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see pt.utl.ist.berserk.logic.filterManager.IFilter#execute(pt.utl.ist.berserk.ServiceRequest,
+	 *      pt.utl.ist.berserk.ServiceResponse)
+	 */
+	public void execute(
+		ServiceRequest serviceRequest,
+		ServiceResponse serviceResponse)
+		throws FilterException, Exception {
+		Integer studentId = (Integer) serviceRequest.getArguments()[0];
+		IUserView userView = getRemoteUser(serviceRequest);
+
+		ISuportePersistente persistentSupport =
+			SuportePersistenteOJB.getInstance();
+
+		IStudent student =
+			(IStudent) persistentSupport.getIPersistentStudent().readByOID(
+				Student.class,
+				studentId);
+		if (student == null
+			|| userView == null
+			|| !userView.getUtilizador().equals(
+				student.getPerson().getUsername())) {
+			throw new NotAuthorizedFilterException();
+		}
+
+	}
+
+}
