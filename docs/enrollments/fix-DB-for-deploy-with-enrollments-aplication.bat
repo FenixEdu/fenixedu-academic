aebdcb08fc2db7de435b4cb7ae707a421af69514
David Santos <david.santos@ist.utl.pt>
2004-02-13 16:54:32 +0000

    *** empty log message ***

--- docs/enrollments/fix-DB-for-deploy-with-enrollments-aplication.bat
+++ docs/enrollments/fix-DB-for-deploy-with-enrollments-aplication.bat
@@ -6,9 +6,10 @@ call mysql ciapl < update_curricular_course_set_type=1.sql | mysql ciapl
 call mysql ciapl < update_curricular_course_set_type=2.sql | mysql ciapl
 call mysql ciapl < update_curricular_course_set_type=4.sql | mysql ciapl
 call mysql ciapl < update_credits_and_precedences.sql
-call mysql ciapl < delete_from_enrolment_evaluation.sql > temp.sql
+call mysql ciapl < delete_from_enrolment_evaluation.sql > temp1.sql
 call mysql ciapl < update_enrolment_set_state=1.sql | mysql ciapl
 call mysql ciapl < update_enrolment_set_state=2.sql | mysql ciapl
 call mysql ciapl < temp.sql
-call mysql ciapl < enrollmentTableUpdates.sql | mysql ciapl
-call del temp.sql
+call mysql ciapl < enrollmentTableUpdates.sql > temp2.sql
+call del temp1.sql
+call del temp2.sql
