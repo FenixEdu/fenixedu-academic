5d9408e2aeafd9c61df6d6278d96dd1a64513047
David Santos <david.santos@ist.utl.pt>
2004-02-19 11:37:42 +0000

    *** empty log message ***

--- docs/enrollments/fix-DB-for-deploy-with-enrollments-aplication.bat
+++ docs/enrollments/fix-DB-for-deploy-with-enrollments-aplication.bat
@@ -1,15 +1,2 @@
-call mysql ciapl < alter_table_restriction.sql
-call ant -f ..\..\build_enrollmentDataMigration.xml migrate-all-ileec-data
-call mysql ciapl < update_curricular_course_set_mandatory1.sql | mysql ciapl
-call mysql ciapl < update_curricular_course_set_mandatory0.sql | mysql ciapl
-call mysql ciapl < update_curricular_course_set_type1.sql | mysql ciapl
-call mysql ciapl < update_curricular_course_set_type2.sql | mysql ciapl
-call mysql ciapl < update_curricular_course_set_type4.sql | mysql ciapl
-call mysql ciapl < update_credits_and_precedences.sql
-call mysql ciapl < delete_from_enrolment_evaluation.sql > temp1.sql
 call mysql ciapl < update_enrolment_set_state1.sql | mysql ciapl
 call mysql ciapl < update_enrolment_set_state2.sql | mysql ciapl
-call mysql ciapl < temp.sql
-call mysql ciapl < enrollmentTableUpdates.sql > temp2.sql
-call del temp1.sql
-call del temp2.sql
