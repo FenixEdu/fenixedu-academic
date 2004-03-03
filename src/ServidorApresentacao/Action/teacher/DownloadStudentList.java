9b31e3043e53143ee56fe2516b5e881483e223cc
Susana Fernandes <susana.fernandes@ist.utl.pt>
2004-03-03 20:24:15 +0000

    *** empty log message ***

--- src/ServidorApresentacao/Action/teacher/DownloadStudentList.java
+++ src/ServidorApresentacao/Action/teacher/DownloadStudentList.java
@@ -19,9 +19,8 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import framework.factory.ServiceManagerServiceFactory;
-
-import DataBeans.InfoFrequenta;
+import DataBeans.InfoAttendWithEnrollment;
+import DataBeans.InfoAttendsSummary;
 import DataBeans.InfoGroupProjectStudents;
 import DataBeans.InfoGroupProperties;
 import DataBeans.InfoSiteProjects;
@@ -32,6 +31,7 @@ import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorApresentacao.Action.base.FenixAction;
 import ServidorApresentacao.Action.exceptions.FenixActionException;
 import ServidorApresentacao.Action.sop.utils.SessionConstants;
+import framework.factory.ServiceManagerServiceFactory;
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
  *
@@ -41,178 +41,189 @@ import ServidorApresentacao.Action.sop.utils.SessionConstants;
  */
 public class DownloadStudentList extends FenixAction
 {
-    static final String COLUMNS_HEADERS = "Nº\tInscrito\tCurso\tNome\tE-mail";
-    public ActionForward execute(
-        ActionMapping mapping,
-        ActionForm form,
-        HttpServletRequest request,
-        HttpServletResponse response)
-        throws FenixActionException
-    {
-        HttpSession session = request.getSession(false);
-        Integer objectCode = null;
-        Integer shiftID = null;
-        try
-        {
-            shiftID = new Integer(request.getParameter("shiftCode"));
-        } catch (NumberFormatException ex)
-        {
-            //ok, we don't want to view a shift's student list
-        }
-        String objectCodeString = request.getParameter("objectCode");
-        if (objectCodeString == null)
-        {
-            objectCodeString = (String) request.getAttribute("objectCode");
-        }
-        objectCode = new Integer(objectCodeString);
-        Integer scopeCode = null;
-        String scopeCodeString = request.getParameter("scopeCode");
-        if (scopeCodeString == null)
-        {
-            scopeCodeString = (String) request.getAttribute("scopeCode");
-        }
-        if (scopeCodeString != null)
-        {
-            scopeCode = new Integer(scopeCodeString);
-        }
-        UserView userView = (UserView) session.getAttribute(SessionConstants.U_VIEW);
-        Object args[] = { objectCode, scopeCode };
-        Object argsProjects[] = { objectCode };
-        Object argsInfos[] = { objectCode };
-        TeacherAdministrationSiteView siteView = null;
-        InfoSiteProjects projects = null;
-        List infosGroups = null;
-        List attendacies = null;
-        List shiftStudents = null;
-        try
-        {
-            siteView =
-                (TeacherAdministrationSiteView) ServiceManagerServiceFactory.executeService(
-                    userView,
-                    "ReadStudentsByCurricularCourse",
-                    args);
+	static final String COLUMNS_HEADERS = "Nº\tInscrito\tCurso\tNome\tE-mail";
+	public ActionForward execute(
+		ActionMapping mapping,
+		ActionForm form,
+		HttpServletRequest request,
+		HttpServletResponse response)
+		throws FenixActionException
+	{
+		HttpSession session = request.getSession(false);
+		Integer objectCode = null;
+		Integer shiftID = null;
+		try
+		{
+			shiftID = new Integer(request.getParameter("shiftCode"));
+		}
+		catch (NumberFormatException ex)
+		{
+			//ok, we don't want to view a shift's student list
+		}
+		String objectCodeString = request.getParameter("objectCode");
+		if (objectCodeString == null)
+		{
+			objectCodeString = (String) request.getAttribute("objectCode");
+		}
+		objectCode = new Integer(objectCodeString);
+		Integer scopeCode = null;
+		String scopeCodeString = request.getParameter("scopeCode");
+		if (scopeCodeString == null)
+		{
+			scopeCodeString = (String) request.getAttribute("scopeCode");
+		}
+		if (scopeCodeString != null)
+		{
+			scopeCode = new Integer(scopeCodeString);
+		}
+		UserView userView = (UserView) session.getAttribute(SessionConstants.U_VIEW);
+		Object args[] = { objectCode, scopeCode };
+		Object argsProjects[] = { objectCode };
+		Object argsInfos[] = { objectCode };
+		TeacherAdministrationSiteView siteView = null;
+		InfoSiteProjects projects = null;
+		List infosGroups = null;
+		List attendacies = null;
+		List shiftStudents = null;
+		try
+		{
+			siteView =
+				(TeacherAdministrationSiteView) ServiceManagerServiceFactory.executeService(
+					userView,
+					"ReadStudentsByCurricularCourse",
+					args);
 
-            projects =
-                (InfoSiteProjects) ServiceManagerServiceFactory.executeService(
-                    userView,
-                    "teacher.ReadExecutionCourseProjects",
-                    argsProjects);
+			projects =
+				(InfoSiteProjects) ServiceManagerServiceFactory.executeService(
+					userView,
+					"teacher.ReadExecutionCourseProjects",
+					argsProjects);
 
-            infosGroups =
-                (List) ServiceManagerServiceFactory.executeService(
-                    userView,
-                    "teacher.GetProjectsGroupsByExecutionCourseID",
-                    argsInfos);
-            InfoSiteStudents infoSiteStudents = (InfoSiteStudents) siteView.getComponent();
-            Collections.sort(infoSiteStudents.getStudents(), new BeanComparator("number"));
-            if (shiftID != null)
-            {
-                //the objectCode is needed by the filter...doing this is awfull !!!
-                //please read http://www.dcc.unicamp.br/~oliva/fun/prog/resign-patterns
-                Object[] argsReadShiftStudents = { objectCode, shiftID };
-                shiftStudents =
-                    (List) ServiceManagerServiceFactory.executeService(
-                        userView,
-                        "teacher.ReadStudentsByShiftID",
-                        argsReadShiftStudents);
-                infoSiteStudents.setStudents(shiftStudents);
-            }
-            Object[] argsAttendacies = { objectCode, infoSiteStudents.getStudents()};
-            attendacies =
-                (List) ServiceManagerServiceFactory.executeService(userView, "teacher.GetAttendaciesByStudentList", argsAttendacies);
-        } catch (FenixServiceException e)
-        {
-            throw new FenixActionException(e);
-        }
-        String result = new String(COLUMNS_HEADERS);
-        if (projects != null && projects.getInfoGroupPropertiesList() != null)
-        {
-            for (Iterator iter = projects.getInfoGroupPropertiesList().iterator(); iter.hasNext();)
-            {
-                InfoGroupProperties infoGroupProperties = (InfoGroupProperties) iter.next();
-                result += "\t" + "\"" + "Grupo do Projecto " + infoGroupProperties.getName() + "\"";
-            }
-        }
-        result += "\n";
+			infosGroups =
+				(List) ServiceManagerServiceFactory.executeService(
+					userView,
+					"teacher.GetProjectsGroupsByExecutionCourseID",
+					argsInfos);
+			InfoSiteStudents infoSiteStudents = (InfoSiteStudents) siteView.getComponent();
+			Collections.sort(infoSiteStudents.getStudents(), new BeanComparator("number"));
+			if (shiftID != null)
+			{
+				//the objectCode is needed by the filter...doing this is awfull !!!
+				//please read http://www.dcc.unicamp.br/~oliva/fun/prog/resign-patterns
+				Object[] argsReadShiftStudents = { objectCode, shiftID };
+				shiftStudents =
+					(List) ServiceManagerServiceFactory.executeService(
+						userView,
+						"teacher.ReadStudentsByShiftID",
+						argsReadShiftStudents);
+				infoSiteStudents.setStudents(shiftStudents);
+			}
+			Object[] argsAttendacies = { objectCode, infoSiteStudents.getStudents()};
+			attendacies =
+				((InfoAttendsSummary) ServiceManagerServiceFactory
+					.executeService(userView, "teacher.GetAttendaciesByStudentList", argsAttendacies))
+					.getAttends();
+		}
+		catch (FenixServiceException e)
+		{
+			throw new FenixActionException(e);
+		}
+		String result = new String(COLUMNS_HEADERS);
+		if (projects != null && projects.getInfoGroupPropertiesList() != null)
+		{
+			for (Iterator iter = projects.getInfoGroupPropertiesList().iterator(); iter.hasNext();)
+			{
+				InfoGroupProperties infoGroupProperties = (InfoGroupProperties) iter.next();
+				result += "\t" + "\"" + "Grupo do Projecto " + infoGroupProperties.getName() + "\"";
+			}
+		}
+		result += "\n";
 
-        for (Iterator attendaciesIterator = attendacies.iterator(); attendaciesIterator.hasNext();)
-        {
-            InfoFrequenta infoFrequenta = (InfoFrequenta) attendaciesIterator.next();
-            result += infoFrequenta.getAluno().getNumber();
-            result += "\t";
-            if (infoFrequenta.getInfoEnrolment() == null)
-                result += "Não";
-            else
-                result += "Sim";
-            result += "\t";
-            if (infoFrequenta.getInfoEnrolment() == null)
-                result += "N/A";
-            else
-                result += "\""
-                    + infoFrequenta
-                        .getInfoEnrolment()
-                        .getInfoStudentCurricularPlan()
-                        .getInfoDegreeCurricularPlan()
-                        .getName()
-                    + "\"";
-            result += "\t" + "\"" + infoFrequenta.getAluno().getInfoPerson().getNome() + "\"";
-            result += "\t" + "\"" + infoFrequenta.getAluno().getInfoPerson().getEmail() + "\"";
-            if (projects != null && projects.getInfoGroupPropertiesList() != null)
-            {
-                for (Iterator projectsIterator = projects.getInfoGroupPropertiesList().iterator();
-                    projectsIterator.hasNext();
-                    )
-                {
-                    InfoGroupProperties infoGroupProperties =
-                        (InfoGroupProperties) projectsIterator.next();
-                    int projectIdInternal = infoGroupProperties.getIdInternal().intValue();
-                    int groupNumber = -1;
-//                    int shiftCode = -1;
-//                    int executionCourseCode = -1;
-//                    int groupPropertiesCode = -1;
-//                    int studentGroupCode = -1;
-                    Integer studentNumber = infoFrequenta.getAluno().getNumber();
-                    for (Iterator groupsIterator = infosGroups.iterator(); groupsIterator.hasNext();)
-                    {
-                        InfoGroupProjectStudents groupInfo =
-                            (InfoGroupProjectStudents) groupsIterator.next();
-                        if (projectIdInternal
-                            == groupInfo.getStudentGroup().getInfoGroupProperties().getIdInternal().intValue()
-                            && groupInfo.isStudentMemberOfThisGroup(studentNumber))
-                        {
-//                            groupNumber = groupInfo.getStudentGroup().getGroupNumber().intValue();
-//                            studentGroupCode = groupInfo.getStudentGroup().getIdInternal().intValue();
-//                            shiftCode =
-//                                groupInfo.getStudentGroup().getInfoShift().getIdInternal().intValue();
-//                            executionCourseCode =
-//                                groupInfo
-//                                    .getStudentGroup()
-//                                    .getInfoGroupProperties()
-//                                    .getInfoExecutionCourse()
-//                                    .getIdInternal()
-//                                    .intValue();
-                            break;
-                        }
-                    }
-                    if (groupNumber != -1)
-                        result += "\t" + groupNumber;
-                    else
-                        result += "\t" + "N/A";
-                }
-            }
-            result += "\n";
-        }
-        try
-        {
-            ServletOutputStream writer = response.getOutputStream();
-            response.setContentType("application/vnd.ms-excel");
-            writer.print(result);
-            writer.flush();
-            response.flushBuffer();
-        } catch (IOException e1)
-        {
-            throw new FenixActionException();
-        }
-        return null;
-    }
+		for (Iterator attendaciesIterator = attendacies.iterator(); attendaciesIterator.hasNext();)
+		{
+			InfoAttendWithEnrollment infoFrequenta =
+				(InfoAttendWithEnrollment) attendaciesIterator.next();
+			result += infoFrequenta.getAluno().getNumber();
+			result += "\t";
+			if (infoFrequenta.getInfoEnrolment() == null)
+				result += "Não";
+			else
+				result += "Sim";
+			result += "\t";
+			if (infoFrequenta.getInfoEnrolment() == null)
+				result += "N/A";
+			else
+				result += "\""
+					+ infoFrequenta
+						.getInfoEnrolment()
+						.getInfoStudentCurricularPlan()
+						.getInfoDegreeCurricularPlan()
+						.getName()
+					+ "\"";
+			result += "\t" + "\"" + infoFrequenta.getAluno().getInfoPerson().getNome() + "\"";
+			result += "\t" + "\"" + infoFrequenta.getAluno().getInfoPerson().getEmail() + "\"";
+			if (projects != null && projects.getInfoGroupPropertiesList() != null)
+			{
+				for (Iterator projectsIterator = projects.getInfoGroupPropertiesList().iterator();
+					projectsIterator.hasNext();
+					)
+				{
+					InfoGroupProperties infoGroupProperties =
+						(InfoGroupProperties) projectsIterator.next();
+					int projectIdInternal = infoGroupProperties.getIdInternal().intValue();
+					int groupNumber = -1;
+					//                    int shiftCode = -1;
+					//                    int executionCourseCode = -1;
+					//                    int groupPropertiesCode = -1;
+					//                    int studentGroupCode = -1;
+					Integer studentNumber = infoFrequenta.getAluno().getNumber();
+					for (Iterator groupsIterator = infosGroups.iterator(); groupsIterator.hasNext();)
+					{
+						InfoGroupProjectStudents groupInfo =
+							(InfoGroupProjectStudents) groupsIterator.next();
+						if (projectIdInternal
+							== groupInfo
+								.getStudentGroup()
+								.getInfoGroupProperties()
+								.getIdInternal()
+								.intValue()
+							&& groupInfo.isStudentMemberOfThisGroup(studentNumber))
+						{
+							groupNumber = groupInfo.getStudentGroup().getGroupNumber().intValue();
+							//                            studentGroupCode = groupInfo.getStudentGroup().getIdInternal().intValue();
+							//                            shiftCode =
+							//                                groupInfo.getStudentGroup().getInfoShift().getIdInternal().intValue();
+							//                            executionCourseCode =
+							//                                groupInfo
+							//                                    .getStudentGroup()
+							//                                    .getInfoGroupProperties()
+							//                                    .getInfoExecutionCourse()
+							//                                    .getIdInternal()
+							//                                    .intValue();
+							break;
+						}
+					}
+					if (groupNumber != -1)
+						result += "\t" + groupNumber;
+					else
+						result += "\t" + "N/A";
+				}
+			}
+			result += "\n";
+		}
+		try
+		{
+			ServletOutputStream writer = response.getOutputStream();
+			response.setContentType("application/vnd.ms-excel");
+			response.setHeader("Content-disposition", "attachment; filename=listaDeAlunos.xls");
+			writer.print(result);
+			writer.flush();
+			response.flushBuffer();
+		}
+		catch (IOException e1)
+		{
+			throw new FenixActionException();
+		}
+		return null;
+	}
 }
