ad8a2db36a7c4834ba646674239bd964f3423f2e
João Luz <joao.luz@ist.utl.pt>
2004-03-08 03:12:56 +0000

    added two tables crd_EXEMPTION_SERVICE_CREDIT_LINE and crd_MANAGEMENT_POSITION_CREDIT_LINE

--- etc/tables_creditsTeacher.sql
+++ etc/tables_creditsTeacher.sql
@@ -30,10 +30,6 @@ create table CREDITS (
    unique U1 (KEY_EXECUTION_PERIOD, KEY_TEACHER))
    type=InnoDB;
    
-----------------------------
--- Table structure for CREDIT_LINE
--- Types : Sabática, Outro, Dispensa de Serviço Docente, Cargos de Gestão
-----------------------------
 drop table if exists crd_OTHER_TYPE_CREDIT_LINE;
 create table crd_OTHER_TYPE_CREDIT_LINE (
    ID_INTERNAL int(11) not null auto_increment,
@@ -45,7 +41,27 @@ create table crd_OTHER_TYPE_CREDIT_LINE (
    primary key (ID_INTERNAL)
 )type=InnoDB;   
 
+drop table if exists crd_MANAGEMENT_POSITION_CREDIT_LINE;
+create table crd_MANAGEMENT_POSITION_CREDIT_LINE (
+   ID_INTERNAL int(11) not null auto_increment,
+   ACK_OPT_LOCK int(11),
+   KEY_TEACHER int(11) not null,
+   POSITION TEXT not null,
+   START_DATE DATE not null,
+   END_DATE DATE not null,
+   primary key (ID_INTERNAL)
+)type=InnoDB;   
 
+drop table if exists crd_EXEMPTION_SITUATION_CREDIT_LINE;
+create table crd_EXEMPTION_SITUATION_CREDIT_LINE(
+   ID_INTERNAL int(11) not null auto_increment,
+   ACK_OPT_LOCK int(11),
+   KEY_TEACHER int(11) not null,
+   TYPE int(11) not null,
+   START_DATE DATE not null,
+   END_DATE DATE not null,
+   primary key (ID_INTERNAL)
+)type=InnoDB;   
 
 
 drop table if exists CREDIT_LINE;
