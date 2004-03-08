5790e40c7622850b56bb5f385d48b114a54ec42c
Luis Cruz <luis.cruz@ist.utl.pt>
2004-03-08 09:50:26 +0000

    Renamed directory.

---
+++ src/Dominio/finalDegreeWork/IProposal.java
@@ -0,0 +1,90 @@
+/*
+ * Created on Mar 7, 2004
+ *
+ */
+package Dominio.finalDegreeWork;
+
+import Dominio.DegreeCurricularPlan;
+import Dominio.IDomainObject;
+import Dominio.Teacher;
+import Util.TipoCurso;
+
+/**
+ * @author Luis Cruz
+ *
+ */
+public interface IProposal extends IDomainObject
+{
+	public String getCompanyContact();
+
+	public void setCompanyContact(String companyContact);
+
+	public String getCompanyLink();
+
+	public void setCompanyLink(String companyLink);
+
+	public String getCompanyName();
+
+	public void setCompanyName(String companyName);
+
+	public Teacher getCoorientator();
+
+	public void setCoorientator(Teacher coorientator);
+
+	public Integer getCoorientatorsCreditsPercentage();
+
+	public void setCoorientatorsCreditsPercentage(Integer coorientatorsCreditsPercentage);
+
+	public DegreeCurricularPlan getDegreeCurricularPlan();
+
+	public void setDegreeCurricularPlan(DegreeCurricularPlan degreeCurricularPlan);
+
+	public TipoCurso getDegreeType();
+
+	public void setDegreeType(TipoCurso degreeType);
+
+	public String getDescription();
+
+	public void setDescription(String description);
+
+	public Integer getNumberOfGroupElements();
+
+	public void setNumberOfGroupElements(Integer numberOfGroupElements);
+
+	public String getObjectives();
+
+	public void setObjectives(String objectives);
+
+	public String getObservations();
+
+	public void setObservations(String observations);
+
+	public Teacher getOrientator();
+
+	public void setOrientator(Teacher orientator);
+
+	public Integer getOrientatorsCreditsPercentage();
+
+	public void setOrientatorsCreditsPercentage(Integer orientatorsCreditsPercentage);
+
+	public String getPartA();
+
+	public void setPartA(String partA);
+
+	public String getPartB();
+
+	public void setPartB(String partB);
+
+	public String getRequirements();
+
+	public void setRequirements(String requirements);
+
+	public String getTitle();
+
+	public void setTitle(String title);
+
+	public String getUrl();
+
+	public void setUrl(String url);
+
+}
