8c65669e19a83cf6e545312a0c47679850962a0b
Luis Cruz <luis.cruz@ist.utl.pt>
2004-06-27 21:23:07 +0000

    Bug fix.

--- src/DataBeans/InfoExecutionCourseWithExecutionPeriod.java
+++ src/DataBeans/InfoExecutionCourseWithExecutionPeriod.java
@@ -24,6 +24,7 @@ public class InfoExecutionCourseWithExecutionPeriod extends InfoExecutionCourse 
             IExecutionCourse executionCourse) {
         InfoExecutionCourseWithExecutionPeriod infoExecutionCourse = null;
         if (executionCourse != null) {
+        	infoExecutionCourse = new InfoExecutionCourseWithExecutionPeriod();
             infoExecutionCourse.copyFromDomain(executionCourse);
         }
         return infoExecutionCourse;
