426aee80c0c1a5401514f7b219c4684a6288779a
David Santos <david.santos@ist.utl.pt>
2004-07-06 16:43:37 +0000

    *** empty log message ***

--- src/UtilTests/enrollment/LoadDBForCurricularCourseEnrollmentTest.java
+++ src/UtilTests/enrollment/LoadDBForCurricularCourseEnrollmentTest.java
@@ -1,6 +1,16 @@
 package UtilTests.enrollment;
 
+import java.io.File;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
+
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
 
 import Dominio.CreditsInAnySecundaryArea;
 import Dominio.CreditsInScientificArea;
@@ -10,23 +20,33 @@ import Dominio.ICreditsInAnySecundaryArea;
 import Dominio.ICreditsInScientificArea;
 import Dominio.IEnrollment;
 import Dominio.IEnrolmentEvaluation;
+import Dominio.IPersonRole;
 import Dominio.IPessoa;
+import Dominio.IRole;
 import Dominio.IStudent;
 import Dominio.IStudentCurricularPlan;
+import Dominio.PersonRole;
 import Dominio.Pessoa;
+import Dominio.Role;
 import Dominio.Student;
 import Dominio.StudentCurricularPlan;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
+import Util.RoleType;
 
 public class LoadDBForCurricularCourseEnrollmentTest {
-    
+
     private static final String FILE = "D:/Fenix/Files/others/dataSet.xml";
 
     private static final Integer PERSON_ID = new Integer(18000);
     private static final Integer STUDENT_ID = new Integer(13000);
     private static final Integer STUDENT_CURRICULAR_PLAN_ID = new Integer(23000);
+
+    private IDatabaseConnection connection = null;
+    private String dbName;
+    private String username;
+    private String password;
 
     public LoadDBForCurricularCourseEnrollmentTest() {
     }
@@ -46,8 +66,19 @@ public class LoadDBForCurricularCourseEnrollmentTest {
     private static void cleanDB(ISuportePersistente persistenceDAO) throws ExcepcaoPersistencia {
 
         // -------------------------------------------------------------------------------------------------------
+
         IPessoa person = (IPessoa) persistenceDAO.getIPessoaPersistente().readByOID(Pessoa.class, PERSON_ID);
+        
+        IRole role = new Role();
+
+        role.setRoleType(RoleType.PERSON);
+        IPersonRole personRole1 = persistenceDAO.getIPersistentPersonRole().readByPersonAndRole(person, role);
+
+        role.setRoleType(RoleType.STUDENT);
+        IPersonRole personRole2 = persistenceDAO.getIPersistentPersonRole().readByPersonAndRole(person, role);
+
         IStudent student = (IStudent) persistenceDAO.getIPersistentStudent().readByOID(Student.class, STUDENT_ID);
+        
         IStudentCurricularPlan studentCurricularPlan = (IStudentCurricularPlan) persistenceDAO
                 .getIStudentCurricularPlanPersistente().readByOID(StudentCurricularPlan.class, STUDENT_CURRICULAR_PLAN_ID);
         
@@ -104,17 +135,84 @@ public class LoadDBForCurricularCourseEnrollmentTest {
         if (student != null) {
             persistenceDAO.getIPersistentStudent().deleteByOID(Student.class, student.getIdInternal());
         }
+
+        if (personRole1 != null) {
+            persistenceDAO.getIPersistentPersonRole().deleteByOID(PersonRole.class, personRole1.getIdInternal());
+        }
+
+        if (personRole2 != null) {
+            persistenceDAO.getIPersistentPersonRole().deleteByOID(PersonRole.class, personRole2.getIdInternal());
+        }
     }
 
     private static void loadDB() throws Exception {
-        dbaccess dbAcessPoint = new dbaccess();
-        dbAcessPoint.openConnection();
-        dbAcessPoint.loadDataBase(FILE);
-        dbAcessPoint.closeConnection();
+        LoadDBForCurricularCourseEnrollmentTest obj = new LoadDBForCurricularCourseEnrollmentTest();
+        obj.openConnection();
+        obj.loadDataBase(FILE);
+        obj.closeConnection();
     }
 
     private static void clearCache(ISuportePersistente persistenceDAO) {
         persistenceDAO.clearCache();
+    }
+
+    public void openConnection() throws Exception {
+        if (this.connection == null) {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection jdbcConnection = DriverManager.getConnection(this.getDbName(), this.getUsername(), this.getPassword());
+            this.connection = new DatabaseConnection(jdbcConnection);
+        }
+    }
+
+    public void closeConnection() throws Exception {
+        this.connection.close();
+        this.connection = null;
+    }
+
+    public void loadDataBase(String filename) throws Exception {
+        FileReader fileReader = new FileReader(new File(filename));
+        IDataSet dataSet = new FlatXmlDataSet(fileReader);
+        DatabaseOperation.INSERT.execute(this.connection, dataSet);
+    }
+
+    public IDatabaseConnection getConnection() {
+        return this.connection;
+    }
+
+    public String getDbName() {
+        if (this.dbName == null) {
+            return "jdbc:mysql://localhost/ciapl";
+        } else {
+            return "jdbc:mysql://localhost/" + this.dbName;
+        }
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public String getPassword() {
+        if (this.password == null) {
+            return "";
+        } else {
+            return this.password;
+        }
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        if (this.username == null) {
+            return "root";
+        } else {
+            return this.username;
+        }
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
 }
