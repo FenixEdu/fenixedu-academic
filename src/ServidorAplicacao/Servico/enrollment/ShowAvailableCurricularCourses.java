be036663e9ae48bf5646c3d272f49e976262177c
João Mota <joao.mota@ist.utl.pt>
2004-07-12 14:39:31 +0000

    *** empty log message ***

--- src/ServidorAplicacao/Servico/enrollment/ShowAvailableCurricularCourses.java
+++ src/ServidorAplicacao/Servico/enrollment/ShowAvailableCurricularCourses.java
@@ -24,6 +24,7 @@ public class ShowAvailableCurricularCourses extends
             Integer studentCurricularPlanId, Integer studentNumber)
             throws FenixServiceException {
         try {
+
             IStudent student = getStudent(studentNumber);
 
             if (student != null) {
