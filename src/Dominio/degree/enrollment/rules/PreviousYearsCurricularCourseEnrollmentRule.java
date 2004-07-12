78b516430965583f739cb5b240f66f19e3aa02d3
David Santos <david.santos@ist.utl.pt>
2004-07-12 13:10:08 +0000

    *** empty log message ***

--- src/Dominio/degree/enrollment/rules/PreviousYearsCurricularCourseEnrollmentRule.java
+++ src/Dominio/degree/enrollment/rules/PreviousYearsCurricularCourseEnrollmentRule.java
@@ -17,123 +17,114 @@ import Dominio.degree.enrollment.CurricularCourse2Enroll;
  * @author David Santos in Jun 23, 2004
  */
 
-public class PreviousYearsCurricularCourseEnrollmentRule implements IEnrollmentRule
-{
-	private IExecutionPeriod executionPeriod;
-	private IBranch studentBranch;
-	private int degreeDuration;
+public class PreviousYearsCurricularCourseEnrollmentRule implements IEnrollmentRule {
 
-	public PreviousYearsCurricularCourseEnrollmentRule(IStudentCurricularPlan studentCurricularPlan,
-														IExecutionPeriod executionPeriod)
-	{
-		this.executionPeriod = executionPeriod;
-		this.studentBranch = studentCurricularPlan.getBranch();
-		this.degreeDuration = studentCurricularPlan.getDegreeCurricularPlan().getDegreeDuration().intValue();
-	}
+    private IExecutionPeriod executionPeriod;
+    private IBranch studentBranch;
+    private int degreeDuration;
 
-	public List apply(List curricularCoursesToBeEnrolledIn)
-	{
-		Map map = buildHashMapForCurricularCoursesList(curricularCoursesToBeEnrolledIn);
-		boolean canPassToNextYear = false;
+    public PreviousYearsCurricularCourseEnrollmentRule(IStudentCurricularPlan studentCurricularPlan,
+            IExecutionPeriod executionPeriod) {
+        this.executionPeriod = executionPeriod;
+        this.studentBranch = studentCurricularPlan.getBranch();
+        this.degreeDuration = studentCurricularPlan.getDegreeCurricularPlan().getDegreeDuration()
+                .intValue();
+    }
 
-		for (int i = 1; i <= this.degreeDuration; i++)
-		{
-			List curricularCourses = (List) map.get(new Integer(i));
-			
-			if ( (curricularCourses != null) && (!curricularCourses.isEmpty()) )
-			{
-				int size = curricularCourses.size();
-				int counter = 0;
+    public List apply(List curricularCoursesToBeEnrolledIn) {
 
-				for (int j = 0; j < size; j++)
-				{
-					CurricularCourse2Enroll curricularCourse2Enroll = (CurricularCourse2Enroll) curricularCourses.get(j);
-					if (curricularCourse2Enroll.getEnrollmentType().equals(CurricularCourseEnrollmentType.TEMPORARY))
-					{
-						counter++;
-					}
-				}
-				
-				if (counter == size)
-				{
-					canPassToNextYear = true;
-				} else
-				{
-					canPassToNextYear = false;
+        Map map = buildHashMapForCurricularCoursesList(curricularCoursesToBeEnrolledIn);
+        boolean canPassToNextYear = true;
 
-					for (int j = (i+1); j <= this.degreeDuration; j++)
-					{
-						List curricularCoursesList = (List) map.get(new Integer(j));
-						
-						if ( (curricularCoursesList != null) && (!curricularCoursesList.isEmpty()) )
-						{
-							curricularCoursesToBeEnrolledIn.removeAll(curricularCoursesList);
-						}
-					}
-				}
-			}
-			
-			if (!canPassToNextYear)
-			{
-				break;
-			}
-		}
+        for (int i = 1; (i <= this.degreeDuration) && canPassToNextYear; i++) {
+        
+            List curricularCourses = (List) map.get(new Integer(i));
 
-		return curricularCoursesToBeEnrolledIn;
-	}
+            if ((curricularCourses != null) && (!curricularCourses.isEmpty())) {
+            
+                int size = curricularCourses.size();
+                int counter = 0;
 
-	private Map buildHashMapForCurricularCoursesList(List curricularCourses2Enroll)
-	{
-		Map map = new HashMap();
-		
-		List copy = new ArrayList();
-		copy.addAll(curricularCourses2Enroll);
-		
-		for (int i = 1; i <= this.degreeDuration; i++)
-		{
-			int size = copy.size();
+                for (int j = 0; j < size; j++) {
+                
+                    CurricularCourse2Enroll curricularCourse2Enroll = (CurricularCourse2Enroll) curricularCourses
+                            .get(j);
+                    if (curricularCourse2Enroll.getEnrollmentType().equals(
+                            CurricularCourseEnrollmentType.TEMPORARY)) {
+                        counter++;
+                    }
+                }
 
-			List curricularCoursesToRemove = new ArrayList();
-			
-			for (int j = 0; j < size; j++)
-			{
-				CurricularCourse2Enroll curricularCourse2Enroll = (CurricularCourse2Enroll) copy.get(j);
-				
-				if (isCurricularCourseFromYear(i, curricularCourse2Enroll.getCurricularCourse()))
-				{
-					putCurricularCourseInHashMap(map, i, curricularCourse2Enroll);
-					curricularCoursesToRemove.add(curricularCourse2Enroll);
-				}
-			}
-			
-			copy.removeAll(curricularCoursesToRemove);
-		}
+                if (counter == size) {
+                    canPassToNextYear = true;
+                } else {
+                    
+                    canPassToNextYear = false;
 
-		return map;
-	}
+                    for (int j = (i + 1); j <= this.degreeDuration; j++) {
+                        List curricularCoursesList = (List) map.get(new Integer(j));
 
-	private boolean isCurricularCourseFromYear(int i, ICurricularCourse curricularCourse)
-	{
-		return (curricularCourse.getCurricularYearByBranchAndSemester(this.studentBranch, this.executionPeriod.getSemester())
-			.getYear().intValue() == i);
-	}
+                        if ((curricularCoursesList != null) && (!curricularCoursesList.isEmpty())) {
+                            curricularCoursesToBeEnrolledIn.removeAll(curricularCoursesList);
+                        }
+                    }
+                }
+            } else {
+                canPassToNextYear = true;
+            }
+        }
 
-	private void putCurricularCourseInHashMap(Map map, int i, CurricularCourse2Enroll curricularCourse2Enroll)
-	{
-		Integer key = new Integer(i);
+        return curricularCoursesToBeEnrolledIn;
+    }
 
-		List curricularCoursesList = (List) map.get(key);
-		if (curricularCoursesList == null)
-		{
-			curricularCoursesList = new ArrayList();
-			curricularCoursesList.add(curricularCourse2Enroll);
-		} else
-		{
-			if (!curricularCoursesList.contains(curricularCourse2Enroll)) {
-				curricularCoursesList.add(curricularCourse2Enroll);
-			}
-		}
-		
-		map.put(key, curricularCoursesList);
-	}
+    private Map buildHashMapForCurricularCoursesList(List curricularCourses2Enroll) {
+
+        Map map = new HashMap();
+
+        List copy = new ArrayList();
+        copy.addAll(curricularCourses2Enroll);
+
+        for (int i = 1; i <= this.degreeDuration; i++) {
+            int size = copy.size();
+
+            List curricularCoursesToRemove = new ArrayList();
+
+            for (int j = 0; j < size; j++) {
+                CurricularCourse2Enroll curricularCourse2Enroll = (CurricularCourse2Enroll) copy.get(j);
+
+                if (isCurricularCourseFromYear(i, curricularCourse2Enroll.getCurricularCourse())) {
+                    putCurricularCourseInHashMap(map, i, curricularCourse2Enroll);
+                    curricularCoursesToRemove.add(curricularCourse2Enroll);
+                }
+            }
+
+            copy.removeAll(curricularCoursesToRemove);
+        }
+
+        return map;
+    }
+
+    private boolean isCurricularCourseFromYear(int i, ICurricularCourse curricularCourse) {
+        return (curricularCourse.getCurricularYearByBranchAndSemester(this.studentBranch,
+                this.executionPeriod.getSemester()).getYear().intValue() == i);
+    }
+
+    private void putCurricularCourseInHashMap(Map map, int i,
+            CurricularCourse2Enroll curricularCourse2Enroll) {
+        
+        Integer key = new Integer(i);
+
+        List curricularCoursesList = (List) map.get(key);
+        
+        if (curricularCoursesList == null) {
+            curricularCoursesList = new ArrayList();
+            curricularCoursesList.add(curricularCourse2Enroll);
+        } else {
+            if (!curricularCoursesList.contains(curricularCourse2Enroll)) {
+                curricularCoursesList.add(curricularCourse2Enroll);
+            }
+        }
+
+        map.put(key, curricularCoursesList);
+    }
 }
