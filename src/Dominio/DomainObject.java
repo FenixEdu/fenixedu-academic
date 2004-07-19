fa7f8f13594942219783e24c85e9c9875503b86b
Luis Cruz <luis.cruz@ist.utl.pt>
2004-07-19 10:59:59 +0000

    Possible solution for usefull equals/hashcode pair.

--- src/Dominio/DomainObject.java
+++ src/Dominio/DomainObject.java
@@ -4,53 +4,72 @@
  */
 package Dominio;
 
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author jpvl
  */
 
-abstract public class DomainObject implements IDomainObject
-{
+abstract public class DomainObject implements IDomainObject {
 
-    private Integer idInternal;
-    private Integer ackOptLock;
+	private Integer idInternal;
 
-    public DomainObject()
-    {
-    }
+	private Integer ackOptLock;
 
-    public DomainObject(Integer idInternal)
-    {
-        setIdInternal(idInternal);
-    }
+	public DomainObject() {
+	}
 
-    /**
+	public DomainObject(Integer idInternal) {
+		setIdInternal(idInternal);
+	}
+
+	/**
 	 * @return Integer
 	 */
-    public Integer getIdInternal()
-    {
-        return idInternal;
-    }
+	public Integer getIdInternal() {
+		return idInternal;
+	}
 
-    /**
+	/**
 	 * Sets the idInternal.
 	 * 
 	 * @param idInternal
 	 *            The idInternal to set
 	 */
-    public void setIdInternal(Integer idInternal)
-    {
-        this.idInternal = idInternal;
-    }
+	public void setIdInternal(Integer idInternal) {
+		this.idInternal = idInternal;
+	}
 
-    public Integer getAckOptLock()
-    {
-        return ackOptLock;
-    }
+	public Integer getAckOptLock() {
+		return ackOptLock;
+	}
 
-    public void setAckOptLock(Integer ackOptLock)
-    {
-        this.ackOptLock = ackOptLock;
-    }
+	public void setAckOptLock(Integer ackOptLock) {
+		this.ackOptLock = ackOptLock;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj != null && obj instanceof IDomainObject) {
+			IDomainObject domainObject = (IDomainObject) obj;
+			if (getIdInternal() != null && domainObject.getIdInternal() != null
+					&& getIdInternal().equals(domainObject.getIdInternal())) {
+				List thisInterfaces = Arrays.asList(getClass().getInterfaces());
+				List objInterfaces = Arrays.asList(domainObject.getClass().getInterfaces());
+				if (thisInterfaces.containsAll(objInterfaces) && objInterfaces.containsAll(thisInterfaces)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		if (getIdInternal() != null) {
+			return getIdInternal().intValue();
+		} else {
+			return super.hashCode();
+		}
+	}
 
 }
