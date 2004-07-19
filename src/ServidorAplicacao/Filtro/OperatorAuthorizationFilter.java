e7070c40012c9d3fb483b2202e430122758f2928
Tânia Pousão <tania.pousao@ist.utl.pt>
2004-07-19 14:40:32 +0000

    *** empty log message ***

---
+++ src/ServidorAplicacao/Filtro/OperatorAuthorizationFilter.java
@@ -0,0 +1,36 @@
+/*
+ * Created on 19/Jul/2004 by Tânia Pousão
+ *
+ */
+package ServidorAplicacao.Filtro;
+
+import Util.RoleType;
+
+/**
+ * @author Tânia Pousão
+ */
+public class OperatorAuthorizationFilter extends AuthorizationByRoleFilter {
+
+  
+	// the singleton of this class
+	public final static PersonAuthorizationFilter instance = new PersonAuthorizationFilter();
+
+	/**
+	 * The singleton access method of this class.
+	 *
+	 * @return Returns the instance of this class responsible for the
+	 * authorization access to services.
+	 **/
+	public static Filtro getInstance() {
+	  return instance;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see ServidorAplicacao.Filtro.AuthorizationByRoleFilter#getRoleType()
+	 */
+	protected RoleType getRoleType() {
+		return RoleType.OPERATOR;
+	}
+
+}
