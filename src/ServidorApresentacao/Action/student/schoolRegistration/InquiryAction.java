5168fefab0010819146fb513141c989a3c78505f
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2004-07-20 12:44:58 +0000

    *** empty log message ***

--- src/ServidorApresentacao/Action/student/schoolRegistration/InquiryAction.java
+++ src/ServidorApresentacao/Action/student/schoolRegistration/InquiryAction.java
@@ -4,10 +4,7 @@
  */
 package ServidorApresentacao.Action.student.schoolRegistration;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +16,7 @@ import org.apache.struts.action.DynaActionForm;
 
 import ServidorAplicacao.IUserView;
 import ServidorApresentacao.Action.base.FenixAction;
+import ServidorApresentacao.Action.sop.utils.ServiceUtils;
 import ServidorApresentacao.Action.sop.utils.SessionUtils;
 
 /**
@@ -27,55 +25,17 @@ import ServidorApresentacao.Action.sop.utils.SessionUtils;
  */
 public class InquiryAction extends FenixAction {
 
-   /* public ActionForward prepareViewQuestions(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
-            throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
 
-        IUserView userView = SessionUtils.getUserView(request);
-
-        System.out.println("Eu venho aqui, depois daqui who knows...");
-        Object args[] = {};      
-
-        List inquiryQuestions = (List) ServiceUtils.executeService(userView, "ReadInquiryQuestions", args);
-        
-        DynaActionForm inquiryForm = (DynaActionForm) form;
-        HashMap answersMap = new HashMap();
-        inquiryForm.set("answersMap", answersMap);
-       
-        
-        request.setAttribute("inquiryQuestions", inquiryQuestions);
-
-        System.out.println("inquiryQuestions.size()= " + inquiryQuestions.size());
-
-        DynaActionForm inquiryForm = (DynaActionForm) form;
-        Integer answers[] = new Integer[inquiryQuestions.size()];
-        inquiryForm.set("answers", answers);
-        request.setAttribute("inquiryForm", inquiryForm);
-
-        return mapping.findForward("viewQuestions");
-    }*/
-
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        IUserView userView = SessionUtils.getUserView(request);
-        System.out.println("As respostas sao: ");
+        IUserView userView = SessionUtils.getUserView(request);        
 
         DynaActionForm inquiryForm = (DynaActionForm) form;
         HashMap answersMap = (HashMap) inquiryForm.get("answersMap");
         
+        Object args[] = {userView,answersMap};
+        ServiceUtils.executeService(userView, "WriteInquiryAnswers", args);
 
-        //* INICIO P/ APAGAR
-        List list = new ArrayList();
-        Iterator iterator = answersMap.keySet().iterator();
-
-        while(iterator.hasNext()) {
-           
-            String key = (String) iterator.next(); 
-            System.out.println("O obejcto é isto: " + key);
-            System.out.println("A entrada com id: " + key + "tem o valor: " + answersMap.get(key));
-        }
-            
-        //FIM
-
-        return mapping.findForward("");
+        return mapping.findForward("viewQuestions");
     }
 }
