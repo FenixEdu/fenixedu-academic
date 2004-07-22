70c085de5d1cb94e3cc1194936989b2ca158f157
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2004-07-22 23:02:34 +0000

    added guide entry information

--- src/Dominio/transactions/PaymentTransaction.java
+++ src/Dominio/transactions/PaymentTransaction.java
@@ -1,5 +1,7 @@
 package Dominio.transactions;
 
+import Dominio.IGuideEntry;
+
 /**
  * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
  * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
@@ -7,4 +9,37 @@ package Dominio.transactions;
  */
 public abstract class PaymentTransaction extends Transaction {
 
+    private Integer keyGuideEntry;
+
+    private IGuideEntry guideEntry;
+
+    /**
+     * @return Returns the guideEntry.
+     */
+    public IGuideEntry getGuideEntry() {
+        return guideEntry;
+    }
+
+    /**
+     * @param guideEntry
+     *            The guideEntry to set.
+     */
+    public void setGuideEntry(IGuideEntry guideEntry) {
+        this.guideEntry = guideEntry;
+    }
+
+    /**
+     * @return Returns the keyGuideEntry.
+     */
+    public Integer getKeyGuideEntry() {
+        return keyGuideEntry;
+    }
+
+    /**
+     * @param keyGuideEntry
+     *            The keyGuideEntry to set.
+     */
+    public void setKeyGuideEntry(Integer keyGuideEntry) {
+        this.keyGuideEntry = keyGuideEntry;
+    }
 }
