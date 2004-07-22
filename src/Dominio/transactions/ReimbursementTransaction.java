63b938f989af73d0d01aacbf372206be3e3835ae
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2004-07-22 23:02:47 +0000

    added reimbursement guide entry information

--- src/Dominio/transactions/ReimbursementTransaction.java
+++ src/Dominio/transactions/ReimbursementTransaction.java
@@ -1,5 +1,7 @@
 package Dominio.transactions;
 
+import Dominio.reimbursementGuide.ReimbursementGuideEntry;
+
 /**
  * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
  * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
@@ -7,4 +9,37 @@ package Dominio.transactions;
  */
 public abstract class ReimbursementTransaction extends Transaction {
 
+    private Integer keyReimbursementGuideEntry;
+
+    private ReimbursementGuideEntry reimbursementGuideEntry;
+
+    /**
+     * @return Returns the keyReimbursementGuideEntry.
+     */
+    public Integer getKeyReimbursementGuideEntry() {
+        return keyReimbursementGuideEntry;
+    }
+
+    /**
+     * @param keyReimbursementGuideEntry
+     *            The keyReimbursementGuideEntry to set.
+     */
+    public void setKeyReimbursementGuideEntry(Integer keyReimbursementGuideEntry) {
+        this.keyReimbursementGuideEntry = keyReimbursementGuideEntry;
+    }
+
+    /**
+     * @return Returns the reimbursementGuideEntry.
+     */
+    public ReimbursementGuideEntry getReimbursementGuideEntry() {
+        return reimbursementGuideEntry;
+    }
+
+    /**
+     * @param reimbursementGuideEntry
+     *            The reimbursementGuideEntry to set.
+     */
+    public void setReimbursementGuideEntry(ReimbursementGuideEntry reimbursementGuideEntry) {
+        this.reimbursementGuideEntry = reimbursementGuideEntry;
+    }
 }
