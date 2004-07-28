9160e2cffe34899fa34422b7afef1d83fddd6593
João Luz <joao.luz@ist.utl.pt>
2004-07-28 22:41:55 +0000

    added MasterDegreeOnly of DetailedProfessorship calculation

--- src/ServidorAplicacao/Servico/teacher/professorship/ReadDetailedTeacherProfessorshipsAbstractService.java
+++ src/ServidorAplicacao/Servico/teacher/professorship/ReadDetailedTeacherProfessorshipsAbstractService.java
@@ -27,94 +27,96 @@ import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.IPersistentTeacher;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
+import Util.TipoCurso;
 
 /**
  * @author jpvl
  */
-public class ReadDetailedTeacherProfessorshipsAbstractService implements IService
-{
+public class ReadDetailedTeacherProfessorshipsAbstractService implements IService {
+    private final class Professorships2DetailProfessorship implements Transformer {
+        private final List responsibleFors;
+
+        private Professorships2DetailProfessorship(List responsibleFors) {
+            super();
+            this.responsibleFors = responsibleFors;
+        }
+
+        public Object transform(Object input) {
+            IProfessorship professorship = (IProfessorship) input;
+            InfoProfessorship infoProfessorShip = InfoProfessorshipWithInfoExecutionCourseAndInfoExecutionPeriod
+                    .newInfoFromDomain(professorship);
+
+            final DetailedProfessorship detailedProfessorship = new DetailedProfessorship();
+
+            IExecutionCourse executionCourse = professorship.getExecutionCourse();
+            List executionCourseCurricularCoursesList = getInfoCurricularCourses(detailedProfessorship,
+                    executionCourse);
+
+            IResponsibleFor responsibleFor = new ResponsibleFor();
+            responsibleFor.setExecutionCourse(professorship.getExecutionCourse());
+            responsibleFor.setTeacher(professorship.getTeacher());
+
+            Boolean responsible = Boolean.valueOf(responsibleFors.contains(responsibleFor));
+            detailedProfessorship.setResponsibleFor(responsible);
+
+            detailedProfessorship.setInfoProfessorship(infoProfessorShip);
+            detailedProfessorship
+                    .setExecutionCourseCurricularCoursesList(executionCourseCurricularCoursesList);
+
+            return detailedProfessorship;
+        }
+
+        private List getInfoCurricularCourses(final DetailedProfessorship detailedProfessorship,
+                IExecutionCourse executionCourse) {
+
+            List infoCurricularCourses = (List) CollectionUtils.collect(executionCourse
+                    .getAssociatedCurricularCourses(), new Transformer() {
+
+                public Object transform(Object input) {
+                    ICurricularCourse curricularCourse = (ICurricularCourse) input;
+                    InfoCurricularCourse infoCurricularCourse = InfoCurricularCourseWithInfoDegree
+                            .newInfoFromDomain(curricularCourse);
+                    TipoCurso degreeType = curricularCourse.getDegreeCurricularPlan().getDegree()
+                            .getTipoCurso();
+                    if (degreeType.equals(TipoCurso.LICENCIATURA_OBJ)) {
+                        detailedProfessorship.setMasterDegreeOnly(Boolean.FALSE);
+                    }
+                    return infoCurricularCourse;
+                }
+            });
+            return infoCurricularCourses;
+        }
+    }
+
     /**
-	 * @author jpvl
-	 */
-    public class NotFoundTeacher extends FenixServiceException
-    {
+     * @author jpvl
+     */
+    public class NotFoundTeacher extends FenixServiceException {
 
     }
+
     protected List getDetailedProfessorships(List professorships, final List responsibleFors,
-            ISuportePersistente sp)
-    {
+            ISuportePersistente sp) {
 
         List detailedProfessorshipList = (List) CollectionUtils.collect(professorships,
-                new Transformer()
-                {
-
-                    public Object transform(Object input)
-                    {
-                        IProfessorship professorship = (IProfessorship) input;
-                        //CLONER
-                        //InfoProfessorship infoProfessorShip = Cloner
-                                //.copyIProfessorship2InfoProfessorship(professorship);
-                        InfoProfessorship infoProfessorShip = InfoProfessorshipWithInfoExecutionCourseAndInfoExecutionPeriod.newInfoFromDomain(professorship);
-
-                        List executionCourseCurricularCoursesList = getInfoCurricularCourses(professorship
-                                .getExecutionCourse());
-
-                        DetailedProfessorship detailedProfessorship = new DetailedProfessorship();
-
-                        IResponsibleFor responsibleFor = new ResponsibleFor();
-                        responsibleFor.setExecutionCourse(professorship.getExecutionCourse());
-                        responsibleFor.setTeacher(professorship.getTeacher());
-                        detailedProfessorship.setResponsibleFor(Boolean.valueOf(responsibleFors
-                                .contains(responsibleFor)));
-
-                        detailedProfessorship.setInfoProfessorship(infoProfessorShip);
-                        detailedProfessorship
-                                .setExecutionCourseCurricularCoursesList(executionCourseCurricularCoursesList);
-
-                        return detailedProfessorship;
-                    }
-
-                    private List getInfoCurricularCourses(IExecutionCourse executionCourse)
-                    {
-
-                        List infoCurricularCourses = (List) CollectionUtils.collect(executionCourse
-                                .getAssociatedCurricularCourses(),
-                                new Transformer()
-                                {
-
-                                    public Object transform(Object input)
-                                    {
-                                        ICurricularCourse curricularCourse = (ICurricularCourse) input;
-                                        //CLONER
-                                        //InfoCurricularCourse infoCurricularCourse = Cloner
-                                                //.copyCurricularCourse2InfoCurricularCourse(curricularCourse);
-                                        InfoCurricularCourse infoCurricularCourse = InfoCurricularCourseWithInfoDegree.newInfoFromDomain(curricularCourse);
-                                        return infoCurricularCourse;
-                                    }
-                                });
-                        return infoCurricularCourses;
-                    }
-                });
+                new Professorships2DetailProfessorship(responsibleFors));
 
         return detailedProfessorshipList;
     }
 
-    protected ISuportePersistente getDAOFactory() throws ExcepcaoPersistencia
-    {
+    protected ISuportePersistente getDAOFactory() throws ExcepcaoPersistencia {
         return SuportePersistenteOJB.getInstance();
     }
 
     /**
-	 * @param teacherId
-	 * @return
-     * @throws ExcepcaoPersistencia
-	 */
+     * @param teacherId
+     * @return @throws
+     *         ExcepcaoPersistencia
+     */
     protected ITeacher readTeacher(Integer teacherId, IPersistentTeacher teacherDAO)
-            throws NotFoundTeacher, ExcepcaoPersistencia
-    {
-        ITeacher teacher = (ITeacher) teacherDAO.readByOID(Teacher.class,teacherId);
-        if (teacher == null)
-        {
+            throws NotFoundTeacher, ExcepcaoPersistencia {
+        ITeacher teacher = (ITeacher) teacherDAO.readByOID(Teacher.class, teacherId);
+        if (teacher == null) {
             throw new NotFoundTeacher();
         }
         return teacher;
