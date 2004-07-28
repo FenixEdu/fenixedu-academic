b2a8b46f3a12136648c94133c3ce47acc1158895
João Luz <joao.luz@ist.utl.pt>
2004-07-28 22:49:47 +0000

    added code for to make possible reuse this action in facultyAdmOffice application

--- src/ServidorApresentacao/Action/department/ReadTeacherProfessorshipsByExecutionYearAction.java
+++ src/ServidorApresentacao/Action/department/ReadTeacherProfessorshipsByExecutionYearAction.java
@@ -4,13 +4,15 @@
  */
 package ServidorApresentacao.Action.department;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
-import org.apache.commons.collections.Transformer;
 import org.apache.struts.action.DynaActionForm;
 
 import DataBeans.InfoDepartment;
@@ -21,38 +23,37 @@ import ServidorAplicacao.IUserView;
 import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorApresentacao.Action.sop.utils.ServiceUtils;
 import Util.PeriodState;
+import Util.RoleType;
 
 /**
  * @author jpvl
  */
-public class ReadTeacherProfessorshipsByExecutionYearAction extends AbstractReadProfessorshipsAction
-{
+public class ReadTeacherProfessorshipsByExecutionYearAction extends AbstractReadProfessorshipsAction {
 
     /*
-	 * (non-Javadoc)
-	 * 
-	 * @see ServidorApresentacao.Action.department.AbstractReadProfessorshipsAction#getDetailedProfessorships(ServidorAplicacao.IUserView,
-	 *          java.lang.Integer, org.apache.struts.action.DynaActionForm,
-	 *          javax.servlet.http.HttpServletRequest)
-	 */
+     * (non-Javadoc)
+     * 
+     * @see ServidorApresentacao.Action.department.AbstractReadProfessorshipsAction#getDetailedProfessorships(ServidorAplicacao.IUserView,
+     *      java.lang.Integer, org.apache.struts.action.DynaActionForm,
+     *      javax.servlet.http.HttpServletRequest)
+     */
     List getDetailedProfessorships(IUserView userView, Integer teacherId, DynaActionForm actionForm,
-            HttpServletRequest request) throws FenixServiceException
-    {
-        
+            HttpServletRequest request) throws FenixServiceException {
+
         List detailedInfoProfessorshipList = (List) ServiceUtils.executeService(userView,
-                "ReadDetailedTeacherProfessorshipsByExecutionYear", new Object[]{teacherId, actionForm.get("executionYearId")});
+                "ReadDetailedTeacherProfessorshipsByExecutionYear", new Object[] { teacherId,
+                        actionForm.get("executionYearId") });
         return detailedInfoProfessorshipList;
     }
 
     /*
-	 * (non-Javadoc)
-	 * 
-	 * @see ServidorApresentacao.Action.department.AbstractReadProfessorshipsAction#readExtraInformation(javax.servlet.http.HttpServletRequest,
-	 *          org.apache.struts.action.DynaActionForm)
-	 */
+     * (non-Javadoc)
+     * 
+     * @see ServidorApresentacao.Action.department.AbstractReadProfessorshipsAction#readExtraInformation(javax.servlet.http.HttpServletRequest,
+     *      org.apache.struts.action.DynaActionForm)
+     */
     protected void extraPreparation(IUserView userView, InfoTeacher infoTeacher,
-            HttpServletRequest request, DynaActionForm dynaForm) throws FenixServiceException
-    {
+            HttpServletRequest request, DynaActionForm dynaForm) throws FenixServiceException {
 
         prepareConstants(userView, infoTeacher, request);
 
@@ -61,76 +62,74 @@ public class ReadTeacherProfessorshipsByExecutionYearAction extends AbstractRead
     }
 
     /**
-	 * @param dynaForm
-	 */
-    private void prepareForm(DynaActionForm dynaForm, HttpServletRequest request)
-    {
+     * @param dynaForm
+     */
+    private void prepareForm(DynaActionForm dynaForm, HttpServletRequest request) {
         InfoExecutionYear infoExecutionYear = (InfoExecutionYear) request.getAttribute("executionYear");
         InfoTeacher infoTeacher = (InfoTeacher) request.getAttribute("infoTeacher");
         dynaForm.set("idInternal", infoTeacher.getIdInternal());
         dynaForm.set("teacherId", infoTeacher.getIdInternal());
-        if(dynaForm.get("executionYearId") == null) {
+        if (dynaForm.get("executionYearId") == null) {
             dynaForm.set("executionYearId", infoExecutionYear.getIdInternal());
         }
 
         List detailedProfessorshipList = (List) request.getAttribute("detailedProfessorshipList");
 
-        List predicatedList = (List) CollectionUtils.select(detailedProfessorshipList, new Predicate()
-        {
+        List executionCourseIds = new ArrayList();
+        Map hours = new HashMap();
+        for (int i = 0; i < detailedProfessorshipList.size(); i++) {
+            DetailedProfessorship dps = (DetailedProfessorship) detailedProfessorshipList.get(i);
 
-            public boolean evaluate(Object input)
-            {
-                DetailedProfessorship detailedProfessorship = (DetailedProfessorship) input;
-                return detailedProfessorship.getResponsibleFor().booleanValue();
+            Integer executionCourseId = dps.getInfoProfessorship().getInfoExecutionCourse()
+                    .getIdInternal();
+            if (dps.getResponsibleFor().booleanValue()) {
+                executionCourseIds.add(executionCourseId);
             }
-        });
-
-        List executionCourseIds = (List) CollectionUtils.collect(predicatedList, new Transformer()
-        {
-
-            public Object transform(Object input)
-            {
-                DetailedProfessorship detailedProfessorship = (DetailedProfessorship) input;
-                Integer executionCourseId = detailedProfessorship.getInfoProfessorship()
-                        .getInfoExecutionCourse().getIdInternal();
-                return executionCourseId;
+            if (dps.getMasterDegreeOnly().booleanValue()) {
+                hours
+                        .put(executionCourseId.toString(), dps.getInfoProfessorship().getHours()
+                                .toString());
             }
-        });
-        
-        dynaForm.set("executionCourseResponsability", executionCourseIds.toArray(new Integer [] {}));
+        }
+
+        dynaForm.set("executionCourseResponsability", executionCourseIds.toArray(new Integer[] {}));
+        dynaForm.set("hours", hours);
+
     }
 
     private void prepareConstants(IUserView userView, InfoTeacher infoTeacher, HttpServletRequest request)
-            throws FenixServiceException
-    {
+            throws FenixServiceException {
 
-        List executionYears = (List) ServiceUtils.executeService(userView, "ReadNotClosedExecutionYears", null);
-        
-        InfoExecutionYear infoExecutionYear = (InfoExecutionYear) CollectionUtils.find(executionYears, new Predicate() {
-            public boolean evaluate(Object arg0) {
-                InfoExecutionYear infoExecutionYearElem = (InfoExecutionYear)arg0;
-                if(infoExecutionYearElem.getState().equals(PeriodState.CURRENT)) {
-                    return true;
-                }
-                return false;
-            }
-        });
+        List executionYears = (List) ServiceUtils.executeService(userView,
+                "ReadNotClosedExecutionYears", null);
 
-//        InfoExecutionYear infoExecutionYear = (InfoExecutionYear) ServiceUtils.executeService(userView,
-//                "ReadCurrentExecutionYear", null);
+        InfoExecutionYear infoExecutionYear = (InfoExecutionYear) CollectionUtils.find(executionYears,
+                new Predicate() {
+                    public boolean evaluate(Object arg0) {
+                        InfoExecutionYear infoExecutionYearElem = (InfoExecutionYear) arg0;
+                        if (infoExecutionYearElem.getState().equals(PeriodState.CURRENT)) {
+                            return true;
+                        }
+                        return false;
+                    }
+                });
 
-        Object args[] = {userView.getUtilizador()};
-        InfoDepartment userDepartment = (InfoDepartment) ServiceUtils.executeService(userView,
-                "ReadDepartmentByUser", args);
+        Object args[] = { userView.getUtilizador() };
 
         args[0] = infoTeacher;
         InfoDepartment teacherDepartment = (InfoDepartment) ServiceUtils.executeService(userView,
                 "ReadDepartmentByTeacher", args);
 
-        request.setAttribute("teacherDepartment", teacherDepartment);
+        if (!request.isUserInRole(RoleType.CREDITS_MANAGER.getName())) {
+            InfoDepartment userDepartment = (InfoDepartment) ServiceUtils.executeService(userView,
+                    "ReadDepartmentByUser", args);
+            request.setAttribute("isDepartmentManager", Boolean.valueOf(userDepartment
+                    .equals(teacherDepartment)));
+        } else {
+            request.setAttribute("isDepartmentManager", Boolean.FALSE);
+        }
 
-        request.setAttribute("isDepartmentManager", Boolean.valueOf(userDepartment
-                .equals(teacherDepartment)));
+        request.setAttribute("teacherDepartment", teacherDepartment);
 
         request.setAttribute("executionYear", infoExecutionYear);
         request.setAttribute("executionYears", executionYears);
