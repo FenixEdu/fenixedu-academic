3a5b1bf48626db799da0a04aa42e91864a5b997b
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2004-07-28 22:10:32 +0000

    initial version

---
+++ src/DataBeans/ISmsDTO.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Jul 18, 2004
+ *
+ */
+package DataBeans;
+
+/**
+ * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali</a>
+ * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed</a>
+ *  
+ */
+public interface ISmsDTO {
+    /**
+     * Returns an Sms Text with length <= 160 characters
+     * @return
+     */
+    public String toSmsText();
+}
