5d529097a94999f4f3be53226e6a64cf9f829d7d
João Luz <joao.luz@ist.utl.pt>
2004-07-28 22:44:08 +0000

    added masterDegreeOnly field

--- src/DataBeans/teacher/professorship/DetailedProfessorship.java
+++ src/DataBeans/teacher/professorship/DetailedProfessorship.java
@@ -16,70 +16,86 @@ import DataBeans.InfoProfessorship;
 /**
  * @author jpvl
  */
-public class DetailedProfessorship extends DataTranferObject
-{
+public class DetailedProfessorship extends DataTranferObject {
     private InfoProfessorship infoProfessorship;
+
     private Boolean responsibleFor;
+
     private List executionCourseCurricularCoursesList;
+
+    private Boolean masterDegreeOnly = Boolean.TRUE;
+
     /**
-	 * @return Returns the executionCourseCurricularCoursesList.
-	 */
-    public List getExecutionCourseCurricularCoursesList()
-    {
+     * @return Returns the executionCourseCurricularCoursesList.
+     */
+    public List getExecutionCourseCurricularCoursesList() {
         return this.executionCourseCurricularCoursesList;
     }
+
     /**
-	 * @param executionCourseCurricularCoursesList
-	 *                   The executionCourseCurricularCoursesList to set.
-	 */
-    public void setExecutionCourseCurricularCoursesList(List executionCourseCurricularCoursesList)
-    {
+     * @param executionCourseCurricularCoursesList
+     *            The executionCourseCurricularCoursesList to set.
+     */
+    public void setExecutionCourseCurricularCoursesList(List executionCourseCurricularCoursesList) {
         this.executionCourseCurricularCoursesList = executionCourseCurricularCoursesList;
     }
+
     /**
-	 * @return Returns the infoProfessorship.
-	 */
-    public InfoProfessorship getInfoProfessorship()
-    {
+     * @return Returns the infoProfessorship.
+     */
+    public InfoProfessorship getInfoProfessorship() {
         return this.infoProfessorship;
     }
+
     /**
-	 * @param infoProfessorship
-	 *                   The infoProfessorship to set.
-	 */
-    public void setInfoProfessorship(InfoProfessorship infoProfessorship)
-    {
+     * @param infoProfessorship
+     *            The infoProfessorship to set.
+     */
+    public void setInfoProfessorship(InfoProfessorship infoProfessorship) {
         this.infoProfessorship = infoProfessorship;
     }
 
-    public List getInfoDegreeList()
-    {
+    public List getInfoDegreeList() {
 
         List infoDegreeList = new ArrayList();
         Iterator iter = executionCourseCurricularCoursesList.iterator();
-        while (iter.hasNext())
-        {
+        while (iter.hasNext()) {
             InfoCurricularCourse infoCurricularCourse = (InfoCurricularCourse) iter.next();
             InfoDegree infoDegree = infoCurricularCourse.getInfoDegreeCurricularPlan().getInfoDegree();
-            if (!infoDegreeList.contains(infoDegree))
-            {
+            if (!infoDegreeList.contains(infoDegree)) {
                 infoDegreeList.add(infoDegree);
             }
         }
         return infoDegreeList;
     }
+
     /**
      * @return Returns the responsibleFor.
      */
-    public Boolean getResponsibleFor()
-    {
+    public Boolean getResponsibleFor() {
         return this.responsibleFor;
     }
+
     /**
-     * @param responsibleFor The responsibleFor to set.
+     * @param responsibleFor
+     *            The responsibleFor to set.
      */
-    public void setResponsibleFor(Boolean responsibleFor)
-    {
+    public void setResponsibleFor(Boolean responsibleFor) {
         this.responsibleFor = responsibleFor;
+    }
+
+    /**
+     * @return Returns the masterDegreeOnly.
+     */
+    public Boolean getMasterDegreeOnly() {
+        return masterDegreeOnly;
+    }
+
+    /**
+     * @param masterDegreeOnly
+     *            The masterDegreeOnly to set.
+     */
+    public void setMasterDegreeOnly(Boolean masterDegreeOnly) {
+        this.masterDegreeOnly = masterDegreeOnly;
     }
 }
