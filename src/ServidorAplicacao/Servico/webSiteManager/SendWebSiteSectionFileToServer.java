c037e402ce52b0120b2a02ba132fc0256a6606f7
Fernanda Quitério <fsq@rnl.ist.utl.pt>
2004-08-02 16:06:50 +0000

    *** empty log message ***

--- src/ServidorAplicacao/Servico/webSiteManager/SendWebSiteSectionFileToServer.java
+++ src/ServidorAplicacao/Servico/webSiteManager/SendWebSiteSectionFileToServer.java
@@ -528,7 +528,7 @@ public class SendWebSiteSectionFileToServer extends ManageWebSiteItem
                 .concat("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
                         + "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
                         + "<head>\n"
-                        + "<title>www.ist.utl.pt - "
+                        + "<title>Instituto Superior T&eacute;cnico | "
                         + infoWebSiteSection.getName()
                         + "</title>\n"
                         + "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />\n"
