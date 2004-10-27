1e78e677b0bc0219cb293a3e88ad9abee01b3f4c
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2004-10-27 18:32:23 +0000

    *** empty log message ***

---
+++ src/ServidorPersistente/OJB/publication/PublicationAuthorOJB.java
@@ -0,0 +1,32 @@
+/*
+ * Created on 27/Out/2004
+ */
+package ServidorPersistente.OJB.publication;
+
+import java.util.List;
+
+import org.apache.ojb.broker.query.Criteria;
+
+import Dominio.publication.PublicationAuthor;
+import ServidorPersistente.ExcepcaoPersistencia;
+import ServidorPersistente.OJB.ObjectFenixOJB;
+
+/**
+ * @author Ricardo Rodrigues
+ */
+public class PublicationAuthorOJB extends ObjectFenixOJB implements IPersistentPublicationAuthor{
+        
+    
+    public List readByPublicationId(Integer publicationId) throws ExcepcaoPersistencia{
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo("keyPublication",publicationId);
+        return queryList(PublicationAuthor.class,criteria);
+    }
+
+    public List readByAuthorId(Integer authorId) throws ExcepcaoPersistencia{
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo("keyAuthor",authorId);
+        return queryList(PublicationAuthor.class,criteria);
+    }
+
+}
