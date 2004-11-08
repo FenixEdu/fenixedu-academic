b30a6a295bec5033420047cccf7bd0be62986397
Carlos Pereira <cgmp@ist.utl.pt>
2004-11-08 11:08:05 +0000

    Small change in FilterTestCase

--- src_tests/ServidorAplicacao/Filtros/FilterTestCase.java
+++ src_tests/ServidorAplicacao/Filtros/FilterTestCase.java
@@ -16,6 +16,7 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.operation.DatabaseOperation;
 
+import pt.utl.ist.berserk.ServiceRequest;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
 
@@ -65,123 +66,12 @@ public abstract class FilterTestCase extends TestCase {
     protected void tearDown() throws Exception {
     }
 
-//    protected void compareDataSet(String expectedFileName) {
-//
-//        try {
-//
-//            FileReader fileReader = new FileReader(new File(expectedFileName));
-//            IDataSet expectedDataSet = new FlatXmlDataSet(fileReader);
-//
-//            IDataSet currentDataSet = getConnection().createDataSet();
-//
-//            LinkedList tableNamesToFilter = readTableNamesToFilter();
-//
-//            int size = tableNamesToFilter.size();
-//            String[] tableNames = new String[size];
-//            for (int i = 0; i < size; i++) {
-//                tableNames[i] = (String) tableNamesToFilter.get(i);
-//            }
-//
-//            IDataSet filteredDateSet = new FilteredDataSet(tableNames, currentDataSet);
-//            Assertion.assertEquals(expectedDataSet, filteredDateSet);
-//        } catch (Exception ex) {
-//            fail("compareDataSet failed to read data set files" + ex);
-//        }
-//    }
-//
-//    protected LinkedList readTableNamesToFilter() {
-//
-//        LinkedList listTableNamesToFilter = new LinkedList();
-//        LinkedList defaultListTableNamesToFilter = new LinkedList();
-//        String stringTableNamesToFilter = "";
-//        String defaultStringTableNamesToFilter = "";
-//
-//        try {
-//            ResourceBundle bundle = new PropertyResourceBundle(new FileInputStream(
-//                    getTableNamesToFilterFilePath()));
-//
-//            stringTableNamesToFilter = bundle.getString(getNameOfServiceToBeTested());
-//
-//            defaultStringTableNamesToFilter = bundle.getString("Default");
-//        } catch (MissingResourceException ex) {
-//            fail("Resource " + getNameOfServiceToBeTested() + " not found in "
-//                    + getTableNamesToFilterFilePath());
-//        } catch (FileNotFoundException ex) {
-//            fail("File " + getTableNamesToFilterFilePath() + " not found.");
-//        } catch (IOException ex) {
-//            fail("IOException reading file " + getTableNamesToFilterFilePath() + " " + ex);
-//        }
-//
-//        StringTokenizer st = new StringTokenizer(stringTableNamesToFilter, ",");
-//        while (st.hasMoreElements())
-//            listTableNamesToFilter.add(st.nextElement());
-//
-//        st = new StringTokenizer(defaultStringTableNamesToFilter, ",");
-//        while (st.hasMoreElements())
-//            defaultListTableNamesToFilter.add(st.nextElement());
-//
-//        //listTableNamesToFilter.addAll(defaultListTableNamesToFilter);
-//
-//        return listTableNamesToFilter;
-//    }
-
-//    protected String getBackUpDataSetFilePath() {
-//        return "etc/testBackup.xml";
-//    }
-
-//    protected String getTableNamesToFilterFilePath() {
-//        return "etc/filterTables.properties";
-//    }
-
-//    /**
-//     * Compares two datasets and uses expected dataset table columns. <br/><b>
-//     * IMPORTANT: </b> DOES NOT USE filterTables.properties ANYMORE Expected
-//     * dataset cannot refer to .dtd, otherwise the method will not use expected
-//     * dataset table columns, but the initial dataset columns.
-//     * 
-//     * @param expectedFileName
-//     */
-//
-//    protected void compareDataSetUsingExceptedDataSetTableColumns(String expectedFileName) {
-//        compareDataSetUsingExceptedDataSetTablesAndColumns(expectedFileName);
-//
-//    }
-//
-//    /**
-//     * Compares two datasets using expected dataset tables and columns
-//     * 
-//     * @param expectedFileName
-//     */
-//    protected void compareDataSetUsingExceptedDataSetTablesAndColumns(String expectedFileName) {
-//        try {
-//
-//            FileReader fileReader = new FileReader(new File(expectedFileName));
-//            IDataSet expectedDataSet = new FlatXmlDataSet(fileReader);
-//
-//            IDataSet currentDataSet = getConnection().createDataSet();
-//
-//            String[] tableNames = expectedDataSet.getTableNames();
-//
-//            IDataSet filteredDateSet = new FilteredDataSet(tableNames, currentDataSet);
-//            int totalTables = tableNames.length;
-//
-//            for (int i = 0; i < totalTables; i++) {
-//                ITable expectedTable = expectedDataSet.getTable(tableNames[i]);
-//                ITable actualTable = filteredDateSet.getTable(tableNames[i]);
-//                SortedTable sortedExpectedTable = new SortedTable(expectedTable);
-//                SortedTable sortedActualTable = new SortedTable(actualTable, expectedTable
-//                        .getTableMetaData());
-//                Assertion.assertEquals(sortedExpectedTable, new CompositeTable(expectedTable
-//                        .getTableMetaData(), sortedActualTable));
-//            }
-//
-//        } catch (Exception ex) {
-//            fail("compareDataSet failed to read data set files" + ex);
-//        }
-//    }
-
     protected abstract String getDataSetFilePath();
 
     protected abstract String getNameOfFilterToBeTested();
+    
+    protected abstract ServiceRequest createWorkingRequest();
+    
+    protected abstract ServiceRequest createNonWorkingRequest();
 
 }
