5777a150e3e7292351ffff8e1c4810d1780e62cb
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-01-18 14:39:38 +0000

    EnrolmentLog

---
+++ src/ServidorPersistente/OJB/log/EnrolmentLogOJB.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Nov 4, 2004
+ */
+package ServidorPersistente.OJB.log;
+
+import ServidorPersistente.OJB.PersistentObjectOJB;
+
+/**
+ * @author nmgo
+ * @author lmre
+ */
+public class EnrolmentLogOJB extends PersistentObjectOJB implements IPersistentEnrolmentLog {
+    
+    public EnrolmentLogOJB() {
+        super();
+    }
+
+}
---
+++ src/ServidorPersistente/OJB/log/IPersistentEnrolmentLog.java
@@ -0,0 +1,13 @@
+/*
+ * Created on Nov 4, 2004
+ */
+package ServidorPersistente.OJB.log;
+
+import ServidorPersistente.IPersistentObject;
+
+/**
+ * @author nmgo
+ * @author lmre
+ */
+public interface IPersistentEnrolmentLog extends IPersistentObject{
+}
