fc8e09c8c6951c366b47fd14f770c7995dd95e7d
Luis Cruz <luis.cruz@ist.utl.pt>
2005-01-29 18:32:41 +0000

    Simplified code and removed recursive cloning.

--- src/ServidorAplicacao/Servico/sop/ReadShiftsByClass.java
+++ src/ServidorAplicacao/Servico/sop/ReadShiftsByClass.java
@@ -8,7 +8,7 @@ package ServidorAplicacao.Servico.sop;
 
 /**
  * @author Luis Cruz & Sara Ribeiro
- *  
+ * 
  */
 import java.util.List;
 
@@ -17,6 +17,7 @@ import org.apache.commons.collections.Transformer;
 
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 import DataBeans.InfoClass;
+import DataBeans.InfoExecutionCourse;
 import DataBeans.InfoLesson;
 import DataBeans.InfoShift;
 import DataBeans.util.Cloner;
@@ -24,48 +25,42 @@ import Dominio.IAula;
 import Dominio.ITurma;
 import Dominio.ITurno;
 import Dominio.Turma;
-import ServidorAplicacao.Servico.exceptions.FenixServiceException;
 import ServidorPersistente.ExcepcaoPersistencia;
 import ServidorPersistente.ISuportePersistente;
 import ServidorPersistente.OJB.SuportePersistenteOJB;
 
 public class ReadShiftsByClass implements IService {
 
-    public Object run(InfoClass infoClass) throws FenixServiceException {
+    public Object run(InfoClass infoClass) throws ExcepcaoPersistencia {
 
-        List infoShifts = null;
+        ISuportePersistente sp = SuportePersistenteOJB.getInstance();
 
-        try {
-            ISuportePersistente sp = SuportePersistenteOJB.getInstance();
+        ITurma shcoolClass = (ITurma) sp.getITurmaPersistente().readByOID(Turma.class,
+                infoClass.getIdInternal());
 
-            ITurma shcoolClass = (ITurma) sp.getITurmaPersistente().readByOID(Turma.class,
-                    infoClass.getIdInternal());
+        List shifts = sp.getITurmaTurnoPersistente().readByClass(shcoolClass);
 
-            List shifts = sp.getITurmaTurnoPersistente().readByClass(shcoolClass);
+        return CollectionUtils.collect(shifts, new Transformer() {
+            public Object transform(Object arg0) {
+                ITurno shift = (ITurno) arg0;
+                InfoShift infoShift = InfoShift.newInfoFromDomain(shift);
+                infoShift.setInfoLessons((List) CollectionUtils.collect(shift.getAssociatedLessons(),
+                        new Transformer() {
+                            public Object transform(Object arg0) {
+                                InfoLesson infoLesson = Cloner.copyILesson2InfoLesson((IAula) arg0);
+                                ITurno shift = ((IAula) arg0).getShift();
+                                InfoShift infoShift = InfoShift.newInfoFromDomain(shift);
+                                infoLesson.setInfoShift(infoShift);
 
-            infoShifts = (List) CollectionUtils.collect(shifts, new Transformer() {
-                public Object transform(Object arg0) {
-                    ITurno shift = (ITurno) arg0;
-                    InfoShift infoShift = Cloner.copyShift2InfoShift(shift);
-                    infoShift.setInfoLessons((List) CollectionUtils.collect(
-                            shift.getAssociatedLessons(), new Transformer() {
-                                public Object transform(Object arg0) {
-                                    InfoLesson infoLesson = Cloner.copyILesson2InfoLesson((IAula) arg0);
-                                    ITurno shift = ((IAula) arg0).getShift();
-                                    InfoShift infoShift = Cloner.copyShift2InfoShift(shift);
-                                    infoLesson.setInfoShift(infoShift);
-
-                                    return infoLesson;
-                                }
-                            }));
-                    return infoShift;
-                }
-            });
-        } catch (ExcepcaoPersistencia ex) {
-            throw new FenixServiceException(ex);
-        }
-
-        return infoShifts;
+                                return infoLesson;
+                            }
+                        }));
+                InfoExecutionCourse infoExecutionCourse = InfoExecutionCourse.newInfoFromDomain(shift
+                        .getDisciplinaExecucao());
+                infoShift.setInfoDisciplinaExecucao(infoExecutionCourse);
+                return infoShift;
+            }
+        });
     }
 
 }
