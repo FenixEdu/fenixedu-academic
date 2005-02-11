5605798e2fdb5a104e23c3f60786106f244179a9
Gonçalo Luíz <goncalo@ist.utl.pt>
2005-02-11 20:08:01 +0000

    script for seminaries 2004/2005, 2?\186 semester

---
+++ etc/database_operations/seminaries2Sem20042005.sql
@@ -0,0 +1,15 @@
+-- Seminary
+insert into SEMINARY (id_internal, name,description,allowed_candidacies_per_student,ACK_OPT_LOCK,enrollment_begin_time,enrollment_begin_date,enrollment_end_time,enrollment_end_date,has_theme,has_case_study) values (4,"Inovação 2004/2005","Ano Lectivo de 2004 / 2005",1,1,'00:00:00','2005-2-11','23:59:59','2005-2-28',0,0);
+
+-- Equivalencies
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (89,4,2819,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (90,4,10994,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (91,4,3806,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (92,4,3841,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (93,4,3842,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (94,4,3338,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (95,4,4920,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (96,4,3633,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (97,4,11682,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (98,4,11677,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (99,4,4924,1,1);
