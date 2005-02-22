6277a5afad0feae0a2ca1ecbefb5ee27e5229401
Shezad Anavarali <shezad@ist.utl.pt>
2005-02-22 16:10:56 +0000

    when master degree, it returns also the previous execution year's execution periods

--- src/ServidorAplicacao/Filtro/ExecutionPeriodsEnrollmentFenix.java
+++ src/ServidorAplicacao/Filtro/ExecutionPeriodsEnrollmentFenix.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import Util.TipoCurso;
+
 import pt.utl.ist.berserk.ServiceRequest;
 import pt.utl.ist.berserk.ServiceResponse;
+import pt.utl.ist.berserk.logic.serviceManager.ServiceParameters;
 import DataBeans.InfoExecutionPeriod;
 
 /**
@@ -17,17 +20,38 @@ import DataBeans.InfoExecutionPeriod;
 public class ExecutionPeriodsEnrollmentFenix extends Filtro {
 
     private int EXECUTION_PERIOD_ENROLMENT_FENIX = 81;
-    
-    /* (non-Javadoc)
-     * @see ServidorAplicacao.Filtro.AccessControlFilter#execute(pt.utl.ist.berserk.ServiceRequest, pt.utl.ist.berserk.ServiceResponse)
+
+    private int EXECUTION_PERIOD_ENROLMENT_PREVIOUS = 80;
+
+    private int EXECUTION_PERIOD_ENROLMENT_PREVIOUS_PREVIOUS = 2;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ServidorAplicacao.Filtro.AccessControlFilter#execute(pt.utl.ist.berserk.ServiceRequest,
+     *      pt.utl.ist.berserk.ServiceResponse)
      */
     public void execute(ServiceRequest request, ServiceResponse response) throws Exception {
         List serviceResult = (List) response.getReturnObject();
+        ServiceParameters parameters = request.getServiceParameters();
+
+        TipoCurso degreeType = (TipoCurso) parameters.getParameter(0);
+        // should be replaced with: 'parameters.getParameter("degreeType")',
+        // when the services starts to genereate stubs
+
         List newRes = new ArrayList();
         for (Iterator iter = serviceResult.iterator(); iter.hasNext();) {
             InfoExecutionPeriod executionPeriod = (InfoExecutionPeriod) iter.next();
-            if(executionPeriod.getIdInternal().intValue() >= EXECUTION_PERIOD_ENROLMENT_FENIX) {
+
+            if (executionPeriod.getIdInternal().intValue() >= EXECUTION_PERIOD_ENROLMENT_FENIX) {
                 newRes.add(executionPeriod);
+            }
+
+            // master degree extra execution periods
+            if ((executionPeriod.getIdInternal().intValue() == EXECUTION_PERIOD_ENROLMENT_PREVIOUS
+                    || executionPeriod.getIdInternal().intValue() == EXECUTION_PERIOD_ENROLMENT_PREVIOUS_PREVIOUS)
+                    && degreeType != null && degreeType.equals(TipoCurso.MESTRADO_OBJ)) {
+                newRes.add((executionPeriod));
             }
         }
         response.setReturnObject(newRes);
