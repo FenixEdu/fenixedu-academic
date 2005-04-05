71538c29a37cc4b2193f52da472d5b0bce9aad47
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-05 12:55:20 +0000

    Added dml mapping for Advisory domain class.

--- src/net/sourceforge/fenixedu/domain/Advisory.java
+++ src/net/sourceforge/fenixedu/domain/Advisory.java
@@ -1,147 +1,49 @@
 package net.sourceforge.fenixedu.domain;
 
-import java.util.Date;
 
 /**
  * @author Nuno Nunes & Luis Cruz
  * 
  * /2003/08/26
  */
-public class Advisory extends Advisory_Base implements IAdvisory {
+public class Advisory extends Advisory_Base {
 
-    private String sender;
-
-    private String subject;
-
-    private String message;
-
-    private Date created;
-
-    private Date expires;
-
-    private Boolean onlyShowOnce;
-
-    public boolean equals(Object obj) {
-        boolean result = false;
+    public boolean equals(final Object obj) {
         if (obj instanceof IAdvisory) {
-            IAdvisory advisory = (IAdvisory) obj;
-            result = this.getIdInternal().equals(advisory.getIdInternal());
+            final IAdvisory advisory = (IAdvisory) obj;
+            return this.getIdInternal().equals(advisory.getIdInternal());
         }
-        return result;
+        return false;
     }
 
     public String toString() {
 		final StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append("[");
-		stringBuilder.append(this.getClass().getName());
+		stringBuilder.append(getClass().getName());
 		stringBuilder.append(": ");
 		stringBuilder.append("idInternal = ");
 		stringBuilder.append(getIdInternal());
 		stringBuilder.append("; ");
 		stringBuilder.append("subject = ");
-		stringBuilder.append(this.subject);
+		stringBuilder.append(getSubject());
 		stringBuilder.append("; ");
 		stringBuilder.append("sender = ");
-		stringBuilder.append(this.sender);
+		stringBuilder.append(getSender());
 		stringBuilder.append("\n");
 		stringBuilder.append("message = ");
-		stringBuilder.append(this.message);
+		stringBuilder.append(getMessage());
 		stringBuilder.append("\n");
 		stringBuilder.append("created = ");
-		stringBuilder.append(this.created);
+		stringBuilder.append(getCreated());
 		stringBuilder.append("\n");
 		stringBuilder.append("expires = ");
-		stringBuilder.append(this.expires);
+		stringBuilder.append(getExpires());
 		stringBuilder.append("\n");
 		stringBuilder.append("onlyShowOnce = ");
-		stringBuilder.append(this.onlyShowOnce);
+		stringBuilder.append(getOnlyShowOnce());
 		stringBuilder.append("]\n");
 		
         return stringBuilder.toString();
-    }
-
-    /**
-     * @return
-     */
-    public Date getCreated() {
-        return created;
-    }
-
-    /**
-     * @return
-     */
-    public Date getExpires() {
-        return expires;
-    }
-
-    /**
-     * @return
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * @return
-     */
-    public Boolean getOnlyShowOnce() {
-        return onlyShowOnce;
-    }
-
-    /**
-     * @return
-     */
-    public String getSubject() {
-        return subject;
-    }
-
-    /**
-     * @param date
-     */
-    public void setCreated(Date date) {
-        created = date;
-    }
-
-    /**
-     * @param date
-     */
-    public void setExpires(Date date) {
-        expires = date;
-    }
-
-    /**
-     * @param string
-     */
-    public void setMessage(String string) {
-        message = string;
-    }
-
-    /**
-     * @param boolean1
-     */
-    public void setOnlyShowOnce(Boolean boolean1) {
-        onlyShowOnce = boolean1;
-    }
-
-    /**
-     * @param string
-     */
-    public void setSubject(String string) {
-        subject = string;
-    }
-
-    /**
-     * @return
-     */
-    public String getSender() {
-        return sender;
-    }
-
-    /**
-     * @param string
-     */
-    public void setSender(String string) {
-        sender = string;
     }
 
 }
