13d22474af1325174e6365a2c7269477285d6ccf
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-05 14:56:24 +0000

    Added dml mapping for GlossayEntry domain class.

--- src/net/sourceforge/fenixedu/domain/support/GlossaryEntry.java
+++ src/net/sourceforge/fenixedu/domain/support/GlossaryEntry.java
@@ -4,35 +4,10 @@
  */
 package net.sourceforge.fenixedu.domain.support;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
 
 /**
  * @author Luis Cruz
  *  
  */
-public class GlossaryEntry extends DomainObject implements IGlossaryEntry {
-
-    private String term = null;
-
-    private String definition = null;
-
-    public GlossaryEntry() {
-        super();
-    }
-
-    public String getDefinition() {
-        return definition;
-    }
-
-    public void setDefinition(String definition) {
-        this.definition = definition;
-    }
-
-    public String getTerm() {
-        return term;
-    }
-
-    public void setTerm(String term) {
-        this.term = term;
-    }
+public class GlossaryEntry extends GlossaryEntry_Base {
 }
