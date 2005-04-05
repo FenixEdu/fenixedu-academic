32c90a4a5b0e7e1502b9451c7327b896cce0dd89
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-05 14:46:15 +0000

    Added dml mapping for FAQEntry domain class.

--- src/net/sourceforge/fenixedu/domain/support/FAQEntry.java
+++ src/net/sourceforge/fenixedu/domain/support/FAQEntry.java
@@ -4,25 +4,14 @@
  */
 package net.sourceforge.fenixedu.domain.support;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
 
 /**
  * @author Luis Cruz
  *  
  */
-public class FAQEntry extends DomainObject implements IFAQEntry {
-
-    private Integer keyParentSection = null;
+public class FAQEntry extends FAQEntry_Base {
 
     private IFAQSection parentSection = null;
-
-    private String question = null;
-
-    private String answer = null;
-
-    public FAQEntry() {
-        super();
-    }
 
     public IFAQSection getParentSection() {
         return parentSection;
@@ -32,27 +21,4 @@ public class FAQEntry extends DomainObject implements IFAQEntry {
         this.parentSection = parentSection;
     }
 
-    public Integer getKeyParentSection() {
-        return keyParentSection;
-    }
-
-    public void setKeyParentSection(Integer keyParentSection) {
-        this.keyParentSection = keyParentSection;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
 }
