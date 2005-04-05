b0c7662b5423d16186c1c78e9de8be232d939fcf
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-05 16:48:00 +0000

    Added dml mapping for Role domain class.

--- src/net/sourceforge/fenixedu/domain/Role.java
+++ src/net/sourceforge/fenixedu/domain/Role.java
@@ -9,23 +9,10 @@ import net.sourceforge.fenixedu.domain.person.RoleType;
 /**
  * @author jpvl
  */
-public class Role extends DomainObject implements IRole {
-    private String portalSubApplication;
-
-    private String page;
-
-    private String pageNameProperty;
+public class Role extends Role_Base {
 
     private RoleType roleType;
 
-    public Role() {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
     public boolean equals(Object obj) {
         return ((obj instanceof IRole) && (((IRole) obj).getRoleType().equals(getRoleType())));
     }
@@ -35,78 +22,12 @@ public class Role extends DomainObject implements IRole {
     }
 
     /**
-     * @return String
-     */
-    public String getPage() {
-        return page;
-    }
-
-    /**
-     * @return String
-     */
-    public String getPageNameProperty() {
-        return pageNameProperty;
-    }
-
-    /**
-     * @return String
-     */
-    public String getPortalSubApplication() {
-        return portalSubApplication;
-    }
-
-    /**
-     * @return Collection
-     */
-    //	public Collection getRolePersons() {
-    //		return rolePersons;
-    //	}
-    /**
      * @return RoleType
      */
     public RoleType getRoleType() {
         return roleType;
     }
 
-    /**
-     * Sets the page.
-     * 
-     * @param page
-     *            The page to set
-     */
-    public void setPage(String page) {
-        this.page = page;
-    }
-
-    /**
-     * Sets the pageNameProperty.
-     * 
-     * @param pageNameProperty
-     *            The pageNameProperty to set
-     */
-    public void setPageNameProperty(String pageNameProperty) {
-        this.pageNameProperty = pageNameProperty;
-    }
-
-    /**
-     * Sets the portalSubApplication.
-     * 
-     * @param portalSubApplication
-     *            The portalSubApplication to set
-     */
-    public void setPortalSubApplication(String portalSubApplication) {
-        this.portalSubApplication = portalSubApplication;
-    }
-
-    /**
-     * Sets the rolePersons.
-     * 
-     * @param rolePersons
-     *            The rolePersons to set
-     */
-    //	public void setRolePersons(Collection rolePersons) {
-    //		this.rolePersons = rolePersons;
-    //	}
     /**
      * Sets the roleType.
      * 
