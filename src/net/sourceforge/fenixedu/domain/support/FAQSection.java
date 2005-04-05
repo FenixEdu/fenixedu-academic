dd8fd4ee1e82b156e4b9fa0661ae384ee8fcb0c6
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-05 14:53:42 +0000

    Added dml mapping for FAQSection domain class.

--- src/net/sourceforge/fenixedu/domain/support/FAQSection.java
+++ src/net/sourceforge/fenixedu/domain/support/FAQSection.java
@@ -4,23 +4,14 @@
  */
 package net.sourceforge.fenixedu.domain.support;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
 
 /**
  * @author Luis Cruz
  *  
  */
-public class FAQSection extends DomainObject implements IFAQSection {
-
-    private Integer keyParentSection = null;
+public class FAQSection extends FAQSection_Base {
 
     private IFAQSection parentSection = null;
-
-    private String sectionName = null;
-
-    public FAQSection() {
-        super();
-    }
 
     public IFAQSection getParentSection() {
         return parentSection;
@@ -30,19 +21,4 @@ public class FAQSection extends DomainObject implements IFAQSection {
         this.parentSection = parentSection;
     }
 
-    public Integer getKeyParentSection() {
-        return keyParentSection;
-    }
-
-    public void setKeyParentSection(Integer keyParentSection) {
-        this.keyParentSection = keyParentSection;
-    }
-
-    public String getSectionName() {
-        return sectionName;
-    }
-
-    public void setSectionName(String sectionName) {
-        this.sectionName = sectionName;
-    }
 }
