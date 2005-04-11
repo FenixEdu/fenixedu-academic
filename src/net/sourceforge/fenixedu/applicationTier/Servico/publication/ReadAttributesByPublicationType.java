f3b46f8026c9aa52b296996693d5765dc3226ea6
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2005-04-11 14:31:52 +0000

    added a service to read the attributes of a publication type

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/publication/ReadAttributesByPublicationType.java
@@ -0,0 +1,55 @@
+/*
+ * Created on Apr 8, 2005
+ *
+ */
+package net.sourceforge.fenixedu.applicationTier.Servico.publication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.publication.IAttribute;
+import net.sourceforge.fenixedu.domain.publication.PublicationType;
+import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
+import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
+import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import net.sourceforge.fenixedu.persistenceTier.publication.IPersistentPublicationType;
+import pt.utl.ist.berserk.logic.serviceManager.IService;
+
+/**
+ * @author Ricardo Rodrigues
+ *
+ */
+
+public class ReadAttributesByPublicationType implements IService {
+
+    /**
+     * @param args
+     * @throws ExcepcaoPersistencia 
+     */
+    public HashMap run(Integer publicationTypeID) throws ExcepcaoPersistencia {
+        ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
+
+        IPersistentPublicationType persistentPublicationType = persistentSuport
+                .getIPersistentPublicationType();
+        PublicationType publicationType = (PublicationType) persistentPublicationType.readByOID(
+                PublicationType.class, new Integer(publicationTypeID));
+
+        List requiredAttributes = publicationType.getRequiredAttributes();
+        List nonRequiredAttributes = publicationType.getNonRequiredAttributes();
+        List allAttributes = new ArrayList(requiredAttributes);
+        allAttributes.addAll(nonRequiredAttributes);
+        
+        HashMap result = new HashMap();
+        for (Iterator iter = allAttributes.iterator(); iter.hasNext();) {
+            IAttribute attribute = (IAttribute) iter.next();
+            String attributeName = attribute.getAttributeType();
+            result.put(attributeName, attributeName);
+        }
+
+        return result;
+
+    }
+
+}
