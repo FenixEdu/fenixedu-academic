f2ddd1ba3d884d5387ed6b6e7d168cbee4dfbe51
Pedro Miguel Amaral Pinhanços dos Santos <pedro.m.santos@ist.utl.pt>
2005-04-12 15:33:08 +0000

    Added dml mapping for Evaluation domain class.

--- src/net/sourceforge/fenixedu/domain/Evaluation.java
+++ src/net/sourceforge/fenixedu/domain/Evaluation.java
@@ -5,49 +5,18 @@ import java.util.List;
 /**
  * @author Tânia Pousão 24 de Junho de 2003
  */
-public class Evaluation extends DomainObject implements IEvaluation {
-    private String ojbConcreteClass;
-
-    private String publishmentMessage;
-
+public class Evaluation extends Evaluation_Base {
     private List associatedExecutionCourses;
 
     public Evaluation() {
         super();
-        this.ojbConcreteClass = this.getClass().getName();
+        setOjbConcreteClass(this.getClass().getName());
     }
 
     public Evaluation(Integer idInternal) {
         setIdInternal(idInternal);
     }
 
-    /**
-     * @return String
-     */
-    public String getOjbConcreteClass() {
-        return ojbConcreteClass;
-    }
-
-    /**
-     * @param string
-     */
-    public void setOjbConcreteClass(String string) {
-        ojbConcreteClass = string;
-    }
-
-    /**
-     * @return String
-     */
-    public String getPublishmentMessage() {
-        return publishmentMessage;
-    }
-
-    /**
-     * @param string
-     */
-    public void setPublishmentMessage(String string) {
-        publishmentMessage = string;
-    }
 
     /**
      * @return
