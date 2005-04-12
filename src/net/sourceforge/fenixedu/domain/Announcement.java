c81484da9f9277954a9cf721a57dfd47a2c75762
João Florindo <jdnf@linkare.pt>
2005-04-12 16:59:43 +0000

    Announcement domain model

--- src/net/sourceforge/fenixedu/domain/Announcement.java
+++ src/net/sourceforge/fenixedu/domain/Announcement.java
@@ -8,20 +8,11 @@ import java.sql.Timestamp;
 /**
  * @author Ivo Brandão
  */
-public class Announcement extends DomainObject implements IAnnouncement {
-
-    private String title;
-
+public class Announcement extends Announcement_Base {
     private Timestamp creationDate;
-
     private Timestamp lastModifiedDate;
 
-    private String information;
-
-    private ISite site;
-
-    private Integer keySite;
-
+    
     /**
      * Construtor
      */
@@ -41,11 +32,11 @@ public class Announcement extends DomainObject implements IAnnouncement {
     public Announcement(String title, Timestamp date, Timestamp lastModifiedDate, String information,
             ISite site) {
 
-        this.title = title;
-        this.creationDate = date;
-        this.lastModifiedDate = lastModifiedDate;
-        this.information = information;
-        this.site = site;
+        setTitle(title);
+        setCreationDate(date);
+        setLastModifiedDate(lastModifiedDate);
+        setInformation(information);
+        setSite(site);
     }
 
     /**
@@ -53,9 +44,10 @@ public class Announcement extends DomainObject implements IAnnouncement {
      */
     public Announcement(String title, Timestamp date, ISite site) {
 
-        this.title = title;
-        this.creationDate = date;
-        this.site = site;
+        setTitle(title);
+        setCreationDate(date);
+        setSite(site);
+        
     }
 
     /**
@@ -96,106 +88,19 @@ public class Announcement extends DomainObject implements IAnnouncement {
         return result;
     }
 
-    /**
-     * @return Timestamp
-     */
     public Timestamp getCreationDate() {
         return creationDate;
     }
-
-    /**
-     * @return String
-     */
-    public String getInformation() {
-        return information;
-    }
-
-    /**
-     * @return Integer
-     */
-    public Integer getKeySite() {
-        return keySite;
-    }
-
-    /**
-     * @return Timestamp
-     */
-    public Timestamp getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    /**
-     * @return ISite
-     */
-    public ISite getSite() {
-        return site;
-    }
-
-    /**
-     * @return String
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * Sets the creationDate.
-     * 
-     * @param creationDate
-     *            The creationDate to set
-     */
+    
     public void setCreationDate(Timestamp creationDate) {
         this.creationDate = creationDate;
     }
 
-    /**
-     * Sets the information.
-     * 
-     * @param information
-     *            The information to set
-     */
-    public void setInformation(String information) {
-        this.information = information;
+    public Timestamp getLastModifiedDate() {
+        return lastModifiedDate;
     }
 
-    /**
-     * Sets the keySite.
-     * 
-     * @param keySite
-     *            The keySite to set
-     */
-    public void setKeySite(Integer keySite) {
-        this.keySite = keySite;
-    }
-
-    /**
-     * Sets the lastModifiedDate.
-     * 
-     * @param lastModifiedDate
-     *            The lastModifiedDate to set
-     */
     public void setLastModifiedDate(Timestamp lastModifiedDate) {
         this.lastModifiedDate = lastModifiedDate;
     }
-
-    /**
-     * Sets the site.
-     * 
-     * @param site
-     *            The site to set
-     */
-    public void setSite(ISite site) {
-        this.site = site;
-    }
-
-    /**
-     * Sets the title.
-     * 
-     * @param title
-     *            The title to set
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
 }
