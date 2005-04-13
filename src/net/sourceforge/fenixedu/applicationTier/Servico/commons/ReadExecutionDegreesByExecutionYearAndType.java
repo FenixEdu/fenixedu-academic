d17d49c73027bf9a3f476a95acc2cf838c39b3be
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-13 10:40:09 +0000

    bug fix (missing data in Info)

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionDegreesByExecutionYearAndType.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionDegreesByExecutionYearAndType.java
@@ -7,6 +7,8 @@ package net.sourceforge.fenixedu.applicationTier.Servico.commons;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.fenixedu.dataTransferObject.InfoDegree;
+import net.sourceforge.fenixedu.dataTransferObject.InfoDegreeCurricularPlan;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegree;
 import net.sourceforge.fenixedu.domain.IDegree;
 import net.sourceforge.fenixedu.domain.IExecutionDegree;
@@ -30,7 +32,7 @@ public class ReadExecutionDegreesByExecutionYearAndType implements IService {
 
         final List executionDegrees = executionDegreeDAO.readByExecutionYearOIDAndDegreeType(executionYearOID,
                 typeOfCourse);
-        return getInfoExecutiionDegrees(executionDegrees);
+        return getInfoExecutionDegrees(executionDegrees);
     }
 
     public List run(IDegree degree, IExecutionYear executionYear, String tmp)
@@ -39,15 +41,22 @@ public class ReadExecutionDegreesByExecutionYearAndType implements IService {
         final IPersistentExecutionDegree executionDegreeDAO = sp.getIPersistentExecutionDegree();
 
         final List executionDegrees = executionDegreeDAO.readByDegreeAndExecutionYear(degree, executionYear);
-        return getInfoExecutiionDegrees(executionDegrees);
+        return getInfoExecutionDegrees(executionDegrees);
     }
 
-    private List getInfoExecutiionDegrees(final List executionDegrees) {
+    private List getInfoExecutionDegrees(final List executionDegrees) {
         final List infoExecutionDegrees = new ArrayList(executionDegrees.size());
         for (int i = 0; i < executionDegrees.size(); i++) {
             final IExecutionDegree executionDegree = (IExecutionDegree) executionDegrees.get(i);
             final InfoExecutionDegree infoExecutionDegree = InfoExecutionDegree
                     .newInfoFromDomain(executionDegree);
+
+            infoExecutionDegree.setInfoDegreeCurricularPlan(InfoDegreeCurricularPlan
+                    .newInfoFromDomain(executionDegree.getDegreeCurricularPlan()));
+
+            infoExecutionDegree.getInfoDegreeCurricularPlan().setInfoDegree(
+                    InfoDegree.newInfoFromDomain(executionDegree.getDegreeCurricularPlan().getDegree()));
+
             infoExecutionDegrees.add(infoExecutionDegree);
         }
         return infoExecutionDegrees;
