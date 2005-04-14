d51a16e8077c9f837e48e47d564b265ccac2c429
Shezad Anavarali <shezad@ist.utl.pt>
2005-04-14 16:33:46 +0000

    Guide DTO with all...

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoGuideWithPersonAndExecutionDegreeAndContributor.java
@@ -0,0 +1,69 @@
+/*
+ * Created on 8/Jul/2004
+ *
+ */
+package net.sourceforge.fenixedu.dataTransferObject;
+
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+
+import net.sourceforge.fenixedu.domain.IGuide;
+import net.sourceforge.fenixedu.domain.IGuideEntry;
+import net.sourceforge.fenixedu.domain.IGuideSituation;
+
+/**
+ * @author Tânia Pousão
+ * 
+ */
+public class InfoGuideWithPersonAndExecutionDegreeAndContributor extends InfoGuide {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see net.sourceforge.fenixedu.dataTransferObject.InfoGuide#copyFromDomain(Dominio.IGuide)
+     */
+    public void copyFromDomain(IGuide guide) {
+        super.copyFromDomain(guide);
+        if (guide != null) {
+            setInfoPerson(InfoPerson.newInfoFromDomain(guide.getPerson()));
+            setInfoExecutionDegree(InfoExecutionDegreeWithInfoDegreeCurricularPlan
+                    .newInfoFromDomain(guide.getExecutionDegree()));
+            setInfoContributor(InfoContributor.newInfoFromDomain(guide.getContributor()));
+            setInfoGuideSituation(InfoGuideSituation.newInfoFromDomain(guide.getActiveSituation()));
+
+            List infoGuideEntryList = (List) CollectionUtils.collect(guide.getGuideEntries(),
+                    new Transformer() {
+
+                        public Object transform(Object arg0) {
+                            IGuideEntry guideEntry = (IGuideEntry) arg0;
+                            return InfoGuideEntry.newInfoFromDomain(guideEntry);
+                        }
+                    });
+
+            setInfoGuideEntries(infoGuideEntryList);
+
+            List infoGuideSituationList = (List) CollectionUtils.collect(guide.getGuideSituations(),
+                    new Transformer() {
+
+                        public Object transform(Object arg0) {
+                            IGuideSituation guideSituation = (IGuideSituation) arg0;
+                            return InfoGuideSituation.newInfoFromDomain(guideSituation);
+                        }
+                    });
+
+            setInfoGuideSituations(infoGuideSituationList);
+
+        }
+    }
+
+    public static InfoGuide newInfoFromDomain(IGuide guide) {
+        InfoGuideWithPersonAndExecutionDegreeAndContributor infoGuide = new InfoGuideWithPersonAndExecutionDegreeAndContributor();
+        if (guide != null) {
+            infoGuide.copyFromDomain(guide);
+        }
+
+        return infoGuide;
+    }
+}
