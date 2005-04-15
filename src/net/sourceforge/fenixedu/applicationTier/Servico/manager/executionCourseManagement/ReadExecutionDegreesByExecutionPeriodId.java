9a9d542b80c90f557c72ca15d71ef11d9be498a8
Luis Cruz <luis.cruz@ist.utl.pt>
2005-04-15 00:29:44 +0000

    Bug fix. Removed cloner. Simplified code.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/executionCourseManagement/ReadExecutionDegreesByExecutionPeriodId.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/executionCourseManagement/ReadExecutionDegreesByExecutionPeriodId.java
@@ -5,8 +5,12 @@ import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoDegree;
+import net.sourceforge.fenixedu.dataTransferObject.InfoDegreeCurricularPlan;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegree;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
+import net.sourceforge.fenixedu.domain.IDegree;
+import net.sourceforge.fenixedu.domain.IDegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.IExecutionDegree;
 import net.sourceforge.fenixedu.domain.IExecutionPeriod;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -23,36 +27,42 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class ReadExecutionDegreesByExecutionPeriodId implements IService {
 
-    public List run(Integer executionPeriodId) throws FenixServiceException {
+    public List run(Integer executionPeriodId) throws FenixServiceException, ExcepcaoPersistencia {
 
         List infoExecutionDegreeList = null;
 
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentExecutionDegree executionDegreeDAO = sp.getIPersistentExecutionDegree();
-            IPersistentExecutionPeriod persistentExecutionPeriod = sp.getIPersistentExecutionPeriod();
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        IPersistentExecutionDegree executionDegreeDAO = sp.getIPersistentExecutionDegree();
+        IPersistentExecutionPeriod persistentExecutionPeriod = sp.getIPersistentExecutionPeriod();
 
-            if (executionPeriodId == null) {
-                throw new FenixServiceException("nullId");
-            }
-
-            IExecutionPeriod executionPeriod = (IExecutionPeriod) persistentExecutionPeriod.readByOID(
-                    ExecutionPeriod.class, executionPeriodId);
-
-            List executionDegrees = executionDegreeDAO.readByExecutionYear(executionPeriod
-                    .getExecutionYear().getYear());
-
-            Iterator iterator = executionDegrees.iterator();
-            infoExecutionDegreeList = new ArrayList();
-
-            while (iterator.hasNext()) {
-                IExecutionDegree executionDegree = (IExecutionDegree) iterator.next();
-                infoExecutionDegreeList.add(InfoExecutionDegree.newInfoFromDomain(executionDegree));
-            }
-
-        } catch (ExcepcaoPersistencia ex) {
-            throw new FenixServiceException(ex);
+        if (executionPeriodId == null) {
+            throw new FenixServiceException("nullId");
         }
+
+        IExecutionPeriod executionPeriod = (IExecutionPeriod) persistentExecutionPeriod.readByOID(
+                ExecutionPeriod.class, executionPeriodId);
+
+        List executionDegrees = executionDegreeDAO.readByExecutionYear(executionPeriod
+                .getExecutionYear().getYear());
+
+        Iterator iterator = executionDegrees.iterator();
+        infoExecutionDegreeList = new ArrayList();
+
+        while (iterator.hasNext()) {
+            final IExecutionDegree executionDegree = (IExecutionDegree) iterator.next();
+            final InfoExecutionDegree infoExecutionDegree = InfoExecutionDegree.newInfoFromDomain(executionDegree);
+
+            final IDegreeCurricularPlan degreeCurricularPlan = executionDegree.getDegreeCurricularPlan();
+            final InfoDegreeCurricularPlan infoDegreeCurricularPlan = InfoDegreeCurricularPlan.newInfoFromDomain(degreeCurricularPlan);
+            infoExecutionDegree.setInfoDegreeCurricularPlan(infoDegreeCurricularPlan);
+
+            final IDegree degree = degreeCurricularPlan.getDegree();
+            final InfoDegree infoDegree = InfoDegree.newInfoFromDomain(degree);
+            infoDegreeCurricularPlan.setInfoDegree(infoDegree);
+
+            infoExecutionDegreeList.add(infoExecutionDegree);
+        }
+
         return infoExecutionDegreeList;
     }
 }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/TeacherAdministrationSiteComponentService.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/TeacherAdministrationSiteComponentService.java
@@ -17,41 +17,27 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 /**
  * @author Fernanda Quitério
  * 
- *  
+ * 
  */
 public class TeacherAdministrationSiteComponentService implements IService {
 
-    public TeacherAdministrationSiteComponentService() {
-
-    }
-
     public Object run(Integer infoExecutionCourseCode, ISiteComponent commonComponent,
             ISiteComponent bodyComponent, Integer infoSiteCode, Object obj1, Object obj2)
-            throws FenixServiceException {
-        TeacherAdministrationSiteView siteView = null;
+            throws FenixServiceException, ExcepcaoPersistencia {
 
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentExecutionCourse persistentExecutionCourse = sp.getIPersistentExecutionCourse();
-            IPersistentSite persistentSite = sp.getIPersistentSite();
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final IPersistentExecutionCourse persistentExecutionCourse = sp.getIPersistentExecutionCourse();
+        final IPersistentSite persistentSite = sp.getIPersistentSite();
 
-            ISite site = null;
-            IExecutionCourse executionCourse = (IExecutionCourse) persistentExecutionCourse.readByOID(
-                    ExecutionCourse.class, infoExecutionCourseCode);
-            site = persistentSite.readByExecutionCourse(executionCourse);
+        final IExecutionCourse executionCourse = (IExecutionCourse) persistentExecutionCourse.readByOID(
+                ExecutionCourse.class, infoExecutionCourseCode);
+        final ISite site = persistentSite.readByExecutionCourse(executionCourse);
 
-            TeacherAdministrationSiteComponentBuilder componentBuilder = TeacherAdministrationSiteComponentBuilder
-                    .getInstance();
-            commonComponent = componentBuilder.getComponent(commonComponent, site, null, null, null);
-            bodyComponent = componentBuilder.getComponent(bodyComponent, site, commonComponent, obj1,
-                    obj2);
+        final TeacherAdministrationSiteComponentBuilder componentBuilder = TeacherAdministrationSiteComponentBuilder
+                .getInstance();
+        commonComponent = componentBuilder.getComponent(commonComponent, site, null, null, null);
+        bodyComponent = componentBuilder.getComponent(bodyComponent, site, commonComponent, obj1, obj2);
 
-            siteView = new TeacherAdministrationSiteView(commonComponent, bodyComponent);
-
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
-        }
-
-        return siteView;
+        return new TeacherAdministrationSiteView(commonComponent, bodyComponent);
     }
 }
