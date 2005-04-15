02b6232d31ca7f24bd6cc79138debe7cf41b34c7
André Fernandes <ajfe@ist.utl.pt>
2005-04-15 12:12:07 +0000

    DegreeInfo definition added

--- src/net/sourceforge/fenixedu/domain/DegreeInfo.java
+++ src/net/sourceforge/fenixedu/domain/DegreeInfo.java
@@ -3,59 +3,12 @@ package net.sourceforge.fenixedu.domain;
 import java.sql.Timestamp;
 
 /**
- * @author Tânia Pousão Created on 30/Out/2003
+ * @author Tania Pousao Created on 30/Out/2003
  */
-public class DegreeInfo extends DomainObject implements IDegreeInfo {
-    private Integer degreeKey;
-
+public class DegreeInfo extends DegreeInfo_Base {
     private IDegree degree;
 
-    private String description;
-
-    private String objectives;
-
-    private String history;
-
-    private String professionalExits;
-
-    private String additionalInfo;
-
-    private String links;
-
-    private String testIngression;
-
-    private Integer driftsInitial;
-
-    private Integer driftsFirst;
-
-    private Integer driftsSecond;
-
-    private String classifications;
-
-    private Double markMin;
-
-    private Double markMax;
-
-    private Double markAverage;
-
     private Timestamp lastModificationDate;
-
-    //Data in english
-    private String descriptionEn;
-
-    private String objectivesEn;
-
-    private String historyEn;
-
-    private String professionalExitsEn;
-
-    private String additionalInfoEn;
-
-    private String linksEn;
-
-    private String testIngressionEn;
-
-    private String classificationsEn;
 
     public DegreeInfo(Integer idInternal) {
         setIdInternal(idInternal);
@@ -68,60 +21,44 @@ public class DegreeInfo extends DomainObject implements IDegreeInfo {
             String additionalInfo, String links, String testIngression, Integer driftsInitial,
             Integer driftsFirst, Integer driftsSecond, String classifications, Double markMin,
             Double markMax, Double markAverage, Timestamp lastModificationDate) {
-        this.degreeKey = degree.getIdInternal();
-        this.degree = degree;
-        this.objectives = objectives;
-        this.history = history;
-        this.professionalExits = professionalExits;
-        this.additionalInfo = additionalInfo;
-        this.links = links;
-        this.testIngression = testIngression;
-        this.driftsInitial = driftsInitial;
-        this.driftsFirst = driftsFirst;
-        this.driftsSecond = driftsSecond;
-        this.classifications = classifications;
-        this.markMin = markMin;
-        this.markMax = markMax;
-        this.markAverage = markAverage;
-        this.lastModificationDate = lastModificationDate;
+        this.setDegreeKey(degree.getIdInternal());
+        this.setDegree(degree);
+        this.setObjectives(objectives);
+        this.setHistory(history);
+        this.setProfessionalExits(professionalExits);
+        this.setAdditionalInfo(additionalInfo);
+        this.setLinks(links);
+        this.setTestIngression(testIngression);
+        this.setDriftsInitial(driftsInitial);
+        this.setDriftsFirst(driftsFirst);
+        this.setDriftsSecond(driftsSecond);
+        this.setClassifications(classifications);
+        this.setMarkMin(markMin);
+        this.setMarkMax(markMax);
+        this.setMarkAverage(markAverage);
+        this.setLastModificationDate(lastModificationDate);
     }
 
     public DegreeInfo(Integer degreeKey, IDegree degree, String objectives, String history,
             String professionalExits, String additionalInfo, String links, String testIngression,
             Integer driftsInitial, Integer driftsFirst, Integer driftsSecond, String classifications,
             Double markMin, Double markMax, Double markAverage, Timestamp lastModificationDate) {
-        this.degreeKey = degreeKey;
-        this.degree = degree;
-        this.objectives = objectives;
-        this.history = history;
-        this.professionalExits = professionalExits;
-        this.additionalInfo = additionalInfo;
-        this.links = links;
-        this.testIngression = testIngression;
-        this.driftsInitial = driftsInitial;
-        this.driftsFirst = driftsFirst;
-        this.driftsSecond = driftsSecond;
-        this.classifications = classifications;
-        this.markMin = markMin;
-        this.markMax = markMax;
-        this.markAverage = markAverage;
-        this.lastModificationDate = lastModificationDate;
-    }
-
-    public String getAdditionalInfo() {
-        return additionalInfo;
-    }
-
-    public void setAdditionalInfo(String additionalInfo) {
-        this.additionalInfo = additionalInfo;
-    }
-
-    public String getClassifications() {
-        return classifications;
-    }
-
-    public void setClassifications(String classifications) {
-        this.classifications = classifications;
+        this.setDegreeKey(degreeKey);
+        this.setDegree(degree);
+        this.setObjectives(objectives);
+        this.setHistory(history);
+        this.setProfessionalExits(professionalExits);
+        this.setAdditionalInfo(additionalInfo);
+        this.setLinks(links);
+        this.setTestIngression(testIngression);
+        this.setDriftsInitial(driftsInitial);
+        this.setDriftsFirst(driftsFirst);
+        this.setDriftsSecond(driftsSecond);
+        this.setClassifications(classifications);
+        this.setMarkMin(markMin);
+        this.setMarkMax(markMax);
+        this.setMarkAverage(markAverage);
+        this.setLastModificationDate(lastModificationDate);
     }
 
     public IDegree getDegree() {
@@ -132,180 +69,12 @@ public class DegreeInfo extends DomainObject implements IDegreeInfo {
         this.degree = degree;
     }
 
-    public Integer getDegreeKey() {
-        return degreeKey;
-    }
-
-    public void setDegreeKey(Integer degreeKey) {
-        this.degreeKey = degreeKey;
-    }
-
-    public Integer getDriftsFirst() {
-        return driftsFirst;
-    }
-
-    public void setDriftsFirst(Integer driftsFirst) {
-        this.driftsFirst = driftsFirst;
-    }
-
-    public Integer getDriftsInitial() {
-        return driftsInitial;
-    }
-
-    public void setDriftsInitial(Integer driftsInitial) {
-        this.driftsInitial = driftsInitial;
-    }
-
-    public Integer getDriftsSecond() {
-        return driftsSecond;
-    }
-
-    public void setDriftsSecond(Integer driftsSecond) {
-        this.driftsSecond = driftsSecond;
-    }
-
-    public String getHistory() {
-        return history;
-    }
-
-    public void setHistory(String history) {
-        this.history = history;
-    }
-
     public Timestamp getLastModificationDate() {
         return lastModificationDate;
     }
 
     public void setLastModificationDate(Timestamp lastModificationDate) {
         this.lastModificationDate = lastModificationDate;
-    }
-
-    public String getLinks() {
-        return links;
-    }
-
-    public void setLinks(String links) {
-        this.links = links;
-    }
-
-    public Double getMarkAverage() {
-        return markAverage;
-    }
-
-    public void setMarkAverage(Double markAverage) {
-        this.markAverage = markAverage;
-    }
-
-    public Double getMarkMax() {
-        return markMax;
-    }
-
-    public void setMarkMax(Double markMax) {
-        this.markMax = markMax;
-    }
-
-    public Double getMarkMin() {
-        return markMin;
-    }
-
-    public void setMarkMin(Double markMin) {
-        this.markMin = markMin;
-    }
-
-    public String getObjectives() {
-        return objectives;
-    }
-
-    public void setObjectives(String objectives) {
-        this.objectives = objectives;
-    }
-
-    public String getProfessionalExits() {
-        return professionalExits;
-    }
-
-    public void setProfessionalExits(String professionalExits) {
-        this.professionalExits = professionalExits;
-    }
-
-    public String getTestIngression() {
-        return testIngression;
-    }
-
-    public void setTestIngression(String testIngression) {
-        this.testIngression = testIngression;
-    }
-
-    public String getAdditionalInfoEn() {
-        return additionalInfoEn;
-    }
-
-    public void setAdditionalInfoEn(String additionalInfoEn) {
-        this.additionalInfoEn = additionalInfoEn;
-    }
-
-    public String getClassificationsEn() {
-        return classificationsEn;
-    }
-
-    public void setClassificationsEn(String classificationsEn) {
-        this.classificationsEn = classificationsEn;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDescriptionEn() {
-        return descriptionEn;
-    }
-
-    public void setDescriptionEn(String descriptionEn) {
-        this.descriptionEn = descriptionEn;
-    }
-
-    public String getHistoryEn() {
-        return historyEn;
-    }
-
-    public void setHistoryEn(String historyEn) {
-        this.historyEn = historyEn;
-    }
-
-    public String getLinksEn() {
-        return linksEn;
-    }
-
-    public void setLinksEn(String linksEn) {
-        this.linksEn = linksEn;
-    }
-
-    public String getObjectivesEn() {
-        return objectivesEn;
-    }
-
-    public void setObjectivesEn(String objectivesEn) {
-        this.objectivesEn = objectivesEn;
-    }
-
-    public String getProfessionalExitsEn() {
-        return professionalExitsEn;
-    }
-
-    public void setProfessionalExitsEn(String professionalExitsEn) {
-        this.professionalExitsEn = professionalExitsEn;
-    }
-
-    public String getTestIngressionEn() {
-        return testIngressionEn;
-    }
-
-    public void setTestIngressionEn(String testIngressionEn) {
-        this.testIngressionEn = testIngressionEn;
     }
 
     public boolean equals(Object obj) {
@@ -322,29 +91,29 @@ public class DegreeInfo extends DomainObject implements IDegreeInfo {
         String result = "[DEGREE_INFO:";
         result += " codigo interno= " + getIdInternal();
         result += " degree= " + getDegreeKey();
-        result += " descrição= " + getDescription();
+        result += " descriï¿½ï¿½o= " + getDescription();
         result += " objectivos= " + getObjectives();
         result += " historial= " + getHistory();
         result += " saidas profissionais=" + getProfessionalExits();
-        result += " informação adicional= " + getAdditionalInfo();
+        result += " informaï¿½ï¿½o adicional= " + getAdditionalInfo();
         result += " links= " + getLinks();
         result += " provas de ingresso= " + getTestIngression();
-        result += " classificações= " + getClassifications();
-        result += " descrição(En)= " + getDescriptionEn();
+        result += " classificaï¿½ï¿½es= " + getClassifications();
+        result += " descriï¿½ï¿½o(En)= " + getDescriptionEn();
         result += " objectivos(En)= " + getObjectivesEn();
         result += " historial(En)= " + getHistoryEn();
         result += " saidas profissionais(En)=" + getProfessionalExitsEn();
-        result += " informação adicional(En)= " + getAdditionalInfoEn();
+        result += " informaï¿½ï¿½o adicional(En)= " + getAdditionalInfoEn();
         result += " links(En)= " + getLinksEn();
         result += " provas de ingresso(En)= " + getTestIngressionEn();
-        result += " classificações(En)= " + getClassificationsEn();
+        result += " classificaï¿½ï¿½es(En)= " + getClassificationsEn();
         result += " vagas iniciais= " + getDriftsInitial();
-        result += " vagas 1ª fase= " + getDriftsFirst();
-        result += " vagas 2ªfase= " + getDriftsSecond();
+        result += " vagas 1ï¿½ fase= " + getDriftsFirst();
+        result += " vagas 2ï¿½fase= " + getDriftsSecond();
         result += " nota minima= " + getMarkMin();
-        result += " nota máxima= " + getMarkMax();
-        result += " nota média= " + getMarkAverage();
-        result += " data última modificação= " + getLastModificationDate();
+        result += " nota mï¿½xima= " + getMarkMax();
+        result += " nota mï¿½dia= " + getMarkAverage();
+        result += " data ï¿½ltima modificaï¿½ï¿½o= " + getLastModificationDate();
         result += "]";
         return result;
     }
