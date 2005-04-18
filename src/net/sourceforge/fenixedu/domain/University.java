9ffba592d0312dccd4e99ef929ad8e03dd1e7724
João Florindo <jdnf@linkare.pt>
2005-04-18 21:20:16 +0000

    Added University dml mapping

--- src/net/sourceforge/fenixedu/domain/University.java
+++ src/net/sourceforge/fenixedu/domain/University.java
@@ -6,11 +6,7 @@ package net.sourceforge.fenixedu.domain;
  * 24/Mar/2003
  */
 
-public class University extends DomainObject implements IUniversity {
-
-    private String code;
-
-    private String name;
+public class University extends University_Base {
 
     public University() {
     }
@@ -33,21 +29,4 @@ public class University extends DomainObject implements IUniversity {
         result += "name = " + this.getName() + "; ";
         return result;
     }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String string) {
-        code = string;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String string) {
-        name = string;
-    }
-
 }
