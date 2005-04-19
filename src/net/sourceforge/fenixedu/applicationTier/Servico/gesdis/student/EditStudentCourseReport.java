5ecbe22e312e1fcd937bfc91b3957074fb13de55
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-19 10:40:14 +0000

    removed Cloner.copyInfoStudentCourseReport2IStudentCourseReport

--- src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/student/EditStudentCourseReport.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/student/EditStudentCourseReport.java
@@ -7,7 +7,6 @@ package net.sourceforge.fenixedu.applicationTier.Servico.gesdis.student;
 import net.sourceforge.fenixedu.applicationTier.Servico.framework.EditDomainObjectService;
 import net.sourceforge.fenixedu.dataTransferObject.InfoObject;
 import net.sourceforge.fenixedu.dataTransferObject.student.InfoStudentCourseReport;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
 import net.sourceforge.fenixedu.domain.IDomainObject;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
@@ -25,8 +24,7 @@ public class EditStudentCourseReport extends EditDomainObjectService {
      * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#clone2DomainObject(net.sourceforge.fenixedu.dataTransferObject.InfoObject)
      */
     protected IDomainObject clone2DomainObject(InfoObject infoObject) {
-        return Cloner
-                .copyInfoStudentCourseReport2IStudentCourseReport((InfoStudentCourseReport) infoObject);
+        return InfoStudentCourseReport.newDomainFromInfo((InfoStudentCourseReport) infoObject);
     }
 
     /*
