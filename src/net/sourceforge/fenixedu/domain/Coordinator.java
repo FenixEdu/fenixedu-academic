b21f7f9d5683ced6291a67be19dac207cd19e290
Francisco Paulo <frnp@ist.utl.pt>
2005-04-20 13:14:47 +0000

    DML migration: Coordinator update

--- src/net/sourceforge/fenixedu/domain/Coordinator.java
+++ src/net/sourceforge/fenixedu/domain/Coordinator.java
@@ -8,50 +8,34 @@ package net.sourceforge.fenixedu.domain;
  * Dominio
  * 
  * @author João Mota 27/Out/2003
- *  
+ * 
  */
 public class Coordinator extends Coordinator_Base {
 
-    private ITeacher teacher;
+	private ITeacher teacher;
 
-    private IExecutionDegree executionDegree;
+	/**
+	 * @param integer
+	 */
+	public Coordinator(Integer integer) {
+		setIdInternal(integer);
+	}
 
-    /**
-     * @param integer
-     */
-    public Coordinator(Integer integer) {
-        setIdInternal(integer);
-    }
+	public Coordinator() {
+	}
 
-    public Coordinator() {
-    }
+	/**
+	 * @return
+	 */
+	public ITeacher getTeacher() {
+		return teacher;
+	}
 
-    /**
-     * @return
-     */
-    public IExecutionDegree getExecutionDegree() {
-        return executionDegree;
-    }
-
-    /**
-     * @param executionDegree
-     */
-    public void setExecutionDegree(IExecutionDegree executionDegree) {
-        this.executionDegree = executionDegree;
-    }
-
-    /**
-     * @return
-     */
-    public ITeacher getTeacher() {
-        return teacher;
-    }
-
-    /**
-     * @param teacher
-     */
-    public void setTeacher(ITeacher teacher) {
-        this.teacher = teacher;
-    }
+	/**
+	 * @param teacher
+	 */
+	public void setTeacher(ITeacher teacher) {
+		this.teacher = teacher;
+	}
 
 }
