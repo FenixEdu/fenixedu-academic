132b30f979c122c7a5b419f6c466350b5296982c
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-20 17:18:16 +0000

    added dml mapping for Attends

--- src/net/sourceforge/fenixedu/domain/Attends.java
+++ src/net/sourceforge/fenixedu/domain/Attends.java
@@ -11,136 +11,30 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- *
- * @author  tfc130
+ * 
+ * @author tfc130
  */
-public class Attends extends DomainObject implements IAttends {
-    protected IStudent _aluno;
+public class Attends extends Attends_Base {
 
-    protected IExecutionCourse _disciplinaExecucao;
-
-    protected IEnrolment _enrolment;
-
-    // códigos internos da base de dados
-    private Integer _chaveAluno;
-
-    private Integer _chaveDisciplinaExecucao;
-
-    private Integer _keyEnrolment;
-
-	private List _attendInAttendsSet;
-
-    /** Construtor sem argumentos público requerido pela moldura de objectos OJB */
     public Attends() {
     }
 
-	public Attends(IStudent aluno, IExecutionCourse disciplinaExecucao) {
-		setAluno(aluno);
-		setDisciplinaExecucao(disciplinaExecucao);
-	}
+    public Attends(IStudent aluno, IExecutionCourse disciplinaExecucao) {
+        setAluno(aluno);
+        setDisciplinaExecucao(disciplinaExecucao);
+    }
 
-	public Attends(
-		IStudent aluno,
-		IExecutionCourse disciplinaExecucao,
-		IEnrolment enrolment) {
-		setAluno(aluno);
-		setDisciplinaExecucao(disciplinaExecucao);
-		setEnrolment(enrolment);
-	}
-
-	public IStudent getAluno() {
-		return _aluno;
-	}
-
-	public void setAluno(IStudent aluno) {
-		_aluno = aluno;
-	}
-
-	public Integer getChaveAluno() {
-		return _chaveAluno;
-	}
-
-	public void setChaveAluno(Integer chaveAluno) {
-		_chaveAluno = chaveAluno;
-	}
-
-	public IExecutionCourse getDisciplinaExecucao() {
-		return _disciplinaExecucao;
-	}
-
-	public void setDisciplinaExecucao(IExecutionCourse disciplinaExecucao) {
-		_disciplinaExecucao = disciplinaExecucao;
-	}
-
-	public Integer getChaveDisciplinaExecucao() {
-		return _chaveDisciplinaExecucao;
-	}
-
-	public void setChaveDisciplinaExecucao(Integer chaveDisciplinaExecucao) {
-		_chaveDisciplinaExecucao = chaveDisciplinaExecucao;
-	}
-
-	public Integer getKeyEnrolment() {
-		return _keyEnrolment;
-	}
-
-	public void setKeyEnrolment(Integer integer) {
-		_keyEnrolment = integer;
-	}
-
-	public IEnrolment getEnrolment() {
-		return _enrolment;
-	}
-
-	public void setEnrolment(IEnrolment enrolment) {
-		this._enrolment = enrolment;
-	}
-
-	
-	public List getAttendInAttendsSet() {
-		return _attendInAttendsSet;
-	}
-
-	public void setAttendInAttendsSet(List attendInAttendsSet) {
-		this._attendInAttendsSet = attendInAttendsSet;
-	}
-	
-	public List getAttendsSets() {
-		List attendsSets = new ArrayList();
-		Iterator iterAttendInAttendsSet = 
-			getAttendInAttendsSet().iterator();
-		IAttendInAttendsSet attendInAttendsSet = null;
-		while(iterAttendInAttendsSet.hasNext()){
-			attendInAttendsSet = (IAttendInAttendsSet)iterAttendInAttendsSet.next();
-			attendsSets.add(attendInAttendsSet.getAttendsSet());
-		}
-		return attendsSets;
-	}
-		
-	public void addAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
-		if( _attendInAttendsSet==null){
-			_attendInAttendsSet=new ArrayList();
-			_attendInAttendsSet.add(attendInAttendsSet);
-		}
-		else{
-			_attendInAttendsSet.add(attendInAttendsSet);	
-		}
-		
-	}
-	
-	public void removeAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
-			_attendInAttendsSet.remove(attendInAttendsSet);
-	}
-	
-	public boolean existsAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
-		return _attendInAttendsSet.contains(attendInAttendsSet);
-	}
+    public Attends(IStudent aluno, IExecutionCourse disciplinaExecucao, IEnrolment enrolment) {
+        setAluno(aluno);
+        setDisciplinaExecucao(disciplinaExecucao);
+        setEnrolment(enrolment);
+    }
 
     public boolean equals(Object obj) {
         boolean resultado = false;
         if (obj instanceof IAttends) {
             IAttends frequenta = (IAttends) obj;
-			resultado = getIdInternal().equals(frequenta.getIdInternal());
+            resultado = getIdInternal().equals(frequenta.getIdInternal());
         }
         return resultado;
     }
@@ -148,10 +42,39 @@ public class Attends extends DomainObject implements IAttends {
     public String toString() {
         String result = "[ATTEND";
         result += ", codigoInterno=" + getIdInternal();
-        result += ", Student=" + _aluno;
-        result += ", ExecutionCourse=" + _disciplinaExecucao;
-        result += ", Enrolment=" + _enrolment;
+        result += ", Student=" + getAluno();
+        result += ", ExecutionCourse=" + getDisciplinaExecucao();
+        result += ", Enrolment=" + getEnrolment();
         result += "]";
         return result;
     }
+
+    public List getAttendsSets() {
+        List attendsSets = new ArrayList();
+        Iterator iterAttendInAttendsSet = getAttendInAttendsSet().iterator();
+        IAttendInAttendsSet attendInAttendsSet = null;
+        while (iterAttendInAttendsSet.hasNext()) {
+            attendInAttendsSet = (IAttendInAttendsSet) iterAttendInAttendsSet.next();
+            attendsSets.add(attendInAttendsSet.getAttendsSet());
+        }
+        return attendsSets;
+    }
+
+    public void addAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
+        if (getAttendInAttendsSet() == null) {
+            setAttendInAttendsSet(new ArrayList());
+            getAttendInAttendsSet().add(attendInAttendsSet);
+        } else {
+            getAttendInAttendsSet().add(attendInAttendsSet);
+        }
+    }
+
+    public void removeAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
+        getAttendInAttendsSet().remove(attendInAttendsSet);
+    }
+
+    public boolean existsAttendInAttendsSet(IAttendInAttendsSet attendInAttendsSet) {
+        return getAttendInAttendsSet().contains(attendInAttendsSet);
+    }
+
 }
