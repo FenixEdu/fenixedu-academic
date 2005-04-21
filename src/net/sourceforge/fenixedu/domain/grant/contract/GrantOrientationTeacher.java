29dc3148645200b78525449f6013a22be1415171
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-21 13:06:29 +0000

    added dml mapping for GrantOrientationTeacher

--- src/net/sourceforge/fenixedu/domain/grant/contract/GrantOrientationTeacher.java
+++ src/net/sourceforge/fenixedu/domain/grant/contract/GrantOrientationTeacher.java
@@ -4,112 +4,12 @@
  */
 package net.sourceforge.fenixedu.domain.grant.contract;
 
-import java.util.Date;
-
-import net.sourceforge.fenixedu.domain.DomainObject;
-import net.sourceforge.fenixedu.domain.ITeacher;
 
 /**
  * @author Barbosa
  * @author Pica
  *  
  */
-public class GrantOrientationTeacher extends DomainObject implements IGrantOrientationTeacher {
-
-    private Date beginDate;
-
-    private Date endDate;
-
-    private ITeacher OrientationTeacher;
-
-    private IGrantContract grantContract;
-
-    private Integer keyTeacher;
-
-    private Integer keyContract;
-
-    /**
-     * @return
-     */
-    public Date getBeginDate() {
-        return beginDate;
-    }
-
-    /**
-     * @param beginDate
-     */
-    public void setBeginDate(Date beginDate) {
-        this.beginDate = beginDate;
-    }
-
-    /**
-     * @return
-     */
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    /**
-     * @param endDate
-     */
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    /**
-     * @return
-     */
-    public IGrantContract getGrantContract() {
-        return grantContract;
-    }
-
-    /**
-     * @param grantContract
-     */
-    public void setGrantContract(IGrantContract grantContract) {
-        this.grantContract = grantContract;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getKeyContract() {
-        return keyContract;
-    }
-
-    /**
-     * @param keyContract
-     */
-    public void setKeyContract(Integer keyContract) {
-        this.keyContract = keyContract;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getKeyTeacher() {
-        return keyTeacher;
-    }
-
-    /**
-     * @param keyTeacher
-     */
-    public void setKeyTeacher(Integer keyTeacher) {
-        this.keyTeacher = keyTeacher;
-    }
-
-    /**
-     * @return
-     */
-    public ITeacher getOrientationTeacher() {
-        return OrientationTeacher;
-    }
-
-    /**
-     * @param orientaionTeacher
-     */
-    public void setOrientationTeacher(ITeacher orientationTeacher) {
-        OrientationTeacher = orientationTeacher;
-    }
+public class GrantOrientationTeacher extends GrantOrientationTeacher_Base {
 
 }
