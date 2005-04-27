1648d4c340cde4bcda38e637e2ce3c75d1032936
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-27 16:06:44 +0000

    added dml mapping for PersonRole

--- src/net/sourceforge/fenixedu/domain/PersonRole.java
+++ src/net/sourceforge/fenixedu/domain/PersonRole.java
@@ -4,77 +4,11 @@ package net.sourceforge.fenixedu.domain;
  * 
  * @author Nuno Nunes (nmsn@rnl.ist.utl.pt) Joana Mota (jccm@rnl.ist.utl.pt)
  */
-public class PersonRole extends DomainObject implements IPersonRole {
-    private Integer keyPerson;
-
-    private Integer keyRole;
-
-    private IPerson person;
-
-    private IRole role;
-
-    public PersonRole() {
-    }
+public class PersonRole extends PersonRole_Base implements IPersonRole {
 
     public boolean equals(Object obj) {
         return ((obj instanceof PersonRole) && (((PersonRole) obj).getRole().equals(getRole())) && (((PersonRole) obj)
                 .getPerson().equals(getPerson())));
-    }
-
-    /**
-     * @return
-     */
-    public Integer getKeyPerson() {
-        return keyPerson;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getKeyRole() {
-        return keyRole;
-    }
-
-    /**
-     * @return
-     */
-    public IPerson getPerson() {
-        return person;
-    }
-
-    /**
-     * @return
-     */
-    public IRole getRole() {
-        return role;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setKeyPerson(Integer integer) {
-        keyPerson = integer;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setKeyRole(Integer integer) {
-        keyRole = integer;
-    }
-
-    /**
-     * @param pessoa
-     */
-    public void setPerson(IPerson pessoa) {
-        person = pessoa;
-    }
-
-    /**
-     * @param role
-     */
-    public void setRole(IRole role) {
-        this.role = role;
     }
 
 }
