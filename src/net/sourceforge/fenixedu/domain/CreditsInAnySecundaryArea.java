ef525884193ce451ae6dd84bae3f17319111d7bd
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-27 11:54:28 +0000

    added dml mapping for CreditsInAnySecundaryArea

--- src/net/sourceforge/fenixedu/domain/CreditsInAnySecundaryArea.java
+++ src/net/sourceforge/fenixedu/domain/CreditsInAnySecundaryArea.java
@@ -4,19 +4,7 @@ package net.sourceforge.fenixedu.domain;
  * @author David Santos Jan 14, 2004
  */
 
-public class CreditsInAnySecundaryArea extends DomainObject implements ICreditsInAnySecundaryArea {
-    private Integer studentCurricularPlanKey;
-
-    private Integer enrolmentKey;
-
-    private Integer givenCredits;
-
-    private IStudentCurricularPlan studentCurricularPlan;
-
-    private IEnrolment enrolment;
-
-    public CreditsInAnySecundaryArea() {
-    }
+public class CreditsInAnySecundaryArea extends CreditsInAnySecundaryArea_Base {
 
     public boolean equals(Object obj) {
         boolean result = false;
@@ -36,78 +24,4 @@ public class CreditsInAnySecundaryArea extends DomainObject implements ICreditsI
         return result;
     }
 
-    /**
-     * @return Returns the enrolment.
-     */
-    public IEnrolment getEnrolment() {
-        return enrolment;
-    }
-
-    /**
-     * @param enrolment
-     *            The enrolment to set.
-     */
-    public void setEnrolment(IEnrolment enrolment) {
-        this.enrolment = enrolment;
-    }
-
-    /**
-     * @return Returns the enrolmentKey.
-     */
-    public Integer getEnrolmentKey() {
-        return enrolmentKey;
-    }
-
-    /**
-     * @param enrolmentKey
-     *            The enrolmentKey to set.
-     */
-    public void setEnrolmentKey(Integer enrolmentKey) {
-        this.enrolmentKey = enrolmentKey;
-    }
-
-    /**
-     * @return Returns the givenCredits.
-     */
-    public Integer getGivenCredits() {
-        return givenCredits;
-    }
-
-    /**
-     * @param givenCredits
-     *            The givenCredits to set.
-     */
-    public void setGivenCredits(Integer givenCredits) {
-        this.givenCredits = givenCredits;
-    }
-
-    /**
-     * @return Returns the studentCurricularPlan.
-     */
-    public IStudentCurricularPlan getStudentCurricularPlan() {
-        return studentCurricularPlan;
-    }
-
-    /**
-     * @param studentCurricularPlan
-     *            The studentCurricularPlan to set.
-     */
-    public void setStudentCurricularPlan(IStudentCurricularPlan studentCurricularPlan) {
-        this.studentCurricularPlan = studentCurricularPlan;
-    }
-
-    /**
-     * @return Returns the studentCurricularPlanKey.
-     */
-    public Integer getStudentCurricularPlanKey() {
-        return studentCurricularPlanKey;
-    }
-
-    /**
-     * @param studentCurricularPlanKey
-     *            The studentCurricularPlanKey to set.
-     */
-    public void setStudentCurricularPlanKey(Integer studentCurricularPlanKey) {
-        this.studentCurricularPlanKey = studentCurricularPlanKey;
-    }
 }
