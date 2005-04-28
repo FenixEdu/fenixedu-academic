92dd4d9912b969d31819e608d5c369c7108c2576
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 10:02:35 +0000

    added dml mapping for Tutor

--- src/net/sourceforge/fenixedu/domain/Tutor.java
+++ src/net/sourceforge/fenixedu/domain/Tutor.java
@@ -8,14 +8,7 @@ package net.sourceforge.fenixedu.domain;
  * @author Tânia Pousão
  *  
  */
-public class Tutor extends DomainObject implements ITutor {
-    private Integer teacherKey;
-
-    private ITeacher teacher;
-
-    private Integer studentKey;
-
-    private IStudent student;
+public class Tutor extends Tutor_Base {
 
     public Tutor() {
     }
@@ -25,66 +18,6 @@ public class Tutor extends DomainObject implements ITutor {
      */
     public Tutor(Integer idInternal) {
         setIdInternal(idInternal);
-    }
-
-    /**
-     * @return Returns the student.
-     */
-    public IStudent getStudent() {
-        return student;
-    }
-
-    /**
-     * @param student
-     *            The student to set.
-     */
-    public void setStudent(IStudent student) {
-        this.student = student;
-    }
-
-    /**
-     * @return Returns the studentKey.
-     */
-    public Integer getStudentKey() {
-        return studentKey;
-    }
-
-    /**
-     * @param studentKey
-     *            The studentKey to set.
-     */
-    public void setStudentKey(Integer studentKey) {
-        this.studentKey = studentKey;
-    }
-
-    /**
-     * @return Returns the teacher.
-     */
-    public ITeacher getTeacher() {
-        return teacher;
-    }
-
-    /**
-     * @param teacher
-     *            The teacher to set.
-     */
-    public void setTeacher(ITeacher teacher) {
-        this.teacher = teacher;
-    }
-
-    /**
-     * @return Returns the teacherKey.
-     */
-    public Integer getTeacherKey() {
-        return teacherKey;
-    }
-
-    /**
-     * @param teacherKey
-     *            The teacherKey to set.
-     */
-    public void setTeacherKey(Integer teacherKey) {
-        this.teacherKey = teacherKey;
     }
 
 }
