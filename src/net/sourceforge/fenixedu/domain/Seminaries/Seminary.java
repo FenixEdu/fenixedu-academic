6a9fe60b3b1bcefbb483a8c19a171d8195a04ee4
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 13:59:33 +0000

    added dml mapping for Seminary

--- src/net/sourceforge/fenixedu/domain/Seminaries/Seminary.java
+++ src/net/sourceforge/fenixedu/domain/Seminaries/Seminary.java
@@ -8,8 +8,6 @@ package net.sourceforge.fenixedu.domain.Seminaries;
 import java.util.Calendar;
 import java.util.List;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
-
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
  * 
@@ -17,29 +15,11 @@ import net.sourceforge.fenixedu.domain.DomainObject;
  * Created at Jul 23, 2003, 9:49:19 AM
  *  
  */
-public class Seminary extends DomainObject implements ISeminary {
-
-    private String name;
-
-    private String description;
-
-    private List equivalencies;
-
-    private Integer allowedCandidaciesPerStudent;
-
+public class Seminary extends Seminary_Base {
     private Calendar enrollmentBeginDate;
-
     private Calendar enrollmentBeginTime;
-
     private Calendar enrollmentEndDate;
-
     private Calendar enrollmentEndTime;
-
-    private Boolean hasTheme;
-
-    private Boolean hasCaseStudy;
-
-    private List candidacies;
 
     public Seminary() {
     }
@@ -53,48 +33,6 @@ public class Seminary extends DomainObject implements ISeminary {
     public Seminary(String name, String description) {
         this.setDescription(description);
         this.setName(name);
-    }
-
-    /**
-     * @return
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @return
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return
-     */
-    public List getEquivalencies() {
-        return equivalencies;
-    }
-
-    /**
-     * @param string
-     */
-    public void setDescription(String string) {
-        description = string;
-    }
-
-    /**
-     * @param string
-     */
-    public void setName(String string) {
-        name = string;
-    }
-
-    /**
-     * @param list
-     */
-    public void setEquivalencies(List list) {
-        equivalencies = list;
     }
 
     public String toString() {
@@ -121,20 +59,6 @@ public class Seminary extends DomainObject implements ISeminary {
                 equalsResult = this.getName().equalsIgnoreCase(seminary.getName());
         }
         return equalsResult;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getAllowedCandidaciesPerStudent() {
-        return allowedCandidaciesPerStudent;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setAllowedCandidaciesPerStudent(Integer integer) {
-        allowedCandidaciesPerStudent = integer;
     }
 
     /**
@@ -193,46 +117,4 @@ public class Seminary extends DomainObject implements ISeminary {
         enrollmentEndTime = calendar;
     }
 
-    /**
-     * @return
-     */
-    public Boolean getHasTheme() {
-        return hasTheme;
-    }
-
-    /**
-     * @param boolean1
-     */
-    public void setHasTheme(Boolean boolean1) {
-        hasTheme = boolean1;
-    }
-
-    /**
-     * @return
-     */
-    public Boolean getHasCaseStudy() {
-        return hasCaseStudy;
-    }
-
-    /**
-     * @param boolean1
-     */
-    public void setHasCaseStudy(Boolean boolean1) {
-        hasCaseStudy = boolean1;
-    }
-
-    /**
-     * @return Returns the candidacies.
-     */
-    public List getCandidacies() {
-        return candidacies;
-    }
-
-    /**
-     * @param candidacies
-     *            The candidacies to set.
-     */
-    public void setCandidacies(List candidacies) {
-        this.candidacies = candidacies;
-    }
 }
