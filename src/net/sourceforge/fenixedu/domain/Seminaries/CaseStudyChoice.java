e3fd83edef6925090a3706c93c95cf13b3d37c1b
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 13:32:16 +0000

    added dml mapping for CaseStudyChoice

--- src/net/sourceforge/fenixedu/domain/Seminaries/CaseStudyChoice.java
+++ src/net/sourceforge/fenixedu/domain/Seminaries/CaseStudyChoice.java
@@ -5,7 +5,6 @@
  */
 package net.sourceforge.fenixedu.domain.Seminaries;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
 
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
@@ -14,89 +13,6 @@ import net.sourceforge.fenixedu.domain.DomainObject;
  * Created at 29/Jul/2003, 14:09:34
  *  
  */
-public class CaseStudyChoice extends DomainObject implements ICaseStudyChoice {
-    private Integer order;
+public class CaseStudyChoice extends CaseStudyChoice_Base {
 
-    private Integer caseStudyIdInternal;
-
-    private ICaseStudy caseStudy;
-
-    private Integer candidacyIdInternal;
-
-    private ICandidacy candidacy;
-
-    public CaseStudyChoice() {
-    }
-
-    /**
-     * @return
-     */
-    public Integer getCandidacyIdInternal() {
-        return candidacyIdInternal;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getCaseStudyIdInternal() {
-        return caseStudyIdInternal;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getOrder() {
-        return order;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setCandidacyIdInternal(Integer integer) {
-        candidacyIdInternal = integer;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setCaseStudyIdInternal(Integer integer) {
-        caseStudyIdInternal = integer;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setOrder(Integer integer) {
-        order = integer;
-    }
-
-    /**
-     * @return Returns the candidacy.
-     */
-    public ICandidacy getCandidacy() {
-        return candidacy;
-    }
-
-    /**
-     * @param candidacy
-     *            The candidacy to set.
-     */
-    public void setCandidacy(ICandidacy candidacy) {
-        this.candidacy = candidacy;
-    }
-
-    /**
-     * @return Returns the caseStudy.
-     */
-    public ICaseStudy getCaseStudy() {
-        return caseStudy;
-    }
-
-    /**
-     * @param caseStudy
-     *            The caseStudy to set.
-     */
-    public void setCaseStudy(ICaseStudy caseStudy) {
-        this.caseStudy = caseStudy;
-    }
 }
