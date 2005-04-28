027ac76530ac38921e12524d632f3bd8947bd13d
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 14:18:03 +0000

    added dml mapping for PersonalDataUseInquiryAnswers

--- src/net/sourceforge/fenixedu/domain/student/PersonalDataUseInquiryAnswers.java
+++ src/net/sourceforge/fenixedu/domain/student/PersonalDataUseInquiryAnswers.java
@@ -1,7 +1,5 @@
 package net.sourceforge.fenixedu.domain.student;
 
-import net.sourceforge.fenixedu.domain.DomainObject;
-import net.sourceforge.fenixedu.domain.IStudent;
 
 /**
  * @author Nuno Correia & Ricardo Rodrigues
@@ -9,97 +7,29 @@ import net.sourceforge.fenixedu.domain.IStudent;
  * 12/07/2004
  */
 
-public class PersonalDataUseInquiryAnswers extends DomainObject implements
-        IPersonalDataUseInquiryAnswers {
+public class PersonalDataUseInquiryAnswers extends PersonalDataUseInquiryAnswers_Base {
 
-    private Boolean answer1;
-
-    private Boolean answer2;
-
-    private Boolean answer3;
-
-    private Boolean answer4;
-
-    private Boolean answer5;
-
-    private Boolean answer6;
-
-    private Boolean answer7;
-
-    private Integer keyStudent;
-
-    private IStudent student;
-
-    public Boolean getAnswer1() {
-        return this.answer1;
+    public String toString() {
+        String result = "[" + this.getClass().getName() + ": ";
+        result += " keyStudent " + this.getKeyStudent() + "]\n ";
+        result += "idInternal " + this.getIdInternal() + ";\n ";
+        result += " answer1 " + this.getAnswer1() + "\n ";
+        result += " answer2 " + this.getAnswer2() + "\n ";
+        result += " answer3 " + this.getAnswer3() + "\n ";
+        result += " answer4 " + this.getAnswer4() + "\n ";
+        result += " answer5 " + this.getAnswer5() + "\n ";
+        result += " answer6 " + this.getAnswer6() + "\n ";
+        result += " answer7 " + this.getAnswer7() + "\n ";
+        return result;
     }
 
-    public void setAnswer1(Boolean answer) {
-        this.answer1 = answer;
-    }
-
-    public Boolean getAnswer2() {
-        return answer2;
-    }
-
-    public void setAnswer2(Boolean answer2) {
-        this.answer2 = answer2;
-    }
-
-    public Boolean getAnswer3() {
-        return answer3;
-    }
-
-    public void setAnswer3(Boolean answer3) {
-        this.answer3 = answer3;
-    }
-
-    public Boolean getAnswer4() {
-        return answer4;
-    }
-
-    public void setAnswer4(Boolean answer4) {
-        this.answer4 = answer4;
-    }
-
-    public Boolean getAnswer5() {
-        return answer5;
-    }
-
-    public void setAnswer5(Boolean answer5) {
-        this.answer5 = answer5;
-    }
-
-    public Boolean getAnswer6() {
-        return answer6;
-    }
-
-    public void setAnswer6(Boolean answer6) {
-        this.answer6 = answer6;
-    }
-
-    public Boolean getAnswer7() {
-        return answer7;
-    }
-
-    public void setAnswer7(Boolean answer7) {
-        this.answer7 = answer7;
-    }
-
-    public Integer getKeyStudent() {
-        return this.keyStudent;
-    }
-
-    public void setKeyStudent(Integer keyStudent) {
-        this.keyStudent = keyStudent;
-    }
-
-    public IStudent getStudent() {
-        return student;
-    }
-
-    public void setStudent(IStudent student) {
-        this.student = student;
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (obj instanceof PersonalDataUseInquiryAnswers) {
+            PersonalDataUseInquiryAnswers inquiryanswer = (PersonalDataUseInquiryAnswers) obj;
+            result = getKeyStudent().equals(inquiryanswer.getKeyStudent());
+        }
+        return result;
     }
 
     public void setAnswer(Integer id, Boolean answer) {
@@ -128,27 +58,5 @@ public class PersonalDataUseInquiryAnswers extends DomainObject implements
         }
     }
 
-    public String toString() {
-        String result = "[" + this.getClass().getName() + ": ";
-        result += " keyStudent " + keyStudent + "]\n ";
-        result += "idInternal " + getIdInternal() + ";\n ";
-        result += " answer1 " + answer1 + "\n ";
-        result += " answer2 " + answer2 + "\n ";
-        result += " answer3 " + answer3 + "\n ";
-        result += " answer4 " + answer4 + "\n ";
-        result += " answer5 " + answer5 + "\n ";
-        result += " answer6 " + answer6 + "\n ";
-        result += " answer7 " + answer7 + "\n ";
-        return result;
-    }
-
-    public boolean equals(Object obj) {
-        boolean result = false;
-        if (obj instanceof PersonalDataUseInquiryAnswers) {
-            PersonalDataUseInquiryAnswers inquiryanswer = (PersonalDataUseInquiryAnswers) obj;
-            result = getKeyStudent().equals(inquiryanswer.getKeyStudent());
-        }
-        return result;
-    }
 
 }
