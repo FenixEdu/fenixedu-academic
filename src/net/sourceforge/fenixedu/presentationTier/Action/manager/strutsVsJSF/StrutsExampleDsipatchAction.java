ee471c6c36d4b9b547397288137beb3ffc004896
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 09:48:23 +0000

    removed unthrowned exception declaration

--- src/net/sourceforge/fenixedu/presentationTier/Action/manager/strutsVsJSF/StrutsExampleDsipatchAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/manager/strutsVsJSF/StrutsExampleDsipatchAction.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
-import net.sourceforge.fenixedu.presentationTier.Action.exceptions.FenixActionException;
 import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
 
 import org.apache.struts.action.ActionForm;
@@ -18,7 +17,7 @@ import org.apache.struts.action.ActionMapping;
 public class StrutsExampleDsipatchAction extends FenixDispatchAction {
     
     public ActionForward showFirstPage(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws FenixActionException, FenixFilterException {
+            HttpServletResponse response) {
         
         return mapping.findForward("showFirstPage");
 
@@ -26,7 +25,7 @@ public class StrutsExampleDsipatchAction extends FenixDispatchAction {
     }
     
     public ActionForward showExecutionPeriods(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws FenixActionException, FenixFilterException {
+            HttpServletResponse response) {
                 
         final Object[] args = {};
         Collection infoExecutionPeriods = null;
