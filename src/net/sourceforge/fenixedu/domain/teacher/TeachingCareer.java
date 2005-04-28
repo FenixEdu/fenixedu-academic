1dac532ef59d13cc2c242fb7d13e6a06ae2cc242
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 16:02:11 +0000

    added dml mapping for TeachingCareer

--- src/net/sourceforge/fenixedu/domain/teacher/TeachingCareer.java
+++ src/net/sourceforge/fenixedu/domain/teacher/TeachingCareer.java
@@ -7,73 +7,15 @@ package net.sourceforge.fenixedu.domain.teacher;
 /**
  * @author Leonor Almeida
  * @author Sergio Montelobo
- *  
+ * 
  */
-public class TeachingCareer extends Career implements ITeachingCareer {
+public class TeachingCareer extends TeachingCareer_Base {
 
-    private ICategory category;
-
-    private Integer keyCategory;
-
-    private String courseOrPosition;
-
-    /**
-     *  
-     */
     public TeachingCareer() {
-        super();
     }
 
-    /**
-     * @param idInternal
-     */
     public TeachingCareer(Integer idInternal) {
-        super(idInternal);
-    }
-
-    /**
-     * @return Returns the category.
-     */
-    public ICategory getCategory() {
-        return category;
-    }
-
-    /**
-     * @param category
-     *            The category to set.
-     */
-    public void setCategory(ICategory category) {
-        this.category = category;
-    }
-
-    /**
-     * @return Returns the courseOrPosition.
-     */
-    public String getCourseOrPosition() {
-        return courseOrPosition;
-    }
-
-    /**
-     * @param courseOrPosition
-     *            The courseOrPosition to set.
-     */
-    public void setCourseOrPosition(String courseOrPosition) {
-        this.courseOrPosition = courseOrPosition;
-    }
-
-    /**
-     * @return Returns the keyCategory.
-     */
-    public Integer getKeyCategory() {
-        return keyCategory;
-    }
-
-    /**
-     * @param keyCategory
-     *            The keyCategory to set.
-     */
-    public void setKeyCategory(Integer keyCategory) {
-        this.keyCategory = keyCategory;
+        setIdInternal(idInternal);
     }
 
     public String toString() {
@@ -86,4 +28,5 @@ public class TeachingCareer extends Career implements ITeachingCareer {
         result += "]";
         return result;
     }
+
 }
