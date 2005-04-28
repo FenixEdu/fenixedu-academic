86e5ebe48ad6ca1a20bb23200c2ac788a6fada6e
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 12:19:16 +0000

    added dml mapping for CaseStudy

--- src/net/sourceforge/fenixedu/domain/Seminaries/CaseStudy.java
+++ src/net/sourceforge/fenixedu/domain/Seminaries/CaseStudy.java
@@ -5,9 +5,6 @@
  */
 package net.sourceforge.fenixedu.domain.Seminaries;
 
-import java.util.List;
-
-import net.sourceforge.fenixedu.domain.DomainObject;
 
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
@@ -16,21 +13,8 @@ import net.sourceforge.fenixedu.domain.DomainObject;
  * Created at 28/Jul/2003, 15:05:18
  *  
  */
-public class CaseStudy extends DomainObject implements ICaseStudy {
-    private Integer idInternal;
-
-    private ITheme seminaryTheme;
-
-    private List seminaryCandidacies;
-
-    private String code;
-
-    private String description;
-
-    private String name;
-
-    private Integer seminaryThemeIdInternal;
-
+public class CaseStudy extends CaseStudy_Base {
+    
     public CaseStudy() {
     }
 
@@ -38,104 +22,6 @@ public class CaseStudy extends DomainObject implements ICaseStudy {
         this.setName(name);
         this.setDescription(description);
         this.setCode(code);
-    }
-
-    /**
-     * @return
-     */
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * @return
-     */
-    public String getDescription() {
-        return description;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getIdInternal() {
-        return idInternal;
-    }
-
-    /**
-     * @return
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return
-     */
-    public ITheme getSeminaryTheme() {
-        return seminaryTheme;
-    }
-
-    /**
-     * @param string
-     */
-    public void setCode(String string) {
-        code = string;
-    }
-
-    /**
-     * @param string
-     */
-    public void setDescription(String string) {
-        description = string;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setIdInternal(Integer integer) {
-        idInternal = integer;
-    }
-
-    /**
-     * @param string
-     */
-    public void setName(String string) {
-        name = string;
-    }
-
-    /**
-     * @param seminary
-     */
-    public void setSeminaryTheme(ITheme seminary) {
-        this.seminaryTheme = seminary;
-    }
-
-    /**
-     * @return
-     */
-    public Integer getSeminaryThemeIdInternal() {
-        return seminaryThemeIdInternal;
-    }
-
-    /**
-     * @param integer
-     */
-    public void setSeminaryThemeIdInternal(Integer integer) {
-        seminaryThemeIdInternal = integer;
-    }
-
-    /**
-     * @return
-     */
-    public List getSeminaryCandidacies() {
-        return seminaryCandidacies;
-    }
-
-    /**
-     * @param list
-     */
-    public void setSeminaryCandidacies(List list) {
-        seminaryCandidacies = list;
     }
 
 }
