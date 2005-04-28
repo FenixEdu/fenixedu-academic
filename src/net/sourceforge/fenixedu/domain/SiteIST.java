2281eea7ff4d5c5a7f89b29e5b580e5d1c536943
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-28 09:48:48 +0000

    added dml mapping for SiteIST

--- src/net/sourceforge/fenixedu/domain/SiteIST.java
+++ src/net/sourceforge/fenixedu/domain/SiteIST.java
@@ -4,6 +4,6 @@ package net.sourceforge.fenixedu.domain;
  * @author Fernanda Quitério 23/09/2003
  *  
  */
-public class SiteIST extends WebSite implements ISiteIST {
+public class SiteIST extends Site_Base {
 
 }
