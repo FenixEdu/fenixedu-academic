f306b8dbad4ec653c0cfd0a899e1553ecdbee8b2
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-29 08:32:10 +0000

    add dml mapping for WeeklyOcupation

--- src/net/sourceforge/fenixedu/domain/teacher/WeeklyOcupation.java
+++ src/net/sourceforge/fenixedu/domain/teacher/WeeklyOcupation.java
@@ -4,161 +4,18 @@
  */
 package net.sourceforge.fenixedu.domain.teacher;
 
-import java.util.Date;
-
-import net.sourceforge.fenixedu.domain.DomainObject;
-import net.sourceforge.fenixedu.domain.ITeacher;
-
 /**
  * @author Leonor Almeida
  * @author Sergio Montelobo
  *  
  */
-public class WeeklyOcupation extends DomainObject implements IWeeklyOcupation {
-    private Integer research;
+public class WeeklyOcupation extends WeeklyOcupation_Base {
 
-    private Integer management;
-
-    private Integer other;
-
-    private Integer lecture;
-
-    private Integer support;
-
-    private ITeacher teacher;
-
-    private Integer keyTeacher;
-
-    private Date lastModificationDate;
-
-    /**
-     *  
-     */
     public WeeklyOcupation() {
     }
 
     public WeeklyOcupation(Integer idInternal) {
         setIdInternal(idInternal);
-    }
-
-    /**
-     * @return Returns the other.
-     */
-    public Integer getOther() {
-        return other;
-    }
-
-    /**
-     * @param other
-     *            The other to set.
-     */
-    public void setOther(Integer other) {
-        this.other = other;
-    }
-
-    /**
-     * @return Returns the research.
-     */
-    public Integer getResearch() {
-        return research;
-    }
-
-    /**
-     * @param research
-     *            The research to set.
-     */
-    public void setResearch(Integer research) {
-        this.research = research;
-    }
-
-    /**
-     * @return Returns the teacher.
-     */
-    public ITeacher getTeacher() {
-        return teacher;
-    }
-
-    /**
-     * @param teacher
-     *            The teacher to set.
-     */
-    public void setTeacher(ITeacher teacher) {
-        this.teacher = teacher;
-    }
-
-    /**
-     * @return Returns the keyTeacher.
-     */
-    public Integer getKeyTeacher() {
-        return keyTeacher;
-    }
-
-    /**
-     * @param keyTeacher
-     *            The keyTeacher to set.
-     */
-    public void setKeyTeacher(Integer keyTeacher) {
-        this.keyTeacher = keyTeacher;
-    }
-
-    /**
-     * @return Returns the management.
-     */
-    public Integer getManagement() {
-        return management;
-    }
-
-    /**
-     * @param management
-     *            The management to set.
-     */
-    public void setManagement(Integer management) {
-        this.management = management;
-    }
-
-    /**
-     * @return Returns the lastModificationDate.
-     */
-    public Date getLastModificationDate() {
-        return lastModificationDate;
-    }
-
-    /**
-     * @param lastModificationDate
-     *            The lastModificationDate to set.
-     */
-    public void setLastModificationDate(Date lastModificationDate) {
-        this.lastModificationDate = lastModificationDate;
-    }
-
-    /**
-     * @return Returns the lecture.
-     */
-    public Integer getLecture() {
-        return lecture;
-    }
-
-    /**
-     * @param lecture
-     *            The lecture to set.
-     */
-    public void setLecture(Integer lecture) {
-        this.lecture = lecture;
-    }
-
-    /**
-     * @return Returns the support.
-     */
-    public Integer getSupport() {
-        return support;
-    }
-
-    /**
-     * @param support
-     *            The support to set.
-     */
-    public void setSupport(Integer support) {
-        this.support = support;
     }
 
 }
