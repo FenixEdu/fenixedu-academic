dc5ab931f4feaf3edf8e63b7de933f066b114c3b
Luís Egídio <luis.egidio@ist.utl.pt>
2005-04-29 08:55:07 +0000

    added dml mapping for CostCenter

--- src/net/sourceforge/fenixedu/domain/CostCenter.java
+++ src/net/sourceforge/fenixedu/domain/CostCenter.java
@@ -6,16 +6,9 @@ package net.sourceforge.fenixedu.domain;
 
 /**
  * @author Tânia Pousão
- *  
+ * 
  */
-public class CostCenter extends DomainObject implements ICostCenter {
-    private String code;
-
-    private String departament;
-
-    private String section1;
-
-    private String section2;
+public class CostCenter extends CostCenter_Base {
 
     public CostCenter() {
     }
@@ -25,66 +18,6 @@ public class CostCenter extends DomainObject implements ICostCenter {
         setDepartament(departament);
         setSection1(section1);
         setSection2(section2);
-    }
-
-    /**
-     * @return Returns the departament.
-     */
-    public String getDepartament() {
-        return departament;
-    }
-
-    /**
-     * @param departament
-     *            The departament to set.
-     */
-    public void setDepartament(String departament) {
-        this.departament = departament;
-    }
-
-    /**
-     * @return Returns the section1.
-     */
-    public String getSection1() {
-        return section1;
-    }
-
-    /**
-     * @param section1
-     *            The section1 to set.
-     */
-    public void setSection1(String section1) {
-        this.section1 = section1;
-    }
-
-    /**
-     * @return Returns the section2.
-     */
-    public String getSection2() {
-        return section2;
-    }
-
-    /**
-     * @param section2
-     *            The section2 to set.
-     */
-    public void setSection2(String section2) {
-        this.section2 = section2;
-    }
-
-    /**
-     * @return Returns the sigla.
-     */
-    public String getCode() {
-        return code;
-    }
-
-    /**
-     * @param sigla
-     *            The sigla to set.
-     */
-    public void setCode(String code) {
-        this.code = code;
     }
 
 }
