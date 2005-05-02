5e52e4717085e021748bb37c994a259ee3a6e517
Shezad Anavarali <shezad@ist.utl.pt>
2005-05-02 17:52:34 +0000

    readActiveByDegreeCurricularPlan

--- src/net/sourceforge/fenixedu/persistenceTier/OJB/MasterDegreeThesisDataVersionOJB.java
+++ src/net/sourceforge/fenixedu/persistenceTier/OJB/MasterDegreeThesisDataVersionOJB.java
@@ -6,8 +6,6 @@ package net.sourceforge.fenixedu.persistenceTier.OJB;
 
 import java.util.List;
 
-import org.apache.ojb.broker.query.Criteria;
-
 import net.sourceforge.fenixedu.domain.IMasterDegreeThesis;
 import net.sourceforge.fenixedu.domain.IMasterDegreeThesisDataVersion;
 import net.sourceforge.fenixedu.domain.IStudentCurricularPlan;
@@ -16,10 +14,12 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentMasterDegreeThesisDataVersion;
 import net.sourceforge.fenixedu.util.State;
 
+import org.apache.ojb.broker.query.Criteria;
+
 /**
  * @author - Shezad Anavarali (sana@mega.ist.utl.pt) - Nadir Tarmahomed
  *         (naat@mega.ist.utl.pt)
- *  
+ * 
  */
 public class MasterDegreeThesisDataVersionOJB extends PersistentObjectOJB implements
         IPersistentMasterDegreeThesisDataVersion {
@@ -75,6 +75,17 @@ public class MasterDegreeThesisDataVersionOJB extends PersistentObjectOJB implem
         List result = queryList(MasterDegreeThesisDataVersion.class, criteria, "lastModification", false);
 
         return result;
+    }
+
+    public List readActiveByDegreeCurricularPlan(Integer degreeCurricularPlanID)
+            throws ExcepcaoPersistencia {
+        Criteria criteria = new Criteria();
+
+        criteria.addEqualTo("masterDegreeThesis.studentCurricularPlan.degreeCurricularPlan.idInternal",
+                degreeCurricularPlanID);
+        criteria.addEqualTo("currentState", new Integer(State.ACTIVE));
+
+        return queryList(MasterDegreeThesisDataVersion.class, criteria);
     }
 
 }
