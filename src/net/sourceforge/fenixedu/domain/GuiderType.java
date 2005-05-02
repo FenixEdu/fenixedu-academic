f12b32bbf03ec2333146a34ae6ad58cf63b0c979
Shezad Anavarali <shezad@ist.utl.pt>
2005-05-02 17:52:00 +0000

    Guider types

---
+++ src/net/sourceforge/fenixedu/domain/GuiderType.java
@@ -0,0 +1,18 @@
+package net.sourceforge.fenixedu.domain;
+
+/**
+ * 
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ *
+ */
+public enum GuiderType {
+
+    GUIDER,
+
+    ASSISTENT,
+
+    EXTERNAL_GUIDER,
+
+    EXTERNAL_ASSISTENT;
+
+}
