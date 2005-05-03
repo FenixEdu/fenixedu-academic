c8f9ab21ff80c1cb22fae6352089c5def11a7982
Luis Cruz <luis.cruz@ist.utl.pt>
2005-05-03 13:28:12 +0000

    New Helper class.

---
+++ src/net/sourceforge/fenixedu/util/StringAppender.java
@@ -0,0 +1,45 @@
+package net.sourceforge.fenixedu.util;
+
+public class StringAppender
+{
+
+    public static final String append(final String string1, final String string2, final String string3) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string1);
+        stringBuilder.append(string2);
+        stringBuilder.append(string3);
+        return stringBuilder.toString();
+    }
+
+    public static final String append(final String string1, final String string2, final String string3, final String string4) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string1);
+        stringBuilder.append(string2);
+        stringBuilder.append(string3);
+        stringBuilder.append(string4);
+        return stringBuilder.toString();
+    }
+
+    public static final String append(final String string1, final String string2, final String string3, final String string4, final String string5) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string1);
+        stringBuilder.append(string2);
+        stringBuilder.append(string3);
+        stringBuilder.append(string4);
+        stringBuilder.append(string5);
+        return stringBuilder.toString();
+    }
+
+    public static final String append(final String string1, final String string2, final String string3, final String string4, final String string5, final String string6) {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(string1);
+        stringBuilder.append(string2);
+        stringBuilder.append(string3);
+        stringBuilder.append(string4);
+        stringBuilder.append(string5);
+        stringBuilder.append(string6);
+        return stringBuilder.toString();
+    }
+
+}
+//
