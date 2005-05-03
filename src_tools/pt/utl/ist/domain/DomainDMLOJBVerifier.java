c17e32883e44417c9ec5b2876ac7ec297a538bf1
Luis Cruz <luis.cruz@ist.utl.pt>
2005-05-03 00:31:54 +0000

    Finished domain DML/OJB mapping verifier.

--- src_tools/pt/utl/ist/domain/DomainDMLOJBVerifier.java
+++ src_tools/pt/utl/ist/domain/DomainDMLOJBVerifier.java
@@ -1,42 +1,52 @@
 package pt.utl.ist.domain;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
 
-import net.sourceforge.fenixedu.domain.IDomainObject;
+import net.sourceforge.fenixedu.domain.DomainObject;
+import net.sourceforge.fenixedu.domain.DomainObject_Base;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.ojb.broker.metadata.ClassDescriptor;
+import org.apache.ojb.broker.metadata.CollectionDescriptor;
 import org.apache.ojb.broker.metadata.DescriptorRepository;
+import org.apache.ojb.broker.metadata.FieldDescriptor;
 import org.apache.ojb.broker.metadata.MetadataManager;
+import org.apache.ojb.broker.metadata.ObjectReferenceDescriptor;
 
 public class DomainDMLOJBVerifier {
 
     private static final Logger logger = Logger.getLogger(DomainDMLOJBVerifier.class);
 
-    public static void main(final String[] args) {
+    private static final Set<Class> unmappedClasses = new TreeSet<Class>(new Comparator() {
+        public int compare(Object o1, Object o2) {
+            final Class class1 = (Class) o1;
+            final Class class2 = (Class) o2;
+            return class1.getName().compareTo(class2.getName());
+        }
+    });
+
+    private static final Set<String> unmappedAttributes = new TreeSet<String>();
+
+    private static final Set<String> unmappedObjectReferenceAttributes = new TreeSet<String>();
+
+    private static final Set<String> unmappedCollectionReferenceAttributes = new TreeSet<String>();
+
+    public static void main(final String[] args) throws SecurityException, NoSuchMethodException {
         final Map ojbMetadata = getDescriptorTable();
         verifyDMLMappingFromOJBMetadata(ojbMetadata);
 
-        logger.warn("Verification complete.");
+        logVerificationResults();
+        logger.info("\nVerification complete.");
         System.exit(0);
-    }
-
-    protected static void verifyDMLMappingFromOJBMetadata(final Map ojbMetadata) {
-        for (final Iterator iterator = ojbMetadata.values().iterator(); iterator.hasNext(); ) {
-            final ClassDescriptor classDescriptor = (ClassDescriptor) iterator.next();
-            final Class mappedClass = classDescriptor.getClassOfObject();
-
-            if (isDomainClass(mappedClass)) {
-                
-            } else {
-                System.out.println("Non-domain class: " + mappedClass.getName() + " included in OJB mapping.");
-            }
-        }
-    }
-
-    protected static boolean isDomainClass(final Class mappedClass) {
-        return false;
     }
 
     protected static Map getDescriptorTable()
@@ -46,4 +56,128 @@ public class DomainDMLOJBVerifier {
         return descriptorRepository.getDescriptorTable();
     }
 
+    protected static void verifyDMLMappingFromOJBMetadata(final Map ojbMetadata) throws SecurityException, NoSuchMethodException {
+        for (final Iterator iterator = ojbMetadata.values().iterator(); iterator.hasNext(); ) {
+            final ClassDescriptor classDescriptor = (ClassDescriptor) iterator.next();
+            final Class mappedClass = classDescriptor.getClassOfObject();
+
+            if (isDomainObject(mappedClass)) {
+                final Class baseClass = mappedClass.getSuperclass();
+                if (baseClass.getName().endsWith("_Base")) {
+                    verify(classDescriptor, baseClass);
+                } else {
+                    unmappedClasses.add(mappedClass);
+                }
+            } else {
+                // These can be ignored, they are classes used by OJB.
+                logger.debug("Ignoreing non-domain class " + mappedClass.getName());
+            }
+        }
+    }
+
+    protected static boolean isDomainObject(final Class mappedClass) {
+        return mappedClass.getName().equals(DomainObject.class.getName())
+            || (!mappedClass.getName().equals(Object.class.getName())
+                    && isDomainObject(mappedClass.getSuperclass()));
+    }
+
+    protected static void verify(final ClassDescriptor classDescriptor, final Class baseClass) throws SecurityException, NoSuchMethodException {
+        final FieldDescriptor[] fieldDescriptors = classDescriptor.getFieldDescriptions();
+        if (fieldDescriptors != null) {
+            for (int i = 0; i < fieldDescriptors.length; i++) {
+                final String attributeName = fieldDescriptors[i].getAttributeName();
+                if (!hasGetterAndSetter(baseClass, attributeName, fieldDescriptors[i].getPersistentField().getType())) {
+                    unmappedAttributes.add(classDescriptor.getClassNameOfObject() + "." + attributeName);
+                }
+            }
+        }
+
+        final Vector referenceDescriptors = classDescriptor.getObjectReferenceDescriptors();
+        for (final Iterator iterator = referenceDescriptors.iterator(); iterator.hasNext(); ) {
+            final ObjectReferenceDescriptor referenceDescriptor = (ObjectReferenceDescriptor) iterator.next();
+            if (!hasGetterAndSetter(baseClass, referenceDescriptor.getAttributeName(), referenceDescriptor.getItemClass())) {
+                unmappedObjectReferenceAttributes.add(classDescriptor.getClassNameOfObject() + "." + referenceDescriptor.getAttributeName());
+            }
+        }
+
+        final Vector collectionDescriptors = classDescriptor.getCollectionDescriptors();
+        for (final Iterator iterator = collectionDescriptors.iterator(); iterator.hasNext(); ) {
+            final CollectionDescriptor collectionDescriptor = (CollectionDescriptor) iterator.next();
+            if (!hasGetterAndSetter(baseClass, collectionDescriptor.getAttributeName(), collectionDescriptor.getItemClass())) {
+                unmappedObjectReferenceAttributes.add(classDescriptor.getClassNameOfObject() + "." + collectionDescriptor.getAttributeName());
+            }
+        }
+    }
+
+    protected static boolean hasGetterAndSetter(final Class baseClass, final String attributeName, final Class attributeClass) throws SecurityException, NoSuchMethodException {
+        final String capitalizedAttributeName = StringUtils.capitalise(attributeName);
+        final String getMethodName = "get" + capitalizedAttributeName;
+        final String setMethodName = "set" + capitalizedAttributeName;
+
+        Method getter;
+        Method setter;
+        try {
+             getter = baseClass.getDeclaredMethod(getMethodName, null);
+        } catch (Exception ex) {
+            getter = null;
+        }
+        try {
+            setter = baseClass.getDeclaredMethod(setMethodName, new Class[] { attributeClass });
+       } catch (Exception ex) {
+           setter = null;
+       }
+       if (setter == null
+               && !baseClass.getName().equals(DomainObject_Base.class.getName())) {
+           try {
+               setter = baseClass.getDeclaredMethod(setMethodName, new Class[] { attributeClass.getSuperclass().getInterfaces()[0] } );
+           } catch (Exception ex) {
+               setter = null;
+           }
+       }
+
+        return (getter != null && setter != null)
+                || (!baseClass.getName().equals(DomainObject_Base.class.getName()) && hasGetterAndSetter(
+                        baseClass.getSuperclass(), attributeName, attributeClass));
+    }
+
+    protected static void logVerificationResults() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("\nNo DML mapping found for ");
+        stringBuilder.append(unmappedClasses.size());
+        stringBuilder.append(" classes: \n");
+        for (final Iterator iterator = unmappedClasses.iterator(); iterator.hasNext(); ) {
+            final Class clazz = (Class) iterator.next();
+            stringBuilder.append(clazz.getName());
+            stringBuilder.append("\n");
+        }
+
+        stringBuilder.append("\nFound ");
+        stringBuilder.append(unmappedAttributes.size());
+        stringBuilder.append(" attributes not mapped in DML mapping: \n");
+        logCollectionOfStrings(stringBuilder, unmappedAttributes);
+        stringBuilder.append("\n");
+
+        stringBuilder.append("\nFound ");
+        stringBuilder.append(unmappedObjectReferenceAttributes.size());
+        stringBuilder.append(" object reference attributes not mapped in DML mapping: \n");
+        logCollectionOfStrings(stringBuilder, unmappedObjectReferenceAttributes);
+        stringBuilder.append("\n");
+
+        stringBuilder.append("\nFound ");
+        stringBuilder.append(unmappedCollectionReferenceAttributes.size());
+        stringBuilder.append(" colletion reference attributes not mapped in DML mapping: \n");
+        logCollectionOfStrings(stringBuilder, unmappedCollectionReferenceAttributes);
+        stringBuilder.append("\n");
+
+        logger.warn(stringBuilder.toString());
+    }
+
+    protected static void logCollectionOfStrings(final StringBuilder stringBuilder, final Collection collection) {
+        for (final Iterator iterator = collection.iterator(); iterator.hasNext(); ) {
+            final String attribute = (String) iterator.next();
+            stringBuilder.append(attribute);
+            stringBuilder.append("\n");
+        }
+    }
 }
