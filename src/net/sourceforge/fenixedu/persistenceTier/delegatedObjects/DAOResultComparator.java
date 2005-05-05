1562038bb93784ae432a12736411511126d074cf
Luis Cruz <luis.cruz@ist.utl.pt>
2005-05-05 14:42:37 +0000

    Added comparison for lists and objects.

--- src/net/sourceforge/fenixedu/persistenceTier/delegatedObjects/DAOResultComparator.java
+++ src/net/sourceforge/fenixedu/persistenceTier/delegatedObjects/DAOResultComparator.java
@@ -12,12 +12,14 @@ public class DAOResultComparator {
         return result1 == result2;
     }
 
-    public static boolean compare(final Collection result1, final Collection result2) {
-        return false;
+    public static boolean compare(final Object result1, final Object result2) {
+        return (result1 == result2) || (result1 != null && result1.equals(result2));
     }
 
-    public static boolean compare(final Object result1, final Object result2) {
-        return false;
+    public static boolean compare(final Collection result1, final Collection result2) {
+        return (result1 == result2)
+                || (result1 != null && result2 != null && result1.size() == result2.size()
+                        && result1.containsAll(result2) && result2.containsAll(result1));
     }
 
 }
