c4778f90186e3116dabc61ae4c71cbd0dc58a3f0
Luis Cruz <luis.cruz@ist.utl.pt>
2005-05-06 18:04:32 +0000

    Added checking of slide database as well.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/CheckIsAliveService.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/CheckIsAliveService.java
@@ -1,6 +1,9 @@
 package net.sourceforge.fenixedu.applicationTier.Servico;
 
 import net.sourceforge.fenixedu.domain.IExecutionYear;
+import net.sourceforge.fenixedu.fileSuport.FileSuport;
+import net.sourceforge.fenixedu.fileSuport.FileSuportObject;
+import net.sourceforge.fenixedu.fileSuport.IFileSuport;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionYear;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
@@ -9,20 +12,81 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class CheckIsAliveService implements IService {
 
-	public Boolean run() throws ExcepcaoPersistencia {
-		checkFenixDatabaseOps();
-		return new Boolean(true);
-	}
+    private static final String filename = "checkIsAlive";
+    private static final String filepath = "/" + filename;
 
-	private void checkFenixDatabaseOps() throws ExcepcaoPersistencia {
-		final ISuportePersistente persistenceSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-		final IPersistentExecutionYear persistentExecutionYear = persistenceSupport.getIPersistentExecutionYear();
+    private static final FileSuportObject fileSuportObject = new FileSuportObject();
 
-		final IExecutionYear executionYear = persistentExecutionYear.readCurrentExecutionYear();
+    static {
+        byte[] fileContents = CheckIsAliveService.class.getName().getBytes();
 
-		if (executionYear.getIdInternal() == null) {
-			throw new RuntimeException("Problems accesing fenix database!");
-		}
-	}
+        fileSuportObject.setFileName(filename);
+        fileSuportObject.setLinkName(filename);
+        fileSuportObject.setContent(fileContents);
+        fileSuportObject.setContentType("txt");
+
+        fileSuportObject.setUri("/");
+        fileSuportObject.setRootUri("/");
+    }
+
+    public Boolean run() throws ExcepcaoPersistencia {
+        checkFenixDatabaseOps();
+        checkSlideDatabaseOps();
+        return new Boolean(true);
+    }
+
+    private void checkFenixDatabaseOps() throws ExcepcaoPersistencia {
+        final ISuportePersistente persistenceSupport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
+        final IPersistentExecutionYear persistentExecutionYear = persistenceSupport
+                .getIPersistentExecutionYear();
+
+        final IExecutionYear executionYear = persistentExecutionYear.readCurrentExecutionYear();
+
+        if (executionYear.getIdInternal() == null) {
+            throw new RuntimeException("Problems accesing fenix database!");
+        }
+    }
+
+    synchronized private void checkSlideDatabaseOps() {
+        final IFileSuport fileSuport = FileSuport.getInstance();
+
+        boolean resultWrite = false;
+        boolean resultRead = false;
+        try {
+            fileSuport.beginTransaction();
+            fileSuport.storeFile(fileSuportObject);
+            fileSuport.commitTransaction();
+
+            fileSuport.beginTransaction();
+            fileSuport.retrieveFile(filepath);
+            fileSuport.commitTransaction();
+
+            resultWrite = true;
+            resultRead = true;
+        } catch (Exception ex) {
+            resultWrite = false;
+            resultRead = false;
+
+            try {
+                fileSuport.abortTransaction();
+            } catch (Exception ex2) {
+                // nothing else to do.
+            }
+        } finally {
+            try {
+                fileSuport.beginTransaction();
+                fileSuport.deleteFile(filepath);
+                fileSuport.commitTransaction();
+            } catch (Exception ex2) {
+                // nothing else can de done.
+            }            
+        }
+
+        if (!(resultWrite && resultRead)) {
+            throw new RuntimeException("Problems accesing slide database!");
+        }
+
+    }
 
 }
