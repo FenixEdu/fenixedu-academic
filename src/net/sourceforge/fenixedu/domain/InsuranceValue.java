5ab3adffbc71b2f4c048b26d702485e68289c2f8
Luís Egídio <luis.egidio@ist.utl.pt>
2005-05-16 12:11:37 +0000

    added missing dml atributes to InsuranceValue

--- src/net/sourceforge/fenixedu/domain/InsuranceValue.java
+++ src/net/sourceforge/fenixedu/domain/InsuranceValue.java
@@ -6,35 +6,18 @@ import java.util.Date;
  * 
  * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
  * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
- *  
+ * 
  */
 public class InsuranceValue extends InsuranceValue_Base {
 
-    private Date endDate;
-
     public InsuranceValue() {
-
     }
 
     public InsuranceValue(IExecutionYear executionYear, Double annualValue, Date endDate) {
         this.setExecutionYear(executionYear);
-        setAnnualValue(annualValue);
-        this.endDate = endDate;
+        this.setAnnualValue(annualValue);
+        this.setEndDate(endDate);
 
     }
 
-    /**
-     * @return Returns the endDate.
-     */
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    /**
-     * @param endDate
-     *            The endDate to set.
-     */
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
 }
