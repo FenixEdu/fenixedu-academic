43a3f3d4ae042855bb572899e9c62a191d9db686
Luís Egídio <luis.egidio@ist.utl.pt>
2005-05-16 16:11:57 +0000

    refined dml mapping for FAQEntry and FAQSection

--- src/net/sourceforge/fenixedu/domain/support/FAQEntry.java
+++ src/net/sourceforge/fenixedu/domain/support/FAQEntry.java
@@ -1,24 +1,9 @@
-/*
- * Created on 2004/08/30
- *
- */
 package net.sourceforge.fenixedu.domain.support;
-
 
 /**
  * @author Luis Cruz
  *  
  */
 public class FAQEntry extends FAQEntry_Base {
-
-    private IFAQSection parentSection = null;
-
-    public IFAQSection getParentSection() {
-        return parentSection;
-    }
-
-    public void setParentSection(IFAQSection parentSection) {
-        this.parentSection = parentSection;
-    }
 
 }
--- src/net/sourceforge/fenixedu/domain/support/FAQSection.java
+++ src/net/sourceforge/fenixedu/domain/support/FAQSection.java
@@ -1,24 +1,9 @@
-/*
- * Created on 2004/08/30
- *
- */
 package net.sourceforge.fenixedu.domain.support;
-
 
 /**
  * @author Luis Cruz
  *  
  */
 public class FAQSection extends FAQSection_Base {
-
-    private IFAQSection parentSection = null;
-
-    public IFAQSection getParentSection() {
-        return parentSection;
-    }
-
-    public void setParentSection(IFAQSection parentSection) {
-        this.parentSection = parentSection;
-    }
 
 }
