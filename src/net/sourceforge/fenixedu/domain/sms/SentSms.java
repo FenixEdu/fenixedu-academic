433b3e36faf26b591706687fd780c96c8e6ac545
Luís Egídio <luis.egidio@ist.utl.pt>
2005-05-16 15:32:17 +0000

    refined dml mapping for SentSms

--- src/net/sourceforge/fenixedu/domain/sms/SentSms.java
+++ src/net/sourceforge/fenixedu/domain/sms/SentSms.java
@@ -4,67 +4,30 @@
  */
 package net.sourceforge.fenixedu.domain.sms;
 
-import java.util.Date;
-
 /**
  * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
  * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
  */
 public class SentSms extends SentSms_Base {
+    protected SmsDeliveryType deliveryType;
 
-	protected Date sendDate;
+    public SentSms() {
+        super();
+    }
 
-	protected Date deliveryDate;
+    /**
+     * @return Returns the deliveryType.
+     */
+    public SmsDeliveryType getDeliveryType() {
+        return deliveryType;
+    }
 
-	protected SmsDeliveryType deliveryType;
-
-	public SentSms() {
-		super();
-	}
-
-	/**
-	 * @return Returns the deliveryDate.
-	 */
-	public Date getDeliveryDate() {
-		return deliveryDate;
-	}
-
-	/**
-	 * @param deliveryDate
-	 *            The deliveryDate to set.
-	 */
-	public void setDeliveryDate(Date deliveryDate) {
-		this.deliveryDate = deliveryDate;
-	}
-
-	/**
-	 * @return Returns the deliveryType.
-	 */
-	public SmsDeliveryType getDeliveryType() {
-		return deliveryType;
-	}
-
-	/**
-	 * @param deliveryType
-	 *            The deliveryType to set.
-	 */
-	public void setDeliveryType(SmsDeliveryType deliveryType) {
-		this.deliveryType = deliveryType;
-	}
-
-	/**
-	 * @return Returns the sendDate.
-	 */
-	public Date getSendDate() {
-		return sendDate;
-	}
-
-	/**
-	 * @param sendDate
-	 *            The sendDate to set.
-	 */
-	public void setSendDate(Date sendDate) {
-		this.sendDate = sendDate;
-	}
+    /**
+     * @param deliveryType
+     *            The deliveryType to set.
+     */
+    public void setDeliveryType(SmsDeliveryType deliveryType) {
+        this.deliveryType = deliveryType;
+    }
 
 }
