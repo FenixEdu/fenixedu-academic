633d53f21f073ff8bc91ee82a839145ca4385a4b
Shezad Anavarali <shezad@ist.utl.pt>
2005-05-31 12:49:04 +0000

    unused methods removed

--- src/net/sourceforge/fenixedu/persistenceTier/IPersistentPaymentPhase.java
+++ src/net/sourceforge/fenixedu/persistenceTier/IPersistentPaymentPhase.java
@@ -4,17 +4,10 @@
  */
 package net.sourceforge.fenixedu.persistenceTier;
 
-import java.util.List;
-
 /**
  * @author Tânia Pousão
- *  
+ * 
  */
 public interface IPersistentPaymentPhase extends IPersistentObject {
-	public void deletePaymentPhasesOfThisGratuity(Integer gratuityValuesID)
-			throws ExcepcaoPersistencia;
 
-	/* delete - not used anywhere */
-	public List readByGratuityValues(Integer gratuityValuesID)
-			throws ExcepcaoPersistencia;
 }
