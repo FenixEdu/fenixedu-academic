0aa89f6cb758df4b63d17789b914615653430b86
Shezad Anavarali <shezad@ist.utl.pt>
2005-05-31 12:48:21 +0000

    Initial Version

---
+++ src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/PaymentPhaseVO.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 6/Jan/2004
+ *  
+ */
+package net.sourceforge.fenixedu.persistenceTier.versionedObjects.dao;
+
+import net.sourceforge.fenixedu.persistenceTier.IPersistentPaymentPhase;
+import net.sourceforge.fenixedu.persistenceTier.versionedObjects.VersionedObjectsBase;
+
+/**
+ * @author Tânia Pousão
+ */
+public class PaymentPhaseVO extends VersionedObjectsBase implements IPersistentPaymentPhase {
+
+}
