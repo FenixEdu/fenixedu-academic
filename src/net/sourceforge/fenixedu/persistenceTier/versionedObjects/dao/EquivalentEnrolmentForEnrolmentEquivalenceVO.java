72d2238e9d37ea64b8bf70850550bfb14be90478
Luís Egídio <luis.egidio@ist.utl.pt>
2005-06-01 16:48:09 +0000

    organized imports

--- src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/EquivalentEnrolmentForEnrolmentEquivalenceVO.java
+++ src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/EquivalentEnrolmentForEnrolmentEquivalenceVO.java
@@ -2,7 +2,6 @@ package net.sourceforge.fenixedu.persistenceTier.versionedObjects.dao;
 
 import java.util.List;
 
-import net.sourceforge.fenixedu.commons.CollectionUtils;
 import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.IEnrolment;
 import net.sourceforge.fenixedu.domain.IEquivalentEnrolmentForEnrolmentEquivalence;
@@ -10,6 +9,7 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentEquivalentEnrolmentForEnrolmentEquivalence;
 import net.sourceforge.fenixedu.persistenceTier.versionedObjects.VersionedObjectsBase;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 
 public class EquivalentEnrolmentForEnrolmentEquivalenceVO extends
