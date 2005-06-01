36fbe0493ff03943fd9dd40319b93bdc64f462f3
Luis Cruz <luis.cruz@ist.utl.pt>
2005-06-01 15:10:32 +0000

    Simplified code.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionPeriods.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionPeriods.java
@@ -5,10 +5,10 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.commons;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
+import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionPeriod;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.IExecutionPeriod;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
@@ -24,30 +24,22 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class ReadExecutionPeriods implements IService {
 
-    public List run() throws FenixServiceException {
+    public List run() throws ExcepcaoPersistencia {
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
 
-        List result = new ArrayList();
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
-
-            List executionPeriods = (List)executionPeriodDAO.readAll(ExecutionPeriod.class);
-
-            if (executionPeriods != null) {
-                for (int i = 0; i < executionPeriods.size(); i++) {
-                    result.add(Cloner.get((IExecutionPeriod) executionPeriods.get(i)));
-                }
-            }
-        } catch (ExcepcaoPersistencia ex) {
-            throw new FenixServiceException(ex);
+        final Collection<IExecutionPeriod> executionPeriods = executionPeriodDAO.readAll(ExecutionPeriod.class);
+        final List result = new ArrayList(executionPeriods.size());
+        for (final IExecutionPeriod executionPeriod : executionPeriods) {
+            result.add(InfoExecutionPeriod.newInfoFromDomain(executionPeriod));
         }
 
         return result;
     }
 
-    public List run(DegreeType degreeType) throws FenixServiceException {
+    public List run(DegreeType degreeType) throws ExcepcaoPersistencia {
         // the degree type is used in the pos-filtration
         return this.run();
     }
-    
+
 }
