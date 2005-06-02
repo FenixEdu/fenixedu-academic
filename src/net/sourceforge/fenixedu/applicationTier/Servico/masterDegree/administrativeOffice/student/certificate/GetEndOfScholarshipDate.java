2484a8f0edd7f19afc26ef94dcd47235ce5c00bf
Shezad Anavarali <shezad@ist.utl.pt>
2005-06-02 14:02:33 +0000

    Service now receives StudentCurricularPlanID

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/student/certificate/GetEndOfScholarshipDate.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/student/certificate/GetEndOfScholarshipDate.java
@@ -5,10 +5,11 @@ import java.util.Date;
 import net.sourceforge.fenixedu.applicationTier.strategy.degreeCurricularPlan.DegreeCurricularPlanStrategyFactory;
 import net.sourceforge.fenixedu.applicationTier.strategy.degreeCurricularPlan.IDegreeCurricularPlanStrategyFactory;
 import net.sourceforge.fenixedu.applicationTier.strategy.degreeCurricularPlan.strategys.IMasterDegreeCurricularPlanStrategy;
-import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
 import net.sourceforge.fenixedu.domain.IStudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
+import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
+import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
@@ -16,10 +17,13 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class GetEndOfScholarshipDate implements IService {
 
-    public Date run(InfoStudentCurricularPlan infoStudentCurricularPlan) throws ExcepcaoPersistencia {
+    public Date run(Integer studentCurricularPlanID) throws ExcepcaoPersistencia {
 
-        IStudentCurricularPlan studentCurricularPlan = Cloner
-                .copyInfoStudentCurricularPlan2IStudentCurricularPlan(infoStudentCurricularPlan);
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+                
+        IStudentCurricularPlan studentCurricularPlan = (IStudentCurricularPlan) sp
+                .getIStudentCurricularPlanPersistente().readByOID(StudentCurricularPlan.class,
+                        studentCurricularPlanID);
 
         IDegreeCurricularPlanStrategyFactory degreeCurricularPlanStrategyFactory = DegreeCurricularPlanStrategyFactory
                 .getInstance();
