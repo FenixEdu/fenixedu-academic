61a6202becd028daa7ea1ed8011177b0f80087df
Luís Egídio <luis.egidio@ist.utl.pt>
2005-06-02 16:05:38 +0000

    added attribute
        State state;
        ReimbursementGuideState reimbursementGuideState;
    
    to ReimbursementGuideSituation dml mapping

--- src/net/sourceforge/fenixedu/domain/reimbursementGuide/ReimbursementGuideSituation.java
+++ src/net/sourceforge/fenixedu/domain/reimbursementGuide/ReimbursementGuideSituation.java
@@ -7,102 +7,55 @@ package net.sourceforge.fenixedu.domain.reimbursementGuide;
 
 import java.util.Calendar;
 
-import net.sourceforge.fenixedu.domain.gratuity.ReimbursementGuideState;
-import net.sourceforge.fenixedu.util.State;
-
 /**
  * @author <a href="mailto:joao.mota@ist.utl.pt">João Mota </a> 13/Nov/2003
  * 
  */
-public class ReimbursementGuideSituation extends
-		ReimbursementGuideSituation_Base {
+public class ReimbursementGuideSituation extends ReimbursementGuideSituation_Base {
+    protected Calendar modificationDate;
+    protected Calendar officialDate;
 
-	protected State state;
-	
-	protected Calendar modificationDate;
+    /**
+     * @return
+     */
+    public Calendar getModificationDate() {
+        return modificationDate;
+    }
 
-	protected Calendar officialDate;
+    /**
+     * @param modificationDate
+     */
+    public void setModificationDate(Calendar modificationDate) {
+        this.modificationDate = modificationDate;
+    }
 
-	protected ReimbursementGuideState reimbursementGuideState;
+    /**
+     * @return Returns the officialDate.
+     */
+    public Calendar getOfficialDate() {
+        return officialDate;
+    }
 
-	/**
-	 * 
-	 */
-	public ReimbursementGuideSituation() {
-	}
+    /**
+     * @param officialDate
+     *            The officialDate to set.
+     */
+    public void setOfficialDate(Calendar officialDate) {
+        this.officialDate = officialDate;
+    }
 
-	/**
-	 * @return
-	 */
-	public Calendar getModificationDate() {
-		return modificationDate;
-	}
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (obj instanceof IReimbursementGuideSituation) {
+            IReimbursementGuideSituation reimbursementGuideSituation = (IReimbursementGuideSituation) obj;
 
-	/**
-	 * @param modificationDate
-	 */
-	public void setModificationDate(Calendar modificationDate) {
-		this.modificationDate = modificationDate;
-	}
+            if ((this.getIdInternal() == null && reimbursementGuideSituation.getIdInternal() == null)
+                    || (this.getIdInternal().equals(reimbursementGuideSituation.getIdInternal()))) {
+                result = true;
+            }
+        }
 
-	/**
-	 * @return
-	 */
-	public ReimbursementGuideState getReimbursementGuideState() {
-		return reimbursementGuideState;
-	}
-
-	/**
-	 * @param reimbursementGuideState
-	 */
-	public void setReimbursementGuideState(
-			ReimbursementGuideState reimbursementGuideState) {
-		this.reimbursementGuideState = reimbursementGuideState;
-	}
-
-	/**
-	 * @return
-	 */
-	public State getState() {
-		return state;
-	}
-
-	/**
-	 * @param state
-	 */
-	public void setState(State state) {
-		this.state = state;
-	}
-
-	/**
-	 * @return Returns the officialDate.
-	 */
-	public Calendar getOfficialDate() {
-		return officialDate;
-	}
-
-	/**
-	 * @param officialDate
-	 *            The officialDate to set.
-	 */
-	public void setOfficialDate(Calendar officialDate) {
-		this.officialDate = officialDate;
-	}
-
-	public boolean equals(Object obj) {
-		boolean result = false;
-		if (obj instanceof IReimbursementGuideSituation) {
-			IReimbursementGuideSituation reimbursementGuideSituation = (IReimbursementGuideSituation) obj;
-
-			if ((this.getIdInternal() == null && reimbursementGuideSituation
-					.getIdInternal() == null)
-					|| (this.getIdInternal().equals(reimbursementGuideSituation
-							.getIdInternal()))) {
-				result = true;
-			}
-		}
-
-		return result;
-	}
+        return result;
+    }
 
 }
