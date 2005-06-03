3299f0343372561d524353428a5aae670121fa18
Luís Egídio <luis.egidio@ist.utl.pt>
2005-06-03 13:52:29 +0000

    added valueType mapping to dml

--- src/net/sourceforge/fenixedu/domain/teacher/OldPublication.java
+++ src/net/sourceforge/fenixedu/domain/teacher/OldPublication.java
@@ -4,29 +4,11 @@
  */
 package net.sourceforge.fenixedu.domain.teacher;
 
-import net.sourceforge.fenixedu.util.OldPublicationType;
-
 /**
  * @author Leonor Almeida
  * @author Sergio Montelobo
  * 
  */
 public class OldPublication extends OldPublication_Base {
-    private OldPublicationType oldPublicationType;
-
-    /**
-     * @return Returns the oldPublicationType.
-     */
-    public OldPublicationType getOldPublicationType() {
-        return oldPublicationType;
-    }
-
-    /**
-     * @param oldPublicationType
-     *            The oldPublicationType to set.
-     */
-    public void setOldPublicationType(OldPublicationType oldPublicationType) {
-        this.oldPublicationType = oldPublicationType;
-    }
 
 }
--- src/net/sourceforge/fenixedu/domain/teacher/Orientation.java
+++ src/net/sourceforge/fenixedu/domain/teacher/Orientation.java
@@ -4,29 +4,11 @@
  */
 package net.sourceforge.fenixedu.domain.teacher;
 
-import net.sourceforge.fenixedu.util.OrientationType;
-
 /**
  * @author Leonor Almeida
  * @author Sergio Montelobo
  * 
  */
 public class Orientation extends Orientation_Base {
-    private OrientationType orientationType;
-
-    /**
-     * @return Returns the orientationType.
-     */
-    public OrientationType getOrientationType() {
-        return orientationType;
-    }
-
-    /**
-     * @param orientationType
-     *            The orientationType to set.
-     */
-    public void setOrientationType(OrientationType orientationType) {
-        this.orientationType = orientationType;
-    }
 
 }
