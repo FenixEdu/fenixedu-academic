574cf5d400f5db8b31e023dd213150432aac7beb
Luís Egídio <luis.egidio@ist.utl.pt>
2005-06-03 12:17:17 +0000

    added valueType mapping to dml

--- src/net/sourceforge/fenixedu/domain/student/Delegate.java
+++ src/net/sourceforge/fenixedu/domain/student/Delegate.java
@@ -4,29 +4,11 @@
  */
 package net.sourceforge.fenixedu.domain.student;
 
-import net.sourceforge.fenixedu.util.DelegateYearType;
-
 /**
  * @author <a href="mailto:lesa@mega.ist.utl.pt">Leonor Almeida </a>
  * @author <a href="mailto:shmc@mega.ist.utl.pt">Sergio Montelobo </a>
  * 
  */
 public class Delegate extends Delegate_Base {
-    private DelegateYearType yearType;
-
-    /**
-     * @return Returns the type.
-     */
-    public DelegateYearType getYearType() {
-        return yearType;
-    }
-
-    /**
-     * @param type
-     *            The type to set.
-     */
-    public void setYearType(DelegateYearType yearType) {
-        this.yearType = yearType;
-    }
 
 }
