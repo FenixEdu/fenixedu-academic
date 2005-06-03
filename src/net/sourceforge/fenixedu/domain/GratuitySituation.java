aeb78cfc536a956e1a0d201927a28f5c1656d657
Luís Egídio <luis.egidio@ist.utl.pt>
2005-06-03 13:15:34 +0000

    added valueType mapping to dml

--- src/net/sourceforge/fenixedu/domain/GratuitySituation.java
+++ src/net/sourceforge/fenixedu/domain/GratuitySituation.java
@@ -4,29 +4,11 @@
  */
 package net.sourceforge.fenixedu.domain;
 
-import net.sourceforge.fenixedu.domain.gratuity.ExemptionGratuityType;
-
 /**
  * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
  * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
  * @author Tânia Pousão
  */
 public class GratuitySituation extends GratuitySituation_Base {
-	private ExemptionGratuityType exemptionType;
-        
-	/**
-	 * @return Returns the exemptionType.
-	 */
-	public ExemptionGratuityType getExemptionType() {
-		return exemptionType;
-	}
-
-	/**
-	 * @param exemptionType
-	 *            The exemptionType to set.
-	 */
-	public void setExemptionType(ExemptionGratuityType exemptionType) {
-		this.exemptionType = exemptionType;
-	}
 
 }
--- src/net/sourceforge/fenixedu/domain/GroupPropertiesExecutionCourse.java
+++ src/net/sourceforge/fenixedu/domain/GroupPropertiesExecutionCourse.java
@@ -8,80 +8,65 @@ import net.sourceforge.fenixedu.util.ProposalState;
 /**
  * @author joaosa & rmalo
  */
- 
+
 public class GroupPropertiesExecutionCourse extends GroupPropertiesExecutionCourse_Base {
-		
-	
-	private ProposalState proposalState;
 
-	
-	/** 
-	 * Construtor
-	 */
-	public GroupPropertiesExecutionCourse() {}
-	
-	/** 
- 	* Construtor
- 	*/
-	public GroupPropertiesExecutionCourse(Integer idInternal){
-			setIdInternal(idInternal);
-		}
-	
-	/** 
-	 * Construtor
-	 */
-	public GroupPropertiesExecutionCourse(IGroupProperties groupProperties,IExecutionCourse executionCourse) {
-			super.setGroupProperties(groupProperties);
-			super.setExecutionCourse(executionCourse);
-	}
-	
-	/** 
-	 * Construtor
-	 */
-	public GroupPropertiesExecutionCourse(IGroupProperties groupProperties,IExecutionCourse executionCourse,ProposalState proposalState) {
-	    	super.setGroupProperties(groupProperties);
-	    	super.setExecutionCourse(executionCourse);
-			this.proposalState = proposalState;
-	}
-	
-	
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	public boolean equals(Object arg0) {
-		boolean result = false;
-		if (arg0 instanceof IGroupPropertiesExecutionCourse) {
-			result =(getGroupProperties().equals(((IGroupPropertiesExecutionCourse) arg0).getGroupProperties()))&&
-					(getExecutionCourse().equals(((IGroupPropertiesExecutionCourse) arg0).getExecutionCourse()));
-		} 
-		return result;		
-	}
+    /**
+     * Construtor
+     */
+    public GroupPropertiesExecutionCourse() {
+    }
 
-	/**
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString() {
-		String result = "[GROUPPROPERTIESEXECUTIONCOURSE";
-		result += ", groupProperties=" + getGroupProperties();
-		result += ", executionCourse=" + getExecutionCourse();
-		result += ", proposalState=" + getProposalState();
-		result += "]";
-		return result;
-	}
-	
-	/**
-	* @return ProposalState
-	**/
-	public ProposalState getProposalState() {
-		return proposalState;
-	}
+    /**
+     * Construtor
+     */
+    public GroupPropertiesExecutionCourse(Integer idInternal) {
+        this.setIdInternal(idInternal);
+    }
 
-	/**
-	* Sets the proposalState.
-	* @param proposalState The proposalState to set
-	*/
-	public void setProposalState(ProposalState proposalState) {
-		this.proposalState=proposalState;
-	}
+    /**
+     * Construtor
+     */
+    public GroupPropertiesExecutionCourse(IGroupProperties groupProperties,
+            IExecutionCourse executionCourse) {
+        super.setGroupProperties(groupProperties);
+        super.setExecutionCourse(executionCourse);
+    }
+
+    /**
+     * Construtor
+     */
+    public GroupPropertiesExecutionCourse(IGroupProperties groupProperties,
+            IExecutionCourse executionCourse, ProposalState proposalState) {
+        super.setGroupProperties(groupProperties);
+        super.setExecutionCourse(executionCourse);
+        this.setProposalState(proposalState);
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        String result = "[GROUPPROPERTIESEXECUTIONCOURSE";
+        result += ", groupProperties=" + getGroupProperties();
+        result += ", executionCourse=" + getExecutionCourse();
+        result += ", proposalState=" + getProposalState();
+        result += "]";
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object arg0) {
+        boolean result = false;
+        if (arg0 instanceof IGroupPropertiesExecutionCourse) {
+            result = (getGroupProperties().equals(((IGroupPropertiesExecutionCourse) arg0)
+                    .getGroupProperties()))
+                    && (getExecutionCourse().equals(((IGroupPropertiesExecutionCourse) arg0)
+                            .getExecutionCourse()));
+        }
+        return result;
+    }
 
 }
