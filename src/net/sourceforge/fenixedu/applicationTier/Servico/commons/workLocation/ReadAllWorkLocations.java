828e3fbca255c4bc595aab3dab5f756ceec68b81
Shezad Anavarali <shezad@ist.utl.pt>
2005-06-15 12:38:31 +0000

    Bug Fix and code cleaning

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/workLocation/ReadAllWorkLocations.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/workLocation/ReadAllWorkLocations.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
+import net.sourceforge.fenixedu.dataTransferObject.InfoWorkLocation;
+import net.sourceforge.fenixedu.domain.IWorkLocation;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
@@ -16,32 +17,14 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class ReadAllWorkLocations implements IService {
 
-    private static ReadAllWorkLocations service = new ReadAllWorkLocations();
-
-    /**
-     * The singleton access method of this class.
-     */
-    public static ReadAllWorkLocations getService() {
-        return service;
-    }
-
-    /**
-     * The actor of this class.
-     */
-    private ReadAllWorkLocations() {
-    }
-
-    public Object run() throws FenixServiceException {
+    public Object run() throws FenixServiceException, ExcepcaoPersistencia {
         List infoWorkLocations = new ArrayList();
 
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            List workLocations = sp.getIPersistentWorkLocation().readAll();
-            infoWorkLocations = Cloner.copyListIWorkLocation2ListInfoWorkLocation(workLocations);
-        } catch (ExcepcaoPersistencia ex) {
-            FenixServiceException newEx = new FenixServiceException("Persistence layer error");
-            newEx.fillInStackTrace();
-            throw newEx;
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        List<IWorkLocation> workLocations = sp.getIPersistentWorkLocation().readAll();
+
+        for (IWorkLocation workLocation : workLocations) {
+            infoWorkLocations.add(InfoWorkLocation.newInfoFromDomain(workLocation));
         }
 
         return infoWorkLocations;
