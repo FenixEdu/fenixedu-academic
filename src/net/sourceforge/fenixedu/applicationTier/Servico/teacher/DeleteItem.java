1d1a2d75a8873be8c85668da574f4fc6edd16f22
Luis Cruz <luis.cruz@ist.utl.pt>
2005-06-16 15:28:17 +0000

    Bug fix and bettered code. The bug was due to a NPE that occured because of a set(null). This bug was introduced when the delete service for the Item class was rewritten. We can only unreference the objects, after we take care of everything else.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/DeleteItem.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/DeleteItem.java
@@ -1,12 +1,11 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.teacher;
 
-import java.util.Iterator;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.IServico;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.notAuthorizedServiceDeleteException;
 import net.sourceforge.fenixedu.domain.IItem;
+import net.sourceforge.fenixedu.domain.ISection;
 import net.sourceforge.fenixedu.domain.Item;
 import net.sourceforge.fenixedu.fileSuport.FileSuport;
 import net.sourceforge.fenixedu.fileSuport.IFileSuport;
@@ -14,70 +13,50 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentItem;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author Fernanda Quitério
- *  
+ * 
  */
-public class DeleteItem implements IServico {
-    private static DeleteItem service = new DeleteItem();
+public class DeleteItem implements IService {
 
-    public static DeleteItem getService() {
-        return service;
-    }
+    public Boolean run(final Integer infoExecutionCourseCode, final Integer itemCode)
+            throws FenixServiceException, ExcepcaoPersistencia {
+        final ISuportePersistente persistentSuport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
+        final IPersistentItem persistentItem = persistentSuport.getIPersistentItem();
+        final IItem deletedItem = (IItem) persistentItem.readByOID(Item.class, itemCode);
+        if (deletedItem == null) {
+            return new Boolean(true);
+        }
 
-    private DeleteItem() {
-    }
+        IFileSuport fileSuport = FileSuport.getInstance();
+        long size = fileSuport.getDirectorySize(deletedItem.getSlideName());
+        if (size > 0) {
+            throw new notAuthorizedServiceDeleteException();
+        }
 
-    public final String getNome() {
-        return "DeleteItem";
-    }
+        final int orderOfDeletedItem = deletedItem.getItemOrder().intValue();
+        if ((deletedItem.getSection() != null)
+                && (deletedItem.getSection().getAssociatedItems() != null)) {
+            final ISection section = deletedItem.getSection();
+            final List<IItem> associatedItems = section.getAssociatedItems();
 
-    public Boolean run(Integer infoExecutionCourseCode, Integer itemCode) throws FenixServiceException {
-        try {
-
-            ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentItem persistentItem = persistentSuport.getIPersistentItem();
-            IItem deletedItem = (IItem) persistentItem.readByOID(Item.class, itemCode);
-            if (deletedItem == null) {
-                return new Boolean(true);
-            }
-            IFileSuport fileSuport = FileSuport.getInstance();
-            long size = 1;
-
-            size = fileSuport.getDirectorySize(deletedItem.getSlideName());
-
-            if (size > 0) {
-                throw new notAuthorizedServiceDeleteException();
-            }
-            Integer orderOfDeletedItem = deletedItem.getItemOrder();
-            
-            if((deletedItem.getSection() != null) && (deletedItem.getSection().getAssociatedItems() != null))
-                deletedItem.getSection().getAssociatedItems().remove(deletedItem);
-            
+            associatedItems.remove(deletedItem);
             deletedItem.setSection(null);
-           
-            persistentItem.deleteByOID(Item.class, deletedItem.getIdInternal());
 
-            persistentSuport.confirmarTransaccao();
-            persistentSuport.iniciarTransaccao();
-            List itemsList = null;
-            itemsList = persistentItem.readAllItemsBySection(deletedItem.getSection().getIdInternal(),
-                    deletedItem.getSection().getSite().getExecutionCourse().getSigla(),
-                    deletedItem.getSection().getSite().getExecutionCourse().getExecutionPeriod().getExecutionYear().getYear(),
-                    deletedItem.getSection().getSite().getExecutionCourse().getExecutionPeriod().getName());
-            Iterator iterItems = itemsList.iterator();
-            while (iterItems.hasNext()) {
-                IItem item = (IItem) iterItems.next();
-                Integer itemOrder = item.getItemOrder();
-                if (itemOrder.intValue() > orderOfDeletedItem.intValue()) {
-                    persistentItem.simpleLockWrite(item);
-                    item.setItemOrder(new Integer(itemOrder.intValue() - 1));
+            for (final IItem associatedItem : associatedItems) {
+                final int associatedItemOrder = 0;
+                if (associatedItemOrder > orderOfDeletedItem) {
+                    persistentItem.simpleLockWrite(associatedItem);
+                    associatedItem.setItemOrder(new Integer(associatedItemOrder - 1));
                 }
             }
-            return new Boolean(true);
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
         }
+
+        persistentItem.deleteByOID(Item.class, deletedItem.getIdInternal());
+
+        return new Boolean(true);
     }
 }
