9fbab864f70028ad527eb5bf1896c85d1966de6b
Shezad Anavarali <shezad@ist.utl.pt>
2005-06-22 12:38:46 +0000

    action to show the master degree candidate's study plan

---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/masterDegree/candidate/ShowStudyPlanForCandidateAction.java
@@ -0,0 +1,96 @@
+package net.sourceforge.fenixedu.presentationTier.Action.masterDegree.candidate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.dataTransferObject.InfoMasterDegreeCandidate;
+import net.sourceforge.fenixedu.framework.factory.ServiceManagerServiceFactory;
+import net.sourceforge.fenixedu.presentationTier.Action.base.FenixAction;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionConstants;
+
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class ShowStudyPlanForCandidateAction extends FenixAction {
+
+    /**
+     * 
+     * @author Ricardo Clerigo & Telmo Nabais
+     */
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        HttpSession session = request.getSession(false);
+        IUserView userView = (IUserView) session.getAttribute(SessionConstants.U_VIEW);
+        ActionErrors actionErrors = new ActionErrors();
+        ArrayList candidateStudyPlan = null;
+
+        InfoMasterDegreeCandidate infoMasterDegreeCandidate = getMasterDegreeCandidate(userView, session);
+
+        if (infoMasterDegreeCandidate != null) {
+            candidateStudyPlan = getCandidateStudyPlanByCandidateID(infoMasterDegreeCandidate
+                    .getIdInternal(), userView);
+
+            request.setAttribute("masterDegreeCandidate", infoMasterDegreeCandidate);
+            request.setAttribute("candidateStudyPlan", candidateStudyPlan);
+        }
+
+        return mapping.findForward("Sucess");
+    }
+
+    /**
+     * 
+     * @author Ricardo Clerigo & Telmo Nabais
+     * @param userView
+     * @param session
+     * @return
+     */
+    private InfoMasterDegreeCandidate getMasterDegreeCandidate(IUserView userView, HttpSession session) {
+        if (session != null) {
+
+            Object args[] = { userView };
+            List candidates = null;
+            try {
+                candidates = (List) ServiceManagerServiceFactory.executeService(userView,
+                        "ReadPersonCandidates", args);
+            } catch (Exception e) {
+                return null;
+            }
+
+            if (candidates.size() == 1) {
+                session.setAttribute(SessionConstants.MASTER_DEGREE_CANDIDATE, candidates.get(0));
+                return (InfoMasterDegreeCandidate) candidates.get(0);
+            }
+
+            session.setAttribute(SessionConstants.MASTER_DEGREE_CANDIDATE_LIST, candidates);
+            return (InfoMasterDegreeCandidate) candidates.get(0);
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * @author Ricardo Clerigo & Telmo Nabais
+     * @param candidateID
+     * @param userView
+     * @return
+     */
+    private ArrayList getCandidateStudyPlanByCandidateID(Integer candidateID, IUserView userView) {
+        Object[] args = { candidateID };
+
+        try {
+            return (ArrayList) ServiceManagerServiceFactory.executeService(userView,
+                    "ReadCandidateEnrolmentsByCandidateID", args);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
