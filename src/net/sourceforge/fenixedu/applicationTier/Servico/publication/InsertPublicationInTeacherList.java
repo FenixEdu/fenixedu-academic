8ad3108930b112eefd8d38dfcbe0166e6fa05d62
Luis Cruz <luis.cruz@ist.utl.pt>
2005-06-23 09:21:10 +0000

    Bug fix: Domain objects must be cast to the respective interface. Simplified code.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/publication/InsertPublicationInTeacherList.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/publication/InsertPublicationInTeacherList.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.IServico;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.ExistingServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.constants.publication.PublicationConstants;
@@ -36,90 +35,54 @@ import net.sourceforge.fenixedu.util.PublicationArea;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Transformer;
 
+import pt.utl.ist.berserk.logic.serviceManager.IService;
+
 /**
  * @author TJBF & PFON
  * 
  * To change the template for this generated type comment go to Window -
  * Preferences - Java - Code Generation - Code and Comments
  */
-public class InsertPublicationInTeacherList implements IServico {
-
-    /**
-     *  
-     */
-    public InsertPublicationInTeacherList() {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ServidorAplicacao.IServico#getNome()
-     */
-    public String getNome() {
-
-        return "InsertPublicationInTeacherList";
-    }
+public class InsertPublicationInTeacherList implements IService {
 
     public SiteView run(Integer teacherId, Integer publicationId, String publicationArea)
-            throws FenixServiceException, ExistingServiceException {
+            throws ExcepcaoPersistencia {
         InfoSitePublications infoSitePublications = new InfoSitePublications();
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-            IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
-            ITeacher teacher = (Teacher) persistentTeacher.readByOID(Teacher.class, teacherId);
-            InfoTeacher infoTeacher = Cloner.copyITeacher2InfoTeacher(teacher);
-            infoSitePublications.setInfoTeacher(infoTeacher);
+        IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
+        ITeacher teacher = (ITeacher) persistentTeacher.readByOID(Teacher.class, teacherId);
+        InfoTeacher infoTeacher = Cloner.copyITeacher2InfoTeacher(teacher);
+        infoSitePublications.setInfoTeacher(infoTeacher);
 
-            IPersistentPublication persistentPublication = sp.getIPersistentPublication();
-            IPublication publication = (IPublication) persistentPublication.readByOID(Publication.class,
-                    publicationId);
+        IPersistentPublication persistentPublication = sp.getIPersistentPublication();
+        IPublication publication = (IPublication) persistentPublication.readByOID(Publication.class,
+                publicationId);
 
-            IPersistentPublicationTeacher persistentPublicationTeacher = sp
-                    .getIPersistentPublicationTeacher();
-            IPublicationTeacher publicationTeacher = persistentPublicationTeacher
-                    .readByTeacherAndPublication(teacher, publication);
+        IPersistentPublicationTeacher persistentPublicationTeacher = sp
+                .getIPersistentPublicationTeacher();
+        IPublicationTeacher publicationTeacher = persistentPublicationTeacher
+                .readByTeacherAndPublication(teacher, publication);
 
-            if (publicationTeacher == null)
-                publicationTeacher = new PublicationTeacher();
+        if (publicationTeacher == null)
+            publicationTeacher = new PublicationTeacher();
 
-            persistentPublicationTeacher.simpleLockWrite(publicationTeacher);
-            publicationTeacher.setPublication(publication);
-            publicationTeacher.setTeacher(teacher);
-            publicationTeacher.setPublicationArea(PublicationArea.getEnum(publicationArea));
-            
-            
-            
-//            List infoPublications = getInfoPublications(teacher, publication, persistentTeacher,
-//                    persistentPublication);
-//
-//            List infoPublicationCientifics = getInfoPublicationsType(infoPublications,
-//                    PublicationConstants.CIENTIFIC);
-//
-//            List infoPublicationDidatics = getInfoPublicationsType(infoPublications,
-//                    PublicationConstants.DIDATIC);
-//
-//            infoSitePublications.setInfoCientificPublications(infoPublicationCientifics);
-//            infoSitePublications.setInfoDidaticPublications(infoPublicationDidatics);
-//
-//            return new SiteView(infoSitePublications);
-            
-            return null;
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
-        }
+        persistentPublicationTeacher.simpleLockWrite(publicationTeacher);
+        publicationTeacher.setPublication(publication);
+        publicationTeacher.setTeacher(teacher);
+        publicationTeacher.setPublicationArea(PublicationArea.getEnum(publicationArea));
+        return null;
     }
 
     public List getInfoPublications(ITeacher teacher, IPublication publication,
             IPersistentTeacher persistentTeacher, IPersistentPublication persistentPublication)
-            throws FenixServiceException {
+            throws FenixServiceException, ExcepcaoPersistencia {
         List publicationTeachers = teacher.getTeacherPublications();
-        List publications = (List) CollectionUtils.collect(publicationTeachers,
-                new Transformer() {
-                    public Object transform(Object input) {
-                        IPublicationTeacher publicationTeacher = (IPublicationTeacher) input;
-                        return publicationTeacher.getPublication();
-                    }
+        List publications = (List) CollectionUtils.collect(publicationTeachers, new Transformer() {
+            public Object transform(Object input) {
+                IPublicationTeacher publicationTeacher = (IPublicationTeacher) input;
+                return publicationTeacher.getPublication();
+            }
         });
         List infoPublications = new ArrayList();
 
@@ -131,36 +94,32 @@ public class InsertPublicationInTeacherList implements IServico {
             }
         }
 
-        try {
-            persistentTeacher.simpleLockWrite(teacher);
-            persistentPublication.simpleLockWrite(publication);
+        persistentTeacher.simpleLockWrite(teacher);
+        persistentPublication.simpleLockWrite(publication);
 
-            PublicationTeacher publicationTeacher = new PublicationTeacher();
-            publicationTeacher.setPublication(publication);
-            publicationTeacher.setTeacher(teacher);
-            
-            publication.getPublicationTeachers().add(publicationTeacher);
-            teacher.getTeacherPublications().add(publicationTeacher);
-            List newPublications = teacher.getTeacherPublications();
-            infoPublications = (List) CollectionUtils.collect(newPublications, new Transformer() {
-                public Object transform(Object object) {
-                    IPublicationTeacher publicationTeacher = (IPublicationTeacher)object;
-                    IPublication publication = publicationTeacher.getPublication();
-                    IPublication publication2 = publication;
-                    publication2.setPublicationString(publication.toString());
-                    InfoPublication infoPublication = new InfoPublication();
-                    infoPublication.copyFromDomain(publication2);
-                    return infoPublication;
-                }
-            });
-            
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentPublicationTeacher persistentPublicationTeacher = sp.getIPersistentPublicationTeacher();
-            persistentPublicationTeacher.simpleLockWrite(publicationTeacher);
-            
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
-        }
+        PublicationTeacher publicationTeacher = new PublicationTeacher();
+        publicationTeacher.setPublication(publication);
+        publicationTeacher.setTeacher(teacher);
+
+        publication.getPublicationTeachers().add(publicationTeacher);
+        teacher.getTeacherPublications().add(publicationTeacher);
+        List newPublications = teacher.getTeacherPublications();
+        infoPublications = (List) CollectionUtils.collect(newPublications, new Transformer() {
+            public Object transform(Object object) {
+                IPublicationTeacher publicationTeacher = (IPublicationTeacher) object;
+                IPublication publication = publicationTeacher.getPublication();
+                IPublication publication2 = publication;
+                publication2.setPublicationString(publication.toString());
+                InfoPublication infoPublication = new InfoPublication();
+                infoPublication.copyFromDomain(publication2);
+                return infoPublication;
+            }
+        });
+
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        IPersistentPublicationTeacher persistentPublicationTeacher = sp
+                .getIPersistentPublicationTeacher();
+        persistentPublicationTeacher.simpleLockWrite(publicationTeacher);
 
         return infoPublications;
     }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/publico/ReadPublishedFinalDegreeWorkProposalHeaders.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/publico/ReadPublishedFinalDegreeWorkProposalHeaders.java
@@ -7,7 +7,6 @@ package net.sourceforge.fenixedu.applicationTier.Servico.publico;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoBranch;
 import net.sourceforge.fenixedu.dataTransferObject.InfoPerson;
 import net.sourceforge.fenixedu.dataTransferObject.InfoStudent;
@@ -25,109 +24,100 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author Luis Cruz
- *  
+ * 
  */
 public class ReadPublishedFinalDegreeWorkProposalHeaders implements IService {
 
-    public ReadPublishedFinalDegreeWorkProposalHeaders() {
-        super();
-    }
-
-    public List run(Integer executionDegreeOID) throws FenixServiceException {
+    public List run(Integer executionDegreeOID) throws ExcepcaoPersistencia {
         List finalDegreeWorkProposalHeaders = new ArrayList();
 
-        try {
-            ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentFinalDegreeWork persistentFinalDegreeWork = persistentSupport
-                    .getIPersistentFinalDegreeWork();
-            List finalDegreeWorkProposals = persistentFinalDegreeWork
-                    .readPublishedFinalDegreeWorkProposalsByExecutionDegree(executionDegreeOID);
-            if (finalDegreeWorkProposals != null) {
-                finalDegreeWorkProposalHeaders = new ArrayList();
-                for (int i = 0; i < finalDegreeWorkProposals.size(); i++) {
-                    IProposal proposal = (IProposal) finalDegreeWorkProposals.get(i);
+        ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        IPersistentFinalDegreeWork persistentFinalDegreeWork = persistentSupport
+                .getIPersistentFinalDegreeWork();
+        List finalDegreeWorkProposals = persistentFinalDegreeWork
+                .readPublishedFinalDegreeWorkProposalsByExecutionDegree(executionDegreeOID);
+        if (finalDegreeWorkProposals != null) {
+            finalDegreeWorkProposalHeaders = new ArrayList();
+            for (int i = 0; i < finalDegreeWorkProposals.size(); i++) {
+                IProposal proposal = (IProposal) finalDegreeWorkProposals.get(i);
 
-                    if (proposal != null) {
-                        FinalDegreeWorkProposalHeader finalDegreeWorkProposalHeader = new FinalDegreeWorkProposalHeader();
+                if (proposal != null) {
+                    FinalDegreeWorkProposalHeader finalDegreeWorkProposalHeader = new FinalDegreeWorkProposalHeader();
 
-                        finalDegreeWorkProposalHeader.setIdInternal(proposal.getIdInternal());
-                        finalDegreeWorkProposalHeader.setProposalNumber(proposal.getProposalNumber());
-                        finalDegreeWorkProposalHeader.setTitle(proposal.getTitle());
-                        finalDegreeWorkProposalHeader.setExecutionYear(proposal.getExecutionDegree().getExecutionYear().getYear());
-                        if (proposal.getOrientator() != null) {
-                            finalDegreeWorkProposalHeader.setOrientatorOID(proposal.getOrientator()
-                                    .getIdInternal());
-                            finalDegreeWorkProposalHeader.setOrientatorName(proposal.getOrientator()
-                                    .getPerson().getNome());
-                        }
-                        if (proposal.getCoorientator() != null) {
-                            finalDegreeWorkProposalHeader.setCoorientatorOID(proposal.getCoorientator()
-                                    .getIdInternal());
-                            finalDegreeWorkProposalHeader.setCoorientatorName(proposal.getCoorientator()
-                                    .getPerson().getNome());
-                        }
-                        finalDegreeWorkProposalHeader.setCompanyLink(proposal.getCompanionName());
-                        finalDegreeWorkProposalHeader.setStatus(proposal.getStatus());
-
-                        if (proposal.getBranches() != null) {
-                            finalDegreeWorkProposalHeader.setBranches(new ArrayList());
-                            for (int j = 0; j < proposal.getBranches().size(); j++) {
-                                IBranch branch = (IBranch) proposal.getBranches().get(j);
-
-                                if (branch != null) {
-                                    InfoBranch infoBranch = new InfoBranch();
-                                    infoBranch.setIdInternal(branch.getIdInternal());
-                                    infoBranch.setName(branch.getName());
-                                    finalDegreeWorkProposalHeader.getBranches().add(infoBranch);
-                                }
-                            }
-                        }
-
-                        if (proposal.getGroupAttributedByTeacher() != null) {
-                            InfoGroup infoGroup = new InfoGroup();
-                            infoGroup.setGroupStudents(new ArrayList());
-                            for (int j = 0; j < proposal.getGroupAttributedByTeacher()
-                                    .getGroupStudents().size(); j++) {
-                                IGroupStudent groupStudent = (IGroupStudent) proposal
-                                        .getGroupAttributedByTeacher().getGroupStudents().get(j);
-                                InfoGroupStudent infoGroupStudent = new InfoGroupStudent();
-                                InfoStudent infoStudent = new InfoStudent();
-                                InfoPerson infoPerson = new InfoPerson();
-                                infoPerson.setUsername(groupStudent.getStudent().getPerson()
-                                        .getUsername());
-                                infoStudent.setInfoPerson(infoPerson);
-                                infoStudent.setNumber(groupStudent.getStudent().getNumber());
-                                infoGroupStudent.setStudent(infoStudent);
-                                infoGroup.getGroupStudents().add(infoGroupStudent);
-                            }
-                            finalDegreeWorkProposalHeader.setGroupAttributedByTeacher(infoGroup);
-                        }
-
-                        if (proposal.getGroupAttributed() != null) {
-                            InfoGroup infoGroup = new InfoGroup();
-                            infoGroup.setGroupStudents(new ArrayList());
-                            for (int j = 0; j < proposal.getGroupAttributed().getGroupStudents().size(); j++) {
-                                IGroupStudent groupStudent = (IGroupStudent) proposal
-                                        .getGroupAttributed().getGroupStudents().get(j);
-                                InfoGroupStudent infoGroupStudent = new InfoGroupStudent();
-                                InfoStudent infoStudent = new InfoStudent();
-                                InfoPerson infoPerson = new InfoPerson();
-                                infoPerson.setUsername(groupStudent.getStudent().getPerson()
-                                        .getUsername());
-                                infoStudent.setInfoPerson(infoPerson);
-                                infoStudent.setNumber(groupStudent.getStudent().getNumber());
-                                infoGroupStudent.setStudent(infoStudent);
-                                infoGroup.getGroupStudents().add(infoGroupStudent);
-                            }
-                            finalDegreeWorkProposalHeader.setGroupAttributed(infoGroup);
-                        }
-
-                        finalDegreeWorkProposalHeaders.add(finalDegreeWorkProposalHeader);
+                    finalDegreeWorkProposalHeader.setIdInternal(proposal.getIdInternal());
+                    finalDegreeWorkProposalHeader.setProposalNumber(proposal.getProposalNumber());
+                    finalDegreeWorkProposalHeader.setTitle(proposal.getTitle());
+                    finalDegreeWorkProposalHeader.setExecutionYear(proposal.getExecutionDegree()
+                            .getExecutionYear().getYear());
+                    if (proposal.getOrientator() != null) {
+                        finalDegreeWorkProposalHeader.setOrientatorOID(proposal.getOrientator()
+                                .getIdInternal());
+                        finalDegreeWorkProposalHeader.setOrientatorName(proposal.getOrientator()
+                                .getPerson().getNome());
                     }
+                    if (proposal.getCoorientator() != null) {
+                        finalDegreeWorkProposalHeader.setCoorientatorOID(proposal.getCoorientator()
+                                .getIdInternal());
+                        finalDegreeWorkProposalHeader.setCoorientatorName(proposal.getCoorientator()
+                                .getPerson().getNome());
+                    }
+                    finalDegreeWorkProposalHeader.setCompanyLink(proposal.getCompanionName());
+                    finalDegreeWorkProposalHeader.setStatus(proposal.getStatus());
+
+                    if (proposal.getBranches() != null) {
+                        finalDegreeWorkProposalHeader.setBranches(new ArrayList());
+                        for (int j = 0; j < proposal.getBranches().size(); j++) {
+                            IBranch branch = (IBranch) proposal.getBranches().get(j);
+
+                            if (branch != null) {
+                                InfoBranch infoBranch = new InfoBranch();
+                                infoBranch.setIdInternal(branch.getIdInternal());
+                                infoBranch.setName(branch.getName());
+                                finalDegreeWorkProposalHeader.getBranches().add(infoBranch);
+                            }
+                        }
+                    }
+
+                    if (proposal.getGroupAttributedByTeacher() != null) {
+                        InfoGroup infoGroup = new InfoGroup();
+                        infoGroup.setGroupStudents(new ArrayList());
+                        for (int j = 0; j < proposal.getGroupAttributedByTeacher().getGroupStudents()
+                                .size(); j++) {
+                            IGroupStudent groupStudent = (IGroupStudent) proposal
+                                    .getGroupAttributedByTeacher().getGroupStudents().get(j);
+                            InfoGroupStudent infoGroupStudent = new InfoGroupStudent();
+                            InfoStudent infoStudent = new InfoStudent();
+                            InfoPerson infoPerson = new InfoPerson();
+                            infoPerson.setUsername(groupStudent.getStudent().getPerson().getUsername());
+                            infoStudent.setInfoPerson(infoPerson);
+                            infoStudent.setNumber(groupStudent.getStudent().getNumber());
+                            infoGroupStudent.setStudent(infoStudent);
+                            infoGroup.getGroupStudents().add(infoGroupStudent);
+                        }
+                        finalDegreeWorkProposalHeader.setGroupAttributedByTeacher(infoGroup);
+                    }
+
+                    if (proposal.getGroupAttributed() != null) {
+                        InfoGroup infoGroup = new InfoGroup();
+                        infoGroup.setGroupStudents(new ArrayList());
+                        for (int j = 0; j < proposal.getGroupAttributed().getGroupStudents().size(); j++) {
+                            IGroupStudent groupStudent = (IGroupStudent) proposal.getGroupAttributed()
+                                    .getGroupStudents().get(j);
+                            InfoGroupStudent infoGroupStudent = new InfoGroupStudent();
+                            InfoStudent infoStudent = new InfoStudent();
+                            InfoPerson infoPerson = new InfoPerson();
+                            infoPerson.setUsername(groupStudent.getStudent().getPerson().getUsername());
+                            infoStudent.setInfoPerson(infoPerson);
+                            infoStudent.setNumber(groupStudent.getStudent().getNumber());
+                            infoGroupStudent.setStudent(infoStudent);
+                            infoGroup.getGroupStudents().add(infoGroupStudent);
+                        }
+                        finalDegreeWorkProposalHeader.setGroupAttributed(infoGroup);
+                    }
+
+                    finalDegreeWorkProposalHeaders.add(finalDegreeWorkProposalHeader);
                 }
             }
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
         }
 
         return finalDegreeWorkProposalHeaders;
