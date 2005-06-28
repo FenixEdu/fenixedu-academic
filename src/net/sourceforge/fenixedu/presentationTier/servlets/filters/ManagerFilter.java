15955e1a5a76bcf294313676d3475b2273c8937c
Luis Cruz <luis.cruz@ist.utl.pt>
2005-06-28 17:15:40 +0000

    Fixed security issue.

---
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/ManagerFilter.java
@@ -0,0 +1,41 @@
+package net.sourceforge.fenixedu.presentationTier.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.domain.person.RoleType;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionUtils;
+
+/**
+ * 17/Fev/2003
+ * 
+ * @author jpvl
+ */
+public class ManagerFilter implements Filter {
+
+    public void init(FilterConfig config) {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        IUserView userView = SessionUtils.getUserView((HttpServletRequest) request);
+        if (userView == null || !userView.hasRoleType(RoleType.MANAGER)) {
+            throw new RuntimeException();
+        }
+
+        chain.doFilter(request, response);
+    }
+
+}
