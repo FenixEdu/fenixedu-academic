756a823f4b6f5f21a35d2ee8ac1063bbc29510a2
Shezad Anavarali <shezad@ist.utl.pt>
2005-06-29 12:47:33 +0000

    Cloner removed

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/thesis/ReadNonActivesMasterDegreeProofVersionsByStudentCurricularPlan.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/thesis/ReadNonActivesMasterDegreeProofVersionsByStudentCurricularPlan.java
@@ -1,47 +1,45 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.thesis;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoMasterDegreeProofVersion;
 import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
-import net.sourceforge.fenixedu.domain.IStudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.IMasterDegreeProofVersion;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * 
  * @author - Shezad Anavarali (sana@mega.ist.utl.pt) - Nadir Tarmahomed
  *         (naat@mega.ist.utl.pt)
- *  
+ * 
  */
 public class ReadNonActivesMasterDegreeProofVersionsByStudentCurricularPlan implements IService {
 
-    public List run(InfoStudentCurricularPlan infoStudentCurricularPlan) throws FenixServiceException {
-        List infoMasterDegreeProofVersions = new ArrayList();
+    public List run(InfoStudentCurricularPlan infoStudentCurricularPlan) throws FenixServiceException,
+            ExcepcaoPersistencia {
 
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IStudentCurricularPlan studentCurricularPlan = Cloner
-                    .copyInfoStudentCurricularPlan2IStudentCurricularPlan(infoStudentCurricularPlan);
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-            List masterDegreeProofVersions = sp.getIPersistentMasterDegreeProofVersion()
-                    .readNotActiveByStudentCurricularPlan(studentCurricularPlan);
+        List masterDegreeProofVersions = sp.getIPersistentMasterDegreeProofVersion()
+                .readNotActiveByStudentCurricularPlan(infoStudentCurricularPlan.getIdInternal());
 
-            if (masterDegreeProofVersions.isEmpty() == false) {
-                infoMasterDegreeProofVersions = Cloner
-                        .copyListIMasterDegreeProofVersion2ListInfoMasterDegreeProofVersion(masterDegreeProofVersions);
+        CollectionUtils.transform(masterDegreeProofVersions, new Transformer() {
+
+            public Object transform(Object arg0) {
+                IMasterDegreeProofVersion masterDegreeProofVersion = (IMasterDegreeProofVersion) arg0;
+                return InfoMasterDegreeProofVersion.newInfoFromDomain(masterDegreeProofVersion);
             }
 
-        } catch (ExcepcaoPersistencia ex) {
-            FenixServiceException newEx = new FenixServiceException("Persistence layer error");
-            newEx.fillInStackTrace();
-            throw newEx;
-        }
+        });
 
-        return infoMasterDegreeProofVersions;
+        return masterDegreeProofVersions;
     }
 }
