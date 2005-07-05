a9b18200972918f5ea2346d5d3fad4ebd0254063
Francisco Paulo <frnp@ist.utl.pt>
2005-07-05 12:55:17 +0000

    *** empty log message ***

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/guide/DeleteGuideEntryAndPaymentTransactionInManager.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/guide/DeleteGuideEntryAndPaymentTransactionInManager.java
@@ -1,10 +1,14 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.guide;
 
+import java.util.List;
+
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidChangeServiceException;
 import net.sourceforge.fenixedu.domain.GuideEntry;
 import net.sourceforge.fenixedu.domain.IGratuitySituation;
 import net.sourceforge.fenixedu.domain.IGuide;
 import net.sourceforge.fenixedu.domain.IGuideEntry;
+import net.sourceforge.fenixedu.domain.reimbursementGuide.IReimbursementGuideEntry;
+import net.sourceforge.fenixedu.domain.reimbursementGuide.ReimbursementGuideEntry;
 import net.sourceforge.fenixedu.domain.transactions.IGratuityTransaction;
 import net.sourceforge.fenixedu.domain.transactions.IPaymentTransaction;
 import net.sourceforge.fenixedu.domain.transactions.PaymentTransaction;
@@ -53,12 +57,25 @@ public class DeleteGuideEntryAndPaymentTransactionInManager implements IService 
                 sp.getIPersistentGratuitySituation().simpleLockWrite(gratuitySituation);
             }
 
+			sp.getIPersistentPersonAccount().simpleLockWrite(paymentTransaction.getPersonAccount());
+			paymentTransaction.getPersonAccount().getPaymentTransactions().remove(paymentTransaction);
             sp.getIPersistentPaymentTransaction().deleteByOID(PaymentTransaction.class,
                     paymentTransaction.getIdInternal());
 
         }
 
-        sp.getIPersistentGuideEntry().delete(guideEntry);
+		if(guideEntry.getReimbursementGuideEntries()!=null){
+			for(IReimbursementGuideEntry reimbursementGuideEntry : (List<IReimbursementGuideEntry>)guideEntry.getReimbursementGuideEntries()){
+				sp.getIPersistentReimbursementGuide().simpleLockWrite(reimbursementGuideEntry.getReimbursementGuide());
+				reimbursementGuideEntry.getReimbursementGuide().getReimbursementGuideEntries().remove(reimbursementGuideEntry);
+				sp.getIPersistentGuideEntry().deleteByOID(ReimbursementGuideEntry.class,reimbursementGuideEntry.getIdInternal());
+			}
+		}
+		
+		guideEntry.getGuide().getGuideEntries().remove(guideEntry);
+        sp.getIPersistentGuideEntry().deleteByOID(GuideEntry.class,guideEntry.getIdInternal());
+		
+		
 
         // update guide's total value
         guide.setTotal(NumberUtils.formatNumber(new Double(guide.getTotal().doubleValue()
