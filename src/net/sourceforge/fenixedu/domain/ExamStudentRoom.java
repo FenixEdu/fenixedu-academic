08940f9229ba7ee07642ff444172880ea8edd313
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-07 13:34:04 +0000

    removed unnecessary constructors

--- src/net/sourceforge/fenixedu/domain/ExamStudentRoom.java
+++ src/net/sourceforge/fenixedu/domain/ExamStudentRoom.java
@@ -7,21 +7,9 @@ package net.sourceforge.fenixedu.domain;
 
 /**
  * @author João Mota
- *  
+ * 
  */
 public class ExamStudentRoom extends ExamStudentRoom_Base {
-
-    /**
-     *  
-     */
-    public ExamStudentRoom() {
-    }
-
-    public ExamStudentRoom(IExam exam, IStudent student, IRoom room) {
-        this.setExam(exam);
-        this.setStudent(student);
-        this.setRoom(room);
-    }
 
     public boolean equals(Object obj) {
         if (obj instanceof IExamStudentRoom) {
