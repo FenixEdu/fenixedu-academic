876f5cabb13722ad294b8d0751d1bc30caeaa2d1
Luis Cruz <luis.cruz@ist.utl.pt>
2005-07-07 16:58:36 +0000

    Domain excpetion class.

---
+++ src/net/sourceforge/fenixedu/domain/exceptions/DomainException.java
@@ -0,0 +1,21 @@
+package net.sourceforge.fenixedu.domain.exceptions;
+
+public class DomainException extends Exception {
+
+    private final String key;
+    private final String[] args;
+
+    public DomainException(final String key, final String ... args) {
+        this.key = key;
+        this.args = args;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+}
