d759d3c5b44fd87a68001fd5c206b310b3efe3b8
Amin Mahomed Amirali <amin@netcabo.pt>
2005-07-13 15:40:41 +0000

    created the Versioned Object Transaction

---
+++ src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/TransactionVO.java
@@ -0,0 +1,8 @@
+package net.sourceforge.fenixedu.persistenceTier.versionedObjects.dao;
+
+import net.sourceforge.fenixedu.persistenceTier.transactions.IPersistentTransaction;
+import net.sourceforge.fenixedu.persistenceTier.versionedObjects.VersionedObjectsBase;
+
+public class TransactionVO extends VersionedObjectsBase implements IPersistentTransaction {
+
+}
