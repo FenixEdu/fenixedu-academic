fac6be2417e77f01b9e6f256676eec7c6461b31b
Joao Cachopo <joao.cachopo@inesc-id.pt>
2005-07-13 15:04:03 +0000

    Fix bug that affected classes with an ojbConcreteClass. In some cases, the ClassDescriptor for these classes might have not the appropriate factoryClass/method that instructed OJB to call the special allocate_obj method to allocate a domain object.

--- src/net/sourceforge/fenixedu/persistenceTier/OJB/FenixRowReader.java
+++ src/net/sourceforge/fenixedu/persistenceTier/OJB/FenixRowReader.java
@@ -1,27 +1,38 @@
 package net.sourceforge.fenixedu.persistenceTier.OJB;
 
 import java.lang.reflect.Method;
+import java.util.Map;
 
 import org.apache.ojb.broker.accesslayer.RowReaderDefaultImpl;
 import org.apache.ojb.broker.metadata.ClassDescriptor;
+import org.apache.ojb.broker.PersistenceBrokerException;
 
 
 public class FenixRowReader extends RowReaderDefaultImpl {
 
     public FenixRowReader(ClassDescriptor cld) {
         super(cld);
+    }
 
-        String className = cld.getClassOfObject().getName();
-        try {
-            Method m = DomainAllocator.class.getMethod("allocate_" + className.replace('.', '_'));
-            if (m != null) {
-                //System.out.println("FenixRowReader: setting factoryMethod for class " + className + " to " + m.getName());
-                cld.setFactoryClass(DomainAllocator.class);
-                cld.setFactoryMethod(m.getName());
+    private void forceFactoryClassAndMethod(ClassDescriptor cld) {
+        if (cld.getFactoryClass() != DomainAllocator.class) {
+            String className = cld.getClassOfObject().getName();
+            try {
+                Method m = DomainAllocator.class.getMethod("allocate_" + className.replace('.', '_'));
+                if (m != null) {
+                    //System.out.println("FenixRowReader: setting factoryMethod for class " + className + " to " + m.getName());
+                    cld.setFactoryClass(DomainAllocator.class);
+                    cld.setFactoryMethod(m.getName());
+                }
+            } catch (Exception e) {
+                System.out.println("FenixRowReader: NOT setting factoryMethod for class " + className + " because " + e);
             }
-        } catch (Exception e) {
-            System.out.println("FenixRowReader: NOT setting factoryMethod for class " + className + " because " + e);
         }
     }
 
+    protected ClassDescriptor selectClassDescriptor(Map row) throws PersistenceBrokerException {
+        ClassDescriptor result = super.selectClassDescriptor(row);
+        forceFactoryClassAndMethod(result);
+        return result;
+    }
 }
