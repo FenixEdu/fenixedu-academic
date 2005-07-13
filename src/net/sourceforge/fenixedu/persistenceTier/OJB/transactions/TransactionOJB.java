4138630cd592f4252b020eb7e1bd3c0b15d3415c
Amin Mahomed Amirali <amin@netcabo.pt>
2005-07-13 15:37:35 +0000

    removed empty constructor and the readAllByPersonAccount method (it wasn't being called anywhere)

--- src/net/sourceforge/fenixedu/persistenceTier/OJB/transactions/TransactionOJB.java
+++ src/net/sourceforge/fenixedu/persistenceTier/OJB/transactions/TransactionOJB.java
@@ -1,14 +1,7 @@
 package net.sourceforge.fenixedu.persistenceTier.OJB.transactions;
 
-import java.util.List;
-
-import net.sourceforge.fenixedu.domain.IPersonAccount;
-import net.sourceforge.fenixedu.domain.transactions.InsuranceTransaction;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.OJB.ObjectFenixOJB;
 import net.sourceforge.fenixedu.persistenceTier.transactions.IPersistentTransaction;
-
-import org.apache.ojb.broker.query.Criteria;
 
 /**
  * @author - Shezad Anavarali (sana@mega.ist.utl.pt) - Nadir Tarmahomed
@@ -16,16 +9,5 @@ import org.apache.ojb.broker.query.Criteria;
  *  
  */
 public class TransactionOJB extends ObjectFenixOJB implements IPersistentTransaction {
-
-    public TransactionOJB() {
-    }
-
-    public List readAllByPersonAccount(IPersonAccount personAccount) throws ExcepcaoPersistencia {
-
-        Criteria crit = new Criteria();
-        crit.addEqualTo("personAccount.idInternal", personAccount.getIdInternal());
-
-        return queryList(InsuranceTransaction.class, crit);
-    }
 
 }
