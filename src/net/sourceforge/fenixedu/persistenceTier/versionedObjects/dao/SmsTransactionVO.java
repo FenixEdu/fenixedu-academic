4b4bc1bc1d334a92846b09f82d50cca7bf097c22
Amin Mahomed Amirali <amin@netcabo.pt>
2005-07-13 16:06:54 +0000

    created the Versioned Object SmsTransaction

---
+++ src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/SmsTransactionVO.java
@@ -0,0 +1,7 @@
+package net.sourceforge.fenixedu.persistenceTier.versionedObjects.dao;
+
+import net.sourceforge.fenixedu.persistenceTier.transactions.IPersistentSmsTransaction;
+import net.sourceforge.fenixedu.persistenceTier.versionedObjects.VersionedObjectsBase;
+
+public class SmsTransactionVO extends VersionedObjectsBase implements IPersistentSmsTransaction {
+}
