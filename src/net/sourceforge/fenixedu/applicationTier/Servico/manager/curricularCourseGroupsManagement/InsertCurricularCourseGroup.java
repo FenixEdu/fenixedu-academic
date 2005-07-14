374bd77e3692c6fa910cf012c54302ca7ae2e68b
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-14 08:59:29 +0000

    *** empty log message ***

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/curricularCourseGroupsManagement/InsertCurricularCourseGroup.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/curricularCourseGroupsManagement/InsertCurricularCourseGroup.java
@@ -4,7 +4,6 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Servico.manager.curricularCourseGroupsManagement;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
 import net.sourceforge.fenixedu.domain.AreaCurricularCourseGroup;
 import net.sourceforge.fenixedu.domain.Branch;
@@ -26,52 +25,42 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class InsertCurricularCourseGroup implements IService {
 
-    /**
-     *  
-     */
-    public InsertCurricularCourseGroup() {
-    }
-
     public void run(Integer groupId, String name, Integer branchId, Integer minimumValue,
-            Integer maximumValue, String areaType, String className) throws FenixServiceException {
+            Integer maximumValue, String areaType, String className) throws ExcepcaoPersistencia,
+            InvalidArgumentsServiceException {
 
-        try {
-            ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentBranch persistentBranch = persistentSuport.getIPersistentBranch();
-            IPersistentCurricularCourseGroup persistentCurricularCourseGroup = persistentSuport
-                    .getIPersistentCurricularCourseGroup();
+        ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        IPersistentBranch persistentBranch = persistentSuport.getIPersistentBranch();
+        IPersistentCurricularCourseGroup persistentCurricularCourseGroup = persistentSuport
+                .getIPersistentCurricularCourseGroup();
 
-            IBranch branch = (IBranch) persistentBranch.readByOID(Branch.class, branchId);
-            if (branch == null) {
+        IBranch branch = (IBranch) persistentBranch.readByOID(Branch.class, branchId);
+        if (branch == null) {
+            throw new InvalidArgumentsServiceException();
+        }
+
+        ICurricularCourseGroup curricularCourseGroup = null;
+        if (groupId != null) {
+            curricularCourseGroup = (ICurricularCourseGroup) persistentCurricularCourseGroup.readByOID(
+                    CurricularCourseGroup.class, groupId);
+            if (curricularCourseGroup == null) {
                 throw new InvalidArgumentsServiceException();
             }
-            ICurricularCourseGroup curricularCourseGroup = null;
-            if (groupId != null) {
-                curricularCourseGroup = (ICurricularCourseGroup) persistentCurricularCourseGroup
-                        .readByOID(CurricularCourseGroup.class, groupId);
-                if (curricularCourseGroup == null) {
-                    throw new InvalidArgumentsServiceException();
-                }
-            } else {
+        } else {
+            curricularCourseGroup = getInstance(className);
+        }
 
-                curricularCourseGroup = getInstance(className);
-            }
+        persistentCurricularCourseGroup.simpleLockWrite(curricularCourseGroup);
+        curricularCourseGroup.setBranch(branch);
+        curricularCourseGroup.setName(name);
 
-            persistentCurricularCourseGroup.simpleLockWrite(curricularCourseGroup);
-            curricularCourseGroup.setBranch(branch);
-            curricularCourseGroup.setName(name);
-            if (curricularCourseGroup instanceof AreaCurricularCourseGroup) {
-                curricularCourseGroup.setAreaType(AreaType.valueOf(areaType));
-                curricularCourseGroup.setMaximumCredits(maximumValue);
-                curricularCourseGroup.setMinimumCredits(minimumValue);
-            } else {
-                curricularCourseGroup.setMaximumNumberOfOptionalCourses(maximumValue);
-                curricularCourseGroup.setMinimumNumberOfOptionalCourses(minimumValue);
-            }
-
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e);
-
+        if (curricularCourseGroup instanceof AreaCurricularCourseGroup) {
+            curricularCourseGroup.setAreaType(AreaType.valueOf(areaType));
+            curricularCourseGroup.setMaximumCredits(maximumValue);
+            curricularCourseGroup.setMinimumCredits(minimumValue);
+        } else {
+            curricularCourseGroup.setMaximumNumberOfOptionalCourses(maximumValue);
+            curricularCourseGroup.setMinimumNumberOfOptionalCourses(minimumValue);
         }
 
     }
