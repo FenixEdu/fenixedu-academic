c9899f57df46615abd655018065c9b3045348b79
Luis Cruz <luis.cruz@ist.utl.pt>
2005-07-18 15:39:49 +0000

    Added constructor and fixed compare function.

--- src/net/sourceforge/fenixedu/domain/Role.java
+++ src/net/sourceforge/fenixedu/domain/Role.java
@@ -5,18 +5,32 @@
 package net.sourceforge.fenixedu.domain;
 
 import net.sourceforge.fenixedu.dataTransferObject.InfoRole;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 
 /**
  * @author jpvl
  */
 public class Role extends Role_Base implements Comparable {
 
+    public Role() {
+        super();
+    }
+
+    public Role(final RoleType roleType, final String portalSubApplication, final String page,
+            final String pageNameProperty) {
+        super();
+        setRoleType(roleType);
+        setPortalSubApplication(portalSubApplication);
+        setPage(page);
+        setPageNameProperty(pageNameProperty);
+    }
+
     public int compareTo(Object o) {
         if (o != null && o instanceof InfoRole) {
             final InfoRole infoRole = (InfoRole) o;
             return getRoleType().compareTo(infoRole.getRoleType());
         }
-        return 0;
+        return -1;
     }
 
 }
