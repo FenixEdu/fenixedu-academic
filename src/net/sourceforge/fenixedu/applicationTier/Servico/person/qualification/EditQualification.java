5254e101dd1203306ac18db132db8b6ad17f9b76
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-18 16:17:47 +0000

    removed commented code

--- src/net/sourceforge/fenixedu/applicationTier/Servico/person/qualification/EditQualification.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/person/qualification/EditQualification.java
@@ -7,7 +7,6 @@ package net.sourceforge.fenixedu.applicationTier.Servico.person.qualification;
 import net.sourceforge.fenixedu.applicationTier.Servico.framework.EditDomainObjectService;
 import net.sourceforge.fenixedu.dataTransferObject.InfoObject;
 import net.sourceforge.fenixedu.dataTransferObject.person.InfoQualification;
-import net.sourceforge.fenixedu.dataTransferObject.person.InfoQualificationWithPersonAndCountry;
 import net.sourceforge.fenixedu.domain.Country;
 import net.sourceforge.fenixedu.domain.ICountry;
 import net.sourceforge.fenixedu.domain.IDomainObject;
@@ -28,102 +27,48 @@ import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 public class EditQualification extends EditDomainObjectService {
 
     @Override
-	protected void copyInformationFromIntoToDomain(ISuportePersistente sp, InfoObject infoObject, IDomainObject domainObject) throws ExcepcaoPersistencia {
-		InfoQualification infoQualification = (InfoQualification)infoObject;
-		IQualification qualification = (Qualification)domainObject;
-		
+    protected void copyInformationFromIntoToDomain(ISuportePersistente sp, InfoObject infoObject,
+            IDomainObject domainObject) throws ExcepcaoPersistencia {
+        InfoQualification infoQualification = (InfoQualification) infoObject;
+        IQualification qualification = (Qualification) domainObject;
 
         ICountry country = (ICountry) sp.getIPersistentCountry().readByOID(Country.class,
-                infoQualification.getInfoCountry().getIdInternal());      
+                infoQualification.getInfoCountry().getIdInternal());
 
         IPerson person = (IPerson) sp.getIPessoaPersistente().readByOID(Person.class,
                 infoQualification.getInfoPerson().getIdInternal());
-        
-		
-		qualification.setBranch(infoQualification.getBranch());
-		qualification.setCountry(country);
-		qualification.setCountryKey(country.getIdInternal());
-		qualification.setDate(infoQualification.getDate());
-		qualification.setDegree(infoQualification.getDegree());
-		qualification.setDegreeRecognition(infoQualification.getDegreeRecognition());
-		qualification.setEquivalenceDate(infoQualification.getEquivalenceDate());
-		qualification.setEquivalenceSchool(infoQualification.getEquivalenceSchool());
-		
-		//qualification.setLastModificationDate();
-		qualification.setMark( infoQualification.getMark());
-		qualification.setPerson(person);
-		qualification.setPersonKey(person.getIdInternal());
-		qualification.setSchool(infoQualification.getSchool());
-		qualification.setSpecializationArea(infoQualification.getSpecializationArea());
-		qualification.setTitle(infoQualification.getTitle());
-		
-		
-	}
 
-	@Override
-	protected IDomainObject createNewDomainObject(InfoObject infoObject) {
-		// TODO Auto-generated method stub
-		return new Qualification();
-	}
+        qualification.setBranch(infoQualification.getBranch());
+        qualification.setCountry(country);
+        qualification.setCountryKey(country.getIdInternal());
+        qualification.setDate(infoQualification.getDate());
+        qualification.setDegree(infoQualification.getDegree());
+        qualification.setDegreeRecognition(infoQualification.getDegreeRecognition());
+        qualification.setEquivalenceDate(infoQualification.getEquivalenceDate());
+        qualification.setEquivalenceSchool(infoQualification.getEquivalenceSchool());
 
-	@Override
-	protected Class getDomainObjectClass() {
-		// TODO Auto-generated method stub
-		return Qualification.class ;
-	}
-
-	public EditQualification() {
+        // qualification.setLastModificationDate();
+        qualification.setMark(infoQualification.getMark());
+        qualification.setPerson(person);
+        qualification.setPersonKey(person.getIdInternal());
+        qualification.setSchool(infoQualification.getSchool());
+        qualification.setSpecializationArea(infoQualification.getSpecializationArea());
+        qualification.setTitle(infoQualification.getTitle());
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#getIPersistentObject(ServidorPersistente.ISuportePersistente)
-     */
+    @Override
+    protected IDomainObject createNewDomainObject(InfoObject infoObject) {
+        return new Qualification();
+    }
+
+    @Override
+    protected Class getDomainObjectClass() {
+        return Qualification.class;
+    }
+
     protected IPersistentObject getIPersistentObject(ISuportePersistente sp) throws ExcepcaoPersistencia {
         ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
         return persistentSuport.getIPersistentQualification();
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ServidorAplicacao.Servico.framework.EditDomainObjectService#clone2DomainObject(net.sourceforge.fenixedu.dataTransferObject.InfoObject)
-     */
-    protected IDomainObject clone2DomainObject(InfoObject infoObject) throws ExcepcaoPersistencia {
-        return InfoQualificationWithPersonAndCountry.newDomainFromInfo((InfoQualification) infoObject);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ServidorAplicacao.IServico#getNome()
-     */
-    public String getNome() {
-        return "EditQualification";
-    }
-
-    //	 Qualification UNIQUE was drop (by PICA). At this point all qualifications
-    // are valid!
-    //    /**
-    //	 * This method invokes a persistent method to read an IDomainObject from
-    // database
-    //	 *
-    //	 * @param domainObject
-    //	 * @return
-    //	 */
-    //    protected IDomainObject readObjectByUnique(IDomainObject domainObject,
-    // ISuportePersistente sp)
-    //        throws ExcepcaoPersistencia
-    //    {
-    //        IPersistentQualification persistentQualification =
-    // sp.getIPersistentQualification();
-    //        IQualification oldQualification = (IQualification) domainObject;
-    //        IQualification newQualification =
-    //            persistentQualification.readByDateAndSchoolAndPerson(
-    //                oldQualification.getDate(),
-    //                oldQualification.getSchool(),
-    //                oldQualification.getPerson());
-    //        return newQualification;
-    //    }
 }
