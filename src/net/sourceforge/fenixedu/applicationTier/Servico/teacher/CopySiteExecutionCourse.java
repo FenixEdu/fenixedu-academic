78f489e4c73ad2006f64218cee370ff007d62d08
João Florindo <jdnf@linkare.pt>
2005-07-18 13:28:32 +0000

    Removed TODO

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/CopySiteExecutionCourse.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/CopySiteExecutionCourse.java
@@ -24,54 +24,60 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author tfs
- *  
+ * 
  */
 public class CopySiteExecutionCourse implements IService {
-    
+
     public void run(Integer executionCourseFromID, Integer executionCourseToID)
-            throws ExcepcaoPersistencia, FenixServiceException, DomainException {        
-        
-        final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();      
-        final IPersistentExecutionCourse persistentExecutionCourse = persistentSupport.getIPersistentExecutionCourse();
-        
-        final IExecutionCourse executionCourseFrom = (IExecutionCourse) persistentExecutionCourse.readByOID(ExecutionCourse.class, executionCourseFromID);
+            throws ExcepcaoPersistencia, FenixServiceException, DomainException {
+
+        final ISuportePersistente persistentSupport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
+        final IPersistentExecutionCourse persistentExecutionCourse = persistentSupport
+                .getIPersistentExecutionCourse();
+
+        final IExecutionCourse executionCourseFrom = (IExecutionCourse) persistentExecutionCourse
+                .readByOID(ExecutionCourse.class, executionCourseFromID);
         if (executionCourseFrom == null)
             throw new InvalidArgumentsServiceException();
-        
-        final IExecutionCourse executionCourseTo = (IExecutionCourse) persistentExecutionCourse.readByOID(ExecutionCourse.class, executionCourseToID);
+
+        final IExecutionCourse executionCourseTo = (IExecutionCourse) persistentExecutionCourse
+                .readByOID(ExecutionCourse.class, executionCourseToID);
         if (executionCourseTo == null)
             throw new InvalidArgumentsServiceException();
-                
+
         ISite siteFrom = executionCourseFrom.getSite();
         if (siteFrom == null) {
-            throw new FenixServiceException();            
+            throw new FenixServiceException();
         }
-        
+
         ISite siteTo = executionCourseTo.getSite();
         if (siteTo == null) {
             executionCourseTo.createSite();
             siteTo = executionCourseTo.getSite();
         }
-                
-        persistentExecutionCourse.simpleLockWrite(executionCourseTo);               
-        siteTo.edit(siteFrom.getInitialStatement(), siteFrom.getIntroduction(), siteFrom.getMail(), siteFrom.getAlternativeSite());
-        
+
+        persistentExecutionCourse.simpleLockWrite(executionCourseTo);
+        siteTo.edit(siteFrom.getInitialStatement(), siteFrom.getIntroduction(), siteFrom.getMail(),
+                siteFrom.getAlternativeSite());
+
         copySectionsAndItems(siteFrom, siteTo);
         copyBibliographicReference(executionCourseFrom, executionCourseTo);
         copyEvaluationMethod(executionCourseFrom, executionCourseTo);
-        
+
     }
 
-    private void copySectionsAndItems(ISite siteFrom, ISite siteTo) throws DomainException {        
-        
+    private void copySectionsAndItems(ISite siteFrom, ISite siteTo) throws DomainException {
+
         deleteSectionsAndItemsIfExistFrom(siteTo);
-        
+
         if (siteFrom.getAssociatedSectionsCount() > 0) {
             Iterator associatedSections = siteFrom.getAssociatedSectionsIterator();
-            while (associatedSections.hasNext()) {                
+            while (associatedSections.hasNext()) {
                 ISection sectionFrom = (ISection) associatedSections.next();
                 if (sectionFrom.getSuperiorSection() == null) {
-                    ISection sectionTo = siteTo.createSection(sectionFrom.getName(), null, sectionFrom.getSectionOrder());
+                    ISection sectionTo = siteTo.createSection(sectionFrom.getName(), null, sectionFrom
+                            .getSectionOrder());
                     copyItemsFrom(sectionFrom, sectionTo);
                     copySubSectionsAndItemsFrom(sectionFrom, sectionTo, siteTo);
                 }
@@ -79,28 +85,31 @@ public class CopySiteExecutionCourse implements IService {
         }
     }
 
-    private void copySubSectionsAndItemsFrom(ISection sectionFrom, ISection sectionTo, ISite siteTo) {                
+    private void copySubSectionsAndItemsFrom(ISection sectionFrom, ISection sectionTo, ISite siteTo)
+            throws DomainException {
         if (sectionFrom.getAssociatedSectionsCount() > 0) {
             Iterator associatedSections = sectionFrom.getAssociatedSectionsIterator();
-            while (associatedSections.hasNext()) {                
+            while (associatedSections.hasNext()) {
                 ISection subSectionFrom = (ISection) associatedSections.next();
                 if (subSectionFrom.getSuperiorSection() != null) {
-                    ISection subSectionTo = siteTo.createSection(subSectionFrom.getName(), sectionTo, subSectionFrom.getSectionOrder());
+                    ISection subSectionTo = siteTo.createSection(subSectionFrom.getName(), sectionTo,
+                            subSectionFrom.getSectionOrder());
                     copyItemsFrom(subSectionFrom, subSectionTo);
                     copySubSectionsAndItemsFrom(subSectionFrom, subSectionTo, siteTo);
                 }
             }
-        }                
+        }
     }
 
-    private void copyItemsFrom(ISection sectionFrom, ISection sectionTo) {
+    private void copyItemsFrom(ISection sectionFrom, ISection sectionTo) throws DomainException {
         if (sectionFrom.getAssociatedItemsCount() > 0) {
             Iterator associatedItems = sectionFrom.getAssociatedItemsIterator();
             while (associatedItems.hasNext()) {
                 IItem item = (IItem) associatedItems.next();
-                //TODO: sectionTo.insertItem(item.getName(), item.getInformation(), item.getUrgent(), item.getItemOrder());
+                sectionTo.insertItem(item.getName(), item.getInformation(), item.getUrgent(), item
+                        .getItemOrder());
             }
-        }        
+        }
     }
 
     private void deleteSectionsAndItemsIfExistFrom(ISite siteTo) throws DomainException {
@@ -109,24 +118,31 @@ public class CopySiteExecutionCourse implements IService {
             while (associatedSections.hasNext()) {
                 ISection section = (ISection) associatedSections.next();
                 section.delete();
-            }            
-        }        
-    }
-
-    private void copyBibliographicReference(final IExecutionCourse executionCourseFrom, IExecutionCourse executionCourseTo) {
-        if (executionCourseFrom.getAssociatedBibliographicReferencesCount() > 0) {
-            Iterator bibliographicReferences = executionCourseFrom.getAssociatedBibliographicReferencesIterator();
-            while (bibliographicReferences.hasNext()) {
-                IBibliographicReference bibliographicReference = (IBibliographicReference) bibliographicReferences.next();
-                executionCourseTo.createBibliographicReference(bibliographicReference.getTitle(), bibliographicReference.getAuthors(), bibliographicReference.getReference(), bibliographicReference.getYear(), bibliographicReference.getOptional());
             }
-        }        
+        }
     }
 
-    private void copyEvaluationMethod(final IExecutionCourse executionCourseFrom, IExecutionCourse executionCourseTo) {
+    private void copyBibliographicReference(final IExecutionCourse executionCourseFrom,
+            IExecutionCourse executionCourseTo) {
+        if (executionCourseFrom.getAssociatedBibliographicReferencesCount() > 0) {
+            Iterator bibliographicReferences = executionCourseFrom
+                    .getAssociatedBibliographicReferencesIterator();
+            while (bibliographicReferences.hasNext()) {
+                IBibliographicReference bibliographicReference = (IBibliographicReference) bibliographicReferences
+                        .next();
+                executionCourseTo.createBibliographicReference(bibliographicReference.getTitle(),
+                        bibliographicReference.getAuthors(), bibliographicReference.getReference(),
+                        bibliographicReference.getYear(), bibliographicReference.getOptional());
+            }
+        }
+    }
+
+    private void copyEvaluationMethod(final IExecutionCourse executionCourseFrom,
+            IExecutionCourse executionCourseTo) {
         if (executionCourseFrom.getEvaluationMethod() != null) {
             IEvaluationMethod evaluationMethod = executionCourseFrom.getEvaluationMethod();
-            executionCourseTo.createEvaluationMethod(evaluationMethod.getEvaluationElements(), evaluationMethod.getEvaluationElementsEn());
+            executionCourseTo.createEvaluationMethod(evaluationMethod.getEvaluationElements(),
+                    evaluationMethod.getEvaluationElementsEn());
         }
     }
 }
