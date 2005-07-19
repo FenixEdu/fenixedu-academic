419464d7d5ad72f8622c36915e19e950ea69e112
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-19 16:13:41 +0000

    removed unused code

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/EditOldPublication.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/EditOldPublication.java
@@ -7,7 +7,6 @@ package net.sourceforge.fenixedu.applicationTier.Servico.teacher;
 import net.sourceforge.fenixedu.applicationTier.Servico.framework.EditDomainObjectService;
 import net.sourceforge.fenixedu.dataTransferObject.InfoObject;
 import net.sourceforge.fenixedu.dataTransferObject.teacher.InfoOldPublication;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
 import net.sourceforge.fenixedu.domain.IDomainObject;
 import net.sourceforge.fenixedu.domain.ITeacher;
 import net.sourceforge.fenixedu.domain.teacher.IOldPublication;
@@ -16,73 +15,43 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentTeacher;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
-import net.sourceforge.fenixedu.persistenceTier.teacher.IPersistentOldPublication;
 
 /**
  * @author Leonor Almeida
  * @author Sergio Montelobo
- *  
+ * 
  */
 public class EditOldPublication extends EditDomainObjectService {
 
-    private static EditOldPublication service = new EditOldPublication();
-
-    public static EditOldPublication getService() {
-        return service;
-    }
-
-    /**
-     *  
-     */
-    private EditOldPublication() {
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see ServidorAplicacao.IServico#getNome()
-     */
-    public String getNome() {
-        return "EditOldPublication";
-    }
-
     protected IPersistentObject getIPersistentObject(ISuportePersistente sp) {
-        IPersistentOldPublication persistentOldPublication = sp.getIPersistentOldPublication();
-        return persistentOldPublication;
+        return sp.getIPersistentOldPublication();
     }
 
-    protected IDomainObject clone2DomainObject(InfoObject infoObject) {
-        IOldPublication oldPublication = Cloner
-                .copyInfoOldPublication2IOldPublication((InfoOldPublication) infoObject);
-        return oldPublication;
+    @Override
+    protected void copyInformationFromIntoToDomain(ISuportePersistente sp, InfoObject infoObject,
+            IDomainObject domainObject) throws ExcepcaoPersistencia {
+        InfoOldPublication infoOldPublication = (InfoOldPublication) infoObject;
+        IOldPublication oldPublication = (OldPublication) domainObject;
+
+        oldPublication.setLastModificationDate(infoOldPublication.getLastModificationDate());
+        oldPublication.setOldPublicationType(infoOldPublication.getOldPublicationType());
+        oldPublication.setPublication(infoOldPublication.getPublication());
+
+        IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
+        ITeacher teacher = persistentTeacher.readByNumber(infoOldPublication.getInfoTeacher()
+                .getTeacherNumber());
+        oldPublication.setTeacher(teacher);
+        oldPublication.setKeyTeacher(teacher.getIdInternal());
     }
 
-	@Override
-	protected void copyInformationFromIntoToDomain(ISuportePersistente sp, InfoObject infoObject, IDomainObject domainObject) throws ExcepcaoPersistencia {
-		InfoOldPublication infoOldPublication = (InfoOldPublication)infoObject;
-		IOldPublication oldPublication = (OldPublication) domainObject;
-		
-		oldPublication.setLastModificationDate(infoOldPublication.getLastModificationDate());
-		oldPublication.setOldPublicationType(infoOldPublication.getOldPublicationType());
-		oldPublication.setPublication(infoOldPublication.getPublication());
-		
-		IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
-        ITeacher teacher = persistentTeacher.readByNumber(infoOldPublication.getInfoTeacher().getTeacherNumber());
-		oldPublication.setTeacher(teacher);
-		oldPublication.setKeyTeacher(teacher.getIdInternal());
-		
-		
-	}
+    @Override
+    protected IDomainObject createNewDomainObject(InfoObject infoObject) {
+        return null;
+    }
 
-	@Override
-	protected IDomainObject createNewDomainObject(InfoObject infoObject) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    protected Class getDomainObjectClass() {
+        return null;
+    }
 
-	@Override
-	protected Class getDomainObjectClass() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
