0be736959f3ab6b9c4144f59fa296e156eb3808d
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-20 13:28:45 +0000

    removed commented code

--- src/net/sourceforge/fenixedu/applicationTier/Servico/publico/ReadExecutionCourse.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/publico/ReadExecutionCourse.java
@@ -1,8 +1,5 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.publico;
 
-import net.sourceforge.fenixedu.applicationTier.IServico;
-import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionPeriod;
 import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
@@ -11,61 +8,25 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionCourse;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author João Mota
  */
-public class ReadExecutionCourse implements IServico {
+public class ReadExecutionCourse implements IService {
 
-    private static ReadExecutionCourse _servico = new ReadExecutionCourse();
+    public Object run(InfoExecutionPeriod infoExecutionPeriod, String code) throws ExcepcaoPersistencia {
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final IPersistentExecutionCourse executionCourseDAO = sp.getIPersistentExecutionCourse();
 
-    /**
-     * The actor of this class.
-     */
+        IExecutionCourse iExecCourse = executionCourseDAO
+                .readByExecutionCourseInitialsAndExecutionPeriodId(code, infoExecutionPeriod
+                        .getIdInternal());
 
-    private ReadExecutionCourse() {
-
-    }
-
-    /**
-     * Returns Service Name
-     */
-    public String getNome() {
-        return "ReadExecutionCourse";
-    }
-
-    /**
-     * Returns the _servico.
-     * 
-     * @return ReadExecutionCourse
-     */
-    public static ReadExecutionCourse getService() {
-        return _servico;
-    }
-
-    public Object run(InfoExecutionPeriod infoExecutionPeriod, String code) throws ExcepcaoInexistente,
-            FenixServiceException {
-
-        IExecutionCourse iExecCourse = null;
-        InfoExecutionCourse infoExecCourse = null;
-
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentExecutionCourse executionCourseDAO = sp.getIPersistentExecutionCourse();
-//            IExecutionPeriod executionPeriod = Cloner
-//                    .copyInfoExecutionPeriod2IExecutionPeriod(infoExecutionPeriod);
-            iExecCourse = executionCourseDAO.readByExecutionCourseInitialsAndExecutionPeriodId(code,
-                    infoExecutionPeriod.getIdInternal());
-            if (iExecCourse != null)
-                infoExecCourse = (InfoExecutionCourse) Cloner.get(iExecCourse);
-        } catch (ExcepcaoPersistencia ex) {
-            ex.printStackTrace();
-            FenixServiceException newEx = new FenixServiceException("");
-            newEx.fillInStackTrace();
-            throw newEx;
+        if (iExecCourse != null) {
+            return (InfoExecutionCourse) Cloner.get(iExecCourse);
         }
-
-        return infoExecCourse;
+        return null;
     }
 
 }
