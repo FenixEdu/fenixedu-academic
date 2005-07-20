0027a0cb9250f042756732d31e67760482fd9aba
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-20 16:26:44 +0000

    removed call to Cloner.copyInfoCandicacy2ICandidacy

--- src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/WriteCandidacy.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/WriteCandidacy.java
@@ -4,15 +4,19 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Servico.Seminaries;
 
-import java.util.Iterator;
+import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoCandidacy;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
+import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoCaseStudyChoice;
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.ICurricularCourse;
 import net.sourceforge.fenixedu.domain.IStudent;
 import net.sourceforge.fenixedu.domain.Student;
+import net.sourceforge.fenixedu.domain.Seminaries.Candidacy;
+import net.sourceforge.fenixedu.domain.Seminaries.CaseStudy;
+import net.sourceforge.fenixedu.domain.Seminaries.CaseStudyChoice;
 import net.sourceforge.fenixedu.domain.Seminaries.ICandidacy;
+import net.sourceforge.fenixedu.domain.Seminaries.ICaseStudy;
 import net.sourceforge.fenixedu.domain.Seminaries.ICaseStudyChoice;
 import net.sourceforge.fenixedu.domain.Seminaries.IModality;
 import net.sourceforge.fenixedu.domain.Seminaries.ISeminary;
@@ -26,10 +30,10 @@ import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminary;
 import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminaryCandidacy;
+import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminaryCaseStudy;
 import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminaryCaseStudyChoice;
 import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminaryModality;
 import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminaryTheme;
-import net.sourceforge.fenixedu.presentationTier.Action.Seminaries.Exceptions.BDException;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
@@ -38,64 +42,62 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
  */
 public class WriteCandidacy implements IService {
 
-    /**
-     * The actor of this class.
-     */
-    public WriteCandidacy() {
-    }
+    public void run(InfoCandidacy infoCandidacy) throws ExcepcaoPersistencia {
+        final ISuportePersistente persistenceSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-    public void run(InfoCandidacy infoCandidacy) throws BDException {
-        try {
-            ISuportePersistente persistenceSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentSeminaryCandidacy persistentCandidacy = persistenceSupport
-                    .getIPersistentSeminaryCandidacy();
-            IPersistentSeminaryModality persistentModality = persistenceSupport
-                    .getIPersistentSeminaryModality();
-            IPersistentSeminaryCaseStudyChoice persistentChoice = persistenceSupport
-                    .getIPersistentSeminaryCaseStudyChoice();
+        final IPersistentSeminaryCandidacy persistentCandidacy = persistenceSupport.getIPersistentSeminaryCandidacy();
+        ICandidacy candidacy = new Candidacy();
+        persistentCandidacy.simpleLockWrite(candidacy);
+        candidacy.setApproved(infoCandidacy.getApproved());
+        candidacy.setMotivation(infoCandidacy.getMotivation());
+        
+        // Modality
+        final IPersistentSeminaryModality persistentModality = persistenceSupport.getIPersistentSeminaryModality();
+        final IModality modality = persistentModality.readByName("Completa");
+        candidacy.setModality(modality);
 
-            IPersistentStudent persistentStudent = persistenceSupport.getIPersistentStudent();
-            IPersistentSeminary persistentSeminary = persistenceSupport.getIPersistentSeminary();
-            IPersistentCurricularCourse persistentCurricularCourse = persistenceSupport
-                    .getIPersistentCurricularCourse();
-            IPersistentSeminaryTheme persistentTheme = persistenceSupport.getIPersistentSeminaryTheme();
+        // Student
+        final IPersistentStudent persistentStudent = persistenceSupport.getIPersistentStudent();
+        final IStudent readStudent = (IStudent) persistentStudent.readByOID(Student.class, infoCandidacy.getInfoStudent().getIdInternal());
+        candidacy.setStudent(readStudent);
 
-            ICandidacy candidacy = Cloner.copyInfoCandicacy2ICandidacy(infoCandidacy);
+        // Seminary
+        final IPersistentSeminary persistentSeminary = persistenceSupport.getIPersistentSeminary();
+        final ISeminary readSeminary = (ISeminary) persistentSeminary.readByOID(Seminary.class, infoCandidacy.getInfoSeminary().getIdInternal());
+        candidacy.setSeminary(readSeminary);
+        
+        // Curricular Course
+        final IPersistentCurricularCourse persistentCurricularCourse = persistenceSupport.getIPersistentCurricularCourse();
+        final ICurricularCourse readCurricularCourse = (ICurricularCourse) persistentCurricularCourse.readByOID(CurricularCourse.class, infoCandidacy.getCurricularCourse().getIdInternal());
+        candidacy.setCurricularCourse(readCurricularCourse);
 
-            IStudent readStudent = (IStudent) persistentStudent.readByOID(Student.class, candidacy
-                    .getStudent().getIdInternal());
-            ISeminary readSeminary = (ISeminary) persistentSeminary.readByOID(Seminary.class, candidacy
-                    .getSeminary().getIdInternal());
-            ICurricularCourse readCurricularCourse = (ICurricularCourse) persistentCurricularCourse
-                    .readByOID(CurricularCourse.class, candidacy.getCurricularCourse().getIdInternal());
+        // Theme
+        if (modality.getIdInternal().equals(infoCandidacy.getInfoModality().getIdInternal())) {
+            candidacy.setTheme(null);
+        } else {
+            final IPersistentSeminaryTheme persistentTheme = persistenceSupport.getIPersistentSeminaryTheme();
+            final ITheme readTheme = (ITheme) persistentTheme.readByOID(Theme.class, infoCandidacy.getTheme().getIdInternal());
+            candidacy.setTheme(readTheme);
+        }
+        if (!infoCandidacy.getInfoSeminary().getHasThemes().booleanValue()) {
+            candidacy.setTheme(null);
+        }
 
-            IModality modality = persistentModality.readByName("Completa");
-            persistentCandidacy.simpleLockWrite(candidacy);
-            candidacy.setModality(modality);
-            candidacy.setStudent(readStudent);
-            candidacy.setSeminary(readSeminary);
-            candidacy.setCurricularCourse(readCurricularCourse);
+        // Seminary Case Study Choices
+        final IPersistentSeminaryCaseStudyChoice persistentChoice = persistenceSupport.getIPersistentSeminaryCaseStudyChoice();
+        final IPersistentSeminaryCaseStudy persistentSeminaryCaseStudy = persistenceSupport.getIPersistentSeminaryCaseStudy();
+        for (InfoCaseStudyChoice infoCaseStudyChoice : (List<InfoCaseStudyChoice>) infoCandidacy.getCaseStudyChoices()) {
+            final ICaseStudyChoice caseStudyChoice = new CaseStudyChoice();
+            persistentChoice.simpleLockWrite(caseStudyChoice);
+            
+            caseStudyChoice.setOrder(infoCaseStudyChoice.getOrder());
 
-            if (modality.getIdInternal().equals(infoCandidacy.getInfoModality().getIdInternal())) {
-                candidacy.setThemeIdInternal(null);
-                candidacy.setTheme(null);
-            } else {
-                ITheme readTheme = (ITheme) persistentTheme.readByOID(Theme.class,
-                        candidacy.getTheme().getIdInternal());
-                candidacy.setTheme(readTheme);
-            }
-            if (!infoCandidacy.getInfoSeminary().getHasThemes().booleanValue())
-            {
-                candidacy.setThemeIdInternal(null);
-                candidacy.setTheme(null);                
-            }
-            for (Iterator iter = candidacy.getCaseStudyChoices().iterator(); iter.hasNext();) {
-                ICaseStudyChoice element = (ICaseStudyChoice) iter.next();
-                persistentChoice.simpleLockWrite(element);
-                element.setCandidacy(candidacy);
-            }
-        } catch (ExcepcaoPersistencia ex) {
-            throw new BDException("Got an error while trying to write a candidacy to the database", ex);
+            final ICaseStudy caseStudy = (ICaseStudy) persistentSeminaryCaseStudy.readByOID(CaseStudy.class, infoCaseStudyChoice.getCaseStudy().getIdInternal());
+            caseStudyChoice.setCaseStudy(caseStudy);
+
+            caseStudyChoice.setCandidacy(candidacy);
+            candidacy.addCaseStudyChoices(caseStudyChoice);
         }
     }
+
 }
