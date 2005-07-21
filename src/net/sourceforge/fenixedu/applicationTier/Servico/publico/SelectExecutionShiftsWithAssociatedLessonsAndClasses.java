0a571b0bef4b61d5c784b6c6f0763fd3191f6e4e
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-21 16:14:54 +0000

    removed call to Cloner methods

--- src/net/sourceforge/fenixedu/applicationTier/Servico/publico/SelectExecutionShiftsWithAssociatedLessonsAndClasses.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/publico/SelectExecutionShiftsWithAssociatedLessonsAndClasses.java
@@ -3,100 +3,66 @@ package net.sourceforge.fenixedu.applicationTier.Servico.publico;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.IServico;
 import net.sourceforge.fenixedu.dataTransferObject.InfoClass;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
 import net.sourceforge.fenixedu.dataTransferObject.InfoLesson;
+import net.sourceforge.fenixedu.dataTransferObject.InfoLessonWithInfoRoomAndInfoRoomOccupationAndInfoPeriod;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShift;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShiftWithAssociatedInfoClassesAndInfoLessons;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
-import net.sourceforge.fenixedu.domain.IExecutionCourse;
 import net.sourceforge.fenixedu.domain.ILesson;
 import net.sourceforge.fenixedu.domain.ISchoolClassShift;
 import net.sourceforge.fenixedu.domain.IShift;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author João Mota
- *  
+ * 
  */
-public class SelectExecutionShiftsWithAssociatedLessonsAndClasses implements IServico {
+public class SelectExecutionShiftsWithAssociatedLessonsAndClasses implements IService {
 
-    private static SelectExecutionShiftsWithAssociatedLessonsAndClasses _servico = new SelectExecutionShiftsWithAssociatedLessonsAndClasses();
+    public Object run(InfoExecutionCourse infoExecutionCourse) throws ExcepcaoPersistencia {
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-    /**
-     * The actor of this class.
-     */
-
-    private SelectExecutionShiftsWithAssociatedLessonsAndClasses() {
-
-    }
-
-    /**
-     * Returns Service Name
-     */
-    public String getNome() {
-        return "SelectExecutionShiftsWithAssociatedLessonsAndClasses";
-    }
-
-    /**
-     * Returns the _servico.
-     * 
-     * @return SelectShifts
-     */
-    public static SelectExecutionShiftsWithAssociatedLessonsAndClasses getService() {
-        return _servico;
-    }
-
-    public Object run(InfoExecutionCourse infoExecutionCourse) {
-
-        List shiftsWithAssociatedClassesAndLessons = new ArrayList();
-
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IExecutionCourse disciplinaExecucao = Cloner
-                    .copyInfoExecutionCourse2ExecutionCourse(infoExecutionCourse);
-            List shifts = sp.getITurnoPersistente().readByExecutionCourse(disciplinaExecucao.getIdInternal());
-
-            if (shifts == null || shifts.isEmpty()) {
-
-            } else {
-
-                for (int i = 0; i < shifts.size(); i++) {
-                    IShift shift = (IShift) shifts.get(i);
-                    InfoShiftWithAssociatedInfoClassesAndInfoLessons shiftWithAssociatedClassesAndLessons = new InfoShiftWithAssociatedInfoClassesAndInfoLessons(
-							InfoShift.newInfoFromDomain(shift), null, null);
-
-                    //List lessons =
-                    // sp.getITurnoAulaPersistente().readByShift(shift);
-                    List lessons = shift.getAssociatedLessons();
-                    List infoLessons = new ArrayList();
-                    List classesShifts = sp.getITurmaTurnoPersistente().readClassesWithShift(shift.getIdInternal());
-                    List infoClasses = new ArrayList();
-
-                    for (int j = 0; j < lessons.size(); j++) {
-                        InfoShift infoShift = InfoShift.newInfoFromDomain(shift);
-                        InfoLesson infoLesson = Cloner.copyILesson2InfoLesson((ILesson) lessons.get(j));
-                        infoLesson.setInfoShift(infoShift);
-                        infoLessons.add(infoLesson);
-                    }
-                    shiftWithAssociatedClassesAndLessons.setInfoLessons(infoLessons);
-
-                    for (int j = 0; j < classesShifts.size(); j++)
-                        infoClasses.add(InfoClass.newInfoFromDomain(
-								((ISchoolClassShift) classesShifts.get(j))
-                                .getTurma()));
-
-                    shiftWithAssociatedClassesAndLessons.setInfoClasses(infoClasses);
-
-                    shiftsWithAssociatedClassesAndLessons.add(shiftWithAssociatedClassesAndLessons);
-                }
-            }
-
-        } catch (ExcepcaoPersistencia e) {
+        final List<IShift> shifts = sp.getITurnoPersistente().readByExecutionCourse(
+                infoExecutionCourse.getIdInternal());
+        if (shifts == null || shifts.isEmpty()) {
+            return null;
         }
+
+        List<InfoShiftWithAssociatedInfoClassesAndInfoLessons> shiftsWithAssociatedClassesAndLessons = new ArrayList<InfoShiftWithAssociatedInfoClassesAndInfoLessons>();
+        for (IShift shift : shifts) {
+            InfoShiftWithAssociatedInfoClassesAndInfoLessons shiftWithAssociatedClassesAndLessons = new InfoShiftWithAssociatedInfoClassesAndInfoLessons();
+
+            // InfoShift
+            InfoShift infoShift = InfoShift.newInfoFromDomain(shift);
+            shiftWithAssociatedClassesAndLessons.setInfoShift(infoShift);
+
+            // InfoClasses
+            List<ISchoolClassShift> classesShifts = sp.getITurmaTurnoPersistente().readClassesWithShift(
+                    shift.getIdInternal());
+            List<InfoClass> infoClasses = new ArrayList<InfoClass>();
+            for (ISchoolClassShift schoolClassShift : classesShifts) {
+                infoClasses.add(InfoClass.newInfoFromDomain(schoolClassShift.getTurma()));
+            }
+            shiftWithAssociatedClassesAndLessons.setInfoClasses(infoClasses);
+
+            // InfoLessons
+            List<ILesson> lessons = shift.getAssociatedLessons();
+            List<InfoLesson> infoLessons = new ArrayList<InfoLesson>();
+            for (ILesson lesson : lessons) {
+                InfoLesson infoLesson = InfoLessonWithInfoRoomAndInfoRoomOccupationAndInfoPeriod
+                        .newInfoFromDomain(lesson);
+                infoLesson.setInfoShift(infoShift);
+                infoLessons.add(infoLesson);
+            }
+            shiftWithAssociatedClassesAndLessons.setInfoLessons(infoLessons);
+
+            shiftsWithAssociatedClassesAndLessons.add(shiftWithAssociatedClassesAndLessons);
+        }
+
         return shiftsWithAssociatedClassesAndLessons;
     }
 
