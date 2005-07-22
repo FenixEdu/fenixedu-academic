69af70d911a1c1e3a002f24ba8521b59e52294c7
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-22 14:05:40 +0000

    typifying return list

--- src/net/sourceforge/fenixedu/persistenceTier/ISalaPersistente.java
+++ src/net/sourceforge/fenixedu/persistenceTier/ISalaPersistente.java
@@ -16,17 +16,17 @@ import net.sourceforge.fenixedu.domain.IRoom;
 public interface ISalaPersistente extends IPersistentObject {
     public IRoom readByName(String nome) throws ExcepcaoPersistencia;
 
-    public List readAll() throws ExcepcaoPersistencia;
+    public List<IRoom> readAll() throws ExcepcaoPersistencia;
 
     public List readSalas(String nome, String edificio, Integer piso, Integer tipo,
             Integer capacidadeNormal, Integer capacidadeExame) throws ExcepcaoPersistencia;
 
     public List readAvailableRooms(Integer examOID) throws ExcepcaoPersistencia;
 
-    public List readForRoomReservation() throws ExcepcaoPersistencia;
+    public List<IRoom> readForRoomReservation() throws ExcepcaoPersistencia;
 
     public List readByPavillions(List pavillionsName) throws ExcepcaoPersistencia;
 
-    public List readByNormalCapacity(Integer capacity) throws ExcepcaoPersistencia;
+    public List<IRoom> readByNormalCapacity(Integer capacity) throws ExcepcaoPersistencia;
 
 }
