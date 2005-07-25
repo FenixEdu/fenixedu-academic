ed48dca371a8ae8d6b39ac508a4536019d0eb597
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-25 12:39:29 +0000

    removed call to Cloner.copyInfoExecutionPeriod2IExecutionPeriod

--- src/net/sourceforge/fenixedu/applicationTier/Servico/sop/LerAulasDeSalaEmSemestre.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/sop/LerAulasDeSalaEmSemestre.java
@@ -10,21 +10,14 @@ package net.sourceforge.fenixedu.applicationTier.Servico.sop;
  * @author tfc130
  */
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionPeriod;
 import net.sourceforge.fenixedu.dataTransferObject.InfoLesson;
 import net.sourceforge.fenixedu.dataTransferObject.InfoRoom;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShift;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
-import net.sourceforge.fenixedu.domain.ExecutionPeriod;
-import net.sourceforge.fenixedu.domain.IExecutionPeriod;
 import net.sourceforge.fenixedu.domain.ILesson;
-import net.sourceforge.fenixedu.domain.IShift;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IAulaPersistente;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionPeriod;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
@@ -33,36 +26,28 @@ public class LerAulasDeSalaEmSemestre implements IService {
 
     public List run(InfoExecutionPeriod infoExecutionPeriod, InfoRoom infoRoom, Integer executionPeriodId)
             throws ExcepcaoPersistencia {
-        List infoAulas = null;
-
-        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-        IPersistentExecutionPeriod persistentExecutionPeriod = sp.getIPersistentExecutionPeriod();
-        IAulaPersistente lessonDAO = sp.getIAulaPersistente();
-        IExecutionPeriod executionPeriod = null;
-        if (executionPeriodId != null) {
-            executionPeriod = (IExecutionPeriod) persistentExecutionPeriod.readByOID(
-                    ExecutionPeriod.class, executionPeriodId, false);
-        } else {
-            executionPeriod = Cloner.copyInfoExecutionPeriod2IExecutionPeriod(infoExecutionPeriod);
+        
+        if (executionPeriodId == null) {
+            executionPeriodId = infoExecutionPeriod.getIdInternal();
         }
 
-        List lessonList = lessonDAO.readByRoomAndExecutionPeriod(infoRoom.getIdInternal(),
-                executionPeriod.getIdInternal());
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final List<ILesson> lessonList = sp.getIAulaPersistente().readByRoomAndExecutionPeriod(infoRoom.getIdInternal(), executionPeriodId);
 
-        Iterator iterator = lessonList.iterator();
-        infoAulas = new ArrayList();
-        while (iterator.hasNext()) {
-            ILesson elem = (ILesson) iterator.next();
-            InfoLesson infoLesson = Cloner.copyILesson2InfoLesson(elem);
-            IShift shift = elem.getShift();
-            if (shift == null) {
+        List<InfoLesson> infoAulas = new ArrayList<InfoLesson>();
+        for (ILesson elem : lessonList) {
+            if (elem.getShift() == null) {
                 continue;
             }
-            InfoShift infoShift = InfoShift.newInfoFromDomain(shift);
+
+            InfoLesson infoLesson = InfoLesson.newInfoFromDomain(elem);
+            
+            InfoShift infoShift = InfoShift.newInfoFromDomain(elem.getShift());
             infoLesson.setInfoShift(infoShift);
 
             infoAulas.add(infoLesson);
         }
+        
         return infoAulas;
     }
 
