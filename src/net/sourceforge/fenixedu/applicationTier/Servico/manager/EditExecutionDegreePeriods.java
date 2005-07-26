76afe1db3ebf46fab8191f702e40f1d2db20232c
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-26 12:33:39 +0000

    confirmed correct creation of new domain object by calling DomainFactory instead of the constructor and removing existing simpleLockWrite

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/EditExecutionDegreePeriods.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/EditExecutionDegreePeriods.java
@@ -7,11 +7,9 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.IServico;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.ExistingServiceException;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegree;
 import net.sourceforge.fenixedu.dataTransferObject.InfoPeriod;
+import net.sourceforge.fenixedu.domain.DomainFactory;
 import net.sourceforge.fenixedu.domain.ExecutionDegree;
 import net.sourceforge.fenixedu.domain.IExecutionDegree;
 import net.sourceforge.fenixedu.domain.IPeriod;
@@ -21,120 +19,93 @@ import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionDegree;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentPeriod;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-import net.sourceforge.fenixedu.persistenceTier.exceptions.ExistingPersistentException;
+import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
  * @author Ana e Ricardo
  */
-public class EditExecutionDegreePeriods implements IServico {
+public class EditExecutionDegreePeriods implements IService {
 
-    private static EditExecutionDegreePeriods service = new EditExecutionDegreePeriods();
+    public void run(InfoExecutionDegree infoExecutionDegree) throws ExcepcaoPersistencia {
 
-    public static EditExecutionDegreePeriods getService() {
-        return service;
+        final ISuportePersistente persistentSuport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
+
+        final IPersistentExecutionDegree persistentExecutionDegree = persistentSuport
+                .getIPersistentExecutionDegree();
+        IExecutionDegree oldExecutionDegree = (IExecutionDegree) persistentExecutionDegree.readByOID(
+                ExecutionDegree.class, infoExecutionDegree.getIdInternal(), false);
+
+        InfoPeriod infoPeriodExamsFirstSemester = infoExecutionDegree.getInfoPeriodExamsFirstSemester();
+        IPeriod periodExamsFirstSemester = setCompositePeriod(infoPeriodExamsFirstSemester);
+
+        InfoPeriod infoPeriodExamsSecondSemester = infoExecutionDegree
+                .getInfoPeriodExamsSecondSemester();
+        IPeriod periodExamsSecondSemester = setCompositePeriod(infoPeriodExamsSecondSemester);
+
+        InfoPeriod infoPeriodLessonsFirstSemester = infoExecutionDegree
+                .getInfoPeriodLessonsFirstSemester();
+        IPeriod periodLessonsFirstSemester = setCompositePeriod(infoPeriodLessonsFirstSemester);
+
+        InfoPeriod infoPeriodLessonsSecondSemester = infoExecutionDegree
+                .getInfoPeriodLessonsSecondSemester();
+        IPeriod periodLessonsSecondSemester = setCompositePeriod(infoPeriodLessonsSecondSemester);
+
+        persistentExecutionDegree.simpleLockWrite(oldExecutionDegree);
+        oldExecutionDegree.setPeriodLessonsFirstSemester(periodLessonsFirstSemester);
+        oldExecutionDegree.setPeriodLessonsSecondSemester(periodLessonsSecondSemester);
+        oldExecutionDegree.setPeriodExamsFirstSemester(periodExamsFirstSemester);
+        oldExecutionDegree.setPeriodExamsSecondSemester(periodExamsSecondSemester);
     }
 
-    private EditExecutionDegreePeriods() {
-    }
+    // retorna o primeiro period do executiondegree
+    private IPeriod setCompositePeriod(InfoPeriod infoPeriod) throws ExcepcaoPersistencia {
+        List<InfoPeriod> infoPeriodList = new ArrayList<InfoPeriod>();
 
-    public final String getNome() {
-        return "EditExecutionDegreePeriods";
-    }
-
-    public void run(InfoExecutionDegree infoExecutionDegree) throws FenixServiceException {
-
-        IPersistentExecutionDegree persistentExecutionDegree = null;
-
-        try {
-            ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            persistentExecutionDegree = persistentSuport.getIPersistentExecutionDegree();
-
-            IExecutionDegree oldExecutionDegree = (IExecutionDegree) persistentExecutionDegree.readByOID(
-                    ExecutionDegree.class, infoExecutionDegree.getIdInternal(), false);
-
-            InfoPeriod infoPeriodExamsFirstSemester = infoExecutionDegree
-                    .getInfoPeriodExamsFirstSemester();
-            IPeriod periodExamsFirstSemester = setCompositePeriod(infoPeriodExamsFirstSemester);
-
-            InfoPeriod infoPeriodExamsSecondSemester = infoExecutionDegree
-                    .getInfoPeriodExamsSecondSemester();
-            IPeriod periodExamsSecondSemester = setCompositePeriod(infoPeriodExamsSecondSemester);
-
-            InfoPeriod infoPeriodLessonsFirstSemester = infoExecutionDegree
-                    .getInfoPeriodLessonsFirstSemester();
-            IPeriod periodLessonsFirstSemester = setCompositePeriod(infoPeriodLessonsFirstSemester);
-
-            InfoPeriod infoPeriodLessonsSecondSemester = infoExecutionDegree
-                    .getInfoPeriodLessonsSecondSemester();
-            IPeriod periodLessonsSecondSemester = setCompositePeriod(infoPeriodLessonsSecondSemester);
-
-            persistentExecutionDegree.simpleLockWrite(oldExecutionDegree);
-            oldExecutionDegree.setPeriodLessonsFirstSemester(periodLessonsFirstSemester);
-            oldExecutionDegree.setPeriodLessonsSecondSemester(periodLessonsSecondSemester);
-            oldExecutionDegree.setPeriodExamsFirstSemester(periodExamsFirstSemester);
-            oldExecutionDegree.setPeriodExamsSecondSemester(periodExamsSecondSemester);
-
-        } catch (ExistingPersistentException ex) {
-            throw new ExistingServiceException(ex);
-        } catch (ExcepcaoPersistencia excepcaoPersistencia) {
-            throw new FenixServiceException(excepcaoPersistencia);
+        infoPeriodList.add(infoPeriod);
+        while (infoPeriod.getNextPeriod() != null) {
+            infoPeriodList.add(infoPeriod.getNextPeriod());
+            infoPeriod = infoPeriod.getNextPeriod();
         }
-    }
 
-    //retorna o primeiro period do executiondegree
-    private IPeriod setCompositePeriod(InfoPeriod infoPeriod) throws FenixServiceException {
-        try {
-            ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentPeriod periodDAO = persistentSuport.getIPersistentPeriod();
-            List infoPeriodList = new ArrayList();
+        // inicializacao
+        int infoPeriodListSize = infoPeriodList.size();
 
-            infoPeriodList.add(infoPeriod);
+        InfoPeriod infoPeriodNew = infoPeriodList.get(infoPeriodListSize - 1);
 
-            while (infoPeriod.getNextPeriod() != null) {
-                infoPeriodList.add(infoPeriod.getNextPeriod());
-                infoPeriod = infoPeriod.getNextPeriod();
-            }
+        final ISuportePersistente persistentSuport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
+        final IPersistentPeriod periodDAO = persistentSuport.getIPersistentPeriod();
+        IPeriod period = (IPeriod) periodDAO.readByCalendarAndNextPeriod(infoPeriodNew.getStartDate(),
+                infoPeriodNew.getEndDate(), null);
 
-            //inicializacao
-            int infoPeriodListSize = infoPeriodList.size();
+        if (period == null) {
+            Calendar startDate = infoPeriodNew.getStartDate();
+            Calendar endDate = infoPeriodNew.getEndDate();
+            period = DomainFactory.makePeriod(startDate, endDate);
+        }
 
-            InfoPeriod infoPeriodNew = (InfoPeriod) infoPeriodList.get(infoPeriodListSize - 1);
+        // iteracoes
+        for (int i = infoPeriodListSize - 2; i >= 0; i--) {
+            Integer keyNextPeriod = period.getIdInternal();
 
-            IPeriod period = (IPeriod) periodDAO.readByCalendarAndNextPeriod(infoPeriodNew
-                    .getStartDate(), infoPeriodNew.getEndDate(), null);
+            IPeriod nextPeriod = period;
+
+            infoPeriodNew = infoPeriodList.get(i);
+
+            period = (IPeriod) periodDAO.readByCalendarAndNextPeriod(infoPeriodNew.getStartDate(),
+                    infoPeriodNew.getEndDate(), keyNextPeriod);
 
             if (period == null) {
                 Calendar startDate = infoPeriodNew.getStartDate();
                 Calendar endDate = infoPeriodNew.getEndDate();
                 period = new Period(startDate, endDate);
                 periodDAO.simpleLockWrite(period);
+                period.setNextPeriod(nextPeriod);
             }
-
-            //iteracoes
-            for (int i = infoPeriodListSize - 2; i >= 0; i--) {
-                Integer keyNextPeriod = period.getIdInternal();
-
-                IPeriod nextPeriod = period;
-
-                infoPeriodNew = (InfoPeriod) infoPeriodList.get(i);
-
-                period = (IPeriod) periodDAO.readByCalendarAndNextPeriod(infoPeriodNew.getStartDate(),
-                        infoPeriodNew.getEndDate(), keyNextPeriod);
-
-                if (period == null) {
-                    Calendar startDate = infoPeriodNew.getStartDate();
-                    Calendar endDate = infoPeriodNew.getEndDate();
-                    period = new Period(startDate, endDate);
-                    periodDAO.simpleLockWrite(period);
-                    period.setNextPeriod(nextPeriod);
-                }
-            }
-
-            return period;
-
-        } catch (ExcepcaoPersistencia excepcaoPersistencia) {
-            throw new FenixServiceException(excepcaoPersistencia);
         }
+
+        return period;
     }
 
 }
