12a44b701aff8ca8cdf81d71baed45e26a60c9b7
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-26 15:33:18 +0000

    - confirmed correct creation of new domain object by calling DomainFactory instead of the constructor and removing existing simpleLockWrite - removed set on Key attributes

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/onlineTests/InsertTestAsNewTest.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/onlineTests/InsertTestAsNewTest.java
@@ -11,10 +11,10 @@ import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
+import net.sourceforge.fenixedu.domain.DomainFactory;
 import net.sourceforge.fenixedu.domain.onlineTests.ITest;
 import net.sourceforge.fenixedu.domain.onlineTests.ITestQuestion;
 import net.sourceforge.fenixedu.domain.onlineTests.Test;
-import net.sourceforge.fenixedu.domain.onlineTests.TestQuestion;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
@@ -35,15 +35,13 @@ public class InsertTestAsNewTest implements IService {
             ITest oldTest = (ITest) persistentTest.readByOID(Test.class, oldTestId);
             if (oldTest == null)
                 throw new InvalidArgumentsServiceException();
-            ITest test = new Test();
-            persistentTest.simpleLockWrite(test);
+            ITest test = DomainFactory.makeTest();
             test.setTitle(oldTest.getTitle().concat(" (Duplicado)"));
             test.setInformation(oldTest.getInformation());
             test.setNumberOfQuestions(oldTest.getNumberOfQuestions());
             Date date = Calendar.getInstance().getTime();
             test.setCreationDate(date);
             test.setLastModifiedDate(date);
-            test.setKeyTestScope(oldTest.getKeyTestScope());
             test.setTestScope(oldTest.getTestScope());
 
             IPersistentTestQuestion persistentTestQuestion = persistentSuport.getIPersistentTestQuestion();
@@ -51,14 +49,11 @@ public class InsertTestAsNewTest implements IService {
             List<ITestQuestion> testQuestionList = persistentTestQuestion.readByTest(oldTestId);
 
             for (ITestQuestion testQuestion : testQuestionList) {
-                ITestQuestion newTestQuestion = new TestQuestion();
-                persistentTestQuestion.simpleLockWrite(newTestQuestion);
-                newTestQuestion.setKeyQuestion(testQuestion.getKeyQuestion());
+                ITestQuestion newTestQuestion = DomainFactory.makeTestQuestion();
                 newTestQuestion.setQuestion(testQuestion.getQuestion());
                 newTestQuestion.setTestQuestionOrder(testQuestion.getTestQuestionOrder());
                 newTestQuestion.setTestQuestionValue(testQuestion.getTestQuestionValue());
                 newTestQuestion.setCorrectionFormula(testQuestion.getCorrectionFormula());
-                newTestQuestion.setKeyTest(test.getIdInternal());
                 newTestQuestion.setTest(test);
             }
             return test.getIdInternal();
