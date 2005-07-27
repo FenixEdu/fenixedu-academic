c2cf7cf181458b92b06c49bd2ddd4d4780260331
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-27 09:09:52 +0000

    removed warnings

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/externalPerson/InsertExternalPersons.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/externalPerson/InsertExternalPersons.java
@@ -7,7 +7,6 @@ import net.sourceforge.fenixedu.applicationTier.Servico.commons.workLocation.Ins
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.ExistingServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExternalPerson;
 import net.sourceforge.fenixedu.domain.DomainFactory;
-import net.sourceforge.fenixedu.domain.ExternalPerson;
 import net.sourceforge.fenixedu.domain.IExternalPerson;
 import net.sourceforge.fenixedu.domain.IWorkLocation;
 import net.sourceforge.fenixedu.domain.person.Gender;
@@ -18,48 +17,48 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class InsertExternalPersons implements IService {
 
-    public List<IExternalPerson> run(List<InfoExternalPerson> infoExternalPersons) throws ExcepcaoPersistencia, ExistingServiceException {
+    public List<IExternalPerson> run(List<InfoExternalPerson> infoExternalPersons)
+            throws ExcepcaoPersistencia, ExistingServiceException {
 
         List<IExternalPerson> externalPersons = new ArrayList<IExternalPerson>();
 
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        List<IWorkLocation> workLocations = sp.getIPersistentWorkLocation().readAll();
 
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            List<IWorkLocation> workLocations = sp.getIPersistentWorkLocation().readAll();
+        // generate new identification number
+        String lastDocumentIdNumber = sp.getIPersistentExternalPerson().readLastDocumentIdNumber();
+        int nextID = Integer.parseInt(lastDocumentIdNumber);
 
-            // generate new identification number
-            String lastDocumentIdNumber = sp.getIPersistentExternalPerson().readLastDocumentIdNumber();
-            int nextID = Integer.parseInt(lastDocumentIdNumber);
+        for (InfoExternalPerson infoExternalPerson : infoExternalPersons) {
 
-            for (InfoExternalPerson infoExternalPerson : infoExternalPersons) {
+            IWorkLocation currentWorkLocation = null;
 
-	            IWorkLocation currentWorkLocation = null;
-	
-	            // retrieving existing work location
-	            for (IWorkLocation workLocation : workLocations) {
-	                if (workLocation.getName()
-	                        .equals(infoExternalPerson.getInfoWorkLocation().getName())) {
-	                    currentWorkLocation = workLocation;
-	                    break;
-	                }
-	            }
-	
-	            // creating a new one if it doesn't already exist
-	            if (currentWorkLocation == null) {
-	                InsertWorkLocation iwl = new InsertWorkLocation();
-	                currentWorkLocation = iwl.run(infoExternalPerson.getInfoWorkLocation().getName());
-	                workLocations.add(currentWorkLocation);
-	            }
+            // retrieving existing work location
+            for (IWorkLocation workLocation : workLocations) {
+                if (workLocation.getName().equals(infoExternalPerson.getInfoWorkLocation().getName())) {
+                    currentWorkLocation = workLocation;
+                    break;
+                }
+            }
 
-	            String name = infoExternalPerson.getInfoPerson().getNome();
-	            String documentIDNumber = String.valueOf(++nextID);
-	
-	            // creating a new ExternalPerson
-	            IExternalPerson externalPerson = DomainFactory.makeExternalPerson(name, Gender.MALE, "", "", "", "", "", documentIDNumber ,currentWorkLocation);
+            // creating a new one if it doesn't already exist
+            if (currentWorkLocation == null) {
+                InsertWorkLocation iwl = new InsertWorkLocation();
+                currentWorkLocation = iwl.run(infoExternalPerson.getInfoWorkLocation().getName());
+                workLocations.add(currentWorkLocation);
+            }
 
-	            externalPersons.add(externalPerson);
+            String name = infoExternalPerson.getInfoPerson().getNome();
+            String documentIDNumber = String.valueOf(++nextID);
+
+            // creating a new ExternalPerson
+            IExternalPerson externalPerson = DomainFactory.makeExternalPerson(name, Gender.MALE, "", "",
+                    "", "", "", documentIDNumber, currentWorkLocation);
+
+            externalPersons.add(externalPerson);
         }
-
 
         return externalPersons;
     }
+
 }
