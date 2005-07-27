c1e06a3c1029adaf81e8f81ca8bbaaf0cd6f479d
Luís Egídio <luis.egidio@ist.utl.pt>
2005-07-27 15:56:41 +0000

    removed commented code

--- src/net/sourceforge/fenixedu/applicationTier/Factory/ExecutionCourseSiteComponentBuilder.java
+++ src/net/sourceforge/fenixedu/applicationTier/Factory/ExecutionCourseSiteComponentBuilder.java
@@ -486,7 +486,6 @@ public class ExecutionCourseSiteComponentBuilder {
         try {
             ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
             IPersistentItem persistentItem = sp.getIPersistentItem();
-            //ISection section = Cloner.copyInfoSection2ISection(infoSection);
             itemsList = persistentItem.readAllItemsBySection(infoSection.getIdInternal(), 
                     infoSection.getInfoSite().getInfoExecutionCourse().getSigla(), 
                     infoSection.getInfoSite().getInfoExecutionCourse().getInfoExecutionPeriod().getInfoExecutionYear().getYear(),
