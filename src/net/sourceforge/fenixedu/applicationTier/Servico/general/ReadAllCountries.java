f4b92dbfc097034e996b4e1f0b0d9479a8d45b04
Shezad Anavarali <shezad@ist.utl.pt>
2005-08-05 10:24:32 +0000

    Cloner removed

--- src/net/sourceforge/fenixedu/applicationTier/Servico/general/ReadAllCountries.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/general/ReadAllCountries.java
@@ -15,13 +15,11 @@
 
 package net.sourceforge.fenixedu.applicationTier.Servico.general;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoCountry;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
 import net.sourceforge.fenixedu.domain.ICountry;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
@@ -34,55 +32,22 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class ReadAllCountries implements IService {
 
-    private static ReadAllCountries servico = new ReadAllCountries();
+    public Object run() throws ExcepcaoInexistente, FenixServiceException, ExcepcaoPersistencia {
 
-    /**
-     * The singleton access method of this class.
-     */
-    public static ReadAllCountries getService() {
-        return servico;
-    }
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        List countries = sp.getIPersistentCountry().readAllCountrys();
 
-    /**
-     * The actor of this class.
-     */
-    private ReadAllCountries() {
-    }
-
-    /**
-     * Returns the service name
-     */
-
-    public final String getNome() {
-        return "ReadAllCountries";
-    }
-
-    public Object run() throws ExcepcaoInexistente, FenixServiceException {
-
-        ISuportePersistente sp = null;
-        List paises = new ArrayList();
-
-        try {
-            sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            paises = sp.getIPersistentCountry().readAllCountrys();
-        } catch (ExcepcaoPersistencia ex) {
-            FenixServiceException newEx = new FenixServiceException("Persistence layer error");
-            newEx.fillInStackTrace();
-            throw newEx;
+        if (countries.size() == 0) {
+            throw new ExcepcaoInexistente("Non existing Countries !!");
         }
 
-        if (paises.size() == 0)
-            throw new ExcepcaoInexistente("Non existing Countries !!");
-
-        List countryList = (ArrayList) CollectionUtils.collect(paises, new Transformer() {
+        return CollectionUtils.collect(countries, new Transformer() {
 
             public Object transform(Object input) {
                 ICountry country = (ICountry) input;
-                InfoCountry infoCountry = Cloner.copyICountry2InfoCountry(country);
-                return infoCountry;
+                return InfoCountry.newInfoFromDomain(country);
             }
         });
 
-        return countryList;
     }
 }
