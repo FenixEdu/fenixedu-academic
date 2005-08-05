ab94398e1661e0fa3b6b617ecb80f6a98d3c37ce
Shezad Anavarali <shezad@ist.utl.pt>
2005-08-05 10:24:59 +0000

    Code Cleaning

--- src/net/sourceforge/fenixedu/applicationTier/Servico/grant/contract/ReadAllGrantTypes.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/grant/contract/ReadAllGrantTypes.java
@@ -7,13 +7,11 @@ package net.sourceforge.fenixedu.applicationTier.Servico.grant.contract;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.grant.contract.InfoGrantType;
 import net.sourceforge.fenixedu.domain.grant.contract.IGrantType;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-import net.sourceforge.fenixedu.persistenceTier.grant.IPersistentGrantType;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Transformer;
@@ -23,34 +21,26 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 /**
  * @author Barbosa
  * @author Pica
- *  
+ * 
  */
 public class ReadAllGrantTypes implements IService {
-    public ReadAllGrantTypes() {
-    }
 
-    public List run() throws FenixServiceException {
-        List grantTypes = null;
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentGrantType pgt = sp.getIPersistentGrantType();
-            grantTypes = pgt.readAll();
-        } catch (ExcepcaoPersistencia e) {
-            throw new FenixServiceException(e.getMessage());
-        }
+    public List run() throws ExcepcaoPersistencia {
+
+        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        List grantTypes = sp.getIPersistentGrantType().readAll();
 
         if (grantTypes == null)
             return new ArrayList();
 
-        List grantTypeList = (ArrayList) CollectionUtils.collect(grantTypes, new Transformer() {
+        return (List) CollectionUtils.collect(grantTypes, new Transformer() {
             public Object transform(Object input) {
                 IGrantType grantType = (IGrantType) input;
-                InfoGrantType infoGrantType = InfoGrantType.newInfoFromDomain(grantType);
-                return infoGrantType;
+                return InfoGrantType.newInfoFromDomain(grantType);
+
             }
         });
 
-        return grantTypeList;
     }
 
 }
