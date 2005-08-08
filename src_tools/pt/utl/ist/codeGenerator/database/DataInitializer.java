e7480bf2a994f4f8adb3a35bee62c619880741ab
Luis Cruz <luis.cruz@ist.utl.pt>
2005-08-08 17:23:52 +0000

    Indicate intentional domain object creation.

--- src_tools/pt/utl/ist/codeGenerator/database/DataInitializer.java
+++ src_tools/pt/utl/ist/codeGenerator/database/DataInitializer.java
@@ -1,7 +1,7 @@
 package pt.utl.ist.codeGenerator.database;
 
 import net.sourceforge.fenixedu.domain.CurricularYear;
-import net.sourceforge.fenixedu.domain.Role;
+import net.sourceforge.fenixedu.domain.DomainFactory;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
@@ -37,33 +37,33 @@ public class DataInitializer {
     }
 
     private static void createRoles() {
-        new Role(RoleType.PERSON, "/person", "/index.do", "portal.person");
-        new Role(RoleType.STUDENT, "/student", "/index.do", "portal.student");
-        new Role(RoleType.TEACHER, "/teacher", "/index.do", "portal.teacher");
-        new Role(RoleType.TIME_TABLE_MANAGER, "/sop", "/paginaPrincipal.jsp", "portal.sop");
-        new Role(RoleType.MASTER_DEGREE_CANDIDATE, "/candidato", "/index.do", "portal.candidate");
-        new Role(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE, "/posGraduacao", "/index.do", "portal.masterDegree");
-        new Role(RoleType.TREASURY, "/treasury", "/index.do", "portal.treasury");
-        new Role(RoleType.COORDINATOR, "/coordinator", "/index.do", "portal.coordinator");
-        new Role(RoleType.EMPLOYEE, "/employee", "/index.do", "portal.employee");
-        new Role(RoleType.MANAGEMENT_ASSIDUOUSNESS, "/managementAssiduousness", "/index.do", "portal.managementAssiduousness");
-        new Role(RoleType.MANAGER, "/manager", "/index.do", "portal.manager");
-        new Role(RoleType.CREDITS_MANAGER, "/facultyAdmOffice", "/index.do", "portal.credits");
-        new Role(RoleType.DEPARTMENT_CREDITS_MANAGER, "/departmentAdmOffice", "/index.do", "portal.credits.department");
-        new Role(RoleType.GRANT_OWNER, "/grantOwner", "", "");
-        new Role(RoleType.SEMINARIES_COORDINATOR, "/teacher", "/seminariesIndex.jsp", "portal.seminariesCoordinator");
-        new Role(RoleType.OPERATOR, "/operator", "/index.do", "portal.operator");
-        new Role(RoleType.WEBSITE_MANAGER, "/webSiteManager", "/index.do", "portal.webSiteManager");
-        new Role(RoleType.GRANT_OWNER_MANAGER, "/facultyAdmOffice", "/index.do", "portal.facultyAdmOffice");
-        new Role(RoleType.DEPARTMENT_MEMBER, "/departmentAdmOffice", "/index.do", "portal.departmentAdmOffice");
-        new Role(RoleType.GEP, "/gep", "/index.do", "portal.gep");
-        new Role(RoleType.DIRECTIVE_COUNCIL, "/directiveCouncil", "/index.do", "portal.directiveCouncil");
-        new Role(RoleType.DEGREE_ADMINISTRATIVE_OFFICE, "/degreeAdministrativeOffice", "/index.do", "portal.degreeAdministrativeOffice");
-        new Role(RoleType.DEGREE_ADMINISTRATIVE_OFFICE_SUPER_USER, "/degreeAdministrativeOffice", "/index.do", "portal.degreeAdministrativeOfficeSuperUser");
-        new Role(RoleType.DELEGATE, "/delegate", "/index.do", "portal.delegate");
-        new Role(RoleType.FIRST_TIME_STUDENT, "/schoolRegistration", "/index.do", "portal.schoolRegistration");
-        new Role(RoleType.PROJECTS_MANAGER, "/projectsManagement", "/index.do", "portal.projectsManager");
-        new Role(RoleType.INSTITUCIONAL_PROJECTS_MANAGER, "/institucionalProjectsManagement", "/institucionalProjectIndex.do", "portal.institucionalProjectsManager");
+        DomainFactory.makeRole(RoleType.PERSON, "/person", "/index.do", "portal.person");
+        DomainFactory.makeRole(RoleType.STUDENT, "/student", "/index.do", "portal.student");
+        DomainFactory.makeRole(RoleType.TEACHER, "/teacher", "/index.do", "portal.teacher");
+        DomainFactory.makeRole(RoleType.TIME_TABLE_MANAGER, "/sop", "/paginaPrincipal.jsp", "portal.sop");
+        DomainFactory.makeRole(RoleType.MASTER_DEGREE_CANDIDATE, "/candidato", "/index.do", "portal.candidate");
+        DomainFactory.makeRole(RoleType.MASTER_DEGREE_ADMINISTRATIVE_OFFICE, "/posGraduacao", "/index.do", "portal.masterDegree");
+        DomainFactory.makeRole(RoleType.TREASURY, "/treasury", "/index.do", "portal.treasury");
+        DomainFactory.makeRole(RoleType.COORDINATOR, "/coordinator", "/index.do", "portal.coordinator");
+        DomainFactory.makeRole(RoleType.EMPLOYEE, "/employee", "/index.do", "portal.employee");
+        DomainFactory.makeRole(RoleType.MANAGEMENT_ASSIDUOUSNESS, "/managementAssiduousness", "/index.do", "portal.managementAssiduousness");
+        DomainFactory.makeRole(RoleType.MANAGER, "/manager", "/index.do", "portal.manager");
+        DomainFactory.makeRole(RoleType.CREDITS_MANAGER, "/facultyAdmOffice", "/index.do", "portal.credits");
+        DomainFactory.makeRole(RoleType.DEPARTMENT_CREDITS_MANAGER, "/departmentAdmOffice", "/index.do", "portal.credits.department");
+        DomainFactory.makeRole(RoleType.GRANT_OWNER, "/grantOwner", "", "");
+        DomainFactory.makeRole(RoleType.SEMINARIES_COORDINATOR, "/teacher", "/seminariesIndex.jsp", "portal.seminariesCoordinator");
+        DomainFactory.makeRole(RoleType.OPERATOR, "/operator", "/index.do", "portal.operator");
+        DomainFactory.makeRole(RoleType.WEBSITE_MANAGER, "/webSiteManager", "/index.do", "portal.webSiteManager");
+        DomainFactory.makeRole(RoleType.GRANT_OWNER_MANAGER, "/facultyAdmOffice", "/index.do", "portal.facultyAdmOffice");
+        DomainFactory.makeRole(RoleType.DEPARTMENT_MEMBER, "/departmentAdmOffice", "/index.do", "portal.departmentAdmOffice");
+        DomainFactory.makeRole(RoleType.GEP, "/gep", "/index.do", "portal.gep");
+        DomainFactory.makeRole(RoleType.DIRECTIVE_COUNCIL, "/directiveCouncil", "/index.do", "portal.directiveCouncil");
+        DomainFactory.makeRole(RoleType.DEGREE_ADMINISTRATIVE_OFFICE, "/degreeAdministrativeOffice", "/index.do", "portal.degreeAdministrativeOffice");
+        DomainFactory.makeRole(RoleType.DEGREE_ADMINISTRATIVE_OFFICE_SUPER_USER, "/degreeAdministrativeOffice", "/index.do", "portal.degreeAdministrativeOfficeSuperUser");
+        DomainFactory.makeRole(RoleType.DELEGATE, "/delegate", "/index.do", "portal.delegate");
+        DomainFactory.makeRole(RoleType.FIRST_TIME_STUDENT, "/schoolRegistration", "/index.do", "portal.schoolRegistration");
+        DomainFactory.makeRole(RoleType.PROJECTS_MANAGER, "/projectsManagement", "/index.do", "portal.projectsManager");
+        DomainFactory.makeRole(RoleType.INSTITUCIONAL_PROJECTS_MANAGER, "/institucionalProjectsManagement", "/institucionalProjectIndex.do", "portal.institucionalProjectsManager");
     }
 
     private static void createCurricularYearsAndSemesters() {
