234cf80b7b93a29cee151b67dcb2cf0778c4b09b
Susana Fernandes <susana.fernandes@ist.utl.pt>
2005-08-12 13:53:11 +0000

    update onlineTest package name

---
+++ etc/database_operations/fixOnlineTestPackageName.sql
@@ -0,0 +1,1 @@
+update EVALUATION set CLASS_NAME="net.sourceforge.fenixedu.domain.onlineTests.OnlineTest", ACK_OPT_LOCK=ACK_OPT_LOCK + 1 where CLASS_NAME="net.sourceforge.fenixedu.domain.OnlineTest"
