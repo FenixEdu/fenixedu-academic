34616ffc79aa17d4f6e12e15ed4c72261458682f
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-08-22 09:21:24 +0000

    New DAO for CompetenceCourse

---
+++ src/net/sourceforge/fenixedu/persistenceTier/OJB/CompetenceCourseOJB.java
@@ -0,0 +1,8 @@
+package net.sourceforge.fenixedu.persistenceTier.OJB;
+
+import net.sourceforge.fenixedu.persistenceTier.IPersistentCompetenceCourse;
+
+public class CompetenceCourseOJB extends ObjectFenixOJB implements IPersistentCompetenceCourse{
+
+
+}
