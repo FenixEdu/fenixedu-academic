6dba48dfc5ae9534389f902371a03d92b5f218e4
Luis Cruz <luis.cruz@ist.utl.pt>
2005-08-22 15:17:12 +0000

    New domain class.

---
+++ src/net/sourceforge/fenixedu/domain/DegreeCurricularPlanLEFT.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.domain;
+
+public class DegreeCurricularPlanLEFT extends DegreeCurricularPlanLEFT_Base {
+    
+    public DegreeCurricularPlanLEFT() {
+        super();
+    }
+    
+}
