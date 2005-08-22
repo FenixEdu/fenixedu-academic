cee6a4750eb70be20d5b1116c4f04489ddb3eb5d
Luis Cruz <luis.cruz@ist.utl.pt>
2005-08-22 15:15:49 +0000

    Moved already run scripts.

--- etc/database_operations/updateExecutionCourseWithSites.sql
+++
@@ -1,6 +0,0 @@
-select 
-	concat('update EXECUTION_COURSE set EXECUTION_COURSE.KEY_SITE = ', 
-		SITE.ID_INTERNAL, 
-		' where EXECUTION_COURSE.ID_INTERNAL = ',
-		SITE.KEY_EXECUTION_COURSE, ';') 
-as "" from SITE;
