6289a29f3c7a7b0d5577dfd8b73b80fdfed8a613
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-08-23 07:48:05 +0000

    DegreeCurricularPlan for LEFT

--- src/net/sourceforge/fenixedu/domain/DegreeCurricularPlanLEFT.java
+++ src/net/sourceforge/fenixedu/domain/DegreeCurricularPlanLEFT.java
@@ -1,9 +1,32 @@
 package net.sourceforge.fenixedu.domain;
 
-public class DegreeCurricularPlanLEFT extends DegreeCurricularPlanLEFT_Base {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.MaximumNumberOfAcumulatedEnrollmentsRule;
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.MaximumNumberOfCurricularCoursesEnrollmentRule;
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.PrecedencesEnrollmentRule;
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.PreviousYearsCurricularCourseEnrollmentRuleIgnoringLastYears;
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.SpecificLEFTEnrolmentRule;
+
+public class DegreeCurricularPlanLEFT extends DegreeCurricularPlan {
+	
     public DegreeCurricularPlanLEFT() {
-        super();
+        setOjbConcreteClass(getClass().getName());
     }
-    
+
+    public List getListOfEnrollmentRules(IStudentCurricularPlan studentCurricularPlan,
+            IExecutionPeriod executionPeriod) {
+
+        List result = new ArrayList();
+        result.add(new MaximumNumberOfAcumulatedEnrollmentsRule(studentCurricularPlan, executionPeriod));
+        result.add(new MaximumNumberOfCurricularCoursesEnrollmentRule(studentCurricularPlan,
+                executionPeriod));
+        result.add(new PrecedencesEnrollmentRule(studentCurricularPlan, executionPeriod));
+        result.add(new PreviousYearsCurricularCourseEnrollmentRuleIgnoringLastYears(
+                studentCurricularPlan, executionPeriod, 4));
+        result.add(new SpecificLEFTEnrolmentRule(studentCurricularPlan, executionPeriod));
+
+        return result;
+    }
 }
