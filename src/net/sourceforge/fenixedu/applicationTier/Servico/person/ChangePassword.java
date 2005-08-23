efca62f28fa1b141e442d7d6704affbe5583bd70
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-08-23 11:50:16 +0000

    catch DomainException

--- src/net/sourceforge/fenixedu/applicationTier/Servico/person/ChangePassword.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/person/ChangePassword.java
@@ -1,7 +1,9 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.person;
 
 import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidPasswordServiceException;
 import net.sourceforge.fenixedu.domain.IPerson;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
@@ -9,14 +11,16 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class ChangePassword implements IService {
 
-    public void run(IUserView userView, String oldPassword, String newPassword) throws ExcepcaoPersistencia {
+    public void run(IUserView userView, String oldPassword, String newPassword) throws Exception{
 
         ISuportePersistente suportePersistente = PersistenceSupportFactory
                 .getDefaultPersistenceSupport();
         IPerson person = suportePersistente.getIPessoaPersistente().lerPessoaPorUsername(
                 userView.getUtilizador());
-
-        person.changePassword(oldPassword, newPassword);
-
+        try {
+        	person.changePassword(oldPassword, newPassword);
+        }catch(DomainException e) {
+        	throw new InvalidPasswordServiceException(e.getKey());
+        }
     }
 }
