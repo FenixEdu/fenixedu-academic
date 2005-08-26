6f6e81ba3d6b2a7fe5a4f8414bb525bfb47c9b06
Luis Cruz <luis.cruz@ist.utl.pt>
2005-08-26 09:17:58 +0000

    The execution year is almost always necessary... for now we'll keep on copying it... later this should be fixed.

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoExecutionPeriod.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoExecutionPeriod.java
@@ -210,6 +210,7 @@ public class InfoExecutionPeriod extends InfoObject implements Serializable {
             setBeginDate(period.getBeginDate());
             setEndDate(period.getEndDate());
             setSemester(period.getSemester());
+            setInfoExecutionYear(InfoExecutionYear.newInfoFromDomain(period.getExecutionYear()));
         }
     }
 
