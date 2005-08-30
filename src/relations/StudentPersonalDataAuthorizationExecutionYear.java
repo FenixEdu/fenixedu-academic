d465b60c97fd3281e8ff30a6a4ed4f2c2212964d
Luis Cruz <luis.cruz@ist.utl.pt>
2005-08-30 23:34:03 +0000

    Added missing files.

---
+++ src/relations/StudentPersonalDataAuthorizationExecutionYear.java
@@ -0,0 +1,8 @@
+package relations;
+
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
+public class StudentPersonalDataAuthorizationExecutionYear extends StudentPersonalDataAuthorizationExecutionYear_Base {
+    
+}
