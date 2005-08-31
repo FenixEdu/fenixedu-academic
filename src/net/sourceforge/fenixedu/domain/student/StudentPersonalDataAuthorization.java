a50d8303c3ceada87be0e7980e3eed55f4b02f2a
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-08-31 15:53:34 +0000

    *** empty log message ***

---
+++ src/net/sourceforge/fenixedu/domain/student/StudentPersonalDataAuthorization.java
@@ -0,0 +1,12 @@
+package net.sourceforge.fenixedu.domain.student;
+
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
+public class StudentPersonalDataAuthorization extends StudentPersonalDataAuthorization_Base {
+    
+    public StudentPersonalDataAuthorization() {
+        super();
+    }
+    
+}
