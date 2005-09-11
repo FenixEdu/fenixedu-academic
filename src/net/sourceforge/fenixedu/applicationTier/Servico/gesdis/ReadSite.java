a9aa6bf44ed25bcf20decea9e18f981804179e45
Luis Cruz <luis.cruz@ist.utl.pt>
2005-09-11 19:07:56 +0000

    Removed cloner.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/ReadSite.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/ReadSite.java
@@ -9,6 +9,7 @@ package net.sourceforge.fenixedu.applicationTier.Servico.gesdis;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
 import net.sourceforge.fenixedu.dataTransferObject.InfoSite;
+import net.sourceforge.fenixedu.dataTransferObject.InfoSiteWithInfoExecutionCourse;
 import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
 import net.sourceforge.fenixedu.domain.ISite;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -18,16 +19,14 @@ import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class ReadSite implements IService {
 
-    public InfoSite run(InfoExecutionCourse infoExecutionCourse) throws FenixServiceException,
-            ExcepcaoPersistencia {
-
-        ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-        final ISite site = sp.getIPersistentSite().readByExecutionCourse(
-                infoExecutionCourse.getIdInternal());
-        InfoSite infoSite = null;
+    public InfoSite run(InfoExecutionCourse infoExecutionCourse) throws ExcepcaoPersistencia {
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final ISite site = sp.getIPersistentSite().readByExecutionCourse(infoExecutionCourse.getIdInternal());
         if (site != null) {
-            infoSite = Cloner.copyISite2InfoSite(site);
+            return InfoSiteWithInfoExecutionCourse.newInfoFromDomain(site);
+        } else {
+            return null;
         }
-        return infoSite;
     }
+
 }
