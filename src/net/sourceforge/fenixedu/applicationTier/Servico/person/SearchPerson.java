ee86e6107323863218122de20ea361cb0cbd62d3
Ângela Almeida <angela.almeida@ist.utl.pt>
2005-09-12 10:11:28 +0000

    added args roleType

--- src/net/sourceforge/fenixedu/applicationTier/Servico/person/SearchPerson.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/person/SearchPerson.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.InfoPerson;
 import net.sourceforge.fenixedu.domain.IPerson;
+import net.sourceforge.fenixedu.domain.IRole;
+import net.sourceforge.fenixedu.domain.Role;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
+import net.sourceforge.fenixedu.persistenceTier.IPersistentRole;
 import net.sourceforge.fenixedu.persistenceTier.IPessoaPersistente;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
@@ -23,27 +27,35 @@ public class SearchPerson implements IService {
     public List run(HashMap searchParameters) throws ExcepcaoPersistencia {
         ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
         IPessoaPersistente persistentPerson = sp.getIPessoaPersistente();
-
+        IPersistentRole persistentRole = sp.getIPersistentRole();
+        
         String name = (String) searchParameters.get(new String("name"));
         String email = (String) searchParameters.get(new String("email"));
         String username = (String) searchParameters.get(new String("username"));
         String documentIdNumber = (String) searchParameters.get(new String("documentIdNumber"));
         Integer startIndex = (Integer) searchParameters.get(new String("startIndex"));
         Integer numberOfElementsInSpan = (Integer) searchParameters.get(new String("numberOfElements"));
-
-        
+        String roleType = (String) searchParameters.get(new String("roleType"));
+        IRole roleBd = null;
+     
+        if (roleType != null && roleType.length() > 0){
+        	roleBd = persistentRole.readByRoleType(RoleType.valueOf(roleType));	
+        }
         List<IPerson> persons = persistentPerson.readActivePersonByNameAndEmailAndUsernameAndDocumentId(name, email,
-                username, documentIdNumber, startIndex, numberOfElementsInSpan);
-        Integer totalPersons = persistentPerson.countActivePersonByNameAndEmailAndUsernameAndDocumentId(name, email,
-                username, documentIdNumber, startIndex);
+                    username, documentIdNumber, startIndex, numberOfElementsInSpan,roleBd);
+      
 
+        Integer totalPersons = persistentPerson.countActivePersonByNameAndEmailAndUsernameAndDocumentId(name, email,
+                username, documentIdNumber, startIndex,roleBd);
+        
+       
         List<InfoPerson> infoPersons = new ArrayList<InfoPerson>();
         for (IPerson person : persons) {
-        	infoPersons.add(InfoPerson.newInfoFromDomain(person));
+            	infoPersons.add(InfoPerson.newInfoFromDomain(person));
         }
-
-        List<Object> result = new ArrayList<Object>(2);
         
+       
+        List<Object> result = new ArrayList<Object>(2);
         result.add(totalPersons);
         result.add(infoPersons);
 
