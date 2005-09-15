d0c0bddef81d1c1ef0d475d833fc417d25a0b211
Luis Cruz <luis.cruz@ist.utl.pt>
2005-09-15 13:12:05 +0000

    Removed cloner calls.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionPeriodsByExecutionYear.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/ReadExecutionPeriodsByExecutionYear.java
@@ -3,59 +3,32 @@ package net.sourceforge.fenixedu.applicationTier.Servico.commons;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionPeriod;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionYear;
-import net.sourceforge.fenixedu.dataTransferObject.util.Cloner;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.IExecutionPeriod;
 import net.sourceforge.fenixedu.domain.IExecutionYear;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionPeriod;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionYear;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-
 import pt.utl.ist.berserk.logic.serviceManager.IService;
-
-/**
- * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
- */
 
 public class ReadExecutionPeriodsByExecutionYear implements IService {
 
-    public List run(InfoExecutionYear infoExecutionYear) throws FenixServiceException {
+    public List run(InfoExecutionYear infoExecutionYear) throws ExcepcaoPersistencia {
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final IPersistentExecutionYear executionYearDAO = sp.getIPersistentExecutionYear();
 
-        List result = new ArrayList();
-        try {
-            ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            IPersistentExecutionPeriod executionPeriodDAO = sp.getIPersistentExecutionPeriod();
-            IPersistentExecutionYear executionYearDAO = sp.getIPersistentExecutionYear();
-            IExecutionYear executionYear;
-            List executionPeriods;
+        final IExecutionYear executionYear = (infoExecutionYear != null) ?
+                (IExecutionYear) executionYearDAO.readByOID(ExecutionYear.class, infoExecutionYear.getIdInternal())
+                : executionYearDAO.readCurrentExecutionYear();
 
-            if (infoExecutionYear == null) {
-                executionYear = executionYearDAO.readCurrentExecutionYear();
-                executionPeriods = executionPeriodDAO.readByExecutionYear(executionYear.getIdInternal());
-            } 
-            else{
-                executionPeriods = executionPeriodDAO.readByExecutionYear(infoExecutionYear.getIdInternal());
-            }
-            
-            result = (List) CollectionUtils.collect(executionPeriods, new Transformer() {
-
-                public Object transform(Object input) {
-                    IExecutionPeriod executionPeriod = (IExecutionPeriod) input;
-                    InfoExecutionPeriod infoExecutionPeriod = (InfoExecutionPeriod) Cloner
-                            .get(executionPeriod);
-                    return infoExecutionPeriod;
-                }
-            });
-        } catch (ExcepcaoPersistencia ex) {
-            throw new FenixServiceException(ex);
+        final List<InfoExecutionPeriod> infoExecutionPeriods = new ArrayList<InfoExecutionPeriod>();
+        for (final IExecutionPeriod executionPeriod : executionYear.getExecutionPeriods()) {
+            infoExecutionPeriods.add(InfoExecutionPeriod.newInfoFromDomain(executionPeriod));
         }
-        return result;
+        return infoExecutionPeriods;
     }
+
 }
