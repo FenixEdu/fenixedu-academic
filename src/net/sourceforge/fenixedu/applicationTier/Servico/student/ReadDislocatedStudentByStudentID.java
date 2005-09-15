8b2714fe9df6d50985b707a692c130a046ab5720
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2005-09-15 19:51:29 +0000

    reversed to previous code, it implide changes that will be done later

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadDislocatedStudentByStudentID.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadDislocatedStudentByStudentID.java
@@ -3,14 +3,17 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Servico.student;
 
+import java.util.List;
+
 import net.sourceforge.fenixedu.dataTransferObject.student.InfoDislocatedStudent;
-import net.sourceforge.fenixedu.domain.IDislocatedStudent;
+import net.sourceforge.fenixedu.domain.DislocatedStudent;
 import net.sourceforge.fenixedu.domain.IStudent;
 import net.sourceforge.fenixedu.domain.Student;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentStudent;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import net.sourceforge.fenixedu.persistenceTier.student.IPersistentDislocatedStudent;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 /**
@@ -24,14 +27,20 @@ public class ReadDislocatedStudentByStudentID implements IService {
 
         ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
         IPersistentStudent persistentStudent = sp.getIPersistentStudent();
+        IPersistentDislocatedStudent persistentDislocatedStudent = sp.getIPersistentDislocatedStudent();
 
         IStudent student = (IStudent) persistentStudent.readByOID(Student.class, studentID);
-        IDislocatedStudent dislocatedStudent = student.getDislocatedStudent();
+        List<DislocatedStudent> dislocatedStudents = (List<DislocatedStudent>) persistentDislocatedStudent
+                .readAll(DislocatedStudent.class);
 
-        if (dislocatedStudent != null) {
-            return InfoDislocatedStudent.newInfoFromDomain(dislocatedStudent);
+        if (dislocatedStudents != null) {
+            for (DislocatedStudent dislocatedStudent : dislocatedStudents) {
+                if(dislocatedStudent.getStudent().equals(student)){
+                    return InfoDislocatedStudent.newInfoFromDomain(dislocatedStudent);
+                }
+            }
         }
-
+        
         return null;
     }
 }
