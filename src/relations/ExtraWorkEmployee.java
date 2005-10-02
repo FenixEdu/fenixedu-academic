3a7b859131b95d498cfc312751f97cac302ab6e2
Luis Cruz <luis.cruz@ist.utl.pt>
2005-10-02 22:00:07 +0000

    Wrote ExtraWorkCompensation class in dml.

---
+++ src/relations/ExtraWorkEmployee.java
@@ -0,0 +1,8 @@
+package relations;
+
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
+public class ExtraWorkEmployee extends ExtraWorkEmployee_Base {
+    
+}
---
+++ src/relations/ExtraWorkWhoEmployee.java
@@ -0,0 +1,8 @@
+package relations;
+
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
+public class ExtraWorkWhoEmployee extends ExtraWorkWhoEmployee_Base {
+    
+}
