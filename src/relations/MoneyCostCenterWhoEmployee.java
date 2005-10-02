0de92d11ac0ea8b714c693392a02e8243857c201
Luis Cruz <luis.cruz@ist.utl.pt>
2005-10-02 22:35:32 +0000

    Wrote MoneyCostCenter class in dml.

---
+++ src/relations/MoneyCostCenterWhoEmployee.java
@@ -0,0 +1,8 @@
+package relations;
+
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
+public class MoneyCostCenterWhoEmployee extends MoneyCostCenterWhoEmployee_Base {
+    
+}
