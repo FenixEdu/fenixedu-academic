0e256931d582513f0abb0d01992fe0fce96013dd
Ana Rita Carvalho Ferreira <arcf@ist.utl.pt>
2005-10-02 21:41:25 +0000

    New VO

---
+++ src/net/sourceforge/fenixedu/persistenceTier/versionedObjects/dao/person/qualification/QualificationVO.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 11/Set/2005 - 15:30:32
+ * 
+ */
+
+package net.sourceforge.fenixedu.persistenceTier.versionedObjects.dao.person.qualification;
+
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.IPerson;
+import net.sourceforge.fenixedu.domain.IQualification;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
+import net.sourceforge.fenixedu.persistenceTier.IPersistentQualification;
+import net.sourceforge.fenixedu.persistenceTier.versionedObjects.VersionedObjectsBase;
+
+/**
+ * @author João Fialho & Rita Ferreira
+ *
+ */
+public class QualificationVO extends VersionedObjectsBase implements
+		IPersistentQualification {
+
+	public List readQualificationsByPersonId(Integer personId)
+			throws ExcepcaoPersistencia {
+		
+		IPerson person = (IPerson) readByOID(Person.class, personId);	
+		List<IQualification> res = person.getAssociatedQualifications();
+		return res;
+		
+	}
+
+}
