b7f707c724fb60142fda17561bcaa33a2900455f
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2005-10-06 16:28:23 +0000

    initial version

---
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/components/UIViewState.java
@@ -0,0 +1,24 @@
+package net.sourceforge.fenixedu.presentationTier.jsf.components;
+
+import javax.faces.component.UIComponentBase;
+
+public class UIViewState extends UIComponentBase {
+
+    public UIViewState() {
+        super();
+    }
+
+    public void setAttribute(String name, Object value) {
+        this.getAttributes().put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return this.getAttributes().get(name);
+    }
+
+    @Override
+    public String getFamily() {
+        return null;
+    }
+
+}
---
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/components/UIViewStateTag.java
@@ -0,0 +1,50 @@
+package net.sourceforge.fenixedu.presentationTier.jsf.components;
+
+import javax.faces.component.UIComponent;
+import javax.faces.webapp.UIComponentTag;
+
+import net.sourceforge.fenixedu.presentationTier.jsf.components.util.JsfTagUtils;
+
+public class UIViewStateTag extends UIComponentTag {
+
+    private static final String COMPONENT_TYPE = "net.sourceforge.fenixedu.presentationTier.jsf.components.UIViewState";
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String getComponentType() {
+
+        return COMPONENT_TYPE;
+    }
+
+    @Override
+    public String getRendererType() {
+        return null;
+    }
+
+    @Override
+    protected void setProperties(UIComponent component) {
+
+        super.setProperties(component);
+
+        JsfTagUtils.setString(component, "value", this.value);
+
+    }
+
+    @Override
+    public void release() {
+        super.release();
+
+        value = null;
+
+    }
+
+}
