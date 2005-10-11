4e03454c61079f8d8cb37ef9d8bafa0c2b91bf48
João Florindo <jdnf@linkare.pt>
2005-10-11 12:28:02 +0000

    Created new jsf validator (regexValidator).

---
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/validators/RegexValidator.java
@@ -0,0 +1,62 @@
+/*
+ * Created on Oct 11, 2005
+ *  by jdnf
+ */
+package net.sourceforge.fenixedu.presentationTier.jsf.validators;
+
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.StateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
+
+public class RegexValidator implements Validator, StateHolder {
+    private String regex;
+
+    private boolean _transient = false;
+
+    private final String INVALID_INPUT = "net.sourceforge.fenixedu.presentationTier.jsf.validators.INVALID_INPUT";
+
+    public void validate(FacesContext _context, UIComponent _component, Object _value)
+            throws ValidatorException {
+
+        String val = "";
+        if (_value != null) {
+            val = _value.toString();
+        }
+        if (!val.matches(regex)) {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle(_context.getApplication()
+                    .getMessageBundle(), _context.getViewRoot().getLocale());
+            throw new ValidatorException(new FacesMessage(resourceBundle.getString(INVALID_INPUT)));
+        }
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    public Object saveState(FacesContext context) {
+        return getRegex();
+    }
+
+    public void restoreState(FacesContext context, Object state) {
+        setRegex((String) state);
+    }
+
+    public boolean isTransient() {
+        return _transient;
+    }
+
+    public void setTransient(boolean arg0) {
+        _transient = arg0;
+    }
+}
---
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/validators/RegexValidatorTag.java
@@ -0,0 +1,34 @@
+/*
+ * Created on Oct 11, 2005
+ *  by jdnf
+ */
+package net.sourceforge.fenixedu.presentationTier.jsf.validators;
+
+import javax.faces.validator.Validator;
+import javax.faces.webapp.ValidatorTag;
+import javax.servlet.jsp.JspException;
+
+public class RegexValidatorTag extends ValidatorTag {
+
+    private String regex;
+
+    public RegexValidatorTag() {
+        super();
+        super.setValidatorId("regexValidator");
+    }
+
+    protected Validator createValidator() throws JspException {
+        RegexValidator result = (RegexValidator) super.createValidator();
+        result.setRegex(regex);
+        return result;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+}
