2666487648eee6f653238790f42f989a4005bc27
Manuel Pinto <manuel.pinto@ist.utl.pt>
2005-10-12 16:40:14 +0000

    OrganizationalStructure continuation...

--- etc/database_operations/createFunctionTable.sql
+++ etc/database_operations/createFunctionTable.sql
@@ -3,7 +3,7 @@ CREATE TABLE `FUNCTION` (
 	`NAME` varchar(100) NOT NULL default '',
 	`KEY_UNIT` int(11) default NULL,
 	`KEY_INHERENTE_PARENT_FUNCTION` int(11) default NULL,
-	`TYPE` varchar(80) default '',
+	`TYPE` varchar(80) default 'UNKNOWN',
 	`ACK_OPT_LOCK` int(11) default NULL,
     PRIMARY KEY (`ID_INTERNAL`)
 )TYPE=InnoDB;
