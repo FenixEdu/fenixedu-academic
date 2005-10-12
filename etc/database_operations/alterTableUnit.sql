4ef4d956474c036eaf6ca72872dce633f45af1a3
Manuel Pinto <manuel.pinto@ist.utl.pt>
2005-10-12 12:27:52 +0000

    bugs fixed...

--- etc/database_operations/alterTableUnit.sql
+++ etc/database_operations/alterTableUnit.sql
@@ -1,3 +1,2 @@
 alter table UNIT add column TYPE varchar(80) default '';
-alter table UNIT add column REAL_NAME varchar(100) default '';
 alter table UNIT change column TYPE TYPE varchar(80) default 'UNKNOWN';
