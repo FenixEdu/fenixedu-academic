13a63672f78759c9501ef3e0870965b58a016600
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2005-10-20 12:49:06 +0000

    initial version

---
+++ etc/database_operations/R2005-10-20/alterTableEvaluation.sql
@@ -0,0 +1,1 @@
+alter table EVALUATION add column DESCRIPTION varchar(120) default NULL;
---
+++ etc/database_operations/R2005-10-20/alterTableFunction.sql
@@ -0,0 +1,2 @@
+alter table FUNCTION add column BEGIN_DATE date default NULL;
+alter table FUNCTION add column END_DATE date default NULL;
