f22bf30d129c3e7ec3ee83a77c79e8323a70611d
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-10-28 09:00:35 +0000

    interface to generate passwords

---
+++ src/net/sourceforge/fenixedu/applicationTier/utils/IGeneratePassword.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.applicationTier.utils;
+
+import net.sourceforge.fenixedu.domain.IPerson;
+
+public interface IGeneratePassword {
+
+	public String generatePassword(IPerson person);
+	
+}
