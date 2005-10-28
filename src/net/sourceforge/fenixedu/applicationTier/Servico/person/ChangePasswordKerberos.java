6744e4192dd509b007c7c9bfe1aeb13603a399ff
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-10-28 08:57:22 +0000

    big changes

--- src/net/sourceforge/fenixedu/applicationTier/Servico/person/ChangePasswordKerberos.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/person/ChangePasswordKerberos.java
@@ -8,6 +8,7 @@ import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import net.sourceforge.fenixedu.util.kerberos.KerberosException;
 import net.sourceforge.fenixedu.util.kerberos.UpdateKerberos;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
@@ -15,7 +16,6 @@ public class ChangePasswordKerberos implements IService {
 
 	public void run(IUserView userView, String oldPassword, String newPassword)
 			throws Exception {
-		String returnCode = null;
 		ISuportePersistente suportePersistente = PersistenceSupportFactory
 				.getDefaultPersistenceSupport();
 		IPerson person = suportePersistente.getIPessoaPersistente()
@@ -27,23 +27,33 @@ public class ChangePasswordKerberos implements IService {
 			throw new InvalidPasswordServiceException(e.getKey());
 		}
 
-		if (person.getIsPassInKerberos()) {
-			try {
-				if ((returnCode = UpdateKerberos.changeKerberosPass(person
-						.getIstUsername(), newPassword)) != null) {
-					if (returnCode.equals("CHANGE_PASSWORD_TOO_SHORT")
-							|| returnCode
-									.equals("CHANGE_PASSWORD_NOT_ENOUGH_CHARACTER_CLASSES")
-							|| returnCode
-									.equals("CHANGE_PASSWORD_CANNOT_REUSE")) {
-						throw new InvalidPasswordServiceException(returnCode);
-					} else {
-						throw new InvalidPasswordServiceException(
-								"error.person.impossible.change");
-					}
+		try {
+			if(person.getIstUsername() != null) {
+				if (person.getIsPassInKerberos()) {
+					UpdateKerberos.changeKerberosPass(person.getIstUsername(), newPassword);
+				} else {
+					UpdateKerberos.createUser(person.getIstUsername(), newPassword);
 				}
-			} catch (ExcepcaoPersistencia e) {
-				throw new FenixServiceException(e.getMessage());
+			}
+			person.setIsPassInKerberos(true);
+		}catch(ExcepcaoPersistencia e) {
+			throw new FenixServiceException(e);
+		}catch(KerberosException ke) {
+			if(ke.getExitCode() == 1) {
+				String returnCode = ke.getReturnCode();
+				if (returnCode.equals("CHANGE_PASSWORD_TOO_SHORT")
+						|| returnCode
+								.equals("CHANGE_PASSWORD_NOT_ENOUGH_CHARACTER_CLASSES")
+						|| returnCode
+								.equals("CHANGE_PASSWORD_CANNOT_REUSE")) {
+					throw new InvalidPasswordServiceException(returnCode);
+				} else {
+					throw new InvalidPasswordServiceException(
+							"error.person.impossible.change");
+				}
+			}
+			else {
+				throw new FenixServiceException(ke);
 			}
 		}
 	}
