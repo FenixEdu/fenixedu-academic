ef7e8d3aa8f75aa3d0720f44ce3fd68b80e13ebb
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-10-28 09:00:10 +0000

    singleton class to generate new passwords

--- src/net/sourceforge/fenixedu/applicationTier/utils/GeneratePassword.java
+++ src/net/sourceforge/fenixedu/applicationTier/utils/GeneratePassword.java
@@ -4,17 +4,31 @@
  */
 package net.sourceforge.fenixedu.applicationTier.utils;
 
-import net.sourceforge.fenixedu.util.RandomStringGenerator;
+import net.sourceforge.fenixedu._development.PropertiesManager;
 
 /**
  * @author Barbosa
  * @author Pica
  */
-public class GeneratePassword {
-
-    public static String generatePassword() {
-        String password = RandomStringGenerator.getRandomStringGenerator(8);
-        return password;
+public class GeneratePassword  {
+	
+	private static IGeneratePassword instance = null; 
+	
+	public static synchronized IGeneratePassword getInstance() {
+        if (instance == null) {
+            instance = getSpecificInstance();
+        }
+        return instance;
     }
+
+    private static IGeneratePassword getSpecificInstance() {
+		try {
+			String stringClass = PropertiesManager.getProperty("passGenerator");
+			Class clazz = Class.forName(stringClass);
+			return (IGeneratePassword) clazz.newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
 
