c8905421e8e224d20f12d016d11d51c29081e134
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2005-11-14 14:57:21 +0000

    added resourse bundle support

--- src/net/sourceforge/fenixedu/presentationTier/backBeans/base/FenixBackingBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/backBeans/base/FenixBackingBean.java
@@ -1,5 +1,8 @@
 package net.sourceforge.fenixedu.presentationTier.backBeans.base;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
@@ -13,7 +16,9 @@ import net.sourceforge.fenixedu.presentationTier.jsf.components.UIViewState;
 public class FenixBackingBean {
 
     protected IUserView userView;
+
     protected String errorMessage;
+
     private UIViewState viewState;
 
     public FenixBackingBean() {
@@ -37,7 +42,7 @@ public class FenixBackingBean {
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
     }
-    
+
     protected String getRequestParameter(String parameterName) {
         return (String) FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
                 .get(parameterName);
@@ -47,7 +52,16 @@ public class FenixBackingBean {
         return ((HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest())
                 .getContextPath();
     }
-    
+
+    public ResourceBundle getResourceBundle(String bundleName) {
+        return ResourceBundle.getBundle(bundleName, FacesContext.getCurrentInstance().getViewRoot()
+                .getLocale());
+    }
+
+    public ResourceBundle getResourceBundle(String bundleName, Locale locale) {
+        return ResourceBundle.getBundle(bundleName, locale);
+    }
+
     public UIViewState getViewState() {
         if (this.viewState == null) {
             this.viewState = new UIViewState();
@@ -60,19 +74,19 @@ public class FenixBackingBean {
         this.viewState = viewState;
     }
 
-    private void addMessage(Severity facesMessage ,String message) {
+    private void addMessage(Severity facesMessage, String message) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.addMessage(null, new FacesMessage(facesMessage, message, null));
     }
-    
+
     protected void addErrorMessage(String message) {
         addMessage(FacesMessage.SEVERITY_ERROR, message);
     }
-    
+
     protected void addInfoMessage(String message) {
         addMessage(FacesMessage.SEVERITY_INFO, message);
     }
-    
+
     protected void addWarnMessage(String message) {
         addMessage(FacesMessage.SEVERITY_WARN, message);
     }
