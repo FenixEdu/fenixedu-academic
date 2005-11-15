408545a7c2ae0c97c2b812a7d265421a3cfb69be
João Florindo <jdnf@linkare.pt>
2005-11-15 17:08:32 +0000

    remove unnecessary condition from sql script

--- etc/database_operations/updateTableSummaryWithSummaryType.sql
+++ etc/database_operations/updateTableSummaryWithSummaryType.sql
@@ -1,6 +1,5 @@
 select concat('update SUMMARY set SUMMARY.SUMMARY_TYPE = \'', SHIFT.TYPE,
   '\' where SUMMARY.ID_INTERNAL = ', SUMMARY.ID_INTERNAL, ';') as ""
   from SUMMARY inner join SHIFT 
-  on SUMMARY.SUMMARY_TYPE is null 
-  and SUMMARY.KEY_SHIFT is not null 
+  on SUMMARY.SUMMARY_TYPE is null
   and SHIFT.ID_INTERNAL = SUMMARY.KEY_SHIFT;
