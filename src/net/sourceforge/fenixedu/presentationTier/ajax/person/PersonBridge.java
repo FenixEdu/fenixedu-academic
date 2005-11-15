c85108c2e1a22fe1a17d3d73fa4d18a6d86243f8
Shezad Anavarali <shezad@ist.utl.pt>
2005-11-15 15:41:38 +0000

    Class that will be accessed by DWR

---
+++ src/net/sourceforge/fenixedu/presentationTier/ajax/person/PersonBridge.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package net.sourceforge.fenixedu.presentationTier.ajax.person;
+
+import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoPerson;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
+
+/**
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ * 
+ */
+public class PersonBridge {
+
+    public static String readNameByPersonUsername(String username) throws FenixFilterException,
+            FenixServiceException {
+
+        Object[] args = { username };
+        InfoPerson person = (InfoPerson) ServiceUtils.executeService(null, "ReadPersonByUsername", args);
+
+        return person.getNome();
+    }
+
+}
---
+++ src/net/sourceforge/fenixedu/presentationTier/ajax/teacher/TeacherBridge.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package net.sourceforge.fenixedu.presentationTier.ajax.teacher;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+import org.apache.commons.lang.StringUtils;
+
+import uk.ltd.getahead.dwr.ExecutionContext;
+
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoTeacher;
+import net.sourceforge.fenixedu.dataTransferObject.teacher.InfoNonAffiliatedTeacher;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionUtils;
+
+/**
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ * 
+ */
+public class TeacherBridge {
+
+    public static String readNameByTeacherNumber(String number) throws FenixFilterException,
+            FenixServiceException {
+
+        if (number == null || number.length() == 0 || !StringUtils.isNumeric(number)) {
+            return "";
+        }
+
+        Object[] args = { Integer.valueOf(number) };
+        InfoTeacher teacher = (InfoTeacher) ServiceUtils.executeService(null, "ReadTeacherByNumber",
+                args);
+
+        return (teacher != null && teacher.getInfoPerson() != null) ? teacher.getInfoPerson().getNome()
+                : "";
+    }
+
+    public static Collection readNonAffiliatedTeachersByName(String name) throws FenixFilterException,
+            FenixServiceException {
+
+        
+        if (name == null || name.length() == 0) {
+            return new ArrayList();
+        }
+
+        IUserView userView = SessionUtils.getUserView(ExecutionContext.get().getHttpServletRequest());        
+        Object[] args = { name };
+        List nonAffiliatedTeachers = (List) ServiceUtils.executeService(userView, "ReadNonAffiliatedTeachersByName", args);
+        return nonAffiliatedTeachers;
+//        return CollectionUtils.collect(nonAffiliatedTeachers, new Transformer() {
+//        
+//            public Object transform(Object arg0) {
+//                InfoNonAffiliatedTeacher infoNonAffiliatedTeacher = (InfoNonAffiliatedTeacher) arg0;
+//                return infoNonAffiliatedTeacher.getName();
+//            }
+//        
+//        });
+
+        
+    }
+
+}
