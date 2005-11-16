17e3805ebd41195e3c443efd80609fbe6163d0f6
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2005-11-16 17:07:46 +0000

    moved to deploy dir

--- etc/database_operations/AlterQualificationTable.sql
+++
@@ -1,4 +0,0 @@
-alter table QUALIFICATION add column `TYPE` varchar(100) NOT NULL;
-alter table QUALIFICATION add column `YEAR` varchar(10) default NULL;
-alter table QUALIFICATION change column `DEGREE` `DEGREE` varchar(200) default NULL;
-alter table QUALIFICATION change column `MARK` `MARK` varchar(100) default NULL;
