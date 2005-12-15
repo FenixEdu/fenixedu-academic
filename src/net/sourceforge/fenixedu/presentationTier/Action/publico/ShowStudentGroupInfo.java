3b9871bdca9e097c3c8e805fb5161e789934cfae
Gonçalo Luíz <goncalo@ist.utl.pt>
2005-12-15 14:57:09 +0000

    changed show student group info from publico to external subapplication fixed show student group info severe bugs due to refactor and arch change

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/ShowStudentGroupInfo.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/ShowStudentGroupInfo.java
@@ -4,13 +4,15 @@
  * changed on 13/Out/2004 20:12:33 (changed servlet output, giving more information)
  *By Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
  */
+
+
 package net.sourceforge.fenixedu.presentationTier.Action.publico;
 
+
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -18,18 +20,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NotAuthorizedException;
-import net.sourceforge.fenixedu.applicationTier.utils.MockUserView;
-import net.sourceforge.fenixedu.dataTransferObject.InfoCurricularCourse;
-import net.sourceforge.fenixedu.dataTransferObject.InfoGrouping;
-import net.sourceforge.fenixedu.dataTransferObject.InfoLesson;
-import net.sourceforge.fenixedu.dataTransferObject.InfoSiteGroupsByShift;
-import net.sourceforge.fenixedu.dataTransferObject.InfoSiteProjects;
-import net.sourceforge.fenixedu.dataTransferObject.InfoSiteShiftsAndGroups;
-import net.sourceforge.fenixedu.dataTransferObject.InfoSiteStudentGroup;
-import net.sourceforge.fenixedu.dataTransferObject.InfoSiteStudentInformation;
-import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
-import net.sourceforge.fenixedu.domain.degree.DegreeType;
+import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.domain.IAttends;
+import net.sourceforge.fenixedu.domain.ICurricularCourse;
+import net.sourceforge.fenixedu.domain.IExecutionCourse;
+import net.sourceforge.fenixedu.domain.IGrouping;
+import net.sourceforge.fenixedu.domain.ILesson;
+import net.sourceforge.fenixedu.domain.IRole;
+import net.sourceforge.fenixedu.domain.IStudentGroup;
 import net.sourceforge.fenixedu.framework.factory.ServiceManagerServiceFactory;
 import net.sourceforge.fenixedu.presentationTier.Action.exceptions.FenixActionException;
 import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
@@ -44,180 +42,152 @@ import org.apache.struts.action.ActionMapping;
  * 
  * 
  * Created at 10/Set/2003, 18:36:21
- *  
+ * 
  */
 
-public class ShowStudentGroupInfo extends Action {
-    public String buildInfo(Integer curricularCourseID, String username, String password) {
-        String result = new String();
-        if (username == null)
-            return new String();
+public class ShowStudentGroupInfo extends Action
+{
+	public String buildInfo(Integer executionCourseID, String username, String password,
+			String requestURL)
+	{
+		String result = new String();
+		if (username == null) return new String();
 
-        try {
-            Object argsAutenticacao[] = { username, password, "" };
-            IUserView userView = (IUserView) ServiceManagerServiceFactory.executeService(null,
-                    "Autenticacao", argsAutenticacao);
+		try
+		{
+			Object argsAutenticacao[] =
+			{ username, password, "", requestURL };
+			IUserView userView = (IUserView) ServiceManagerServiceFactory.executeService(null, "Autenticacao", argsAutenticacao);
 
-            InfoSiteProjects projects = null;
-            try {
-                projects = (InfoSiteProjects) ServiceUtils.executeService(userView,
-                        "teacher.ReadExecutionCourseProjects", new Integer[] { curricularCourseID });
+			IExecutionCourse executionCourse = null;
+			Collection<IGrouping> groupings = null;
+			try
+			{
+				executionCourse = (IExecutionCourse) ServiceUtils.executeService(userView, "ReadDomainObject", new Object[]
+				{ ExecutionCourse.class, executionCourseID });
+				groupings = executionCourse.getGroupings();
+			}
+			catch (FenixServiceException e)
+			{
+				e.printStackTrace();
+				throw new FenixActionException(e);
+			}
 
-            } catch (FenixServiceException e) {
-                e.printStackTrace();
-                throw new FenixActionException(e);
-            }
+			if (executionCourse == null || groupings == null) return new String("-1");
 
-            if (projects == null)
-                return new String("-1");
+			for (Iterator iterGroupings = groupings.iterator(); iterGroupings.hasNext();)
+			{
+				IGrouping grouping = (IGrouping) iterGroupings.next();
 
-            for (Iterator iterProjects = projects.getInfoGroupPropertiesList().iterator(); iterProjects
-                    .hasNext();) {
-                InfoGrouping project = (InfoGrouping) iterProjects.next();
+				if (grouping != null)
+				{
 
-                if (project != null) {
+					for (IStudentGroup studentGroup : grouping.getStudentGroups())
+					{
+						for (IAttends searchingAttendsSet : studentGroup.getAttends())
+						{
+							if (username.equalsIgnoreCase(searchingAttendsSet.getAluno().getPerson().getUsername()))
+							{
+								result = executionCourse.getNome() + "(";
+								Collection<ICurricularCourse> curricularCourses = executionCourse.getAssociatedCurricularCourses();
+								boolean firstCurricularCourse = true;
+								for (ICurricularCourse course : curricularCourses)
+								{
+									if (!firstCurricularCourse) result += ",";
+									result += course.getDegreeCurricularPlan().getName();
+								}
+								result += ")\n";
+								result += grouping.getName() + "\n";
+								int remainingStudents = grouping.getMaximumCapacity().intValue();
+								for (IAttends attends : studentGroup.getAttends())
+								{
+									result += attends.getAluno().getNumber();
+									result += "-";
+									result += attends.getAluno().getActiveStudentCurricularPlan().getDegreeCurricularPlan().getName()
+											+ "\n";
+									remainingStudents--;
+								}
+								while (remainingStudents > 0)
+								{
+									remainingStudents--;
+									result += "N/A\n";
+								}
 
-                    InfoSiteShiftsAndGroups shiftsAndGroups = null;
-                    try {
-                        shiftsAndGroups = (InfoSiteShiftsAndGroups) ServiceUtils.executeService(
-                                userView, "ReadShiftsAndGroups",
-                                new Object[] { project.getIdInternal(),username});
+								result += studentGroup.getGroupNumber() + "\n"
+										+ studentGroup.getShift().getNome() + "\n";
+								for (ILesson lesson : studentGroup.getShift().getAssociatedLessons())
+								{
+									result += lesson.getDiaSemana().toString() + " "
+											+ lesson.getInicio().get(Calendar.HOUR_OF_DAY);
+									int minute = lesson.getInicio().get(Calendar.MINUTE);
+									result += ":";
+									if (minute < 10) result += "0";
+									result += minute + "\n\n";
+								}
 
-                    } catch (NotAuthorizedException e) {
-                        shiftsAndGroups = new InfoSiteShiftsAndGroups();
-                        shiftsAndGroups.setInfoSiteGroupsByShiftList(new ArrayList());
-                    }catch (FenixServiceException e) {
-                        e.printStackTrace();
-                        throw new FenixActionException(e);
-                    }
+							}
+						}
+					}
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			result = "-1";
+		}
 
-                    List shiftsAndGroupsList = shiftsAndGroups.getInfoSiteGroupsByShiftList();
+		return result;
+	}
 
-                    for (Iterator iter = shiftsAndGroupsList.iterator(); iter.hasNext();) {
-                        InfoSiteGroupsByShift infoSiteShiftAndGroups = (InfoSiteGroupsByShift) iter
-                                .next();
-                        for (Iterator iterator2 = infoSiteShiftAndGroups.getInfoSiteStudentGroupsList()
-                                .iterator(); iterator2.hasNext();) {
-                            InfoSiteStudentGroup infoSiteStudentGroup = (InfoSiteStudentGroup) iterator2
-                                    .next();
-                            for (Iterator iterator3 = infoSiteStudentGroup
-                                    .getInfoSiteStudentInformationList().iterator(); iterator3.hasNext();) {
-                                InfoSiteStudentInformation infoSiteStudentInformation = (InfoSiteStudentInformation) iterator3
-                                        .next();
-                                if (username.equalsIgnoreCase(infoSiteStudentInformation.getUsername())) {
-                                    result = projects.getInfoExecutionCourse().getNome() + " ( ";
-                                    int counter = 0;
-                                    for (Iterator iteratorCurricularCourses = projects.getInfoExecutionCourse()
-                                            .getAssociatedInfoCurricularCourses().iterator(); iteratorCurricularCourses
-                                            .hasNext();) {
-                                        InfoCurricularCourse infoCurricularCourse = (InfoCurricularCourse) iteratorCurricularCourses
-                                                .next();
-                                        if (counter > 0)
-                                            result += ",";
-                                        result += infoCurricularCourse.getInfoDegreeCurricularPlan()
-                                                .getName();
-                                        counter++;
-                                    }
-                                    result += " )\n";
-                                    result += project.getName() + "\n";
-                                    int remainingStudents = project.getMaximumCapacity().intValue();
-                                    for (Iterator iteratorWritingResult = infoSiteStudentGroup
-                                            .getInfoSiteStudentInformationList().iterator(); iteratorWritingResult
-                                            .hasNext();) {
-                                        InfoSiteStudentInformation studentInfo = (InfoSiteStudentInformation) iteratorWritingResult
-                                                .next();
-                                        result += studentInfo.getNumber();
-                                        Object[] args = {
-                                                new MockUserView(studentInfo.getUsername(), new ArrayList(), null),
-                                                DegreeType.DEGREE };
-                                        InfoStudentCurricularPlan infoStudentCurricularPlan = null;
-                                        try {
-                                            infoStudentCurricularPlan = (InfoStudentCurricularPlan) ServiceUtils
-                                                    .executeService(
-                                                            userView,
-                                                            "ReadActiveStudentCurricularPlanByDegreeType",
-                                                            args);
-                                        } catch (FenixServiceException e) {
-                                            e.printStackTrace();
-                                            throw new FenixActionException(e);
-                                        }
-                                        result += "-"
-                                                + infoStudentCurricularPlan
-                                                        .getInfoDegreeCurricularPlan().getName() + "\n";
-                                        remainingStudents--;
-                                    }
-                                    while (remainingStudents > 0) {
-                                        result += "N/A\n";
-                                        remainingStudents--;
-                                    }
-                                    result += infoSiteStudentGroup.getInfoStudentGroup()
-                                            .getGroupNumber()
-                                            + "\n"
-                                            + infoSiteStudentGroup.getInfoStudentGroup().getInfoShift()
-                                                    .getNome() + "\n";
-                                    for (Iterator iteratorLessons = infoSiteStudentGroup
-                                            .getInfoStudentGroup().getInfoShift().getInfoLessons()
-                                            .iterator(); iteratorLessons.hasNext();) {
-                                        InfoLesson infoLesson = (InfoLesson) iteratorLessons.next();
-                                        result += infoLesson.getDiaSemana().toString() + " "
-                                                + infoLesson.getInicio().get(Calendar.HOUR_OF_DAY);
-                                        int minute = infoLesson.getInicio().get(Calendar.MINUTE);
-                                        result += ":";
-                                        if (minute < 10)
-                                            result += "0";
-                                        result += minute + "\n\n";
-                                    }
-                                }
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+			HttpServletResponse response) throws FenixActionException
+	{
 
-                            }
+		String result = new String();
 
-                        }
-                    }
-                } else
-                    return new String();
+		if (!request.getScheme().equalsIgnoreCase("https"))
+		{
+			result = "-1";
+		}
+		else
+		{
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            result = "-1";
-        }
+			String studentUsername = request.getParameter("username");
+			String studentPassword = request.getParameter("password");
+			String idsString = request.getParameter("ids");
+			final String requestURL = request.getRequestURL().toString();
 
-        return result;
-    }
+			Integer[] coursesIds =
+			{ new Integer(34811), new Integer(34661), new Integer(34950) };
+			if (idsString != null)
+			{
+				String[] ids = idsString.split(",");
+				coursesIds = new Integer[ids.length];
+				for (int i = 0; i < ids.length; i++)
+					coursesIds[i] = new Integer(ids[i]);
 
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws FenixActionException {
+			}
 
-        String studentUsername = request.getParameter("username");
-        String studentPassword = request.getParameter("password");
-        String idsString = request.getParameter("ids");
+			for (int i = 0; i < coursesIds.length; i++)
+			{
 
-        Integer[] coursesIds = { new Integer(34811), new Integer(34661), new Integer(34950) };
-        if (idsString != null) {
-            String[] ids = idsString.split(",");
-            coursesIds = new Integer[ids.length];
-            for (int i = 0; i < ids.length; i++)
-                coursesIds[i] = new Integer(ids[i]);
-
-        }
-
-        String result = new String();
-
-        for (int i = 0; i < coursesIds.length; i++) {
-
-            result += this.buildInfo(coursesIds[i], studentUsername, studentPassword);
-        }
-        if (result.equals(""))
-            result = "-1";
-        try {
-            ServletOutputStream writer = response.getOutputStream();
-            response.setContentType("text/plain");
-            writer.print(result);
-            writer.flush();
-            response.flushBuffer();
-        } catch (IOException ex) {
-            throw new FenixActionException();
-        }
-        return null;
-    }
+				result += this.buildInfo(coursesIds[i], studentUsername, studentPassword, requestURL);
+			}
+			if (result.equals("")) result = "-1";
+		}
+		try
+		{
+			ServletOutputStream writer = response.getOutputStream();
+			response.setContentType("text/plain");
+			writer.print(result);
+			writer.flush();
+			response.flushBuffer();
+		}
+		catch (IOException ex)
+		{
+			throw new FenixActionException();
+		}
+		return null;
+	}
 }
