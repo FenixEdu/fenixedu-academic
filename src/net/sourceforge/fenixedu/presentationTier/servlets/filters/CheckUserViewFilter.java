1d7e1c8f9111676894db320346cad699913dcfca
Luis Cruz <luis.cruz@ist.utl.pt>
2005-12-20 17:39:48 +0000

    Fixed UserView session checking with a specific filter.

---
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/CheckUserViewFilter.java
@@ -0,0 +1,67 @@
+package net.sourceforge.fenixedu.presentationTier.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionConstants;
+
+public class CheckUserViewFilter implements Filter {
+
+    private static final String REDIRECT = PropertiesManager.getProperty("login.page");
+
+    public void init(FilterConfig config) {
+    }
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        final String uri = request.getRequestURI();
+        final IUserView userView = getUserView(request);
+        if (isPrivateURI(uri) && !validUserView(userView)) {
+            response.sendRedirect(REDIRECT);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private IUserView getUserView(final HttpServletRequest request) {
+        final HttpSession session = request.getSession(false);
+        return (IUserView) ((session != null) ? session.getAttribute(SessionConstants.U_VIEW) : null);
+    }
+
+    private boolean isPrivateURI(final String uri) {
+        return ((uri.indexOf("/CSS/") == -1)
+                && (uri.indexOf("/images/") == -1)
+                && (uri.indexOf("login.do") == -1)
+                && (uri.indexOf("loginPage.jsp") == -1)
+                && (uri.indexOf("logoff.do") == -1)
+                && (uri.indexOf("/publico/") == -1)
+                && (uri.indexOf("showErrorPage.do") == -1)
+                && (uri.indexOf("/manager/manageCache.do") == -1)
+                && (uri.indexOf("/siteMap.do") == -1)
+                && (uri.indexOf("/changeLocaleTo.do") == -1)
+                && (uri.indexOf("/isAlive.do") == -1));
+    }
+
+    private boolean validUserView(final IUserView userView) {
+        return userView != null;
+    }
+
+}
