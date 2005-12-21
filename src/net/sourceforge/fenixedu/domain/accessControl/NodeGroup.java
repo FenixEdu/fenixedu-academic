f6ce7dd43c5aba62e88f9fe7b7ab751e3ae853ee
Shezad Anavarali <shezad@ist.utl.pt>
2005-12-21 15:03:20 +0000

    added method: removePart

--- src/net/sourceforge/fenixedu/domain/accessControl/NodeGroup.java
+++ src/net/sourceforge/fenixedu/domain/accessControl/NodeGroup.java
@@ -8,18 +8,28 @@ public abstract class NodeGroup extends NodeGroup_Base {
         super();
     }
     
+    public void removePart(IUserGroup group)
+    {
+        for (IUserGroup currentGroup : this.getParts()) {
+            if (currentGroup.equals(group))
+            {
+              GroupComposition.remove(group, this);
+              
+              if(group.getAggregatorsCount() == 0){
+                  group.delete();
+              }
+              
+            }
+        }
+    }
+    
     @Override
     public void delete()
     {
     	for (IUserGroup part : this.getParts())
 		{
 			GroupComposition.remove(part,this);
-		}
-    	for(INodeGroup aggregator: this.getAggregators())
-    	{
-    		GroupComposition.remove(this,aggregator);
-    	}
-    	
+		}    	
     	super.delete();
     }
         
