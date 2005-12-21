64b73265634884854469ea3a3c30179c66e26492
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2005-12-21 18:55:49 +0000

    new class that represents a scale

---
+++ src/net/sourceforge/fenixedu/domain/GradeScale.java
@@ -0,0 +1,114 @@
+package net.sourceforge.fenixedu.domain;
+
+import net.sourceforge.fenixedu.util.EvaluationType;
+
+public enum GradeScale {
+	
+	TYPE20  { 
+		protected boolean checkFinal(final String mark) {
+			if(mark.equals(NA) || mark.equals(RE)) {
+				return true;
+			}
+			try {
+				Integer markValue = Integer.valueOf(mark);
+				if((markValue >= 10) && (markValue <= 20)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch(NumberFormatException e) {
+				return false;
+			}
+		}
+		
+		protected boolean checkNotFinal(final String mark) {
+			if(mark.equals(NA)) {
+				return true;
+			}
+			try {
+				Double markValue = Double.valueOf(mark);
+				if((markValue >= 0) && (markValue <= 20)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch(NumberFormatException e) {
+				return false;
+			}
+		}
+	},
+	
+	TYPE5{
+		protected boolean checkFinal(final String mark) {
+			if(mark.equals(NA) || mark.equals(RE)) {
+				return true;
+			}
+			try {
+				Integer markValue = Integer.valueOf(mark);
+				if((markValue >= 3) && (markValue <= 5)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch(NumberFormatException e) {
+				return false;
+			}
+		}
+		
+		protected boolean checkNotFinal(final String mark) {
+			if(mark.equals(NA)) {
+				return true;
+			}
+			try {
+				Double markValue = Double.valueOf(mark);
+				if((markValue >= 0) && (markValue <= 20)) {
+					return true;
+				} else {
+					return false;
+				}
+			} catch(NumberFormatException e) {
+				return false;
+			}
+		}
+	},
+	
+	TYPEAP{
+		protected boolean checkFinal(final String mark) {
+			if(mark.equals(NA) || mark.equals(RE) || mark.equals(AP)) {
+				return true;
+			}
+			return false;
+		}
+		
+		protected boolean checkNotFinal(final String mark) {
+			if(mark.equals(NA) || mark.equals(AP)) {
+				return true;
+			}
+			return false;
+		}
+	};
+	
+
+	private static final String NA = "NA";
+	private static final String RE = "RE";
+	private static final String AP = "AP";
+	
+	public String getName() {
+        return name();
+    }
+    
+    public boolean isValid(String mark, EvaluationType	evaluationType) {
+    	mark = mark.toUpperCase();
+    	if(EvaluationType.FINAL_TYPE.equals(evaluationType)) {
+    		return checkFinal(mark);
+    	} else {
+    		return checkNotFinal(mark);
+    	}
+    	
+    }
+    
+    protected abstract boolean checkFinal(final String mark);
+    
+    protected abstract boolean checkNotFinal(final String mark);
+
+}
