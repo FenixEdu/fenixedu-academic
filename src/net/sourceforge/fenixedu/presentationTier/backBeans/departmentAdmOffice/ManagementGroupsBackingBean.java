c21de67dffd32d8631e090c29781dcceb987e082
Shezad Anavarali <shezad@ist.utl.pt>
2005-12-21 15:04:05 +0000

    now it works...

--- src/net/sourceforge/fenixedu/presentationTier/backBeans/departmentAdmOffice/ManagementGroupsBackingBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/backBeans/departmentAdmOffice/ManagementGroupsBackingBean.java
@@ -3,58 +3,103 @@ package net.sourceforge.fenixedu.presentationTier.backBeans.departmentAdmOffice;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.event.ActionEvent;
+import javax.faces.model.SelectItem;
+
+import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.domain.IDepartment;
+import net.sourceforge.fenixedu.domain.IEmployee;
+import net.sourceforge.fenixedu.domain.IPerson;
+import net.sourceforge.fenixedu.domain.accessControl.IPersonGroup;
+import net.sourceforge.fenixedu.domain.accessControl.IUserGroup;
+import net.sourceforge.fenixedu.domain.department.ICompetenceCourseMembersGroup;
+import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
 import net.sourceforge.fenixedu.presentationTier.backBeans.base.FenixBackingBean;
 
 public class ManagementGroupsBackingBean extends FenixBackingBean {
 
-    private List completeList = getDepartment().getCurrentActiveWorkingEmployees();
-    //private List indexedList = indexFullList();
-    
-    private static int RESULTS_PER_PAGE = 15;
-    private int currentPageIndex;
-    private boolean listIndexing = true;
-    private List<Integer> pageIndexes = initializePageIndexesList();
-    
+    private List<IEmployee> employees = getDepartment().getCurrentActiveWorkingEmployees();
+
+    private Integer[] selectedPersonGroupsIDsToRemove;
+
+    private Integer[] selectedPersonsIDsToAdd;
+
     public IDepartment getDepartment() {
         return getUserView().getPerson().getEmployee().getCurrentDepartmentWorkingPlace();
     }
-    
-    public List getCompleteList() {
-        return completeList;
-    }
-    
-    public int getIndexingSize() {
-        return ManagementGroupsBackingBean.RESULTS_PER_PAGE;
-    }
 
-    public int getCurrentPageIndex() {
-        return this.currentPageIndex;
-    }
-    
-    public boolean getListIndexing() {
-        return this.listIndexing;
-    }
-    
-    public void setListIndexing(boolean listIndexing) {
-        this.listIndexing =  listIndexing;
-    }
-
-    public List<Integer> getPageIndexes() {
-        return this.pageIndexes;
-    }
-
-    private List<Integer> initializePageIndexesList() {
-        int nrPages = completeList.size() / ManagementGroupsBackingBean.RESULTS_PER_PAGE;
-        List<Integer> result = new ArrayList<Integer>(nrPages);
-        for (int iter = 0; iter <= nrPages; iter++) {
-            result.add(iter);
+    public List getDepartmentEmployeesSelectItems() {
+        List<SelectItem> result = new ArrayList<SelectItem>(employees.size());
+        for (IEmployee departmentEmployee : employees) {
+            IPerson person = departmentEmployee.getPerson();
+            result.add(new SelectItem(person.getIdInternal(), person.getNome() + " ("
+                    + person.getUsername() + ")"));
         }
         return result;
     }
 
-    public void onPageChange() {
-        this.currentPageIndex = Integer.valueOf(this.getRequestParameter("pageNumber"));
+    public int getDepartmentEmployeesSize() {
+        return employees.size();
+    }
+
+    public List<SelectItem> getSelectedDepartmentEmployeesSelectItems() throws FenixFilterException,
+            FenixServiceException {
+
+        List<SelectItem> result = new ArrayList<SelectItem>();
+
+        ICompetenceCourseMembersGroup competenceCourseMembersGroup = getDepartment()
+                .getCompetenceCourseMembersGroup();
+        if (competenceCourseMembersGroup != null) {
+            for (IUserGroup member : competenceCourseMembersGroup.getParts()) {
+                IPerson person = ((IPersonGroup) member).getPerson();
+                result.add(new SelectItem(member.getIdInternal(), person.getNome() + " ("
+                        + person.getUsername() + ")"));
+            }
+        }
+
+        return result;
+    }
+
+    public Integer[] getSelectedPersonsIDs() {
+        return null;
+    }
+
+    public void setSelectedPersonsIDs(Integer[] selectedPersonsIDs) {
+        this.selectedPersonsIDsToAdd = selectedPersonsIDs;
+    }
+
+    public Integer[] getSelectedPersonGroupsIDsToRemove() {
+        return null;
+    }
+
+    public void setSelectedPersonGroupsIDsToRemove(Integer[] selectedPersonGroupsIDsToRemove) {
+        this.selectedPersonGroupsIDsToRemove = selectedPersonGroupsIDsToRemove;
+    }
+
+    public void addMembers(ActionEvent event) throws FenixFilterException, FenixServiceException {
+        
+        ICompetenceCourseMembersGroup competenceCourseMembersGroup = getDepartment()
+                .getCompetenceCourseMembersGroup();
+        if (competenceCourseMembersGroup == null) {
+            Object[] args = { getDepartment() };
+            competenceCourseMembersGroup = (ICompetenceCourseMembersGroup) ServiceUtils.executeService(
+                    getUserView(), "CreateCompetenceCourseMembersGroup", args);
+        }
+
+        if (selectedPersonsIDsToAdd != null) {
+            Object[] args = { competenceCourseMembersGroup, selectedPersonsIDsToAdd, null };
+            ServiceUtils.executeService(getUserView(), "UpdateCompetenceCourseMembersGroup", args);
+        }
+    }
+
+    public void removeMembers(ActionEvent event) throws FenixFilterException, FenixServiceException {
+
+        if (selectedPersonGroupsIDsToRemove != null) {
+            Object[] args = { getDepartment().getCompetenceCourseMembersGroup(), null,
+                    selectedPersonGroupsIDsToRemove };
+            ServiceUtils.executeService(getUserView(), "UpdateCompetenceCourseMembersGroup", args);
+        }
     }
 
 }
