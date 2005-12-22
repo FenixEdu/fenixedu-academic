a3d9b74402f2d5959c486d8b73de240978d16ebd
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2005-12-22 16:32:55 +0000

    moved to R2005-12-22

--- etc/database_operations/alterTableDegreeModule.sql
+++
@@ -1,9 +0,0 @@
-alter table DEGREE_MODULE change column NAME NAME varchar(100) default NULL;
-alter table DEGREE_MODULE change column CODE CODE varchar(50) default NULL;
-alter table DEGREE_MODULE change column ENROLLMENT_ALLOWED ENROLLMENT_ALLOWED tinyint(1) default '1';
-alter table DEGREE_MODULE change column ACRONYM ACRONYM varchar(20) default NULL;
-alter table DEGREE_MODULE drop key U1;
-alter table DEGREE_MODULE drop key KEY_DEGREE_CURRICULAR_PLAN;
-
-alter table DEGREE_MODULE add column PREREQUISITES text;
-alter table DEGREE_MODULE add column PREREQUISITES_EN text;
