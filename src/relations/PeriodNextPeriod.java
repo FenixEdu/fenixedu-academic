b2e22dd2406e0827120c86c2ac7dc2a0941acc75
Luis Cruz <luis.cruz@ist.utl.pt>
2006-01-04 20:16:36 +0000

    Removed unused imports.

--- src/relations/PeriodNextPeriod.java
+++ src/relations/PeriodNextPeriod.java
@@ -1,5 +1,8 @@
 package relations;
 
+import net.sourceforge.fenixedu.stm.VBox;
+import net.sourceforge.fenixedu.stm.RelationList;
+import net.sourceforge.fenixedu.stm.OJBFunctionalSetWrapper;
 public class PeriodNextPeriod extends PeriodNextPeriod_Base {
     
 }
