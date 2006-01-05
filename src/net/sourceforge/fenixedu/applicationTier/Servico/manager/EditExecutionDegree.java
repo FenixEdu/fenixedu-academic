5b4816436afd9a8d7c39358bc62cf31682313881
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-05 16:02:20 +0000

    removed simpleLockWrites

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/EditExecutionDegree.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/EditExecutionDegree.java
@@ -1,9 +1,5 @@
-/*
- * Created on 18/Ago/2003
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.ExistingServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegree;
@@ -14,60 +10,41 @@ import net.sourceforge.fenixedu.domain.ICampus;
 import net.sourceforge.fenixedu.domain.IExecutionDegree;
 import net.sourceforge.fenixedu.domain.IExecutionYear;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionDegree;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionYear;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-import net.sourceforge.fenixedu.persistenceTier.exceptions.ExistingPersistentException;
-import net.sourceforge.fenixedu.persistenceTier.places.campus.IPersistentCampus;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
-/**
- * @author lmac1
- */
 public class EditExecutionDegree implements IService {
 
-    public void run(InfoExecutionDegree infoExecutionDegree) throws FenixServiceException, ExcepcaoPersistencia {
+    public void run(InfoExecutionDegree infoExecutionDegree) throws FenixServiceException,
+            ExcepcaoPersistencia {
 
-        IPersistentExecutionDegree persistentExecutionDegree = null;
-        IExecutionYear executionYear = null;
+        final ISuportePersistente persistentSuport = PersistenceSupportFactory
+                .getDefaultPersistenceSupport();
 
-        try {
-            ISuportePersistente persistentSuport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-            persistentExecutionDegree = persistentSuport.getIPersistentExecutionDegree();
-
-            IExecutionDegree oldExecutionDegree = (IExecutionDegree) persistentExecutionDegree.readByOID(
-                    ExecutionDegree.class, infoExecutionDegree.getIdInternal());
-
-            IPersistentCampus campusDAO = persistentSuport.getIPersistentCampus();
-
-            ICampus campus = (ICampus) campusDAO.readByOID(Campus.class, infoExecutionDegree
-                    .getInfoCampus().getIdInternal());
-            if (campus == null) {
-                throw new NonExistingServiceException("message.nonExistingCampus", null);
-            }
-
-            if (oldExecutionDegree == null) {
-                throw new NonExistingServiceException("message.nonExistingExecutionDegree", null);
-            }
-            IPersistentExecutionYear persistentExecutionYear = persistentSuport
-                    .getIPersistentExecutionYear();
-
-            executionYear = (IExecutionYear) persistentExecutionYear.readByOID(ExecutionYear.class,
-                    infoExecutionDegree.getInfoExecutionYear().getIdInternal());
-
-            if (executionYear == null) {
-                throw new NonExistingServiceException("message.non.existing.execution.year", null);
-            }
-            persistentExecutionDegree.simpleLockWrite(oldExecutionDegree);
-            oldExecutionDegree.setExecutionYear(executionYear);
-
-            oldExecutionDegree.setTemporaryExamMap(infoExecutionDegree.getTemporaryExamMap());
-
-            oldExecutionDegree.setCampus(campus);
-
-        } catch (ExistingPersistentException ex) {
-            throw new ExistingServiceException(ex);
+        final IExecutionDegree oldExecutionDegree = (IExecutionDegree) persistentSuport
+                .getIPersistentObject().readByOID(ExecutionDegree.class,
+                        infoExecutionDegree.getIdInternal());
+        if (oldExecutionDegree == null) {
+            throw new NonExistingServiceException("message.nonExistingExecutionDegree", null);
         }
+
+        final ICampus campus = (ICampus) persistentSuport.getIPersistentObject().readByOID(Campus.class,
+                infoExecutionDegree.getInfoCampus().getIdInternal());
+        if (campus == null) {
+            throw new NonExistingServiceException("message.nonExistingCampus", null);
+        }
+
+        final IExecutionYear executionYear = (IExecutionYear) persistentSuport.getIPersistentObject()
+                .readByOID(ExecutionYear.class,
+                        infoExecutionDegree.getInfoExecutionYear().getIdInternal());
+        if (executionYear == null) {
+            throw new NonExistingServiceException("message.non.existing.execution.year", null);
+        }
+
+        oldExecutionDegree.setCampus(campus);
+        oldExecutionDegree.setExecutionYear(executionYear);
+        oldExecutionDegree.setTemporaryExamMap(infoExecutionDegree.getTemporaryExamMap());
     }
+
 }
