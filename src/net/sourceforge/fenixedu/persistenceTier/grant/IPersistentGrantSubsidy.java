a5999047c21ef14b559ab70c68f7e683d9f184f9
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-05 14:05:00 +0000

    *** empty log message ***

--- src/net/sourceforge/fenixedu/persistenceTier/grant/IPersistentGrantSubsidy.java
+++ src/net/sourceforge/fenixedu/persistenceTier/grant/IPersistentGrantSubsidy.java
@@ -2,16 +2,14 @@ package net.sourceforge.fenixedu.persistenceTier.grant;
 
 import java.util.List;
 
+import net.sourceforge.fenixedu.domain.grant.contract.IGrantSubsidy;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
 
-/**
- * @author Pica
- * @author Barbosa
- */
 public interface IPersistentGrantSubsidy extends IPersistentObject {
     public List readAllSubsidiesByGrantContract(Integer idContract) throws ExcepcaoPersistencia;
 
-    public List readAllSubsidiesByGrantContractAndState(Integer idContract, Integer state)
+    public List<IGrantSubsidy> readAllSubsidiesByGrantContractAndState(Integer idContract, Integer state)
             throws ExcepcaoPersistencia;
+
 }
