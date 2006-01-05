1d7ba77e91b6be88c7c79bbe1b06ea650bc055a9
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-05 14:41:36 +0000

    removed simpleLockWrites

--- src/net/sourceforge/fenixedu/applicationTier/Servico/managementAssiduousness/WriteExtraWork.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/managementAssiduousness/WriteExtraWork.java
@@ -1,11 +1,7 @@
-/*
- * Created on 12/Feb/2005
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.managementAssiduousness;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.managementAssiduousness.InfoExtraWork;
@@ -25,90 +21,77 @@ import org.apache.commons.collections.Transformer;
 
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
-/**
- * @author Tânia Pousão
- * 
- */
 public class WriteExtraWork implements IService {
 
-	public List run(String usernameWho, List infoExtraWorkList, Integer employeeNumber,
-			String compensation) throws Exception {
-		List infoExtraWorkListAfterWrite = null;
-		List extraWorkList = null;
-		ISuportePersistente sp;
+    public List run(String usernameWho, List<InfoExtraWork> infoExtraWorkList, Integer employeeNumber,
+            String compensation) throws Exception {
 
-		sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-		IPessoaPersistente personDAO = sp.getIPessoaPersistente();
-		IPersistentEmployee employeeDAO = sp.getIPersistentEmployee();
+        final IPessoaPersistente personDAO = sp.getIPessoaPersistente();
+        IPerson personWho = personDAO.lerPessoaPorUsername(usernameWho);
 
-		// Read employee logged
-		IEmployee employeeWho = null;
-		IPerson personWho = personDAO.lerPessoaPorUsername(usernameWho);
-		if (personWho != null) {
-			employeeWho = employeeDAO.readByPerson(personWho.getIdInternal().intValue());
-		}
-		IEmployee employee = employeeDAO.readByNumber(employeeNumber);
-		if (employee == null) {
-			// TODO
-		}
+        // Read employee logged
+        final IPersistentEmployee employeeDAO = sp.getIPersistentEmployee();
+        IEmployee employeeWho = null;
+        if (personWho != null) {
+            employeeWho = employeeDAO.readByPerson(personWho.getIdInternal().intValue());
+        }
+        IEmployee employee = employeeDAO.readByNumber(employeeNumber);
+        if (employee == null) {
+            // TODO
+        }
 
-		Iterator iterator = infoExtraWorkList.listIterator();
-		extraWorkList = new ArrayList();
-		IPersistentExtraWork extraWorkDAO = sp.getIPersistentExtraWork();
-		while (iterator.hasNext()) {
-			InfoExtraWork infoExtraWork = (InfoExtraWork) iterator.next();
+        final IPersistentExtraWork extraWorkDAO = sp.getIPersistentExtraWork();
+        List<IExtraWork> extraWorkList = new ArrayList<IExtraWork>();
+        for (InfoExtraWork infoExtraWork : infoExtraWorkList) {
+            IExtraWork extraWork = null;
+            if (infoExtraWork.getIdInternal() != null && infoExtraWork.getIdInternal().intValue() > 0) {
+                extraWork = (IExtraWork) extraWorkDAO.readByOID(ExtraWork.class, infoExtraWork
+                        .getIdInternal());
+            }
+            if (extraWork == null) {
+                extraWork = extraWorkDAO.readExtraWorkByDay(infoExtraWork.getDay());
+            }
 
-			IExtraWork extraWork = null;
-			if (infoExtraWork.getIdInternal() != null && infoExtraWork.getIdInternal().intValue() > 0) {
-				extraWork = (IExtraWork) extraWorkDAO.readByOID(ExtraWork.class, infoExtraWork
-						.getIdInternal());
-			}
-			if (extraWork == null) {
-				extraWork = extraWorkDAO.readExtraWorkByDay(infoExtraWork.getDay());
-			}
+            if (extraWork == null) {
+                extraWork = new ExtraWork();
+            }
 
-			if (extraWork == null) {
-				extraWork = new ExtraWork();
-			}
+            extraWork.setDay(infoExtraWork.getDay());
+            extraWork.setBeginHour(infoExtraWork.getBeginHour());
+            extraWork.setEndHour(infoExtraWork.getEndHour());
+            extraWork.setMealSubsidy(infoExtraWork.getMealSubsidy());
+            extraWork.setMealSubsidyAuthorized(infoExtraWork.getMealSubsidyAuthorized());
+            extraWork.setDiurnalFirstHour(infoExtraWork.getDiurnalFirstHour());
+            extraWork.setDiurnalFirstHourAuthorized(infoExtraWork.getDiurnalFirstHourAuthorized());
+            extraWork.setDiurnalAfterSecondHour(infoExtraWork.getDiurnalAfterSecondHour());
+            extraWork.setDiurnalAfterSecondHourAuthorized(infoExtraWork
+                    .getDiurnalAfterSecondHourAuthorized());
+            extraWork.setNocturnalFirstHour(infoExtraWork.getNocturnalFirstHour());
+            extraWork.setNocturnalFirstHourAuthorized(infoExtraWork.getNocturnalFirstHourAuthorized());
+            extraWork.setNocturnalAfterSecondHour(infoExtraWork.getNocturnalAfterSecondHour());
+            extraWork.setNocturnalAfterSecondHourAuthorized(infoExtraWork
+                    .getNocturnalAfterSecondHourAuthorized());
+            extraWork.setRestDay(infoExtraWork.getRestDay());
+            extraWork.setRestDayAuthorized(infoExtraWork.getRestDayAuthorized());
 
-			extraWorkDAO.simpleLockWrite(extraWork);
+            extraWork.setEmployee(employee);
 
-			extraWork.setDay(infoExtraWork.getDay());
-			extraWork.setBeginHour(infoExtraWork.getBeginHour());
-			extraWork.setEndHour(infoExtraWork.getEndHour());
-			extraWork.setMealSubsidy(infoExtraWork.getMealSubsidy());
-			extraWork.setMealSubsidyAuthorized(infoExtraWork.getMealSubsidyAuthorized());
-			extraWork.setDiurnalFirstHour(infoExtraWork.getDiurnalFirstHour());
-			extraWork.setDiurnalFirstHourAuthorized(infoExtraWork.getDiurnalFirstHourAuthorized());
-			extraWork.setDiurnalAfterSecondHour(infoExtraWork.getDiurnalAfterSecondHour());
-			extraWork.setDiurnalAfterSecondHourAuthorized(infoExtraWork
-					.getDiurnalAfterSecondHourAuthorized());
-			extraWork.setNocturnalFirstHour(infoExtraWork.getNocturnalFirstHour());
-			extraWork.setNocturnalFirstHourAuthorized(infoExtraWork.getNocturnalFirstHourAuthorized());
-			extraWork.setNocturnalAfterSecondHour(infoExtraWork.getNocturnalAfterSecondHour());
-			extraWork.setNocturnalAfterSecondHourAuthorized(infoExtraWork
-					.getNocturnalAfterSecondHourAuthorized());
-			extraWork.setRestDay(infoExtraWork.getRestDay());
-			extraWork.setRestDayAuthorized(infoExtraWork.getRestDayAuthorized());
+            extraWork.setWho(employeeWho.getIdInternal().intValue());
+            extraWork.setWhoEmployee(employeeWho);
+            extraWork.setWhen(Calendar.getInstance().getTime());
+            extraWorkList.add(extraWork);
+        }
 
-			extraWork.setEmployee(employee);
+        return (List) CollectionUtils.collect(extraWorkList, new Transformer() {
 
-			extraWork.setWho(employeeWho.getIdInternal().intValue());
-			extraWork.setWhoEmployee(employeeWho);
-			extraWork.setWhen(Calendar.getInstance().getTime());
-			extraWorkList.add(extraWork);
-		}
+            public Object transform(Object arg0) {
+                ExtraWork extraWork = (ExtraWork) arg0;
+                return InfoExtraWorkWithAll.newInfoFromDomain(extraWork);
+            }
 
-		infoExtraWorkListAfterWrite = (List) CollectionUtils.collect(extraWorkList, new Transformer() {
+        });
+    }
 
-			public Object transform(Object arg0) {
-				ExtraWork extraWork = (ExtraWork) arg0;
-				return InfoExtraWorkWithAll.newInfoFromDomain(extraWork);
-			}
-
-		});
-
-		return infoExtraWorkListAfterWrite;
-	}
 }
