f09e8beed8b7d6c39e8def9e3d3c92a0115474b9
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-06 14:33:33 +0000

    bug fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/SaveTeachersBody.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/SaveTeachersBody.java
@@ -12,24 +12,22 @@ import net.sourceforge.fenixedu.domain.Professorship;
 import net.sourceforge.fenixedu.domain.Teacher;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentExecutionCourse;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentTeacher;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
 public class SaveTeachersBody implements IService {
 
-    public Boolean run(final List responsibleTeachersIds, final List professorShipTeachersIds,
+    public Boolean run(final List responsibleTeachersIds, final List<Integer> professorShipTeachersIds,
             final Integer executionCourseId) throws FenixServiceException, ExcepcaoPersistencia {
 
         final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
         final IPersistentExecutionCourse persistentExecutionCourse = sp.getIPersistentExecutionCourse();
-        final IPersistentTeacher persistentTeacher = sp.getIPersistentTeacher();
 
-        final IExecutionCourse executionCourse = (IExecutionCourse) persistentExecutionCourse.
-                readByOID(ExecutionCourse.class, executionCourseId);
+        final IExecutionCourse executionCourse = (IExecutionCourse) persistentExecutionCourse.readByOID(
+                ExecutionCourse.class, executionCourseId);
 
-        final List<Integer> auxProfessorshipTeacherIDs = new ArrayList(professorShipTeachersIds);
+        final List<Integer> auxProfessorshipTeacherIDs = new ArrayList<Integer>(professorShipTeachersIds);
 
         final List<IProfessorship> professorships = executionCourse.getProfessorships();
         for (int i = 0; i < professorships.size(); i++) {
@@ -38,7 +36,10 @@ public class SaveTeachersBody implements IService {
             final Integer teacherID = teacher.getIdInternal();
             if (auxProfessorshipTeacherIDs.contains(teacherID)) {
                 if (responsibleTeachersIds.contains(teacherID) && !professorship.getResponsibleFor()) {
-                        professorship.setResponsibleFor(Boolean.TRUE);
+                    professorship.setResponsibleFor(Boolean.TRUE);
+                } else if (!responsibleTeachersIds.contains(teacherID)
+                        && professorship.getResponsibleFor()) {
+                    professorship.setResponsibleFor(Boolean.FALSE);
                 }
                 auxProfessorshipTeacherIDs.remove(teacherID);
             } else {
@@ -48,11 +49,13 @@ public class SaveTeachersBody implements IService {
         }
 
         for (final Integer teacherID : auxProfessorshipTeacherIDs) {
-            final ITeacher teacher = (ITeacher) persistentTeacher.readByOID(Teacher.class, teacherID);
+            final ITeacher teacher = (ITeacher) sp.getIPersistentObject().readByOID(Teacher.class,
+                    teacherID);
             final Boolean isResponsible = Boolean.valueOf(responsibleTeachersIds.contains(teacherID));
             Professorship.create(isResponsible, executionCourse, teacher, null);
         }
 
         return Boolean.TRUE;
     }
+
 }
