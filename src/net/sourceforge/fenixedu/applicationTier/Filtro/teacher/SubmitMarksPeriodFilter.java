6fc633f094d6c7ed15e5bfdbdf88ddcb18d703f3
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-01-09 10:48:46 +0000

    filter that checks submit marks period

---
+++ src/net/sourceforge/fenixedu/applicationTier/Filtro/teacher/SubmitMarksPeriodFilter.java
@@ -0,0 +1,38 @@
+package net.sourceforge.fenixedu.applicationTier.Filtro.teacher;
+
+import java.util.Date;
+
+import net.sourceforge.fenixedu.applicationTier.Filtro.Filtro;
+import net.sourceforge.fenixedu.applicationTier.Filtro.exception.OutOfPeriodFilterException;
+import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
+import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
+import net.sourceforge.fenixedu.util.DateFormatUtil;
+import pt.utl.ist.berserk.ServiceRequest;
+import pt.utl.ist.berserk.ServiceResponse;
+
+public class SubmitMarksPeriodFilter extends Filtro {
+	
+	@Override
+	public void execute(ServiceRequest request, ServiceResponse response)
+			throws Exception {
+		String startString = "2006-01-04";
+		String endString = "2006-02-20";
+		Date start = DateFormatUtil.parse("yyyy-MM-dd", startString);
+		Date end = DateFormatUtil.parse("yyyy-MM-dd", endString);
+		Date now = new Date();
+		Integer executionPeriodId = 83;
+		
+		ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
+		Object[] arguments = getServiceCallArguments(request);
+		ExecutionCourse executionCourse = (ExecutionCourse) sp.getIPersistentObject().readByOID(ExecutionCourse.class, (Integer) arguments[0]);
+		if(executionCourse != null) {
+			if(executionCourse.getExecutionPeriod().getIdInternal().equals(executionPeriodId)) {
+				if(now.after(start) && now.before(end)) {
+					return;
+				}
+			}
+		}
+		throw new OutOfPeriodFilterException("error.submitMarks.outOfPeriod", null, null); 
+	}
+}
