10f43b3a3b038d1855313fa55aa0ff8972202c48
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-09 08:00:56 +0000

    removed simpleLockWrites

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/EditInsuranceValueByExecutionYearID.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/EditInsuranceValueByExecutionYearID.java
@@ -4,20 +4,12 @@ import java.util.Date;
 
 import net.sourceforge.fenixedu.domain.DomainFactory;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
-import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.InsuranceValue;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentInsuranceValue;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
-/**
- * 
- * @author <a href="mailto:sana@ist.utl.pt">Shezad Anavarali </a>
- * @author <a href="mailto:naat@ist.utl.pt">Nadir Tarmahomed </a>
- *  
- */
 public class EditInsuranceValueByExecutionYearID implements IService {
 
     public void run(Integer executionYearID, Double annualValue, Date endDate)
@@ -25,20 +17,16 @@ public class EditInsuranceValueByExecutionYearID implements IService {
 
         final ISuportePersistente sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
-        final ExecutionYear executionYear = (ExecutionYear) sp.getIPersistentExecutionYear().readByOID(
+        final ExecutionYear executionYear = (ExecutionYear) sp.getIPersistentObject().readByOID(
                 ExecutionYear.class, executionYearID);
 
-        final IPersistentInsuranceValue insuranceValueDAO = sp.getIPersistentInsuranceValue();
-        InsuranceValue insuranceValue = insuranceValueDAO.readByExecutionYear(executionYear
-                .getIdInternal());
-
+        InsuranceValue insuranceValue = executionYear.getInsuranceValue();
+        
         if (insuranceValue != null) {
-            insuranceValueDAO.simpleLockWrite(insuranceValue);
             insuranceValue.setEndDate(endDate);
             insuranceValue.setAnnualValue(annualValue);
         } else {
             insuranceValue = DomainFactory.makeInsuranceValue(executionYear, annualValue, endDate);
-            insuranceValueDAO.simpleLockWrite(insuranceValue);
         }
     }
 
