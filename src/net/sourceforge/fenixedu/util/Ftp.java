e392f63cc3828cfb2b746d52f1c1dc60c9c8b5c1
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-01-13 09:32:34 +0000

    added public static void enviarFicheiros(String nomeFicheiroConfig, Map<String, InputStream> files, String caminho)

--- src/net/sourceforge/fenixedu/util/Ftp.java
+++ src/net/sourceforge/fenixedu/util/Ftp.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import net.sourceforge.fenixedu.persistenceTierJDBC.config.IST2002Properties;
@@ -17,155 +18,204 @@ import org.apache.commons.net.ftp.FTPReply;
  * @author Fernanda Quitério 06/Out/2003
  */
 public class Ftp extends FenixUtil {
-    private static Properties _properties = null;
+	private static Properties _properties = null;
 
-    private static String _userName;
+	private static String _userName;
 
-    private static String _password;
+	private static String _password;
 
-    private static String _urlFTP;
+	private static String _urlFTP;
 
-    private static String _comandoSCP;
+	private static String _comandoSCP;
 
-    private static String _argsSCP;
+	private static String _argsSCP;
 
-    private static void carregaPropriedades(String filename) {
-        _properties = new IST2002Properties(filename);
-        _userName = _properties.getProperty("IST.FTP.server.usernameFTP");
-        _password = _properties.getProperty("IST.FTP.server.passwordFTP");
-        _urlFTP = _properties.getProperty("IST.FTP.server.urlFTP");
-        _comandoSCP = _properties.getProperty("IST.FTP.scp.command");
-        _argsSCP = _properties.getProperty("IST.FTP.scp.args");
-        return;
-    }
+	private static void carregaPropriedades(String filename) {
+		_properties = new IST2002Properties(filename);
+		_userName = _properties.getProperty("IST.FTP.server.usernameFTP");
+		_password = _properties.getProperty("IST.FTP.server.passwordFTP");
+		_urlFTP = _properties.getProperty("IST.FTP.server.urlFTP");
+		_comandoSCP = _properties.getProperty("IST.FTP.scp.command");
+		_argsSCP = _properties.getProperty("IST.FTP.scp.args");
+		return;
+	}
 
-    public static void enviarFicheiro(String nomeFicheiroConfig, String nomeFicheiro, String caminho)
-            throws IOException {
-        carregaPropriedades(nomeFicheiroConfig);
+	public static void enviarFicheiro(String nomeFicheiroConfig,
+			String nomeFicheiro, String caminho) throws IOException {
+		carregaPropriedades(nomeFicheiroConfig);
 
-        FTPClient ftp = new FTPClient();
-        try {
-            int reply;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
 
-            ftp.connect(_urlFTP);
+			ftp.connect(_urlFTP);
 
-            // After connection attempt, you should check the reply code to
-            // verify
-            // success.
-            reply = ftp.getReplyCode();
+			// After connection attempt, you should check the reply code to
+			// verify
+			// success.
+			reply = ftp.getReplyCode();
 
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                ftp.disconnect();
-                System.err.println("FTP server refused connection.");
-                throw new IOException("error.impossible.submit");
-            }
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				throw new IOException("error.impossible.submit");
+			}
 
-            ftp.login(_userName, _password);
-            InputStream inputStream = new FileInputStream(System.getProperty("java.io.tmpdir")
-                    + File.separator + nomeFicheiro);
+			ftp.login(_userName, _password);
+			InputStream inputStream = new FileInputStream(System
+					.getProperty("java.io.tmpdir")
+					+ File.separator + nomeFicheiro);
 
-            ftp.storeFile(caminho + nomeFicheiro, inputStream);
-            inputStream.close();
-            ftp.disconnect();
+			ftp.storeFile(caminho + nomeFicheiro, inputStream);
+			inputStream.close();
+			ftp.disconnect();
 
-        } catch (IOException e) {
-            if (ftp.isConnected()) {
-                try {
-                    ftp.disconnect();
-                } catch (IOException f) {
-                    // do nothing
-                }
-            }
-            System.err.println("Could not connect to server.");
-            e.printStackTrace();
-            throw new IOException("error.impossible.submit");
-        }
-    }
+		} catch (IOException e) {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException f) {
+					// do nothing
+				}
+			}
+			System.err.println("Could not connect to server.");
+			e.printStackTrace();
+			throw new IOException("error.impossible.submit");
+		}
+	}
 
-    public static void enviarFicheiros(String nomeFicheiroConfig, List files, String caminho)
-            throws IOException {
-        carregaPropriedades(nomeFicheiroConfig);
+	public static void enviarFicheiros(String nomeFicheiroConfig, List files,
+			String caminho) throws IOException {
+		carregaPropriedades(nomeFicheiroConfig);
 
-        FTPClient ftp = new FTPClient();
-        try {
-            int reply;
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
 
-            ftp.connect(_urlFTP);
+			ftp.connect(_urlFTP);
 
-            // After connection attempt, you should check the reply code to
-            // verify
-            // success.
-            reply = ftp.getReplyCode();
+			// After connection attempt, you should check the reply code to
+			// verify
+			// success.
+			reply = ftp.getReplyCode();
 
-            if (!FTPReply.isPositiveCompletion(reply)) {
-                ftp.disconnect();
-                System.err.println("FTP server refused connection.");
-                throw new IOException("error.impossible.submit");
-            }
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				throw new IOException("error.impossible.submit");
+			}
 
-            ftp.login(_userName, _password);
-            Iterator iter = files.iterator();
-            while (iter.hasNext()) {
-                File file = (File) iter.next();
-                InputStream inputStream = new FileInputStream(System.getProperty("java.io.tmpdir")
-                        + File.separator + file.getName());
+			ftp.login(_userName, _password);
+			Iterator iter = files.iterator();
+			while (iter.hasNext()) {
+				File file = (File) iter.next();
+				InputStream inputStream = new FileInputStream(System
+						.getProperty("java.io.tmpdir")
+						+ File.separator + file.getName());
 
-                ftp.storeFile(caminho + file.getName(), inputStream);
-                inputStream.close();
-            }
+				ftp.storeFile(caminho + file.getName(), inputStream);
+				inputStream.close();
+			}
 
-            ftp.disconnect();
+			ftp.disconnect();
 
-        } catch (IOException e) {
-            if (ftp.isConnected()) {
-                try {
-                    ftp.disconnect();
-                } catch (IOException f) {
-                    // do nothing
-                }
-            }
-            System.err.println("Could not connect to server.");
-            e.printStackTrace();
-            throw new IOException("error.impossible.submit");
-        }
-    }
+		} catch (IOException e) {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException f) {
+					// do nothing
+				}
+			}
+			System.err.println("Could not connect to server.");
+			e.printStackTrace();
+			throw new IOException("error.impossible.submit");
+		}
+	}
 
-    public static void enviarFicheiroScp(String nomeFicheiroConfig, String nomeFicheiro, String caminho)
-            throws IOException {
+	public static void enviarFicheiros(String nomeFicheiroConfig, Map<String, InputStream> files,
+			String caminho) throws IOException {
+		carregaPropriedades(nomeFicheiroConfig);
 
-        carregaPropriedades(nomeFicheiroConfig);
+		FTPClient ftp = new FTPClient();
+		try {
+			int reply;
 
-        try {
+			ftp.connect(_urlFTP);
 
-            // as seguintes variaveis sao para ser definidas no ficheiro de
-            // propriedades
-            // depois de definidas comentar estas linhas
+			// After connection attempt, you should check the reply code to
+			// verify
+			// success.
+			reply = ftp.getReplyCode();
 
-            //			String _scp_path = "/usr/bin/scp";
-            //			String _scp_args = "-v -i /etc/tomcat4/id_dsa";
-            //			String _scp_user = "newss";
-            //			String _scp_host = "www.ist.utl.pt";
-            // fim de comentario
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				throw new IOException("error.impossible.submit");
+			}
 
-            String command = _comandoSCP + " " + _argsSCP + " " + System.getProperty("java.io.tmpdir")
-                    + "/" + nomeFicheiro + " " + _userName + "@" + _urlFTP + ":" + caminho;
+			ftp.login(_userName, _password);
+			Iterator<String> iter = files.keySet().iterator();
+			while (iter.hasNext()) {
+				String fileName = iter.next();
+				InputStream inputStream = files.get(fileName);
+				ftp.storeFile(caminho + fileName, inputStream);
+				inputStream.close();
+			}
 
-            Runtime runtime = Runtime.getRuntime();
-            Process proc = runtime.exec(command);
+			ftp.disconnect();
 
-            // check for scp failure
-            try {
-                if (proc.waitFor() != 0) {
-                    System.err.println("exit value = " + proc.exitValue());
-                }
-            } catch (InterruptedException e) {
-                System.err.println(e);
-            }
+		} catch (IOException e) {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException f) {
+					// do nothing
+				}
+			}
+			System.err.println("Could not connect to server.");
+			e.printStackTrace();
+			throw new IOException("error.impossible.submit");
+		}
+	}
 
-        } catch (IOException e) {
-            System.err.println("Could not connect to server.");
-            throw new IOException("error.impossible.submit");
-        }
-    }
+	public static void enviarFicheiroScp(String nomeFicheiroConfig,
+			String nomeFicheiro, String caminho) throws IOException {
+
+		carregaPropriedades(nomeFicheiroConfig);
+
+		try {
+
+			// as seguintes variaveis sao para ser definidas no ficheiro de
+			// propriedades
+			// depois de definidas comentar estas linhas
+
+			//			String _scp_path = "/usr/bin/scp";
+			//			String _scp_args = "-v -i /etc/tomcat4/id_dsa";
+			//			String _scp_user = "newss";
+			//			String _scp_host = "www.ist.utl.pt";
+			// fim de comentario
+
+			String command = _comandoSCP + " " + _argsSCP + " "
+					+ System.getProperty("java.io.tmpdir") + "/" + nomeFicheiro
+					+ " " + _userName + "@" + _urlFTP + ":" + caminho;
+
+			Runtime runtime = Runtime.getRuntime();
+			Process proc = runtime.exec(command);
+
+			// check for scp failure
+			try {
+				if (proc.waitFor() != 0) {
+					System.err.println("exit value = " + proc.exitValue());
+				}
+			} catch (InterruptedException e) {
+				System.err.println(e);
+			}
+
+		} catch (IOException e) {
+			System.err.println("Could not connect to server.");
+			throw new IOException("error.impossible.submit");
+		}
+	}
 
 }
