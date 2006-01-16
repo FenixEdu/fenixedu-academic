b4b86e7d6ddc3fc95a885ef6a533d7172c24a5c8
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-16 09:14:21 +0000

    quicker way to verify unique pair name/degree

--- src/net/sourceforge/fenixedu/applicationTier/Servico/scientificCouncil/curricularPlans/EditDegreeCurricularPlan.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/scientificCouncil/curricularPlans/EditDegreeCurricularPlan.java
@@ -1,7 +1,5 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.scientificCouncil.curricularPlans;
 
-import java.util.List;
-
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
@@ -31,16 +29,12 @@ public class EditDegreeCurricularPlan implements IService {
         }
 
         // assert unique pair name/degree
-        final List<DegreeCurricularPlan> dcps = (List<DegreeCurricularPlan>) persistentSupport
-                .getIPersistentDegreeCurricularPlan().readFromNewDegreeStructure();
-        for (DegreeCurricularPlan dcp : dcps) {
-            if (dcp != dcpToEdit) {
-                if (dcp.getDegree() == dcpToEdit.getDegree() && dcp.getName().equalsIgnoreCase(name)) {
-                    throw new FenixServiceException("error.degreeCurricularPlan.existing.name.and.degree");
-                }
+        for (final DegreeCurricularPlan degreeCurricularPlan : dcpToEdit.getDegree().getDegreeCurricularPlans()) {
+            if (degreeCurricularPlan != dcpToEdit && degreeCurricularPlan.getName().equalsIgnoreCase(name)) {
+                throw new FenixServiceException("error.degreeCurricularPlan.existing.name.and.degree");
             }
         }
-
+        
         dcpToEdit.edit(name, ectsCredits, curricularStage, gradeScale);
     }
 
