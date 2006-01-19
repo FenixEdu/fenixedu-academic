a17c5c8cf68de5228f171cae37cfb172a0d945f1
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-01-19 15:06:37 +0000

    added catch for FenixServiceException

--- src/net/sourceforge/fenixedu/presentationTier/Action/person/ChangePersonPasswordAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/person/ChangePersonPasswordAction.java
@@ -9,12 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidPasswordServiceException;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixAction;
 import net.sourceforge.fenixedu.presentationTier.Action.exceptions.InvalidPasswordActionException;
 import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
 import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionConstants;
 
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -43,6 +46,12 @@ public class ChangePersonPasswordAction extends FenixAction {
                 ServiceUtils.executeService(userView, "ChangePassword", args);
             } catch (InvalidPasswordServiceException e) {
                 throw new InvalidPasswordActionException(e);
+            } catch (FenixServiceException e) {
+            	ActionError error = new ActionError("error.person.impossible.change", e.getMessage());
+            	ActionErrors actionErrors = new ActionErrors();
+            	actionErrors.add("error.person.impossible.change", error);
+            	saveErrors(request, actionErrors);
+            	return mapping.getInputForward();
             }
             return mapping.findForward("Success");
         }
