57e43e143faa75d12a195ff5aac9409428d9f54d
Luis Cruz <luis.cruz@ist.utl.pt>
2006-01-19 23:56:42 +0000

    Added persistentSupport instance.

--- src/net/sourceforge/fenixedu/applicationTier/Service.java
+++ src/net/sourceforge/fenixedu/applicationTier/Service.java
@@ -1,11 +1,14 @@
 package net.sourceforge.fenixedu.applicationTier;
 
 import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
+import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 
 
 public class Service implements pt.utl.ist.berserk.logic.serviceManager.IService {
 
-    protected static final IPersistentObject persistentObject = PersistenceSupportFactory.getPersistentObject();
+	protected static final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
+
+    protected static final IPersistentObject persistentObject = persistentSupport.getIPersistentObject();
 
 }
