f2873a98ba73f354ae85386b6a590625812a2aa2
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-19 17:10:11 +0000

    sp --> persistentSupport (preparing generic Service class variable introduction)

--- src/net/sourceforge/fenixedu/applicationTier/Servico/coordinator/ReadExecutionDegreesByDegree.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/coordinator/ReadExecutionDegreesByDegree.java
@@ -1,7 +1,6 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.coordinator;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
@@ -14,42 +13,24 @@ import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.serviceManager.IService;
 
-/**
- * @author Tânia Pousão Created on 24/Nov/2003
- */
-
 public class ReadExecutionDegreesByDegree implements IService {
 
-    /**
-     * Executes the service. Returns the current collection of
-     * infoExecutionDegrees.
-     * @throws ExcepcaoPersistencia 
-     */
     public List run(Integer idDegree) throws FenixServiceException, ExcepcaoPersistencia {
-        ISuportePersistente sp;
-        List allExecutionDegrees = null;
-        sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-        IPersistentExecutionDegree cursoExecucaoPersistente = sp.getIPersistentExecutionDegree();
+        final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
+        IPersistentExecutionDegree cursoExecucaoPersistente = persistentSupport.getIPersistentExecutionDegree();
 
-        allExecutionDegrees = cursoExecucaoPersistente.readExecutionsDegreesByDegree(idDegree,
-                CurricularStage.OLD);
-
+        List<ExecutionDegree> allExecutionDegrees = cursoExecucaoPersistente.readExecutionsDegreesByDegree(idDegree,CurricularStage.OLD);
         if (allExecutionDegrees == null || allExecutionDegrees.isEmpty()) {
             throw new FenixServiceException();
         }
 
-        // build the result of this service
-        Iterator iterator = allExecutionDegrees.iterator();
-        List allInfoExecutionDegrees = new ArrayList(allExecutionDegrees.size());
-
-        while (iterator.hasNext()) {
-            ExecutionDegree executionDegree = (ExecutionDegree) iterator.next();
-            InfoExecutionDegree infoExecutionDegree = InfoExecutionDegree
-                    .newInfoFromDomain(executionDegree);
-
+        List<InfoExecutionDegree> allInfoExecutionDegrees = new ArrayList<InfoExecutionDegree>(allExecutionDegrees.size());
+        for (ExecutionDegree executionDegree : allExecutionDegrees) {
+            InfoExecutionDegree infoExecutionDegree = InfoExecutionDegree.newInfoFromDomain(executionDegree);
             allInfoExecutionDegrees.add(infoExecutionDegree);
         }
 
         return allInfoExecutionDegrees;
     }
+
 }
