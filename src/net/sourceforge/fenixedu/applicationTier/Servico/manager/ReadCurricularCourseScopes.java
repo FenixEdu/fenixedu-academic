7eed30817159cb126d0eef34631d1263366ea655
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-20 09:44:02 +0000

    using Service.persistentObject variable

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/ReadCurricularCourseScopes.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/ReadCurricularCourseScopes.java
@@ -1,51 +1,36 @@
-/*
- * Created on 19/Ago/2003
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoCurricularCourseScope;
 import net.sourceforge.fenixedu.dataTransferObject.InfoCurricularCourseScopeWithBranchAndSemesterAndYear;
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.CurricularCourseScope;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
-import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-import net.sourceforge.fenixedu.applicationTier.Service;
 
-/**
- * @author lmac1
- */
 public class ReadCurricularCourseScopes extends Service {
 
-	/**
-	 * Executes the service. Returns the current collection of
-	 * infoCurricularCourseScopes.
-	 * 
-	 * @throws ExcepcaoPersistencia
-	 */
-	public List run(Integer curricularCourseId) throws FenixServiceException, ExcepcaoPersistencia {
-		ISuportePersistente sp;
-		List allCurricularCourseScopes = null;
+    public List<InfoCurricularCourseScope> run(Integer curricularCourseId) throws FenixServiceException,
+            ExcepcaoPersistencia {
+        CurricularCourse curricularCourse = (CurricularCourse) persistentObject.readByOID(
+                CurricularCourse.class, curricularCourseId);
 
-		sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-		CurricularCourse curricularCourse = (CurricularCourse) sp.getIPersistentCurricularCourse()
-				.readByOID(CurricularCourse.class, curricularCourseId);
-		allCurricularCourseScopes = curricularCourse.getScopes();
+        List<CurricularCourseScope> curricularCourseScopes = curricularCourse.getScopes();
+        if (curricularCourseScopes == null || curricularCourseScopes.isEmpty()) {
+            return new ArrayList<InfoCurricularCourseScope>();
+        }
 
-		if (allCurricularCourseScopes == null || allCurricularCourseScopes.isEmpty())
-			return new ArrayList();
+        List<InfoCurricularCourseScope> result = new ArrayList<InfoCurricularCourseScope>(
+                curricularCourseScopes.size());
+        for (CurricularCourseScope curricularCourseScope : curricularCourseScopes) {
+            result.add(InfoCurricularCourseScopeWithBranchAndSemesterAndYear
+                    .newInfoFromDomain(curricularCourseScope));
+        }
 
-		Iterator iterator = allCurricularCourseScopes.iterator();
-		List result = new ArrayList(allCurricularCourseScopes.size());
+        return result;
+    }
 
-		while (iterator.hasNext())
-			result.add(InfoCurricularCourseScopeWithBranchAndSemesterAndYear
-					.newInfoFromDomain((CurricularCourseScope) iterator.next()));
-
-		return result;
-	}
 }
