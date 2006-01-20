700c3c2358e71b3d98da1ceca56b09c19c0ff665
Luís Egídio <luis.egidio@ist.utl.pt>
2006-01-20 15:20:44 +0000

    using Service.persistentObject variable

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/ReadExecutionCoursesByCurricularCourse.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/ReadExecutionCoursesByCurricularCourse.java
@@ -1,12 +1,9 @@
-/*
- * Created on 16/Ago/2003
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
@@ -14,55 +11,24 @@ import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourseWithExecut
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
-import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
-import net.sourceforge.fenixedu.applicationTier.Service;
 
-/**
- * @author lmac1
- */
 public class ReadExecutionCoursesByCurricularCourse extends Service {
 
 	public List run(Integer curricularCourseId) throws FenixServiceException, ExcepcaoPersistencia {
-		ISuportePersistente sp;
-		List allExecutionCourses = null;
-
-		sp = PersistenceSupportFactory.getDefaultPersistenceSupport();
-
-		CurricularCourse curricularCourse = (CurricularCourse) sp.getIPersistentCurricularCourse()
-				.readByOID(CurricularCourse.class, curricularCourseId);
-
+		CurricularCourse curricularCourse = (CurricularCourse) persistentObject.readByOID(CurricularCourse.class, curricularCourseId);
 		if (curricularCourse == null) {
-
 			throw new NonExistingServiceException("message.nonExistingCurricularCourse", null);
 		}
 
-		allExecutionCourses = curricularCourse.getAssociatedExecutionCourses();
+		List<ExecutionCourse> executionCourses = curricularCourse.getAssociatedExecutionCourses();
 
-		if (allExecutionCourses == null || allExecutionCourses.isEmpty()) {
-			// return allExecutionCourses;
-			return new ArrayList();
-		}
-
-		// build the result of this service
-		Iterator iterator = allExecutionCourses.iterator();
-		List result = new ArrayList(allExecutionCourses.size());
-
-		Boolean hasSite;
-		while (iterator.hasNext()) {
-
-			InfoExecutionCourse infoExecutionCourse = InfoExecutionCourseWithExecutionPeriod
-					.newInfoFromDomain((ExecutionCourse) iterator.next());
-
-			ExecutionCourse executionCourse = (ExecutionCourse) sp.getIPersistentExecutionCourse()
-					.readByOID(ExecutionCourse.class, infoExecutionCourse.getIdInternal());
-			if (executionCourse.getSite() != null)
-				hasSite = true;
-			else
-				hasSite = false;
-			infoExecutionCourse.setHasSite(hasSite);
+		List<InfoExecutionCourse> result = new ArrayList<InfoExecutionCourse>(executionCourses.size());
+		for (ExecutionCourse executionCourse : executionCourses) {
+			InfoExecutionCourse infoExecutionCourse = InfoExecutionCourseWithExecutionPeriod.newInfoFromDomain(executionCourse);
+			infoExecutionCourse.setHasSite(executionCourse.hasSite());
 			result.add(infoExecutionCourse);
 		}
 		return result;
 	}
+    
 }
