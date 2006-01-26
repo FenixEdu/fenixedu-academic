a7a4481d6628bf6c34ca285cda087eee90507d84
Catarina Simões <velouria@velouria.org>
2006-01-26 22:14:38 +0000

    Exceptions related with the Assiduousness domain.They will be deleted soon!

---
+++ src/net/sourceforge/fenixedu/domain/exceptions/assiduousness/UnknownEmployeeException.java
@@ -0,0 +1,7 @@
+package net.sourceforge.fenixedu.domain.exceptions.assiduousness;
+
+import net.sourceforge.fenixedu.domain.exceptions.FenixDomainException;
+
+public class UnknownEmployeeException extends FenixDomainException {
+
+}
