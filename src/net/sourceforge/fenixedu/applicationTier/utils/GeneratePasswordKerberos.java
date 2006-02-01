5bdff7145fa041e647297d4f88904c025c92a4b7
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-02-01 15:41:59 +0000

    added RandPass

--- src/net/sourceforge/fenixedu/applicationTier/utils/GeneratePasswordKerberos.java
+++ src/net/sourceforge/fenixedu/applicationTier/utils/GeneratePasswordKerberos.java
@@ -1,15 +1,46 @@
 package net.sourceforge.fenixedu.applicationTier.utils;
 
+import java.security.SecureRandom;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.util.kerberos.Script;
 
 import org.apache.commons.lang.RandomStringUtils;
 
+import com.Ostermiller.util.PasswordVerifier;
+import com.Ostermiller.util.RandPass;
+
 public class GeneratePasswordKerberos implements IGeneratePassword {
+	
+	private RandPass randPass;
+	
+	public GeneratePasswordKerberos() {
+		inicializeRandandomPassGenerator();
+	}
+
+	private void inicializeRandandomPassGenerator() {
+		try {
+			randPass = new RandPass(SecureRandom.getInstance("SHA1PRNG"));
+			randPass.setMaxRepetition(1);
+			randPass.addVerifier(getPasswordVerifier());
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private PasswordVerifier getPasswordVerifier() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		String stringClass = PropertiesManager.getProperty("passVerifier");
+		if(stringClass != null) {
+			Class clazz = Class.forName(stringClass);
+			return (PasswordVerifier) clazz.newInstance();
+		}
+		return null;
+	}
 
 	public String generatePassword(Person person){
 		try {
-			String password = RandomStringUtils.randomAlphanumeric(8);
+			String password = randPass.getPass(PropertiesManager.getIntegerProperty("passSize"));
 	    	if(person.getIstUsername() != null) {
 	    		if (person.getIsPassInKerberos()) {
 					Script.changeKerberosPass(person.getIstUsername(), password);
