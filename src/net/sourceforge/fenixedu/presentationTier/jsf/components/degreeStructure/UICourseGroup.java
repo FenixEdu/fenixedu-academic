5ee3909009143a6164b0edcf6161d68ec393601d
Luís Egídio <luis.egidio@ist.utl.pt>
2006-02-08 18:38:11 +0000

    - giving contextID to delete link - not encoding sums footer...

--- src/net/sourceforge/fenixedu/presentationTier/jsf/components/degreeStructure/UICourseGroup.java
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/components/degreeStructure/UICourseGroup.java
@@ -154,7 +154,7 @@ public class UICourseGroup extends UIDegreeModule {
             
             if (((CourseGroup)this.degreeModule).getContextsWithCurricularCourses().size() > 0) {
                 List<Double> sums = encodeChildCurricularCourses();
-                encodeSumsFooter(sums);
+                //encodeSumsFooter(sums);
             } else {
                 encodeEmptyCourseGroupInfo();   
             }
@@ -222,7 +222,7 @@ public class UICourseGroup extends UIDegreeModule {
                 .get("degreeCurricularPlanID") + "&courseGroupID=" + this.degreeModule.getIdInternal(), "edit");
         writer.append(" , ");
         encodeLink("deleteCourseGroup.faces?degreeCurricularPlanID=" + this.facesContext.getExternalContext().getRequestParameterMap()
-                .get("degreeCurricularPlanID") + "&courseGroupID=" + this.degreeModule.getIdInternal(), "delete");
+                .get("degreeCurricularPlanID") + "&courseGroupID=" + this.degreeModule.getIdInternal() + "&contextID=" + this.previousContext, "delete");
         writer.append(") ");
         writer.endElement("td");
     }
