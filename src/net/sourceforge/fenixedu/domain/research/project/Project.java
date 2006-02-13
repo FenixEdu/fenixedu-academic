0a27671dee4143d9b318edb4c8f15f17a6289f42
Carlos Pereira <cgmp@ist.utl.pt>
2006-02-13 16:48:25 +0000

    changes to research.project

--- src/net/sourceforge/fenixedu/domain/research/project/Project.java
+++ src/net/sourceforge/fenixedu/domain/research/project/Project.java
@@ -1,9 +1,20 @@
 package net.sourceforge.fenixedu.domain.research.project;
 
+import org.joda.time.Period;
+
 public class Project extends Project_Base {
     
     public  Project() {
         super();
+    }
+    
+    public Period getPeriod () {
+        return new Period(getStartDate(), getEndDate());
+    }
+    
+    public int getDurationInMonths () {
+        Period period = getPeriod();
+        return period.getYears() * 12 + period.getMonths();
     }
     
 }
