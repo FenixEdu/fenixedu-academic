97cb6e1ee84d23154df0e83ea77b1d289e5192ac
Luis Cruz <luis.cruz@ist.utl.pt>
2006-02-13 13:02:54 +0000

    Always terminate the transaction.

--- src/net/sourceforge/fenixedu/presentationTier/servlets/filters/CloseTransactionFilter.java
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/CloseTransactionFilter.java
@@ -85,10 +85,12 @@ public class CloseTransactionFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		Transaction.begin();
-		Transaction.currentFenixTransaction().setReadOnly();
-		setTransactionOwner(request);
-		try {
+        try {
+            if (Transaction.currentFenixTransaction() == null) {
+                Transaction.begin();
+                Transaction.currentFenixTransaction().setReadOnly();
+           }
+            setTransactionOwner(request);
 			chain.doFilter(request, response);
 		} finally {
 			Transaction.forceFinish();
