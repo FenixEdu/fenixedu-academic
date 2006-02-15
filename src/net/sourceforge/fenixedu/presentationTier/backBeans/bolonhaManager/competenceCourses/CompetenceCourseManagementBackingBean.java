c4115a8a9fb32b795835e0a4475e991e6830ec88
Luís Egídio <luis.egidio@ist.utl.pt>
2006-02-15 10:03:50 +0000

    test if competence course members group is null

--- src/net/sourceforge/fenixedu/presentationTier/backBeans/bolonhaManager/competenceCourses/CompetenceCourseManagementBackingBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/backBeans/bolonhaManager/competenceCourses/CompetenceCourseManagementBackingBean.java
@@ -114,7 +114,7 @@ public class CompetenceCourseManagementBackingBean extends FenixBackingBean {
     public List<String> getGroupMembersLabels() throws FenixFilterException, FenixServiceException {
         List<String> result = null;
 
-        if(getSelectedDepartmentUnit() == null){
+        if (getSelectedDepartmentUnit() == null || getSelectedDepartmentUnit().getDepartment().getCompetenceCourseMembersGroup() == null) {
             return result;
         }
         
