34e0cd25f01330a2a6dd79f9dbdf93f53ed0af1b
Luis Cruz <luis.cruz@ist.utl.pt>
2006-02-21 18:30:36 +0000

    Continued implementation of weekly work load on teachers portal.

--- jsp/teacher/weeklyWorkLoad.jsp
+++ jsp/teacher/weeklyWorkLoad.jsp
@@ -3,4 +3,33 @@
 <%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
 <%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
 
-<h2><bean:write name="siteView" property="commonComponent.executionCourse.name"/></h2>
+<h2><bean:write name="siteView" property="commonComponent.executionCourse.nome"/></h2>
+<h3>
+	<bean:write name="executionCourse" property="executionPeriod.semester"/>
+	<bean:message key="label.semester"/>
+	<bean:write name="executionCourse" property="executionPeriod.executionYear.year"/>
+</h3>
+
+<br/>
+<br/>
+
+<logic:present name="weeklyWorkLoadView">
+	<table class="style1">
+		<tr>
+			<logic:iterate id="interval" type="org.joda.time.Interval" name="weeklyWorkLoadView" property="intervals">
+				<td class="listClasses-header">
+					<bean:define id="start" type="org.joda.time.DateTime" name="interval" property="start"/>
+					<bean:define id="end" type="org.joda.time.DateTime" name="interval" property="end"/>				
+					[<bean:write name="start" property="year"/>-<bean:write name="start" property="monthOfYear"/>-<bean:write name="start" property="dayOfMonth"/>,
+					 <bean:write name="end" property="year"/>-<bean:write name="end" property="monthOfYear"/>-<bean:write name="end" property="dayOfMonth"/>[
+				</td>
+			</logic:iterate>
+		</tr>
+
+		<logic:iterate id="weeklyWorkLoadArray" name="weeklyWorkLoadView" property="weeklyWorkLoadArrays">
+			<logic:iterate id="weeklyWorkLoad" name="weeklyWorkLoadArray">
+			</logic:iterate>		
+		</logic:iterate>
+
+	</table>
+</logic:present>
--- src/net/sourceforge/fenixedu/presentationTier/Action/teacher/WeeklyWorkLoadDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/teacher/WeeklyWorkLoadDA.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.presentationTier.Action.teacher;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -10,24 +12,71 @@ import javax.servlet.http.HttpServletResponse;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.ExecutionCourseSiteView;
-import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
+import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourseWithExecutionPeriod;
 import net.sourceforge.fenixedu.dataTransferObject.InfoSiteCommon;
 import net.sourceforge.fenixedu.domain.Attends;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
+import net.sourceforge.fenixedu.domain.Student;
+import net.sourceforge.fenixedu.domain.student.WeeklyWorkLoad;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.Interval;
 
 public class WeeklyWorkLoadDA extends FenixDispatchAction {
 
-	private static final Comparator<Attends> ATTENDS_COMPARATOR = new Comparator<Attends>(){
+	public class WeeklyWorkLoadView {
+        final Interval executionPeriodInterval;
+        final int numberOfWeeks;
+
+        final Collection<WeeklyWorkLoad[]> weeklyWorkLoadArrays = new ArrayList<WeeklyWorkLoad[]>();
+        final Interval[] intervals;
+
+        public WeeklyWorkLoadView(final Interval executionPeriodInterval) {
+            this.executionPeriodInterval = executionPeriodInterval;
+            numberOfWeeks = executionPeriodInterval.toPeriod().getWeeks();
+            intervals = new Interval[numberOfWeeks];
+            for (int i = 0; i < numberOfWeeks; i++) {
+                final DateTime start = executionPeriodInterval.getStart().plusWeeks(i);
+                final DateTime end = start.plusWeeks(1);
+                intervals[i] = new Interval(start, end);
+            }
+        }
+
+        public void add(final Attends attends) {
+            final WeeklyWorkLoad[] weeklyWorkLoadArray = new WeeklyWorkLoad[numberOfWeeks];
+            for (final WeeklyWorkLoad weeklyWorkLoad : attends.getWeeklyWorkLoads()) {
+                weeklyWorkLoadArray[weeklyWorkLoad.getWeekOffset()] = weeklyWorkLoad;
+            }
+            weeklyWorkLoadArrays.add(weeklyWorkLoadArray);
+        }
+
+        public Collection<WeeklyWorkLoad[]> getWeeklyWorkLoadArrays() {
+            return weeklyWorkLoadArrays;
+        }
+
+        public Interval[] getIntervals() {
+            return intervals;
+        }
+
+        public Interval getExecutionPeriodInterval() {
+            return executionPeriodInterval;
+        }
+
+    }
+
+    private static final Comparator<Attends> ATTENDS_COMPARATOR = new Comparator<Attends>() {
 		public int compare(final Attends attends1, final Attends attends2) {
-			final ExecutionCourse executionCourse1 = attends1.getDisciplinaExecucao();
-			final ExecutionCourse executionCourse2 = attends2.getDisciplinaExecucao();
-			return executionCourse1.getNome().compareTo(executionCourse2.getNome());
-		}};
+            final Student student1 = attends1.getAluno();
+            final Student student2 = attends2.getAluno();
+            return student1.getNumber().compareTo(student2.getNumber());
+		}
+    };
 
     public ActionForward prepare(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
             throws FenixFilterException, FenixServiceException {
@@ -38,9 +87,10 @@ public class WeeklyWorkLoadDA extends FenixDispatchAction {
 
         final Object[] args = { ExecutionCourse.class, executionCourseID };
         final ExecutionCourse executionCourse = (ExecutionCourse) executeService(request, "ReadDomainObject", args);
+        request.setAttribute("executionCourse", executionCourse);
 
         final InfoSiteCommon infoSiteCommon = new InfoSiteCommon();
-        infoSiteCommon.setExecutionCourse(InfoExecutionCourse.newInfoFromDomain(executionCourse));
+        infoSiteCommon.setExecutionCourse(InfoExecutionCourseWithExecutionPeriod.newInfoFromDomain(executionCourse));
         final ExecutionCourseSiteView executionCourseSiteView = new ExecutionCourseSiteView(infoSiteCommon, null);
         request.setAttribute("siteView", executionCourseSiteView);
 
@@ -48,7 +98,26 @@ public class WeeklyWorkLoadDA extends FenixDispatchAction {
         attends.addAll(executionCourse.getAttends());
         request.setAttribute("attends", attends);
 
+        final Interval executionPeriodInterval = getExecutionPeriodInterval(executionCourse);
+        final WeeklyWorkLoadView weeklyWorkLoadView = new WeeklyWorkLoadView(executionPeriodInterval);
+        for (final Attends attend : attends) {
+            weeklyWorkLoadView.add(attend);
+        }
+        request.setAttribute("weeklyWorkLoadView", weeklyWorkLoadView);
+
         return mapping.findForward("showWeeklyWorkLoad");
+    }
+
+    private Interval getExecutionPeriodInterval(final ExecutionCourse executionCourse) {
+        final ExecutionPeriod executionPeriod = executionCourse.getExecutionPeriod();
+        final DateTime beginningOfSemester = new DateTime(executionPeriod.getBeginDate());
+        final DateTime firstMonday = beginningOfSemester.withField(DateTimeFieldType.dayOfWeek(), 1);
+        final DateTime endOfSemester = new DateTime(executionPeriod.getEndDate());
+        final DateTime nextLastMonday = endOfSemester.withField(DateTimeFieldType.dayOfWeek(), 1).plusWeeks(1);
+        System.out.println("   -----> " + new Interval(firstMonday, nextLastMonday));
+        System.out.println("   -----> " + new Interval(firstMonday, nextLastMonday).toPeriod());
+        System.out.println("   -----> " + new Interval(firstMonday, nextLastMonday).toPeriod().getWeeks());
+        return new Interval(firstMonday, nextLastMonday);
     }
 
 }
