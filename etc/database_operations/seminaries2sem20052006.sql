f954b3f45fdc5d16e73331bca2c921f19dfa5544
Gonçalo Luíz <goncalo@ist.utl.pt>
2006-02-22 16:59:38 +0000

    updated to new seminaries about Inova?\231?\227o (2005/2006)

---
+++ etc/database_operations/seminaries2sem20052006.sql
@@ -0,0 +1,30 @@
+-- Seminary
+insert into SEMINARY (id_internal, name,description,allowed_candidacies_per_student,ACK_OPT_LOCK,enrollment_begin_time,enrollment_begin_date,enrollment_end_time,enrollment_end_date,has_theme,has_case_study) values (6,"Inovação 2005/2006","Ano Lectivo de 2005 / 2006",1,1,'00:00:00','2006-2-22','23:59:59','2006-3-6',0,0);
+
+-- Equivalencies
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2200,6,2819,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2201,6,10994,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2202,6,3806,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2203,6,3841,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2204,6,3338,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2205,6,4924,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2206,6,4920,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2207,6,13961,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2208,6,3633,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2209,6,11677,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2210,6,11682,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2211,6,3634,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2212,6,3660,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2213,6,13607,3,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2214,6,11254,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2215,6,4921,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2216,6,13581,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2217,6,12161,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2218,6,12158,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2219,6,14563,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2220,6,4918,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2221,6,13142,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2222,6,14184,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2223,6,14011,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2224,6,13299,1,1);
+insert into SEMINARY_CURRICULARCOURSE (id_internal,seminary_id_internal,curricular_course_id_internal,modality_id_internal,ACK_OPT_LOCK) values (2225,6,15510,1,1);
--- jsp/teacher/showSeminariesIndex_bd.jsp
+++ jsp/teacher/showSeminariesIndex_bd.jsp
@@ -2,5 +2,5 @@
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
 <%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
 
-<html:link page="/showCandidacies.do?seminaryID=5"><bean:message key="label.seminaries.viewSubmissions"/></html:link><br/>
-<html:link page="/selectCandidacies.do?method=prepare&amp;seminaryID=5"><bean:message key="label.seminaries.selectSubmissions"/></html:link>
+<html:link page="/showCandidacies.do?seminaryID=6"><bean:message key="label.seminaries.viewSubmissions"/></html:link><br/>
+<html:link page="/selectCandidacies.do?method=prepare&amp;seminaryID=6"><bean:message key="label.seminaries.selectSubmissions"/></html:link>
