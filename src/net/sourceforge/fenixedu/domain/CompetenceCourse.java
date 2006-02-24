b021b52d0f96ef9671bf95f509904204fc3c8ff2
João Florindo <jdnf@linkare.pt>
2006-02-24 12:37:23 +0000

    removed unsed imports

--- src/net/sourceforge/fenixedu/domain/CompetenceCourse.java
+++ src/net/sourceforge/fenixedu/domain/CompetenceCourse.java
@@ -4,14 +4,10 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 import net.sourceforge.fenixedu.accessControl.Checked;
 import net.sourceforge.fenixedu.domain.curricularPeriod.CurricularPeriodType;
