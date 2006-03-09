8a04b1ef05b22fda15843e5a32c467a3c54daa09
Luis Cruz <luis.cruz@ist.utl.pt>
2006-03-09 17:07:09 +0000

    Use properties manager from tools project.

--- src/net/sourceforge/fenixedu/_development/PropertiesManager.java
+++ src/net/sourceforge/fenixedu/_development/PropertiesManager.java
@@ -5,35 +5,21 @@
 package net.sourceforge.fenixedu._development;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 /**
  * @author Luis Cruz
  *
  */
-public class PropertiesManager {
+public class PropertiesManager extends pt.utl.ist.fenix.tools.util.PropertiesManager {
 
     private static final Properties properties = new Properties();
-
-    private static final PropertiesManager instance = new PropertiesManager();
 
     static {
         try {
             loadProperties(properties, "/configuration.properties");
         } catch (IOException e) {
             throw new RuntimeException("Unable to load properties file.", e);
-        }
-    }
-
-    private PropertiesManager() {
-        super();
-    }
-
-    public static void loadProperties(final Properties properties, final String fileName) throws IOException {
-        final InputStream inputStream = instance.getClass().getResourceAsStream(fileName);
-        if (inputStream != null) {
-            properties.load(inputStream);
         }
     }
 
