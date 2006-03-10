8630d3502f078421c7e9fc7433b5e3d112d8ae99
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-10 08:51:36 +0000

    actually removed PersonRole entity

--- src/net/sourceforge/fenixedu/domain/PersonRole.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.fenixedu.domain;
-
-/**
- * 
- * @author Nuno Nunes (nmsn@rnl.ist.utl.pt) Joana Mota (jccm@rnl.ist.utl.pt)
- */
-public class PersonRole extends PersonRole_Base {
-
-}
