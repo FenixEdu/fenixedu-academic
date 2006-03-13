14a75b49549d3d0cc103f245e43a25d2a838facb
Catarina Simões <velouria@velouria.org>
2006-03-13 13:13:59 +0000

    *** empty log message ***

---
+++ src/net/sourceforge/fenixedu/domain/assiduousness/util/JustificationMotiveType.java
@@ -0,0 +1,8 @@
+package net.sourceforge.fenixedu.domain.assiduousness.util;
+
+// TODO find decent names for this...
+public enum JustificationMotiveType {
+    TIME, // justificacao horas
+    OCCURRENCE, // justificacao ocorrencia
+    BALANCE // justificacao saldo
+}
