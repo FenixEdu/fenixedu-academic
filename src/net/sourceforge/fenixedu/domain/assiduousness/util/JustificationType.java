e24a1981595a2cd2fbdb2f8fdd531e302e9f62bf
Catarina Simões <velouria@velouria.org>
2006-03-13 13:15:05 +0000

    Renamed to JustificationMotiveType.

--- src/net/sourceforge/fenixedu/domain/assiduousness/util/JustificationType.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.fenixedu.domain.assiduousness.util;
-
-// TODO find decent names for this...
-public enum JustificationType {
-    TIME, // justificacao horas
-    OCCURRENCE, // justificacao ocorrencia
-    BALANCE // justificacao saldo
-}
