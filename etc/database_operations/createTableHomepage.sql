e01ec1a444297c92b8f4380695b928196e0b5a59
Luis Cruz <luis.cruz@ist.utl.pt>
2006-03-14 16:28:19 +0000

    Deployed HEAD.

--- etc/database_operations/createTableHomepage.sql
+++
@@ -1,9 +0,0 @@
-create table HOMEPAGE (
-	ID_INTERNAL int(11) NOT NULL auto_increment,
-	KEY_PERSON int(11) NOT NULL,
-	MY_URL varchar(150) NOT NULL,
-	PRIMARY KEY  (ID_INTERNAL),
-	UNIQUE KEY (MY_URL)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-alter table PARTY add column KEY_HOMEPAGE int(11) default null;
