722e6a932bf885de196b0e5d7e5dcebd7e797918
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-16 09:20:47 +0000

    removed readAll

--- src/net/sourceforge/fenixedu/persistenceTier/IPersistentContributor.java
+++ src/net/sourceforge/fenixedu/persistenceTier/IPersistentContributor.java
@@ -1,33 +1,9 @@
-/*
- * Created on 21/Mar/2003
- *
- * To change this generated comment go to 
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
 package net.sourceforge.fenixedu.persistenceTier;
 
-import java.util.List;
-
 import net.sourceforge.fenixedu.domain.Contributor;
-import net.sourceforge.fenixedu.persistenceTier.exceptions.ExistingPersistentException;
 
-/**
- * @author Nuno Nunes (nmsn@rnl.ist.utl.pt) Joana Mota (jccm@rnl.ist.utl.pt)
- */
 public interface IPersistentContributor extends IPersistentObject {
-    /**
-     * 
-     * @param contributorNumber
-     * @return IContributor
-     * @throws ExcepcaoPersistencia
-     */
+
     public Contributor readByContributorNumber(Integer contributorNumber) throws ExcepcaoPersistencia;
 
-    /**
-     * 
-     * @return List
-     * @throws ExcepcaoPersistencia
-     * @throws ExistingPersistentException
-     */
-    public List readAll() throws ExcepcaoPersistencia, ExistingPersistentException;
 }
--- src/net/sourceforge/fenixedu/persistenceTier/OJB/ContributorOJB.java
+++ src/net/sourceforge/fenixedu/persistenceTier/OJB/ContributorOJB.java
@@ -1,11 +1,4 @@
-/*
- * Created on 21/Mar/2003
- * 
- *  
- */
 package net.sourceforge.fenixedu.persistenceTier.OJB;
-
-import java.util.List;
 
 import net.sourceforge.fenixedu.domain.Contributor;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -13,20 +6,12 @@ import net.sourceforge.fenixedu.persistenceTier.IPersistentContributor;
 
 import org.apache.ojb.broker.query.Criteria;
 
-/**
- * @author Nuno Nunes (nmsn@rnl.ist.utl.pt) Joana Mota (jccm@rnl.ist.utl.pt)
- */
-public class ContributorOJB extends PersistentObjectOJB implements
-		IPersistentContributor {
+public class ContributorOJB extends PersistentObjectOJB implements IPersistentContributor {
 
-	public Contributor readByContributorNumber(Integer contributorNumber)
-			throws ExcepcaoPersistencia {
+	public Contributor readByContributorNumber(Integer contributorNumber) throws ExcepcaoPersistencia {
 		Criteria crit = new Criteria();
 		crit.addEqualTo("contributorNumber", contributorNumber);
 		return (Contributor) queryObject(Contributor.class, crit);
 	}
 
-	public List readAll() throws ExcepcaoPersistencia {
-		return queryList(Contributor.class, null);
-	}
 }
