c765312cb5928bb7c66fd5e9ad8c6a080ecdbacf
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-16 08:49:47 +0000

    using readAll form persistentObject

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/contributor/ReadAllContributors.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/contributor/ReadAllContributors.java
@@ -1,11 +1,6 @@
-/*
- * Created on 14/Mar/2003
- *
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.contributor;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
@@ -14,20 +9,17 @@ import net.sourceforge.fenixedu.dataTransferObject.InfoContributor;
 import net.sourceforge.fenixedu.domain.Contributor;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
-/**
- * @author Nuno Nunes (nmsn@rnl.ist.utl.pt) Joana Mota (jccm@rnl.ist.utl.pt)
- */
 public class ReadAllContributors extends Service {
 
-	public List run() throws FenixServiceException, ExcepcaoPersistencia {
-		List result = persistentSupport.getIPersistentContributor().readAll();
+	public List<InfoContributor> run() throws FenixServiceException, ExcepcaoPersistencia {
+        List<InfoContributor> result = new ArrayList<InfoContributor>();
+        
+        List<Contributor> contributors = (List<Contributor>) persistentObject.readAll(Contributor.class);
+        for (Contributor contributor : contributors) {
+            result.add(InfoContributor.newInfoFromDomain(contributor));
+        }
 
-		List contributors = new ArrayList();
-		Iterator iterator = result.iterator();
-		while (iterator.hasNext())
-			contributors.add(InfoContributor.newInfoFromDomain((Contributor) iterator.next()));
-
-		return contributors;
-
+		return result;
 	}
+
 }
