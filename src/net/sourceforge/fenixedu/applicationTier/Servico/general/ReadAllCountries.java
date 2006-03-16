51cc6df291feb6ff9617d69c6ae3230051fb0fe4
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-16 13:04:11 +0000

    using readAll from persistentObject

--- src/net/sourceforge/fenixedu/applicationTier/Servico/general/ReadAllCountries.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/general/ReadAllCountries.java
@@ -1,20 +1,6 @@
-/*
- * ReadAllCountries.java
- * 
- * O Servico ReadAllCountries devolve a lista de paises existentes
- * 
- * Created on 16 de Dezembro de 2002, 12:54
- */
-
-/**
- * 
- * Autores : - Nuno Nunes (nmsn@rnl.ist.utl.pt) - Joana Mota
- * (jccm@rnl.ist.utl.pt)
- *  
- */
-
 package net.sourceforge.fenixedu.applicationTier.Servico.general;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
@@ -24,25 +10,21 @@ import net.sourceforge.fenixedu.dataTransferObject.InfoCountry;
 import net.sourceforge.fenixedu.domain.Country;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-
 public class ReadAllCountries extends Service {
 
     public Object run() throws ExcepcaoInexistente, FenixServiceException, ExcepcaoPersistencia {
-        List countries = persistentSupport.getIPersistentCountry().readAllCountrys();
-
-        if (countries.size() == 0) {
+        List<InfoCountry> result = new ArrayList<InfoCountry>();
+        
+        List<Country> countries = (List<Country>) persistentObject.readAll(Country.class);
+        if (countries.isEmpty()) {
             throw new ExcepcaoInexistente("Non existing Countries !!");
         }
 
-        return CollectionUtils.collect(countries, new Transformer() {
-
-            public Object transform(Object input) {
-                Country country = (Country) input;
-                return InfoCountry.newInfoFromDomain(country);
-            }
-        });
-
+        for (Country country : countries) {
+            result.add(InfoCountry.newInfoFromDomain(country));
+        }
+        
+        return result;
     }
+    
 }
