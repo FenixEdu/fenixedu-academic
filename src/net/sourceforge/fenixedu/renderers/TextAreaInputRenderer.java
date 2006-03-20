7ca688cd48ec25269590d042d5c0cc845672f0bb
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-03-20 15:18:17 +0000

    Added an input renderer for a string that allows the text to span over multiple lines

---
+++ src/net/sourceforge/fenixedu/renderers/TextAreaInputRenderer.java
@@ -0,0 +1,76 @@
+package net.sourceforge.fenixedu.renderers;
+
+import net.sourceforge.fenixedu.renderers.components.HtmlComponent;
+import net.sourceforge.fenixedu.renderers.components.HtmlTextArea;
+import net.sourceforge.fenixedu.renderers.layouts.Layout;
+import net.sourceforge.fenixedu.renderers.model.MetaSlotKey;
+
+/**
+ * This renderer provides an input mechanism for a string that allows long
+ * texts to be inserted or span over multiple lines.
+ * 
+ * <p>
+ * Example:
+ * <textarea>&lt;the text&gt;</textarea>
+ * 
+ * @author cfgi
+ */
+public class TextAreaInputRenderer extends InputRenderer {
+
+    private int columns;
+    private int rows;
+    
+    public int getColumns() {
+        return this.columns;
+    }
+
+    /**
+     * The number of columns of the generated text area.
+     * 
+     * @property
+     */
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public int getRows() {
+        return this.rows;
+    }
+
+    /**
+     * The number of rows of the generated text area.
+     * 
+     * @property
+     */
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+        return new Layout() {
+
+            @Override
+            public HtmlComponent createComponent(Object object, Class type) {
+                HtmlTextArea textArea = new HtmlTextArea();
+                textArea.setTargetSlot((MetaSlotKey) getInputContext().getMetaObject().getKey());
+                
+                textArea.setValue((String) object);
+                
+                
+                return textArea;
+            }
+
+            @Override
+            public void applyStyle(HtmlComponent component) {
+                super.applyStyle(component);
+                
+                HtmlTextArea textArea = (HtmlTextArea) component; 
+                
+                textArea.setColumns(getColumns());
+                textArea.setRows(getRows());
+            }
+        };
+    }
+
+}
