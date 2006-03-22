8919525dcec14b7e2f4c63a6897eea16e9cd7bb8
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-22 09:41:52 +0000

    organized imports

--- src/net/sourceforge/fenixedu/domain/degreeStructure/DegreeModule.java
+++ src/net/sourceforge/fenixedu/domain/degreeStructure/DegreeModule.java
@@ -1,7 +1,6 @@
 package net.sourceforge.fenixedu.domain.degreeStructure;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
