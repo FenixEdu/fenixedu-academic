23048dd5a487a239003ba4e73709f11260079a63
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-03-22 14:28:04 +0000

    - added a host based access control mechanism that can be used by the actions - updated the CheckPasswordKerberosAction - added a filter to protect web paths based on hosts configured for those paths

--- src/net/sourceforge/fenixedu/presentationTier/Action/CheckPasswordKerberosAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/CheckPasswordKerberosAction.java
@@ -1,48 +1,35 @@
 package net.sourceforge.fenixedu.presentationTier.Action;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoAutenticacao;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidPasswordServiceException;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NotAuthorizedException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.PasswordExpiredServiceException;
 import net.sourceforge.fenixedu.framework.factory.ServiceManagerServiceFactory;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixAction;
-import net.sourceforge.fenixedu.presentationTier.Action.exceptions.NotAuthorizedActionException;
+import net.sourceforge.fenixedu.util.HostAccessControl;
 import net.sourceforge.fenixedu.util.kerberos.KerberosException;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 public class CheckPasswordKerberosAction extends FenixAction {
 
-    private static final Logger logger = Logger.getLogger(CheckPasswordKerberosAction.class);
-
-    private static final List<String> authorizedHosts;
-
     private static final String SUCCESS_MESSAGE = "SUCCESS";
 
     private static final String UNEXPECTED_ERROR_MESSAGE = "UNEXPECTED_ERROR";
 
-    static {
-        authorizedHosts = Arrays.asList(StringUtils.split(PropertiesManager
-                .getProperty("checkPasswordKerberos.authorizedHosts"), ","));
-    }
-
     public final ActionForward execute(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        checkAuthorizationForRequestHost(request);
-
+        // check hosts accessing this action
+        if (! HostAccessControl.isAllowed(this, request)) {
+            throw new NotAuthorizedException();
+        }
+        
         if (request.getMethod().equalsIgnoreCase("post")) {
 
             final String username = request.getParameter("username");
@@ -73,27 +60,6 @@ public class CheckPasswordKerberosAction extends FenixAction {
         }
 
         return null;
-
-    }
-
-    private void checkAuthorizationForRequestHost(HttpServletRequest request)
-            throws NotAuthorizedActionException {
-
-        String remoteHostName = null;
-
-        try {
-            remoteHostName = InetAddress.getByName(request.getRemoteAddr()).getHostName();
-        } catch (UnknownHostException e) {
-            logger.warn(String.format("Problems solving IP %s to Hostname ", request.getRemoteAddr()));
-            throw new NotAuthorizedActionException("error.NotAuthorized");
-        }
-
-        if (!authorizedHosts.contains(request.getRemoteAddr())
-                && !authorizedHosts.contains(remoteHostName)) {
-            logger.warn("Host " + remoteHostName + "(" + request.getRemoteAddr() + ")");
-
-            throw new NotAuthorizedActionException("error.NotAuthorized");
-        }
 
     }
 }
---
+++ src/net/sourceforge/fenixedu/util/HostAccessControl.java
@@ -0,0 +1,127 @@
+package net.sourceforge.fenixedu.util;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.ServletRequest;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
+import net.sourceforge.fenixedu.presentationTier.servlets.filters.PathAccessControlFilter;
+
+import org.apache.log4j.Logger;
+
+public class HostAccessControl {
+    private static final Logger logger = Logger.getLogger(PathAccessControlFilter.class);
+    
+    private static final String HOST_CONTROL_PROPERTIES = "/.hostAccessControl.properties";
+    private static final String HOST_CONTROL_NAME_PREFIX = "host.control.name.";
+    
+    private static HostAccessControl intance = new HostAccessControl();
+    
+    public static HostAccessControl getInstance() {
+        return HostAccessControl.intance;
+    }
+
+    private Map<String, List<InetAddress>> configuration;
+
+    public HostAccessControl() {
+        super();
+        
+        setupConfiguration();
+    }
+    
+    private void setupConfiguration() {
+        this.configuration = new HashMap<String, List<InetAddress>>();
+        
+        try {
+            Properties properties = new Properties();
+            PropertiesManager.loadProperties(properties, HOST_CONTROL_PROPERTIES );
+            
+            for (Object key : properties.keySet()) {
+                String keyName = (String) key;
+                
+                if (keyName.startsWith(HOST_CONTROL_NAME_PREFIX)) {
+                    String name = keyName.substring(HOST_CONTROL_NAME_PREFIX.length());
+                    String[] hostList = properties.get(key).toString().split(",");
+                    
+                    for (int i = 0; i < hostList.length; i++) {
+                        String host = hostList[i].trim();
+                    
+                        if (host.length() > 0) {
+                            addEntry(name, host);
+                        }
+                    }
+                    
+                    // just for a cleaner message
+                    logger.debug("access for '" + name + "' limited to " + this.configuration.get(name));
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("failed to read the host access control properties '" + HOST_CONTROL_PROPERTIES + "'", e);
+        }
+    }
+    
+    private void addEntry(String name, String host) {
+        List<InetAddress> hostList = this.configuration.get(name);
+        
+        if (hostList == null) {
+            hostList = new ArrayList<InetAddress>();
+            
+            this.configuration.put(name, hostList);
+        }
+
+        try {
+            InetAddress[] addresses = InetAddress.getAllByName(host);
+
+            for (int i = 0; i < addresses.length; i++) {
+                hostList.add(addresses[i]);
+            }
+        } catch (UnknownHostException e) {
+            logger.warn("could not find host '" + host + "', host ignored.");
+        }
+    }
+    
+    public static boolean isAllowed(String name, ServletRequest request) {
+        return getInstance().isAllowed(name, request.getRemoteAddr());
+    }
+    
+    public static boolean isAllowed(Class type, ServletRequest request) {
+        return isAllowed(type.getName(), request);
+    }
+
+    public static boolean isAllowed(Object object, ServletRequest request) {
+        return isAllowed(object.getClass(), request);
+    }
+
+    private boolean isAllowed(String name, String address) {
+        try {
+            InetAddress remoteAddress = InetAddress.getByName(address);
+
+            List<InetAddress> hostList = this.configuration.get(name);
+            if (hostList == null) {
+                logger.warn(name + " denied[" + remoteAddress + "]: allowed hosts not defined");
+                return false;
+            }
+
+            for (InetAddress allowedHost : hostList) {
+                if (remoteAddress.equals(allowedHost)) {
+                    logger.info(name + " allowed[" + remoteAddress + "]: matches group " + hostList);
+                    
+                    return true;
+                }
+            }
+            
+            logger.info(name + " denied["+ remoteAddress + "]: is not member of " + hostList);
+        } catch (UnknownHostException e) {
+            logger.warn(name + " denied["+ address + "]: could not find host");
+        }
+        
+        return false;
+    }
+}
