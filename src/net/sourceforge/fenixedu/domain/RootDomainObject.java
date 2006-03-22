b17b9733cf07d8a38857a6c5bf6f9e2e38f943a5
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-22 15:52:35 +0000

    bug fix: Tomcat requires loading of persistent sessions before calling StartupServlet. Therefore, RootDomainObject must call init if no instance is found, or else the Person in the UserView will not be retrieved

--- src/net/sourceforge/fenixedu/domain/RootDomainObject.java
+++ src/net/sourceforge/fenixedu/domain/RootDomainObject.java
@@ -3,7 +3,6 @@ package net.sourceforge.fenixedu.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Party;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -16,24 +15,24 @@ public class RootDomainObject extends RootDomainObject_Base {
     private static RootDomainObject instance = null;
 
     public static synchronized void init() {
-        final ISuportePersistente suportePersistente = PersistenceSupportFactory.getDefaultPersistenceSupport();
-        try {
-            suportePersistente.iniciarTransaccao();
-            final IPersistentObject persistentObject = suportePersistente.getIPersistentObject();
-            instance = persistentObject.readRootDomainObject();
-            suportePersistente.confirmarTransaccao();
-        } catch (ExcepcaoPersistencia e) {
-            throw new Error("error.root.domain.object.not.retrieved", e);
+        if (instance == null) {
+            final ISuportePersistente suportePersistente = PersistenceSupportFactory.getDefaultPersistenceSupport();
+            try {
+                suportePersistente.iniciarTransaccao();
+                final IPersistentObject persistentObject = suportePersistente.getIPersistentObject();
+                instance = persistentObject.readRootDomainObject();
+                suportePersistente.confirmarTransaccao();
+            } catch (ExcepcaoPersistencia e) {
+                throw new Error("error.root.domain.object.not.retrieved", e);
+            }
         }
     }
     
     public static RootDomainObject getInstance() {
-        if (instance != null) {
-            return instance;
-        } else {
-            throw new DomainException(RootDomainObject.class.getSimpleName() + " not initialized. Run "
-                    + RootDomainObject.class.getName() + ".init() first.");
-        }
+        if (instance == null) {
+            init();
+        } 
+        return instance;
     }
 
     private RootDomainObject() {
