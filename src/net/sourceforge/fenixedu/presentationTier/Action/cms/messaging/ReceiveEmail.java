3006cf98d1f3c5c1e9a3b9102207f9f093aaec74
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-03-22 14:24:42 +0000

    make an additional check to prevent auto-reply messages like bounces to be processed as regular mail

--- src/net/sourceforge/fenixedu/presentationTier/Action/cms/messaging/ReceiveEmail.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/cms/messaging/ReceiveEmail.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
 
+import javax.mail.Address;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletOutputStream;
@@ -56,14 +58,17 @@ public class ReceiveEmail extends FenixAction
 			{
 				InputStream source = formFile.getInputStream();
 				message = new MimeMessage(Session.getDefaultInstance(System.getProperties()), source);
-				String messageName = resources.getMessage(this.getLocale(request), "cms.messaging.mailMessage.received.name");
-				String messageDescription = resources.getMessage(this.getLocale(request), "cms.messaging.mailMessage.received.description", new Date(System.currentTimeMillis()));
-				Object args[] =
-				{ message, messageName, messageDescription,username,password };
-				ServiceUtils.executeService(null, "ProcessReceivedMail", args);
-				result = true;
+                
+                if (! isBounceMessage(message)) {
+                    String messageName = resources.getMessage(this.getLocale(request), "cms.messaging.mailMessage.received.name");
+                    String messageDescription = resources.getMessage(this.getLocale(request), "cms.messaging.mailMessage.received.description", new Date(System.currentTimeMillis()));
+                    
+                    Object args[] = { message, messageName, messageDescription,username,password };
+                    ServiceUtils.executeService(null, "ProcessReceivedMail", args);
+                }
+
+                result = true;
 			}
-			
 		}
 		catch (MailerNotAuthenticated e)
 		{
@@ -89,7 +94,7 @@ public class ReceiveEmail extends FenixAction
 
 		try
 		{
-			this.sendAnswer(response, result);
+			sendAnswer(response, result);
 		}
 		catch (IOException e)
 		{
@@ -99,7 +104,58 @@ public class ReceiveEmail extends FenixAction
 		return null;
 	}
 
-	private void sendExplanationToSenders(MimeMessage message, FenixFilterException e) throws Exception
+    /**
+     * Checks if the messages is considered an auto reply and should be
+     * discarded to prevent loops. If the message has the null address
+     * as the return path then the message is considered an auto reply.
+     * If the return path exists then other heuristics are used.
+     * 
+     * Sources:
+     *  <ul>
+     *      <li>http://en.wikipedia.org/wiki/Bounce_message (2006-03-20)</li>
+     *      <li>http://www.ietf.org/rfc/rfc3834.txt (2006-03-20)</li>
+     *  </ul>
+     *  
+     * @return true if the message is considered an auto reply
+     */
+	private boolean isBounceMessage(MimeMessage message) throws MessagingException {
+        String[] returnPaths = message.getHeader("Return-Path"); // RFC 882
+        
+        // mail with no return path is surely fake ou an auto reply
+        if (returnPaths == null) {
+            return true;
+        }
+        
+        // check for the null address in the return path
+        for (int i = 0; i < returnPaths.length; i++) {
+            String path = returnPaths[i];
+            
+            if (path.trim().equals("<>")) {
+                return true;
+            }
+        }
+        
+        // the from address may be used to indicate an auto reply
+        Address[] fromAddresses = message.getFrom();
+        boolean allNullSender = true;
+        
+        for (int i = 0; i < fromAddresses.length; i++) {
+            Address address = fromAddresses[i];
+            
+            if (! address.toString().trim().equals("<>")) {
+                allNullSender = false;
+                break;
+            }
+        }
+        
+        if (allNullSender) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    private void sendExplanationToSenders(MimeMessage message, FenixFilterException e) throws Exception
 	{
 		// TODO Auto-generated method stub
 
