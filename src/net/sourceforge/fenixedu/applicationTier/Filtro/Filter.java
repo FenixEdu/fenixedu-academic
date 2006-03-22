6a60369e85377deb6ac14d4375aaca0b3d4239d4
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-22 08:03:44 +0000

    mark persistentSupport and persistentObject as deprecated

--- src/net/sourceforge/fenixedu/applicationTier/Filtro/Filter.java
+++ src/net/sourceforge/fenixedu/applicationTier/Filtro/Filter.java
@@ -1,5 +1,6 @@
 package net.sourceforge.fenixedu.applicationTier.Filtro;
 
+import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
 import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
@@ -7,8 +8,12 @@ import pt.utl.ist.berserk.logic.filterManager.IFilter;
 
 abstract public class Filter implements IFilter {
 
+    protected static final RootDomainObject rootDomainObject = RootDomainObject.getInstance();
+    
+    @Deprecated
 	protected static final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
 
+    @Deprecated
     protected static final IPersistentObject persistentObject = persistentSupport.getIPersistentObject();
-
+    
 }
