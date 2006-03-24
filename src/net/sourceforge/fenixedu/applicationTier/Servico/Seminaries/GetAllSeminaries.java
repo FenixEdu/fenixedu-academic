eb6dbb9becbaa77df6d9b762b4327cfaf6bead64
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-24 13:50:49 +0000

    dereferencing instead of using DAOs, as much as possible

--- src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/GetAllSeminaries.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/GetAllSeminaries.java
@@ -1,53 +1,39 @@
-/*
- * Created on 31/Jul/2003, 11:53:18
- *
- *By Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.Seminaries;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoSeminary;
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoSeminaryWithEquivalencies;
 import net.sourceforge.fenixedu.domain.Seminaries.Seminary;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminary;
 import net.sourceforge.fenixedu.presentationTier.Action.Seminaries.Exceptions.BDException;
 
-/**
- * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
- * 
- * 
- * Created at 31/Jul/2003, 11:53:18
- * 
- */
 public class GetAllSeminaries extends Service {
 
 	public List run(Boolean inEnrollmentPeriod) throws BDException, ExcepcaoPersistencia {
-		List seminariesInfo = new LinkedList();
+		List<InfoSeminary> result = new LinkedList<InfoSeminary>();
 
-		IPersistentSeminary persistentSeminary = persistentSupport.getIPersistentSeminary();
-		List seminaries = persistentSeminary.readAll();
-		for (Iterator iterator = seminaries.iterator(); iterator.hasNext();) {
-
-			InfoSeminaryWithEquivalencies infoSeminary = InfoSeminaryWithEquivalencies
-					.newInfoFromDomain((Seminary) iterator.next());
-
+		List<Seminary> seminaries = rootDomainObject.getSeminarys();
+		for (Seminary seminary : seminaries) {
 			Calendar now = new GregorianCalendar();
-			Calendar endDate = new GregorianCalendar();
-			Calendar beginDate = new GregorianCalendar();
-			endDate.setTimeInMillis(infoSeminary.getEnrollmentEndTime().getTimeInMillis()
-					+ infoSeminary.getEnrollmentEndDate().getTimeInMillis());
-			beginDate.setTimeInMillis(infoSeminary.getEnrollmentBeginTime().getTimeInMillis()
-					+ infoSeminary.getEnrollmentBeginDate().getTimeInMillis());
-			if ((!inEnrollmentPeriod.booleanValue()) || (endDate.after(now) && beginDate.before(now)))
-				seminariesInfo.add(infoSeminary);
+			
+            Calendar beginDate = new GregorianCalendar();
+            beginDate.setTimeInMillis(seminary.getEnrollmentBeginTime().getTimeInMillis()
+                    + seminary.getEnrollmentBeginDate().getTimeInMillis());
+			
+            Calendar endDate = new GregorianCalendar();
+            endDate.setTimeInMillis(seminary.getEnrollmentEndTime().getTimeInMillis()
+					+ seminary.getEnrollmentEndDate().getTimeInMillis());
+			
+			if (!inEnrollmentPeriod || (endDate.after(now) && beginDate.before(now)))
+                result.add(InfoSeminaryWithEquivalencies.newInfoFromDomain(seminary));
 		}
 
-		return seminariesInfo;
+		return result;
 	}
+    
 }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/SelectCandidaciesService.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/Seminaries/SelectCandidaciesService.java
@@ -1,7 +1,3 @@
-/*
- * Created on Aug 24, 2004
- *
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.Seminaries;
 
 import java.text.DecimalFormat;
@@ -9,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.CandidacyDTO;
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoClassification;
+import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoSeminary;
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.InfoSeminaryWithEquivalencies;
 import net.sourceforge.fenixedu.dataTransferObject.Seminaries.SelectCandidaciesDTO;
 import net.sourceforge.fenixedu.domain.Enrolment;
@@ -25,41 +21,31 @@ import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.Seminaries.Candidacy;
 import net.sourceforge.fenixedu.domain.Seminaries.Seminary;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
-import net.sourceforge.fenixedu.persistenceTier.Seminaries.IPersistentSeminary;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 
-/**
- * @author João Mota
- * 
- */
 public class SelectCandidaciesService extends Service {
 
 	public SelectCandidaciesDTO run(Boolean inEnrollmentPeriod, Integer seminaryID)
 			throws FenixServiceException, ExcepcaoPersistencia {
 		SelectCandidaciesDTO result = new SelectCandidaciesDTO();
 
-		IPersistentSeminary persistentSeminary = persistentSupport.getIPersistentSeminary();
-		List seminaries = persistentSeminary.readAll();
+		List<Seminary> seminaries = rootDomainObject.getSeminarys();
 		List infoSeminaries = getSeminaries(inEnrollmentPeriod, seminaries);
 		result.setSeminaries(infoSeminaries);
-		List candidacies = getCandidacies(seminaryID, seminaries, persistentObject);
-		Iterator iter = candidacies.iterator();
-		List infoCandidacies = new ArrayList();
-		while (iter.hasNext()) {
-			Candidacy candidacy = (Candidacy) iter.next();
+
+		List<CandidacyDTO> infoCandidacies = new ArrayList<CandidacyDTO>();
+		for (Candidacy candidacy : getCandidacies(seminaryID, seminaries)) {
 			Student student = candidacy.getStudent();
 			StudentCurricularPlan studentCurricularPlan = getStudentCurricularPlan(student);
-			List enrollments = studentCurricularPlan.getEnrolments();
 
 			CandidacyDTO candidacyDTO = new CandidacyDTO();
 			candidacyDTO.setNumber(student.getNumber());
 			candidacyDTO.setName(student.getPerson().getNome());
 			candidacyDTO.setUsername(student.getPerson().getUsername());
 			candidacyDTO.setEmail(student.getPerson().getNome());
-			candidacyDTO.setInfoClassification(getInfoClassification(enrollments));
+			candidacyDTO.setInfoClassification(getInfoClassification(studentCurricularPlan.getEnrolments()));
 			candidacyDTO.setCandidacyId(candidacy.getIdInternal());
 			if (candidacy.getApproved() != null) {
 				candidacyDTO.setApproved(candidacy.getApproved());
@@ -68,64 +54,53 @@ public class SelectCandidaciesService extends Service {
 			}
 			infoCandidacies.add(candidacyDTO);
 		}
-
 		result.setCandidacies(infoCandidacies);
 
 		return result;
 	}
 
-	/**
-	 * @param enrollments
-	 * @param infoClassification
-	 */
-	private InfoClassification getInfoClassification(List enrollments) {
+	private InfoClassification getInfoClassification(List<Enrolment> enrollments) {
 		InfoClassification infoClassification = new InfoClassification();
 		int auxInt = 0;
 		float acc = 0;
 		float grade = 0;
-		for (Iterator iter1 = enrollments.iterator(); iter1.hasNext();) {
-			Enrolment enrollment = (Enrolment) iter1.next();
-			List enrollmentEvaluations = enrollment.getEvaluations();
+		for (Enrolment enrollment : enrollments) {
+			List<EnrolmentEvaluation> enrollmentEvaluations = enrollment.getEvaluations();
 			EnrolmentEvaluation enrollmentEvaluation = null;
-			if (enrollmentEvaluations != null && !enrollmentEvaluations.isEmpty()) {
-				enrollmentEvaluation = (EnrolmentEvaluation) Collections.max(enrollmentEvaluations);
+			if (!enrollmentEvaluations.isEmpty()) {
+				enrollmentEvaluation = Collections.max(enrollmentEvaluations);
 			}
 
 			String stringGrade;
 			if (enrollmentEvaluation != null) {
-
 				stringGrade = enrollmentEvaluation.getGrade();
 			} else {
 				stringGrade = "NA";
 			}
 
-			if (stringGrade != null && !stringGrade.equals("") && !stringGrade.equals("RE")
-					&& !stringGrade.equals("NA") && !stringGrade.equals("AP")) {
+			if (stringGrade != null 
+                    && !stringGrade.equals("") 
+                    && !stringGrade.equals("RE")
+					&& !stringGrade.equals("NA") 
+                    && !stringGrade.equals("AP")) {
 				Float gradeObject = new Float(stringGrade);
 				grade = gradeObject.floatValue();
 				acc += grade;
 				auxInt++;
 			}
-
 		}
 		if (auxInt != 0) {
 			String value = new DecimalFormat("#0.0").format(acc / auxInt);
 			infoClassification.setAritmeticClassification(value);
 		}
-		infoClassification.setCompletedCourses(new Integer(auxInt).toString());
+		infoClassification.setCompletedCourses(Integer.valueOf(auxInt).toString());
 		return infoClassification;
 	}
 
-	/**
-	 * @param student
-	 * @return
-	 */
 	private StudentCurricularPlan getStudentCurricularPlan(Student student) {
-		List curricularPlans = student.getStudentCurricularPlans();
 		long startDate = Long.MAX_VALUE;
 		StudentCurricularPlan selectedSCP = null;
-		for (Iterator iter = curricularPlans.iterator(); iter.hasNext();) {
-			StudentCurricularPlan studentCurricularPlan = (StudentCurricularPlan) iter.next();
+		for (StudentCurricularPlan studentCurricularPlan : student.getStudentCurricularPlans()) {
 			if (studentCurricularPlan.getStartDate().getTime() < startDate) {
 				startDate = studentCurricularPlan.getStartDate().getTime();
 				selectedSCP = studentCurricularPlan;
@@ -134,14 +109,7 @@ public class SelectCandidaciesService extends Service {
 		return selectedSCP;
 	}
 
-	/**
-	 * @param seminaryID
-	 * @param seminaries
-	 * @param persistentObject
-	 * @return
-	 */
-	private List getCandidacies(final Integer seminaryID, List seminaries,
-			IPersistentObject persistentObject) {
+	private List<Candidacy> getCandidacies(final Integer seminaryID, List seminaries) {
 		Seminary seminary = (Seminary) CollectionUtils.find(seminaries, new Predicate() {
 
 			public boolean evaluate(Object arg0) {
@@ -153,28 +121,22 @@ public class SelectCandidaciesService extends Service {
 		return seminary.getCandidacies();
 	}
 
-	/**
-	 * @param persistentSeminary
-	 * @param inEnrollmentPeriod
-	 * @return
-	 * @throws ExcepcaoPersistencia
-	 */
-	private List getSeminaries(Boolean inEnrollmentPeriod, List seminaries) {
-		List result = new ArrayList();
+	private List<InfoSeminary> getSeminaries(Boolean inEnrollmentPeriod, List<Seminary> seminaries) {
+		List<InfoSeminary> result = new ArrayList<InfoSeminary>();
 
-		for (Iterator iterator = seminaries.iterator(); iterator.hasNext();) {
-			InfoSeminaryWithEquivalencies infoSeminary = InfoSeminaryWithEquivalencies
-					.newInfoFromDomain((Seminary) iterator.next());
-
+		for (Seminary seminary : seminaries) {
 			Calendar now = new GregorianCalendar();
-			Calendar endDate = new GregorianCalendar();
+			
 			Calendar beginDate = new GregorianCalendar();
-			endDate.setTimeInMillis(infoSeminary.getEnrollmentEndTime().getTimeInMillis()
-					+ infoSeminary.getEnrollmentEndDate().getTimeInMillis());
-			beginDate.setTimeInMillis(infoSeminary.getEnrollmentBeginTime().getTimeInMillis()
-					+ infoSeminary.getEnrollmentBeginDate().getTimeInMillis());
-			if ((!inEnrollmentPeriod.booleanValue()) || (endDate.after(now) && beginDate.before(now)))
-				result.add(infoSeminary);
+            beginDate.setTimeInMillis(seminary.getEnrollmentBeginTime().getTimeInMillis()
+                    + seminary.getEnrollmentBeginDate().getTimeInMillis());
+            
+            Calendar endDate = new GregorianCalendar();
+            endDate.setTimeInMillis(seminary.getEnrollmentEndTime().getTimeInMillis()
+					+ seminary.getEnrollmentEndDate().getTimeInMillis());
+			
+			if (!inEnrollmentPeriod || (endDate.after(now) && beginDate.before(now)))
+				result.add(InfoSeminaryWithEquivalencies.newInfoFromDomain(seminary));
 		}
 		return result;
 	}
