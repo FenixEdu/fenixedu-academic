e2ef5c66bab369a531ee329493a4c6f466cdcc5d
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-24 12:06:11 +0000

    dereferencing instead of using DAOs

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/transactions/ReadPaymentTransactionByGuideEntryID.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/transactions/ReadPaymentTransactionByGuideEntryID.java
@@ -4,31 +4,20 @@ import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.transactions.InfoPaymentTransaction;
+import net.sourceforge.fenixedu.domain.GuideEntry;
 import net.sourceforge.fenixedu.domain.transactions.PaymentTransaction;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
-/**
- * @author <a href="mailto:shezad@ist.utl.pt">Shezad Anavarali </a>
- * 
- */
-
 public class ReadPaymentTransactionByGuideEntryID extends Service {
 
-    public InfoPaymentTransaction run(Integer guideEntryId) throws FenixServiceException,
-            ExcepcaoPersistencia {
-
-        InfoPaymentTransaction infoPaymentTransaction = null;
-
-        PaymentTransaction paymentTransaction = persistentSupport.getIPersistentPaymentTransaction()
-                .readByGuideEntryID(guideEntryId);
-
+    public InfoPaymentTransaction run(Integer guideEntryId) throws FenixServiceException, ExcepcaoPersistencia {
+        GuideEntry guideEntry = rootDomainObject.readGuideEntryByOID(guideEntryId);
+        PaymentTransaction paymentTransaction = guideEntry.getPaymentTransaction();
         if (paymentTransaction == null) {
             throw new ExcepcaoInexistente();
         }
 
-        infoPaymentTransaction = InfoPaymentTransaction.newInfoFromDomain(paymentTransaction);
-
-        return infoPaymentTransaction;
+        return InfoPaymentTransaction.newInfoFromDomain(paymentTransaction);
     }
 
 }
