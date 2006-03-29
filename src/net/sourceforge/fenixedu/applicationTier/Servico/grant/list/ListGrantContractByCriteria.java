7991dc3bbe76a212aa1e22e43fa0a7ec76144fdf
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-29 14:29:28 +0000

    - removed unused code - using rootDomainObject instead of DAOs access

--- src/net/sourceforge/fenixedu/applicationTier/Servico/grant/list/ListGrantContractByCriteria.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/grant/list/ListGrantContractByCriteria.java
@@ -1,7 +1,3 @@
-/*
- * Created on Jun 21, 2004
- *
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.grant.list;
 
 import java.util.ArrayList;
@@ -17,12 +13,9 @@ import net.sourceforge.fenixedu.dataTransferObject.grant.list.InfoListGrantOwner
 import net.sourceforge.fenixedu.dataTransferObject.grant.list.InfoSpanByCriteriaListGrantContract;
 import net.sourceforge.fenixedu.domain.grant.contract.GrantContract;
 import net.sourceforge.fenixedu.domain.grant.contract.GrantContractRegime;
-import net.sourceforge.fenixedu.domain.grant.contract.GrantInsurance;
 import net.sourceforge.fenixedu.domain.grant.contract.GrantPart;
 import net.sourceforge.fenixedu.domain.grant.contract.GrantSubsidy;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.grant.IPersistentGrantContractRegime;
-import net.sourceforge.fenixedu.persistenceTier.grant.IPersistentGrantInsurance;
 import net.sourceforge.fenixedu.presentationTier.Action.grant.utils.SessionConstants;
 import net.sourceforge.fenixedu.util.DateFormatUtil;
 import net.sourceforge.fenixedu.util.NameUtils;
@@ -30,14 +23,7 @@ import net.sourceforge.fenixedu.util.NameUtils;
 import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.collections.comparators.ComparatorChain;
 
-/**
- * @author Pica
- * @author Barbosa
- */
 public class ListGrantContractByCriteria extends Service {
-
-	public ListGrantContractByCriteria() {
-	}
 
 	/**
 	 * Query the grant owner by criteria of grant contract
@@ -50,7 +36,6 @@ public class ListGrantContractByCriteria extends Service {
 			throws FenixServiceException, ExcepcaoPersistencia,FenixFilterException,Exception {
 
 		// Read the grant contracts ordered by persistentSupportan
-
 		List<GrantContractRegime> grantContractBySpanAndCriteria = readAllContractsByCriteria(
 				propertyOrderBy(infoSpanByCriteriaListGrantOwner.getOrderBy()),
 				infoSpanByCriteriaListGrantOwner.getJustActiveContract(),
@@ -60,25 +45,19 @@ public class ListGrantContractByCriteria extends Service {
 				SessionConstants.NUMBER_OF_ELEMENTS_IN_SPAN, infoSpanByCriteriaListGrantOwner
 						.getGrantTypeId(),infoSpanByCriteriaListGrantOwner.getValidToTheDate());
 
-		List listGrantContract = null;
+		List<InfoListGrantOwnerByOrder> listGrantContract = null;
 
 		if (grantContractBySpanAndCriteria != null && grantContractBySpanAndCriteria.size() != 0) {
-
-			/*
-			 * Construct the info list and add to the result.
-			 */
-
-			listGrantContract = new ArrayList();
+		    // Construct the info list and add to the result.
+			listGrantContract = new ArrayList<InfoListGrantOwnerByOrder>();
 
 			for (GrantContractRegime grantContractRegime : grantContractBySpanAndCriteria ){
-				
 				convertToInfoListGrantOwnerByOrder(grantContractRegime, infoSpanByCriteriaListGrantOwner, listGrantContract);
 			}
 		}
 
 		if (infoSpanByCriteriaListGrantOwner.getTotalElements() == null) {
 			// Setting the search attributes
-
 			infoSpanByCriteriaListGrantOwner.setTotalElements(countAllByCriteria(infoSpanByCriteriaListGrantOwner.getJustActiveContract(),
 							infoSpanByCriteriaListGrantOwner.getJustDesactiveContract(),
 							infoSpanByCriteriaListGrantOwner.getBeginContract(),
@@ -96,17 +75,8 @@ public class ListGrantContractByCriteria extends Service {
 	 */
 	private void convertToInfoListGrantOwnerByOrder(GrantContractRegime grantContractRegime,
 			InfoSpanByCriteriaListGrantContract infoSpanByCriteriaListGrantOwner,
-			List result) throws ExcepcaoPersistencia {
+			List<InfoListGrantOwnerByOrder> result) throws ExcepcaoPersistencia {
 
-		IPersistentGrantContractRegime persistentGrantContractRegime = persistentSupport
-				.getIPersistentGrantContractRegime();
-		IPersistentGrantInsurance persistentGrantInsurance = persistentSupport.getIPersistentGrantInsurance();
-
-		// Read the actual regime and insurance
-		
-		GrantInsurance grantInsurance = persistentGrantInsurance
-				.readGrantInsuranceByGrantContract(grantContractRegime.getGrantContract().getIdInternal());
-		
 		InfoListGrantOwnerByOrder infoListGrantOwnerByOrder = new InfoListGrantOwnerByOrder();
 
 		infoListGrantOwnerByOrder.setGrantOwnerId(grantContractRegime.getGrantContract().getGrantOwner().getIdInternal());
@@ -117,7 +87,7 @@ public class ListGrantContractByCriteria extends Service {
 					.getPerson().getNome()));
 			infoListGrantOwnerByOrder.setLastName(NameUtils.getLastName(grantContractRegime.getGrantContract().getGrantOwner()
 				.getPerson().getNome()));
-		}else{
+		} else {
 			infoListGrantOwnerByOrder.setFirstName("");
 			infoListGrantOwnerByOrder.setLastName("");
 		}
@@ -129,19 +99,15 @@ public class ListGrantContractByCriteria extends Service {
 		infoListGrantOwnerByOrder.setEndContract(grantContractRegime.getDateEndContract());
 		
 		List<GrantSubsidy> grantSubsidyList = grantContractRegime.getGrantContract().getAssociatedGrantSubsidies();
-		
 		for (GrantSubsidy grantSubsidy : grantSubsidyList){
-			 List<GrantPart> grantPartList = (List<GrantPart>) persistentObject.readAll(GrantPart.class);
-			 
-			 for(GrantPart grantPart : grantPartList){ 
-				 if (grantSubsidy.getIdInternal().equals(grantPart.getKeyGrantSubsidy())){
+			 for(GrantPart grantPart : rootDomainObject.getGrantParts()){ 
+				 if (grantSubsidy.equals(grantPart.getGrantSubsidy())){
 					 infoListGrantOwnerByOrder.setInsurancePaymentEntity(grantPart.getGrantPaymentEntity().getNumber());
 					 infoListGrantOwnerByOrder.setNumberPaymentEntity(grantPart.getGrantPaymentEntity().getNumber());
 					 infoListGrantOwnerByOrder.setDesignation(grantPart.getGrantPaymentEntity().getDesignation());
 				 }
 			 }
 		}
-
 
 		result.add(infoListGrantOwnerByOrder);
 	}
@@ -166,31 +132,30 @@ public class ListGrantContractByCriteria extends Service {
 		}
 		return result;
 	}
+    
 	public List<GrantContractRegime> readAllContractsByCriteria(String orderBy, Boolean justActiveContracts,
             Boolean justDesactiveContracts, Date dateBeginContract, Date dateEndContract,
             Integer spanNumber, Integer numberOfElementsInSpan, Integer grantTypeId,Date validToTheDate)
             	throws FenixFilterException, FenixServiceException, ExcepcaoPersistencia ,Exception{
 			
-			List<GrantContractRegime> result = new ArrayList();
+			List<GrantContractRegime> result = new ArrayList<GrantContractRegime>();
 			Date ToTheDate = null;
 			
-			List<GrantContractRegime> grantContractRegimes = (List<GrantContractRegime>) persistentObject.readAll(GrantContractRegime.class);
+			List<GrantContractRegime> grantContractRegimes = rootDomainObject.getGrantContractRegimes();
 			ComparatorChain comparatorChain = new ComparatorChain(new BeanComparator("grantContract.grantOwner.number"), true);
 	        Collections.sort(grantContractRegimes, comparatorChain);
 	        Collections.reverse(grantContractRegimes);
 	        for (final GrantContractRegime grantContractRegime : ((List<GrantContractRegime>) grantContractRegimes)) {
 	            final GrantContract grantContract = grantContractRegime.getGrantContract();
-
 	            if (grantContract == null) {
 	                continue;
 	            }
 	            
 	            if ((validToTheDate == null || validToTheDate.equals(""))
-	            		&&(dateBeginContract == null || dateBeginContract.equals(""))
-	            			&&(dateEndContract == null || dateEndContract.equals(""))){
+	                    && (dateBeginContract == null || dateBeginContract.equals(""))
+	                    && (dateEndContract == null || dateEndContract.equals(""))){
                 	if (justActiveContracts != null
-	                        && justActiveContracts.booleanValue()){
-	                        
+                	        && justActiveContracts.booleanValue()){
                 		if (grantContractRegime.getGrantContract().getEndContractMotive()!= null && !grantContractRegime.getGrantContract().getEndContractMotive().equals("")){
     	            		continue;
     	            	}
@@ -199,8 +164,7 @@ public class ListGrantContractByCriteria extends Service {
                          }
                 	}
                 	if (justDesactiveContracts != null
-	                        && justDesactiveContracts.booleanValue()){
-                		
+                	        && justDesactiveContracts.booleanValue()){
                 		if(grantContractRegime.getContractRegimeActive()
                 				&& (grantContractRegime.getGrantContract().getEndContractMotive()== null 
                 						|| grantContractRegime.getGrantContract().getEndContractMotive().equals(""))) {
@@ -210,7 +174,6 @@ public class ListGrantContractByCriteria extends Service {
 	            }
 	            
 	            if (validToTheDate != null){
-	            	
 	            	if (grantContractRegime.getDateBeginContract() == null || grantContractRegime.getDateEndContract() == null) {
 	                    continue;
 	                }
@@ -255,13 +218,11 @@ public class ListGrantContractByCriteria extends Service {
 	            		}
 	                        
 	            	}
-	            	
 	            }
 	            if (grantTypeId != null) {
 	            	if(!grantContractRegime.getGrantContract().getGrantType().getIdInternal().equals(grantTypeId)){
 	            		continue;
 	            	}
-	            		
 	            }
 	            result.add(grantContractRegime);
 	        }
@@ -270,15 +231,10 @@ public class ListGrantContractByCriteria extends Service {
 	        int end = begin + numberOfElementsInSpan;
 
 	        return result.subList(begin, Math.min(end,result.size()));
-       
     }
-	 public List<GrantContract> readAll() throws FenixServiceException,FenixFilterException, ExcepcaoPersistencia {
-	       return persistentSupport.getIPersistentGrantContract().readAll();
-	}
-	
-	public List<GrantContract> readBySpan(Integer spanNumber, Integer numberOfElementsInSpan,List<GrantContract> grantContract){
-		
-		List<GrantContract> result = new ArrayList();
+
+    public List<GrantContract> readBySpan(Integer spanNumber, Integer numberOfElementsInSpan,List<GrantContract> grantContract){
+		List<GrantContract> result = new ArrayList<GrantContract>();
 		Iterator iter = grantContract.iterator();
 
 		int begin = (spanNumber.intValue() - 1) * numberOfElementsInSpan.intValue();
@@ -302,7 +258,7 @@ public class ListGrantContractByCriteria extends Service {
 	  			throws FenixServiceException,FenixFilterException, ExcepcaoPersistencia {
 			Integer result =new Integer(0);
 			
-			List<GrantContractRegime> grantContractRegimes = (List<GrantContractRegime>) persistentObject.readAll(GrantContractRegime.class);
+			List<GrantContractRegime> grantContractRegimes = rootDomainObject.getGrantContractRegimes();
 			ComparatorChain comparatorChain = new ComparatorChain(new BeanComparator("grantContract.grantOwner.number"), true);
 	        Collections.sort(grantContractRegimes, comparatorChain);
 	        Collections.reverse(grantContractRegimes);
@@ -367,7 +323,6 @@ public class ListGrantContractByCriteria extends Service {
 	            	
 	            	if (justActiveContracts != null
 	                        && justActiveContracts.booleanValue()){
-	            		
 	            		if(!grantContractRegime.getState().equals(new Integer(1))){
 	            			continue;
 	            		}      
@@ -378,7 +333,6 @@ public class ListGrantContractByCriteria extends Service {
 	            	
 	            	if (justDesactiveContracts != null
 	                        && justDesactiveContracts.booleanValue()){
-	            		
 	            		if(grantContractRegime.getState().equals(new Integer(1))&& (grantContractRegime.getGrantContract().getEndContractMotive()== null 
         						|| grantContractRegime.getGrantContract().getEndContractMotive().equals(""))){
 	            			continue;
@@ -388,19 +342,15 @@ public class ListGrantContractByCriteria extends Service {
 	                        
 	            }
 	            	
-	            	
-	            
-	            
 	            if (grantTypeId != null) {
 	            	if(!grantContractRegime.getGrantContract().getGrantType().getIdInternal().equals(grantTypeId)){
 	            		continue;
 	            	}
-	            		
 	            }
 
 	            result++;
-	       
 	        }
 	       return result;
 	  }
+      
 }
