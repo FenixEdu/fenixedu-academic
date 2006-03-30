61d7f6859220ff151038636861504eaaff1cf215
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-30 10:03:31 +0000

    dereferencing instead of DAO access

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/gaugingTests/physics/readGaugingTestsResults.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/gaugingTests/physics/readGaugingTestsResults.java
@@ -1,7 +1,3 @@
-/*
- * Created on 26/Nov/2003
- *  
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.student.gaugingTests.physics;
 
 import net.sourceforge.fenixedu.applicationTier.IUserView;
@@ -13,35 +9,23 @@ import net.sourceforge.fenixedu.domain.Student;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.domain.gaugingTests.physics.GaugingTestResult;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentStudent;
-import net.sourceforge.fenixedu.persistenceTier.OJB.gaugingTests.physics.IPersistentGaugingTestResult;
-
-/**
- * @author <a href="mailto:joao.mota@ist.utl.pt">João Mota </a>
- * 
- */
 
 public class readGaugingTestsResults extends Service {
 
 	public InfoGaugingTestResult run(IUserView userView) throws FenixServiceException, ExcepcaoPersistencia {
-		IPersistentGaugingTestResult persistentGaugingTestResult = persistentSupport.getIPersistentGaugingTestResult();
 		Person person = Person.readPersonByUsername(userView.getUtilizador());
 
-		IPersistentStudent persistentStudent = persistentSupport.getIPersistentStudent();
-
-		Student student = persistentStudent.readByPersonAndDegreeType(person.getIdInternal(),
-				DegreeType.DEGREE);
+		Student student = persistentSupport.getIPersistentStudent().readByPersonAndDegreeType(person.getIdInternal(),DegreeType.DEGREE);
 		if (student == null) {
 			return null;
 		}
-		GaugingTestResult gaugingTestsResult = persistentGaugingTestResult.readByStudent(student);
+		
+        GaugingTestResult gaugingTestsResult = student.getAssociatedGaugingTestResult();
 		if (gaugingTestsResult != null) {
-
-			InfoGaugingTestResult infoGaugingTestResult = InfoGaugingTestResult
-					.newInfoFromDomain(gaugingTestsResult);
-			return infoGaugingTestResult;
+			return InfoGaugingTestResult.newInfoFromDomain(gaugingTestsResult);
 		}
 
 		return null;
 	}
+
 }
