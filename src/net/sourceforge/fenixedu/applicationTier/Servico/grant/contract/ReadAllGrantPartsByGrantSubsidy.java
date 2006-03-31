7ca26f88b82eb6f3b0f8ca2750d610cd104550e9
Luís Egídio <luis.egidio@ist.utl.pt>
2006-03-31 15:59:11 +0000

    dereferencing instead of DAO accessing

--- src/net/sourceforge/fenixedu/applicationTier/Servico/grant/contract/ReadAllGrantPartsByGrantSubsidy.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/grant/contract/ReadAllGrantPartsByGrantSubsidy.java
@@ -1,44 +1,29 @@
-/*
- * Created on 23/Jan/2004
- *  
- */
 package net.sourceforge.fenixedu.applicationTier.Servico.grant.contract;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.grant.contract.InfoGrantPart;
 import net.sourceforge.fenixedu.dataTransferObject.grant.contract.InfoGrantPartWithSubsidyAndTeacherAndPaymentEntity;
 import net.sourceforge.fenixedu.domain.grant.contract.GrantPart;
+import net.sourceforge.fenixedu.domain.grant.contract.GrantSubsidy;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.grant.IPersistentGrantPart;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
-
-/**
- * @author Barbosa
- * @author Pica
- * 
- */
 public class ReadAllGrantPartsByGrantSubsidy extends Service {
 
 	public List run(Integer grantSubsidyId) throws FenixServiceException, ExcepcaoPersistencia {
-		List result = null;
+		List<InfoGrantPart> result = new ArrayList<InfoGrantPart>();
 
-		IPersistentGrantPart pgp = persistentSupport.getIPersistentGrantPart();
-		List grantParts = pgp.readAllGrantPartsByGrantSubsidy(grantSubsidyId);
+		GrantSubsidy grantSubsidy = rootDomainObject.readGrantSubsidyByOID(grantSubsidyId);
+        List<GrantPart> grantParts = grantSubsidy.getAssociatedGrantParts();
 
-		if (grantParts != null) {
-			result = (List) CollectionUtils.collect(grantParts, new Transformer() {
-				public Object transform(Object o) {
-					GrantPart grantPart = (GrantPart) o;
-					return InfoGrantPartWithSubsidyAndTeacherAndPaymentEntity
-							.newInfoFromDomain(grantPart);
-				}
-			});
-		}
-
+        for (GrantPart grantPart : grantParts) {
+            result.add(InfoGrantPartWithSubsidyAndTeacherAndPaymentEntity.newInfoFromDomain(grantPart));
+        }
+        
 		return result;
 	}
+    
 }
