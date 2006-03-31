4cfac5dc8b47742f50851cd95a8844c6e05bd570
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-03-31 16:43:35 +0000

    *** empty log message ***

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoSiteRSS.java
@@ -0,0 +1,13 @@
+/*
+ * Created on 21/Jul/2003
+ *
+ * 
+ */
+package net.sourceforge.fenixedu.dataTransferObject;
+
+
+
+public class InfoSiteRSS extends DataTranferObject implements ISiteComponent {
+
+    
+}
