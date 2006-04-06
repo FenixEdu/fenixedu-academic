c01ff904a34c930d4edfb5416d8e5b53786ecdb1
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-06 06:49:38 +0000

    bug fix

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoWebSiteSection.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoWebSiteSection.java
@@ -25,7 +25,7 @@ public class InfoWebSiteSection extends InfoObject {
 
     private InfoWebSite infoWebSite;
 
-    private List<InfoItem> infoItemsList;
+    private List<InfoWebSiteItem> infoItemsList;
 
     /**
      * @return
@@ -44,14 +44,14 @@ public class InfoWebSiteSection extends InfoObject {
     /**
      * @return
      */
-    public List<InfoItem> getInfoItemsList() {
+    public List<InfoWebSiteItem> getInfoItemsList() {
         return infoItemsList;
     }
 
     /**
      * @param itemsList
      */
-    public void setInfoItemsList(List<InfoItem> itemsList) {
+    public void setInfoItemsList(List<InfoWebSiteItem> itemsList) {
         this.infoItemsList = itemsList;
     }
 
