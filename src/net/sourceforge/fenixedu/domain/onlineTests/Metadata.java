6276283207be164ff2aa8ba411213e67a3568dfe
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-07 14:43:53 +0000

    missing de-referencing of execution course

--- src/net/sourceforge/fenixedu/domain/onlineTests/Metadata.java
+++ src/net/sourceforge/fenixedu/domain/onlineTests/Metadata.java
@@ -98,6 +98,7 @@ public class Metadata extends Metadata_Base {
 
     public void delete() {
         for (;!getQuestions().isEmpty();getQuestions().get(0).delete());
+        removeExecutionCourse();
         removeRootDomainObject();
         super.deleteDomainObject();
     }
