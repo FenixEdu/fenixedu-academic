79ba1e7c00fa548f4ede8e457146eca4703e8dd9
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-07 12:50:01 +0000

    using domain specific delete method

--- src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteClasses.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteClasses.java
@@ -9,43 +9,21 @@ package net.sourceforge.fenixedu.applicationTier.Servico.sop;
  * 
  * @author Luis Cruz & Sara Ribeiro
  */
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.domain.SchoolClass;
-import net.sourceforge.fenixedu.domain.Shift;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class DeleteClasses extends Service {
 
-    public Object run(List classOIDs) throws ExcepcaoPersistencia {
-        for (int i = 0; i < classOIDs.size(); i++) {
-            Integer classId = (Integer) classOIDs.get(i);
+    public Boolean run(List<Integer> classOIDs) throws ExcepcaoPersistencia {
+        for (Integer classId : classOIDs) {
             final SchoolClass schoolClass = rootDomainObject.readSchoolClassByOID(classId);
-
-            // Shift
-            Iterator iter = schoolClass.getAssociatedShiftsIterator();
-            while(iter.hasNext()){
-                Shift shift = (Shift) iter.next();
-                iter.remove();
-                shift.getAssociatedClasses().remove(schoolClass);
-            }
-            schoolClass.getAssociatedShifts().clear();
-
-            // ExecutionDegree
-            schoolClass.getExecutionDegree().getSchoolClasses().remove(schoolClass);
-            schoolClass.setExecutionDegree(null);
-
-            // ExecutionPeriod
-            schoolClass.getExecutionPeriod().getSchoolClasses().remove(schoolClass);
-            schoolClass.setExecutionPeriod(null);
-
-            persistentObject.deleteByOID(SchoolClass.class, schoolClass.getIdInternal());
+            schoolClass.delete();
         }
 
-        return new Boolean(true);
-
+        return Boolean.TRUE;
     }
 
 }
