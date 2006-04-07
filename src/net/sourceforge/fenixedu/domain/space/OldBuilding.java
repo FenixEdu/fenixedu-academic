487c37187bb0d935a940bb5df5403bd7a5786b48
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-07 11:59:10 +0000

    added delete method

--- src/net/sourceforge/fenixedu/domain/space/OldBuilding.java
+++ src/net/sourceforge/fenixedu/domain/space/OldBuilding.java
@@ -1,10 +1,20 @@
 package net.sourceforge.fenixedu.domain.space;
 
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+
 
 public class OldBuilding extends OldBuilding_Base {
 
     public OldBuilding() {
     	super();
+    }
+    
+    public void delete() {
+        if (!hasAnyRooms()) {
+            deleteDomainObject();    
+        } else {
+            throw new DomainException("");
+        }
     }
 
 }
