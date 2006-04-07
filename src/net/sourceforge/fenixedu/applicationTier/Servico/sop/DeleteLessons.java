16447bad0530233c75b5e7d34469b69917062a13
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-07 11:03:22 +0000

    using domain specific delete method

--- src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteLessons.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteLessons.java
@@ -8,42 +8,16 @@ package net.sourceforge.fenixedu.applicationTier.Servico.sop;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
-import net.sourceforge.fenixedu.domain.Lesson;
-import net.sourceforge.fenixedu.domain.space.RoomOccupation;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
 
 public class DeleteLessons extends Service {
 
-    public Boolean run(final List lessonOIDs) throws ExcepcaoPersistencia {
-        for (int j = 0; j < lessonOIDs.size(); j++) {
-            final Integer lessonOID = (Integer) lessonOIDs.get(j);
-            deleteLesson(persistentSupport, lessonOID);
+    public Boolean run(final List<Integer> lessonOIDs) throws ExcepcaoPersistencia {
+        for (final Integer lessonOID : lessonOIDs) {
+            rootDomainObject.readLessonByOID(lessonOID).delete();
         }
 
-        return new Boolean(true);
-    }
-
-    public static void deleteLesson(final ISuportePersistente persistentSupport, final Integer lessonOID)
-            throws ExcepcaoPersistencia {
-        final Lesson lesson = rootDomainObject.readLessonByOID(lessonOID);
-        deleteLesson(persistentSupport, lesson);
-    }
-
-    public static void deleteLesson(final ISuportePersistente persistentSupport, final Lesson lesson)
-            throws ExcepcaoPersistencia {
-        final RoomOccupation roomOccupation = lesson.getRoomOccupation();
-
-        roomOccupation.setPeriod(null);
-        roomOccupation.setRoom(null);
-
-        lesson.setShift(null);
-        lesson.setRoomOccupation(null);
-        lesson.setSala(null);
-        lesson.setExecutionPeriod(null);
-
-        roomOccupation.delete();
-        persistentObject.deleteByOID(Lesson.class, lesson.getIdInternal());
+        return Boolean.TRUE;
     }
 
 }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteShift.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/sop/DeleteShift.java
@@ -8,15 +8,10 @@ package net.sourceforge.fenixedu.applicationTier.Servico.sop;
 /**
  * @author Luis Cruz & Sara Ribeiro
  */
-import java.util.List;
-
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShift;
-import net.sourceforge.fenixedu.domain.Lesson;
-import net.sourceforge.fenixedu.domain.SchoolClass;
 import net.sourceforge.fenixedu.domain.Shift;
-import net.sourceforge.fenixedu.domain.ShiftProfessorship;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class DeleteShift extends Service {
@@ -36,38 +31,21 @@ public class DeleteShift extends Service {
     public static void deleteShift(final Integer shiftID) throws ExcepcaoPersistencia, FenixServiceException {
         Shift shift = rootDomainObject.readShiftByOID(shiftID);
         if (shift != null) {
-            List studentShifts = shift.getStudents();
-            if (studentShifts != null && studentShifts.size() > 0) {
+            if (shift.hasAnyStudents()) {
                 throw new FenixServiceException("error.deleteShift.with.students");
             }
 
-            // if the shift has student groups associated it can't be
-            // deleted
-            List studentGroupShift = shift.getAssociatedStudentGroups();
-
-            if (studentGroupShift.size() > 0) {
+            // if the shift has student groups associated it can't be deleted
+            if (shift.hasAnyAssociatedStudentGroups()) {
                 throw new FenixServiceException("error.deleteShift.with.studentGroups");
             }
 
             // if the shift has summaries it can't be deleted            
-            List summariesShift = shift.getAssociatedSummaries();
-            if (summariesShift != null && summariesShift.size() > 0) {
+            if (shift.hasAnyAssociatedSummaries()) {
                 throw new FenixServiceException("error.deleteShift.with.summaries");
             }
 
-            for (final List<ShiftProfessorship> shiftProfessorship = shift.getAssociatedShiftProfessorship();
-                    !shiftProfessorship.isEmpty();
-                    shiftProfessorship.get(0).delete());
-
-            for (final List<Lesson> lessons = shift.getAssociatedLessons();
-                    !lessons.isEmpty();
-                    DeleteLessons.deleteLesson(persistentSupport, lessons.get(0)));
-
-            for (final List<SchoolClass> schoolClasses = shift.getAssociatedClasses();
-                    !schoolClasses.isEmpty(); shift.removeAssociatedClasses(schoolClasses.get(0)));
-
-            shift.setDisciplinaExecucao(null);
-            persistentObject.deleteByOID(Shift.class, shift.getIdInternal());
+            shift.delete();
         }
     }
 
