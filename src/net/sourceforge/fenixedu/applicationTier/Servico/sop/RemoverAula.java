5dcc39026eda216f939e9846490e9bcbb2184217
Luís Egídio <luis.egidio@ist.utl.pt>
2006-04-07 11:46:49 +0000

    using domain specific delete method

--- src/net/sourceforge/fenixedu/applicationTier/Servico/sop/RemoverAula.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/sop/RemoverAula.java
@@ -3,13 +3,11 @@ package net.sourceforge.fenixedu.applicationTier.Servico.sop;
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.dataTransferObject.InfoLesson;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShift;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class RemoverAula extends Service {
 
-    public Object run(final InfoLesson infoLesson, final InfoShift infoShift)
-            throws ExcepcaoPersistencia {
-        DeleteLessons.deleteLesson(persistentSupport, infoLesson.getIdInternal());
+    public Object run(final InfoLesson infoLesson, final InfoShift infoShift) {
+        rootDomainObject.readLessonByOID(infoLesson.getIdInternal()).delete();
         return Boolean.TRUE;
     }
 
