a7da35d2256c244778ed21a0f3926dea5f8ccfb3
Catarina Simões <velouria@velouria.org>
2006-04-07 15:46:46 +0000

    Added a converter from JodaTime constants that represent a day of the week to WeekDay.

--- src/net/sourceforge/fenixedu/util/WeekDay.java
+++ src/net/sourceforge/fenixedu/util/WeekDay.java
@@ -3,13 +3,14 @@
 */
 package net.sourceforge.fenixedu.util;
 
+import org.joda.time.DateTime;
+
 /**
  * @author Ricardo Rodrigues
  *
  */
 
 public enum WeekDay {
-
     MONDAY,
     TUESDAY,
     WEDNESDAY,
@@ -42,6 +43,28 @@ public enum WeekDay {
             return null;
         }        
     }
+    
+    // Return a WeekDay converted from JodaTime DateTime.
+    public static WeekDay fromJodaTimeToWeekDay(DateTime date) {
+    		int dayOfWeek = date.dayOfWeek().get();
+    		switch (dayOfWeek) {
+    		case 1:
+    			return WeekDay.MONDAY;
+    		case 2:
+    			return WeekDay.TUESDAY;
+    		case 3:
+    			return WeekDay.WEDNESDAY;
+    		case 4:
+    			return WeekDay.THURSDAY;
+    		case 5:
+    			return WeekDay.FRIDAY;
+    		case 6:
+    			return WeekDay.SATURDAY;
+    		case 7:
+    			return WeekDay.SUNDAY;
+    		default:
+    			return null;
+    		}
+    }
+    
 }
-
-
