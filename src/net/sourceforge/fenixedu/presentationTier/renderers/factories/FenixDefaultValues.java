2bd2bff71df8b4cfacbc7e72a89ced1010fe366b
Luis Cruz <luis.cruz@ist.utl.pt>
2006-04-17 16:00:25 +0000

    Cannot override static methods.

--- src/net/sourceforge/fenixedu/presentationTier/renderers/factories/FenixDefaultValues.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/factories/FenixDefaultValues.java
@@ -4,8 +4,7 @@ import net.sourceforge.fenixedu.domain.DomainObject;
 import net.sourceforge.fenixedu.renderers.model.DefaultValues;
 
 public class FenixDefaultValues  extends net.sourceforge.fenixedu.renderers.model.DefaultValues {
-    
-    @Override
+
     public static DefaultValues getInstance() {
         if (DefaultValues.instance == null) {
             DefaultValues.instance = new FenixDefaultValues();
