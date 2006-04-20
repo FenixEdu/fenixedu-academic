a2a6e4157df5265be904b2eaa9d69a33848523cf
Luis Cruz <luis.cruz@ist.utl.pt>
2006-04-20 16:33:58 +0000

    Removed deprecated code.

--- src/net/sourceforge/fenixedu/applicationTier/Service.java
+++ src/net/sourceforge/fenixedu/applicationTier/Service.java
@@ -1,19 +1,10 @@
 package net.sourceforge.fenixedu.applicationTier;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
-import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
-import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 
 
 public class Service implements pt.utl.ist.berserk.logic.serviceManager.IService {
 
 	protected static final RootDomainObject rootDomainObject = RootDomainObject.getInstance();
-
-	@Deprecated
-	private static final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-
-	@Deprecated
-    protected static final IPersistentObject persistentObject = persistentSupport.getIPersistentObject();
 
 }
