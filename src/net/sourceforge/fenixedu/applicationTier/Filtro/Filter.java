e42db0817d04dbf54dd08243b2d00f08c0b47976
Luis Cruz <luis.cruz@ist.utl.pt>
2006-04-20 17:30:23 +0000

    Removed deprecated code.

--- src/net/sourceforge/fenixedu/applicationTier/Filtro/Filter.java
+++ src/net/sourceforge/fenixedu/applicationTier/Filtro/Filter.java
@@ -1,19 +1,10 @@
 package net.sourceforge.fenixedu.applicationTier.Filtro;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
-import net.sourceforge.fenixedu.persistenceTier.IPersistentObject;
-import net.sourceforge.fenixedu.persistenceTier.ISuportePersistente;
-import net.sourceforge.fenixedu.persistenceTier.PersistenceSupportFactory;
 import pt.utl.ist.berserk.logic.filterManager.IFilter;
 
 abstract public class Filter implements IFilter {
 
     protected static final RootDomainObject rootDomainObject = RootDomainObject.getInstance();
-    
-    @Deprecated
-	protected static final ISuportePersistente persistentSupport = PersistenceSupportFactory.getDefaultPersistenceSupport();
-
-    @Deprecated
-    protected static final IPersistentObject persistentObject = persistentSupport.getIPersistentObject();
     
 }
