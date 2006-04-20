f8779fdbd3d8472785e693ef76c45fa802a530c4
Shezad Anavarali <shezad@ist.utl.pt>
2006-04-20 14:01:21 +0000

    Added comparator

--- src/net/sourceforge/fenixedu/domain/User.java
+++ src/net/sourceforge/fenixedu/domain/User.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.domain;
 
+import java.util.Comparator;
+
 /**
  * @author mrsp
  * @author shezad
@@ -7,12 +9,29 @@ package net.sourceforge.fenixedu.domain;
 
 public class User extends User_Base {
 
+    final public static Comparator<User> USER_UID_COMPARATOR = new Comparator<User>() {
+        public int compare(User u1, User u2) {
+            if (u1.getUserUId() == null && u2.getUserUId() == null) {
+                return 0;
+            }
+            if (u1.getUserUId() == null) {
+                return -1;
+            }
+            if (u2.getUserUId() == null) {
+                return 1;
+            }
+            String u1Id = u1.getUserUId().replaceFirst("[a-zA-Z]+", "");
+            String u2Id = u2.getUserUId().replaceFirst("[a-zA-Z]+", "");
+            return Integer.valueOf(u1Id).compareTo(Integer.valueOf(u2Id));
+        }
+    };
+
     public User() {
         super();
         setRootDomainObject(RootDomainObject.getInstance());
     }
-    
-    public User(Person person){
+
+    public User(Person person) {
         this();
         this.setPerson(person);
     }
@@ -25,14 +44,14 @@ public class User extends User_Base {
         }
         return null;
     }
-    
-    public Login readUserLoginIdentification(){
-        
-        /// In present exist only one Person Login Identification
+
+    public Login readUserLoginIdentification() {
+
+        // / In present exist only one Person Login Identification
         for (Identification identification : this.getIdentifications()) {
             if (identification instanceof Login) {
-                return (Login)identification;                
-            }     
+                return (Login) identification;
+            }
         }
         return null;
     }
