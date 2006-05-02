59b73e29239a32fa49062170a70975f31c595d90
Luís Egídio <luis.egidio@ist.utl.pt>
2006-05-02 15:04:08 +0000

    bug fix, using PeriodState static value

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/AlterExecutionPeriodState.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/AlterExecutionPeriodState.java
@@ -5,13 +5,12 @@ import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceE
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionPeriod;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 import net.sourceforge.fenixedu.util.PeriodState;
 
 public class AlterExecutionPeriodState extends Service {
 
     public void run(InfoExecutionPeriod infoExecutionPeriod, PeriodState periodState)
-            throws FenixServiceException, ExcepcaoPersistencia {
+            throws FenixServiceException {
 
         final ExecutionPeriod executionPeriod = ExecutionYear.readExecutionYearByName(
                 infoExecutionPeriod.getInfoExecutionYear().getYear()).readExecutionPeriodForSemester(infoExecutionPeriod.getSemester()); 
@@ -23,7 +22,7 @@ public class AlterExecutionPeriodState extends Service {
         if (periodState.getStateCode().equals(PeriodState.CURRENT.getStateCode())) {
             // Deactivate the current
             ExecutionPeriod currentExecutionPeriod = ExecutionPeriod.readActualExecutionPeriod();
-            currentExecutionPeriod.setState(new PeriodState(PeriodState.OPEN));
+            currentExecutionPeriod.setState(PeriodState.OPEN);
         }
 
         executionPeriod.setState(periodState);
