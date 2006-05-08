787ff19c24d9501842867958fca303ee001f29e2
Shezad Anavarali <shezad@ist.utl.pt>
2006-05-08 13:08:26 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/NonAffiliatedTeacher.java
+++ src/net/sourceforge/fenixedu/domain/NonAffiliatedTeacher.java
@@ -6,28 +6,34 @@ package net.sourceforge.fenixedu.domain;
 
 import java.util.HashSet;
 import java.util.Set;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author Ricardo Rodrigues
- *
+ * 
  */
 
 public class NonAffiliatedTeacher extends NonAffiliatedTeacher_Base {
 
-	public NonAffiliatedTeacher() {
-		super();
-		setRootDomainObject(RootDomainObject.getInstance());
-	}
+    public NonAffiliatedTeacher() {
+        super();
+        setRootDomainObject(RootDomainObject.getInstance());
+    }
 
-	public static Set<NonAffiliatedTeacher> findNonAffiliatedTeacherByName(final String name) {
-		final Set<NonAffiliatedTeacher> nonAffiliatedTeachers = new HashSet<NonAffiliatedTeacher>();
-		for (final NonAffiliatedTeacher nonAffiliatedTeacher : RootDomainObject.getInstance().getNonAffiliatedTeachersSet()) {
-			if (nonAffiliatedTeacher.getName().equalsIgnoreCase(name)) {
-				nonAffiliatedTeachers.add(nonAffiliatedTeacher);
-			}
-		}
-		return nonAffiliatedTeachers;
-	}
+    public static Set<NonAffiliatedTeacher> findNonAffiliatedTeacherByName(final String name) {
+
+        Pattern pattern = Pattern.compile(name.toLowerCase());
+
+        final Set<NonAffiliatedTeacher> nonAffiliatedTeachers = new HashSet<NonAffiliatedTeacher>();
+        for (final NonAffiliatedTeacher nonAffiliatedTeacher : RootDomainObject.getInstance()
+                .getNonAffiliatedTeachersSet()) {
+            Matcher matcher = pattern.matcher(nonAffiliatedTeacher.getName().toLowerCase());
+            if (matcher.find()) {
+                nonAffiliatedTeachers.add(nonAffiliatedTeacher);
+            }
+        }
+        return nonAffiliatedTeachers;
+    }
 
 }
