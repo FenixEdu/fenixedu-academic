607e3f2c3065c8b3e7f09b24f0352f7abd37eff3
Shezad Anavarali <shezad@ist.utl.pt>
2006-05-16 18:15:06 +0000

    Tools to help the OJB FieldDescriptors removal feature

---
+++ src/net/sourceforge/fenixedu/tools/CheckAndFixDMLDateTypes.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ */
+package net.sourceforge.fenixedu.tools;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import net.sourceforge.fenixedu.util.StringFormatter;
+
+import org.apache.ojb.broker.metadata.ClassDescriptor;
+import org.apache.ojb.broker.metadata.FieldDescriptor;
+import org.apache.ojb.broker.metadata.MetadataManager;
+import org.joda.time.chrono.AssembledChronology.Fields;
+
+/**
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ * 
+ */
+public class CheckAndFixDMLDateTypes {
+
+    /**
+     * @param args
+     * @throws FileNotFoundException
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Map ojbMetadata = MetadataManager.getInstance().getGlobalRepository().getDescriptorTable();
+
+        Formatter newFile = new Formatter("new_domain_model.dml");
+        Set<String> alterCommands = new HashSet<String>();
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(args[0]));
+
+            boolean insideClass = false;
+            ClassDescriptor classDescriptor = null;
+            String dmlFileLine;
+            while ((dmlFileLine = in.readLine()) != null) {
+                String dmlFileLineToAppend = dmlFileLine;
+                dmlFileLine = dmlFileLine.trim();
+                if (dmlFileLine.startsWith("class")) {
+                    insideClass = true;
+                    String className = dmlFileLine.split(" ")[1].replace(";", "");
+                    classDescriptor = (ClassDescriptor) ojbMetadata.get(className);
+
+                } else if (insideClass) {
+                    if (dmlFileLine.contains("}")) {
+                        insideClass = false;
+                        classDescriptor = null;
+                    } else if (classDescriptor != null) {
+                        String[] lineSplitted = dmlFileLine.split(" ");
+                        if (lineSplitted.length > 1) {
+                            String fieldName = lineSplitted[1].replace(";", "");
+                            FieldDescriptor fieldDescriptor = classDescriptor
+                                    .getFieldDescriptorByName(fieldName);
+                            if (fieldDescriptor != null) {
+                                dmlFileLineToAppend = buildNewLineToAppend(dmlFileLineToAppend,
+                                        fieldName, fieldDescriptor, alterCommands);
+                            } else {
+                                boolean foundInExtentClass = false;
+                                for (String extentClassName : (Vector<String>) classDescriptor
+                                        .getExtentClassNames()) {
+                                    ClassDescriptor extentClassDescriptor = (ClassDescriptor) ojbMetadata
+                                            .get(extentClassName);
+                                    if (extentClassDescriptor != null) {
+                                        fieldDescriptor = extentClassDescriptor
+                                                .getFieldDescriptorByName(fieldName);
+                                        if (fieldDescriptor != null) {
+                                            dmlFileLineToAppend = buildNewLineToAppend(
+                                                    dmlFileLineToAppend, fieldName, fieldDescriptor,
+                                                    alterCommands);
+                                            foundInExtentClass = true;
+                                            break;
+                                        }
+                                    } else {
+                                        System.out.println("WTF??? " + extentClassName);
+                                    }
+                                }
+                                if (!foundInExtentClass) {
+                                    System.out
+                                            .println("ERROR: " + classDescriptor.getClassNameOfObject()
+                                                    + " - " + fieldName);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                newFile.format(dmlFileLineToAppend);
+                newFile.format("\n");
+            }
+            in.close();
+        } catch (IOException e) {
+            newFile.close();
+        }
+
+        newFile.flush();
+        newFile.close();
+
+        Formatter renameColumnsFile = new Formatter("renameDateColumns.sql");
+        for (String command : alterCommands) {
+            renameColumnsFile.format("%s\n", command);
+        }
+        renameColumnsFile.flush();
+        renameColumnsFile.close();
+
+    }
+
+    private static String buildNewLineToAppend(String dmlFileLineToAppend, String fieldName,
+            FieldDescriptor fieldDescriptor, Set<String> alterCommands) {
+        if (fieldDescriptor.getColumnType().equals("TIME")) {
+            dmlFileLineToAppend = "\tHourMinuteSecond " + fieldName + "HourMinuteSecond;";
+            buildColumnRenamer(alterCommands, fieldDescriptor, "_HOUR_MINUTE_SECOND", fieldName);
+        } else if (fieldDescriptor.getColumnType().equals("DATE")) {
+            dmlFileLineToAppend = "\tYearMonthDay " + fieldName + "YearMonthDay;";
+            buildColumnRenamer(alterCommands, fieldDescriptor, "_YEAR_MONTH_DAY", fieldName);
+        } else if (fieldDescriptor.getColumnType().equals("TIMESTAMP")) {
+            dmlFileLineToAppend = "\tDateTime " + fieldName + "DateTime;";
+            buildColumnRenamer(alterCommands, fieldDescriptor, "_DATE_TIME", fieldName);
+        }
+        return dmlFileLineToAppend;
+    }
+
+    private static void buildColumnRenamer(Set<String> alterCommands, FieldDescriptor descriptor,
+            String columnNameSuffix, String fieldName) {
+
+        ClassDescriptor classDescriptor = descriptor.getClassDescriptor();
+        if (classDescriptor.getFullTableName() != null) {
+            String renamedFieldName = StringFormatter.convertToDBStyle(fieldName);
+            alterCommands.add("alter table "
+                    + classDescriptor.getFullTableName()
+                    + " change column "
+                    + renamedFieldName
+                    + " "
+                    + renamedFieldName
+                    + columnNameSuffix
+                    + " "
+                    + DBColumnInfo.getInfoWithType(classDescriptor.getFullTableName(), renamedFieldName,
+                            true) + ";");
+        }
+    }
+
+}
---
+++ src/net/sourceforge/fenixedu/tools/FixColumnsNames.java
@@ -0,0 +1,140 @@
+/**
+ * 
+ */
+package net.sourceforge.fenixedu.tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import net.sourceforge.fenixedu.util.StringFormatter;
+
+import org.apache.ojb.broker.metadata.ClassDescriptor;
+import org.apache.ojb.broker.metadata.FieldDescriptor;
+import org.apache.ojb.broker.metadata.MetadataManager;
+
+/**
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ * 
+ */
+public class FixColumnsNames {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Map ojbMetadata = MetadataManager.getInstance().getGlobalRepository().getDescriptorTable();
+
+        checkColumnsNames(ojbMetadata);
+
+    }
+
+    private static void checkColumnsNames(Map ojbMetadata) {
+
+        try {
+            final Connection connection = getConnection("localhost", "ciapl", "root", "");
+
+            Set<String> alterCommands = new HashSet<String>();
+            int invalidColumnNames = 0;
+
+            for (ClassDescriptor classDescriptor : (Collection<ClassDescriptor>) ojbMetadata.values()) {
+
+                if (!classDescriptor.getClassNameOfObject().startsWith("org.apache.ojb")
+                        && classDescriptor.getFieldDescriptions() != null
+                        && classDescriptor.getFullTableName() != null) {
+
+                    final Statement statement = connection.createStatement();
+                    final ResultSet resultSet;
+                    try {
+                        resultSet = statement.executeQuery("show create table "
+                                + classDescriptor.getFullTableName());
+                    } catch (SQLException e) {
+                        continue;
+                    }
+
+                    resultSet.next();
+
+                    final Map<String, String> tableColumns = new HashMap<String, String>();
+                    final String[] tableInfo = resultSet.getString(2).split("\n");
+                    for (String tableInfoLine : tableInfo) {
+                        tableInfoLine = tableInfoLine.trim();
+                        if (!tableInfoLine.startsWith("`")) {
+                            continue;
+                        }
+                        String[] tableInfoLineSplitted = tableInfoLine.split(" ");
+                        if (tableInfoLineSplitted.length > 1) {
+                            tableInfoLine = tableInfoLine.substring(0, tableInfoLine.length() - 1);
+                            tableColumns.put(tableInfoLineSplitted[0].toUpperCase(), tableInfoLine.replaceFirst(
+                                    tableInfoLineSplitted[0], ""));
+                        }
+                    }
+
+                    for (FieldDescriptor descriptor : classDescriptor.getFieldDescriptions()) {
+                        String name = descriptor.getAttributeName();
+                        String convertedName = StringFormatter.convertToDBStyle(name);
+                        String columnName = descriptor.getColumnName();
+
+                        if (!columnName.equals(convertedName)) {
+
+                            String columnMetadata = tableColumns.get("`" + columnName.toUpperCase() + "`");
+
+                            alterCommands.add("alter table " + classDescriptor.getFullTableName()
+                                    + " change column " + columnName + " " + convertedName + " "
+                                    + columnMetadata + ";");
+
+                            // System.out.println("CLASS: " +
+                            // classDescriptor.getClassNameOfObject()
+                            // + " - " + columnName + " -> " + convertedName + "
+                            // :: "
+                            // + columnMetadata);
+                            invalidColumnNames++;
+                        }
+                    }
+
+                }
+
+            }
+
+            System.out.println("\nInvalid Names: " + invalidColumnNames);
+            
+            Formatter resultFile = new Formatter("renameColumns.sql");
+            for (String command : alterCommands) {
+                resultFile.format("%s\n", command);
+            }
+            resultFile.flush();
+            resultFile.close();            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Connection getConnection(final String host, final String db, final String user,
+            final String pass) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        final String connectionString = getConnectionString(host, db);
+
+        return DriverManager.getConnection(connectionString, user, pass);
+    }
+
+    private static String getConnectionString(final String host, final String db) {
+        final StringBuilder url = new StringBuilder();
+        url.append("jdbc:mysql://");
+        url.append(host);
+        url.append("/");
+        url.append(db);
+        return url.toString();
+    }
+
+}
