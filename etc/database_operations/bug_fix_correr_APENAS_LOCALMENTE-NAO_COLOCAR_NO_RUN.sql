b97c0ccddf80b24620b673a032a648d292d249b7
Luis Cruz <luis.cruz@ist.utl.pt>
2006-05-22 10:48:14 +0000

    Deployed HEAD.

--- etc/database_operations/bug_fix_correr_APENAS_LOCALMENTE-NAO_COLOCAR_NO_RUN.sql
+++
@@ -1,3 +0,0 @@
-alter table EVALUATION change column BEGIN_DATE_TIME PROJECT_BEGIN_DATE_TIME  timestamp NOT NULL default '0000-00-00 00:00:00';
-alter table EVALUATION change column END_DATE_TIME PROJECT_END_DATE_TIME  timestamp NOT NULL default '0000-00-00 00:00:00';
-alter table CURRICULUM_MODULE change column CONDITION ENROLMENT_CONDITION  varchar(255) NOT NULL default '';
