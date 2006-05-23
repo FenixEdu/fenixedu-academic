0b568a00507422d50be942d7246b38878a79f7ef
João Florindo <jdnf@linkare.pt>
2006-05-23 12:30:20 +0000

    provider used by marksheet for teacher management

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/gradeSubmission/CurricularCoursesByExecutionPeriodProvider.java
@@ -0,0 +1,34 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers.gradeSubmission;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import net.sourceforge.fenixedu.dataTransferObject.teacher.gradeSubmission.MarkSheetTeacherGradeSubmissionBean;
+import net.sourceforge.fenixedu.domain.CurricularCourse;
+import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+import net.sourceforge.fenixedu.renderers.DataProvider;
+import net.sourceforge.fenixedu.renderers.components.converters.Converter;
+
+public class CurricularCoursesByExecutionPeriodProvider implements DataProvider {
+
+    public Object provide(Object source) {
+
+        final MarkSheetTeacherGradeSubmissionBean submissionBean = (MarkSheetTeacherGradeSubmissionBean) source;
+        final List<CurricularCourse> result = (submissionBean.getExecutionCourse() != null) ? submissionBean
+                .getExecutionCourse().getCurricularCoursesWithDegreeType()
+                : Collections.EMPTY_LIST;
+        
+        Collections.sort(result, new Comparator<CurricularCourse>() {
+            public int compare(CurricularCourse o1, CurricularCourse o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        return result;
+    }
+
+    public Converter getConverter() {
+        return new DomainObjectKeyConverter();
+    }
+
+}
