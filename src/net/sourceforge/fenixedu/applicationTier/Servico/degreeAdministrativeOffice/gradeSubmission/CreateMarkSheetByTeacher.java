336b9aeb11759745fc61bf3f9d8f25606632921d
João Florindo <jdnf@linkare.pt>
2006-05-23 12:28:36 +0000

    create several marksheets according to selected curricularcourses and enrolment evaluation type students

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/degreeAdministrativeOffice/gradeSubmission/CreateMarkSheetByTeacher.java
@@ -0,0 +1,170 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.degreeAdministrativeOffice.gradeSubmission;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.degreeAdministrativeOffice.gradeSubmission.MarkSheetEnrolmentEvaluationBean;
+import net.sourceforge.fenixedu.dataTransferObject.teacher.gradeSubmission.MarkSheetTeacherGradeSubmissionBean;
+import net.sourceforge.fenixedu.dataTransferObject.teacher.gradeSubmission.MarkSheetTeacherMarkBean;
+import net.sourceforge.fenixedu.domain.Attends;
+import net.sourceforge.fenixedu.domain.CurricularCourse;
+import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.domain.FinalMark;
+import net.sourceforge.fenixedu.domain.MarkSheetType;
+import net.sourceforge.fenixedu.domain.Teacher;
+
+import org.joda.time.YearMonthDay;
+
+public class CreateMarkSheetByTeacher extends Service {
+
+    public void run(MarkSheetTeacherGradeSubmissionBean submissionBean)
+            throws InvalidArgumentsServiceException {
+
+        ExecutionCourse executionCourse = submissionBean.getExecutionCourse();
+        Teacher teacher = submissionBean.getResponsibleTeacher();
+
+        checkIfTeacherLecturesExecutionCourse(teacher, executionCourse);
+
+        Map<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> 
+            markSheetsInformation = new HashMap<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>>();
+
+        createMarkSheetEnrolmentEvaluationBeans(submissionBean, executionCourse, markSheetsInformation);
+        createMarkSheets(markSheetsInformation, executionCourse, teacher, submissionBean
+                .getEvaluationDate());
+    }
+
+    private void createMarkSheetEnrolmentEvaluationBeans(
+            MarkSheetTeacherGradeSubmissionBean submissionBean,
+            ExecutionCourse executionCourse,
+            Map<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> markSheetsInformation)
+            throws InvalidArgumentsServiceException {
+
+        Date nowDate = new Date();
+        for (MarkSheetTeacherMarkBean markBean : submissionBean.getSelectedMarksToSubmit()) {
+            Attends attends = markBean.getAttends();
+            Enrolment enrolment = attends.getEnrolment();
+            CurricularCourse curricularCourse = enrolment.getCurricularCourse();
+            String grade = getGrade(attends, markBean, markBean.getEvaluationDate(), nowDate);
+
+            MarkSheetEnrolmentEvaluationBean evaluationBean = new MarkSheetEnrolmentEvaluationBean(
+                    enrolment, markBean.getEvaluationDate(), grade);
+            addMarkSheetEvaluationBeanToMap(markSheetsInformation, curricularCourse, executionCourse,
+                    evaluationBean);
+        }
+    }
+
+    private void createMarkSheets(
+            Map<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> markSheetsInformation,
+            ExecutionCourse executionCourse, Teacher responsibleTeacher, Date evaluationDate) {
+
+        for (Entry<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> curricularCourseEntry : markSheetsInformation
+                .entrySet()) {
+
+            CurricularCourse curricularCourse = curricularCourseEntry.getKey();
+            for (Entry<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>> markSheetTypeEntry : curricularCourseEntry
+                    .getValue().entrySet()) {
+
+                MarkSheetType markSheetType = markSheetTypeEntry.getKey();
+                Collection<MarkSheetEnrolmentEvaluationBean> markSheetEnrolmentEvaluationBeans = markSheetTypeEntry
+                        .getValue();
+
+                if (markSheetEnrolmentEvaluationBeans != null) {
+                    curricularCourse.createMarkSheet(executionCourse.getExecutionPeriod(),
+                            responsibleTeacher, evaluationDate, markSheetType, Boolean.TRUE,
+                            markSheetEnrolmentEvaluationBeans);
+                }
+            }
+        }
+    }
+
+    private void checkIfTeacherLecturesExecutionCourse(Teacher teacher, ExecutionCourse executionCourse)
+            throws InvalidArgumentsServiceException {
+        if (!teacher.hasProfessorshipForExecutionCourse(executionCourse)) {
+            throw new InvalidArgumentsServiceException("error.teacher.doesnot.lectures.executionCourse");
+        }
+    }
+
+    private void addMarkSheetEvaluationBeanToMap(
+            Map<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> markSheetsInformation,
+            CurricularCourse curricularCourse, ExecutionCourse executionCourse,
+            MarkSheetEnrolmentEvaluationBean markSheetEvaluationBean)
+            throws InvalidArgumentsServiceException {
+
+        Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>> evaluationBeansForMarkSheetType = getEvaluationBeansForMarkSheetType(
+                markSheetsInformation, curricularCourse);
+
+        MarkSheetType markSheetType = findMarkSheetType(executionCourse, markSheetEvaluationBean
+                .getEnrolment());
+        Collection<MarkSheetEnrolmentEvaluationBean> evaluationBeans = getEvaluationBeans(
+                evaluationBeansForMarkSheetType, markSheetType);
+
+        evaluationBeans.add(markSheetEvaluationBean);
+    }
+
+    private Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>> getEvaluationBeansForMarkSheetType(
+            Map<CurricularCourse, Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>> markSheetsInformation,
+            CurricularCourse curricularCourse) {
+        Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>> evaluationBeansForMarkSheetType = markSheetsInformation
+                .get(curricularCourse);
+        if (evaluationBeansForMarkSheetType == null) {
+            evaluationBeansForMarkSheetType = new HashMap<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>>();
+            markSheetsInformation.put(curricularCourse, evaluationBeansForMarkSheetType);
+        }
+        return evaluationBeansForMarkSheetType;
+    }
+
+    private Collection<MarkSheetEnrolmentEvaluationBean> getEvaluationBeans(
+            Map<MarkSheetType, Collection<MarkSheetEnrolmentEvaluationBean>> evaluationBeansForMarkSheetType,
+            MarkSheetType markSheetType) {
+        Collection<MarkSheetEnrolmentEvaluationBean> evaluationBeans = evaluationBeansForMarkSheetType
+                .get(markSheetType);
+        if (evaluationBeans == null) {
+            evaluationBeans = new ArrayList<MarkSheetEnrolmentEvaluationBean>();
+            evaluationBeansForMarkSheetType.put(markSheetType, evaluationBeans);
+        }
+        return evaluationBeans;
+    }
+
+    private MarkSheetType findMarkSheetType(ExecutionCourse executionCourse, Enrolment enrolment)
+            throws InvalidArgumentsServiceException {
+
+        if (enrolment.isImprovementForExecutionCourse(executionCourse)) {
+            return MarkSheetType.IMPROVEMENT;
+
+        } else {
+            switch (enrolment.getEnrolmentEvaluationType()) {
+            case NORMAL:
+                return MarkSheetType.NORMAL;
+            case IMPROVEMENT:
+                return MarkSheetType.IMPROVEMENT;
+            case SPECIAL_SEASON:
+                return MarkSheetType.SPECIAL_SEASON;
+            default:
+                throw new InvalidArgumentsServiceException(
+                        "error.markSheetType.invalid.enrolmentEvaluationType");
+            }
+        }
+    }
+
+    private String getGrade(Attends attends, MarkSheetTeacherMarkBean markBean, Date evaluationDate,
+            Date nowDate) {
+        String grade = "NA";
+
+        FinalMark finalMark = attends.getFinalMark();
+        if (finalMark != null) {
+            finalMark.setSubmitedMark(markBean.getGrade());
+            finalMark.setSubmitDateYearMonthDay(YearMonthDay.fromDateFields(evaluationDate));
+            finalMark.setWhenSubmitedYearMonthDay(YearMonthDay.fromDateFields(nowDate));
+            grade = markBean.getGrade();
+        }
+        return grade;
+    }
+
+}
