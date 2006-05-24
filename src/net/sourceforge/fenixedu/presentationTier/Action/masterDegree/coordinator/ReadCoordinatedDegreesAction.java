7a6e2570f7792d7b2e2b4f145c9c513637887699
Luís Egídio <luis.egidio@ist.utl.pt>
2006-05-24 08:14:22 +0000

    cleaned up code

--- src/net/sourceforge/fenixedu/presentationTier/Action/masterDegree/coordinator/ReadCoordinatedDegreesAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/masterDegree/coordinator/ReadCoordinatedDegreesAction.java
@@ -1,17 +1,3 @@
-/**
- * 
- * Created on 27 of March de 2003
- * 
- * 
- * Autores : - Nuno Nunes (nmsn@rnl.ist.utl.pt) - Joana Mota
- * (jccm@rnl.ist.utl.pt)
- * 
- * modified by Fernanda Quitério
- * 
- * @author Francisco Paulo 27/Out/04 frnp@mega.ist.utl.pt (edit)
- *  
- */
-
 package net.sourceforge.fenixedu.presentationTier.Action.masterDegree.coordinator;
 
 import java.util.List;
@@ -22,6 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.InfoDegreeCurricularPlan;
 import net.sourceforge.fenixedu.framework.factory.ServiceManagerServiceFactory;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixAction;
 import net.sourceforge.fenixedu.presentationTier.Action.exceptions.FenixActionException;
@@ -32,40 +19,26 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class ReadCoordinatedDegreesAction extends
-		FenixAction {
+public class ReadCoordinatedDegreesAction extends FenixAction {
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-
+	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession(false);
-		if (session != null) {
+
+        if (session != null) {
 			IUserView userView = SessionUtils.getUserView(request);
 
-			Object args[] = new Object[1];
-			args[0] = userView;
-			List degrees = null;
 			try {
-				degrees = (List) ServiceManagerServiceFactory.executeService(
-						userView, "ReadCoordinatedDegrees", args);
-
+                Object args[] = { userView };
+                List<InfoDegreeCurricularPlan> degrees = (List<InfoDegreeCurricularPlan>) ServiceManagerServiceFactory.executeService(userView, "ReadCoordinatedDegrees", args);
+                session.setAttribute(SessionConstants.MASTER_DEGREE_LIST, degrees);
 			} catch (FenixServiceException e) {
 				throw new FenixActionException(e);
 			}
-
 			
-			//sets in the session a list containing the master degree curricular plans
-			session.setAttribute(SessionConstants.MASTER_DEGREE_LIST, degrees);
-
-//			if (degrees.size() == 1) {
-//				request.setAttribute("degree", "0");
-//				return mapping.findForward("oneDegreeSucces");
-//			}
-
 			return mapping.findForward("ChooseDegree");
-		}
-		throw new Exception();
+		} else {
+            throw new FenixActionException();    
+        }
 	}
 
 }
