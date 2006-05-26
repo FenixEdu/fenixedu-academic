9735866b2867b2cead1c19d8ae4c0e629ad8faea
Luís Egídio <luis.egidio@ist.utl.pt>
2006-05-26 07:59:44 +0000

    getting isBolonha attribute from domain

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoExecutionDegree.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoExecutionDegree.java
@@ -1,8 +1,3 @@
-/*
- * InfoExecutionDegree.java
- * 
- * Created on 24 de Novembro de 2002, 23:05
- */
 package net.sourceforge.fenixedu.dataTransferObject;
 
 import java.util.ArrayList;
@@ -16,24 +11,18 @@ import org.apache.commons.collections.Predicate;
 import org.apache.struts.util.LabelValueBean;
 import org.apache.struts.util.MessageResources;
 
-/**
- * @author tfc130
- */
 public class InfoExecutionDegree extends InfoObject {
 
     private InfoExecutionYear infoExecutionYear;
 
     private InfoDegreeCurricularPlan infoDegreeCurricularPlan;
 
-    //added by Tânia Pousão
     private List coordinatorsList;
 
     private Boolean temporaryExamMap;
 
-    //added by Tânia Pousão
     private InfoCampus infoCampus;
 
-    // added by amsg 4 Jun 2004
     private InfoPeriod infoPeriodLessonsFirstSemester;
 
     private InfoPeriod infoPeriodExamsFirstSemester;
@@ -41,124 +30,55 @@ public class InfoExecutionDegree extends InfoObject {
     private InfoPeriod infoPeriodLessonsSecondSemester;
 
     private InfoPeriod infoPeriodExamsSecondSemester;
-    
-    // added by gedl 26 Sept 2005
+
     private String qualifiedName;
 
+    public boolean bolonha;
+    
     public InfoExecutionDegree() {
     }
 
-    /**
-     * @param infoDegreeCurricularPlan
-     * @param infoExecutionYear
-     */
-    public InfoExecutionDegree(InfoDegreeCurricularPlan infoDegreeCurricularPlan,
-            InfoExecutionYear infoExecutionYear) {
+    public InfoExecutionDegree(InfoDegreeCurricularPlan infoDegreeCurricularPlan, InfoExecutionYear infoExecutionYear) {
         setInfoDegreeCurricularPlan(infoDegreeCurricularPlan);
         setInfoExecutionYear(infoExecutionYear);
     }
 
-    public boolean equals(Object obj) {
-        boolean result = false;
-        if (obj instanceof InfoExecutionDegree) {
-            InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) obj;
-            result = getIdInternal().equals(infoExecutionDegree.getIdInternal());
-        }
-        return result;
-    }
-
-    public String toString() {
-        String result = "[INFOEXECUTIONDEGREE";
-        result += ", infoExecutionYear=" + infoExecutionYear;
-        result += ", infoDegreeCurricularPlan=" + infoDegreeCurricularPlan;
-        if (coordinatorsList != null) {
-            result += ", coordinatorsList=" + coordinatorsList.size();
-        } else {
-            result += ", coordinatorsList is NULL";
-        }
-        result += ", infoCampus= " + infoCampus;
-        result += "]";
-        return result;
-    }
-
-    /**
-     * Returns the infoExecutionYear.
-     * 
-     * @return InfoExecutionYear
-     */
     public InfoExecutionYear getInfoExecutionYear() {
         return infoExecutionYear;
     }
 
-    /**
-     * Sets the infoExecutionYear.
-     * 
-     * @param infoExecutionYear
-     *            The infoExecutionYear to set
-     */
     public void setInfoExecutionYear(InfoExecutionYear infoExecutionYear) {
         this.infoExecutionYear = infoExecutionYear;
     }
 
-    /**
-     * Returns the infoDegreeCurricularPlan.
-     * 
-     * @return InfoDegreeCurricularPlan
-     */
     public InfoDegreeCurricularPlan getInfoDegreeCurricularPlan() {
         return infoDegreeCurricularPlan;
     }
 
-    /**
-     * Sets the infoDegreeCurricularPlan.
-     * 
-     * @param infoDegreeCurricularPlan
-     *            The infoDegreeCurricularPlan to set
-     */
     public void setInfoDegreeCurricularPlan(InfoDegreeCurricularPlan infoDegreeCurricularPlan) {
         this.infoDegreeCurricularPlan = infoDegreeCurricularPlan;
     }
 
-    /**
-     * @return
-     */
     public Boolean getTemporaryExamMap() {
         return temporaryExamMap;
     }
 
-    /**
-     * @param boolean1
-     */
     public void setTemporaryExamMap(Boolean temporary) {
         temporaryExamMap = temporary;
     }
 
-    /**
-     * @return Returns the infoCampus.
-     */
     public InfoCampus getInfoCampus() {
         return infoCampus;
     }
 
-    /**
-     * @param infoCampus
-     *            The infoCampus to set.
-     */
     public void setInfoCampus(InfoCampus infoCampus) {
         this.infoCampus = infoCampus;
     }
 
-    /**
-     * @return Returns the coordinatorList.
-     */
     public List getCoordinatorsList() {
         return coordinatorsList;
     }
 
-    /**
-     * @param coordinatorList
-     *            The coordinatorList to set.
-     */
     public void setCoordinatorsList(List coordinatorsList) {
         this.coordinatorsList = coordinatorsList;
     }
@@ -193,6 +113,45 @@ public class InfoExecutionDegree extends InfoObject {
 
     public void setInfoPeriodLessonsSecondSemester(InfoPeriod infoPeriodLessonsSecondSemester) {
         this.infoPeriodLessonsSecondSemester = infoPeriodLessonsSecondSemester;
+    }
+    
+    public String getQualifiedName() {
+        return qualifiedName;
+    }
+
+    public void setQualifiedName(String qualifiedName) {
+        this.qualifiedName = qualifiedName;
+    }
+
+    public boolean isBolonha() {
+        return bolonha;
+    }
+
+    public void setBolonha(boolean bolonha) {
+        this.bolonha = bolonha;
+    }
+
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if (obj instanceof InfoExecutionDegree) {
+            InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) obj;
+            result = getIdInternal().equals(infoExecutionDegree.getIdInternal());
+        }
+        return result;
+    }
+
+    public String toString() {
+        String result = "[INFOEXECUTIONDEGREE";
+        result += ", infoExecutionYear=" + infoExecutionYear;
+        result += ", infoDegreeCurricularPlan=" + infoDegreeCurricularPlan;
+        if (coordinatorsList != null) {
+            result += ", coordinatorsList=" + coordinatorsList.size();
+        } else {
+            result += ", coordinatorsList is NULL";
+        }
+        result += ", infoCampus= " + infoCampus;
+        result += "]";
+        return result;
     }
 
     public static List buildLabelValueBeansForList(List executionDegrees,
@@ -250,17 +209,6 @@ public class InfoExecutionDegree extends InfoObject {
 
     }
 
-    public void copyFromDomain(ExecutionDegree executionDegree) {
-        super.copyFromDomain(executionDegree);
-        if (executionDegree != null) {
-            setTemporaryExamMap(executionDegree.getTemporaryExamMap());
-        }
-    }
-
-    /**
-     * @param executionDegree
-     * @return
-     */
     public static InfoExecutionDegree newInfoFromDomain(ExecutionDegree executionDegree) {
         InfoExecutionDegree infoExecutionDegree = null;
         if (executionDegree != null) {
@@ -270,19 +218,12 @@ public class InfoExecutionDegree extends InfoObject {
         return infoExecutionDegree;
     }
 
-    public void copyToDomain(InfoExecutionDegree infoExecutionDegree, ExecutionDegree executionDegree) {
-        super.copyToDomain(infoExecutionDegree, executionDegree);
-        executionDegree.setTemporaryExamMap(infoExecutionDegree.getTemporaryExamMap());
+    public void copyFromDomain(ExecutionDegree executionDegree) {
+        super.copyFromDomain(executionDegree);
+        if (executionDegree != null) {
+            setBolonha(executionDegree.isBolonha());
+            setTemporaryExamMap(executionDegree.getTemporaryExamMap());
+        }
     }
-
-	public String getQualifiedName()
-	{
-		return qualifiedName;
-	}
-
-	public void setQualifiedName(String qualifiedName)
-	{
-		this.qualifiedName = qualifiedName;
-	}
 
 }
