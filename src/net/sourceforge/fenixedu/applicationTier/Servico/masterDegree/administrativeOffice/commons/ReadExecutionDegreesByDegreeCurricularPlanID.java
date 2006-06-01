03a60f9010fc4074644b0d7cc1595b3954e92975
Luís Egídio <luis.egidio@ist.utl.pt>
2006-06-01 12:50:21 +0000

    refactored

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/commons/ReadExecutionDegreesByDegreeCurricularPlanID.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/commons/ReadExecutionDegreesByDegreeCurricularPlanID.java
@@ -5,10 +5,10 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.commons;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegree;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionDegreeWithInfoExecutionYear;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.ExecutionDegree;
@@ -18,24 +18,21 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
  * @author Shezad Anavarali (sana@mega.ist.utl.pt)
  * @author Nadir Tarmahomed (naat@mega.ist.utl.pt)
  * 
- * @return List containing all InfoExecutionDegrees, corresponding to Degree
- *         Curricular Plan
+ * @return List containing all InfoExecutionDegrees, corresponding to Degree Curricular Plan
+ * 
  */
 public class ReadExecutionDegreesByDegreeCurricularPlanID extends Service {
 
-    public List run(Integer degreeCurricularPlanID) throws ExcepcaoPersistencia {
-        DegreeCurricularPlan degreeCurricularPlan = rootDomainObject.readDegreeCurricularPlanByOID(
-                        degreeCurricularPlanID);
+    public List<InfoExecutionDegree> run(Integer degreeCurricularPlanID) throws ExcepcaoPersistencia {
+        DegreeCurricularPlan degreeCurricularPlan = rootDomainObject.readDegreeCurricularPlanByOID(degreeCurricularPlanID);
 
-        List infoExecutionDegreeList = new ArrayList();
+        List<InfoExecutionDegree> result = new ArrayList<InfoExecutionDegree>();
 
-        for (Iterator iter = degreeCurricularPlan.getExecutionDegrees().iterator(); iter.hasNext();) {
-            ExecutionDegree executionDegree = (ExecutionDegree) iter.next();
-            infoExecutionDegreeList.add(InfoExecutionDegreeWithInfoExecutionYear
-                    .newInfoFromDomain(executionDegree));
+        for (final ExecutionDegree executionDegree : degreeCurricularPlan.getExecutionDegrees()) {
+            result.add(InfoExecutionDegreeWithInfoExecutionYear.newInfoFromDomain(executionDegree));
         }
 
-        return infoExecutionDegreeList;
-
+        return result;
     }
+    
 }
