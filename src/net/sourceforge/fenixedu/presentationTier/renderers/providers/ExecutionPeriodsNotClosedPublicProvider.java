a77e58a35fd99af7a4c483ef65e5fb5fabfbb687
João Florindo <jdnf@linkare.pt>
2006-06-07 08:17:52 +0000

    filter executionPeriods from 2 Semestre 2005/2006

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
@@ -1,21 +1,34 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import org.apache.commons.collections.comparators.ReverseComparator;
 
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
 import net.sourceforge.fenixedu.renderers.DataProvider;
 import net.sourceforge.fenixedu.renderers.components.converters.Converter;
 
+import org.apache.commons.collections.comparators.ReverseComparator;
+
 public class ExecutionPeriodsNotClosedPublicProvider implements DataProvider {
 
     public Object provide(Object source) {
-        final List<ExecutionPeriod> executionPeriods = ExecutionPeriod.readNotClosedPublicExecutionPeriods();
-        Collections.sort(executionPeriods, new ReverseComparator());
-        return executionPeriods; 
+
+        /*
+         * get executionPeriods after '2 Semestre 2005/2006'
+         * 
+         */ 
+        final ExecutionPeriod currentExecutionPeriod = ExecutionPeriod.readByNameAndExecutionYear("2 Semestre", "2005/2006");
+
+        final List<ExecutionPeriod> result = new ArrayList<ExecutionPeriod>();
+        for (final ExecutionPeriod executionPeriod : ExecutionPeriod.readNotClosedPublicExecutionPeriods()) {
+            if (executionPeriod.isAfterOrEquals(currentExecutionPeriod)) {
+                result.add(executionPeriod);
+            }
+        }
+        Collections.sort(result, new ReverseComparator());
+        return result; 
     }
 
     public Converter getConverter() {
