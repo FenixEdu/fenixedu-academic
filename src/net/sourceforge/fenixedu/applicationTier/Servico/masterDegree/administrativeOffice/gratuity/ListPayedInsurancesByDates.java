b08b8d37b6cb32c7dea7c3f703ed9c25601367e7
Shezad Anavarali <shezad@ist.utl.pt>
2006-06-08 12:04:03 +0000

    initial version

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/ListPayedInsurancesByDates.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.gratuity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.transactions.InsuranceTransaction;
+import net.sourceforge.fenixedu.domain.transactions.Transaction;
+
+import org.joda.time.YearMonthDay;
+
+/**
+ * @author - Shezad Anavarali (shezad@ist.utl.pt)
+ * 
+ */
+public class ListPayedInsurancesByDates extends Service {
+
+    public List<InsuranceTransaction> run(ExecutionYear executionYear, YearMonthDay beginDate,
+            YearMonthDay endDate) {
+
+        List<InsuranceTransaction> insuranceTransactions = new ArrayList<InsuranceTransaction>();
+
+        List<Transaction> transactions = rootDomainObject.getTransactions();
+        for (Transaction transaction : transactions) {
+            if (transaction instanceof InsuranceTransaction) {
+                InsuranceTransaction insuranceTransaction = (InsuranceTransaction) transaction;
+
+                if (executionYear != null
+                        && !insuranceTransaction.getExecutionYear().equals(executionYear)) {
+                    continue;
+                }
+
+                if (beginDate != null
+                        && insuranceTransaction.getTransactionDateDateTime().isBefore(
+                                beginDate.toDateTimeAtMidnight())) {
+                    continue;
+                }
+
+                if (endDate != null
+                        && insuranceTransaction.getTransactionDateDateTime().isAfter(
+                                endDate.toDateTimeAtMidnight())) {
+                    continue;
+                }
+
+                insuranceTransactions.add(insuranceTransaction);
+            }
+        }
+
+        return insuranceTransactions;
+
+    }
+
+}
