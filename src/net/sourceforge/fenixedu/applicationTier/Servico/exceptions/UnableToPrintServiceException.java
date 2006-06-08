16b14b99f37891c9b17f94ed06d26c8c2c94790d
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-06-08 17:37:56 +0000

    exception thrown when is not possible to print

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/exceptions/UnableToPrintServiceException.java
@@ -0,0 +1,52 @@
+/*
+ * Created on 12/Mar/2003
+ *
+ * 
+ */
+package net.sourceforge.fenixedu.applicationTier.Servico.exceptions;
+
+/**
+ * @author João Mota
+ */
+public class UnableToPrintServiceException extends FenixServiceException {
+
+    /**
+     *  
+     */
+    public UnableToPrintServiceException() {
+
+    }
+
+    /**
+     * @param s
+     */
+    public UnableToPrintServiceException(String s) {
+        super(s);
+
+    }
+
+    /**
+     * @param cause
+     */
+    public UnableToPrintServiceException(Throwable cause) {
+        super(cause);
+
+    }
+
+    /**
+     * @param message
+     * @param cause
+     */
+    public UnableToPrintServiceException(String message, Throwable cause) {
+        super(message, cause);
+
+    }
+
+    public String toString() {
+        String result = "[UnableToPrintServiceException\n";
+        result += "message" + this.getMessage() + "\n";
+        result += "cause" + this.getCause() + "\n";
+        result += "]";
+        return result;
+    }
+}
