2331c789043f6e08c242b9162c3ffe1c6b8ff307
Luis Cruz <luis.cruz@ist.utl.pt>
2006-06-15 20:16:46 +0000

    Fix issue of deleteing publications that don't exist.

--- src/net/sourceforge/fenixedu/presentationTier/Action/publication/DeletePublicationAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publication/DeletePublicationAction.java
@@ -9,46 +9,38 @@ package net.sourceforge.fenixedu.presentationTier.Action.publication;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.domain.research.result.Publication;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionUtils;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+
 /**
  * @author Carlos Pereira & Francisco Passos
- *
+ * 
  */
 public class DeletePublicationAction extends FenixDispatchAction {
-	public ActionForward delete(
-			ActionMapping mapping,
-			ActionForm actionForm,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws Exception
-	{
-		IUserView userView = SessionUtils.getUserView(request);
-		Integer internalId = new Integer(request.getParameter("idInternal"));
 
-		Object[] args = { internalId };
-		
-		ServiceUtils.executeService(userView, "DeletePublication", args);
-		
-
-		request.setAttribute("msg", "message.publications.managementDeleted");
-
-		return mapping.findForward("deleted");
+    public ActionForward delete(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response)
+    		throws Exception {
+	final Publication publication = getPublication(request);
+	if (publication != null) {
+	    Object[] args = { publication.getIdInternal() };
+	    executeService(request, "DeletePublication", args);
+	    request.setAttribute("msg", "message.publications.managementDeleted");
 	}
-	
-	public ActionForward cancel(
-			ActionMapping mapping,
-			ActionForm actionForm,
-			HttpServletRequest request,
-			HttpServletResponse response)
-	throws Exception
-	{
-		return mapping.findForward("cancelled");
-	}
+	return mapping.findForward("deleted");
+    }
+
+    protected Publication getPublication(final HttpServletRequest request) {
+	final Integer publicationID = Integer.valueOf(request.getParameter("idInternal"));
+	return (Publication) rootDomainObject.readResultByOID(publicationID);
+    }
+
+    public ActionForward cancel(ActionMapping mapping, ActionForm actionForm,
+	    HttpServletRequest request, HttpServletResponse response) throws Exception {
+	return mapping.findForward("cancelled");
+    }
+
 }
