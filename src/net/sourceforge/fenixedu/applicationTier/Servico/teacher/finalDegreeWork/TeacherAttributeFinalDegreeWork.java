8d1c7b38d52d3c11b422fb5abaf81217058970ef
Luis Cruz <luis.cruz@ist.utl.pt>
2006-06-16 00:06:21 +0000

    Refactored code: made it a little easier to read.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/finalDegreeWork/TeacherAttributeFinalDegreeWork.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/finalDegreeWork/TeacherAttributeFinalDegreeWork.java
@@ -9,72 +9,64 @@ import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceE
 import net.sourceforge.fenixedu.domain.finalDegreeWork.Group;
 import net.sourceforge.fenixedu.domain.finalDegreeWork.GroupProposal;
 import net.sourceforge.fenixedu.domain.finalDegreeWork.Proposal;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 /**
  * @author Luis Cruz
  */
 public class TeacherAttributeFinalDegreeWork extends Service {
 
-	public TeacherAttributeFinalDegreeWork() {
-		super();
-	}
+    public Boolean run(Integer selectedGroupProposalOID) throws FenixServiceException {
+	final GroupProposal groupProposal = rootDomainObject.readGroupProposalByOID(selectedGroupProposalOID);
 
-	public Boolean run(Integer selectedGroupProposalOID) throws FenixServiceException,
-			ExcepcaoPersistencia {
-		GroupProposal groupProposal = rootDomainObject.readGroupProposalByOID(selectedGroupProposalOID);
-		if (groupProposal != null) {
-			Proposal proposal = groupProposal.getFinalDegreeWorkProposal();
-			Group group = groupProposal.getFinalDegreeDegreeWorkGroup();
-			if (proposal != null && group != null) {
-				Proposal proposalAttributedToGroup = group.getProposalAttributedByTeacher();
-				if (proposalAttributedToGroup != null
-						&& !proposalAttributedToGroup.getIdInternal().equals(proposal.getIdInternal())) {
-					throw new GroupAlreadyAttributed(proposalAttributedToGroup.getProposalNumber()
-							.toString());
-				}
+	if (groupProposal != null) {
+	    final Proposal proposal = groupProposal.getFinalDegreeWorkProposal();
+	    final Group group = groupProposal.getFinalDegreeDegreeWorkGroup();
 
-				if (proposal.getGroupAttributedByTeacher() == null
-						|| !proposal.getGroupAttributedByTeacher().equals(group)) {
-					proposal.setGroupAttributedByTeacher(group);
-					for (int i = 0; i < group.getGroupProposals().size(); i++) {
-						GroupProposal otherGroupProposal = group.getGroupProposals().get(i);
-						Proposal otherProposal = otherGroupProposal.getFinalDegreeWorkProposal();
-						if (!otherProposal.getIdInternal().equals(proposal.getIdInternal())
-								&& group.equals(otherProposal.getGroupAttributedByTeacher())) {
-							otherProposal.setGroupAttributedByTeacher(null);
-						}
-					}
-				} else {
-					proposal.setGroupAttributedByTeacher(null);
-				}
+	    if (proposal != null && group != null) {
+		final Proposal proposalAttributedToGroup = group.getProposalAttributedByTeacher();
+		if (proposalAttributedToGroup != null && proposalAttributedToGroup != proposal) {
+		    throw new GroupAlreadyAttributed(proposalAttributedToGroup.getProposalNumber().toString());
+		}
+
+		if (proposal.getGroupAttributedByTeacher() == null || proposal.getGroupAttributedByTeacher() != group) {
+		    proposal.setGroupAttributedByTeacher(group);
+		    for (int i = 0; i < group.getGroupProposals().size(); i++) {
+			GroupProposal otherGroupProposal = group.getGroupProposals().get(i);
+			Proposal otherProposal = otherGroupProposal.getFinalDegreeWorkProposal();
+			if (otherProposal != proposal && group == otherProposal.getGroupAttributedByTeacher()) {
+			    otherProposal.setGroupAttributedByTeacher(null);
 			}
+		    }
+		} else {
+		    proposal.setGroupAttributedByTeacher(null);
 		}
-		return new Boolean(true);
+	    }
+	}
+	return Boolean.TRUE;
+    }
+
+    public class GroupAlreadyAttributed extends FenixServiceException {
+
+	public GroupAlreadyAttributed() {
+	    super();
 	}
 
-	public class GroupAlreadyAttributed extends FenixServiceException {
-
-		public GroupAlreadyAttributed() {
-			super();
-		}
-
-		public GroupAlreadyAttributed(int errorType) {
-			super(errorType);
-		}
-
-		public GroupAlreadyAttributed(String s) {
-			super(s);
-		}
-
-		public GroupAlreadyAttributed(Throwable cause) {
-			super(cause);
-		}
-
-		public GroupAlreadyAttributed(String message, Throwable cause) {
-			super(message, cause);
-		}
-
+	public GroupAlreadyAttributed(int errorType) {
+	    super(errorType);
 	}
+
+	public GroupAlreadyAttributed(String s) {
+	    super(s);
+	}
+
+	public GroupAlreadyAttributed(Throwable cause) {
+	    super(cause);
+	}
+
+	public GroupAlreadyAttributed(String message, Throwable cause) {
+	    super(message, cause);
+	}
+
+    }
 
 }
