6c3c4c2a6970d61e40c6084b6096b2853a81332a
João Florindo <jdnf@linkare.pt>
2006-06-23 12:23:25 +0000

    - new ist acronyn label - created method in unitUtils to return institution unit

--- src/net/sourceforge/fenixedu/domain/organizationalStructure/UnitUtils.java
+++ src/net/sourceforge/fenixedu/domain/organizationalStructure/UnitUtils.java
@@ -8,21 +8,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
-
 import org.joda.time.YearMonthDay;
 
 public class UnitUtils {
 
     public static final String IST_UNIT_NAME = getLabel("ist.unit.name");
+    public static final String IST_UNIT_ACRONYM = getLabel("ist.unit.acronym");
 
     public static final String EXTERNAL_INSTITUTION_UNIT_NAME = getLabel("external.instituions.name");
 
     public static List<Unit> readAllExternalInstitutionUnits() {
-        List<Unit> allUnits = Unit.readAllUnits();
         List<Unit> allExternalUnits = new ArrayList<Unit>();
 
-        for (Unit unit : allUnits) {
+        for (Unit unit : Unit.readAllUnits()) {
             if (unit.getType() != null && unit.getType().equals(PartyTypeEnum.EXTERNAL_INSTITUTION)) {
                 allExternalUnits.add(unit);
             }
@@ -30,23 +28,18 @@ public class UnitUtils {
         return allExternalUnits;
     }
 
-    public static Unit readExternalInstitutionUnitByName(String name) throws ExcepcaoPersistencia {
-        List<Unit> allUnits = Unit.readAllUnits();
-        Unit externalUnit = null;
-
-        for (Unit unit : allUnits) {
+    public static Unit readExternalInstitutionUnitByName(String name) {
+        for (Unit unit : Unit.readAllUnits()) {
             if (unit.getType() != null && unit.getType().equals(PartyTypeEnum.EXTERNAL_INSTITUTION)
                     && unit.getName().equals(name)) {
-                externalUnit = unit;
+                return unit;
             }
         }
-
-        return externalUnit;
+        return null;
     }
 
-    public static Unit readUnitWithoutParentstByName(String name) throws ExcepcaoPersistencia {
-        List<Unit> allUnitsWithoutParent = readAllUnitsWithoutParents();
-        for (Unit unit : allUnitsWithoutParent) {
+    public static Unit readUnitWithoutParentstByName(String name)  {
+        for (Unit unit : readAllUnitsWithoutParents()) {
             if (unit.getName().equals(name)) {
                 return unit;
             }
@@ -54,11 +47,11 @@ public class UnitUtils {
         return null;
     }
 
-    public static List<Unit> readAllUnitsWithoutParents() throws ExcepcaoPersistencia {
-        List<Unit> allUnits = Unit.readAllUnits();
-        List<Unit> allUnitsWithoutParent = new ArrayList<Unit>();
+    public static List<Unit> readAllUnitsWithoutParents() {
 
-        for (Unit unit : allUnits) {
+        List<Unit> allUnitsWithoutParent = new ArrayList<Unit>();
+        
+        for (Unit unit : Unit.readAllUnits()) {
             if (unit.getParentUnits().isEmpty()) {
                 allUnitsWithoutParent.add(unit);
             }
@@ -82,15 +75,16 @@ public class UnitUtils {
         return bundle.getString(key);
     }
 
-    public static Unit readUnitWithoutParentstByAcronym(String acronym) throws ExcepcaoPersistencia {
-        List<Unit> topUnits = readAllUnitsWithoutParents();
-
-        for (Unit topUnit : topUnits) {
+    public static Unit readUnitWithoutParentstByAcronym(String acronym) {
+        for (Unit topUnit : readAllUnitsWithoutParents()) {
             if (topUnit.getAcronym() != null && topUnit.getAcronym().equals(acronym)) {
                 return topUnit;
             }
         }
-
         return null;
+    }
+
+    public static Unit readInstitutionUnit() {
+        return readUnitWithoutParentstByName(IST_UNIT_NAME);
     }
 }
