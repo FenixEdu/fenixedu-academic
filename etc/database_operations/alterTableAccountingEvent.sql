2298d89600e232a5f1be1ed38c3d5cf6fadbb940
João Florindo <jdnf@linkare.pt>
2006-06-26 17:51:42 +0000

    added two relations between event and itself (payment events and reimbursement events)

--- etc/database_operations/alterTableAccountingEvent.sql
+++ etc/database_operations/alterTableAccountingEvent.sql
@@ -1,2 +1,4 @@
 alter table ACCOUNTING_EVENT add column CLOSED tinyint(1) NOT NULL default '0';
 alter table ACCOUNTING_EVENT drop column WHEN_NOTICED;
+alter table ACCOUNTING_EVENT add column KEY_PAYED_EVENT tinyint(1) NOT NULL default '0';
+alter table ACCOUNTING_EVENT add column KEY_REIMBURSED_EVENT tinyint(1) NOT NULL default '0';
