4642c3b341b2b21b5012b4625206e149cc44aec4
João Florindo <jdnf@linkare.pt>
2006-06-26 14:19:58 +0000

    modified column name

--- etc/database_operations/alterTableAccountingEvent.sql
+++ etc/database_operations/alterTableAccountingEvent.sql
@@ -1,2 +1,2 @@
-alter table ACCOUNTING_EVENT add column PROCESSED tinyint(1) NOT NULL default '0';
+alter table ACCOUNTING_EVENT add column CLOSED tinyint(1) NOT NULL default '0';
 alter table ACCOUNTING_EVENT drop column WHEN_NOTICED;
