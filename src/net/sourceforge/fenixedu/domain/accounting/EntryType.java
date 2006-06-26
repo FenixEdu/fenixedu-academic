cbb3d479858aef63d16d55014c55b2226ee8e0ce
João Florindo <jdnf@linkare.pt>
2006-06-26 17:54:16 +0000

    new entry types

--- src/net/sourceforge/fenixedu/domain/accounting/EntryType.java
+++ src/net/sourceforge/fenixedu/domain/accounting/EntryType.java
@@ -2,7 +2,7 @@ package net.sourceforge.fenixedu.domain.accounting;
 
 public enum EntryType {
     
-    EMOLUMENTO;
+    TRANSFER, CANDIDACY_ENROLMENT_FEE;
     
     public String getName() {
         return name();
