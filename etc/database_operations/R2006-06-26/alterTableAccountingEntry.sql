3de925afdc9899e6fec2650f7afd3084edebc392
Luis Cruz <luis.cruz@ist.utl.pt>
2006-06-27 08:31:10 +0000

    Deployed HEAD:

--- etc/database_operations/R2006-06-26/alterTableAccountingEntry.sql
+++ etc/database_operations/R2006-06-26/alterTableAccountingEntry.sql
@@ -1,1 +1,2 @@
 alter table ACCOUNTING_ENTRY add column KEY_RECEIPT int(11) DEFAULT NULL;
+alter table ACCOUNTING_ENTRY add column ENTRY_TYPE varchar(100) NOT NULL;
--- etc/database_operations/alterTableAccountingEntry.sql
+++
@@ -1,1 +0,0 @@
-alter table ACCOUNTING_ENTRY add column ENTRY_TYPE varchar(100) NOT NULL;
