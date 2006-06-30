8d7b0825e09b6e99da01e185a5b2906a4cfa7384
Luís Santos <lmhs@ist.utl.pt>
2006-06-30 11:04:01 +0000

    Added new conversors necessary for types used in result unit and event association. Fixed some conversors related to types used in ResultPatent.

---
+++ src/net/sourceforge/fenixedu/persistenceTier/Conversores/ResultEventAssociationRole2SqlResultEventAssociationRoleConversion.java
@@ -0,0 +1,24 @@
+package net.sourceforge.fenixedu.persistenceTier.Conversores;
+
+import org.apache.ojb.broker.accesslayer.conversions.ConversionException;
+import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
+import net.sourceforge.fenixedu.domain.research.result.ResultEventAssociation.ResultEventAssociationRole;
+
+public class ResultEventAssociationRole2SqlResultEventAssociationRoleConversion implements FieldConversion{
+
+    public Object javaToSql(Object source) throws ConversionException {
+        if (source instanceof ResultEventAssociationRole) {
+            ResultEventAssociationRole s = (ResultEventAssociationRole) source;
+            return s.name();
+        }
+        return source;       
+    }
+
+    public Object sqlToJava(Object source) throws ConversionException {
+        if (source instanceof String) {            
+            String src = (String) source;            
+            return ResultEventAssociationRole.valueOf(src);
+        }
+        return source;        
+    }
+}
---
+++ src/net/sourceforge/fenixedu/persistenceTier/Conversores/ResultPatentType2SqlResultPatentTypeConversion.java
@@ -0,0 +1,25 @@
+package net.sourceforge.fenixedu.persistenceTier.Conversores;
+
+import net.sourceforge.fenixedu.domain.research.result.patent.ResultPatent.ResultPatentType;
+
+import org.apache.ojb.broker.accesslayer.conversions.ConversionException;
+import org.apache.ojb.broker.accesslayer.conversions.FieldConversion;
+
+public class ResultPatentType2SqlResultPatentTypeConversion implements FieldConversion{
+    
+        public Object javaToSql(Object source) throws ConversionException {
+            if (source instanceof ResultPatentType) {
+                ResultPatentType s = (ResultPatentType) source;
+                return s.name();
+            }
+            return source;       
+        }
+
+        public Object sqlToJava(Object source) throws ConversionException {
+            if (source instanceof String) {            
+                String src = (String) source;            
+                return ResultPatentType.valueOf(src);
+            }
+            return source;        
+        }
+}
