2ce07ea711c291198ab0cd618fba2f7937becf32
Luís Santos <lmhs@ist.utl.pt>
2006-06-30 10:59:35 +0000

    Added new Beans for ResultUnitAssociation and ResultEventAssociation

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/research/result/ResultEventAssociationSimpleCreationBean.java
@@ -0,0 +1,38 @@
+package net.sourceforge.fenixedu.dataTransferObject.research.result;
+
+import java.io.Serializable;
+
+import net.sourceforge.fenixedu.domain.DomainReference;
+import net.sourceforge.fenixedu.domain.research.event.Event;
+import net.sourceforge.fenixedu.domain.research.result.ResultEventAssociation.ResultEventAssociationRole;
+
+public class ResultEventAssociationSimpleCreationBean implements Serializable {
+    
+    private DomainReference<Event> event;
+    private ResultEventAssociationRole role;
+    private String eventName;
+
+    public ResultEventAssociationRole getRole() {
+        return role;
+    }
+
+    public void setRole(ResultEventAssociationRole associationRole) {
+        this.role = associationRole;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String name) {
+        this.eventName = name;
+    }
+
+    public Event getEvent() {
+        return (this.event == null) ? null : this.event.getObject();
+    }
+
+    public void setEvent(Event event) {
+        this.event = (event != null) ? new DomainReference<Event>(event) : null;
+    }
+}
---
+++ src/net/sourceforge/fenixedu/dataTransferObject/research/result/ResultUnitAssociationCreateBean.java
@@ -0,0 +1,43 @@
+package net.sourceforge.fenixedu.dataTransferObject.research.result;
+
+import java.io.Serializable;
+
+import net.sourceforge.fenixedu.domain.DomainReference;
+import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.research.result.ResultUnitAssociation.ResultUnitAssociationRole;;
+
+public class ResultUnitAssociationCreateBean implements Serializable {
+  
+    private DomainReference<Unit> unit;
+    private String role;
+    private String unitName;
+
+    public ResultUnitAssociationCreateBean() {
+        role = ResultUnitAssociationRole.getDefaultUnitRoleType().toString();
+    }
+    
+    public ResultUnitAssociationRole getRole() {
+        return ResultUnitAssociationRole.valueOf(role); 
+    }
+
+    public void setRole(ResultUnitAssociationRole resultUnitAssociationRole) {
+        this.role = resultUnitAssociationRole.toString();
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String name) {
+        this.unitName = name;
+    }
+
+    public Unit getUnit() {
+        return (this.unit == null) ? null : this.unit.getObject();
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = (unit != null) ? new DomainReference<Unit>(unit) : null;
+    }
+
+}
