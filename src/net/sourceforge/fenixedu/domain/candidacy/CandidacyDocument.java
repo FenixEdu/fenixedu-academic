2a03aa6e48b7185d50a740aa31e999aad3baab81
Shezad Anavarali <shezad@ist.utl.pt>
2006-07-03 16:38:20 +0000

    removed unnecessary method

--- src/net/sourceforge/fenixedu/domain/candidacy/CandidacyDocument.java
+++ src/net/sourceforge/fenixedu/domain/candidacy/CandidacyDocument.java
@@ -1,7 +1,5 @@
 package net.sourceforge.fenixedu.domain.candidacy;
 
-import org.joda.time.DateTime;
-
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 
 public class CandidacyDocument extends CandidacyDocument_Base {
@@ -15,12 +13,6 @@ public class CandidacyDocument extends CandidacyDocument_Base {
         super();
         setRootDomainObject(RootDomainObject.getInstance());
         setDocumentDescription(description);
-    }
-
-    @Override
-    public void setFile(CandidacyDocumentFile file) {
-        setFileUploadTime(new DateTime());
-        super.setFile(file);
     }
 
 }
