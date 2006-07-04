0480ad2a1f91f68035d05bfd0b4aae316daf2088
José Pereira <jpereira@linkare.com>
2006-07-04 11:49:04 +0000

    This is an additional Property Reader that enumerates roleTypes for the installer to use... It could have been done more generally for enums,,, but is ok this way... at least for now... jpereira

---
+++ src_tools/pt/utl/ist/propreaders/RoleTypePropertyReader.java
@@ -0,0 +1,48 @@
+package pt.utl.ist.propreaders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.person.RoleType;
+import pt.linkare.ant.InvalidPropertySpecException;
+import pt.linkare.ant.MenuMessage;
+import pt.linkare.ant.propreaders.AbstractPropertyReader;
+
+public class RoleTypePropertyReader extends AbstractPropertyReader{
+
+	public RoleTypePropertyReader() {
+		super();
+	}
+
+	public String readProperty() throws InvalidPropertySpecException {
+		
+		return getRoleTypesString();
+	}
+	
+	public String getRoleTypesString() throws InvalidPropertySpecException
+	{
+		MenuMessage menuOptionsLang=new MenuMessage();
+		menuOptionsLang.setMessage(buildDefaultMessage(false));
+		List<String>[] optionsAndValuesLang=null;
+		optionsAndValuesLang=buildRoleTypesOptions();
+		menuOptionsLang.setOptions(optionsAndValuesLang[0]);
+		menuOptionsLang.setOptionValues(optionsAndValuesLang[1]);
+		if(getProperty().getPropertyDefaultValue()!=null)
+			return getInput().readMultipleOptionOrDefault(menuOptionsLang, getProperty().getPropertyDefaultValue());
+		else
+			return getInput().readMultipleOption(menuOptionsLang);
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<String>[] buildRoleTypesOptions() throws InvalidPropertySpecException {
+		List<String>[] optionsValuesRetVal=(List<String>[])new List[]{new ArrayList<String>(RoleType.values().length),new ArrayList<String>(RoleType.values().length)};
+		for(RoleType roleType:RoleType.values())
+		{
+			optionsValuesRetVal[0].add(roleType.getDefaultLabel());
+			optionsValuesRetVal[1].add(roleType.name());
+		}
+		return optionsValuesRetVal;
+		
+	}
+	
+}
