0ea7a7c1a9d315cc612eb56ae46ed782c25b1505
Sérgio Patrício <sergio.patricio@ist.utl.pt>
2006-07-25 14:01:34 +0000

    result publication bean

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/research/result/publication/ResultPublicationCreationBean.java
@@ -0,0 +1,373 @@
+package net.sourceforge.fenixedu.dataTransferObject.research.result.publication;
+
+import java.io.Serializable;
+
+import net.sourceforge.fenixedu.domain.DomainReference;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.research.result.publication.Article;
+import net.sourceforge.fenixedu.domain.research.result.publication.Book;
+import net.sourceforge.fenixedu.domain.research.result.publication.BookPart;
+import net.sourceforge.fenixedu.domain.research.result.publication.Booklet;
+import net.sourceforge.fenixedu.domain.research.result.publication.Manual;
+import net.sourceforge.fenixedu.domain.research.result.publication.Misc;
+import net.sourceforge.fenixedu.domain.research.result.publication.ResultPublication;
+import net.sourceforge.fenixedu.domain.research.result.publication.TechnicalReport;
+import net.sourceforge.fenixedu.domain.research.result.publication.Thesis;
+import net.sourceforge.fenixedu.domain.research.result.publication.Unpublished;
+import net.sourceforge.fenixedu.domain.research.result.publication.BookPart.BookPartType;
+import net.sourceforge.fenixedu.domain.research.result.publication.ResultPublication.ScopeType;
+import net.sourceforge.fenixedu.domain.research.result.publication.Thesis.ThesisType;
+import net.sourceforge.fenixedu.util.Month;
+
+public class ResultPublicationCreationBean implements Serializable{
+
+    private ResultPublicationType publicationType;
+    private String activeSchema;
+    
+    private DomainReference<Person> participator;
+    
+    private DomainReference<Unit> publisher;
+    private String publisherName;
+    
+    private DomainReference<Unit> organization;
+    private String organizationName;
+    
+    private String title;
+    private String address;
+    private Integer year;
+    private Month month;
+    private String volume;
+    private String series;
+    private String edition;
+    private Integer isbn;
+    private Integer numberPages;
+    private String language;
+    private ScopeType scope;
+    private String url;
+    private String note;
+    private String journal;
+    private String number;
+    private Integer firstPage;
+    private Integer lastPage;
+    private Integer issn;
+    private BookPartType bookPartType;
+    private String chapter;
+    private String bookTitle;
+    private ThesisType thesisType;
+    private String technicalReportType;
+    private String howPublished;
+    private String otherPublicationType;
+    private Integer idInternal;
+     
+    public enum ResultPublicationType {
+        /*Types based on BibTex*/
+        Book,
+        BookPart,
+        Article,
+        Inproceedings,
+        Proceedings,
+        Thesis,
+        Manual,
+        TechnicalReport,
+        Booklet,
+        Misc,
+        Unpublished;
+              
+        public static ResultPublicationType getDefaultResultPublicationType() {
+            return Book;
+        }
+    }
+
+    public void fillFromPublication(ResultPublication publication) {
+        //common fields
+        this.setIdInternal(publication.getIdInternal());
+        this.setTitle(publication.getTitle());
+        this.setNote(publication.getNote());
+        this.setUrl(publication.getUrl());
+        this.setYear(publication.getYear());
+        this.setMonth(publication.getMonth());
+        this.setPublisher(publication.getPublisher());
+        this.setOrganization(publication.getOrganization());
+        
+        if(publication instanceof Book) {
+            this.setPublicationType(ResultPublicationType.Book);
+            Book book = (Book) publication;
+            this.setAddress(book.getAddress());
+            this.setVolume(book.getVolume());
+            this.setSeries(book.getSeries());
+            this.setEdition(book.getEdition());
+            this.setIsbn(book.getIsbn());
+            this.setNumberPages(book.getNumberPages());
+            this.setLanguage(book.getLanguage());
+            this.setScope(book.getScope());
+        }
+        else if(publication instanceof BookPart) {
+            this.setPublicationType(ResultPublicationType.BookPart);
+            BookPart bookPart = (BookPart) publication;
+            this.setBookPartType(bookPart.getBookPartType());
+            this.setChapter(bookPart.getChapter());
+            this.setBookTitle(bookPart.getBookTitle());
+            this.setFirstPage(bookPart.getFirstPage());
+            this.setLastPage(bookPart.getLastPage());
+            this.setAddress(bookPart.getAddress());
+            this.setVolume(bookPart.getVolume());
+            this.setSeries(bookPart.getSeries());
+            this.setEdition(bookPart.getEdition());
+        }
+        else if(publication instanceof Article) {
+            this.setPublicationType(ResultPublicationType.Article);
+            Article article = (Article) publication;
+            this.setJournal(article.getJournal());
+            this.setVolume(article.getVolume());
+            this.setNumber(article.getNumber());
+            this.setFirstPage(article.getFirstPage());
+            this.setLastPage(article.getLastPage());
+            this.setIssn(article.getIssn());
+            this.setLanguage(article.getLanguage());
+            this.setScope(article.getScope());
+        }
+        else if(publication instanceof Thesis) {
+            this.setPublicationType(ResultPublicationType.Thesis);
+            Thesis thesis = (Thesis) publication;
+            this.setThesisType(thesis.getThesisType());
+            this.setAddress(thesis.getAddress());
+            this.setNumberPages(thesis.getNumberPages());
+            this.setLanguage(thesis.getLanguage());
+        }
+        else if(publication instanceof Manual) {
+            this.setPublicationType(ResultPublicationType.Manual);
+            Manual manual = (Manual) publication;
+            this.setAddress(manual.getAddress());
+            this.setEdition(manual.getEdition());
+        }
+        else if(publication instanceof TechnicalReport) {
+            this.setPublicationType(ResultPublicationType.TechnicalReport);
+            TechnicalReport technicalReport = (TechnicalReport) publication;
+            this.setTechnicalReportType(technicalReport.getTechnicalReportType());
+            this.setNumber(technicalReport.getNumber());
+            this.setAddress(technicalReport.getAddress());
+            this.setNumberPages(technicalReport.getNumberPages());
+            this.setLanguage(technicalReport.getLanguage());
+        }
+        else if(publication instanceof Booklet) {
+            this.setPublicationType(ResultPublicationType.Booklet);
+            Booklet booklet = (Booklet) publication;
+            this.setHowPublished(booklet.getHowPublished());
+            this.setAddress(booklet.getAddress());
+        }
+        else if(publication instanceof Misc) {
+            this.setPublicationType(ResultPublicationType.Misc);
+            Misc misc = (Misc) publication;
+            this.setHowPublished(misc.getHowPublished());
+            this.setOtherPublicationType(misc.getOtherPublicationType());
+            this.setNumberPages(misc.getNumberPages());
+            this.setLanguage(misc.getLanguage());
+            this.setAddress(misc.getAddress());
+        }
+        else if(publication instanceof Unpublished) {
+            this.setPublicationType(ResultPublicationType.Unpublished);
+            //Unpublished has no more attributtes
+        }
+    }
+    
+    public Person getParticipator() {
+        return (this.participator == null) ? null : this.participator.getObject();
+    }
+    public void setParticipator(Person participator) {
+        this.participator = (participator != null) ? new DomainReference<Person>(participator) : null;
+    }
+    public Unit getPublisher() {
+        return (this.publisher == null) ? null : this.publisher.getObject();
+    }
+    public void setPublisher(Unit publisher) {
+        this.publisher = (publisher != null) ? new DomainReference<Unit>(publisher) : null;
+    }
+    public String getPublisherName() {
+        return publisherName;
+    }
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+    public Unit getOrganization() {
+        return (this.organization == null) ? null : this.organization.getObject();
+    }
+    public void setOrganization(Unit organization) {
+        this.organization = (organization != null) ? new DomainReference<Unit>(organization) : null;
+    }
+    public String getOrganizationName() {
+        return organizationName;
+    }
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getEdition() {
+        return edition;
+    }
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+    public Integer getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(Integer isbn) {
+        this.isbn = isbn;
+    }
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+    public Month getMonth() {
+        return month;
+    }
+    public void setMonth(Month month) {
+        this.month = month;
+    }
+    public String getNote() {
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+    public Integer getNumberPages() {
+        return numberPages;
+    }
+    public void setNumberPages(Integer numberPages) {
+        this.numberPages = numberPages;
+    }
+    public ScopeType getScope() {
+        return scope;
+    }
+    public void setScope(ScopeType scope) {
+        this.scope = scope;
+    }
+    public String getSeries() {
+        return series;
+    }
+    public void setSeries(String series) {
+        this.series = series;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getVolume() {
+        return volume;
+    }
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+    public Integer getFirstPage() {
+        return firstPage;
+    }
+    public void setFirstPage(Integer firstPage) {
+        this.firstPage = firstPage;
+    }
+    public Integer getIssn() {
+        return issn;
+    }
+    public void setIssn(Integer issn) {
+        this.issn = issn;
+    }
+    public String getJournal() {
+        return journal;
+    }
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+    public Integer getLastPage() {
+        return lastPage;
+    }
+    public void setLastPage(Integer lastPage) {
+        this.lastPage = lastPage;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public BookPartType getBookPartType() {
+        return bookPartType;
+    }
+    public void setBookPartType(BookPartType bookPartType) {
+        this.bookPartType = bookPartType;
+    }
+    public String getChapter() {
+        return chapter;
+    }
+    public void setChapter(String chapter) {
+        this.chapter = chapter;
+    }
+    public String getActiveSchema() {
+        return activeSchema;
+    }
+    public void setActiveSchema(String activeSchema) {
+        this.activeSchema = activeSchema;
+    }
+    public String getBookTitle() {
+        return bookTitle;
+    }
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+    public ResultPublicationType getPublicationType() {
+        return publicationType;
+    }
+    public void setPublicationType(ResultPublicationType publicationType) {
+        this.publicationType = publicationType;
+    }
+    public ThesisType getThesisType() {
+        return thesisType;
+    }
+    public void setThesisType(ThesisType thesisType) {
+        this.thesisType = thesisType;
+    }
+    public String getTechnicalReportType() {
+        return technicalReportType;
+    }
+    public void setTechnicalReportType(String technicalReportType) {
+        this.technicalReportType = technicalReportType;
+    }
+    public String getHowPublished() {
+        return howPublished;
+    }
+    public void setHowPublished(String howPublished) {
+        this.howPublished = howPublished;
+    }
+    public String getOtherPublicationType() {
+        return otherPublicationType;
+    }
+    public void setOtherPublicationType(String otherPublicationType) {
+        this.otherPublicationType = otherPublicationType;
+    }
+    public Integer getIdInternal() {
+        return idInternal;
+    }
+    public void setIdInternal(Integer idInternal) {
+        this.idInternal = idInternal;
+    }
+
+}
