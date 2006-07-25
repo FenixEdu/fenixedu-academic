3b0eded3cacbf1ab461f5391503707d72ebb19b6
Sérgio Patrício <sergio.patricio@ist.utl.pt>
2006-07-25 13:59:03 +0000

    result changes

---
+++ etc/database_operations/alterTableResult.sql
@@ -0,0 +1,3 @@
+alter table RESULT change JOURNAL_NAME JOURNAL varchar(250) default NULL;
+alter table RESULT change CHAPTER_SECTION CHAPTER varchar(250) default NULL;
+alter table RESULT change TYPE TECHNICAL_REPORT_TYPE varchar(250) default NULL;
