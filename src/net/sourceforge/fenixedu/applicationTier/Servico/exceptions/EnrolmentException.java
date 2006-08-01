114787b92346517d23a3866f1e2a389058dc04e0
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-08-01 17:19:40 +0000

    new exception for special season enrolments

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/exceptions/EnrolmentException.java
@@ -0,0 +1,14 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.exceptions;
+
+public class EnrolmentException extends FenixServiceException {
+	
+	public EnrolmentException(String message) {
+		super(message);
+	}
+	
+	public EnrolmentException(String message, String[] args) {
+		super(message, args);
+	}
+
+
+}
