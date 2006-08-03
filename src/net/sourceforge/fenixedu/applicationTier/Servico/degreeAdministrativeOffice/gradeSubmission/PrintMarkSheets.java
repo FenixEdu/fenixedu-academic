15e8dcf163a1bfb0a2b3f7c8333c51bb24f06b6f
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-08-03 15:12:05 +0000

    now extends PrintMarkSheet

--- src/net/sourceforge/fenixedu/applicationTier/Servico/degreeAdministrativeOffice/gradeSubmission/PrintMarkSheets.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/degreeAdministrativeOffice/gradeSubmission/PrintMarkSheets.java
@@ -1,44 +1,21 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.degreeAdministrativeOffice.gradeSubmission;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
 
-import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.UnableToPrintServiceException;
-import net.sourceforge.fenixedu.domain.EnrolmentEvaluation;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
 import net.sourceforge.fenixedu.domain.MarkSheet;
-import net.sourceforge.fenixedu.util.FenixDigestUtils;
-import net.sourceforge.fenixedu.util.ReportsUtils;
 
 
-public class PrintMarkSheets extends Service{
+public class PrintMarkSheets extends PrintMarkSheet{
 	
 	public void run(Collection<MarkSheet> markSheets, String printerName) throws FenixServiceException {
 		for (MarkSheet markSheet : markSheets) {
-			printMarkSheet(markSheet, printerName);
+			try {
+				super.run(markSheet, printerName);
+			} catch(InvalidArgumentsServiceException e) {
+				
+			}
 		}
 	}
-	
-	
-	private void printMarkSheet(MarkSheet markSheet, String printerName) throws FenixServiceException {
-		Map parameters = new HashMap();
-		parameters.put("markSheet", markSheet);
-		parameters.put("checkSum", FenixDigestUtils.getPrettyCheckSum(markSheet.getCheckSum()));
-		ResourceBundle bundle = ResourceBundle.getBundle("resources.ReportsResources");
-		List<EnrolmentEvaluation> evaluations = new ArrayList<EnrolmentEvaluation>(markSheet.getEnrolmentEvaluations());
-		Collections.sort(evaluations, EnrolmentEvaluation.SORT_BY_STUDENT_NUMBER);
-
-		boolean result = ReportsUtils.printReport("markSheet", parameters, bundle, evaluations, printerName);
-		if(!result) {
-			throw new UnableToPrintServiceException("error.print.failed");
-		}
-		markSheet.setPrinted(Boolean.TRUE);
-	}
-
 }
