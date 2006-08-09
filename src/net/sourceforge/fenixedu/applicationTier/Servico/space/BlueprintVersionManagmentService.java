7557bd4355d23f72e7d6c48fea17f1619f4eec5c
Luis Cruz <luis.cruz@ist.utl.pt>
2006-08-09 11:16:42 +0000

    Removed duplicate blocks of code.

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/space/BlueprintVersionManagmentService.java
@@ -0,0 +1,57 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.space;
+
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.dataTransferObject.spaceManager.CreateBlueprintSubmissionBean;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.Role;
+import net.sourceforge.fenixedu.domain.accessControl.RoleGroup;
+import net.sourceforge.fenixedu.domain.person.RoleType;
+import net.sourceforge.fenixedu.domain.space.Blueprint;
+import net.sourceforge.fenixedu.domain.space.BlueprintFile;
+import net.sourceforge.fenixedu.domain.space.Space;
+import net.sourceforge.fenixedu.domain.space.SpaceInformation;
+import pt.utl.ist.fenix.tools.file.FileDescriptor;
+import pt.utl.ist.fenix.tools.file.FileManagerFactory;
+import pt.utl.ist.fenix.tools.file.FileMetadata;
+import pt.utl.ist.fenix.tools.file.FilePath;
+import pt.utl.ist.fenix.tools.file.Node;
+
+public abstract class BlueprintVersionManagmentService extends Service {
+
+    protected Space getSpace(CreateBlueprintSubmissionBean blueprintSubmissionBean) throws FenixServiceException {
+        final SpaceInformation spaceInformation = blueprintSubmissionBean.getSpaceInformation();
+        final Space space = spaceInformation.getSpace();
+        if (space == null) {
+            throw new FenixServiceException("error.blueprint.submission.no.space");
+        }
+        return space;
+    }
+
+    protected void editBlueprintVersion(CreateBlueprintSubmissionBean blueprintSubmissionBean,
+            final Space space, final Person person, final Blueprint blueprint) {
+        final String filename = blueprintSubmissionBean.getSpaceInformation().getIdInternal()
+                + String.valueOf(System.currentTimeMillis());
+        final FileMetadata fileMetadata = new FileMetadata(filename, person.getName());
+
+        final FileDescriptor fileDescriptor = FileManagerFactory.getFileManager().saveFile(
+                getFilePath(space.getMostRecentSpaceInformation()), filename, true, fileMetadata,
+                blueprintSubmissionBean.getInputStream());
+
+        final String displayName = blueprintSubmissionBean.getFilename();
+        new BlueprintFile(blueprint, filename, displayName, fileDescriptor.getMimeType(), fileDescriptor
+                .getChecksum(), fileDescriptor.getChecksumAlgorithm(), fileDescriptor.getSize(),
+                fileDescriptor.getUniqueId(), new RoleGroup(Role
+                        .getRoleByRoleType(RoleType.SPACE_MANAGER)));
+    }
+
+    protected FilePath getFilePath(SpaceInformation spaceInformation) {
+        final FilePath filePath = new FilePath();
+        filePath.addNode(new Node("Spaces", "Spaces"));
+        filePath.addNode(new Node("Spaces" + spaceInformation.getSpace().getIdInternal(),
+                spaceInformation.getPresentationName()));
+        filePath.addNode(new Node("Blueprints", "Blueprints"));
+        return filePath;
+    }
+
+}
--- src/net/sourceforge/fenixedu/applicationTier/Servico/space/EditBlueprintVersion.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/space/EditBlueprintVersion.java
@@ -1,63 +1,26 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.space;
 
 import net.sourceforge.fenixedu.accessControl.AccessControl;
-import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.spaceManager.CreateBlueprintSubmissionBean;
 import net.sourceforge.fenixedu.domain.Person;
-import net.sourceforge.fenixedu.domain.Role;
-import net.sourceforge.fenixedu.domain.accessControl.RoleGroup;
-import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.space.Blueprint;
-import net.sourceforge.fenixedu.domain.space.BlueprintFile;
 import net.sourceforge.fenixedu.domain.space.Space;
-import net.sourceforge.fenixedu.domain.space.SpaceInformation;
-import pt.utl.ist.fenix.tools.file.FileDescriptor;
-import pt.utl.ist.fenix.tools.file.FileManagerFactory;
-import pt.utl.ist.fenix.tools.file.FileMetadata;
-import pt.utl.ist.fenix.tools.file.FilePath;
-import pt.utl.ist.fenix.tools.file.Node;
 
-public class EditBlueprintVersion extends Service {
+public class EditBlueprintVersion extends BlueprintVersionManagmentService {
 
     public Blueprint run(Blueprint blueprint, CreateBlueprintSubmissionBean blueprintSubmissionBean)
             throws FenixServiceException {
 
         blueprint.removeBlueprintFile();
 
-        String filename = blueprintSubmissionBean.getSpaceInformation().getIdInternal()
-                + String.valueOf(System.currentTimeMillis());
-        String displayName = blueprintSubmissionBean.getFilename();
+        final Space space = getSpace(blueprintSubmissionBean);
 
-        SpaceInformation spaceInformation = blueprintSubmissionBean.getSpaceInformation();
-        Space space = spaceInformation.getSpace();
+        final Person person = AccessControl.getUserView().getPerson();
 
-        if (space == null) {
-            throw new FenixServiceException("error.blueprint.submission.no.space");
-        }
-
-        Person person = AccessControl.getUserView().getPerson();
-
-        final FileMetadata fileMetadata = new FileMetadata(filename, person.getName());
-
-        final FileDescriptor fileDescriptor = FileManagerFactory.getFileManager().saveFile(
-                getFilePath(space.getMostRecentSpaceInformation()), filename, true, fileMetadata,
-                blueprintSubmissionBean.getInputStream());
-
-        new BlueprintFile(blueprint, filename, displayName, fileDescriptor.getMimeType(), fileDescriptor
-                .getChecksum(), fileDescriptor.getChecksumAlgorithm(), fileDescriptor.getSize(),
-                fileDescriptor.getUniqueId(), new RoleGroup(Role
-                        .getRoleByRoleType(RoleType.SPACE_MANAGER)));
+        editBlueprintVersion(blueprintSubmissionBean, space, person, blueprint);
 
         return blueprint;
     }
 
-    private FilePath getFilePath(SpaceInformation spaceInformation) {
-        final FilePath filePath = new FilePath();
-        filePath.addNode(new Node("Spaces", "Spaces"));
-        filePath.addNode(new Node("Spaces" + spaceInformation.getSpace().getIdInternal(),
-                spaceInformation.getPresentationName()));
-        filePath.addNode(new Node("Blueprints", "Blueprints"));
-        return filePath;
-    }
 }
