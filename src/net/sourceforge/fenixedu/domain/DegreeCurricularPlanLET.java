b59cfcbdd96f1ba611c5ccdb9cb62206a47bd19d
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-08-10 08:15:50 +0000

    added LETBolonhaEnrolmentRule new rule

--- src/net/sourceforge/fenixedu/domain/DegreeCurricularPlanLET.java
+++ src/net/sourceforge/fenixedu/domain/DegreeCurricularPlanLET.java
@@ -6,6 +6,7 @@ package net.sourceforge.fenixedu.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.fenixedu.domain.degree.enrollment.rules.LETBolonhaEnrolmentRule;
 import net.sourceforge.fenixedu.domain.degree.enrollment.rules.LETOptionalExclusiveEnrollmentRule;
 
 /**
@@ -25,6 +26,7 @@ public class DegreeCurricularPlanLET extends DegreeCurricularPlanLET_Base {
         List result = new ArrayList();       
         result.addAll(super.getListOfEnrollmentRules(studentCurricularPlan, executionPeriod));
         result.add(new LETOptionalExclusiveEnrollmentRule(studentCurricularPlan, executionPeriod));
+        result.add(new LETBolonhaEnrolmentRule(studentCurricularPlan, executionPeriod));
         return result;
     }
 
