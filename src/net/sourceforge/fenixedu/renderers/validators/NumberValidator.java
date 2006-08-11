1299a0490ede177156381e2a832a0a1fe9cb7a17
João Silva <joao.silva@ist.utl.pt>
2006-08-11 11:41:59 +0000

    Corrected a bug by which the required property was not honored

--- src/net/sourceforge/fenixedu/renderers/validators/NumberValidator.java
+++ src/net/sourceforge/fenixedu/renderers/validators/NumberValidator.java
@@ -5,59 +5,60 @@ import net.sourceforge.fenixedu.renderers.utils.RenderUtils;
 
 public class NumberValidator extends RequiredValidator {
 
-    private int base;
-    private boolean required;
-    
-    public NumberValidator(Validatable component) {
-        super(component);
+	private int base;
 
-        component.setValidator(this);
-        setRequired(true);
-        setBase(10);
-    }
-    
-    public NumberValidator(Validatable component, int base) {
-        this(component);
-        
-        setBase(base);
-    }
-    
-    public boolean isRequired() {
-        return this.required;
-    }
+	private boolean required;
 
-    public void setRequired(boolean required) {
-        this.required = required;
-    }
+	public NumberValidator(Validatable component) {
+		super(component);
 
-    public int getBase() {
-        return this.base;
-    }
+		component.setValidator(this);
+		setRequired(true);
+		setBase(10);
+	}
 
-    public void setBase(int base) {
-        this.base = base;
-    }
+	public NumberValidator(Validatable component, int base) {
+		this(component);
 
-    @Override
-    public String getErrorMessage() {
-        return RenderUtils.getResourceString("renderers.validator.number");
-    }
+		setBase(base);
+	}
 
-    @Override
-    public void performValidation() {
-        if (isRequired()) {
-            super.performValidation();
-        }
-        
-        if (isValid()) {
-            String numberText = getComponent().getValue().trim();
-            
-            try {
-                Integer.parseInt(numberText, getBase());
-                setValid(true);
-            } catch (NumberFormatException e) {
-                setValid(false);
-            }
-        }
-    }
+	public boolean isRequired() {
+		return this.required;
+	}
+
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+
+	public int getBase() {
+		return this.base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return RenderUtils.getResourceString("renderers.validator.number");
+	}
+
+	@Override
+	public void performValidation() {
+		super.performValidation();
+
+		if (isValid()) {
+			String numberText = getComponent().getValue().trim();
+
+			try {
+				Integer.parseInt(numberText, getBase());
+				setValid(true);
+			} catch (NumberFormatException e) {
+				setValid(false);
+			}
+		} else {
+			setValid(!isRequired());
+		}
+	}
 }
