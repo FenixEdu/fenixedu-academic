8806f83fba438276a7ecdc05aa52772c14f09df7
João Florindo <jdnf@linkare.pt>
2006-08-23 15:03:27 +0000

    corrected files identation

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/Credits.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/Credits.java
@@ -5,29 +5,29 @@ import net.sourceforge.fenixedu.domain.degreeStructure.DegreeModule;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
 public class Credits extends Credits_Base {
-    
+
     public Credits() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
-    }
-    
-    @Override
-    public void setDegreeModule(DegreeModule degreeModule) {
-    	if(degreeModule != null) {
-    		throw new DomainException("can't set degree modulo for class Credits");
-    	}
-    }
-    
-    @Override
-    public void delete() {
-    	getEnrolments().clear();
-    	super.delete();
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
 
-	@Override
-	public StringBuilder print(String tabs) {
-		// TODO Auto-generated method stub
-		return null;
+    @Override
+    public void setDegreeModule(DegreeModule degreeModule) {
+	if (degreeModule != null) {
+	    throw new DomainException("can't set degree modulo for class Credits");
 	}
-    
+    }
+
+    @Override
+    public void delete() {
+	getEnrolments().clear();
+	super.delete();
+    }
+
+    @Override
+    public StringBuilder print(String tabs) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
 }
--- src/net/sourceforge/fenixedu/domain/studentCurriculum/Equivalence.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/Equivalence.java
@@ -4,27 +4,27 @@ import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
 public class Equivalence extends Equivalence_Base {
-    
-    public  Equivalence(CurriculumGroup curriculumGroup, Enrolment enrolment) {
-    	super();
-    	if(curriculumGroup == null || enrolment == null) {
-    		throw new DomainException("invalid arguments");
-    	}
-    	validateDegreeModuleLink(curriculumGroup, enrolment.getCurricularCourse());
-    	setCurriculumGroup(curriculumGroup);
-    	addEnrolments(enrolment);
-    }
-    
-    @Override
-    public void delete() {
-    	getEnrolments().clear();
-    	super.delete();
+
+    public Equivalence(CurriculumGroup curriculumGroup, Enrolment enrolment) {
+	super();
+	if (curriculumGroup == null || enrolment == null) {
+	    throw new DomainException("invalid arguments");
+	}
+	validateDegreeModuleLink(curriculumGroup, enrolment.getCurricularCourse());
+	setCurriculumGroup(curriculumGroup);
+	addEnrolments(enrolment);
     }
 
-	@Override
-	public StringBuilder print(String tabs) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-    
+    @Override
+    public void delete() {
+	getEnrolments().clear();
+	super.delete();
+    }
+
+    @Override
+    public StringBuilder print(String tabs) {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
 }
