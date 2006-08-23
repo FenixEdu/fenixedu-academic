a5d35475df6a78e15cbade6d037bea0e85d405d3
João Florindo <jdnf@linkare.pt>
2006-08-23 14:55:44 +0000

    corrected file identation

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoTeacher.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoTeacher.java
@@ -17,66 +17,66 @@ import net.sourceforge.fenixedu.domain.Teacher;
  */
 public class InfoTeacher extends InfoObject implements ISmsDTO {
 
-	private final Teacher teacher;
+    private final Teacher teacher;
 
     public InfoTeacher(final Teacher teacher) {
-    	this.teacher = teacher;
+	this.teacher = teacher;
     }
 
     public List getProfessorShipsExecutionCourses() {
-    	final List<InfoProfessorship> infoProfessorships = new ArrayList<InfoProfessorship>();
-    	for (final Professorship professorship : teacher.getProfessorshipsSet()) {
-    		infoProfessorships.add(InfoProfessorship.newInfoFromDomain(professorship));
-    	}
-        return infoProfessorships;
+	final List<InfoProfessorship> infoProfessorships = new ArrayList<InfoProfessorship>();
+	for (final Professorship professorship : teacher.getProfessorshipsSet()) {
+	    infoProfessorships.add(InfoProfessorship.newInfoFromDomain(professorship));
+	}
+	return infoProfessorships;
     }
 
     public List getResponsibleForExecutionCourses() {
-    	final List<InfoProfessorship> infoProfessorships = new ArrayList<InfoProfessorship>();
-    	for (final Professorship professorship : teacher.getProfessorshipsSet()) {
-    		if (professorship.isResponsibleFor()) {
-    			infoProfessorships.add(InfoProfessorship.newInfoFromDomain(professorship));
-    		}
-    	}
-        return infoProfessorships;
+	final List<InfoProfessorship> infoProfessorships = new ArrayList<InfoProfessorship>();
+	for (final Professorship professorship : teacher.getProfessorshipsSet()) {
+	    if (professorship.isResponsibleFor()) {
+		infoProfessorships.add(InfoProfessorship.newInfoFromDomain(professorship));
+	    }
+	}
+	return infoProfessorships;
     }
 
     public Integer getTeacherNumber() {
-        return teacher.getTeacherNumber();
+	return teacher.getTeacherNumber();
     }
 
     public InfoPerson getInfoPerson() {
-        return InfoPerson.newInfoFromDomain(teacher.getPerson());
+	return InfoPerson.newInfoFromDomain(teacher.getPerson());
     }
 
     public InfoCategory getInfoCategory() {
-        return InfoCategory.newInfoFromDomain(teacher.getCategory());
+	return InfoCategory.newInfoFromDomain(teacher.getCategory());
     }
 
     public boolean equals(Object obj) {
-    	return obj != null && teacher == ((InfoTeacher) obj).teacher;
+	return obj != null && teacher == ((InfoTeacher) obj).teacher;
     }
 
     public String toString() {
-    	return teacher.toString();
+	return teacher.toString();
     }
 
     public static InfoTeacher newInfoFromDomain(final Teacher teacher) {
-    	return teacher == null ? null : new InfoTeacher(teacher);
+	return teacher == null ? null : new InfoTeacher(teacher);
     }
 
     public String toSmsText() {
-        return "Nome: " + teacher.getPerson().getNome() + " Numero: " + getTeacherNumber();
+	return "Nome: " + teacher.getPerson().getNome() + " Numero: " + getTeacherNumber();
     }
 
-	@Override
-	public Integer getIdInternal() {
-		return teacher.getIdInternal();
-	}
+    @Override
+    public Integer getIdInternal() {
+	return teacher.getIdInternal();
+    }
 
     @Override
     public void setIdInternal(Integer integer) {
-        throw new Error("Method should not be called!");
+	throw new Error("Method should not be called!");
     }
 
 }
