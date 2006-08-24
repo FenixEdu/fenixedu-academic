028b066a58cb3a12ba9b0b0d96eb166202f45427
João Florindo <jdnf@linkare.pt>
2006-08-24 09:07:38 +0000

    corrected file indentation

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoStudentCurricularPlan.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoStudentCurricularPlan.java
@@ -30,128 +30,135 @@ import org.apache.commons.collections.comparators.ComparatorChain;
  */
 public class InfoStudentCurricularPlan extends InfoObject implements Serializable, Comparable {
 
-	private final StudentCurricularPlan studentCurricularPlan;
+    private final StudentCurricularPlan studentCurricularPlan;
 
-	public InfoStudentCurricularPlan(final StudentCurricularPlan studentCurricularPlan) {
-		this.studentCurricularPlan = studentCurricularPlan;
+    public InfoStudentCurricularPlan(final StudentCurricularPlan studentCurricularPlan) {
+	this.studentCurricularPlan = studentCurricularPlan;
     }
 
     public boolean equals(Object obj) {
-    	return obj instanceof InfoStudentCurricularPlan && studentCurricularPlan == ((InfoStudentCurricularPlan) obj).studentCurricularPlan;
+	return obj instanceof InfoStudentCurricularPlan
+		&& studentCurricularPlan == ((InfoStudentCurricularPlan) obj).studentCurricularPlan;
     }
 
     public String toString() {
-    	return studentCurricularPlan.toString();
+	return studentCurricularPlan.toString();
     }
 
     public StudentCurricularPlanState getCurrentState() {
-        return studentCurricularPlan.getCurrentState();
+	return studentCurricularPlan.getCurrentState();
     }
 
     public InfoBranch getInfoBranch() {
-        return InfoBranch.newInfoFromDomain(studentCurricularPlan.getBranch());
+	return InfoBranch.newInfoFromDomain(studentCurricularPlan.getBranch());
     }
 
     public InfoDegreeCurricularPlan getInfoDegreeCurricularPlan() {
-        return InfoDegreeCurricularPlan.newInfoFromDomain(studentCurricularPlan.getDegreeCurricularPlan());
+	return InfoDegreeCurricularPlan.newInfoFromDomain(studentCurricularPlan
+		.getDegreeCurricularPlan());
     }
 
     public InfoStudent getInfoStudent() {
-        return InfoStudent.newInfoFromDomain(studentCurricularPlan.getStudent());
+	return InfoStudent.newInfoFromDomain(studentCurricularPlan.getStudent());
     }
 
     public Date getStartDate() {
-        return studentCurricularPlan.getStartDate();
+	return studentCurricularPlan.getStartDate();
     }
 
     public String getStartDateFormatted() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(getStartDate());
-        String result = calendar.get(Calendar.DAY_OF_MONTH) + "/" + (calendar.get(Calendar.MONTH) + 1)
-                + "/" + calendar.get(Calendar.YEAR);
-        return result;
+	Calendar calendar = Calendar.getInstance();
+	calendar.setTime(getStartDate());
+	String result = calendar.get(Calendar.DAY_OF_MONTH) + "/" + (calendar.get(Calendar.MONTH) + 1)
+		+ "/" + calendar.get(Calendar.YEAR);
+	return result;
     }
 
     public Specialization getSpecialization() {
-        return studentCurricularPlan.getSpecialization();
+	return studentCurricularPlan.getSpecialization();
     }
 
     public Double getClassification() {
-        return studentCurricularPlan.getClassification();
+	return studentCurricularPlan.getClassification();
     }
 
     public Integer getEnrolledCourses() {
-        return studentCurricularPlan.getEnrolledCourses();
+	return studentCurricularPlan.getEnrolledCourses();
     }
 
     public Integer getCompletedCourses() {
-        return studentCurricularPlan.getCompletedCourses();
+	return studentCurricularPlan.getCompletedCourses();
     }
 
     public List getInfoEnrolments() {
-    	final List<InfoEnrolment> infoEnrolments = new ArrayList<InfoEnrolment>();
-    	for (final Enrolment enrolment : studentCurricularPlan.getEnrolmentsSet()) {
-    		infoEnrolments.add(InfoEnrolment.newInfoFromDomain(enrolment));
-    	}
-        return infoEnrolments;
+	final List<InfoEnrolment> infoEnrolments = new ArrayList<InfoEnrolment>();
+	for (final Enrolment enrolment : studentCurricularPlan.getEnrolmentsSet()) {
+	    infoEnrolments.add(InfoEnrolment.newInfoFromDomain(enrolment));
+	}
+	return infoEnrolments;
     }
 
     public List getInfoEnrolmentsSorted() {
-        final List<InfoEnrolment> infoEnrolments = getInfoEnrolments();
-        ComparatorChain comparatorChain = new ComparatorChain();
-        comparatorChain.addComparator(new BeanComparator("infoExecutionPeriod.infoExecutionYear.year"));
-        comparatorChain.addComparator(new BeanComparator("infoExecutionPeriod.semester"));
-        comparatorChain.addComparator(new BeanComparator("infoCurricularCourse.name", Collator.getInstance()));
-        Collections.sort(infoEnrolments, comparatorChain);
-        return infoEnrolments;
+	final List<InfoEnrolment> infoEnrolments = getInfoEnrolments();
+	ComparatorChain comparatorChain = new ComparatorChain();
+	comparatorChain.addComparator(new BeanComparator("infoExecutionPeriod.infoExecutionYear.year"));
+	comparatorChain.addComparator(new BeanComparator("infoExecutionPeriod.semester"));
+	comparatorChain.addComparator(new BeanComparator("infoCurricularCourse.name", Collator
+		.getInstance()));
+	Collections.sort(infoEnrolments, comparatorChain);
+	return infoEnrolments;
     }
 
     public int compareTo(Object arg0) {
-        InfoStudentCurricularPlan obj0 = (InfoStudentCurricularPlan) arg0;
-        return obj0.getCurrentState().compareTo(this.getCurrentState());
+	InfoStudentCurricularPlan obj0 = (InfoStudentCurricularPlan) arg0;
+	return obj0.getCurrentState().compareTo(this.getCurrentState());
     }
 
     public InfoPerson getInfoEmployee() {
-    	final Employee employee = studentCurricularPlan.getEmployee();
-    	return employee == null ? null : InfoPerson.newInfoFromDomain(employee.getPerson());
+	final Employee employee = studentCurricularPlan.getEmployee();
+	return employee == null ? null : InfoPerson.newInfoFromDomain(employee.getPerson());
     }
 
     public Date getWhen() {
-        return studentCurricularPlan.getWhen();
+	return studentCurricularPlan.getWhen();
     }
 
     public String getObservations() {
-        return studentCurricularPlan.getObservations();
+	return studentCurricularPlan.getObservations();
     }
 
     public InfoBranch getInfoSecundaryBranch() {
-        return InfoBranch.newInfoFromDomain(studentCurricularPlan.getSecundaryBranch());
+	return InfoBranch.newInfoFromDomain(studentCurricularPlan.getSecundaryBranch());
     }
 
-    public static InfoStudentCurricularPlan newInfoFromDomain(final StudentCurricularPlan studentCurricularPlan) {
-    	return studentCurricularPlan == null ? null : new InfoStudentCurricularPlan(studentCurricularPlan);
+    public static InfoStudentCurricularPlan newInfoFromDomain(
+	    final StudentCurricularPlan studentCurricularPlan) {
+	return studentCurricularPlan == null ? null : new InfoStudentCurricularPlan(
+		studentCurricularPlan);
     }
 
     public Double getGivenCredits() {
-    	return studentCurricularPlan.getGivenCredits();
+	return studentCurricularPlan.getGivenCredits();
     }
 
     public List<InfoNotNeedToEnrollInCurricularCourse> getInfoNotNeedToEnrollCurricularCourses() {
-    	final List<InfoNotNeedToEnrollInCurricularCourse> infoNotNeedToEnrollInCurricularCourses = new ArrayList<InfoNotNeedToEnrollInCurricularCourse>();
-    	for (final NotNeedToEnrollInCurricularCourse notNeedToEnrollInCurricularCourse : studentCurricularPlan.getNotNeedToEnrollCurricularCoursesSet()) {
-    		infoNotNeedToEnrollInCurricularCourses.add(InfoNotNeedToEnrollInCurricularCourse.newInfoFromDomain(notNeedToEnrollInCurricularCourse));
-    	}
-    	return infoNotNeedToEnrollInCurricularCourses;
+	final List<InfoNotNeedToEnrollInCurricularCourse> infoNotNeedToEnrollInCurricularCourses = new ArrayList<InfoNotNeedToEnrollInCurricularCourse>();
+	for (final NotNeedToEnrollInCurricularCourse notNeedToEnrollInCurricularCourse : studentCurricularPlan
+		.getNotNeedToEnrollCurricularCoursesSet()) {
+	    infoNotNeedToEnrollInCurricularCourses.add(InfoNotNeedToEnrollInCurricularCourse
+		    .newInfoFromDomain(notNeedToEnrollInCurricularCourse));
+	}
+	return infoNotNeedToEnrollInCurricularCourses;
     }
 
-	@Override
-	public Integer getIdInternal() {
-		return studentCurricularPlan.getIdInternal();
-	}
+    @Override
+    public Integer getIdInternal() {
+	return studentCurricularPlan.getIdInternal();
+    }
 
     @Override
     public void setIdInternal(Integer integer) {
-        throw new Error("Method should not be called!");
+	throw new Error("Method should not be called!");
     }
 
 }
