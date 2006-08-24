81d1fd85812ea562584b450c518edd315c58d3f9
João Florindo <jdnf@linkare.pt>
2006-08-24 07:21:16 +0000

    corrected file indentation

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoAnnouncement.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoAnnouncement.java
@@ -12,81 +12,81 @@ import net.sourceforge.fenixedu.domain.Announcement;
 
 public class InfoAnnouncement extends InfoObject implements Comparable, ISiteComponent {
 
-	private final Announcement announcement;
+    private final Announcement announcement;
 
     public InfoAnnouncement(final Announcement announcement) {
-    	this.announcement = announcement;
+	this.announcement = announcement;
     }
 
     public boolean equals(Object obj) {
-    	return obj != null && announcement == ((InfoAnnouncement) obj).announcement;
+	return obj != null && announcement == ((InfoAnnouncement) obj).announcement;
     }
 
     public String toString() {
-    	return announcement.toString();
+	return announcement.toString();
     }
 
     public Date getCreationDate() {
-        return announcement.getCreationDate();
+	return announcement.getCreationDate();
     }
 
     public String getInformation() {
-        return announcement.getInformation();
+	return announcement.getInformation();
     }
 
     public InfoSite getInfoSite() {
-        return InfoSite.newInfoFromDomain(announcement.getSite());
+	return InfoSite.newInfoFromDomain(announcement.getSite());
     }
 
     public Date getLastModifiedDate() {
-        return announcement.getLastModifiedDate();
+	return announcement.getLastModifiedDate();
     }
 
     public String getTitle() {
-        return announcement.getTitle();
+	return announcement.getTitle();
     }
 
     public int compareTo(Object arg0) {
-        if (getYoungerDate().after(((InfoAnnouncement) arg0).getYoungerDate())) {
-            return -1;
-        }
-        if (getYoungerDate().before(((InfoAnnouncement) arg0).getYoungerDate())) {
-            return 1;
-        }
-        return 0;
+	if (getYoungerDate().after(((InfoAnnouncement) arg0).getYoungerDate())) {
+	    return -1;
+	}
+	if (getYoungerDate().before(((InfoAnnouncement) arg0).getYoungerDate())) {
+	    return 1;
+	}
+	return 0;
 
     }
 
     public String getCreationDateFormatted() {
-        String result = getCreationDate().toString();
-        return result.substring(0, result.indexOf("."));
+	String result = getCreationDate().toString();
+	return result.substring(0, result.indexOf("."));
     }
 
     public String getLastModifiedDateFormatted() {
-        String result = getLastModifiedDate().toString();
-        return result.substring(0, result.indexOf("."));
+	String result = getLastModifiedDate().toString();
+	return result.substring(0, result.indexOf("."));
     }
 
     private Date getYoungerDate() {
-        if (getLastModifiedDate() != null && getLastModifiedDate().after(getCreationDate())) {
-            return getLastModifiedDate();
-        }
-        return getCreationDate();
+	if (getLastModifiedDate() != null && getLastModifiedDate().after(getCreationDate())) {
+	    return getLastModifiedDate();
+	}
+	return getCreationDate();
 
     }
 
     public static InfoAnnouncement newInfoFromDomain(Announcement announcement) {
-    	return announcement == null ? null : new InfoAnnouncement(announcement);
+	return announcement == null ? null : new InfoAnnouncement(announcement);
     }
 
-	@Override
-	public Integer getIdInternal() {
-		return announcement.getIdInternal();
-	}
+    @Override
+    public Integer getIdInternal() {
+	return announcement.getIdInternal();
+    }
 
     @Override
     public void setIdInternal(Integer integer) {
-        throw new Error("Method should not be called!");
+	throw new Error("Method should not be called!");
     }
 
 }
--- src/net/sourceforge/fenixedu/domain/BibliographicReference.java
+++ src/net/sourceforge/fenixedu/domain/BibliographicReference.java
@@ -1,31 +1,28 @@
 package net.sourceforge.fenixedu.domain;
 
-/**
- * @author PTRLV
- *  
- */
 public class BibliographicReference extends BibliographicReference_Base {
 
     public BibliographicReference() {
-		super();
-		setRootDomainObject(RootDomainObject.getInstance());
-	}
-
-	public void edit(final String title, final String authors, final String reference, final String year, final Boolean optional) {
-        if (title == null || authors == null || reference == null || year == null || optional == null)
-            throw new NullPointerException();
-        
-        setTitle(title);
-        setAuthors(authors);
-        setReference(reference);
-        setYear(year);
-        setOptional(optional);
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
-    
+
+    public void edit(final String title, final String authors, final String reference,
+	    final String year, final Boolean optional) {
+	if (title == null || authors == null || reference == null || year == null || optional == null)
+	    throw new NullPointerException();
+
+	setTitle(title);
+	setAuthors(authors);
+	setReference(reference);
+	setYear(year);
+	setOptional(optional);
+    }
+
     public void delete() {
-        removeExecutionCourse();        
-        removeRootDomainObject();
-        super.deleteDomainObject();
+	removeExecutionCourse();
+	removeRootDomainObject();
+	super.deleteDomainObject();
     }
 
 }
