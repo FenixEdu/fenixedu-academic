4eefb4a7fa5a57ed0a99784a73fb9acfc1bfd773
Shezad Anavarali <shezad@ist.utl.pt>
2006-08-31 11:44:36 +0000

    Added constructor

--- src/net/sourceforge/fenixedu/domain/student/RegistrationDataByPeriod.java
+++ src/net/sourceforge/fenixedu/domain/student/RegistrationDataByPeriod.java
@@ -1,9 +1,14 @@
 package net.sourceforge.fenixedu.domain.student;
 
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+
 public class RegistrationDataByPeriod extends RegistrationDataByPeriod_Base {
     
     public  RegistrationDataByPeriod() {
         super();
+        setRootDomainObject(RootDomainObject.getInstance());
+        setExecutionPeriod(ExecutionPeriod.readActualExecutionPeriod());
     }
     
 }
