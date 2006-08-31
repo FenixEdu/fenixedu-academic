bacdf447913b798d02ac7f9fa98cafca22af6de6
Shezad Anavarali <shezad@ist.utl.pt>
2006-08-31 10:56:59 +0000

    initial version

---
+++ src/net/sourceforge/fenixedu/domain/student/RegistrationDataByPeriod.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.domain.student;
+
+public class RegistrationDataByPeriod extends RegistrationDataByPeriod_Base {
+    
+    public  RegistrationDataByPeriod() {
+        super();
+    }
+    
+}
