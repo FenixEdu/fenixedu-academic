e12d16f585e7aaae341d2555fdf0bc701e88b3ff
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-09-01 12:52:19 +0000

    delete 5th year enrolments

--- src/net/sourceforge/fenixedu/applicationTier/Servico/enrollment/DeleteEnrolment.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/enrollment/DeleteEnrolment.java
@@ -1,11 +1,14 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.enrollment;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.domain.CurricularYear;
 import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
@@ -30,10 +33,10 @@ public class DeleteEnrolment extends Service {
         final StudentCurricularPlan studentCurricularPlan = registration.getActiveStudentCurricularPlan();
         final Enrolment enrollment1 = studentCurricularPlan.findEnrolmentByEnrolmentID(enrolmentID);
 
-        List<Enrolment> enrollments2Delete = new ArrayList<Enrolment>();
-        List studentEnrolledEnrollmentsInExecutionPeriod = enrollment1.getStudentCurricularPlan()
+        Set<Enrolment> enrollments2Delete = new HashSet<Enrolment>();
+        List<Enrolment> studentEnrolledEnrollmentsInExecutionPeriod = enrollment1.getStudentCurricularPlan()
                 .getAllStudentEnrolledEnrollmentsInExecutionPeriod(enrollment1.getExecutionPeriod());
-        List finalEnrollments2Delete = new ArrayList();
+        Set finalEnrollments2Delete = new HashSet();
         enrollments2Delete.addAll(CollectionUtils.select(studentEnrolledEnrollmentsInExecutionPeriod,
                 new Predicate() {
 
@@ -75,6 +78,18 @@ public class DeleteEnrolment extends Service {
                 }
             }
         }
+        
+        //Código q apaga as cadeiras do 5º ano, caso seja apagada uma cadeira do 5º ano
+        CurricularYear curricularYear = enrollment1.getCurricularCourse().getCurricularYearByBranchAndSemester(enrollment1.getStudentCurricularPlan().getBranch(), enrollment1.getExecutionPeriod().getSemester());
+        if(curricularYear.getYear().equals(5)) {
+            for (Enrolment enrolment : studentEnrolledEnrollmentsInExecutionPeriod) {
+        	if(enrolment.getCurricularCourse().getCurricularYearByBranchAndSemester(enrolment.getStudentCurricularPlan().getBranch(), enrolment.getExecutionPeriod().getSemester()).getYear().equals(5)){
+        	    finalEnrollments2Delete.add(enrolment);
+        	}
+	    }
+        }
+        //
+        
         finalEnrollments2Delete.add(enrollment1);
         finalEnrollments2Delete.addAll(enrollments2Delete);
         Iterator<Enrolment> iter = finalEnrollments2Delete.iterator();
