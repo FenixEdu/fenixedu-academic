9ed12fdde8e63681c176fa5526e860af37482151
Luis Cruz <luis.cruz@ist.utl.pt>
2006-09-05 23:27:00 +0000

    Created external service for setting user emails.

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/externalServices/SetEmail.java
@@ -0,0 +1,79 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.externalServices;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.User;
+
+public class SetEmail extends Service {
+
+    public class NotAuthorizedException extends FenixServiceException {
+    }
+
+    public class UserAlreadyHasEmailException extends FenixServiceException {
+
+	private final String email;
+
+	public UserAlreadyHasEmailException(final String email) {
+	    this.email = email;
+	}
+
+	public String getEmail() {
+	    return email;
+	}
+
+    }
+
+    public class UserDoesNotExistException extends FenixServiceException {
+
+    }
+
+    private static final Set<String> allowedHosts = new HashSet<String>();
+    private static final String password;
+    static {
+	final String allowedHostString = PropertiesManager.getProperty("email.admin.allowed.hosts");
+	if (allowedHostString != null) {
+	    final String[] allowedHostTokens = allowedHostString.split(",");
+	    for (int i = 0; i < allowedHostTokens.length; i++) {
+		allowedHosts.add(allowedHostTokens[i]);
+	    }
+	}
+	password = PropertiesManager.getProperty("email.admin.password");
+    }
+
+    public static boolean isAllowed(final String host, final String ip, final String password) {
+	return SetEmail.password != null && SetEmail.password.equals(password) &&
+		(allowedHosts.contains(host) || allowedHosts.contains(ip));
+    }
+
+    private void set(final String userUId, final String email) throws FenixServiceException {
+	final User user = User.readUserByUserUId(userUId);
+	if (user == null) {
+	    throw new UserDoesNotExistException();
+	}
+	final Person person = user.getPerson();
+	if (person == null) {
+	    throw new UserDoesNotExistException();
+	}
+	final String currentEmail = person.getEmail();
+	if (currentEmail == null || currentEmail.length() == 0) {
+	    person.setEmail(email);
+	} else {
+	    throw new UserAlreadyHasEmailException(currentEmail);
+	}
+    }
+
+    public void run(final String host, final String ip, final String password, final String userUId, final String email)
+    		throws FenixServiceException {
+	if (isAllowed(host, ip, password)) {
+	    set(userUId, email);
+	} else {
+	    throw new NotAuthorizedException();
+	}
+    }
+
+}
---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/externalServices/SetEmail.java
@@ -0,0 +1,48 @@
+package net.sourceforge.fenixedu.presentationTier.Action.externalServices;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.fenixedu.applicationTier.Servico.externalServices.SetEmail.NotAuthorizedException;
+import net.sourceforge.fenixedu.applicationTier.Servico.externalServices.SetEmail.UserAlreadyHasEmailException;
+import net.sourceforge.fenixedu.applicationTier.Servico.externalServices.SetEmail.UserDoesNotExistException;
+import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+public class SetEmail extends FenixDispatchAction {
+
+    public ActionForward setEmail(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+	final String host = request.getRemoteHost();
+	final String ip = request.getRemoteAddr();
+	final String password = request.getParameter("password");
+	final String userUId = request.getParameter("userUId");
+	final String email = request.getParameter("email");
+
+	String message = "ko";
+
+	try {
+	    final Object[] args = new Object[] { host, ip, password, userUId, email };
+	    executeService(request, "SetEmail", args);
+	    message = "ok";
+	} catch (NotAuthorizedException ex) {
+	    message = "Not authorized";
+	} catch (UserAlreadyHasEmailException ex) {
+	    message = "User already has email.";
+	} catch (UserDoesNotExistException ex) {
+	    message = "User does not exist.";
+	} finally {
+	    final ServletOutputStream servletOutputStream = response.getOutputStream();
+	    response.setContentType("text/html");
+	    servletOutputStream.print(message);
+	    servletOutputStream.flush();
+	    response.flushBuffer();
+	}
+
+	return null;
+    }
+
+}
