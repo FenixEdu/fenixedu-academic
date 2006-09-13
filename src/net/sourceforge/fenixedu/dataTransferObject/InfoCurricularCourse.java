db278bf53b3fbe1f3c6a8ba9bd6042b0eca39d34
Luís Egídio <luis.egidio@ist.utl.pt>
2006-09-13 13:11:11 +0000

    understand the need of rendering info in english from inspection of the user language

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoCurricularCourse.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoCurricularCourse.java
@@ -13,8 +13,10 @@ import java.util.Locale;
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.DomainReference;
 import net.sourceforge.fenixedu.domain.GradeScale;
+import net.sourceforge.fenixedu.domain.Language;
 import net.sourceforge.fenixedu.domain.curriculum.CurricularCourseType;
 import net.sourceforge.fenixedu.util.CurricularCourseExecutionScope;
+import net.sourceforge.fenixedu.util.LanguageUtils;
 
 /**
  * @author tfc130
@@ -23,242 +25,244 @@ public class InfoCurricularCourse extends InfoObject implements Comparable, ISit
 
     private final DomainReference<CurricularCourse> curricularCourseDomainReference;
 
-    private boolean showEnVersion = false;
-
+    private final boolean showEnVersion = (LanguageUtils.getUserLanguage() == Language.en); 
+    
     private List<InfoCurricularCourseScope> infoScopes;
 
     private List infoAssociatedExecutionCourses;
 
     private CurricularCourseType type;
 
-    private CurricularCourseExecutionScope curricularCourseExecutionScope;
-
     private InfoUniversity infoUniversity;
 
     private String chosen;
 
     public InfoCurricularCourse(final CurricularCourse curricularCourse) {
-    	curricularCourseDomainReference = new DomainReference<CurricularCourse>(curricularCourse);
+	curricularCourseDomainReference = new DomainReference<CurricularCourse>(curricularCourse);
     }
 
     public CurricularCourse getCurricularCourse() {
-        return curricularCourseDomainReference == null ? null : curricularCourseDomainReference.getObject();
+	return curricularCourseDomainReference == null ? null : curricularCourseDomainReference
+		.getObject();
     }
 
     public Boolean getBasic() {
-        return getCurricularCourse().getBasic();
+	return getCurricularCourse().getBasic();
     }
 
     public String getOwnershipType() {
-    	return getBasic() == null ? "" : getBasic().booleanValue() ? "Básica" : "Não Básica";
+	return getBasic() == null ? "" : getBasic().booleanValue() ? "Básica" : "Não Básica";
     }
 
     public boolean equals(Object obj) {
-    	return obj instanceof InfoCurricularCourse && getCurricularCourse() == ((InfoCurricularCourse) obj).getCurricularCourse();
+	return obj instanceof InfoCurricularCourse
+		&& getCurricularCourse() == ((InfoCurricularCourse) obj).getCurricularCourse();
     }
 
     public String toString() {
-    	return getCurricularCourse().toString();
+	return getCurricularCourse().toString();
     }
 
     public String getCode() {
-        return getCurricularCourse().getCode();
+	return getCurricularCourse().getCode();
     }
 
     public Double getCredits() {
-        return getCurricularCourse().getCredits();
+	return getCurricularCourse().getCredits();
     }
 
     public Double getLabHours() {
-        return getCurricularCourse().getLabHours();
+	return getCurricularCourse().getLabHours();
     }
 
     public Double getPraticalHours() {
-        return getCurricularCourse().getPraticalHours();
+	return getCurricularCourse().getPraticalHours();
     }
 
     public Double getTheoPratHours() {
-        return getCurricularCourse().getTheoPratHours();
+	return getCurricularCourse().getTheoPratHours();
     }
 
     public Double getTheoreticalHours() {
-        return getCurricularCourse().getTheoreticalHours();
+	return getCurricularCourse().getTheoreticalHours();
     }
 
     public InfoDegreeCurricularPlan getInfoDegreeCurricularPlan() {
-    	final InfoDegreeCurricularPlan infoDegreeCurricularPlan = InfoDegreeCurricularPlan.newInfoFromDomain(getCurricularCourse().getDegreeCurricularPlan());
-    	if (showEnVersion) {
-    		infoDegreeCurricularPlan.prepareEnglishPresentation(Locale.ENGLISH);
-    	}
-    	return infoDegreeCurricularPlan;
+	final InfoDegreeCurricularPlan infoDegreeCurricularPlan = InfoDegreeCurricularPlan
+		.newInfoFromDomain(getCurricularCourse().getDegreeCurricularPlan());
+	if (showEnVersion) {
+	    infoDegreeCurricularPlan.prepareEnglishPresentation(Locale.ENGLISH);
+	}
+	return infoDegreeCurricularPlan;
     }
 
     public List getInfoScopes() {
-        return infoScopes;
+	return infoScopes;
     }
 
     public void setInfoScopes(List<InfoCurricularCourseScope> infoScopes) {
-        this.infoScopes = infoScopes;
+	this.infoScopes = infoScopes;
     }
 
     public CurricularCourseType getType() {
-        return getCurricularCourse().getType();
+	return getCurricularCourse().getType();
     }
 
     public CurricularCourseExecutionScope getCurricularCourseExecutionScope() {
-        return getCurricularCourse().getCurricularCourseExecutionScope();
+	return getCurricularCourse().getCurricularCourseExecutionScope();
     }
 
     public Boolean getMandatory() {
-        return getCurricularCourse().getMandatory();
+	return getCurricularCourse().getMandatory();
     }
 
     public boolean infoCurricularCourseIsMandatory() {
-        return getMandatory().booleanValue();
+	return getMandatory().booleanValue();
     }
 
     public InfoCurricularCourseScope getInfoCurricularCourseScope(InfoBranch infoBranch, Integer semester) {
-        InfoCurricularCourseScope infoCurricularCourseScope = null;
-        Iterator iterator = this.getInfoScopes().iterator();
-        while (iterator.hasNext()) {
-            InfoCurricularCourseScope infoCurricularCourseScope2 = (InfoCurricularCourseScope) iterator
-                    .next();
-            if (infoCurricularCourseScope2.getInfoBranch().equals(infoBranch)
-                    && infoCurricularCourseScope2.getInfoCurricularSemester().getSemester().equals(
-                            semester)) {
-                infoCurricularCourseScope = infoCurricularCourseScope2;
-                break;
-            }
-        }
-        return infoCurricularCourseScope;
+	InfoCurricularCourseScope infoCurricularCourseScope = null;
+	Iterator iterator = this.getInfoScopes().iterator();
+	while (iterator.hasNext()) {
+	    InfoCurricularCourseScope infoCurricularCourseScope2 = (InfoCurricularCourseScope) iterator
+		    .next();
+	    if (infoCurricularCourseScope2.getInfoBranch().equals(infoBranch)
+		    && infoCurricularCourseScope2.getInfoCurricularSemester().getSemester().equals(
+			    semester)) {
+		infoCurricularCourseScope = infoCurricularCourseScope2;
+		break;
+	    }
+	}
+	return infoCurricularCourseScope;
     }
 
     public InfoUniversity getInfoUniversity() {
-        return infoUniversity;
+	return infoUniversity;
     }
 
     public void setInfoUniversity(InfoUniversity university) {
-        this.infoUniversity = university;
+	this.infoUniversity = university;
     }
 
     public int compareTo(Object arg0) {
-        int result = 0;
-        if (getMinScope() < ((InfoCurricularCourse) arg0).getMinScope()) {
-            result = -1;
-        } else if (getMinScope() > ((InfoCurricularCourse) arg0).getMinScope()) {
-            return 1;
-        }
-        return result;
+	int result = 0;
+	if (getMinScope() < ((InfoCurricularCourse) arg0).getMinScope()) {
+	    result = -1;
+	} else if (getMinScope() > ((InfoCurricularCourse) arg0).getMinScope()) {
+	    return 1;
+	}
+	return result;
     }
 
     private int getMinScope() {
-        int minScope = 0;
-        List scopes = getInfoScopes();
-        Iterator iter = scopes.iterator();
-        while (iter.hasNext()) {
-            InfoCurricularCourseScope infoScope = (InfoCurricularCourseScope) iter.next();
-            if (minScope == 0
-                    || minScope > infoScope.getInfoCurricularSemester().getInfoCurricularYear()
-                            .getYear().intValue()) {
-                minScope = infoScope.getInfoCurricularSemester().getInfoCurricularYear().getYear()
-                        .intValue();
-            }
-        }
+	int minScope = 0;
+	List scopes = getInfoScopes();
+	Iterator iter = scopes.iterator();
+	while (iter.hasNext()) {
+	    InfoCurricularCourseScope infoScope = (InfoCurricularCourseScope) iter.next();
+	    if (minScope == 0
+		    || minScope > infoScope.getInfoCurricularSemester().getInfoCurricularYear()
+			    .getYear().intValue()) {
+		minScope = infoScope.getInfoCurricularSemester().getInfoCurricularYear().getYear()
+			.intValue();
+	    }
+	}
 
-        return minScope;
+	return minScope;
     }
 
     public List getInfoAssociatedExecutionCourses() {
-        return infoAssociatedExecutionCourses;
+	return infoAssociatedExecutionCourses;
     }
 
     public void setInfoAssociatedExecutionCourses(List infoAssociatedExecutionCourses) {
-        this.infoAssociatedExecutionCourses = infoAssociatedExecutionCourses;
+	this.infoAssociatedExecutionCourses = infoAssociatedExecutionCourses;
     }
 
     public String getChosen() {
-        return chosen;
+	return chosen;
     }
 
     public void setChosen(String chosen) {
-        this.chosen = chosen;
+	this.chosen = chosen;
     }
 
     public Double getEctsCredits() {
-        return getCurricularCourse().getEctsCredits();
+	return getCurricularCourse().getEctsCredits();
     }
 
     public Integer getEnrollmentWeigth() {
-        return getCurricularCourse().getEnrollmentWeigth();
+	return getCurricularCourse().getEnrollmentWeigth();
     }
 
     public Integer getMaximumValueForAcumulatedEnrollments() {
-        return getCurricularCourse().getMaximumValueForAcumulatedEnrollments();
+	return getCurricularCourse().getMaximumValueForAcumulatedEnrollments();
     }
 
     public Integer getMinimumValueForAcumulatedEnrollments() {
-        return getCurricularCourse().getMinimumValueForAcumulatedEnrollments();
+	return getCurricularCourse().getMinimumValueForAcumulatedEnrollments();
     }
 
     public Double getWeigth() {
-        return getCurricularCourse().getWeigth();
+	return getCurricularCourse().getWeigth();
     }
 
     public Boolean getMandatoryEnrollment() {
-        return getCurricularCourse().getMandatoryEnrollment();
+	return getCurricularCourse().getMandatoryEnrollment();
     }
 
     public Boolean getEnrollmentAllowed() {
-        return getCurricularCourse().getEnrollmentAllowed();
+	return getCurricularCourse().getEnrollmentAllowed();
     }
 
-    public String getAcronym(){
-    	return getCurricularCourse().getAcronym();
+    public String getAcronym() {
+	return getCurricularCourse().getAcronym();
     }
-    
+
     public static InfoCurricularCourse newInfoFromDomain(CurricularCourse curricularCourse) {
-        InfoCurricularCourse infoCurricularCourse = null;
-        if (curricularCourse != null) {
-            infoCurricularCourse = new InfoCurricularCourse(curricularCourse);
-            infoCurricularCourse.copyFromDomain(curricularCourse);
-        }
-        return infoCurricularCourse;
+	InfoCurricularCourse infoCurricularCourse = null;
+	if (curricularCourse != null) {
+	    infoCurricularCourse = new InfoCurricularCourse(curricularCourse);
+	    infoCurricularCourse.copyFromDomain(curricularCourse);
+	}
+	return infoCurricularCourse;
     }
 
     public String getName() {
-    	return showEnVersion && getCurricularCourse().getNameEn() != null && getCurricularCourse().getNameEn().length() > 0 
-    			? getCurricularCourse().getNameEn() : getCurricularCourse().getName();
+	return showEnVersion && getCurricularCourse().getNameEn() != null
+		&& getCurricularCourse().getNameEn().length() > 0 ? getCurricularCourse().getNameEn()
+		: getCurricularCourse().getName();
     }
 
     public String getNameEn() {
-        return getCurricularCourse().getNameEn();
-    }
-    
-    public void prepareEnglishPresentation(Locale locale) {
-        if (locale.getLanguage().equals(Locale.ENGLISH.getLanguage())) {
-        	showEnVersion = true;
-            
-            for (InfoCurricularCourseScope infoCurricularCourseScope : this.infoScopes) {
-                infoCurricularCourseScope.prepareEnglishPresentation(locale);
-            }
-        }
-    }
-    
-    public String getNameAndCode() {
-        return getCode() + " - " + getName();
+	return getCurricularCourse().getNameEn();
     }
 
-	public GradeScale getGradeScale() {
-		return getCurricularCourse().getGradeScale();
+    public void prepareEnglishPresentation(Locale locale) {
+	if (locale.getLanguage().equals(Locale.ENGLISH.getLanguage())) {
+	    if (this.infoScopes != null) {
+		for (InfoCurricularCourseScope infoCurricularCourseScope : this.infoScopes) {
+		    infoCurricularCourseScope.prepareEnglishPresentation(locale);
+		}
+	    }
 	}
+    }
+
+    public String getNameAndCode() {
+	return getCode() + " - " + getName();
+    }
+
+    public GradeScale getGradeScale() {
+	return getCurricularCourse().getGradeScale();
+    }
 
     @Override
     public Integer getIdInternal() {
-        return getCurricularCourse().getIdInternal();
+	return getCurricularCourse().getIdInternal();
     }
 
     public boolean getIsBolonha() {
-        return getCurricularCourse().isBolonha();
+	return getCurricularCourse().isBolonha();
     }
 
 }
