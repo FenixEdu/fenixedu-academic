a25e3ef0a8de2869a6c5fc5a15a37fa51eb0c746
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-09-14 15:46:37 +0000

    added more attributes to the basic component

--- src/net/sourceforge/fenixedu/renderers/components/HtmlTextArea.java
+++ src/net/sourceforge/fenixedu/renderers/components/HtmlTextArea.java
@@ -9,6 +9,10 @@ public class HtmlTextArea extends HtmlSimpleValueComponent {
     private Integer rows;
     private Integer columns;
     private boolean readOnly;
+
+    private String onFocus;
+    private String onBlur;
+    private String onSelect;
     private String onChange;
     
     public Integer getColumns() {
@@ -35,6 +39,30 @@ public class HtmlTextArea extends HtmlSimpleValueComponent {
         this.rows = rows;
     }
 
+    public String getOnBlur() {
+        return this.onBlur;
+    }
+
+    public void setOnBlur(String onBlur) {
+        this.onBlur = onBlur;
+    }
+
+    public String getOnFocus() {
+        return this.onFocus;
+    }
+
+    public void setOnFocus(String onFocus) {
+        this.onFocus = onFocus;
+    }
+
+    public String getOnSelect() {
+        return this.onSelect;
+    }
+
+    public void setOnSelect(String onSelect) {
+        this.onSelect = onSelect;
+    }
+
     public String getOnChange() {
         return onChange;
     }
@@ -52,7 +80,10 @@ public class HtmlTextArea extends HtmlSimpleValueComponent {
         
         tag.setAttribute("rows", getRows());
         tag.setAttribute("cols", getColumns());
-        tag.setAttribute("onChange", getOnChange());
+        tag.setAttribute("onfocus", getOnFocus());
+        tag.setAttribute("onblur", getOnBlur());
+        tag.setAttribute("onselect", getOnSelect());
+        tag.setAttribute("onchange", getOnChange());
         
         if (isReadOnly()) {
             tag.setAttribute("readonly", "readonly");
