9135405a37649c794a8c66aba35e402e1333cb2c
Luís Santos <lmhs@ist.utl.pt>
2006-09-18 12:02:15 +0000

    Changes on the text returned for each person.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/research/SearchPersonsForParticipations.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/research/SearchPersonsForParticipations.java
@@ -2,7 +2,9 @@ package net.sourceforge.fenixedu.applicationTier.Servico.research;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import net.sourceforge.fenixedu.applicationTier.Servico.commons.SearchObjects;
 import net.sourceforge.fenixedu.domain.DomainObject;
@@ -28,8 +30,12 @@ public class SearchPersonsForParticipations extends SearchObjects {
         private Integer idInternal;
         private String text;
 
-        public PersonWrapper(Person person) {
+        public PersonWrapper() {
             super();
+        }
+        
+        public PersonWrapper(Person person) {
+            this();
             this.idInternal = person.getIdInternal();
             this.text = buildText(person);
         }
@@ -37,25 +43,53 @@ public class SearchPersonsForParticipations extends SearchObjects {
 	public Integer getIdInternal() {
             return idInternal;
         }
-        
+	public void setIdInternal(Integer idInternal) {
+	    this.idInternal = idInternal;
+	}
         public String getText() {
             return text;
         }
-        
-        private String buildText(Person person) {
-            StringBuilder text = new StringBuilder();
-            text.append(person.getName());
-            text.append(" - ");
-            
-	    if (person.getExternalPerson() == null) {
-		text.append((person.getUsername() != null) ? person.getUsername() : "Interno");
-	    }
-	    else {
-		text.append((person.getExternalPerson().getInstitutionUnit()!= null) ?
-			person.getExternalPerson().getInstitutionUnit().getName() : "Externo");
-	    }
+	public void setText(String text) {
+	    this.text = text;
+	}
+
+	private String buildText(Person person) {
 	    
-	    return text.toString();
+	    final ResourceBundle bundle = ResourceBundle.getBundle("resources.ResearcherResources",Locale.getDefault());
+            final StringBuilder text = new StringBuilder();
+            text.append(person.getName());
+            
+            if(person.hasTeacher()) {
+        	text.append(" - " + bundle.getString("label.teacher"));
+        	text.append(" - " + bundle.getString("label.number"));
+        	text.append(": " + person.getTeacher().getTeacherNumber());
+        	return text.toString();
+            }
+            if(person.hasExternalPerson()) {
+        	text.append(" - " + bundle.getString("label.externalPerson"));
+        	if(person.getExternalPerson().hasInstitutionUnit()){
+        	    text.append(" - " + bundle.getString("label.institutionUnit"));
+        	    text.append(": " + person.getExternalPerson().getInstitutionUnit().getName());
+        	}
+        	return text.toString();
+            }
+            if(person.hasStudent()) {
+        	text.append(" - " + bundle.getString("label.student"));
+        	text.append(" - " + bundle.getString("label.number"));
+        	text.append(": " + person.getStudent().getNumber());
+        	return text.toString();
+            }
+            if(person.hasEmployee()) {
+        	text.append(" - " + bundle.getString("label.employee"));
+        	text.append(" - " + bundle.getString("label.number"));
+        	text.append(": " + person.getEmployee().getEmployeeNumber());
+        	return text.toString();
+            }
+            if(person.hasIstUsername()) {
+        	text.append(" - " + person.getIstUsername());
+        	return text.toString();
+            }
+            return text.append(" - ???").toString();
 	}
     }
 }
