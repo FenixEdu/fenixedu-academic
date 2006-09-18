dc97425f2053de50cf2f7ff0c4f2e1bfb8e178cc
Luís Santos <lmhs@ist.utl.pt>
2006-09-18 12:03:06 +0000

    FilePath changed.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/research/result/CreateResultDocumentFile.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/research/result/CreateResultDocumentFile.java
@@ -6,6 +6,8 @@ import net.sourceforge.fenixedu.dataTransferObject.research.result.ResultDocumen
 import net.sourceforge.fenixedu.domain.accessControl.Group;
 import net.sourceforge.fenixedu.domain.research.result.Result;
 import net.sourceforge.fenixedu.domain.research.result.ResultDocumentFile;
+import net.sourceforge.fenixedu.domain.research.result.patent.ResultPatent;
+import net.sourceforge.fenixedu.domain.research.result.publication.ResultPublication;
 import pt.utl.ist.fenix.tools.file.FileDescriptor;
 import pt.utl.ist.fenix.tools.file.FileManagerFactory;
 import pt.utl.ist.fenix.tools.file.FileMetadata;
@@ -13,32 +15,44 @@ import pt.utl.ist.fenix.tools.file.FilePath;
 import pt.utl.ist.fenix.tools.file.Node;
 
 public class CreateResultDocumentFile extends Service {
-    public ResultDocumentFile run(ResultDocumentFileSubmissionBean bean) {
+    public void run(ResultDocumentFileSubmissionBean bean) {
         final Result result = bean.getResult();
-        //final String fileName = result.getIdInternal() + String.valueOf(System.currentTimeMillis());
         final String displayName = bean.getDisplayName();
-        final FileMetadata fileMetadata = new FileMetadata(displayName, AccessControl.getUserView().getPerson().getName());
+        
+        final String fileName = result.getIdInternal() + String.valueOf(System.currentTimeMillis());
+        final FileMetadata fileMetadata = new FileMetadata(fileName, AccessControl.getUserView().getPerson().getName());
+        
         final Group permittedGroup = ResultDocumentFile.getPermittedGroup(bean.getPermission());
+        
         final FileDescriptor fileDescriptor = FileManagerFactory.getFileManager().saveFile(
                 getFilePath(result), bean.getFileName(), (permittedGroup != null) ? true : false, fileMetadata, bean.getInputStream());
         
-        return new ResultDocumentFile(result, fileDescriptor.getFilename(), displayName, fileDescriptor.getMimeType(), fileDescriptor
-	                .getChecksum(), fileDescriptor.getChecksumAlgorithm(), fileDescriptor.getSize(),
-	                fileDescriptor.getUniqueId(), permittedGroup);
+        result.addDocumentFile(fileDescriptor.getFilename(), displayName, bean.getPermission(), fileDescriptor.getMimeType(), fileDescriptor
+				        .getChecksum(), fileDescriptor.getChecksumAlgorithm(),
+                fileDescriptor.getSize(), fileDescriptor.getUniqueId(), permittedGroup);
     }
 
     /**
      * @param result
      * @return filePath
      *
-     * Path examples:   "/Result/ResultPublication12345/"
-     *                  "/Result/ResultPatent54321/"
+     * Path examples:   /Research/Results/Publications/pub{idInternal}/
+     *                  /Research/Results/Patents/pat{idInternal}/
      */
     private FilePath getFilePath(Result result) {
         final FilePath filePath = new FilePath();
         
-        filePath.addNode(new Node(Result.class.getSimpleName(), Result.class.getSimpleName()));
-        filePath.addNode(new Node(result.getClass().getSimpleName() + result.getIdInternal(), result.getIdInternal().toString()));
+        filePath.addNode(new Node("Research", "Research"));
+        filePath.addNode(new Node("Results", "Results"));
+        
+        if (result instanceof ResultPublication) {
+            filePath.addNode(new Node("Publications", "Publications"));
+            filePath.addNode(new Node("pub" + result.getIdInternal(), "pub" + result.getIdInternal().toString()));
+        }
+        if (result instanceof ResultPatent){
+            filePath.addNode(new Node("Patents", "Patents"));
+            filePath.addNode(new Node("pat" + result.getIdInternal(), "pat" + result.getIdInternal().toString()));
+        }
         
         return filePath;
     }
