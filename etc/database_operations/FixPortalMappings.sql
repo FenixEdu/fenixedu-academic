dc73cf9d0fe2573a54ff70b918819718ac46a26b
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-09-22 12:29:05 +0000

    correct some errors in the AvailabilityPolicy table and functionalities' mappings

---
+++ etc/database_operations/FixPortalMappings.sql
@@ -0,0 +1,5 @@
+UPDATE FUNCTIONALITY SET PATH = "/sop/paginaPrincipal.do" 
+	WHERE UUID = "6a97cab2-491d-4e9f-a876-3a15d1d1033f";
+	
+UPDATE FUNCTIONALITY SET PATH = "/departmentMember/index.do", PREFIX = "/departmentMember"
+	WHERE UUID = "123c9a8e-de47-4b4f-9543-a38116c03b54"
