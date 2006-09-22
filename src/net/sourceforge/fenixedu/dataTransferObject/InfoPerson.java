e89797ab2be6fdb55c59c9dee6202c1275b2728b
Luís Egídio <luis.egidio@ist.utl.pt>
2006-09-22 15:31:41 +0000

    avoid possible null pointers, return toString of domain object

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoPerson.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoPerson.java
@@ -31,43 +31,7 @@ public class InfoPerson extends InfoObject {
     }
 
     public String toString() {
-        StringBuilder result = new StringBuilder();
-        result.append("Person :\n");
-        result.append("\n  - Identification Document Number : ").append(
-                getNumeroDocumentoIdentificacao());
-        result.append("\n  - Identification Document Type : ").append(getTipoDocumentoIdentificacao());
-        result.append("\n  - Identification Document Issue Place : ").append(
-                getLocalEmissaoDocumentoIdentificacao());
-        result.append("\n  - Identification Document Issue Date : ").append(
-                getDataEmissaoDocumentoIdentificacao());
-        result.append("\n  - Identification Document Expiration Date : ").append(
-                getDataValidadeDocumentoIdentificacao());
-        result.append("\n  - Name : ").append(getNome());
-        ;
-        result.append("\n  - Birth : ").append(getNascimento());
-        result.append("\n  - Father Name : ").append(getNomePai());
-        result.append("\n  - Mother Name : ").append(getNomeMae());
-        result.append("\n  - Nationality : ").append(getInfoPais().getNationality());
-        result.append("\n  - Birth Place Parish : ").append(getFreguesiaNaturalidade());
-        result.append("\n  - Birth Place Municipality : ").append(getConcelhoNaturalidade());
-        result.append("\n  - Birth Place District : ").append(getDistritoNaturalidade());
-        result.append("\n  - Address : ").append(getMorada());
-        result.append("\n  - Place : ").append(getLocalidade());
-        result.append("\n  - Post Code : ").append(getCodigoPostal());
-        result.append("\n  - Address Parish : ").append(getFreguesiaMorada());
-        result.append("\n  - Address Municipality : ").append(getConcelhoMorada());
-        result.append("\n  - Address District : ").append(getDistritoMorada());
-        result.append("\n  - Telephone : ").append(getTelefone());
-        result.append("\n  - MobilePhone : ").append(getTelemovel());
-        result.append("\n  - E-Mail : ").append(getEmail());
-        result.append("\n  - HomePage : ").append(getEnderecoWeb());
-        result.append("\n  - Contributor Number : ").append(getNumContribuinte());
-        result.append("\n  - Username : ").append(getUsername());
-        result.append("\n  - Password : ").append(getPassword());
-        result.append("\n  - Occupation : ").append(getProfissao());
-        result.append("\n  - Codigo Fiscal : ").append(getCodigoFiscal());
-        result.append("\n  - studentList : ").append(getInfoStudentCurricularPlanList());
-        return result.toString();
+	return getPerson().toString();
     }
 
     public String getCodigoFiscal() {
@@ -277,7 +241,7 @@ public class InfoPerson extends InfoObject {
     }
 
     public List<String> getMainRoles() {
-        return getImportantRoles(getPerson(), new ArrayList());
+        return getImportantRoles(getPerson(), new ArrayList<String>());
     }
 
     public String getIstUsername() {
