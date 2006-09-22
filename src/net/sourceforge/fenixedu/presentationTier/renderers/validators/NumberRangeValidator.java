e0cef739525f2df412cf5f7f23f628484d691a8c
João Silva <joao.silva@ist.utl.pt>
2006-09-22 09:34:38 +0000

    Allows to create a validation ranger for integer

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/validators/NumberRangeValidator.java
@@ -0,0 +1,95 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.validators;
+
+import net.sourceforge.fenixedu.renderers.components.Validatable;
+import net.sourceforge.fenixedu.renderers.utils.RenderUtils;
+import net.sourceforge.fenixedu.renderers.validators.RequiredValidator;
+
+public class NumberRangeValidator extends RequiredValidator {
+
+	private boolean isNumber;
+
+	private Integer upperBound;
+
+	private Integer lowerBound;
+
+	private boolean required;
+
+	public NumberRangeValidator(Validatable component) {
+		super(component);
+
+		upperBound = null;
+		lowerBound = null;
+		component.setValidator(this);
+		setRequired(true);
+	}
+
+	public int getLowerBound() {
+		return lowerBound;
+	}
+
+	public void setLowerBound(int lowerBound) {
+		this.lowerBound = lowerBound;
+	}
+
+	public int getUpperBound() {
+		return upperBound;
+	}
+
+	public void setUpperBound(int upperBound) {
+		this.upperBound = upperBound;
+	}
+
+	public boolean isRequired() {
+		return this.required;
+	}
+
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+
+	@Override
+	public void performValidation() {
+		super.performValidation();
+
+		isNumber = this.isValid();
+
+		if (isValid()) {
+			int number = Integer.parseInt(getComponent().getValue().trim());
+
+			boolean inRange = true;
+
+			if (lowerBound != null) {
+				inRange &= lowerBound <= number;
+			}
+
+			if (upperBound != null) {
+				inRange &= upperBound >= number;
+			}
+
+			this.setValid(inRange);
+		} else {
+			setValid(!isRequired());
+		}
+	}
+
+	@Override
+	public String getErrorMessage() {
+		if (!isNumber) {
+			return RenderUtils.getResourceString("renderers.validator.number");
+		}
+
+		if (lowerBound != null && upperBound != null) {
+			return RenderUtils.getFormatedResourceString("renderers.validator.number.range.both",
+					lowerBound, upperBound);
+		}
+
+		if (lowerBound != null) {
+			return RenderUtils.getFormatedResourceString("renderers.validator.number.range.lower",
+					lowerBound);
+		}
+
+		return RenderUtils.getFormatedResourceString("renderers.validator.number.range.upper",
+				upperBound);
+	}
+
+}
