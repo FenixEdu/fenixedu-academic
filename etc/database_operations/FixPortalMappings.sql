ab064b6952f8160195421222d33c62bf2085cea5
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-09-22 16:07:29 +0000

    corrected name for "Envio de E-mails"

--- etc/database_operations/FixPortalMappings.sql
+++ etc/database_operations/FixPortalMappings.sql
@@ -6,3 +6,6 @@ UPDATE FUNCTIONALITY SET PATH = "/departmentMember/index.do", PREFIX = "/departm
 
 UPDATE FUNCTIONALITY SET PARAMETERS = "degreeId,degreeCurricularPlanId,curricularCourseId,executionCourseId,professorShipTeachersIds,responsibleTeachersIds" 
 	WHERE UUID = "81bcaf76-48d3-46e4-9698-87a5eb5b2a0d";
+	
+UPDATE FUNCTIONALITY SET NAME = "pt16:Envio de E-mails"
+	WHERE UUID = "1b93ed76-0947-4628-9a10-adae46846e01";
