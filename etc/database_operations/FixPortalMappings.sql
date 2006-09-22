8f3e077e52554bbefcdf391e461811bd13e5ea72
Cláudio Gil <claudio.gil@ist.utl.pt>
2006-09-22 13:34:04 +0000

    corrected required parameters

--- etc/database_operations/FixPortalMappings.sql
+++ etc/database_operations/FixPortalMappings.sql
@@ -2,4 +2,7 @@ UPDATE FUNCTIONALITY SET PATH = "/sop/paginaPrincipal.do"
 	WHERE UUID = "6a97cab2-491d-4e9f-a876-3a15d1d1033f";
 	
 UPDATE FUNCTIONALITY SET PATH = "/departmentMember/index.do", PREFIX = "/departmentMember"
-	WHERE UUID = "123c9a8e-de47-4b4f-9543-a38116c03b54"
+	WHERE UUID = "123c9a8e-de47-4b4f-9543-a38116c03b54";
+
+UPDATE FUNCTIONALITY SET PARAMETERS = "degreeId,degreeCurricularPlanId,curricularCourseId,executionCourseId,professorShipTeachersIds,responsibleTeachersIds" 
+	WHERE UUID = "81bcaf76-48d3-46e4-9698-87a5eb5b2a0d";
