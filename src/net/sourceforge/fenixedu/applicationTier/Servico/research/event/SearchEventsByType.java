5afa2ee960f3e3c08dedb21921d668eacfd90d06
Sérgio Patrício <sergio.patricio@ist.utl.pt>
2006-09-25 15:42:40 +0000

    search events by type

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/research/event/SearchEventsByType.java
@@ -0,0 +1,36 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.research.event;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.sourceforge.fenixedu.applicationTier.Servico.commons.searchers.SearchObjectsByMultiLanguageString;
+import net.sourceforge.fenixedu.domain.DomainObject;
+import net.sourceforge.fenixedu.domain.research.event.Event;
+import net.sourceforge.fenixedu.domain.research.event.EventType;
+
+public class SearchEventsByType extends SearchObjectsByMultiLanguageString{
+
+    public List<DomainObject> run(Class type, String value, int limit, Map<String, String> arguments) {
+        if (type != Event.class)
+            return null;
+        
+        final List<DomainObject> objects = super.run(type, value, limit, arguments);
+        List<DomainObject> result = new ArrayList<DomainObject>();
+        
+        EventType eventType;
+        try {
+            String eventTypeString = arguments.get("type");
+            eventType = EventType.valueOf(eventTypeString);
+        } catch (Exception e) {
+            return null;
+        }
+        
+        for (Object object : objects) {
+            Event event = (Event) object;
+            if (event.getEventType() == eventType)
+                result.add(event);
+        }
+        return result;
+    }
+}
