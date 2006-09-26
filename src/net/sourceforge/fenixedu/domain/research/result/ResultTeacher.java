5526f2f39969351fb0df3816dbcc965174a8ff5a
Sérgio Patrício <sergio.patricio@ist.utl.pt>
2006-09-26 14:20:23 +0000

    Added ResultTeacher

---
+++ src/net/sourceforge/fenixedu/domain/research/result/ResultTeacher.java
@@ -0,0 +1,43 @@
+package net.sourceforge.fenixedu.domain.research.result;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.Teacher;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.util.PublicationArea;
+
+public class ResultTeacher extends ResultTeacher_Base {
+    
+    public ResultTeacher()
+    {
+        super();
+        setRootDomainObject(RootDomainObject.getInstance());
+    }
+    
+    public ResultTeacher(Result result, Teacher teacher, PublicationArea area) {
+        this();
+        verifyIfParticipator(teacher, result);
+        setPublicationArea(area);
+        setResult(result);
+        setTeacher(teacher);
+    }
+
+    public void delete()
+    {
+        removeResult();
+        removeTeacher();
+        setPublicationArea(null);
+        removeRootDomainObject();
+        super.deleteDomainObject();
+    }
+ 
+    private void verifyIfParticipator(Teacher teacher, Result result) {
+        boolean isParticipator = false;
+        Person author = teacher.getPerson();
+        for(ResultParticipation participation: result.getResultParticipations()) {
+            if (participation.getPerson().equals(author)) isParticipator = true;
+        }
+        if (!isParticipator) throw new DomainException("error.result.teacherNotAuthor");
+    }
+    
+}
