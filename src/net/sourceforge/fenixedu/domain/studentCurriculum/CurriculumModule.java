cd8e4614320cfbe613af9fbb96ac5689739c49fa
Luís Egídio <luis.egidio@ist.utl.pt>
2006-09-27 13:09:05 +0000

    getName

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/CurriculumModule.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/CurriculumModule.java
@@ -1,10 +1,14 @@
 package net.sourceforge.fenixedu.domain.studentCurriculum;
 
 import java.util.List;
+import java.util.Locale;
 
 import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.Language;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.degreeStructure.DegreeModule;
+import net.sourceforge.fenixedu.util.LanguageUtils;
 
 public abstract class CurriculumModule extends CurriculumModule_Base {
 
@@ -36,4 +40,10 @@ public abstract class CurriculumModule extends CurriculumModule_Base {
     public abstract List<Enrolment> getEnrolments();
     
     public abstract StudentCurricularPlan getStudentCurricularPlan();
+    
+    public String getName() {
+	final DegreeModule degreeModule = getDegreeModule();
+	return LanguageUtils.getUserLanguage() == Language.en ? degreeModule.getNameEn() : degreeModule.getName(); 
+    }
+    
 }
