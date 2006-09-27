051be358bfb95adc12bd6a9fad7425e352953ec8
Luís Egídio <luis.egidio@ist.utl.pt>
2006-09-27 13:09:40 +0000

    moved logic to domain level

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadStudentByNumberAndAllDegreeTypes.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadStudentByNumberAndAllDegreeTypes.java
@@ -2,10 +2,7 @@ package net.sourceforge.fenixedu.applicationTier.Servico.student;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.dataTransferObject.InfoStudent;
-import net.sourceforge.fenixedu.dataTransferObject.InfoStudent;
-import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.domain.student.Registration;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 /**
  * 
@@ -14,19 +11,10 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
  */
 public class ReadStudentByNumberAndAllDegreeTypes extends Service {
 
-    public Object run(Integer number) throws ExcepcaoPersistencia {
-        InfoStudent infoStudent = null;
+    public InfoStudent run (Integer number) {
+        final Registration registration = Registration.readStudentByNumberAndAllDegreeTypes(number);
 
-        Registration registration = Registration.readStudentByNumberAndDegreeType(number, DegreeType.DEGREE);
-
-        if (registration == null) {
-            registration = Registration.readStudentByNumberAndDegreeType(number, DegreeType.MASTER_DEGREE);
-        }
-        if (registration != null) {
-            infoStudent = InfoStudent.newInfoFromDomain(registration);
-        }
-
-        return infoStudent;
+        return registration == null ? null : InfoStudent.newInfoFromDomain(registration);
     }
 
 }
