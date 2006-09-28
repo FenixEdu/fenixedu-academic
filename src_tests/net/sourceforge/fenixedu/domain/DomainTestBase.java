b611d73dc2081724dcbf4933dfb1afd42fd231f2
Luís Santos <lmhs@ist.utl.pt>
2006-09-28 13:20:22 +0000

    bug fix: domain_model metadata initialization and descriptors fixed. (lu?\237s cruz supervised).

--- src_tests/net/sourceforge/fenixedu/domain/DomainTestBase.java
+++ src_tests/net/sourceforge/fenixedu/domain/DomainTestBase.java
@@ -5,12 +5,18 @@
 package net.sourceforge.fenixedu.domain;
 
 import junit.framework.TestCase;
+import net.sourceforge.fenixedu._development.MetadataManager;
+import net.sourceforge.fenixedu.persistenceTier.OJB.SuportePersistenteOJB;
 import net.sourceforge.fenixedu.stm.Transaction;
 
 public class DomainTestBase extends TestCase {
 
     protected void setUp() throws Exception {
-        super.setUp();        
+        super.setUp();    
+        
+        MetadataManager.init("domain_model.dml");
+        SuportePersistenteOJB.fixDescriptors();
+        
         try {
         	Transaction.abort();
         	Transaction.commit();
