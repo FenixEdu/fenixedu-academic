a72c33f668fdca8db45f95c57618313f00b32d4f
Luís Santos <lmhs@ist.utl.pt>
2006-09-29 07:48:30 +0000

    bug fix: removed variable person. predicates now verify conditions using userView.

--- src/net/sourceforge/fenixedu/predicates/ResultPredicates.java
+++ src/net/sourceforge/fenixedu/predicates/ResultPredicates.java
@@ -2,6 +2,7 @@ package net.sourceforge.fenixedu.predicates;
 
 import net.sourceforge.fenixedu.accessControl.AccessControl;
 import net.sourceforge.fenixedu.accessControl.AccessControlPredicate;
+import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.research.result.Result;
@@ -12,14 +13,15 @@ import net.sourceforge.fenixedu.domain.research.result.ResultUnitAssociation;
 
 public class ResultPredicates {
 
-    static Person person = AccessControl.getUserView().getPerson();
+    final Person person = AccessControl.getUserView().getPerson();
 
     /**
      * Predicates to access Result objects.
      */
     public static final AccessControlPredicate<Result> createPredicate = new AccessControlPredicate<Result>() {
 	public boolean evaluate(Result result) {
-	    if (person.hasRole(RoleType.RESEARCHER) && !result.hasAnyResultParticipations()) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && !result.hasAnyResultParticipations()) {
 		return true;
 	    }
 	    return false;
@@ -28,7 +30,8 @@ public class ResultPredicates {
     
     public static final AccessControlPredicate<Result> writePredicate = new AccessControlPredicate<Result>() {
 	public boolean evaluate(Result result) {
-	    if (person.hasRole(RoleType.RESEARCHER) && result.hasPersonParticipation(person)) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && result.hasPersonParticipation(userView.getPerson())) {
 		return true;
 	    }
 	    return false;
@@ -41,8 +44,8 @@ public class ResultPredicates {
     public static final AccessControlPredicate<ResultUnitAssociation> unitWritePredicate = new AccessControlPredicate<ResultUnitAssociation>() {
 	public boolean evaluate(ResultUnitAssociation association) {
 	    final Result result = association.getResult();
-	    
-	    if (person.hasRole(RoleType.RESEARCHER) && result.hasPersonParticipation(person)) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && result.hasPersonParticipation(userView.getPerson())) {
 		return true;
 	    }
 	    return false;
@@ -55,8 +58,8 @@ public class ResultPredicates {
     public static final AccessControlPredicate<ResultEventAssociation> eventWritePredicate = new AccessControlPredicate<ResultEventAssociation>() {
 	public boolean evaluate(ResultEventAssociation association) {
 	    final Result result = association.getResult();
-	    
-	    if (person.hasRole(RoleType.RESEARCHER) && result.hasPersonParticipation(person)) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && result.hasPersonParticipation(userView.getPerson())) {
 		return true;
 	    }
 	    return false;
@@ -69,8 +72,8 @@ public class ResultPredicates {
     public static final AccessControlPredicate<ResultParticipation> participationWritePredicate = new AccessControlPredicate<ResultParticipation>() {
 	public boolean evaluate(ResultParticipation participation) {
 	    final Result result = participation.getResult();
-	
-	    if (person.hasRole(RoleType.RESEARCHER) && result.hasPersonParticipation(person)) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && result.hasPersonParticipation(userView.getPerson())) {
 		    return true;    
 	    }
 	    return false;
@@ -83,8 +86,8 @@ public class ResultPredicates {
     public static final AccessControlPredicate<ResultDocumentFile> documentFileWritePredicate = new AccessControlPredicate<ResultDocumentFile>() {
 	public boolean evaluate(ResultDocumentFile documentFile) {
 	    final Result result = documentFile.getResult();
-	    
-	    if (person.hasRole(RoleType.RESEARCHER) && result.hasPersonParticipation(person)) {
+	    final IUserView userView = AccessControl.getUserView();
+	    if (userView.hasRoleType(RoleType.RESEARCHER) && result.hasPersonParticipation(userView.getPerson())) {
 		return true;
 	    }
 	    return false;
