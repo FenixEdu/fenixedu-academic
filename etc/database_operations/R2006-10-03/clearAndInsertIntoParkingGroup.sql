5f180de58115acef571987e50166a37dd381e6eb
Luis Cruz <luis.cruz@ist.utl.pt>
2006-10-03 14:04:42 +0000

    Deployed HEAD.

---
+++ etc/database_operations/R2006-10-03/clearAndInsertIntoParkingGroup.sql
@@ -0,0 +1,12 @@
+delete from PARKING_GROUP;
+
+insert into PARKING_GROUP values(null,"Limitados",1);
+insert into PARKING_GROUP values(null,"Jubilados",1);
+insert into PARKING_GROUP values(null,"IPSFL",1);
+insert into PARKING_GROUP values(null,"2º ciclo",1);
+insert into PARKING_GROUP values(null,"3º ciclo",1);
+insert into PARKING_GROUP values(null,"Investigadores",1);
+insert into PARKING_GROUP values(null,"Bolseiros",1);
+insert into PARKING_GROUP values(null,"Especiais",1);
+insert into PARKING_GROUP values(null,"Não Docentes",1);
+insert into PARKING_GROUP values(null,"Docentes",1);
