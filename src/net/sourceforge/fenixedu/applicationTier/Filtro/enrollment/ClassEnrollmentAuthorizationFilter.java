e844ea4f45a36bcba3039dbf8401f5fac96cd318
Luis Cruz <luis.cruz@ist.utl.pt>
2006-10-03 14:19:18 +0000

    Bug fix.

--- src/net/sourceforge/fenixedu/applicationTier/Filtro/enrollment/ClassEnrollmentAuthorizationFilter.java
+++ src/net/sourceforge/fenixedu/applicationTier/Filtro/enrollment/ClassEnrollmentAuthorizationFilter.java
@@ -5,19 +5,17 @@
 package net.sourceforge.fenixedu.applicationTier.Filtro.enrollment;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.SortedSet;
 
 import net.sourceforge.fenixedu.applicationTier.Filtro.Filtro;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.NotAuthorizedFilterException;
 import net.sourceforge.fenixedu.domain.EnrolmentPeriodInClasses;
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
-import net.sourceforge.fenixedu.domain.degree.DegreeType;
-import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.util.DateFormatUtil;
 import pt.utl.ist.berserk.ServiceRequest;
 import pt.utl.ist.berserk.ServiceResponse;
-import pt.utl.ist.berserk.logic.filterManager.exceptions.FilterException;
 
 /**
  * @author Luis Cruz
@@ -29,40 +27,49 @@ public class ClassEnrollmentAuthorizationFilter extends Filtro {
     
     private static String comparableDateFormatString = "yyyyMMddHHmm";
 
-    public void execute(ServiceRequest request, ServiceResponse response) throws FilterException, Exception {
+    public void execute(ServiceRequest request, ServiceResponse response) throws Exception {
+        final Person person = getRemoteUser(request).getPerson();
+        final SortedSet<StudentCurricularPlan> activeStudentCurricularPlans
+                = person.getActiveStudentCurricularPlansSortedByDegreeTypeAndDegreeName();
 
-        Registration registration = getRemoteUser(request).getPerson().getStudentByUsername();
-        if(registration == null){
-            registration = getRemoteUser(request).getPerson().getStudentByType(DegreeType.DEGREE);
-        }
-        
-        final StudentCurricularPlan studentCurricularPlan = registration.getActiveStudentCurricularPlan(); 
-        
-        if (studentCurricularPlan != null) {
-        	EnrolmentPeriodInClasses enrolmentPeriodInClasses = studentCurricularPlan.getDegreeCurricularPlan().getCurrentClassesEnrollmentPeriod();
-            if (enrolmentPeriodInClasses == null || enrolmentPeriodInClasses.getStartDateDateTime() == null
-                    || enrolmentPeriodInClasses.getEndDateDateTime() == null) {
-                throw new CurrentClassesEnrolmentPeriodUndefinedForDegreeCurricularPlan();
-            }
-
-            Date now = new Date();
-            Date startDate = enrolmentPeriodInClasses.getStartDate();
-            Date endDate = enrolmentPeriodInClasses.getEndDate();
-            
-            if(DateFormatUtil.compareDates(comparableDateFormatString, startDate, now) > 0 || DateFormatUtil.compareDates(comparableDateFormatString, endDate, now) < 0) { 
-                String startDateString = outputDateFormat.format(startDate);
-                String endDateString = outputDateFormat.format(endDate);
-
-                StringBuilder buffer = new StringBuilder();
-                buffer.append(startDateString);
-                buffer.append(" - ");
-                buffer.append(endDateString);
-                throw new OutsideOfCurrentClassesEnrolmentPeriodForDegreeCurricularPlan(buffer
-                        .toString());
-            }
-        } else {
+        if (activeStudentCurricularPlans.isEmpty()) {
             throw new NoActiveStudentCurricularPlanOfCorrectTypeException();
         }
+
+        boolean hasOneOpen = false;
+        Exception toThrow = null;
+        for (final StudentCurricularPlan studentCurricularPlan : activeStudentCurricularPlans) {
+            final Exception exception = verify(studentCurricularPlan);
+            hasOneOpen = hasOneOpen || exception == null;
+            toThrow = exception == null ? toThrow : exception;
+        }
+        if (!hasOneOpen) {
+            throw toThrow;
+        }
+    }
+
+    private Exception verify(StudentCurricularPlan studentCurricularPlan) {
+        final EnrolmentPeriodInClasses enrolmentPeriodInClasses = studentCurricularPlan.getDegreeCurricularPlan().getCurrentClassesEnrollmentPeriod();
+        if (enrolmentPeriodInClasses == null || enrolmentPeriodInClasses.getStartDateDateTime() == null
+                || enrolmentPeriodInClasses.getEndDateDateTime() == null) {
+            return new CurrentClassesEnrolmentPeriodUndefinedForDegreeCurricularPlan();
+        }
+
+        Date now = new Date();
+        Date startDate = enrolmentPeriodInClasses.getStartDate();
+        Date endDate = enrolmentPeriodInClasses.getEndDate();
+        
+        if(DateFormatUtil.compareDates(comparableDateFormatString, startDate, now) > 0 || DateFormatUtil.compareDates(comparableDateFormatString, endDate, now) < 0) { 
+            String startDateString = outputDateFormat.format(startDate);
+            String endDateString = outputDateFormat.format(endDate);
+
+            StringBuilder buffer = new StringBuilder();
+            buffer.append(startDateString);
+            buffer.append(" - ");
+            buffer.append(endDateString);
+            return new OutsideOfCurrentClassesEnrolmentPeriodForDegreeCurricularPlan(buffer.toString());
+        }
+        return null;
     }
 
     public class NoActiveStudentCurricularPlanOfCorrectTypeException extends
