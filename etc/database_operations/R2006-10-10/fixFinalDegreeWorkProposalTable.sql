7c88c16b2c06622ac15cb5128419cd8fa490df82
Luis Cruz <luis.cruz@ist.utl.pt>
2006-10-10 22:36:22 +0000

    Scripts for fixing data truncation problems.

---
+++ etc/database_operations/R2006-10-10/fixFinalDegreeWorkProposalTable.sql
@@ -0,0 +1,1 @@
+alter table FINAL_DEGREE_WORK_PROPOSAL change column TITLE TITLE varchar(255) NOT NULL default '';
---
+++ etc/database_operations/R2006-10-10/fixMarkTable.sql
@@ -0,0 +1,3 @@
+alter table MARK change column MARK MARK varchar(5) default NULL;
+alter table MARK change column PUBLISHED_MARK PUBLISHED_MARK varchar(5) default NULL;
+alter table MARK change column SUBMITED_MARK SUBMITED_MARK varchar(5) default NULL;
