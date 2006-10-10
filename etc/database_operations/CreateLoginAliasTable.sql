5ca2505886c02713221340773af27f0eb036e027
Manuel Pinto <manuel.pinto@ist.utl.pt>
2006-10-10 17:47:36 +0000

    bugs fixed.

--- etc/database_operations/CreateLoginAliasTable.sql
+++ etc/database_operations/CreateLoginAliasTable.sql
@@ -2,7 +2,7 @@ CREATE TABLE `LOGIN_ALIAS` (
   `ID_INTERNAL` int(11) unsigned NOT NULL auto_increment,
   `ALIAS` varchar(50) NOT NULL default '',  
   `KEY_LOGIN` int(11) unsigned NOT NULL default '0', 
-  `TYPE` varchar(50) NOT NULL default NULL,
+  `TYPE` varchar(50) NOT NULL default '',
   `ROLE_TYPE`  varchar(50) default NULL,
   `KEY_ROOT_DOMAIN_OBJECT` int(11) NOT NULL default '1',
   PRIMARY KEY  (`ID_INTERNAL`),
