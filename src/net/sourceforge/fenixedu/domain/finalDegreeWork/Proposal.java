24ea10f843188e091e96fb2e53327ac8289e8223
Luis Cruz <luis.cruz@ist.utl.pt>
2006-10-10 22:37:13 +0000

    Check if data truncation will occur before setting value.

--- src/net/sourceforge/fenixedu/domain/finalDegreeWork/Proposal.java
+++ src/net/sourceforge/fenixedu/domain/finalDegreeWork/Proposal.java
@@ -1,17 +1,26 @@
 package net.sourceforge.fenixedu.domain.finalDegreeWork;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
 public class Proposal extends Proposal_Base {
 
-	public Proposal() {
-		super();
-		setRootDomainObject(RootDomainObject.getInstance());
-	}
+    public Proposal() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+    }
 
     public void delete() {
-        removeRootDomainObject();
-        deleteDomainObject();
+	removeRootDomainObject();
+	deleteDomainObject();
+    }
+
+    @Override
+    public void setTitle(String title) {
+	if (title != null && title.length() > 255) {
+	    throw new DomainException("error.title.is.to.long.255.max");
+	}
+	super.setTitle(title);
     }
 
 }
