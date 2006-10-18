5af2851e8ccd41c795992d9c317a722f74c33e7d
Luís Egídio <luis.egidio@ist.utl.pt>
2006-10-18 08:55:26 +0000

    allow component to work with collections

--- src/net/sourceforge/fenixedu/presentationTier/jsf/components/UIDataRepeater.java
+++ src/net/sourceforge/fenixedu/presentationTier/jsf/components/UIDataRepeater.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.presentationTier.jsf.components;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -17,223 +19,199 @@ public class UIDataRepeater extends UIData {
     private String rowCountVar;
 
     public UIDataRepeater() {
-        super();
+	super();
     }
 
     @Override
     public String getFamily() {
-        return COMPONENT_FAMILY;
+	return COMPONENT_FAMILY;
     }
 
     public String getClientId(FacesContext context) {
-        String clientId = super.getClientId(context);
+	String clientId = super.getClientId(context);
 
-        int rowIndex = getRowIndex();
+	int rowIndex = getRowIndex();
 
-        if (rowIndex == -1) {
-            return clientId;
-        } else {
-            return clientId + "_" + rowIndex;
-        }
+	if (rowIndex == -1) {
+	    return clientId;
+	} else {
+	    return clientId + "_" + rowIndex;
+	}
 
     }
 
     public void processDecodes(FacesContext context) {
-        int first = getFirst();
-        int rows = getRows();
-        int last;
+	int first = getFirst();
+	int rows = getRows();
+	int last;
 
-        if (rows == 0) {
-            last = getRowCount();
-        } else {
-            last = first + rows;
-        }
+	if (rows == 0) {
+	    last = getRowCount();
+	} else {
+	    last = first + rows;
+	}
 
-        for (int rowIndex = first; rowIndex < last; rowIndex++) {
-            setRowIndex(rowIndex);
-            if (isRowAvailable()) {
-                for (Iterator it = getChildren().iterator(); it.hasNext();) {
-                    UIComponent child = (UIComponent) it.next();
-                    // For some reason its necessary to touch Id property,
-                    // otherwise the child control will not call getClientId on
-                    // parent (NamingContainer)
-                    child.setId(child.getId());
-                    if (!child.isRendered()) {
-                        continue;
-                    }
-                    child.processDecodes(context);
-                }
-            }
-        }
+	for (int rowIndex = first; rowIndex < last; rowIndex++) {
+	    setRowIndex(rowIndex);
+	    if (isRowAvailable()) {
+		for (Iterator it = getChildren().iterator(); it.hasNext();) {
+		    UIComponent child = (UIComponent) it.next();
+		    // For some reason its necessary to touch Id property,
+		    // otherwise the child control will not call getClientId
+		    // on
+		    // parent (NamingContainer)
+		    child.setId(child.getId());
+		    if (!child.isRendered()) {
+			continue;
+		    }
+		    child.processDecodes(context);
+		}
+	    }
+	}
 
-        setRowIndex(-1);
+	setRowIndex(-1);
 
-        super.processDecodes(context);
+	super.processDecodes(context);
     }
 
     public void processUpdates(FacesContext context) {
-        int first = getFirst();
-        int rows = getRows();
-        int last;
-        if (rows == 0) {
-            last = getRowCount();
-        } else {
-            last = first + rows;
-        }
-        for (int rowIndex = first; rowIndex < last; rowIndex++) {
-            setRowIndex(rowIndex);
-            if (isRowAvailable()) {
-                for (Iterator it = getChildren().iterator(); it.hasNext();) {
-                    UIComponent child = (UIComponent) it.next();
-                    // For some reason its necessary to touch Id property,
-                    // otherwise the child control will not call getClientId on
-                    // parent (NamingContainer)
-                    child.setId(child.getId());
-                    
-                    if (!child.isRendered()) {
-                        continue;
-                    }
-                    child.processUpdates(context);
-                }
-            }
-        }
+	int first = getFirst();
+	int rows = getRows();
+	int last;
+	if (rows == 0) {
+	    last = getRowCount();
+	} else {
+	    last = first + rows;
+	}
+	for (int rowIndex = first; rowIndex < last; rowIndex++) {
+	    setRowIndex(rowIndex);
+	    if (isRowAvailable()) {
+		for (Iterator it = getChildren().iterator(); it.hasNext();) {
+		    UIComponent child = (UIComponent) it.next();
+		    // For some reason its necessary to touch Id property,
+		    // otherwise the child control will not call getClientId
+		    // on
+		    // parent (NamingContainer)
+		    child.setId(child.getId());
 
-        setRowIndex(-1);
+		    if (!child.isRendered()) {
+			continue;
+		    }
+		    child.processUpdates(context);
+		}
+	    }
+	}
 
-        super.processUpdates(context);
+	setRowIndex(-1);
+
+	super.processUpdates(context);
     }
 
     public void processValidators(FacesContext context) {
-        int first = getFirst();
-        int rows = getRows();
-        int last;
-        if (rows == 0) {
-            last = getRowCount();
-        } else {
-            last = first + rows;
-        }
-        for (int rowIndex = first; rowIndex < last; rowIndex++) {
-            setRowIndex(rowIndex);
-            if (isRowAvailable()) {
-                for (Iterator it = getChildren().iterator(); it.hasNext();) {
-                    UIComponent child = (UIComponent) it.next();
-                    // For some reason its necessary to touch Id property,
-                    // otherwise the child control will not call getClientId on
-                    // parent (NamingContainer)
-                    child.setId(child.getId());
-                    if (!child.isRendered()) {
-                        continue;
-                    }
-                    child.processValidators(context);
-                }
-            }
-        }
+	int first = getFirst();
+	int rows = getRows();
+	int last;
+	if (rows == 0) {
+	    last = getRowCount();
+	} else {
+	    last = first + rows;
+	}
+	for (int rowIndex = first; rowIndex < last; rowIndex++) {
+	    setRowIndex(rowIndex);
+	    if (isRowAvailable()) {
+		for (Iterator it = getChildren().iterator(); it.hasNext();) {
+		    UIComponent child = (UIComponent) it.next();
+		    // For some reason its necessary to touch Id property,
+		    // otherwise the child control will not call getClientId
+		    // on
+		    // parent (NamingContainer)
+		    child.setId(child.getId());
+		    if (!child.isRendered()) {
+			continue;
+		    }
+		    child.processValidators(context);
+		}
+	    }
+	}
 
-        setRowIndex(-1);
+	setRowIndex(-1);
 
-        super.processValidators(context);
+	super.processValidators(context);
     }
 
     public void setRowIndex(int rowIndex) {
-        super.setRowIndex(rowIndex);
-        String rowIndexVar = getRowIndexVar();
-        String rowCountVar = getRowCountVar();
-        if (rowIndexVar != null || rowCountVar != null) {
-            Map requestMap = FacesContext.getCurrentInstance().getExternalContext().getRequestMap();
-            if (rowIndex >= 0) {
-                // regular row index, update request scope variables
-                if (rowIndexVar != null) {
-                    requestMap.put(getRowIndexVar(), new Integer(rowIndex));
-                }
-                if (rowCountVar != null) {
-                    requestMap.put(getRowCountVar(), new Integer(getRowCount()));
-                }
-            } else {
-                // rowIndex == -1 means end of loop --> remove request scope
-                // variables
-                if (rowIndexVar != null) {
-                    requestMap.remove(getRowIndexVar());
-                }
-                if (rowCountVar != null) {
-                    requestMap.remove(getRowCountVar());
-                }
-            }
-        }
+	super.setRowIndex(rowIndex);
+	String rowIndexVar = getRowIndexVar();
+	String rowCountVar = getRowCountVar();
+	if (rowIndexVar != null || rowCountVar != null) {
+	    Map requestMap = FacesContext.getCurrentInstance().getExternalContext().getRequestMap();
+	    if (rowIndex >= 0) {
+		// regular row index, update request scope variables
+		if (rowIndexVar != null) {
+		    requestMap.put(getRowIndexVar(), new Integer(rowIndex));
+		}
+		if (rowCountVar != null) {
+		    requestMap.put(getRowCountVar(), new Integer(getRowCount()));
+		}
+	    } else {
+		// rowIndex == -1 means end of loop --> remove request scope
+		// variables
+		if (rowIndexVar != null) {
+		    requestMap.remove(getRowIndexVar());
+		}
+		if (rowCountVar != null) {
+		    requestMap.remove(getRowCountVar());
+		}
+	    }
+	}
     }
 
     public void setRowIndexVar(String rowIndexVar) {
-        this.rowIndexVar = rowIndexVar;
+	this.rowIndexVar = rowIndexVar;
     }
 
     public String getRowIndexVar() {
-        if (this.rowIndexVar != null)
-            return this.rowIndexVar;
-        ValueBinding vb = getValueBinding("rowIndexVar");
-        return vb != null ? (String) vb.getValue(getFacesContext()) : null;
+	if (this.rowIndexVar != null)
+	    return this.rowIndexVar;
+	ValueBinding vb = getValueBinding("rowIndexVar");
+	return vb != null ? (String) vb.getValue(getFacesContext()) : null;
     }
 
     public void setRowCountVar(String rowCountVar) {
-        this.rowCountVar = rowCountVar;
+	this.rowCountVar = rowCountVar;
     }
 
     public String getRowCountVar() {
-        if (this.rowCountVar != null)
-            return this.rowCountVar;
-        ValueBinding vb = getValueBinding("rowCountVar");
-        return vb != null ? (String) vb.getValue(getFacesContext()) : null;
+	if (this.rowCountVar != null)
+	    return this.rowCountVar;
+	ValueBinding vb = getValueBinding("rowCountVar");
+	return vb != null ? (String) vb.getValue(getFacesContext()) : null;
     }
-    
-    
+
     @Override
     public Object saveState(FacesContext stateToSave) {
-    	Object state[] = new Object[3];
-    	
-    	state[0] = super.saveState(stateToSave);
-    	state[1] = getRowCountVar();
-    	state[2] = getRowIndexVar();
-    	
-    	return state;
+	Object state[] = new Object[3];
+
+	state[0] = super.saveState(stateToSave);
+	state[1] = getRowCountVar();
+	state[2] = getRowIndexVar();
+
+	return state;
     }
+
     @Override
     public void restoreState(FacesContext context, Object stateToRestore) {
-    	Object state[] = (Object[]) stateToRestore;
+	Object state[] = (Object[]) stateToRestore;
 
-    	super.restoreState(context,state[0]);
-    	setRowCountVar((String) state[1]);
-    	setRowIndexVar((String) state[2]);
+	super.restoreState(context, state[0]);
+	setRowCountVar((String) state[1]);
+	setRowIndexVar((String) state[2]);
     }
 
-    /*
-     * @Override public boolean getRendersChildren() { return true; }
-     * 
-     * @Override public void encodeBegin(FacesContext context) throws
-     * IOException { }
-     * 
-     * @Override public void encodeEnd(FacesContext context) throws IOException { }
-     * 
-     * @Override public void encodeChildren(FacesContext context) throws
-     * IOException {
-     * 
-     * int first = this.getFirst(); int rows = this.getRows(); int rowCount =
-     * this.getRowCount();
-     * 
-     * if (rows <= 0) { rows = rowCount - first; }
-     * 
-     * int last = first + rows;
-     * 
-     * if (last > rowCount) last = rowCount;
-     * 
-     * for (int i = first; i < last; i++) { this.setRowIndex(i); if
-     * (this.isRowAvailable()) { if (this.getChildCount() > 0) { for (Iterator
-     * it = this.getChildren().iterator(); it.hasNext();) { UIComponent child =
-     * (UIComponent) it.next(); child.setId(child.getId());
-     * 
-     * if (!child.isRendered()) { continue; }
-     * 
-     * child.encodeBegin(context);
-     * 
-     * if (child.getRendersChildren()) { child.encodeChildren(context); }
-     * 
-     * child.encodeEnd(context); } } } } }
-     */
+    @Override
+    public Object getValue() {
+	return super.getValue() instanceof Collection ? new ArrayList((Collection) super.getValue())
+		: super.getValue();
+    }
+
 }
