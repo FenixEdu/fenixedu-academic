fd738ed4a52d604f45039e358c6f7ed405a4061d
Luis Cruz <luis.cruz@ist.utl.pt>
2006-10-25 08:56:26 +0000

    Bug fix: avoid data truncation error.

--- src/net/sourceforge/fenixedu/domain/teacher/TeachingCareer.java
+++ src/net/sourceforge/fenixedu/domain/teacher/TeachingCareer.java
@@ -19,37 +19,43 @@ public class TeachingCareer extends TeachingCareer_Base {
         super();
         setOjbConcreteClass(TeachingCareer.class.getName());
     }
-	
-	public TeachingCareer(Teacher teacher, Category category, InfoTeachingCareer infoTeachingCareer) {
-	
-		if(teacher == null || category == null)
-			throw new DomainException("Neither teacher nor category should be null!");
-		
-        setOjbConcreteClass(TeachingCareer.class.getName());
-		setTeacher(teacher);
-		setCategory(category);
-		setBasicProperties(infoTeachingCareer);
-	}
 
-	public void delete() {
-		removeCategory();
-		super.delete();
-	}
-	
-	public void edit(InfoTeachingCareer infoTeachingCareer, Category category) {
-		
-		if(category == null)
-			throw new DomainException("The category should not be null!");
-		
-		setBasicProperties(infoTeachingCareer);
+    public TeachingCareer(Teacher teacher, Category category, InfoTeachingCareer infoTeachingCareer) {
+        if (teacher == null || category == null)
+            throw new DomainException("Neither teacher nor category should be null!");
+
+        setOjbConcreteClass(TeachingCareer.class.getName());
+        setTeacher(teacher);
+        setCategory(category);
+        setBasicProperties(infoTeachingCareer);
+    }
+
+    public void delete() {
+        removeCategory();
+        super.delete();
+    }
+
+    public void edit(InfoTeachingCareer infoTeachingCareer, Category category) {
+        if (category == null)
+            throw new DomainException("The category should not be null!");
+
+        setBasicProperties(infoTeachingCareer);
         this.setCategory(category);
-	}
-	
-	private void setBasicProperties(InfoTeachingCareer infoTeachingCareer) {
-		this.setBeginYear(infoTeachingCareer.getBeginYear());
-		this.setEndYear(infoTeachingCareer.getEndYear());
+    }
+
+    private void setBasicProperties(InfoTeachingCareer infoTeachingCareer) {
+        this.setBeginYear(infoTeachingCareer.getBeginYear());
+        this.setEndYear(infoTeachingCareer.getEndYear());
         this.setCourseOrPosition(infoTeachingCareer.getCourseOrPosition());
-		
-	}
+
+    }
+
+    @Override
+    public void setCourseOrPosition(String courseOrPosition) {
+        if (courseOrPosition != null && courseOrPosition.length() > 100) {
+            throw new DomainException("error.courseOrPosition.max.length.exceeded");
+        }
+        super.setCourseOrPosition(courseOrPosition);
+    }
 
 }
