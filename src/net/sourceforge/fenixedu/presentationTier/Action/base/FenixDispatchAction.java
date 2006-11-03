6499e3394302efbbafa586356792d71607e4fec7
Shezad Anavarali <shezad@ist.utl.pt>
2006-11-03 17:01:04 +0000

    Added new ExecuteService method

--- src/net/sourceforge/fenixedu/presentationTier/Action/base/FenixDispatchAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/base/FenixDispatchAction.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.sourceforge.fenixedu.accessControl.AccessControl;
 import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
@@ -46,162 +47,168 @@ public abstract class FenixDispatchAction extends DispatchAction implements Exce
     protected static final RootDomainObject rootDomainObject = RootDomainObject.getInstance();
 
     protected static final ResourceBundle enumerationResources = ResourceBundle.getBundle(
-            "resources.EnumerationResources", LanguageUtils.getLocale());
-    
+	    "resources.EnumerationResources", LanguageUtils.getLocale());
+
     protected static final ResourceBundle domainExceptionResources = ResourceBundle.getBundle(
-            "resources.DomainExceptionResources", LanguageUtils.getLocale());
+	    "resources.DomainExceptionResources", LanguageUtils.getLocale());
 
     private static final String ACTION_MESSAGES_REQUEST_KEY = "FENIX_ACTION_MESSAGES";
 
     public ActionForward execute(ActionMapping mapping, ActionForm actionForm,
-            HttpServletRequest request, HttpServletResponse response) throws Exception {
+	    HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        final ActionMessages actionMessages = new ActionMessages();
-        request.setAttribute(ACTION_MESSAGES_REQUEST_KEY, actionMessages);
-        final ActionForward actionForward = super.execute(mapping, actionForm, request, response);
-        if (!actionMessages.isEmpty()) {
-            saveMessages(request, actionMessages);
-        }
+	final ActionMessages actionMessages = new ActionMessages();
+	request.setAttribute(ACTION_MESSAGES_REQUEST_KEY, actionMessages);
+	final ActionForward actionForward = super.execute(mapping, actionForm, request, response);
+	if (!actionMessages.isEmpty()) {
+	    saveMessages(request, actionMessages);
+	}
 
-        return actionForward;
+	return actionForward;
     }
 
     protected static IUserView getUserView(HttpServletRequest request) {
-        return SessionUtils.getUserView(request);
+	return SessionUtils.getUserView(request);
     }
 
     protected Object executeService(final HttpServletRequest request, final String serviceName,
-            final Object[] serviceArgs) throws FenixFilterException, FenixServiceException {
-        return ServiceUtils.executeService(getUserView(request), serviceName, serviceArgs);
+	    final Object[] serviceArgs) throws FenixFilterException, FenixServiceException {
+	return ServiceUtils.executeService(getUserView(request), serviceName, serviceArgs);
+    }
+
+    protected Object executeService(final String serviceName, final Object ... serviceArgs)
+	    throws FenixFilterException, FenixServiceException {
+	return ServiceUtils.executeService(AccessControl.getUserView(), serviceName, serviceArgs);
     }
 
     protected DomainObject readDomainObject(final HttpServletRequest request, final Class clazz,
-            final Integer idInternal) {
-        return rootDomainObject.readDomainObjectByOID(clazz, idInternal);
+	    final Integer idInternal) {
+	return rootDomainObject.readDomainObjectByOID(clazz, idInternal);
     }
 
     protected Collection readAllDomainObjects(final HttpServletRequest request, final Class clazz) {
-        return rootDomainObject.readAllDomainObjects(clazz);
+	return rootDomainObject.readAllDomainObjects(clazz);
     }
 
     protected Person getLoggedPerson(HttpServletRequest request) {
 	final IUserView userView = getUserView(request);
-        return (userView == null) ? null : userView.getPerson();
+	return (userView == null) ? null : userView.getPerson();
     }
 
     protected HttpSession getSession(HttpServletRequest request) throws InvalidSessionActionException {
-        HttpSession result = request.getSession(false);
-        if (result == null)
-            throw new InvalidSessionActionException();
+	HttpSession result = request.getSession(false);
+	if (result == null)
+	    throw new InvalidSessionActionException();
 
-        return result;
+	return result;
     }
 
     /*
-     * Sets an error to display later in the Browser and sets the mapping forward.
-     */
+         * Sets an error to display later in the Browser and sets the mapping
+         * forward.
+         */
     protected ActionForward setError(HttpServletRequest request, ActionMapping mapping,
-            String errorMessage, String forwardPage, Object actionArg) {
-        ActionErrors errors = new ActionErrors();
-        String notMessageKey = errorMessage;
-        ActionError error = new ActionError(notMessageKey, actionArg);
-        errors.add(notMessageKey, error);
-        saveErrors(request, errors);
+	    String errorMessage, String forwardPage, Object actionArg) {
+	ActionErrors errors = new ActionErrors();
+	String notMessageKey = errorMessage;
+	ActionError error = new ActionError(notMessageKey, actionArg);
+	errors.add(notMessageKey, error);
+	saveErrors(request, errors);
 
-        if (forwardPage != null) {
-            return mapping.findForward(forwardPage);
-        }
+	if (forwardPage != null) {
+	    return mapping.findForward(forwardPage);
+	}
 
-        return mapping.getInputForward();
+	return mapping.getInputForward();
 
     }
 
     /*
-     * Verifies if a property of type String in a FormBean is not empty. Returns true if the field is
-     * present and not empty. False otherwhise.
-     */
+         * Verifies if a property of type String in a FormBean is not empty.
+         * Returns true if the field is present and not empty. False otherwhise.
+         */
     protected boolean verifyStringParameterInForm(DynaValidatorForm dynaForm, String field) {
-        if (dynaForm.get(field) != null && !dynaForm.get(field).equals("")) {
-            return true;
-        }
-        return false;
+	if (dynaForm.get(field) != null && !dynaForm.get(field).equals("")) {
+	    return true;
+	}
+	return false;
     }
 
     /*
-     * Verifies if a parameter in a Http Request is not empty. Return true if the field is not empty.
-     * False otherwise.
-     */
+         * Verifies if a parameter in a Http Request is not empty. Return true
+         * if the field is not empty. False otherwise.
+         */
     protected boolean verifyParameterInRequest(HttpServletRequest request, String field) {
-        if (request.getParameter(field) != null && !request.getParameter(field).equals("")) {
-            return true;
-        }
-        return false;
+	if (request.getParameter(field) != null && !request.getParameter(field).equals("")) {
+	    return true;
+	}
+	return false;
     }
 
     protected Integer getInteger(final DynaActionForm dynaActionForm, final String string) {
-        final String value = dynaActionForm.getString(string);
-        return value == null || value.length() == 0 ? null : Integer.valueOf(value);
+	final String value = dynaActionForm.getString(string);
+	return value == null || value.length() == 0 ? null : Integer.valueOf(value);
     }
 
     protected Integer getRequestParameterAsInteger(HttpServletRequest request, String parameterName) {
-        String requestParameter = request.getParameter(parameterName);
+	String requestParameter = request.getParameter(parameterName);
 
-        if (requestParameter != null) {
-            return Integer.valueOf(requestParameter);
-        } else {
-            return null;
-        }
+	if (requestParameter != null) {
+	    return Integer.valueOf(requestParameter);
+	} else {
+	    return null;
+	}
     }
 
     /**
-     * Searches in request parameters first and next in request attributed
-     * 
-     * @param request
-     * @param name
-     * @return
-     */
+         * Searches in request parameters first and next in request attributed
+         * 
+         * @param request
+         * @param name
+         * @return
+         */
     protected Object getFromRequest(HttpServletRequest request, String name) {
-        final String requestParameter = request.getParameter(name);
-        return (requestParameter != null) ? requestParameter : request.getAttribute(name);
+	final String requestParameter = request.getParameter(name);
+	return (requestParameter != null) ? requestParameter : request.getAttribute(name);
     }
 
     public ActionForward processException(HttpServletRequest request, ActionMapping mapping,
-            ActionForward input, Exception e) {
-        if (!(e instanceof DomainException)) {
-            return null;
-        }
+	    ActionForward input, Exception e) {
+	if (!(e instanceof DomainException)) {
+	    return null;
+	}
 
-        DomainException domainException = (DomainException) e;
-        ActionMessages messages = getMessages(request);
+	DomainException domainException = (DomainException) e;
+	ActionMessages messages = getMessages(request);
 
-        if (messages == null) {
-            messages = new ActionMessages();
-        }
+	if (messages == null) {
+	    messages = new ActionMessages();
+	}
 
-        messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(domainException.getKey(),
-                domainException.getArgs()));
-        saveMessages(request, messages);
+	messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(domainException.getKey(),
+		domainException.getArgs()));
+	saveMessages(request, messages);
 
-        IViewState viewState = RenderUtils.getViewState();
+	IViewState viewState = RenderUtils.getViewState();
 
-        if (viewState != null) {
-            ViewDestination destination = viewState.getDestination("exception");
+	if (viewState != null) {
+	    ViewDestination destination = viewState.getDestination("exception");
 
-            if (destination != null) {
-                return destination.getActionForward();
-            }
-        }
+	    if (destination != null) {
+		return destination.getActionForward();
+	    }
+	}
 
-        // show exception in output to ease finding the problem when
-        // messages are not shown in page
-        e.printStackTrace();
-        return input;
+	// show exception in output to ease finding the problem when
+	// messages are not shown in page
+	e.printStackTrace();
+	return input;
     }
 
     protected Object executeFactoryMethod(final HttpServletRequest request) throws FenixFilterException,
-            FenixServiceException {
-        final Object[] args = { getFactoryObject() };
-        return executeService(request, "ExecuteFactoryMethod", args);
+	    FenixServiceException {
+	final Object[] args = { getFactoryObject() };
+	return executeService(request, "ExecuteFactoryMethod", args);
     }
 
     protected Object getRenderedObject() {
@@ -210,68 +217,68 @@ public abstract class FenixDispatchAction extends DispatchAction implements Exce
 	    final MetaObject metaObject = viewState.getMetaObject();
 	    return metaObject == null ? null : metaObject.getObject();
 	}
-        return null;
+	return null;
     }
 
     protected FactoryExecutor getFactoryObject() {
-        return (FactoryExecutor) getRendererObject();
+	return (FactoryExecutor) getRendererObject();
     }
 
     protected Object getRendererObject() {
-        IViewState viewState = RenderUtils.getViewState();
-        if (viewState != null) {
-            MetaObject metaObject = viewState.getMetaObject();
-            if (metaObject != null) {
-                return metaObject.getObject();
-            }
-        }
-        return null;
+	IViewState viewState = RenderUtils.getViewState();
+	if (viewState != null) {
+	    MetaObject metaObject = viewState.getMetaObject();
+	    if (metaObject != null) {
+		return metaObject.getObject();
+	    }
+	}
+	return null;
     }
 
     protected ActionMessages getActionMessages(HttpServletRequest request) {
-        return (ActionMessages) request.getAttribute(ACTION_MESSAGES_REQUEST_KEY);
+	return (ActionMessages) request.getAttribute(ACTION_MESSAGES_REQUEST_KEY);
     }
 
     protected boolean hasActionMessage(HttpServletRequest request) {
-        return !this.getActionMessages(request).isEmpty();
+	return !this.getActionMessages(request).isEmpty();
     }
 
     protected void addActionMessage(HttpServletRequest request, String key, String... args) {
-        this.getActionMessages(request).add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(key, args));
+	this.getActionMessages(request).add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(key, args));
     }
-    
+
     protected void addActionMessage(HttpServletRequest request, String key) {
-        this.getActionMessages(request).add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(key));
+	this.getActionMessages(request).add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage(key));
     }
 
     protected String[] solveLabelFormatterArgs(HttpServletRequest request,
-            LabelFormatter[] labelFormatterArgs) {
-        final String[] args = new String[labelFormatterArgs.length];
-        int i = 0;
-        final StrutsMessageResourceProvider messageResourceProvider = getMessageResourceProvider(request);
-        for (final LabelFormatter labelFormatter : labelFormatterArgs) {
-            args[i++] = labelFormatter.toString(messageResourceProvider);
-        }
+	    LabelFormatter[] labelFormatterArgs) {
+	final String[] args = new String[labelFormatterArgs.length];
+	int i = 0;
+	final StrutsMessageResourceProvider messageResourceProvider = getMessageResourceProvider(request);
+	for (final LabelFormatter labelFormatter : labelFormatterArgs) {
+	    args[i++] = labelFormatter.toString(messageResourceProvider);
+	}
 
-        return args;
+	return args;
     }
 
     protected StrutsMessageResourceProvider getMessageResourceProvider(HttpServletRequest request) {
-        final StrutsMessageResourceProvider strutsMessageResourceProvider = new StrutsMessageResourceProvider(
-                getLocale(request), getServlet().getServletContext(), request);
-        for (final Entry<String, String> entry : getMessageResourceProviderBundleMappings().entrySet()) {
-            strutsMessageResourceProvider.addMapping(entry.getKey(), entry.getValue());
-        }
+	final StrutsMessageResourceProvider strutsMessageResourceProvider = new StrutsMessageResourceProvider(
+		getLocale(request), getServlet().getServletContext(), request);
+	for (final Entry<String, String> entry : getMessageResourceProviderBundleMappings().entrySet()) {
+	    strutsMessageResourceProvider.addMapping(entry.getKey(), entry.getValue());
+	}
 
-        return strutsMessageResourceProvider;
+	return strutsMessageResourceProvider;
     }
 
     protected Map<String, String> getMessageResourceProviderBundleMappings() {
-        final Map<String, String> bundleMappings = new HashMap<String, String>();
-        bundleMappings.put("enum", "ENUMERATION_RESOURCES");
-        bundleMappings.put("application", "DEFAULT");
+	final Map<String, String> bundleMappings = new HashMap<String, String>();
+	bundleMappings.put("enum", "ENUMERATION_RESOURCES");
+	bundleMappings.put("application", "DEFAULT");
 
-        return bundleMappings;
+	return bundleMappings;
     }
 
 }
