c531374f651b4608e4d680282dd450d2f09a5319
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2006-11-22 16:24:58 +0000

    added ExtraCurriculumGroup

---
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/ExtraCurriculumGroup.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.domain.studentCurriculum;
+
+public class ExtraCurriculumGroup extends ExtraCurriculumGroup_Base {
+    
+    public  ExtraCurriculumGroup() {
+        super();
+    }
+    
+}
