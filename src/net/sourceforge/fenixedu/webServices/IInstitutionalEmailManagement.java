aa5ee10c26f8daeb1a03f7de47f511f4f8cff62b
Manuel Pinto <manuel.pinto@ist.utl.pt>
2006-11-24 13:32:38 +0000

    Integrated WebServices in Fenix.

---
+++ src/net/sourceforge/fenixedu/webServices/IInstitutionalEmailManagement.java
@@ -0,0 +1,8 @@
+package net.sourceforge.fenixedu.webServices;
+
+
+public interface IInstitutionalEmailManagement {
+    
+    public String setMail(String userUID, String email);
+
+}
---
+++ src/net/sourceforge/fenixedu/webServices/InstitutionalEmailManagement.java
@@ -0,0 +1,21 @@
+package net.sourceforge.fenixedu.webServices;
+
+
+public class InstitutionalEmailManagement implements IInstitutionalEmailManagement {
+
+    public String setMail(String userUID, String email) {
+
+//	try {
+//	    return (String) ServiceUtils.executeService(null, "SetInstitutionalEmail", new Object[] {
+//		    userUID, email });
+//
+//	} catch (FenixFilterException e) {
+//	    return "Error occured in Fenix Filter!!!";
+//
+//	} catch (FenixServiceException e) {
+//	    return "Error occured in Fenix Service!!!";
+//	}
+
+	 throw new UnsupportedOperationException();
+    }
+}
