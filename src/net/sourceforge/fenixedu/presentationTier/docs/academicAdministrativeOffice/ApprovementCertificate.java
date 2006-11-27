285a648910854d978b1b1a725539702d12cd8494
Luís Egídio <luis.egidio@ist.utl.pt>
2006-11-27 07:51:01 +0000

    added preliminary version of approvement certificate document

--- src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/ApprovementCertificate.java
+++ src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/ApprovementCertificate.java
@@ -3,15 +3,14 @@ package net.sourceforge.fenixedu.presentationTier.docs.academicAdministrativeOff
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.beanutils.BeanComparator;
-import org.apache.commons.collections.comparators.ComparatorChain;
-
-import net.sourceforge.fenixedu.dataTransferObject.GenericPair;
 import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.ApprovementCertificateRequest;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequest;
 import net.sourceforge.fenixedu.util.LanguageUtils;
 import net.sourceforge.fenixedu.util.StringUtils;
+
+import org.apache.commons.beanutils.BeanComparator;
+import org.apache.commons.collections.comparators.ComparatorChain;
 
 public class ApprovementCertificate extends AdministrativeOfficeDocument {
 
@@ -22,23 +21,29 @@ public class ApprovementCertificate extends AdministrativeOfficeDocument {
     @Override
     protected void fillReport() {
 	super.fillReport();
-	
+
 	final ApprovementCertificateRequest approvementCertificateRequest = (ApprovementCertificateRequest) getDocumentRequest();
 
-	final List<Enrolment> approvedEnrolments = approvementCertificateRequest.getRegistration().getLastStudentCurricularPlanExceptPast().getAprovedEnrolments();
+	final List<Enrolment> approvedEnrolments = approvementCertificateRequest.getRegistration()
+		.getLastStudentCurricularPlanExceptPast().getAprovedEnrolments();
 	parameters.put("numberApprovements", Integer.valueOf(approvedEnrolments.size()));
-	
+
 	final ComparatorChain comparatorChain = new ComparatorChain();
 	comparatorChain.addComparator(new BeanComparator("executionPeriod.executionYear"));
 	comparatorChain.addComparator(new BeanComparator("name"));
 	Collections.sort(approvedEnrolments, comparatorChain);
-	
-	GenericPair<String,String> dummy;
+
+	StringBuilder approvementsInfo = new StringBuilder();
 	for (final Enrolment approvedEnrolment : approvedEnrolments) {
-	    dummy = new GenericPair<String, String>(StringUtils.multipleLineRightPad(LINE_LENGTH, approvedEnrolment
-		    .getName().getContent(LanguageUtils.getLanguage()).toUpperCase(), '-'), null);
-	    dataSource.add(dummy);
+	    StringBuilder gradeInfo = new StringBuilder();
+	    gradeInfo.append(" ").append(approvedEnrolment.getLatestEnrolmentEvaluation().getGrade()).append(" em ").append(approvedEnrolment.getExecutionPeriod().getExecutionYear().getYear());
+	    
+	    approvementsInfo.append(
+		    StringUtils.multipleLineRightPad(LINE_LENGTH - gradeInfo.length(), approvedEnrolment.getName()
+			    .getContent(LanguageUtils.getLanguage()).toUpperCase(), '-')).append("\n");
 	}
+
+	parameters.put("approvementsInfo", approvementsInfo.toString());
     }
 
 }
