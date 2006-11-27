446a47ea3fdf37ea79d07fed493718336166f051
Shezad Anavarali <shezad@ist.utl.pt>
2006-11-27 11:52:39 +0000

    The documentTypes are only returned if their administrativeOffice matches with whe logged in employee's office

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DocumentRequestTypeProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DocumentRequestTypeProvider.java
@@ -6,7 +6,10 @@ package net.sourceforge.fenixedu.presentationTier.renderers.providers;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOffice;
+import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOfficeType;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequestType;
+import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.renderers.DataProvider;
 import net.sourceforge.fenixedu.renderers.components.converters.Converter;
 import net.sourceforge.fenixedu.renderers.converters.EnumConverter;
@@ -19,15 +22,24 @@ public class DocumentRequestTypeProvider implements DataProvider {
 
     public Object provide(Object source, Object currentValue) {
 
-        Collection<DocumentRequestType> result = new ArrayList<DocumentRequestType>();
-        result.add(DocumentRequestType.SCHOOL_REGISTRATION_CERTIFICATE);
-        result.add(DocumentRequestType.ENROLMENT_CERTIFICATE);
+	AdministrativeOfficeType administrativeOfficeType = AdministrativeOffice.readByEmployee(
+		AccessControl.getUserView().getPerson().getEmployee()).getAdministrativeOfficeType();
 
-        return result;
+	Collection<DocumentRequestType> result = new ArrayList<DocumentRequestType>();
+	if (administrativeOfficeType.equals(DocumentRequestType.SCHOOL_REGISTRATION_CERTIFICATE
+		.getAdministrativeOfficeType())) {
+	    result.add(DocumentRequestType.SCHOOL_REGISTRATION_CERTIFICATE);
+	}
+	if (administrativeOfficeType.equals(DocumentRequestType.ENROLMENT_CERTIFICATE
+		.getAdministrativeOfficeType())) {
+	    result.add(DocumentRequestType.ENROLMENT_CERTIFICATE);
+	}
+
+	return result;
     }
 
     public Converter getConverter() {
-        return new EnumConverter();
+	return new EnumConverter();
     }
 
 }
