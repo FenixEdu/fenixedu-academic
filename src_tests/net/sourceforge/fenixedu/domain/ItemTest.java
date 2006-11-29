26684e07a14c25aae006812e34de6540eeca5410
Luís Egídio <luis.egidio@ist.utl.pt>
2006-11-29 13:35:33 +0000

    objects of Item are no longer editable

--- src_tests/net/sourceforge/fenixedu/domain/ItemTest.java
+++ src_tests/net/sourceforge/fenixedu/domain/ItemTest.java
@@ -10,13 +10,13 @@ import net.sourceforge.fenixedu.util.MultiLanguageString;
 public class ItemTest extends DomainTestBase {
 
     private Section section;
-    private Site site;
+    private ExecutionCourseSite site;
     private Item item, item2, item3;
 
     protected void setUp() throws Exception {
         super.setUp();
                         
-        site = new Site();            
+        site = new ExecutionCourseSite();            
         site.setIdInternal(0);        
 
         section = new Section();
@@ -78,45 +78,4 @@ public class ItemTest extends DomainTestBase {
         assertNull("Section Unexpected", item3.getSection());                
     }
 
-    public void testEdit() {
-          
-        try{
-            item2.edit((MultiLanguageString) null, null, null);
-            fail("Expected NullPointerException");
-        }
-        catch(NullPointerException e){
-            assertEquals("Name Unexpected", "ItemName2", item2.getName());
-            assertEquals("Information Unexpected", "ItemInformation2", item2.getInformation());
-        }
-        
-        item2.edit(new MultiLanguageString("ItemName2New"), new MultiLanguageString("ItemInformation2New"), 2);
-        
-        testEditInformation("ItemName2New","ItemInformation2New" , false);       
-        testItemsOrder(0, 2, 1);            
-        
-        item2.edit(new MultiLanguageString("ItemName22New"), new MultiLanguageString("ItemInformation22New"), 0);
-        
-        testEditInformation("ItemName22New","ItemInformation22New" , false);              
-        testItemsOrder(1, 0, 2);        
-        
-        item2.edit(new MultiLanguageString("ItemName222New"), new MultiLanguageString("ItemInformation222New"), 1);
-        
-        testEditInformation("ItemName222New","ItemInformation222New" , true);                       
-        testItemsOrder(0, 1, 2);        
-    }
-    
-    private void testEditInformation(String name, String information, boolean urgent){
-        assertEquals("Name Unexpected", name, item2.getName());
-        assertEquals("Information Unexpected", information, item2.getInformation());
-    }
-    
-    private void testItemsOrder(int value1, int value2, int value3){
-        assertEquals("Order Unexpected", value1, item.getItemOrder().intValue());
-        assertEquals("Order Unexpected", value2, item2.getItemOrder().intValue());   
-        assertEquals("Order Unexpected", value3, item3.getItemOrder().intValue());   
-        
-        assertEquals("Order Unexpected", value1, section.getAssociatedItems().get(0).getItemOrder().intValue());
-        assertEquals("Order Unexpected", value2, section.getAssociatedItems().get(1).getItemOrder().intValue());   
-        assertEquals("Order Unexpected", value3, section.getAssociatedItems().get(2).getItemOrder().intValue());
-    }
 }
