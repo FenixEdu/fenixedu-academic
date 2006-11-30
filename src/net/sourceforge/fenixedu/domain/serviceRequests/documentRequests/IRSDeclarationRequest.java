a605f8151bc1786c592501104e1cb922fb4fe72d
Luís Egídio <luis.egidio@ist.utl.pt>
2006-11-30 12:03:52 +0000

    added constructor

--- src/net/sourceforge/fenixedu/domain/serviceRequests/documentRequests/IRSDeclarationRequest.java
+++ src/net/sourceforge/fenixedu/domain/serviceRequests/documentRequests/IRSDeclarationRequest.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOfficeType;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.domain.student.Registration;
 
 public class IRSDeclarationRequest extends IRSDeclarationRequest_Base {
     
@@ -12,7 +13,31 @@ public class IRSDeclarationRequest extends IRSDeclarationRequest_Base {
         super();
     }
 
-    
+    public IRSDeclarationRequest(Registration registration,
+	    DocumentPurposeType documentPurposeType, String otherDocumentPurposeTypeDescription,
+	    Integer year) {
+
+	this();
+
+	init(registration, documentPurposeType, otherDocumentPurposeTypeDescription, year);
+    }
+
+    protected void init(Registration registration, DocumentPurposeType documentPurposeType,
+	    String otherDocumentPurposeTypeDescription, Integer year) {
+
+	super.init(registration, documentPurposeType, otherDocumentPurposeTypeDescription);
+
+	checkParameters(year);
+	super.setYear(year);
+    }
+
+    private void checkParameters(Integer year) {
+	if (year == null) {
+	    throw new DomainException(
+		    "error.serviceRequests.documentRequests.SchoolRegistrationDeclarationRequest.year.cannot.be.null");
+	}
+    }
+
     @Override
     public Set<AdministrativeOfficeType> getPossibleAdministrativeOffices() {
 	final Set<AdministrativeOfficeType> result = new HashSet<AdministrativeOfficeType>();
@@ -20,12 +45,6 @@ public class IRSDeclarationRequest extends IRSDeclarationRequest_Base {
 	result.add(AdministrativeOfficeType.DEGREE);
 	
 	return result;
-    }
-
-    @Override
-    protected void assertProcessingStatePreConditions() throws DomainException {
-	// TODO Auto-generated method stub
-	
     }
 
     @Override
@@ -38,4 +57,10 @@ public class IRSDeclarationRequest extends IRSDeclarationRequest_Base {
 	return getClass().getName();
     }
     
+    @Override
+    public void setYear(Integer year) {
+	throw new DomainException(
+		"error.serviceRequests.documentRequests.IRSDeclarationRequest.cannot.modify.year");
+    }
+
 }
