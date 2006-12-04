aeaa1574540a6e92bbcbed444e59673e01338547
Shezad Anavarali <shezad@ist.utl.pt>
2006-12-04 16:40:46 +0000

    Added delete method

--- src/net/sourceforge/fenixedu/domain/PersonAccount.java
+++ src/net/sourceforge/fenixedu/domain/PersonAccount.java
@@ -7,6 +7,7 @@ package net.sourceforge.fenixedu.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.transactions.InsuranceTransaction;
 import net.sourceforge.fenixedu.domain.transactions.PaymentTransaction;
 import net.sourceforge.fenixedu.domain.transactions.Transaction;
@@ -18,38 +19,49 @@ import net.sourceforge.fenixedu.domain.transactions.Transaction;
 public class PersonAccount extends PersonAccount_Base {
 
     public PersonAccount() {
-    	super();
-    	setRootDomainObject(RootDomainObject.getInstance());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
 
     public PersonAccount(Person person) {
-    	this();
-        setPerson(person);
-        setBalance(new Double(0));
+	this();
+	setPerson(person);
+	setBalance(new Double(0));
 
     }
 
     public List getPaymentTransactions() {
 
-        List result = new ArrayList<PaymentTransaction>();
-        for (Transaction transaction : this.getTransactions()) {
-            if (transaction instanceof PaymentTransaction) {
-                result.add(transaction);
-            }
-        }
-
-        return result;
-    }
-	
-	 public List getInsuranceTransactions() {
-
-	        List result = new ArrayList<InsuranceTransaction>();
-	        for (Transaction transaction : this.getTransactions()) {
-	            if (transaction instanceof InsuranceTransaction) {
-	                result.add(transaction);
-	            }
-	        }
-	        return result;
+	List result = new ArrayList<PaymentTransaction>();
+	for (Transaction transaction : this.getTransactions()) {
+	    if (transaction instanceof PaymentTransaction) {
+		result.add(transaction);
 	    }
+	}
+
+	return result;
+    }
+
+    public List getInsuranceTransactions() {
+
+	List result = new ArrayList<InsuranceTransaction>();
+	for (Transaction transaction : this.getTransactions()) {
+	    if (transaction instanceof InsuranceTransaction) {
+		result.add(transaction);
+	    }
+	}
+	return result;
+    }
+
+    public void delete() {
+
+	if (getTransactionsCount() > 0) {
+	    throw new DomainException("error.person.cannot.be.deleted");
+	}
+
+	removeRootDomainObject();
+	removePerson();
+	super.deleteDomainObject();
+    }
 
 }
