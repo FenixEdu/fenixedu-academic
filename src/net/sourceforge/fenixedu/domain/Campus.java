c4b98fe0a663de5c9cf1ce1ea557967e613a150b
Luís Egídio <luis.egidio@ist.utl.pt>
2006-12-04 15:36:39 +0000

    temporary getLocation method

--- src/net/sourceforge/fenixedu/domain/Campus.java
+++ src/net/sourceforge/fenixedu/domain/Campus.java
@@ -6,7 +6,7 @@ public class Campus extends Campus_Base {
 	super();
 	setRootDomainObject(RootDomainObject.getInstance());
     }
-    
+
     public static Campus readCampusByName(final String name) {
 	for (final Campus campus : RootDomainObject.getInstance().getCampussSet()) {
 	    if (campus.getName().equals(name)) {
@@ -15,4 +15,17 @@ public class Campus extends Campus_Base {
 	}
 	return null;
     }
+
+    // TODO : fix this when the new spaces structure is introduced
+    // and the location of each campus is known.
+    public String getLocation() {
+	if (getName().equals("Alameda")) {
+	    return "Lisboa";
+	} else if (getName().equals("TagusPark")) {
+	    return "Oeiras";
+	}
+	
+	return null;
+    }
+
 }
