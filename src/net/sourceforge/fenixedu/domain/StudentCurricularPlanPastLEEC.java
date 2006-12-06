1db55b64c95154d515b2910c8c131936ef905045
Luis Cruz <luis.cruz@ist.utl.pt>
2006-12-06 17:11:23 +0000

    )gnore PastLEEC student curricular plan enrolments. They are all duplicated in current curricular plan.

---
+++ src/net/sourceforge/fenixedu/domain/StudentCurricularPlanPastLEEC.java
@@ -0,0 +1,13 @@
+package net.sourceforge.fenixedu.domain;
+
+import java.util.Collection;
+
+
+public class StudentCurricularPlanPastLEEC extends StudentCurricularPlanPastLEEC_Base {
+
+    @Override
+    public void addApprovedEnrolments(final Collection<Enrolment> enrolments) {
+        return;
+    }
+
+}
