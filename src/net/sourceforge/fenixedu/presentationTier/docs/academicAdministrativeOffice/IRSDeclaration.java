1fde256a0727eeadd8200d271bf581bfc9b09a38
Luís Egídio <luis.egidio@ist.utl.pt>
2006-12-06 15:56:02 +0000

    indicating payed amounts

--- src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/IRSDeclaration.java
+++ src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/IRSDeclaration.java
@@ -3,10 +3,12 @@ package net.sourceforge.fenixedu.presentationTier.docs.academicAdministrativeOff
 import org.joda.time.YearMonthDay;
 
 import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.accounting.EventType;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequest;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.IRSDeclarationRequest;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.util.LanguageUtils;
+import net.sourceforge.fenixedu.util.Money;
 import net.sourceforge.fenixedu.util.StringUtils;
 
 public class IRSDeclaration extends AdministrativeOfficeDocument {
@@ -22,8 +24,6 @@ public class IRSDeclaration extends AdministrativeOfficeDocument {
 	final Person person = registration.getPerson();
 	
 	parameters.put("registration", registration);
-	//parameters.put("address", person.getAddress().toUpperCase());
-	//parameters.put("postalCode", person.getPostalCode().toUpperCase());
 	
 	final String name = person.getName().toUpperCase();
 	parameters.put("name", StringUtils.multipleLineRightPad(name, LINE_LENGTH, '-'));
@@ -37,8 +37,23 @@ public class IRSDeclaration extends AdministrativeOfficeDocument {
 	final Integer civilYear = IRSDeclarationRequest.getYear();
 	parameters.put("civilYear", civilYear.toString());
 
-	parameters.put("gratuityEventsPayedAmount", person.getGratuityEventsPayedAmount(civilYear));
-	parameters.put("administrativeOfficeFeeAndInsuranceEventsPayedAmount", person.getAdministrativeOfficeFeeAndInsuranceEventsPayedAmount(civilYear));
+	final StringBuilder eventTypes = new StringBuilder();
+	final StringBuilder payedAmounts = new StringBuilder();
+	Money totalPayedAmount = Money.ZERO;
+	for (final EventType eventType : EventType.values()) {
+	    Money payedAmount = person.getPayedAmount(eventType, civilYear);
+	    
+	    if (!payedAmount.isZero()) {
+		eventTypes.append("- ").append(enumerationBundle.getString(eventType.getQualifiedName())).append("\n");
+		payedAmounts.append("*").append(payedAmount.toPlainString()).append("Eur").append("\n");
+	    }
+	    
+	    totalPayedAmount = totalPayedAmount.add(payedAmount);
+	}
+	
+	parameters.put("eventTypes", eventTypes.toString());
+	parameters.put("payedAmounts", payedAmounts.toString());
+	parameters.put("totalPayedAmount", "*" + totalPayedAmount.toString() + "Eur");
 	
 	parameters.put("day", new YearMonthDay().toString("dd 'de' MMMM 'de' yyyy", LanguageUtils.getLocale()));
     }
