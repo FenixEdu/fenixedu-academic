86a390ebc41f5a34a28742da061a9f87aac3d29c
Luis Cruz <luis.cruz@ist.utl.pt>
2006-12-12 11:49:38 +0000

    Make script more generic.

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-mysqladmin -uroot drop isa
-mysqladmin -uroot create isa
-mysql -uroot isa < etc/dml.sql
-mysql -uroot isa < etc/ojb.sql
-mysql -uroot isa < etc/fenix-base.sql
+mysqladmin -uroot drop $1
+mysqladmin -uroot create $1
+mysql -uroot $1 < etc/dml.sql
+mysql -uroot $1 < etc/ojb.sql
+mysql -uroot $1 < etc/fenix-base.sql
 ant -f build_dataBaseUtils.xml generate-fenix-sql
-mysql -uroot isa < etc/fenix.sql
-mysql -uroot isa < etc/functionalities.sql
-mysql -uroot isa < etc/units.sql
+mysql -uroot $1 < etc/fenix.sql
+mysql -uroot $1 < etc/functionalities.sql
+mysql -uroot $1 < etc/units.sql
 ant -f build_dataBaseUtils.xml initialize-database
 echo "update IDENTIFICATION, LOGIN_ALIAS set IDENTIFICATION.PASSWORD = md5('pass') where IDENTIFICATION.ID_INTERNAL = LOGIN_ALIAS.KEY_LOGIN and LOGIN_ALIAS.ALIAS = 'admin';" > .tmp.init
-mysql -uroot isa < .tmp.init
+mysql -uroot $1 < .tmp.init
 rm -rf .tmp.init
