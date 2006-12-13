2b26cf42502eb93d6d2827a1cffe681f3cc925b9
Manuel Pinto <manuel.pinto@ist.utl.pt>
2006-12-13 09:49:07 +0000

    bugs fixed in Student Senior Information Management Interface

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/senior/ReadStudentSenior.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/senior/ReadStudentSenior.java
@@ -11,31 +11,38 @@ import net.sourceforge.fenixedu.domain.student.Senior;
 
 public class ReadStudentSenior extends Service {
 
-	public Senior run(Person person) {
-		Senior senior = null;
+    public Senior run(Person person) {
 
-		final Registration registration = person.getStudentByType(DegreeType.DEGREE);
-		if (registration == null) {
-			return null;
-		}
-
-		final StudentCurricularPlan studentCurricularPlan = registration.getActiveStudentCurricularPlan();
-		if (studentCurricularPlan == null) {
-			return null;
-		}
-
-		final DegreeCurricularPlan degreeCurricularPlan = studentCurricularPlan.getDegreeCurricularPlan();
-		final Degree degree = degreeCurricularPlan.getDegree();
-		senior = registration.getSenior();
-
-		if (senior == null) {
-			final int curricularYear = registration.getCurricularYear();
-			if (curricularYear == degree.getDegreeType().getYears()) {
-				senior = new Senior(registration);
-			} else {
-				return null;
-			}
-		}
-		return senior;
+	Senior senior = null;
+	
+	if(person == null) {
+	    return null;
 	}
+		
+	final Registration registration = person.getStudentByType(DegreeType.DEGREE);
+	if (registration == null) {
+	    return null;
+	}
+
+	final StudentCurricularPlan studentCurricularPlan = registration
+		.getActiveStudentCurricularPlan();
+	if (studentCurricularPlan == null) {
+	    return null;
+	}
+
+	final DegreeCurricularPlan degreeCurricularPlan = studentCurricularPlan
+		.getDegreeCurricularPlan();
+	final Degree degree = degreeCurricularPlan.getDegree();
+	senior = registration.getSenior();
+
+	if (senior == null) {
+	    final int curricularYear = registration.getCurricularYear();
+	    if (curricularYear == degree.getDegreeType().getYears()) {
+		senior = new Senior(registration);
+	    } else {
+		return null;
+	    }
+	}
+	return senior;
+    }
 }
--- src/net/sourceforge/fenixedu/presentationTier/Action/student/ViewExecutionCourseProjectsAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/student/ViewExecutionCourseProjectsAction.java
@@ -28,54 +28,55 @@ import org.apache.struts.action.ActionMapping;
 
 /**
  * @author asnr & scpo
- *
+ * 
  */
 public class ViewExecutionCourseProjectsAction extends FenixContextAction {
 
-	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
-		throws FenixActionException, FenixFilterException, FenixServiceException {
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+	    HttpServletResponse response) throws FenixActionException, FenixFilterException,
+	    FenixServiceException {
 
-		IUserView userView = getUserView(request);
+	IUserView userView = getUserView(request);
 
-		String executionCourseCodeString = request.getParameter("executionCourseCode");
+	String executionCourseCodeString = request.getParameter("executionCourseCode");
 
-		if (executionCourseCodeString == null || executionCourseCodeString.equals("")) {
-			ActionErrors actionErrors1 = new ActionErrors();
-			ActionError error1 = null;
-			// Create an ACTION_ERROR 
-			error1 = new ActionError("errors.notSelected.executionCourse");
-			actionErrors1.add("errors.notSelected.executionCourse", error1);
-			saveErrors(request, actionErrors1);
-			return mapping.findForward("insucess");
-		}
-		Integer executionCourseCode = new Integer(executionCourseCodeString);
-
-		Object[] args = {executionCourseCode, userView.getUtilizador()};
-        ISiteComponent viewProjectsComponent = (InfoSiteProjects) ServiceUtils.executeService(userView, "ReadExecutionCourseProjects", args);
-
-        Object[] args2 = { executionCourseCode };
-        InfoExecutionCourse infoExecutionCourse = (InfoExecutionCourse) ServiceUtils.executeService(userView, "ReadExecutionCourseByOID", args2);        
-        request.setAttribute("infoExecutionCourse", infoExecutionCourse);
-
-		InfoSiteProjects infoSiteProjects = (InfoSiteProjects) viewProjectsComponent;
-		List infoGroupPropertiesList = new ArrayList();
-		if (infoSiteProjects != null) {
-			infoGroupPropertiesList = infoSiteProjects.getInfoGroupPropertiesList();
-		}else{
-			ActionErrors actionErrors1 = new ActionErrors();
-			ActionError error1 = null;
-			error1 = new ActionError("errors.noStudentInAttendsSet");
-			actionErrors1.add("errors.noStudentInAttendsSet", error1);
-			saveErrors(request, actionErrors1);
-			return mapping.findForward("noprojects");
-		}
-
-		
-		
-		request.setAttribute("infoGroupPropertiesList", infoGroupPropertiesList);
-
-		return mapping.findForward("sucess");
-
+	if (executionCourseCodeString == null || executionCourseCodeString.equals("")) {
+	    ActionErrors actionErrors1 = new ActionErrors();
+	    ActionError error1 = null;
+	    // Create an ACTION_ERROR
+	    error1 = new ActionError("errors.notSelected.executionCourse");
+	    actionErrors1.add("errors.notSelected.executionCourse", error1);
+	    saveErrors(request, actionErrors1);
+	    return mapping.findForward("insucess");
 	}
+	Integer executionCourseCode = new Integer(executionCourseCodeString);
+
+	Object[] args = { executionCourseCode, userView.getUtilizador() };
+	ISiteComponent viewProjectsComponent = (InfoSiteProjects) ServiceUtils.executeService(userView,
+		"ReadExecutionCourseProjects", args);
+
+	Object[] args2 = { executionCourseCode };
+	InfoExecutionCourse infoExecutionCourse = (InfoExecutionCourse) ServiceUtils.executeService(
+		userView, "ReadExecutionCourseByOID", args2);
+	request.setAttribute("infoExecutionCourse", infoExecutionCourse);
+
+	InfoSiteProjects infoSiteProjects = (InfoSiteProjects) viewProjectsComponent;
+	List infoGroupPropertiesList = new ArrayList();
+	if (infoSiteProjects != null) {
+	    infoGroupPropertiesList = infoSiteProjects.getInfoGroupPropertiesList();
+	} else {
+	    ActionErrors actionErrors1 = new ActionErrors();
+	    ActionError error1 = null;
+	    error1 = new ActionError("errors.noStudentInAttendsSet");
+	    actionErrors1.add("errors.noStudentInAttendsSet", error1);
+	    saveErrors(request, actionErrors1);
+	    return mapping.findForward("noprojects");
+	}
+
+	request.setAttribute("infoGroupPropertiesList", infoGroupPropertiesList);
+
+	return mapping.findForward("sucess");
+
+    }
 
 }
