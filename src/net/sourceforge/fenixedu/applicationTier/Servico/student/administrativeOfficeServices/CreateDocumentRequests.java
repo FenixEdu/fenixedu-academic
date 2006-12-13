c7218827dedfc8f0b9256bc7bc413d03e2e2e15a
Luís Egídio <luis.egidio@ist.utl.pt>
2006-12-13 17:03:45 +0000

    reverting to previou version

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/administrativeOfficeServices/CreateDocumentRequests.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/administrativeOfficeServices/CreateDocumentRequests.java
@@ -6,7 +6,8 @@ import java.util.List;
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.dataTransferObject.degreeAdministrativeOffice.serviceRequest.documentRequest.DocumentRequestCreateBean;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
-import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequest.DocumentRequestCreator;
+import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.CertificateRequest;
+import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequestType;
 
 public class CreateDocumentRequests extends Service {
 
@@ -16,7 +17,28 @@ public class CreateDocumentRequests extends Service {
         for (final DocumentRequestCreateBean documentRequestCreateBean : documentRequestCreateBeans) {
             
             try {
-        	new DocumentRequestCreator(documentRequestCreateBean.getRegistration()).execute();
+        	final DocumentRequestType documentRequestType = documentRequestCreateBean.getChosenDocumentRequestType();
+        	if (documentRequestType.isCertificate()) {
+                    CertificateRequest.create(documentRequestCreateBean.getRegistration(),
+                            documentRequestCreateBean.getChosenDocumentRequestType(), 
+                            documentRequestCreateBean.getChosenDocumentPurposeType(),
+                            documentRequestCreateBean.getOtherPurpose(), 
+                            documentRequestCreateBean.getNotes(),
+                            documentRequestCreateBean.getUrgentRequest(),
+                            documentRequestCreateBean.getAverage(), 
+                            documentRequestCreateBean.getDetailed(),
+                            documentRequestCreateBean.getExecutionYear());
+        	} else if (documentRequestType.isDeclaration()) {
+//                    DeclarationRequest.create(documentRequestCreateBean.getRegistration(),
+//                            documentRequestCreateBean.getChosenDocumentRequestType(), 
+//                            documentRequestCreateBean.getChosenDocumentPurposeType(),
+//                            documentRequestCreateBean.getOtherPurpose(), 
+//                            documentRequestCreateBean.getNotes(),
+//                            documentRequestCreateBean.getAverage(), 
+//                            documentRequestCreateBean.getDetailed(),
+//                            documentRequestCreateBean.getExecutionYear(),
+//                            documentRequestCreateBean.getYear());
+        	}
             } catch (DomainException e) {
                 messages.add(e.getMessage());
             }
