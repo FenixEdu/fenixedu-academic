87a13ec3adf73107f6225cedddbb745306ec5320
Luis Cruz <luis.cruz@ist.utl.pt>
2006-12-19 14:17:03 +0000

    Bug fixes.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadAttendsByStudentIdAndExecutionPeriodId.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/ReadAttendsByStudentIdAndExecutionPeriodId.java
@@ -11,6 +11,7 @@ import java.util.List;
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.dataTransferObject.InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers;
 import net.sourceforge.fenixedu.domain.Attends;
+import net.sourceforge.fenixedu.domain.ExecutionCourse;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -24,28 +25,22 @@ public class ReadAttendsByStudentIdAndExecutionPeriodId extends Service {
     public List<InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers> run(Integer studentId, Integer executionPeriodId,
             Boolean onlyEnrolledCourses, Boolean onlyAttendsWithTeachers) throws ExcepcaoPersistencia {
 
-        ExecutionPeriod executionPeriod = rootDomainObject.readExecutionPeriodByOID(executionPeriodId);
-        Registration registration = rootDomainObject.readRegistrationByOID(studentId);
-        
-        List<Attends> attendsList = new ArrayList();
-        if(registration != null){
-            attendsList = registration.readAttendsByExecutionPeriod(executionPeriod);
-        }
-        
-        final List<InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers> infoAttendsList =
-			new ArrayList<InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers>(attendsList.size());
+        final List<InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers> infoAttendsList = new ArrayList<InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers>();
 
-        for (final Attends attends : attendsList) {
-            if (!(onlyEnrolledCourses.booleanValue() && (attends.getEnrolment() == null))) {
-				if(!onlyAttendsWithTeachers) {
-					infoAttendsList.add(InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers.newInfoFromDomain(attends));
-				
-				} else if((!attends.getDisciplinaExecucao().getProfessorships().isEmpty()) || 
-						(!attends.getDisciplinaExecucao().getNonAffiliatedTeachers().isEmpty())) {
-
-					infoAttendsList.add(InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers.newInfoFromDomain(attends));
-				}
-				
+        final ExecutionPeriod executionPeriod = rootDomainObject.readExecutionPeriodByOID(executionPeriodId);
+        final Registration someRegistration = rootDomainObject.readRegistrationByOID(studentId);
+        for (final Registration registration : someRegistration.getStudent().getRegistrationsSet()) {
+            for (final Attends attends : registration.getAssociatedAttendsSet()) {
+                final ExecutionCourse executionCourse = attends.getDisciplinaExecucao();
+                if (executionCourse.getExecutionPeriod() == executionPeriod
+                        && (!(onlyEnrolledCourses.booleanValue() && (attends.getEnrolment() == null)))) {
+                    if(!onlyAttendsWithTeachers) {
+                        infoAttendsList.add(InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers.newInfoFromDomain(attends));
+                    } else if((!attends.getDisciplinaExecucao().getProfessorships().isEmpty()) || 
+                            (!attends.getDisciplinaExecucao().getNonAffiliatedTeachers().isEmpty())) {
+                        infoAttendsList.add(InfoAttendsWithProfessorshipTeachersAndNonAffiliatedTeachers.newInfoFromDomain(attends));
+                    }
+                }
             }
         }
 
