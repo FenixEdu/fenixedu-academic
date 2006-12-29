13ad0f4f88860f81cf5daf6060003ea67a6743fd
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2006-12-29 17:44:18 +0000

    Refactored to be generic

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsForExecutionYear.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsForExecutionYear.java
@@ -1,24 +1,27 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
-import net.sourceforge.fenixedu.presentationTier.Action.departmentMember.teacherServiceDistribution.TeacherServiceBean;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
 import net.sourceforge.fenixedu.renderers.DataProvider;
 import net.sourceforge.fenixedu.renderers.components.converters.Converter;
 
-import org.apache.commons.collections.comparators.ReverseComparator;
+import org.apache.commons.beanutils.MethodUtils;
 
 public class ExecutionPeriodsForExecutionYear implements DataProvider {
 
     public Object provide(Object source, Object currentValue) {
-
-    	TeacherServiceBean bean = (TeacherServiceBean) source;
-    	ExecutionYear executionYear = bean.getExecutionYear();
+    	
+    	ExecutionYear executionYear;
+		try {
+			executionYear = (ExecutionYear) MethodUtils.invokeMethod(source, "getExecutionYear", null);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+		
     	List<ExecutionPeriod> periods = new ArrayList<ExecutionPeriod>();
     	if(executionYear!=null) {
     		periods.addAll(executionYear.getExecutionPeriods());
