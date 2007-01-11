9e3b787e957a23a82a961ff5571fac73e4bde8f4
Luis Cruz <luis.cruz@ist.utl.pt>
2007-01-11 00:32:16 +0000

    Refactored services to work with more than one registration. These services should be further refactored to avoid alot of unnecessary work that is still being done (searching for the person, the user, the written evaluation enrolment, etc)...

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/EnrolStudentInWrittenEvaluation.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/EnrolStudentInWrittenEvaluation.java
@@ -1,10 +1,16 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.student;
 
+import java.util.Set;
+
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
+import net.sourceforge.fenixedu.domain.Attends;
+import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.WrittenEvaluation;
 import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.student.Student;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class EnrolStudentInWrittenEvaluation extends Service {
@@ -12,14 +18,31 @@ public class EnrolStudentInWrittenEvaluation extends Service {
     public void run(String username, Integer writtenEvaluationOID) throws FenixServiceException,
             ExcepcaoPersistencia {
 
-        final Registration registration = Registration.readByUsername(username);
-
-        final WrittenEvaluation writtenEvaluation = (WrittenEvaluation) rootDomainObject.readEvaluationByOID(writtenEvaluationOID);
+	final WrittenEvaluation writtenEvaluation = (WrittenEvaluation) rootDomainObject.readEvaluationByOID(writtenEvaluationOID);
+	final Person person = Person.readPersonByUsername(username);
+	final Student student = person.getStudent();
+	final Registration registration = findCorrectRegistration(student, writtenEvaluation.getAssociatedExecutionCoursesSet());
         if (writtenEvaluation == null || registration == null) {
             throw new InvalidArgumentsServiceException();
         }
 
-        writtenEvaluation.enrolStudent(registration);
+        enrolmentAction(writtenEvaluation, registration);
     }
 
+    private Registration findCorrectRegistration(final Student student, final Set<ExecutionCourse> associatedExecutionCoursesSet) {
+	for (final Registration registration : student.getRegistrationsSet()) {
+	    for (final Attends attends : registration.getAssociatedAttendsSet()) {
+		final ExecutionCourse executionCourse = attends.getExecutionCourse();
+		if (associatedExecutionCoursesSet.contains(executionCourse)) {
+		    return registration;
+		}
+	    }
+	}
+	return null;
+    }
+
+    public void enrolmentAction(final WrittenEvaluation writtenEvaluation, final Registration registration) {
+	writtenEvaluation.enrolStudent(registration);
+    }
 }
+
--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/UnEnrollStudentInWrittenEvaluation.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/UnEnrollStudentInWrittenEvaluation.java
@@ -1,27 +1,13 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.student;
 
-import net.sourceforge.fenixedu.applicationTier.Service;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidArgumentsServiceException;
 import net.sourceforge.fenixedu.domain.WrittenEvaluation;
 import net.sourceforge.fenixedu.domain.student.Registration;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
-public class UnEnrollStudentInWrittenEvaluation extends Service {
+public class UnEnrollStudentInWrittenEvaluation extends EnrolStudentInWrittenEvaluation {
 
-    public void run(String username, Integer writtenEvaluationOID) throws FenixServiceException,
-            ExcepcaoPersistencia {
-
-        final Registration registration = Registration.readByUsername(username);
-
-        final WrittenEvaluation writtenEvaluation = (WrittenEvaluation) rootDomainObject
-                .readEvaluationByOID(writtenEvaluationOID);
-
-        if (writtenEvaluation == null || registration == null) {
-            throw new InvalidArgumentsServiceException();
-        }
-
-        writtenEvaluation.unEnrolStudent(registration);
+    @Override
+    public void enrolmentAction(final WrittenEvaluation writtenEvaluation, final Registration registration) {
+	writtenEvaluation.unEnrolStudent(registration);
     }
 
 }
