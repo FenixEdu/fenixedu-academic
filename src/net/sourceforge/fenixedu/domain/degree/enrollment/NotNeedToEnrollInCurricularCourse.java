ec1e12eeae2834f10f1f6a2bf04d608185d2b0cf
Shezad Anavarali <shezad@ist.utl.pt>
2007-01-11 15:37:26 +0000

    added Constructor

--- src/net/sourceforge/fenixedu/domain/degree/enrollment/NotNeedToEnrollInCurricularCourse.java
+++ src/net/sourceforge/fenixedu/domain/degree/enrollment/NotNeedToEnrollInCurricularCourse.java
@@ -1,7 +1,8 @@
 package net.sourceforge.fenixedu.domain.degree.enrollment;
 
+import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
-
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 
 /**
  * @author David Santos in Jun 17, 2004
@@ -10,14 +11,21 @@ import net.sourceforge.fenixedu.domain.RootDomainObject;
 public class NotNeedToEnrollInCurricularCourse extends NotNeedToEnrollInCurricularCourse_Base {
 
     public NotNeedToEnrollInCurricularCourse() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
-	
-	public void delete() {
-		removeStudentCurricularPlan();
-		removeCurricularCourse();
-		deleteDomainObject();
-	}
+
+    public NotNeedToEnrollInCurricularCourse(CurricularCourse curricularCourse,
+	    StudentCurricularPlan studentCurricularPlan) {
+	this();
+	setCurricularCourse(curricularCourse);
+	setStudentCurricularPlan(studentCurricularPlan);
+    }
+
+    public void delete() {
+	removeStudentCurricularPlan();
+	removeCurricularCourse();
+	deleteDomainObject();
+    }
 
 }
