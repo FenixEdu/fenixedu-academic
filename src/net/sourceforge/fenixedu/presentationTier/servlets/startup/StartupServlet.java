414ef4ac1f237f4a0e593f0be9dd80cf2f88294c
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-01-22 17:50:08 +0000

    bugs fixed.

--- src/net/sourceforge/fenixedu/presentationTier/servlets/startup/StartupServlet.java
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/startup/StartupServlet.java
@@ -71,7 +71,7 @@ public class StartupServlet extends HttpServlet {
                 ex.printStackTrace();
             }
 
-            //loadLogins();
+            loadLogins();
         } finally {
             Transaction.forceFinish();
         }
