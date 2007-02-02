146c718b3b3cae17426ac8897ce7f97561c5d6e4
Luis Cruz <luis.cruz@ist.utl.pt>
2007-02-02 12:57:23 +0000

    Remove log of too many pending transactions.

--- src/net/sourceforge/fenixedu/stm/Transaction.java
+++ src/net/sourceforge/fenixedu/stm/Transaction.java
@@ -77,23 +77,23 @@ public abstract class Transaction extends jvstm.Transaction {
 
     public static jvstm.Transaction begin(boolean readOnly) {
 	initializeIfNeeded();	
-        if ((WARN_TX_QUEUE_SIZE_LIMIT > 0) && (ACTIVE_TXS.getQueueSize() > WARN_TX_QUEUE_SIZE_LIMIT)) {
-            System.out.printf("WARNING: the number of active transactions is %d\n", ACTIVE_TXS.getQueueSize());
-            final jvstm.Transaction transaction = ACTIVE_TXS.getOldestTx();
-            System.out.printf("    The oldest active transaction is %s\n", transaction);
-            final StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("### Begin #########################################");
-            for (final StackTraceElement stackTraceElement : transaction.getThread().getStackTrace()) {
-                stringBuilder.append("\n");
-                stringBuilder.append(stackTraceElement.getClassName());
-                stringBuilder.append(" : ");
-                stringBuilder.append(stackTraceElement.getMethodName());
-                stringBuilder.append(" : ");
-                stringBuilder.append(stackTraceElement.getLineNumber());
-            }
-            System.out.println(stringBuilder.toString());
-            stringBuilder.append("\n--- End -------------------------------------------");
-        }
+//        if ((WARN_TX_QUEUE_SIZE_LIMIT > 0) && (ACTIVE_TXS.getQueueSize() > WARN_TX_QUEUE_SIZE_LIMIT)) {
+//            System.out.printf("WARNING: the number of active transactions is %d\n", ACTIVE_TXS.getQueueSize());
+//            final jvstm.Transaction transaction = ACTIVE_TXS.getOldestTx();
+//            System.out.printf("    The oldest active transaction is %s\n", transaction);
+//            final StringBuilder stringBuilder = new StringBuilder();
+//            stringBuilder.append("### Begin #########################################");
+//            for (final StackTraceElement stackTraceElement : transaction.getThread().getStackTrace()) {
+//                stringBuilder.append("\n");
+//                stringBuilder.append(stackTraceElement.getClassName());
+//                stringBuilder.append(" : ");
+//                stringBuilder.append(stackTraceElement.getMethodName());
+//                stringBuilder.append(" : ");
+//                stringBuilder.append(stackTraceElement.getLineNumber());
+//            }
+//            System.out.println(stringBuilder.toString());
+//            stringBuilder.append("\n--- End -------------------------------------------");
+//        }
 	jvstm.Transaction tx = jvstm.Transaction.begin(readOnly);
         if (TRANSACTION_MAX_DURATION_MILLIS > 0) {
             tx.setTimeoutMillis(TRANSACTION_MAX_DURATION_MILLIS);
