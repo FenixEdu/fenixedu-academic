4a87662e2d7bfb6db33d662432e512bb6b2e7aff
Luís Egídio <luis.egidio@ist.utl.pt>
2007-02-06 08:42:34 +0000

    new CurricularRule MaximumNumberOfCreditsForEnrolmentPeriod executor

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/CurricularRuleExecutorFactory.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/CurricularRuleExecutorFactory.java
@@ -9,6 +9,7 @@ import net.sourceforge.fenixedu.domain.curricularRules.CurricularRule;
 import net.sourceforge.fenixedu.domain.curricularRules.DegreeModulesSelectionLimit;
 import net.sourceforge.fenixedu.domain.curricularRules.EnrolmentToBeApprovedByCoordinator;
 import net.sourceforge.fenixedu.domain.curricularRules.Exclusiveness;
+import net.sourceforge.fenixedu.domain.curricularRules.MaximumNumberOfCreditsForEnrolmentPeriod;
 import net.sourceforge.fenixedu.domain.curricularRules.MinimumNumberOfCreditsToEnrol;
 import net.sourceforge.fenixedu.domain.curricularRules.RestrictionBetweenDegreeModules;
 import net.sourceforge.fenixedu.domain.curricularRules.RestrictionDoneDegreeModule;
@@ -20,15 +21,16 @@ public class CurricularRuleExecutorFactory {
     private static Map<Class<? extends CurricularRule>, CurricularRuleExecutor> executors = new HashMap<Class<? extends CurricularRule>, CurricularRuleExecutor>();
 
     static {
-	executors.put(RestrictionDoneDegreeModule.class, 	new RestrictionDoneDegreeModuleExecutor());
-	executors.put(RestrictionEnroledDegreeModule.class,	new RestrictionEnroledDegreeModuleExecutor());	
-	executors.put(RestrictionBetweenDegreeModules.class,	new RestrictionBetweenDegreeModulesExecutor());
-	executors.put(EnrolmentToBeApprovedByCoordinator.class, new EnrolmentToBeApprovedByCoordinatorExecutor());
-	executors.put(Exclusiveness.class, 			new ExclusivenessExecutor());
-	executors.put(MinimumNumberOfCreditsToEnrol.class,	new MinimumNumberOfCreditsToEnrolExecutor());
-	executors.put(DegreeModulesSelectionLimit.class,	new DegreeModulesSelectionLimitExecutor());
-	executors.put(CreditsLimit.class,			new CreditsLimitExecutor());
-	executors.put(AnyCurricularCourse.class,		new AnyCurricularCourseExecutor());
+	executors.put(RestrictionDoneDegreeModule.class, 		new RestrictionDoneDegreeModuleExecutor());
+	executors.put(RestrictionEnroledDegreeModule.class,		new RestrictionEnroledDegreeModuleExecutor());	
+	executors.put(RestrictionBetweenDegreeModules.class,		new RestrictionBetweenDegreeModulesExecutor());
+	executors.put(EnrolmentToBeApprovedByCoordinator.class, 	new EnrolmentToBeApprovedByCoordinatorExecutor());
+	executors.put(Exclusiveness.class, 				new ExclusivenessExecutor());
+	executors.put(MinimumNumberOfCreditsToEnrol.class,		new MinimumNumberOfCreditsToEnrolExecutor());
+	executors.put(DegreeModulesSelectionLimit.class,		new DegreeModulesSelectionLimitExecutor());
+	executors.put(CreditsLimit.class,				new CreditsLimitExecutor());
+	executors.put(AnyCurricularCourse.class,			new AnyCurricularCourseExecutor());
+	executors.put(MaximumNumberOfCreditsForEnrolmentPeriod.class,	new MaximumNumberOfCreditsForEnrolmentPeriodExecutor());
     }
 
     public static CurricularRuleExecutor findExecutor(final CurricularRule curricularRule) {
---
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/MaximumNumberOfCreditsForEnrolmentPeriodExecutor.java
@@ -0,0 +1,41 @@
+package net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors;
+
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.curricularRules.CurricularRule;
+import net.sourceforge.fenixedu.domain.curricularRules.MaximumNumberOfCreditsForEnrolmentPeriod;
+import net.sourceforge.fenixedu.domain.enrolment.DegreeModuleToEnrol;
+import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
+
+public class MaximumNumberOfCreditsForEnrolmentPeriodExecutor extends CurricularRuleExecutor {
+
+    @Override
+    protected RuleResult executeWithRules(final CurricularRule curricularRule, final EnrolmentContext enrolmentContext) {
+	final StudentCurricularPlan studentCurricularPlan = enrolmentContext.getStudentCurricularPlan();
+
+	final double maxECTS = MaximumNumberOfCreditsForEnrolmentPeriod.MAXIMUM_NUMBER_OF_CREDITS;
+	final double ects = studentCurricularPlan.getEctsCredits(enrolmentContext.getExecutionPeriod());
+	double availableEctsCredits = ((maxECTS - ects) > 0) ? maxECTS - ects : 0.0;
+	        
+	for (final DegreeModuleToEnrol degreeModuleToEnrol : enrolmentContext.getDegreeModuleToEnrol()) {
+	    final double degreeModuleEctsCredits = degreeModuleToEnrol.getContext().getChildDegreeModule().getEctsCredits();
+	    if (degreeModuleEctsCredits > availableEctsCredits) {
+		return RuleResult.createFalse();
+	    } else {
+		availableEctsCredits -= degreeModuleEctsCredits;
+	    }
+	}
+	        
+	return RuleResult.createTrue();
+    }
+    
+    @Override
+    protected RuleResult executeWithRulesAndTemporaryEnrolment(final CurricularRule curricularRule, final EnrolmentContext enrolmentContext) {
+	return executeWithRules(curricularRule, enrolmentContext);
+    }
+
+    @Override
+    protected RuleResult executeNoRules(final CurricularRule curricularRule, final EnrolmentContext enrolmentContext) {
+	return RuleResult.createTrue();
+    }
+
+}
