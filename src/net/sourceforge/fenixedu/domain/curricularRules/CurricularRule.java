6b26781c76f5dff8360c1c8d11108642ae00c34d
João Florindo <jdnf@linkare.pt>
2007-02-07 16:56:41 +0000

    new method init

--- src/net/sourceforge/fenixedu/domain/curricularRules/CurricularRule.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/CurricularRule.java
@@ -26,15 +26,35 @@ public abstract class CurricularRule extends CurricularRule_Base {
     
     protected void init(final DegreeModule degreeModuleToApplyRule, final CourseGroup contextCourseGroup,
 	    final ExecutionPeriod begin, final ExecutionPeriod end, final CurricularRuleType type) {
+	
+	init(degreeModuleToApplyRule, contextCourseGroup, begin, end);
+	checkCurricularRuleType(type);
+	setCurricularRuleType(type);
+    }
+    
+    private void checkCurricularRuleType(final CurricularRuleType type) {
+	if (type == null) {
+	    throw new DomainException("curricular.rule.invalid.parameters");
+	}
+    }
 
+    protected void init(final DegreeModule degreeModuleToApplyRule, final CourseGroup contextCourseGroup,
+	    final ExecutionPeriod begin, final ExecutionPeriod end) {
+
+	checkParameters(degreeModuleToApplyRule, begin);
 	checkExecutionPeriods(begin, end);
 	setDegreeModuleToApplyRule(degreeModuleToApplyRule);
 	setContextCourseGroup(contextCourseGroup);
 	setBegin(begin);
 	setEnd(end);
-	setCurricularRuleType(type);
     }
     
+    private void checkParameters(final DegreeModule degreeModuleToApplyRule, final ExecutionPeriod begin) {
+	if (degreeModuleToApplyRule == null || begin == null) {
+	    throw new DomainException("curricular.rule.invalid.parameters");
+	}
+    }
+
     protected void edit(ExecutionPeriod beginExecutionPeriod, ExecutionPeriod endExecutionPeriod) {
         checkExecutionPeriods(beginExecutionPeriod, endExecutionPeriod);
         setBegin(beginExecutionPeriod);
