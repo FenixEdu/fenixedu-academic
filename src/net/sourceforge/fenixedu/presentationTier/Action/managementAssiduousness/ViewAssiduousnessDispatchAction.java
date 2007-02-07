43eda24351055184619c3ed99042b78f3a8b71af
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-02-07 14:51:04 +0000

    organize imports

--- src/net/sourceforge/fenixedu/presentationTier/Action/managementAssiduousness/ViewAssiduousnessDispatchAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/managementAssiduousness/ViewAssiduousnessDispatchAction.java
@@ -7,13 +7,11 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.domain.assiduousness.JustificationMotive;
 import net.sourceforge.fenixedu.domain.assiduousness.WorkScheduleType;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionUtils;
 
 import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.collections.comparators.ComparatorChain;
