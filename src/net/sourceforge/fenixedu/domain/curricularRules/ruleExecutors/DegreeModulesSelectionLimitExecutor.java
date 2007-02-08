3a51d6224b3cd9367c22b10fdf22384ab37c65b7
João Florindo <jdnf@linkare.pt>
2007-02-08 16:33:40 +0000

    Corrected Executor for DegreeModuleSelectionLimit

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/DegreeModulesSelectionLimitExecutor.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/DegreeModulesSelectionLimitExecutor.java
@@ -1,35 +1,48 @@
 package net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors;
 
-import java.util.List;
-
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.curricularRules.CurricularRule;
 import net.sourceforge.fenixedu.domain.curricularRules.DegreeModulesSelectionLimit;
 import net.sourceforge.fenixedu.domain.degreeStructure.Context;
 import net.sourceforge.fenixedu.domain.degreeStructure.CourseGroup;
 import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
+import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumGroup;
 
 public class DegreeModulesSelectionLimitExecutor extends CurricularRuleExecutor {
 
     @Override
-    protected RuleResult executeEnrolmentWithRules(final CurricularRule curricularRule, final EnrolmentContext enrolmentContext) {
-	
+    protected RuleResult executeEnrolmentWithRules(final CurricularRule curricularRule,
+	    final EnrolmentContext enrolmentContext) {
+
 	final DegreeModulesSelectionLimit rule = (DegreeModulesSelectionLimit) curricularRule;
 
-	if (ruleWasSelectedFromAnyModuleToEnrol(enrolmentContext, curricularRule)
-		&& !rule.appliesToContext(searchDegreeModuleToEnrol(enrolmentContext,rule.getDegreeModuleToApplyRule()).getContext())) {
+	if (ruleWasSelectedFromAnyModuleToEnrol(enrolmentContext, curricularRule)) {
 	    return RuleResult.createNA();
 	}
+
+	final CourseGroup courseGroup = rule.getDegreeModuleToApplyRule();
+	final CurriculumGroup curriculumGroup = (CurriculumGroup) searchCurriculumModule(enrolmentContext, rule);
+	final CourseGroup parentCourseGroup = curriculumGroup.getCurriculumGroup().getDegreeModule();
 	
-	final CourseGroup courseGroup = (CourseGroup) rule.getDegreeModuleToApplyRule();
-	final List<Context> childContexts = courseGroup.getChildContexts(enrolmentContext.getExecutionPeriod());
-	
-	int numberOfDegreeModulesToEnrol = countNumberOfDegreeModulesToEnrol(enrolmentContext, childContexts);
-	int numberOfEnroledDegreeModules = countNumberOfEnroledDegreeModules(enrolmentContext, childContexts);
-	
-	if (rule.allowNumberOfDegreeModules(numberOfDegreeModulesToEnrol + numberOfEnroledDegreeModules)) {
-	    return RuleResult.createTrue();
+	if (rule.appliesToCourseGroup(parentCourseGroup)) {
+	    int total = countTotalDegreeModules(enrolmentContext, courseGroup, curriculumGroup);
+	    return rule.allowNumberOfDegreeModules(total) ? RuleResult.createTrue() : createFalseRuleResult(rule);
 	}
+
+	return RuleResult.createNA();
+
+    }
+    
+    private int countTotalDegreeModules(final EnrolmentContext enrolmentContext, final CourseGroup courseGroup, final CurriculumGroup curriculumGroup) {
 	
+	int numberOfDegreeModulesToEnrol = countNumberOfDegreeModulesToEnrol(enrolmentContext, courseGroup);
+	int numberOfApprovedEnrolments = curriculumGroup.getNumberOfApprovedEnrolments();
+	int numberOfEnrolments = curriculumGroup.getNumberOfEnrolments(enrolmentContext.getExecutionPeriod());
+	
+	return numberOfApprovedEnrolments + numberOfEnrolments + numberOfDegreeModulesToEnrol;
+    }
+
+    private RuleResult createFalseRuleResult(final DegreeModulesSelectionLimit rule) {
 	if (rule.getMinimumLimit().equals(rule.getMaximumLimit())) {
 	    return RuleResult.createFalse(
 		    "curricularRules.ruleExecutors.DegreeModulesSelectionLimitExecutor.limit.exceded", 
@@ -41,19 +54,9 @@ public class DegreeModulesSelectionLimitExecutor extends CurricularRuleExecutor 
 	}
     }
 
-    private int countNumberOfEnroledDegreeModules(final EnrolmentContext enrolmentContext, final List<Context> childContexts) {
+    private int countNumberOfDegreeModulesToEnrol(final EnrolmentContext enrolmentContext, final CourseGroup courseGroup) {
 	int result = 0;
-	for (final Context context : childContexts) {
-	    if (isEnroled(enrolmentContext, context.getChildDegreeModule())) {
-		result++;
-	    }
-	}
-	return result;
-    }
-
-    private int countNumberOfDegreeModulesToEnrol(final EnrolmentContext enrolmentContext, final List<Context> childContexts) {
-	int result = 0;
-	for (final Context context : childContexts) {
+	for (final Context context : courseGroup.getChildContexts(enrolmentContext.getExecutionPeriod())) {
 	    if (isEnrolling(enrolmentContext, context.getChildDegreeModule())) {
 		result++;
 	    }
@@ -63,7 +66,32 @@ public class DegreeModulesSelectionLimitExecutor extends CurricularRuleExecutor 
 
     @Override
     protected RuleResult executeEnrolmentWithRulesAndTemporaryEnrolment(final CurricularRule curricularRule, final EnrolmentContext enrolmentContext) {
-	return executeEnrolmentWithRules(curricularRule, enrolmentContext);
+	
+	final DegreeModulesSelectionLimit rule = (DegreeModulesSelectionLimit) curricularRule;
+	
+	if (ruleWasSelectedFromAnyModuleToEnrol(enrolmentContext, curricularRule)) {
+	    return RuleResult.createNA();
+	}
+	
+	final CourseGroup courseGroup = rule.getDegreeModuleToApplyRule();
+	final CurriculumGroup curriculumGroup = (CurriculumGroup) searchCurriculumModule(enrolmentContext, rule);
+	final CourseGroup parentCourseGroup = curriculumGroup.getCurriculumGroup().getDegreeModule();
+	
+	if (rule.appliesToCourseGroup(parentCourseGroup)) {
+
+	    int total = countTotalDegreeModules(enrolmentContext, courseGroup, curriculumGroup);
+
+	    if (!rule.allowNumberOfDegreeModules(total)) {
+		return createFalseRuleResult(rule);
+	    }
+	    
+	    final ExecutionPeriod executionPeriod = enrolmentContext.getExecutionPeriod();
+	    total += curriculumGroup.getNumberOfEnrolments(executionPeriod.getPreviousExecutionPeriod());
+	    return rule.allowNumberOfDegreeModules(total) ? RuleResult.createTrue() : RuleResult.createTrue(EnrolmentResultType.TEMPORARY);
+	    
+	}
+	
+	return RuleResult.createNA();
     }
 
     @Override
