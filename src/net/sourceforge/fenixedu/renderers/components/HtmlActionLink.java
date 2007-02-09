30522f1ae7f2e5902486c75d8d251d5a29aa65ee
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-02-09 15:58:35 +0000

    bug fix

--- src/net/sourceforge/fenixedu/renderers/components/HtmlActionLink.java
+++ src/net/sourceforge/fenixedu/renderers/components/HtmlActionLink.java
@@ -5,10 +5,6 @@ import javax.servlet.jsp.PageContext;
 import net.sourceforge.fenixedu.renderers.components.tags.HtmlTag;
 
 public class HtmlActionLink extends HtmlSimpleValueComponent {
-    private static final String ACTION_LINK_FIELD_KEY = HtmlActionLink.class.getName() + "_key";
-
-    private static final String ACTION_LINK_FIELD_ID = HtmlActionLink.class.getName() + "_id";
-
     private String text;
 
     private HtmlComponent body;
@@ -62,43 +58,31 @@ public class HtmlActionLink extends HtmlSimpleValueComponent {
 
     @Override
     public HtmlTag getOwnTag(PageContext context) {
-	HtmlHiddenField hidden = getActionLinkHiddenField(context);
+
+	HtmlHiddenField hidden = new HtmlHiddenField(getName(), null);
+	hidden.setId("____" + getName());
 
 	HtmlLink link = new HtmlLink();
+
 	link.setText(getText());
 	link.setBody(getBody());
 	link.setUrl("#");
 	link.setModuleRelative(false);
 	link.setContextRelative(false);
 
-	final String onClickEvent = getOnClick() != null ? getOnClick() : "";
-	setOnClick(onClickEvent + " " + "document.getElementById('" + hidden.getId() + "').value='"
+	setOnClick(getOnClick() + "document.getElementById('" + hidden.getId() + "').value='"
 		+ getName() + "'; " + "document.getElementById('" + hidden.getId() + "').form.submit();");
-	
 
 	HtmlTag tag = super.getOwnTag(context);
 
 	tag.setName(null);
+
 	tag.addChild(hidden.getOwnTag(context));
-	
+
 	HtmlTag linkTag = link.getOwnTag(context);
 	linkTag.copyAttributes(tag);
 	tag.addChild(linkTag);
 
 	return tag;
-    }
-
-    private HtmlHiddenField getActionLinkHiddenField(PageContext context) {
-	Object hiddenField = context.findAttribute(ACTION_LINK_FIELD_KEY);
-
-	if (hiddenField != null) {
-	    return (HtmlHiddenField) hiddenField;
-	} else {
-	    HtmlHiddenField hidden = new HtmlHiddenField(getName(), null);
-	    hidden.setId("____" + ACTION_LINK_FIELD_ID);
-
-	    context.setAttribute(ACTION_LINK_FIELD_KEY, hiddenField, PageContext.REQUEST_SCOPE);
-	    return hidden;
-	}
     }
 }
