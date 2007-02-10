7bb5aa8c26302f454288ae7be1b194392558c167
João Florindo <jdnf@linkare.pt>
2007-02-10 13:46:55 +0000

    use init method

--- src/net/sourceforge/fenixedu/domain/curricularRules/MinimumNumberOfCreditsToEnrol.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/MinimumNumberOfCreditsToEnrol.java
@@ -11,37 +11,27 @@ import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
 public class MinimumNumberOfCreditsToEnrol extends MinimumNumberOfCreditsToEnrol_Base {
     
-    private MinimumNumberOfCreditsToEnrol(Double minimumNumberOfCredits) {
+    private MinimumNumberOfCreditsToEnrol(final Double minimumNumberOfCredits) {
         super();
         checkCredits(minimumNumberOfCredits);
         setMinimumCredits(minimumNumberOfCredits);
         setCurricularRuleType(CurricularRuleType.MINIMUM_NUMBER_OF_CREDITS_TO_ENROL);
     }
 
-    private void checkCredits(Double minimumNumberOfCredits) throws DomainException {
+    private void checkCredits(final Double minimumNumberOfCredits) throws DomainException {
         if (minimumNumberOfCredits == null) {
             throw new DomainException("curricular.rule.invalid.parameters");
         }
     }
     
-    protected MinimumNumberOfCreditsToEnrol(DegreeModule degreeModuleToApplyRule, CourseGroup contextCourseGroup,
-            ExecutionPeriod begin, ExecutionPeriod end, Double minimumNumberOfCredits) {
+    protected MinimumNumberOfCreditsToEnrol(final DegreeModule degreeModuleToApplyRule, final CourseGroup contextCourseGroup,
+            final ExecutionPeriod begin, final ExecutionPeriod end, final Double minimumNumberOfCredits) {
         
         this(minimumNumberOfCredits);
-        
-        if (degreeModuleToApplyRule == null || begin == null) {
-            throw new DomainException("curricular.rule.invalid.parameters");
-        }
-        
-        checkExecutionPeriods(begin, end);
-        
-        setDegreeModuleToApplyRule(degreeModuleToApplyRule);
-        setContextCourseGroup(contextCourseGroup);
-        setBegin(begin);
-        setEnd(end);
+        init(degreeModuleToApplyRule, contextCourseGroup, begin, end);
     }
     
-    protected void edit(CourseGroup contextCourseGroup, Double minimumNumberOfCredits) {
+    protected void edit(final CourseGroup contextCourseGroup, final Double minimumNumberOfCredits) {
         checkCredits(minimumNumberOfCredits);
         setContextCourseGroup(contextCourseGroup);
         setMinimumCredits(minimumNumberOfCredits);
