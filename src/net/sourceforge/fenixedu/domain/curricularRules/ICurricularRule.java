e4aa58a9b5752fac43d7d3b70dddb7545ce77d15
João Florindo <jdnf@linkare.pt>
2007-02-12 09:42:31 +0000

    interface to represent a CurricularRule

---
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ICurricularRule.java
@@ -0,0 +1,33 @@
+package net.sourceforge.fenixedu.domain.curricularRules;
+
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors.CurricularRuleLevel;
+import net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors.RuleResult;
+import net.sourceforge.fenixedu.domain.degreeStructure.Context;
+import net.sourceforge.fenixedu.domain.degreeStructure.CourseGroup;
+import net.sourceforge.fenixedu.domain.degreeStructure.DegreeModule;
+import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
+
+public interface ICurricularRule {
+    
+    public DegreeModule getDegreeModuleToApplyRule();
+    public CourseGroup getContextCourseGroup();
+    public CompositeRule getParentCompositeRule();
+    
+    public CurricularRuleType getCurricularRuleType();
+    
+    public ExecutionPeriod getBegin();
+    public ExecutionPeriod getEnd();
+    
+    public boolean appliesToContext(final Context context);
+    public boolean appliesToCourseGroup(final CourseGroup courseGroup);
+    
+    public boolean isCompositeRule();
+    public boolean isValid(ExecutionPeriod executionPeriod);
+    public boolean isValid(ExecutionYear executionYear);
+    
+    public RuleResult evaluate(final EnrolmentContext enrolmentContext);
+    public RuleResult evaluate(final EnrolmentContext enrolmentContext, final CurricularRuleLevel level);
+
+}
