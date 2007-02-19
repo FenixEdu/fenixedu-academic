3ff31cffcb45a4d89a756ca67e12b44027ca8cee
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2007-02-19 14:42:50 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/assiduousness/Justification.java
+++ src/net/sourceforge/fenixedu/domain/assiduousness/Justification.java
@@ -5,6 +5,7 @@ import net.sourceforge.fenixedu.domain.RootDomainObject;
 import org.joda.time.Partial;
 import org.joda.time.TimeOfDay;
 import org.joda.time.YearMonthDay;
+import org.joda.time.chrono.GregorianChronology;
 
 public class Justification extends Justification_Base {
 
@@ -15,13 +16,13 @@ public class Justification extends Justification_Base {
 
     public Partial getPartialDate() {
         Partial p = new Partial();
-        YearMonthDay y = getDate().toYearMonthDay();
+        YearMonthDay y = getDate().toDateTime(GregorianChronology.getInstanceUTC()).toYearMonthDay();
         if (y != null) {
             for (int i = 0; i < y.getFields().length; i++) {
                 p = p.with(y.getFieldType(i), y.getValue(i));
             }
         }
-        TimeOfDay t = getDate().toTimeOfDay();
+        TimeOfDay t = getDate().toDateTime(GregorianChronology.getInstanceUTC()).toTimeOfDay();
         if (t != null && !t.isEqual(new TimeOfDay(0, 0, 0))) {
             for (int i = 0; i < t.getFields().length; i++) {
                 p = p.with(t.getFieldType(i), t.getValue(i));
