a4b25c80ad8346d8dde8424d3c66f6de6e839b64
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-02-21 07:26:21 +0000

    bugs fixed.

--- src/net/sourceforge/fenixedu/domain/organizationalStructure/UnitUtils.java
+++ src/net/sourceforge/fenixedu/domain/organizationalStructure/UnitUtils.java
@@ -18,149 +18,153 @@ import org.joda.time.YearMonthDay;
 
 public class UnitUtils {
 
-	public static List<Unit> readAllExternalInstitutionUnits() {
-		List<Unit> allExternalUnits = new ArrayList<Unit>();
-		allExternalUnits.addAll(readExternalInstitutionUnit().getAllSubUnits());
-		return allExternalUnits;
-	}
+    public static List<Unit> readAllExternalInstitutionUnits() {
+	List<Unit> allExternalUnits = new ArrayList<Unit>();
+	allExternalUnits.addAll(readExternalInstitutionUnit().getAllSubUnits());
+	return allExternalUnits;
+    }
 
-	public static Unit readExternalInstitutionUnitByName(String name) {
-		for (Unit unit : readAllExternalInstitutionUnits()) {
-			if (unit.getName().equals(name)) {
-				return unit;
-			}
+    public static Unit readExternalInstitutionUnitByName(String name) {
+	for (Unit unit : readAllExternalInstitutionUnits()) {
+	    if (unit.getName().equals(name)) {
+		return unit;
+	    }
+	}
+	return null;
+    }
+
+    public static List<Unit> readAllUnitsWithoutParents() {
+	List<Unit> allUnitsWithoutParent = new ArrayList<Unit>();
+	for (Party party : RootDomainObject.getInstance().getPartys()) {
+	    if (party instanceof Unit) {
+                Unit unit = (Unit) party;
+                if (unit.getParentUnits().isEmpty()) {
+                    allUnitsWithoutParent.add(unit);
+                }
+	    }
+	}
+	return allUnitsWithoutParent;
+    }
+
+    public static List<Unit> readAllUnitsByType(PartyTypeEnum type) {
+	final List<Unit> result = new ArrayList<Unit>();
+	final YearMonthDay now = new YearMonthDay();
+	for (Party party : RootDomainObject.getInstance().getPartys()) {
+	    if (party instanceof Unit) {
+		Unit unit = (Unit) party;
+		if (unit.isActive(now) && unit.getType() == type) {
+		    result.add(unit);
 		}
-		return null;
+	    }
+	}
+	return result;
+    }
+
+    public static List<Unit> readAllDepartmentUnits() {
+	return readAllUnitsByType(PartyTypeEnum.DEPARTMENT);
+    }
+
+    public static Unit readUnitWithoutParentstByAcronym(String acronym) {
+	for (Unit topUnit : readAllUnitsWithoutParents()) {
+	    if (topUnit.getAcronym() != null && topUnit.getAcronym().equals(acronym)) {
+		return topUnit;
+	    }
+	}
+	return null;
+    }
+
+    public static Unit readExternalInstitutionUnit() {
+	return RootDomainObject.getInstance().getExternalInstitutionUnit();
+    }
+
+    public static Unit readInstitutionUnit() {
+	return RootDomainObject.getInstance().getInstitutionUnit();
+    }
+
+    public static Unit readEarthUnit() {
+	return RootDomainObject.getInstance().getEarthUnit();
+    }
+
+    public static Set<Unit> readExternalUnitsByNameAndTypes(final String unitName,
+	    List<PartyTypeEnum> types) {
+	if (unitName == null) {
+	    return Collections.emptySet();
 	}
 
-	public static List<Unit> readAllUnitsWithoutParents() {
-		List<Unit> allUnitsWithoutParent = new ArrayList<Unit>();
-		for (Party party : RootDomainObject.getInstance().getPartys()) {
-			Unit unit = (Unit) party;
-			if (unit.getParentUnits().isEmpty()) {
-				allUnitsWithoutParent.add(unit);
-			}
-		}
-		return allUnitsWithoutParent;
+	final String nameToSearch = unitName.replaceAll("%", ".*").toLowerCase();
+	final Set<Unit> result = new HashSet<Unit>();
+	for (final Unit unit : Unit.readAllUnits()) {
+	    if (types.contains(unit.getType()) && unit.getName().toLowerCase().matches(nameToSearch)
+		    && !unit.isInternal()) {
+		result.add(unit);
+	    }
+	}
+	return result;
+    }
+
+    public static List<Unit> getUnitFullPath(final Unit unit,
+	    final List<AccountabilityTypeEnum> validAccountabilityTypes) {
+	final Collection<Unit> parentUnits = unit.getParentUnits(validAccountabilityTypes);
+	if (parentUnits.isEmpty()) {
+	    return Collections.emptyList();
+	}
+	if (parentUnits.size() == 1) {
+	    final List<Unit> result = new ArrayList<Unit>();
+	    result.add(unit);
+	    result.addAll(0, getUnitFullPath(parentUnits.iterator().next(), validAccountabilityTypes));
+	    return result;
+	}
+	throw new DomainException("error.unitUtils.unit.full.path.has.more.than.one.parent");
+    }
+
+    public static StringBuilder getUnitFullPathName(final Unit unit,
+	    final List<AccountabilityTypeEnum> validAccountabilityTypes) {
+
+	if (unit == readEarthUnit()) {
+	    return new StringBuilder(0);
 	}
 
-	public static List<Unit> readAllUnitsByType(PartyTypeEnum type) {
-		final List<Unit> result = new ArrayList<Unit>();
-		final YearMonthDay now = new YearMonthDay();
-		for (Party party : RootDomainObject.getInstance().getPartys()) {
-			if (party instanceof Unit) {
-				Unit unit = (Unit) party;
-				if (unit.isActive(now) && unit.getType() == type) {
-					result.add(unit);
-				}
-			}
-		}
-		return result;
+	final Collection<Unit> parentUnits = unit.getParentUnits(validAccountabilityTypes);
+	if (parentUnits.isEmpty()) {
+	    return new StringBuilder(unit.getName());
+	}
+	if (parentUnits.size() == 1) {
+	    final StringBuilder builder = new StringBuilder();
+	    builder.append(parentUnits.iterator().next() == readEarthUnit() ? "" : " > ").append(
+		    unit.getName());
+	    builder.insert(0, getUnitFullPathName(parentUnits.iterator().next(),
+		    validAccountabilityTypes));
+	    return builder;
+	}
+	throw new DomainException("error.unitUtils.unit.full.path.has.more.than.one.parent");
+    }
+
+    public static List<Unit> readExternalUnitsByNameAndTypesStartingAtEarth(final String unitName,
+	    List<PartyTypeEnum> types) {
+
+	if (unitName == null) {
+	    return Collections.emptyList();
 	}
 
-	public static List<Unit> readAllDepartmentUnits() {
-		return readAllUnitsByType(PartyTypeEnum.DEPARTMENT);
+	final String nameToSearch = unitName.replaceAll("%", ".*").toLowerCase();
+	return readExternalUnitsByNameAndTypesStartingEarth(nameToSearch, types, readEarthUnit());
+    }
+
+    private static List<Unit> readExternalUnitsByNameAndTypesStartingEarth(String unitName,
+	    List<PartyTypeEnum> types, Unit unit) {
+	final List<Unit> result = new ArrayList<Unit>();
+	for (final Unit each : unit.getSubUnits()) {
+	    checkUnit(unitName, types, result, each);
+	    result.addAll(readExternalUnitsByNameAndTypesStartingEarth(unitName, types, each));
 	}
+	return result;
+    }
 
-	public static Unit readUnitWithoutParentstByAcronym(String acronym) {
-		for (Unit topUnit : readAllUnitsWithoutParents()) {
-			if (topUnit.getAcronym() != null && topUnit.getAcronym().equals(acronym)) {
-				return topUnit;
-			}
-		}
-		return null;
+    private static void checkUnit(String unitName, List<PartyTypeEnum> types, final List<Unit> result,
+	    final Unit unit) {
+	if (types.contains(unit.getType()) && unit.getName().toLowerCase().matches(unitName)
+		&& !unit.isInternal()) {
+	    result.add(unit);
 	}
-
-	public static Unit readExternalInstitutionUnit() {
-		return RootDomainObject.getInstance().getExternalInstitutionUnit();
-	}
-
-	public static Unit readInstitutionUnit() {
-		return RootDomainObject.getInstance().getInstitutionUnit();
-	}
-
-	public static Unit readEarthUnit() {
-		return RootDomainObject.getInstance().getEarthUnit();
-	}
-
-	public static Set<Unit> readExternalUnitsByNameAndTypes(final String unitName, List<PartyTypeEnum> types) {
-		if (unitName == null) {
-			return Collections.emptySet();
-		}
-
-		final String nameToSearch = unitName.replaceAll("%", ".*").toLowerCase();
-		final Set<Unit> result = new HashSet<Unit>();
-		for (final Unit unit : Unit.readAllUnits()) {
-			if (types.contains(unit.getType()) && unit.getName().toLowerCase().matches(nameToSearch)
-					&& !unit.isInternal()) {
-				result.add(unit);
-			}
-		}
-		return result;
-	}
-
-	public static List<Unit> getUnitFullPath(final Unit unit,
-			final List<AccountabilityTypeEnum> validAccountabilityTypes) {
-		final Collection<Unit> parentUnits = unit.getParentUnits(validAccountabilityTypes);
-		if (parentUnits.isEmpty()) {
-			return Collections.emptyList();
-		}
-		if (parentUnits.size() == 1) {
-			final List<Unit> result = new ArrayList<Unit>();
-			result.add(unit);
-			result.addAll(0, getUnitFullPath(parentUnits.iterator().next(), validAccountabilityTypes));
-			return result;
-		}
-		throw new DomainException("error.unitUtils.unit.full.path.has.more.than.one.parent");
-	}
-
-	public static StringBuilder getUnitFullPathName(final Unit unit,
-			final List<AccountabilityTypeEnum> validAccountabilityTypes) {
-
-		if (unit == readEarthUnit()) {
-			return new StringBuilder(0);
-		}
-
-		final Collection<Unit> parentUnits = unit.getParentUnits(validAccountabilityTypes);
-		if (parentUnits.isEmpty()) {
-			return new StringBuilder(unit.getName());
-		}
-		if (parentUnits.size() == 1) {
-			final StringBuilder builder = new StringBuilder();
-			builder.append(parentUnits.iterator().next() == readEarthUnit() ? "" : " > ").append(
-					unit.getName());
-			builder.insert(0, getUnitFullPathName(parentUnits.iterator().next(), validAccountabilityTypes));
-			return builder;
-		}
-		throw new DomainException("error.unitUtils.unit.full.path.has.more.than.one.parent");
-	}
-
-	public static List<Unit> readExternalUnitsByNameAndTypesStartingAtEarth(final String unitName,
-			List<PartyTypeEnum> types) {
-
-		if (unitName == null) {
-			return Collections.emptyList();
-		}
-
-		final String nameToSearch = unitName.replaceAll("%", ".*").toLowerCase();
-		return readExternalUnitsByNameAndTypesStartingEarth(nameToSearch, types, readEarthUnit());
-	}
-
-	private static List<Unit> readExternalUnitsByNameAndTypesStartingEarth(String unitName,
-			List<PartyTypeEnum> types, Unit unit) {
-		final List<Unit> result = new ArrayList<Unit>();
-		for (final Unit each : unit.getSubUnits()) {
-			checkUnit(unitName, types, result, each);
-			result.addAll(readExternalUnitsByNameAndTypesStartingEarth(unitName, types, each));
-		}
-		return result;
-	}
-
-	private static void checkUnit(String unitName, List<PartyTypeEnum> types, final List<Unit> result,
-			final Unit unit) {
-		if (types.contains(unit.getType()) && unit.getName().toLowerCase().matches(unitName)
-				&& !unit.isInternal()) {
-			result.add(unit);
-		}
-	}
+    }
 }
