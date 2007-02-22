c3faa5b2d9c36508cedb5c8f8e62b915b5740b17
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2007-02-22 16:11:54 +0000

    Performance improvements

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/SearchAcademicUnits.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/SearchAcademicUnits.java
@@ -1,26 +1,16 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.commons;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import net.sourceforge.fenixedu.domain.Degree;
-import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.organizationalStructure.PartyTypeEnum;
+import net.sourceforge.fenixedu.domain.organizationalStructure.UnitUtils;
 
 public class SearchAcademicUnits extends AbstractSearchObjects {
 	
 	public List run(Class type, String value, int limit, Map<String, String> arguments) {
-		 
-		Set<Degree> degrees = rootDomainObject.readAllDomainObjects(Degree.class);
-		List<Unit> units = new ArrayList<Unit> ();
-		for(Degree degree: degrees) {
-			if(degree.hasUnit()) {
-				units.add(degree.getUnit());
-			}
-		}
 		
-		return super.process(units, value, limit, arguments);
+		return super.process(UnitUtils.readAllActiveUnitsByType(PartyTypeEnum.DEGREE_UNIT), value, limit, arguments);
 	}
 
 }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/SearchOrganizationalUnits.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/SearchOrganizationalUnits.java
@@ -3,21 +3,18 @@ package net.sourceforge.fenixedu.applicationTier.Servico.commons;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import net.sourceforge.fenixedu.domain.Department;
 import net.sourceforge.fenixedu.domain.organizationalStructure.PartyTypeEnum;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.organizationalStructure.UnitUtils;
 
 public class SearchOrganizationalUnits extends AbstractSearchObjects {
 
 	public List run(Class type, String value, int limit, Map<String, String> arguments) {
-		 Set<Department> departments = rootDomainObject.readAllDomainObjects(Department.class);
+		
 		 List<Unit> units = new ArrayList<Unit> ();
-		 for(Department department : departments) {
-			 units.add(department.getDepartmentUnit());
-			 units.addAll(department.getDepartmentUnit().getSubUnits(PartyTypeEnum.SCIENTIFIC_AREA));
-		 }
+		 units.addAll(UnitUtils.readAllActiveUnitsByType(PartyTypeEnum.DEPARTMENT));
+		 units.addAll(UnitUtils.readAllActiveUnitsByType(PartyTypeEnum.SCIENTIFIC_AREA));
 			 		 
 		 return super.process(units, value, limit, arguments);
 	}
