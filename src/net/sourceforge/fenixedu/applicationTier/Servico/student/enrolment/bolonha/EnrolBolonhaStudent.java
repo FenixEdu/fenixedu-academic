eb168f213d3c369581094c097479f19ddf461b66
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-02-27 14:09:57 +0000

    add CurricularRuleLevel as parameter

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/enrolment/bolonha/EnrolBolonhaStudent.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/enrolment/bolonha/EnrolBolonhaStudent.java
@@ -7,15 +7,18 @@ import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors.CurricularRuleLevel;
+import net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors.RuleResult;
 import net.sourceforge.fenixedu.domain.enrolment.DegreeModuleToEnrol;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumModule;
 
 public class EnrolBolonhaStudent extends Service {
 
-    public void run(final Person person, final StudentCurricularPlan studentCurricularPlan,
+    public List<RuleResult> run(final Person person, final StudentCurricularPlan studentCurricularPlan,
 	    final ExecutionPeriod executionPeriod, final List<DegreeModuleToEnrol> degreeModulesToEnrol,
-	    final List<CurriculumModule> curriculumModulesToRemove) {
-	studentCurricularPlan.enrol(person, executionPeriod, new HashSet<DegreeModuleToEnrol>(
-		degreeModulesToEnrol), curriculumModulesToRemove);
+	    final List<CurriculumModule> curriculumModulesToRemove,
+	    final CurricularRuleLevel curricularRuleLevel) {
+	return studentCurricularPlan.enrol(person, executionPeriod, new HashSet<DegreeModuleToEnrol>(
+		degreeModulesToEnrol), curriculumModulesToRemove, curricularRuleLevel);
     }
 }
