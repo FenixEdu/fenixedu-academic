91f8b02ce3005ea34d950e25fc9e46236f033640
Luís Egídio <luis.egidio@ist.utl.pt>
2007-02-27 09:59:52 +0000

    COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH comparator chain

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoCurricularCourseScope.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoCurricularCourseScope.java
@@ -7,18 +7,69 @@
 package net.sourceforge.fenixedu.dataTransferObject;
 
 import java.util.Calendar;
-import java.util.Locale;
+import java.util.Comparator;
 
 import net.sourceforge.fenixedu.domain.CurricularCourseScope;
 import net.sourceforge.fenixedu.domain.DomainReference;
 import net.sourceforge.fenixedu.domain.Language;
 import net.sourceforge.fenixedu.util.LanguageUtils;
 
+import org.apache.commons.collections.comparators.ComparatorChain;
+import org.apache.commons.lang.StringUtils;
+
+
 /**
  * @author tfc130
  */
 public class InfoCurricularCourseScope extends InfoObject {
 
+    public static final ComparatorChain COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH;
+    static {
+	COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH = new ComparatorChain();
+	COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH.addComparator(new Comparator() {
+
+            public int compare(Object o1, Object o2) {
+                final InfoCurricularCourseScope infoScope1 = (InfoCurricularCourseScope) o1;
+                final InfoCurricularCourseScope infoScope2 = (InfoCurricularCourseScope) o2;
+                
+                return infoScope1.getInfoCurricularSemester().getInfoCurricularYear().getYear()
+                        .compareTo(infoScope2.getInfoCurricularSemester().getInfoCurricularYear().getYear());
+            }
+        
+        });
+	COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH.addComparator(new Comparator() {
+
+            public int compare(Object o1, Object o2) {
+                final InfoCurricularCourseScope infoScope1 = (InfoCurricularCourseScope) o1;
+                final InfoCurricularCourseScope infoScope2 = (InfoCurricularCourseScope) o2;
+
+                return infoScope1.getInfoCurricularSemester().getSemester()
+                	.compareTo(infoScope2.getInfoCurricularSemester().getSemester());
+            }
+        
+        });
+	COMPARATOR_BY_YEAR_SEMESTER_AND_BRANCH.addComparator(new Comparator() {
+
+            public int compare(Object o1, Object o2) {
+                final InfoCurricularCourseScope infoScope1 = (InfoCurricularCourseScope) o1;
+                final InfoCurricularCourseScope infoScope2 = (InfoCurricularCourseScope) o2;
+                
+                final String acronym1 = infoScope1.getInfoBranch() == null ? null : infoScope1.getInfoBranch().getAcronym();
+                final String acronym2 = infoScope2.getInfoBranch() == null ? null : infoScope2.getInfoBranch().getAcronym();
+                if (StringUtils.isEmpty(acronym1) && StringUtils.isEmpty(acronym2)) {
+                    return 0;
+                } else if (!StringUtils.isEmpty(acronym1) && StringUtils.isEmpty(acronym2)) {
+                    return 1;
+                } else if (StringUtils.isEmpty(acronym1) && !StringUtils.isEmpty(acronym2)) {
+                    return -1;
+                }
+                
+                return acronym1.compareTo(acronym2);
+            }
+        });
+        
+    }
+    
     private final DomainReference<CurricularCourseScope> curricularCourseScopeDomainReference;
 
     private boolean showEnVersion = (LanguageUtils.getUserLanguage() == Language.en);
