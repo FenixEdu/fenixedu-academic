2561f13f458eef2a49d57e23dbf24ba9091af9ed
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-02-27 14:12:32 +0000

    add CurricularRuleLevel attribute to EnrolmentContext

--- src/net/sourceforge/fenixedu/domain/enrolment/EnrolmentContext.java
+++ src/net/sourceforge/fenixedu/domain/enrolment/EnrolmentContext.java
@@ -7,32 +7,34 @@ import java.util.Set;
 
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors.CurricularRuleLevel;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumModule;
 
 public class EnrolmentContext {
-    
+
     private StudentCurricularPlan studentCurricularPlan;
+
     private ExecutionPeriod executionPeriod;
-    
+
     private Set<IDegreeModuleToEvaluate> degreeModulesToEvaluate;
+
     private List<CurriculumModule> curriculumModulesToRemove;
-    
-    public EnrolmentContext(final StudentCurricularPlan studentCurricularPlan, final ExecutionPeriod executionPeriod, final List<CurriculumModule> curriculumModulesToRemove) {
+
+    private CurricularRuleLevel curricularRuleLevel;
+
+    public EnrolmentContext(final StudentCurricularPlan studentCurricularPlan,
+	    final ExecutionPeriod executionPeriod,
+	    final List<CurriculumModule> curriculumModulesToRemove,
+	    final CurricularRuleLevel curricularRuleLevel) {
 	this.studentCurricularPlan = studentCurricularPlan;
 	this.degreeModulesToEvaluate = new HashSet<IDegreeModuleToEvaluate>();
 	this.executionPeriod = executionPeriod;
 	this.curriculumModulesToRemove = curriculumModulesToRemove;
-    }
-    
-    public EnrolmentContext(final StudentCurricularPlan studentCurricularPlan, final ExecutionPeriod executionPeriod) {
-	this.studentCurricularPlan = studentCurricularPlan;
-	this.degreeModulesToEvaluate = new HashSet<IDegreeModuleToEvaluate>();
-	this.executionPeriod = executionPeriod;
-	this.curriculumModulesToRemove = Collections.EMPTY_LIST;
+	this.curricularRuleLevel = curricularRuleLevel;
     }
 
     public Set<IDegreeModuleToEvaluate> getDegreeModuleToEvaluate() {
-        return degreeModulesToEvaluate;
+	return degreeModulesToEvaluate;
     }
 
     public void addDegreeModuleToEvaluate(final IDegreeModuleToEvaluate degreeModuleToEvaluate) {
@@ -40,23 +42,33 @@ public class EnrolmentContext {
     }
 
     public ExecutionPeriod getExecutionPeriod() {
-        return executionPeriod;
+	return executionPeriod;
     }
 
     public void setExecutionPeriod(ExecutionPeriod executionPeriod) {
-        this.executionPeriod = executionPeriod;
+	this.executionPeriod = executionPeriod;
     }
 
     public StudentCurricularPlan getStudentCurricularPlan() {
-        return studentCurricularPlan;
+	return studentCurricularPlan;
     }
 
     public void setStudentCurricularPlan(StudentCurricularPlan studentCurricularPlan) {
-        this.studentCurricularPlan = studentCurricularPlan;
+	this.studentCurricularPlan = studentCurricularPlan;
     }
 
     public List<CurriculumModule> getCurriculumModulesToRemove() {
-        return curriculumModulesToRemove;
+	return curriculumModulesToRemove;
     }
+
+    public CurricularRuleLevel getCurricularRuleLevel() {
+        return curricularRuleLevel;
+    }
+
+    public void setCurricularRuleLevel(CurricularRuleLevel curricularRuleLevel) {
+        this.curricularRuleLevel = curricularRuleLevel;
+    }
+    
+    
 
 }
