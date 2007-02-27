91ea79d3fcc32938a167a2940a45b09aa5f74df0
Luís Egídio <luis.egidio@ist.utl.pt>
2007-02-27 15:26:45 +0000

    failsafe override of methods to avoid further confusions about the transtion from Student to Registration

--- src/net/sourceforge/fenixedu/domain/finalDegreeWork/GroupStudent.java
+++ src/net/sourceforge/fenixedu/domain/finalDegreeWork/GroupStudent.java
@@ -3,6 +3,7 @@ package net.sourceforge.fenixedu.domain.finalDegreeWork;
 import java.util.Comparator;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.student.Registration;
 
 import org.apache.commons.beanutils.BeanComparator;
 
@@ -18,6 +19,46 @@ public class GroupStudent extends GroupStudent_Base {
     public void delete() {
         removeRootDomainObject();
         deleteDomainObject();
+    }
+
+    @Override
+    @Deprecated
+    public Registration getStudent() {
+	return getRegistration();
+    }
+
+    @Override
+    @Deprecated    
+    public boolean hasStudent() {
+	return hasRegistration();
+    }
+
+    @Override
+    @Deprecated
+    public void removeStudent() {
+	removeRegistration();
+    }
+
+    @Override
+    @Deprecated
+    public void setStudent(Registration registration) {
+	setRegistration(registration);
+    }
+
+    public Registration getRegistration() {
+	return super.getStudent();
+    }
+
+    public boolean hasRegistration() {
+	return super.hasStudent();
+    }
+
+    public void removeRegistration() {
+	super.removeStudent();
+    }
+
+    public void setRegistration(Registration registration) {
+	super.setStudent(registration);
     }
 
 }
