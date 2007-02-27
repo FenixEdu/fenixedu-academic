059752ce3021988061d1989137601b953211e0f4
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-02-27 14:07:23 +0000

    replaced provider for optional curricular courses

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DegreeTypesProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DegreeTypesProvider.java
@@ -1,14 +1,16 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers;
 
+import java.util.Arrays;
+
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.renderers.DataProvider;
 import net.sourceforge.fenixedu.renderers.components.converters.Converter;
 import net.sourceforge.fenixedu.renderers.converters.EnumConverter;
 
-public class BolonhaDegreeTypesProvider implements DataProvider {
+public class DegreeTypesProvider implements DataProvider {
 
     public Object provide(Object source, Object currentValue) {
-	return DegreeType.getBolonhaDegreeTypes();
+	return Arrays.asList(DegreeType.values());
     }
 
     public Converter getConverter() {
