04255b135b592fd4c5919caec7d872ec80f2f598
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2007-03-01 17:59:00 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/assiduousness/Justification.java
+++ src/net/sourceforge/fenixedu/domain/assiduousness/Justification.java
@@ -9,39 +9,38 @@ import org.joda.time.YearMonthDay;
 public class Justification extends Justification_Base {
 
     public Justification() {
-	super();
-	setRootDomainObject(RootDomainObject.getInstance());
+        super();
+        setRootDomainObject(RootDomainObject.getInstance());
     }
 
     public Partial getPartialDate() {
-	Partial p = new Partial();
-	YearMonthDay y = getDate().toYearMonthDay();
-	if (y != null) {
-	    for (int i = 0; i < y.getFields().length; i++) {
-		p = p.with(y.getFieldType(i), y.getValue(i));
-	    }
-	}
-	TimeOfDay t = getDate().toTimeOfDay();
-	if (t != null && !t.isEqual(new TimeOfDay(0, 0, 0))) {
-	    for (int i = 0; i < t.getFields().length; i++) {
-		p = p.with(t.getFieldType(i), t.getValue(i));
-	    }
-	}
-	return p;
+        Partial p = new Partial();
+        YearMonthDay y = getDate().toYearMonthDay();
+        if (y != null) {
+            for (int i = 0; i < y.getFields().length; i++) {
+                p = p.with(y.getFieldType(i), y.getValue(i));
+            }
+        }
+        TimeOfDay t = getDate().toTimeOfDay();
+        if (t != null && !t.isEqual(new TimeOfDay(0, 0, 0))) {
+            for (int i = 0; i < t.getFields().length; i++) {
+                p = p.with(t.getFieldType(i), t.getValue(i));
+            }
+        }
+        return p;
     }
 
     public Partial getPartialEndDate() {
 	if (this.isLeave()) {
-	    return ((Leave) this).getPartialEndDate();
-	}
-	return getPartialEndDate();
+            return ((Leave) this).getPartialEndDate();
+        }
+        return null;
     }
-
-    public Integer getModifiedByNumber() {
-	if (getModifiedBy() != null) {
-	    return getModifiedBy().getEmployeeNumber();
-	}
-	return null;
+    
+    public Integer getModifiedByNumber(){
+        if(getModifiedBy() != null){
+            return getModifiedBy().getEmployeeNumber();
+        }
+        return null;
     }
-
 }
