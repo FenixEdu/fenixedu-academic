50b177dd7bed22388466806c34f9fcadfdbd1720
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-03-06 17:03:39 +0000

    bugs fixed.

--- src/net/sourceforge/fenixedu/presentationTier/TagLib/units/UnitsTree.java
+++ src/net/sourceforge/fenixedu/presentationTier/TagLib/units/UnitsTree.java
@@ -25,147 +25,147 @@ import org.joda.time.YearMonthDay;
  */
 public class UnitsTree extends TagSupport {
 
-	private String initialUnit;
+    private String initialUnit;
 
-	private String unitParamName;
+    private String unitParamName;
 
-	private String state;
+    private String state;
 
-	private String path;
+    private String path;
 
-	private String expanded;
+    private String expanded;
 
-	public int doStartTag() throws JspException {
-		String tree = writeInstitutionTree();
-		try {
-			pageContext.getOut().print(tree);
-		} catch (IOException e) {
-			e.printStackTrace(System.out);
-		}
-		return SKIP_BODY;
+    public int doStartTag() throws JspException {
+	String tree = writeInstitutionTree();
+	try {
+	    pageContext.getOut().print(tree);
+	} catch (IOException e) {
+	    e.printStackTrace(System.out);
+	}
+	return SKIP_BODY;
+    }
+
+    public String writeInstitutionTree() {
+
+	StringBuilder buffer = new StringBuilder();
+	Unit unit = (Unit) pageContext.findAttribute(this.getInitialUnit());
+	YearMonthDay currentDate = new YearMonthDay();
+
+	buffer.append("<ul class='padding1 nobullet'>");
+	getSubUnitsList(unit, null, buffer, currentDate, this.getUnitParamName(), this.getPath());
+	buffer.append("</ul>");
+
+	return buffer.toString();
+    }
+
+    private void getSubUnitsList(Unit parentUnit, Unit parentUnitParent, StringBuilder buffer,
+	    YearMonthDay currentDate, String paramName, String path) {
+
+	buffer.append("<li>");
+
+	HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+	List<Unit> subUnits = getUnitSubUnits(parentUnit, currentDate);
+	if (!subUnits.isEmpty()) {
+	    putImage(parentUnit, parentUnitParent, buffer, request);
 	}
 
-	public String writeInstitutionTree() {
+	buffer.append("<a href=\"").append(request.getContextPath()).append(path).append("&").append(
+		paramName).append("=").append(parentUnit.getIdInternal()).append("\">").append(
+		parentUnit.getNameWithAcronym()).append("</a>").append("</li>");
 
-		StringBuilder buffer = new StringBuilder();
-		Unit unit = (Unit) pageContext.findAttribute(this.getInitialUnit());
-		YearMonthDay currentDate = new YearMonthDay();
+	if (!subUnits.isEmpty()) {
+	    buffer.append("<ul class='mvert0 nobullet' id=\"").append("aa").append(
+		    parentUnit.getIdInternal()).append(
+		    (parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append("\" ")
+		    .append("style='display:" + (getExpanded() != null && Boolean.valueOf(getExpanded()) ? "block" : "none")).append("'>\r\n");
 
-		buffer.append("<ul class='padding1 nobullet'>");
-		getSubUnitsList(unit, null, buffer, currentDate, this.getUnitParamName(), this.getPath());
-		buffer.append("</ul>");
-
-		return buffer.toString();
+	    Collections.sort(subUnits, Unit.UNIT_COMPARATOR_BY_NAME);
 	}
 
-	private void getSubUnitsList(Unit parentUnit, Unit parentUnitParent, StringBuilder buffer,
-			YearMonthDay currentDate, String paramName, String path) {
-
-		buffer.append("<li>");
-
-		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
-		List<Unit> subUnits = getUnitSubUnits(parentUnit, currentDate);
-		if (!subUnits.isEmpty()) {
-			putImage(parentUnit, parentUnitParent, buffer, request);
-		}
-
-		buffer.append("<a href=\"").append(request.getContextPath()).append(path).append("&").append(
-				paramName).append("=").append(parentUnit.getIdInternal()).append("\">").append(
-				parentUnit.getNameWithAcronym()).append("</a>").append("</li>");
-
-		if (!subUnits.isEmpty()) {
-			buffer.append("<ul class='mvert0 nobullet' id=\"").append("aa")
-					.append(parentUnit.getIdInternal()).append(
-							(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append("\" ")
-					.append(
-							"style='display:"
-									+ (getExpanded() != null && Boolean.valueOf(getExpanded()) ? "block"
-											: "none")).append("'>\r\n");
-
-			Collections.sort(subUnits, Unit.UNIT_COMPARATOR_BY_NAME);
-		}
-
-		for (Unit subUnit : subUnits) {
-			getSubUnitsList(subUnit, parentUnit, buffer, currentDate, paramName, path);
-		}
-
-		if (!subUnits.isEmpty()) {
-			buffer.append("</ul>");
-		}
+	for (Unit subUnit : subUnits) {
+	    getSubUnitsList(subUnit, parentUnit, buffer, currentDate, paramName, path);
 	}
 
-	private List<Unit> getUnitSubUnits(Unit parentUnit, YearMonthDay currentDate) {
-		List<AccountabilityTypeEnum> accountabilityEnums = new ArrayList<AccountabilityTypeEnum>();
-		accountabilityEnums.add(AccountabilityTypeEnum.ORGANIZATIONAL_STRUCTURE);
-		accountabilityEnums.add(AccountabilityTypeEnum.ACADEMIC_STRUCTURE);
-		if (!StringUtils.isEmpty(this.getState()) && this.getState().equalsIgnoreCase("true")) {
-			return new ArrayList(parentUnit.getActiveSubUnits(currentDate, accountabilityEnums));
-		} else if (!StringUtils.isEmpty(this.getState()) && this.getState().equalsIgnoreCase("false")) {
-			return new ArrayList(parentUnit.getInactiveSubUnits(currentDate, accountabilityEnums));
-		} else {
-			return new ArrayList(parentUnit.getSubUnits(accountabilityEnums));
-		}
+	if (!subUnits.isEmpty()) {
+	    buffer.append("</ul>");
 	}
+    }
 
-	private void putImage(Unit parentUnit, Unit parentUnitParent, StringBuilder buffer,
-			HttpServletRequest request) {
-		buffer.append("<img ").append("src='").append(request.getContextPath()).append(
-				(getExpanded() != null && Boolean.valueOf(getExpanded()) ? "/images/toggle_plus10.gif"
-						: "/images/toggle_minus10.gif")).append("' id=\"").append(parentUnit.getIdInternal())
-				.append((parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append("\" ")
-				.append("indexed='true' onClick=\"").append("check(document.getElementById('").append("aa")
-				.append(parentUnit.getIdInternal()).append(
-						(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append(
-						"'),document.getElementById('").append(parentUnit.getIdInternal()).append(
-						(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append(
-						"'));return false;").append("\"> ");
+    private List<Unit> getUnitSubUnits(Unit parentUnit, YearMonthDay currentDate) {
+	
+	List<AccountabilityTypeEnum> accountabilityEnums = new ArrayList<AccountabilityTypeEnum>();
+	
+	accountabilityEnums.add(AccountabilityTypeEnum.ORGANIZATIONAL_STRUCTURE);
+	accountabilityEnums.add(AccountabilityTypeEnum.ACADEMIC_STRUCTURE);
+	accountabilityEnums.add(AccountabilityTypeEnum.GEOGRAPHIC);
+	
+	if (!StringUtils.isEmpty(this.getState()) && this.getState().equalsIgnoreCase("true")) {
+	    return new ArrayList(parentUnit.getActiveSubUnits(currentDate, accountabilityEnums));
+	} else if (!StringUtils.isEmpty(this.getState()) && this.getState().equalsIgnoreCase("false")) {
+	    return new ArrayList(parentUnit.getInactiveSubUnits(currentDate, accountabilityEnums));
+	} else {
+	    return new ArrayList(parentUnit.getSubUnits(accountabilityEnums));
 	}
+    }
 
-	public String getId() {
-		return id;
-	}
+    private void putImage(Unit parentUnit, Unit parentUnitParent, StringBuilder buffer, HttpServletRequest request) {
+	buffer.append("<img ").append("src='").append(request.getContextPath()).append(
+		(getExpanded() != null && Boolean.valueOf(getExpanded()) ? "/images/toggle_minus10.gif" : "/images/toggle_plus10.gif")).append("' id=\"").append(
+		parentUnit.getIdInternal()).append(
+		(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append("\" ")
+		.append("indexed='true' onClick=\"").append("check(document.getElementById('").append(
+			"aa").append(parentUnit.getIdInternal()).append(
+			(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append(
+			"'),document.getElementById('").append(parentUnit.getIdInternal()).append(
+			(parentUnitParent != null) ? parentUnitParent.getIdInternal() : "").append(
+			"'));return false;").append("\"> ");
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+	return id;
+    }
 
-	public String getInitialUnit() {
-		return initialUnit;
-	}
+    public void setId(String id) {
+	this.id = id;
+    }
 
-	public void setInitialUnit(String initialUnit) {
-		this.initialUnit = initialUnit;
-	}
+    public String getInitialUnit() {
+	return initialUnit;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public void setInitialUnit(String initialUnit) {
+	this.initialUnit = initialUnit;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public String getPath() {
+	return path;
+    }
 
-	public String getUnitParamName() {
-		return unitParamName;
-	}
+    public void setPath(String path) {
+	this.path = path;
+    }
 
-	public void setUnitParamName(String unitParamName) {
-		this.unitParamName = unitParamName;
-	}
+    public String getUnitParamName() {
+	return unitParamName;
+    }
 
-	public String getState() {
-		return state;
-	}
+    public void setUnitParamName(String unitParamName) {
+	this.unitParamName = unitParamName;
+    }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+    public String getState() {
+	return state;
+    }
 
-	public String getExpanded() {
-		return expanded;
-	}
+    public void setState(String state) {
+	this.state = state;
+    }
 
-	public void setExpanded(String expanded) {
-		this.expanded = expanded;
-	}
+    public String getExpanded() {
+	return expanded;
+    }
+
+    public void setExpanded(String expanded) {
+	this.expanded = expanded;
+    }
 }
