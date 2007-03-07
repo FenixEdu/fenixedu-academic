dd6de786a13ab6ed76ecef9ed1c3b7cc0fae8e0e
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-03-07 16:55:44 +0000

    Created Merge Units interface in Manager Module.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/organizationalStructureManagement/MergeUnits.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/organizationalStructureManagement/MergeUnits.java
@@ -1,13 +1,22 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.manager.organizationalStructureManagement;
 
+import java.util.Collections;
+
 import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.domain.Employee;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.util.Email;
 
 public class MergeUnits extends Service {
 
     public void run(Unit fromUnit, Unit destinationUnit) {
 	if(fromUnit != null && destinationUnit != null) {
+	    
 	    Unit.mergeExternalUnits(fromUnit, destinationUnit);
+	    
+	    Employee employee = Employee.readByNumber(4578);	    
+	    String body = "Foi efectuado um merge de unidades externas: Unidade Origem -> " + fromUnit.getName() + "[" + fromUnit.getIdInternal() + "]  Unidade Destino -> " + destinationUnit.getName() + "[" + destinationUnit.getIdInternal() + "]";
+	    new Email("Fénix", "suporte@ist.utl.pt", null, Collections.singletonList(employee.getPerson().getEmail()), null, null, "MergeUnits", body);
 	}
     }
 }
