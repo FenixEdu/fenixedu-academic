8600f55104e413e14ee5d94f20fc0d4355711639
Luís Egídio <luis.egidio@ist.utl.pt>
2007-03-08 11:44:32 +0000

    initial version of ImprovementOfApprovedEnrolment executor

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/ImprovementOfApprovedEnrolmentExecutor.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/ImprovementOfApprovedEnrolmentExecutor.java
@@ -1,6 +1,10 @@
 package net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors;
 
+import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.curricularRules.ICurricularRule;
+import net.sourceforge.fenixedu.domain.curricularRules.ImprovementOfApprovedEnrolment;
+import net.sourceforge.fenixedu.domain.degreeStructure.DegreeModule;
 import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
 import net.sourceforge.fenixedu.domain.enrolment.IDegreeModuleToEvaluate;
 
@@ -18,7 +22,28 @@ public class ImprovementOfApprovedEnrolmentExecutor extends CurricularRuleExecut
 
     @Override
     protected RuleResult executeEnrolmentInEnrolmentEvaluation(final ICurricularRule curricularRule, final IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, final EnrolmentContext enrolmentContext) {
-	return RuleResult.createFalse();
+	final ImprovementOfApprovedEnrolment improvementOfApprovedEnrolment = (ImprovementOfApprovedEnrolment) curricularRule;
+	final Enrolment enrolment = improvementOfApprovedEnrolment.getEnrolment();
+	final DegreeModule degreeModule = enrolment.getDegreeModule();
+	
+	if (enrolment.hasImprovement()) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.ImprovementOfApprovedEnrolmentExecutor.already.enroled.in.improvement", degreeModule.getName());
+	}
+	
+	if (!enrolment.isApproved()) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.ImprovementOfApprovedEnrolmentExecutor.degree.module.hasnt.been.approved", degreeModule.getName());
+	}
+
+	final ExecutionPeriod executionPeriod = enrolmentContext.getExecutionPeriod();
+	if (!degreeModule.hasAnyParentContexts(executionPeriod)) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.ImprovementOfApprovedEnrolmentExecutor.degree.module.has.no.context.in.present.execution.period", degreeModule.getName(), executionPeriod.getQualifiedName());
+	}
+	
+	if (!enrolment.isImprovingInExecutionPeriodFollowingApproval(executionPeriod)) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.ImprovementOfApprovedEnrolmentExecutor.is.not.improving.in.execution.period.following.approval", degreeModule.getName());
+	}
+	
+	return RuleResult.createTrue();
     }
 
 }
