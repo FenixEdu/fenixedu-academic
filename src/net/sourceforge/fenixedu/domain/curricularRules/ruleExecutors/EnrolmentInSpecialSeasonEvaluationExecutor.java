add494cd7ed2cb3540e1e2c7decbdff53707aa31
Luís Egídio <luis.egidio@ist.utl.pt>
2007-03-09 09:45:58 +0000

    check maximum special evaluation enrolments

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/EnrolmentInSpecialSeasonEvaluationExecutor.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/EnrolmentInSpecialSeasonEvaluationExecutor.java
@@ -1,11 +1,14 @@
 package net.sourceforge.fenixedu.domain.curricularRules.ruleExecutors;
 
 import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.SpecialSeasonCode;
 import net.sourceforge.fenixedu.domain.curricularRules.EnrolmentInSpecialSeasonEvaluation;
 import net.sourceforge.fenixedu.domain.curricularRules.ICurricularRule;
 import net.sourceforge.fenixedu.domain.degreeStructure.DegreeModule;
 import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
 import net.sourceforge.fenixedu.domain.enrolment.IDegreeModuleToEvaluate;
+import net.sourceforge.fenixedu.domain.student.Registration;
 
 public class EnrolmentInSpecialSeasonEvaluationExecutor extends CurricularRuleExecutor {
 
@@ -32,7 +35,20 @@ public class EnrolmentInSpecialSeasonEvaluationExecutor extends CurricularRuleEx
 	if (enrolment.isApproved()) {
 	    return RuleResult.createFalse("curricularRules.rulesExecutor.EnrolmentInSpecialSeasonEvaluationExecutor.degree.module.has.been.approved", degreeModule.getName());
 	}
+	
+	final Registration registration = enrolment.getRegistration();
+	final ExecutionYear executionYear = enrolment.getExecutionPeriod().getExecutionYear();
+	final SpecialSeasonCode specialSeasonCode = registration.getSpecialSeasonCodeByExecutionYear(executionYear);
+	if (specialSeasonCode == null) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.EnrolmentInSpecialSeasonEvaluationExecutor.no.specialSeason.code");
+	}
 
+	final Integer maxEnrolments = specialSeasonCode.getMaxEnrolments();
+	if (maxEnrolments < 
+		registration.getActiveStudentCurricularPlan().getSpecialSeasonEnrolments(executionYear).size()) {
+	    return RuleResult.createFalse("curricularRules.rulesExecutor.EnrolmentInSpecialSeasonEvaluationExecutor.too.many.specialSeason.enrolments", maxEnrolments.toString());
+	}
+	
 	return RuleResult.createTrue();
     }
 
