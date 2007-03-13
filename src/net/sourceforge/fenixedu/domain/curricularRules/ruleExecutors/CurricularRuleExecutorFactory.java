556a0b9111f272529ba4027d1f3f62a4e4397c72
Luís Egídio <luis.egidio@ist.utl.pt>
2007-03-13 10:26:06 +0000

    executor declaration

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/CurricularRuleExecutorFactory.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/CurricularRuleExecutorFactory.java
@@ -13,6 +13,7 @@ import net.sourceforge.fenixedu.domain.curricularRules.Exclusiveness;
 import net.sourceforge.fenixedu.domain.curricularRules.ICurricularRule;
 import net.sourceforge.fenixedu.domain.curricularRules.ImprovementOfApprovedEnrolment;
 import net.sourceforge.fenixedu.domain.curricularRules.MaximumNumberOfCreditsForEnrolmentPeriod;
+import net.sourceforge.fenixedu.domain.curricularRules.MaximumNumberOfEnrolmentsInSpecialSeasonEvaluation;
 import net.sourceforge.fenixedu.domain.curricularRules.MinimumNumberOfCreditsToEnrol;
 import net.sourceforge.fenixedu.domain.curricularRules.PreviousYearsEnrolmentCurricularRule;
 import net.sourceforge.fenixedu.domain.curricularRules.RestrictionBetweenDegreeModules;
@@ -25,20 +26,21 @@ public class CurricularRuleExecutorFactory {
     private static Map<Class<? extends ICurricularRule>, CurricularRuleExecutor> executors = new HashMap<Class<? extends ICurricularRule>, CurricularRuleExecutor>();
 
     static {
-	executors.put(RestrictionDoneDegreeModule.class, 			new RestrictionDoneDegreeModuleExecutor());
-	executors.put(RestrictionEnroledDegreeModule.class,			new RestrictionEnroledDegreeModuleExecutor());	
-	executors.put(RestrictionBetweenDegreeModules.class,			new RestrictionBetweenDegreeModulesExecutor());
-	executors.put(EnrolmentToBeApprovedByCoordinator.class, 		new EnrolmentToBeApprovedByCoordinatorExecutor());
-	executors.put(Exclusiveness.class, 					new ExclusivenessExecutor());
-	executors.put(MinimumNumberOfCreditsToEnrol.class,			new MinimumNumberOfCreditsToEnrolExecutor());
-	executors.put(DegreeModulesSelectionLimit.class,			new DegreeModulesSelectionLimitExecutor());
-	executors.put(CreditsLimit.class,					new CreditsLimitExecutor());
-	executors.put(AnyCurricularCourse.class,				new AnyCurricularCourseExecutor());
-	executors.put(MaximumNumberOfCreditsForEnrolmentPeriod.class,		new MaximumNumberOfCreditsForEnrolmentPeriodExecutor());
-	executors.put(PreviousYearsEnrolmentCurricularRule.class,		new PreviousYearsEnrolmentExecutor());
-	executors.put(AssertUniqueApprovalInCurricularCourseContexts.class,	new AssertUniqueApprovalInCurricularCourseContextsExecutor());
-	executors.put(ImprovementOfApprovedEnrolment.class,			new ImprovementOfApprovedEnrolmentExecutor());
-	executors.put(EnrolmentInSpecialSeasonEvaluation.class,			new EnrolmentInSpecialSeasonEvaluationExecutor());
+	executors.put(RestrictionDoneDegreeModule.class, 				new RestrictionDoneDegreeModuleExecutor());
+	executors.put(RestrictionEnroledDegreeModule.class,				new RestrictionEnroledDegreeModuleExecutor());	
+	executors.put(RestrictionBetweenDegreeModules.class,				new RestrictionBetweenDegreeModulesExecutor());
+	executors.put(EnrolmentToBeApprovedByCoordinator.class, 			new EnrolmentToBeApprovedByCoordinatorExecutor());
+	executors.put(Exclusiveness.class, 						new ExclusivenessExecutor());
+	executors.put(MinimumNumberOfCreditsToEnrol.class,				new MinimumNumberOfCreditsToEnrolExecutor());
+	executors.put(DegreeModulesSelectionLimit.class,				new DegreeModulesSelectionLimitExecutor());
+	executors.put(CreditsLimit.class,						new CreditsLimitExecutor());
+	executors.put(AnyCurricularCourse.class,					new AnyCurricularCourseExecutor());
+	executors.put(MaximumNumberOfCreditsForEnrolmentPeriod.class,			new MaximumNumberOfCreditsForEnrolmentPeriodExecutor());
+	executors.put(PreviousYearsEnrolmentCurricularRule.class,			new PreviousYearsEnrolmentExecutor());
+	executors.put(AssertUniqueApprovalInCurricularCourseContexts.class,		new AssertUniqueApprovalInCurricularCourseContextsExecutor());
+	executors.put(ImprovementOfApprovedEnrolment.class,				new ImprovementOfApprovedEnrolmentExecutor());
+	executors.put(EnrolmentInSpecialSeasonEvaluation.class,				new EnrolmentInSpecialSeasonEvaluationExecutor());
+	executors.put(MaximumNumberOfEnrolmentsInSpecialSeasonEvaluation.class,		new MaximumNumberOfEnrolmentsInSpecialSeasonEvaluationExecutor());
     }
 
     public static CurricularRuleExecutor findExecutor(final ICurricularRule curricularRule) {
