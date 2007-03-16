20f14911bc687a4b20425934a34736d77a859483
João Florindo <jdnf@linkare.pt>
2007-03-16 13:20:25 +0000

    do not create an external enrolment for student that already contains one for the same ExternalCurricularCourse

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/ExternalEnrolment.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/ExternalEnrolment.java
@@ -11,11 +11,12 @@ import net.sourceforge.fenixedu.domain.student.Student;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.util.MultiLanguageString;
 
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
 
 public class ExternalEnrolment extends ExternalEnrolment_Base implements IEnrolment {
     
-    public final static Comparator<ExternalEnrolment> COMPARATOR_BY_NAME = new Comparator<ExternalEnrolment>() {
+    static public final Comparator<ExternalEnrolment> COMPARATOR_BY_NAME = new Comparator<ExternalEnrolment>() {
 	public int compare(ExternalEnrolment o1, ExternalEnrolment o2) {
 	    int result = o1.getName().compareTo(o2.getName());
 	    return (result != 0) ? result : o1.getIdInternal().compareTo(o2.getIdInternal());
@@ -31,18 +32,30 @@ public class ExternalEnrolment extends ExternalEnrolment_Base implements IEnrolm
 	}
     }
     
-    public  ExternalEnrolment(Student student, ExternalCurricularCourse externalCurricularCourse, String grade, ExecutionPeriod executionPeriod) {
+    public ExternalEnrolment(Student student, ExternalCurricularCourse externalCurricularCourse, String grade, ExecutionPeriod executionPeriod) {
         this();
-        if(externalCurricularCourse == null) {
-            throw new DomainException("error.externalEnrolment.externalCurricularCourse.cannot.be.null");
-        }
         if(student == null) {
             throw new DomainException("error.externalEnrolment.student.cannot.be.null");
         }
+        if(externalCurricularCourse == null) {
+            throw new DomainException("error.externalEnrolment.externalCurricularCourse.cannot.be.null");
+        }
+        if (StringUtils.isEmpty(grade)) {
+            throw new DomainException("error.externalEnrolment.invalid.grade");
+        }
+        checkIfCanCreateExternalEnrolment(student, externalCurricularCourse);
         setStudent(student);
         setExternalCurricularCourse(externalCurricularCourse);
         setGrade(grade);
         setExecutionPeriod(executionPeriod);
+    }
+
+    private void checkIfCanCreateExternalEnrolment(final Student student, final ExternalCurricularCourse externalCurricularCourse) {
+	for (final ExternalEnrolment externalEnrolment : student.getExternalEnrolmentsSet()) {
+	    if (externalEnrolment.getExternalCurricularCourse() == externalCurricularCourse) {
+		throw new DomainException("error.studentCurriculum.ExternalEnrolment.already.exists.externalEnrolment.for.externalCurricularCourse", externalCurricularCourse.getName());
+	    }
+	}
     }
 
     public MultiLanguageString getName() {
@@ -58,8 +71,8 @@ public class ExternalEnrolment extends ExternalEnrolment_Base implements IEnrolm
     public void delete() {
 	removeExecutionPeriod();
 	removeExternalCurricularCourse();
-	removeRootDomainObject();
 	removeStudent();
+	removeRootDomainObject();
 	super.deleteDomainObject();
     }
 
