963c1e2d544589c58f6206cdb885138fc69969af
João Florindo <jdnf@linkare.pt>
2007-03-20 12:19:28 +0000

    corrected identation

--- src/net/sourceforge/fenixedu/applicationTier/strategy/groupEnrolment/strategys/IGroupEnrolmentStrategy.java
+++ src/net/sourceforge/fenixedu/applicationTier/strategy/groupEnrolment/strategys/IGroupEnrolmentStrategy.java
@@ -18,18 +18,18 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
  *
  */
 public interface IGroupEnrolmentStrategy {
-	
-	public boolean checkNumberOfGroups(Grouping grouping,Shift shift);
-	public boolean checkEnrolmentDate(Grouping grouping,Calendar actualDate);
-	public boolean checkShiftType(Grouping grouping,Shift shift);
-	public boolean checkNumberOfGroupElements(Grouping grouping,StudentGroup studentGroup) throws ExcepcaoPersistencia;
+    	
+    public boolean checkNumberOfGroups(Grouping grouping,Shift shift);
+    public boolean checkEnrolmentDate(Grouping grouping,Calendar actualDate);
+    public boolean checkShiftType(Grouping grouping,Shift shift);
+    public boolean checkNumberOfGroupElements(Grouping grouping,StudentGroup studentGroup) throws ExcepcaoPersistencia;
     public boolean checkIfStudentGroupIsEmpty(Attends attend, StudentGroup studentGroup);
-	public List checkShiftsType(Grouping groupProperties,List shifts);
-	public boolean checkPossibleToEnrolInExistingGroup(Grouping grouping,StudentGroup studentGroup)throws ExcepcaoPersistencia;
-	public Integer enrolmentPolicyNewGroup(Grouping grouping,int numberOfStudentsToEnrole,Shift shift);
-	public boolean checkAlreadyEnroled(Grouping grouping, String username);
-	public boolean checkNotEnroledInGroup(Grouping grouping,StudentGroup studentGroup, String username) throws ExcepcaoPersistencia;
-	public boolean checkStudentInGrouping (Grouping grouping, String username)throws ExcepcaoPersistencia;
-	public boolean checkHasShift(Grouping grouping);
+    public List checkShiftsType(Grouping groupProperties,List shifts);
+    public boolean checkPossibleToEnrolInExistingGroup(Grouping grouping,StudentGroup studentGroup)throws ExcepcaoPersistencia;
+    public Integer enrolmentPolicyNewGroup(Grouping grouping,int numberOfStudentsToEnrole,Shift shift);
+    public boolean checkAlreadyEnroled(Grouping grouping, String username);
+    public boolean checkNotEnroledInGroup(Grouping grouping,StudentGroup studentGroup, String username) throws ExcepcaoPersistencia;
+    public boolean checkStudentInGrouping (Grouping grouping, String username)throws ExcepcaoPersistencia;
+    public boolean checkHasShift(Grouping grouping);
     public boolean checkStudentsUserNamesInGrouping(List<String> studentUsernames, Grouping grouping);
 }
