333ee01ea5bead41996a31dcaada11ee757c92d1
João Florindo <jdnf@linkare.pt>
2007-03-20 12:20:29 +0000

    removed infos and allow student to select registration

--- src/net/sourceforge/fenixedu/presentationTier/Action/student/ViewEnroledExecutionCoursesAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/student/ViewEnroledExecutionCoursesAction.java
@@ -5,30 +5,48 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.presentationTier.Action.base.FenixContextAction;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
+import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.student.Student;
+import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
 
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-public class ViewEnroledExecutionCoursesAction extends FenixContextAction {
+public class ViewEnroledExecutionCoursesAction extends FenixDispatchAction {
 
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws FenixServiceException, FenixFilterException {
+    public ActionForward prepare(ActionMapping mapping, ActionForm actionForm,
+	    HttpServletRequest request, HttpServletResponse response) throws FenixFilterException, FenixServiceException {
 
-        final IUserView userView = getUserView(request);
-
-        final Object[] args = { userView.getUtilizador() };
-        final List allInfoExecutionCourses = (List) ServiceUtils.executeService(userView,
-                "ReadEnroledExecutionCourses", args);
-        request.setAttribute("infoExecutionCourses", allInfoExecutionCourses);
-
-        return mapping.findForward("sucess");
-
+	final Student student = getLoggedPerson(request).getStudent();
+	final List<Registration> registrations = student.getActiveRegistrations();
+	
+	if (registrations.size() == 1) {
+	    request.setAttribute("executionCourses", executeService("ReadEnroledExecutionCourses", new Object[] {registrations.get(0)}));
+	    return mapping.findForward("showEnroledExecutionCourses");
+	    
+	} else {
+	    request.setAttribute("registrations", registrations);
+	    return mapping.findForward("showActiveRegistrations");
+	}
     }
+    
+    public ActionForward select(ActionMapping mapping, ActionForm actionForm,
+	    HttpServletRequest request, HttpServletResponse response) throws FenixFilterException, FenixServiceException {
 
+	final Registration registration = getRegistrationByID(getLoggedPerson(request).getStudent(), getIntegerFromRequest(request, "registrationId"));
+	request.setAttribute("executionCourses", executeService("ReadEnroledExecutionCourses", new Object[] {registration}));
+	return mapping.findForward("showEnroledExecutionCourses");
+    }
+    
+    private Registration getRegistrationByID(final Student student, final Integer registrationId) {
+	for (final Registration registration : student.getActiveRegistrations()) {
+	    if (registration.getIdInternal().equals(registrationId)) {
+		return registration;
+	    }
+	}
+	return null;
+    }
 }
