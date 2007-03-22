83e08ccc5cea1193b2408c8165f9d063b04f9b71
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-03-22 12:17:41 +0000

    added new constructor

--- src/net/sourceforge/fenixedu/domain/protocols/Protocol.java
+++ src/net/sourceforge/fenixedu/domain/protocols/Protocol.java
@@ -2,14 +2,18 @@ package net.sourceforge.fenixedu.domain.protocols;
 
 import java.util.List;
 
+import org.joda.time.YearMonthDay;
+
 import net.sourceforge.fenixedu.dataTransferObject.person.ChoosePersonBean;
 import net.sourceforge.fenixedu.dataTransferObject.protocol.ProtocolFactory;
 import net.sourceforge.fenixedu.dataTransferObject.protocol.UnitSearchBean;
 import net.sourceforge.fenixedu.dataTransferObject.research.result.OpenFileBean;
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.Role;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.accessControl.Group;
 import net.sourceforge.fenixedu.domain.accessControl.RoleGroup;
+import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.protocols.util.ProtocolAction;
 import pt.utl.ist.fenix.tools.file.FileDescriptor;
@@ -18,101 +22,127 @@ import pt.utl.ist.fenix.tools.file.VirtualPath;
 import pt.utl.ist.fenix.tools.file.VirtualPathNode;
 
 public class Protocol extends Protocol_Base {
-    
-    public  Protocol() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
+
+    public Protocol() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
 
     public Protocol(ProtocolFactory protocolFactory) {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
-        setProtocolNumber(protocolFactory.getProtocolNumber());
-        setSignedDate(protocolFactory.getSignedDate());
-        setDates(protocolFactory);
-        setProtocolAction(new ProtocolAction(protocolFactory.getActionTypes(),protocolFactory.getOtherActionTypes()));
-        setObservations(protocolFactory.getObservations());
-        setResponsables(protocolFactory.getResponsibles());
-        setPartnerResponsibles(protocolFactory.getPartnerResponsibles());
-        setUnits(protocolFactory.getUnits());
-        setPartnerUnits(protocolFactory.getPartnerUnits());
-        writeFiles(protocolFactory.getFiles());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+	setProtocolNumber(protocolFactory.getProtocolNumber());
+	setSignedDate(protocolFactory.getSignedDate());
+	setDates(protocolFactory);
+	setProtocolAction(new ProtocolAction(protocolFactory.getActionTypes(), protocolFactory
+		.getOtherActionTypes()));
+	setObservations(protocolFactory.getObservations());
+	setResponsables(protocolFactory.getResponsibles());
+	setPartnerResponsibles(protocolFactory.getPartnerResponsibles());
+	setUnits(protocolFactory.getUnits());
+	setPartnerUnits(protocolFactory.getPartnerUnits());
+	writeFiles(protocolFactory.getFiles());
     }
-    
+
+    public Protocol(String protocolNumber, YearMonthDay signedDate, Boolean renewable, Boolean active,
+	    String scientificAreas, String observations, ProtocolHistory protocolHistory,
+	    ProtocolAction protocolAction, Person responsible, List<Unit> unitList,
+	    List<Unit> partnersList) {
+	setRootDomainObject(RootDomainObject.getInstance());
+	setProtocolNumber(protocolNumber);
+	setRenewable(renewable);
+	setActive(active);
+	setSignedDate(signedDate);
+	addProtocolHistories(protocolHistory);
+	setProtocolAction(protocolAction);
+	setObservations(observations);
+	if (responsible != null) {
+	    getResponsibles().add(responsible);
+	}
+	if (unitList != null && unitList.size() != 0) {
+	    getUnits().addAll(unitList);
+	}
+	if (partnersList != null && partnersList.size() != 0) {
+	    getPartners().addAll(partnersList);
+	}
+    }
+
     private void setDates(ProtocolFactory protocolFactory) {
-        ProtocolHistory protocolHistory = new ProtocolHistory(protocolFactory.getBeginDate(),protocolFactory.getEndDate());
-        getProtocolHistories().add(protocolHistory);
+	ProtocolHistory protocolHistory = new ProtocolHistory(protocolFactory.getBeginDate(),
+		protocolFactory.getEndDate());
+	getProtocolHistories().add(protocolHistory);
     }
 
-    private void writeFiles(List<OpenFileBean> files){
-        final VirtualPath filePath = getFilePath();
-        for (OpenFileBean openFileBean : files) {
-            writeFile(filePath,openFileBean);
-        }
+    private void writeFiles(List<OpenFileBean> files) {
+	final VirtualPath filePath = getFilePath();
+	for (OpenFileBean openFileBean : files) {
+	    writeFile(filePath, openFileBean);
+	}
     }
-    
-    private void writeFile(VirtualPath filePath, OpenFileBean openFileBean){        
-        final Group group = getGroup();
-        final FileDescriptor fileDescriptor = FileManagerFactory.getFactoryInstance().getFileManager().saveFile(
-                filePath, openFileBean.getFileName(), false, null, openFileBean.getFileName(), openFileBean.getInputStream());
 
-        final ProtocolFile protocolFile = new ProtocolFile(openFileBean.getFileName(), openFileBean.getFileName(), fileDescriptor
-                .getMimeType(), fileDescriptor.getChecksum(), fileDescriptor
-                .getChecksumAlgorithm(), fileDescriptor.getSize(), fileDescriptor.getUniqueId(),
-                group);
-        getProtocolFiles().add(protocolFile);
+    private void writeFile(VirtualPath filePath, OpenFileBean openFileBean) {
+	final Group group = getGroup();
+	final FileDescriptor fileDescriptor = FileManagerFactory.getFactoryInstance().getFileManager()
+		.saveFile(filePath, openFileBean.getFileName(), false, null, openFileBean.getFileName(),
+			openFileBean.getInputStream());
+
+	final ProtocolFile protocolFile = new ProtocolFile(openFileBean.getFileName(), openFileBean
+		.getFileName(), fileDescriptor.getMimeType(), fileDescriptor.getChecksum(),
+		fileDescriptor.getChecksumAlgorithm(), fileDescriptor.getSize(), fileDescriptor
+			.getUniqueId(), group);
+	getProtocolFiles().add(protocolFile);
     }
-    
+
     protected VirtualPath getFilePath() {
-        final VirtualPath filePath = new VirtualPath();
-        filePath.addNode(new VirtualPathNode("ProtocolFiles", "Protocol Files"));
-        filePath.addNode(new VirtualPathNode("Protocol" + getIdInternal(), getProtocolNumber()));
-        return filePath;
+	final VirtualPath filePath = new VirtualPath();
+	filePath.addNode(new VirtualPathNode("ProtocolFiles", "Protocol Files"));
+	filePath.addNode(new VirtualPathNode("Protocol" + getIdInternal(), getProtocolNumber()));
+	return filePath;
     }
-    
+
     private Group getGroup() {
-        //final PersonGroup personGroup = new PersonGroup((Person) party);
-        return new RoleGroup(Role.getRoleByRoleType(RoleType.SCIENTIFIC_COUNCIL));
-        //return new GroupUnion(personGroup, roleGroup);
+	// final PersonGroup personGroup = new PersonGroup((Person) party);
+	return new RoleGroup(Role.getRoleByRoleType(RoleType.SCIENTIFIC_COUNCIL));
+	// return new GroupUnion(personGroup, roleGroup);
     }
-    
+
     private void setResponsables(List<ChoosePersonBean> responsibles) {
-        if(responsibles != null){
-            for (ChoosePersonBean personBean : responsibles) {
-                if(personBean.getPerson() != null){
-                    getResponsibles().add(personBean.getPerson());
-                }
-            }
-        }        
+	if (responsibles != null) {
+	    for (ChoosePersonBean personBean : responsibles) {
+		if (personBean.getPerson() != null) {
+		    getResponsibles().add(personBean.getPerson());
+		}
+	    }
+	}
     }
-    
+
     private void setPartnerResponsibles(List<ChoosePersonBean> partnerResponsibles) {
-        if(partnerResponsibles != null){
-            for (ChoosePersonBean personBean : partnerResponsibles) {
-                if(personBean.getPerson() != null){
-                    getPartnerResponsibles().add(personBean.getPerson());
-                }
-            }
-        }        
+	if (partnerResponsibles != null) {
+	    for (ChoosePersonBean personBean : partnerResponsibles) {
+		if (personBean.getPerson() != null) {
+		    getPartnerResponsibles().add(personBean.getPerson());
+		}
+	    }
+	}
     }
-    
+
     private void setUnits(List<UnitSearchBean> units) {
-        if(units != null){
-            for (UnitSearchBean unitBean : units) {
-                if(unitBean.getUnit() != null){
-                    getUnits().add(unitBean.getUnit());
-                }
-            }
-        }        
+	if (units != null) {
+	    for (UnitSearchBean unitBean : units) {
+		if (unitBean.getUnit() != null) {
+		    getUnits().add(unitBean.getUnit());
+		}
+	    }
+	}
     }
-    
+
     private void setPartnerUnits(List<UnitSearchBean> partnerUnits) {
-        if(partnerUnits != null){
-            for (UnitSearchBean unitBean : partnerUnits) {
-                if(unitBean.getUnit() != null){
-                    getPartners().add(unitBean.getUnit());
-                }
-            }
-        }        
+	if (partnerUnits != null) {
+	    for (UnitSearchBean unitBean : partnerUnits) {
+		if (unitBean.getUnit() != null) {
+		    getPartners().add(unitBean.getUnit());
+		}
+	    }
+	}
     }
 }
