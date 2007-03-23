95e28aa69ca23f6c9a0d9e6103b308d9048f382e
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2007-03-23 09:12:00 +0000

    Fixing location type of two scientific journals (upon user request, since there edition interface is not yet available)

---
+++ etc/database_operations/fixScientificJournalsByRequest.sql
@@ -0,0 +1,3 @@
+update RESEARCH_ACTIVITY set LOCATION_TYPE='INTERNATIONAL' WHERE name like 'Astrophysical Journal';
+update RESEARCH_ACTIVITY set LOCATION_TYPE='INTERNATIONAL' where name like 'General Relativity and Gravitation';
+
