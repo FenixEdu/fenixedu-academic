abd94ceb2e8a559ab2142c614b4f69a45e5641d0
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2007-03-26 12:31:57 +0000

    bug fix in delete

--- src/net/sourceforge/fenixedu/domain/research/activity/Event.java
+++ src/net/sourceforge/fenixedu/domain/research/activity/Event.java
@@ -10,62 +10,65 @@ import net.sourceforge.fenixedu.domain.research.result.publication.ConferenceArt
 
 public class Event extends Event_Base {
 
-	public Event() {
-		super();
-        setOjbConcreteClass(getClass().getName());
-	}
-        
+    public Event() {
+	super();
+	setOjbConcreteClass(getClass().getName());
+    }
 
-	public Event(String name, EventType type, ResearchActivityLocationType scope) {
-		super();
-        setOjbConcreteClass(getClass().getName());
-        
-        if(name == null)
-            throw new DomainException("errors.event.requiredAttributes");
+    public Event(String name, EventType type, ResearchActivityLocationType scope) {
+	super();
+	setOjbConcreteClass(getClass().getName());
 
-		setName(name);
-		setEventType(type);
-		setLocationType(scope);
-	}
-	
-	@Override
-	public void delete() {
-		for (;this.hasAnyParticipations(); getParticipations().get(0).delete());
-    	for (; this.hasAnyParty(); this.getParties().get(0).removeResearchActivities(this));
-    	for (; this.hasAnyEventEditions(); this.getEventEditions().get(0).removeEvent());
-    	super.deleteDomainObject();
-	}
-    
-	/**
+	if (name == null)
+	    throw new DomainException("errors.event.requiredAttributes");
+
+	setName(name);
+	setEventType(type);
+	setLocationType(scope);
+    }
+
+    @Override
+    public void delete() {
+	for (; this.hasAnyParticipations(); getParticipations().get(0).delete())
+	    ;
+	for (; this.hasAnyParty(); this.getParties().get(0).removeResearchActivities(this))
+	    ;
+	for (; this.hasAnyEventEditions(); this.getEventEditions().get(0).removeEvent())
+	    ;
+	super.delete();
+    }
+
+    /**
      * This method is responsible for checking if the object still has active connections
      *if not, the object is deleted.
      */
-    public void sweep(){
-        if (!(this.hasAnyParticipations() || this.hasAnyEventEditions())){
-            delete();
-        }
+    public void sweep() {
+	if (!(this.hasAnyParticipations() || this.hasAnyEventEditions())) {
+	    delete();
+	}
     }
-    
+
     @Override
-    public List<ResearchActivityParticipationRole> getAllowedRoles(){
-    	return ResearchActivityParticipationRole.getAllEventParticipationRoles();
+    public List<ResearchActivityParticipationRole> getAllowedRoles() {
+	return ResearchActivityParticipationRole.getAllEventParticipationRoles();
     }
-    
+
     public static List<Event> readAll() {
 	List<Event> result = new ArrayList<Event>();
-	for (ResearchActivity researchActivity : RootDomainObject.getInstance().getResearchActivitiesSet()) {
-	    if(researchActivity instanceof Event) {
+	for (ResearchActivity researchActivity : RootDomainObject.getInstance()
+		.getResearchActivitiesSet()) {
+	    if (researchActivity instanceof Event) {
 		result.add((Event) researchActivity);
 	    }
 	}
 	return result;
     }
-    
+
     public List<ConferenceArticles> getArticles() {
-    	List<ConferenceArticles> articles = new ArrayList<ConferenceArticles>();
-    	for(EventEdition edition : getEventEditions()) {
-    	    articles.addAll(edition.getArticles());
-    	}
-    	return articles;
+	List<ConferenceArticles> articles = new ArrayList<ConferenceArticles>();
+	for (EventEdition edition : getEventEditions()) {
+	    articles.addAll(edition.getArticles());
+	}
+	return articles;
     }
 }
