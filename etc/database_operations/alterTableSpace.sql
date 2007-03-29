0960872a0a51b00358877111692113b9385f5da1
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-03-29 13:06:05 +0000

    bugs fixed.

--- etc/database_operations/alterTableSpace.sql
+++ etc/database_operations/alterTableSpace.sql
@@ -1,2 +1,3 @@
 alter table SPACE add column NORMAL_CAPACITY int(11) default NULL;
 alter table SPACE add column EXAM_CAPACITY int(11) default NULL;
+alter table SPACE_OCCUPATION change column `ID_INTERNAL` `ID_INTERNAL` int(11) NOT NULL auto_increment;
