f4cc02be790e4d611316c0eb3d0f7b84406b9741
Luis Cruz <luis.cruz@ist.utl.pt>
2007-04-01 19:39:01 +0000

    Make execution period from which mark sheets are managed in fenix configurable in a build time property.

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
 import net.sourceforge.fenixedu.renderers.DataProvider;
 import net.sourceforge.fenixedu.renderers.components.converters.Converter;
@@ -14,19 +16,22 @@ import org.apache.commons.collections.comparators.ReverseComparator;
 public class ExecutionPeriodsNotClosedPublicProvider implements DataProvider {
 
     public Object provide(Object source, Object currentValue) {
+	final List<ExecutionPeriod> result = new ArrayList<ExecutionPeriod>();
 
-        /*
-         * get executionPeriods after '2 Semestre 2005/2006'
-         * 
-         */ 
-        final ExecutionPeriod currentExecutionPeriod = ExecutionPeriod.readByNameAndExecutionYear("2 Semestre", "2005/2006");
+	final String executionPeriodIDString = PropertiesManager.getProperty("object.id.execution.period.for.from.mark.sheet.managment");
+	final ExecutionPeriod fromExecutionPeriod;
+	if (executionPeriodIDString == null || executionPeriodIDString.length() == 0) {
+	    fromExecutionPeriod = null;
+	} else {
+	    fromExecutionPeriod = RootDomainObject.getInstance().readExecutionPeriodByOID(Integer.valueOf(executionPeriodIDString));
+	}
 
-        final List<ExecutionPeriod> result = new ArrayList<ExecutionPeriod>();
-        for (final ExecutionPeriod executionPeriod : ExecutionPeriod.readNotClosedPublicExecutionPeriods()) {
-            if (executionPeriod.isAfterOrEquals(currentExecutionPeriod)) {
+        for (final ExecutionPeriod executionPeriod : RootDomainObject.getInstance().getExecutionPeriodsSet()) {
+            if (executionPeriod.isAfterOrEquals(fromExecutionPeriod)) {
                 result.add(executionPeriod);
             }
         }
+
         Collections.sort(result, new ReverseComparator());
         return result; 
     }
