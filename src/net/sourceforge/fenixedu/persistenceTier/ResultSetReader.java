49bb56b10fe21711909a6f65246a48f962a070e1
Joao Cachopo <joao.cachopo@inesc-id.pt>
2007-04-03 10:42:05 +0000

    Use specific return types for each method, because that is needed to avoid typecasts in the code generated by a forthcoming version of the DML compiler.

--- src/net/sourceforge/fenixedu/persistenceTier/ResultSetReader.java
+++ src/net/sourceforge/fenixedu/persistenceTier/ResultSetReader.java
@@ -1,57 +1,60 @@
 package net.sourceforge.fenixedu.persistenceTier;
 
 import java.sql.Blob;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ResultSetReader {
 
-    public static Object getFromBIGINT(ResultSet rs, String columnName) throws SQLException {
-        Object result = rs.getLong(columnName);
+    public static Long getFromBIGINT(ResultSet rs, String columnName) throws SQLException {
+        Long result = rs.getLong(columnName);
         return (rs.wasNull() ? null : result);
     }
 
-    public static Object getFromBIT(ResultSet rs, String columnName) throws SQLException {
-        Object result = rs.getBoolean(columnName);
+    public static Boolean getFromBIT(ResultSet rs, String columnName) throws SQLException {
+        Boolean result = rs.getBoolean(columnName);
         return (rs.wasNull() ? null : result);
     }
 
-    public static Object getFromBLOB(ResultSet rs, String columnName) throws SQLException {
+    public static byte[] getFromBLOB(ResultSet rs, String columnName) throws SQLException {
         Blob aBlob = rs.getBlob(columnName);
         return (rs.wasNull() ? null : aBlob.getBytes(1L, (int) aBlob.length()));
     }
 
-    public static Object getFromCHAR(ResultSet rs, String columnName) throws SQLException {
+    public static String getFromCHAR(ResultSet rs, String columnName) throws SQLException {
         return rs.getString(columnName);
     }
 
-    public static Object getFromDATE(ResultSet rs, String columnName) throws SQLException {
+    public static Date getFromDATE(ResultSet rs, String columnName) throws SQLException {
         return rs.getDate(columnName);
     }
 
-    public static Object getFromDOUBLE(ResultSet rs, String columnName) throws SQLException {
-        Object result = rs.getDouble(columnName);
+    public static Double getFromDOUBLE(ResultSet rs, String columnName) throws SQLException {
+        Double result = rs.getDouble(columnName);
         return (rs.wasNull() ? null : result);
     }
 
-    public static Object getFromINTEGER(ResultSet rs, String columnName) throws SQLException {
-        Object result = rs.getInt(columnName);
+    public static Integer getFromINTEGER(ResultSet rs, String columnName) throws SQLException {
+        Integer result = rs.getInt(columnName);
         return (rs.wasNull() ? null : result);
     }
 
-    public static Object getFromLONGVARCHAR(ResultSet rs, String columnName) throws SQLException {
+    public static String getFromLONGVARCHAR(ResultSet rs, String columnName) throws SQLException {
         return rs.getString(columnName);
     }
 
-    public static Object getFromTIME(ResultSet rs, String columnName) throws SQLException {
+    public static Time getFromTIME(ResultSet rs, String columnName) throws SQLException {
         return rs.getTime(columnName);
     }
 
-    public static Object getFromTIMESTAMP(ResultSet rs, String columnName) throws SQLException {
+    public static Timestamp getFromTIMESTAMP(ResultSet rs, String columnName) throws SQLException {
         return rs.getTimestamp(columnName);
     }
 
-    public static Object getFromVARCHAR(ResultSet rs, String columnName) throws SQLException {
+    public static String getFromVARCHAR(ResultSet rs, String columnName) throws SQLException {
         return rs.getString(columnName);
     }
 }
