dab4c6e970ea00f2575c2eb6a7cbddc7ba3c199a
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2007-04-03 15:17:01 +0000

    Moving some scientific journal from national to international due to request.

---
+++ etc/database_operations/alterSomeScientificJournalsLocationType.sql
@@ -0,0 +1,1 @@
+update SCIENTIFIC_JOURNAL set LOCATION_TYPE='INTERNATIONAL' WHERE NAME LIKE 'Ecological Economics' OR NAME LIKE 'Bulletin of Mathematical Biology' OR NAME LIKE 'IMA Journal of Mathematics Applied to Biology and Medicine' OR NAME LIKE 'Mathematical Biosciences' OR NAME LIKE 'Ecological Modelling';
