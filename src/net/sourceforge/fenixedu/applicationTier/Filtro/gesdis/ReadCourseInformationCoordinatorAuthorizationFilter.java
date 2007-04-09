cea0979e663c684c27289bd1d8aacaf7ea9a56f1
Cláudio Gil <claudio.gil@ist.utl.pt>
2007-04-09 16:37:56 +0000

    Allow coordinators to read execution course information for all the previous years

--- src/net/sourceforge/fenixedu/applicationTier/Filtro/gesdis/ReadCourseInformationCoordinatorAuthorizationFilter.java
+++ src/net/sourceforge/fenixedu/applicationTier/Filtro/gesdis/ReadCourseInformationCoordinatorAuthorizationFilter.java
@@ -4,10 +4,16 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Filtro.gesdis;
 
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.framework.DomainObjectAuthorizationFilter;
 import net.sourceforge.fenixedu.domain.Coordinator;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.domain.ExecutionDegree;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.person.RoleType;
@@ -21,17 +27,31 @@ public class ReadCourseInformationCoordinatorAuthorizationFilter extends DomainO
 
     protected boolean verifyCondition(IUserView id, Integer objectId) {
         final Person person = id.getPerson();
-        final ExecutionCourse executionCourse = RootDomainObject.getInstance().readExecutionCourseByOID(objectId);
-        for (final Coordinator coordinator : person.getCoordinatorsSet()) {
-            if (coordinator.isCoordinatorOfExecutionDegreeContaining(executionCourse)) {
-                return true;
-            }
+        
+        SortedSet<Coordinator> coordinators = new TreeSet<Coordinator>(new CoordinatorByExecutionDegreeComparator());
+        coordinators.addAll(person.getCoordinators());
+        
+        if (coordinators.isEmpty()) {
+            return false;
         }
-        return false;
+        
+        ExecutionCourse executionCourse = RootDomainObject.getInstance().readExecutionCourseByOID(objectId);
+        
+        Coordinator coordinator = coordinators.first();
+        ExecutionYear executionYear = coordinator.getExecutionDegree().getExecutionYear();
+        
+        return executionYear.compareTo(executionCourse.getExecutionYear()) >= 0;
     }
 
     protected RoleType getRoleType() {
         return RoleType.COORDINATOR;
     }
 
+    public static class CoordinatorByExecutionDegreeComparator implements Comparator<Coordinator> {
+
+        public int compare(Coordinator o1, Coordinator o2) {
+            return ExecutionDegree.EXECUTION_DEGREE_COMPARATORY_BY_YEAR.compare(o2.getExecutionDegree(), o1.getExecutionDegree());
+        }
+        
+    }
 }
--- src/net/sourceforge/fenixedu/domain/Coordinator.java
+++ src/net/sourceforge/fenixedu/domain/Coordinator.java
@@ -28,19 +28,4 @@ public class Coordinator extends Coordinator_Base {
         return getResponsible().booleanValue();
     }
 
-    public boolean isCoordinatorOfExecutionDegreeContaining(final ExecutionCourse executionCourse) {
-        final ExecutionDegree executionDegree = getExecutionDegree();
-        if (executionCourse.isLecturedIn(executionDegree.getExecutionYear())) {
-            final ExecutionPeriod executionPeriod = executionCourse.getExecutionPeriod();
-            final DegreeCurricularPlan degreeCurricularPlan = executionDegree.getDegreeCurricularPlan();
-            for (final CurricularCourse curricularCourse : executionCourse.getAssociatedCurricularCoursesSet()) {
-                if (curricularCourse.getDegreeCurricularPlan() == degreeCurricularPlan
-                        && curricularCourse.hasActiveScopesInExecutionPeriod(executionPeriod)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 }
