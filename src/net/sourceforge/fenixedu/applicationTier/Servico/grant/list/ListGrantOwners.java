38796ebac78d18862697da8c185a48671c64276b
Ângela Almeida <angela.almeida@ist.utl.pt>
2007-04-17 15:06:46 +0000

    bug fixed

--- src/net/sourceforge/fenixedu/applicationTier/Servico/grant/list/ListGrantOwners.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/grant/list/ListGrantOwners.java
@@ -23,69 +23,76 @@ import net.sourceforge.fenixedu.util.NameUtils;
  */
 public class ListGrantOwners extends Service {
 
-    public ListGrantOwners() {
-    }
+	public ListGrantOwners() {
+	}
 
-    /**
-     * Query the grant owner by criteria of grant contract
-     * 
-     * @throws ExcepcaoPersistencia
-     * 
-     * @returns an array of objects object[0] List of result object[1]
-     *          IndoSpanListGrantOwner
-     */
+	/**
+	 * Query the grant owner by criteria of grant contract
+	 * 
+	 * @throws ExcepcaoPersistencia
+	 * 
+	 * @returns an array of objects object[0] List of result object[1]
+	 *          IndoSpanListGrantOwner
+	 */
 
-    public Object[] run(InfoSpanListGrantOwner infoSpanListGrantOwner) throws FenixServiceException,
-            ExcepcaoPersistencia {
-        // Read the grant owners ordered by persistentSupportan
-        List grantOwnerBySpan = null;
+	public Object[] run(InfoSpanListGrantOwner infoSpanListGrantOwner)
+			throws FenixServiceException, ExcepcaoPersistencia {
+		// Read the grant owners ordered by persistentSupportan
+		List grantOwnerBySpan = null;
 
-        grantOwnerBySpan = GrantOwner.readAllGrantOwnersBySpan(infoSpanListGrantOwner.getSpanNumber(),
-                SessionConstants.NUMBER_OF_ELEMENTS_IN_SPAN, propertyOrderBy(infoSpanListGrantOwner
-                        .getOrderBy()));
+		grantOwnerBySpan = GrantOwner.readAllGrantOwnersBySpan(
+				infoSpanListGrantOwner.getSpanNumber(),
+				100,
+				propertyOrderBy(infoSpanListGrantOwner.getOrderBy()));
+	
+		List listGrantOwner = null;
+		if (grantOwnerBySpan != null && grantOwnerBySpan.size() != 0) {
+			listGrantOwner = new ArrayList();
 
-        List listGrantOwner = null;
-        if (grantOwnerBySpan != null && grantOwnerBySpan.size() != 0) {
-            listGrantOwner = new ArrayList();
+			// For each Grant Owner construct the info list object.
+			for (int i = 0; i < grantOwnerBySpan.size(); i++) {
+				GrantOwner grantOwner = (GrantOwner) grantOwnerBySpan.get(i);
+				if (grantOwner.getPerson() != null) {
+					listGrantOwner
+							.add(convertToInfoListGrantOwnerByOrder(grantOwner));
+				}
+			}
+		}
 
-            // For each Grant Owner construct the info list object.
-            for (int i = 0; i < grantOwnerBySpan.size(); i++) {
-                GrantOwner grantOwner = (GrantOwner) grantOwnerBySpan.get(i);
-                listGrantOwner.add(convertToInfoListGrantOwnerByOrder(grantOwner));
-            }
-        }
+		if (infoSpanListGrantOwner.getTotalElements() == null) {
+			// Setting the search attributes
+			List<GrantOwner> grantOwners = rootDomainObject.getGrantOwners();
+			infoSpanListGrantOwner.setTotalElements(grantOwners.size());
+		}
+		Object[] result = { listGrantOwner, infoSpanListGrantOwner };
+		return result;
+	}
 
-        if (infoSpanListGrantOwner.getTotalElements() == null) {
-            // Setting the search attributes
-            List<GrantOwner> grantOwners = rootDomainObject.getGrantOwners();
-            infoSpanListGrantOwner.setTotalElements(grantOwners.size());
-        }
-        Object[] result = { listGrantOwner, infoSpanListGrantOwner };
-        return result;
-    }
+	/*
+	 * Returns the order string to add to the criteria
+	 */
+	private String propertyOrderBy(String orderBy) {
+		String result = null;
+		if (orderBy.equals("orderByNumber")) {
+			result = "number";
+		} else if (orderBy.equals("orderByFirstName")) {
+			result = "person.name";
+		}
+		return result;
+	}
 
-    /*
-     * Returns the order string to add to the criteria
-     */
-    private String propertyOrderBy(String orderBy) {
-        String result = null;
-        if (orderBy.equals("orderByNumber")) {
-            result = "number";
-        } else if (orderBy.equals("orderByFirstName")) {
-            result = "person.name";
-        }
-        return result;
-    }
+	private InfoListGrantOwnerByOrder convertToInfoListGrantOwnerByOrder(
+			GrantOwner grantOwner) {
+		InfoListGrantOwnerByOrder infoListGrantOwnerByOrder = new InfoListGrantOwnerByOrder();
 
-    private InfoListGrantOwnerByOrder convertToInfoListGrantOwnerByOrder(GrantOwner grantOwner) {
-        InfoListGrantOwnerByOrder infoListGrantOwnerByOrder = new InfoListGrantOwnerByOrder();
+		infoListGrantOwnerByOrder.setGrantOwnerId(grantOwner.getIdInternal());
+		infoListGrantOwnerByOrder.setGrantOwnerNumber(grantOwner.getNumber());
 
-        infoListGrantOwnerByOrder.setGrantOwnerId(grantOwner.getIdInternal());
-        infoListGrantOwnerByOrder.setGrantOwnerNumber(grantOwner.getNumber());
+		infoListGrantOwnerByOrder.setFirstName(NameUtils
+				.getFirstName(grantOwner.getPerson().getName()));
+		infoListGrantOwnerByOrder.setLastName(NameUtils.getLastName(grantOwner
+				.getPerson().getName()));
 
-        infoListGrantOwnerByOrder.setFirstName(NameUtils.getFirstName(grantOwner.getPerson().getName()));
-        infoListGrantOwnerByOrder.setLastName(NameUtils.getLastName(grantOwner.getPerson().getName()));
-
-        return infoListGrantOwnerByOrder;
-    }
+		return infoListGrantOwnerByOrder;
+	}
 }
