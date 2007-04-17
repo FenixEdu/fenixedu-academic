1dbaca95228f79d78a569acb9bbca7d92ae257a8
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-04-17 13:22:31 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/assiduousness/WorkWeek.java
+++ src/net/sourceforge/fenixedu/domain/assiduousness/WorkWeek.java
@@ -27,70 +27,69 @@ public class WorkWeek implements Serializable {
     private EnumSet<WeekDay> days;
 
     public WorkWeek(EnumSet<WeekDay> newDays) {
-        EnumSet<WeekDay> myDays = EnumSet.noneOf(WeekDay.class);
-        for (WeekDay day : newDays) {
-            myDays.add(day);
-        }
-        days = myDays;
+	EnumSet<WeekDay> myDays = EnumSet.noneOf(WeekDay.class);
+	for (WeekDay day : newDays) {
+	    myDays.add(day);
+	}
+	days = myDays;
     }
 
     public WorkWeek(WeekDay... newDays) {
-        EnumSet<WeekDay> myDays = EnumSet.noneOf(WeekDay.class);
-        for (WeekDay day : newDays) {
-            myDays.add(day);
-        }
-        days = myDays;
+	EnumSet<WeekDay> myDays = EnumSet.noneOf(WeekDay.class);
+	for (WeekDay day : newDays) {
+	    myDays.add(day);
+	}
+	days = myDays;
     }
 
     public EnumSet<WeekDay> getDays() {
-        return days;
+	return days;
     }
 
     public boolean worksAt(WeekDay day) {
-        return getDays().contains(day);
+	return getDays().contains(day);
     }
 
     // counts how many days per week the employee works
     public int workDaysPerWeek() {
-        int workDays = 0;
-        for (WeekDay day : getDays()) {
-            if (getDays().contains(day)) {
-                workDays++;
-            }
-        }
-        return workDays;
+	int workDays = 0;
+	for (WeekDay day : getDays()) {
+	    if (getDays().contains(day)) {
+		workDays++;
+	    }
+	}
+	return workDays;
     }
 
     public boolean equals(WorkWeek workWeek) {
-        return workWeek.getDays().containsAll(getDays()) && getDays().containsAll(workWeek.getDays());
+	return workWeek.getDays().containsAll(getDays()) && getDays().containsAll(workWeek.getDays());
     }
 
-    // This work week contains workWeek
     public boolean contains(WorkWeek otherWorkWeek) {
-        for (WeekDay otherWorkWeekDay : otherWorkWeek.getDays()) {
-            if (getDays().contains(otherWorkWeekDay))
-                return true;
-        }
-        return false;
+	for (WeekDay otherWorkWeekDay : otherWorkWeek.getDays()) {
+	    if (!getDays().contains(otherWorkWeekDay))
+		return false;
+	}
+	return true;
     }
 
     public boolean contains(DateTime date) {
-        WeekDay dayOfWeek = WeekDay.fromJodaTimeToWeekDay(date);
-        if (getDays().contains(dayOfWeek)) {
-            return true;
-        }
-        return false;
+	WeekDay dayOfWeek = WeekDay.fromJodaTimeToWeekDay(date);
+	if (getDays().contains(dayOfWeek)) {
+	    return true;
+	}
+	return false;
     }
 
     public void validateWorkScheduleDays(HashMap<String, WorkScheduleDaySheet> workScheduleDays,
-            ResourceBundle bundle) {
-        for (WeekDay weekDay : getDays()) {
-            if (workScheduleDays.get(weekDay.toString()) == null) {
-                WorkScheduleDaySheet workScheduleDaySheet = new WorkScheduleDaySheet();
-                workScheduleDaySheet.setSchedule("");
-                workScheduleDaySheet.setWeekDay(bundle.getString(weekDay.toString() + "_ACRONYM"));
-                workScheduleDays.put(weekDay.toString(), workScheduleDaySheet);
-            }
-        }
+	    ResourceBundle bundle) {
+	for (WeekDay weekDay : getDays()) {
+	    if (workScheduleDays.get(weekDay.toString()) == null) {
+		WorkScheduleDaySheet workScheduleDaySheet = new WorkScheduleDaySheet();
+		workScheduleDaySheet.setSchedule("");
+		workScheduleDaySheet.setWeekDay(bundle.getString(weekDay.toString() + "_ACRONYM"));
+		workScheduleDays.put(weekDay.toString(), workScheduleDaySheet);
+	    }
+	}
     }
 }
