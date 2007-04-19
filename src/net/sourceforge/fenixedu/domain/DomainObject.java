d6b688e0d6ce7892a1cbcbee14b2cf16b3bd9909
Luís Egídio <luis.egidio@ist.utl.pt>
2007-04-19 14:27:16 +0000

    comparator by id, to be used in all subclasses

--- src/net/sourceforge/fenixedu/domain/DomainObject.java
+++ src/net/sourceforge/fenixedu/domain/DomainObject.java
@@ -5,6 +5,7 @@
 package net.sourceforge.fenixedu.domain;
 
 import java.lang.reflect.Field;
+import java.util.Comparator;
 
 import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
@@ -23,6 +24,12 @@ import pt.utl.ist.fenix.tools.util.StringAppender;
  * @author jpvl
  */
 public abstract class DomainObject extends DomainObject_Base implements dml.runtime.FenixDomainObject {
+
+    static final protected Comparator<DomainObject> COMPARATOR_BY_ID = new Comparator<DomainObject>() {
+        public int compare(DomainObject o1, DomainObject o2) {
+	    return o1.getIdInternal().compareTo(o2.getIdInternal());
+        }
+    };
 
     private static final boolean ERROR_IF_DELETED_OBJECT_NOT_DISCONNECTED = 
 		PropertiesManager.getBooleanProperty("error.if.deleted.object.not.disconnected");
