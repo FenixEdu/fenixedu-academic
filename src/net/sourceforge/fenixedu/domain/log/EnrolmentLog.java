25b9cbaf58b9a3307d41f1ab9a10f1d9086cb954
João Florindo <jdnf@linkare.pt>
2007-04-20 13:49:34 +0000

    use non deprecated method

--- src/net/sourceforge/fenixedu/domain/log/EnrolmentLog.java
+++ src/net/sourceforge/fenixedu/domain/log/EnrolmentLog.java
@@ -3,13 +3,13 @@
  */
 package net.sourceforge.fenixedu.domain.log;
 
-import java.util.Date;
-
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.util.EnrolmentAction;
+
+import org.joda.time.DateTime;
 
 /**
  * @author nmgo
@@ -20,7 +20,7 @@ public class EnrolmentLog extends EnrolmentLog_Base {
     public EnrolmentLog(EnrolmentAction action, Registration registration, CurricularCourse curricularCourse, ExecutionPeriod executionPeriod, String who) {
     	super();
     	setRootDomainObject(RootDomainObject.getInstance());
-        this.setDate(new Date());
+        this.setDateDateTime(new DateTime());
         this.setAction(action);
         this.setStudent(registration);        
         this.setCurricularCourse(curricularCourse);
