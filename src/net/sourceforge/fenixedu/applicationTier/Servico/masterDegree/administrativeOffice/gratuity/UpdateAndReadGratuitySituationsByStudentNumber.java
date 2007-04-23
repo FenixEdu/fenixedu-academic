2f9468b9019f0ce5af394aabf452f005d1d69748
Shezad Anavarali <shezad@ist.utl.pt>
2007-04-23 14:26:01 +0000

    check every Master Degree registrations with the given number

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/UpdateAndReadGratuitySituationsByStudentNumber.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/gratuity/UpdateAndReadGratuitySituationsByStudentNumber.java
@@ -22,21 +22,23 @@ public class UpdateAndReadGratuitySituationsByStudentNumber extends Service {
 
     public List<InfoGratuitySituation> run(Integer studentNumber) throws ExcepcaoPersistencia {
         
-        Registration registration = Registration.readStudentByNumberAndDegreeType(studentNumber, DegreeType.MASTER_DEGREE);
-        List<StudentCurricularPlan> studentCurricularPlansList = registration.getStudentCurricularPlans();
-
-        List<InfoGratuitySituation> infoGratuitySituationsList = new ArrayList<InfoGratuitySituation>();
-
-        for (StudentCurricularPlan studentCurricularPlan : studentCurricularPlansList) {
-
-            List<GratuitySituation> gratuitySituations = studentCurricularPlan.getGratuitySituations();
-            for (GratuitySituation gratuitySituation : gratuitySituations) {
-                gratuitySituation.updateValues();
-                
-                infoGratuitySituationsList.add(InfoGratuitySituationWithInfoPersonAndInfoExecutionDegree
-                        .newInfoFromDomain(gratuitySituation));
+	List<InfoGratuitySituation> infoGratuitySituationsList = new ArrayList<InfoGratuitySituation>();
+	
+        for (Registration registration : Registration.readByNumberAndDegreeType(studentNumber, DegreeType.MASTER_DEGREE)) {
+            List<StudentCurricularPlan> studentCurricularPlansList = registration.getStudentCurricularPlans();
+            
+            for (StudentCurricularPlan studentCurricularPlan : studentCurricularPlansList) {
+        	
+        	List<GratuitySituation> gratuitySituations = studentCurricularPlan.getGratuitySituations();
+        	for (GratuitySituation gratuitySituation : gratuitySituations) {
+        	    gratuitySituation.updateValues();
+        	    
+        	    infoGratuitySituationsList.add(InfoGratuitySituationWithInfoPersonAndInfoExecutionDegree
+        		    .newInfoFromDomain(gratuitySituation));
+        	}
             }
-        }
+	    
+	}
 
         return infoGratuitySituationsList;
     }
