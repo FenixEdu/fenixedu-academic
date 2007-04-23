e7e815a15e645a324a0eb58496212a29aad8167e
Shezad Anavarali <shezad@ist.utl.pt>
2007-04-23 14:24:17 +0000

    doesn't send mails anymore

--- src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/candidate/EditMasterDegreeCandidate.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/masterDegree/administrativeOffice/candidate/EditMasterDegreeCandidate.java
@@ -1,12 +1,7 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.masterDegree.administrativeOffice.candidate;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
-import net.sourceforge.fenixedu.applicationTier.Servico.commons.SendMail;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoMasterDegreeCandidate;
 import net.sourceforge.fenixedu.dataTransferObject.InfoMasterDegreeCandidateWithInfoPerson;
@@ -15,7 +10,6 @@ import net.sourceforge.fenixedu.domain.CandidateSituation;
 import net.sourceforge.fenixedu.domain.Country;
 import net.sourceforge.fenixedu.domain.MasterDegreeCandidate;
 import net.sourceforge.fenixedu.domain.Person;
-import net.sourceforge.fenixedu.util.LanguageUtils;
 import net.sourceforge.fenixedu.util.State;
 
 import org.joda.time.YearMonthDay;
@@ -48,7 +42,7 @@ public class EditMasterDegreeCandidate extends Service {
 	// Change Situation
 	CandidateSituation oldCandidateSituation = oldMasterDegreeCandidate
 		.getActiveCandidateSituation();
-     CandidateSituation newCandidateSituation = null;
+	CandidateSituation newCandidateSituation = null;
 	if (!oldCandidateSituation.getSituation().equals(
 		newCandidate.getInfoCandidateSituation().getSituation())) {
 
@@ -60,49 +54,16 @@ public class EditMasterDegreeCandidate extends Service {
 	    newCandidateSituation.setSituation(newCandidate.getInfoCandidateSituation().getSituation());
 	    newCandidateSituation.setValidation(new State(State.ACTIVE));
 
-	}else if(oldCandidateSituation.getSituation().equals(newCandidate.getInfoCandidateSituation().getSituation())){  
-        newCandidateSituation = oldCandidateSituation;
-        newCandidateSituation.setDateYearMonthDay(oldCandidateSituation.getDateYearMonthDay());
-        newCandidateSituation.setMasterDegreeCandidate(oldMasterDegreeCandidate);
-        newCandidateSituation.setRemarks(newCandidate.getInfoCandidateSituation().getRemarks());
-        newCandidateSituation.setSituation(newCandidate.getInfoCandidateSituation().getSituation());    
-    }
-    if (person.getEmail() != null) {
-      sendEmailToCandidate(oldMasterDegreeCandidate, newCandidateSituation);
-      }
-	return InfoMasterDegreeCandidateWithInfoPerson.newInfoFromDomain(oldMasterDegreeCandidate);
-    }
-
-    private void sendEmailToCandidate(MasterDegreeCandidate masterDegreeCandidate,
-	    CandidateSituation candidateSituation) {
-	ResourceBundle rb = ResourceBundle.getBundle("resources.ApplicationResources", LanguageUtils.getLocale());
-	List<String> toList = new ArrayList<String>();
-	List CCList, BCCList;
-	CCList = new ArrayList();
-	BCCList = new ArrayList();
-	toList.add(masterDegreeCandidate.getPerson().getEmail());
-	String fromName, from, subject;
-	fromName = rb.getString("masterDegreeCandidate.email.fromName");
-	from = rb.getString("masterDegreeCandidate.email.fromEmail");
-	subject = rb.getString("masterDegreeCandidate.email.subject")
-		+ masterDegreeCandidate.getExecutionDegree().getDegreeCurricularPlan().getName();
-	StringBuilder text = new StringBuilder();
-	text.append(rb.getString("masterDegreeCandidate.email.greeting"));
-	text.append(" ");
-	text.append(masterDegreeCandidate.getPerson().getName());
-	text.append(rb.getString("masterDegreeCandidate.email.period"));
-	text.append(rb.getString("masterDegreeCandidate.email.newLine"));
-	text.append(rb.getString("masterDegreeCandidate.email.body"));
-	text.append(candidateSituation.getSituation().toString());
-	text.append(rb.getString("masterDegreeCandidate.email.period"));
-	text.append(rb.getString("masterDegreeCandidate.email.newLine"));
-	text.append(rb.getString("masterDegreeCandidate.email.newLine"));
-	text.append(rb.getString("masterDegreeCandidate.email.goodbye"));
-	try {
-	    SendMail sendMailService = new SendMail();
-	    sendMailService.run(toList, CCList, BCCList, fromName, from, subject, text.toString());
-	} catch (Exception e) {
+	} else if (oldCandidateSituation.getSituation().equals(
+		newCandidate.getInfoCandidateSituation().getSituation())) {
+	    newCandidateSituation = oldCandidateSituation;
+	    newCandidateSituation.setDateYearMonthDay(oldCandidateSituation.getDateYearMonthDay());
+	    newCandidateSituation.setMasterDegreeCandidate(oldMasterDegreeCandidate);
+	    newCandidateSituation.setRemarks(newCandidate.getInfoCandidateSituation().getRemarks());
+	    newCandidateSituation.setSituation(newCandidate.getInfoCandidateSituation().getSituation());
 	}
+
+	return InfoMasterDegreeCandidateWithInfoPerson.newInfoFromDomain(oldMasterDegreeCandidate);
     }
 
 }
