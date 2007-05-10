22101d89fb8ed814386172ebffda65402e398a09
Cláudio Gil <claudio.gil@ist.utl.pt>
2007-05-10 15:48:44 +0000

    bugfix: vowel participations where not being deleted

--- src/net/sourceforge/fenixedu/applicationTier/Servico/thesis/ChangeThesisPerson.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/thesis/ChangeThesisPerson.java
@@ -3,6 +3,7 @@ package net.sourceforge.fenixedu.applicationTier.Servico.thesis;
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.commons.externalPerson.InsertExternalPerson;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.applicationTier.Servico.manager.organizationalStructureManagement.RemoveParentInherentFunction;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
@@ -68,7 +69,7 @@ public class ChangeThesisPerson extends Service {
             break;
         case vowel:
             if (change.target != null) {
-                thesis.removeParticipations(change.target);
+                change.target.delete();
                 if (person != null) {
                     thesis.addVowel(person);
                 }
