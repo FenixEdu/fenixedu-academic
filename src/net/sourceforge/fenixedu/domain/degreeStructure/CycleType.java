3ce875b0330d385a9becb533720d1e0ccd4e172c
Luís Egídio <luis.egidio@ist.utl.pt>
2007-05-18 14:53:28 +0000

    getQualifiedName

--- src/net/sourceforge/fenixedu/domain/degreeStructure/CycleType.java
+++ src/net/sourceforge/fenixedu/domain/degreeStructure/CycleType.java
@@ -1,11 +1,16 @@
 package net.sourceforge.fenixedu.domain.degreeStructure;
 
+
 public enum CycleType {
 
     FIRST_CYCLE,
 
     SECOND_CYCLE,
 
-    THIRD_CYCLE
+    THIRD_CYCLE;
+
+    public String getQualifiedName() {
+	return this.getClass().getSimpleName() + "." + name();
+    }
 
 }
