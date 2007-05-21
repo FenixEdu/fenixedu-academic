30ccbf19f6496323cac2cf0bcc7ed1a3eb58956f
João Florindo <jdnf@linkare.pt>
2007-05-21 10:57:59 +0000

    formated code

--- src/net/sourceforge/fenixedu/domain/CurricularCourseScope.java
+++ src/net/sourceforge/fenixedu/domain/CurricularCourseScope.java
@@ -12,8 +12,6 @@ import net.sourceforge.fenixedu.util.DateFormatUtil;
 import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.collections.comparators.ComparatorChain;
 
-
-
 public class CurricularCourseScope extends CurricularCourseScope_Base {
 
     public static Comparator<CurricularCourseScope> CURRICULAR_COURSE_NAME_COMPARATOR = new ComparatorChain();
@@ -21,121 +19,104 @@ public class CurricularCourseScope extends CurricularCourseScope_Base {
 	((ComparatorChain) CURRICULAR_COURSE_NAME_COMPARATOR).addComparator(new BeanComparator("curricularCourse.name", Collator.getInstance()));
 	((ComparatorChain) CURRICULAR_COURSE_NAME_COMPARATOR).addComparator(new BeanComparator("curricularCourse.idInternal"));
     }
-    
-	public CurricularCourseScope() {
-		super();
-		setRootDomainObject(RootDomainObject.getInstance());
-	}
-	
+
+    public CurricularCourseScope() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+    }
+
     public CurricularCourseScope(Branch branch, CurricularCourse curricularCourse,
 	    CurricularSemester curricularSemester, Calendar beginDate, Calendar endDate,
 	    String Annotation) {
-		this();
+	this();
 	// check that there isn't another scope active with the same curricular
 	// course, branch and semester
-		
-        if (curricularCourse.hasActiveScopeInGivenSemesterForGivenBranch(curricularSemester, branch)) {
-            throw new DomainException("error.curricular.course.scope.conflict.creation");
-        }
-		
-		setBranch(branch);
-		setCurricularCourse(curricularCourse);
-		setCurricularSemester(curricularSemester);
-		
-		setBeginDate(beginDate);
-		setEndDate(endDate);
-		setAnotation(Annotation);
+
+	if (curricularCourse.hasActiveScopeInGivenSemesterForGivenBranch(curricularSemester, branch)) {
+	    throw new DomainException("error.curricular.course.scope.conflict.creation");
 	}
-	
-	
-    /**
-     * @return Returns the beginDate.
-     */
+
+	setBranch(branch);
+	setCurricularCourse(curricularCourse);
+	setCurricularSemester(curricularSemester);
+
+	setBeginDate(beginDate);
+	setEndDate(endDate);
+	setAnotation(Annotation);
+    }
+
     public Calendar getBeginDate() {
-        if (this.getBegin() != null) {
-            Calendar result = Calendar.getInstance();
-            result.setTime(this.getBegin());
-            return result;
-        }
-        return null;
+	if (this.getBegin() != null) {
+	    Calendar result = Calendar.getInstance();
+	    result.setTime(this.getBegin());
+	    return result;
+	}
+	return null;
     }
 
-    /**
-     * @param beginDate
-     *            The beginDate to set.
-     */
     public void setBeginDate(Calendar beginDate) {
-        if (beginDate != null) {
-            this.setBegin(beginDate.getTime());
-        } else {
-            this.setBegin(null);
-        }
+	if (beginDate != null) {
+	    this.setBegin(beginDate.getTime());
+	} else {
+	    this.setBegin(null);
+	}
     }
 
-    /**
-     * @return Returns the endDate.
-     */
     public Calendar getEndDate() {
-        if (this.getEnd() != null) {
-            Calendar result = Calendar.getInstance();
-            result.setTime(this.getEnd());
-            return result;
-        }
-        return null;
+	if (this.getEnd() != null) {
+	    Calendar result = Calendar.getInstance();
+	    result.setTime(this.getEnd());
+	    return result;
+	}
+	return null;
     }
 
-    /**
-     * @param endDate
-     *            The endDate to set.
-     */
     public void setEndDate(Calendar endDate) {
-        if (endDate != null) {
-            this.setEnd(endDate.getTime());
-        } else {
-            this.setEnd(null);
-        }
+	if (endDate != null) {
+	    this.setEnd(endDate.getTime());
+	} else {
+	    this.setEnd(null);
+	}
     }
 
     public Boolean isActive() {
-        return isActive(new Date());
+	return isActive(new Date());
     }
-    
-    public boolean getActive (){
-    	return this.isActive();
+
+    public boolean getActive() {
+	return this.isActive();
     }
-	
-	
-	public Boolean canBeDeleted() {
-		return !hasAnyAssociatedWrittenEvaluations();
-	}
-	
+
+    public Boolean canBeDeleted() {
+	return !hasAnyAssociatedWrittenEvaluations();
+    }
+
     public void edit(Branch branch, CurricularSemester curricularSemester, Calendar beginDate,
 	    Calendar endDate, String Annotation) {
-	
-		setBranch(branch);
-		setCurricularSemester(curricularSemester);
-		setBeginDate(beginDate);
-		setEndDate(endDate);
-		setAnotation(Annotation);
-	}
-	
-	public void end(Calendar endDate) {
-		setEndDate(endDate);
-	}	
-	
-	public void delete() throws DomainException {
 
-	    if (canBeDeleted()) {
-			removeCurricularSemester();
-			removeCurricularCourse();
-			removeBranch();
-			
-			removeRootDomainObject();
-        super.deleteDomainObject();
-        } else {
-            throw new DomainException("error.curricular.course.scope.has.written.evaluations");
-        }
+	setBranch(branch);
+	setCurricularSemester(curricularSemester);
+	setBeginDate(beginDate);
+	setEndDate(endDate);
+	setAnotation(Annotation);
+    }
+
+    public void end(Calendar endDate) {
+	setEndDate(endDate);
+    }
+
+    public void delete() throws DomainException {
+	if (canBeDeleted()) {
+	    removeCurricularSemester();
+	    removeCurricularCourse();
+	    removeBranch();
+
+	    removeRootDomainObject();
+	    super.deleteDomainObject();
+	} else {
+	    throw new DomainException("error.curricular.course.scope.has.written.evaluations");
 	}
+    }
 
     public Boolean isActive(Date date) {
 	Calendar calendar = Calendar.getInstance();
@@ -154,73 +135,72 @@ public class CurricularCourseScope extends CurricularCourseScope_Base {
 	return DateFormatUtil.compareDates("yyyyMMdd", getBegin(), end) < 0
 		&& (getEnd() == null || DateFormatUtil.compareDates("yyyyMMdd", getEnd(), begin) > 0);
     }
-    
+
     public boolean isActiveForExecutionPeriod(final ExecutionPeriod executionPeriod) {
-        return intersects(executionPeriod.getBeginDate(), executionPeriod.getEndDate())
-                && executionPeriod.getSemester().equals(getCurricularSemester().getSemester());
+	return intersects(executionPeriod.getBeginDate(), executionPeriod.getEndDate())
+		&& executionPeriod.getSemester().equals(getCurricularSemester().getSemester());
     }
 
     private DegreeModuleScopeCurricularCourseScope degreeModuleScopeCurricularCourseScope = null;
-    
+
     private synchronized void initDegreeModuleScopeCurricularCourseScope() {
-        if(degreeModuleScopeCurricularCourseScope == null) {
-            degreeModuleScopeCurricularCourseScope = new DegreeModuleScopeCurricularCourseScope(this);
-        }
+	if (degreeModuleScopeCurricularCourseScope == null) {
+	    degreeModuleScopeCurricularCourseScope = new DegreeModuleScopeCurricularCourseScope(this);
+	}
     }
-    
+
     public DegreeModuleScopeCurricularCourseScope getDegreeModuleScopeCurricularCourseScope() {
-        if(degreeModuleScopeCurricularCourseScope == null) {
-            initDegreeModuleScopeCurricularCourseScope();
-        }
-        return degreeModuleScopeCurricularCourseScope;
-    }   
-    
+	if (degreeModuleScopeCurricularCourseScope == null) {
+	    initDegreeModuleScopeCurricularCourseScope();
+	}
+	return degreeModuleScopeCurricularCourseScope;
+    }
+
     public class DegreeModuleScopeCurricularCourseScope extends DegreeModuleScope {
 
-        private final CurricularCourseScope curricularCourseScope;
-        
-        private DegreeModuleScopeCurricularCourseScope(CurricularCourseScope curricularCourseScope) {
-            this.curricularCourseScope= curricularCourseScope;
-        }
-        
-        @Override
-        public Integer getIdInternal() {        
-            return curricularCourseScope.getIdInternal();
-        }
+	private final CurricularCourseScope curricularCourseScope;
 
-        @Override
-        public Integer getCurricularSemester() {
-            return curricularCourseScope.getCurricularSemester().getSemester();
-        }
+	private DegreeModuleScopeCurricularCourseScope(CurricularCourseScope curricularCourseScope) {
+	    this.curricularCourseScope = curricularCourseScope;
+	}
 
-        @Override
-        public Integer getCurricularYear() {
-            return curricularCourseScope.getCurricularSemester().getCurricularYear().getYear();
-        }
+	@Override
+	public Integer getIdInternal() {
+	    return curricularCourseScope.getIdInternal();
+	}
 
-        @Override
-        public String getBranch() {           
-            return curricularCourseScope.getBranch().getName();
-        }
+	@Override
+	public Integer getCurricularSemester() {
+	    return curricularCourseScope.getCurricularSemester().getSemester();
+	}
 
-        public CurricularCourseScope getCurricularCourseScope() {
-            return curricularCourseScope;
-        }
+	@Override
+	public Integer getCurricularYear() {
+	    return curricularCourseScope.getCurricularSemester().getCurricularYear().getYear();
+	}
 
-        @Override
-        public boolean isActiveForExecutionPeriod(final ExecutionPeriod executionPeriod) {
-            return curricularCourseScope.isActiveForExecutionPeriod(executionPeriod);
-        }
+	@Override
+	public String getBranch() {
+	    return curricularCourseScope.getBranch().getName();
+	}
 
-        @Override
-        public CurricularCourse getCurricularCourse() {            
-            return curricularCourseScope.getCurricularCourse();
-        }
+	public CurricularCourseScope getCurricularCourseScope() {
+	    return curricularCourseScope;
+	}
+
+	@Override
+	public boolean isActiveForExecutionPeriod(final ExecutionPeriod executionPeriod) {
+	    return curricularCourseScope.isActiveForExecutionPeriod(executionPeriod);
+	}
+
+	@Override
+	public CurricularCourse getCurricularCourse() {
+	    return curricularCourseScope.getCurricularCourse();
+	}
 
 	@Override
 	public String getAnotation() {
 	    return curricularCourseScope.getAnotation();
-	}	
+	}
     }
 }
-    
