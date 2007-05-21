6e839c7ca8e1cb1180df60ac8d5a6549ddf5f89a
João Florindo <jdnf@linkare.pt>
2007-05-21 11:05:55 +0000

    formated code

--- src/net/sourceforge/fenixedu/domain/CurricularCourseGroup.java
+++ src/net/sourceforge/fenixedu/domain/CurricularCourseGroup.java
@@ -13,25 +13,27 @@ import net.sourceforge.fenixedu.tools.enrollment.AreaType;
 public abstract class CurricularCourseGroup extends CurricularCourseGroup_Base {
 
     public CurricularCourseGroup() {
-        super();
-        this.setOjbConcreteClass(this.getClass().getName());
-        this.setRootDomainObject(RootDomainObject.getInstance());
+	super();
+	this.setOjbConcreteClass(this.getClass().getName());
+	this.setRootDomainObject(RootDomainObject.getInstance());
     }
-	
-	public void delete() throws DomainException {
-		if (!hasAnyCurricularCourses()) {
-			removeBranch();
-			getScientificAreas().clear();
-            removeRootDomainObject();
-			deleteDomainObject();
-		} else {
-			throw new DomainException("error.curricular.course.group.has.associated.curricular.courses");			
-		}
+
+    public void delete() throws DomainException {
+	if (!hasAnyCurricularCourses()) {
+	    removeBranch();
+	    getScientificAreas().clear();
+	    removeRootDomainObject();
+	    deleteDomainObject();
+	} else {
+	    throw new DomainException("error.curricular.course.group.has.associated.curricular.courses");
 	}
-	
-	public void edit(String name, Branch branch, Integer minimumValue, Integer maximumValue){}
-	
-	public void edit(String name, Branch branch, Integer minimumValue, Integer maximumValue, AreaType areaType){}
+    }
+
+    public void edit(String name, Branch branch, Integer minimumValue, Integer maximumValue) {
+    }
+
+    public void edit(String name, Branch branch, Integer minimumValue, Integer maximumValue, AreaType areaType) {
+    }
 
     public abstract Integer getMaximumCredits();
     public abstract Integer getMinimumCredits();
