7d9b066f85c28e7791a3b76b952aef5f001413d7
Shezad Anavarali <shezad@ist.utl.pt>
2007-05-22 17:21:06 +0000

    added weight

--- src/net/sourceforge/fenixedu/domain/degreeStructure/CycleType.java
+++ src/net/sourceforge/fenixedu/domain/degreeStructure/CycleType.java
@@ -1,16 +1,33 @@
 package net.sourceforge.fenixedu.domain.degreeStructure;
 
+import java.util.Comparator;
 import java.util.ResourceBundle;
 
 import net.sourceforge.fenixedu.util.LanguageUtils;
 
 public enum CycleType {
 
-    FIRST_CYCLE,
+    FIRST_CYCLE(1),
 
-    SECOND_CYCLE,
+    SECOND_CYCLE(2),
 
-    THIRD_CYCLE;
+    THIRD_CYCLE(3);
+
+    public static final Comparator<CycleType> CYCLE_TYPE_COMPARATOR = new Comparator<CycleType>() {
+	public int compare(CycleType o1, CycleType o2) {
+	    return o1.getWeight().compareTo(o2.getWeight());
+	}
+    };
+
+    private Integer weight;
+
+    private CycleType(Integer weight) {
+	this.weight = weight;
+    }
+
+    public Integer getWeight() {
+	return weight;
+    }
 
     public String getQualifiedName() {
 	return this.getClass().getSimpleName() + "." + name();
