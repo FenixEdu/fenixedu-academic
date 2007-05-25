82288e17f68578667f6ca86498636f9112ddbf7e
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-05-25 11:18:06 +0000

    Fixed bugs on receipts. Added equivalence plan domain classes

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/accounting/RegisterReceiptPrint.java
@@ -0,0 +1,13 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.accounting;
+
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.domain.Employee;
+import net.sourceforge.fenixedu.domain.accounting.Receipt;
+
+public class RegisterReceiptPrint extends Service {
+
+    public void run(final Receipt receipt, final Employee employee) throws FenixServiceException {
+        receipt.registerReceiptPrint(employee);
+    }
+}
