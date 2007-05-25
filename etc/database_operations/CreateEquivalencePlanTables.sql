f5df81048c59eb83260cddb5fcad263b6d2eb24a
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-05-25 17:58:28 +0000

    bug fix

--- etc/database_operations/CreateEquivalencePlanTables.sql
+++ etc/database_operations/CreateEquivalencePlanTables.sql
@@ -2,6 +2,7 @@ CREATE TABLE EQUIVALENCE_PLAN (
     ID_INTERNAL int(11) not null auto_increment,
     KEY_ROOT_DOMAIN_OBJECT int(11) not null default 1,
 	KEY_DEGREE_CURRICULAR_PLAN int(11) null,
+	KEY_SOURCE_DEGREE_CURRICULAR_PLAN int(11) null,
 	KEY_STUDENT_CURRICULAR_PLAN int(11) null,
 	OJB_CONCRETE_CLASS varchar(255) not null,
     primary key (ID_INTERNAL),
@@ -14,9 +15,11 @@ CREATE TABLE EQUIVALENCE_PLAN_ENTRY (
     ID_INTERNAL int(11) not null auto_increment,
     KEY_ROOT_DOMAIN_OBJECT int(11) not null default 1,
 	KEY_EQUIVALENCE_PLAN int(11) not null,
+	KEY_NEW_CURRICULAR_COURSE int(11) not null,
     primary key (ID_INTERNAL),
     index (KEY_ROOT_DOMAIN_OBJECT),
-	index (KEY_EQUIVALENCE_PLAN)
+	index (KEY_EQUIVALENCE_PLAN),
+	index (KEY_NEW_CURRICULAR_COURSE)
 ) Type=InnoDB;
 
 
