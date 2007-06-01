8b73813f5e43334147e1665c5629c47600437dcd
Luís Egídio <luis.egidio@ist.utl.pt>
2007-06-01 12:28:49 +0000

    bug fix, wrong check of source

--- src/net/sourceforge/fenixedu/persistenceTier/Conversores/CompetenceCourseType2SqlConversion.java
+++ src/net/sourceforge/fenixedu/persistenceTier/Conversores/CompetenceCourseType2SqlConversion.java
@@ -10,21 +10,11 @@ public class CompetenceCourseType2SqlConversion implements FieldConversion {
     private static final long serialVersionUID = 1L;
 
     public Object javaToSql(Object source) throws ConversionException {
-        if (source == null) {
-            return null;
-        }
-        else {
-            return ((CompetenceCourseType) source).name();
-        }
+	return (source instanceof CompetenceCourseType) ? ((CompetenceCourseType) source).name() : null;    
     }
 
     public Object sqlToJava(Object source) throws ConversionException {
-        if (source == null && source.equals("")) {
-            return null;
-        }
-        else {
-            return CompetenceCourseType.valueOf((String) source);
-        }
+	return (source instanceof String) ? CompetenceCourseType.valueOf((String) source) : null;
     }
 
 }
