713437c187dd237828386b10ed87fd9abc82172c
Luís Egídio <luis.egidio@ist.utl.pt>
2007-06-04 14:15:26 +0000

    filter degree curricular plans for logged employee given administrative office (now using already existing renderer)

--- jsp/commons/curriculumHistoric/chooseDegreeCPlanExecutionYear.jsp
+++ jsp/commons/curriculumHistoric/chooseDegreeCPlanExecutionYear.jsp
@@ -1,7 +1,9 @@
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
-<html:xhtml/>
 <%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
 <%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
+<%@ taglib uri="/WEB-INF/fenix-renderers.tld" prefix="fr" %>
+
+<html:xhtml/>
 
 <h2><bean:message key="title.student.marksSheetConsult"/></h2>
 
@@ -9,69 +11,20 @@
 	<span class="error0"><!-- Error messages go here --><html:errors bundle="CURRICULUM_HISTORIC_RESOURCES"/></span>
 </p>
 
-
-<table class="mtop15">
-
-<html:form action="/chooseExecutionYearAndDegreeCurricularPlan">
-	<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.method" property="method" value="chooseDegreeCurricularPlan"/>
-	<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.page" property="page" value="0"/>
+<fr:form action="/chooseExecutionYearAndDegreeCurricularPlan.do?method=showActiveCurricularCourseScope">
 	
+	<fr:edit id="executionYear"
+			 name="executionDegreeBean"
+			 type="net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.lists.ExecutionDegreeListBean"
+			 schema="choose.curricularCourses">
+		<fr:destination name="executionYearPostBack" path="/chooseExecutionYearAndDegreeCurricularPlan.do?method=chooseDegreeCurricularPlan"/>
+	
+		<fr:layout name="tabular">
+			<fr:property name="classes" value="tstyle4 thlight thright mtop025"/>
+	        <fr:property name="columnClasses" value="width12em,,tdclear tderror1"/>
+		</fr:layout>
+	</fr:edit>
+	
+	<html:submit><bean:message key="button.submit" bundle="ACADEMIC_OFFICE_RESOURCES"/></html:submit>		
 
-		<tr>
-			<td>
-				<bean:message key="label.curriculumHistoric.chooseExecutionYear" bundle="CURRICULUM_HISTORIC_RESOURCES"/>:
-			</td>
-			<td>
-				<html:select bundle="HTMLALT_RESOURCES" property="executionYearID" onchange="this.form.submit()">
-					<option value=""><bean:message key="label.curriculumHistoric.chooseExecutionYear" bundle="CURRICULUM_HISTORIC_RESOURCES"/></option>
-					<logic:notEmpty name="executionYears">
-						<html:options collection="executionYears" property="idInternal" labelProperty="year" />
-					</logic:notEmpty>
-				</html:select>
-			</td>
-		</tr>
-		<tr>
-			<td></td>
-			<td>
-				<html:submit styleId="javascriptButtonID" styleClass="altJavaScriptSubmitButton" bundle="HTMLALT_RESOURCES" altKey="submit.submit">
-					<bean:message key="button.submit"/>
-				</html:submit>
-			</td>
-		</tr>
-
-</html:form>
-
-<html:form action="/chooseExecutionYearAndDegreeCurricularPlan">
-	<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.method" property="method" value="showActiveCurricularCourseScope"/>
-	<bean:define id="executionYearID" name="executionYearDegreeCurricularPlanForm" property="executionYearID"/>
-	<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.executionYearID" property="executionYearID" value="<%= executionYearID.toString() %>"/>
-	<logic:empty name="degreeCurricularPlans">
-		<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.page" property="page" value="1"/>
-	</logic:empty>
-	<logic:notEmpty name="degreeCurricularPlans">
-		<html:hidden bundle="HTMLALT_RESOURCES" altKey="hidden.page" property="page" value="2"/>
-
-
-			<tr>
-				<td>
-					<bean:message key="label.curriculumHistoric.chooseDegreeCurricularPlan" bundle="CURRICULUM_HISTORIC_RESOURCES"/>:
-				</td>
-			<td>
-				<html:select bundle="HTMLALT_RESOURCES" property="degreeCurricularPlanID">
-					<html:options collection="degreeCurricularPlans" property="value" labelProperty="label" />
-				</html:select>
-				</td>
-			</tr>
-
-	</logic:notEmpty>
-
-	<tr>
-		<td>
-			<p>
-				<html:submit bundle="HTMLALT_RESOURCES" altKey="submit.submit"><bean:message key="button.continue" bundle="CURRICULUM_HISTORIC_RESOURCES"/></html:submit>
-			</p>
-		</td>
-	</tr>
-</html:form>
-
-</table>
+</fr:form>
--- src/net/sourceforge/fenixedu/presentationTier/Action/commons/curriculumHistoric/DegreeCurricularPlanExecutionYearDispacthAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/commons/curriculumHistoric/DegreeCurricularPlanExecutionYearDispacthAction.java
@@ -3,34 +3,24 @@
  */
 package net.sourceforge.fenixedu.presentationTier.Action.commons.curriculumHistoric;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.SortedSet;
-import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
+import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.lists.ExecutionDegreeListBean;
 import net.sourceforge.fenixedu.domain.DegreeModuleScope;
-import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
 import net.sourceforge.fenixedu.presentationTier.Action.exceptions.FenixActionException;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.ServiceUtils;
-import net.sourceforge.fenixedu.presentationTier.Action.sop.utils.SessionUtils;
+import net.sourceforge.fenixedu.renderers.utils.RenderUtils;
 
-import org.apache.commons.beanutils.BeanComparator;
-import org.apache.commons.collections.comparators.ComparatorChain;
 import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.DynaActionForm;
-import org.apache.struts.util.LabelValueBean;
 
 /**
  * @author nmgo
@@ -38,62 +28,36 @@ import org.apache.struts.util.LabelValueBean;
  */
 public class DegreeCurricularPlanExecutionYearDispacthAction extends FenixDispatchAction {
 
-    public ActionForward prepare(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws FenixActionException, FenixFilterException {
-        IUserView userView = SessionUtils.getUserView(request);
-        List executionYears = null;
-        try {
-            executionYears = (List) ServiceUtils.executeService(userView, "ReadNotClosedExecutionYears",
-                    new Object[] {});
-        } catch (FenixServiceException e) {
-            throw new FenixActionException(e);
-        }
-        request.setAttribute("executionYears", executionYears);
-        request.setAttribute("degreeCurricularPlans", null);
-        return mapping.findForward("chooseExecutionYear");
+    public ActionForward prepare(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request, final HttpServletResponse response) {
+	request.setAttribute("executionDegreeBean", new ExecutionDegreeListBean());
+
+	return mapping.findForward("chooseExecutionYear");
     }
 
-    public ActionForward chooseDegreeCurricularPlan(final ActionMapping mapping, final ActionForm form,
-	    final HttpServletRequest request, final HttpServletResponse response)
-	    throws FenixFilterException, FenixServiceException {
+    public ActionForward chooseDegreeCurricularPlan(final ActionMapping mapping, final ActionForm form, final HttpServletRequest request, final HttpServletResponse response) {
+	final ExecutionDegreeListBean executionDegreeBean = (ExecutionDegreeListBean) getRenderedObject();
+	executionDegreeBean.setDegreeCurricularPlan(null);
 
-	final String executionYearOID = (String) ((DynaActionForm) form).get("executionYearID");
-        final ExecutionYear executionYear = rootDomainObject.readExecutionYearByOID(Integer.valueOf(executionYearOID));
-        final IUserView userView = SessionUtils.getUserView(request);
-        
-        final ComparatorChain comparatorChain = new ComparatorChain();
-        comparatorChain.addComparator(new BeanComparator("degree.degreeType"));
-        comparatorChain.addComparator(new BeanComparator("degree.name"));
-        comparatorChain.addComparator(new BeanComparator("name"));
-        
-        final SortedSet<DegreeCurricularPlan> degreeCurricularPlans = new TreeSet<DegreeCurricularPlan>(comparatorChain);
-        degreeCurricularPlans.addAll(executionYear.getDegreeCurricularPlans());
+	RenderUtils.invalidateViewState();
 
-        final List<LabelValueBean> labelValueDegreeCurricularPlans = new ArrayList<LabelValueBean>(degreeCurricularPlans.size()); 
-        for (final DegreeCurricularPlan degreeCurricularPlan : degreeCurricularPlans) {
-            labelValueDegreeCurricularPlans.add(
-        	    new LabelValueBean(
-        		    degreeCurricularPlan.getDegree().getName() + " - " + degreeCurricularPlan.getName(),
-        		    degreeCurricularPlan.getIdInternal().toString()));
-	}
-        
-        request.setAttribute("degreeCurricularPlans", labelValueDegreeCurricularPlans);
-        request.setAttribute("executionYears", ServiceUtils.executeService(userView, "ReadNotClosedExecutionYears", new Object[] {}));
+	request.setAttribute("executionDegreeBean", executionDegreeBean);
 
         return mapping.findForward("chooseExecutionYear");
     }
 
+    @SuppressWarnings("unchecked")
     public ActionForward showActiveCurricularCourseScope(ActionMapping mapping, ActionForm form,
             HttpServletRequest request, HttpServletResponse response) throws FenixActionException, FenixFilterException, FenixServiceException {
 
-        DynaActionForm actionForm = (DynaActionForm) form;
-        String executionYearID = (String) actionForm.get("executionYearID");
-        request.setAttribute("executionYearID", executionYearID);
-        String degreeCurricularPlanID = (String) actionForm.get("degreeCurricularPlanID");
-        Object[] args = { Integer.valueOf(degreeCurricularPlanID), Integer.valueOf(executionYearID) };
+	final ExecutionDegreeListBean executionDegreeBean = (ExecutionDegreeListBean) getRenderedObject("executionYear");
 
-        final SortedSet<DegreeModuleScope> degreeModuleScopes = (SortedSet<DegreeModuleScope>)
+	final Object[] args = {
+			executionDegreeBean.getDegreeCurricularPlan().getIdInternal(),
+			executionDegreeBean.getExecutionYear().getIdInternal() };
+
+	final SortedSet<DegreeModuleScope> degreeModuleScopes = (SortedSet<DegreeModuleScope>)
                 executeService(request, "ReadActiveCurricularCourseScopeByDegreeCurricularPlanAndExecutionYear", args);
+
         final ActionErrors errors = new ActionErrors();
         if (degreeModuleScopes.isEmpty()) {
             errors.add("noDegreeCurricularPlan", new ActionError("error.nonExisting.AssociatedCurricularCourses"));
@@ -102,7 +66,8 @@ public class DegreeCurricularPlanExecutionYearDispacthAction extends FenixDispat
             request.setAttribute("degreeModuleScopes", degreeModuleScopes);
         }
 
-        request.setAttribute("degreeCurricularPlan", rootDomainObject.readDegreeCurricularPlanByOID(Integer.valueOf(degreeCurricularPlanID)));
+        request.setAttribute("executionYearID", executionDegreeBean.getExecutionYear().getIdInternal());
+        request.setAttribute("degreeCurricularPlan", executionDegreeBean.getDegreeCurricularPlan());
 
         return mapping.findForward("showActiveCurricularCourses");
     }
