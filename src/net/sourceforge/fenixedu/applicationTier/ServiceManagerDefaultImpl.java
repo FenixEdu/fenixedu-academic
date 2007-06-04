da83ec818fe3e380da743fac2ef006a58b030c15
Luis Cruz <luis.cruz@ist.utl.pt>
2007-06-04 13:50:25 +0000

    Show log of long running transaction.

--- src/net/sourceforge/fenixedu/applicationTier/ServiceManagerDefaultImpl.java
+++ src/net/sourceforge/fenixedu/applicationTier/ServiceManagerDefaultImpl.java
@@ -175,6 +175,9 @@ public class ServiceManagerDefaultImpl implements IServiceManagerWrapper {
         } catch (ServiceManagerException e) {
             throw new ExceptionWrapper(e);
         } catch (Throwable t) {
+            if (t.getMessage().indexOf("JVSTM:") >= 0) {
+        	t.printStackTrace();
+            }
             throw new ExceptionWrapper(t);
         }
     }
