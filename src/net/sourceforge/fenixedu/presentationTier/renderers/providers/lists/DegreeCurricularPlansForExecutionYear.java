0a4f4ee06a901da9a05280ce3fe94021c068e01f
Luís Egídio <luis.egidio@ist.utl.pt>
2007-06-04 14:05:06 +0000

    if logged person is not administrative office employee, show all degree curricular plans

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/lists/DegreeCurricularPlansForExecutionYear.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/lists/DegreeCurricularPlansForExecutionYear.java
@@ -1,13 +1,14 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers.lists;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.lists.ExecutionDegreeListBean;
 import net.sourceforge.fenixedu.domain.Degree;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
 import net.sourceforge.fenixedu.renderers.DataProvider;
@@ -15,32 +16,34 @@ import net.sourceforge.fenixedu.renderers.components.converters.Converter;
 
 public class DegreeCurricularPlansForExecutionYear implements DataProvider {
 
-	public Object provide(Object source, Object currentValue) {
+    public Object provide(Object source, Object currentValue) {
+	final SortedSet<DegreeCurricularPlan> result = new TreeSet<DegreeCurricularPlan>(DegreeCurricularPlan.DEGREE_CURRICULAR_PLAN_COMPARATOR_BY_DEGREE_TYPE_AND_EXECUTION_DEGREE_AND_DEGREE_CODE);
 
-		final ExecutionDegreeListBean executionDegreeBean = (ExecutionDegreeListBean) source;
-		final List<DegreeCurricularPlan> degreeCurricularPlans = new ArrayList<DegreeCurricularPlan>();
+	final ExecutionDegreeListBean executionDegreeBean = (ExecutionDegreeListBean) source;
+	if (executionDegreeBean.getExecutionYear() != null) {
+	    final Person person = AccessControl.getPerson();
 
-	
-		if (executionDegreeBean.getExecutionYear() != null) {
-			final Set<Degree> degrees = AccessControl.getPerson().getEmployee().getAdministrativeOffice().getAdministratedDegrees();
-			
-			for (DegreeCurricularPlan plan : executionDegreeBean.getExecutionYear()
-					.getDegreeCurricularPlans()) {
-				if (degrees.contains(plan.getDegree())){
-					degreeCurricularPlans.add(plan);
-				}
-			}
-			
-			Collections.sort(degreeCurricularPlans,DegreeCurricularPlan.DEGREE_CURRICULAR_PLAN_COMPARATOR_BY_DEGREE_TYPE_AND_EXECUTION_DEGREE_AND_DEGREE_CODE);
-		} else {
-			executionDegreeBean.setDegreeCurricularPlan(null);
+	    final Set<Degree> degrees;
+	    if (person.isAdministrativeOfficeEmployee()) {
+		degrees = person.getEmployee().getAdministrativeOffice().getAdministratedDegrees();
+	    } else {
+		degrees = RootDomainObject.getInstance().getDegreesSet();
+	    }
+
+	    for (DegreeCurricularPlan plan : executionDegreeBean.getExecutionYear().getDegreeCurricularPlans()) {
+		if (degrees.contains(plan.getDegree())) {
+		    result.add(plan);
 		}
-		
-		return degreeCurricularPlans;
+	    }
+	} else {
+	    executionDegreeBean.setDegreeCurricularPlan(null);
 	}
 
-	public Converter getConverter() {
-		return new DomainObjectKeyConverter();
-	}
+	return result;
+    }
+
+    public Converter getConverter() {
+	return new DomainObjectKeyConverter();
+    }
 
 }
