d5b74231b4f34eb097e648349473823dfa3d97b0
Luis Cruz <luis.cruz@ist.utl.pt>
2007-06-04 15:27:48 +0000

    Add more maintnence stuff.

---
+++ etc/database_operations/alterIST_UsernameAndOtherStuff.sql
@@ -0,0 +1,6 @@
+update USER set USER_U_ID = 'ist24661' where USER_U_ID like 'ist10482704';
+update LOGIN_ALIAS set ALIAS = 'ist24661' where ALIAS like 'ist10482704';
+update IDENTIFICATION set IS_PASS_IN_KERBEROS = 0 where ID_INTERNAL = (select KEY_LOGIN from LOGIN_ALIAS where ALIAS like 'ist24661');
+
+delete from FORUM where FORUM.ID_INTERNAL = 5640;
+delete from FORUM_SUBSCRIPTION where FORUM_SUBSCRIPTION.KEY_FORUM = 5640;
