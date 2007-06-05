731aacf37c606ba50ee3aa5cbe0be28c3be94269
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2007-06-05 09:50:54 +0000

    Allowed researchers with website manager to create persisten group members also.

--- src/net/sourceforge/fenixedu/predicates/PersistentGroupMembersPredicates.java
+++ src/net/sourceforge/fenixedu/predicates/PersistentGroupMembersPredicates.java
@@ -1,5 +1,6 @@
 package net.sourceforge.fenixedu.predicates;
 
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.accessControl.PersistentGroupMembers;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
@@ -7,9 +8,12 @@ import net.sourceforge.fenixedu.injectionCode.AccessControlPredicate;
 
 public class PersistentGroupMembersPredicates {
 
-    public static final AccessControlPredicate<PersistentGroupMembers> checkPermissionsToManagePersistentGroups = new AccessControlPredicate<PersistentGroupMembers>() {
-	public boolean evaluate(PersistentGroupMembers persistentGroupMembers) {	    
-	    return AccessControl.getPerson().hasRole(RoleType.MANAGER);
-	}
-    };    
+	public static final AccessControlPredicate<PersistentGroupMembers> checkPermissionsToManagePersistentGroups = new AccessControlPredicate<PersistentGroupMembers>() {
+		public boolean evaluate(PersistentGroupMembers persistentGroupMembers) {
+			Person person = AccessControl.getPerson();
+
+			return person.hasRole(RoleType.MANAGER)
+					|| (person.hasRole(RoleType.RESEARCHER) && person.hasRole(RoleType.WEBSITE_MANAGER));
+		}
+	};
 }
