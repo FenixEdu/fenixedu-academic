c9ade9b661d059781e0c5954dea0692ecaecbaf1
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2007-06-08 12:06:39 +0000

    added a new justification group

--- src/net/sourceforge/fenixedu/domain/assiduousness/util/JustificationGroup.java
+++ src/net/sourceforge/fenixedu/domain/assiduousness/util/JustificationGroup.java
@@ -3,5 +3,6 @@ package net.sourceforge.fenixedu.domain.assiduousness.util;
 public enum JustificationGroup {
     SICKNESS,
     CURRENT_YEAR_HOLIDAYS,
-    LAST_YEAR_HOLIDAYS
+    LAST_YEAR_HOLIDAYS,
+    NEXT_YEAR_HOLIDAYS
 }
