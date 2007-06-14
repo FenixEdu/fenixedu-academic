f95d22943a4b85d1a43d5eef51a175686a5e0fbc
Luis Cruz <luis.cruz@ist.utl.pt>
2007-06-14 15:15:26 +0000

    Fix table types.

---
+++ etc/database_operations/fixTableTypes.sql
@@ -0,0 +1,8 @@
+alter table COMPETENCE_COURSE type = InnoDB;
+-- alter table DEGREE_INFO type = InnoDB;
+alter table FUNCTIONALITY_PRINTERS type = InnoDB;
+alter table REGISTRATION_NUMBER type = InnoDB;
+alter table SCIENTIFIC_COMMISSION type = InnoDB;
+alter table THESIS_EVALUATION_PARTICIPANT type = InnoDB;
+alter table UNIT_FUNCTIONALITY_PRINTERS type = InnoDB;
+alter table USERNAME_COUNTER type = InnoDB;
