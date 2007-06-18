03cac519380aaf3afcfc91112d838b8ce255db15
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-06-18 16:05:26 +0000

    added date columns to justifications file

--- src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/ExportJustifications.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/ExportJustifications.java
@@ -4,19 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
-
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.dataTransferObject.assiduousness.AssiduousnessExportChoices;
-import net.sourceforge.fenixedu.dataTransferObject.assiduousness.AssiduousnessMonthlyResume;
-import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.assiduousness.Assiduousness;
 import net.sourceforge.fenixedu.domain.assiduousness.Justification;
 import net.sourceforge.fenixedu.domain.assiduousness.Leave;
 import net.sourceforge.fenixedu.domain.assiduousness.util.JustificationType;
 import net.sourceforge.fenixedu.util.LanguageUtils;
 import net.sourceforge.fenixedu.util.report.StyledExcelSpreadsheet;
+
+import org.joda.time.YearMonthDay;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class ExportJustifications extends Service {
     private static final DateTimeFormatter dateFormat = DateTimeFormat.forPattern("dd/MM/yyyy");
@@ -35,14 +34,14 @@ public class ExportJustifications extends Service {
 		.getString("link.justifications"));
 	HashMap<Assiduousness, List<Justification>> justificationsMap = assiduousnessExportChoices
 		.getAllJustificationMap();
-
 	spreadsheet.newHeaderRow();
 	spreadsheet.addHeader(bundle.getString("label.employeeNumber"));
 	spreadsheet.addHeader(bundle.getString("label.beginDate"));
 	spreadsheet.addHeader(bundle.getString("label.endDateOrDuration"));
 	spreadsheet.addHeader(bundle.getString("label.acronym"));
 	spreadsheet.addHeader(bundle.getString("label.type"));
-
+	spreadsheet.addHeader(bundle.getString("label.beginDate"));
+	spreadsheet.addHeader(bundle.getString("label.endDate"));
 	for (Assiduousness assiduousness : rootDomainObject.getAssiduousnesss()) {
 	    if (assiduousnessExportChoices.satisfiedAll(assiduousness)) {
 		List<Justification> justificationList = justificationsMap.get(assiduousness);
@@ -69,6 +68,25 @@ public class ExportJustifications extends Service {
 			    spreadsheet.addCell(justification.getJustificationMotive().getAcronym());
 			    spreadsheet.addCell(enumBundle.getString(justification
 				    .getJustificationMotive().getJustificationType().toString()));
+			    YearMonthDay begin = assiduousnessExportChoices.getBeginDate().isAfter(
+				    justification.getDate().toYearMonthDay()) ? assiduousnessExportChoices
+				    .getBeginDate()
+				    : justification.getDate().toYearMonthDay();
+			    YearMonthDay justificationDate = ((Leave) justification)
+				    .getEndYearMonthDay() == null ? ((Leave) justification).getDate()
+				    .toYearMonthDay() : ((Leave) justification).getEndYearMonthDay();
+			    YearMonthDay end = assiduousnessExportChoices.getEndDate().isBefore(
+				    justificationDate) ? assiduousnessExportChoices.getEndDate()
+				    : justificationDate;
+			    spreadsheet.addCell(dateFormat.print(begin));
+			    spreadsheet.addCell(dateFormat.print(end));
+			} else if (justification.isMissingClocking()) {
+			    spreadsheet.newRow();
+			    spreadsheet.addCell(justification.getAssiduousness().getEmployee()
+				    .getEmployeeNumber().toString());
+			    spreadsheet.addCell(dateTimeFormat.print(justification.getDate()));
+			    spreadsheet.addCell("");
+			    spreadsheet.addCell(justification.getJustificationMotive().getAcronym());
 			}
 		    }
 		}
