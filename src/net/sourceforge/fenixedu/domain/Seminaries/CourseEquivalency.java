b8c8bda49482a40a0a3e8618f6b11cdce7f26305
João Florindo <jdnf@linkare.pt>
2007-06-20 18:23:22 +0000

    added delete method

--- src/net/sourceforge/fenixedu/domain/Seminaries/CourseEquivalency.java
+++ src/net/sourceforge/fenixedu/domain/Seminaries/CourseEquivalency.java
@@ -7,19 +7,27 @@ package net.sourceforge.fenixedu.domain.Seminaries;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 
-
 /**
  * @author Goncalo Luiz gedl [AT] rnl [DOT] ist [DOT] utl [DOT] pt
  * 
  * 
  * Created at 31/Jul/2003, 9:20:49
- *  
+ * 
  */
 public class CourseEquivalency extends CourseEquivalency_Base {
 
-	public CourseEquivalency() {
-		super();
-		setRootDomainObject(RootDomainObject.getInstance());
-	}
+    public CourseEquivalency() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+    }
+    
+    public void delete() {
+	removeCurricularCourse();
+	removeModality();
+	removeSeminary();
+	getThemes().clear();
+	removeRootDomainObject();
+	super.deleteDomainObject();
+    }
 
 }
