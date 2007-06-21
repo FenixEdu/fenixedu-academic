d78406304285914c8104e5479ac7ac6ca93fcdf5
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2007-06-21 15:01:32 +0000

    added readBySemesterAndYear method

--- src/net/sourceforge/fenixedu/domain/CurricularSemester.java
+++ src/net/sourceforge/fenixedu/domain/CurricularSemester.java
@@ -9,20 +9,29 @@ package net.sourceforge.fenixedu.domain;
 public class CurricularSemester extends CurricularSemester_Base implements Comparable<CurricularSemester> {
 
     public CurricularSemester() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
 
     public CurricularSemester(final CurricularYear curricularYear, final Integer semester) {
-        this();
-        setCurricularYear(curricularYear);
-        setSemester(semester);
+	this();
+	setCurricularYear(curricularYear);
+	setSemester(semester);
     }
 
-	public int compareTo(final CurricularSemester curricularSemester) {
-		return getCurricularYear() == curricularSemester.getCurricularYear() ?
-				getSemester().compareTo(curricularSemester.getSemester()) :
-				getCurricularYear().compareTo(curricularSemester.getCurricularYear());
+    public int compareTo(final CurricularSemester curricularSemester) {
+	return getCurricularYear() == curricularSemester.getCurricularYear() ?
+		getSemester().compareTo(curricularSemester.getSemester()) :
+		    getCurricularYear().compareTo(curricularSemester.getCurricularYear());
+    }
+
+    public static CurricularSemester readBySemesterAndYear(final Integer semester, final Integer year) {
+	for (CurricularSemester curricularSemester : RootDomainObject.getInstance().getCurricularSemesters()) {
+	    if(curricularSemester.getSemester().equals(semester) && curricularSemester.getCurricularYear().getYear().equals(year)) {
+		return curricularSemester;
+	    }
 	}
+	return null;
+    }
     
 }
