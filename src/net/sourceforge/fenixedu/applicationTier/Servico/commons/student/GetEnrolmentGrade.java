342c97351b5c13fb21e6f0ac287261e527384d33
Luís Egídio <luis.egidio@ist.utl.pt>
2007-07-03 13:16:19 +0000

    remove old code, using domain logic

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/GetEnrolmentGrade.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/GetEnrolmentGrade.java
@@ -1,14 +1,9 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.commons.student;
 
-import java.util.Collections;
-import java.util.List;
-
 import net.sourceforge.fenixedu.dataTransferObject.InfoEnrolmentEvaluation;
 import net.sourceforge.fenixedu.dataTransferObject.InfoPerson;
-import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.EnrolmentEvaluation;
-import net.sourceforge.fenixedu.domain.degree.DegreeType;
 
 /**
  * @author Nuno Nunes (nmsn@rnl.ist.utl.pt)
@@ -16,39 +11,28 @@ import net.sourceforge.fenixedu.domain.degree.DegreeType;
  */
 public class GetEnrolmentGrade {
 
-    public InfoEnrolmentEvaluation run(Enrolment enrolment) {
-
+    final public InfoEnrolmentEvaluation run(final Enrolment enrolment) {
 	if (enrolment == null) {
 	    return null;
 	}
-	final DegreeCurricularPlan degreeCurricularPlan = enrolment.getStudentCurricularPlan()
-		.getDegreeCurricularPlan();
-	return (degreeCurricularPlan.getDegree().getDegreeType() == DegreeType.DEGREE) ? run(enrolment
-		.getAllFinalEnrolmentEvaluations()) : run(enrolment.getEvaluations());
+
+	return getInfoLatestEvaluation(enrolment.getLatestEnrolmentEvaluation());
     }
 
-    private InfoEnrolmentEvaluation run(List<EnrolmentEvaluation> enrolmentEvaluations) {
-	if (enrolmentEvaluations == null || enrolmentEvaluations.isEmpty()) {
-	    return null;
-	}
-	final EnrolmentEvaluation evaluation = (EnrolmentEvaluation) Collections
-		.max(enrolmentEvaluations);
-	return getInfoLatestEvaluation(evaluation);
-    }
-
-    private InfoEnrolmentEvaluation getInfoLatestEvaluation(EnrolmentEvaluation latestEvaluation) {
+    final private InfoEnrolmentEvaluation getInfoLatestEvaluation(final EnrolmentEvaluation latestEvaluation) {
 	
 	final InfoEnrolmentEvaluation infolatestEvaluation = InfoEnrolmentEvaluation.newInfoFromDomain(latestEvaluation);
 	if (latestEvaluation.hasEmployee()) {
 	    if (latestEvaluation.getEmployee().hasPerson()) {
-		infolatestEvaluation.setInfoEmployee(InfoPerson.newInfoFromDomain(latestEvaluation
-			.getEmployee().getPerson()));
+		infolatestEvaluation.setInfoEmployee(InfoPerson.newInfoFromDomain(latestEvaluation.getEmployee().getPerson()));
 	    }
+	    
 	    if (latestEvaluation.hasPersonResponsibleForGrade()) {
-		infolatestEvaluation.setInfoPersonResponsibleForGrade(InfoPerson
-			.newInfoFromDomain(latestEvaluation.getPersonResponsibleForGrade()));
+		infolatestEvaluation.setInfoPersonResponsibleForGrade(InfoPerson.newInfoFromDomain(latestEvaluation.getPersonResponsibleForGrade()));
 	    }
 	}
+	
 	return infolatestEvaluation;
     }
+    
 }
