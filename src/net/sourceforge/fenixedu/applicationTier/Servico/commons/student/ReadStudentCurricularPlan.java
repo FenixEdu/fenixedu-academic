6697891dfc33bef56f2089749f15f6217f002f1a
Luís Egídio <luis.egidio@ist.utl.pt>
2007-07-04 14:07:20 +0000

    refactored

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/ReadStudentCurricularPlan.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/ReadStudentCurricularPlan.java
@@ -8,34 +8,24 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.commons.student;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
-import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
-import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class ReadStudentCurricularPlan extends Service {
 
-    public InfoStudentCurricularPlan run(Integer studentCurricularPlanID) throws ExcepcaoInexistente,
-            FenixServiceException, ExcepcaoPersistencia {
+    final public InfoStudentCurricularPlan run(final Integer studentCurricularPlanID) throws FenixServiceException {
         if (studentCurricularPlanID == null) {
-            FenixServiceException newEx = new FenixServiceException("Persistence layer error");
-            newEx.fillInStackTrace();
-            throw newEx;
+            throw new FenixServiceException("Persistence layer error");
         }
 
-        StudentCurricularPlan studentCurricularPlan = null;
-
-        // The student Curricular plan
-        studentCurricularPlan = rootDomainObject.readStudentCurricularPlanByOID(studentCurricularPlanID);
-
+        final StudentCurricularPlan studentCurricularPlan = rootDomainObject.readStudentCurricularPlanByOID(studentCurricularPlanID);
         if (studentCurricularPlan == null) {
             throw new NonExistingServiceException();
         }
 
-        return InfoStudentCurricularPlan
-                .newInfoFromDomain(studentCurricularPlan);
+        return InfoStudentCurricularPlan.newInfoFromDomain(studentCurricularPlan);
     }
+
 }
