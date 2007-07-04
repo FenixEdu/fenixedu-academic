e923632e303b353782bcc83d2934133499932cbd
Shezad Anavarali <shezad@ist.utl.pt>
2007-07-04 14:52:55 +0000

    master degree exemptions now supports many registrations

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/ReadStudentCurricularPlansByNumberAndDegreeType.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/student/ReadStudentCurricularPlansByNumberAndDegreeType.java
@@ -8,43 +8,36 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.commons.student;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
-import net.sourceforge.fenixedu.applicationTier.Servico.ExcepcaoInexistente;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
-import net.sourceforge.fenixedu.dataTransferObject.InfoStudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.domain.student.Registration;
-import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class ReadStudentCurricularPlansByNumberAndDegreeType extends Service {
 
-    public List run(Integer studentNumber, DegreeType degreeType) throws ExcepcaoInexistente,
-            FenixServiceException, ExcepcaoPersistencia {
-    	Registration registration = Registration.readStudentByNumberAndDegreeType(studentNumber, degreeType);
-    	if(registration == null) {
-    		throw new NonExistingServiceException();
-    	}
-    	List<StudentCurricularPlan> studentCurricularPlans = registration.getStudentCurricularPlans(); 
+    public List<StudentCurricularPlan> run(Integer studentNumber, DegreeType degreeType)
+	    throws NonExistingServiceException {
 
-        if ((studentCurricularPlans == null) || (studentCurricularPlans.size() == 0)) {
-            throw new NonExistingServiceException();
-        }
+	List<Registration> registrations = Registration.readByNumberAndDegreeType(studentNumber,
+		degreeType);
 
-        Iterator iterator = studentCurricularPlans.iterator();
-        List result = new ArrayList();
+	if (registrations.isEmpty()) {
+	    throw new NonExistingServiceException();
+	}
 
-        while (iterator.hasNext()) {
-            StudentCurricularPlan studentCurricularPlan = (StudentCurricularPlan) iterator.next();
-            result
-                    .add(InfoStudentCurricularPlan
-                            .newInfoFromDomain(studentCurricularPlan));
-        }
+	List<StudentCurricularPlan> studentCurricularPlans = new ArrayList<StudentCurricularPlan>();
 
-        return result;
+	for (final Registration registration : registrations) {
+	    studentCurricularPlans.addAll(registration.getStudentCurricularPlans());
+	}
+
+	if (studentCurricularPlans.isEmpty()) {
+	    throw new NonExistingServiceException();
+	}
+
+	return studentCurricularPlans;
     }
 }
