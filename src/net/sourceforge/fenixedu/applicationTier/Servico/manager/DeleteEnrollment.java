b0a2e07244b76c3b8ce066e75c6086d062a92474
Shezad Anavarali <shezad@ist.utl.pt>
2007-07-04 14:56:10 +0000

    force enrolments delete

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteEnrollment.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteEnrollment.java
@@ -6,8 +6,11 @@ package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.EnrolmentEvaluation;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumGroup;
+import net.sourceforge.fenixedu.util.EnrolmentEvaluationState;
 
 /**
  * @author Luis Cruz
@@ -16,11 +19,25 @@ import net.sourceforge.fenixedu.domain.student.Registration;
 public class DeleteEnrollment extends Service {
 
     public void run(final Integer studentNumber, final DegreeType degreeType, final Integer enrollmentId) {
-        final Registration registration = Registration.readStudentByNumberAndDegreeType(studentNumber, degreeType);
-        final Enrolment enrollment = registration.findEnrolmentByEnrolmentID(enrollmentId);
-        if (enrollment != null) {            
-            enrollment.delete();
-        }
+	for (Registration registration : Registration.readByNumberAndDegreeType(studentNumber,
+		degreeType)) {
+	    final Enrolment enrollment = registration.findEnrolmentByEnrolmentID(enrollmentId);
+	    if (enrollment != null) {
+		for (EnrolmentEvaluation evaluation : enrollment.getEvaluations()) {
+		    evaluation.setEnrolmentEvaluationState(EnrolmentEvaluationState.TEMPORARY_OBJ);
+		}
+		
+		final CurriculumGroup parentCurriculumGroup = enrollment.getCurriculumGroup();
+		
+		enrollment.delete();
+		
+		if(parentCurriculumGroup != null && parentCurriculumGroup.canBeDeleted()){
+		    parentCurriculumGroup.delete();
+		}
+		
+		return;
+	    }
+	}
     }
 
 }
--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteStudentCurricularPlan.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteStudentCurricularPlan.java
@@ -6,9 +6,12 @@ package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
+import net.sourceforge.fenixedu.domain.Enrolment;
+import net.sourceforge.fenixedu.domain.EnrolmentEvaluation;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
+import net.sourceforge.fenixedu.util.EnrolmentEvaluationState;
 
 /**
  * @author Luis Cruz
@@ -16,12 +19,22 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
  */
 public class DeleteStudentCurricularPlan extends Service {
 
-    public void run(final Integer studentCurricularPlanId) throws DomainException, ExcepcaoPersistencia, NonExistingServiceException {
-        final StudentCurricularPlan studentCurricularPlan = rootDomainObject.readStudentCurricularPlanByOID(studentCurricularPlanId);
+    public void run(final Integer studentCurricularPlanId) throws DomainException, ExcepcaoPersistencia,
+	    NonExistingServiceException {
+	final StudentCurricularPlan studentCurricularPlan = rootDomainObject
+		.readStudentCurricularPlanByOID(studentCurricularPlanId);
 
-		if (studentCurricularPlan != null)
-			studentCurricularPlan.delete();
-		else
-			throw new NonExistingServiceException();
+	if (studentCurricularPlan != null) {
+	    
+	    for (Enrolment enrolment : studentCurricularPlan.getEnrolments()) {
+		for (EnrolmentEvaluation evaluation : enrolment.getEvaluations()) {
+		    evaluation.setEnrolmentEvaluationState(EnrolmentEvaluationState.TEMPORARY_OBJ);
+		}
+	    }
+	    
+	    studentCurricularPlan.delete();
+	} else {
+	    throw new NonExistingServiceException();
+	}
     }
 }
