9b626f78b4232ac30e31563a932dab6f0e73dd42
Shezad Anavarali <shezad@ist.utl.pt>
2007-07-09 14:30:38 +0000

    bug fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/TransferEnrollments.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/TransferEnrollments.java
@@ -1,6 +1,7 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.Enrolment;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumGroup;
@@ -14,9 +15,14 @@ public class TransferEnrollments extends Service {
 
 	    CurriculumGroup curriculumGroup = (CurriculumGroup) rootDomainObject
 		    .readCurriculumModuleByOID(destinationCurriculumGroupID);
+	    StudentCurricularPlan studentCurricularPlan = curriculumGroup.getStudentCurricularPlan();
+
 	    for (final Integer enrollmentIDToTransfer : enrollmentIDsToTransfer) {
 		Enrolment enrolment = (Enrolment) rootDomainObject
 			.readCurriculumModuleByOID(enrollmentIDToTransfer);
+
+		fixEnrolmentCurricularCourse(studentCurricularPlan, enrolment);
+
 		enrolment.setCurriculumGroup(curriculumGroup);
 		enrolment.removeStudentCurricularPlan();
 	    }
@@ -27,12 +33,27 @@ public class TransferEnrollments extends Service {
 		    .readStudentCurricularPlanByOID(destinationStudentCurricularPlanId);
 	    for (final Integer enrollmentIDToTransfer : enrollmentIDsToTransfer) {
 		final Enrolment enrollment = (Enrolment) rootDomainObject
-			.readCurriculumModuleByOID(enrollmentIDToTransfer);		
+			.readCurriculumModuleByOID(enrollmentIDToTransfer);
+
+		fixEnrolmentCurricularCourse(studentCurricularPlan, enrollment);
+
 		if (enrollment.getStudentCurricularPlan() != studentCurricularPlan) {
 		    enrollment.setStudentCurricularPlan(studentCurricularPlan);
 		    enrollment.removeCurriculumGroup();
 		}
 
+	    }
+	}
+    }
+
+    private void fixEnrolmentCurricularCourse(final StudentCurricularPlan studentCurricularPlan,
+	    final Enrolment enrollment) {
+	if (enrollment.getCurricularCourse().getDegreeCurricularPlan() != studentCurricularPlan
+		.getDegreeCurricularPlan()) {
+	    CurricularCourse curricularCourse = studentCurricularPlan.getDegreeCurricularPlan()
+		    .getCurricularCourseByCode(enrollment.getCurricularCourse().getCode());
+	    if (curricularCourse != null) {
+		enrollment.setCurricularCourse(curricularCourse);
 	    }
 	}
     }
