c72ddf97924544b3d62d54f027add696f83cff7a
Luís Egídio <luis.egidio@ist.utl.pt>
2007-07-13 18:07:27 +0000

    mark sheet management for Tagus only after 2 Semester of 2006/2007

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DegreeCurricularPlansForDegree.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/DegreeCurricularPlansForDegree.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.degreeAdministrativeOffice.gradeSubmission.MarkSheetManagementBaseBean;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
+import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.space.Campus;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
@@ -24,13 +25,29 @@ public class DegreeCurricularPlansForDegree implements DataProvider {
 		&& markSheetManagementBean.getExecutionPeriod() != null) {
 	    Campus employeeCampus = getEmployeeCampus();
 	    if (employeeCampus != null) {
-		for (DegreeCurricularPlan degreeCurricularPlan : markSheetManagementBean.getDegree()
-			.getDegreeCurricularPlansSet()) {
-		    if (degreeCurricularPlan.getExecutionDegreeByYearAndCampus(markSheetManagementBean
-			    .getExecutionPeriod().getExecutionYear(), employeeCampus) != null) {
-			result.add(degreeCurricularPlan);
+		ExecutionPeriod executionPeriod = ExecutionPeriod.readBySemesterAndExecutionYear(1, "2006/2007");
+		
+		if(markSheetManagementBean.getExecutionPeriod().isBeforeOrEquals(executionPeriod)) {
+		    
+		    if(!employeeCampus.getName().equalsIgnoreCase("Taguspark")) {
+			for (DegreeCurricularPlan degreeCurricularPlan : markSheetManagementBean.getDegree()
+				.getDegreeCurricularPlansSet()) {
+			    if (degreeCurricularPlan.getExecutionDegreeByYear(markSheetManagementBean
+				    .getExecutionPeriod().getExecutionYear()) != null) {
+				result.add(degreeCurricularPlan);
+			    }
+			}
 		    }
-		}
+		    
+		} else {
+		    for (DegreeCurricularPlan degreeCurricularPlan : markSheetManagementBean.getDegree()
+			    .getDegreeCurricularPlansSet()) {
+			if (degreeCurricularPlan.getExecutionDegreeByYearAndCampus(markSheetManagementBean
+				.getExecutionPeriod().getExecutionYear(), employeeCampus) != null) {
+			    result.add(degreeCurricularPlan);
+			}
+		    }		    
+		}		
 	    }
 	}
 	Collections.sort(result, new BeanComparator("name"));
