3c67dd35b4be1503215553fe5ffb01406f3ed589
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2007-07-17 13:44:55 +0000

    bug fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/UpdateExtraWorkClosedMonth.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/UpdateExtraWorkClosedMonth.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.assiduousness;
 
+import java.math.BigDecimal;
+
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.domain.assiduousness.ClosedMonth;
 import net.sourceforge.fenixedu.domain.assiduousness.ExtraWorkRequest;
@@ -13,7 +15,7 @@ import org.joda.time.DateTimeFieldType;
 public class UpdateExtraWorkClosedMonth extends Service {
 
     public ActionMessage run(ClosedMonth closedMonth) throws ExcepcaoPersistencia {
-        double totalMonthAmount = 0.0;
+        BigDecimal totalMonthAmount = new BigDecimal(0.0);
         GiafInterface giafInterface = new GiafInterface();
 	for (ExtraWorkRequest extraWorkRequest : rootDomainObject.getExtraWorkRequests()) {
 	    if (extraWorkRequest.getPartialPayingDate().equals(closedMonth.getClosedYearMonth())
@@ -26,14 +28,14 @@ public class UpdateExtraWorkClosedMonth extends Service {
 				    extraWorkRequest.getPartialPayingDate()
 					    .get(DateTimeFieldType.year()));
 		    unitExtraWorkAmount.updateValue(oldValue, extraWorkRequest.getAmount());
-                    totalMonthAmount += extraWorkRequest.getAmount();
+                    totalMonthAmount = totalMonthAmount.add(BigDecimal.valueOf(extraWorkRequest.getAmount()));
 		} catch (ExcepcaoPersistencia e) {
 		    return new ActionMessage("error.connectionError");
 		}
 	    }
 	}
         double giafTotalMonthAmount = giafInterface.getTotalMonthAmount(closedMonth.getClosedYearMonth());
-        if(totalMonthAmount != giafTotalMonthAmount) {
+        if(totalMonthAmount.doubleValue() != giafTotalMonthAmount) {
             return new ActionMessage("error.extraWork.totalMonthValueNotEqual", totalMonthAmount, giafTotalMonthAmount);
         }
 	return null;
