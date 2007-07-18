1b6ca74f413085727e00e3a17af44b3c4f34ed18
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-07-18 11:09:08 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/ExternalCurriculumGroup.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/ExternalCurriculumGroup.java
@@ -6,7 +6,6 @@ import net.sourceforge.fenixedu.domain.degreeStructure.CourseGroup;
 import net.sourceforge.fenixedu.domain.degreeStructure.CycleCourseGroup;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
-
 /**
  * 
  * @author - Shezad Anavarali (shezad@ist.utl.pt)
@@ -17,19 +16,27 @@ public class ExternalCurriculumGroup extends ExternalCurriculumGroup_Base {
     public ExternalCurriculumGroup() {
 	super();
     }
-    
+
     public ExternalCurriculumGroup(final RootCurriculumGroup rootCurriculumGroup,
 	    final CycleCourseGroup cycleCourseGroup, final ExecutionPeriod executionPeriod) {
 	this();
 	init(rootCurriculumGroup, cycleCourseGroup, executionPeriod);
     }
-    
+
     @Override
-    protected void checkInitConstraints(final StudentCurricularPlan studentCurricularPlan, final CourseGroup courseGroup) {
+    protected void checkInitConstraints(final StudentCurricularPlan studentCurricularPlan,
+	    final CourseGroup courseGroup) {
 	super.checkInitConstraints(studentCurricularPlan, courseGroup);
-	if (studentCurricularPlan.getDegreeCurricularPlan() == courseGroup.getParentDegreeCurricularPlan()) {
-	    throw new DomainException("error.studentCurriculum.CurriculumGroup.courseGroup.must.have.different.degreeCurricularPlan");
+	if (studentCurricularPlan.getDegreeCurricularPlan() == courseGroup
+		.getParentDegreeCurricularPlan()) {
+	    throw new DomainException(
+		    "error.studentCurriculum.CurriculumGroup.courseGroup.must.have.different.degreeCurricularPlan");
 	}
+    }
+
+    @Override
+    public Integer getChildOrder() {
+	return Integer.MAX_VALUE;
     }
 
 }
