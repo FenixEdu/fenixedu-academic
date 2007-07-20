83b3a2adee3397cabb44099598934d9dd6077cc8
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-07-20 19:50:17 +0000

    bug fix

--- src/net/sourceforge/fenixedu/predicates/StudentPredicates.java
+++ src/net/sourceforge/fenixedu/predicates/StudentPredicates.java
@@ -1,5 +1,6 @@
 package net.sourceforge.fenixedu.predicates;
 
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.student.Student;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
@@ -7,9 +8,10 @@ import net.sourceforge.fenixedu.injectionCode.AccessControlPredicate;
 
 public class StudentPredicates {
 
-    public static final AccessControlPredicate<Student> checkIfLoggedPersonIsStudentOwner = new AccessControlPredicate<Student>() {
+    public static final AccessControlPredicate<Student> checkIfLoggedPersonIsStudentOwnerOrManager = new AccessControlPredicate<Student>() {
 	public boolean evaluate(Student student) {
-	    return AccessControl.getPerson().getStudent() == student;
+	    final Person person = AccessControl.getPerson();
+	    return person.getStudent() == student || person.hasRole(RoleType.MANAGER);
 	}
     };
 
