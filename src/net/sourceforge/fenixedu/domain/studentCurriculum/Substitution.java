518d5b3f47811b4f4917fdb90de5617bcf22d49b
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-07-24 16:00:09 +0000

    removed grade calculation

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/Substitution.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/Substitution.java
@@ -1,10 +1,6 @@
 package net.sourceforge.fenixedu.domain.studentCurriculum;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Collection;
-
-import org.apache.commons.lang.StringUtils;
 
 import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.dismissal.DismissalBean.SelectedCurricularCourse;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
@@ -43,26 +39,7 @@ public class Substitution extends Substitution_Base {
 
     @Override
     final public String getGivenGrade() {
-	if (super.getGivenGrade() == null) {
-	    BigDecimal result = BigDecimal.ZERO;
-	    for (final IEnrolment enrolment : getIEnrolments()) {
-		if (StringUtils.isNumeric(enrolment.getGradeValue())) {
-		    result = result.add(new BigDecimal(enrolment.getGradeValue()));
-		}
-	    }
-	    // FIXME: This is a temporary solution
-	    if (result == BigDecimal.ZERO) {
-		return null;
-	    }
-
-	    final BigDecimal grade = result.divide(new BigDecimal(getIEnrolments().size()), 2,
-		    RoundingMode.HALF_UP);
-	    
-	    return grade.compareTo(BigDecimal.TEN) < 0 ? null : grade.toPlainString();
-	    
-	} else {
-	    return super.getGivenGrade();
-	}
+	return null;
     }
 
 }
