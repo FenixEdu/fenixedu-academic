b833d274d603ed5ffc7ec630fd4048bf9b7d38f8
Ângela Almeida <angela.almeida@ist.utl.pt>
2007-07-26 10:42:57 +0000

    Registration agreement TIME added

--- src/net/sourceforge/fenixedu/domain/student/RegistrationAgreement.java
+++ src/net/sourceforge/fenixedu/domain/student/RegistrationAgreement.java
@@ -2,7 +2,7 @@ package net.sourceforge.fenixedu.domain.student;
 
 public enum RegistrationAgreement {
 
-    NORMAL, AFA, MA, NC, ERASMUS, SOCRATES, SOCRATES_ERASMUS, TEMPUS, BILATERAL_AGREEMENT, ALFA2, UNIFOR, OTHER_EXTERNAL;
+    NORMAL, AFA, MA, NC, ERASMUS, SOCRATES, SOCRATES_ERASMUS, TEMPUS, BILATERAL_AGREEMENT, ALFA2, UNIFOR,TIME, OTHER_EXTERNAL;
 
     public boolean isNormal() {
 	return this.equals(RegistrationAgreement.NORMAL);
