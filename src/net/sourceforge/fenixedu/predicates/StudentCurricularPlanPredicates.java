496ca177e3d495050372d6a5a8c22ce553556e57
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-08-13 10:09:06 +0000

    initial version

---
+++ src/net/sourceforge/fenixedu/predicates/StudentCurricularPlanPredicates.java
@@ -0,0 +1,30 @@
+package net.sourceforge.fenixedu.predicates;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.person.RoleType;
+import net.sourceforge.fenixedu.injectionCode.AccessControl;
+import net.sourceforge.fenixedu.injectionCode.AccessControlPredicate;
+
+public class StudentCurricularPlanPredicates {
+
+    public static final AccessControlPredicate<StudentCurricularPlan> enrol = new AccessControlPredicate<StudentCurricularPlan>() {
+
+	public boolean evaluate(StudentCurricularPlan studentCurricularPlan) {
+	    final Person person = AccessControl.getPerson();
+	    return person.hasRole(RoleType.STUDENT) || person.hasRole(RoleType.ACADEMIC_ADMINISTRATIVE_OFFICE)
+		    || person.hasRole(RoleType.MANAGER);
+	}
+
+    };
+
+    public static final AccessControlPredicate<StudentCurricularPlan> moveCurriculumLines = new AccessControlPredicate<StudentCurricularPlan>() {
+
+	public boolean evaluate(StudentCurricularPlan studentCurricularPlan) {
+	    final Person person = AccessControl.getPerson();
+	    return person.hasRole(RoleType.ACADEMIC_ADMINISTRATIVE_OFFICE) || person.hasRole(RoleType.MANAGER);
+	}
+
+    };
+
+}
