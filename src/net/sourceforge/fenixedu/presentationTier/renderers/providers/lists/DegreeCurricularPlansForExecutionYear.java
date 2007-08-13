31d9af97d469449ab5182f5f81529321050498ad
Shezad Anavarali <shezad@ist.utl.pt>
2007-08-13 18:07:23 +0000

    bug fix

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/lists/DegreeCurricularPlansForExecutionYear.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/lists/DegreeCurricularPlansForExecutionYear.java
@@ -4,9 +4,11 @@ import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.lists.ExecutionDegreeListBean;
 import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.lists.SearchStudentsByCurricularCourseParametersBean;
 import net.sourceforge.fenixedu.domain.Degree;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
@@ -19,15 +21,17 @@ public class DegreeCurricularPlansForExecutionYear implements DataProvider {
 	final SortedSet<DegreeCurricularPlan> result = new TreeSet<DegreeCurricularPlan>(
 		DegreeCurricularPlan.DEGREE_CURRICULAR_PLAN_COMPARATOR_BY_DEGREE_TYPE_AND_EXECUTION_DEGREE_AND_DEGREE_CODE);
 
-	final SearchStudentsByCurricularCourseParametersBean searchBean = (SearchStudentsByCurricularCourseParametersBean) source;
-
-	if (searchBean.getExecutionYear() != null) {
+	final ExecutionYear executionYear = source instanceof SearchStudentsByCurricularCourseParametersBean ? ((SearchStudentsByCurricularCourseParametersBean) source)
+		.getExecutionYear()
+		: ((ExecutionDegreeListBean) source).getExecutionYear();
+		
+	if (executionYear != null) {
 	    final Person person = AccessControl.getPerson();
 
 	    if (person.isAdministrativeOfficeEmployee()) {
 		final Set<Degree> degrees = person.getEmployee().getAdministrativeOffice().getAdministratedDegrees();
 
-		for (DegreeCurricularPlan plan : searchBean.getExecutionYear().getDegreeCurricularPlans()) {
+		for (DegreeCurricularPlan plan : executionYear.getDegreeCurricularPlans()) {
 		    if (degrees.contains(plan.getDegree())) {
 			result.add(plan);
 		    }
