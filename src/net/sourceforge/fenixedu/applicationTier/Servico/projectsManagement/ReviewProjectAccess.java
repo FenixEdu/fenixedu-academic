af555d7401509c49f8f3e38e7da06d375ce5f23a
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-08-14 11:24:55 +0000

    bug fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/projectsManagement/ReviewProjectAccess.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/projectsManagement/ReviewProjectAccess.java
@@ -8,10 +8,8 @@ import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.domain.Employee;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.Role;
-import net.sourceforge.fenixedu.domain.Teacher;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.projectsManagement.ProjectAccess;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
@@ -24,53 +22,49 @@ import net.sourceforge.fenixedu.persistenceTierOracle.Oracle.PersistentSuportOra
 public class ReviewProjectAccess extends Service {
 
     public void run(Person person, String costCenter, String userNumber) throws FenixServiceException,
-            ExcepcaoPersistencia {
-        Role role = Role.getRoleByRoleType(RoleType.PROJECTS_MANAGER);
-        if (ProjectAccess.getAllByPersonAndCostCenter(person, false, true).size() == 0) {
-            Teacher teacher = person.getTeacher();
-            if (teacher == null) {
-                Employee employee = person.getEmployee();
-                if (employee != null) {
-                    IPersistentSuportOracle persistentSupportOracle = PersistentSuportOracle
-                            .getProjectDBInstance();
-                    if ((persistentSupportOracle.getIPersistentProject().countUserProject(
-                            employee.getEmployeeNumber()) == 0)) {
-                        List<ProjectAccess> accessesToDelete = ProjectAccess
-                                .getAllByPersonAndCostCenter(person, false, false);
+	    ExcepcaoPersistencia {
+	IPersistentSuportOracle persistentSupportOracle = PersistentSuportOracle.getProjectDBInstance();
 
-                        for (ProjectAccess access : accessesToDelete) {
-                            access.delete();
-                        }
+	Role role = Role.getRoleByRoleType(RoleType.PROJECTS_MANAGER);
+	if (ProjectAccess.getAllByPersonAndCostCenter(person, false, true).size() == 0) {
+	    Integer personNumber = getPersonNumber(person);
+	    if (personNumber == null) {
+		throw new FenixServiceException();
+	    }
+	    if ((persistentSupportOracle.getIPersistentProject().countUserProject(personNumber) == 0)) {
+		cleanProjectsAccess(person, personNumber, role, false, false);
+	    }
+	}
+	role = Role.getRoleByRoleType(RoleType.INSTITUCIONAL_PROJECTS_MANAGER);
+	if (ProjectAccess.getAllByPersonAndCostCenter(person, true, true).size() == 0) {
+	    Integer personNumber = getPersonNumber(person);
+	    if (personNumber == null) {
+		throw new FenixServiceException();
+	    }
+	    if ((persistentSupportOracle.getIPersistentProjectUser().getInstitucionalProjectCoordId(
+		    personNumber).size() == 0)) {
+		cleanProjectsAccess(person, personNumber, role, true, false);
+	    }
+	}
+    }
 
-                        person.removePersonRoles(role);
-                    }
-                } else
-                    throw new FenixServiceException();
-            }
-        }
+    private Integer getPersonNumber(Person person) {
+	if (person.getTeacher() != null) {
+	    return person.getTeacher().getTeacherNumber();
+	}
+	if (person.getEmployee() != null) {
+	    return person.getEmployee().getEmployeeNumber();
+	}
+	return null;
+    }
 
-        role = Role.getRoleByRoleType(RoleType.INSTITUCIONAL_PROJECTS_MANAGER);
-        if (ProjectAccess.getAllByPersonAndCostCenter(person, true, true).size() == 0) {
-            Teacher teacher = person.getTeacher();
-            if (teacher == null) {
-                Employee employee = person.getEmployee();
-                if (employee != null) {
-                    IPersistentSuportOracle persistentSupportOracle = PersistentSuportOracle
-                            .getProjectDBInstance();
-                    if ((persistentSupportOracle.getIPersistentProject().countUserProject(
-                            employee.getEmployeeNumber()) == 0)) {
-                        List<ProjectAccess> accessesToDelete = ProjectAccess
-                                .getAllByPersonAndCostCenter(person, true, false);
-
-                        for (ProjectAccess access : accessesToDelete) {
-                            access.delete();
-                        }
-
-                        person.removePersonRoles(role);
-                    }
-                } else
-                    throw new FenixServiceException();
-            }
-        }
+    private void cleanProjectsAccess(Person person, Integer number, Role role, boolean isCostCenter,
+	    boolean limitDates) throws FenixServiceException, ExcepcaoPersistencia {
+	List<ProjectAccess> accessesToDelete = ProjectAccess.getAllByPersonAndCostCenter(person,
+		isCostCenter, limitDates);
+	for (ProjectAccess access : accessesToDelete) {
+	    access.delete();
+	}
+	person.removePersonRoles(role);
     }
 }
