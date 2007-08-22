70a8674f917924a413778fbd12039e09acfb91f4
João Florindo <jdnf@linkare.pt>
2007-08-22 15:27:15 +0000

    bug fix in assert unique curricular rule

--- src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/AssertUniqueApprovalInCurricularCourseContextsExecutor.java
+++ src/net/sourceforge/fenixedu/domain/curricularRules/ruleExecutors/AssertUniqueApprovalInCurricularCourseContextsExecutor.java
@@ -5,24 +5,20 @@ import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.curricularRules.ICurricularRule;
 import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
 import net.sourceforge.fenixedu.domain.enrolment.IDegreeModuleToEvaluate;
-import net.sourceforge.fenixedu.util.CurricularRuleLabelFormatter;
 
 public class AssertUniqueApprovalInCurricularCourseContextsExecutor extends CurricularRuleExecutor {
 
     @Override
     protected RuleResult executeEnrolmentWithRules(final ICurricularRule curricularRule, final IDegreeModuleToEvaluate sourceDegreeModuleToEvaluate, final EnrolmentContext enrolmentContext) {
 	final CurricularCourse curricularCourse = (CurricularCourse) curricularRule.getDegreeModuleToApplyRule();
-
+	final ExecutionPeriod executionPeriod = enrolmentContext.getExecutionPeriod();
+	
 	if (!curricularCourse.hasAnyActiveContext(enrolmentContext.getExecutionPeriod())) {
 	    return RuleResult.createNA();
 	}
-
-	if (isApproved(enrolmentContext, curricularCourse)) {
-	    if (isEnroled(enrolmentContext, curricularCourse, enrolmentContext.getExecutionPeriod())) {
-		return RuleResult.createTrue(EnrolmentResultType.IMPOSSIBLE);
-	    } else { // is enrolling
-		return RuleResult.createFalseWithLiteralMessage(CurricularRuleLabelFormatter.getLabel(curricularRule));
-	    }
+	
+	if (isApproved(enrolmentContext, curricularCourse, executionPeriod.getPreviousExecutionPeriod())) {
+	    return RuleResult.createTrue(EnrolmentResultType.IMPOSSIBLE); 
 	} else {
 	    return RuleResult.createTrue();
 	}
@@ -36,22 +32,16 @@ public class AssertUniqueApprovalInCurricularCourseContextsExecutor extends Curr
 	if (!curricularCourse.hasAnyActiveContext(enrolmentContext.getExecutionPeriod())) {
 	    return RuleResult.createNA();
 	}
-
-	if (isApproved(enrolmentContext, curricularCourse)) {
-
-	    if (isEnroled(enrolmentContext, curricularCourse, executionPeriod)) {
-		return RuleResult.createTrue(EnrolmentResultType.IMPOSSIBLE);
-	    } else { // is enrolling
-		return RuleResult.createFalseWithLiteralMessage(CurricularRuleLabelFormatter.getLabel(curricularRule));
-	    }
-
+	
+	if (isApproved(enrolmentContext, curricularCourse, executionPeriod.getPreviousExecutionPeriod())) {
+	    return RuleResult.createTrue(EnrolmentResultType.IMPOSSIBLE); 
+	    
 	} else if (hasEnrolmentWithEnroledState(enrolmentContext, curricularCourse, executionPeriod.getPreviousExecutionPeriod())) {
 	    return RuleResult.createTrue(EnrolmentResultType.TEMPORARY);
-
+	    
 	} else {
 	    return RuleResult.createTrue();
 	}
-
     }
 
     @Override
