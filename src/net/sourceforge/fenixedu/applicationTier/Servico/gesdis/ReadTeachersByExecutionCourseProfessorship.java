716602845bfcfb5f2625fa89fef29e39cf7b2f9d
Luis Cruz <luis.cruz@ist.utl.pt>
2007-08-30 22:18:15 +0000

    Removed useless code.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/ReadTeachersByExecutionCourseProfessorship.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/gesdis/ReadTeachersByExecutionCourseProfessorship.java
@@ -5,15 +5,12 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Servico.gesdis;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourse;
-import net.sourceforge.fenixedu.dataTransferObject.InfoTeacher;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
-import net.sourceforge.fenixedu.domain.Professorship;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 /**
@@ -23,24 +20,9 @@ import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
  */
 public class ReadTeachersByExecutionCourseProfessorship extends Service {
 
-    public List run(InfoExecutionCourse infoExecutionCourse) throws FenixServiceException,
-            ExcepcaoPersistencia {
-
-        ExecutionCourse executionCourse = rootDomainObject.readExecutionCourseByOID(
-                infoExecutionCourse.getIdInternal());
-
-        List<Professorship> result = null;
-        if(executionCourse != null){
-            result = executionCourse.getProfessorships();
-        }
-
-        final List<InfoTeacher> infoResult = new ArrayList<InfoTeacher>();
-        if (result != null) {
-            for (final Professorship professorship : result) {
-                infoResult.add(InfoTeacher.newInfoFromDomain(professorship.getTeacher()));
-            }
-            return infoResult;
-        }
-        return result;
+    public List run(InfoExecutionCourse infoExecutionCourse) throws FenixServiceException, ExcepcaoPersistencia {
+        ExecutionCourse executionCourse = rootDomainObject.readExecutionCourseByOID(infoExecutionCourse.getIdInternal());
+        return executionCourse == null ? null : executionCourse.getProfessorships();
     }
+
 }
