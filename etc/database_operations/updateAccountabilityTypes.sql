5adc98258f26ec6a350e35fabcfcde5e325b7bc1
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2007-09-12 15:36:33 +0000

    fixed sqls

--- etc/database_operations/updateAccountabilityTypes.sql
+++ etc/database_operations/updateAccountabilityTypes.sql
@@ -1,2 +1,4 @@
-insert into ACCOUNTABILITY_TYPE(TYPE,OJB_CONCRETE_CLASS,NAME,TYPE_NAME) values('RESEARCH_CONTRACT','net.sourceforge.fenixedu.domain.organizationalStructure.AccountabilityType', 'RESEARCH_CONTRACT', 'pt17:RESEARCH_CONTRACT');
+delete from ACCOUNTABILITY_TYPE where OJB_CONCRETE_CLASS like 'net.sourceforge.fenixedu.domain.organizationalStructure.ResearchFunction' ;
+
+insert into ACCOUNTABILITY_TYPE(TYPE,OJB_CONCRETE_CLASS,TYPE_NAME) values('RESEARCH_CONTRACT','net.sourceforge.fenixedu.domain.organizationalStructure.AccountabilityType', 'pt17:RESEARCH_CONTRACT');
 
--- etc/database_operations/updateResearchContracts.sql
+++ etc/database_operations/updateResearchContracts.sql
@@ -5,5 +5,3 @@ update ACCOUNTABILITY set OJB_CONCRETE_CLASS='net.sourceforge.fenixedu.domain.or
 update ACCOUNTABILITY set OJB_CONCRETE_CLASS='net.sourceforge.fenixedu.domain.organizationalStructure.ResearchTechnicalStaffContract' where KEY_ACCOUNTABILITY_TYPE=1384 ;
 
 update ACCOUNTABILITY set KEY_ACCOUNTABILITY_TYPE = (select ID_INTERNAL FROM ACCOUNTABILITY_TYPE WHERE TYPE='RESEARCH_CONTRACT') WHERE OJB_CONCRETE_CLASS like '%Research%';
-
-delete from ACCOUNTABILITY_TYPE where OJB_CONCRETE_CLASS like 'net.sourceforge.fenixedu.domain.organizationalStructure.ResearchFunction' ;
