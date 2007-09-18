b73d566bf4cb1370304b017d53eb0fa4f095db3e
Shezad Anavarali <shezad@ist.utl.pt>
2007-09-18 09:42:20 +0000

    fix

--- src/net/sourceforge/fenixedu/domain/ExecutionDegree.java
+++ src/net/sourceforge/fenixedu/domain/ExecutionDegree.java
@@ -7,6 +7,7 @@
 package net.sourceforge.fenixedu.domain;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
