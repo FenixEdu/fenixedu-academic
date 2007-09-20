e49157f13904a85725acb8e282c24ee2ba0f703f
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-09-20 18:06:24 +0000

    added delete method

--- src/net/sourceforge/fenixedu/domain/student/WeeklyWorkLoad.java
+++ src/net/sourceforge/fenixedu/domain/student/WeeklyWorkLoad.java
@@ -2,6 +2,7 @@ package net.sourceforge.fenixedu.domain.student;
 
 import net.sourceforge.fenixedu.domain.Attends;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.injectionCode.Checked;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeFieldType;
@@ -10,46 +11,55 @@ import org.joda.time.Interval;
 
 public class WeeklyWorkLoad extends WeeklyWorkLoad_Base implements Comparable<WeeklyWorkLoad> {
 
-    public WeeklyWorkLoad(final Attends attends, final Integer weekOffset,
-            final Integer contact, final Integer autonomousStudy, final Integer other) {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
+    public WeeklyWorkLoad(final Attends attends, final Integer weekOffset, final Integer contact, final Integer autonomousStudy,
+	    final Integer other) {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
 
-        if (attends == null || weekOffset == null) {
-            throw new NullPointerException();
-        }
+	if (attends == null || weekOffset == null) {
+	    throw new NullPointerException();
+	}
 
-        setAttends(attends);
-        setContact(contact);
-        setAutonomousStudy(autonomousStudy);
-        setOther(other);
-        setWeekOffset(weekOffset);
+	setAttends(attends);
+	setContact(contact);
+	setAutonomousStudy(autonomousStudy);
+	setOther(other);
+	setWeekOffset(weekOffset);
     }
 
     public int getTotal() {
-    	final int contact = getContact() != null ? getContact() : 0;
-    	final int autonomousStudy = getAutonomousStudy() != null ? getAutonomousStudy() : 0;
-    	final int other = getOther() != null ? getOther() : 0;
-    	return contact + autonomousStudy + other;
+	final int contact = getContact() != null ? getContact() : 0;
+	final int autonomousStudy = getAutonomousStudy() != null ? getAutonomousStudy() : 0;
+	final int other = getOther() != null ? getOther() : 0;
+	return contact + autonomousStudy + other;
     }
 
     public int compareTo(final WeeklyWorkLoad weeklyWorkLoad) {
-        if (weeklyWorkLoad == null) {
-            throw new NullPointerException("Cannot compare weekly work load with null");
-        }
-        if (getAttends() != weeklyWorkLoad.getAttends()) {
-            throw new IllegalArgumentException("Cannot compare weekly work loads of different attends.");
-        }
+	if (weeklyWorkLoad == null) {
+	    throw new NullPointerException("Cannot compare weekly work load with null");
+	}
+	if (getAttends() != weeklyWorkLoad.getAttends()) {
+	    throw new IllegalArgumentException("Cannot compare weekly work loads of different attends.");
+	}
 
-        return getWeekOffset().compareTo(weeklyWorkLoad.getWeekOffset());
+	return getWeekOffset().compareTo(weeklyWorkLoad.getWeekOffset());
     }
 
     public Interval getInterval() {
-   		final DateTime beginningOfSemester = new DateTime(getAttends().getBegginingOfLessonPeriod());
-   		final DateTime firstMonday = beginningOfSemester.withField(DateTimeFieldType.dayOfWeek(), 1);
-   		final DateTime start = firstMonday.withFieldAdded(DurationFieldType.weeks(), getWeekOffset().intValue());
-   		final DateTime end = start.plusWeeks(1);
-   		return new Interval(start, end);
+	final DateTime beginningOfSemester = new DateTime(getAttends().getBegginingOfLessonPeriod());
+	final DateTime firstMonday = beginningOfSemester.withField(DateTimeFieldType.dayOfWeek(), 1);
+	final DateTime start = firstMonday.withFieldAdded(DurationFieldType.weeks(), getWeekOffset().intValue());
+	final DateTime end = start.plusWeeks(1);
+	return new Interval(start, end);
+    }
+
+    @Checked("RolePredicates.MANAGER_PREDICATE")
+    public void delete() {
+	removeAttends();
+	removeRootDomainObject();
+
+	super.deleteDomainObject();
+
     }
 
 }
