29fe367b85765d884479a1df2432dbc457c92db6
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-09-26 17:27:29 +0000

    bugs fix in Export Space xls.

--- src/net/sourceforge/fenixedu/domain/organizationalStructure/Unit.java
+++ src/net/sourceforge/fenixedu/domain/organizationalStructure/Unit.java
@@ -1024,8 +1024,7 @@ public class Unit extends Unit_Base {
 	destinationUnit.getPayedGuides().addAll(fromUnit.getPayedGuides());
 	destinationUnit.getResultUnitAssociations().addAll(fromUnit.getResultUnitAssociations());
 	destinationUnit.getAssociatedNonAffiliatedTeachers().addAll(fromUnit.getAssociatedNonAffiliatedTeachers());
-	destinationUnit.getPrecedentDegreeInformations().addAll(fromUnit.getPrecedentDegreeInformations());
-	
+		
 	fromUnit.delete();
     }
 
