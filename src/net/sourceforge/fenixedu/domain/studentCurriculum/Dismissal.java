45ba5214d7bbc3d8ea958e7cd86e2a247b95023a
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-09-27 13:54:47 +0000

    bug fix

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/Dismissal.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/Dismissal.java
@@ -125,8 +125,11 @@ public class Dismissal extends Dismissal_Base {
 
     @Override
     public boolean isApproved(final CurricularCourse curricularCourse, final ExecutionPeriod executionPeriod) {
-	return (executionPeriod == null || getExecutionPeriod().isBeforeOrEquals(executionPeriod))
-		&& getCurricularCourse().isEquivalent(curricularCourse);
+	return isValid(executionPeriod) && getCurricularCourse().isEquivalent(curricularCourse);
+    }
+
+    private boolean isValid(final ExecutionPeriod executionPeriod) {
+	return (executionPeriod == null || !hasExecutionPeriod() || getExecutionPeriod().isBeforeOrEquals(executionPeriod));
     }
 
     @Override
