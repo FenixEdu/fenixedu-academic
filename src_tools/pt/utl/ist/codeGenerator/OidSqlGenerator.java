37cc03b840ef3ada7f72a4b30c1f8d089e8c737d
Luis Cruz <luis.cruz@ist.utl.pt>
2007-09-27 00:44:54 +0000

    Fixed generation of keys for relations between the same domain entities. Do all add columns and indexes in same alter table instruction for each table to optimize the sql script when it runs.

--- src_tools/pt/utl/ist/codeGenerator/OidSqlGenerator.java
+++ src_tools/pt/utl/ist/codeGenerator/OidSqlGenerator.java
@@ -2,8 +2,14 @@ package pt.utl.ist.codeGenerator;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Map.Entry;
 
 import net.sourceforge.fenixedu._development.MetadataManager;
 import dml.DomainClass;
@@ -15,6 +21,158 @@ import dml.Slot;
 
 public class OidSqlGenerator {
 
+    private static abstract class Writer {
+        protected abstract String getFilename();
+        protected abstract void writeContents(final FileWriter fileWriter) throws IOException;
+
+        public void write() throws IOException {
+            final FileWriter fileWriter = new FileWriter(getFilename());
+            fileWriter.append("set autocommit = 0;\n");
+            fileWriter.append("begin;\n\n");
+
+            writeContents(fileWriter);
+
+            fileWriter.append("\ncommit;\n");
+            fileWriter.close();
+        }
+    }
+
+    private static class AlterTableRegistry extends Writer {
+        private final Map<String, Set<String>> map = new TreeMap<String, Set<String>>();
+
+        public void addOidColumn(final String tablename) {
+            addColumn(tablename, "OID");
+        }
+
+        public void addColumn(final String tablename, final String columnName) {
+            addColumnDefinition(tablename, columnName);
+        }
+
+        private void addColumnDefinition(final String tablename, final String columnName) {
+            Set<String> columnNames = map.get(tablename);
+            if (columnNames == null) {
+                columnNames = new TreeSet<String>();
+                map.put(tablename, columnNames);
+            }
+            columnNames.add(columnName);
+        }
+
+        @Override
+        protected String getFilename() {
+            return "/tmp/alterTables.sql";
+        }
+
+        @Override
+        protected void writeContents(final FileWriter fileWriter) throws IOException {
+            for (final Entry<String, Set<String>> entry : map.entrySet()) {
+                final String tablename = entry.getKey();
+                final Set<String> columnNames = entry.getValue();
+
+                fileWriter.append("alter table ");
+                fileWriter.append(tablename);
+                boolean isFirst = true;
+                for (final String columnName : columnNames) {
+                    if (isFirst) {
+                        isFirst = false;
+                    } else {
+                        fileWriter.append(",");
+                    }
+                    fileWriter.append("\n    add column ");
+                    fileWriter.append(columnName);
+                    fileWriter.append(" bigint unsigned default null,\n    add index (");
+                    fileWriter.append(columnName);
+                    fileWriter.append(")");
+                }
+                fileWriter.append(";\n");
+            }
+        }
+
+    }
+
+    private static class UpdateRegistry extends Writer {
+        private final Map<String, String> mapSimple = new TreeMap<String, String>();
+        private final Map<String, String> mapFamily = new TreeMap<String, String>();
+        private final List<String[]> mapKeys = new ArrayList<String[]>();
+
+        private void addClassnameAndTableForUpdate(final String domainClassName, final String tablename) {
+            mapSimple.put(domainClassName, tablename);
+        }
+
+        private void addClassnameAndTableForUpdateInHierchy(final String domainClassName, final String tablename) {
+            mapFamily.put(domainClassName, tablename);
+        }
+
+        private void addUpdateKeyInstruction(final String tablename, final String otherTablename, final String key, final String newKey) {
+            mapKeys.add(new String[] {tablename, otherTablename, newKey, key});
+        }
+
+        @Override
+        protected String getFilename() {
+            return "/tmp/updates.sql";
+        }
+
+        protected void writeSelect(final FileWriter fileWriter, final String domainClassName, final String tablename) throws IOException {
+            fileWriter.append("select @xpto:=null;\nselect @xpto:=DOMAIN_CLASS_INFO.DOMAIN_CLASS_ID from DOMAIN_CLASS_INFO where DOMAIN_CLASS_INFO.DOMAIN_CLASS_NAME = '");
+            fileWriter.append(domainClassName);
+            fileWriter.append("';\n");
+        }
+
+        protected void writeUpdate(final FileWriter fileWriter, final String tablename) throws IOException {
+            fileWriter.append("update ");
+            fileWriter.append(tablename);
+            fileWriter.append(" set OID = (@xpto << 32) + ID_INTERNAL;\n");
+        }
+
+        protected void writeUpdate(final FileWriter fileWriter, final String domainClassName, final String tablename) throws IOException {
+            fileWriter.append("update ");
+            fileWriter.append(tablename);
+            fileWriter.append(" set OID = (@xpto << 32) + ID_INTERNAL where OJB_CONCRETE_CLASS = '");
+            fileWriter.append(domainClassName);
+            fileWriter.append("';\n");
+        }
+
+        @Override
+        protected void writeContents(final FileWriter fileWriter) throws IOException {
+            for (final Entry<String, String> entry : mapSimple.entrySet()) {
+                final String domainClassName = entry.getKey();
+                final String tablename = entry.getValue();
+
+                writeSelect(fileWriter, domainClassName, tablename);
+                writeUpdate(fileWriter, tablename);
+            }
+
+            for (final Entry<String, String> entry : mapFamily.entrySet()) {
+                final String domainClassName = entry.getKey();
+                final String tablename = entry.getValue();
+
+                writeSelect(fileWriter, domainClassName, tablename);
+                writeUpdate(fileWriter, domainClassName, tablename);
+            }
+
+            for (final String[] strings : mapKeys) {
+                final String tablename = strings[0];
+                final String otherTablename = strings[1];
+                final String newKey = strings[2];
+                final String key = strings[3];
+
+                fileWriter.append("update ");
+                fileWriter.append(tablename);
+                fileWriter.append(" as t1, ");
+                fileWriter.append(otherTablename);
+                fileWriter.append(" as t2 set t1.");
+                fileWriter.append(newKey);
+                fileWriter.append(" = t2.OID where t2.ID_INTERNAL = t1.");
+                fileWriter.append(key);
+                fileWriter.append(";\n");
+            }
+
+        }
+        
+    }
+
+    private static final AlterTableRegistry alterTableRegistry = new AlterTableRegistry();
+    private static final UpdateRegistry updateRegistry = new UpdateRegistry();
+
     public static void main(String[] args) {
 	try {
 	    generate(args[0]);
@@ -24,17 +182,10 @@ public class OidSqlGenerator {
 	System.exit(0);
     }
 
-    static FileWriter fileWriterAlterTables = null;
-    static FileWriter fileWriterUpdates = null;
-    static FileWriter fileWriterUpdateKeys = null;
-
     private static void generate(final String dmlFilePath) throws IOException {
-	fileWriterAlterTables = createFileWriter("/tmp/alterTables.sql");
-	fileWriterUpdates = new FileWriter("/tmp/updates.sql");
-	fileWriterUpdateKeys = new FileWriter("/tmp/updateKeys.sql");
-
 	MetadataManager.init(dmlFilePath);
 	final DomainModel domainModel = MetadataManager.getDomainModel();
+
 	for (final DomainClass domainClass : domainModel.getDomainClasses()) {
 	    final int domainClassHierarchyLevel = calculateHierarchyLevel(domainClass);
 	    if (domainClassHierarchyLevel == 1) {
@@ -45,66 +196,32 @@ public class OidSqlGenerator {
 		    writeSqlInstructions(domainModel, domainClass);
 		}
 	    }
-
 	    writeKeySqlInstructions(domainClass);
 	}
-	closeFile(fileWriterAlterTables);
-	closeFile(fileWriterUpdates);
-	closeFile(fileWriterUpdateKeys);
-    }
 
-    private static FileWriter createFileWriter(final String filename) throws IOException {
-	final FileWriter fileWriter = new FileWriter(filename);
-	fileWriter.append("set autocommit = 0;\n");
-	fileWriter.append("begin;\n\n");
-	return fileWriter;
-    }
-
-    private static void closeFile(final FileWriter fileWriter) throws IOException {
-	fileWriter.append("\ncommit;\n");
-	fileWriter.close();
+	alterTableRegistry.write();
+	updateRegistry.write();
     }
 
     private static void writeSqlInstructions(final DomainModel domainModel, final DomainClass domainClass) throws IOException {
 	final String tablename = getTableName(domainClass.getName());
-	writeAlterTable(tablename);
+	alterTableRegistry.addOidColumn(tablename);
 
 	for (final DomainClass otherDomainClass : domainModel.getDomainClasses()) {
 	    final int domainClassHierarchyLevel = calculateHierarchyLevel(otherDomainClass);
 	    if (domainClassHierarchyLevel > 0) {
 		final DomainClass otherDomainObjectDescendent = findDirectDomainObjectDecendent(otherDomainClass);
 		if (otherDomainObjectDescendent == domainClass) {
-		    fileWriterUpdates.append("select @xpto:=null;\nselect @xpto:=DOMAIN_CLASS_INFO.DOMAIN_CLASS_ID from DOMAIN_CLASS_INFO where DOMAIN_CLASS_INFO.DOMAIN_CLASS_NAME = '");
-		    fileWriterUpdates.append(otherDomainClass.getFullName());
-		    fileWriterUpdates.append("';\n");
-
-		    fileWriterUpdates.append("update ");
-		    fileWriterUpdates.append(tablename);
-		    fileWriterUpdates.append(" set OID = (@xpto << 32) + ID_INTERNAL where OJB_CONCRETE_CLASS = '");
-		    fileWriterUpdates.append(otherDomainClass.getFullName());
-		    fileWriterUpdates.append("';\n");
+		    updateRegistry.addClassnameAndTableForUpdateInHierchy(otherDomainClass.getFullName(), tablename);
 		}
 	    }
 	}
     }
 
-    private static void writeAlterTable(final String tablename) throws IOException {
-	fileWriterAlterTables.append("alter table ");
-	fileWriterAlterTables.append(tablename);
-	fileWriterAlterTables.append(" add column OID bigint unsigned default null;\n");
-    }
-
     private static void writeSqlInstructions(final DomainClass domainClass) throws IOException {
 	final String tablename = getTableName(domainClass.getName());
-	writeAlterTable(tablename);
-
-	fileWriterUpdates.append("select @xpto:=null;\nselect @xpto:=DOMAIN_CLASS_INFO.DOMAIN_CLASS_ID from DOMAIN_CLASS_INFO where DOMAIN_CLASS_INFO.DOMAIN_CLASS_NAME = '");
-	fileWriterUpdates.append(domainClass.getFullName());
-	fileWriterUpdates.append("';\n");
-
-	fileWriterUpdates.append("update ");
-	fileWriterUpdates.append(tablename);
-	fileWriterUpdates.append(" set OID = (@xpto << 32) + ID_INTERNAL;\n");
+	alterTableRegistry.addOidColumn(tablename);
+	updateRegistry.addClassnameAndTableForUpdate(domainClass.getFullName(), tablename);
     }
 
     private static void writeKeySqlInstructions(final DomainClass domainClass) throws IOException {
@@ -120,13 +237,9 @@ public class OidSqlGenerator {
                 final String key = getKeyFromRole(role);
                 final String newKey = getNewKeyFromRole(role);
 
-                fileWriterUpdateKeys.append("alter table ");
-                fileWriterUpdateKeys.append(tablename);
-                fileWriterUpdateKeys.append(" add column ");
-                fileWriterUpdateKeys.append(newKey);
-                fileWriterUpdateKeys.append(" bigint unsigned default null;\n");
+                alterTableRegistry.addColumn(tablename, newKey);
 
-                appendUpdateKeyInstruction(tablename, otherTablename, key, newKey);
+                updateRegistry.addUpdateKeyInstruction(tablename, otherTablename, key, newKey);
             } else if (role.getMultiplicityUpper() == -1) {
                 if (role.getOtherRole().getMultiplicityUpper() == -1) {
                     writeIndirectionKeySqlInstructions(role.getRelation());
@@ -148,42 +261,17 @@ public class OidSqlGenerator {
 
         final String tablename = getTableName(relation.getFullName());
 
-        fileWriterUpdateKeys.append("alter table ");
-        fileWriterUpdateKeys.append(tablename);
-        boolean isFirst = true;
         for (final Role role : relation.getRoles()) {
-            if (isFirst) {
-                isFirst = false;
-            } else {
-                fileWriterUpdateKeys.append(",");
-            }
-
             final String newKey = getNewIndirectionKeyFromRole(role);
-            fileWriterUpdateKeys.append(" add column ");
-            fileWriterUpdateKeys.append(newKey);
-            fileWriterUpdateKeys.append(" bigint unsigned default null");
-
+            alterTableRegistry.addColumn(tablename, newKey);
         }
-        fileWriterUpdateKeys.append(";\n");
 
         for (final Role role : relation.getRoles()) {
             final String otherTablename = getTopLevelTableName((DomainClass) role.getType());
             final String key = getIndirectionKeyFromRole(role);
             final String newKey = getNewIndirectionKeyFromRole(role);
-            appendUpdateKeyInstruction(tablename, otherTablename, key, newKey);
+            updateRegistry.addUpdateKeyInstruction(tablename, otherTablename, key, newKey);
         }
-    }
-
-    private static void appendUpdateKeyInstruction(final String tablename, final String otherTablename, final String key, final String newKey) throws IOException {
-        fileWriterUpdateKeys.append("update ");
-        fileWriterUpdateKeys.append(tablename);
-        fileWriterUpdateKeys.append(" as t1, ");
-        fileWriterUpdateKeys.append(otherTablename);
-        fileWriterUpdateKeys.append(" as t2 set t1.");
-        fileWriterUpdateKeys.append(newKey);
-        fileWriterUpdateKeys.append(" = t2.OID where t2.ID_INTERNAL = t1.");
-        fileWriterUpdateKeys.append(key);
-        fileWriterUpdateKeys.append(";\n");
     }
 
     private static DomainClass findDirectDomainObjectDecendent(final DomainClass domainClass) {
@@ -247,11 +335,24 @@ public class OidSqlGenerator {
     }
 
     private static String getIndirectionKeyFromRole(final Role role) {
-        return getAppendedSqlName("KEY_", role.getType().getName());
+        final String newKey = getAppendedSqlName("KEY_", role.getType().getName());
+        return hasMutualTypeInRelation(role) ? newKey + "_" + getSqlName(role.getName()) : newKey;
     }
 
     private static String getNewIndirectionKeyFromRole(final Role role) {
-        return getAppendedSqlName("OID_", role.getType().getName());
+        final String newKey = getAppendedSqlName("OID_", role.getType().getName());
+        return hasMutualTypeInRelation(role) ? newKey + "_" + getSqlName(role.getName()) : newKey;
+    }
+
+    private static boolean hasMutualTypeInRelation(final Role role) {
+        final DomainEntity domainEntity = role.getType();
+        for (final Role otherRole : role.getRelation().getRoles()) {
+            final DomainEntity otherDomainEntity = otherRole.getType();
+            if (!role.getName().equals(otherRole.getName()) && domainEntity.getFullName().equals(otherDomainEntity.getFullName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
