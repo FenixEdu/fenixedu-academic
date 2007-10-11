7cb078abb038bab3a82c69762e9e70beeecdf89b
Susana Fernandes <susana.fernandes@ist.utl.pt>
2007-10-11 11:02:49 +0000

    fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/onlineTests/ChangeStudentTestQuestion.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/teacher/onlineTests/ChangeStudentTestQuestion.java
@@ -38,211 +38,211 @@ import net.sourceforge.fenixedu.util.tests.TestType;
 
 public class ChangeStudentTestQuestion extends Service {
 
-    public Boolean run(Integer executionCourseId,
-            Integer distributedTestId, Integer oldQuestionId, Integer newMetadataId, Integer studentId,
-            TestQuestionChangesType changesType, Boolean delete,
-            TestQuestionStudentsChangesType studentsType, String path) throws FenixServiceException,
-            ExcepcaoPersistencia {
+    public Boolean run(Integer executionCourseId, Integer distributedTestId, Integer oldQuestionId,
+	    Integer newMetadataId, Integer studentId, TestQuestionChangesType changesType,
+	    Boolean delete, TestQuestionStudentsChangesType studentsType, String path)
+	    throws FenixServiceException, ExcepcaoPersistencia {
 
-        DistributedTest distributedTest = rootDomainObject.readDistributedTestByOID(distributedTestId);
-        Question oldQuestion = distributedTest.findQuestionByOID(oldQuestionId);
+	DistributedTest distributedTest = rootDomainObject.readDistributedTestByOID(distributedTestId);
+	Question oldQuestion = distributedTest.findQuestionByOID(oldQuestionId);
 
-        if (oldQuestion == null)
-            throw new InvalidArgumentsServiceException();
+	if (oldQuestion == null)
+	    throw new InvalidArgumentsServiceException();
 
-        Metadata metadata = null;
+	Metadata metadata = null;
 
-        List<Question> availableQuestions = new ArrayList<Question>();
-        if (newMetadataId != null) {
-            metadata = rootDomainObject.readMetadataByOID(newMetadataId);
-            if (metadata == null)
-                throw new InvalidArgumentsServiceException();
-            availableQuestions.addAll(metadata.getVisibleQuestions());
-        } else {
-            availableQuestions.addAll(oldQuestion.getMetadata().getVisibleQuestions());
-            availableQuestions.remove(oldQuestion);
-        }
+	List<Question> availableQuestions = new ArrayList<Question>();
+	if (newMetadataId != null) {
+	    metadata = rootDomainObject.readMetadataByOID(newMetadataId);
+	    if (metadata == null)
+		throw new InvalidArgumentsServiceException();
+	    availableQuestions.addAll(metadata.getVisibleQuestions());
+	} else {
+	    availableQuestions.addAll(oldQuestion.getMetadata().getVisibleQuestions());
+	    availableQuestions.remove(oldQuestion);
+	}
 
-        final Set<DistributedTest> distributedTestList;
-        if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.ALL_STUDENTS)
-            distributedTestList = oldQuestion.findDistributedTests();
-        else {
-            distributedTestList = new HashSet<DistributedTest>();
+	final Set<DistributedTest> distributedTestList;
+	if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.ALL_STUDENTS)
+	    distributedTestList = oldQuestion.findDistributedTests();
+	else {
+	    distributedTestList = new HashSet<DistributedTest>();
 
-            if (distributedTest == null)
-                throw new InvalidArgumentsServiceException();
-            distributedTestList.add(distributedTest);
+	    if (distributedTest == null)
+		throw new InvalidArgumentsServiceException();
+	    distributedTestList.add(distributedTest);
 
-        }
-        boolean canDelete = true;
-        for (DistributedTest currentDistributedTest : distributedTestList) {
-            Collection<StudentTestQuestion> studentsTestQuestionList = new ArrayList<StudentTestQuestion>();
+	}
+	boolean canDelete = true;
+	for (DistributedTest currentDistributedTest : distributedTestList) {
+	    Collection<StudentTestQuestion> studentsTestQuestionList = new ArrayList<StudentTestQuestion>();
 
-            if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.THIS_STUDENT) {
-                Registration registration = rootDomainObject.readRegistrationByOID(studentId);
-                if (registration == null)
-                    throw new InvalidArgumentsServiceException();
-                studentsTestQuestionList.add(StudentTestQuestion.findStudentTestQuestion(oldQuestion,
-                        registration, currentDistributedTest));
-            } else if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.STUDENTS_FROM_TEST) {
-                Registration registration = rootDomainObject.readRegistrationByOID(studentId);
-                if (registration == null)
-                    throw new InvalidArgumentsServiceException();
-                Integer order = StudentTestQuestion.findStudentTestQuestion(oldQuestion, registration,
-                        currentDistributedTest).getTestQuestionOrder();
-                studentsTestQuestionList = currentDistributedTest
-                        .findStudentTestQuestionsByTestQuestionOrder(order);
-            } else
-                studentsTestQuestionList = StudentTestQuestion.findStudentTestQuestions(oldQuestion,
-                        currentDistributedTest);
+	    if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.THIS_STUDENT) {
+		Registration registration = rootDomainObject.readRegistrationByOID(studentId);
+		if (registration == null)
+		    throw new InvalidArgumentsServiceException();
+		studentsTestQuestionList.add(StudentTestQuestion.findStudentTestQuestion(oldQuestion,
+			registration, currentDistributedTest));
+	    } else if (studentsType.getType().intValue() == TestQuestionStudentsChangesType.STUDENTS_FROM_TEST) {
+		Registration registration = rootDomainObject.readRegistrationByOID(studentId);
+		if (registration == null)
+		    throw new InvalidArgumentsServiceException();
+		Integer order = StudentTestQuestion.findStudentTestQuestion(oldQuestion, registration,
+			currentDistributedTest).getTestQuestionOrder();
+		studentsTestQuestionList = currentDistributedTest
+			.findStudentTestQuestionsByTestQuestionOrder(order);
+	    } else
+		studentsTestQuestionList = StudentTestQuestion.findStudentTestQuestions(oldQuestion,
+			currentDistributedTest);
 
-            List<InfoStudent> group = new ArrayList<InfoStudent>();
+	    List<InfoStudent> group = new ArrayList<InfoStudent>();
 
-            for (StudentTestQuestion studentTestQuestion : studentsTestQuestionList) {
-                if (!compareDates(studentTestQuestion.getDistributedTest().getEndDate(),
-                        studentTestQuestion.getDistributedTest().getEndHour())) {
-                    canDelete = false;
-                } else {
-                    if (availableQuestions.size() == 0)
-                        availableQuestions.addAll(getNewQuestionList(metadata, oldQuestion));
+	    for (StudentTestQuestion studentTestQuestion : studentsTestQuestionList) {
+		if (!compareDates(studentTestQuestion.getDistributedTest().getEndDate(),
+			studentTestQuestion.getDistributedTest().getEndHour())) {
+		    canDelete = false;
+		} else {
+		    if (availableQuestions.size() == 0)
+			availableQuestions.addAll(getNewQuestionList(metadata, oldQuestion));
 
-                    Question newQuestion = getNewQuestion(availableQuestions);
-                    if (newMetadataId == null
-                            && (newQuestion == null || newQuestion.equals(oldQuestion)))
-                        return Boolean.FALSE;
-                    else if (newQuestion == null)
-                        throw new InvalidArgumentsServiceException();
+		    Question newQuestion = getNewQuestion(availableQuestions);
+		    if (newMetadataId == null
+			    && (newQuestion == null || newQuestion.equals(oldQuestion)))
+			return Boolean.FALSE;
+		    else if (newQuestion == null)
+			throw new InvalidArgumentsServiceException();
 
-                    studentTestQuestion.setQuestion(newQuestion);
-                    studentTestQuestion.setItemId(null);
-                    studentTestQuestion.setResponse(null);
-                    studentTestQuestion.setOptionShuffle(null);
-                    availableQuestions.remove(newQuestion);
-                    double oldMark = studentTestQuestion.getTestQuestionMark().doubleValue();
-                    studentTestQuestion.setTestQuestionMark(new Double(0));
-                    if (!group.contains(studentTestQuestion.getStudent().getPerson()))
-                        group.add(InfoStudent.newInfoFromDomain(studentTestQuestion.getStudent()));
-                    if (studentTestQuestion.getDistributedTest().getTestType().equals(
-                            new TestType(TestType.EVALUATION))) {
-                        OnlineTest onlineTest = studentTestQuestion.getDistributedTest().getOnlineTest();
-                        Attends attend = studentTestQuestion.getStudent().readAttendByExecutionCourse(
-                                ((ExecutionCourse) currentDistributedTest.getTestScope()
-                                        .getDomainObject()));
-                        Mark mark = onlineTest.getMarkByAttend(attend);
-                        if (mark != null) {
-                            mark.setMark(getNewStudentMark(studentTestQuestion.getDistributedTest(),
-                                    studentTestQuestion.getStudent(), oldMark));
-                        }
-                    }
-                    StudentTestLog studentTestLog = new StudentTestLog();
-                    studentTestLog.setDistributedTest(studentTestQuestion.getDistributedTest());
-                    studentTestLog.setStudent(studentTestQuestion.getStudent());
-                    studentTestLog.setDate(Calendar.getInstance().getTime());
-                    ResourceBundle bundle = ResourceBundle.getBundle("resources.ApplicationResources",
-                            LanguageUtils.getLocale());
-                    studentTestLog.setEvent(MessageFormat.format(bundle
-                            .getString("message.changeStudentQuestionLogMessage"),
-                            new Object[] { studentTestQuestion.getTestQuestionOrder() }));
-                }
+		    studentTestQuestion.setQuestion(newQuestion);
+		    studentTestQuestion.setItemId(null);
+		    studentTestQuestion.setResponse(null);
+		    studentTestQuestion.setOptionShuffle(null);
+		    studentTestQuestion.setStudentSubQuestions(null);
+		    availableQuestions.remove(newQuestion);
+		    double oldMark = studentTestQuestion.getTestQuestionMark().doubleValue();
+		    studentTestQuestion.setTestQuestionMark(new Double(0));
+		    if (!group.contains(studentTestQuestion.getStudent().getPerson()))
+			group.add(InfoStudent.newInfoFromDomain(studentTestQuestion.getStudent()));
+		    if (studentTestQuestion.getDistributedTest().getTestType().equals(
+			    new TestType(TestType.EVALUATION))) {
+			OnlineTest onlineTest = studentTestQuestion.getDistributedTest().getOnlineTest();
+			Attends attend = studentTestQuestion.getStudent().readAttendByExecutionCourse(
+				((ExecutionCourse) currentDistributedTest.getTestScope()
+					.getDomainObject()));
+			Mark mark = onlineTest.getMarkByAttend(attend);
+			if (mark != null) {
+			    mark.setMark(getNewStudentMark(studentTestQuestion.getDistributedTest(),
+				    studentTestQuestion.getStudent(), oldMark));
+			}
+		    }
+		    StudentTestLog studentTestLog = new StudentTestLog();
+		    studentTestLog.setDistributedTest(studentTestQuestion.getDistributedTest());
+		    studentTestLog.setStudent(studentTestQuestion.getStudent());
+		    studentTestLog.setDate(Calendar.getInstance().getTime());
+		    ResourceBundle bundle = ResourceBundle.getBundle("resources.ApplicationResources",
+			    LanguageUtils.getLocale());
+		    studentTestLog.setEvent(MessageFormat.format(bundle
+			    .getString("message.changeStudentQuestionLogMessage"),
+			    new Object[] { studentTestQuestion.getTestQuestionOrder() }));
+		}
 
-            }
-        }
+	    }
+	}
 
-        if (delete.booleanValue()) {
-            metadata = oldQuestion.getMetadata();
-            if (metadata == null)
-                throw new InvalidArgumentsServiceException();
-            removeOldTestQuestion(oldQuestion);
-            List metadataQuestions = metadata.getVisibleQuestions();
+	if (delete.booleanValue()) {
+	    metadata = oldQuestion.getMetadata();
+	    if (metadata == null)
+		throw new InvalidArgumentsServiceException();
+	    removeOldTestQuestion(oldQuestion);
+	    List metadataQuestions = metadata.getVisibleQuestions();
 
-            if (metadataQuestions != null && metadataQuestions.size() <= 1)
-                metadata.setVisibility(Boolean.FALSE);
+	    if (metadataQuestions != null && metadataQuestions.size() <= 1)
+		metadata.setVisibility(Boolean.FALSE);
 
-            if (canDelete) {
-                oldQuestion.delete();
-                if (metadata.getQuestionsCount() <= 1) {
-                    metadata.delete();
-                }
-            } else {
-                oldQuestion.setVisibility(Boolean.FALSE);
-            }
+	    if (canDelete) {
+		oldQuestion.delete();
+		if (metadata.getQuestionsCount() <= 1) {
+		    metadata.delete();
+		}
+	    } else {
+		oldQuestion.setVisibility(Boolean.FALSE);
+	    }
 
-        }
-        return Boolean.TRUE;
+	}
+	return Boolean.TRUE;
     }
 
     private Question getNewQuestion(List<Question> questions) {
 
-        Question question = null;
-        if (questions.size() != 0) {
-            Random r = new Random();
-            int questionIndex = r.nextInt(questions.size());
-            question = questions.get(questionIndex);
-        }
-        return question;
+	Question question = null;
+	if (questions.size() != 0) {
+	    Random r = new Random();
+	    int questionIndex = r.nextInt(questions.size());
+	    question = questions.get(questionIndex);
+	}
+	return question;
     }
 
     private List<Question> getNewQuestionList(Metadata metadata, Question oldQuestion) {
-        List<Question> result = new ArrayList<Question>();
-        if (metadata != null) {
-            result.addAll(metadata.getVisibleQuestions());
-        } else {
-            result.addAll(oldQuestion.getMetadata().getVisibleQuestions());
-            result.remove(oldQuestion);
-        }
-        return result;
+	List<Question> result = new ArrayList<Question>();
+	if (metadata != null) {
+	    result.addAll(metadata.getVisibleQuestions());
+	} else {
+	    result.addAll(oldQuestion.getMetadata().getVisibleQuestions());
+	    result.remove(oldQuestion);
+	}
+	return result;
     }
 
     private boolean compareDates(Calendar date, Calendar hour) {
-        Calendar calendar = Calendar.getInstance();
-        CalendarDateComparator dateComparator = new CalendarDateComparator();
-        CalendarHourComparator hourComparator = new CalendarHourComparator();
-        if (dateComparator.compare(calendar, date) <= 0) {
-            if (dateComparator.compare(calendar, date) == 0) {
-                if (hourComparator.compare(calendar, hour) <= 0) {
-                    return true;
-                }
+	Calendar calendar = Calendar.getInstance();
+	CalendarDateComparator dateComparator = new CalendarDateComparator();
+	CalendarHourComparator hourComparator = new CalendarHourComparator();
+	if (dateComparator.compare(calendar, date) <= 0) {
+	    if (dateComparator.compare(calendar, date) == 0) {
+		if (hourComparator.compare(calendar, hour) <= 0) {
+		    return true;
+		}
 
-                return false;
-            }
-            return true;
-        }
-        return false;
+		return false;
+	    }
+	    return true;
+	}
+	return false;
     }
 
     private void removeOldTestQuestion(Question oldQuestion) {
-        List<TestQuestion> testQuestionOldList = oldQuestion.getTestQuestions();
-        List<Question> availableQuestions = new ArrayList<Question>();
-        availableQuestions.addAll(oldQuestion.getMetadata().getVisibleQuestions());
-        availableQuestions.remove(oldQuestion);
+	List<TestQuestion> testQuestionOldList = oldQuestion.getTestQuestions();
+	List<Question> availableQuestions = new ArrayList<Question>();
+	availableQuestions.addAll(oldQuestion.getMetadata().getVisibleQuestions());
+	availableQuestions.remove(oldQuestion);
 
-        Question newQuestion = getNewQuestion(availableQuestions);
+	Question newQuestion = getNewQuestion(availableQuestions);
 
-        if (newQuestion == null || newQuestion.equals(oldQuestion)) {
-            for (TestQuestion oldTestQuestion : testQuestionOldList) {
-                Test test = oldTestQuestion.getTest();
-                test.deleteTestQuestion(oldTestQuestion);
-            }
-        } else {
-            for (TestQuestion oldTestQuestion : testQuestionOldList) {
-                oldTestQuestion.setQuestion(newQuestion);
-            }
-        }
+	if (newQuestion == null || newQuestion.equals(oldQuestion)) {
+	    for (TestQuestion oldTestQuestion : testQuestionOldList) {
+		Test test = oldTestQuestion.getTest();
+		test.deleteTestQuestion(oldTestQuestion);
+	    }
+	} else {
+	    for (TestQuestion oldTestQuestion : testQuestionOldList) {
+		oldTestQuestion.setQuestion(newQuestion);
+	    }
+	}
     }
 
     private String getNewStudentMark(DistributedTest dt, Registration s, double mark2Remove)
-            throws ExcepcaoPersistencia {
-        double totalMark = 0;
-        Set<StudentTestQuestion> studentTestQuestionList = StudentTestQuestion.findStudentTestQuestions(
-                s, dt);
-        for (StudentTestQuestion studentTestQuestion : studentTestQuestionList) {
-            totalMark += studentTestQuestion.getTestQuestionMark().doubleValue();
-        }
-        DecimalFormat df = new DecimalFormat("#0.##");
-        DecimalFormatSymbols decimalFormatSymbols = df.getDecimalFormatSymbols();
-        decimalFormatSymbols.setDecimalSeparator('.');
-        df.setDecimalFormatSymbols(decimalFormatSymbols);
-        return (df.format(Math.max(0, totalMark)));
+	    throws ExcepcaoPersistencia {
+	double totalMark = 0;
+	Set<StudentTestQuestion> studentTestQuestionList = StudentTestQuestion.findStudentTestQuestions(
+		s, dt);
+	for (StudentTestQuestion studentTestQuestion : studentTestQuestionList) {
+	    totalMark += studentTestQuestion.getTestQuestionMark().doubleValue();
+	}
+	DecimalFormat df = new DecimalFormat("#0.##");
+	DecimalFormatSymbols decimalFormatSymbols = df.getDecimalFormatSymbols();
+	decimalFormatSymbols.setDecimalSeparator('.');
+	df.setDecimalFormatSymbols(decimalFormatSymbols);
+	return (df.format(Math.max(0, totalMark)));
     }
 
 }
