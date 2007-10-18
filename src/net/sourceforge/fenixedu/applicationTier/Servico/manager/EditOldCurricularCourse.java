02145e46be89bb3b2ca1b47fcd63646ab2b8962f
João Florindo <jdnf@linkare.pt>
2007-10-18 16:03:19 +0000

    new services to create curricular courses in migrate degree curricular plans

---
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/EditOldCurricularCourse.java
@@ -0,0 +1,25 @@
+package net.sourceforge.fenixedu.applicationTier.Servico.manager;
+
+import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.domain.CurricularCourse;
+
+public class EditOldCurricularCourse extends Service {
+
+    public void run(final Integer curricularCourseId, final String name, final String nameEn, final String code,
+	    final String acronym, final Integer minimumValueForAcumulatedEnrollments,
+	    final Integer maximumValueForAcumulatedEnrollments, final Double weigth, final Integer enrolmentWeigth,
+	    final Double credits, final Double ectsCredits) throws FenixServiceException {
+
+	final CurricularCourse curricularCourse = (CurricularCourse) rootDomainObject.readDegreeModuleByOID(curricularCourseId);
+	if (curricularCourse == null) {
+	    throw new FenixServiceException("error.createOldCurricularCourse.no.courseGroup");
+	}
+
+	curricularCourse.edit(name, nameEn, code, acronym, weigth, credits, ectsCredits);
+	curricularCourse.setEnrollmentWeigth(enrolmentWeigth);
+	curricularCourse.setMinimumValueForAcumulatedEnrollments(minimumValueForAcumulatedEnrollments);
+	curricularCourse.setMaximumValueForAcumulatedEnrollments(maximumValueForAcumulatedEnrollments);
+    }
+
+}
