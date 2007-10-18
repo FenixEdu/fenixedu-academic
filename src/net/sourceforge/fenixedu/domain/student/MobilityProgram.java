af839b622d28b354ee3013e2d7f0bb7bae4bbe75
Luís Egídio <luis.egidio@ist.utl.pt>
2007-10-18 10:41:44 +0000

    - new type of MobilityProgram - TODO: enrich RegistrationAgreement and remove MobilityProgram

--- src/net/sourceforge/fenixedu/domain/student/MobilityProgram.java
+++ src/net/sourceforge/fenixedu/domain/student/MobilityProgram.java
@@ -4,13 +4,22 @@ import java.util.ResourceBundle;
 
 import net.sourceforge.fenixedu.util.LanguageUtils;
 
+/**
+ * TODO: remove this, enrich RegistrationAgreement instead
+ * This enum shouldn't have been created. 
+ *
+ */
+
+@Deprecated
 public enum MobilityProgram {
 
     SOCRATES,
     
     ERASMUS,
 
-    MINERVA;
+    MINERVA,
+    
+    COVENANT_WITH_AZORES;
 
     public String getQualifiedName() {
 	return this.getClass().getSimpleName() + "." + name();
