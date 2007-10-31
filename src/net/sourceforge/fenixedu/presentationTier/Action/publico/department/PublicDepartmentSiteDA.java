2f0ec5da17a29a18a9c5461e6b5286f9a68cdaf2
Manuel Pinto <manuel.pinto@ist.utl.pt>
2007-10-31 16:48:09 +0000

    remove employees with RoleType Teacher in deparment employees listing.

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/department/PublicDepartmentSiteDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/department/PublicDepartmentSiteDA.java
@@ -17,6 +17,7 @@ import net.sourceforge.fenixedu.domain.Teacher;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Party;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.teacher.Category;
 import net.sourceforge.fenixedu.presentationTier.Action.publico.UnitSiteVisualizationDA;
 import net.sourceforge.fenixedu.presentationTier.servlets.filters.pathProcessors.DepartmentProcessor;
@@ -32,197 +33,202 @@ public class PublicDepartmentSiteDA extends UnitSiteVisualizationDA {
 
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	request.setAttribute("department", getDepartment(request));
-        return super.execute(mapping, actionForm, request, response);
+	request.setAttribute("department", getDepartment(request));
+	return super.execute(mapping, actionForm, request, response);
     }
 
     @Override
     protected ActionForward getSiteDefaultView(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        return presentation(mapping, form, request, response);
+	return presentation(mapping, form, request, response);
     }
 
     @Override
     protected String getContextParamName(HttpServletRequest request) {
-    	return "selectedDepartmentUnitID";
+	return "selectedDepartmentUnitID";
     }
-    
+
     @Override
     protected String getDirectLinkContext(HttpServletRequest request) {
-        Department department = getDepartment(request);
-        try {
-            return RequestUtils.absoluteURL(request, DepartmentProcessor.getDepartmentPath(department)).toString();
-        } catch (MalformedURLException e) {
-            return null;
-        }
+	Department department = getDepartment(request);
+	try {
+	    return RequestUtils.absoluteURL(request, DepartmentProcessor.getDepartmentPath(department)).toString();
+	} catch (MalformedURLException e) {
+	    return null;
+	}
     }
-    
+
     private Department getDepartment(HttpServletRequest request) {
-        Unit unit = getUnit(request);
-        if (unit == null) {
-            return null;
-        }
-        else {
-            return unit.getDepartment();
-        }
+	Unit unit = getUnit(request);
+	if (unit == null) {
+	    return null;
+	}
+	else {
+	    return unit.getDepartment();
+	}
     }
 
     public ActionForward employees(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) {
-        Unit unit = getUnit(request);
-        
-        BeanComparator employeeComparator = new BeanComparator("person", Party.COMPARATOR_BY_NAME_AND_ID);
-        
-        SortedSet<Unit> workingUnits = new TreeSet<Unit>(Unit.COMPARATOR_BY_NAME_AND_ID);
-		SortedSet<Employee> noUnitAvailable = new TreeSet<Employee>(employeeComparator);
-        Map<String, SortedSet<Employee>> employeesMap = new Hashtable<String, SortedSet<Employee>>();
-        
-        for (Employee employee : unit.getAllCurrentNonTeacherEmployees()) {
-        	Unit workingUnit = employee.getCurrentWorkingPlace();
-        	
-        	if (workingUnit != null) {
-        		workingUnits.add(workingUnit);
-        		
-        		String areaKey = workingUnit.getIdInternal().toString();
-				SortedSet<Employee> employees = employeesMap.get(areaKey);
-        		if (employees == null) {
-        			employees = new TreeSet<Employee>(employeeComparator);
-        			employeesMap.put(areaKey, employees);
-        		}
-        		
-        		employees.add(employee);
-        	}
-        	else {
-        		noUnitAvailable.add(employee);
-        	}
-        }
-        
-        if (workingUnits.isEmpty()) {
-        	request.setAttribute("ignoreAreas", true);
-        }
-        
-        request.setAttribute("areas", workingUnits);
-        request.setAttribute("employees", employeesMap);
-        request.setAttribute("employeesNoArea", noUnitAvailable);
-        
-        return mapping.findForward("department-employees");
+	Unit unit = getUnit(request);
+
+	BeanComparator employeeComparator = new BeanComparator("person", Party.COMPARATOR_BY_NAME_AND_ID);
+
+	SortedSet<Unit> workingUnits = new TreeSet<Unit>(Unit.COMPARATOR_BY_NAME_AND_ID);
+	SortedSet<Employee> noUnitAvailable = new TreeSet<Employee>(employeeComparator);
+	Map<String, SortedSet<Employee>> employeesMap = new Hashtable<String, SortedSet<Employee>>();
+
+	for (Employee employee : unit.getAllCurrentNonTeacherEmployees()) {
+
+	    if(employee.getPerson().hasRole(RoleType.TEACHER)) {
+		continue;
+	    }
+	    
+	    Unit workingUnit = employee.getCurrentWorkingPlace();
+
+	    if (workingUnit != null) {
+		workingUnits.add(workingUnit);
+
+		String areaKey = workingUnit.getIdInternal().toString();
+		SortedSet<Employee> employees = employeesMap.get(areaKey);
+		if (employees == null) {
+		    employees = new TreeSet<Employee>(employeeComparator);
+		    employeesMap.put(areaKey, employees);
+		}
+
+		employees.add(employee);
+	    }
+	    else {
+		noUnitAvailable.add(employee);
+	    }
+	}
+
+	if (workingUnits.isEmpty()) {
+	    request.setAttribute("ignoreAreas", true);
+	}
+
+	request.setAttribute("areas", workingUnits);
+	request.setAttribute("employees", employeesMap);
+	request.setAttribute("employeesNoArea", noUnitAvailable);
+
+	return mapping.findForward("department-employees");
     }
 
     public ActionForward degrees(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) {
-        Unit unit = getUnit(request);
-        
-        Department department = unit.getDepartment();
-        if (department == null) {
-            return presentation(mapping, actionForm, request, response);
-        }
-        
-        Map<DegreeType, SortedSet<Degree>> degreeAndTypes = new HashMap<DegreeType, SortedSet<Degree>>();
-        
-        for (Degree degree : department.getDegrees()) {
-            DegreeType type = degree.getDegreeType();
-            
-            SortedSet<Degree> current = degreeAndTypes.get(type);
-            if (current == null) {
-                current = new TreeSet<Degree>(Degree.COMPARATOR_BY_NAME_AND_ID);
-                degreeAndTypes.put(type, current);
-            }
-            
-            current.add(degree);
-        }
-        
-        SortedSet<DegreeType> types = new TreeSet<DegreeType>(degreeAndTypes.keySet());
-        request.setAttribute("types", types);
-        
-        for (DegreeType type : types) {
-            request.setAttribute(type.getName(), degreeAndTypes.get(type));
-        }
+	Unit unit = getUnit(request);
 
-        return mapping.findForward("department-degrees");
+	Department department = unit.getDepartment();
+	if (department == null) {
+	    return presentation(mapping, actionForm, request, response);
+	}
+
+	Map<DegreeType, SortedSet<Degree>> degreeAndTypes = new HashMap<DegreeType, SortedSet<Degree>>();
+
+	for (Degree degree : department.getDegrees()) {
+	    DegreeType type = degree.getDegreeType();
+
+	    SortedSet<Degree> current = degreeAndTypes.get(type);
+	    if (current == null) {
+		current = new TreeSet<Degree>(Degree.COMPARATOR_BY_NAME_AND_ID);
+		degreeAndTypes.put(type, current);
+	    }
+
+	    current.add(degree);
+	}
+
+	SortedSet<DegreeType> types = new TreeSet<DegreeType>(degreeAndTypes.keySet());
+	request.setAttribute("types", types);
+
+	for (DegreeType type : types) {
+	    request.setAttribute(type.getName(), degreeAndTypes.get(type));
+	}
+
+	return mapping.findForward("department-degrees");
     }
-    
+
     public ActionForward teachers(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) {
-        Unit unit = getUnit(request);
-        
-        Department department = unit.getDepartment();
-        if (department == null) {
-            return presentation(mapping, actionForm, request, response);
-        }
+	Unit unit = getUnit(request);
 
-        String mode = getTeachersMode(request.getParameter("viewBy"));
-        
-        if (mode.equals("category")) {
-        	setupTeachersCategories(request, department);
-        }
-        else if (mode.equals("area")) {
-        	setupTeachersAreas(request, department);
-        }
-        
-        return mapping.findForward("department-teachers-" + mode);
+	Department department = unit.getDepartment();
+	if (department == null) {
+	    return presentation(mapping, actionForm, request, response);
+	}
+
+	String mode = getTeachersMode(request.getParameter("viewBy"));
+
+	if (mode.equals("category")) {
+	    setupTeachersCategories(request, department);
+	}
+	else if (mode.equals("area")) {
+	    setupTeachersAreas(request, department);
+	}
+
+	return mapping.findForward("department-teachers-" + mode);
     }
 
-	private String getTeachersMode(String parameter) {
-		if (parameter == null) {
-			return "category";
-		}
-		
-		if (parameter.equals("category") || parameter.equals("area")) {
-			return parameter;
-		}
-		
-		return "category";
+    private String getTeachersMode(String parameter) {
+	if (parameter == null) {
+	    return "category";
 	}
 
-	private void setupTeachersCategories(HttpServletRequest request, Department department) {
-		YearMonthDay today = new YearMonthDay();
-		YearMonthDay tomorrow = today.plusDays(1);
-		
-		SortedSet<Category> categories = new TreeSet<Category>();
-		Map<String, SortedSet<Teacher>> teachers = new Hashtable<String, SortedSet<Teacher>>();
-		
-		for (Teacher teacher : department.getAllTeachers(today, tomorrow)) {
-			categories.add(teacher.getCategory());
-			addListTeacher(teachers, teacher.getCategory().getCode(), teacher);
-		}
-		
-		request.setAttribute("categories", categories);
-		request.setAttribute("teachers", teachers);
+	if (parameter.equals("category") || parameter.equals("area")) {
+	    return parameter;
 	}
 
-	private void setupTeachersAreas(HttpServletRequest request, Department department) {
-		YearMonthDay today = new YearMonthDay();
-		YearMonthDay tomorrow = today.plusDays(1);
-		
-		SortedSet<Unit> areas = new TreeSet<Unit>(Unit.COMPARATOR_BY_NAME_AND_ID);
-		SortedSet<Teacher> teachersNoArea = new TreeSet<Teacher>(Teacher.TEACHER_COMPARATOR_BY_CATEGORY_AND_NUMBER);
-		Map<String, SortedSet<Teacher>> teachers = new Hashtable<String, SortedSet<Teacher>>();
-		
-		for (Teacher teacher : department.getAllTeachers(today, tomorrow)) {
-			Unit area = teacher.getCurrentSectionOrScientificArea();
-			
-			if (area != null) {
-				areas.add(area);
-				addListTeacher(teachers, area.getIdInternal().toString(), teacher);
-			}
-			else {
-				teachersNoArea.add(teacher);
-			}
-		}
+	return "category";
+    }
 
-		if (areas.isEmpty()) {
-			request.setAttribute("ignoreAreas", true);
-		}
-		
-		request.setAttribute("areas", areas);
-		request.setAttribute("teachers", teachers);
-		request.setAttribute("teachersNoArea", teachersNoArea);
+    private void setupTeachersCategories(HttpServletRequest request, Department department) {
+	YearMonthDay today = new YearMonthDay();
+	YearMonthDay tomorrow = today.plusDays(1);
+
+	SortedSet<Category> categories = new TreeSet<Category>();
+	Map<String, SortedSet<Teacher>> teachers = new Hashtable<String, SortedSet<Teacher>>();
+
+	for (Teacher teacher : department.getAllTeachers(today, tomorrow)) {
+	    categories.add(teacher.getCategory());
+	    addListTeacher(teachers, teacher.getCategory().getCode(), teacher);
 	}
-	
-	private void addListTeacher(Map<String, SortedSet<Teacher>> teachersMap, String key, Teacher teacher) {
-		SortedSet<Teacher> teachers = teachersMap.get(key);
-		
-		if (teachers == null) {
-			teachers = new TreeSet<Teacher>(Teacher.TEACHER_COMPARATOR_BY_CATEGORY_AND_NUMBER);
-			teachersMap.put(key, teachers);
-		}
-		
-		teachers.add(teacher);
+
+	request.setAttribute("categories", categories);
+	request.setAttribute("teachers", teachers);
+    }
+
+    private void setupTeachersAreas(HttpServletRequest request, Department department) {
+	YearMonthDay today = new YearMonthDay();
+	YearMonthDay tomorrow = today.plusDays(1);
+
+	SortedSet<Unit> areas = new TreeSet<Unit>(Unit.COMPARATOR_BY_NAME_AND_ID);
+	SortedSet<Teacher> teachersNoArea = new TreeSet<Teacher>(Teacher.TEACHER_COMPARATOR_BY_CATEGORY_AND_NUMBER);
+	Map<String, SortedSet<Teacher>> teachers = new Hashtable<String, SortedSet<Teacher>>();
+
+	for (Teacher teacher : department.getAllTeachers(today, tomorrow)) {
+	    Unit area = teacher.getCurrentSectionOrScientificArea();
+
+	    if (area != null) {
+		areas.add(area);
+		addListTeacher(teachers, area.getIdInternal().toString(), teacher);
+	    }
+	    else {
+		teachersNoArea.add(teacher);
+	    }
 	}
+
+	if (areas.isEmpty()) {
+	    request.setAttribute("ignoreAreas", true);
+	}
+
+	request.setAttribute("areas", areas);
+	request.setAttribute("teachers", teachers);
+	request.setAttribute("teachersNoArea", teachersNoArea);
+    }
+
+    private void addListTeacher(Map<String, SortedSet<Teacher>> teachersMap, String key, Teacher teacher) {
+	SortedSet<Teacher> teachers = teachersMap.get(key);
+
+	if (teachers == null) {
+	    teachers = new TreeSet<Teacher>(Teacher.TEACHER_COMPARATOR_BY_CATEGORY_AND_NUMBER);
+	    teachersMap.put(key, teachers);
+	}
+
+	teachers.add(teacher);
+    }
 }
