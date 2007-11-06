2cb985891570113866d99f9a8e1903ee948fdd01
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2007-11-06 12:55:27 +0000

    Bug fix on delete

--- src/net/sourceforge/fenixedu/domain/messaging/ConversationMessage.java
+++ src/net/sourceforge/fenixedu/domain/messaging/ConversationMessage.java
@@ -13,67 +13,73 @@ import org.apache.commons.beanutils.BeanComparator;
 public class ConversationMessage extends ConversationMessage_Base {
 
     public static class DateComparatorOldestFirst implements Comparator<java.util.Date> {
-        public int compare(Date o1, Date o2) {
-            return -o1.compareTo(o2);
-        }
+	public int compare(Date o1, Date o2) {
+	    return -o1.compareTo(o2);
+	}
     }
 
-    public static final Comparator CONVERSATION_MESSAGE_COMPARATOR_BY_CREATION_DATE = new BeanComparator(
-            "creationDate", new DateComparatorOldestFirst());
+    public static final Comparator CONVERSATION_MESSAGE_COMPARATOR_BY_CREATION_DATE = new BeanComparator("creationDate",
+	    new DateComparatorOldestFirst());
 
     public ConversationMessage() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
-        setCreationDate(Calendar.getInstance().getTime());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+	setCreationDate(Calendar.getInstance().getTime());
     }
 
     public ConversationMessage(ConversationThread conversationThread, Person creator, String body) {
-        this();
-        init(conversationThread, creator, body);
+	this();
+	init(conversationThread, creator, body);
     }
 
     private void init(ConversationThread conversationThread, Person creator, String body) {
-        setConversationThread(conversationThread);
-        setBody(body);
-        setCreator(creator);
+	setConversationThread(conversationThread);
+	setBody(body);
+	setCreator(creator);
     }
 
     public void delete() {
-        removeConversationThread();
-        removeRootDomainObject();
-        super.deleteDomainObject();
+	removeConversationThread();
+	removeRootDomainObject();
+	removeCreator();
+	super.deleteDomainObject();
     }
 
     @Override
     public void removeConversationThread() {
-        super.setConversationThread(null);
+	super.setConversationThread(null);
+    }
+
+    @Override
+    public void removeCreator() {
+	super.setCreator(null);
     }
 
     @Override
     public void setCreator(Person creator) {
-        if (creator == null) {
-            throw new DomainException("conversationMessage.creator.cannot.be.null");
-        }
+	if (creator == null) {
+	    throw new DomainException("conversationMessage.creator.cannot.be.null");
+	}
 
-        super.setCreator(creator);
+	super.setCreator(creator);
     }
 
     @Override
     public void setBody(String body) {
-        if (body == null) {
-            throw new DomainException("conversationMessage.body.cannot.be.null");
-        }
+	if (body == null) {
+	    throw new DomainException("conversationMessage.body.cannot.be.null");
+	}
 
-        super.setBody(body);
+	super.setBody(body);
     }
 
     @Override
     public void setConversationThread(ConversationThread conversationThread) {
-        if (conversationThread == null) {
-            throw new DomainException("conversationMessage.conversationThread.cannot.be.null");
-        }
+	if (conversationThread == null) {
+	    throw new DomainException("conversationMessage.conversationThread.cannot.be.null");
+	}
 
-        super.setConversationThread(conversationThread);
+	super.setConversationThread(conversationThread);
     }
 
 }
