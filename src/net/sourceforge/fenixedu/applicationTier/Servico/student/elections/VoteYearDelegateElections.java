6487a2aa22544bd06539e42b5fc785a1fc16cb76
Ângela Almeida <angela.almeida@ist.utl.pt>
2007-11-06 17:48:15 +0000

    new email()

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/elections/VoteYearDelegateElections.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/elections/VoteYearDelegateElections.java
@@ -1,11 +1,15 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.student.elections;
 
+import java.util.Collections;
+
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.elections.DelegateElectionVote;
 import net.sourceforge.fenixedu.domain.elections.YearDelegateElection;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.student.Student;
+import net.sourceforge.fenixedu.domain.util.Email;
 import net.sourceforge.fenixedu.persistenceTier.ExcepcaoPersistencia;
 
 public class VoteYearDelegateElections extends Service  {
@@ -14,9 +18,13 @@ public class VoteYearDelegateElections extends Service  {
 		
 		try {
 			if(!yearDelegateElection.getVotingStudents().contains(student)) {
+				final String msg = "A sua votação para a eleição de Delegado de Ano encontra-se registada. Obrigado pela sua participação.";
+				final Person person = student.getPerson();
 				DelegateElectionVote vote = new DelegateElectionVote(yearDelegateElection, votedStudent);
 				yearDelegateElection.addVotingStudents(student);
 				yearDelegateElection.addVotes(vote);
+				new Email("Comissão Eleitoral de Delegados de Ano", "ce-delegados-ano@mlists.ist.utl.pt", null, Collections
+					    .singletonList(person.getEmail()), null, null, "Eleição de Delegado de Ano", msg);
 			}
 			else {
 				throw new FenixServiceException("error.student.elections.voting.studentAlreadyVoted");
