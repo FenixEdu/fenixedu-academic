ebf8abe1fb866db00b9d244ce15b11f2ad0d2cf0
Ângela Almeida <angela.almeida@ist.utl.pt>
2007-11-28 12:56:15 +0000

    checkParametres method added

--- src/net/sourceforge/fenixedu/domain/elections/DelegateElectionVote.java
+++ src/net/sourceforge/fenixedu/domain/elections/DelegateElectionVote.java
@@ -1,6 +1,8 @@
 package net.sourceforge.fenixedu.domain.elections;
 
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.student.Student;
 
 public class DelegateElectionVote extends DelegateElectionVote_Base {
@@ -12,6 +14,7 @@ public class DelegateElectionVote extends DelegateElectionVote_Base {
     
     public DelegateElectionVote(DelegateElection election, Student student) {
     	this();
+    	checkParameters(student, election);
     	setDelegateElection(election);
     	setStudent(student);
     }
@@ -22,5 +25,13 @@ public class DelegateElectionVote extends DelegateElectionVote_Base {
     	removeRootDomainObject();
     	super.deleteDomainObject();
     }
+    private void checkParameters(final Student student, final DelegateElection election) {
+    	if (student == null) {
+    	    throw new DomainException("error.student.cannot.be.null");
+    	}
+    	if (election == null) {
+    	    throw new DomainException("error.delegateElection.cannot.be.null");
+    	}
+        }
     
 }
