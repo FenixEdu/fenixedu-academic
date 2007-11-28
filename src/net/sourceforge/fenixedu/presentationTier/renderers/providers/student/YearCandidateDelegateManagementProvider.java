46b0384d3d4722f4bbb33544a2c1976082c8aa27
Ângela Almeida <angela.almeida@ist.utl.pt>
2007-11-28 13:11:16 +0000

    new Provider of Delegate Managment

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/student/YearCandidateDelegateManagementProvider.java
@@ -0,0 +1,42 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers.student;
+
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanComparator;
+
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.dataTransferObject.student.elections.StudentVoteBean;
+import net.sourceforge.fenixedu.domain.CurricularYear;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.Teacher;
+import net.sourceforge.fenixedu.domain.elections.DelegateElection;
+import net.sourceforge.fenixedu.domain.elections.DelegateElectionPeriod;
+import net.sourceforge.fenixedu.domain.elections.YearDelegateElection;
+import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.student.Student;
+import net.sourceforge.fenixedu.injectionCode.AccessControl;
+import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+import net.sourceforge.fenixedu.renderers.DataProvider;
+import net.sourceforge.fenixedu.renderers.components.converters.Converter;
+
+
+
+public class YearCandidateDelegateManagementProvider implements DataProvider {
+
+    public Object provide(Object source, Object currentValue) {
+    	final StudentVoteBean studentVoteBean = (StudentVoteBean) source;
+    	return studentVoteBean.getSelectedStudentVote("candidate");
+    }
+
+    public Converter getConverter() {
+        return new DomainObjectKeyConverter();
+    }
+    
+   
+}
