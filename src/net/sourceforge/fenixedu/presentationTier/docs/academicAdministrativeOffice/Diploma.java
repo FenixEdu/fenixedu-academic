174ef60b4cabf358551e9d95cdb113719b71c8e4
Luís Egídio <luis.egidio@ist.utl.pt>
2007-11-30 12:54:35 +0000

    bug fix: take into account cycle curriculum group or registration when appropriate

--- src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/Diploma.java
+++ src/net/sourceforge/fenixedu/presentationTier/docs/academicAdministrativeOffice/Diploma.java
@@ -1,5 +1,6 @@
 package net.sourceforge.fenixedu.presentationTier.docs.academicAdministrativeOffice;
 
+import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.ResourceBundle;
 
@@ -11,6 +12,8 @@ import net.sourceforge.fenixedu.domain.organizationalStructure.UniversityUnit;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DiplomaRequest;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DocumentRequest;
 import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.student.curriculum.ICurriculum;
+import net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup;
 import net.sourceforge.fenixedu.util.LanguageUtils;
 import net.sourceforge.fenixedu.util.StringFormatter;
 
@@ -41,20 +44,86 @@ public class Diploma extends AdministrativeOfficeDocument {
 	parameters.put("nameOfMother", StringFormatter.prettyPrint(person.getNameOfMother()));
 	parameters.put("birthLocale", StringFormatter.prettyPrint(person.getDistrictOfBirth()));
 
-	final CycleType requestedCycle = diplomaRequest.getRequestedCycle();
-	parameters.put("conclusionDate", registration.getConclusionDate(requestedCycle).toString("dd 'de' MMMM 'de' yyyy", LanguageUtils.getLocale()));
+
+	parameters.put("conclusionDate", getConclusionDate().toString("dd 'de' MMMM 'de' yyyy", LanguageUtils.getLocale()));
 	parameters.put("institutionName", RootDomainObject.getInstance().getInstitutionUnit().getName());
 	parameters.put("day", new YearMonthDay().toString("dd 'de' MMMM 'de' yyyy", LanguageUtils.getLocale()));
 
 	if (diplomaRequest.hasFinalAverageDescription()) {
-	    parameters.put("finalAverageDescription", StringUtils.capitalize(registration.getFinalAverageDescription(requestedCycle)));
+	    parameters.put("finalAverageDescription", StringUtils.capitalize(ResourceBundle.getBundle("resources.EnumerationResources").getString(
+			getFinalAverage().toString())));
+	    parameters.put("finalAverageQualified", registration.getDegreeType().getGradeScale().getQualifiedName(getFinalAverage().toString()));
 	} else if (diplomaRequest.hasDissertationTitle()) {
 	    parameters.put("dissertationTitle", registration.getDissertationEnrolment().getThesis().getFinalFullTitle().getContent());
 	}
 	
 	parameters.put("conclusionStatus", getConclusionStatusAndDegreeType(diplomaRequest, registration));
-	parameters.put("graduateTitle", registration.getGraduateTitle(requestedCycle));
+
+	final CycleType cycleToInspect = getCycleToInspect(diplomaRequest, registration);
+	parameters.put("graduateTitle", registration.getGraduateTitle(cycleToInspect));
+	
 	parameters.put("degreeDescription", getDegreeDescription());
+    }
+
+    private CycleType getCycleToInspect(final DiplomaRequest diplomaRequest, final Registration registration) {
+	final CycleType cycleToInspect;
+	if (diplomaRequest.getRequestedCycle() == null) {
+	    if (registration.getDegreeType().hasExactlyOneCycleType()) {
+		cycleToInspect = registration.getLastStudentCurricularPlan().getLastCycleCurriculumGroup().getCycleType();
+	    } else {
+		cycleToInspect = null;
+	    }
+	} else {
+	    cycleToInspect = diplomaRequest.getRequestedCycle();
+	}
+	return cycleToInspect;
+    }
+
+    
+    public Integer getFinalAverage() {
+	return hasCycleCurriculumGroup() ? getCycleCurriculumGroup().calculateRoundedAverage() : getRegistration()
+		.calculateFinalAverage();
+    }
+
+    public BigDecimal getAverage() {
+	return hasCycleCurriculumGroup() ? getCycleCurriculumGroup().calculateAverage() : getRegistration().getAverage();
+    }
+
+    public YearMonthDay getConclusionDate() {
+	return hasCycleCurriculumGroup() ? getCycleCurriculumGroup().calculateConclusionDate() : getRegistration()
+		.calculateConclusionDate();
+    }
+
+    public double getEctsCredits() {
+	return hasCycleCurriculumGroup() ? getCycleCurriculumGroup().getCreditsConcluded() : getRegistration().getEctsCredits();
+    }
+
+    public ICurriculum getCurriculum() {
+	return hasCycleCurriculumGroup() ? getCycleCurriculumGroup().getCurriculum() : getRegistration().getCurriculum();
+    }
+
+    private boolean hasCycleCurriculumGroup() {
+	return getCycleCurriculumGroup() != null;
+    }
+
+    private CycleCurriculumGroup getCycleCurriculumGroup() {
+	final DiplomaRequest diplomaRequest = (DiplomaRequest) getDocumentRequest();
+	final CycleType requestedCycle = diplomaRequest.getRequestedCycle();
+	final Registration registration = getRegistration();
+
+	if (requestedCycle == null) {
+	    if (registration.getDegreeType().hasExactlyOneCycleType()) {
+		return registration.getLastStudentCurricularPlan().getLastCycleCurriculumGroup();
+	    } else {
+		return null;
+	    }
+	} else {
+	    return registration.getLastStudentCurricularPlan().getCycle(requestedCycle);
+	}
+    }
+
+    private Registration getRegistration() {
+	return getDocumentRequest().getRegistration();
     }
 
     final private String getConclusionStatusAndDegreeType(final DiplomaRequest diplomaRequest, final Registration registration) {
