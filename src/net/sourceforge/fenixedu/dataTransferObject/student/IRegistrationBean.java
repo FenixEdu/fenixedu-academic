c23bc433fb07bf4bcdb63d964c44f2022b6a1080
Luís Egídio <luis.egidio@ist.utl.pt>
2007-12-04 14:26:35 +0000

    IRegistrationBean

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/student/IRegistrationBean.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.dataTransferObject.student;
+
+import net.sourceforge.fenixedu.domain.student.Registration;
+
+public interface IRegistrationBean {
+
+    public Registration getRegistration();
+    
+}
