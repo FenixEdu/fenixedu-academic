71c65910d8145ec613e897484668ae3ccf352e2c
Luís Egídio <luis.egidio@ist.utl.pt>
2007-12-05 12:42:49 +0000

    - new schemas - generalized a provider

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/student/RegistrationInternalCycleCurriculumGroupsProvider.java
@@ -0,0 +1,27 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers.student;
+
+import java.util.Collection;
+import java.util.TreeSet;
+
+import net.sourceforge.fenixedu.dataTransferObject.student.IRegistrationBean;
+import net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup;
+import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+import net.sourceforge.fenixedu.renderers.DataProvider;
+import net.sourceforge.fenixedu.renderers.components.converters.Converter;
+
+public class RegistrationInternalCycleCurriculumGroupsProvider implements DataProvider {
+
+    public Converter getConverter() {
+	return new DomainObjectKeyConverter();
+    }
+
+    public Object provide(Object source, Object currentValue) {
+	final IRegistrationBean iRegistrationBean = (IRegistrationBean) source;
+
+	final Collection<CycleCurriculumGroup> result = new TreeSet<CycleCurriculumGroup>(CycleCurriculumGroup.COMPARATOR_BY_CYCLE_TYPE_AND_ID);
+	result.addAll(iRegistrationBean.getRegistration().getLastStudentCurricularPlan().getInternalCycleCurriculumGrops());
+	
+	return result;
+    }
+
+}
