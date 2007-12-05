55a4488d21cc2418606b63844b807a5f3996e63e
Shezad Anavarali <shezad@ist.utl.pt>
2007-12-05 19:49:45 +0000

    fix

--- src/net/sourceforge/fenixedu/dataTransferObject/degreeAdministrativeOffice/gradeSubmission/MarkSheetSearchResultBean.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/degreeAdministrativeOffice/gradeSubmission/MarkSheetSearchResultBean.java
@@ -10,64 +10,76 @@ import net.sourceforge.fenixedu.domain.MarkSheet;
 import net.sourceforge.fenixedu.domain.MarkSheetState;
 
 public class MarkSheetSearchResultBean {
-    
+
     private boolean showStatistics;
+
     private int totalNumberOfStudents;
+
     private List<MarkSheet> markSheets;
-    
+
     public MarkSheetSearchResultBean() {
-        markSheets = new ArrayList<MarkSheet>();  
+	markSheets = new ArrayList<MarkSheet>();
     }
 
     public List<MarkSheet> getMarkSheets() {
-        return markSheets;
+	return markSheets;
     }
-    
+
     public Collection<MarkSheet> getMarkSheetsSortedByEvaluationDate() {
-        Collections.sort(getMarkSheets(), new Comparator<MarkSheet>() {
-            public int compare(MarkSheet o1, MarkSheet o2) {
-                return o1.getEvaluationDateDateTime().compareTo(o2.getEvaluationDateDateTime()); 
-            }
-        });
-        return getMarkSheets();
+	Collections.sort(getMarkSheets(), new Comparator<MarkSheet>() {
+	    public int compare(MarkSheet o1, MarkSheet o2) {
+		if (o1.getEvaluationDateDateTime() == null && o2.getEvaluationDateDateTime() == null) {
+		    return 0;
+		}
+		if (o1.getEvaluationDateDateTime() == null) {
+		    return -1;
+		}
+		if (o2.getEvaluationDateDateTime() == null) {
+		    return 1;
+		}
+
+		return o1.getEvaluationDateDateTime().compareTo(o2.getEvaluationDateDateTime());
+	    }
+	});
+	return getMarkSheets();
     }
 
     public void setMarkSheets(List<MarkSheet> markSheets) {
-        this.markSheets = markSheets;
+	this.markSheets = markSheets;
     }
 
     public void addMarkSheet(MarkSheet markSheet) {
-        getMarkSheets().add(markSheet);
+	getMarkSheets().add(markSheet);
     }
 
     public int getNumberOfEnroledStudents() {
-        int numberOfEnroledStudents = 0;
-        for (MarkSheet markSheet : getMarkSheets()) {
-            if (! isRectificationMarkSheet(markSheet.getMarkSheetState())) {
-                numberOfEnroledStudents += markSheet.getEnrolmentEvaluationsCount();
-            }
-        }
-        return numberOfEnroledStudents;
+	int numberOfEnroledStudents = 0;
+	for (MarkSheet markSheet : getMarkSheets()) {
+	    if (!isRectificationMarkSheet(markSheet.getMarkSheetState())) {
+		numberOfEnroledStudents += markSheet.getEnrolmentEvaluationsCount();
+	    }
+	}
+	return numberOfEnroledStudents;
     }
 
     private boolean isRectificationMarkSheet(MarkSheetState markSheetState) {
-        return (markSheetState == MarkSheetState.RECTIFICATION || markSheetState == MarkSheetState.RECTIFICATION_NOT_CONFIRMED);
+	return (markSheetState == MarkSheetState.RECTIFICATION || markSheetState == MarkSheetState.RECTIFICATION_NOT_CONFIRMED);
     }
 
     public int getTotalNumberOfStudents() {
-        return totalNumberOfStudents;
+	return totalNumberOfStudents;
     }
 
     public void setTotalNumberOfStudents(int totalNumberStudents) {
-        this.totalNumberOfStudents = totalNumberStudents;
+	this.totalNumberOfStudents = totalNumberStudents;
     }
 
     public boolean isShowStatistics() {
-        return showStatistics;
+	return showStatistics;
     }
 
     public void setShowStatistics(boolean showStatistics) {
-        this.showStatistics = showStatistics;
+	this.showStatistics = showStatistics;
     }
 
 }
