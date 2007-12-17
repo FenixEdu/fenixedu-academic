d2415e4b705420d960f558a3093de2993b9a2f56
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2007-12-17 19:04:52 +0000

    added checksum

--- src/net/sourceforge/fenixedu/presentationTier/Action/CreateContentsContextAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/CreateContentsContextAction.java
@@ -1,6 +1,5 @@
 package net.sourceforge.fenixedu.presentationTier.Action;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,6 +10,7 @@ import net.sourceforge.fenixedu.domain.contents.MetaDomainObjectPortal;
 import net.sourceforge.fenixedu.domain.contents.Portal;
 import net.sourceforge.fenixedu.domain.functionalities.FunctionalityContext;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixAction;
+import net.sourceforge.fenixedu.presentationTier.servlets.filters.ChecksumRewriter;
 import net.sourceforge.fenixedu.presentationTier.servlets.filters.functionalities.FilterFunctionalityContext;
 
 import org.apache.struts.action.ActionForm;
@@ -18,30 +18,33 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 public class CreateContentsContextAction extends FenixAction {
-    
+
     @Override
     public ActionForward execute(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-	
+	    HttpServletResponse response) throws Exception {
+
 	final FunctionalityContext functionalityContext = new FilterFunctionalityContext(request);
 	request.setAttribute(FunctionalityContext.CONTEXT_KEY, functionalityContext);
-	
+
 	final MenuEntry initialMenuEntry = getInitialMenuEntry(functionalityContext);
 	Content content = initialMenuEntry.getReferingContent();
-	if(content.isContainer()) {
+	if (content.isContainer()) {
 	    Container container = (Container) content;
-	    if(container.getInitialContent() != null) {
+	    if (container.getInitialContent() != null) {
 		content = container.getInitialContent();
 	    }
 	}
-	
-	return menuActionForward(content);
+
+	return menuActionForward(content, request);
     }
-    
-    private ActionForward menuActionForward(Content content) {
+
+    private ActionForward menuActionForward(Content content, HttpServletRequest request) {
 	ActionForward actionForward = new ActionForward();
 	actionForward.setRedirect(true);
-	actionForward.setPath(content.getPath());
+	final String path = content.getPath();
+	final String seperator = path.indexOf('?') >= 0 ? "&" : "?";
+	actionForward.setPath(path + seperator + ChecksumRewriter.CHECKSUM_ATTRIBUTE_NAME + "="
+		+ ChecksumRewriter.calculateChecksum(request.getContextPath() + path));
 	return actionForward;
     }
 
