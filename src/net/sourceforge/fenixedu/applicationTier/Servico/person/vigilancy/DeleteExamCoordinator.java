74021f2231d01b6665c9dfd628ad224f7b475f29
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-01-03 16:55:23 +0000

    Bug fix regarding vigilancies and not all the content refactoring. Since it's a low priority fix no need to commit it to the patches branch.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/person/vigilancy/DeleteExamCoordinator.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/person/vigilancy/DeleteExamCoordinator.java
@@ -1,6 +1,7 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.person.vigilancy;
 
 import net.sourceforge.fenixedu.applicationTier.Service;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.vigilancy.ExamCoordinator;
@@ -11,9 +12,11 @@ public class DeleteExamCoordinator extends Service {
     public void run(ExamCoordinator coordinator) throws ExcepcaoPersistencia {
 
         Person person = coordinator.getPerson();
-        person.removeRoleByType(RoleType.EXAM_COORDINATOR);
         coordinator.delete();
 
+        if(person.hasRole(RoleType.EXAM_COORDINATOR) && person.getExamCoordinatorForGivenExecutionYear(ExecutionYear.readCurrentExecutionYear()) != null) {
+            person.removeRoleByType(RoleType.EXAM_COORDINATOR);
+        }
     }
 
 }
