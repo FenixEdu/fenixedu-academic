3602bbf5b75ae4f3b7bee809e4578a6a1f091106
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-01-22 15:52:35 +0000

    Bug fix while proving children of a given section.

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/contents/SectionProviderForContents.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/contents/SectionProviderForContents.java
@@ -1,6 +1,13 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers.contents;
 
+import java.util.Collections;
+
+import net.sourceforge.fenixedu.domain.MetaDomainObject;
 import net.sourceforge.fenixedu.domain.Section;
+import net.sourceforge.fenixedu.domain.contents.Container;
+import net.sourceforge.fenixedu.domain.contents.MetaDomainObjectPortal;
+import net.sourceforge.fenixedu.domain.contents.Node;
+import net.sourceforge.fenixedu.domain.functionalities.Module;
 import net.sourceforge.fenixedu.presentationTier.Action.person.SectionBean;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
 import net.sourceforge.fenixedu.renderers.DataProvider;
@@ -11,11 +18,22 @@ public class SectionProviderForContents implements DataProvider {
     public Object provide(Object source, Object currentValue) {
 	if (source instanceof SectionBean) {
 	    SectionBean bean = (SectionBean) source;
-	    return bean.getContainer().getOrderedChildren(Section.class);
+	    return bean.getContainer().getOrderedChildren();
 	} else {
 	    Section section = (Section) source;
-	    return section.getOrderedChildren(Section.class);
+	    Container container = getParent(section);
+	    return container == null ? Collections.EMPTY_LIST : container.getOrderedChildren(new Class[] {Section.class, MetaDomainObjectPortal.class});
 	}
+    }
+
+    private Container getParent(Section section) {
+	for(Node node : section.getParents()) {
+	    Container container = node.getParent();
+	    if(!(container instanceof Module)) {
+		return container;
+	    }
+	}
+	return null;
     }
 
     public Converter getConverter() {
