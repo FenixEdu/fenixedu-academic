09b75859cb8dd85505fc7c0616d131a35d660344
João Florindo <jdnf@linkare.pt>
2008-01-22 11:26:57 +0000

    created provider ro read approved enrolments

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ApprovedEnrolmentsToCreateDocumentRequestProvider.java
@@ -0,0 +1,19 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers;
+
+import net.sourceforge.fenixedu.dataTransferObject.degreeAdministrativeOffice.serviceRequest.documentRequest.DocumentRequestCreateBean;
+import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyArrayConverter;
+import net.sourceforge.fenixedu.renderers.DataProvider;
+import net.sourceforge.fenixedu.renderers.components.converters.Converter;
+
+public class ApprovedEnrolmentsToCreateDocumentRequestProvider implements DataProvider {
+
+    public Converter getConverter() {
+	return new DomainObjectKeyArrayConverter();
+    }
+
+    public Object provide(Object source, Object currentValue) {
+	final DocumentRequestCreateBean documentRequestCreateBean = (DocumentRequestCreateBean) source;
+	return documentRequestCreateBean.getRegistration().getLastStudentCurricularPlan().getAprovedEnrolments();
+    }
+
+}
