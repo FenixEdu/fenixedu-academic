2465455ef26dd2e635b93ca704034977e9353ff6
Luís Egídio <luis.egidio@ist.utl.pt>
2008-02-18 17:41:44 +0000

    bug fix, must not take into account for curriculum calculus

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/TemporarySubstitution.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/TemporarySubstitution.java
@@ -1,26 +1,35 @@
 package net.sourceforge.fenixedu.domain.studentCurriculum;
 
 import java.util.Collection;
+import java.util.Collections;
 
 import net.sourceforge.fenixedu.dataTransferObject.administrativeOffice.dismissal.DismissalBean.SelectedCurricularCourse;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.IEnrolment;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.domain.student.curriculum.ICurriculumEntry;
 
 public class TemporarySubstitution extends TemporarySubstitution_Base {
-    
+
     public TemporarySubstitution() {
-        super();
+	super();
     }
-    
-    public TemporarySubstitution(StudentCurricularPlan studentCurricularPlan,
-	    Collection<SelectedCurricularCourse> dismissals, Collection<IEnrolment> enrolments, ExecutionPeriod executionPeriod) {
+
+    public TemporarySubstitution(StudentCurricularPlan studentCurricularPlan, Collection<SelectedCurricularCourse> dismissals,
+	    Collection<IEnrolment> enrolments, ExecutionPeriod executionPeriod) {
 	this();
 	init(studentCurricularPlan, dismissals, enrolments, executionPeriod);
     }
-    
+
     @Override
     public boolean isTemporary() {
 	return true;
     }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Collection<ICurriculumEntry> getAverageEntries() {
+	return Collections.EMPTY_SET;
+    }
+
 }
