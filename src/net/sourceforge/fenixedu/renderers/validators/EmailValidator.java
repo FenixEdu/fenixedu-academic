79ec784baf7bd2bb2cc97eb49e55ec442fb08b4c
Luís Egídio <luis.egidio@ist.utl.pt>
2008-02-28 18:56:09 +0000

    using Email.W3C_EMAIL_SINTAX_VALIDATOR

--- src/net/sourceforge/fenixedu/renderers/validators/EmailValidator.java
+++ src/net/sourceforge/fenixedu/renderers/validators/EmailValidator.java
@@ -1,12 +1,13 @@
 package net.sourceforge.fenixedu.renderers.validators;
 
 import net.sourceforge.fenixedu.renderers.components.Validatable;
+import net.sourceforge.fenixedu.util.EMail;
 
 public class EmailValidator extends RegexpValidator {
 
     public EmailValidator(Validatable component) {
-        super(component, "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*$");
-        
-        setMessage("renderers.validator.email");
+	super(component, EMail.W3C_EMAIL_SINTAX_VALIDATOR);
+	
+	setMessage("renderers.validator.email");
     }
 }
