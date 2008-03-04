189972e7af3e206f30bdf7363490bd4faaf45733
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-03-04 12:02:16 +0000

    Changing access control in order to allow anyone from the scientific council to manage the academic calendar.

--- src/net/sourceforge/fenixedu/predicates/AcademicCalendarPredicates.java
+++ src/net/sourceforge/fenixedu/predicates/AcademicCalendarPredicates.java
@@ -1,5 +1,6 @@
 package net.sourceforge.fenixedu.predicates;
 
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.time.calendarStructure.AcademicCalendarEntry;
@@ -10,7 +11,8 @@ public class AcademicCalendarPredicates {
     
     public static final AccessControlPredicate<AcademicCalendarEntry> checkPermissionsToManageAcademicCalendarEntry = new AccessControlPredicate<AcademicCalendarEntry>() {
 	public boolean evaluate(AcademicCalendarEntry academicCalendarEntry) {
-	    if(!AccessControl.getPerson().hasRole(RoleType.MANAGER)) {
+	    Person person = AccessControl.getPerson();
+	    if(!person.hasRole(RoleType.MANAGER) && !person.hasRole(RoleType.SCIENTIFIC_COUNCIL)) {
 		throw new DomainException("error.logged.person.not.authorized.to.make.operation");
 	    }
 	    return true;
