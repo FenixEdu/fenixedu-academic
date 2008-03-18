47520275e712808b587101e33628b96d72bdf605
Luís Egídio <luis.egidio@ist.utl.pt>
2008-03-18 23:04:05 +0000

    using ExecutionPeriod.readFirstEnrolmentsExecutionPeriod

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsNotClosedPublicProvider.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.domain.ExecutionPeriod;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
@@ -18,28 +17,18 @@ public class ExecutionPeriodsNotClosedPublicProvider implements DataProvider {
     public Object provide(Object source, Object currentValue) {
 	final List<ExecutionPeriod> result = new ArrayList<ExecutionPeriod>();
 
-	final String yearString = PropertiesManager.getProperty("year.for.from.mark.sheet.managment");
-	final String semesterString = PropertiesManager.getProperty("semester.for.from.mark.sheet.managment");
-	
-	final ExecutionPeriod fromExecutionPeriod;
-	if (yearString == null || yearString.length() == 0 || semesterString == null || semesterString.length() == 0) {
-	    fromExecutionPeriod = null;
-	} else {
-	    fromExecutionPeriod = ExecutionPeriod.readBySemesterAndExecutionYear(Integer.valueOf(semesterString), yearString);
+	for (final ExecutionPeriod executionPeriod : RootDomainObject.getInstance().getExecutionPeriodsSet()) {
+	    if (executionPeriod.isAfterOrEquals(ExecutionPeriod.readMarkSheetManagmentExecutionPeriod())) {
+		result.add(executionPeriod);
+	    }
 	}
 
-        for (final ExecutionPeriod executionPeriod : RootDomainObject.getInstance().getExecutionPeriodsSet()) {
-            if (executionPeriod.isAfterOrEquals(fromExecutionPeriod)) {
-                result.add(executionPeriod);
-            }
-        }
-
-        Collections.sort(result, new ReverseComparator());
-        return result; 
+	Collections.sort(result, new ReverseComparator());
+	return result;
     }
 
     public Converter getConverter() {
-        return new DomainObjectKeyConverter();
+	return new DomainObjectKeyConverter();
     }
 
 }
