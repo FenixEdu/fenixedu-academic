36b4268ba33b4f49a63291888fbe5b3c77d03863
Joao Cachopo <joao.cachopo@inesc-id.pt>
2008-04-01 15:01:58 +0000

    Update the init.sh script to reflect the need to download an up-to-date copy of the SQL script containing the content-structure of the fenix system.

--- init.sh
+++ init.sh
@@ -4,8 +4,16 @@ mysql -uroot $1 < etc/dml.sql
 mysql -uroot $1 < etc/ojb.sql
 ant -f build_dataBaseUtils.xml generate-fenix-sql
 mysql -uroot $1 < etc/fenix.sql
-mysql -uroot $1 < etc/functionalities.sql
 ant -f build_dataBaseUtils.xml initialize-database
 echo "update IDENTIFICATION, LOGIN_ALIAS set IDENTIFICATION.PASSWORD = md5('pass') where IDENTIFICATION.ID_INTERNAL = LOGIN_ALIAS.KEY_LOGIN and LOGIN_ALIAS.ALIAS = 'admin';" > .tmp.init
 mysql -uroot $1 < .tmp.init
 rm -rf .tmp.init
+cat <<EOF
+You should now obtain a copy of an up-to-date dump of the 
+content structure to conclude the initialization of the database.
+
+After the download, run the command
+
+  mysql -uroot $1 < content-structure.sql
+
+EOF
