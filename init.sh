cd64d6ac0059924147ed442b51a5032b4fb661c7
Luis Cruz <luis.cruz@ist.utl.pt>
2008-04-14 11:38:08 +0000

    Completed automation of the database initialization process.

--- init.sh
+++ init.sh
@@ -8,12 +8,7 @@ ant -f build_dataBaseUtils.xml initialize-database
 echo "update IDENTIFICATION, LOGIN_ALIAS set IDENTIFICATION.PASSWORD = md5('pass') where IDENTIFICATION.ID_INTERNAL = LOGIN_ALIAS.KEY_LOGIN and LOGIN_ALIAS.ALIAS = 'admin';" > .tmp.init
 mysql -uroot $1 < .tmp.init
 rm -rf .tmp.init
-cat <<EOF
-You should now obtain a copy of an up-to-date dump of the 
-content structure to conclude the initialization of the database.
-
-After the download, run the command
-
-  mysql -uroot $1 < content-structure.sql
-
-EOF
+wget --no-check-certificate https://fenix-ashes.ist.utl.pt/FenixSetup\?action=AttachFile\&do=get\&target=content-structure.sql
+mv FenixSetup\?action=AttachFile\&do=get\&target=content-structure.sql content-structure.sql
+mysql -uroot -f $1 < content-structure.sql > /dev/null
+rm -rf content-structure.sql
