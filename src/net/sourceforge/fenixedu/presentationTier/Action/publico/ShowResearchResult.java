170dcc6ef7a20b615d4262545b89819c8a5437fa
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-04-15 09:25:52 +0000

    NullPointer bug fix when it cannot retrieve the publication.

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/ShowResearchResult.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/ShowResearchResult.java
@@ -14,28 +14,31 @@ import org.apache.struts.action.ActionMapping;
 
 public abstract class ShowResearchResult extends FenixDispatchAction {
 
-	public ActionForward showPatent(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) {
-		
-		String resultOID = request.getParameter("resultId");
-		ResearchResult result = (ResearchResult) RootDomainObject.readDomainObjectByOID(ResearchResult.class, Integer.valueOf(resultOID));
-		request.setAttribute("result",result);
-		putSiteOnRequest(request);
-		return mapping.findForward("showResult");
-	}
-	
-	public ActionForward showPublication(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) {
-		
-		String resultOID = request.getParameter("resultId");
-		ResearchResultPublication result = (ResearchResultPublication) RootDomainObject.readDomainObjectByOID(ResearchResult.class, Integer.valueOf(resultOID));
-		request.setAttribute("result",result);
-		request.setAttribute("resultPublicationType", result.getClass().getSimpleName());
-		putSiteOnRequest(request);
-		return mapping.findForward("showResult");
-	}
+    public ActionForward showPatent(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+	    HttpServletResponse response) {
 
-	abstract protected void putSiteOnRequest(HttpServletRequest request);
-	
-	
+	String resultOID = request.getParameter("resultId");
+	ResearchResult result = (ResearchResult) RootDomainObject.readDomainObjectByOID(ResearchResult.class, Integer
+		.valueOf(resultOID));
+	request.setAttribute("result", result);
+	putSiteOnRequest(request);
+	return mapping.findForward("showResult");
+    }
+
+    public ActionForward showPublication(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+	    HttpServletResponse response) {
+
+	String resultOID = request.getParameter("resultId");
+	ResearchResultPublication result = (ResearchResultPublication) RootDomainObject.readDomainObjectByOID(
+		ResearchResult.class, Integer.valueOf(resultOID));
+	if (result != null) {
+	    request.setAttribute("result", result);
+	    request.setAttribute("resultPublicationType", result.getClass().getSimpleName());
+	    putSiteOnRequest(request);
+	}
+	return mapping.findForward("showResult");
+    }
+
+    abstract protected void putSiteOnRequest(HttpServletRequest request);
+
 }
