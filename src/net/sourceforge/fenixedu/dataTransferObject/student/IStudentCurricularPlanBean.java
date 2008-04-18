615815500f726cf82fe7ad7ad2ab7d893c08dfaa
Luís Egídio <luis.egidio@ist.utl.pt>
2008-04-18 16:11:21 +0000

    IStudentCurricularPlanBean

---
+++ src/net/sourceforge/fenixedu/dataTransferObject/student/IStudentCurricularPlanBean.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.dataTransferObject.student;
+
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+
+public interface IStudentCurricularPlanBean {
+
+    public StudentCurricularPlan getStudentCurricularPlan();
+
+}
