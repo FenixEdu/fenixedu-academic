94bfd4a4a73808dcb3e56e5b44de974fc698b39f
Susana Fernandes <susana.fernandes@ist.utl.pt>
2008-04-21 12:05:37 +0000

    removed old assiduousness exceptions

--- src/net/sourceforge/fenixedu/domain/exceptions/assiduousness/ScheduleIsNotInSchedulesException.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.fenixedu.domain.exceptions.assiduousness;
-
-import net.sourceforge.fenixedu.domain.exceptions.FenixDomainException;
-
-public class ScheduleIsNotInSchedulesException extends FenixDomainException {
-
-}
--- src/net/sourceforge/fenixedu/domain/exceptions/assiduousness/UnknownEmployeeException.java
+++
@@ -1,7 +0,0 @@
-package net.sourceforge.fenixedu.domain.exceptions.assiduousness;
-
-import net.sourceforge.fenixedu.domain.exceptions.FenixDomainException;
-
-public class UnknownEmployeeException extends FenixDomainException {
-
-}
