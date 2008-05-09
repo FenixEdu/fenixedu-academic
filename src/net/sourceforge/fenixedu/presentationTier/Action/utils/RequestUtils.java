dc9a9e4968a049b6aa5c0aa8285fe2d4c431279e
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-05-09 13:37:14 +0000

    Even though an ArrayList implements Serializable interface it doesn't mean that everything  contained by the list (actually by any collection) is serializable. This situation could result in having non-serializable objects in the http session, which would be a problem for tomcat's session replication. So now, if the attribute we're copying is a collection we first make sure all its elements are indeed serializable.
    This is, I hope, the fix for two annoying bugs that have been for a while and weren't yet discovered:
    
    * The NotSerializableException for a Section that would show up various times on our logs 
    * Sometimes when logging in from a public section, instead of being redirected to the actual section the user would be redirected to the intranet.
    
    If you're reading until here I just want to say one other thing, this was a damn nasty bug I've been looking for it for way too long and never had luck.. In the end, it was a matter of luck... I tripped on the bastard :-)

--- src/net/sourceforge/fenixedu/presentationTier/Action/utils/RequestUtils.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/utils/RequestUtils.java
@@ -34,77 +34,77 @@ import org.apache.commons.collections.Transformer;
 import org.apache.struts.util.LabelValueBean;
 
 /**
- *
+ * 
  * @author Luis Cruz
  * @version 1.1, Oct 24, 2004
  * @since 1.1
- *
+ * 
  */
 public class RequestUtils {
 
     public static final int APP_CONTEXT_LENGTH = PropertiesManager.getProperty("app.context").length() + 1;
 
     public static String getAndSetStringToRequest(HttpServletRequest request, String name) {
-        String parameter = request.getParameter(name);
-        if (parameter == null) {
-            parameter = (String) request.getAttribute(name);
-        }
-        request.setAttribute(name, parameter);
-        return parameter;
+	String parameter = request.getParameter(name);
+	if (parameter == null) {
+	    parameter = (String) request.getAttribute(name);
+	}
+	request.setAttribute(name, parameter);
+	return parameter;
     }
 
     public static Collection buildExecutionDegreeLabelValueBean(Collection executionDegrees) {
-        final Map duplicateDegreesMap = new HashMap();
-        for (Iterator iterator = executionDegrees.iterator(); iterator.hasNext();) {
-            InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) iterator.next();
-            InfoDegree infoDegree = infoExecutionDegree.getInfoDegreeCurricularPlan().getInfoDegree();
-            String degreeName = infoDegree.getNome();
+	final Map duplicateDegreesMap = new HashMap();
+	for (Iterator iterator = executionDegrees.iterator(); iterator.hasNext();) {
+	    InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) iterator.next();
+	    InfoDegree infoDegree = infoExecutionDegree.getInfoDegreeCurricularPlan().getInfoDegree();
+	    String degreeName = infoDegree.getNome();
 
-            if (duplicateDegreesMap.get(degreeName) == null) {
-                duplicateDegreesMap.put(degreeName, new Boolean(false));
-            } else {
-                duplicateDegreesMap.put(degreeName, new Boolean(true));
-            }
-        }
+	    if (duplicateDegreesMap.get(degreeName) == null) {
+		duplicateDegreesMap.put(degreeName, new Boolean(false));
+	    } else {
+		duplicateDegreesMap.put(degreeName, new Boolean(true));
+	    }
+	}
 
-        Collection lableValueList = CollectionUtils.collect(executionDegrees, new Transformer() {
+	Collection lableValueList = CollectionUtils.collect(executionDegrees, new Transformer() {
 
-            public Object transform(Object arg0) {
-                InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) arg0;
-                InfoDegreeCurricularPlan infoDegreeCurricularPlan = infoExecutionDegree.getInfoDegreeCurricularPlan();
-                InfoDegree infoDegree = infoDegreeCurricularPlan.getInfoDegree();
+	    public Object transform(Object arg0) {
+		InfoExecutionDegree infoExecutionDegree = (InfoExecutionDegree) arg0;
+		InfoDegreeCurricularPlan infoDegreeCurricularPlan = infoExecutionDegree.getInfoDegreeCurricularPlan();
+		InfoDegree infoDegree = infoDegreeCurricularPlan.getInfoDegree();
 
-                StringBuilder label = new StringBuilder();
-                label.append(infoDegree.getTipoCurso().toString());
-                label.append(" em ");
-                label.append(infoDegree.getNome());
-                if (((Boolean) duplicateDegreesMap.get(infoDegree.getNome())).booleanValue()) {
-                    label.append(" - ");
-                    label.append(infoDegreeCurricularPlan.getName());
-                }
+		StringBuilder label = new StringBuilder();
+		label.append(infoDegree.getTipoCurso().toString());
+		label.append(" em ");
+		label.append(infoDegree.getNome());
+		if (((Boolean) duplicateDegreesMap.get(infoDegree.getNome())).booleanValue()) {
+		    label.append(" - ");
+		    label.append(infoDegreeCurricularPlan.getName());
+		}
 
-                String value = infoExecutionDegree.getIdInternal().toString();
+		String value = infoExecutionDegree.getIdInternal().toString();
 
-                return new LabelValueBean(label.toString(), value);
-            }
-            
-        });
+		return new LabelValueBean(label.toString(), value);
+	    }
 
-        Comparator comparator = new BeanComparator("label", Collator.getInstance());
-        Collections.sort((List) lableValueList, comparator);
+	});
 
-        return lableValueList;
+	Comparator comparator = new BeanComparator("label", Collator.getInstance());
+	Collections.sort((List) lableValueList, comparator);
+
+	return lableValueList;
     }
 
     public static final List buildCurricularYearLabelValueBean() {
-        final List curricularYears = new ArrayList();
-        curricularYears.add(new LabelValueBean("escolher", ""));
-        curricularYears.add(new LabelValueBean("1 º", "1"));
-        curricularYears.add(new LabelValueBean("2 º", "2"));
-        curricularYears.add(new LabelValueBean("3 º", "3"));
-        curricularYears.add(new LabelValueBean("4 º", "4"));
-        curricularYears.add(new LabelValueBean("5 º", "5"));
-        return curricularYears;
+	final List curricularYears = new ArrayList();
+	curricularYears.add(new LabelValueBean("escolher", ""));
+	curricularYears.add(new LabelValueBean("1 º", "1"));
+	curricularYears.add(new LabelValueBean("2 º", "2"));
+	curricularYears.add(new LabelValueBean("3 º", "3"));
+	curricularYears.add(new LabelValueBean("4 º", "4"));
+	curricularYears.add(new LabelValueBean("5 º", "5"));
+	return curricularYears;
     }
 
     /**
@@ -112,76 +112,74 @@ public class RequestUtils {
      * be used to force the user to login before a certain request is fulfilled.
      * 
      * @param request
-     *            the current request
+     *                the current request
      * @param response
-     *            the reponse were the redirect will be sent
+     *                the reponse were the redirect will be sent
      * 
      * @throws IOException
-     *             when it's not possible to send the redirect to the client
+     *                 when it's not possible to send the redirect to the client
      */
     public static void sendLoginRedirect(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        final String uri = request.getRequestURI();
-        
-        final HttpSession httpSession = request.getSession(true);
-        httpSession.setAttribute("ORIGINAL_REQUEST", uri);
+	final String uri = request.getRequestURI();
 
-        httpSession.setAttribute("ORIGINAL_URI", uri);
+	final HttpSession httpSession = request.getSession(true);
+	httpSession.setAttribute("ORIGINAL_REQUEST", uri);
 
-        final String relativeURI = uri.substring(APP_CONTEXT_LENGTH);
-        final int indexOfSlash = relativeURI.indexOf('/');
-        final String module = (indexOfSlash >= 0) ? relativeURI.substring(0, indexOfSlash) : "";
+	httpSession.setAttribute("ORIGINAL_URI", uri);
 
-        httpSession.setAttribute("ORIGINAL_MODULE", module);
+	final String relativeURI = uri.substring(APP_CONTEXT_LENGTH);
+	final int indexOfSlash = relativeURI.indexOf('/');
+	final String module = (indexOfSlash >= 0) ? relativeURI.substring(0, indexOfSlash) : "";
 
-        final Map<String, Object> parameterMap = new HashMap<String, Object>();
+	httpSession.setAttribute("ORIGINAL_MODULE", module);
+
+	final Map<String, Object> parameterMap = new HashMap<String, Object>();
 	for (final Entry<String, Object> entry : ((Map<String, Object>) request.getParameterMap()).entrySet()) {
 	    parameterMap.put(entry.getKey(), entry.getValue());
 	}
-        httpSession.setAttribute("ORIGINAL_PARAMETER_MAP", parameterMap);
+	httpSession.setAttribute("ORIGINAL_PARAMETER_MAP", parameterMap);
 
-        final Map<String, Object> attributeMap = new HashMap<String, Object>();
-        final Enumeration enumeration = request.getAttributeNames();
-        while (enumeration.hasMoreElements()) {
+	final Map<String, Object> attributeMap = new HashMap<String, Object>();
+	final Enumeration enumeration = request.getAttributeNames();
+	while (enumeration.hasMoreElements()) {
 	    final String attributeName = (String) enumeration.nextElement();
 	    final Object attribute = request.getAttribute(attributeName);
-	    if (attribute instanceof Serializable) {
+
+	    if (shouldBeAdded(attribute)) {
 		attributeMap.put(attributeName, attribute);
 	    }
-        }
-        httpSession.setAttribute("ORIGINAL_ATTRIBUTE_MAP", attributeMap);
+	}
+	httpSession.setAttribute("ORIGINAL_ATTRIBUTE_MAP", attributeMap);
 
-        response.sendRedirect(HostRedirector.getRedirectPageLogin(request.getRequestURL().toString()));
+	response.sendRedirect(HostRedirector.getRedirectPageLogin(request.getRequestURL().toString()));
     }
-    
+
+    private static boolean shouldBeAdded(Object attribute) {
+
+	if (attribute instanceof Collection) {
+	    Iterator iterator = ((Collection) attribute).iterator();
+	    while (iterator.hasNext()) {
+		if (!(iterator.next() instanceof Serializable)) {
+		    return false;
+		}
+	    }
+	}
+	return attribute instanceof Serializable;
+    }
+
     public static boolean isPrivateURI(HttpServletRequest request) {
-        final String uri = request.getRequestURI().substring(APP_CONTEXT_LENGTH);
-        
-        return (uri.length() > 1
-                && (uri.indexOf("CSS/") == -1)
-                && (uri.indexOf("ajax/") == -1)
-                && (uri.indexOf("images/") == -1)
-                && (uri.indexOf("img/") == -1)
-                && (uri.indexOf("download/") == -1)
-                && (uri.indexOf("external/") == -1)
-                && (uri.indexOf("services/") == -1)
-                && (uri.indexOf("index.jsp") == -1)
-                && (uri.indexOf("index.html") == -1)
-                && (uri.indexOf("login.do") == -1)
-                && (uri.indexOf("loginCAS.do") == -1)
-                && (uri.indexOf("privado") == -1)
-                && (uri.indexOf("loginPage.jsp") == -1)
-                && (uri.indexOf("loginExpired.jsp") == -1)
-                && (uri.indexOf("loginExpired.do") == -1)                
-                && (uri.indexOf("logoff.do") == -1)
-                && (uri.indexOf("publico/") == -1)
-                && (uri.indexOf("showErrorPage.do") == -1)
-                && (uri.indexOf("showErrorPageRegistered.do") == -1)
-                && (uri.indexOf("exceptionHandlingAction.do") == -1)
-                && (uri.indexOf("manager/manageCache.do") == -1)
-                && (uri.indexOf("checkPasswordKerberos.do") == -1)
-                && (uri.indexOf("siteMap.do") == -1)
-                && (uri.indexOf("changeLocaleTo.do") == -1)
-                && (uri.indexOf("cms/forwardEmailAction.do") == -1)
-                && (uri.indexOf("isAlive.do") == -1));
-    }    
+	final String uri = request.getRequestURI().substring(APP_CONTEXT_LENGTH);
+
+	return (uri.length() > 1 && (uri.indexOf("CSS/") == -1) && (uri.indexOf("ajax/") == -1) && (uri.indexOf("images/") == -1)
+		&& (uri.indexOf("img/") == -1) && (uri.indexOf("download/") == -1) && (uri.indexOf("external/") == -1)
+		&& (uri.indexOf("services/") == -1) && (uri.indexOf("index.jsp") == -1) && (uri.indexOf("index.html") == -1)
+		&& (uri.indexOf("login.do") == -1) && (uri.indexOf("loginCAS.do") == -1) && (uri.indexOf("privado") == -1)
+		&& (uri.indexOf("loginPage.jsp") == -1) && (uri.indexOf("loginExpired.jsp") == -1)
+		&& (uri.indexOf("loginExpired.do") == -1) && (uri.indexOf("logoff.do") == -1) && (uri.indexOf("publico/") == -1)
+		&& (uri.indexOf("showErrorPage.do") == -1) && (uri.indexOf("showErrorPageRegistered.do") == -1)
+		&& (uri.indexOf("exceptionHandlingAction.do") == -1) && (uri.indexOf("manager/manageCache.do") == -1)
+		&& (uri.indexOf("checkPasswordKerberos.do") == -1) && (uri.indexOf("siteMap.do") == -1)
+		&& (uri.indexOf("changeLocaleTo.do") == -1) && (uri.indexOf("cms/forwardEmailAction.do") == -1) && (uri
+		.indexOf("isAlive.do") == -1));
+    }
 }
