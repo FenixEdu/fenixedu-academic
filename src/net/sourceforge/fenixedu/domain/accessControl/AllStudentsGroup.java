b5dd536e385615fbbd6516cef09cf6af435080ce
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-06-16 10:12:18 +0000

    Modified AllStudentsGroup to have the semantic of all __ACTIVE__ Students, this means we'll not only look for the Student's role but also check if there's at least one active registration.

--- src/net/sourceforge/fenixedu/domain/accessControl/AllStudentsGroup.java
+++ src/net/sourceforge/fenixedu/domain/accessControl/AllStudentsGroup.java
@@ -1,11 +1,36 @@
 package net.sourceforge.fenixedu.domain.accessControl;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.Role;
+import net.sourceforge.fenixedu.domain.accessControl.groups.language.Argument;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 
-public class AllStudentsGroup extends RoleTypeGroup {
+public class AllStudentsGroup extends Group {
 
     public AllStudentsGroup() {
-	super(RoleType.STUDENT);
+
+    }
+
+    @Override
+    public Set<Person> getElements() {
+	Set<Person> elements = new HashSet<Person>();
+	List<Person> people = Role.getRoleByRoleType(RoleType.STUDENT).getAssociatedPersons();
+
+	for (Person person : people) {
+	    if (!person.getStudent().getActiveRegistrations().isEmpty()) {
+		elements.add(person);
+	    }
+	}
+	return elements;
+    }
+
+    @Override
+    protected Argument[] getExpressionArguments() {
+	return null;
     }
 
 }
