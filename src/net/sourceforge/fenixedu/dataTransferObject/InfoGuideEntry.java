20a845aeb6cef4a6f8f961f71a188996bbf31b95
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2008-06-30 09:13:19 +0000

    bug resolved in toString

--- src/net/sourceforge/fenixedu/dataTransferObject/InfoGuideEntry.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/InfoGuideEntry.java
@@ -29,152 +29,152 @@ public class InfoGuideEntry extends InfoObject {
     public InfoGuideEntry() {
     }
 
-    public InfoGuideEntry(GraduationType graduationType, DocumentType documentType, String description,
-            Double price, Integer quantity, InfoGuide infoGuide) {
-        this.description = description;
-        this.documentType = documentType;
-        this.graduationType = graduationType;
-        this.infoGuide = infoGuide;
-        this.price = price;
-        this.quantity = quantity;
+    public InfoGuideEntry(GraduationType graduationType, DocumentType documentType, String description, Double price,
+	    Integer quantity, InfoGuide infoGuide) {
+	this.description = description;
+	this.documentType = documentType;
+	this.graduationType = graduationType;
+	this.infoGuide = infoGuide;
+	this.price = price;
+	this.quantity = quantity;
     }
 
     public boolean equals(Object obj) {
-        boolean resultado = false;
-        if (obj instanceof InfoGuideEntry) {
-            InfoGuideEntry infoGuideEntry = (InfoGuideEntry) obj;
+	boolean resultado = false;
+	if (obj instanceof InfoGuideEntry) {
+	    InfoGuideEntry infoGuideEntry = (InfoGuideEntry) obj;
 
-            resultado = getInfoGuide().equals(infoGuideEntry.getInfoGuide())
-                    && getGraduationType().equals(infoGuideEntry.getGraduationType())
-                    && getDocumentType().equals(infoGuideEntry.getDocumentType())
-                    && getDescription().equals(infoGuideEntry.getDescription());
-        }
-        return resultado;
+	    resultado = getInfoGuide().equals(infoGuideEntry.getInfoGuide())
+		    && getGraduationType().equals(infoGuideEntry.getGraduationType())
+		    && getDocumentType().equals(infoGuideEntry.getDocumentType())
+		    && getDescription().equals(infoGuideEntry.getDescription());
+	}
+	return resultado;
     }
 
     public String toString() {
-        String result = "[GUIDE ENTRY";
-        result += ", description=" + description;
-        result += ", infoGuide=" + infoGuide;
-        result += ", documentType=" + documentType;
-        result += ", graduationType=" + graduationType;
-        result += ", price=" + price;
-        result += ", quantity=" + quantity;
-        result += "]";
-        return result;
+	String result = "[GUIDE ENTRY";
+	result += ", description=" + description;
+	result += ", infoGuide=" + infoGuide.getIdInternal();
+	result += ", documentType=" + documentType;
+	result += ", graduationType=" + graduationType;
+	result += ", price=" + price;
+	result += ", quantity=" + quantity;
+	result += "]";
+	return result;
     }
 
     /**
      * @return
      */
     public String getDescription() {
-        return description;
+	return description;
     }
 
     /**
      * @return
      */
     public DocumentType getDocumentType() {
-        return documentType;
+	return documentType;
     }
 
     /**
      * @return
      */
     public GraduationType getGraduationType() {
-        return graduationType;
+	return graduationType;
     }
 
     /**
      * @return
      */
     public InfoGuide getInfoGuide() {
-        return infoGuide;
+	return infoGuide;
     }
 
     /**
      * @return
      */
     public Double getPrice() {
-        return price;
+	return price;
     }
 
     /**
      * @return
      */
     public Integer getQuantity() {
-        return quantity;
+	return quantity;
     }
 
     /**
      * @param string
      */
     public void setDescription(String string) {
-        description = string;
+	description = string;
     }
 
     /**
      * @param type
      */
     public void setDocumentType(DocumentType type) {
-        documentType = type;
+	documentType = type;
     }
 
     /**
      * @param type
      */
     public void setGraduationType(GraduationType type) {
-        graduationType = type;
+	graduationType = type;
     }
 
     /**
      * @param guide
      */
     public void setInfoGuide(InfoGuide guide) {
-        infoGuide = guide;
+	infoGuide = guide;
     }
 
     /**
      * @param double1
      */
     public void setPrice(Double double1) {
-        price = double1;
+	price = double1;
     }
 
     /**
      * @param integer
      */
     public void setQuantity(Integer integer) {
-        quantity = integer;
+	quantity = integer;
     }
 
     public void copyFromDomain(GuideEntry guideEntry) {
-        super.copyFromDomain(guideEntry);
-        if (guideEntry != null) {
-            setDescription(guideEntry.getDescription());
-            setDocumentType(guideEntry.getDocumentType());
-            setGraduationType(guideEntry.getGraduationType());
-            setPrice(guideEntry.getPrice());
-            setQuantity(guideEntry.getQuantity());
-        }
+	super.copyFromDomain(guideEntry);
+	if (guideEntry != null) {
+	    setDescription(guideEntry.getDescription());
+	    setDocumentType(guideEntry.getDocumentType());
+	    setGraduationType(guideEntry.getGraduationType());
+	    setPrice(guideEntry.getPrice());
+	    setQuantity(guideEntry.getQuantity());
+	}
     }
 
     public static InfoGuideEntry newInfoFromDomain(GuideEntry guideEntry) {
-        InfoGuideEntry infoGuideEntry = null;
-        if (guideEntry != null) {
-            infoGuideEntry = new InfoGuideEntry();
-            infoGuideEntry.copyFromDomain(guideEntry);
-        }
-        return infoGuideEntry;
+	InfoGuideEntry infoGuideEntry = null;
+	if (guideEntry != null) {
+	    infoGuideEntry = new InfoGuideEntry();
+	    infoGuideEntry.copyFromDomain(guideEntry);
+	}
+	return infoGuideEntry;
     }
 
     public void copyToDomain(InfoGuideEntry infoGuideEntry, GuideEntry guideEntry) {
-        super.copyToDomain(infoGuideEntry, guideEntry);
-        guideEntry.setDescription(infoGuideEntry.getDescription());
-        guideEntry.setDocumentType(infoGuideEntry.getDocumentType());
-        guideEntry.setGraduationType(infoGuideEntry.getGraduationType());
-        guideEntry.setPrice(infoGuideEntry.getPrice());
-        guideEntry.setQuantity(infoGuideEntry.getQuantity());
+	super.copyToDomain(infoGuideEntry, guideEntry);
+	guideEntry.setDescription(infoGuideEntry.getDescription());
+	guideEntry.setDocumentType(infoGuideEntry.getDocumentType());
+	guideEntry.setGraduationType(infoGuideEntry.getGraduationType());
+	guideEntry.setPrice(infoGuideEntry.getPrice());
+	guideEntry.setQuantity(infoGuideEntry.getQuantity());
     }
 
 }
