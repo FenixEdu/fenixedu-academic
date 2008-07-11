53c0f6ef6318ed129f92de242b8c039fb0d5ddeb
João Florindo <jdnf@linkare.pt>
2008-07-11 08:50:51 +0000

    add missing edit method to ExternalCourseLoadRequestPR class

--- src/net/sourceforge/fenixedu/domain/accounting/postingRules/serviceRequests/ExternalCourseLoadRequestPR.java
+++ src/net/sourceforge/fenixedu/domain/accounting/postingRules/serviceRequests/ExternalCourseLoadRequestPR.java
@@ -3,6 +3,7 @@ package net.sourceforge.fenixedu.domain.accounting.postingRules.serviceRequests;
 import net.sourceforge.fenixedu.domain.accounting.EntryType;
 import net.sourceforge.fenixedu.domain.accounting.EventType;
 import net.sourceforge.fenixedu.domain.accounting.ServiceAgreementTemplate;
+import net.sourceforge.fenixedu.injectionCode.Checked;
 import net.sourceforge.fenixedu.util.Money;
 
 import org.joda.time.DateTime;
@@ -20,5 +21,12 @@ public class ExternalCourseLoadRequestPR extends ExternalCourseLoadRequestPR_Bas
 		serviceAgreementTemplate, certificateAmount, amountPerPage);
 	checkParameters(amountFirstPage);
 	super.setAmountFirstPage(amountFirstPage);
+    }
+
+    @Checked("PostingRulePredicates.editPredicate")
+    public ExternalCourseLoadRequestPR edit(final Money baseAmount, final Money amountFirstPage, final Money amountPerUnit) {
+	deactivate();
+	return new ExternalCourseLoadRequestPR(getServiceAgreementTemplate(), new DateTime().minus(1000), null, baseAmount,
+		amountFirstPage, amountPerUnit);
     }
 }
