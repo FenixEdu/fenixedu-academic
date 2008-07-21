fc1b8fb619ca836360fe35446501598942572d67
Pedro Miguel Reis Santos <pedro.miguel.santos@ist.utl.pt>
2008-07-21 16:06:30 +0000

    fixed SqlUpdateGenerator

---
+++ src_tools/pt/utl/ist/codeGenerator/SQLUpdateGenerator.java
@@ -0,0 +1,47 @@
+package pt.utl.ist.codeGenerator;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import net.sourceforge.fenixedu._development.PropertiesManager;
+
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerFactory;
+
+import pt.ist.fenixWebFramework.Config;
+import pt.ist.fenixframework.FenixFramework;
+
+public class SQLUpdateGenerator {
+
+    public static void main(String[] args) {
+	Connection connection = null;
+	try {
+	    Config config = PropertiesManager.getFenixFrameworkConfig("build/WEB-INF/classes/domain_model.dml");
+	    FenixFramework.initialize(config);
+
+	    final PersistenceBroker persistenceBroker = PersistenceBrokerFactory.defaultPersistenceBroker();
+	    connection = persistenceBroker.serviceConnectionManager().getConnection();
+	    generate(connection);
+	} catch (Exception ex) {
+	    ex.printStackTrace();
+	} finally {
+	    if (connection != null) {
+		try {
+		    connection.close();
+		} catch (SQLException e) {
+		    // nothing can be done.
+		}
+	    }
+	}
+
+	System.out.println("Generation Complete.");
+	System.exit(0);
+    }
+
+    private static void generate(final Connection connection) throws Exception {
+	final String destinationFilename = "etc/database_operations/updates.sql";
+	final String result = pt.ist.fenixWebFramework.repository.SQLUpdateGenerator.generateInMem(connection);
+	pt.ist.fenixWebFramework.repository.SQLUpdateGenerator.writeFile(destinationFilename, result);
+    }
+
+}
