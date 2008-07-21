8719b035aeab132d106293eac5df7e77a239e539
Shezad Anavarali <shezad@ist.utl.pt>
2008-07-21 16:37:16 +0000

    initial version

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/validators/DoubleRangeValidator.java
@@ -0,0 +1,85 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.validators;
+
+import org.apache.commons.lang.StringUtils;
+
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+import pt.ist.fenixWebFramework.renderers.validators.HtmlChainValidator;
+import pt.ist.fenixWebFramework.renderers.validators.HtmlValidator;
+
+public class DoubleRangeValidator extends HtmlValidator {
+
+    private boolean isNumber;
+
+    private Double upperBound;
+
+    private Double lowerBound;
+
+    public DoubleRangeValidator(HtmlChainValidator htmlChainValidator) {
+	super(htmlChainValidator);
+
+	upperBound = null;
+	lowerBound = null;
+    }
+
+    public double getLowerBound() {
+	return lowerBound;
+    }
+
+    public void setLowerBound(double lowerBound) {
+	this.lowerBound = lowerBound;
+    }
+
+    public double getUpperBound() {
+	return upperBound;
+    }
+
+    public void setUpperBound(double upperBound) {
+	this.upperBound = upperBound;
+    }
+
+    @Override
+    public void performValidation() {
+
+	String numberText = getComponent().getValue();
+
+	if (!StringUtils.isEmpty(numberText)) {
+	    try {
+		double number = Double.parseDouble(numberText.trim());
+
+		boolean inRange = true;
+		isNumber = true;
+
+		if (lowerBound != null) {
+		    inRange &= lowerBound <= number;
+		}
+
+		if (upperBound != null) {
+		    inRange &= upperBound >= number;
+		}
+
+		this.setValid(inRange);
+	    } catch (NumberFormatException e) {
+		isNumber = false;
+		setValid(false);
+	    }
+	}
+    }
+
+    @Override
+    public String getErrorMessage() {
+	if (!isNumber) {
+	    return RenderUtils.getResourceString("renderers.validator.number");
+	}
+
+	if (lowerBound != null && upperBound != null) {
+	    return RenderUtils.getFormatedResourceString("renderers.validator.number.range.both", lowerBound, upperBound);
+	}
+
+	if (lowerBound != null) {
+	    return RenderUtils.getFormatedResourceString("renderers.validator.number.range.lower", lowerBound);
+	}
+
+	return RenderUtils.getFormatedResourceString("renderers.validator.number.range.upper", upperBound);
+    }
+
+}
