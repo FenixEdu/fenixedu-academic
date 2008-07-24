1418cb33a774deaae03e86acc4727f6bffc6e5d0
Luís Egídio <luis.egidio@ist.utl.pt>
2008-07-24 10:17:59 +0000

    fixConcreteClassForCycleCurricumGroups.sql

---
+++ etc/database_operations/fixConcreteClassForCycleCurricumGroups.sql
@@ -0,0 +1,17 @@
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6117202;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6116945;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6115836;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6115778;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6098622;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6069575;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6069571;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6069562;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6064529;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6064358;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6050885;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6050882;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6050876;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6050741;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6047741;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6045660;
+UPDATE CURRICULUM_MODULE SET OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.studentCurriculum.CycleCurriculumGroup' WHERE ID_INTERNAL=6019147;
