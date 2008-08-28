8b937ce9f99d64d656863a698fcc23b4486d1ebc
Luis Cruz <luis.cruz@ist.utl.pt>
2008-08-28 19:35:00 +0000

    Fix compile errors

--- src/net/sourceforge/fenixedu/presentationTier/renderers/AnnouncementRenderer.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/AnnouncementRenderer.java
@@ -172,13 +172,13 @@ public class AnnouncementRenderer extends OutputRenderer {
 	private HtmlComponent getContent() {
 	    HtmlBlockContainer container = new HtmlBlockContainer();
 	    if (announcement.isExcerptEmpty()) {
-		container.addChild(new HtmlText(ContentInjectionRewriter.BLOCK_HAS_CONTEXT_PREFIX, false));
+		container.addChild(new HtmlText(new String(ContentInjectionRewriter.BLOCK_HAS_CONTEXT_PREFIX), false));
 		container.addChild(new HtmlText(announcement.getBody().getContent(), false));
-		container.addChild(new HtmlText(ContentInjectionRewriter.END_BLOCK_HAS_CONTEXT_PREFIX, false));
+		container.addChild(new HtmlText(new String(ContentInjectionRewriter.END_BLOCK_HAS_CONTEXT_PREFIX), false));
 	    } else {
-		container.addChild(new HtmlText(ContentInjectionRewriter.BLOCK_HAS_CONTEXT_PREFIX, false));
+		container.addChild(new HtmlText(new String(ContentInjectionRewriter.BLOCK_HAS_CONTEXT_PREFIX), false));
 		container.addChild(new HtmlText(announcement.getExcerpt().getContent(), false));
-		container.addChild(new HtmlText(ContentInjectionRewriter.END_BLOCK_HAS_CONTEXT_PREFIX, false));
+		container.addChild(new HtmlText(new String(ContentInjectionRewriter.END_BLOCK_HAS_CONTEXT_PREFIX), false));
 		HtmlLink link = new HtmlLink();
 		link.setUrl(RenderUtils.getFormattedProperties(getViewMoreUrl(), announcement));
 		link.setText(RenderUtils.getFormatedResourceString(getBundle(), getViewMoreLabel()));
