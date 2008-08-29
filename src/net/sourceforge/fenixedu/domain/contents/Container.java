8bbb22b2ba3daec8d79875d2336a04a18e469e24
Luis Cruz <luis.cruz@ist.utl.pt>
2008-08-29 17:01:04 +0000

    Code optimization.

--- src/net/sourceforge/fenixedu/domain/contents/Container.java
+++ src/net/sourceforge/fenixedu/domain/contents/Container.java
@@ -231,7 +231,8 @@ public abstract class Container extends Container_Base {
 	if (policy != null) {
 	    return policy;
 	}
-	Collection<Node> nodes = getOrderedChildrenNodes();
+	//Collection<Node> nodes = getOrderedChildrenNodes();
+	Collection<Node> nodes = getChildrenSet();
 	return nodes.isEmpty() ? null : nodes.iterator().next().getChild().getAvailabilityPolicy();
     }
 
