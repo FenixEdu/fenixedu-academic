a418d00f523218d0eaf4e18a53d4d2898728c8d7
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-09-22 10:36:24 +0000

    removed unused code

--- src/net/sourceforge/fenixedu/presentationTier/Action/resourceAllocationManager/utils/ServiceUtils.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/resourceAllocationManager/utils/ServiceUtils.java
@@ -8,11 +8,9 @@
  */
 package net.sourceforge.fenixedu.presentationTier.Action.resourceAllocationManager.utils;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Filtro.exception.FenixFilterException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.framework.factory.ServiceManagerServiceFactory;
-import pt.ist.fenixWebFramework.security.UserView;
 
 /**
  * @author jpvl
@@ -23,9 +21,9 @@ public abstract class ServiceUtils {
 
     public static Object executeService(String serviceName, Object... serviceArgs) throws FenixServiceException,
 	    FenixFilterException {
-	final IUserView userView = UserView.getUser();
-	if (serviceArgs == null)
+	if (serviceArgs == null) {
 	    serviceArgs = new Object[0];
+	}
 	return ServiceManagerServiceFactory.executeService(serviceName, serviceArgs);
     }
 
