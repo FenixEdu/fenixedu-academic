8bb76468dc87f0cb068c6c2f0f01aef0175ca7dc
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-09-23 12:05:48 +0000

    do not run rules when moving information

--- src/net/sourceforge/fenixedu/applicationTier/Servico/student/curriculumLines/MoveCurriculumLines.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/student/curriculumLines/MoveCurriculumLines.java
@@ -1,15 +1,11 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.student.curriculumLines;
 
-import java.util.Collections;
-
 import net.sourceforge.fenixedu.applicationTier.Service;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.student.OptionalCurricularCoursesLocationBean;
 import net.sourceforge.fenixedu.dataTransferObject.student.OptionalCurricularCoursesLocationBean.EnrolmentLocationBean;
 import net.sourceforge.fenixedu.dataTransferObject.student.OptionalCurricularCoursesLocationBean.OptionalEnrolmentLocationBean;
-import net.sourceforge.fenixedu.domain.ExecutionSemester;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
-import net.sourceforge.fenixedu.domain.curricularRules.executors.ruleExecutors.CurricularRuleLevel;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumGroup;
 import net.sourceforge.fenixedu.domain.studentCurriculum.curriculumLine.MoveCurriculumLinesBean;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
@@ -24,10 +20,6 @@ public class MoveCurriculumLines extends Service {
     public void run(final OptionalCurricularCoursesLocationBean bean) throws FenixServiceException {
 	moveEnrolments(bean);
 	moveOptionalEnrolments(bean);
-	final ExecutionSemester executionPeriod = ExecutionSemester.readActualExecutionSemester();
-	bean.getStudentCurricularPlan().enrol(AccessControl.getPerson(), executionPeriod,
-		bean.getIDegreeModulesToEvaluate(executionPeriod), Collections.EMPTY_LIST,
-		CurricularRuleLevel.ENROLMENT_WITH_RULES);
     }
 
     private void moveOptionalEnrolments(final OptionalCurricularCoursesLocationBean bean) {
