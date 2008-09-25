7fe5ec0354b1f53b1534ddb77745bdda29fc0eb8
Luis Cruz <luis.cruz@ist.utl.pt>
2008-09-25 10:43:08 +0000

    Reverted code.

---
+++ src/net/sourceforge/fenixedu/injectionCode/FenixVoidAccessControlCallsGenerator.java
@@ -0,0 +1,13 @@
+package net.sourceforge.fenixedu.injectionCode;
+
+import java.util.Map;
+
+import net.sourceforge.fenixedu.injectionCode.injector.CodeGenerator;
+
+public class FenixVoidAccessControlCallsGenerator implements CodeGenerator {
+
+    public String getCode(Map<String, Object> annotationParameters) {
+	return "";
+    }
+
+}
