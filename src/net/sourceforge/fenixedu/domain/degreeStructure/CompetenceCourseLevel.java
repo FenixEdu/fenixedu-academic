0f4836bf3bca657538954a0c43e25a1245022a49
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-09-25 10:00:18 +0000

    added getLocalizedName method

--- src/net/sourceforge/fenixedu/domain/degreeStructure/CompetenceCourseLevel.java
+++ src/net/sourceforge/fenixedu/domain/degreeStructure/CompetenceCourseLevel.java
@@ -1,5 +1,10 @@
 package net.sourceforge.fenixedu.domain.degreeStructure;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public enum CompetenceCourseLevel {
 
     FIRST_CYCLE,
@@ -15,4 +20,13 @@ public enum CompetenceCourseLevel {
     public String getName() {
 	return name();
     }
+
+    public String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    public String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(name());
+    }
+
 }
