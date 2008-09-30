92b6c13c259b75c066c1c49c27142a9bd20798ec
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-09-30 16:47:34 +0000

    re-check the EnrolmentPeriod in StudentReingressionRequest

--- src/net/sourceforge/fenixedu/domain/serviceRequests/StudentReingressionRequest.java
+++ src/net/sourceforge/fenixedu/domain/serviceRequests/StudentReingressionRequest.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.serviceRequests.AcademicServiceRequestBean;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
+import net.sourceforge.fenixedu.domain.EnrolmentPeriod;
+import net.sourceforge.fenixedu.domain.ExecutionSemester;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.accounting.EventType;
 import net.sourceforge.fenixedu.domain.accounting.events.serviceRequests.StudentReingressionRequestEvent;
@@ -93,16 +95,15 @@ public class StudentReingressionRequest extends StudentReingressionRequest_Base 
 
     private boolean hasOpenEnrolmentPeriod(final DegreeCurricularPlan degreeCurricularPlan, final ExecutionYear executionYear,
 	    final DateTime requestDate) {
-	/*
-	 * for (final ExecutionSemester executionSemester :
-	 * executionYear.getExecutionPeriodsSet()) { final EnrolmentPeriod
-	 * enrolmentPeriod = degreeCurricularPlan
-	 * .getEnrolmentPeriodInCurricularCoursesBy(executionSemester); if
-	 * (enrolmentPeriod != null &&
-	 * enrolmentPeriod.containsDate(requestDate)) { return true; } } return
-	 * false;
-	 */
-	return true;
+
+	for (final ExecutionSemester executionSemester : executionYear.getExecutionPeriodsSet()) {
+	    final EnrolmentPeriod enrolmentPeriod = degreeCurricularPlan
+		    .getEnrolmentPeriodInCurricularCoursesBy(executionSemester);
+	    if (enrolmentPeriod != null && enrolmentPeriod.containsDate(requestDate)) {
+		return true;
+	    }
+	}
+	return false;
     }
 
     @Override
