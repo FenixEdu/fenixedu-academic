5549045f592f47f81081f4b0011f56337fdeef21
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-10-07 14:32:16 +0000

    created new provider for student dismissals management interfaces

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsForDismissalsStudentCurricularPlanProvider.java
@@ -0,0 +1,33 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import net.sourceforge.fenixedu.dataTransferObject.student.IStudentCurricularPlanBean;
+import net.sourceforge.fenixedu.domain.ExecutionSemester;
+import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
+import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+
+import org.apache.commons.collections.comparators.ReverseComparator;
+
+import pt.ist.fenixWebFramework.renderers.DataProvider;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+
+public class ExecutionPeriodsForDismissalsStudentCurricularPlanProvider implements DataProvider {
+
+    public Object provide(Object source, Object currentValue) {
+	final List<ExecutionSemester> result = new ArrayList<ExecutionSemester>();
+	final StudentCurricularPlan studentCurricularPlan = ((IStudentCurricularPlanBean) source).getStudentCurricularPlan();
+	result.addAll(ExecutionSemester.readExecutionPeriodsInTimePeriod(studentCurricularPlan.getStartDate(), Calendar
+		.getInstance().getTime()));
+	Collections.sort(result, new ReverseComparator());
+	return result;
+    }
+
+    public Converter getConverter() {
+	return new DomainObjectKeyConverter();
+    }
+
+}
