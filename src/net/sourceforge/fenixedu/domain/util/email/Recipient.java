9f91b23e2c96403c64ee520e139c151d79355d29
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-10-22 12:24:40 +0000

    Temporary fix for problem regarding emails.

--- src/net/sourceforge/fenixedu/domain/util/email/Recipient.java
+++ src/net/sourceforge/fenixedu/domain/util/email/Recipient.java
@@ -2,22 +2,23 @@ package net.sourceforge.fenixedu.domain.util.email;
 
 import java.util.Set;
 
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.accessControl.Group;
 import pt.ist.fenixWebFramework.services.Service;
 
 public class Recipient extends Recipient_Base {
-    
+
     public Recipient() {
-        super();
-        setRootDomainObject(RootDomainObject.getInstance());
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
 
     public Recipient(final String toName, final Group members) {
-        this();
-        setRootDomainObject(RootDomainObject.getInstance());
-        setToName(toName);
-        setMembers(members);
+	this();
+	setRootDomainObject(RootDomainObject.getInstance());
+	setToName(toName);
+	setMembers(members);
     }
 
     public void delete() {
@@ -29,12 +30,11 @@ public class Recipient extends Recipient_Base {
     }
 
     public void addDestinationEmailAddresses(final Set<String> emailAddresses) {
-	final EmailAddressThread thread = new EmailAddressThread(getMembers(), emailAddresses);
-	thread.start();
-	try {
-	    thread.join();
-	} catch (InterruptedException e) {
-	    throw new Error(e);
+	for (final Person person : getMembers().getElements()) {
+	    final String emailAddress = person.getEmail();
+	    if (emailAddress != null && !emailAddress.isEmpty()) {
+		emailAddresses.add(emailAddress);
+	    }
 	}
     }
 
