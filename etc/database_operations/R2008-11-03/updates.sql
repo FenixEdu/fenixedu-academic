f5e1af5fd91b25c3eb8d8271ca6bca85c605e0d8
Luis Cruz <luis.cruz@ist.utl.pt>
2008-11-03 19:52:26 +0000

    Deployed HEAD.

--- etc/database_operations/R2008-11-03/updates.sql
+++ etc/database_operations/R2008-11-03/updates.sql
@@ -3,10 +3,6 @@
 
 -- Inserted at 2008-11-03T14:13:00.225Z
 
-alter table ACADEMIC_SERVICE_REQUEST add column LANGUAGE text;
-alter table COUNTRY add column COUNTRY_NATIONALITY text;
-alter table PARTY_CONTACT add column VISIBLE_TO_ALUMNI tinyint(1);
-
 
 create table PROFILE_REPORT (
   `DATE` text,
