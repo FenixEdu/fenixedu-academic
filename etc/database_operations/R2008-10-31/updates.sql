c94f099d02ad0b2ed36029c4fdef2f11efe79bbb
Luis Cruz <luis.cruz@ist.utl.pt>
2008-11-03 11:48:16 +0000

    Added missing sql statements.

--- etc/database_operations/R2008-10-31/updates.sql
+++ etc/database_operations/R2008-10-31/updates.sql
@@ -7,3 +7,5 @@ alter table SENDER add column KEY_UNIT int(11);
 alter table SENDER add column OJB_CONCRETE_CLASS text;
 alter table SENDER add index (KEY_UNIT);
 
+alter table TEACHING_INQUIRY add column GLOBAL_CLASSIFICATION_OF_THIS_C_U int(11);
+alter table TEACHING_INQUIRY add column WEAK_AND_STRONG_POINTS_OF_C_U_TEACHING_PROCESS text;
