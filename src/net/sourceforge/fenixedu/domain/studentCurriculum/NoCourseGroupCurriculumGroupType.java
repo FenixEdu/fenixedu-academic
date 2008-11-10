b3963e5f2356e3cceb87edc38a7342e2f8b4f403
Luís Egídio <luis.egidio@ist.utl.pt>
2008-11-10 18:01:46 +0000

    NoCourseGroupCurriculumGroupType: - getLocalizedName - getQualifiedName

--- src/net/sourceforge/fenixedu/domain/studentCurriculum/NoCourseGroupCurriculumGroupType.java
+++ src/net/sourceforge/fenixedu/domain/studentCurriculum/NoCourseGroupCurriculumGroupType.java
@@ -1,6 +1,11 @@
 package net.sourceforge.fenixedu.domain.studentCurriculum;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import net.sourceforge.fenixedu.domain.curricularRules.executors.ruleExecutors.CurricularRuleLevel;
+import pt.utl.ist.fenix.tools.util.StringAppender;
+import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public enum NoCourseGroupCurriculumGroupType {
 
@@ -23,4 +28,17 @@ public enum NoCourseGroupCurriculumGroupType {
     public CurricularRuleLevel getCurricularRuleLevel() {
 	return level;
     }
+
+    public String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    public String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(getQualifiedName());
+    }
+
+    public String getQualifiedName() {
+	return StringAppender.append(getClass().getSimpleName(), ".", name());
+    }
+
 }
