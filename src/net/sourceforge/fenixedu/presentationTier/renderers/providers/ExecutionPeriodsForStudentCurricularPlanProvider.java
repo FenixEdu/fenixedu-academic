c5a83b040a57e36cb3b16062fa59c36428be21fc
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-11-17 17:33:44 +0000

    ExecutionPeriodsForStudentCurricularPlanProvider: add all open or current execution semesters until current execution year

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsForStudentCurricularPlanProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/ExecutionPeriodsForStudentCurricularPlanProvider.java
@@ -4,13 +4,18 @@
 package net.sourceforge.fenixedu.presentationTier.renderers.providers;
 
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import net.sourceforge.fenixedu.dataTransferObject.student.IStudentCurricularPlanBean;
 import net.sourceforge.fenixedu.domain.ExecutionSemester;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+
+import org.apache.commons.collections.comparators.ReverseComparator;
+
 import pt.ist.fenixWebFramework.renderers.DataProvider;
 import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
 
@@ -27,16 +32,22 @@ public class ExecutionPeriodsForStudentCurricularPlanProvider implements DataPro
 	final StudentCurricularPlan studentCurricularPlan = ((IStudentCurricularPlanBean) source).getStudentCurricularPlan();
 
 	final List<ExecutionSemester> executionPeriodsInTimePeriod = ExecutionSemester.readExecutionPeriodsInTimePeriod(
-		studentCurricularPlan.getStartDate(), Calendar.getInstance().getTime());
+		studentCurricularPlan.getStartDate(), getEndDate());
 
 	final ExecutionSemester first = studentCurricularPlan.getDegreeCurricularPlan().getFirstExecutionPeriodEnrolments();
 	for (final ExecutionSemester executionSemester : executionPeriodsInTimePeriod) {
-	    if (first.isBeforeOrEquals(executionSemester)) {
+	    if (first.isBeforeOrEquals(executionSemester) && !executionSemester.isNotOpen()) {
 		result.add(executionSemester);
 	    }
 	}
 
+	Collections.sort(result, new ReverseComparator(ExecutionSemester.EXECUTION_PERIOD_COMPARATOR_BY_SEMESTER_AND_YEAR));
+
 	return result;
+    }
+
+    private Date getEndDate() {
+	return ExecutionYear.readCurrentExecutionYear().getLastExecutionPeriod().getEndDate();
     }
 
     public Converter getConverter() {
