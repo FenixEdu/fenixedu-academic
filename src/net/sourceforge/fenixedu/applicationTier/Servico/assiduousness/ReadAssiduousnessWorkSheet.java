ca358adf9b5d9258d5d5e7806158f265163ccaa9
Luís Egídio <luis.egidio@ist.utl.pt>
2008-11-24 11:16:27 +0000

    comment debug sysout

--- src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/ReadAssiduousnessWorkSheet.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/assiduousness/ReadAssiduousnessWorkSheet.java
@@ -185,7 +185,8 @@ public class ReadAssiduousnessWorkSheet extends FenixService {
 	Unit unit = assiduousness.getLastMailingUnitInDate(beginDate, endDate);
 	employeeWorkSheet.setUnit(unit);
 	if (unit != null) {
-	    System.out.println("Unit: " + unit.getIdInternal() + " " + unit.getName() + " " + unit.getCostCenterCode());
+	    // System.out.println("Unit: " + unit.getIdInternal() + " " +
+	    // unit.getName() + " " + unit.getCostCenterCode());
 	    employeeWorkSheet.setUnitCode((new DecimalFormat("0000")).format(unit.getCostCenterCode()));
 	} else {
 	    employeeWorkSheet.setUnitCode("");
