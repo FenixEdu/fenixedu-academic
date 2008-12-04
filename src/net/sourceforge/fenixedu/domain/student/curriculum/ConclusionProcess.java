d394cbd25a48c48b29a2c445fc2003911f7839d6
Luís Egídio <luis.egidio@ist.utl.pt>
2008-12-04 17:49:14 +0000

    ConclusionProcess, ConclusionProcessVersion model definition and first implementation

---
+++ src/net/sourceforge/fenixedu/domain/student/curriculum/ConclusionProcess.java
@@ -0,0 +1,116 @@
+package net.sourceforge.fenixedu.domain.student.curriculum;
+
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import net.sourceforge.fenixedu.dataTransferObject.student.RegistrationConclusionBean;
+import net.sourceforge.fenixedu.domain.Degree;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.degree.DegreeType;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.domain.student.Registration;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.YearMonthDay;
+
+abstract public class ConclusionProcess extends ConclusionProcess_Base {
+
+    private SortedSet<ConclusionProcessVersion> getSortedVersions(final Comparator<ConclusionProcessVersion> comparator) {
+	final SortedSet<ConclusionProcessVersion> sorted = new TreeSet<ConclusionProcessVersion>(comparator);
+	sorted.addAll(getVersionsSet());
+	return sorted;
+    }
+
+    private ConclusionProcessVersion getFirstVersion() {
+	return getSortedVersions(ConclusionProcessVersion.COMPARATOR_BY_CREATION_DATE_TIME_AND_ID).first();
+    }
+
+    public ConclusionProcessVersion getLastVersion() {
+	return getSortedVersions(ConclusionProcessVersion.COMPARATOR_BY_CREATION_DATE_TIME_AND_ID).last();
+    }
+
+    public DateTime getCreationDateTime() {
+	return getFirstVersion().getCreationDateTime();
+    }
+
+    public DateTime getLastModificationDateTime() {
+	return getLastVersion().getCreationDateTime();
+    }
+
+    public Person getResponsible() {
+	return getFirstVersion().getResponsible();
+    }
+
+    public Person getLastResponsible() {
+	return getLastVersion().getResponsible();
+    }
+
+    public Integer getFinalAverage() {
+	return getLastVersion().getFinalAverage();
+    }
+
+    public BigDecimal getAverage() {
+	return getLastVersion().getAverage();
+    }
+
+    public LocalDate getConclusionDate() {
+	return getLastVersion().getConclusionDate();
+    }
+
+    @Deprecated
+    public YearMonthDay getConclusionYearMonthDay() {
+	return new YearMonthDay(getConclusionDate());
+    }
+
+    public String getNotes() {
+	return getLastVersion().getNotes();
+    }
+
+    public ExecutionYear getIngressionYear() {
+	return getLastVersion().getIngressionYear();
+    }
+
+    public BigDecimal getCredits() {
+	return getLastVersion().getCredits();
+    }
+
+    final public void addVersions(final RegistrationConclusionBean bean) {
+	super.addVersions(new ConclusionProcessVersion(bean));
+	addSpecificVersionInfo();
+    }
+
+    abstract protected void addSpecificVersionInfo();
+
+    public boolean isCycleConclusionProcess() {
+	return false;
+    }
+
+    public boolean isRegistrationConclusionProcess() {
+	return false;
+    }
+
+    @Override
+    final public void addVersions(final ConclusionProcessVersion versions) {
+	throw new DomainException("error.ConclusionProcess.must.use.addVersions.with.bean");
+    }
+
+    @Override
+    public void removeVersions(ConclusionProcessVersion versions) {
+	throw new DomainException("error.ConclusionProcess.method.not.allowed");
+    }
+
+    abstract public Registration getRegistration();
+
+    public Degree getDegree() {
+	return getRegistration().getDegree();
+    }
+
+    public DegreeType getDegreeType() {
+	return getDegree().getDegreeType();
+    }
+
+}
---
+++ src/net/sourceforge/fenixedu/domain/student/curriculum/RegistrationConclusionProcess.java
@@ -0,0 +1,86 @@
+package net.sourceforge.fenixedu.domain.student.curriculum;
+
+import net.sourceforge.fenixedu.dataTransferObject.student.RegistrationConclusionBean;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.degree.DegreeType;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.domain.student.Registration;
+import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationState;
+import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationStateType;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+public class RegistrationConclusionProcess extends RegistrationConclusionProcess_Base {
+
+    private RegistrationConclusionProcess(final RegistrationConclusionBean bean) {
+	super();
+	super.setRootDomainObject(RootDomainObject.getInstance());
+
+	final Registration registration = bean.getRegistration();
+	final ExecutionYear conclusionYear = bean.getConclusionYear();
+
+	check(registration, "error.RegistrationConclusionProcess.argument.must.not.be.null");
+	check(conclusionYear, "error.RegistrationConclusionProcess.argument.must.not.be.null");
+
+	super.setRegistration(registration);
+	super.setConclusionYear(conclusionYear);
+	addVersions(bean);
+    }
+
+    @Override
+    public boolean isRegistrationConclusionProcess() {
+	return true;
+    }
+
+    public static void conclude(final RegistrationConclusionBean bean) {
+	if (bean.isConclusionProcessed()) {
+	    throw new DomainException("error.ConclusionProcess.already.concluded.must.update");
+	}
+
+	createRegistrationStates(new RegistrationConclusionProcess(bean));
+    }
+
+    private static void createRegistrationStates(final RegistrationConclusionProcess conclusionProcess) {
+	final Registration reg = conclusionProcess.getRegistration();
+	final Person responsible = conclusionProcess.getResponsible();
+	final DateTime creation = conclusionProcess.getCreationDateTime();
+
+	if (reg.getDegreeType() == DegreeType.MASTER_DEGREE) {
+	    if (!reg.isSchoolPartConcluded()) {
+		RegistrationState.createState(reg, responsible, creation, RegistrationStateType.SCHOOLPARTCONCLUDED);
+	    }
+	} else {
+	    RegistrationState.createState(reg, responsible, creation, RegistrationStateType.CONCLUDED);
+	}
+    }
+
+    final public void update(final Person responsible, final Integer finalAverage, final LocalDate conclusionDate,
+	    final String notes) {
+	addVersions(new RegistrationConclusionBean(getRegistration()));
+	getLastVersion().update(responsible, finalAverage, conclusionDate, notes);
+    }
+
+    @Override
+    protected void addSpecificVersionInfo() {
+	getLastVersion().setMasterDegreeThesis(getRegistration().getMasterDegreeThesis());
+    }
+
+    @Override
+    public void setRootDomainObject(RootDomainObject rootDomainObject) {
+	throw new DomainException("error.ConclusionProcess.method.not.allowed");
+    }
+
+    @Override
+    public void setRegistration(Registration registration) {
+	throw new DomainException("error.ConclusionProcess.method.not.allowed");
+    }
+
+    @Override
+    public void setConclusionYear(ExecutionYear conclusionYear) {
+	throw new DomainException("error.ConclusionProcess.method.not.allowed");
+    }
+
+}
