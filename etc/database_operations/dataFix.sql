bec371b93848129a4ac4a4246a2cdbc50e388138
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-12-04 20:36:17 +0000

    Data fix regarding a executionCourse merge that went wrong regarding the permittion group of files...

---
+++ etc/database_operations/dataFix.sql
@@ -0,0 +1,2 @@
+update FILE set PERMITTED_GROUP='executionCourseTeachersAndStudents($I(74049, \'ExecutionCourse\'))' WHERE PERMITTED_GROUP='executionCourseTeachersAndStudents($I(73828, \'ExecutionCourse\'))';
+
