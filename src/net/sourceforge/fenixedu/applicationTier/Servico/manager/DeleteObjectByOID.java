d40611cd200062350b9ab1163a8f5deccd22697f
Shezad Anavarali <shezad@ist.utl.pt>
2008-12-10 12:27:27 +0000

    fix

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteObjectByOID.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteObjectByOID.java
@@ -12,6 +12,7 @@ import org.apache.commons.beanutils.MethodUtils;
 
 import pt.ist.fenixWebFramework.security.accessControl.Checked;
 import pt.ist.fenixWebFramework.services.Service;
+import pt.ist.fenixframework.pstm.IllegalWriteException;
 
 /**
  * @author - Shezad Anavarali (shezad@ist.utl.pt)
@@ -26,6 +27,9 @@ public class DeleteObjectByOID extends FenixService {
 	    MethodUtils.invokeMethod(rootDomainObject.readDomainObjectByOID(clazz, idInternal), "delete", null);
 	} catch (InvocationTargetException e) {
 	    if (e.getTargetException() != null) {
+		if (e.getTargetException() instanceof IllegalWriteException) {
+		    throw ((IllegalWriteException) e.getTargetException());
+		}
 		throw new FenixServiceException(e.getTargetException());
 	    }
 	    throw new FenixServiceException(e);
