2be6211f6fa9958ce03dd412366e8412dfa15646
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2008-12-11 17:34:33 +0000

    Data fix

---
+++ etc/database_operations/dataFix.sql
@@ -0,0 +1,1 @@
+update FILE set PERMITTED_GROUP='executionCourseTeachersAndStudents($I(77420, \'ExecutionCourse\'))' WHERE PERMITTED_GROUP='executionCourseTeachersAndStudents($I(77291, \'ExecutionCourse\'))';
