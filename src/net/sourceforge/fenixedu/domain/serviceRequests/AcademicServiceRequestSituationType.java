d74d8751b9cecfb697a80ceef0ef23452aad04bd
Luís Egídio <luis.egidio@ist.utl.pt>
2008-12-12 10:28:08 +0000

    getLocalizedName

--- src/net/sourceforge/fenixedu/domain/serviceRequests/AcademicServiceRequestSituationType.java
+++ src/net/sourceforge/fenixedu/domain/serviceRequests/AcademicServiceRequestSituationType.java
@@ -1,5 +1,10 @@
 package net.sourceforge.fenixedu.domain.serviceRequests;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public enum AcademicServiceRequestSituationType {
 
     NEW,
@@ -28,6 +33,14 @@ public enum AcademicServiceRequestSituationType {
 
     public String getFullyQualifiedName() {
 	return AcademicServiceRequestSituationType.class.getName() + "." + name();
+    }
+
+    protected String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    protected String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(getQualifiedName());
     }
 
 }
