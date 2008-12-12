bcdabb46334bf0eb4a3c2d9f6453be9411e767ec
Luís Egídio <luis.egidio@ist.utl.pt>
2008-12-12 12:10:54 +0000

    AcademicServiceRequestSituationTypeProvider

---
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/serviceRequests/AcademicServiceRequestSituationTypeProvider.java
@@ -0,0 +1,20 @@
+package net.sourceforge.fenixedu.presentationTier.renderers.providers.serviceRequests;
+
+import java.util.Arrays;
+
+import net.sourceforge.fenixedu.domain.serviceRequests.AcademicServiceRequestSituationType;
+import pt.ist.fenixWebFramework.renderers.DataProvider;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+import pt.ist.fenixWebFramework.renderers.converters.EnumConverter;
+
+public class AcademicServiceRequestSituationTypeProvider implements DataProvider {
+
+    public Object provide(Object source, Object currentValue) {
+	return Arrays.asList(AcademicServiceRequestSituationType.values());
+    }
+
+    public Converter getConverter() {
+	return new EnumConverter();
+    }
+
+}
