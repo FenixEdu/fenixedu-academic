e431c507b16b2619a47bb8944f067a48f6d5ae53
João Figueiredo <joao.figueiredo@ist.utl.pt>
2008-12-30 18:47:05 +0000

    created hasShiftEnrolment method in Shift

--- src/net/sourceforge/fenixedu/domain/ShiftEnrolment.java
+++ src/net/sourceforge/fenixedu/domain/ShiftEnrolment.java
@@ -8,14 +8,12 @@ import org.joda.time.DateTime;
 
 public class ShiftEnrolment extends ShiftEnrolment_Base {
 
-    public static final Comparator<ShiftEnrolment> COMPARATOR_BY_DATE = new Comparator<ShiftEnrolment>() {
-
+    static public final Comparator<ShiftEnrolment> COMPARATOR_BY_DATE = new Comparator<ShiftEnrolment>() {
 	@Override
 	public int compare(ShiftEnrolment o1, ShiftEnrolment o2) {
 	    final int c = o1.getCreatedOn().compareTo(o2.getCreatedOn());
 	    return c == 0 ? DomainObject.COMPARATOR_BY_ID.compare(o1, o2) : c;
 	}
-
     };
 
     public ShiftEnrolment(final Shift shift, final Registration registration) {
@@ -31,6 +29,10 @@ public class ShiftEnrolment extends ShiftEnrolment_Base {
 	removeRegistration();
 	removeRootDomainObject();
 	deleteDomainObject();
+    }
+
+    public boolean hasRegistration(final Registration registration) {
+	return getRegistration() == registration;
     }
 
 }
