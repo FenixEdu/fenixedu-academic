44f0cf7b2f2bcb3cec184d7565653f2b4bda1ad0
Luis Cruz <luis.cruz@ist.utl.pt>
2009-01-14 17:34:26 +0000

    Changed semantics of Employee to mean an employee that is neither a teacher of an investigator.

--- src/net/sourceforge/fenixedu/domain/accessControl/AllEmployeesGroup.java
+++ src/net/sourceforge/fenixedu/domain/accessControl/AllEmployeesGroup.java
@@ -1,11 +1,32 @@
 package net.sourceforge.fenixedu.domain.accessControl;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.Role;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 
 public class AllEmployeesGroup extends RoleTypeGroup {
 
     public AllEmployeesGroup() {
 	super(RoleType.EMPLOYEE);
+    }
+
+    @Override
+    public Set<Person> getElements() {
+	final Set<Person> people = new HashSet<Person>();
+	for (final Person person : Role.getRoleByRoleType(getRoleType()).getAssociatedPersons()) {
+	    if (!person.hasRole(RoleType.TEACHER)) {
+		people.add(person);
+	    }
+	}
+	return people;
+    }
+
+    @Override
+    public boolean isMember(final Person person) {
+	return person != null && person.hasRole(RoleType.EMPLOYEE) && !person.hasRole(RoleType.TEACHER);
     }
 
 }
