d8faa673a4dd8153f17cb2733c2eb2b804db26ef
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2009-01-19 20:26:35 +0000

    Ticket #208171: fixed

--- src/net/sourceforge/fenixedu/presentationTier/TagLib/sop/v3/renderers/SopClassTimeTableLessonContentRenderer.java
+++ src/net/sourceforge/fenixedu/presentationTier/TagLib/sop/v3/renderers/SopClassTimeTableLessonContentRenderer.java
@@ -10,6 +10,7 @@ import net.sourceforge.fenixedu.dataTransferObject.InfoShift;
 import net.sourceforge.fenixedu.dataTransferObject.InfoShowOccupation;
 import net.sourceforge.fenixedu.dataTransferObject.InfoWrittenTest;
 import net.sourceforge.fenixedu.domain.FrequencyType;
+import net.sourceforge.fenixedu.presentationTier.Action.resourceAllocationManager.utils.SessionConstants;
 import net.sourceforge.fenixedu.presentationTier.TagLib.sop.v3.LessonSlot;
 import net.sourceforge.fenixedu.presentationTier.TagLib.sop.v3.LessonSlotContentRenderer;
 
@@ -46,8 +47,9 @@ public class SopClassTimeTableLessonContentRenderer implements LessonSlotContent
 	    strBuffer.append(context).append("/resourceAllocationManager/").append(
 		    "manageShift.do?method=prepareEditShift&amp;page=0").append("&amp;shift_oid=").append(
 		    infoShift.getIdInternal()).append("&amp;execution_course_oid=").append(infoExecutionCourse.getIdInternal())
-		    .append("&amp;executionPeriodOID=").append(infoExecutionCourse.getInfoExecutionPeriod().getIdInternal())
-		    .append("&amp;curricular_year_oid=").append(infoCurricularYear.getIdInternal()).append(
+		    .append("&amp;" + SessionConstants.ACADEMIC_INTERVAL + "=").append(
+			    infoExecutionCourse.getAcademicInterval().getResumedRepresentationInStringFormat()).append(
+			    "&amp;curricular_year_oid=").append(infoCurricularYear.getIdInternal()).append(
 			    "&amp;execution_degree_oid=").append(infoExecutionDegree.getIdInternal()).append("'>").append(
 			    lesson.getInfoShift().getShiftTypesCodePrettyPrint()).append("</a>").append(")&nbsp;");
 
@@ -76,8 +78,9 @@ public class SopClassTimeTableLessonContentRenderer implements LessonSlotContent
 	    strBuffer.append(context).append("/resourceAllocationManager/").append(
 		    "manageShift.do?method=prepareEditShift&amp;page=0").append("&amp;shift_oid=").append(
 		    infoShift.getIdInternal()).append("&amp;execution_course_oid=").append(infoExecutionCourse.getIdInternal())
-		    .append("&amp;executionPeriodOID=").append(infoExecutionCourse.getInfoExecutionPeriod().getIdInternal())
-		    .append("&amp;curricular_year_oid=").append(infoCurricularYear.getIdInternal()).append(
+		    .append("&amp;" + SessionConstants.ACADEMIC_INTERVAL + "=").append(
+			    infoExecutionCourse.getAcademicInterval().getResumedRepresentationInStringFormat()).append(
+			    "&amp;curricular_year_oid=").append(infoCurricularYear.getIdInternal()).append(
 			    "&amp;execution_degree_oid=").append(infoExecutionDegree.getIdInternal()).append("'>").append(
 			    lesson.getShiftTypeCodesPrettyPrint()).append("</a>").append(")&nbsp;");
 
@@ -100,7 +103,7 @@ public class SopClassTimeTableLessonContentRenderer implements LessonSlotContent
 	    }
 	    strBuffer.append(" - ");
 	    strBuffer.append(infoExam.getSeason().getSeason());
-	    strBuffer.append("ª época");
+	    strBuffer.append("ï¿½ ï¿½poca");
 
 	} else if (showOccupation instanceof InfoWrittenTest) {
 	    InfoWrittenTest infoWrittenTest = (InfoWrittenTest) showOccupation;
