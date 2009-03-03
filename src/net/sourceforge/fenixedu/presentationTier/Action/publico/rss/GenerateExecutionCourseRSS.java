a97c15574cdfbbbfb89599335089cb5fa36e88e0
Artur Ventura <artur.ventura@ist.utl.pt>
2009-03-03 17:32:03 +0000

    Correction to small bug with Summaries RSS link

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/rss/GenerateExecutionCourseRSS.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/rss/GenerateExecutionCourseRSS.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.domain.DomainObject;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
+import net.sourceforge.fenixedu.presentationTier.servlets.filters.ContentInjectionRewriter;
 import pt.utl.ist.fenix.tools.util.StringAppender;
 import de.nava.informa.core.ChannelIF;
 import de.nava.informa.core.ItemIF;
@@ -30,7 +31,8 @@ public abstract class GenerateExecutionCourseRSS extends InformaRSSAction {
 	final String appContext = PropertiesManager.getProperty("app.context");
 	final String context = (appContext != null && appContext.length() > 0) ? "/" + appContext : "";
 	final String commonLocalUrl = StringAppender.append(context, "/publico/executionCourse.do?method=", getMethodName(),
-		"&executionCourseID=", executionCourse.getIdInternal().toString(), "#");
+		"&executionCourseID=", executionCourse.getIdInternal().toString(), "&",
+		ContentInjectionRewriter.CONTEXT_ATTRIBUTE_NAME, "=", executionCourse.getSite().getReversePath(), "#");
 
 	for (final DomainObject domainObject : ((Collection<DomainObject>) getObjects(executionCourse))) {
 	    final ItemIF item = new Item();
