6410e9659f01ce0140b51c5acdab4bb9d4f98ed0
Luis Cruz <luis.cruz@ist.utl.pt>
2009-03-18 17:22:59 +0000

    Replace publication file as well when replacing thesis file.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/thesis/CreateThesisDissertationFile.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/thesis/CreateThesisDissertationFile.java
@@ -1,7 +1,10 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.thesis;
 
+import net.sourceforge.fenixedu.domain.accessControl.Group;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import net.sourceforge.fenixedu.domain.person.RoleType;
+import net.sourceforge.fenixedu.domain.research.result.ResearchResultDocumentFile;
+import net.sourceforge.fenixedu.domain.research.result.ResearchResultDocumentFile.FileResultPermittedGroupType;
 import net.sourceforge.fenixedu.domain.thesis.Thesis;
 import net.sourceforge.fenixedu.domain.thesis.ThesisFile;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
@@ -32,6 +35,22 @@ public class CreateThesisDissertationFile extends CreateThesisFile {
 	file.setLanguage(language);
 
 	thesis.setDissertation(file);
+
+	final net.sourceforge.fenixedu.domain.research.result.publication.Thesis publication = thesis.getPublication();
+	if (publication != null) {
+	    final ResearchResultDocumentFile researchResultDocumentFile = publication.getResultDocumentFilesIterator().next();
+
+	    final FileResultPermittedGroupType permittedGroupType = researchResultDocumentFile.getFileResultPermittedGroupType();
+	    final Group permittedGroup = researchResultDocumentFile.getPermittedGroup();
+
+	    researchResultDocumentFile.delete();
+
+	    publication.addDocumentFile(file.getFilename(), file.getFilename(), permittedGroupType,
+		    file.getMimeType(), file.getChecksum(), file.getChecksumAlgorithm(), file.getSize(),
+		    file.getExternalStorageIdentification(), permittedGroup);
+	    publication.setThesis(thesis);
+
+	}
     }
 
 }
