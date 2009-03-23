24a381a00b0e239885ed46ffd2519ef1683f4939
Shezad Anavarali <shezad@ist.utl.pt>
2009-03-23 20:35:02 +0000

    teacher report alterations

--- src/net/sourceforge/fenixedu/domain/inquiries/teacher/TeachingInquiry.java
+++ src/net/sourceforge/fenixedu/domain/inquiries/teacher/TeachingInquiry.java
@@ -40,49 +40,100 @@ public class TeachingInquiry extends TeachingInquiry_Base {
 	teachingInquiry.setEstablishedScheduleSuitable(answersMap.get("establishedScheduleSuitable").getValueAsInteger());
 	teachingInquiry
 		.setEstablishedScheduleNotSuitableReason(answersMap.get("establishedScheduleNotSuitableReason").getValue());
+	teachingInquiry.setAvailableInfrastructureSuitable(answersMap.get("availableInfrastructureSuitable").getValueAsInteger());
+	teachingInquiry.setAvailableInfrastructureSuitableReason(answersMap.get("availableInfrastructureSuitableReason")
+		.getValue());
+
 	teachingInquiry.setStudentsReadyForFollowingCU(answersMap.get("studentsReadyForFollowingCU").getValueAsInteger());
 	teachingInquiry.setEvaluationMethodSuitableForCUTeachingTypeAndObjective(answersMap.get(
 		"evaluationMethodSuitableForCUTeachingTypeAndObjective").getValueAsInteger());
 	teachingInquiry.setDisturbingEventsInClasses(answersMap.get("disturbingEventsInClasses").getValueAsInteger());
+	teachingInquiry.setDisturbingEventsInClassesReasonInfrastructure(answersMap.get(
+		"disturbingEventsInClassesReasonInfrastructure").getValueAsBoolean());
+	teachingInquiry.setDisturbingEventsInClassesReasonLowAssiduity(answersMap.get(
+		"disturbingEventsInClassesReasonLowAssiduity").getValueAsBoolean());
+	teachingInquiry.setDisturbingEventsInClassesReasonIndiscipline(answersMap.get(
+		"disturbingEventsInClassesReasonIndiscipline").getValueAsBoolean());
+	teachingInquiry.setDisturbingEventsInClassesReasonIntermediateEvaluations(answersMap.get(
+		"disturbingEventsInClassesReasonIntermediateEvaluations").getValueAsBoolean());
 	teachingInquiry
 		.setDisturbingEventsInClassesDescription(answersMap.get("disturbingEventsInClassesDescription").getValue());
 
 	teachingInquiry.setSemesterStartAverageStudentNumberInTheorical(answersMap.get(
-		"semesterStartAverageStudentNumberInTheorical").getValueAsInteger());
+		"semesterStartAverageStudentNumberInTheorical").getValue());
 	teachingInquiry.setSemesterMiddleAverageStudentNumberInTheorical(answersMap.get(
-		"semesterMiddleAverageStudentNumberInTheorical").getValueAsInteger());
+		"semesterMiddleAverageStudentNumberInTheorical").getValue());
 	teachingInquiry.setSemesterEndAverageStudentNumberInTheorical(answersMap
-		.get("semesterEndAverageStudentNumberInTheorical").getValueAsInteger());
+		.get("semesterEndAverageStudentNumberInTheorical").getValue());
+	teachingInquiry.setSemesterStartActiveAndInteressedStudentsRatioInTheorical(answersMap.get(
+		"semesterStartActiveAndInteressedStudentsRatioInTheorical").getValue());
+	teachingInquiry.setSemesterMiddleActiveAndInteressedStudentsRatioInTheorica(answersMap.get(
+		"semesterMiddleActiveAndInteressedStudentsRatioInTheorica").getValue());
+	teachingInquiry.setSemesterEndActiveAndInteressedStudentsRatioInTheorical(answersMap.get(
+		"semesterEndActiveAndInteressedStudentsRatioInTheorical").getValue());
 
 	teachingInquiry.setSemesterStartAverageStudentNumberInProblems(answersMap.get(
-		"semesterStartAverageStudentNumberInProblems").getValueAsInteger());
+		"semesterStartAverageStudentNumberInProblems").getValue());
 	teachingInquiry.setSemesterMiddleAverageStudentNumberInProblems(answersMap.get(
-		"semesterMiddleAverageStudentNumberInProblems").getValueAsInteger());
+		"semesterMiddleAverageStudentNumberInProblems").getValue());
 	teachingInquiry.setSemesterEndAverageStudentNumberInProblems(answersMap.get("semesterEndAverageStudentNumberInProblems")
-		.getValueAsInteger());
+		.getValue());
+	teachingInquiry.setSemesterStartActiveAndInteressedStudentsRatioInProblems(answersMap.get(
+		"semesterStartActiveAndInteressedStudentsRatioInProblems").getValue());
+	teachingInquiry.setSemesterMiddleActiveAndInteressedStudentsRatioInProblems(answersMap.get(
+		"semesterMiddleActiveAndInteressedStudentsRatioInProblems").getValue());
+	teachingInquiry.setSemesterEndActiveAndInteressedStudentsRatioInProblems(answersMap.get(
+		"semesterEndActiveAndInteressedStudentsRatioInProblems").getValue());
 
 	teachingInquiry.setSemesterStartAverageStudentNumberInLabs(answersMap.get("semesterStartAverageStudentNumberInLabs")
-		.getValueAsInteger());
+		.getValue());
 	teachingInquiry.setSemesterMiddleAverageStudentNumberInLabs(answersMap.get("semesterMiddleAverageStudentNumberInLabs")
-		.getValueAsInteger());
+		.getValue());
 	teachingInquiry.setSemesterEndAverageStudentNumberInLabs(answersMap.get("semesterEndAverageStudentNumberInLabs")
-		.getValueAsInteger());
+		.getValue());
+	teachingInquiry.setSemesterStartActiveAndInteressedStudentsRatioInLabs(answersMap.get(
+		"semesterStartActiveAndInteressedStudentsRatioInLabs").getValue());
+	teachingInquiry.setSemesterMiddleActiveAndInteressedStudentsRatioInLabs(answersMap.get(
+		"semesterMiddleActiveAndInteressedStudentsRatioInLabs").getValue());
+	teachingInquiry.setSemesterEndActiveAndInteressedStudentsRatioInLabs(answersMap.get(
+		"semesterEndActiveAndInteressedStudentsRatioInLabs").getValue());
 
 	teachingInquiry.setSemesterStartAverageStudentNumberInSeminary(answersMap.get(
-		"semesterStartAverageStudentNumberInSeminary").getValueAsInteger());
+		"semesterStartAverageStudentNumberInSeminary").getValue());
 	teachingInquiry.setSemesterMiddleAverageStudentNumberInSeminary(answersMap.get(
-		"semesterMiddleAverageStudentNumberInSeminary").getValueAsInteger());
+		"semesterMiddleAverageStudentNumberInSeminary").getValue());
 	teachingInquiry.setSemesterEndAverageStudentNumberInSeminary(answersMap.get("semesterEndAverageStudentNumberInSeminary")
-		.getValueAsInteger());
+		.getValue());
+	teachingInquiry.setSemesterStartActiveAndInteressedStudentsRatioInSeminary(answersMap.get(
+		"semesterStartActiveAndInteressedStudentsRatioInSeminary").getValue());
+	teachingInquiry.setSemesterMiddleActiveAndInteressedStudentsRatioInSeminary(answersMap.get(
+		"semesterMiddleActiveAndInteressedStudentsRatioInSeminary").getValue());
+	teachingInquiry.setSemesterEndActiveAndInteressedStudentsRatioInSeminary(answersMap.get(
+		"semesterEndActiveAndInteressedStudentsRatioInSeminary").getValue());
 
 	teachingInquiry.setSemesterStartAverageStudentNumberInProject(answersMap
-		.get("semesterStartAverageStudentNumberInProject").getValueAsInteger());
+		.get("semesterStartAverageStudentNumberInProject").getValue());
 	teachingInquiry.setSemesterMiddleAverageStudentNumberInProject(answersMap.get(
-		"semesterMiddleAverageStudentNumberInProject").getValueAsInteger());
+		"semesterMiddleAverageStudentNumberInProject").getValue());
 	teachingInquiry.setSemesterEndAverageStudentNumberInProject(answersMap.get("semesterEndAverageStudentNumberInProject")
-		.getValueAsInteger());
+		.getValue());
+	teachingInquiry.setSemesterStartActiveAndInteressedStudentsRatioInProject(answersMap.get(
+		"semesterStartActiveAndInteressedStudentsRatioInProject").getValue());
+	teachingInquiry.setSemesterMiddleActiveAndInteressedStudentsRatioInProject(answersMap.get(
+		"semesterMiddleActiveAndInteressedStudentsRatioInProject").getValue());
+	teachingInquiry.setSemesterEndActiveAndInteressedStudentsRatioInProject(answersMap.get(
+		"semesterEndActiveAndInteressedStudentsRatioInProject").getValue());
+
+	teachingInquiry.setReportDisclosureToAcademicComunity(answersMap.get("reportDisclosureToAcademicComunity")
+		.getValueAsBoolean());
+	teachingInquiry.setResultsDisclosureToAcademicComunity(answersMap.get("resultsDisclosureToAcademicComunity")
+		.getValueAsBoolean());
 
 	teachingInquiry.setActiveAndInteressedStudentsRatio(answersMap.get("activeAndInteressedStudentsRatio").getValue());
+
+	teachingInquiry.setClarificationOfDoubtsOutsideClasses(answersMap.get("clarificationOfDoubtsOutsideClasses")
+		.getValueAsInteger());
+	teachingInquiry.setAutonomousWork(answersMap.get("autonomousWork").getValueAsInteger());
 	teachingInquiry.setStudentsPerformance(answersMap.get("studentsPerformance").getValueAsInteger());
 	teachingInquiry.setClassesAndOtherActivitiesFrequency(answersMap.get("classesAndOtherActivitiesFrequency")
 		.getValueAsInteger());
@@ -91,6 +142,7 @@ public class TeachingInquiry extends TeachingInquiry_Base {
 	teachingInquiry.setGlobalQualityOfTeachingInCU(answersMap.get("globalQualityOfTeachingInCU").getValueAsInteger());
 	teachingInquiry.setPedagogicalActivitiesDeveloped(answersMap.get("pedagogicalActivitiesDeveloped").getValueAsInteger());
 	teachingInquiry.setRelativePedagogicalInitiatives(answersMap.get("relativePedagogicalInitiatives").getValue());
+	teachingInquiry.setGeneralCommentToCUOperation(answersMap.get("generalCommentToCUOperation").getValue());
 
 	teachingInquiry.setNumberOfExams(answersMap.get("numberOfExams").getValueAsInteger());
 	teachingInquiry.setNumberOfTests(answersMap.get("numberOfTests").getValueAsInteger());
@@ -110,12 +162,20 @@ public class TeachingInquiry extends TeachingInquiry_Base {
 	teachingInquiry.setCriticalSenseAndReflexiveSpirit(answersMap.get("criticalSenseAndReflexiveSpirit").getValueAsInteger());
 	teachingInquiry.setCooperationAndCommunicationCapacity(answersMap.get("cooperationAndCommunicationCapacity")
 		.getValueAsInteger());
+	teachingInquiry.setIncreaseAutonoumousLearningCapacity(answersMap.get("increaseAutonoumousLearningCapacity")
+		.getValueAsInteger());
+	teachingInquiry.setSocialAndProfessionalContextAnalysis(answersMap.get("socialAndProfessionalContextAnalysis")
+		.getValueAsInteger());	
+	
 	teachingInquiry.setGlobalClassificationOfThisCU(answersMap.get("globalClassificationOfThisCU").getValueAsInteger());
 	teachingInquiry.setWeakPointsOfCUTeachingProcess(answersMap.get("weakPointsOfCUTeachingProcess").getValue());
 	teachingInquiry.setStrongPointsOfCUTeachingProcess(answersMap.get("strongPointsOfCUTeachingProcess").getValue());
 	teachingInquiry.setFinalCommentsAndImproovements(answersMap.get("finalCommentsAndImproovements").getValue());
 	teachingInquiry.setNegativeResultsResolutionAndImproovementPlanOfAction(answersMap.get(
 		"negativeResultsResolutionAndImproovementPlanOfAction").getValue());
+	
+	teachingInquiry.setResponsibleTeacherReportDisclosureToAcademicComunity(answersMap.get(
+		"responsibleTeacherReportDisclosureToAcademicComunity").getValueAsBoolean());
     }
 
     public void delete() {
