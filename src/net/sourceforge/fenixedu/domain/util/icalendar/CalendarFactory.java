615944c8e24380012ee4f6dfc3fe066ca9c7d7fe
Artur Ventura <artur.ventura@ist.utl.pt>
2009-04-27 13:32:32 +0000

    Bug fix on timezone info.

--- src/net/sourceforge/fenixedu/domain/util/icalendar/CalendarFactory.java
+++ src/net/sourceforge/fenixedu/domain/util/icalendar/CalendarFactory.java
@@ -1,6 +1,5 @@
 package net.sourceforge.fenixedu.domain.util.icalendar;
 
-import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.MessageDigest;
@@ -12,13 +11,11 @@ import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.DateTime;
 import net.fortuna.ical4j.model.TimeZone;
-import net.fortuna.ical4j.model.TimeZoneRegistry;
 import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.component.VTimeZone;
 import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.Description;
-import net.fortuna.ical4j.model.property.Geo;
 import net.fortuna.ical4j.model.property.Location;
 import net.fortuna.ical4j.model.property.ProdId;
 import net.fortuna.ical4j.model.property.Uid;
@@ -28,28 +25,29 @@ import net.fortuna.ical4j.model.property.Version;
 public class CalendarFactory {
     public static String PROD_ID_COMPANY = "Instituto Superior Técnico";
     public static String PROD_ID_APPLICATION = "Sistema Fenix";
+    public static TimeZone TIMEZONE = TimeZoneRegistryFactory.getInstance().createRegistry().getTimeZone("Europe/Lisbon");
 
-    private static String digest(String obj){
+    private static String digest(String obj) {
 	byte[] defaultBytes = obj.getBytes();
-	try{
-		MessageDigest algorithm = MessageDigest.getInstance("MD5");
-		algorithm.reset();
-		algorithm.update(defaultBytes);
-		byte messageDigest[] = algorithm.digest();
-	            
-		StringBuffer hexString = new StringBuffer();
-		for (int i=0;i<messageDigest.length;i++) {
-			hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
-		}
-		String foo = messageDigest.toString();
-		obj=hexString+"";
-	}catch(NoSuchAlgorithmException nsae){
-	            
+	try {
+	    MessageDigest algorithm = MessageDigest.getInstance("MD5");
+	    algorithm.reset();
+	    algorithm.update(defaultBytes);
+	    byte messageDigest[] = algorithm.digest();
+
+	    StringBuffer hexString = new StringBuffer();
+	    for (int i = 0; i < messageDigest.length; i++) {
+		hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+	    }
+	    String foo = messageDigest.toString();
+	    obj = hexString + "";
+	} catch (NoSuchAlgorithmException nsae) {
+
 	}
 	return obj;
 
     }
-    
+
     private static VEvent convertEventBean(EventBean event) {
 	Date begin;
 	Date end;
@@ -62,29 +60,27 @@ public class CalendarFactory {
 	}
 	VEvent vEvent = new VEvent(begin, end, event.getTitle());
 
-	TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
-	TimeZone timezone = registry.getTimeZone("Europe/Lisbon");
-	VTimeZone tz = timezone.getVTimeZone();
-	vEvent.getProperties().add(tz.getTimeZoneId());
-	
-	if (event.getLocation() != null){
+	vEvent.getStartDate().setTimeZone(TIMEZONE);
+	vEvent.getEndDate().setTimeZone(TIMEZONE);
+
+	if (event.getLocation() != null) {
 	    vEvent.getProperties().add(new Location(event.getLocation()));
 	}
-	
-	if (event.getUrl() != null){
+
+	if (event.getUrl() != null) {
 	    try {
 		vEvent.getProperties().add(new Url(new URI(event.getUrl())));
-	    } catch (URISyntaxException e) {}
+	    } catch (URISyntaxException e) {
+	    }
 	}
-	
-	if (event.getNote() != null){
+
+	if (event.getNote() != null) {
 	    vEvent.getProperties().add(new Description(event.getNote()));
 	}
-	
+
 	Uid uid = new Uid(digest(event.getTitle() + "-" + begin.toGMTString() + "-" + end.toGMTString()));
 	vEvent.getProperties().add(uid);
-	 
-	
+
 	return vEvent;
     }
 
@@ -94,7 +90,10 @@ public class CalendarFactory {
 	calendar.getProperties().add(new ProdId("-//" + PROD_ID_COMPANY + "//" + PROD_ID_APPLICATION + "//PT"));
 	calendar.getProperties().add(Version.VERSION_2_0);
 	calendar.getProperties().add(CalScale.GREGORIAN);
-	
+
+	VTimeZone tz = TIMEZONE.getVTimeZone();
+	calendar.getComponents().add(tz);
+
 	for (EventBean eventBean : events) {
 	    calendar.getComponents().add(convertEventBean(eventBean));
 	}
