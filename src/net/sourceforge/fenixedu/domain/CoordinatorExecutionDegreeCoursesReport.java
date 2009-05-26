cc925d4e52d0de34c9fea867b1f1db5337d99402
Shezad Anavarali <shezad@ist.utl.pt>
2009-05-26 17:20:05 +0000

    intial version

---
+++ src/net/sourceforge/fenixedu/domain/CoordinatorExecutionDegreeCoursesReport.java
@@ -0,0 +1,38 @@
+package net.sourceforge.fenixedu.domain;
+
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.injectionCode.AccessControl;
+import pt.ist.fenixWebFramework.services.Service;
+
+public class CoordinatorExecutionDegreeCoursesReport extends CoordinatorExecutionDegreeCoursesReport_Base {
+
+    public CoordinatorExecutionDegreeCoursesReport() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
+    }
+
+    private CoordinatorExecutionDegreeCoursesReport(final ExecutionDegree executionDegree,
+	    final ExecutionInterval executionInterval) {
+	this();
+
+	if (executionDegree.getExecutionDegreeCoursesReports(executionInterval) != null) {
+	    throw new DomainException(
+		    "error.CoordinatorExecutionDegreeCoursesReport.alreadyExistsForDegreeAndIntervaAndCoordinator");
+	}
+
+	final Coordinator coordinator = executionDegree.getCoordinatorByTeacher(AccessControl.getPerson());
+	if (coordinator == null || !coordinator.isResponsible()) {
+	    throw new DomainException("error.CoordinatorExecutionDegreeCoursesReport.isNotResponsible");
+	}
+	setCoordinator(coordinator);
+	setExecutionDegree(executionDegree);
+	setExecutionInterval(executionInterval);
+    }
+
+    @Service
+    public static CoordinatorExecutionDegreeCoursesReport makeNew(final ExecutionDegree executionDegree,
+	    final ExecutionInterval executionInterval) {
+	return new CoordinatorExecutionDegreeCoursesReport(executionDegree, executionInterval);
+    }
+
+}
