c45c1071503dd061794f34cba4b3c8f08dae3212
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2009-05-27 16:01:42 +0000

    Added deposit support

--- src/net/sourceforge/fenixedu/domain/accounting/events/serviceRequests/BolonhaDegreeDiplomaRequestEvent.java
+++ src/net/sourceforge/fenixedu/domain/accounting/events/serviceRequests/BolonhaDegreeDiplomaRequestEvent.java
@@ -1,6 +1,10 @@
 package net.sourceforge.fenixedu.domain.accounting.events.serviceRequests;
 
+import java.util.Collections;
+import java.util.Set;
+
 import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.accounting.EntryType;
 import net.sourceforge.fenixedu.domain.accounting.EventType;
 import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOffice;
 import net.sourceforge.fenixedu.domain.serviceRequests.documentRequests.DiplomaRequest;
@@ -15,6 +19,16 @@ public class BolonhaDegreeDiplomaRequestEvent extends BolonhaDegreeDiplomaReques
 	    final Person person, final DiplomaRequest diplomaRequest) {
 	this();
 	super.init(administrativeOffice, eventType, person, diplomaRequest);
+    }
+
+    @Override
+    public boolean isDepositSupported() {
+	return true;
+    }
+
+    @Override
+    public Set<EntryType> getPossibleEntryTypesForDeposit() {
+	return Collections.singleton(EntryType.DIPLOMA_REQUEST_FEE);
     }
 
 }
