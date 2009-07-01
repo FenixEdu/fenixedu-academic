052aaab48551fe5098fdff36a02a718a80561627
João Neves <joaoroxoneves@ist.utl.pt>
2009-07-01 18:06:48 +0000

    Refactor: @Mappings added to AcademicAdminOfficePricesManagementDispatchAction Old struts and tiles definitions removed.

--- src/net/sourceforge/fenixedu/presentationTier/Action/administrativeOffice/pricesManagement/AcademicAdminOfficePricesManagementDispatchAction.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/administrativeOffice/pricesManagement/AcademicAdminOfficePricesManagementDispatchAction.java
@@ -4,7 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOffice;
 import net.sourceforge.fenixedu.presentationTier.Action.commons.administrativeOffice.pricesManagement.PricesManagementDispatchAction;
+import pt.ist.fenixWebFramework.struts.annotations.Forward;
+import pt.ist.fenixWebFramework.struts.annotations.Forwards;
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
 
+@Mapping(path = "/pricesManagement", module = "academicAdminOffice")
+@Forwards( {
+	@Forward(name = "viewPrices", path = "/academicAdminOffice/pricesManagement/viewPrices.jsp"),
+	@Forward(name = "editPrice", path = "/academicAdminOffice/pricesManagement/editPrice.jsp")
+})
 public class AcademicAdminOfficePricesManagementDispatchAction extends PricesManagementDispatchAction {
 
     @Override
