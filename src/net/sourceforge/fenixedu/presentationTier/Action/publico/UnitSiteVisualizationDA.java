d0b3b489b4944f4fcb479d1a16f53d9970cae36a
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2009-07-02 15:45:45 +0000

    Fixed "missing" announcements. The problem was a constant string encoding

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/UnitSiteVisualizationDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/UnitSiteVisualizationDA.java
@@ -38,7 +38,7 @@ public class UnitSiteVisualizationDA extends SiteVisualizationDA {
 
     public static final int ANNOUNCEMENTS_NUMBER = 3;
 
-    public static final MultiLanguageString ANNOUNCEMENTS_NAME = MultiLanguageString.i18n().add("pt", "Anï¿½ncios").finish();
+    public static final MultiLanguageString ANNOUNCEMENTS_NAME = MultiLanguageString.i18n().add("pt", "Anúncios").finish();
 
     public static final MultiLanguageString EVENTS_NAME = MultiLanguageString.i18n().add("pt", "Eventos").finish();
 
