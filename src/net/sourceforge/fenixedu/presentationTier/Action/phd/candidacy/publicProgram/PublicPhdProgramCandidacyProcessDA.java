2a59423c76a938456f826f1191328ae404240897
Luis Cruz <luis.cruz@ist.utl.pt>
2009-07-13 11:44:53 +0000

    Fixed import problems.

--- src/net/sourceforge/fenixedu/presentationTier/Action/phd/candidacy/publicProgram/PublicPhdProgramCandidacyProcessDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/phd/candidacy/publicProgram/PublicPhdProgramCandidacyProcessDA.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
