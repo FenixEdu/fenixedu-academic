009866ddae7c45126c206e931e4c70cd996cd361
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-07-14 16:12:34 +0000

    Public phd program candidacy: when creating referee also send email information

--- src/net/sourceforge/fenixedu/domain/phd/candidacy/PhdCandidacyReferee.java
+++ src/net/sourceforge/fenixedu/domain/phd/candidacy/PhdCandidacyReferee.java
@@ -1,9 +1,13 @@
 package net.sourceforge.fenixedu.domain.phd.candidacy;
 
+import java.util.ResourceBundle;
 import java.util.UUID;
 
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.phd.PhdIndividualProgramProcess;
 import net.sourceforge.fenixedu.domain.phd.alert.PhdCandidacyRefereeAlert;
+import pt.ist.fenixWebFramework.services.Service;
+import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public class PhdCandidacyReferee extends PhdCandidacyReferee_Base {
 
@@ -25,6 +29,7 @@ public class PhdCandidacyReferee extends PhdCandidacyReferee_Base {
 	setValue(UUID.randomUUID().toString());
 
 	new PhdCandidacyRefereeAlert(this);
+	sendEmail();
     }
 
     @Override
@@ -38,5 +43,24 @@ public class PhdCandidacyReferee extends PhdCandidacyReferee_Base {
 
     public PhdIndividualProgramProcess getIndividualProgramProcess() {
 	return getPhdProgramCandidacyProcess().getIndividualProgramProcess();
+    }
+
+    @Service
+    public void sendEmail() {
+	sendEmail(createSubjet(), createBody());
+    }
+
+    private String createSubjet() {
+	final ResourceBundle bundle = ResourceBundle.getBundle("resources.PhdResources", Language.getLocale());
+	return String.format(bundle.getString("message.phd.email.subject.referee"), getPerson().getName());
+    }
+
+    private Person getPerson() {
+	return getPhdProgramCandidacyProcess().getPerson();
+    }
+
+    private String createBody() {
+	final ResourceBundle bundle = ResourceBundle.getBundle("resources.PhdResources", Language.getLocale());
+	return String.format(bundle.getString("message.phd.email.body.referee"), getValue());
     }
 }
