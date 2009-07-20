a0f9d4cda0d121685f16aabcbf531adfbff8297c
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2009-07-20 11:05:19 +0000

    Fixed Flunked Reports, now only showing flunks that are the last state on the requested year.

--- src/net/sourceforge/fenixedu/domain/reports/FlunkedReportFile.java
+++ src/net/sourceforge/fenixedu/domain/reports/FlunkedReportFile.java
@@ -1,9 +1,16 @@
 package net.sourceforge.fenixedu.domain.reports;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
 import net.sourceforge.fenixedu.domain.Degree;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationState;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationStateType;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+
 import pt.utl.ist.fenix.tools.util.excel.Spreadsheet;
 import pt.utl.ist.fenix.tools.util.excel.Spreadsheet.Row;
 
@@ -31,14 +38,20 @@ public class FlunkedReportFile extends FlunkedReportFile_Base {
 	for (final Degree degree : Degree.readNotEmptyDegrees()) {
 	    if (checkDegreeType(getDegreeType(), degree)) {
 		for (final Registration registration : degree.getRegistrationsSet()) {
-		    for (final RegistrationState registrationState : registration.getRegistrationStates()) {
-			final RegistrationStateType registrationStateType = registrationState.getStateType();
-			if (registrationStateType == RegistrationStateType.FLUNKED
-				&& registrationState.getExecutionYear() == getExecutionYear()) {
-			    final Row row = spreadsheet.addRow();
-			    row.setCell(registration.getNumber());
-			    setDegreeCells(row, degree);
+		    LinkedList<RegistrationState> states = new LinkedList<RegistrationState>();
+		    states.addAll(registration.getRegistrationStates());
+		    Collections.sort(states, RegistrationState.DATE_COMPARATOR);
+		    CollectionUtils.filter(states, new Predicate() {
+			@Override
+			public boolean evaluate(Object item) {
+			    return ((RegistrationState) item).getExecutionYear() != null
+				    && ((RegistrationState) item).getExecutionYear().isBeforeOrEquals(getExecutionYear());
 			}
+		    });
+		    if (!states.isEmpty() && states.getLast().getStateType().equals(RegistrationStateType.FLUNKED)) {
+			final Row row = spreadsheet.addRow();
+			row.setCell(registration.getNumber());
+			setDegreeCells(row, degree);
 		    }
 		}
 	    }
