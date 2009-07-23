69485cf4bf85fecd01cd3f9475a35ab6dcecfc31
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-07-23 18:54:31 +0000

    added localized name methods

--- src/net/sourceforge/fenixedu/domain/phd/candidacy/ApplicantOverallPromise.java
+++ src/net/sourceforge/fenixedu/domain/phd/candidacy/ApplicantOverallPromise.java
@@ -1,17 +1,33 @@
 package net.sourceforge.fenixedu.domain.phd.candidacy;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public enum ApplicantOverallPromise {
-    
+
     OUTSTANDING,
-    
-    EXCELLENT, 
-    
+
+    EXCELLENT,
+
     GOOD,
-    
+
     ABOVE_AVERAGE,
-    
+
     BELOW_AVERAGE,
-    
+
     UNABLE_TO_COMMENT;
 
+    public String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    public String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(getQualifiedName());
+    }
+
+    public String getQualifiedName() {
+	return getClass().getSimpleName() + "." + name();
+    }
 }
