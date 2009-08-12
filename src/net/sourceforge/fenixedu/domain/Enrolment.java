6d93793e1927bbcd408bafe77f8f2b5c8b9e8076
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2009-08-12 09:40:20 +0000

    getCreditsConcluded(ExecutionYear) now correctly filters out enrolments after the requested year

--- src/net/sourceforge/fenixedu/domain/Enrolment.java
+++ src/net/sourceforge/fenixedu/domain/Enrolment.java
@@ -33,7 +33,6 @@ import net.sourceforge.fenixedu.domain.student.curriculum.ICurriculumEntry;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumGroup;
 import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumLine;
 import net.sourceforge.fenixedu.domain.studentCurriculum.InternalEnrolmentWrapper;
-import net.sourceforge.fenixedu.domain.studentCurriculum.CurriculumModule.ConclusionValue;
 import net.sourceforge.fenixedu.domain.thesis.Thesis;
 import net.sourceforge.fenixedu.domain.util.FactoryExecutor;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
@@ -1240,7 +1239,7 @@ public class Enrolment extends Enrolment_Base implements IEnrolment {
 
     @Override
     final public Double getCreditsConcluded(ExecutionYear executionYear) {
-	return getAprovedEctsCredits();
+	return executionYear == null || getExecutionYear().isBeforeOrEquals(executionYear) ? getAprovedEctsCredits() : 0d;
     }
 
     @Override
