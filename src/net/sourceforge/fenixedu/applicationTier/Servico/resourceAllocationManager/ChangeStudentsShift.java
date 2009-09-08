3976efd2aede34340912f79973679709064ed164
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-09-08 10:46:27 +0000

    corrected bundles

--- src/net/sourceforge/fenixedu/applicationTier/Servico/resourceAllocationManager/ChangeStudentsShift.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/resourceAllocationManager/ChangeStudentsShift.java
@@ -1,12 +1,14 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.resourceAllocationManager;
 
+import java.text.MessageFormat;
 import java.util.HashSet;
+import java.util.ResourceBundle;
 import java.util.Set;
 
-import net.sourceforge.fenixedu.applicationTier.FenixService;
 import net.sourceforge.fenixedu.applicationTier.IUserView;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
 import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
 import net.sourceforge.fenixedu.domain.Shift;
 import net.sourceforge.fenixedu.domain.accessControl.FixedSetGroup;
 import net.sourceforge.fenixedu.domain.student.Registration;
@@ -14,19 +16,22 @@ import net.sourceforge.fenixedu.domain.util.email.ConcreteReplyTo;
 import net.sourceforge.fenixedu.domain.util.email.Message;
 import net.sourceforge.fenixedu.domain.util.email.Recipient;
 import net.sourceforge.fenixedu.domain.util.email.Sender;
-import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
 import pt.ist.fenixWebFramework.security.accessControl.Checked;
 import pt.ist.fenixWebFramework.services.Service;
 
-public class ChangeStudentsShift extends FenixService {
+public class ChangeStudentsShift {
 
-    @Checked("RolePredicates.RESOURCE_ALLOCATION_MANAGER_PREDICATE")
+    static private RootDomainObject getRootDomainObject() {
+	return RootDomainObject.getInstance();
+    }
+
     @Service
+    @Checked("RolePredicates.RESOURCE_ALLOCATION_MANAGER_PREDICATE")
     public static void run(IUserView userView, Integer oldShiftId, Integer newShiftId, final Set<Registration> registrations)
 	    throws FenixServiceException {
 
-	final Shift oldShift = rootDomainObject.readShiftByOID(oldShiftId);
-	final Shift newShift = rootDomainObject.readShiftByOID(newShiftId);
+	final Shift oldShift = getRootDomainObject().readShiftByOID(oldShiftId);
+	final Shift newShift = getRootDomainObject().readShiftByOID(newShiftId);
 
 	if (newShift != null) {
 	    if (oldShift == null || !oldShift.getTypes().containsAll(newShift.getTypes())
@@ -46,19 +51,22 @@ public class ChangeStudentsShift extends FenixService {
 	    recievers.add(registration.getPerson());
 	}
 
-	final String subject = RenderUtils.getResourceString("SOP_RESOURCES", "changeStudentsShift.email.subject");
-	final String groupName = RenderUtils.getResourceString("SOP_RESOURCES", "changeStudentsShift.email.groupName",
-		new Object[] { oldShift.getNome() });
-	final String messagePrefix = RenderUtils.getResourceString("SOP_RESOURCES", "changeStudentsShift.email.body",
-		new Object[] { oldShift.getNome() });
+	final ResourceBundle bundle = ResourceBundle.getBundle("resources.ResourceManagerResources");
 
-	final String messagePosfix = newShift == null ? RenderUtils.getResourceString("SOP_RESOURCES",
-		"changeStudentsShift.email.notNewShift") : RenderUtils.getResourceString("SOP_RESOURCES",
-		"changeStudentsShift.email.newShift", new Object[] { oldShift.getNome() });
+	final String subject = bundle.getString("changeStudentsShift.email.subject");
+
+	final String groupName = MessageFormat
+		.format(bundle.getString("changeStudentsShift.email.groupName"), oldShift.getNome());
+
+	final String messagePrefix = MessageFormat.format(bundle.getString("changeStudentsShift.email.body"), oldShift.getNome());
+
+	final String messagePosfix = newShift == null ? bundle.getString("changeStudentsShift.email.body.notNewShift") : MessageFormat
+		.format(bundle.getString("changeStudentsShift.email.body..newShift"), oldShift.getNome());
+
 	final String message = messagePrefix + messagePosfix;
 
 	Recipient recipient = new Recipient(groupName, new FixedSetGroup(recievers));
-	Sender sender = rootDomainObject.getSystemSender();
+	Sender sender = getRootDomainObject().getSystemSender();
 	new Message(sender, new ConcreteReplyTo("gop@ist.utl.pt").asCollection(), recipient.asCollection(), subject, message, "");
     }
 
