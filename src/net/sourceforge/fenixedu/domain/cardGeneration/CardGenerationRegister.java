81b3a5b8aae96814f5505692a16dc45608b8ad47
Luis Cruz <luis.cruz@ist.utl.pt>
2009-09-21 14:22:20 +0000

    New Structure for storing card emission information.

---
+++ src/net/sourceforge/fenixedu/domain/cardGeneration/CardGenerationRegister.java
@@ -0,0 +1,23 @@
+package net.sourceforge.fenixedu.domain.cardGeneration;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+
+import org.joda.time.LocalDate;
+
+public class CardGenerationRegister extends CardGenerationRegister_Base {
+    
+    public CardGenerationRegister() {
+        super();
+        setRootDomainObject(RootDomainObject.getInstance());
+    }
+
+    public CardGenerationRegister(final Person person, final String linePrefix, final LocalDate emission, final Boolean withAccountInformation) {
+	this();
+	setPerson(person);
+	setEmission(emission);
+	setWithAccountInformation(withAccountInformation);
+	setLinePrefix(linePrefix);
+    }
+    
+}
