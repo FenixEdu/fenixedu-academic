6cabd4062a169e149a79dff57fe27828ac2f2dd3
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2009-09-28 12:41:27 +0000

    Added notification document to phd candidacy

--- src/net/sourceforge/fenixedu/presentationTier/docs/phd/notification/PhdNotificationDocument.java
+++ src/net/sourceforge/fenixedu/presentationTier/docs/phd/notification/PhdNotificationDocument.java
@@ -1,12 +1,23 @@
 package net.sourceforge.fenixedu.presentationTier.docs.phd.notification;
 
 import net.sourceforge.fenixedu.domain.DomainReference;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.accounting.EventType;
+import net.sourceforge.fenixedu.domain.accounting.postingRules.FixedAmountPR;
+import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOffice;
+import net.sourceforge.fenixedu.domain.administrativeOffice.AdministrativeOfficeType;
+import net.sourceforge.fenixedu.domain.phd.PhdIndividualProgramProcess;
+import net.sourceforge.fenixedu.domain.phd.candidacy.PhdProgramCandidacyProcess;
 import net.sourceforge.fenixedu.domain.phd.notification.PhdNotification;
 import net.sourceforge.fenixedu.presentationTier.docs.FenixReport;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 public class PhdNotificationDocument extends FenixReport {
+
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
 
     /**
      * 
@@ -17,6 +28,7 @@ public class PhdNotificationDocument extends FenixReport {
 
     public PhdNotificationDocument(PhdNotification notification) {
 	setNotification(notification);
+	fillReport();
     }
 
     private PhdNotification getNotification() {
@@ -30,6 +42,44 @@ public class PhdNotificationDocument extends FenixReport {
     @Override
     protected void fillReport() {
 
+	final PhdProgramCandidacyProcess candidacyProcess = getNotification().getCandidacyProcess();
+	final Person person = candidacyProcess.getPerson();
+	final PhdIndividualProgramProcess individualProgramProcess = candidacyProcess.getIndividualProgramProcess();
+
+	addParameter("administrativeOfficeCoordinator", AdministrativeOffice.readByAdministrativeOfficeType(
+		AdministrativeOfficeType.MASTER_DEGREE).getUnit().getActiveUnitCoordinator().getFirstAndLastName());
+
+	addParameter("name", person.getName());
+	addParameter("address", person.getAddress());
+	addParameter("areaCode", person.getAreaCode());
+	addParameter("areaOfAreaCode", person.getAreaOfAreaCode());
+	addParameter("programName", individualProgramProcess.getPhdProgram().getName().getContent(getLanguage()));
+
+	addParameter("processNumber", individualProgramProcess.getProcessNumber());
+
+	final LocalDate whenRatified = candidacyProcess.getWhenRatified();
+
+	addParameter("ratificationDate", whenRatified != null ? whenRatified.toString(DATE_FORMAT) : "");
+
+	addParameter("insuranceFee", getInsuranceFee(individualProgramProcess));
+	addParameter("registrationFee", getRegistrationFee(individualProgramProcess, whenRatified));
+
+	addParameter("date", new LocalDate().toString(DATE_FORMAT));
+
+    }
+
+    private String getRegistrationFee(final PhdIndividualProgramProcess individualProgramProcess, final LocalDate whenRatified) {
+	return whenRatified != null ? ((FixedAmountPR) individualProgramProcess.getPhdProgram().getServiceAgreementTemplate()
+		.findPostingRuleByEventTypeAndDate(EventType.PHD_REGISTRATION_FEE, whenRatified.toDateTimeAtMidnight()))
+		.getFixedAmount().toPlainString() : "";
+    }
+
+    private String getInsuranceFee(final PhdIndividualProgramProcess individualProgramProcess) {
+	return ((FixedAmountPR) RootDomainObject.getInstance().getInstitutionUnit().getUnitServiceAgreementTemplate()
+		.findPostingRuleBy(EventType.INSURANCE,
+			individualProgramProcess.getExecutionYear().getBeginDateYearMonthDay().toDateTimeAtMidnight(),
+			individualProgramProcess.getExecutionYear().getEndDateYearMonthDay().toDateTimeAtMidnight()))
+		.getFixedAmount().toPlainString();
     }
 
     @Override
