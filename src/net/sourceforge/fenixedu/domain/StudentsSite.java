7d2d53ca74a785be9c282a0a52bc77178a39fb58
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-09-29 17:21:02 +0000

    Small refactor in the specific unit sites.

--- src/net/sourceforge/fenixedu/domain/StudentsSite.java
+++ src/net/sourceforge/fenixedu/domain/StudentsSite.java
@@ -1,27 +1,13 @@
 package net.sourceforge.fenixedu.domain;
 
-import net.sourceforge.fenixedu.domain.accessControl.FixedSetGroup;
-import net.sourceforge.fenixedu.domain.accessControl.GroupUnion;
-import net.sourceforge.fenixedu.injectionCode.IGroup;
-import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
+import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
 
 public class StudentsSite extends StudentsSite_Base {
 
-    private StudentsSite() {
+    private StudentsSite(Unit unit) {
 	super();
+	setUnit(unit);
     }
 
-    @Override
-    public IGroup getOwner() {
-	return new GroupUnion(new FixedSetGroup(getManagers()));
-    }
-
-    @Override
-    public void appendReversePathPart(final StringBuilder stringBuilder) {
-    }
-
-    @Override
-    public MultiLanguageString getName() {
-	return new MultiLanguageString("");
-    }
+   
 }
--- src/net/sourceforge/fenixedu/domain/TutorSite.java
+++ src/net/sourceforge/fenixedu/domain/TutorSite.java
@@ -1,31 +1,12 @@
 package net.sourceforge.fenixedu.domain;
 
-import net.sourceforge.fenixedu.domain.accessControl.FixedSetGroup;
-import net.sourceforge.fenixedu.domain.accessControl.GroupUnion;
 import net.sourceforge.fenixedu.domain.organizationalStructure.TutorUnit;
-import net.sourceforge.fenixedu.injectionCode.IGroup;
-import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class TutorSite extends TutorSite_Base {
 
     public TutorSite(TutorUnit unit) {
 	super();
-
 	setUnit(unit);
-    }
-
-    @Override
-    public IGroup getOwner() {
-	return new GroupUnion(new FixedSetGroup(getManagers()));
-    }
-
-    @Override
-    public void appendReversePathPart(final StringBuilder stringBuilder) {
-    }
-
-    @Override
-    public MultiLanguageString getName() {
-	return new MultiLanguageString("");
     }
 
 }
