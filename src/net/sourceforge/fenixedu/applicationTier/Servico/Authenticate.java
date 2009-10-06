a25fa5b35bc4604656d5977788d5ac559715ae07
Luis Cruz <luis.cruz@ist.utl.pt>
2009-10-06 11:57:10 +0000

    Deactive register login thread during high load period.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/Authenticate.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/Authenticate.java
@@ -289,8 +289,8 @@ public class Authenticate extends FenixService implements Serializable {
     }
 
     private void setLoginHostNameAndDateTime(final String remoteHost, Person person) {
-	final User user = person.getUser();
-	RegisterUserLoginThread.runThread(user, remoteHost);
+//	final User user = person.getUser();
+//	RegisterUserLoginThread.runThread(user, remoteHost);
     }
 
     public static CASReceipt getCASReceipt(final String serverName, final String casTicket, final String requestURL)
