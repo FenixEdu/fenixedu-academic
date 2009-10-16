93e0b3c5daab389ae5355830b06aa4bf7c7b4b99
Nadir Tarmahomed <nadir.amin@ist.utl.pt>
2009-10-16 12:56:25 +0000

    bug fix

---
+++ etc/database_operations/FixPhdDocumentsOjbConcreteClass.sql
@@ -0,0 +1,1 @@
+update FILE set OJB_CONCRETE_CLASS ='net.sourceforge.fenixedu.domain.phd.PhdProgramProcessDocument' where OJB_CONCRETE_CLASS='net.sourceforge.fenixedu.domain.phd.candidacy.PhdProgramCandidacyProcessDocument';
