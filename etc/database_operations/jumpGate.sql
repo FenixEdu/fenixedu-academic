025bb982d4aaf819ee87f20935dfc53acd26c21e
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-10-20 10:52:16 +0000

    Merged both alter tables into one to be more efficient.

--- etc/database_operations/jumpGate.sql
+++ etc/database_operations/jumpGate.sql
@@ -1,2 +1,1 @@
-alter table `CONTENT` add column `OID_JUMP_POINT` bigint(20);
-alter table `CONTENT` add column `OID_JUMP_TO` bigint(20);
+alter table `CONTENT` add column `OID_JUMP_POINT` bigint(20), add column `OID_JUMP_TO` bigint(20);
