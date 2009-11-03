5bc238674cacd6e9ad3abeefaf05c479982ec2b4
Shezad Anavarali <shezad@ist.utl.pt>
2009-11-03 17:46:35 +0000

    fix

--- src/net/sourceforge/fenixedu/presentationTier/Action/messaging/EmailsDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/messaging/EmailsDA.java
@@ -49,6 +49,11 @@ public class EmailsDA extends FenixDispatchAction {
     public ActionForward newEmail(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
 	    HttpServletResponse response) {
 	EmailBean emailBean = (EmailBean) getRenderedObject("emailBean");
+	
+	if (emailBean == null) {
+	    emailBean = (EmailBean) request.getAttribute("emailBean");
+	}
+	
 	if (emailBean == null) {
 	    emailBean = new EmailBean();
 	    final Set<Sender> availableSenders = Sender.getAvailableSenders();
