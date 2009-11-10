0ce77e458c2cd1cc583a3444c46da5e7f26895ac
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-11-10 19:11:10 +0000

    academic admin office phd thesis action

---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/phd/thesis/academicAdminOffice/PhdThesisProcessDA.java
@@ -0,0 +1,7 @@
+package net.sourceforge.fenixedu.presentationTier.Action.phd.thesis.academicAdminOffice;
+
+import net.sourceforge.fenixedu.presentationTier.Action.phd.thesis.CommonPhdThesisProcessDA;
+
+public class PhdThesisProcessDA extends CommonPhdThesisProcessDA {
+
+}
