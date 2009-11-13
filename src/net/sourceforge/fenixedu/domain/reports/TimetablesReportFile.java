72f7f909135a2423b5279caca791670b7062fc75
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2009-11-13 09:47:23 +0000

    [GEP] fix: teacher lessons shifts corrected and semester properly formatted

--- src/net/sourceforge/fenixedu/domain/reports/TimetablesReportFile.java
+++ src/net/sourceforge/fenixedu/domain/reports/TimetablesReportFile.java
@@ -3,35 +3,25 @@ package net.sourceforge.fenixedu.domain.reports;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import net.sourceforge.fenixedu.domain.CourseLoad;
 import net.sourceforge.fenixedu.domain.Degree;
-import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
-import net.sourceforge.fenixedu.domain.Employee;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
 import net.sourceforge.fenixedu.domain.ExecutionSemester;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Lesson;
-import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.Professorship;
-import net.sourceforge.fenixedu.domain.RootDomainObject;
-import net.sourceforge.fenixedu.domain.Shift;
 import net.sourceforge.fenixedu.domain.ShiftType;
 import net.sourceforge.fenixedu.domain.Teacher;
 import net.sourceforge.fenixedu.domain.degree.DegreeType;
-import net.sourceforge.fenixedu.domain.personnelSection.contracts.EmployeeContractSituation;
 import net.sourceforge.fenixedu.domain.teacher.DegreeTeachingService;
-import org.apache.velocity.runtime.parser.node.GetExecutor;
-import org.joda.time.YearMonthDay;
 import pt.utl.ist.fenix.tools.util.excel.Spreadsheet;
 import pt.utl.ist.fenix.tools.util.excel.Spreadsheet.Row;
 
 public class TimetablesReportFile extends TimetablesReportFile_Base {
 
     public TimetablesReportFile() {
-        super();
+	super();
     }
 
     @Override
@@ -68,92 +58,95 @@ public class TimetablesReportFile extends TimetablesReportFile_Base {
 	spreadsheet.setHeader("Nr Alunos Inscritos");
     }
 
-    private void timetables(Spreadsheet spreadsheet, final ExecutionYear executionYear, final DegreeType degreeType) throws IOException {
+    private void timetables(Spreadsheet spreadsheet, final ExecutionYear executionYear, final DegreeType degreeType)
+	    throws IOException {
 	generateNameAndHeaders(spreadsheet, executionYear, degreeType);
 
 	Map<ExecutionCourse, Boolean> areLessonsWithoutTeacherConsidered = new HashMap<ExecutionCourse, Boolean>();
-	for(ExecutionCourse executionCourse : getRootDomainObject().getExecutionCourses()) {
+	for (ExecutionCourse executionCourse : getRootDomainObject().getExecutionCourses()) {
 	    areLessonsWithoutTeacherConsidered.put(executionCourse, false);
 	}
 	for (Teacher teacher : getRootDomainObject().getTeachers()) {
 	    for (final ExecutionSemester semester : executionYear.getExecutionPeriods()) {
 		ExecutionCourse executionCourse = null;
-		for(Professorship professorship : teacher.getDegreeProfessorshipsByExecutionPeriod(semester)) {
+		for (Professorship professorship : teacher.getDegreeProfessorshipsByExecutionPeriod(semester)) {
 		    executionCourse = professorship.getExecutionCourse();
 		    boolean isSameDegreeType = false;
-		    for(Degree degree : professorship.getExecutionCourse().getDegreesSortedByDegreeName()) {
-			if(degree.getDegreeType().compareTo(degreeType) == 0) {
+		    for (Degree degree : professorship.getExecutionCourse().getDegreesSortedByDegreeName()) {
+			if (degree.getDegreeType().compareTo(degreeType) == 0) {
 			    isSameDegreeType = true;
 			}
 		    }
-		    if(!isSameDegreeType) {
+		    if (!isSameDegreeType) {
 			continue;
 		    }
-		    for(DegreeTeachingService service : professorship.getDegreeTeachingServicesOrderedByShift()) {
-			for(Lesson lesson : service.getShift().getAssociatedLessons()) {
-			    for(ShiftType shiftType : service.getShift().getTypes()) {
+		    for (DegreeTeachingService service : professorship.getDegreeTeachingServicesOrderedByShift()) {
+			for (Lesson lesson : service.getShift().getAssociatedLessons()) {
+			    for (ShiftType shiftType : service.getShift().getTypes()) {
 				final Row row = spreadsheet.addRow();
 
-			    	// Nr Docente
-			    	row.setCell(teacher.getTeacherNumber());
+				// Nr Docente
+				row.setCell(teacher.getTeacherNumber());
 
-			    	// Nome Docente
-			    	row.setCell(teacher.getPerson().getName());
+				// Nome Docente
+				row.setCell(teacher.getPerson().getName());
 
-			    	// Ano Lectivo
-			    	row.setCell(executionYear.getQualifiedName());
+				// Ano Lectivo
+				row.setCell(executionYear.getQualifiedName());
 
-			    	// Semestre
-			    	row.setCell(semester.getQualifiedName());
+				// Semestre
+				row.setCell(semester.getSemester());
 
-			    	// ID Execution Course
-			    	row.setCell(executionCourse.getIdInternal());
+				// ID Execution Course
+				row.setCell(executionCourse.getIdInternal());
 
-			    	// OID Execution Course
-			    	row.setCell(String.valueOf(executionCourse.getOid()));
+				// OID Execution Course
+				row.setCell(String.valueOf(executionCourse.getOid()));
 
-			    	// ID Turno
-			    	row.setCell(service.getShift().getIdInternal());
+				// ID Turno
+				row.setCell(service.getShift().getIdInternal());
 
-			    	// Nome Turno
-			    	row.setCell(service.getShift().getNome());
+				// Nome Turno
+				row.setCell(service.getShift().getNome());
 
-			    	// Tipo Aula
-			    	row.setCell(shiftType.getFullNameTipoAula());
+				// Tipo Aula
+				row.setCell(shiftType.getFullNameTipoAula());
 
-			    	// Dia Semana
-			    	row.setCell(lesson.getDiaSemana().getDiaSemanaString());
+				// Dia Semana
+				row.setCell(lesson.getDiaSemana().getDiaSemanaString());
 
-			    	// Hora Início
-			    	row.setCell(lesson.getBeginHourMinuteSecond().toString());
+				// Hora Início
+				row.setCell(lesson.getBeginHourMinuteSecond().toString());
 
-			    	// Hora Fim
-			    	row.setCell(lesson.getEndHourMinuteSecond().toString());
+				// Hora Fim
+				row.setCell(lesson.getEndHourMinuteSecond().toString());
 
-			    	// Sala
-			    	if(lesson.getSala() != null) {
-			    	    row.setCell(lesson.getSala().getNome());
-			    	}
+				// Sala
+				if (lesson.getSala() != null) {
+				    row.setCell(lesson.getSala().getNome());
+				} else {
+				    row.setCell("");
+				}
 
-			    	// Percentagem Assegurada pelo Docente
-			    	row.setCell(((Math.round(((Double)service.getPercentage()).doubleValue() * 100.0)) / 100.0));
+				// Percentagem Assegurada pelo Docente
+				row.setCell(((Math.round((service.getPercentage()).doubleValue() * 100.0)) / 100.0));
 
-			    	// Nº Alunos Inscritos
-			    	row.setCell(service.getShift().getShiftEnrolmentsOrderedByDate().size());
+				// Nº Alunos Inscritos
+				row.setCell(service.getShift().getShiftEnrolmentsOrderedByDate().size());
 			    }
 			}
 		    }
 		}
-		if((executionCourse != null) && (areLessonsWithoutTeacherConsidered.get(executionCourse) == false)) {
-		    for(Lesson lesson : getRootDomainObject().getLessons()) {
-			if(lesson.getExecutionCourse() != executionCourse) {
+		if ((executionCourse != null) && (areLessonsWithoutTeacherConsidered.get(executionCourse) == false)) {
+		    for (Lesson lesson : getRootDomainObject().getLessons()) {
+			if (lesson.getExecutionCourse() != executionCourse) {
 			    continue;
 			}
-			if(lesson.getShift().getDegreeTeachingServices().isEmpty()) {
-			    for(ShiftType shiftType : lesson.getShift().getTypes()) {
+			if (lesson.getShift().getDegreeTeachingServices().isEmpty()) {
+			    for (ShiftType shiftType : lesson.getShift().getTypes()) {
 				// Licoes sem professor
 				final Row row = spreadsheet.addRow();
-			    
+
 				// Nr Docente
 				row.setCell("");
 
@@ -164,7 +157,7 @@ public class TimetablesReportFile extends TimetablesReportFile_Base {
 				row.setCell(executionYear.getQualifiedName());
 
 				// Semestre
-				row.setCell(semester.getQualifiedName());
+				row.setCell(semester.getSemester());
 
 				// ID Execution Course
 				row.setCell(executionCourse.getIdInternal());
@@ -191,8 +184,10 @@ public class TimetablesReportFile extends TimetablesReportFile_Base {
 				row.setCell(lesson.getEndHourMinuteSecond().toString());
 
 				// Sala
-				if(lesson.getSala() != null) {
+				if (lesson.getSala() != null) {
 				    row.setCell(lesson.getSala().getNome());
+				} else {
+				    row.setCell("");
 				}
 
 				// Percentagem Assegurada pelo Docente
@@ -204,13 +199,14 @@ public class TimetablesReportFile extends TimetablesReportFile_Base {
 				areLessonsWithoutTeacherConsidered.put(executionCourse, true);
 			    }
 			}
-			if(areLessonsWithoutTeacherConsidered.get(executionCourse) == true) {
+			if (areLessonsWithoutTeacherConsidered.get(executionCourse) == true) {
 			    break;
 			}
 		    }
 		}
 	    }
 	}
-	spreadsheet.exportToXLSSheet(new File("Horarios " + executionYear.getQualifiedName().replace("/", "") + " " + degreeType.getName() + ".xls"));
+	spreadsheet.exportToXLSSheet(new File("Horarios " + executionYear.getQualifiedName().replace("/", "") + " "
+		+ degreeType.getName() + ".xls"));
     }
 }
