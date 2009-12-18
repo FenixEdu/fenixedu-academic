60771b0822dc4f1e0e5c7f04977e605f0a91d451
João Figueiredo <joao.figueiredo@ist.utl.pt>
2009-12-18 11:37:19 +0000

    added localized name methods to phd individual program process state

--- src/net/sourceforge/fenixedu/domain/phd/PhdIndividualProgramProcessState.java
+++ src/net/sourceforge/fenixedu/domain/phd/PhdIndividualProgramProcessState.java
@@ -1,5 +1,10 @@
 package net.sourceforge.fenixedu.domain.phd;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public enum PhdIndividualProgramProcessState {
 
     CANDIDACY(true),
@@ -32,4 +37,15 @@ public enum PhdIndividualProgramProcessState {
 	return activeState;
     }
 
+    public String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    public String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(getQualifiedName());
+    }
+
+    public String getQualifiedName() {
+	return getClass().getSimpleName() + "." + name();
+    }
 }
