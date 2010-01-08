9506562bdd63c812af7e1c89533293d60d42de5c
Luis Cruz <luis.cruz@ist.utl.pt>
2010-01-08 15:20:03 +0000

    Place CC signature in correct place.

--- src/net/sourceforge/fenixedu/presentationTier/docs/thesis/ApproveJuryDocument.java
+++ src/net/sourceforge/fenixedu/presentationTier/docs/thesis/ApproveJuryDocument.java
@@ -2,7 +2,10 @@ package net.sourceforge.fenixedu.presentationTier.docs.thesis;
 
 import java.util.Locale;
 
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.domain.thesis.Thesis;
+import net.sourceforge.fenixedu.domain.thesis.ThesisEvaluationParticipant;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -20,10 +23,30 @@ public class ApproveJuryDocument extends ThesisDocument {
 
 	Thesis thesis = getThesis();
 
-	addParameter("author", thesis.getProposalApprover() != null ? thesis.getProposalApprover().getPersonName()
-		: StringUtils.EMPTY);
-	addParameter("date", thesis.getApproval() != null ? String.format(new Locale("pt"), "%1$td de %1$tB de %1$tY", thesis
-		.getApproval().toDate()) : StringUtils.EMPTY);
+	final ThesisEvaluationParticipant thesisEvaluationParticipant = thesis.getProposalApprover();
+	final String author;
+	final String date;
+	final String ccAuthor;
+	final String ccDate;
+	if (thesisEvaluationParticipant == null) {
+	    author = date = ccAuthor = ccDate = StringUtils.EMPTY;
+	} else {
+	    final Person person = thesisEvaluationParticipant.getPerson();
+	    if (person != null && person.hasRole(RoleType.SCIENTIFIC_COUNCIL)) {
+		author = date = StringUtils.EMPTY;
+		ccAuthor = thesisEvaluationParticipant.getPersonName();
+		ccDate = String.format(new Locale("pt"), "%1$td de %1$tB de %1$tY", thesis.getApproval().toDate());
+	    } else {
+		ccAuthor = ccDate = StringUtils.EMPTY;
+		author = thesisEvaluationParticipant.getPersonName();
+		date = String.format(new Locale("pt"), "%1$td de %1$tB de %1$tY", thesis.getApproval().toDate());
+	    }
+	}
+
+	addParameter("author", author);
+	addParameter("date", date);
+	addParameter("ccAuthor", ccAuthor);
+	addParameter("ccDate", ccDate);
     }
 
     @Override
