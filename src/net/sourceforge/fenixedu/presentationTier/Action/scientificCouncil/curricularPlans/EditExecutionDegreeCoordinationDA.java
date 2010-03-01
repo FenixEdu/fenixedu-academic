8ea3a6855e876a7361c4c9e30fc7d0ef819ae07e
Diogo Simões <diogo.fonseca.simoes@ist.utl.pt>
2010-03-01 11:06:58 +0000

    CoordinationTeams Management: Besides the already available entry point for this task through the Degrees listing, added an entry point centered on the ExecutionYear. For a chosen ExecutionYear it lists all degrees, showing whether their respective coordination teams have been created and responsible(s) nominated or not. (Action and Bean)

--- src/net/sourceforge/fenixedu/presentationTier/Action/scientificCouncil/curricularPlans/EditExecutionDegreeCoordinationDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/scientificCouncil/curricularPlans/EditExecutionDegreeCoordinationDA.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.presentationTier.Action.scientificCouncil.curricularPlans;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import net.sourceforge.fenixedu.domain.Coordinator;
 import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.ExecutionDegree;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.degree.DegreeType;
 import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
 
 import org.apache.struts.action.ActionForm;
@@ -18,10 +22,12 @@ import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
 import pt.ist.fenixWebFramework.struts.annotations.Forward;
 import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+import pt.ist.fenixframework.pstm.AbstractDomainObject;
 
 @Mapping(path = "/curricularPlans/editExecutionDegreeCoordination", module = "scientificCouncil")
 @Forwards( { @Forward(name = "presentCoordination", path = "/scientificCouncil/curricularPlans/presentCoordination.jsp"),
-	@Forward(name = "editCoordination", path = "/scientificCouncil/curricularPlans/editCoordination.jsp") })
+	@Forward(name = "editCoordination", path = "/scientificCouncil/curricularPlans/editCoordination.jsp"),
+	@Forward(name = "selectYearAndDegree", path = "/scientificCouncil/curricularPlans/selectYearAndDegree.jsp") })
 public class EditExecutionDegreeCoordinationDA extends FenixDispatchAction {
 
     public ActionForward prepareEditCoordination(ActionMapping mapping, ActionForm form, HttpServletRequest request,
@@ -44,8 +50,18 @@ public class EditExecutionDegreeCoordinationDA extends FenixDispatchAction {
 
 	final Integer executionDegreeId = Integer.valueOf(request.getParameter("executionDegreeId"));
 	ExecutionDegree executionDegree = rootDomainObject.readExecutionDegreeByOID(executionDegreeId);
+	
+	String backTo = String.valueOf(request.getParameter("from"));
+	String backPath;
+	if(backTo.equals("byYears")){
+	    backPath = "/curricularPlans/editExecutionDegreeCoordination.do?method=editByYears&executionYearId=" +  executionDegree.getExecutionYear().getExternalId().toString();
+	} else {
+	    backPath = "/curricularPlans/editExecutionDegreeCoordination.do?method=prepareEditCoordination&degreeCurricularPlanId=" +  executionDegree.getDegreeCurricularPlan().getIdInternal().toString() ;
+	}
 
 	ExecutionDegreeCoordinatorsBean coordsBean = new ExecutionDegreeCoordinatorsBean(executionDegree);
+	
+	coordsBean.setBackPath(backPath);
 
 	request.setAttribute("coordsBean", coordsBean);
 	RenderUtils.invalidateViewState("coordsBean");
@@ -116,5 +132,46 @@ public class EditExecutionDegreeCoordinationDA extends FenixDispatchAction {
 	request.setAttribute("startVisible", true);
 
 	return mapping.findForward("editCoordination");
+    }
+    
+    public ActionForward editByYears(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+	
+	ExecutionDegreeCoordinatorsBean sessionBean = (ExecutionDegreeCoordinatorsBean) getRenderedObject("sessionBean");
+	if(sessionBean == null) {
+	    sessionBean = new ExecutionDegreeCoordinatorsBean();
+	    final String executionYearId = String.valueOf(request.getParameter("executionYearId"));
+	    if(!executionYearId.equals("null")) {
+		ExecutionYear executionYear = AbstractDomainObject.fromExternalId(executionYearId);
+		sessionBean.setExecutionYear(executionYear);
+	    } else {
+		request.setAttribute("sessionBean", sessionBean);
+		RenderUtils.invalidateViewState("sessionBean");
+		
+		return mapping.findForward("selectYearAndDegree");
+	    }
+	}
+	
+	List<ExecutionDegree> bachelors = sessionBean.getExecutionYear().getExecutionDegreesFor(DegreeType.BOLONHA_DEGREE);
+	request.setAttribute("bachelors", bachelors);
+	    
+	List<ExecutionDegree> masters = sessionBean.getExecutionYear().getExecutionDegreesFor(DegreeType.BOLONHA_MASTER_DEGREE);
+	request.setAttribute("masters", masters);
+	    
+	List<ExecutionDegree> integratedMasters = sessionBean.getExecutionYear().getExecutionDegreesFor(DegreeType.BOLONHA_INTEGRATED_MASTER_DEGREE);
+	request.setAttribute("integratedMasters", integratedMasters);
+	    
+	List<ExecutionDegree> otherDegrees = new ArrayList<ExecutionDegree>(sessionBean.getExecutionYear().getExecutionDegrees());
+	otherDegrees.removeAll(bachelors);
+	otherDegrees.removeAll(masters);
+	otherDegrees.removeAll(integratedMasters);
+	request.setAttribute("otherDegrees", otherDegrees);
+	    
+	boolean hasYearSelected = true;
+	request.setAttribute("hasYearSelected", hasYearSelected);
+
+	request.setAttribute("sessionBean", sessionBean);
+	RenderUtils.invalidateViewState("sessionBean");
+	
+	return mapping.findForward("selectYearAndDegree");
     }
 }
--- src/net/sourceforge/fenixedu/presentationTier/Action/scientificCouncil/curricularPlans/ExecutionDegreeCoordinatorsBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/scientificCouncil/curricularPlans/ExecutionDegreeCoordinatorsBean.java
@@ -5,13 +5,24 @@ import java.util.Set;
 
 import net.sourceforge.fenixedu.domain.Coordinator;
 import net.sourceforge.fenixedu.domain.ExecutionDegree;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 
 public class ExecutionDegreeCoordinatorsBean implements Serializable {
 
+    private ExecutionYear executionYear;
     private ExecutionDegree executionDegree;
     private Person newCoordinator;
+    private String backPath;
 
+    public ExecutionYear getExecutionYear() {
+	return executionYear;
+    }
+    
+    public void setExecutionYear(ExecutionYear executionYear) {
+	this.executionYear = executionYear;
+    }
+    
     public ExecutionDegree getExecutionDegree() {
 	return executionDegree;
     }
@@ -34,6 +45,14 @@ public class ExecutionDegreeCoordinatorsBean implements Serializable {
 
     public ExecutionDegreeCoordinatorsBean(ExecutionDegree executionDegree) {
 	this.executionDegree = executionDegree;
+    }
+    
+    public String getBackPath() {
+	return backPath;
+    }
+    
+    public void setBackPath(String backPath) {
+	this.backPath = backPath;
     }
 
     public ExecutionDegreeCoordinatorsBean() {
