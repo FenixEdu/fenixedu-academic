1d22acbf53d8292b6d83d3bfceb4bb3bacacc227
Jaime Jorge <jaime.fjorge@gmail.com>
2010-03-09 18:27:08 +0000

    Adding date interval filter to Photograph listing of managers

--- src/net/sourceforge/fenixedu/dataTransferObject/photographs/PhotographFilterBean.java
+++ src/net/sourceforge/fenixedu/dataTransferObject/photographs/PhotographFilterBean.java
@@ -8,6 +8,8 @@ import net.sourceforge.fenixedu.domain.PhotoType;
 import net.sourceforge.fenixedu.domain.Photograph;
 import net.sourceforge.fenixedu.domain.person.RoleType;
 
+import org.joda.time.LocalDate;
+
 /**
  * @author Pedro Santos (pmrsa)
  */
@@ -17,10 +19,31 @@ public class PhotographFilterBean implements Serializable {
     private PhotoState state;
 
     private PhotoType type;
-
+    
+    private LocalDate startDate;
+    
+    private LocalDate endDate;
+    
     private RoleType personType;
 
     private String name;
+
+    
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 
     public PhotoState getState() {
 	return state;
@@ -53,11 +76,29 @@ public class PhotographFilterBean implements Serializable {
     public void setName(String name) {
 	this.name = name;
     }
-
+    /**
+     * @param photograph
+     * @return boolean representing failure to accept the photo
+     */
+    public boolean processDates(Photograph photograph){
+	/*
+	 * If there is a "start date"
+	 * If photograph is after the defined "start date"
+	 * If there is an "end date", "start date" must be before "end date"
+	 */
+	if (getStartDate() != null && getStartDate().isAfter(photograph.getStateChange().toDateMidnight().toLocalDate()) && (getEndDate() == null ? false : getStartDate().isAfter(getEndDate())))
+	    return true;
+	if (getEndDate() != null && getEndDate().isBefore(photograph.getStateChange().toDateMidnight().toLocalDate()) && (getStartDate() == null ? false : getEndDate().isBefore(getStartDate())))
+	    return true;
+	return false;
+    }
+    
     public boolean accepts(Photograph photograph) {
 	if (getState() != null && photograph.getState() != getState())
 	    return false;
 	if (getType() != null && photograph.getPhotoType() != getType())
+	    return false;
+	if (processDates(photograph))
 	    return false;
 	Person person = photograph.getPerson();
 	if (person != null) {
