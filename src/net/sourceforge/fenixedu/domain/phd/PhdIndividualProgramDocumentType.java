8ad65873efffdd8a008e58d8de10932dee54c4bf
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-03-16 17:08:51 +0000

    format

--- src/net/sourceforge/fenixedu/domain/phd/PhdIndividualProgramDocumentType.java
+++ src/net/sourceforge/fenixedu/domain/phd/PhdIndividualProgramDocumentType.java
@@ -54,9 +54,9 @@ public enum PhdIndividualProgramDocumentType {
     JURY_ELEMENTS,
 
     JURY_PRESIDENT_ELEMENT,
-    
+
     JURY_REPORT_FEEDBACK,
-    
+
     CANDIDACY_FEEDBACK_DOCUMENT,
 
     OTHER(false);
