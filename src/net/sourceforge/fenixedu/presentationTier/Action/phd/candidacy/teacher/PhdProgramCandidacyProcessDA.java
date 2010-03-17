1dea18f668a2562216fbe0461d568953e83612c7
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-03-17 20:47:59 +0000

    set feedback element in request if exists

--- src/net/sourceforge/fenixedu/presentationTier/Action/phd/candidacy/teacher/PhdProgramCandidacyProcessDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/phd/candidacy/teacher/PhdProgramCandidacyProcessDA.java
@@ -1,6 +1,16 @@
 package net.sourceforge.fenixedu.presentationTier.Action.phd.candidacy.teacher;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.fenixedu.domain.phd.candidacy.PhdProgramCandidacyProcess;
+import net.sourceforge.fenixedu.domain.phd.candidacy.feedbackRequest.PhdCandidacyFeedbackRequestElement;
 import net.sourceforge.fenixedu.presentationTier.Action.phd.candidacy.CommonPhdCandidacyDA;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
 import pt.ist.fenixWebFramework.struts.annotations.Forward;
 import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
@@ -14,4 +24,20 @@ import pt.ist.fenixWebFramework.struts.annotations.Tile;
 })
 public class PhdProgramCandidacyProcessDA extends CommonPhdCandidacyDA {
 
+    @Override
+    public ActionForward manageCandidacyDocuments(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+	    HttpServletResponse response) {
+
+	final PhdProgramCandidacyProcess process = getProcess(request);
+
+	if (process.hasFeedbackRequest()) {
+
+	    final PhdCandidacyFeedbackRequestElement element = process.getFeedbackRequest().getElement(getLoggedPerson(request));
+	    if (element != null) {
+		request.setAttribute("element", element);
+	    }
+	}
+
+	return super.manageCandidacyDocuments(mapping, form, request, response);
+    }
 }
