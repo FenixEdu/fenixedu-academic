bfb94ec6965951c7f1d7032ec323f850d8e8f37c
Diogo Simões <diogo.fonseca.simoes@ist.utl.pt>
2010-03-19 17:51:32 +0000

    External Supervisor: Fixed the XLS generator (inactive regs where being listed)

--- src/net/sourceforge/fenixedu/presentationTier/Action/externalSupervision/consult/ExternalSupervisorViewsBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/externalSupervision/consult/ExternalSupervisorViewsBean.java
@@ -196,7 +196,7 @@ public class ExternalSupervisorViewsBean implements Serializable, HasExecutionYe
 	List<StudentCurricularPlan> curricularPlans = new ArrayList<StudentCurricularPlan>();
 	for(Person person : students){
 	    for(Registration registration : person.getStudent().getRegistrations()){
-		if(registration.getRegistrationAgreement() == protocol.getRegistrationAgreement()){
+		if(registration.getRegistrationAgreement() == protocol.getRegistrationAgreement() && registration.isActive()){
 		    if(executionDegree != null){
 			if(registration.getDegree() != executionDegree.getDegree()){
 			    continue;
