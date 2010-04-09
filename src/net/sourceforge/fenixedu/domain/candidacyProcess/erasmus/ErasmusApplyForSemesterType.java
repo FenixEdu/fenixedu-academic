670a7abc8fdfb47f5be5a72457e321e36b81edee
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2010-04-09 16:20:42 +0000

    Removed final project option

--- src/net/sourceforge/fenixedu/domain/candidacyProcess/erasmus/ErasmusApplyForSemesterType.java
+++ src/net/sourceforge/fenixedu/domain/candidacyProcess/erasmus/ErasmusApplyForSemesterType.java
@@ -7,7 +7,7 @@ import pt.utl.ist.fenix.tools.util.StringAppender;
 import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public enum ErasmusApplyForSemesterType {
-    FIRST_SEMESTER, SECOND_SEMESTER, FINAL_PROJECT;
+    FIRST_SEMESTER, SECOND_SEMESTER;
 
     protected String localizedName(Locale locale) {
 	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(fullQualifiedName());
