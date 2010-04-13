bd77ee65ee91f33464b6fb6dc170fb3663262bc7
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-04-13 15:47:48 +0000

    Bug fix in phd normal enrolments

--- src/net/sourceforge/fenixedu/domain/phd/enrolments/PhdStudentCurricularPlanEnrolmentManager.java
+++ src/net/sourceforge/fenixedu/domain/phd/enrolments/PhdStudentCurricularPlanEnrolmentManager.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import net.sourceforge.fenixedu.domain.CurricularCourse;
 import net.sourceforge.fenixedu.domain.curricularRules.ICurricularRule;
 import net.sourceforge.fenixedu.domain.enrolment.EnrolmentContext;
+import net.sourceforge.fenixedu.domain.phd.PhdIndividualProgramProcess;
 import net.sourceforge.fenixedu.domain.studentCurriculum.StudentCurricularPlanEnrolmentManager;
 
 public class PhdStudentCurricularPlanEnrolmentManager extends StudentCurricularPlanEnrolmentManager {
@@ -16,7 +17,22 @@ public class PhdStudentCurricularPlanEnrolmentManager extends StudentCurricularP
     @Override
     protected void addRuntimeRules(final Set<ICurricularRule> curricularRules, final CurricularCourse curricularCourse) {
 	super.addRuntimeRules(curricularRules, curricularCourse);
-	curricularRules.add(new PhdValidCurricularCoursesRule(curricularCourse));
+
+	if (mustValidateCurricularCourses()) {
+	    curricularRules.add(new PhdValidCurricularCoursesRule(curricularCourse));
+	}
+    }
+
+    private boolean mustValidateCurricularCourses() {
+
+	if (!getRegistration().hasPhdIndividualProgramProcess()) {
+	    return false;
+	}
+
+	final PhdIndividualProgramProcess process = getRegistration().getPhdIndividualProgramProcess();
+	return process.hasStudyPlan() && !process.getStudyPlan().isExempted()
+		&& process.getStudyPlan().isToEnrolInCurricularCourses();
+
     }
 
 }
