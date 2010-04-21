371242c423254330358e2377f1f1028786af0304
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-04-21 14:46:20 +0000

    added localized name to curricular state enum

--- src/net/sourceforge/fenixedu/domain/degreeStructure/CurricularStage.java
+++ src/net/sourceforge/fenixedu/domain/degreeStructure/CurricularStage.java
@@ -1,5 +1,10 @@
 package net.sourceforge.fenixedu.domain.degreeStructure;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import pt.utl.ist.fenix.tools.util.i18n.Language;
+
 public enum CurricularStage {
 
     OLD,
@@ -12,6 +17,14 @@ public enum CurricularStage {
 
     public String getName() {
 	return name();
+    }
+
+    public String getLocalizedName() {
+	return getLocalizedName(Language.getLocale());
+    }
+
+    public String getLocalizedName(final Locale locale) {
+	return ResourceBundle.getBundle("resources.EnumerationResources", locale).getString(name());
     }
 
 }
