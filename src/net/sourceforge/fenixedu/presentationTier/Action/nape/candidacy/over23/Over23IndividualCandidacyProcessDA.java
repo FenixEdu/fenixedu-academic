066712f38f1661dc3863c64a81cc7d0d0422c048
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2010-05-03 09:30:44 +0000

    nape candidacies functionality

--- src/net/sourceforge/fenixedu/presentationTier/Action/nape/candidacy/over23/Over23IndividualCandidacyProcessDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/nape/candidacy/over23/Over23IndividualCandidacyProcessDA.java
@@ -30,9 +30,9 @@ import pt.ist.fenixWebFramework.struts.annotations.Forward;
 import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
 
-@Mapping(path = "/caseHandlingOver23IndividualCandidacyProcess", module = "academicAdminOffice", formBeanClass = Over23IndividualCandidacyProcessDA.CandidacyForm.class)
-@Forwards( { @Forward(name = "intro", path = "/candidacy/mainCandidacyProcess.jsp"),
-	@Forward(name = "list-allowed-activities", path = "/candidacy/over23/listIndividualCandidacyActivities.jsp")
+@Mapping(path = "/caseHandlingOver23IndividualCandidacyProcess", module = "nape", formBeanClass = Over23IndividualCandidacyProcessDA.CandidacyForm.class)
+@Forwards( { @Forward(name = "intro", path = "/nape/candidacy/mainCandidacyProcess.jsp"),
+	@Forward(name = "list-allowed-activities", path = "/nape/candidacy/over23/listIndividualCandidacyActivities.jsp")
 })
 public class Over23IndividualCandidacyProcessDA extends IndividualCandidacyProcessDA {
 
