0c23c2b2b5a214b4b258f061b532df8ba5802993
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-05-04 09:29:56 +0000

    compile fix: title changed to multilanguage string

--- src/net/sourceforge/fenixedu/domain/teacher/evaluation/FacultyEvaluationProcess.java
+++ src/net/sourceforge/fenixedu/domain/teacher/evaluation/FacultyEvaluationProcess.java
@@ -4,6 +4,8 @@ import net.sourceforge.fenixedu.domain.RootDomainObject;
 
 import org.joda.time.Interval;
 
+import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
+
 public class FacultyEvaluationProcess extends FacultyEvaluationProcess_Base {
     
     public FacultyEvaluationProcess() {
@@ -11,7 +13,7 @@ public class FacultyEvaluationProcess extends FacultyEvaluationProcess_Base {
         setRootDomainObject(RootDomainObject.getInstance());
     }
 
-    public FacultyEvaluationProcess(final String title, final Interval autoEvaluationInterval, final Interval evaluationInterval) {
+    public FacultyEvaluationProcess(final MultiLanguageString title, final Interval autoEvaluationInterval, final Interval evaluationInterval) {
 	this();
 	setTitle(title);
 	setAutoEvaluationInterval(autoEvaluationInterval);
--- src/net/sourceforge/fenixedu/domain/teacher/evaluation/FacultyEvaluationProcessBean.java
+++ src/net/sourceforge/fenixedu/domain/teacher/evaluation/FacultyEvaluationProcessBean.java
@@ -6,10 +6,13 @@ import org.joda.time.DateTime;
 import org.joda.time.Interval;
 
 import pt.ist.fenixWebFramework.services.Service;
+import pt.utl.ist.fenix.tools.util.i18n.MultiLanguageString;
 
 public class FacultyEvaluationProcessBean implements Serializable {
 
-    private String title;
+    static private final long serialVersionUID = 1L;
+
+    private MultiLanguageString title;
     private DateTime autoEvaluationIntervalStart;
     private DateTime autoEvaluationIntervalEnd;
     private DateTime evaluationIntervalStart;
@@ -30,54 +33,68 @@ public class FacultyEvaluationProcessBean implements Serializable {
 	setFacultyEvaluationProcess(facultyEvaluationProcess);
     }
 
-    public String getTitle() {
-        return title;
+    public MultiLanguageString getTitle() {
+	return title;
     }
-    public void setTitle(String title) {
-        this.title = title;
+
+    public void setTitle(MultiLanguageString title) {
+	this.title = title;
     }
+
     public DateTime getAutoEvaluationIntervalStart() {
-        return autoEvaluationIntervalStart;
+	return autoEvaluationIntervalStart;
     }
+
     public void setAutoEvaluationIntervalStart(DateTime autoEvaluationIntervalStart) {
-        this.autoEvaluationIntervalStart = autoEvaluationIntervalStart;
+	this.autoEvaluationIntervalStart = autoEvaluationIntervalStart;
     }
+
     public DateTime getAutoEvaluationIntervalEnd() {
-        return autoEvaluationIntervalEnd;
+	return autoEvaluationIntervalEnd;
     }
+
     public void setAutoEvaluationIntervalEnd(DateTime autoEvaluationIntervalEnd) {
-        this.autoEvaluationIntervalEnd = autoEvaluationIntervalEnd;
+	this.autoEvaluationIntervalEnd = autoEvaluationIntervalEnd;
     }
+
     public DateTime getEvaluationIntervalStart() {
-        return evaluationIntervalStart;
+	return evaluationIntervalStart;
     }
+
     public void setEvaluationIntervalStart(DateTime evaluationIntervalStart) {
-        this.evaluationIntervalStart = evaluationIntervalStart;
+	this.evaluationIntervalStart = evaluationIntervalStart;
     }
+
     public DateTime getEvaluationIntervalEnd() {
-        return evaluationIntervalEnd;
+	return evaluationIntervalEnd;
     }
+
     public void setEvaluationIntervalEnd(DateTime evaluationIntervalEnd) {
-        this.evaluationIntervalEnd = evaluationIntervalEnd;
+	this.evaluationIntervalEnd = evaluationIntervalEnd;
     }
+
     public byte[] getEvaluatorListFileContent() {
-        return evaluatorListFileContent;
+	return evaluatorListFileContent;
     }
+
     public void setEvaluatorListFileContent(byte[] evaluatorListFileContent) {
-        this.evaluatorListFileContent = evaluatorListFileContent;
+	this.evaluatorListFileContent = evaluatorListFileContent;
     }
+
     public FacultyEvaluationProcess getFacultyEvaluationProcess() {
-        return facultyEvaluationProcess;
+	return facultyEvaluationProcess;
     }
+
     public void setFacultyEvaluationProcess(FacultyEvaluationProcess facultyEvaluationProcess) {
-        this.facultyEvaluationProcess = facultyEvaluationProcess;
+	this.facultyEvaluationProcess = facultyEvaluationProcess;
     }
 
     public Interval getAutoEvaluationInterval() {
-        return new Interval(getAutoEvaluationIntervalStart(), getAutoEvaluationIntervalEnd());
+	return new Interval(getAutoEvaluationIntervalStart(), getAutoEvaluationIntervalEnd());
     }
+
     public Interval getEvaluationInterval() {
-        return new Interval(getEvaluationIntervalStart(), getEvaluationIntervalEnd());
+	return new Interval(getEvaluationIntervalStart(), getEvaluationIntervalEnd());
     }
 
     @Service
