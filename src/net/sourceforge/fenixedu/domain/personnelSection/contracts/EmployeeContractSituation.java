37645e6576b570bda9ab5cf4522732aae45f72bc
Susana Fernandes <susana.fernandes@ist.utl.pt>
2010-05-20 18:49:03 +0000

    Minor Change: allow to create invalid employeeContractSituations. (This data is a mirror of GIAF's data)

--- src/net/sourceforge/fenixedu/domain/personnelSection/contracts/EmployeeContractSituation.java
+++ src/net/sourceforge/fenixedu/domain/personnelSection/contracts/EmployeeContractSituation.java
@@ -2,7 +2,6 @@ package net.sourceforge.fenixedu.domain.personnelSection.contracts;
 
 import net.sourceforge.fenixedu.domain.Employee;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
-import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
@@ -15,7 +14,6 @@ public class EmployeeContractSituation extends EmployeeContractSituation_Base {
 	    final DateTime creationDate, final DateTime modifiedDate) {
 	super();
 	setRootDomainObject(RootDomainObject.getInstance());
-	check(beginDate, endDate);
 	setEmployee(employee);
 	setBeginDate(beginDate);
 	setEndDate(endDate);
@@ -25,15 +23,6 @@ public class EmployeeContractSituation extends EmployeeContractSituation_Base {
 	setCreationDate(creationDate);
 	setModifiedDate(modifiedDate);
 	setImportationDate(new DateTime());
-    }
-
-    protected void check(LocalDate begin, LocalDate end) {
-	if (begin == null) {
-	    throw new DomainException("");
-	}
-	if (end != null && begin.isAfter(end)) {
-	    throw new DomainException("");
-	}
     }
 
     public static EmployeeContractSituation getCurrentEmployeeContractSituation(Employee employee) {
