915ef2d29e00c319895e019a89236b007c82ba0c
Diogo Simões <diogo.fonseca.simoes@ist.utl.pt>
2010-05-26 15:59:35 +0000

    Minor Change: [GWT]: A set of three (3) examples using GWT within Fenix. It illustrates GWT-RPC usage, module creation and configuration, CSS usage and RaphaelGWT integration.

---
+++ src/net/sourceforge/fenixedu/presentationTier/gwt/manager/UIPlayground/listStudents/client/RetrieveSpecificStudentSetAsync.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.presentationTier.gwt.manager.UIPlayground.listStudents.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface RetrieveSpecificStudentSetAsync {
+    
+    public void getStudentNames(AsyncCallback callback);
+
+}
---
+++ src/net/sourceforge/fenixedu/presentationTier/gwt/manager/UIPlayground/listStudents/client/StudentListRender.java
@@ -0,0 +1,73 @@
+package net.sourceforge.fenixedu.presentationTier.gwt.manager.UIPlayground.listStudents.client;
+
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.student.Student;
+import net.sourceforge.fenixedu.presentationTier.gwt.manager.UIPlayground.listStudents.client.RetrieveSpecificStudentSet;
+import net.sourceforge.fenixedu.presentationTier.gwt.manager.UIPlayground.listStudents.client.RetrieveSpecificStudentSetAsync;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+
+@Deprecated
+public class StudentListRender extends Composite{
+    
+    private final RetrieveSpecificStudentSetAsync service;
+    private final DockPanel panel = new DockPanel();
+    private final Button button = new Button("Mostrar Lista");
+    private final Label label = new Label("-- Mensagens de erro aqui --");
+    private Grid table = new Grid(1,1);
+    private List<String> pupilsList;
+    
+    
+    public StudentListRender() {
+	service = (RetrieveSpecificStudentSetAsync) GWT.create(RetrieveSpecificStudentSet.class);
+        ServiceDefTarget endpoint = (ServiceDefTarget) service;
+        endpoint.setServiceEntryPoint(GWT.getModuleBaseURL() + "RetrieveSpecificStudentSet.gwt");
+        
+        table.setTitle("Lista dos alunos");
+        
+        initWidget(panel);
+        panel.add(button, DockPanel.NORTH);
+        panel.add(label, DockPanel.CENTER);
+        panel.add(table, DockPanel.SOUTH);
+        
+        button.addClickHandler(new ClickHandler() {
+            
+            @Override
+            public void onClick(ClickEvent event) {
+        	service.getStudentNames(new AsyncCallback() {
+        	    public void onFailure(Throwable e) {
+                        label.setText("Server call failed");
+                    }
+                    public void onSuccess(Object obj) {
+                        if (obj != null) {
+                            pupilsList = (List<String>) obj;
+                            displayStudents();
+                        } else {
+                            label.setText("Server call returned nothing");
+                        }
+                    }
+        	});
+            }
+        });
+    }
+    
+    private void displayStudents() {
+	int iter = 0;
+	table.resizeRows(pupilsList.size());
+	for(String student : pupilsList) {
+	    iter++;
+	    table.setText(iter, 1, student);
+	}
+    }
+
+}
