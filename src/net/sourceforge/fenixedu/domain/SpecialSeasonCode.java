e1e60ed94ea42a17e0d0be20a48eeb9d251eea44
Diogo Simões <diogo.fonseca.simoes@ist.utl.pt>
2010-06-15 14:15:29 +0000

    Refactor: [SpecialSeason]: SpecialSeasonCode entity is now deprecated. Will be removed on a future release.

--- src/net/sourceforge/fenixedu/domain/SpecialSeasonCode.java
+++ src/net/sourceforge/fenixedu/domain/SpecialSeasonCode.java
@@ -1,7 +1,17 @@
 package net.sourceforge.fenixedu.domain;
 
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
-
+import net.sourceforge.fenixedu.domain.student.StudentStatute;
+import net.sourceforge.fenixedu.domain.student.StudentStatuteType;
+/** 
+ * @deprecated This entity will be removed in a future release.
+ * Special season enrollments are now granted by the StudentStatute
+ * (certain StudentStatuteTypes allow access to special season).
+ * 
+ * 
+ *@see StudentStatute
+ *@see StudentStatuteType
+ */
 public class SpecialSeasonCode extends SpecialSeasonCode_Base {
 
     public SpecialSeasonCode() {
