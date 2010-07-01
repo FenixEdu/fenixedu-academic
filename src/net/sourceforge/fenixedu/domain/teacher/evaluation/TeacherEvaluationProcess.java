5d4ec264a2486acbade600b0264676755e1bd1d0
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2010-07-01 17:37:49 +0000

    Minor Change: allow manager to see what ccad already does

--- src/net/sourceforge/fenixedu/domain/teacher/evaluation/TeacherEvaluationProcess.java
+++ src/net/sourceforge/fenixedu/domain/teacher/evaluation/TeacherEvaluationProcess.java
@@ -9,6 +9,7 @@ import java.util.TreeSet;
 
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 
 public class TeacherEvaluationProcess extends TeacherEvaluationProcess_Base {
@@ -24,8 +25,8 @@ public class TeacherEvaluationProcess extends TeacherEvaluationProcess_Base {
     public static Comparator<TeacherEvaluationProcess> COMPARATOR_BY_INTERVAL = new Comparator<TeacherEvaluationProcess>() {
 	@Override
 	public int compare(TeacherEvaluationProcess p1, TeacherEvaluationProcess p2) {
-	    return FacultyEvaluationProcess.COMPARATOR_BY_INTERVAL.compare(p1.getFacultyEvaluationProcess(), p2
-		    .getFacultyEvaluationProcess());
+	    return FacultyEvaluationProcess.COMPARATOR_BY_INTERVAL.compare(p1.getFacultyEvaluationProcess(),
+		    p2.getFacultyEvaluationProcess());
 	}
     };
 
@@ -107,7 +108,8 @@ public class TeacherEvaluationProcess extends TeacherEvaluationProcess_Base {
 
     public boolean isPossibleToInsertApprovedMark() {
 	return (isEvaluationLocked() || getFacultyEvaluationProcess().getEvaluationInterval().getEnd().isBeforeNow())
-		&& AccessControl.getPerson().isTeacherEvaluationCoordinatorCouncilMember();
+		&& (AccessControl.getPerson().isTeacherEvaluationCoordinatorCouncilMember() || AccessControl.getPerson().hasRole(
+			RoleType.MANAGER));
     }
 
     public boolean isPossibleToLockAutoEvaluation() {
