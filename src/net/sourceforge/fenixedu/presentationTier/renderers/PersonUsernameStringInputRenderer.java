7b4d6fa96444ba6999015027b688aa55e91baab3
Luis Cruz <luis.cruz@ist.utl.pt>
2010-07-07 22:18:13 +0000

    Bug Fix: Avoid exceptions because of missing resources. Updated fenix-web lib to avoid class cast exceptions and array out of bound exceptions related to usage of StringInputRendrer.

--- src/net/sourceforge/fenixedu/presentationTier/renderers/PersonUsernameStringInputRenderer.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/PersonUsernameStringInputRenderer.java
@@ -1,5 +1,7 @@
 package net.sourceforge.fenixedu.presentationTier.renderers;
 
+import org.apache.commons.collections.Predicate;
+
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.presentationTier.renderers.converters.PersonUsernameConverter;
 import pt.ist.fenixWebFramework.renderers.StringInputRenderer;
@@ -14,7 +16,13 @@ public class PersonUsernameStringInputRenderer extends StringInputRenderer {
 	Person person = (Person) object;
 	String username = (person != null) ? person.getUsername() : null;
 
-	HtmlFormComponent formComponent = (HtmlFormComponent) super.createTextField(username, type);
+	final HtmlComponent container = super.createTextField(username, type);
+	final HtmlFormComponent formComponent = (HtmlFormComponent)container.getChild(new Predicate() {
+	    @Override
+	    public boolean evaluate(Object arg0) {
+		return arg0 instanceof HtmlFormComponent;
+	    }
+	});
 	formComponent.setConverter(new PersonUsernameConverter());
 
 	return formComponent;
