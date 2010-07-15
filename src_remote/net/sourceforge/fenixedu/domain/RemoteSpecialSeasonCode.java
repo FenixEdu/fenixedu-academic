f082d141350945fa4a9715f20f057c4d7f9d1d5a
Diogo Simões <diogo.fonseca.simoes@ist.utl.pt>
2010-07-15 10:55:35 +0000

    Minor Change: [StudentStatute]: These classes reflect the remote side of 2 entities no longer existing in the Fenix domain model and need to be deleted manually.

--- src_remote/net/sourceforge/fenixedu/domain/RemoteSpecialSeasonCode.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.fenixedu.domain;
-
-public class RemoteSpecialSeasonCode extends RemoteSpecialSeasonCode_Base {
-    
-    public  RemoteSpecialSeasonCode() {
-        super();
-    }
-    
-}
--- src_remote/net/sourceforge/fenixedu/domain/RemoteYearStudentSpecialSeasonCode.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.fenixedu.domain;
-
-public class RemoteYearStudentSpecialSeasonCode extends RemoteYearStudentSpecialSeasonCode_Base {
-    
-    public  RemoteYearStudentSpecialSeasonCode() {
-        super();
-    }
-    
-}
