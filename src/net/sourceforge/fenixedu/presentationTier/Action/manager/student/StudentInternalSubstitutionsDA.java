8e5e1b943db669b215ed784679cbb8e3038c7241
João Figueiredo <joao.figueiredo@ist.utl.pt>
2010-09-06 17:42:55 +0000

    Minor Change: added internal substitutions to manager

---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/manager/student/StudentInternalSubstitutionsDA.java
@@ -0,0 +1,10 @@
+package net.sourceforge.fenixedu.presentationTier.Action.manager.student;
+
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+
+@Mapping(path = "/studentInternalSubstitutions", module = "manager", formBean = "studentDismissalForm")
+
+public class StudentInternalSubstitutionsDA extends
+	net.sourceforge.fenixedu.presentationTier.Action.administrativeOffice.student.StudentInternalSubstitutionsDA {
+
+}
