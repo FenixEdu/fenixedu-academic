09f1c97ede228ebf4dafebc9f7d33b9dfcdd63ab
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2010-09-09 17:21:05 +0000

    Bug fix: Fixed access control to the residence management portal.

--- src/net/sourceforge/fenixedu/predicates/EventsPredicates.java
+++ src/net/sourceforge/fenixedu/predicates/EventsPredicates.java
@@ -1,7 +1,9 @@
 package net.sourceforge.fenixedu.predicates;
 
+import net.sourceforge.fenixedu.domain.Employee;
 import net.sourceforge.fenixedu.domain.accounting.ResidenceEvent;
 import net.sourceforge.fenixedu.domain.organizationalStructure.Unit;
+import net.sourceforge.fenixedu.domain.person.RoleType;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.injectionCode.AccessControlPredicate;
 
@@ -9,9 +11,10 @@ public class EventsPredicates {
 
     public static final AccessControlPredicate<ResidenceEvent> MANAGER_OR_RESIDENCE_UNIT_EMPLOYEE = new AccessControlPredicate<ResidenceEvent>() {
 	public boolean evaluate(ResidenceEvent residenceEvent) {
+	    Employee employee = AccessControl.getPerson().getEmployee();
 	    return RolePredicates.MANAGER_PREDICATE.evaluate(residenceEvent)
-		    || isManagementUnitOrParent(residenceEvent.getManagementUnit(), AccessControl.getPerson().getEmployee()
-			    .getCurrentWorkingPlace());
+		    || employee.getPerson().hasRole(RoleType.RESIDENCE_MANAGER)
+		    || isManagementUnitOrParent(residenceEvent.getManagementUnit(), employee.getCurrentWorkingPlace());
 	}
 
 	private boolean isManagementUnitOrParent(Unit managementUnit, Unit currentWorkingPlace) {
