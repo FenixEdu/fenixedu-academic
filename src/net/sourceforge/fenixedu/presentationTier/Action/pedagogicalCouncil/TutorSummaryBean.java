9f5848641d314db6bfeeb9ca1b96cc426ccf6a0e
Luis Cruz <luis.cruz@ist.utl.pt>
2010-10-20 22:48:21 +0000

    Bug Fix: Added missing imports.

--- src/net/sourceforge/fenixedu/presentationTier/Action/pedagogicalCouncil/TutorSummaryBean.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/pedagogicalCouncil/TutorSummaryBean.java
@@ -2,6 +2,7 @@ package net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
