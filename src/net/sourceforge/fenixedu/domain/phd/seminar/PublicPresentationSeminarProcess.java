3239b26a93981522ab7b3a9fa1a07065952b3234
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2010-11-12 12:35:32 +0000

    Bug fix: removed check to see if the alert is to be generated

--- src/net/sourceforge/fenixedu/domain/phd/seminar/PublicPresentationSeminarProcess.java
+++ src/net/sourceforge/fenixedu/domain/phd/seminar/PublicPresentationSeminarProcess.java
@@ -86,11 +86,9 @@ public class PublicPresentationSeminarProcess extends PublicPresentationSeminarP
 	    process.createState(PublicPresentationSeminarProcessStateType.COMMISSION_WAITING_FOR_VALIDATION,
 		    userView.getPerson(), bean.getRemarks());
 
-	    if (bean.getGenerateAlert()) {
-		AlertService.alertAcademicOffice(process.getIndividualProgramProcess(), getPublicPresentationSeminarPermission(),
-			"message.phd.alert.public.presentation.seminar.comission.validation.subject",
-			"message.phd.alert.public.presentation.seminar.comission.validation.body");
-	    }
+	    AlertService.alertAcademicOffice(process.getIndividualProgramProcess(), getPublicPresentationSeminarPermission(),
+		    "message.phd.alert.public.presentation.seminar.comission.validation.subject",
+		    "message.phd.alert.public.presentation.seminar.comission.validation.body");
 
 	    return process;
 	}
