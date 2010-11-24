d85e682a619fce545e49e211b7cd4795bcb3e42b
Susana Fernandes <susana.fernandes@ist.utl.pt>
2010-11-24 12:21:26 +0000

    Bug Fix: RoleOperationLog may not have user logged (when roles are added/removed by scripts)

--- src/net/sourceforge/fenixedu/domain/RoleOperationLog.java
+++ src/net/sourceforge/fenixedu/domain/RoleOperationLog.java
@@ -3,28 +3,27 @@ package net.sourceforge.fenixedu.domain;
 import org.joda.time.DateTime;
 
 public class RoleOperationLog extends RoleOperationLog_Base {
-    
-    public  RoleOperationLog() {
-        super();
+
+    public RoleOperationLog() {
+	super();
     }
 
     public RoleOperationLog(Role role, Person person, Person whoGranted, RoleOperationType operationType) {
-	this.setOperationType(operationType);
-	this.setLogDate(new DateTime()); // current time
-
-	this.setIstUsername(person.getIstUsername());
-	this.setWhoGrantedIstUsername(whoGranted.getIstUsername());
-
-	this.setRole(role);
-	this.setPerson(person);
-	this.setPersonWhoGranted(whoGranted);
-
-	person.addPersonRoleOperationLog(this);
-	whoGranted.addGivenRoleOperationLog(this);
+	setOperationType(operationType);
+	setLogDate(new DateTime());
+	setRole(role);
+	setIstUsername(person.getIstUsername());
+	setPerson(person);
+	if (whoGranted != null) {
+	    setWhoGrantedIstUsername(whoGranted.getIstUsername());
+	    setPersonWhoGranted(whoGranted);
+	} else {
+	    setWhoGrantedIstUsername("No user");
+	}
     }
 
     @Override
     protected RootDomainObject getRootDomainObject() {
-	return this.getRole().getRootDomainObject();
+	return hasRole() ? getRole().getRootDomainObject() : null;
     }
 }
