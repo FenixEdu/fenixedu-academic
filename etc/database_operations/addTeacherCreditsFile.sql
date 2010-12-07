2b143c94d1d36423a797b3e8dbdd84719cfcadd1
Susana Fernandes <susana.fernandes@ist.utl.pt>
2010-12-07 16:32:36 +0000

    Bug Fix:

--- etc/database_operations/addTeacherCreditsFile.sql
+++ etc/database_operations/addTeacherCreditsFile.sql
@@ -1,3 +1,1 @@
-alter table `FILE` add `OID_TEACHER_CREDITS` bigint unsigned, add index (OID_TEACHER_CREDITS);
-
-ERROR 1069 (42000): Too many keys specified; max 64 keys allowed
+alter table `FILE` add `OID_TEACHER_CREDITS` bigint unsigned;
