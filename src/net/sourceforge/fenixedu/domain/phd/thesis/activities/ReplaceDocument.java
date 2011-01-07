6ea20e7985a266a79448996d62fb6ade71810f10
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-01-07 15:29:19 +0000

    Minor Change: Functionality to replace thesis documents, fixed upload feedback reports on academic admin office; Bug Fix: The method used to retrieve last version of documents was hiding valid documents of same type

---
+++ src/net/sourceforge/fenixedu/domain/phd/thesis/activities/ReplaceDocument.java
@@ -0,0 +1,29 @@
+package net.sourceforge.fenixedu.domain.phd.thesis.activities;
+
+import net.sourceforge.fenixedu.applicationTier.IUserView;
+import net.sourceforge.fenixedu.domain.caseHandling.PreConditionNotValidException;
+import net.sourceforge.fenixedu.domain.phd.PhdProgramDocumentUploadBean;
+import net.sourceforge.fenixedu.domain.phd.PhdProgramProcessDocument;
+import net.sourceforge.fenixedu.domain.phd.thesis.PhdThesisProcess;
+import net.sourceforge.fenixedu.injectionCode.AccessControl;
+
+public class ReplaceDocument extends PhdThesisActivity {
+
+    @Override
+    protected void activityPreConditions(PhdThesisProcess process, IUserView userView) {
+	if (!PhdThesisProcess.isMasterDegreeAdministrativeOfficeEmployee(userView)) {
+	    throw new PreConditionNotValidException();
+	}
+    }
+
+    @Override
+    protected PhdThesisProcess executeActivity(PhdThesisProcess process, IUserView userView, Object object) {
+	PhdProgramDocumentUploadBean documentBean = (PhdProgramDocumentUploadBean) object;
+	PhdProgramProcessDocument document = process.getLastestDocumentVersionFor(documentBean.getType());
+
+	document.replaceDocument(documentBean.getType(), documentBean.getRemarks(), documentBean.getFileContent(), documentBean
+		.getFilename(), AccessControl.getPerson());
+
+	return process;
+    }
+}
