7258b55a97cc1aabc376a4529217b3f259d519a2
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-01-17 16:35:14 +0000

    Minor Change: Phd Migration Process

---
+++ src/net/sourceforge/fenixedu/domain/phd/migration/common/exceptions/ParseException.java
@@ -0,0 +1,10 @@
+package net.sourceforge.fenixedu.domain.phd.migration.common.exceptions;
+
+public class ParseException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/net/sourceforge/fenixedu/domain/phd/migration/common/exceptions/SocialSecurityNumberMismatchException.java
@@ -0,0 +1,5 @@
+package net.sourceforge.fenixedu.domain.phd.migration.common.exceptions;
+
+public class SocialSecurityNumberMismatchException extends RuntimeException {
+
+}
