23dcb2ef00004fd6c35e05ffbb757513ce3da2af
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2011-02-24 18:34:14 +0000

    Minor change: added a new method to retrieve the template by execution semester Bug fix: was not setting the rootDomainObject

--- src/net/sourceforge/fenixedu/domain/inquiries/ResultsInquiryTemplate.java
+++ src/net/sourceforge/fenixedu/domain/inquiries/ResultsInquiryTemplate.java
@@ -1,9 +1,24 @@
 package net.sourceforge.fenixedu.domain.inquiries;
 
+import java.util.List;
+
+import net.sourceforge.fenixedu.domain.ExecutionSemester;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+
 public class ResultsInquiryTemplate extends ResultsInquiryTemplate_Base {
-    
-    public  ResultsInquiryTemplate() {
-        super();
+
+    public ResultsInquiryTemplate() {
+	super();
+	setRootDomainObject(RootDomainObject.getInstance());
     }
-    
+
+    public static ResultsInquiryTemplate getTemplateByExecutionPeriod(ExecutionSemester executionSemester) {
+	final List<InquiryTemplate> inquiryTemplates = RootDomainObject.getInstance().getInquiryTemplates();
+	for (final InquiryTemplate inquiryTemplate : inquiryTemplates) {
+	    if (inquiryTemplate instanceof ResultsInquiryTemplate && executionSemester == inquiryTemplate.getExecutionPeriod()) {
+		return (ResultsInquiryTemplate) inquiryTemplate;
+	    }
+	}
+	return null;
+    }
 }
