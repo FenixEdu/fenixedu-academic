07a26dd52cab2b2e373f17da8f039d9d6d554911
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2011-03-10 05:30:30 +0000

    Minor change: corrected syntax error

--- etc/database_operations/updateQUCTables.sql
+++ etc/database_operations/updateQUCTables.sql
@@ -1,4 +1,4 @@
 alter table `INQUIRY_QUESTION_HEADER` add `OID_RESULT_GROUP_QUESTION` bigint unsigned;
 alter table `INQUIRY_GROUP_QUESTION` add `OID_RESULT_QUESTION_HEADER` bigint unsigned;
-alter table `INQUIRY_RESULT_COMMENT` drop ORDER, add `RESULT_ORDER` int(11);
+alter table `INQUIRY_RESULT_COMMENT` drop `ORDER`, add `RESULT_ORDER` int(11);
 alter table `INQUIRY_BLOCK` add `OID_GROUP_RESULT_QUESTION` bigint unsigned, add index (OID_GROUP_RESULT_QUESTION);
