2cbf08d805aa30b113a5cf29544f24f48bb58acb
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2011-03-30 10:32:09 +0000

    Minor change: setting the response date time

--- src/net/sourceforge/fenixedu/domain/inquiries/InquiryAnswer.java
+++ src/net/sourceforge/fenixedu/domain/inquiries/InquiryAnswer.java
@@ -2,11 +2,14 @@ package net.sourceforge.fenixedu.domain.inquiries;
 
 import net.sourceforge.fenixedu.domain.RootDomainObject;
 
+import org.joda.time.DateTime;
+
 public class InquiryAnswer extends InquiryAnswer_Base {
 
     public InquiryAnswer() {
 	super();
 	setRootDomainObject(RootDomainObject.getInstance());
+	setResponseDateTime(new DateTime());
     }
 
     public QuestionAnswer getQuestionAnswer(InquiryQuestion inquiryQuestion) {
