e167f84a6dc40387eed59682ab5ce92b74a6aaeb
Luis Cruz <luis.cruz@ist.utl.pt>
2011-04-07 17:22:36 +0000

    New Feature: Blank filter for allowing users to be logged into multiple applications with iframes.

---
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/BlankResponseFilter.java
@@ -0,0 +1,30 @@
+package net.sourceforge.fenixedu.presentationTier.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class BlankResponseFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig config) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+	    throws IOException, ServletException {
+	final ServletOutputStream outputStream = response.getOutputStream();
+	outputStream.close();
+    }
+
+}
