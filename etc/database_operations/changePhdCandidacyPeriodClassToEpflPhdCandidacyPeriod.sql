ffe6cb5632d0aca21451522d1d69e6773115d2b8
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-04-21 10:13:39 +0000

    Bug Fix: Changed sql script in order to be included on run

--- etc/database_operations/changePhdCandidacyPeriodClassToEpflPhdCandidacyPeriod.sql
+++ etc/database_operations/changePhdCandidacyPeriodClassToEpflPhdCandidacyPeriod.sql
@@ -1,4 +1,5 @@
-SELECT @phd_candidacy_period_class_id := DOMAIN_CLASS_ID FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.PhdCandidacyPeriod';
-SELECT @epfl_candidacy_period_class_id := DOMAIN_CLASS_ID FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod';
-
-update CANDIDACY_PERIOD SET OID = ((@epfl_candidacy_period_class_id << 32) + (OID & 0x00000000ffffffff)), TYPE = 'EPFL', OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod' WHERE (OID >> 32) = @phd_candidacy_period_class_id;
+SELECT @entry_exists := COUNT(*) FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod';
+ 
+update FF$DOMAIN_CLASS_INFO SET DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod' 
+	WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.PhdCandidacyPeriod' 
+			AND @entry_exists = 0;
