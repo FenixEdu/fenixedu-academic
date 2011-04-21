829ab3ac8bc789c543535f3cf7edf29209f4699e
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-04-21 18:05:19 +0000

    Bug Fix: Ensure the PhdCandidacyPeriods are EPFLCandidacyPeriods in the event of EPFLPhdCandidacyPeriod is created on FF$DOMAIN_CLASS_INFO

--- etc/database_operations/R2011-04-21/changePhdCandidacyPeriodClassToEpflPhdCandidacyPeriod.sql
+++ etc/database_operations/R2011-04-21/changePhdCandidacyPeriodClassToEpflPhdCandidacyPeriod.sql
@@ -3,3 +3,7 @@ SELECT @entry_exists := COUNT(*) FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NA
 update FF$DOMAIN_CLASS_INFO SET DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod' 
 	WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.PhdCandidacyPeriod' 
 			AND @entry_exists = 0;
+
+SELECT @epfl_candidacy_period_class_id := DOMAIN_CLASS_ID FROM FF$DOMAIN_CLASS_INFO WHERE DOMAIN_CLASS_NAME = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod';
+
+update CANDIDACY_PERIOD SET OID = ((@epfl_candidacy_period_class_id << 32) + (OID & 0x00000000ffffffff)), TYPE = 'EPFL', OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.phd.candidacy.EPFLPhdCandidacyPeriod' WHERE OJB_CONCRETE_CLASS = 'net.sourceforge.fenixedu.domain.phd.candidacy.PhdCandidacyPeriod';
