ff1e4431c60325b352cdeaf226111acdeb27f009
Luis Cruz <luis.cruz@ist.utl.pt>
2011-05-13 17:42:38 +0000

    Refactor: Get photo from common method.

--- src/net/sourceforge/fenixedu/presentationTier/Action/publico/PhotographRetrievalOnPublicSpaceDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/publico/PhotographRetrievalOnPublicSpaceDA.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sourceforge.fenixedu.domain.Person;
-import net.sourceforge.fenixedu.domain.Photograph;
 import net.sourceforge.fenixedu.domain.User;
 import net.sourceforge.fenixedu.presentationTier.Action.person.RetrievePersonalPhotoAction;
 
@@ -20,24 +19,12 @@ public class PhotographRetrievalOnPublicSpaceDA extends RetrievePersonalPhotoAct
     public ActionForward retrievePhotographOnPublicSpace(ActionMapping mapping, ActionForm actionForm,
 	    HttpServletRequest request, HttpServletResponse response) {
 	Person person = AbstractDomainObject.fromExternalId(request.getParameter("personId"));
-	return retrieve(response, person);
-    }
-
-    private ActionForward retrieve(HttpServletResponse response, Person person) {
-	final Photograph personalPhoto = person.getPersonalPhoto();
-	if (personalPhoto != null) {
-	    if (person.isPhotoPubliclyAvailable()) {
-		writePhoto(response, personalPhoto);
-		return null;
-	    }
-	}
-	writeUnavailablePhoto(response);
-	return null;
+	return retrievePhotograph(request, response, person);
     }
 
     public ActionForward retrieveByIstId(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
 	    HttpServletResponse response) {
 	User user = User.readUserByUserUId(request.getParameter("istId"));
-	return retrieve(response, user.getPerson());
+	return retrievePhotograph(request, response, user.getPerson());
     }
 }
