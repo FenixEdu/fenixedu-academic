d0f846b8dbc420be8793c6da544f5fc87a574396
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-05-25 17:22:56 +0000

    Bug Fix: Set correct column name

--- etc/database_operations/R2011-05-24/bindGuiderAcceptanceLetterToPhdParticipant.sql
+++ etc/database_operations/R2011-05-24/bindGuiderAcceptanceLetterToPhdParticipant.sql
@@ -1,2 +1,2 @@
-alter table `FILE` add `OID_PHD_PARTICIPANT` bigint unsigned, add index (OID_PHD_PARTICIPANT);
+alter table `FILE` add `OID_PHD_GUIDER` bigint unsigned, add index (OID_PHD_GUIDER);
 alter table `PHD_PARTICIPANT` add `OID_ACCEPTANCE_LETTER` bigint unsigned;
