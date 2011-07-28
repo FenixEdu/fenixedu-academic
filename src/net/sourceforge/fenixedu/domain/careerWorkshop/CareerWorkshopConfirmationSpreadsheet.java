060553296d0a813a972ef04fde5d239739a47bb0
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2011-07-28 14:33:35 +0000

    Bug Fix: Dspace required at least a 2 node depth path.

--- src/net/sourceforge/fenixedu/domain/careerWorkshop/CareerWorkshopConfirmationSpreadsheet.java
+++ src/net/sourceforge/fenixedu/domain/careerWorkshop/CareerWorkshopConfirmationSpreadsheet.java
@@ -7,21 +7,26 @@ import pt.utl.ist.fenix.tools.file.VirtualPath;
 import pt.utl.ist.fenix.tools.file.VirtualPathNode;
 
 public class CareerWorkshopConfirmationSpreadsheet extends CareerWorkshopConfirmationSpreadsheet_Base {
-    
+
     private static final String ROOT_DIR_DESCRIPTION = "IST Career Workshops";
 
     private static final String ROOT_DIR = "CareerWorkshops";
-    
+
+    private static final String SHEETS_DIR = "Sheet";
+
+    private static final String SHEETS_DIR_DESCRIPTION = "Spreadsheets";
+
     public  CareerWorkshopConfirmationSpreadsheet(String filename, byte[] content) {
-        super();
-        RoleGroup cg = new RoleGroup(Role.getRoleByRoleType(RoleType.DIRECTIVE_COUNCIL));
+	super();
+	RoleGroup cg = new RoleGroup(Role.getRoleByRoleType(RoleType.DIRECTIVE_COUNCIL));
 	init(getVirtualPath(), filename, filename, null, content, cg);
     }
-    
+
     private VirtualPath getVirtualPath() {
 	final VirtualPath filePath = new VirtualPath();
 	filePath.addNode(new VirtualPathNode(ROOT_DIR, ROOT_DIR_DESCRIPTION));
+	filePath.addNode(new VirtualPathNode(SHEETS_DIR, SHEETS_DIR_DESCRIPTION));
 	return filePath;
     }
-    
+
 }
