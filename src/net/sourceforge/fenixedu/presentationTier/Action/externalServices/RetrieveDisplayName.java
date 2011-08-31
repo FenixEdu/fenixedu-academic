12f778048e289a45a9245723483620f5963520af
Luis Cruz <luis.cruz@ist.utl.pt>
2011-08-31 18:58:15 +0000

    New Feature: Simple service for exporting a display name for a givin username.

---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/externalServices/RetrieveDisplayName.java
@@ -0,0 +1,46 @@
+package net.sourceforge.fenixedu.presentationTier.Action.externalServices;
+
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.User;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+
+@Mapping(module = "external", path = "/retrieveDisplayName", scope = "request", parameter = "method")
+public class RetrieveDisplayName extends ExternalInterfaceDispatchAction {
+
+    public ActionForward check(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+	    HttpServletResponse response) throws Exception {
+	final String username = request.getParameter("username");
+	String responseMessage = getNickname(username);
+
+	response.setContentType("text/plain");
+
+	final OutputStream outputStream = response.getOutputStream();
+	outputStream.write(responseMessage.getBytes());
+	outputStream.close();
+
+	return null;
+    }
+
+    private String getNickname(final String username) {
+	return username == null || username.isEmpty() ? "" : getNickname(User.readUserByUserUId(username));
+    }
+
+    private String getNickname(final User user) {
+	return user == null ? "" : getNickname(user.getPerson());
+    }
+
+    private String getNickname(final Person person) {
+	return person == null ? "" : person.getNickname();
+    }
+
+}
