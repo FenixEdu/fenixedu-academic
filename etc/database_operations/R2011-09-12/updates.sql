9626310246b7183db0d683ecc2c0d4fdeac004be
Luis Cruz <luis.cruz@ist.utl.pt>
2011-09-15 16:21:45 +0000

    Minor Change: Added missing sql instruction.

--- etc/database_operations/R2011-09-12/updates.sql
+++ etc/database_operations/R2011-09-12/updates.sql
@@ -1,1 +1,2 @@
 create table `PERSON_IDENTIFICATION_DOCUMENT_EXTRA_INFO` (`OID` bigint unsigned, `VALUE` text, `OID_PERSON` bigint unsigned, `REGISTERED_IN_SYSTEM_TIMESTAMP` timestamp NULL default NULL, `OJB_CONCRETE_CLASS` varchar(255) NOT NULL DEFAULT '', `ID_INTERNAL` int(11) NOT NULL auto_increment, primary key (ID_INTERNAL), index (OID), index (OID_PERSON)) ENGINE=InnoDB, character set latin1;
+alter table PERSON_IDENTIFICATION_DOCUMENT_EXTRA_INFO add column OID_ROOT_DOMAIN_OBJECT bigint(20) unsigned DEFAULT NULL;
