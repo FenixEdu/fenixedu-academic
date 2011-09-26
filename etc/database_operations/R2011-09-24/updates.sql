96ff9ade09a8b4d6c67b80323fc79540f3de47ad
Luis Cruz <luis.cruz@ist.utl.pt>
2011-09-26 00:08:13 +0000

    Minor Change: Updated schedule plugin jar.

--- etc/database_operations/R2011-09-24/updates.sql
+++ etc/database_operations/R2011-09-24/updates.sql
@@ -2,3 +2,7 @@ create table `TASK_LOG` (`SUCCESSFUL` tinyint(1), `TASK_END` timestamp NULL defa
 create table `SCHEDULER_SYSTEM` (`OID` bigint unsigned, `OID_PENDING_TASK` bigint unsigned, `ID_INTERNAL` int(11) NOT NULL auto_increment, primary key (ID_INTERNAL), index (OID)) ENGINE=InnoDB, character set latin1;
 create table `TASK` (`OID_PREVIOUS_TASK` bigint unsigned, `OID_NEXT_TASK` bigint unsigned, `OID` bigint unsigned, `OID_SCHEDULER_SYSTEM` bigint unsigned, `OID_SCHEDULER_SYSTEM_FROM_PENDING_TASK` bigint unsigned, `LAST_RUN` timestamp NULL default NULL, `OJB_CONCRETE_CLASS` varchar(255) NOT NULL DEFAULT '', `ID_INTERNAL` int(11) NOT NULL auto_increment, primary key (ID_INTERNAL), index (OID), index (OID_SCHEDULER_SYSTEM)) ENGINE=InnoDB, character set latin1;
 create table `TASK_SCHEDULE` (`OID` bigint unsigned, `OID_TASK` bigint unsigned, `DAY` int(11), `HOUR` int(11), `MINUTE` int(11), `DAYOFWEEK` int(11), `MONTH` int(11), `ID_INTERNAL` int(11) NOT NULL auto_increment, primary key (ID_INTERNAL), index (OID), index (OID_TASK)) ENGINE=InnoDB, character set latin1;
+
+alter table TASK add column LAST_RUN_START timestamp NULL DEFAULT NULL;
+alter table TASK add column LAST_RUN_END timestamp NULL DEFAULT NULL;
+
