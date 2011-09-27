7ba52a08814bea7d5d71cd620052641e890039a4
Luis Cruz <luis.cruz@ist.utl.pt>
2011-09-27 01:12:55 +0000

    Bug Fix: No assiduousness available for researchers.

--- src/net/sourceforge/fenixedu/domain/cardGeneration/CardGenerationBatch.java
+++ src/net/sourceforge/fenixedu/domain/cardGeneration/CardGenerationBatch.java
@@ -535,6 +535,11 @@ public class CardGenerationBatch extends CardGenerationBatch_Base {
 	} else if (category2 == Category.CODE_81 && category1 == Category.CODE_96) {
 	    return line2;
 	}
+	if (category1 == Category.CODE_81 && category2 == Category.CODE_99) {
+	    return mergeStudent(line1, line2, Category.CODE_82);
+	} else if (category2 == Category.CODE_81 && category1 == Category.CODE_99) {
+	    return mergeStudent(line2, line1, Category.CODE_82);
+	}
 	if (category1 == Category.CODE_82 && category2 == Category.CODE_92) {
 	    return mergeInMergedFormat(line1, line2, Category.CODE_82);
 	} else if (category2 == Category.CODE_82 && category1 == Category.CODE_92) {
