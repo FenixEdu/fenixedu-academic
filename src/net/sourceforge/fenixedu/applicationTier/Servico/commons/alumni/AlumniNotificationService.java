27fa94da5319e1277aa1bfed5647182292b1f3fa
Luis Cruz <luis.cruz@ist.utl.pt>
2011-10-21 17:26:32 +0000

    Bug Fix: Access to users local can only be determined during each request.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/commons/alumni/AlumniNotificationService.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/commons/alumni/AlumniNotificationService.java
@@ -20,9 +20,17 @@ import pt.utl.ist.fenix.tools.util.i18n.Language;
 
 public class AlumniNotificationService extends FenixService {
 
-    final static ResourceBundle ALUMNI_RESOURCES = ResourceBundle.getBundle("resources.AlumniResources", Language.getLocale());
-    final static ResourceBundle GLOBAL_RESOURCES = ResourceBundle.getBundle("resources.GlobalResources", Language.getLocale());
-    final static ResourceBundle MANAGER_RESOURCES = ResourceBundle.getBundle("resources.ManagerResources", Language.getLocale());
+    static ResourceBundle getAlumniBundle() {
+	return ResourceBundle.getBundle("resources.AlumniResources", Language.getLocale());
+    }
+
+    static ResourceBundle getGlobalBundle() {
+	return ResourceBundle.getBundle("resources.GlobalResources", Language.getLocale());
+    }
+
+    static ResourceBundle getManagerBundle() {
+	return ResourceBundle.getBundle("resources.ManagerResources", Language.getLocale());
+    }
 
     private static void sendEmail(final Collection<Recipient> recipients, final String subject, final String body,
 	    final String bccs) {
@@ -36,40 +44,40 @@ public class AlumniNotificationService extends FenixService {
 
     protected static void sendPublicAccessMail(final Alumni alumni, final String alumniEmail) {
 
-	final String subject = ALUMNI_RESOURCES.getString("alumni.public.registration.mail.subject");
+	final String subject = getAlumniBundle().getString("alumni.public.registration.mail.subject");
 	final Person person = alumni.getStudent().getPerson();
-	final String body = MessageFormat.format(ALUMNI_RESOURCES.getString("alumni.public.registration.url"),
+	final String body = MessageFormat.format(getAlumniBundle().getString("alumni.public.registration.url"),
 		person.getFirstAndLastName(), getRegisterConclusionURL(alumni));
 
 	sendEmail(Collections.EMPTY_LIST, subject, body, alumniEmail);
     }
 
     public static String getRegisterConclusionURL(final Alumni alumni) {
-	final String fenixURL = GLOBAL_RESOURCES.getString("fenix.url");
-	return MessageFormat.format(ALUMNI_RESOURCES.getString("alumni.public.registration.conclusion.url"), fenixURL, alumni
+	final String fenixURL = getGlobalBundle().getString("fenix.url");
+	return MessageFormat.format(getAlumniBundle().getString("alumni.public.registration.conclusion.url"), fenixURL, alumni
 		.getIdInternal().toString(), alumni.getUrlRequestToken());
     }
 
     protected static void sendIdentityCheckEmail(AlumniIdentityCheckRequest request, Boolean approval) {
 
-	final String subject = MANAGER_RESOURCES.getString("alumni.identity.request.mail.subject");
+	final String subject = getManagerBundle().getString("alumni.identity.request.mail.subject");
 
 	String body;
 	if (approval) {
-	    body = MessageFormat.format(MANAGER_RESOURCES.getString("alumni.identity.request.confirm.identity"), request
+	    body = MessageFormat.format(getManagerBundle().getString("alumni.identity.request.confirm.identity"), request
 		    .getAlumni().getStudent().getPerson().getFirstAndLastName());
 
 	    switch (request.getRequestType()) {
 
 	    case IDENTITY_CHECK: // legacy behavior
-		body += MANAGER_RESOURCES.getString("alumni.identity.request.curriculum.access");
+		body += getManagerBundle().getString("alumni.identity.request.curriculum.access");
 		break;
 	    case PASSWORD_REQUEST:
-		body += MessageFormat.format(MANAGER_RESOURCES.getString("alumni.identity.request.password.request"), request
+		body += MessageFormat.format(getManagerBundle().getString("alumni.identity.request.password.request"), request
 			.getAlumni().getLoginUsername(), String.valueOf(request.getOID()), request.getRequestToken().toString());
 		break;
 	    case STUDENT_NUMBER_RECOVERY:
-		body += MessageFormat.format(MANAGER_RESOURCES.getString("alumni.identity.request.student.number.info"), request
+		body += MessageFormat.format(getManagerBundle().getString("alumni.identity.request.student.number.info"), request
 			.getAlumni().getStudent().getNumber().toString());
 		break;
 
@@ -77,14 +85,14 @@ public class AlumniNotificationService extends FenixService {
 		return;
 	    }
 	} else {
-	    body = MessageFormat.format(MANAGER_RESOURCES.getString("alumni.identity.request.refuse.identity"), request
+	    body = MessageFormat.format(getManagerBundle().getString("alumni.identity.request.refuse.identity"), request
 		    .getAlumni().getStudent().getPerson().getFirstAndLastName());
 	}
 
 	body = body + " " + request.getComment();
 	if (!approval && request.getRequestType().equals(AlumniRequestType.PASSWORD_REQUEST)) {
 	    body += "\n"
-		    + MessageFormat.format(MANAGER_RESOURCES.getString("alumni.identity.request.password.request.refuse"),
+		    + MessageFormat.format(getManagerBundle().getString("alumni.identity.request.password.request.refuse"),
 			    getRegisterConclusionURL(request.getAlumni()));
 	}
 
@@ -93,8 +101,8 @@ public class AlumniNotificationService extends FenixService {
 
     protected static void sendRegistrationSuccessMail(final Alumni alumni) {
 
-	final String subject = ALUMNI_RESOURCES.getString("alumni.public.success.mail.subject");
-	final String body = MessageFormat.format(ALUMNI_RESOURCES.getString("alumni.public.username.login.url"), alumni
+	final String subject = getAlumniBundle().getString("alumni.public.success.mail.subject");
+	final String body = MessageFormat.format(getAlumniBundle().getString("alumni.public.username.login.url"), alumni
 		.getStudent().getPerson().getFirstAndLastName(), alumni.getLoginUsername());
 
 	sendEmail(getAlumniRecipients(alumni), subject, body, null);
