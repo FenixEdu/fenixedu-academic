2a2cde1b52e6d88fcaeea0891348128c51bd1328
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2011-11-21 11:49:36 +0000

    Minor Change: [RT#315960]: problem related with person duplication, needed to fix the delete() and remove duplicated library card

--- src/net/sourceforge/fenixedu/domain/library/LibraryCard.java
+++ src/net/sourceforge/fenixedu/domain/library/LibraryCard.java
@@ -2,6 +2,8 @@ package net.sourceforge.fenixedu.domain.library;
 
 import net.sourceforge.fenixedu.dataTransferObject.library.LibraryCardDTO;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.documents.LibraryMissingCardsDocument;
+import net.sourceforge.fenixedu.domain.documents.LibraryMissingLettersDocument;
 
 public class LibraryCard extends LibraryCard_Base {
 
@@ -32,7 +34,22 @@ public class LibraryCard extends LibraryCard_Base {
 
     public void delete() {
 	removePerson();
+	for (LibraryMissingCardsDocument document : getDocumentSet()) {
+	    removeDocument(document);
+	}
+	for (LibraryMissingLettersDocument letter : getLettersSet()) {
+	    removeLetters(letter);
+	}
 	removeRootDomainObject();
 	deleteDomainObject();
+    }
+
+    public static final LibraryCard readByCode(String code) {
+	for (LibraryCard card : RootDomainObject.getInstance().getLibraryCardsSet()) {
+	    if (card.getCardNumber().equals(code)) {
+		return card;
+	    }
+	}
+	return null;
     }
 }
