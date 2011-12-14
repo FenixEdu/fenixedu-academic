01f72d22ebd10907f8194126d20ed8e2dd11e37c
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2011-12-14 11:39:43 +0000

    Refactor:
    - ObjectKey of Fenix Web Framework is used to index slot in MetaObject with changes represented by ObjectChange. ObjectKey includes the Type of MetaSlot in order to create an object when fr:create is used.
    
    - FenixMetaObjectFactory supports CreationDomainMetaObject (for fr:create)
    
    - ServicePredicateWithResult of DomainMetaObject is extended in CreationDomainMetaObject. CreationServicePredicateWithResult inserts in changes an ObjectChange to create domain object with an InstanceCreator. Besides supports the creation of new objects in getNewObject.
    
    - CreateObjects, ObjectChange, ObjectKey, UpdateObjects e CreatePermissionUnit were removed from Fénix because they are no longer necessary.
    
    - DomainObjectKeyConverter e DomainObjectKeyArrayConverter were removed from Fénix due to idInternal usage. Same classes of Fenix Web Framework read domain objects with OID. All providers were changed in order to use the latter converters. Also renderers must use ExternalId to work with these latter converters.
    
    - FenixMetaObjectFactory and package classes were deleted from Fénix. Although they supported fr:create, DomainMetaObjects used idInternal instead of oid. Esta factory was replaced, in struts configuration, by Fenix Web Framework.
    
    - The consequence of this replacement is the removal of choiceTypes by providers created in net.sourceforge.fenixedu.presentationTier.renderers.providers.choiceType.replacement.* package.
    
    - FenixEditObjectTag was removed from Fénix. FenixCreateObjectTag's service property was removed.
    
    - In services.xml CreateObjects, UpdateObjects and CreatePermissionUnit
    
    - In struts-default.xml metaObjectFactory, userIdentityFactory and fenixSchemaFactory were replaced by same factories of Fenix Web Framework
    
    - In schemas and jsp choiceType was replaced by providerClass
    
    - InputMenuOptionListRenderer of Fenix was replaced by Fenix Web Framework
    
    - EnumRadioInputRendererWithPostBack of Fenix was replaced by Fenix Web Framework

--- src/net/sourceforge/fenixedu/presentationTier/renderers/providers/accounting/events/ExecutionPeriodsForAccountingEventCreationProvider.java
+++ src/net/sourceforge/fenixedu/presentationTier/renderers/providers/accounting/events/ExecutionPeriodsForAccountingEventCreationProvider.java
@@ -3,7 +3,9 @@ package net.sourceforge.fenixedu.presentationTier.renderers.providers.accounting
 import java.util.Collections;
 
 import net.sourceforge.fenixedu.dataTransferObject.accounting.events.AccountingEventCreateBean;
-import net.sourceforge.fenixedu.presentationTier.renderers.converters.DomainObjectKeyConverter;
+
+import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyConverter;
+
 import pt.ist.fenixWebFramework.renderers.DataProvider;
 import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
 
