192c02ba2601e2956a6ab7a8ba360d889bd286a3
Luis Cruz <luis.cruz@ist.utl.pt>
2012-01-05 01:48:56 +0000

    Minor Change: Added missing resources to allow adist project interfaces to work.

---
+++ src/net/sourceforge/fenixedu/presentationTier/Action/projectsManagement/adistInstitucionalProjectsManagement/InstitucionalProjectManagerIndexActionForInstitucionalADISTProjectsManagement.java
@@ -0,0 +1,11 @@
+package net.sourceforge.fenixedu.presentationTier.Action.projectsManagement.adistInstitucionalProjectsManagement;
+
+import pt.ist.fenixWebFramework.struts.annotations.Forward;
+import pt.ist.fenixWebFramework.struts.annotations.Forwards;
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+import pt.ist.fenixWebFramework.struts.annotations.Tile;
+
+@Mapping(module = "adistInstitucionalProjectsManagement", path = "/adistInstitucionalProjectIndex", scope = "session")
+@Forwards(value = { @Forward(name = "success", path = "/projectsManagement/firstPage.jsp", tileProperties = @Tile(navLocal = "/projectsManagement/costCenterNavBar.jsp")) })
+public class InstitucionalProjectManagerIndexActionForInstitucionalADISTProjectsManagement extends net.sourceforge.fenixedu.presentationTier.Action.projectsManagement.InstitucionalProjectManagerIndexAction {
+}
