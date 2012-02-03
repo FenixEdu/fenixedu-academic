d3d825c46008001d353f457ca66d7fc1ee615701
Luis Cruz <luis.cruz@ist.utl.pt>
2012-02-03 19:24:40 +0000

    Minor Change: If user already has session sent him to home, not to authentication.

--- src/net/sourceforge/fenixedu/presentationTier/servlets/filters/CASFilter.java
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/CASFilter.java
@@ -8,20 +8,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sourceforge.fenixedu.presentationTier.Action.utils.RequestUtils;
 import pt.ist.fenixWebFramework.Config.CasConfig;
+import pt.ist.fenixWebFramework.security.UserView;
 
 public class CASFilter extends pt.ist.fenixWebFramework.servlets.filters.CASFilter {
 
     protected void redirectToCAS(final CasConfig casConfig, final HttpServletRequest request, final HttpServletResponse response)
 	    throws IOException, ServletException {
-
-	String pendingRequest = request.getParameter("pendingRequest");
-	if (pendingRequest == null) {
-	    pendingRequest = (String) request.getAttribute("pendingRequest");
+	if (UserView.getUser() == null) {
+	    String pendingRequest = request.getParameter("pendingRequest");
+	    if (pendingRequest == null) {
+		pendingRequest = (String) request.getAttribute("pendingRequest");
+	    }
+	    final String serviceString = encodeUrl(RequestUtils.generateRedirectLink(casConfig.getServiceUrl(), pendingRequest));
+	    final String casLoginUrl = casConfig.getCasLoginUrl();
+	    final String casLoginString = casLoginUrl + "?service=" + serviceString;
+	    response.sendRedirect(casLoginString);
+	} else {
+	    response.sendRedirect(request.getContextPath() + "/home.do");
 	}
-	final String serviceString = encodeUrl(RequestUtils.generateRedirectLink(casConfig.getServiceUrl(), pendingRequest));
-	final String casLoginUrl = casConfig.getCasLoginUrl();
-	final String casLoginString = casLoginUrl + "?service=" + serviceString;
-	response.sendRedirect(casLoginString);
     }
 
 }
