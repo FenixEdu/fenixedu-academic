cc6ab880193cbbf1cd007a7f7941552c79c4106b
Luis Cruz <luis.cruz@ist.utl.pt>
2012-02-09 15:11:29 +0000

    Minor Change: Added a new category.

--- src/net/sourceforge/fenixedu/domain/messaging/AnnouncementCategoryType.java
+++ src/net/sourceforge/fenixedu/domain/messaging/AnnouncementCategoryType.java
@@ -6,7 +6,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.Predicate;
 
 public enum AnnouncementCategoryType {
-    COMPETITION, CONFERENCE, CONGRESS, CULTURAL_EVENT, SPORTS, TRIBUTE, LECTURE, SEMINAR, AGREEMENTS_CONTRACTS, APPLICATIONS, FUNDING, INFORMATION, AWARD, PROJECTS, SPECIAL_EDITIONS;
+    COMPETITION, CONFERENCE, CONGRESS, CULTURAL_EVENT, SPORTS, TRIBUTE, LECTURE, SEMINAR, AGREEMENTS_CONTRACTS, APPLICATIONS, FUNDING, INFORMATION, AWARD, PROJECTS, SPECIAL_EDITIONS, NEWSLETTER;
 
     public static AnnouncementCategory getAnnouncementCategoryByType(final AnnouncementCategoryType type) {
 	return (AnnouncementCategory) CollectionUtils.find(RootDomainObject.readAllDomainObjects(AnnouncementCategory.class),
