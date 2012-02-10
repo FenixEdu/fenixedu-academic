c140340c3327b58c3b4cd2aa3da9898fe84343f7
Luis Cruz <luis.cruz@ist.utl.pt>
2012-02-10 17:57:32 +0000

    Bug Fix: Avoid NPE.

--- src/net/sourceforge/fenixedu/domain/space/Space.java
+++ src/net/sourceforge/fenixedu/domain/space/Space.java
@@ -481,7 +481,8 @@ public abstract class Space extends Space_Base {
     public boolean isActive() {
 	// System.out.println("SPACE INFO: " + getMostRecentSpaceInformation() +
 	// " for " + new YearMonthDay());
-	return getMostRecentSpaceInformation().isActive(new YearMonthDay());
+	final SpaceInformation mostRecentSpaceInformation = getMostRecentSpaceInformation();
+	return mostRecentSpaceInformation != null && mostRecentSpaceInformation.isActive(new YearMonthDay());
     }
 
     public Boolean getActiveFlag() {
