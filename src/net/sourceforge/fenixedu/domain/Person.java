90cfceaea2956ef1b686c79aff8e6f40eaad007a
Luis Cruz <luis.cruz@ist.utl.pt>
2012-03-13 19:54:23 +0000

    Bug Fix: Resolved compile issue.

--- src/net/sourceforge/fenixedu/domain/Person.java
+++ src/net/sourceforge/fenixedu/domain/Person.java
@@ -4018,7 +4018,7 @@ public class Person extends Person_Base {
 			    result.append(':');
 			    result.append(costCenterCode);
 			    result.append(':');
-			    result.append('IST');
+			    result.append("IST");
 			}
 		    }
 		}
