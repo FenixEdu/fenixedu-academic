7fe5b5703ebbc18dcf6d27114a8db64709df514c
Artur Ventura <artur.ventura@ist.utl.pt>
2012-03-30 16:34:31 +0000

    Bug Fix: Removing unintended commited file.

--- etc/database_operations/updates.sql
+++
@@ -1,9 +0,0 @@
-drop table CATEGORY;
-drop table PROFESSIONAL_SITUATION;
-alter table `TEACHER` drop TEACHER_NUMBER;
-alter table `FILE` drop key OID_PHD_PARTICIPANT, drop OID_PHD_PARTICIPANT;
-alter table `PARTY` drop AVAILABLE_PHOTO;
-alter table `T_S_D_TEACHER` drop key OID_CATEGORY, drop OID_CATEGORY;
-alter table `CAREER` drop key OID_CATEGORY, drop OID_CATEGORY;
-alter table `PHD_GRATUITY_PAYMENT_PERIOD` drop MONTH_END, drop DAY_LAST_PAYMENT, drop DAY_START, drop MONTH_START, drop MONTH_LAST_PAYMENT, drop DAY_END;
-alter table `EMPLOYEE` drop ACTIVE;
