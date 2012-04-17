342849e988fda17163627ade0aa664763d71879d
Luis Cruz <luis.cruz@ist.utl.pt>
2012-04-17 10:00:55 +0000

    Refactor: Updated to reflect new setup process. Changes made possible by Luis Egídio.

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
-mysqladmin -uroot drop $1
-mysqladmin -uroot create $1
-mysql -uroot $1 < etc/dml.sql
-mysql -uroot $1 < etc/ojb.sql
-ant -f build_dataBaseUtils.xml generate-fenix-sql
-mysql -uroot $1 < etc/fenix.sql
-ant -f build_dataBaseUtils.xml initialize-database
-echo "update IDENTIFICATION, LOGIN_ALIAS set IDENTIFICATION.PASSWORD = md5('pass') where IDENTIFICATION.ID_INTERNAL = LOGIN_ALIAS.KEY_LOGIN and LOGIN_ALIAS.ALIAS = 'admin';" > .tmp.init
-mysql -uroot $1 < .tmp.init
-rm -rf .tmp.init
-wget --no-check-certificate https://fenix-ashes.ist.utl.pt/FenixSetup\?action=AttachFile\&do=get\&target=content-structure.sql
+mysqladmin -uroot -p<password> drop $1
+mysqladmin -uroot -p<password> create $1
+
+ant clean-all
+ant -f build_dataBaseUtils.xml pre-init-data-structure
+
+wget --no-check-certificate https://fenix-ashes.ist.utl.pt/fenixWiki/FenixSetup\?action=AttachFile\&do=get\&target=content-structure.sql
 mv FenixSetup\?action=AttachFile\&do=get\&target=content-structure.sql content-structure.sql
-mysql -uroot -f $1 < content-structure.sql > /dev/null
+mysql -uroot -p<password> -f $1 < content-structure.sql 
 rm -rf content-structure.sql
+
+ant -f build_dataBaseUtils.xml initialize-database
+echo "update IDENTIFICATION, LOGIN_ALIAS set IDENTIFICATION.PASSWORD = md5('pass') where IDENTIFICATION.OID = LOGIN_ALIAS.OID_LOGIN and LOGIN_ALIAS.ALIAS = 'admin';" > .tmp.init
+mysql -uroot -p<password> $1 < .tmp.init
+rm -rf .tmp.init
