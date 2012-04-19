c5bd988aada284b2c076eaf74b04684c7e4c1b22
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2012-04-19 15:30:03 +0000

    Bug Fix: On Master degree candidacies visualization by coordinator

--- src/net/sourceforge/fenixedu/presentationTier/Action/coordinator/candidacy/secondCycle/SecondCycleIndividualCandidacyProcessDA.java
+++ src/net/sourceforge/fenixedu/presentationTier/Action/coordinator/candidacy/secondCycle/SecondCycleIndividualCandidacyProcessDA.java
@@ -1,6 +1,15 @@
 package net.sourceforge.fenixedu.presentationTier.Action.coordinator.candidacy.secondCycle;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.fenixedu.presentationTier.Action.masterDegree.coordinator.CoordinatedDegreeInfo;
 import net.sourceforge.fenixedu.presentationTier.formbeans.FenixActionForm;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
 import pt.ist.fenixWebFramework.struts.annotations.Forward;
 import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
@@ -11,4 +20,10 @@ import pt.ist.fenixWebFramework.struts.annotations.Mapping;
 	@Forward(name = "list-allowed-activities", path = "/candidacy/secondCycle/listIndividualCandidacyActivities.jsp") })
 public class SecondCycleIndividualCandidacyProcessDA extends net.sourceforge.fenixedu.presentationTier.Action.candidacy.secondCycle.SecondCycleIndividualCandidacyProcessDA {
 
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+	    HttpServletResponse response) throws Exception {
+	CoordinatedDegreeInfo.setCoordinatorContext(request);
+	return super.execute(mapping, actionForm, request, response);
+    }
 }
