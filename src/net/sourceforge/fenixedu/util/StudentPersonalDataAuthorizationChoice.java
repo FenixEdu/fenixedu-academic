65e50cabe0ebe282e27a797b2fbc338aa2f97c54
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2012-09-07 19:32:35 +0000

    Minor change: - added a question about sharing private data for the students association, in the 1rst year process

--- src/net/sourceforge/fenixedu/util/StudentPersonalDataAuthorizationChoice.java
+++ src/net/sourceforge/fenixedu/util/StudentPersonalDataAuthorizationChoice.java
@@ -4,6 +4,8 @@
  */
 package net.sourceforge.fenixedu.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import pt.utl.ist.fenix.tools.util.i18n.Language;
@@ -14,15 +16,23 @@ import pt.utl.ist.fenix.tools.util.i18n.Language;
  */
 public enum StudentPersonalDataAuthorizationChoice {
 
-    PROFESSIONAL_ENDS, /*
-		        * only to professional ends (job propositions, scholarships, internship, etc)
-		        */
-    SEVERAL_ENDS, /*
-		   * several non comercial ends (biographic, recreational, cultural, etc)
-		   */
-    ALL_ENDS, /* all ends, including comercial ones */
+    PROFESSIONAL_ENDS(false), /*
+			       * only to professional ends (job propositions, scholarships, internship, etc)
+			       */
+    SEVERAL_ENDS(false), /*
+			  * several non comercial ends (biographic, recreational, cultural, etc)
+			  */
+    ALL_ENDS(false), /* all ends, including comercial ones */
 
-    NO_END; /* doesn't authorize the use of the data */
+    NO_END(false), /* doesn't authorize the use of the data */
+
+    STUDENTS_ASSOCIATION(true); /* allows for the student association */
+
+    private boolean forStudentsAssociation;
+
+    private StudentPersonalDataAuthorizationChoice(boolean forStudentsAssociation) {
+	setForStudentsAssociation(forStudentsAssociation);
+    }
 
     public String getName() {
 	return name();
@@ -40,5 +50,33 @@ public enum StudentPersonalDataAuthorizationChoice {
 	final ResourceBundle ENUMERATION_RESOURCES = ResourceBundle.getBundle("resources.EnumerationResources", Language
 		.getLocale());
 	return ENUMERATION_RESOURCES.getString(getQualifiedName());
+    }
+
+    public static List<StudentPersonalDataAuthorizationChoice> getGeneralPersonalDataAuthorizationsTypes() {
+	List<StudentPersonalDataAuthorizationChoice> authorizationsTypes = new ArrayList<StudentPersonalDataAuthorizationChoice>();
+	StudentPersonalDataAuthorizationChoice[] values = StudentPersonalDataAuthorizationChoice.values();
+	for (StudentPersonalDataAuthorizationChoice studentPersonalDataAuthorizationChoice : values) {
+	    if (!studentPersonalDataAuthorizationChoice.isForStudentsAssociation()) {
+		authorizationsTypes.add(studentPersonalDataAuthorizationChoice);
+	    }
+	}
+	return authorizationsTypes;
+    }
+
+    public static StudentPersonalDataAuthorizationChoice getPersonalDataAuthorizationForStudentsAssociationType(
+	    boolean allowsForStudentAssociation) {
+	if (allowsForStudentAssociation) {
+	    return STUDENTS_ASSOCIATION;
+	} else {
+	    return NO_END;
+	}
+    }
+
+    public void setForStudentsAssociation(boolean forStudentsAssociation) {
+	this.forStudentsAssociation = forStudentsAssociation;
+    }
+
+    public boolean isForStudentsAssociation() {
+	return forStudentsAssociation;
     }
 }
