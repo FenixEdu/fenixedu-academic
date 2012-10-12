aa67d8115eadfc89d59a203233262114191e78db
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2012-10-12 14:18:49 +0000

    New Feature: using sentry to watch uncaught exceptions

--- src/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
@@ -8,12 +8,23 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import net.kencochrane.sentry.RavenClient;
+import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.dataTransferObject.support.SupportRequestBean;
 import net.sourceforge.fenixedu.domain.functionalities.AbstractFunctionalityContext;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.util.ExceptionInformation;
 
 public class UncaughtExceptionFilter implements Filter {
+    private RavenClient sentry = null;
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+	String sentryUrl = PropertiesManager.getProperty("sentry.url");
+	if (sentryUrl != null) {
+	    sentry = new RavenClient(sentryUrl);
+	}
+    }
 
     @Override
     public void destroy() {
@@ -27,7 +38,6 @@ public class UncaughtExceptionFilter implements Filter {
 	    filterChain.doFilter(request, response);
 
 	} catch (Throwable e) {
-
 	    if (request.getAttribute("requestBean") == null) {
 
 		SupportRequestBean requestBean = SupportRequestBean.generateExceptionBean(AccessControl.getPerson());
@@ -37,17 +47,19 @@ public class UncaughtExceptionFilter implements Filter {
 			    .getSelectedTopLevelContainer());
 		}
 		request.setAttribute("requestBean", requestBean);
-		request.setAttribute("exceptionInfo", ExceptionInformation.buildUncaughtExceptionInfo(
-			(HttpServletRequest) request, e));
+		request.setAttribute("exceptionInfo",
+			ExceptionInformation.buildUncaughtExceptionInfo((HttpServletRequest) request, e));
 	    }
 
+	    if (sentry != null) {
+		try {
+		    sentry.captureException(e);
+		} catch (Throwable t) {
+		    // no problem
+		}
+	    }
 	    e.printStackTrace();
 	    throw new RuntimeException(e);
 	}
     }
-
-    @Override
-    public void init(FilterConfig arg0) throws ServletException {
-    }
-
 }
