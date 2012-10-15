77ad6d0ce05ada14a8e1a9f1a5aa8de7baf2cccb
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2012-10-15 10:03:06 +0000

    Bug Fix: removed logged user from raven info

--- src/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
+++ src/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
@@ -15,7 +15,6 @@ import net.sourceforge.fenixedu.dataTransferObject.support.SupportRequestBean;
 import net.sourceforge.fenixedu.domain.functionalities.AbstractFunctionalityContext;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.util.ExceptionInformation;
-import pt.ist.fenixWebFramework.security.UserView;
 
 public class UncaughtExceptionFilter implements Filter {
     private RavenClient sentry = null;
@@ -56,8 +55,7 @@ public class UncaughtExceptionFilter implements Filter {
 
 	    if (sentry != null) {
 		try {
-		    String culprit = UserView.hasUser() ? UserView.getUser().getUsername() : null;
-		    sentry.captureException("" + e.getMessage(), RavenUtils.getTimestampLong(), "root", 50, culprit, e);
+		    sentry.captureException("" + e.getMessage(), RavenUtils.getTimestampLong(), "root", 50, null, e);
 		} catch (Throwable t) {
 		    t.printStackTrace();
 		}
