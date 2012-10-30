6ec48dda40bc0e77648a5e3ff757f0faf434c4a0
Ricardo Rodrigues <ricardo.rodrigues@ist.utl.pt>
2012-10-30 14:50:05 +0000

    Minor change: - added missing instruction

--- etc/database_operations/updateInquiryStudentCycleAnswerHierarchy.sql
+++ etc/database_operations/updateInquiryStudentCycleAnswerHierarchy.sql
@@ -3,3 +3,4 @@ update FF$DOMAIN_CLASS_INFO set DOMAIN_CLASS_NAME = "net.sourceforge.fenixedu.do
 update INQUIRY_ANSWER set OJB_CONCRETE_CLASS = "net.sourceforge.fenixedu.domain.inquiries.InquiryStudentCycleAnswer" where OJB_CONCRETE_CLASS like "%InquiryStudent1rstCycleAnswer%";
 
 alter table `INQUIRY_ANSWER` add `OID_PHD_PROCESS` bigint unsigned;
+alter table `PROCESS` add `OID_INQUIRY_STUDENT_CYCLE_ANSWER` bigint unsigned;
