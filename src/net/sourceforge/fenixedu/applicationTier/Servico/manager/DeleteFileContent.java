9a987ed966a1cb4d0f2ac636a2f8aa3b53e98bbd
Luis Cruz <luis.cruz@ist.utl.pt>
2012-11-02 18:09:24 +0000

    Bug Fix: Only do this if file is not local. This is already done in a files delete method.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteFileContent.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/manager/DeleteFileContent.java
@@ -2,10 +2,8 @@ package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
 import net.sourceforge.fenixedu.applicationTier.FenixService;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.domain.DeleteFileRequest;
 import net.sourceforge.fenixedu.domain.FileContent;
 import net.sourceforge.fenixedu.domain.exceptions.DomainException;
-import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import pt.utl.ist.fenix.tools.file.FileManagerException;
 
 /**
@@ -15,6 +13,5 @@ public class DeleteFileContent extends FenixService {
 
     public void run(FileContent fileContent) throws FenixServiceException, DomainException, FileManagerException {
 	fileContent.delete();
-	new DeleteFileRequest(AccessControl.getPerson(), fileContent.getExternalStorageIdentification());
     }
 }
