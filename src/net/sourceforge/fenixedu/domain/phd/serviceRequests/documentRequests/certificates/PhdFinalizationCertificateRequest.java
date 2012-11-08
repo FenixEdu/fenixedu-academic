692486107bef3f4ea0377991a42a0fb05c193a45
Luis Cruz <luis.cruz@ist.utl.pt>
2012-11-08 00:24:30 +0000

    Bug Fix: Avoid NullPointerException and inform user of what went wrong with the operation.

--- src/net/sourceforge/fenixedu/domain/phd/serviceRequests/documentRequests/certificates/PhdFinalizationCertificateRequest.java
+++ src/net/sourceforge/fenixedu/domain/phd/serviceRequests/documentRequests/certificates/PhdFinalizationCertificateRequest.java
@@ -34,6 +34,11 @@ public class PhdFinalizationCertificateRequest extends PhdFinalizationCertificat
 
 	PhdRegistryDiplomaRequest registryDiplomaRequest = getPhdIndividualProgramProcess().getRegistryDiplomaRequest();
 
+	if (registryDiplomaRequest == null) {
+	    throw new PhdDomainOperationException(
+		    "error.PhdFinalizationCertificateRequest.registry.diploma.request.none");		
+	}
+
 	RectorateSubmissionBatch rectorateSubmissionBatch = registryDiplomaRequest.getRectorateSubmissionBatch();
 
 	if (rectorateSubmissionBatch == null) {
