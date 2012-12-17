c8b21ec92103bffa80883c4a42f7aae40f749c03
Luis Cruz <luis.cruz@ist.utl.pt>
2012-12-17 14:05:39 +0000

    Bug Fix: Use file API only to delete files. The file delete method has to do everything internally.

--- src/net/sourceforge/fenixedu/applicationTier/Servico/research/DeleteUnitFile.java
+++ src/net/sourceforge/fenixedu/applicationTier/Servico/research/DeleteUnitFile.java
@@ -1,17 +1,13 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.research;
 
 import net.sourceforge.fenixedu.applicationTier.FenixService;
-import net.sourceforge.fenixedu.domain.DeleteFileRequest;
 import net.sourceforge.fenixedu.domain.UnitFile;
-import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import pt.ist.fenixWebFramework.services.Service;
 
 public class DeleteUnitFile extends FenixService {
 
     @Service
-    public static void run(UnitFile file) {
-	String uniqueID = file.getExternalStorageIdentification();
+    public static void run(final UnitFile file) {
 	file.delete();
-	new DeleteFileRequest(AccessControl.getPerson(), uniqueID);
     }
 }
