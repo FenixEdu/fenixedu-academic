def28863990cc75a27a13dc79ba1b96c8d4aaeb6
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2013-04-01 15:00:37 +0000

    Bug Fix: proper sorting of evaluation processes

--- src/main/java/net/sourceforge/fenixedu/domain/teacher/evaluation/TeacherEvaluationProcess.java
+++ src/main/java/net/sourceforge/fenixedu/domain/teacher/evaluation/TeacherEvaluationProcess.java
@@ -26,7 +26,7 @@ public class TeacherEvaluationProcess extends TeacherEvaluationProcess_Base {
     public static Comparator<TeacherEvaluationProcess> COMPARATOR_BY_INTERVAL = new Comparator<TeacherEvaluationProcess>() {
         @Override
         public int compare(TeacherEvaluationProcess p1, TeacherEvaluationProcess p2) {
-            return FacultyEvaluationProcess.COMPARATOR_BY_INTERVAL.compare(p1.getFacultyEvaluationProcess(),
+            return -FacultyEvaluationProcess.COMPARATOR_BY_INTERVAL.compare(p1.getFacultyEvaluationProcess(),
                     p2.getFacultyEvaluationProcess());
         }
     };
