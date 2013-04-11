d79ee9245b0460b247e7c6d5b0c2d4693481b0c7
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2013-04-11 09:10:54 +0000

    Minor Change: removed sentry. jodaplace depends on release version (no changes in codebase)

--- src/main/java/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/servlets/filters/UncaughtExceptionFilter.java
@@ -8,52 +8,12 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import net.kencochrane.sentry.RavenClient;
-import net.kencochrane.sentry.RavenUtils;
-import net.sourceforge.fenixedu._development.PropertiesManager;
 import net.sourceforge.fenixedu.dataTransferObject.support.SupportRequestBean;
 import net.sourceforge.fenixedu.domain.functionalities.AbstractFunctionalityContext;
 import net.sourceforge.fenixedu.injectionCode.AccessControl;
 import net.sourceforge.fenixedu.presentationTier.util.ExceptionInformation;
 
 public class UncaughtExceptionFilter implements Filter {
-    public class RavenCommunicator extends Thread {
-        private final Throwable exception;
-
-        public RavenCommunicator(Throwable exception) {
-            this.exception = exception;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Throwable e = unwrap(exception);
-                sentry.captureException("" + e.getMessage(), RavenUtils.getTimestampLong(), "root", 50, null, e);
-            } catch (Throwable t) {
-                t.printStackTrace();
-            }
-        }
-
-        private Throwable unwrap(Throwable exception) {
-            if (exception.getCause() != null) {
-                if (exception instanceof ServletException) {
-                    return unwrap(exception.getCause());
-                }
-            }
-            return exception;
-        }
-    }
-
-    private static RavenClient sentry = null;
-
-    @Override
-    public void init(FilterConfig arg0) throws ServletException {
-        String sentryUrl = PropertiesManager.getProperty("sentry.url");
-        if (sentryUrl != null) {
-            System.out.println("Turning on Sentry: " + sentryUrl);
-            sentry = new RavenClient(sentryUrl);
-        }
-    }
 
     @Override
     public void destroy() {
@@ -67,6 +27,7 @@ public class UncaughtExceptionFilter implements Filter {
             filterChain.doFilter(request, response);
 
         } catch (Throwable e) {
+
             if (request.getAttribute("requestBean") == null) {
 
                 SupportRequestBean requestBean = SupportRequestBean.generateExceptionBean(AccessControl.getPerson());
@@ -81,14 +42,12 @@ public class UncaughtExceptionFilter implements Filter {
             }
 
             e.printStackTrace();
-            if (sentry != null) {
-                try {
-                    new RavenCommunicator(e).start();
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
-            }
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public void init(FilterConfig arg0) throws ServletException {
+    }
+
 }
