8c84158d8179cb65b2643e0d357ec5de26a6abb1
André Costa <andre.v.m.costa@ist.utl.pt>
2013-05-08 18:46:49 +0000

    [BugFix][Migration] Some error occurences when deleting a CalendarEntry.

--- src/main/java/net/sourceforge/fenixedu/domain/ExecutionSemester.java
+++ src/main/java/net/sourceforge/fenixedu/domain/ExecutionSemester.java
@@ -481,6 +481,7 @@ public class ExecutionSemester extends ExecutionSemester_Base implements Compara
             throw new Error("cannot.delete.execution.period.because.enrolments.exist");
         }
         super.setExecutionYear(null);
+        removeRootDomainObjectForExecutionPeriod();
         removeRootDomainObject();
         deleteDomainObject();
     }
