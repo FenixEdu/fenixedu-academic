1c17b76ab03d0fd18682e8034175378878048543
André Costa <andre.v.m.costa@ist.utl.pt>
2013-05-16 15:22:45 +0000

    [BugFix][PreMigration] Execution Course managing for Manager: Insert Execution Course: bug fixes and more information display.

--- src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/manager/InsertExecutionCourseAtExecutionPeriod.java
+++ src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/manager/InsertExecutionCourseAtExecutionPeriod.java
@@ -3,36 +3,37 @@
  */
 package net.sourceforge.fenixedu.applicationTier.Servico.manager;
 
-import net.sourceforge.fenixedu.applicationTier.FenixService;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.ExistingServiceException;
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.FenixServiceException;
-import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.NonExistingServiceException;
 import net.sourceforge.fenixedu.dataTransferObject.InfoExecutionCourseEditor;
 import net.sourceforge.fenixedu.domain.ExecutionCourse;
 import net.sourceforge.fenixedu.domain.ExecutionSemester;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
 import pt.ist.fenixWebFramework.security.accessControl.Checked;
 import pt.ist.fenixWebFramework.services.Service;
 
 /**
  * @author lmac1 modified by Fernanda Quitério
  */
-public class InsertExecutionCourseAtExecutionPeriod extends FenixService {
+public class InsertExecutionCourseAtExecutionPeriod {
 
     @Checked("RolePredicates.MANAGER_OR_OPERATOR_PREDICATE")
     @Service
     public static void run(InfoExecutionCourseEditor infoExecutionCourse) throws FenixServiceException {
 
         final ExecutionSemester executionSemester =
-                rootDomainObject.readExecutionSemesterByOID(infoExecutionCourse.getInfoExecutionPeriod().getIdInternal());
+                RootDomainObject.getInstance().readExecutionSemesterByOID(
+                        infoExecutionCourse.getInfoExecutionPeriod().getIdInternal());
         if (executionSemester == null) {
-            throw new NonExistingServiceException("message.nonExistingExecutionPeriod", null);
+            throw new DomainException("message.nonExistingExecutionPeriod");
         }
 
         final ExecutionCourse existentExecutionCourse =
                 executionSemester.getExecutionCourseByInitials(infoExecutionCourse.getSigla());
         if (existentExecutionCourse != null) {
-            throw new ExistingServiceException("A disciplina execução com sigla " + existentExecutionCourse.getSigla()
-                    + " e período execução " + executionSemester.getName() + "-" + executionSemester.getExecutionYear().getYear());
+            throw new DomainException("error.manager.executionCourseManagement.acronym.exists",
+                    existentExecutionCourse.getSigla(), executionSemester.getName(), executionSemester.getExecutionYear()
+                            .getYear(), existentExecutionCourse.getName());
         }
 
         final ExecutionCourse executionCourse =
--- src/main/webapp/manager/executionCourseManagement/welcomeScreen.jsp
+++ src/main/webapp/manager/executionCourseManagement/welcomeScreen.jsp
@@ -1,8 +1,8 @@
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %><html:xhtml/>
-<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean"%>
+<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean"%><%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic"%>
 
 <h2><bean:message bundle="MANAGER_RESOURCES"  key="label.manager.executionCourseManagement"/></h2>
-
+<logic:messagesPresent message="true" property="success">	<p>		<span class="success0">			<html:messages id="messages" message="true" bundle="MANAGER_RESOURCES" property="success">				<bean:write name="messages" />			</html:messages>		</span>	</p></logic:messagesPresent><logic:messagesPresent message="true" property="error">	<p>		<span class="error0">			<html:messages id="messages" message="true" bundle="MANAGER_RESOURCES" property="error">				<bean:write name="messages" />			</html:messages>		</span>	</p></logic:messagesPresent>
 <table>
 	<tr>
 		<td class="infoop">
