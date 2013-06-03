0aad95fc51dca536e994486318f433823df261db
Paulo <buulseye@gmail.com>
2013-06-03 17:21:09 +0100

    Minor Change: [Mobility]: edit provider

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/candidacy/erasmus/DegreesForExecutionYearProviderForMobilityIndividualApplicationProcess.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/candidacy/erasmus/DegreesForExecutionYearProviderForMobilityIndividualApplicationProcess.java
@@ -7,8 +7,6 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import net.sourceforge.fenixedu.domain.Degree;
-import net.sourceforge.fenixedu.domain.ExecutionDegree;
-import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.candidacyProcess.mobility.MobilityIndividualApplicationProcessBean;
 import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyConverter;
 import pt.ist.fenixWebFramework.renderers.DataProvider;
@@ -23,45 +21,8 @@ public class DegreesForExecutionYearProviderForMobilityIndividualApplicationProc
         final MobilityIndividualApplicationProcessBean chooseDegreeBean = (MobilityIndividualApplicationProcessBean) source;
 
         result.addAll(chooseDegreeBean.getPossibleDegreesFromSelectedUniversity());
-        /*    for (final Degree degree : Degree.readAllByDegreeType(DegreeType.BOLONHA_INTEGRATED_MASTER_DEGREE,
-                    DegreeType.BOLONHA_MASTER_DEGREE, DegreeType.BOLONHA_DEGREE)) {
-
-                if (degree.getSigla().equals("MSCIT")) {
-                    continue;
-                }
-
-                if (degree.getSigla().equals("MCR")) {
-                    continue;
-                }
-
-                if (degree.getSigla().equals("MEEst")) {
-                    continue;
-                }
-
-                if (degree.getSigla().equals("MMM")) {
-                    continue;
-                }
-
-                if (degree.getSigla().equals("MEFarm")) {
-                    continue;
-                }
-
-                if (matchesExecutionYear(degree, chooseDegreeBean.getExecutionYear())) {
-                    result.add(degree);
-                }
-            }*/
 
         return result;
-    }
-
-    private boolean matchesExecutionYear(Degree degree, ExecutionYear executionYear) {
-        for (final ExecutionDegree executionDegree : executionYear.getExecutionDegrees()) {
-            if (executionDegree.getDegree() == degree) {
-                return true;
-            }
-        }
-
-        return false;
     }
 
     @Override
