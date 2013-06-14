e823f6c6fb8d6c5d7e4e4401bc289fcc722c5ca2
ricardorcr <ricardo.rcr@gmail.com>
2013-06-14 21:30:50 +0100

    New feature: #377096 - added the possibility to see the tutors by year and degree

---
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/pedagogicalCouncil/ViewTutorsDA.java
@@ -0,0 +1,129 @@
+package net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sourceforge.fenixedu.domain.ExecutionDegree;
+import net.sourceforge.fenixedu.domain.ExecutionSemester;
+import net.sourceforge.fenixedu.domain.ExecutionYear;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.Teacher;
+import net.sourceforge.fenixedu.domain.degree.DegreeType;
+import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
+
+import org.apache.commons.collections.comparators.ReverseComparator;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import pt.ist.fenixWebFramework.rendererExtensions.converters.DomainObjectKeyConverter;
+import pt.ist.fenixWebFramework.renderers.DataProvider;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+import pt.ist.fenixWebFramework.struts.annotations.Forward;
+import pt.ist.fenixWebFramework.struts.annotations.Forwards;
+import pt.ist.fenixWebFramework.struts.annotations.Mapping;
+
+@Mapping(path = "/viewTutors", module = "pedagogicalCouncil")
+@Forwards({ @Forward(name = "viewTutors", path = "/pedagogicalCouncil/tutorship/viewTutors.jsp") })
+public class ViewTutorsDA extends FenixDispatchAction {
+
+    private static final Integer SEMESTER = 1;
+
+    public ActionForward listTutors(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        ViewTutorsBean bean = getRenderedObject("tutorsBean");
+        if (bean == null) {
+            bean = new ViewTutorsBean();
+        }
+        request.setAttribute("tutorsBean", bean);
+        RenderUtils.invalidateViewState();
+        return mapping.findForward("viewTutors");
+    }
+
+    public static class ContextDegreesProvider implements DataProvider {
+
+        @Override
+        public Converter getConverter() {
+            return new DomainObjectKeyConverter();
+        }
+
+        @Override
+        public Object provide(Object source, Object arg1) {
+            final List<ExecutionDegree> executionDegrees = new ArrayList<ExecutionDegree>();
+            final ViewTutorsBean bean = (ViewTutorsBean) source;
+            final ExecutionSemester executionPeriod = bean.getExecutionSemester();
+            if (executionPeriod != null) {
+                final ExecutionYear executionYear = executionPeriod.getExecutionYear();
+                for (ExecutionDegree executionDegree : executionYear.getExecutionDegreesSet()) {
+                    DegreeType degreeType = executionDegree.getDegreeType();
+                    if (degreeType == DegreeType.BOLONHA_INTEGRATED_MASTER_DEGREE || degreeType == DegreeType.BOLONHA_DEGREE) {
+                        executionDegrees.add(executionDegree);
+                    }
+                }
+            }
+            Collections.sort(executionDegrees, ExecutionDegree.EXECUTION_DEGREE_COMPARATORY_BY_DEGREE_TYPE_AND_NAME);
+            return executionDegrees;
+        }
+    }
+
+    public static class ExecutionSemestersProvider implements DataProvider {
+
+        @Override
+        public Object provide(Object source, Object currentValue) {
+            List<ExecutionSemester> executionSemesters = new ArrayList<ExecutionSemester>();
+            for (ExecutionSemester executionSemester : RootDomainObject.getInstance().getExecutionPeriods()) {
+                if (executionSemester.isFor(SEMESTER)) {
+                    executionSemesters.add(executionSemester);
+                }
+            }
+            Collections.sort(executionSemesters, new ReverseComparator());
+            return executionSemesters;
+        }
+
+        @Override
+        public Converter getConverter() {
+            return new DomainObjectKeyConverter();
+        }
+    }
+
+    public static class ViewTutorsBean implements Serializable {
+
+        private ExecutionSemester executionSemester;
+        private ExecutionDegree executionDegree;
+
+        public List<Person> getTutors() {
+            List<Person> tutors = new ArrayList<Person>();
+            if (getExecutionDegree() != null) {
+                for (final Teacher teacher : getExecutionDegree().getPossibleTutorsFromExecutionDegreeDepartments()) {
+                    if (teacher.hasTutorshipIntentionFor(getExecutionDegree())) {
+                        tutors.add(teacher.getPerson());
+                    }
+                }
+            }
+            return tutors;
+        }
+
+        public ExecutionSemester getExecutionSemester() {
+            return executionSemester;
+        }
+
+        public void setExecutionSemester(ExecutionSemester executionSemester) {
+            this.executionSemester = executionSemester;
+        }
+
+        public ExecutionDegree getExecutionDegree() {
+            return executionDegree;
+        }
+
+        public void setExecutionDegree(ExecutionDegree executionDegree) {
+            this.executionDegree = executionDegree;
+        }
+    }
+}
---
+++ src/main/webapp/pedagogicalCouncil/tutorship/viewTutors.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" %>
+<%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean" %>
+<%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
+<%@ taglib uri="/WEB-INF/struts-logic.tld" prefix="logic" %>
+<%@ taglib uri="/WEB-INF/fenix-renderers.tld" prefix="fr" %>
+
+
+<em><bean:message key="pedagogical.council" bundle="PEDAGOGICAL_COUNCIL" /></em>
+<h2><bean:message key="title.tutorship.view" bundle="PEDAGOGICAL_COUNCIL" /></h2>
+
+<fr:edit id="tutorsBean" name="tutorsBean" action="/viewTutors.do?method=listTutors">
+	<fr:schema type="net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil.ViewTutorsDA$ViewTutorsBean" bundle="PEDAGOGICAL_COUNCIL">
+		<fr:slot name="executionSemester" layout="menu-select-postback" key="label.tutorship.year">
+			<fr:property name="providerClass"
+				value="net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil.ViewTutorsDA$ExecutionSemestersProvider" />
+			<fr:property name="bundle" value="PEDAGOGICAL_COUNCIL" />
+			<fr:property name="format" value="${executionYear.year}" />
+			<fr:property name="destination" value="semesterPostBack" />
+		</fr:slot>
+		<fr:slot name="executionDegree" layout="menu-select" key="label.degree">
+			<fr:property name="providerClass"
+				value="net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil.ViewTutorsDA$ContextDegreesProvider"/>
+			<fr:property name="bundle" value="PEDAGOGICAL_COUNCIL" />
+			<fr:property name="format" value="${presentationName}" />
+			<fr:property name="sortBy" value="presentationName" />
+		</fr:slot>
+	</fr:schema>
+	<fr:layout>
+		<fr:property name="classes" value="tstyle5 thlight thleft mtop0" />
+		<fr:property name="columnClasses" value=",,tdclear tderror1" />
+	</fr:layout>
+	<fr:destination name="semesterPostBack" path="/viewTutors.do?method=listTutors"/>
+</fr:edit>
+
+
+<logic:notEmpty name="tutorsBean" property="tutors">		
+	<fr:view name="tutorsBean" property="tutors">
+		<fr:schema type="net.sourceforge.fenixedu.domain.Person" bundle="APPLICATION_RESOURCES">
+			<fr:slot name="istUsername" key="label.istUsername"/>
+			<fr:slot name="name" key="label.name"/>
+		</fr:schema>
+		<fr:layout name="tabular">
+			<fr:property name="classes" value="tstyle1" />
+		</fr:layout>
+	</fr:view>
+</logic:notEmpty>	
+
+<logic:empty name="tutorsBean" property="tutors">
+	<p><bean:message key="message.tutorship.dontExist.tutors" bundle="PEDAGOGICAL_COUNCIL" /></p>
+</logic:empty>
