84f6c73bf01ef465544d41460d87f2d3518eda04
Pedro Rijo <pedrorijo91@gmail.com>
2013-07-26 22:43:23 +0000

    added method for user cancelation on oauth authorization

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/externalServices/OAuthAction.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/externalServices/OAuthAction.java
@@ -6,6 +6,12 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sourceforge.fenixedu.domain.ExternalApplication;
+import net.sourceforge.fenixedu.domain.Person;
+import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
+import net.sourceforge.fenixedu.presentationTier.Action.utils.RequestUtils;
+
 import org.apache.amber.oauth2.as.issuer.MD5Generator;
 import org.apache.amber.oauth2.as.issuer.OAuthIssuer;
 import org.apache.amber.oauth2.as.issuer.OAuthIssuerImpl;
@@ -17,110 +23,140 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import net.sourceforge.fenixedu.applicationTier.IUserView;
-import net.sourceforge.fenixedu.domain.Person;
-import net.sourceforge.fenixedu.domain.Section;
-import net.sourceforge.fenixedu.injectionCode.AccessControl;
-import net.sourceforge.fenixedu.presentationTier.Action.CASAuthenticationAction;
-import net.sourceforge.fenixedu.presentationTier.Action.base.FenixDispatchAction;
-import net.sourceforge.fenixedu.presentationTier.Action.utils.RequestUtils;
-import net.sourceforge.fenixedu.presentationTier.servlets.filters.functionalities.FilterFunctionalityContext;
 import pt.ist.fenixWebFramework.struts.annotations.Forward;
 import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
-
+import pt.ist.fenixframework.pstm.AbstractDomainObject;
 
 @Mapping(module = "external", path = "/oauth", scope = "request", parameter = "method")
-@Forwards({
-	@Forward(name = "showAuthorizationPage", path = "showAuthorizationPage"),
-	@Forward(name = "oauthErrorPage", path = "/auth/oauthErrorPage.jsp")	
-})
-
+@Forwards({ @Forward(name = "showAuthorizationPage", path = "showAuthorizationPage"),
+        @Forward(name = "oauthErrorPage", path = "/auth/oauthErrorPage.jsp") })
 public class OAuthAction extends FenixDispatchAction {
 
-	//http://localhost:8080/ciapl/external/oauth.do?method=getUserPermission&client_id=123123&redirect_uri=http://www.google.com
-	public ActionForward getUserPermission(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		
-		Person person = getLoggedPerson(request);
-		if (person == null) {
-			RequestUtils.sendLoginRedirect(request,response);
-			return null;
-		} else {
-			return mapping.findForward("showAuthorizationPage");
-		}
+    //http://localhost:8080/ciapl/external/oauth.do?method=getUserPermission&client_id=123123&redirect_uri=http://www.google.com
+    public ActionForward getUserPermission(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
 
-	}
+        Person person = getLoggedPerson(request);
+        if (person == null) {
+            RequestUtils.sendLoginRedirect(request, response);
+            return null;
+        } else {
+            String clientId = request.getParameter("client_id");
+            String redirectUrl = request.getParameter("redirect_url");
 
-	//http://localhost:8080/ciapl/external/oauth.do?method=userConfirmation&...
-	public ActionForward userConfirmation(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+            //TODO check if clienteID and redirectURL are correct!
 
-		Person person = getLoggedPerson(request);
-		if (person == null) {
-			return null;
-		}
+            ExternalApplication clientApplication = AbstractDomainObject.fromExternalId(clientId);
 
-		//TODO check if redirect_uri and client_id are correct
-		String client_id = request.getParameter("client_id");
-		String redirect_uri = request.getParameter("redirect_uri");
+            /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+             * used only for testing
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+            ExternalApplication clientApplicationTest = RootDomainObject.getInstance().getApps().get(0);
 
-		try {
-			String code = oauthIssuerImpl.authorizationCode();
-			OAuthResponse resp = OAuthASResponse
-					.authorizationResponse(request, HttpServletResponse.SC_FOUND)
-					.location(redirect_uri)
-					.setCode(code)
-					.buildQueryMessage();			
-			
-			//TODO save code/state(?) > domain <-> time
-			//Build object
-			response.sendRedirect(resp.getLocationUri());
+            System.out.println("-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            System.out.println("clientApp != null? " + (clientApplicationTest != null));
+            System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 
-		} catch (OAuthSystemException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
-		return null;
-	}
+            //if (redirectUrl.equals(clientApplication.getUrl())) {
+            request.setAttribute("app", clientApplicationTest);
+            return mapping.findForward("showAuthorizationPage");
+            //} else {
+            //      return mapping.findForward("oauthErrorPage");
+            //}
 
-	//http://localhost:8080/ciapl/external/oauth.do?method=getTokens&...
-	public ActionForward getTokens(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+        }
 
-		OAuthTokenRequest oauthRequest = new OAuthTokenRequest(request);
-		OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+    }
 
-		String client_id = oauthRequest.getClientId();
-		String client_secret = oauthRequest.getClientSecret();
-		String redirect_uri = oauthRequest.getRedirectURI();
-		String authzCode = oauthRequest.getCode();
-		String expires = "3600";
-			
-		//TODO check if all the information is correct
+    //http://localhost:8080/ciapl/external/oauth.do?method=userConfirmation&...
+    public ActionForward userConfirmation(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
 
-		//build tokens base64(oid_app ; istid ; rand ; device_id(?))
-		String accessToken = oauthIssuerImpl.accessToken();
-		String refreshToken = oauthIssuerImpl.refreshToken();
+        Person person = getLoggedPerson(request);
+        if (person == null) {
+            return null;
+        }
 
-		//TODO save access, refresh token and timestamp
-		//clean Code
+        //TODO check if redirect_uri and client_id are correct
 
-		OAuthResponse r = OAuthASResponse
-				.tokenResponse(HttpServletResponse.SC_OK)
-				.location(redirect_uri)
-				.setAccessToken(accessToken)
-				.setExpiresIn(expires)
-				.setRefreshToken(refreshToken)
-				.buildJSONMessage();
+        try {
+            String code = oauthIssuerImpl.authorizationCode();
+            //TODO change the location. Get location from domain or request.getParameter("redirect_uri")
+            OAuthResponse resp =
+                    OAuthASResponse.authorizationResponse(request, HttpServletResponse.SC_FOUND).location("fenix://mobile/")
+                            .setCode(code).buildQueryMessage();
 
-		response.setStatus(r.getResponseStatus());
-		PrintWriter pw = response.getWriter();
-		pw.print(r.getBody());
-		pw.flush();
-		pw.close();
-		return null;
-	}	
+            //TODO save code/state(?) > domain
+            response.sendRedirect(resp.getLocationUri());
+
+        } catch (OAuthSystemException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void userCancelation(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+            HttpServletResponse response) {
+
+        /*
+         "In the case that the user denies the access request, an error response will be generated, 
+         and the user will be redirected to the redirect_uri with a query parameter called error 
+         indicating the type of error as access_denied. Additionally, the server can include an 
+         error_description message and/or an error_uri indicating the URL of a web page containing 
+         more information about the error."
+         */
+
+        try {
+            String redirectUrl = request.getParameter("redirect_url");
+
+            OAuthResponse r =
+                    OAuthResponse.errorResponse(401).setErrorUri(redirectUrl).setError("Permission denied")
+                            .setErrorDescription("User didn't allow the application").buildJSONMessage();
+
+            response.setStatus(r.getResponseStatus());
+
+            response.sendError(401);
+        } catch (OAuthSystemException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    //http://localhost:8080/ciapl/external/oauth.do?method=getTokens&...
+    public ActionForward getTokens(ActionMapping mapping, ActionForm actionForm, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        OAuthTokenRequest oauthRequest = new OAuthTokenRequest(request);
+        OAuthIssuer oauthIssuerImpl = new OAuthIssuerImpl(new MD5Generator());
+
+        //Get client_id
+        //Get client_secret
+        //get redirect_uri
+        String authzCode = oauthRequest.getCode();
+        //TODO check if all the information is correct
+
+        //build tokens base64(oid_app ; istid ; rand ; device_id(?))
+        String accessToken = oauthIssuerImpl.accessToken();
+        String refreshToken = oauthIssuerImpl.refreshToken();
+
+        //TODO save access, refresh token and timestamp
+
+        OAuthResponse r =
+                OAuthASResponse.tokenResponse(HttpServletResponse.SC_OK).location("fenix://mobile").setAccessToken(accessToken)
+                        .setExpiresIn("3600").setRefreshToken(refreshToken).buildJSONMessage();
+
+        response.setStatus(r.getResponseStatus());
+        PrintWriter pw = response.getWriter();
+        pw.print(r.getBody());
+        pw.flush();
+        pw.close();
+        return null;
+    }
 }
--- src/main/webapp/auth/showAuthorizationPage.jsp
+++ src/main/webapp/auth/showAuthorizationPage.jsp
@@ -1,16 +1,31 @@
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html"%>
 <%@ taglib uri="/WEB-INF/fenix-renderers.tld" prefix="fr"%>
 <%@ taglib uri="/WEB-INF/struts-bean.tld" prefix="bean"%>
-<h2>
-	Teste
-</h2>
-<!-- TODO Show information about author -->
-<!-- TODO Show information about scopes -->
-<!-- TODO Show add cancel -->
+<h2>Confirmar Autorização - Teste</h2>
+
+
+<!--
+http://localhost:8080/fenix/external/oauth.do?method=getUserPermission&contentContextPath_PATH=/pessoal/pessoal&_request_checksum_=ebf8ba2ebb6645de4caa9241dd6f359912fd8a58
+-->
+
+
+<logic:present name="app">
+	<fr:view name="app" layout="tabular" schema="my.schema.confirm.app" />
+</logic:present>
+
+
 <fr:form action="/oauth.do?method=userConfirmation">
 <input name="client_id" value="<%= request.getParameter("client_id") %>" type="hidden">
 <input name="redirect_uri" value="<%= request.getParameter("redirect_uri") %>" type="hidden">
 	<html:submit>
 		<bean:message key="button.submit" />
 	</html:submit>
+</fr:form>
+
+
+<fr:form action="/oauth.do?method=userCancelation">
+	<html:submit>
+		<bean:message key="button.cancel" />
+	</html:submit>
+	</p>
 </fr:form>
