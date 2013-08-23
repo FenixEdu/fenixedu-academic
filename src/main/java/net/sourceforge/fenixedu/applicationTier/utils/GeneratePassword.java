d403ed05c583e6f23cabd3426d5e12bc1b058af5
João Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-08-23 15:51:59 +0100

    Don't use magic to generate passwords

--- src/main/java/net/sourceforge/fenixedu/applicationTier/utils/GeneratePassword.java
+++ src/main/java/net/sourceforge/fenixedu/applicationTier/utils/GeneratePassword.java
@@ -4,7 +4,6 @@
  */
 package net.sourceforge.fenixedu.applicationTier.utils;
 
-import pt.ist.bennu.core.util.ConfigurationManager;
 
 /**
  * @author Barbosa
@@ -12,22 +11,10 @@ import pt.ist.bennu.core.util.ConfigurationManager;
  */
 public class GeneratePassword {
 
-    private static IGeneratePassword instance = null;
+    private static IGeneratePassword instance = new GeneratePasswordBase();
 
-    public static synchronized IGeneratePassword getInstance() {
-        if (instance == null) {
-            instance = getSpecificInstance();
-        }
+    public static IGeneratePassword getInstance() {
         return instance;
     }
 
-    private static IGeneratePassword getSpecificInstance() {
-        try {
-            String stringClass = ConfigurationManager.getProperty("password.generator");
-            Class clazz = Class.forName(stringClass);
-            return (IGeneratePassword) clazz.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
