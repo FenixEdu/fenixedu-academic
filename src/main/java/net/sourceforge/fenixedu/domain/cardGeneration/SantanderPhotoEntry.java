7bbcb698d71108dd9bbbed8e0291729e4069989a
Luis Cruz <luis.cruz@ist.utl.pt>
2013-09-05 17:58:03 +0100

    Finalized methods for getting photo for santander photo sheet.

--- src/main/java/net/sourceforge/fenixedu/domain/cardGeneration/SantanderPhotoEntry.java
+++ src/main/java/net/sourceforge/fenixedu/domain/cardGeneration/SantanderPhotoEntry.java
@@ -1,14 +1,21 @@
 package net.sourceforge.fenixedu.domain.cardGeneration;
 
+import java.awt.Color;
+
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.Photograph;
 import net.sourceforge.fenixedu.domain.RootDomainObject;
+import net.sourceforge.fenixedu.domain.exceptions.DomainException;
+import net.sourceforge.fenixedu.domain.photograph.AspectRatio;
+import net.sourceforge.fenixedu.domain.photograph.PictureAvatar;
+import net.sourceforge.fenixedu.domain.photograph.PictureMode;
+import net.sourceforge.fenixedu.domain.photograph.PictureSize;
 
 import org.joda.time.DateTime;
 
 public class SantanderPhotoEntry extends SantanderPhotoEntry_Base {
-    
-    public SantanderPhotoEntry(final Person person) {
+
+    public SantanderPhotoEntry(final Person person, final Photograph photograph) {
         super();
         setRootDomainObject(RootDomainObject.getInstance());
         setWhenGenerated(new DateTime());
@@ -16,17 +23,36 @@ public class SantanderPhotoEntry extends SantanderPhotoEntry_Base {
             setNext(person.getSantanderPhotoEntry());
         }
         setPerson(person);
+        setPhotograph(photograph);
         setSequenceNumber(SantanderSequenceNumberGenerator.getNewPhotoSequenceNumber());
     }
 
     public static SantanderPhotoEntry getOrCreatePhotoEntryForPerson(final Person person) {
         final Photograph personalPhoto = person.getPersonalPhoto();
         if (personalPhoto != null) {
-            final SantanderPhotoEntry entry = person.getSantanderPhotoEntry();
-            return entry == null || entry.getWhenGenerated().isBefore(personalPhoto.getStateChange())
-                    ? new SantanderPhotoEntry(person) : entry;
+            final PictureAvatar avatar = getAvatar(personalPhoto);
+            if (avatar != null) {
+                final SantanderPhotoEntry entry = person.getSantanderPhotoEntry();
+                return entry == null || entry.getWhenGenerated().isBefore(personalPhoto.getStateChange()) ? new SantanderPhotoEntry(
+                        person, personalPhoto) : entry;
+            }
         }
         return null;
+    }
+
+    private static PictureAvatar getAvatar(Photograph personalPhoto) {
+        try {
+            return personalPhoto.getAvatar(AspectRatio.ª9_by_10, PictureSize.LARGE, PictureMode.FIT);
+        } catch (final DomainException ex) {
+            // Ignore non existing photo, but don't generate santander photo sheet.
+            return null;
+        }
+    }
+
+    public byte[] getPhotoAsByteArray() {
+        final Photograph photograph = getPhotograph();
+        final PictureAvatar avatar = photograph.getAvatar(AspectRatio.ª9_by_10, PictureSize.LARGE, PictureMode.FIT);
+        return avatar.exportAsJPEG(Color.BLACK);
     }
 
 }
