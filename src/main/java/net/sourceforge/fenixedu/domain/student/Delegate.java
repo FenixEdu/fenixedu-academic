0e84fdc65b8058eb0c82faa4726fab2a57eb49fc
Miguel Borges <mb.borgez@gmail.com>
2013-10-30 18:52:18 +0000

    Changing the senders so that the fromName matches the new rules
    Signed-off-by: Miguel Borges <mb.borgez@gmail.com>

--- src/main/java/net/sourceforge/fenixedu/domain/student/Delegate.java
+++ src/main/java/net/sourceforge/fenixedu/domain/student/Delegate.java
@@ -30,7 +30,7 @@ public class Delegate extends Delegate_Base {
         return false;
     }
 
-    protected Degree getDegree() {
+    public Degree getDegree() {
         return ((DegreeUnit) getDelegateFunction().getFunction().getUnit()).getDegree();
     }
 
