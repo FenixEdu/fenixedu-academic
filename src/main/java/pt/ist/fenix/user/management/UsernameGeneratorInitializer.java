51db5306026084e1928c018ec5793b24a6115f05
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2013-11-29 18:39:07 +0000

    Add IST-Specific UsernameGenerator

---
+++ src/main/java/pt/ist/fenix/user/management/UsernameGeneratorInitializer.java
@@ -0,0 +1,23 @@
+package pt.ist.fenix.user.management;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import pt.ist.bennu.user.management.UsernameGenerator;
+
+@WebListener
+public class UsernameGeneratorInitializer implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        IstUsernameCounter counter = IstUsernameCounter.ensureSingleton();
+        UsernameGenerator.setDefault(new ISTUsernameGenerator(counter));
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+
+    }
+
+}
