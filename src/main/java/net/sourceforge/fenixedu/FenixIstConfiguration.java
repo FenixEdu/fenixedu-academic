039b44656260c9f15ab88dfed444808ef82f565b
Pedro Santos <pedro.san7os@gmail.com>
2014-01-06 11:48:12 +0000

    Copied defaults from configuration.properties.sample to ConfigurationManagers

--- src/main/java/net/sourceforge/fenixedu/FenixIstConfiguration.java
+++ src/main/java/net/sourceforge/fenixedu/FenixIstConfiguration.java
@@ -7,13 +7,13 @@ import org.fenixedu.bennu.core.util.ConfigurationInvocationHandler;
 public class FenixIstConfiguration {
     @ConfigurationManager(description = "Fenix IST specific properties")
     public interface ConfigurationProperties {
-        @ConfigurationProperty(key = "db.giaf.user")
+        @ConfigurationProperty(key = "db.giaf.user", defaultValue = "root")
         public String dbGiafUser();
 
         @ConfigurationProperty(key = "db.giaf.pass")
         public String dbGiafPass();
 
-        @ConfigurationProperty(key = "db.giaf.alias")
+        @ConfigurationProperty(key = "db.giaf.alias", defaultValue = "//localhost:3306/AssiduidadeOracleTeste")
         public String dbGiafAlias();
     }
 
