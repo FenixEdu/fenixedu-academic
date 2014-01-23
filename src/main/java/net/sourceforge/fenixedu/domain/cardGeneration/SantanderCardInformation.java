289ec31490ff22ebad36cd276e5f84e9840c0b94
Joao Amaral <johndoe.amaral@gmail.com>
2014-01-23 19:05:40 +0000

    Change the domain to be according to the fenix 2.0

---
+++ src/main/java/net/sourceforge/fenixedu/domain/cardGeneration/SantanderCardInformation.java
@@ -0,0 +1,9 @@
+package net.sourceforge.fenixedu.domain.cardGeneration;
+
+public class SantanderCardInformation extends SantanderCardInformation_Base {
+    
+    public SantanderCardInformation() {
+        super();
+    }
+    
+}
