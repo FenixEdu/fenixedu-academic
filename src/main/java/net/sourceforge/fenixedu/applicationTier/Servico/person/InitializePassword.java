1916f6b584e03cae0ce2ccaca376d2db1c68831d
Miguel Borges <mb.borgez@gmail.com>
2014-02-05 16:23:14 +0000

    Starting the development of the InitializePassword WebService

--- src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/person/InitializePassword.java
+++ src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/person/InitializePassword.java
@@ -1,5 +1,10 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.person;
 
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidPasswordServiceException;
 
 import org.fenixedu.bennu.core.domain.User;
@@ -8,9 +13,26 @@ import pt.ist.fenixframework.Atomic;
 
 public class InitializePassword {
 
+    private static final String NAMESPACE_URI = "";
+
+    private static final String LOCAL_PART = "";
+
     @Atomic
     public static void run(User user, String password) throws InvalidPasswordServiceException, Exception {
-        //TODO - verify if the password was allready initialized and throw exception
         user.changePassword(password);
     }
+
+    private static void run2() throws InvalidPasswordServiceException {
+        try {
+            QName qname = new QName(NAMESPACE_URI, LOCAL_PART);
+            URL serviceUrl = new URL("http://localhost:9999/ws/hello?wsdl");
+            Service service = Service.create(serviceUrl, qname);
+
+            Class clazz = null;
+            service.getPort(clazz);
+        } catch (Exception e) {
+            throw new InvalidPasswordServiceException(e.getCause());
+        }
+    }
+
 }
