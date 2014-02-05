576417ab4c1a31fb21ac4d996aed3dfe54ecfc99
Miguel Borges <mb.borgez@gmail.com>
2014-02-05 19:07:01 +0000

    developing the webservice client with a dummy endpoint

--- src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/person/InitializePassword.java
+++ src/main/java/net/sourceforge/fenixedu/applicationTier/Servico/person/InitializePassword.java
@@ -1,38 +1,47 @@
 package net.sourceforge.fenixedu.applicationTier.Servico.person;
 
-import java.net.URL;
-
-import javax.xml.namespace.QName;
-import javax.xml.ws.Service;
-
 import net.sourceforge.fenixedu.applicationTier.Servico.exceptions.InvalidPasswordServiceException;
 
 import org.fenixedu.bennu.core.domain.User;
 
 import pt.ist.fenixframework.Atomic;
 
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
 public class InitializePassword {
 
-    private static final String NAMESPACE_URI = "";
-
-    private static final String LOCAL_PART = "";
+    private static final String RESOURCE_URL = "http://localhost:8080/RESTfulExample/rest/json/metallica/post";
 
     @Atomic
     public static void run(User user, String password) throws InvalidPasswordServiceException, Exception {
-        user.changePassword(password);
-    }
-
-    private static void run2() throws InvalidPasswordServiceException {
         try {
-            QName qname = new QName(NAMESPACE_URI, LOCAL_PART);
-            URL serviceUrl = new URL("http://localhost:9999/ws/hello?wsdl");
-            Service service = Service.create(serviceUrl, qname);
+            Client client = Client.create();
+            WebResource webResource = client.resource(RESOURCE_URL);
 
-            Class clazz = null;
-            service.getPort(clazz);
+            UserPassword userPassword = new UserPassword(user.getUsername(), password);
+            String input = new Gson().toJson(userPassword);
+
+            ClientResponse response = webResource.type("application/json").post(ClientResponse.class, input);
+
+            if (response.getStatus() != 201) {
+                throw new InvalidPasswordServiceException("Failed : HTTP error code : " + response.getStatus());
+            }
+
         } catch (Exception e) {
             throw new InvalidPasswordServiceException(e.getCause());
         }
     }
 
+    static class UserPassword {
+        final String username;
+        final String password;
+
+        public UserPassword(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
 }
