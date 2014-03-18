829401ae60b0a869fc5d3b24fc7df41cf3f4fe91
ricardorcr <ricardo.rcr@gmail.com>
2014-03-18 18:29:42 +0000

    Bug fix: Closes #429 - the access to this files can be made by academic office, degree coordinators and own person, this is the easiest way to grant that access vs groups

--- src/main/java/net/sourceforge/fenixedu/domain/candidacyProcess/IndividualCandidacyDocumentFile.java
+++ src/main/java/net/sourceforge/fenixedu/domain/candidacyProcess/IndividualCandidacyDocumentFile.java
@@ -1,6 +1,11 @@
 package net.sourceforge.fenixedu.domain.candidacyProcess;
 
+import net.sourceforge.fenixedu.domain.AcademicProgram;
+import net.sourceforge.fenixedu.domain.Degree;
+import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.accessControl.NoOneGroup;
+import net.sourceforge.fenixedu.domain.accessControl.academicAdministration.AcademicAuthorizationGroup;
+import net.sourceforge.fenixedu.domain.accessControl.academicAdministration.AcademicOperationType;
 import pt.ist.fenixframework.Atomic;
 
 public class IndividualCandidacyDocumentFile extends IndividualCandidacyDocumentFile_Base {
@@ -17,10 +22,6 @@ public class IndividualCandidacyDocumentFile extends IndividualCandidacyDocument
         addIndividualCandidacy(candidacy);
         setCandidacyFileType(type);
         init(filename, filename, contents, new NoOneGroup());
-
-        if (type.equals(IndividualCandidacyDocumentFileType.PHOTO)) {
-            // storeToContentManager();
-        }
     }
 
     protected IndividualCandidacyDocumentFile(IndividualCandidacyDocumentFileType type, byte[] contents, String filename) {
@@ -28,16 +29,50 @@ public class IndividualCandidacyDocumentFile extends IndividualCandidacyDocument
         this.setCandidacyFileActive(Boolean.TRUE);
         setCandidacyFileType(type);
         init(filename, filename, contents, new NoOneGroup());
-
-        if (type.equals(IndividualCandidacyDocumentFileType.PHOTO)) {
-            // storeToContentManager();
-        }
     }
 
     @Atomic
     public static IndividualCandidacyDocumentFile createCandidacyDocument(byte[] contents, String filename,
             IndividualCandidacyDocumentFileType type, String processName, String documentIdNumber) {
         return new IndividualCandidacyDocumentFile(type, contents, filename);
+    }
+
+    @Override
+    public boolean isPersonAllowedToAccess(Person person) {
+        if (person == null) {
+            return false;
+        }
+
+        // Academic Administration Permissions
+        for (AcademicProgram program : AcademicAuthorizationGroup.getProgramsForOperation(person,
+                AcademicOperationType.MANAGE_CANDIDACY_PROCESSES)) {
+            for (IndividualCandidacy individualCandidacy : getIndividualCandidacySet()) {
+                if (individualCandidacy.getAllDegrees().contains(program)) {
+                    return true;
+                }
+            }
+        }
+
+        // Coordinators
+        for (IndividualCandidacy individualCandidacy : getIndividualCandidacySet()) {
+            for (Degree degree : individualCandidacy.getAllDegrees()) {
+                if (degree.isCurrentCoordinator(person)) {
+                    return true;
+                }
+            }
+        }
+
+        // Candidates
+        for (IndividualCandidacy individualCandidacy : getIndividualCandidacySet()) {
+            IndividualCandidacyPersonalDetails personalDetails = individualCandidacy.getPersonalDetails();
+            if (personalDetails != null) {
+                if (person.equals(personalDetails.getPerson())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     @Deprecated
