e18840aaadd2dfcd32c81d431b40ebfaac7264a0
Luis Cruz <luis.cruz@ist.utl.pt>
2014-03-31 16:31:40 +0100

    Bug Fix: Avoid exception of docs that are not versioned... just don't add them to the result set.

--- src/main/java/net/sourceforge/fenixedu/domain/phd/candidacy/feedbackRequest/PhdCandidacyFeedbackRequestProcess.java
+++ src/main/java/net/sourceforge/fenixedu/domain/phd/candidacy/feedbackRequest/PhdCandidacyFeedbackRequestProcess.java
@@ -101,9 +101,11 @@ public class PhdCandidacyFeedbackRequestProcess extends PhdCandidacyFeedbackRequ
         final Set<PhdProgramProcessDocument> result = new HashSet<PhdProgramProcessDocument>();
 
         for (final PhdIndividualProgramDocumentType type : getSortedSharedDocumentTypes()) {
-            final PhdProgramProcessDocument document = getCandidacyProcess().getLatestDocumentVersionFor(type);
-            if (document != null) {
-                result.add(document);
+            if (type.isVersioned()) {
+                final PhdProgramProcessDocument document = getCandidacyProcess().getLatestDocumentVersionFor(type);
+                if (document != null) {
+                    result.add(document);
+                }
             }
         }
         return result;
