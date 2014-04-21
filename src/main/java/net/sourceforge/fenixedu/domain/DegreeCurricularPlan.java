871d447d2e2dcb44d05ba2d6d7d07cc226a8b562
Pedro Santos <pedro.san7os@gmail.com>
2014-04-21 15:26:34 +0100

    Deleted group conversion

--- src/main/java/net/sourceforge/fenixedu/domain/DegreeCurricularPlan.java
+++ src/main/java/net/sourceforge/fenixedu/domain/DegreeCurricularPlan.java
@@ -1190,16 +1190,6 @@ public class DegreeCurricularPlan extends DegreeCurricularPlan_Base {
         super.setCurricularPlanMembersGroup(curricularPlanMembersGroup);
     }
 
-    /**
-     * Delete after group migration
-     * 
-     * @param curricularPlanMembersGroup
-     */
-    @Deprecated
-    public void setCurricularPlanMembersGroupWithoutCheckingPermissions(Group curricularPlanMembersGroup) {
-        super.setCurricularPlanMembersGroup(curricularPlanMembersGroup);
-    }
-
     @Override
     public void setCurricularStage(CurricularStage curricularStage) {
         check(this, DegreeCurricularPlanPredicates.scientificCouncilWritePredicate);
