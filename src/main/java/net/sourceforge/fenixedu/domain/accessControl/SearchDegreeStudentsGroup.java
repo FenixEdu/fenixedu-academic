jcarvalho/fenix

SearchDegreeStudentsGroup is not a group
Conflicts:
	src/main/java/net/sourceforge/fenixedu/domain/accessControl/SearchDegreeStudentsGroup.java

src/main/java/net/sourceforge/fenixedu/domain/accessControl/SearchDegreeStudentsGroup.java
@@ -1,7 +1,9 @@
 package net.sourceforge.fenixedu.domain.accessControl;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -12,27 +14,21 @@ import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.Argument;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.GroupBuilder;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.StaticArgument;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.operators.OidOperator;
+import net.sourceforge.fenixedu.domain.accessControl.groups.BennuGroupBridge;
 import net.sourceforge.fenixedu.domain.contacts.EmailAddress;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationState;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationStateType;
 
 import org.fenixedu.bennu.core.domain.User;
-import org.fenixedu.bennu.core.domain.groups.NobodyGroup;
 import org.fenixedu.bennu.core.domain.groups.UserGroup;
 
+import pt.ist.fenixframework.FenixFramework;
 import pt.utl.ist.fenix.tools.util.i18n.Language;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.base.Joiner;
 
-public class SearchDegreeStudentsGroup extends Group {
+public class SearchDegreeStudentsGroup implements Serializable {
 
     private static final long serialVersionUID = -1670838875686375271L;
 
@@ -206,9 +202,8 @@ public class SearchDegreeStudentsGroup extends Group {
         this.sortBy = sortBy;
     }
 
-    @Override
     public Set<Person> getElements() {
-        final Set<Person> elements = super.buildSet();
+        final Set<Person> elements = new HashSet<>();
         final Map<StudentCurricularPlan, RegistrationStateType> students = searchStudentCurricularPlans(null, null);
         for (StudentCurricularPlan student : students.keySet()) {
             elements.add(student.getPerson());
@@ -216,14 +211,16 @@ public class SearchDegreeStudentsGroup extends Group {
         return elements;
     }
 
-    @Override
-    protected Argument[] getExpressionArguments() {
-        return new Argument[] { new OidOperator(getDegreeCurricularPlan()), new OidOperator(getExecutionYear()),
-                new StaticArgument(getSortBy()), new StaticArgument(getRegistrationStateType()),
-                new StaticArgument(getMinGrade()), new StaticArgument(getMaxGrade()), new StaticArgument(getMinNumberApproved()),
-                new StaticArgument(getMaxNumberApproved()), new StaticArgument(getMinStudentNumber()),
-                new StaticArgument(getMaxStudentNumber()), new StaticArgument(getMinimumYear()),
-                new StaticArgument(getMaximumYear()) };
+    public Group getUserGroup() {
+        Set<User> users = new HashSet<>();
+        final Map<StudentCurricularPlan, RegistrationStateType> students = searchStudentCurricularPlans(null, null);
+        for (StudentCurricularPlan student : students.keySet()) {
+            User user = student.getPerson().getUser();
+            if (user != null) {
+                users.add(user);
+            }
+        }
+        return new BennuGroupBridge(UserGroup.getInstance(users));
     }
 
     private Comparator<StudentCurricularPlan> determineComparatorKind() {
@@ -490,65 +487,49 @@ public class SearchDegreeStudentsGroup extends Group {
         return label;
     }
 
-    public static class Builder implements GroupBuilder {
-
-        private void verifyArgs(Object[] arguments) {
-            for (int i = 4; i < 10; i++) {
-                String arg = (String) arguments[i];
-                if (arg.equalsIgnoreCase("null")) {
-                    arguments[i] = null;
-                } else if (i < 10) {
-                    arguments[i] = Double.valueOf(arg);
-                }
-            }
-            for (int i = 10; i < 12; i++) {
-                if (arguments[i] instanceof String) {
-                    arguments[i] = null;
-                }
-            }
-        }
-
-        @Override
-        public Group build(Object[] arguments) {
-            verifyArgs(arguments);
-            try {
-                return new SearchDegreeStudentsGroup((DegreeCurricularPlan) arguments[0], (ExecutionYear) arguments[1],
-                        (String) arguments[2], RegistrationStateType.valueOf((String) arguments[3]), (Double) arguments[4],
-                        (Double) arguments[5], (Double) arguments[6], (Double) arguments[7], (Double) arguments[8],
-                        (Double) arguments[9], (Integer) arguments[10], (Integer) arguments[11]);
-            } catch (final ClassCastException e) {
-                throw new Error(e);
-            }
-        }
-
-        @Override
-        public int getMinArguments() {
-            return 12;
-        }
-
-        @Override
-        public int getMaxArguments() {
-            return 12;
-        }
-
+    public static SearchDegreeStudentsGroup parse(String serialized) {
+        String[] parts = serialized.split(":");
+        DegreeCurricularPlan degreeCurricularPlan = FenixFramework.getDomainObject(parts[0]);
+        ExecutionYear executionYear = FenixFramework.getDomainObject(parts[1]);
+        String sortBy = parts[2];
+        RegistrationStateType registrationStateType = RegistrationStateType.valueOf(parts[3]);
+        Double minGrade = parts[4].equals("ND") ? null : Double.valueOf(parts[4]);
+        Double maxGrade = parts[5].equals("ND") ? null : Double.valueOf(parts[5]);
+        Double minNumberApproved = parts[6].equals("ND") ? null : Double.valueOf(parts[6]);
+        Double maxNumberApproved = parts[7].equals("ND") ? null : Double.valueOf(parts[7]);
+        Double minStudentNumber = parts[8].equals("ND") ? null : Double.valueOf(parts[8]);
+        Double maxStudentNumber = parts[9].equals("ND") ? null : Double.valueOf(parts[9]);
+        Integer minimumYear = parts[10].equals("ND") ? null : Integer.valueOf(parts[10]);
+        Integer maximumYear = parts[11].equals("ND") ? null : Integer.valueOf(parts[11]);
+        return new SearchDegreeStudentsGroup(degreeCurricularPlan, executionYear, sortBy, registrationStateType, minGrade,
+                maxGrade, minNumberApproved, maxNumberApproved, minStudentNumber, maxStudentNumber, minimumYear, maximumYear);
     }
 
-    @Override
-    public org.fenixedu.bennu.core.domain.groups.Group convert() {
-        ImmutableSet<User> users = FluentIterable.from(getElements()).filter(new Predicate<Person>() {
-            @Override
-            public boolean apply(Person person) {
-                return person.getUser() != null;
-            }
-        }).transform(new Function<Person, User>() {
-            @Override
-            public User apply(Person person) {
-                return person.getUser();
-            }
-        }).toSet();
-        if (users.isEmpty()) {
-            return NobodyGroup.getInstance();
-        }
-        return UserGroup.getInstance(users);
+    public String serialize() {
+        Set<String> parts = new HashSet<>();
+        parts.add(getDegreeCurricularPlan().getExternalId());
+        parts.add(getExecutionYear().getExternalId());
+        parts.add(getSortBy());
+        parts.add(getRegistrationStateType().getName());
+        parts.add(getMinGradeString());
+        parts.add(getMaxGradeString());
+        parts.add(getMinNumberApprovedString());
+        parts.add(getMaxNumberApprovedString());
+        parts.add(getMinStudentNumberString());
+        parts.add(getMaxStudentNumberString());
+        parts.add(getMinimumYearString());
+        parts.add(getMaximumYearString());
+        return Joiner.on(':').join(parts);
     }
+
+//  @Override
+//  protected Argument[] getExpressionArguments() {
+//      return new Argument[] { new OidOperator(getDegreeCurricularPlan()), new OidOperator(getExecutionYear()),
+//              new StaticArgument(getSortBy()), new StaticArgument(getRegistrationStateType()),
+//              new StaticArgument(getMinGrade()), new StaticArgument(getMaxGrade()), new StaticArgument(getMinNumberApproved()),
+//              new StaticArgument(getMaxNumberApproved()), new StaticArgument(getMinStudentNumber()),
+//              new StaticArgument(getMaxStudentNumber()), new StaticArgument(getMinimumYear()),
+//              new StaticArgument(getMaximumYear()) };
+//  }
+
 }

FenixEdu/fenixedu-academic

src/main/java/net/sourceforge/fenixedu/domain/accessControl/SearchDegreeStudentsGroup.java
@@ -1,7 +1,9 @@
 package net.sourceforge.fenixedu.domain.accessControl;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -12,26 +14,21 @@ import net.sourceforge.fenixedu.domain.DegreeCurricularPlan;
 import net.sourceforge.fenixedu.domain.ExecutionYear;
 import net.sourceforge.fenixedu.domain.Person;
 import net.sourceforge.fenixedu.domain.StudentCurricularPlan;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.Argument;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.GroupBuilder;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.StaticArgument;
-import net.sourceforge.fenixedu.domain.accessControl.groups.language.operators.OidOperator;
+import net.sourceforge.fenixedu.domain.accessControl.groups.BennuGroupBridge;
 import net.sourceforge.fenixedu.domain.contacts.EmailAddress;
 import net.sourceforge.fenixedu.domain.student.Registration;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationState;
 import net.sourceforge.fenixedu.domain.student.registrationStates.RegistrationStateType;
 
 import org.fenixedu.bennu.core.domain.User;
-import org.fenixedu.bennu.core.domain.groups.NobodyGroup;
 import org.fenixedu.bennu.core.domain.groups.UserGroup;
 import org.fenixedu.commons.i18n.I18N;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableSet;
+import pt.ist.fenixframework.FenixFramework;
 
-public class SearchDegreeStudentsGroup extends Group {
+import com.google.common.base.Joiner;
+
+public class SearchDegreeStudentsGroup implements Serializable {
 
     private static final long serialVersionUID = -1670838875686375271L;
 
@@ -198,16 +195,15 @@ public class SearchDegreeStudentsGroup extends Group {
     }
 
     public String getSortBy() {
-        return sortBy;
+        return sortBy != null ? sortBy : "student.number";
     }
 
     public void setSortBy(String sortBy) {
         this.sortBy = sortBy;
     }
 
-    @Override
     public Set<Person> getElements() {
-        final Set<Person> elements = super.buildSet();
+        final Set<Person> elements = new HashSet<>();
         final Map<StudentCurricularPlan, RegistrationStateType> students = searchStudentCurricularPlans(null, null);
         for (StudentCurricularPlan student : students.keySet()) {
             elements.add(student.getPerson());
@@ -215,14 +211,16 @@ public class SearchDegreeStudentsGroup extends Group {
         return elements;
     }
 
-    @Override
-    protected Argument[] getExpressionArguments() {
-        return new Argument[] { new OidOperator(getDegreeCurricularPlan()), new OidOperator(getExecutionYear()),
-                new StaticArgument(getSortBy()), new StaticArgument(getRegistrationStateType()),
-                new StaticArgument(getMinGrade()), new StaticArgument(getMaxGrade()), new StaticArgument(getMinNumberApproved()),
-                new StaticArgument(getMaxNumberApproved()), new StaticArgument(getMinStudentNumber()),
-                new StaticArgument(getMaxStudentNumber()), new StaticArgument(getMinimumYear()),
-                new StaticArgument(getMaximumYear()) };
+    public Group getUserGroup() {
+        Set<User> users = new HashSet<>();
+        final Map<StudentCurricularPlan, RegistrationStateType> students = searchStudentCurricularPlans(null, null);
+        for (StudentCurricularPlan student : students.keySet()) {
+            User user = student.getPerson().getUser();
+            if (user != null) {
+                users.add(user);
+            }
+        }
+        return new BennuGroupBridge(UserGroup.getInstance(users));
     }
 
     private Comparator<StudentCurricularPlan> determineComparatorKind() {
@@ -489,65 +487,49 @@ public class SearchDegreeStudentsGroup extends Group {
         return label;
     }
 
-    public static class Builder implements GroupBuilder {
-
-        private void verifyArgs(Object[] arguments) {
-            for (int i = 4; i < 10; i++) {
-                String arg = (String) arguments[i];
-                if (arg.equalsIgnoreCase("null")) {
-                    arguments[i] = null;
-                } else if (i < 10) {
-                    arguments[i] = Double.valueOf(arg);
-                }
-            }
-            for (int i = 10; i < 12; i++) {
-                if (arguments[i] instanceof String) {
-                    arguments[i] = null;
-                }
-            }
-        }
-
-        @Override
-        public Group build(Object[] arguments) {
-            verifyArgs(arguments);
-            try {
-                return new SearchDegreeStudentsGroup((DegreeCurricularPlan) arguments[0], (ExecutionYear) arguments[1],
-                        (String) arguments[2], RegistrationStateType.valueOf((String) arguments[3]), (Double) arguments[4],
-                        (Double) arguments[5], (Double) arguments[6], (Double) arguments[7], (Double) arguments[8],
-                        (Double) arguments[9], (Integer) arguments[10], (Integer) arguments[11]);
-            } catch (final ClassCastException e) {
-                throw new Error(e);
-            }
-        }
-
-        @Override
-        public int getMinArguments() {
-            return 12;
-        }
-
-        @Override
-        public int getMaxArguments() {
-            return 12;
-        }
-
+    public static SearchDegreeStudentsGroup parse(String serialized) {
+        String[] parts = serialized.split(":");
+        DegreeCurricularPlan degreeCurricularPlan = FenixFramework.getDomainObject(parts[0]);
+        ExecutionYear executionYear = FenixFramework.getDomainObject(parts[1]);
+        String sortBy = parts[2];
+        RegistrationStateType registrationStateType = parts[3].equals("ND") ? null : RegistrationStateType.valueOf(parts[3]);
+        Double minGrade = parts[4].equals("ND") ? null : Double.valueOf(parts[4]);
+        Double maxGrade = parts[5].equals("ND") ? null : Double.valueOf(parts[5]);
+        Double minNumberApproved = parts[6].equals("ND") ? null : Double.valueOf(parts[6]);
+        Double maxNumberApproved = parts[7].equals("ND") ? null : Double.valueOf(parts[7]);
+        Double minStudentNumber = parts[8].equals("ND") ? null : Double.valueOf(parts[8]);
+        Double maxStudentNumber = parts[9].equals("ND") ? null : Double.valueOf(parts[9]);
+        Integer minimumYear = parts[10].equals("ND") ? null : Integer.valueOf(parts[10]);
+        Integer maximumYear = parts[11].equals("ND") ? null : Integer.valueOf(parts[11]);
+        return new SearchDegreeStudentsGroup(degreeCurricularPlan, executionYear, sortBy, registrationStateType, minGrade,
+                maxGrade, minNumberApproved, maxNumberApproved, minStudentNumber, maxStudentNumber, minimumYear, maximumYear);
     }
 
-    @Override
-    public org.fenixedu.bennu.core.domain.groups.Group convert() {
-        ImmutableSet<User> users = FluentIterable.from(getElements()).filter(new Predicate<Person>() {
-            @Override
-            public boolean apply(Person person) {
-                return person.getUser() != null;
-            }
-        }).transform(new Function<Person, User>() {
-            @Override
-            public User apply(Person person) {
-                return person.getUser();
-            }
-        }).toSet();
-        if (users.isEmpty()) {
-            return NobodyGroup.getInstance();
-        }
-        return UserGroup.getInstance(users);
+    public String serialize() {
+        List<String> parts = new ArrayList<>();
+        parts.add(getDegreeCurricularPlan().getExternalId());
+        parts.add(getExecutionYear().getExternalId());
+        parts.add(getSortBy());
+        parts.add(getRegistrationStateType() != null ? getRegistrationStateType().getName() : "ND");
+        parts.add(getMinGradeString());
+        parts.add(getMaxGradeString());
+        parts.add(getMinNumberApprovedString());
+        parts.add(getMaxNumberApprovedString());
+        parts.add(getMinStudentNumberString());
+        parts.add(getMaxStudentNumberString());
+        parts.add(getMinimumYearString());
+        parts.add(getMaximumYearString());
+        return Joiner.on(':').join(parts);
     }
+
+//  @Override
+//  protected Argument[] getExpressionArguments() {
+//      return new Argument[] { new OidOperator(getDegreeCurricularPlan()), new OidOperator(getExecutionYear()),
+//              new StaticArgument(getSortBy()), new StaticArgument(getRegistrationStateType()),
+//              new StaticArgument(getMinGrade()), new StaticArgument(getMaxGrade()), new StaticArgument(getMinNumberApproved()),
+//              new StaticArgument(getMaxNumberApproved()), new StaticArgument(getMinStudentNumber()),
+//              new StaticArgument(getMaxStudentNumber()), new StaticArgument(getMinimumYear()),
+//              new StaticArgument(getMaximumYear()) };
+//  }
+
 }