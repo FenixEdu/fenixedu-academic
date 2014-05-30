17e6147eca5561ad7339555b5be921d8fabca112
Carlos Bartolomeu <carlosgbartolomeu@gmail.com>
2014-05-30 17:49:29 +0100

    Added spring service for other modules

---
+++ src/main/java/org/fenixedu/bennu/FenixSpringConfiguration.java
@@ -0,0 +1,8 @@
+package org.fenixedu.bennu;
+
+import org.fenixedu.bennu.spring.BennuSpringModule;
+
+@BennuSpringModule(basePackages = "org.fenixedu.core.service")
+public class FenixSpringConfiguration {
+
+}
