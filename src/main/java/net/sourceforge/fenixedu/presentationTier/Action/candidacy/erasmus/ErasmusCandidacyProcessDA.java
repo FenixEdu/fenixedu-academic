dba237b2e327c5e0e1e8969e1a11d318997956b6
Afonso F. Garcia <afonsogarcia@icloud.com>
2014-06-02 17:12:07 +0100

    Removed bolonha 1st cycle degrees from incoming mobility programmes options

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/candidacy/erasmus/ErasmusCandidacyProcessDA.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/candidacy/erasmus/ErasmusCandidacyProcessDA.java
@@ -352,7 +352,7 @@ public class ErasmusCandidacyProcessDA extends CandidacyProcessDA {
         public Object provide(Object source, Object currentValue) {
             final List<Degree> degrees =
                     new ArrayList<Degree>(Degree.readAllByDegreeType(DegreeType.BOLONHA_INTEGRATED_MASTER_DEGREE,
-                            DegreeType.BOLONHA_MASTER_DEGREE, DegreeType.BOLONHA_DEGREE));
+                            DegreeType.BOLONHA_MASTER_DEGREE));
 
             degrees.remove(Degree.readBySigla("MSCIT"));
 
