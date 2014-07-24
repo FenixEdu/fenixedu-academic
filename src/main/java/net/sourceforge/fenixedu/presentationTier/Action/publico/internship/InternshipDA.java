048fa5bbc1e7823b445de7fe661b7b3ea1ccbc9f
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-07-24 17:27:32 +0100

    IAESTE Internships no longer use Tiles

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/publico/internship/InternshipDA.java
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/publico/internship/InternshipDA.java
@@ -31,29 +31,30 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import pt.ist.fenixWebFramework.struts.annotations.Forward;
-import pt.ist.fenixWebFramework.struts.annotations.Forwards;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
 
 @Mapping(path = "/internship", module = "publico")
-@Forwards({ @Forward(name = "candidacy", path = "internship.candidacy"),
-        @Forward(name = "rules", path = "internship.candidacy.rules"),
-        @Forward(name = "final", path = "internship.candidacy.final") })
 public class InternshipDA extends FenixDispatchAction {
+
+    private final ActionForward forward(HttpServletRequest request, String page) {
+        request.setAttribute("actual$page", "/publico/internship/" + page + ".jsp");
+        return new ActionForward("/internship/frame.jsp");
+    }
+
     public ActionForward prepareCandidacy(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) {
         InternshipCandidacySession session = InternshipCandidacySession.getMostRecentCandidacySession();
         if (session.getCandidacyInterval().containsNow()) {
             request.setAttribute("candidacy", new InternshipCandidacyBean(session));
         }
-        return mapping.findForward("candidacy");
+        return forward(request, "candidacy");
     }
 
     public ActionForward submitCandidacy(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) {
         InternshipCandidacyBean bean = getRenderedObject();
         request.setAttribute("candidacy", bean);
-        return mapping.findForward("rules");
+        return forward(request, "candidacyRules");
     }
 
     public ActionForward confirmCandidacyRules(ActionMapping mapping, ActionForm form, HttpServletRequest request,
@@ -63,11 +64,11 @@ public class InternshipDA extends FenixDispatchAction {
             request.setAttribute("candidacy", bean);
             Integer candidacyNumber = InternshipCandidacy.create(bean);
             request.setAttribute("candidacyNumber", candidacyNumber);
-            return mapping.findForward("final");
+            return forward(request, "candidacyFinal");
         } catch (DuplicateInternshipCandidacy e) {
             addActionMessage(request, "error.internationalrelations.internship.candidacy.duplicateStudentNumber", e.getNumber(),
                     e.getUniversity());
-            return mapping.findForward("rules");
+            return forward(request, "candidacyRules");
         }
     }
 
@@ -75,6 +76,6 @@ public class InternshipDA extends FenixDispatchAction {
             HttpServletResponse response) {
         InternshipCandidacyBean bean = getRenderedObject();
         request.setAttribute("candidacy", bean);
-        return mapping.findForward("candidacy");
+        return forward(request, "candidacy");
     }
 }
