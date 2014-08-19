026d2792fe28a32cb507874f3f7630fbccf4911f
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-08-19 18:08:18 +0100

    New Feature: Thesis library sites are no longer CMS-based
      * The old ones still work, but this is now the official
        mechanism
      * Updated the library link to the new path

--- src/main/java/org/fenixedu/bennu/FenixSpringConfiguration.java
+++ src/main/java/org/fenixedu/bennu/FenixSpringConfiguration.java
@@ -2,7 +2,7 @@ package org.fenixedu.bennu;
 
 import org.fenixedu.bennu.spring.BennuSpringModule;
 
-@BennuSpringModule(basePackages = "org.fenixedu.core.service")
+@BennuSpringModule(basePackages = { "org.fenixedu.core.service", "org.fenixedu.academic.ui" })
 public class FenixSpringConfiguration {
 
 }
