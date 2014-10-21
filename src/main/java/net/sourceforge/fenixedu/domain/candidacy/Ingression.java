f46793bb35bee6358c5267f81078b18994d883d9
ricardorcr <ricardo.rcr@gmail.com>
2014-10-21 14:51:31 +0100

    Minor change: changed some messages and added a new one

--- src/main/java/net/sourceforge/fenixedu/domain/candidacy/Ingression.java
+++ src/main/java/net/sourceforge/fenixedu/domain/candidacy/Ingression.java
@@ -60,30 +60,31 @@ public enum Ingression implements IPresentableEnum {
 
     ENC02("Mudanças de Curso Externas"),
 
-    REA01("Funcionários portugueses de missão diplomática no estrangeiro e seus familiares que os acompanham"),
+    REA01("Funcionários portugueses de missão diplomática no estrangeiro e seus familiares que os acompanhem"),
 
     REA02(
-            "Cidadãos portugueses bolseiros no estrangeiro, funcionários públicos em missão oficial no estrangeiro ou funcionários portugueses da CE e seus familiares que os acompanham"),
+            "Cidadãos portugueses bolseiros ou equiparados, do governo português no estrangeiro, funcionários públicos em missão oficial no estrangeiro ou funcionários portugueses da UE e seus familiares que os acompanhem"),
 
     REA03(
             "Oficiais do quadro permanente das Forças Armadas Portuguesas, no âmbito da satisfação de necessidades específicas de formação das Forças Armadas."),
 
     REA04(
-            "Funcionários estrangeiros de missão diplomática acreditada em Portugal e seus familiares aqui residentes em regime de reciprocidade"),
+            "Funcionários estrangeiros de missão diplomática acreditada em Portugal e seus familiares aqui residentes, em regime de reciprocidade"),
 
     REA05(
-            "Estudantes nacionais dos países africanos de expressão portuguesa, bolseiros do Governo Português, da Fundação Calouste Gulbenkian e ao abrigo de convenções com a CE, com frequência de ensino superior"),
+            "Estudantes nacionais dos países africanos de expressão portuguesa, bolseiros do Governo Português, dos Governos respectivos, da Fundação Calouste Gulbenkian, ao abrigo de convenções com a UE, com frequência de ensino superior"),
 
     REA06(
-            "Estudantes nacionais dos países africanos de expressão portuguesa, bolseiros do Governo Português, da Fundação Calouste Gulbenkian e ao abrigo de convenções com a CE, com o 12º ano de escolaridade português"),
+            "Estudantes nacionais dos países africanos de expressão portuguesa, bolseiros do Governo Português, dos Governos respectivos, da Fundação Calouste Gulbenkian, ao abrigo de convenções com a UE, com o 12º ano de escolaridade do ensino secundário português ou equivalente"),
 
-    REA07("Atletas de Alta Competição"),
+    REA07("Praticantes desportivos de alto rendimento"),
 
     REA08(
             "Naturais e filhos de naturais de territórios sob administração portuguesa, mas temporariamente ocupados por Forças Armadas e Estados Estrangeiros"),
 
     REA09(
             "Estudantes nacionais da República de Angola, não bolseiros e que não tenham residido em território português durante a aquisição da habilitação precedente ao 12º ano de escolaridade"),
+    REA10("Naturais e filhos de naturais do território de Timor Leste"),
 
     VAG01(
             "Vagas Adicionais - Vagas que são necessárias criar por erros de serviços do Ministério da Educação (Direcção Geral de Acesso ao Ensino Superior)"),
