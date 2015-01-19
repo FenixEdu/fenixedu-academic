a5d9ed221e31eeb317344e434d5d2af3bddad74c
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-01-19 16:13:17 +0000

    Marks publishing signal should now send all the relevant information

--- src/main/java/org/fenixedu/academic/service/services/teacher/PublishMarks.java
+++ src/main/java/org/fenixedu/academic/service/services/teacher/PublishMarks.java
@@ -19,12 +19,12 @@
 package org.fenixedu.academic.service.services.teacher;
 
 import org.fenixedu.academic.domain.Evaluation;
+import org.fenixedu.academic.domain.ExecutionCourse;
 import org.fenixedu.academic.domain.Mark;
 import org.fenixedu.academic.service.filter.ExecutionCourseLecturingTeacherAuthorizationFilter;
 import org.fenixedu.academic.service.services.ExcepcaoInexistente;
 import org.fenixedu.academic.service.services.exceptions.FenixServiceException;
 import org.fenixedu.academic.service.services.exceptions.NotAuthorizedException;
-import org.fenixedu.bennu.signals.DomainObjectEvent;
 import org.fenixedu.bennu.signals.Signal;
 
 import pt.ist.fenixframework.Atomic;
@@ -37,9 +37,10 @@ public class PublishMarks {
 
     public static final String MARKS_PUBLISHED_SIGNAL = "academic.PublishMarks.marks.published";
 
-    protected Object run(String evaluationCode, String publishmentMessage, String announcementTitle) throws ExcepcaoInexistente,
-            FenixServiceException {
+    protected Object run(String executionCourseCode, String evaluationCode, String publishmentMessage, String announcementTitle)
+            throws ExcepcaoInexistente, FenixServiceException {
 
+        final ExecutionCourse executionCourse = FenixFramework.getDomainObject(executionCourseCode);
         final Evaluation evaluation = FenixFramework.getDomainObject(evaluationCode);
 
         if (publishmentMessage == null || publishmentMessage.length() == 0) {
@@ -48,7 +49,7 @@ public class PublishMarks {
             evaluation.setPublishmentMessage(publishmentMessage);
         }
 
-        Signal.emit(MARKS_PUBLISHED_SIGNAL, new DomainObjectEvent<Evaluation>(evaluation));
+        Signal.emit(MARKS_PUBLISHED_SIGNAL, new MarkPublishingBean(executionCourse, evaluation, announcementTitle));
 
         for (Mark mark : evaluation.getMarksSet()) {
             if (!mark.getMark().equals(mark.getPublishedMark())) {
@@ -60,6 +61,32 @@ public class PublishMarks {
         return Boolean.TRUE;
     }
 
+    public static class MarkPublishingBean {
+
+        private final ExecutionCourse course;
+        private final Evaluation evaluation;
+        private final String title;
+
+        public MarkPublishingBean(ExecutionCourse course, Evaluation evaluation, String title) {
+            this.course = course;
+            this.evaluation = evaluation;
+            this.title = title;
+        }
+
+        public ExecutionCourse getCourse() {
+            return course;
+        }
+
+        public Evaluation getEvaluation() {
+            return evaluation;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+    }
+
     // Service Invokers migrated from Berserk
 
     private static final PublishMarks serviceInstance = new PublishMarks();
@@ -68,7 +95,7 @@ public class PublishMarks {
     public static Object runPublishMarks(String executionCourseCode, String evaluationCode, String publishmentMessage,
             Boolean sendSMS, String announcementTitle) throws ExcepcaoInexistente, FenixServiceException, NotAuthorizedException {
         ExecutionCourseLecturingTeacherAuthorizationFilter.instance.execute(executionCourseCode);
-        return serviceInstance.run(evaluationCode, publishmentMessage, announcementTitle);
+        return serviceInstance.run(executionCourseCode, evaluationCode, publishmentMessage, announcementTitle);
     }
 
 }
