13df01801569014d99d7e4d2ec4be37b2efbe05e
Pedro Santos <pedro.san7os@gmail.com>
2015-01-23 22:01:00 +0000

    Formatted source code and updated license headers

--- src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/ManageFinalDegreeWorkDispatchAction.java
+++ src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/ManageFinalDegreeWorkDispatchAction.java
@@ -1,20 +1,20 @@
 /**
  * Copyright © 2002 Instituto Superior Técnico
  *
- * This file is part of FenixEdu Core.
+ * This file is part of FenixEdu Academic.
  *
- * FenixEdu Core is free software: you can redistribute it and/or modify
+ * FenixEdu Academic is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FenixEdu Core is distributed in the hope that it will be useful,
+ * FenixEdu Academic is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with FenixEdu Core.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FenixEdu Academic.  If not, see <http://www.gnu.org/licenses/>.
  */
 /*
  * Created on 2004/03/09
@@ -54,9 +54,9 @@ import edu.emory.mathcs.backport.java.util.Collections;
 @Mapping(path = "/manageFinalDegreeWork", module = "coordinator", formBean = "finalDegreeWorkCandidacyRequirements",
         functionality = DegreeCoordinatorIndex.class)
 @Forwards({
-    @Forward(name = "show-final-degree-work-info", path = "/coordinator/finalDegreeWork/showFinalDegreeWorkInfo.jsp"),
-    @Forward(name = "show-choose-execution-degree-page",
-    path = "/coordinator/finalDegreeWork/showFinalDegreeChooseExecutionDegree_bd.jsp") })
+        @Forward(name = "show-final-degree-work-info", path = "/coordinator/finalDegreeWork/showFinalDegreeWorkInfo.jsp"),
+        @Forward(name = "show-choose-execution-degree-page",
+                path = "/coordinator/finalDegreeWork/showFinalDegreeChooseExecutionDegree_bd.jsp") })
 public class ManageFinalDegreeWorkDispatchAction extends FenixDispatchAction {
 
     @Mapping(path = "/finalDegreeWorkProposal", module = "coordinator",
--- src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/thesis/ManageThesisDA.java
+++ src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/thesis/ManageThesisDA.java
@@ -64,20 +64,20 @@ import pt.ist.fenixframework.FenixFramework;
 
 @Mapping(module = "coordinator", path = "/manageThesis", functionality = DegreeCoordinatorIndex.class)
 @Forwards({ @Forward(name = "search-student", path = "/coordinator/thesis/searchStudent.jsp"),
-    @Forward(name = "review-proposal", path = "/coordinator/thesis/reviewProposal.jsp"),
-    @Forward(name = "change-information", path = "/coordinator/thesis/changeInformation.jsp"),
-    @Forward(name = "editParticipant", path = "/coordinator/thesis/editParticipant.jsp"),
-    @Forward(name = "view-submitted", path = "/coordinator/thesis/viewSubmitted.jsp"),
-    @Forward(name = "change-information-with-docs", path = "/coordinator/thesis/changeInformationWithDocs.jsp"),
-    @Forward(name = "viewOperationsThesis", path = "/coordinator/thesis/viewOperationsThesis.jsp"),
-    @Forward(name = "select-unit", path = "/coordinator/thesis/selectUnit.jsp"),
-    @Forward(name = "view-confirmed", path = "/coordinator/thesis/viewConfirmed.jsp"),
-    @Forward(name = "list-thesis", path = "/coordinator/thesis/listThesis.jsp"),
-    @Forward(name = "select-person", path = "/coordinator/thesis/selectPerson.jsp"),
-    @Forward(name = "view-approved", path = "/coordinator/thesis/viewApproved.jsp"),
-    @Forward(name = "view-evaluated", path = "/coordinator/thesis/viewEvaluated.jsp"),
-    @Forward(name = "collect-basic-information", path = "/coordinator/thesis/collectBasicInformation.jsp"),
-    @Forward(name = "edit-thesis", path = "/coordinator/thesis/editThesis.jsp") })
+        @Forward(name = "review-proposal", path = "/coordinator/thesis/reviewProposal.jsp"),
+        @Forward(name = "change-information", path = "/coordinator/thesis/changeInformation.jsp"),
+        @Forward(name = "editParticipant", path = "/coordinator/thesis/editParticipant.jsp"),
+        @Forward(name = "view-submitted", path = "/coordinator/thesis/viewSubmitted.jsp"),
+        @Forward(name = "change-information-with-docs", path = "/coordinator/thesis/changeInformationWithDocs.jsp"),
+        @Forward(name = "viewOperationsThesis", path = "/coordinator/thesis/viewOperationsThesis.jsp"),
+        @Forward(name = "select-unit", path = "/coordinator/thesis/selectUnit.jsp"),
+        @Forward(name = "view-confirmed", path = "/coordinator/thesis/viewConfirmed.jsp"),
+        @Forward(name = "list-thesis", path = "/coordinator/thesis/listThesis.jsp"),
+        @Forward(name = "select-person", path = "/coordinator/thesis/selectPerson.jsp"),
+        @Forward(name = "view-approved", path = "/coordinator/thesis/viewApproved.jsp"),
+        @Forward(name = "view-evaluated", path = "/coordinator/thesis/viewEvaluated.jsp"),
+        @Forward(name = "collect-basic-information", path = "/coordinator/thesis/collectBasicInformation.jsp"),
+        @Forward(name = "edit-thesis", path = "/coordinator/thesis/editThesis.jsp") })
 public class ManageThesisDA extends AbstractManageThesisDA {
 
     @Override
