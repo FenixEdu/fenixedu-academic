3a19bcefc613231c997f5ddba52fcf035db47ece
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-04-16 17:29:17 +0100

    Moved stray Json Adapters to the proper package

--- src/main/java/org/fenixedu/academic/json/adapters/DegreeCurricularPlanJsonAdapter.java
+++ src/main/java/org/fenixedu/academic/json/adapters/DegreeCurricularPlanJsonAdapter.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with FenixEdu Academic.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.fenixedu.academic.core.json.adapter;
+package org.fenixedu.academic.json.adapters;
 
 import org.fenixedu.academic.domain.DegreeCurricularPlan;
 import org.fenixedu.bennu.core.annotation.DefaultJsonAdapter;
