c67948593c37d1a7b5a0fd7708b1c95943d27d35
Luís Egídio <luis.egidio@gmail.com>
2017-06-08 16:15:12 +0100

    Minor Change: enchancing curricular structure API

--- src/main/java/org/fenixedu/academic/domain/organizationalStructure/Accountability.java
+++ src/main/java/org/fenixedu/academic/domain/organizationalStructure/Accountability.java
@@ -35,10 +35,10 @@ public class Accountability extends Accountability_Base {
 
     public Accountability(Party parentParty, Party childParty, AccountabilityType accountabilityType) {
         this();
-        setParentParty(parentParty);
-        setChildParty(childParty);
         setAccountabilityType(accountabilityType);
         setBeginDate(new YearMonthDay());
+        setParentParty(parentParty);
+        setChildParty(childParty);
     }
 
     public void delete() {
