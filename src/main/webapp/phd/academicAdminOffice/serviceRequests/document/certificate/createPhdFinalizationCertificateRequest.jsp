gil-l/fenix

Enable repeating document requests
The affected documents are the diplomas and finalization certificates.
Documents now have registry codes since their creation. In some cases this code may be overriden later.
The creation order and processes between these documents where mostly preserved but are now done in relation to their registry codes.
For this to work, creation interfaces now have registry code fields to link required pre-condition documents.

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/certificate/createPhdFinalizationCertificateRequest.jsp
@@ -36,6 +36,14 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${code}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="true"/>
+			</fr:slot>
 			<fr:slot name="requestDate" required="true" />
 			<fr:slot name="urgentRequest" />
 			<fr:slot name="freeProcessed" />			

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/diploma/createDiplomaRequest.jsp
@@ -36,6 +36,17 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${code}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="false"/>
+				<fr:property name="key" value="true"/>
+				<fr:property name="defaultText" value="label.documentRequestsManagement.searchDocumentRequests.generate.code"/>
+				<fr:property name="bundle" value="ACADEMIC_OFFICE_RESOURCES"/>
+			</fr:slot>
 			<fr:slot name="freeProcessed" />
 			<fr:slot name="requestDate" required="true" />
 		</fr:schema>

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/diploma/createRegistryDiplomaRequest.jsp
@@ -36,6 +36,17 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${code}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="false"/>
+				<fr:property name="key" value="true"/>
+				<fr:property name="defaultText" value="label.documentRequestsManagement.searchDocumentRequests.generate.code"/>
+				<fr:property name="bundle" value="ACADEMIC_OFFICE_RESOURCES"/>
+			</fr:slot>
 			<fr:slot name="requestDate" required="true" />
 			<fr:slot name="language" >
 				<fr:property name="includedValues" value="pt, en" />

FenixEdu/fenixedu-academic

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/certificate/createPhdFinalizationCertificateRequest.jsp
@@ -36,6 +36,14 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${description}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="true"/>
+			</fr:slot>
 			<fr:slot name="requestDate" required="true" />
 			<fr:slot name="urgentRequest" />
 			<fr:slot name="freeProcessed" />			

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/diploma/createDiplomaRequest.jsp
@@ -36,6 +36,17 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${description}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="false"/>
+				<fr:property name="key" value="true"/>
+				<fr:property name="defaultText" value="label.documentRequestsManagement.searchDocumentRequests.generate.code"/>
+				<fr:property name="bundle" value="ACADEMIC_OFFICE_RESOURCES"/>
+			</fr:slot>
 			<fr:slot name="freeProcessed" />
 			<fr:slot name="requestDate" required="true" />
 		</fr:schema>

src/main/webapp/phd/academicAdminOffice/serviceRequests/document/diploma/createRegistryDiplomaRequest.jsp
@@ -36,6 +36,17 @@
 				<fr:property name="providerClass" value="org.fenixedu.academic.domain.phd.serviceRequests.documentRequests.PhdDocumentRequestTypeProvider" />
 				<fr:property name="destination" value="postback" />
 			</fr:slot>
+			<fr:slot name="registryCode" key="label.documentRequestsManagement.searchDocumentRequests.registry.code" bundle="ACADEMIC_OFFICE_RESOURCES"
+					 layout="menu-select" validator="pt.ist.fenixWebFramework.renderers.validators.RequiredValidator">
+				<fr:property name="providerClass"
+							 value="org.fenixedu.academic.ui.renderers.providers.documentRequests.AssociatePhdRegistryCodesProvider"/>
+				<fr:property name="format" value="${description}"/>
+				<fr:property name="sortBy" value="code"/>
+				<fr:property name="nullOptionHidden" value="false"/>
+				<fr:property name="key" value="true"/>
+				<fr:property name="defaultText" value="label.documentRequestsManagement.searchDocumentRequests.generate.code"/>
+				<fr:property name="bundle" value="ACADEMIC_OFFICE_RESOURCES"/>
+			</fr:slot>
 			<fr:slot name="requestDate" required="true" />
 			<fr:slot name="language" >
 				<fr:property name="includedValues" value="pt, en" />