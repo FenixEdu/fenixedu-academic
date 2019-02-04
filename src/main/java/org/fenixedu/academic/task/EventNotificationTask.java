66a48f3b3ff0a0d1da09c1c8199977f2377fe169
Luis Cruz <luis.cruz@tecnico.pt>
2019-02-04 15:18:58 +0000

    Send notification messages in the users preferred locale FIST-718 #resolve

--- src/main/java/org/fenixedu/academic/task/EventNotificationTask.java
+++ src/main/java/org/fenixedu/academic/task/EventNotificationTask.java
@@ -2,7 +2,9 @@ package org.fenixedu.academic.task;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.fenixedu.academic.domain.Person;
@@ -10,9 +12,12 @@ import org.fenixedu.academic.domain.accounting.Event;
 import org.fenixedu.academic.domain.accounting.calculator.DebtInterestCalculator;
 import org.fenixedu.academic.domain.organizationalStructure.Party;
 import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.core.domain.UserProfile;
 import org.fenixedu.bennu.core.groups.Group;
 import org.fenixedu.bennu.scheduler.CronTask;
 import org.fenixedu.bennu.scheduler.annotation.Task;
+import org.fenixedu.commons.i18n.I18N;
 import org.fenixedu.messaging.core.domain.Message;
 import org.fenixedu.messaging.core.domain.MessageTemplate;
 import org.fenixedu.messaging.core.template.DeclareMessageTemplate;
@@ -75,8 +80,8 @@ public class EventNotificationTask extends CronTask {
         return params;
     }
 
-    private String createMessage(final String templateKey, final Event event, final String paramKey, final String paramValue) {
-        return MessageTemplate.get(templateKey).getCompiledTextBody(paramMap(event, paramKey, paramValue)).getContent();
+    private String createMessage(final String templateKey, final Event event, final String paramKey, final String paramValue, final Locale locale) {
+        return MessageTemplate.get(templateKey).getCompiledTextBody(paramMap(event, paramKey, paramValue)).getContent(locale);
     }
 
     @Override
@@ -91,36 +96,45 @@ public class EventNotificationTask extends CronTask {
     }
 
     private void process(final Party party, final DateTime now, final LocalDate afterTomorrow, final LocalDate yesterday) {
-        FenixFramework.atomic(() -> {
-            if (party.isPerson()) {
-                final Person person = (Person) party;
+        try {
+            FenixFramework.atomic(() -> {
+                if (party.isPerson()) {
+                    final Person person = (Person) party;
+                    final User user = person.getUser();
+                    if (user != null) {
+                        final Locale locale = getLocale(person);
 
-                final String message = person.getEventsSet().stream()
-                    .flatMap(e -> generateNotifications(e, now, afterTomorrow, yesterday))
-                    .reduce("", String::concat);
-                if (!message.isEmpty()) {
-                    Message.fromSystem()
-                        .to(Group.users(person.getUser()))
-                        .template("event.notifications")
-                        .parameter("notificationMessage", message)
-                        .and()
-                        .wrapped()
-                        .send();
+                        final String message = person.getEventsSet().stream()
+                                .flatMap(e -> generateNotifications(e, now, afterTomorrow, yesterday, locale))
+                                .reduce("", String::concat);
+                        if (!message.isEmpty()) {
+                            Message.fromSystem()
+                                .preferredLocale(locale)
+                                .to(Group.users(person.getUser()))
+                                .template("event.notifications")
+                                .parameter("notificationMessage", message)
+                                .and()
+                                .wrapped()
+                                .send();
+                        }
+                    }
                 }
-            }
-        });
+            });
+        } catch (final Throwable t) {
+            taskLog("Unable to process party: %s - %s%n", party.getExternalId(), t.getMessage());
+        }
     }
 
-    private Stream<String> generateNotifications(final Event event, final DateTime now, final LocalDate afterTomorrow, final LocalDate yesterday) {
+    private Stream<String> generateNotifications(final Event event, final DateTime now, final LocalDate afterTomorrow, final LocalDate yesterday, final Locale locale) {
         try {
             final DebtInterestCalculator calculator = event.getDebtInterestCalculator(now);
             if (event.getWhenOccured().toLocalDate().equals(yesterday) && calculator.getDueAmount().signum() == 1) {
-                return Stream.of(createMessage("event.notification.new.event", event, "debtAmount", calculator.getDebtAmount().toPlainString()));
+                return Stream.of(createMessage("event.notification.new.event", event, "debtAmount", calculator.getDebtAmount().toPlainString(), locale));
             }
             
             final Stream<String> dueAmountNotifications = calculator.getDebtsOrderedByDueDate().stream()
                     .filter(d -> d.isOpen() && d.getDueDate().equals(afterTomorrow))
-                    .map(d -> createMessage("event.notification.due.event", event, "dueAmount", d.getAmount().toPlainString()));
+                    .map(d -> createMessage("event.notification.due.event", event, "dueAmount", d.getAmount().toPlainString(), locale));
             
             final BigDecimal totalUnusedAmount = calculator.getTotalUnusedAmount();
             if (totalUnusedAmount.signum() == 1) {
@@ -129,7 +143,7 @@ public class EventNotificationTask extends CronTask {
                         .max((dt1, dt2) -> dt1.compareTo(dt2))
                         .orElse(null);
                 if (lastPaymentDate != null && lastPaymentDate.toLocalDate().equals(yesterday)) {
-                    return Stream.concat(dueAmountNotifications, Stream.of(createMessage("event.notification.unused.amount", event, "totalUnusedAmount", totalUnusedAmount.toPlainString())));
+                    return Stream.concat(dueAmountNotifications, Stream.of(createMessage("event.notification.unused.amount", event, "totalUnusedAmount", totalUnusedAmount.toPlainString(), locale)));
                 }
             }
             return dueAmountNotifications;
@@ -137,6 +151,10 @@ public class EventNotificationTask extends CronTask {
             taskLog("Unable to process event: %s - %s%n", event.getExternalId(), t.getMessage());
             return Stream.empty();
         }
+    }
+
+    private static Locale getLocale(final Person person) {
+        return Optional.ofNullable(person.getProfile()).map(UserProfile::getPreferredLocale).orElseGet(I18N::getLocale);
     }
 
 }
