0073bf5613e713a5d1bdd29d77d89228bbd54ce9
AngelaFrancisca <angela.almeida@tecnico.ulisboa.pt>
2019-05-21 19:07:17 +0100

    Fixed NPE on coordinator portal in the applications downloads (#1514)

--- src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/candidacy/degreeTransfer/DegreeTransferCandidacyProcessDA.java
+++ src/main/java/org/fenixedu/academic/ui/struts/action/coordinator/candidacy/degreeTransfer/DegreeTransferCandidacyProcessDA.java
@@ -71,11 +71,12 @@ public class DegreeTransferCandidacyProcessDA extends
                     (interval instanceof ExecutionYear) ? (ExecutionYear) interval : ((ExecutionSemester) interval)
                             .getExecutionYear();
             final ExecutionDegree executionDegree = degreeCurricularPlan.getExecutionDegreeByYear(executionYear);
-
-            for (Coordinator coordinator : executionDegree.getCoordinatorsListSet()) {
-                if (coordinator.getPerson() == AccessControl.getPerson()) {
-                    returnExecutionIntervals.add(interval);
-                    break;
+            if (executionDegree != null) {
+                for (Coordinator coordinator : executionDegree.getCoordinatorsListSet()) {
+                    if (coordinator.getPerson() == AccessControl.getPerson()) {
+                        returnExecutionIntervals.add(interval);
+                        break;
+                    }
                 }
             }
         }
