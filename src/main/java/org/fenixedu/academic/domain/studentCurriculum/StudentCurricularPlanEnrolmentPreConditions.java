698a412a1eec4a044408b602b844b158e5d7c0c4
Ricardo Rodrigues <ricardo.rcr@gmail.com>
2019-09-04 20:37:00 +0100

    Don't allow enrollments for 1st cycle concluded registration ACDM-1669 #resolve

--- src/main/java/org/fenixedu/academic/domain/studentCurriculum/StudentCurricularPlanEnrolmentPreConditions.java
+++ src/main/java/org/fenixedu/academic/domain/studentCurriculum/StudentCurricularPlanEnrolmentPreConditions.java
@@ -99,7 +99,19 @@ public class StudentCurricularPlanEnrolmentPreConditions {
             return result;
         }
 
+        if (registrationIsOnly1stCycleAndConcluded(scp)) {
+            return createFalse("error.StudentCurricularPlan.cannot.enrol.registration.concluded");
+        }
+
         return checkDebts(scp);
+    }
+
+    private static boolean registrationIsOnly1stCycleAndConcluded(final StudentCurricularPlan scp) {
+        if (scp.getDegree().isFirstCycle()) {
+            final CycleCurriculumGroup firstCycle = scp.getFirstCycle();
+            return firstCycle != null && firstCycle.isConcluded();
+        }
+        return false;
     }
 
     /**
