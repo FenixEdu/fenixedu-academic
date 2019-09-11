6978106528b0a6c38420b8af14f281689fd0178a
Daniel Pires <daniel.pires@qub-it.com>
2019-09-11 10:46:57 +0000

    added group resolution to menus for permissions

---
+++ src/main/java/org/fenixedu/academic/domain/groups/PersistentPermissionGroup.java
@@ -0,0 +1,24 @@
+package org.fenixedu.academic.domain.groups;
+
+import java.util.Optional;
+
+import org.fenixedu.bennu.core.domain.Bennu;
+
+public class PersistentPermissionGroup extends PersistentPermissionGroup_Base {
+    
+    public PersistentPermissionGroup(String code) {
+        super();
+        this.setCode(code);
+    }
+
+    public static Optional<PersistentPermissionGroup> getInstance(String code) {
+        return Bennu.getInstance().getPermissionGroupSet().stream().filter(group -> group.getCode().equals(code))
+                .findAny();
+    }
+
+    @Override
+    public PermissionGroup toGroup() {
+        return new PermissionGroup(this.getCode());
+    }
+    
+}
