2413c6d0d36e6a184acfc0c0c932a377d2bc243c
Daniel Pires <daniel.pires@qub-it.com>
2019-09-25 13:46:33 +0000

    moved access control initializer to other module

--- src/main/java/org/fenixedu/academic/servlet/FenixInitializer.java
+++ src/main/java/org/fenixedu/academic/servlet/FenixInitializer.java
@@ -34,12 +34,10 @@ import org.fenixedu.academic.service.StudentWarningsDefaultCheckers;
 import org.fenixedu.academic.service.StudentWarningsService;
 import org.fenixedu.academic.ui.struts.action.externalServices.PhoneValidationUtils;
 import org.fenixedu.bennu.core.api.SystemResource;
-import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.rest.Healthcheck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.qubit.terra.qubAccessControl.domain.AccessControlProfile;
 import com.sun.mail.smtp.SMTPTransport;
 
 import pt.ist.fenixWebFramework.servlets.filters.contentRewrite.RequestChecksumFilter;
@@ -50,95 +48,88 @@ import pt.ist.fenixframework.Atomic.TxMode;
 @WebListener
 public class FenixInitializer implements ServletContextListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(FenixInitializer.class);
+	private static final Logger logger = LoggerFactory.getLogger(FenixInitializer.class);
 
-    @Override
-    @Atomic(mode = TxMode.READ)
-    public void contextInitialized(ServletContextEvent event) {
+	@Override
+	@Atomic(mode = TxMode.READ)
+	public void contextInitialized(ServletContextEvent event) {
 
-        Installation.ensureInstallation();
-        loadUnitNames();
-        startContactValidationServices();
+		Installation.ensureInstallation();
+		loadUnitNames();
+		startContactValidationServices();
 
-        registerChecksumFilterRules();
+		registerChecksumFilterRules();
 
-        registerHealthchecks();
-        registerDefaultStudentWarningCheckers();
+		registerHealthchecks();
+		registerDefaultStudentWarningCheckers();
 
-        addAdminToProfile();
-        
-    }
-    
-    @Atomic(mode = TxMode.WRITE)
-    private void addAdminToProfile() {
-        AccessControlProfile.manager().addFenixMember(User.findByUsername("qubit_admin"));
-    }
+	}
 
-    private void registerDefaultStudentWarningCheckers() {
-        StudentWarningsService.register(StudentWarningsDefaultCheckers.WARNING_VALID_ID_DOCUMENT);
-    }
+	private void registerDefaultStudentWarningCheckers() {
+		StudentWarningsService.register(StudentWarningsDefaultCheckers.WARNING_VALID_ID_DOCUMENT);
+	}
 
-    private void registerHealthchecks() {
-        SystemResource.registerHealthcheck(new Healthcheck() {
-            @Override
-            public String getName() {
-                return "SMTP";
-            }
+	private void registerHealthchecks() {
+		SystemResource.registerHealthcheck(new Healthcheck() {
+			@Override
+			public String getName() {
+				return "SMTP";
+			}
 
-            @Override
-            protected Result check() throws Exception {
-                final Properties properties = new Properties();
-                properties.put("mail.transport.protocol", "smtp");
-                Transport transport = Session.getInstance(properties).getTransport();
-                transport.connect(FenixEduAcademicConfiguration.getConfiguration().getMailSmtpHost(), null, null);
-                String response = ((SMTPTransport) transport).getLastServerResponse();
-                transport.close();
-                return Result.healthy("SMTP server returned response: " + response);
-            }
-        });
-    }
+			@Override
+			protected Result check() throws Exception {
+				final Properties properties = new Properties();
+				properties.put("mail.transport.protocol", "smtp");
+				Transport transport = Session.getInstance(properties).getTransport();
+				transport.connect(FenixEduAcademicConfiguration.getConfiguration().getMailSmtpHost(), null, null);
+				String response = ((SMTPTransport) transport).getLastServerResponse();
+				transport.close();
+				return Result.healthy("SMTP server returned response: " + response);
+			}
+		});
+	}
 
-    @Override
-    public void contextDestroyed(ServletContextEvent arg0) {
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
 
-    }
+	}
 
-    private void startContactValidationServices() {
-        PhoneValidationUtils.getInstance();
-    }
+	private void startContactValidationServices() {
+		PhoneValidationUtils.getInstance();
+	}
 
-    private void loadUnitNames() {
-        long start = System.currentTimeMillis();
-        UnitNamePart.find("...PlaceANonExistingUnitNameHere...");
-        long end = System.currentTimeMillis();
-        logger.debug("Load of all unit names took: " + (end - start) + "ms.");
-    }
+	private void loadUnitNames() {
+		long start = System.currentTimeMillis();
+		UnitNamePart.find("...PlaceANonExistingUnitNameHere...");
+		long end = System.currentTimeMillis();
+		logger.debug("Load of all unit names took: " + (end - start) + "ms.");
+	}
 
-    private void registerChecksumFilterRules() {
-        RequestChecksumFilter.registerFilterRule(new ChecksumPredicate() {
-            @Override
-            public boolean shouldFilter(HttpServletRequest request) {
-                final String uri = request.getRequestURI().substring(request.getContextPath().length());
-                if (uri.indexOf("home.do") >= 0) {
-                    return false;
-                }
-                if (uri.indexOf("/student/fillInquiries.do") >= 0) {
-                    return false;
-                }
-                if ((uri.indexOf("/teacher/executionCourseForumManagement.do") >= 0
-                        || uri.indexOf("/student/viewExecutionCourseForuns.do") >= 0)
-                        && request.getQueryString().indexOf("method=viewThread") >= 0) {
-                    return false;
-                }
-                if (uri.indexOf("notAuthorized.do") >= 0) {
-                    return false;
-                }
-                return (uri.indexOf("external/") == -1) && (uri.indexOf("login.do") == -1) && (uri.indexOf("loginCAS.do") == -1)
-                        && (uri.indexOf("logoff.do") == -1) && (uri.indexOf("publico/") == -1)
-                        && (uri.indexOf("siteMap.do") == -1);
-            }
+	private void registerChecksumFilterRules() {
+		RequestChecksumFilter.registerFilterRule(new ChecksumPredicate() {
+			@Override
+			public boolean shouldFilter(HttpServletRequest request) {
+				final String uri = request.getRequestURI().substring(request.getContextPath().length());
+				if (uri.indexOf("home.do") >= 0) {
+					return false;
+				}
+				if (uri.indexOf("/student/fillInquiries.do") >= 0) {
+					return false;
+				}
+				if ((uri.indexOf("/teacher/executionCourseForumManagement.do") >= 0
+						|| uri.indexOf("/student/viewExecutionCourseForuns.do") >= 0)
+						&& request.getQueryString().indexOf("method=viewThread") >= 0) {
+					return false;
+				}
+				if (uri.indexOf("notAuthorized.do") >= 0) {
+					return false;
+				}
+				return (uri.indexOf("external/") == -1) && (uri.indexOf("login.do") == -1)
+						&& (uri.indexOf("loginCAS.do") == -1) && (uri.indexOf("logoff.do") == -1)
+						&& (uri.indexOf("publico/") == -1) && (uri.indexOf("siteMap.do") == -1);
+			}
 
-        });
-    }
+		});
+	}
 
 }
