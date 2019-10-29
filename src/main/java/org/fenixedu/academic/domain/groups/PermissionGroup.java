412b1af1bbbbd36cc840e99eeb1761058ce8c675
Daniel Pires <daniel.pires@qub-it.com>
2019-10-29 16:39:00 +0000

    change due permission cache

--- src/main/java/org/fenixedu/academic/domain/groups/PermissionGroup.java
+++ src/main/java/org/fenixedu/academic/domain/groups/PermissionGroup.java
@@ -2,12 +2,10 @@ package org.fenixedu.academic.domain.groups;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang.StringUtils;
-import org.fenixedu.academic.domain.degree.DegreeType;
 import org.fenixedu.bennu.core.annotation.GroupArgument;
 import org.fenixedu.bennu.core.annotation.GroupOperator;
 import org.fenixedu.bennu.core.domain.User;
@@ -39,31 +37,31 @@ public class PermissionGroup extends CustomGroup {
         }
         return false;
     }
-    
-    public Stream<User> getDirectMembers(){
+
+    public Stream<User> getDirectMembers() {
         Set<User> members = new HashSet<>();
-        AccessControlPermission.findByCode(code).getProfileSet().stream().forEach(profile -> members.addAll(profile.getFenixMemberSet()));
+        AccessControlPermission.findByCode(code).getProfileSet().stream()
+                .forEach(profile -> members.addAll(profile.getFenixMemberSet()));
         return members.stream();
-        
+
     }
 
     @Override
     public Stream<User> getMembers() {
-         
+
         Set<AccessControlProfile> profiles = AccessControlPermission.findByCode(code).getProfileSet();
         Set<User> members = new HashSet<>();
 
-        
-        profiles.forEach(profile ->{
-           
+        profiles.forEach(profile -> {
+
             if (!(profile.getCustomExpression() == null || StringUtils.isEmpty(profile.getCustomExpression()))) {
                 members.addAll(Group.parse(profile.getCustomExpression()).getMembers().collect(Collectors.toSet()));
             }
-            
+
             members.addAll(profile.getFenixMemberSet());
             profile.findAllParents().forEach(parent -> members.addAll(parent.getFenixMemberSet()));
         });
-        
+
         return members.stream();
     }
 
@@ -90,7 +88,9 @@ public class PermissionGroup extends CustomGroup {
             return false;
         }
 
-        return getMembers().collect(Collectors.toSet()).contains(user);
+        return PermissionService.hasAccess(code, user);
+//
+//        return getMembers().collect(Collectors.toSet()).contains(user);
     }
 
     @Override
@@ -101,9 +101,7 @@ public class PermissionGroup extends CustomGroup {
     @Override
     @pt.ist.fenixframework.Atomic
     public PersistentPermissionGroup toPersistentGroup() {
-        return PersistentPermissionGroup.getInstance(this.code)
-                .orElseGet(() -> new PersistentPermissionGroup(this.code));
+        return PersistentPermissionGroup.getInstance(this.code).orElseGet(() -> new PersistentPermissionGroup(this.code));
     }
-    
-    
+
 }
