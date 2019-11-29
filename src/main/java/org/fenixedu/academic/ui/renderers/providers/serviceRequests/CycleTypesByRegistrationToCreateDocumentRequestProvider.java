ccb2dcc38a2134d8b69183328485e1d13ebedf2d
Ricardo Rodrigues <ricardo.rcr@gmail.com>
2019-11-29 15:20:45 +0000

    Added all cycles from all SCP ACDM-1695 #resolve

--- src/main/java/org/fenixedu/academic/ui/renderers/providers/serviceRequests/CycleTypesByRegistrationToCreateDocumentRequestProvider.java
+++ src/main/java/org/fenixedu/academic/ui/renderers/providers/serviceRequests/CycleTypesByRegistrationToCreateDocumentRequestProvider.java
@@ -1,34 +1,31 @@
 /**
  * Copyright © 2002 Instituto Superior Técnico
- *
+ * <p>
  * This file is part of FenixEdu Academic.
- *
+ * <p>
  * FenixEdu Academic is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * FenixEdu Academic is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with FenixEdu Academic.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.fenixedu.academic.ui.renderers.providers.serviceRequests;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.fenixedu.academic.domain.StudentCurricularPlan;
 import org.fenixedu.academic.domain.degreeStructure.CycleType;
-import org.fenixedu.academic.domain.studentCurriculum.CycleCurriculumGroup;
 import org.fenixedu.academic.dto.serviceRequests.DocumentRequestCreateBean;
-
 import pt.ist.fenixWebFramework.renderers.DataProvider;
 import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
 import pt.ist.fenixWebFramework.renderers.converters.EnumConverter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class CycleTypesByRegistrationToCreateDocumentRequestProvider implements DataProvider {
 
@@ -40,13 +37,12 @@ public class CycleTypesByRegistrationToCreateDocumentRequestProvider implements 
     @Override
     public Object provide(final Object source, final Object currentValue) {
         final DocumentRequestCreateBean documentRequestCreateBean = (DocumentRequestCreateBean) source;
-        final StudentCurricularPlan scp = documentRequestCreateBean.getRegistration().getLastStudentCurricularPlan();
 
-        final List<CycleType> result = new ArrayList<CycleType>();
-        for (final CycleCurriculumGroup group : scp.getInternalCycleCurriculumGrops()) {
-            result.add(group.getCycleType());
-        }
+        final Set<CycleType> result = new HashSet<>();
+        documentRequestCreateBean.getRegistration().getStudentCurricularPlanStream()
+                .flatMap(scp -> scp.getInternalCycleCurriculumGrops().stream())
+                .forEach(gr -> result.add(gr.getCycleType()));
+
         return result;
     }
-
 }
