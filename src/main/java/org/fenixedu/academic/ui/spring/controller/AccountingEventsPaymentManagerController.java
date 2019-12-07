2246877c282130be9f01d86a44580986409671e0
Inês Filipe <ines.filipe@tecnico.ulisboa.pt>
2019-12-07 23:05:37 +0000

    Add interface for custom payment plan creation ACDM-1696 #resolve

--- src/main/java/org/fenixedu/academic/ui/spring/controller/AccountingEventsPaymentManagerController.java
+++ src/main/java/org/fenixedu/academic/ui/spring/controller/AccountingEventsPaymentManagerController.java
@@ -1,35 +1,14 @@
 package org.fenixedu.academic.ui.spring.controller;
 
-import java.math.BigDecimal;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
-import java.util.UUID;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpSession;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.fenixedu.academic.domain.Person;
-import org.fenixedu.academic.domain.accounting.AccountingTransaction;
-import org.fenixedu.academic.domain.accounting.Discount;
-import org.fenixedu.academic.domain.accounting.Event;
-import org.fenixedu.academic.domain.accounting.Exemption;
-import org.fenixedu.academic.domain.accounting.PaymentMethod;
-import org.fenixedu.academic.domain.accounting.Refund;
+import org.fenixedu.academic.domain.accounting.*;
 import org.fenixedu.academic.domain.accounting.calculator.DebtInterestCalculator;
 import org.fenixedu.academic.domain.accounting.events.EventExemptionJustificationType;
 import org.fenixedu.academic.domain.exceptions.DomainException;
-import org.fenixedu.academic.dto.accounting.AnnulAccountingTransactionBean;
-import org.fenixedu.academic.dto.accounting.CreateExemptionBean;
+import org.fenixedu.academic.dto.accounting.*;
 import org.fenixedu.academic.dto.accounting.CreateExemptionBean.ExemptionType;
-import org.fenixedu.academic.dto.accounting.DepositAmountBean;
-import org.fenixedu.academic.dto.accounting.EntryDTO;
-import org.fenixedu.academic.dto.accounting.PaymentsManagementDTO;
 import org.fenixedu.academic.predicate.AcademicPredicates;
 import org.fenixedu.academic.predicate.AccessControl;
 import org.fenixedu.academic.service.services.accounting.AnnulAccountingTransaction;
@@ -43,22 +22,26 @@ import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
 import org.fenixedu.commons.i18n.I18N;
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import pt.ist.fenixframework.DomainObject;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Created by Sérgio Silva (hello@fenixedu.org).
@@ -406,6 +389,45 @@ public class AccountingEventsPaymentManagerController extends AccountingControll
         model.addAttribute("person", person);
         model.addAttribute("success", true);
         return view("events-multiple-payments-confirm");
+    }
+
+    @RequestMapping(value = "{event}/customPaymentPlan/create", method = RequestMethod.GET)
+    public String customPaymentPlan(@PathVariable Event event, User user, Model model) {
+        accessControlService.checkPaymentManager(event, user);
+        final Person person = event.getPerson();
+        DateTime exemptionDate = new DateTime();
+
+        model.addAttribute("person", person);
+        model.addAttribute("exemptionDate", exemptionDate);
+        model.addAttribute("totalAmount", event.getDebtInterestCalculator(exemptionDate.minusSeconds(2)).getDueAmount().toString());
+        return view("create-custom-payment-plan");
+    }
+
+    @RequestMapping(value = "{event}/customPaymentPlan/create", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> customPaymentPlanCreate(@PathVariable Event event, @RequestBody String dataJson, User user) {
+        accessControlService.checkPaymentManager(event, user);
+
+        try {
+            event.createCustomPaymentPlan(getExemptionDateFromJSON(dataJson), createMapFromJSON(dataJson));
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (DomainException de) {
+            return new ResponseEntity<>(de.getLocalizedMessage(), HttpStatus.PRECONDITION_FAILED);
+        }
+    }
+
+    private DateTime getExemptionDateFromJSON(String jsonString) {
+        JsonObject json = (JsonObject) new JsonParser().parse(jsonString);
+        return new DateTime(json.get("exemptionDate").getAsString());
+    }
+
+    private Map<LocalDate, Money> createMapFromJSON(String jsonString) {
+        JsonObject json = (JsonObject) new JsonParser().parse(jsonString);
+        JsonObject map = json.getAsJsonObject("map");
+        HashMap<LocalDate, Money> hashMap = new HashMap<>();
+
+        map.entrySet().forEach(e -> hashMap.put(new LocalDate(e.getKey()), new Money(e.getValue().getAsString())));
+        return hashMap;
     }
 
     @Override
