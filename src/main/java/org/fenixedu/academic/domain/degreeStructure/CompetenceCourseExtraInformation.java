08e9c46fe5cbb6fa1a4c9120faa446e334a72e9e
SusanaFernandes <susana.fernandes@ist.utl.pt>
2020-02-11 13:03:16 +0000

    Separate competence cource information (#1595)

---
+++ src/main/java/org/fenixedu/academic/domain/degreeStructure/CompetenceCourseExtraInformation.java
@@ -0,0 +1,31 @@
+package org.fenixedu.academic.domain.degreeStructure;
+
+public class CompetenceCourseExtraInformation extends CompetenceCourseExtraInformation_Base {
+
+    public CompetenceCourseExtraInformation(CompetenceCourseInformation competenceCourseInformation, String prerequisites,
+            String prerequisitesEn, String laboratorialComponent, String laboratorialComponentEn,
+            String programmingAndComputingComponent, String programmingAndComputingComponentEn, String crossCompetenceComponent,
+            String crossCompetenceComponentEn, String ethicalPrinciples, String ethicalPrinciplesEn) {
+        super();
+        setCompetenceCourseInformation(competenceCourseInformation);
+        edit(prerequisites, prerequisitesEn, laboratorialComponent, laboratorialComponentEn, programmingAndComputingComponent,
+                programmingAndComputingComponentEn, crossCompetenceComponent, crossCompetenceComponentEn, ethicalPrinciples,
+                ethicalPrinciplesEn);
+    }
+
+    public void edit(String prerequisites, String prerequisitesEn, String laboratorialComponent, String laboratorialComponentEn,
+            String programmingAndComputingComponent, String programmingAndComputingComponentEn, String crossCompetenceComponent,
+            String crossCompetenceComponentEn, String ethicalPrinciples, String ethicalPrinciplesEn) {
+        setPrerequisites(prerequisites);
+        setLaboratorialComponent(laboratorialComponent);
+        setProgrammingAndComputingComponent(programmingAndComputingComponent);
+        setCrossCompetenceComponent(crossCompetenceComponent);
+        setEthicalPrinciples(ethicalPrinciples);
+        setPrerequisitesEn(prerequisitesEn);
+        setLaboratorialComponentEn(laboratorialComponentEn);
+        setProgrammingAndComputingComponentEn(programmingAndComputingComponentEn);
+        setCrossCompetenceComponentEn(crossCompetenceComponentEn);
+        setEthicalPrinciplesEn(ethicalPrinciplesEn);
+    }
+
+}
---
+++ src/main/java/org/fenixedu/academic/domain/degreeStructure/CompetenceCourseExtraInformationChangeRequest.java
@@ -0,0 +1,32 @@
+package org.fenixedu.academic.domain.degreeStructure;
+
+public class CompetenceCourseExtraInformationChangeRequest extends CompetenceCourseExtraInformationChangeRequest_Base {
+
+    public CompetenceCourseExtraInformationChangeRequest(
+            CompetenceCourseInformationChangeRequest competenceCourseInformationChangeRequest, String prerequisites,
+            String prerequisitesEn, String laboratorialComponent, String laboratorialComponentEn,
+            String programmingAndComputingComponent, String programmingAndComputingComponentEn, String crossCompetenceComponent,
+            String crossCompetenceComponentEn, String ethicalPrinciples, String ethicalPrinciplesEn) {
+        super();
+        setCompetenceCourseInformationChangeRequest(competenceCourseInformationChangeRequest);
+        edit(prerequisites, prerequisitesEn, laboratorialComponent, laboratorialComponentEn, programmingAndComputingComponent,
+                programmingAndComputingComponentEn, crossCompetenceComponent, crossCompetenceComponentEn, ethicalPrinciples,
+                ethicalPrinciplesEn);
+    }
+
+    public void edit(String prerequisites, String prerequisitesEn, String laboratorialComponent, String laboratorialComponentEn,
+            String programmingAndComputingComponent, String programmingAndComputingComponentEn, String crossCompetenceComponent,
+            String crossCompetenceComponentEn, String ethicalPrinciples, String ethicalPrinciplesEn) {
+        setPrerequisites(prerequisites);
+        setPrerequisitesEn(prerequisitesEn);
+        setLaboratorialComponent(laboratorialComponent);
+        setLaboratorialComponentEn(laboratorialComponentEn);
+        setProgrammingAndComputingComponent(programmingAndComputingComponent);
+        setProgrammingAndComputingComponentEn(programmingAndComputingComponentEn);
+        setCrossCompetenceComponent(crossCompetenceComponent);
+        setCrossCompetenceComponentEn(crossCompetenceComponentEn);
+        setEthicalPrinciples(ethicalPrinciples);
+        setEthicalPrinciplesEn(ethicalPrinciplesEn);
+    }
+
+}
