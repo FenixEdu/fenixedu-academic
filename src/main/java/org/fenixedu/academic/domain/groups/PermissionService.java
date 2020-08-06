a9076692056d37b3c37b31ebbed638dc720c11c9
Daniel Pires <daniel.pires@qub-it.com>
2020-08-06 12:39:54 +0000

    fixed NPE in PermissionService

--- src/main/java/org/fenixedu/academic/domain/groups/PermissionService.java
+++ src/main/java/org/fenixedu/academic/domain/groups/PermissionService.java
@@ -41,7 +41,7 @@ public class PermissionService {
 
     public static <T extends DomainObject> Set<T> getObjects(AccessControlPermission permission, Class<T> clazz, User user) {
         return (Set<T>) profileProvider.apply(permission, user).stream()
-                .filter(p -> p.getFenixMemberSet().contains(user) && p.getObjectsClass().equals(clazz.getName()))
+                .filter(p -> p.getFenixMemberSet().contains(user) && clazz != null && clazz.getName().equals(p.getObjectsClass()))
                 .flatMap(p -> p.provideObjects().stream()).collect(Collectors.toSet());
     }
 
