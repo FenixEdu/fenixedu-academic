58b181b848444a94e248796aaf63a7a40f3d8fef
Luis Cruz <luis.cruz@tecnico.pt>
2020-12-20 19:18:55 +0000

    Add support for custom events ACDM-1819 #resolve

---
+++ src/main/java/org/fenixedu/academic/domain/accounting/CustomEvent.java
@@ -0,0 +1,41 @@
+package org.fenixedu.academic.domain.accounting;
+
+import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.Person;
+import org.fenixedu.academic.domain.organizationalStructure.Unit;
+import org.fenixedu.academic.util.Money;
+import org.joda.time.LocalDate;
+
+import java.util.Map;
+
+public class CustomEvent extends CustomEvent_Base {
+
+    public CustomEvent(final Person person, final Account account, final Map<LocalDate, Money> dueDateAmountMap,
+                       final JsonObject config) {
+        init(EventType.CUSTOM, person);
+        setDueDateAmountMap(new DueDateAmountMap(dueDateAmountMap));
+        setCustomToAccount(account);
+        setConfig(config.toString());
+    }
+
+    @Override
+    protected Account getFromAccount() {
+        return getPerson().getExternalAccount();
+    }
+
+    @Override
+    public Account getToAccount() {
+        return getCustomToAccount();
+    }
+
+    @Override
+    public PostingRule getPostingRule() {
+        return null;
+    }
+
+    @Override
+    public Unit getOwnerUnit() {
+        return null;
+    }
+
+}
