00f689eaf437e7a6901303f1b9606112cd0f4abd
Shezad Anavarali <shezad.anavarali@qub-it.com>
2022-10-06 19:19:57 +0100

    Refactor: Remove deprecated OccupationPeriodType values and management UI

--- src/main/java/org/fenixedu/academic/domain/OccupationPeriodType.java
+++ src/main/java/org/fenixedu/academic/domain/OccupationPeriodType.java
@@ -31,7 +31,7 @@ import pt.ist.fenixWebFramework.rendererExtensions.util.IPresentableEnum;
 @Deprecated
 public enum OccupationPeriodType implements IPresentableEnum {
 
-    LESSONS, EXAMS, GRADE_SUBMISSION, EXAMS_SPECIAL_SEASON, GRADE_SUBMISSION_SPECIAL_SEASON;
+    LESSONS;
 
     @Override
     public String getLocalizedName() {
