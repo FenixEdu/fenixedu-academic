db71ce320c9ba5cee0541c84c403ec54555f5057
Nadir Amin <nadir.amin@qub-it.com>
2022-10-26 10:59:03 +0100

    Refactor: Remove branch group subclasses

---
+++ src/main/java/org/fenixedu/academic/domain/studentCurriculum/BranchCurriculumGroup.java
@@ -0,0 +1,15 @@
+package org.fenixedu.academic.domain.studentCurriculum;
+
+/**
+ * @deprecated
+ * 
+ *
+ */
+@Deprecated(forRemoval = true)
+public class BranchCurriculumGroup extends BranchCurriculumGroup_Base {
+
+    protected BranchCurriculumGroup() {
+        super();
+    }
+
+}
